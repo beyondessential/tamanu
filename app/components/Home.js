2fa4dd3d03c03eab171f72b768f331383213e340
reactionic127 <richard.gill127@gmail.com>
2018-04-17 04:48:59 +0100

    removed useless files

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,7 +1,5 @@
 // @flow
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import styles from './Home.css';
 
 type Props = {};
 
@@ -11,10 +9,7 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div>
-        <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-        </div>
+        This is Home page
       </div>
     );
   }
--- app/main.dev.js
+++ app/main.dev.js
@@ -61,8 +61,10 @@ app.on('ready', async () => {
 
   mainWindow = new BrowserWindow({
     show: false,
-    width: 1024,
-    height: 728
+    // width: 1024,
+    // height: 728
+    width: 1400,
+    height: 900
   });
 
   mainWindow.loadURL(`file://${__dirname}/app.html`);
