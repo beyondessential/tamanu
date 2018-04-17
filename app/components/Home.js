9dea219b0d79b4a2a792b2d0428b1f1f6f8fd7d5
reactionic127 <richard.gill127@gmail.com>
2018-04-17 19:39:11 +0100

    added sidebar

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,7 +9,60 @@ export default class Home extends Component<Props> {
   render() {
     return (
       <div>
-        This is Home page
+        <div className="sidebar">
+          <div className="header">
+            <span>
+              Tamanu
+            </span>
+          </div>
+          <div className="scroll-container">
+            <div className="item">
+              <span>
+                Inventory
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Patients
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Scheduling
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Imaging
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Medication
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Labs
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Billing
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Incident
+              </span>
+            </div>
+            <div className="item">
+              <span>
+                Administration
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
