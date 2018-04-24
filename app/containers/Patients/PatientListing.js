ac009583210ad3c6174f3a2358d797bd236fca38
reactionic127 <richard.gill127@gmail.com>
2018-04-24 13:01:32 +0100

    bootstrap table is customized

--- app/containers/Patients/PatientListing.js
+++ app/containers/Patients/PatientListing.js
@@ -4,27 +4,62 @@ import { Link } from 'react-router-dom';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { fetchPatients } from '../../actions/patients';
 
+const headerSortingStyle = { backgroundColor: '#c8e6c9' };
+
 const columns = [{
   dataField: 'id',
-  text: 'id'
+  text: 'id',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  },
 }, {
   dataField: 'firstName',
-  text: 'First Name'
+  text: 'First Name',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }, {
   dataField: 'lastName',
-  text: 'Last Name'
+  text: 'Last Name',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }, {
   dataField: 'sex',
-  text: 'Sex'
+  text: 'Sex',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }, {
   dataField: 'birthday',
-  text: 'DOB'
+  text: 'DOB',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }, {
   dataField: 'patientStatus',
-  text: 'Status'
+  text: 'Status',
+  sort: true,
+  headerSortingStyle,
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }, {
   dataField: 'action',
-  text: 'Actions'
+  text: 'Actions',
+  headerStyle: {
+    backgroundColor: 'rgba(38, 54, 72, 0.3)'
+  }
 }];
 
 class PatientListing extends Component {
@@ -56,7 +91,12 @@ class PatientListing extends Component {
             </div>
             :
             <div>
-              <BootstrapTable keyField="id" data={patients} columns={columns} />
+              <BootstrapTable
+                keyField="id"
+                data={patients}
+                columns={columns}
+                defaultSortDirection="asc"
+              />
             </div>
           }
         </div>
