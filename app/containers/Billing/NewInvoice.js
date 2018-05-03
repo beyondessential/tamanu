f4f48378fb15fc3612261fdef5fd30fafa51c673
reactionic127 <richard.gill127@gmail.com>
2018-05-03 20:13:14 +0100

    added form field

--- app/containers/Billing/NewInvoice.js
+++ app/containers/Billing/NewInvoice.js
@@ -1,12 +1,33 @@
-// @flow
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import Select from 'react-select';
 
-type Props = {};
+import InputGroup from '../../components/InputGroup';
+import CustomDateInput from '../../components/CustomDateInput';
+import { visitOptions } from '../../constants';
 
-export default class NewInvoice extends Component<Props> {
-  props: Props;
+class NewInvoice extends Component {
+  state = {
+    billDate: moment(),
+    selectValue: '',
+  }
+
+  onChangeDate = (date) => {
+    this.setState({
+      billDate: date,
+    });
+  }
+
+  updateValue = (newValue) => {
+    this.setState({
+      selectValue: newValue,
+    });
+  }
 
   render() {
+    const { billDate } = this.state;
     return (
       <div>
         <div className="content">
@@ -23,8 +44,106 @@ export default class NewInvoice extends Component<Props> {
               </button>
             </div>
           </div>
+          <div className="detail">
+            <div className="tabs-container">
+              <Link to="/invoices" replace>
+                Billed
+              </Link>
+              <Link to="/invoices/draft" replace>
+                Drafts
+              </Link>
+              <Link to="/invoices/all" replace>
+                All Invoices
+              </Link>
+              <Link to="/invoices/paid" replace>
+                Paid
+              </Link>
+            </div>
+            <div className="invoice-form">
+              <div className="columns">
+                <div className="column is-4">
+                  <div className="column">
+                    <span>
+                      Bill Date
+                    </span>
+                    <DatePicker
+                      name="billDate"
+                      customInput={<CustomDateInput />}
+                      selected={billDate}
+                      onChange={this.onChangeDate}
+                      peekNextMonth
+                      showMonthDropdown
+                      showYearDropdown
+                      dropdownMode="select"
+                    />
+                  </div>
+                </div>
+                <div className="column is-4">
+                  <InputGroup
+                    name="patient"
+                    label="Patient"
+                    required
+                  />
+                </div>
+                <div className="column is-4">
+                  <div className="column">
+                    <span className="header">
+                      Visit
+                    </span>
+                    <Select
+                      id="state-select"
+                      ref={(ref) => { this.select = ref; }}
+                      onBlurResetsInput={false}
+                      onSelectResetsInput={false}
+                      options={visitOptions}
+                      simpleValue
+                      clearable
+                      name="selected-state"
+                      disabled={this.state.disabled}
+                      value={this.state.selectValue}
+                      onChange={this.updateValue}
+                      rtl={this.state.rtl}
+                      searchable={this.state.searchable}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="columns">
+                <div className="column is-4">
+                  <InputGroup
+                    name="patient"
+                    label="External Invoice #"
+                  />
+                </div>
+                <div className="column is-4">
+                  <div className="column">
+                    <span className="header">
+                      Payment Profile
+                    </span>
+                    <Select
+                      id="state-select"
+                      ref={(ref) => { this.select = ref; }}
+                      onBlurResetsInput={false}
+                      onSelectResetsInput={false}
+                      options={visitOptions}
+                      simpleValue
+                      clearable
+                      name="selected-state"
+                      disabled={this.state.disabled}
+                      value={this.state.selectValue}
+                      onChange={this.updateValue}
+                      rtl={this.state.rtl}
+                      searchable={this.state.searchable}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+export default NewInvoice;
