1238772d2b27b543bdb3613e657ede53949ddc3b
forward88 <lijin1991@aol.com>
2018-05-08 16:48:18 +0100

    input form added in address field

--- app/containers/Administration/AddressFields.js
+++ app/containers/Administration/AddressFields.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import InputGroup from '../../components/InputGroup';
+
 class AddressFields extends Component {
   render() {
     return (
@@ -11,7 +13,35 @@ class AddressFields extends Component {
         </div>
         <div className="create-container">
           <div className="form">
-            AddressFields
+            <div className="columns">
+              <div className="column">
+                <InputGroup
+                  name="address1"
+                  label="Address 1 Label"
+                />
+                <InputGroup
+                  name="address2"
+                  label="Address 2 Label"
+                />
+                <InputGroup
+                  name="address3"
+                  label="Address 3 Label"
+                />
+                <InputGroup
+                  name="address4"
+                  label="Address 4 Label"
+                />
+              </div>
+            </div>
+            <div className="columns">
+              <div className="column">
+                <div className="column">
+                  <div className="has-text-right">
+                    <button className="button is-primary" type="submit">Update</button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
