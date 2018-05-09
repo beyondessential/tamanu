428acba33f4a3b139fa758e16b924b37034a9dc5
reactionic127 <richard.gill127@gmail.com>
2018-05-09 08:53:58 +0100

    today appointment ui is refined

--- app/containers/Scheduling/TodayAppointment.js
+++ app/containers/Scheduling/TodayAppointment.js
@@ -1,12 +1,17 @@
-// @flow
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import { fetchMedications } from '../../actions/medications';
+import { medicationColumns } from '../../constants';
 
-type Props = {};
-
-export default class TodayAppointment extends Component<Props> {
-  props: Props;
+class TodayAppointment extends Component {
+  componentDidMount() {
+    this.props.fetchMedications();
+  }
 
   render() {
+    const { medications } = this.props;
     return (
       <div className="content">
         <div className="view-top-bar">
@@ -14,12 +19,32 @@ export default class TodayAppointment extends Component<Props> {
             Today's Appointments
           </span>
           <div className="view-action-buttons">
-            <button>
+            <Link to="/appointments/edit/new">
               + New Appointment
-            </button>
+            </Link>
           </div>
+        </div>
+        <div className="detail">
+          <BootstrapTable
+            keyField="id"
+            data={medications}
+            columns={medicationColumns}
+            defaultSortDirection="asc"
+          />
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    medications: state.medications.medications
+  };
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchMedications: () => dispatch(fetchMedications()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodayAppointment);
