4075e7d2f8067794941fc5a116cb06c45e587e52
Unknown <salmanamjad89@gmail.com>
2018-05-21 06:46:44 +1000

    create database views

--- app/utils/dbHelper.js
+++ app/utils/dbHelper.js
@@ -1,18 +1,20 @@
-import Promise from 'bluebird';
-import nano from 'nano';
 import PouchDB from 'pouchdb';
 import Backbone from 'backbone';
 import BackbonePouch from 'backbone-pouch';
+import createPouchViews from '../utils/pouch-views';
 
 const dbHost = 'localhost';
 const dbPort = 5984;
 const dbUser = 'couchadmin';
 const dbPassword = 'test';
 let configDB = `http://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/config`;
-let patientDB = `http://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/patient`;
+let patientDB = `http://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/main`;
 
 configDB = new PouchDB(configDB);
 patientDB = new PouchDB(patientDB);
+
+// Generate views
+createPouchViews(patientDB);
 
 Backbone.sync = BackbonePouch.sync({
   db: patientDB,
@@ -21,13 +23,11 @@ Backbone.sync = BackbonePouch.sync({
     allDocs: {
       include_docs: true,
       limit: 10
-    },
-    query: {
-      include_docs: true,
-      limit: 10
     }
   }
 });
+
+// Backbone.sync = adaptor;
 Backbone.Model.prototype.idAttribute = '_id';
 
 module.exports = { configDB, patientDB };
