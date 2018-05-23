6a6c19668abb2ad04ca61700f541ad734743b1ee
reactionic127 <richard.gill127@gmail.com>
2018-05-23 08:34:57 +0100

    history pane is implemented

--- app/containers/Patients/EditPatient/History.js
+++ app/containers/Patients/EditPatient/History.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class History extends Component {
   render() {
     return (
       <div>
-        No history available.
+        <div className="column has-text-right">
+          <button className="button is-primary" type="submit">New Note</button>
+        </div>
+        <div className="column">
+          <div className="history-pane">
+            <Link className="button admission" to="/patients">5/14/2018 - 5/16/2018 Admission</Link>
+          </div>
+        </div>
       </div>
     );
   }
