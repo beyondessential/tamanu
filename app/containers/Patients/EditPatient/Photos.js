7e79477b5180dc1e054d1cd0d0237e17d1c62ef3
reactionic127 <richard.gill127@gmail.com>
2018-05-24 09:12:31 +0100

    added edit/delete modal for photo

--- app/containers/Patients/EditPatient/Photos.js
+++ app/containers/Patients/EditPatient/Photos.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import NewPhotoModal from '../components/NewPhotoModal';
+import EditPhotoModal from '../components/EditPhotoModal';
+import DeletePhotoModal from '../components/DeletePhotoModal';
 
 class Photos extends Component {
   state = {
-    photoModalVisible: false,
-    photos: []
-  }
-
-  onCloseModal = () => {
-    this.setState({ photoModalVisible: false });
+    photos: [],
+    addPhotoModalVisible: false,
+    editPhotoModalVisible: false,
+    deletePhotoModalVisible: false,
   }
 
   addPhoto = (file) => {
-    this.setState({ photoModalVisible: false });
+    this.setState({ addPhotoModalVisible: false });
     const { photos } = this.state;
     const selectedFiles = photos;
     selectedFiles.push(file);
@@ -21,12 +21,37 @@ class Photos extends Component {
     });
   }
 
+  editPhoto = () => {
+    this.setState({ editPhotoModalVisible: false });
+  }
+
+  deletePhoto = () => {
+    this.setState({ deletePhotoModalVisible: false });
+  }
+
+  onCloseAddModal = () => {
+    this.setState({ addPhotoModalVisible: false });
+  }
+
+  onCloseEditModal = () => {
+    this.setState({ editPhotoModalVisible: false });
+  }
+
+  onCloseDeleteModal = () => {
+    this.setState({ deletePhotoModalVisible: false });
+  }
+
   render() {
-    const { photoModalVisible, photos } = this.state;
+    const {
+      photos,
+      addPhotoModalVisible,
+      editPhotoModalVisible,
+      deletePhotoModalVisible
+    } = this.state;
     return (
       <div>
         <div className="column has-text-right">
-          <button className="button is-primary" onClick={() => this.setState({ photoModalVisible: true })}>+ New Photo</button>
+          <button className="button is-primary" onClick={() => this.setState({ addPhotoModalVisible: true })}>+ New Photo</button>
         </div>
         <div className="column">
           <div className="columns is-multiline">
@@ -34,13 +59,15 @@ class Photos extends Component {
               <div key={index} className="column is-3">
                 <div className="card">
                   <div className="column">
-                    <img src={photo.preview} alt="preview" />
-                    <span>
-                      {photo.caption}
-                    </span>
+                    <div onClick={() => this.setState({ editPhotoModalVisible: true })}>
+                      <img src={photo.preview} alt="preview" />
+                      <span>
+                        {photo.caption}
+                      </span>
+                    </div>
                     <div className="btn-group">
-                      <button className="button is-small">Edit</button>
-                      <button className="button is-danger is-small">Delete</button>
+                      <button className="button is-small" onClick={() => this.setState({ editPhotoModalVisible: true })}>Edit</button>
+                      <button className="button is-danger is-small" onClick={() => this.setState({ deletePhotoModalVisible: true })}>Delete</button>
                     </div>
                   </div>
                 </div>
@@ -50,9 +77,21 @@ class Photos extends Component {
           </div>
         </div>
         <NewPhotoModal
-          isVisible={photoModalVisible}
-          onClose={this.onCloseModal}
+          isVisible={addPhotoModalVisible}
+          onClose={this.onCloseAddModal}
           addPhoto={this.addPhoto}
+          little
+        />
+        <EditPhotoModal
+          isVisible={editPhotoModalVisible}
+          onClose={this.onCloseEditModal}
+          editPhoto={this.editPhoto}
+          little
+        />
+        <DeletePhotoModal
+          isVisible={deletePhotoModalVisible}
+          onClose={this.onCloseDeleteModal}
+          deletePhoto={this.deletePhoto}
           little
         />
       </div>
