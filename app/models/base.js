61e4329764d5168925eabb2570c285012308d0b0
Sal <salmanamjad89@gmail.com>
2018-06-06 19:50:38 +1000

    Promises support added for Backbone models

--- app/models/base.js
+++ app/models/base.js
@@ -1,6 +1,6 @@
 import Backbone from 'backbone-associations';
 import moment from 'moment';
-import { mapValues, assignIn, isEmpty, clone } from 'lodash';
+import { mapValues, assignIn, isEmpty, clone, map } from 'lodash';
 
 export default Backbone.AssociatedModel.extend({
   idAttribute: '_id',
@@ -11,7 +11,7 @@ export default Backbone.AssociatedModel.extend({
     modifiedAt: null,
   },
 
-  save(attrs, options) {
+  async save(attrs, options) {
     // Set last modified times
     if (this.changedAttributes() !== false) {
       let modifiedFields = this.changedAttributes();
@@ -25,38 +25,25 @@ export default Backbone.AssociatedModel.extend({
     }
 
     // Proxy the call to the original save function
-    Backbone.Model.prototype.save.call(this, attrs, options);
+    const res = await Backbone.Model.prototype.save.apply(this, [attrs, options]);
+    return res;
   },
 
   toJSON(options) {
-    if (options.relations) {
-      return new Promise((resolve, reject) => {
+    if (options && options.relations) {
+      return new Promise(async (resolve, reject) => {
         const attributes = clone(this.attributes);
         const { relations } = this;
 
         // Fetch all the relations
         if (!isEmpty(relations)) {
-          relations.forEach((relation) => {
-            const Model = relation.relatedModel();
-            attributes[relation.key] = [];
-
-            // Fetch the models
-            const models = this.get(relation.key);
-            if (models.length > 0) {
-              const tasks = [];
-
-              models.forEach((_model) => {
-                const _m = new Model();
-                _m.set({ _id: _model.id });
-                tasks.push(_m.fetch());
-              });
-
-              Promise.all(tasks).then((values) => {
-                attributes[relation.key] = values;
-                resolve(attributes);
-              }).catch((err) => reject(err));
-            }
-          });
+          try {
+            const tasks = relations.map((relation) => this.fetchRelations({ relation, attributes, reject }));
+            await Promise.all(tasks);
+            resolve(attributes);
+          } catch (err) {
+            reject(err);
+          }
         } else {
           resolve(attributes);
         }
@@ -64,5 +51,32 @@ export default Backbone.AssociatedModel.extend({
     }
 
     return clone(this.attributes);
+  },
+
+  async fetchRelations(options) {
+    const { relation, attributes, reject } = options;
+    const Model = relation.relatedModel();
+    attributes[relation.key] = [];
+
+    // Fetch the models
+    const models = this.get(relation.key);
+    if (models.length > 0) {
+      const tasks = [];
+
+      models.forEach((_model) => {
+        const _m = new Model();
+        _m.set({ _id: _model.id });
+        tasks.push(_m.fetch());
+      });
+
+      try {
+        const values = await Promise.all(tasks);
+        attributes[relation.key] = map(values, (value) => value.toJSON());
+      } catch (err) {
+        reject(err);
+      }
+    }
+
+    return attributes;
   }
 });
--- app/utils/dbHelper/index.js
+++ app/utils/dbHelper/index.js
@@ -1,6 +1,7 @@
 import PouchDB from 'pouchdb';
 import Backbone from 'backbone-associations';
 import BackbonePouch from 'backbone-pouch';
+import { defaults } from 'lodash';
 import createViews from './createViews';
 import createIndex from './createIndex';
 
@@ -31,6 +32,43 @@ Backbone.sync = BackbonePouch.sync({
     }
   }
 });
+
+// Add promise support to backbone model
+const originalSave = Backbone.Model.prototype.save;
+Backbone.Model.prototype.save = function saveData(data, options) {
+  return new Promise((resolve, reject) => {
+    const newOptions = defaults({
+      success: resolve,
+      error: reject
+    }, options);
+
+    originalSave.apply(this, [data, newOptions]);
+  });
+};
+
+const originalFetch = Backbone.Model.prototype.fetch;
+Backbone.Model.prototype.fetch = function fetchData(options) {
+  return new Promise((resolve, reject) => {
+    const newOptions = defaults({
+      success: resolve,
+      error: reject
+    }, options);
+
+    originalFetch.apply(this, [newOptions]);
+  });
+};
+
+const originalDestroy = Backbone.Model.prototype.destroy;
+Backbone.Model.prototype.destroy = function destroyData(options) {
+  return new Promise((resolve, reject) => {
+    const newOptions = defaults({
+      success: resolve,
+      error: reject
+    }, options);
+
+    originalDestroy.apply(this, [newOptions]);
+  });
+};
 
 // Backbone.sync = adaptor;
 Backbone.Model.prototype.idAttribute = '_id';
