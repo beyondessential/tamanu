5286ea4593132a525570c77325b94ca6b0719b1a
Sal <salmanamjad89@gmail.com>
2018-09-17 12:13:14 +1000

    Splash screen added resolves #93

--- app/containers/Home/index.js
+++ app/containers/Home/index.js
@@ -1,22 +1,34 @@
-// @flow
 import React, { Component } from 'react';
+import { random } from 'lodash';
 
-type Props = {};
+class HomePage extends Component {
+  state: {
+    splashStyle: {}
+  }
 
-export default class HomePage extends Component<Props> {
-  props: Props;
+  componentWillMount() {
+    const numImages = 3;
+    const splashStyle = { backgroundImage: `url('./assets/images/splashscreens/screen_${random(1, numImages)}.jpg')` };
+    this.setState({ splashStyle });
+  }
 
   render() {
+    const { splashStyle } = this.state;
+
     return (
       <div>
         <div className="content">
-          <div className="view-top-bar">
-            <span>
-              Welcome to Tamanu!
-            </span>
+          <div className="splash-screen" style={splashStyle}>
+            <div className="view-top-bar">
+              <span>
+                Welcome to Tamanu!
+              </span>
+            </div>
           </div>
         </div>
       </div>
     );
   }
 }
+
+export default HomePage;
