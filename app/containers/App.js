ed0fc261ad914e5fc2939064eb30df934193e1cc
Sal <salmanamjad89@gmail.com>
2018-09-18 15:09:05 +1000

    Notifications added

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ToastContainer, Slide } from 'react-toastify';
 import Sidebar from '../components/Sidebar';
 
 export default class App extends Component<Props> {
@@ -10,6 +11,11 @@ export default class App extends Component<Props> {
       <React.Fragment>
         <Sidebar />
         {children}
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          transition={Slide}
+        />
       </React.Fragment>
     );
   }
