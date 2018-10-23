d84863f7ac334a79b29480abbac1ff6e98937a86
Sal <salmanamjad89@gmail.com>
2018-10-23 11:54:14 +1100

    Sorting & filtering added

--- app/controllers/realm/get.js
+++ app/controllers/realm/get.js
@@ -1,9 +1,16 @@
 const { objectToJSON } = require('../../utils');
+const { parseInt, ceil } = require('lodash');
 
 module.exports = (req, res) => {
   const realm = req.app.get('database');
-  const { params } = req;
+  const { params, query } = req;
   const { model, id } = params;
+  let {
+    current_page: currentPage,
+    page_size: pageSize,
+    sort_by: sortBy,
+    order
+  } = query;
 
   try {
     return realm.write(() => {
@@ -15,7 +22,21 @@ module.exports = (req, res) => {
       }
 
       objects = objects.map(object => objectToJSON(object));
-      return res.json(objects);
+      const paginationParams = { total_entries: objects.length };
+
+      // Limit results
+      if (currentPage && pageSize) {
+        currentPage = parseInt(currentPage);
+        pageSize = parseInt(pageSize);
+        const start = currentPage * pageSize;
+        const end = start + pageSize;
+        objects = objects.slice(start, end);
+        paginationParams.total_pages = ceil(paginationParams.total_entries / pageSize);
+      }
+
+      if (sortBy) objects.sorted(sortBy, order === 'desc');
+      const response = [paginationParams, objects];
+      return res.send(response);
     });
   } catch (err) {
     return res.status(500).send(err.toString());
