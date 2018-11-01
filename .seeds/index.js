a06926fcb742603efac6083f77ee4ac63f708a16
Sal <salmanamjad89@gmail.com>
2018-11-01 16:30:48 +1100

    Seeds added for views

--- .seeds/index.js
+++ .seeds/index.js
@@ -1,7 +1,9 @@
 const users = require('./users');
+const views = require('./views');
 
 module.exports = (database) => {
   database.write(() => {
     users(database);
+    views(database);
   });
 }
---
+++ .seeds/views.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt');
+const AuthService = require('../app/services/auth');
+
+module.exports = (database) => {
+  const views = [
+    {
+      name: 'patients_admitted',
+      filters: { "admitted": true }
+    },
+    {
+      name: 'patients_female',
+      filters: { "sex": "female" }
+    },
+    {
+      name: 'patients_male',
+      filters: { "sex": "male" }
+    },
+    {
+      name: 'medication_completed',
+      filters: { "status": "Completed"}
+    },
+    {
+      name: 'medication_requested',
+      filters: { "status": "Requested" }
+    },
+    {
+      name: 'medication_fulfilled',
+      filters: { "status": "Fulfilled" }
+    }
+  ];
+
+  views.forEach(view => {
+    const { name, filters } = view;
+    const viewObject = database.findOne('view', name, 'name');
+    if (!viewObject || viewObject.length <= 0) database.create('view', { name, filters: JSON.stringify(filters) }, true);
+  });
+}
