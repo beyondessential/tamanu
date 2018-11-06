cbb7b49003b631b98e7bd8a6baad7ce9e02e3583
Tom McLean <tom.a.mclean@gmail.com>
2018-11-06 12:24:48 +1100

    Add "export to excel" button

--- app/containers/Reports/ReportTable.js
+++ app/containers/Reports/ReportTable.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import moment from 'moment';
 
+import { SaveSpreadsheetButton } from '../../components/SaveSpreadsheetButton';
+
 const dataColumns = [
   { 
     Header: 'Date',
     id: 'date', 
-    accessor: record => record.date,
+    accessor: row => row.date,
     sortMethod: (a, b) => a - b,
     Cell: record => moment(record.row.date).format('L'),
+    exporter: row => moment(row.date).format('L'),
   },
   { 
     Header: 'Amount',
@@ -17,9 +20,18 @@ const dataColumns = [
 ];
 
 export const ReportTable = ({ data }) => (
-  <ReactTable
-    data={ data }
-    columns={dataColumns}
-    minRows={ 1 }
-  />
+  <div>
+    <div style={ { textAlign: 'right' } }>
+      <SaveSpreadsheetButton 
+        filename="report"
+        data={data}
+        columns={dataColumns}
+      />
+    </div>
+    <ReactTable
+      data={ data }
+      columns={dataColumns}
+      minRows={ 1 }
+    />
+  </div>
 );
