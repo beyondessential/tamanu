70f054ee0ad3a82b472554de681ea893dee9439a
Salman <salmanamjad89@gmail.com>
2018-12-17 14:09:14 +1100

    branch based config enabled

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 type=$1
 type_upper=${type^^}
-lan_config="${type_upper}_LAN_CONFIG"
-server_config="${type_upper}_SERVER_CONFIG"
+branch=${CI_BRANCH}
+branch_upper=${branch^^}
+lan_config="CONFIG_LAN_${type_upper}_${branch_upper}"
+server_config="CONFIG_SERVER_${type_upper}_${branch_upper}"
 echo "Building - ${type}"
 
-echo "${DESKTOP_CONFIG}" > ${DESKTOP_ROOT}/.env
+echo "${CONFIG_DESKTOP}" > ${DESKTOP_ROOT}/.env
 printf '%s\n' "${!lan_config}" > ${LAN_ROOT}/config.json
 printf '%s\n' "${!server_config}" > ${SERVER_ROOT}/config.json
 
