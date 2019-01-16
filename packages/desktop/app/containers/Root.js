b6fa1467becd3a0983d5993170f5a98aeed8bad2
Salman Amjad <salmanamjad89@gmail.com>
2019-01-16 15:19:00 +1100

    redux persist added

--- packages/desktop/app/containers/Root.js
+++ packages/desktop/app/containers/Root.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
-import Routes from '../routes';
+import { PersistGate } from 'redux-persist/integration/react'
 
+import Routes from '../routes';
+import { initClient } from '../utils';
+import Preloader from '../components/Preloader';
 import { ThemeProvider } from '../components/ThemeProvider';
 
 export default class Root extends Component {
   render() {
-    const { store, history } = this.props;
+    const {
+      store,
+      history,
+      persistor
+    } = this.props;
+
     return (
       <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <ThemeProvider>
-            <Routes />
-          </ThemeProvider>
-        </ConnectedRouter>
+        <PersistGate
+          loading={<Preloader />}
+          persistor={persistor}
+          onBeforeLift={initClient()}
+        >
+          <ConnectedRouter history={history}>
+            <ThemeProvider>
+              <Routes />
+            </ThemeProvider>
+          </ConnectedRouter>
+        </PersistGate>
       </Provider>
     );
   }
--- packages/desktop/app/store/configureStore.prod.js
+++ packages/desktop/app/store/configureStore.prod.js
@@ -3,16 +3,30 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'react-router-redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/patients';
-import dbService from '../services/database';
 
 const history = createBrowserHistory();
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
 function configureStore(initialState?: counterStateType) {
-  return createStore(rootReducer, initialState, enhancer);
+  // Create Store
+  const persistConfig = {
+    key: 'tamanu',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whitelist: ['auth'],
+    debug: true,
+  };
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+  const store = createStore(persistedReducer, initialState, enhancer);
+
+  const persistor = persistStore(store);
+  return { store, persistor, persistConfig };
 }
 
 export default { configureStore, history };
