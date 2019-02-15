8d5f5c0be88fb0007a8963093a4272da29682330
Salman Amjad <salmanamjad89@gmail.com>
2019-02-15 16:13:23 +1100

    diagnosesAll -> allDiagnoses

--- packages/desktop/app/containers/Patients/components/Diagnosis.js
+++ packages/desktop/app/containers/Patients/components/Diagnosis.js
@@ -61,7 +61,7 @@ class Diagnosis extends Component {
       diagnoses: allDiagnoses
     } = this.state;
     // filter diagnosis type i-e primary or secondary
-    const diagnoses = diagnosesAll.toJSON().filter(diagnosis => diagnosis.active && diagnosis.secondaryDiagnosis === showSecondary);
+    const diagnoses = allDiagnoses.toJSON().filter(diagnosis => diagnosis.active && diagnosis.secondaryDiagnosis === showSecondary);
 
     return (
       <div>
