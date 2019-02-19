47e24b62f71118ce825709522c709bc81dfb39c3
Edwin <edmofro@gmail.com>
2019-02-19 16:19:27 +1100

    Update schema version

--- packages/shared/schemas/index.js
+++ packages/shared/schemas/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { each } = require('lodash');
 const defaults = require('./defaults');
 const { SYNC_MODES } = require('../constants');
-const version = 32;
+const version = 33;
 const defaultSchema = {
   primaryKey: '_id',
   sync: SYNC_MODES.ON,
