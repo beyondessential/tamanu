15db3b3fdbd6322f7ce2df2a7341ab43bc25bc0c
Salman Amjad <salmanamjad89@gmail.com>
2019-02-20 13:23:18 +1100

    pregnancy program added to .seeds

--- packages/server/.seeds/index.js
+++ packages/server/.seeds/index.js
@@ -1,6 +1,7 @@
 const users = require('./users');
 const views = require('./views');
 const userRoles = require('./user-roles');
+const programs = require('./programs');
 
 module.exports = (database) => {
   const patients = database.objects('patient');
@@ -8,5 +9,6 @@ module.exports = (database) => {
     views(database);
     userRoles(database);
     users(database);
+    programs(database);
   });
 }
