3261cba3c9a950a0c2a6d581a8a3267ce52bb882
Edwin <edmofro@gmail.com>
2019-02-20 13:11:01 +1100

    Remove references to flow typing (unused)

--- packages/desktop/app/containers/Labs/NewRequest.js
+++ packages/desktop/app/containers/Labs/NewRequest.js
@@ -1,27 +1,18 @@
-// @flow
-import React, { Component } from 'react';
+import React from 'react';
 
-type Props = {};
-
-export default class NewRequest extends Component<Props> {
-  props: Props;
-
-  render() {
-    return (
-      <div>
-        <div className="content">
-          <div className="view-top-bar">
-            <span>
-              New Lab Request
-            </span>
-            <div className="view-action-buttons">
-              <button>
-                + New Lab
-              </button>
-            </div>
-          </div>
+export default () => (
+  <div>
+    <div className="content">
+      <div className="view-top-bar">
+        <span>
+          New Lab Request
+        </span>
+        <div className="view-action-buttons">
+          <button>
+            + New Lab
+          </button>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
