70f735a7d6e69083212141a5ec17828d2b76f2b4
Salman Amjad <salmanamjad89@gmail.com>
2019-02-21 15:27:33 +1100

    stretch container

--- packages/desktop/app/containers/Patients/components/TopRow.js
+++ packages/desktop/app/containers/Patients/components/TopRow.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const FullWidthhWrapper = styled.div`
+  flex-grow: 1
+`;
 
 class TopRow extends Component {
   static propTypes = {
@@ -9,35 +14,37 @@ class TopRow extends Component {
   render() {
     const { patient } = this.props;
     return (
-      <div className="columns is-multiline is-variable m-b-0 p-b-15">
-        <div className="column p-b-0 is-8">
-          <div className="column p-b-5 p-t-0">
-            <span className="title">Name: </span>
-            <span className="full-name">
-              {patient.firstName} {patient.lastName}
-            </span>
+      <FullWidthhWrapper>
+        <div className="columns is-multiline is-variable m-b-0 p-b-15">
+          <div className="column p-b-0 is-8">
+            <div className="column p-b-5 p-t-0">
+              <span className="title">Name: </span>
+              <span className="full-name">
+                {patient.firstName} {patient.lastName}
+              </span>
+            </div>
+            <div className="column p-b-5 p-t-5">
+              <span className="title is-medium">Sex: </span>
+              <span className="is-medium">
+                {patient.sex}
+              </span>
+            </div>
+            <div className="column p-t-5 p-b-5">
+              <span className="title is-medium">Age: </span>
+              <span className="is-medium">
+                {patient.age}
+              </span>
+            </div>
           </div>
-          <div className="column p-b-5 p-t-5">
-            <span className="title is-medium">Sex: </span>
-            <span className="is-medium">
-              {patient.sex}
-            </span>
-          </div>
-          <div className="column p-t-5 p-b-5">
-            <span className="title is-medium">Age: </span>
-            <span className="is-medium">
-              {patient.age}
-            </span>
-          </div>
-        </div>
-        <div className="column p-b-0 is-4">
-          <div className="align-left">
-            <div className="card-info">
-              {patient.displayId}
+          <div className="column p-b-0 is-4">
+            <div className="align-left">
+              <div className="card-info">
+                {patient.displayId}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </FullWidthhWrapper>
     );
   }
 }
