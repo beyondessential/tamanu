3f4d2f60073bf3931aad4a2b39d57fdb056ac1ee
Salman Amjad <salmanamjad89@gmail.com>
2019-02-21 18:04:05 +1100

    .seeds added for tests

--- packages/server/.seeds/index.js
+++ packages/server/.seeds/index.js
@@ -2,13 +2,16 @@ const users = require('./users');
 const views = require('./views');
 const userRoles = require('./user-roles');
 const programs = require('./programs');
+const tests = require('./tests');
 
 module.exports = (database) => {
   const patients = database.objects('patient');
+  const createdBy = database.findOne('user', 'demo--admin');
   database.write(() => {
     views(database);
     userRoles(database);
     users(database);
     programs(database);
+    tests(database, createdBy);
   });
 }
