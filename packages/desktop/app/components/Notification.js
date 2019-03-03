875e92be9cd293055903a241f34f9a0dacdd8daa
Tom McLean <tom.a.mclean@gmail.com>
2019-03-04 10:57:35 +1100

    Fix incorrect parameter on notification component

--- packages/desktop/app/components/Notification.js
+++ packages/desktop/app/components/Notification.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 
 export const Notification = ({ message }) => (
   <div className="notification">
-    <span>
-      No requests found.
-    </span>
+    <span>{ message }</span>
   </div>
 );
