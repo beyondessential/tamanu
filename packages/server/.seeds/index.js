6007018841e87157f93c20522e3bac9075d9ffcb
Salman Amjad <salmanamjad89@gmail.com>
2019-03-04 22:25:18 +1100

    seed added for imaging types

--- packages/server/.seeds/index.js
+++ packages/server/.seeds/index.js
@@ -3,6 +3,7 @@ const views = require('./views');
 const userRoles = require('./user-roles');
 const programs = require('./programs');
 const tests = require('./tests');
+const imagingTypes = require('./imaging-types');
 const patients = require('./patients');
 
 module.exports = async (database) => {
