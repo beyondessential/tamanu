79b70dd3af697215ed5f1fe6e9c7472c92a7e69d
Edwin <edmofro@gmail.com>
2019-03-08 13:22:07 +0600

    Revert auth logout change, caused more errors

--- packages/desktop/app/utils/backbone-sync.js
+++ packages/desktop/app/utils/backbone-sync.js
@@ -37,11 +37,8 @@ export default (store) => {
       }
       // 401 - Unauthorized
       if (status === 401) {
-        // TODO reinstate after demo
-        // store.dispatch({ type: AUTH_LOGOUT });
-        // return history.push('/login');
-        notify("You don't have enough permissions to make this request!");
-        if (method === 'read') return history.goBack();
+        store.dispatch({ type: AUTH_LOGOUT });
+        return history.push('/login');
       }
       // 405 - Not enough permissions
       if (status === 405) {
