0265300eed5dcc10a88a6413a722cd774c42bc83
Edwin <edmofro@gmail.com>
2019-03-08 11:48:48 +0600

    Update seed instructions

--- packages/scripts/seed_main_server.sh
+++ packages/scripts/seed_main_server.sh
@@ -1,4 +1,3 @@
-pm2 delete server-demo-dev
 export NODE_ENV='development'
 export DB_BASE_PATH="/home/ubuntu/data/"
 export NVM_DIR="$HOME/.nvm"
@@ -6,4 +5,12 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm use 10.14.0
 node -v
-pm2 start -name server-demo-dev node
+node index
+(ctrl-c)
+cd ~/data/
+mv demo-dev demo-dev-backup-190308
+mv ~/tamanu-demo-dev/packages/server/data ./demo-dev
+pm2 restart server-demo-dev
+
+---------
+DB_BASE_PATH=/home/ubuntu/data NODE_ENV=production pm2 start  index.js --name server-demo-dev --env production && pm2 logs
