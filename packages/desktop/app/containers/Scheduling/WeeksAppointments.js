02a984639a25ad3bb3ad96fa8d5fd9b7da40d9bc
Salman Amjad <salmanamjad89@gmail.com>
2019-03-13 11:05:59 +1100

    route components updated

---
+++ packages/desktop/app/containers/Scheduling/WeeksAppointments.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Appointments from './Appointments';
+
+const WeeksAppointments = props => <Appointments duration="week" {...props} />
+export default WeeksAppointments;
