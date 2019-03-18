7101e8b91b678e3e5872549c2bd311ea8664305a
Tom McLean <tom.a.mclean@gmail.com>
2019-03-18 13:19:04 +1100

    Add placeholder stories file

---
+++ packages/desktop/stories/index.stories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import TopBar from '../app/components/TopBar';
+
+// placeholder stories, to be fleshed out
+
+storiesOf('Layout/MainGrid', module).add('placeholder', () => <div></div>);
+storiesOf('Layout/Sidebar', module).add('placeholder', () => <div></div>);
+storiesOf('Layout/Topbar', module).add('placeholder', () => <div></div>);
+storiesOf('Layout/MainPane', module).add('placeholder', () => <div></div>);
+
+storiesOf('Button/Action', module).add('placeholder', () => <div></div>);
+storiesOf('Button/Link', module).add('placeholder', () => <div></div>);
+
+storiesOf('Forms/TextInput', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/TextArea', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/Dropdown', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/Radio', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/Checkbox', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/SearchField', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/DatePicker', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/DateRangePicker', module).add('placeholder', () => <div></div>);
+
+storiesOf('Forms/Autocomplete', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/AsyncAutocomplete', module).add('placeholder', () => <div></div>);
+storiesOf('Forms/AddMany', module).add('placeholder', () => <div></div>);
+
+storiesOf('Preloader', module).add('placeholder', () => <div></div>);
+
+storiesOf('Notification', module).add('placeholder', () => <div></div>);
+
+storiesOf('DateDisplay', module).add('placeholder', () => <div></div>);
+storiesOf('DateRangeDisplay', module).add('placeholder', () => <div></div>);
+
+storiesOf('Charts/Line', module).add('placeholder', () => <div></div>);
+storiesOf('Charts/Bar', module).add('placeholder', () => <div></div>);
+storiesOf('Charts/Pie', module).add('placeholder', () => <div></div>);
+storiesOf('DataTable', module).add('placeholder', () => <div></div>);
+
+// not really sure but we have a common pattern to show a group of 
+// assorted details of a single topic/object -- patient info, contact details, etc
+storiesOf('DetailsView', module).add('placeholder', () => <div></div>);
+
+storiesOf('TabBar', module).add('placeholder', () => <div></div>);
+storiesOf('BrowsableTable', module).add('placeholder', () => <div></div>);
+storiesOf('Calendar', module).add('placeholder', () => <div></div>);
+
