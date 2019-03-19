304a589cbd948f90d38fd88b145707e72eb6f1f2
Salman Amjad <salmanamjad89@gmail.com>
2019-03-20 10:03:31 +1100

    unused files removed

--- packages/server/app/routes/staticRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-
-module.exports = function (app, config) {
-  app.use('/patientimages', express.static(config.imagesdir));
-  app.use('/attachments', express.static(config.attachmentsDir));
-};
