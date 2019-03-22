778d21e19cc0a2c0449c4c73d565ae2e59664abe
Salman Amjad <salmanamjad89@gmail.com>
2019-03-22 15:15:47 +1100

    `CssBaseline` added

--- packages/desktop/app/components/ThemeProvider.js
+++ packages/desktop/app/components/ThemeProvider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { grey } from '@material-ui/core/colors';
 
 const palette = {
   primary: {
@@ -9,9 +10,13 @@ const palette = {
     main: '#ffcc24',
     dark: '#905a00',
   },
+  background: {
+    paper: 'white',
+    default: grey[200],
+  },
 };
 const themeName = 'Tamanu';
-const typography = { useNextVariants: true };
+const typography = { useNextVariants: true, fontSize: 15 };
 const theme = createMuiTheme({ palette, themeName, typography });
 
 export const ThemeProvider = (props) => (
