30914c4bccd7c9fa4e0fb4040877c89e7f4daf9f
Tom McLean <tom.a.mclean@gmail.com>
2019-03-27 16:51:46 +1100

    Update comments

--- packages/desktop/app/store/index.js
+++ packages/desktop/app/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore as configureStoreProd } from './configureStore.prod';
 
-// @TODO: update build system to exclude this from prod build entirely
+// TODO: update build system to exclude this from prod build entirely
 import { configureStore as configureStoreDev } from './configureStore.dev';
   
 const configurer = (process.env.NODE_ENV === 'production') 
