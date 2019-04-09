eef15580a6c2b74018371157e7a1bf516f6166cd
Salman Amjad <salmanamjad89@gmail.com>
2019-04-10 09:54:42 +1000

    auto select current visit

--- packages/desktop/app/components/PatientRelationSelect.js
+++ packages/desktop/app/components/PatientRelationSelect.js
@@ -36,7 +36,11 @@ export default class PatientRelationSelect extends Component {
   }
 
   componentWillReceiveProps(newProps) {
-    if (newProps.patient !== this.props.patient) this.loadRelations(newProps);
+    const { name, onChange } = this.props;
+    if (newProps.patient !== this.props.patient) {
+      onChange({ target: { name, value: null } }); // reset
+      this.loadRelations(newProps);
+    }
   }
 
   handleChange = (value) => {
@@ -47,8 +51,9 @@ export default class PatientRelationSelect extends Component {
 
   async loadRelations(props = this.props) {
     const {
-      patient, relation, template,
+      patient, relation, template, onChange, name,
     } = props;
+    const updates = { initValue: null };
     if (patient) {
       let patientModel = patient;
       if (typeof patient === 'string') {
@@ -57,9 +62,22 @@ export default class PatientRelationSelect extends Component {
         patientModel = this.patientModel;
       }
 
-      let { [relation]: options } = patientModel.toJSON();
-      options = options.map(item => ({ value: item._id, label: template(item) }));
-      this.setState({ options });
+      // get select options
+      const { [relation]: relationCollection } = patientModel.attributes;
+      const optionsTransformed = relationCollection.map(model => ({ value: model.get('_id'), label: template(model.toJSON()) }));
+
+      // find patients current visit and set that as initial value
+      if (relation === 'visits') {
+        const currentVisit = relationCollection.find(visitModel => visitModel.isCurrentVisit());
+        if (currentVisit) {
+          updates.initValue = currentVisit.get('_id');
+          onChange({ target: { name, value: updates.initValue } });
+        }
+        // add new visit option
+        optionsTransformed.push({ value: ADD_NEW_VISIT, label: 'Add new Visit' });
+      }
+
+      this.setState({ ...updates, options: optionsTransformed });
     }
   }
 
@@ -68,17 +86,18 @@ export default class PatientRelationSelect extends Component {
       label,
       template,
       name,
+      value,
       ...props
     } = this.props;
-    const { options, value } = this.state;
+    const { options, initValue } = this.state;
     return (
       <SelectInput
         label={label}
         options={options}
         name={name}
-        value={value}
-        onChange={this.handleChange}
+        value={value || initValue}
         {...props}
+        onChange={this.handleOnChange}
       />
     );
   }
