41b3185e572e0902443be933ec7fef70dea0be61
Tom McLean <tom.a.mclean@gmail.com>
2019-04-10 13:31:14 +1000

    Add propTypes to Form, throw instead of rendering an error message

--- packages/desktop/app/components/Field/Form.js
+++ packages/desktop/app/components/Field/Form.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Formik } from 'formik';
+import PropTypes from 'prop-types';
 
 export { Field } from 'formik';
 
 export class Form extends React.PureComponent {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    render: PropTypes.func.isRequired,
+  }
+
   render() {
     const {
-      onSubmit, render, children, ...props
+      onSubmit, render, ...props
     } = this.props;
-    if (children) {
-      return <div>use `render` prop instead please</div>;
+
+    // read children from additional props rather than destructuring so
+    // eslint ignores it (there's not good support for "forbidden" props)
+    if (props.children) {
+      throw new Error('Form must not have any children -- use the `render` prop instead please!');
     }
+
     return (
       <Formik
         onSubmit={onSubmit}
