742f75330592c79c07c52816a18520c2af64201e
Edwin <edmofro@gmail.com>
2019-05-17 17:09:07 +1000

    Include utility scripts on remote server

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -10,5 +10,5 @@ mkdir -p deploy
   && zip -q -r ${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip . \
   && echo "${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated")
 zip -q -x packages/server/node_modules/**\* data/**\* \
-  -r ${DEPLOY_DIR}/server-$type-$CI_BRANCH-$CI_COMMIT_ID.zip packages/server packages/shared package.json yarn.lock babel.config.js\
+  -r ${DEPLOY_DIR}/server-$type-$CI_BRANCH-$CI_COMMIT_ID.zip packages/server packages/shared packages/scripts package.json yarn.lock babel.config.js\
   && echo "${DEPLOY_DIR}/server-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated"
