bc8f39b34e1136e4e6fe6b9d17521f15c6dafb79
Edwin <edmofro@gmail.com>
2019-05-17 12:26:34 +1000

    Trying to get builds working through CI

--- packages/server/webpack.config.js
+++ packages/server/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const NodemonPlugin = require('nodemon-webpack-plugin');
 
 module.exports = {
   target: 'node',
@@ -28,7 +27,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            rootMode: 'upward',
+            rootMode: 'upward-optional',
           },
         },
       },
