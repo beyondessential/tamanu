bad21d2927b6948a1ffd10d0fe18425a7affd8b5
Edwin <edmofro@gmail.com>
2019-05-19 16:23:24 +1000

    Correctly include default config with lan installer

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -6,8 +6,10 @@ mkdir -p deploy
 (cd ${DESKTOP_RELEASE_DIR} \
   && zip -q -r ${DEPLOY_DIR}/desktop-$type-$CI_BRANCH-$CI_COMMIT_ID.zip . \
   && echo "${DEPLOY_DIR}/desktop-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated")
-zip -q -r ${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip ${LAN_RELEASE_DIR} packages/lan/config \
-  && echo "${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated"
+(cd ${LAN_RELEASE_DIR} \
+  && cp -r ../config . \
+  && zip -q -r ${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip . ../config \
+  && echo "${DEPLOY_DIR}/lan-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated")
 zip -q -x packages/server/node_modules/**\* data/**\* \
   -r ${DEPLOY_DIR}/server-$type-$CI_BRANCH-$CI_COMMIT_ID.zip packages/server packages/shared packages/scripts package.json yarn.lock babel.config.js\
   && echo "${DEPLOY_DIR}/server-$type-$CI_BRANCH-$CI_COMMIT_ID.zip generated"
