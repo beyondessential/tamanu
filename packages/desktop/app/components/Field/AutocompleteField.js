a9d646738d76bebf8cab22073f72476750878bf5
Tom McLean <tom.a.mclean@gmail.com>
2019-07-31 17:00:59 +1000

    Move option fetchers out (and reorganise some stories)

--- packages/desktop/app/components/Field/AutocompleteField.js
+++ packages/desktop/app/components/Field/AutocompleteField.js
@@ -32,14 +32,14 @@ const renderInputComponent = (inputProps) => {
     classes, inputRef = () => {}, ref, ...other
   } = inputProps;
   return (
-      <TextField
-        fullWidth
-        inputRef={node => {
-          ref(node);
-          inputRef(node);
-        }}
-        {...other}
-      />
+    <TextField
+      fullWidth
+      inputRef={node => {
+        ref(node);
+        inputRef(node);
+      }}
+      {...other}
+    />
   );
 };
 
--- packages/desktop/app/forms/VisitForm.js
+++ packages/desktop/app/forms/VisitForm.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
+
 import {
   Form,
   Field,
   DateField,
   SelectField,
+  AutocompleteField,
   TextField,
 } from '../components/Field';
+import { FormGrid } from '../components/FormGrid';
 import { Button } from '../components/Button';
 
 const visitTypes = [
@@ -16,60 +20,68 @@ const visitTypes = [
   { label: "Emergency", value: "emergency" },
 ];
 
-const locations = [
-  { label: "Ward 1", value: "ward1" },
-  { label: "Ward 2", value: "ward2" },
-  { label: "Ward 3", value: "ward3" },
-];
-
-const practitioners = [
-  { label: "Doctor 1", value: "doctor1" },
-  { label: "Nurse 2", value: "nurse2" },
-  { label: "Doctor 3", value: "doctor3" },
-];
-
 export class VisitForm extends React.PureComponent {
+
+  propTypes = {
+    fetchLocations: PropTypes.func.isRequired,
+    fetchPractitioners: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+  }
 
   renderForm = ({
     isValid,
     isSubmitting,
     submitForm,
-  }) => (
-    <React.Fragment>
-      <Field
-        name="startDate"
-        label="Check-in"
-        component={DateField}
-        options={visitTypes}
-      />
-      <Field
-        name="type"
-        label="Visit type"
-        component={SelectField}
-        options={visitTypes}
-      />
-      <Field
-        name="location"
-        label="Location"
-        component={SelectField}
-        options={locations}
-      />
-      <Field
-        name="practitioner"
-        label="Practitioner"
-        component={SelectField}
-        options={practitioners}
-      />
-      <Field
-        name="reasonForVisit"
-        label="Reason for visit"
-        component={TextField}
-        multiline
-        rows={4}
-      />
-      <Button onClick={submitForm}>Start visit</Button>
-    </React.Fragment>
-  );
+  }) => {
+    const { fetchLocations, fetchPractitioners } = this.props;
+    return (
+      <FormGrid>
+        <Field
+          name="type"
+          label="Visit type"
+          required
+          component={SelectField}
+          options={visitTypes}
+        />
+        <Field
+          name="startDate"
+          label="Check-in"
+          required
+          component={DateField}
+          options={visitTypes}
+        />
+        <Field
+          name="location"
+          label="Location"
+          required
+          component={AutocompleteField}
+          fetchOptions={fetchLocations}
+        />
+        <Field
+          name="practitioner"
+          label="Practitioner"
+          required
+          component={AutocompleteField}
+          fetchOptions={fetchPractitioners}
+        />
+        <Field
+          name="reasonForVisit"
+          label="Reason for visit"
+          component={TextField}
+          multiline
+          rows={4}
+          style={{ gridColumn: "span 2" }}
+        />
+        <div style={{ gridColumn: 2, textAlign: "right" }}>
+          <Button 
+            variant="contained"
+            onClick={submitForm}
+            color="primary"
+          >Start visit</Button>
+        </div>
+      </FormGrid>
+    );
+  }
 
   render() {
     const { onSubmit } = this.props;
