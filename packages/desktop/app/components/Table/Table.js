c5681f726f728b2fbfd84d8c5e93639d192244e6
Edwin <edmofro@gmail.com>
2019-08-06 13:58:15 +1000

    Don't memoize Table

--- packages/desktop/app/components/Table/Table.js
+++ packages/desktop/app/components/Table/Table.js
@@ -36,112 +36,110 @@ const Cell = React.memo(({ value, CellComponent, sortDirection, align }) => (
   </TableCell>
 ));
 
-export const Table = React.memo(
-  ({
-    columns,
-    data,
-    errorMessage,
-    noDataMessage,
-    isLoading,
-    order,
-    orderBy,
-    count,
-    page,
-    rowsPerPage,
-    onChangePage,
-    onChangeOrderBy,
-    onChangeRowsPerPage,
-    onRowClick,
-  }) => {
-    function getErrorMessage() {
-      if (isLoading) return 'Loading...';
-      if (errorMessage) return errorMessage;
-      if (data.length === 0) return noDataMessage;
-      return null;
-    }
+export function Table({
+  columns,
+  data,
+  errorMessage,
+  noDataMessage,
+  isLoading,
+  order,
+  orderBy,
+  count,
+  page,
+  rowsPerPage,
+  onChangePage,
+  onChangeOrderBy,
+  onChangeRowsPerPage,
+  onRowClick,
+}) {
+  function getErrorMessage() {
+    if (isLoading) return 'Loading...';
+    if (errorMessage) return errorMessage;
+    if (data.length === 0) return noDataMessage;
+    return null;
+  }
 
-    function handleChangePage(event, newPage) {
-      if (onChangePage) onChangePage(newPage);
-    }
+  function handleChangePage(event, newPage) {
+    if (onChangePage) onChangePage(newPage);
+  }
 
-    function handleChangeRowsPerPage(event) {
-      const { value: newRowsPerPage } = parseInt(event.target, 10);
-      if (onChangeRowsPerPage) onChangeRowsPerPage(newRowsPerPage);
-    }
+  function handleChangeRowsPerPage(event) {
+    const { value: newRowsPerPage } = parseInt(event.target, 10);
+    if (onChangeRowsPerPage) onChangeRowsPerPage(newRowsPerPage);
+  }
 
-    function renderHeaders() {
-      return columns.map(({ key, title, numeric, sortable = true }) => {
-        if (sortable) {
-          return (
-            <TableCell
-              key={key}
-              align={numeric ? 'right' : 'left'}
-              sortDirection={orderBy === key ? order : false}
-            >
-              <TableSortLabel
-                active={orderBy === key}
-                direction={order}
-                onClick={() => onChangeOrderBy(key)}
-              >
-                {title}
-              </TableSortLabel>
-            </TableCell>
-          );
-        }
+  function renderHeaders() {
+    return columns.map(({ key, title, numeric, sortable = true }) => {
+      if (sortable) {
         return (
-          <TableCell key={key} align={numeric ? 'right' : 'left'}>
-            {title}
+          <TableCell
+            key={key}
+            align={numeric ? 'right' : 'left'}
+            sortDirection={orderBy === key ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === key}
+              direction={order}
+              onClick={() => onChangeOrderBy(key)}
+            >
+              {title}
+            </TableSortLabel>
           </TableCell>
         );
-      });
-    }
-
-    function renderRow(rowData) {
-      const cells = columns.map(({ key, accessor, CellComponent, numeric }) => (
-        <Cell
-          key={key}
-          value={accessor ? accessor(rowData) : rowData[key]}
-          align={numeric ? 'right' : 'left'}
-          sortDirection={orderBy === key ? order : false}
-          CellComponent={CellComponent}
-        />
-      ));
-      return <Row onClick={() => onRowClick(rowData)}>{cells}</Row>;
-    }
-
-    function renderBodyContent() {
-      const error = getErrorMessage();
-      if (error) {
-        return <ErrorRow message={error} colSpan={columns.length} />;
       }
-      return data.map(renderRow);
-    }
-
-    function renderPaginator() {
       return (
-        <TableRow>
-          <TablePagination
-            rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
-            colSpan={columns.length}
-            page={page}
-            count={count}
-            rowsPerPage={rowsPerPage}
-            onChangePage={handleChangePage}
-            onChangeRowsPerPage={handleChangeRowsPerPage}
-          />
-        </TableRow>
+        <TableCell key={key} align={numeric ? 'right' : 'left'}>
+          {title}
+        </TableCell>
       );
-    }
+    });
+  }
 
+  function renderRow(rowData) {
+    const cells = columns.map(({ key, accessor, CellComponent, numeric }) => (
+      <Cell
+        key={key}
+        value={accessor ? accessor(rowData) : rowData[key]}
+        align={numeric ? 'right' : 'left'}
+        sortDirection={orderBy === key ? order : false}
+        CellComponent={CellComponent}
+      />
+    ));
+    return <Row onClick={() => onRowClick(rowData)}>{cells}</Row>;
+  }
+
+  function renderBodyContent() {
+    const error = getErrorMessage();
+    if (error) {
+      return <ErrorRow message={error} colSpan={columns.length} />;
+    }
+    return data.map(renderRow);
+  }
+
+  function renderPaginator() {
     return (
-      <MaterialTable>
-        <TableHead>{renderHeaders()}</TableHead>
-        <TableBody>{renderBodyContent()}</TableBody>
-        {page !== null && <TableFooter>{renderPaginator()}</TableFooter>}
-      </MaterialTable>
+      <TableRow>
+        <TablePagination
+          rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
+          colSpan={columns.length}
+          page={page}
+          count={count}
+          rowsPerPage={rowsPerPage}
+          onChangePage={handleChangePage}
+          onChangeRowsPerPage={handleChangeRowsPerPage}
+        />
+      </TableRow>
     );
-  },
-);
+  }
+
+  return (
+    <MaterialTable>
+      <TableHead>{renderHeaders()}</TableHead>
+      <TableBody>{renderBodyContent()}</TableBody>
+      {page !== null && <TableFooter>{renderPaginator()}</TableFooter>}
+    </MaterialTable>
+  );
+}
 
 Table.propTypes = {
   columns: PropTypes.arrayOf(
