b6066629464b73a1f14c004cebfafd918fc5bbf6
Tom McLean <tom.a.mclean@gmail.com>
2019-08-08 11:03:35 +1000

    Add search icon on autocomplete fields

--- packages/desktop/app/components/Field/AutocompleteField.js
+++ packages/desktop/app/components/Field/AutocompleteField.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
 import { TextField, MenuItem, Popper, Paper, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Search from '@material-ui/icons/Search';
 
 const styles = () => ({
   root: {
@@ -31,6 +33,14 @@ const renderInputComponent = inputProps => {
     <TextField
       variant="outlined"
       InputLabelProps={{ shrink: true }}
+      InputProps={{
+        endAdornment: <InputAdornment position="end" style={{ 
+          paddingRight: '14px',
+        }}><Search style={{ opacity: 0.5 }} /></InputAdornment>,
+        style: {
+          paddingRight: 0,
+        }
+      }}
       fullWidth
       inputRef={node => {
         ref(node);
