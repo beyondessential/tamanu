3a4471e5035ff1b1742df301a59f0d27096fee77
Edwin <edmofro@gmail.com>
2019-08-09 12:10:58 +1000

    Fix table story

--- packages/desktop/stories/table.stories.js
+++ packages/desktop/stories/table.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import Chance from 'chance';
 
 import { Table } from '../app/components/Table/Table';
 
@@ -9,7 +10,7 @@ function TableStateWrapper({ columns, data }) {
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState(columns[0].key);
   const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
   function changeOrderBy(columnKey) {
     const isDesc = orderBy === columnKey && order === 'desc';
@@ -24,19 +25,23 @@ function TableStateWrapper({ columns, data }) {
     return order === 'asc' ? a - b : b - a;
   });
 
+  const startIndex = page * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+
   return (
     <Table
       columns={columns}
-      data={sortedData}
+      data={sortedData.slice(startIndex, endIndex)}
       page={page}
       rowsPerPage={rowsPerPage}
-      count={500}
+      count={data.length}
       orderBy={orderBy}
       order={order}
       onChangePage={setPage}
       onChangeRowsPerPage={setRowsPerPage}
       onChangeOrderBy={changeOrderBy}
       onRowClick={action('row clicked')}
+      rowsPerPageOptions={[5, 10, 15]}
     />
   );
 }
@@ -51,11 +56,13 @@ const sortableColumns = [
   { key: 'quantity', title: 'Quantity', numeric: true },
 ];
 
-const dummyData = [
-  { name: 'Apples', quantity: 53 },
-  { name: 'Bananas', quantity: 14 },
-  { name: 'Persimmon', quantity: 6 },
-];
+const chance = new Chance();
+const fakeFruit = () => ({
+  name: chance.pick(['Apples', 'Bananas', 'Persimmon', 'Oranges', 'Melon']),
+  quantity: chance.age(),
+});
+
+const dummyData = new Array(7).fill(0).map(fakeFruit);
 
 storiesOf('Table', module)
   .add('Plain', () => <Table columns={dummyColumns} data={dummyData} />)
