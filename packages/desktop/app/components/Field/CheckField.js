5602635bc4282be4597a0a08d9d6943383aa9db4
Edwin <edmofro@gmail.com>
2019-08-20 12:50:17 +1000

    Allow CheckField to span multiple columns

--- packages/desktop/app/components/Field/CheckField.js
+++ packages/desktop/app/components/Field/CheckField.js
@@ -3,20 +3,23 @@ import PropTypes from 'prop-types';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-export const CheckInput = ({ label, value, ...props }) => (
+export const CheckInput = ({ label, value, style, ...props }) => (
   <FormControlLabel
     control={<Checkbox checked={value} value="checked" {...props} />}
+    style={style}
     label={label}
   />
 );
 
-export const CheckField = ({ field, error, ...props }) => (
+export const CheckField = ({ field, error, label, disabled, required }) => (
   <CheckInput
     name={field.name}
     value={field.value || false}
     onChange={field.onChange}
+    label={label}
     error={error || undefined}
-    {...props}
+    disabled={disabled}
+    required={required}
   />
 );
 
