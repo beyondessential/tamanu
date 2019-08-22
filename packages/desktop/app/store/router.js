996c1c86e70c7500fb417c47d0fc5f4fe251d3ed
Tom McLean <tom.a.mclean@gmail.com>
2019-08-22 10:41:57 +1000

    Lint

--- packages/desktop/app/store/router.js
+++ packages/desktop/app/store/router.js
@@ -1,11 +1,7 @@
-
-
 // actions and reducers are provided by connected-react-router
-
 
 // selectors
 
 export const getCurrentRoute = state => state.router.location.pathname;
 
-export const getCurrentRouteEndsWith = (state, suffix) =>
-  getCurrentRoute(state).endsWith(suffix);
+export const getCurrentRouteEndsWith = (state, suffix) => getCurrentRoute(state).endsWith(suffix);
