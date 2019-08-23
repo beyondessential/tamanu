6cfedfe1b75fb3d1ab76c7524119b2d51550c1c0
Tom McLean <tom.a.mclean@gmail.com>
2019-08-23 16:49:50 +1000

    Put diagnosis form in its own file

--- packages/desktop/app/components/DiagnosisModal.js
+++ packages/desktop/app/components/DiagnosisModal.js
@@ -1,49 +1,11 @@
 import React from 'react';
 
-import { diagnosisCertainty } from '../constants';
 import { connectApi } from '../api/connectApi';
 import { Suggester } from '../utils/suggester';
 import { viewVisit } from '../store/visit';
 
-import { ConfirmCancelRow } from './ButtonRow';
 import { Modal } from './Modal';
-import { FormGrid } from './FormGrid';
-import { Form, Field, SelectField, CheckField, AutocompleteField, DateField } from './Field';
-
-const DiagnosisForm = React.memo(({ onCancel, onSave, diagnosis, icd10Suggester }) => (
-  <Form
-    onSubmit={onSave}
-    initialValues={{
-      date: new Date(),
-      isPrimary: true,
-      certainty: 'confirmed',
-      ...diagnosis,
-    }}
-    render={({ submitForm }) => (
-      <FormGrid>
-        <div style={{ gridColumn: 'span 2' }}>
-          <Field
-            name="diagnosis._id"
-            label="ICD10 Code"
-            component={AutocompleteField}
-            suggester={icd10Suggester}
-            required
-          />
-        </div>
-        <Field name="isPrimary" label="Is primary" component={CheckField} />
-        <Field
-          name="certainty"
-          label="Certainty"
-          component={SelectField}
-          options={diagnosisCertainty}
-          required
-        />
-        <Field name="date" label="Date" component={DateField} required />
-        <ConfirmCancelRow onConfirm={submitForm} onCancel={onCancel} />
-      </FormGrid>
-    )}
-  />
-));
+import { DiagnosisForm } from '../forms/DiagnosisForm';
 
 const DumbDiagnosisModal = React.memo(({ diagnosis, onClose, onSaveDiagnosis, icd10Suggester }) => (
   <Modal title="Diag" open={!!diagnosis} onClose={onClose}>
---
+++ packages/desktop/app/forms/DiagnosisForm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { diagnosisCertainty } from '../constants';
+
+import { ConfirmCancelRow } from '../components/ButtonRow';
+import { FormGrid } from '../components/FormGrid';
+import {
+  Form,
+  Field,
+  SelectField,
+  CheckField,
+  AutocompleteField,
+  DateField,
+} from '../components/Field';
+
+export const DiagnosisForm = React.memo(({ onCancel, onSave, diagnosis, icd10Suggester }) => (
+  <Form
+    onSubmit={onSave}
+    initialValues={{
+      date: new Date(),
+      isPrimary: true,
+      certainty: 'confirmed',
+      ...diagnosis,
+    }}
+    render={({ submitForm }) => (
+      <FormGrid>
+        <div style={{ gridColumn: 'span 2' }}>
+          <Field
+            name="diagnosis._id"
+            label="ICD10 Code"
+            component={AutocompleteField}
+            suggester={icd10Suggester}
+            required
+          />
+        </div>
+        <Field name="isPrimary" label="Is primary" component={CheckField} />
+        <Field
+          name="certainty"
+          label="Certainty"
+          component={SelectField}
+          options={diagnosisCertainty}
+          required
+        />
+        <Field name="date" label="Date" component={DateField} required />
+        <ConfirmCancelRow onConfirm={submitForm} onCancel={onCancel} />
+      </FormGrid>
+    )}
+  />
+));
