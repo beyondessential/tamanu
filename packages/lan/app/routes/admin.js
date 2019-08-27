d3b09f344b5dc997113b58beda869f8c1716a564
Tom McLean <tom.a.mclean@gmail.com>
2019-08-27 17:16:05 +1000

    Update admin route to include general-purpose idempotent bulk import function

--- packages/lan/app/routes/admin.js
+++ packages/lan/app/routes/admin.js
@@ -1,37 +1,66 @@
 import express from 'express';
 import shortid from 'shortid';
+import { objectToJSON } from '../utils';
 
 export const adminRoutes = express.Router();
+
+function updateObject(base, patch) {
+  // ignore any keys that aren't in base as they aren't in the schema
+  const flattenedObject = objectToJSON(base);
+  const updates = Object.entries(flattenedObject).map(([key, value]) => {
+    const updatedValue = patch[key];
+    if (patch.hasOwnProperty(key) && value !== updatedValue) {
+      // eslint-disable-next-line no-param-reassign
+      base[key] = updatedValue;
+      return true;
+    }
+    return false;
+  });
+
+  // return true iff any keys were updated
+  if (updates.some(x => x)) {
+    return base; // now with updated fields
+  }
+  return null;
+}
+
+function addOrUpdateMany(db, table, items, findExisting, defaultValues = {}) {
+  const objects = db.objects(table);
+
+  let recordsWritten = [];
+  db.write(() => {
+    recordsWritten = items
+      .map(item => {
+        const existing = findExisting(objects, item);
+
+        if (existing) {
+          return updateObject(existing, { ...defaultValues, ...item });
+        }
+
+        const newItem = {
+          _id: shortid(),
+          ...defaultValues,
+          ...item,
+        };
+        db.create(table, newItem);
+        return newItem;
+      })
+      .filter(x => x);
+  });
+  return recordsWritten;
+}
 
 adminRoutes.put('/diagnoses', (req, res) => {
   const db = req.app.get('database');
   const items = req.body;
 
-  const codes = db.objects('diagnosis');
-  db.write(() => {
-    const recordsWritten = items
-      .map(({ code, name, type = 'icd10' }) => {
-        const existing = codes.filtered('code = $0', code);
+  const recordsWritten = addOrUpdateMany(
+    db,
+    'diagnosis',
+    items,
+    (objects, item) => objects.filtered('code = $0', item.code)[0],
+    { type: 'icd10' },
+  );
 
-        if (existing.length > 0) {
-          const existingItem = existing[0];
-          if (existingItem.name === name) return null;
-
-          existingItem.name = name;
-          return existingItem;
-        }
-
-        const newItem = {
-          _id: shortid(),
-          code,
-          name,
-          type,
-        };
-        db.create('diagnosis', newItem);
-        return newItem;
-      })
-      .filter(x => x);
-
-    res.send(recordsWritten);
-  });
+  res.send(recordsWritten);
 });
