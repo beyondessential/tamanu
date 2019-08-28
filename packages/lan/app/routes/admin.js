6a11ce007e9fbc1067eae88797f46451ef0185d7
Tom McLean <tom.a.mclean@gmail.com>
2019-08-28 10:45:07 +1000

    Lint

--- packages/lan/app/routes/admin.js
+++ packages/lan/app/routes/admin.js
@@ -75,25 +75,24 @@ adminRoutes.put('/labTestType', (req, res) => {
 
   // create/update categories
   const categories = db.objects('labTestCategory');
-  const categorisedItems = items.map((testType) => {
+  const categorisedItems = items.map(testType => {
     const { category } = testType;
     const existing = categories.filtered('_id = $0 OR name = $1', category._id, category.name)[0];
-    if(existing) {
+    if (existing) {
       return { ...testType, category: existing };
-    } else {
-      const newCategory = { _id: shortid(), ...category };
-      db.write(() => {
-        const created = db.create('labTestCategory', newCategory);
-        category._id = created._id;
-      });
-      return { ...testType, category: newCategory };
     }
+    const newCategory = { _id: shortid(), ...category };
+    db.write(() => {
+      const created = db.create('labTestCategory', newCategory);
+      category._id = created._id;
+    });
+    return { ...testType, category: newCategory };
   });
 
   const recordsWritten = addOrUpdateMany(
     db,
     'labTestType',
-    categorisedItems, 
+    categorisedItems,
     (objects, item) => objects.filtered('_id = $0 OR name = $1', item._id, item.name)[0],
   );
 
