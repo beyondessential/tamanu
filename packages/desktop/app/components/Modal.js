b031504f1991227cbf83f841d40622a575d248ab
Bastien Goepfert <bas.goepfert@gmail.com>
2019-08-28 15:22:44 +1000

    style base modal

--- packages/desktop/app/components/Modal.js
+++ packages/desktop/app/components/Modal.js
@@ -4,15 +4,25 @@ import { Dialog as MuiDialog, DialogTitle, DialogActions } from '@material-ui/co
 
 const ModalContent = styled.div`
   flex: 1 1 auto;
-  padding: 0 24px 24px;
+  padding: 18px;
+`;
+
+const ModalContainer = styled.div`
+  background: #f3f5f7;
+`;
+
+const DialogTitleComponent = styled(DialogTitle)`
+  padding: 14px !important;
 `;
 
 export const Modal = memo(({ title, children, actions, ...props }) => {
   return (
     <MuiDialog fullWidth maxWidth="sm" {...props}>
-      <DialogTitle>{title}</DialogTitle>
-      <ModalContent>{children}</ModalContent>
-      <DialogActions>{actions}</DialogActions>
+      <DialogTitleComponent>{title}</DialogTitleComponent>
+      <ModalContainer>
+        <ModalContent>{children}</ModalContent>
+        <DialogActions>{actions}</DialogActions>
+      </ModalContainer>
     </MuiDialog>
   );
 });
