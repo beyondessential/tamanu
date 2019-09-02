f947719297a9d2f69a404a12fa378ed8d3d1c784
Tom McLean <tom.a.mclean@gmail.com>
2019-09-02 15:34:53 +1000

    Add referral modal to visit view

---
+++ packages/desktop/app/components/ReferralModal.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Modal } from './Modal';
+import { Suggester } from '../utils/suggester';
+
+import { connectApi } from '../api/connectApi';
+import { reloadVisit } from '../store/visit';
+
+import { ReferralForm } from '../forms/ReferralForm';
+
+const DumbReferralModal = React.memo(
+  ({ open, icd10Suggester, practitionerSuggester, onClose, onCreateReferral }) => {
+    return (
+      <Modal title="Check in" open={open} onClose={onClose}>
+        <ReferralForm
+          onSubmit={onCreateReferral}
+          onCancel={onClose}
+          practitionerSuggester={practitionerSuggester}
+          icd10Suggester={icd10Suggester}
+        />
+      </Modal>
+    );
+  },
+);
+
+export const ReferralModal = connectApi((api, dispatch, { visitId }) => ({
+  onCreateReferral: async data => {
+    await api.post(`visit/${visitId}/referrals`, data);
+    dispatch(reloadVisit(visitId));
+  },
+  icd10Suggester: new Suggester(api, 'icd10'),
+  practitionerSuggester: new Suggester(api, 'practitioner'),
+}))(DumbReferralModal);
