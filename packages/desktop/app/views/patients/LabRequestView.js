f815e800c9decaf1cb97518f7f4c268c8933bcae
Tom McLean <tom.a.mclean@gmail.com>
2019-09-02 11:02:47 +1000

    Lint

--- packages/desktop/app/views/patients/LabRequestView.js
+++ packages/desktop/app/views/patients/LabRequestView.js
@@ -49,8 +49,8 @@ const LabRequestInfoPane = React.memo(({ labRequest }) => (
   <FormGrid columns={3}>
     <TextInput value={labRequest._id} label="Request ID" />
     <TextInput value={(labRequest.category || {}).name} label="Request type" />
-    <TextInput value={labRequest.urgent ? "Urgent" : "Standard"} label="Urgency" />
-    <TextInput value={LAB_REQUEST_STATUS_LABELS[labRequest.status] || "Unknown"} label="Status" />
+    <TextInput value={labRequest.urgent ? 'Urgent' : 'Standard'} label="Urgency" />
+    <TextInput value={LAB_REQUEST_STATUS_LABELS[labRequest.status] || 'Unknown'} label="Status" />
     <DateInput value={labRequest.requestedDate} label="Requested date" />
     <DateTimeInput value={labRequest.sampleTime} label="Sample date" />
     <TextInput multiline value={labRequest.notes} label="Notes" style={{ gridColumn: '1 / -1' }} />
