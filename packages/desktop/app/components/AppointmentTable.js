e643fa5111f911eb8f967ea6df17ed77506c3ba3
Tom McLean <tom.a.mclean@gmail.com>
2019-09-04 16:44:48 +1000

    Update some fields to match appointment form

--- packages/desktop/app/components/AppointmentTable.js
+++ packages/desktop/app/components/AppointmentTable.js
@@ -20,22 +20,16 @@ const StatusDisplay = React.memo(({ status }) => {
 const getDate = ({ date }) => <DateDisplay date={date} />;
 const getDepartment = ({ location }) => (location ? location.name : 'Unknown');
 const getFacility = ({ facility }) => (facility ? facility.name : 'Unknown');
-const getDisplayName = ({ referringDoctor }) => (referringDoctor || {}).displayName || 'Unknown';
+const getDisplayName = ({ practitioner }) => (practitioner || {}).displayName || 'Unknown';
 const getStatus = ({ status }) => <StatusDisplay status={status} />;
 
 const columns = [
   { key: 'date', title: 'Appointment date', accessor: getDate },
   { key: 'department', title: 'Department', accessor: getDepartment },
-  { key: 'facility', title: 'Facility', accessor: getFacility },
-  { key: 'practitioner', title: 'Referring doctor', accessor: getDisplayName },
+  { key: 'practitioner', title: 'Practitioner', accessor: getDisplayName },
   { key: 'status', title: 'Status', accessor: getStatus },
 ];
 
-const DumbAppointmentTable = React.memo(({ appointments, onAppointmentSelect }) => (
-  <Table columns={columns} data={appointments} onRowClick={row => onAppointmentSelect(row)} />
+export const AppointmentTable = React.memo(({ appointments }) => (
+  <Table columns={columns} data={appointments} />
 ));
-
-export const AppointmentTable = connect(
-  null,
-  dispatch => ({ onAppointmentSelect: appointment => dispatch(viewAppointment(appointment._id)) }),
-)(DumbAppointmentTable);
--- packages/shared/schemas/appointment.js
+++ packages/shared/schemas/appointment.js
@@ -6,45 +6,17 @@ export const AppointmentSchema = {
   primaryKey: '_id',
   properties: {
     _id: 'string',
-    allDay: 'bool',
-    provider: {
-      type: 'string',
-      optional: true,
-    },
-    location: {
-      type: 'string',
-      optional: true,
-    },
-    appointmentType: {
-      type: 'string',
-      optional: true,
-    },
-    startDate: {
-      type: 'date',
-      optional: true,
-    },
-    endDate: {
-      type: 'date',
-      optional: true,
-    },
-    notes: {
-      type: 'string',
-      optional: true,
-    },
+    location: 'location?',
+    practitioner: 'user?',
+    date: 'date',
+
+    endDate: 'date?',
     status: {
       type: 'string',
-      optional: true,
       default: APPOINTMENT_STATUSES.SCHEDULED,
     },
-    visits: {
-      type: 'list',
-      objectType: 'visit',
-    },
-    patients: {
-      type: 'linkingObjects',
-      objectType: 'patient',
-      property: 'appointments',
-    },
+
+    patients: { type: 'linkingObjects', objectType: 'patient', property: 'appointments' },
     ...defaults,
   },
 };
