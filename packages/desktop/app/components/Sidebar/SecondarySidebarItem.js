05cf2bbcc9fc8618320b08cdc77b997eba2d61c9
Bastien Goepfert <bas.goepfert@gmail.com>
2019-09-23 11:34:23 +1000

    remove extra padding on secondary items

--- packages/desktop/app/components/Sidebar/SecondarySidebarItem.js
+++ packages/desktop/app/components/Sidebar/SecondarySidebarItem.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import ListItem from '@material-ui/core/ListItem';
 import { SidebarItemText } from './SidebarItemText';
-
-const Icon = styled.i`
-  margin-right: 5px;
-`;
 
 export const SecondarySidebarItem = React.memo(
   ({ path, icon, label, isCurrent, disabled, onClick }) => (
     <ListItem button to={path} disabled={disabled} selected={isCurrent} onClick={onClick}>
-      <Icon className={icon} />
+      <i className={icon} />
       <SidebarItemText disableTypography primary={label} />
     </ListItem>
   ),
