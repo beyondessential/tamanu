fbab283099353f4cf422e214554a05e5b47da9a5
Tom McLean <tom.a.mclean@gmail.com>
2019-10-09 15:37:14 +1100

    Flesh out change type form a little

--- packages/desktop/app/forms/ChangeVisitTypeForm.js
+++ packages/desktop/app/forms/ChangeVisitTypeForm.js
@@ -1,5 +1,34 @@
 import React from 'react';
 
-export const ChangeVisitTypeForm = ({ visit, ...otherProps }) => {
-  return JSON.stringify(otherProps);
+import {
+  Form,
+  Field,
+  DateTimeField,
+  AutocompleteField,
+  TextField,
+  CheckField,
+  SelectField,
+} from '../components/Field';
+import { FormGrid } from '../components/FormGrid';
+import { ConfirmCancelRow } from '../components/ButtonRow';
+
+const FormContents = ({ submitForm, onCancel, values }) => (
+  <FormGrid columns={1}>
+    <div>{`Changing visit to type ${values.visitType}`}</div>
+    <ConfirmCancelRow onConfirm={submitForm} confirmText="Save" onCancel={onCancel} />
+  </FormGrid>
+);
+
+export const ChangeVisitTypeForm = ({ visit, onSubmit, extraRoute, onCancel }) => {
+
+  return (
+    <Form
+      initialValues={{
+        visitType: extraRoute,
+      }}
+      render={FormContents}
+      onSubmit={onSubmit}
+      onCancel={onCancel}
+    />
+  ); 
 };
