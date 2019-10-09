50f7469e79b4df2eb10f508a4e7769ee51960be5
Tom McLean <tom.a.mclean@gmail.com>
2019-10-09 17:52:28 +1100

    Use visitType endpoint to change visit type

--- packages/desktop/app/components/ChangeTypeModal.js
+++ packages/desktop/app/components/ChangeTypeModal.js
@@ -15,7 +15,7 @@ const DumbChangeTypeModal = React.memo(({ open, visit, onClose, onSubmit, ...res
 
 export const ChangeTypeModal = connectApi((api, dispatch, { visit }) => ({
   onSubmit: async data => {
-    await api.put(`visit/${visit._id}`, data);
+    await api.put(`visit/${visit._id}/visitType`, data);
     dispatch(viewVisit(visit._id));
   },
 }))(DumbChangeTypeModal);
