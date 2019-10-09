03d7a5eca267323688d53fcb3fba87d2db55b2a4
Tom McLean <tom.a.mclean@gmail.com>
2019-10-09 17:15:18 +1100

    Track what kind of visit it's changing from

--- packages/desktop/app/forms/ChangeVisitTypeForm.js
+++ packages/desktop/app/forms/ChangeVisitTypeForm.js
@@ -1,34 +1,33 @@
 import React from 'react';
 
-import {
-  Form,
-  Field,
-  DateTimeField,
-  AutocompleteField,
-  TextField,
-  CheckField,
-  SelectField,
-} from '../components/Field';
+import { Form } from '../components/Field';
 import { FormGrid } from '../components/FormGrid';
 import { ConfirmCancelRow } from '../components/ButtonRow';
 
-const FormContents = ({ submitForm, onCancel, values }) => (
-  <FormGrid columns={1}>
-    <div>{`Changing visit to type ${values.visitType}`}</div>
-    <ConfirmCancelRow onConfirm={submitForm} confirmText="Save" onCancel={onCancel} />
-  </FormGrid>
-);
+export class ChangeVisitTypeForm extends React.PureComponent {
+  
+  renderForm = ({ submitForm, values }) => { 
+    const { onCancel, visit } = this.props;
+    const currentType = visit.visitType;
+    return (
+      <FormGrid columns={1}>
+        <div>{`Changing visit from ${currentType} to type ${values.visitType}`}</div>
+        <ConfirmCancelRow onConfirm={submitForm} confirmText="Save" onCancel={onCancel} />
+      </FormGrid>
+    );
+  };
 
-export const ChangeVisitTypeForm = ({ visit, onSubmit, extraRoute, onCancel }) => {
+  render() {
+    const { extraRoute, onSubmit } = this.props;
+    return (
+      <Form
+        initialValues={{
+          visitType: extraRoute,
+        }}
+        render={this.renderForm}
+        onSubmit={onSubmit}
+      />
+    );
+  }
+}
 
-  return (
-    <Form
-      initialValues={{
-        visitType: extraRoute,
-      }}
-      render={FormContents}
-      onSubmit={onSubmit}
-      onCancel={onCancel}
-    />
-  ); 
-};
