511411527a23baf10a4bb63d57c53286eb3ceefd
Tom McLean <tom.a.mclean@gmail.com>
2019-10-14 14:43:40 +1100

    Add table to show all lab requests

--- packages/desktop/app/routes/LabsRoutes.js
+++ packages/desktop/app/routes/LabsRoutes.js
@@ -3,18 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 
 import { NotActiveView } from '../views';
 
-export const LabsRoutes = React.memo(({ match }) => {
-  return (
-    <Switch>
-      <Route exact path={match.path} component={NotActiveView} />
-      <Route path={`${match.path}/requests`} component={NotActiveView} />
-      <Route path={`${match.path}/request/by-patient/:patientId/:id`} component={NotActiveView} />
-      <Route path={`${match.path}/request/by-patient/:patientId`} component={NotActiveView} />
-      <Route path={`${match.path}/request/:id`} component={NotActiveView} />
-      <Route path={`${match.path}/request`} component={NotActiveView} />
-      <Route path={`${match.path}/completed`} component={NotActiveView} />
-      <Route path={`${match.path}/edit/new`} component={NotActiveView} />
-      <Route path={`${match.path}/edit/:id`} component={NotActiveView} />
-    </Switch>
-  );
-});
+import { LabRequestListingView } from '../views/LabRequestListingView';
+
+export const LabsRoutes = React.memo(({ match }) => (
+  <Switch>
+    <Route exact path={match.path} component={LabRequestListingView} />
+    <Route path={`${match.path}/requests`} component={LabRequestListingView} />
+    <Route path={`${match.path}/request/by-patient/:patientId/:id`} component={NotActiveView} />
+    <Route path={`${match.path}/request/by-patient/:patientId`} component={NotActiveView} />
+    <Route path={`${match.path}/request/:id`} component={NotActiveView} />
+    <Route path={`${match.path}/request`} component={NotActiveView} />
+    <Route path={`${match.path}/completed`} component={NotActiveView} />
+    <Route path={`${match.path}/edit/new`} component={NotActiveView} />
+    <Route path={`${match.path}/edit/:id`} component={NotActiveView} />
+  </Switch>
+));
