597287ead08ee4a7b48fd46451a724c780395400
Tom McLean <tom.a.mclean@gmail.com>
2019-10-18 10:41:16 +1100

    Add imaging listing to routes

--- packages/desktop/app/routes/ImagingRoutes.js
+++ packages/desktop/app/routes/ImagingRoutes.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { NotActiveView } from '../views';
+import { ImagingRequestListingView } from '../views/ImagingRequestListingView';
 
 export const ImagingRoutes = React.memo(({ match }) => {
   return (
     <div>
       <Switch>
-        <Route exact path={match.path} component={NotActiveView} />
-        <Route path={`${match.path}/requests`} component={NotActiveView} />
+        <Route exact path={match.path} component={ImagingRequestListingView} />
+        <Route path={`${match.path}/requests`} component={ImagingRequestListingView} />
         <Route path={`${match.path}/request/by-patient/:patientId/:id`} component={NotActiveView} />
         <Route path={`${match.path}/request/by-patient/:patientId`} component={NotActiveView} />
         <Route path={`${match.path}/request/:id`} component={NotActiveView} />
