a1312bfe7276d0c0a2f68ecd23b43a425d3d5817
Tom McLean <tom.a.mclean@gmail.com>
2019-10-23 11:58:41 +1100

    Implement a separate form for manual result

--- packages/desktop/app/components/ManualLabResultModal.js
+++ packages/desktop/app/components/ManualLabResultModal.js
@@ -4,13 +4,35 @@ import { Modal } from './Modal';
 import { connectApi } from '../api/connectApi';
 import { reloadLab } from '../store/labRequest';
 
+import { Form, Field, NumberField } from './Field';
+import { FormGrid } from './FormGrid';
+import { ConfirmCancelRow } from './ButtonRow';
+
+const ManualResultForm = ({ labTest, onSubmit, onClose }) => {
+  const renderForm = React.useCallback(({ submitForm }) => (
+    <FormGrid columns={1}>
+      <Field
+        name="result"
+        required
+        component={NumberField}
+      />
+      <ConfirmCancelRow onConfirm={submitForm} onCancel={onClose} />
+    </FormGrid>
+  ));
+
+  return (<Form
+    onSubmit={onSubmit}
+    render={renderForm}
+  />);
+};
+
 export const ManualLabResultModal = connectApi((api, dispatch, { labTest, labRequest }) => ({
   onSubmit: async data => {
-    await api.put(`labTest/${labTest._id}`, data);
+    await api.put(`labTest/${labTest._id}`, { result: `${data.result}` });
     dispatch(reloadLab(labRequest._id));
   },
 }))(({ open, labTest, onClose, onSubmit }) => (
   <Modal open={!!labTest} onClose={onClose} title="Enter lab result">
-    <button onClick={() => onSubmit({ result: "100" })}>100</button>
+    <ManualResultForm labTest={labTest} onSubmit={onSubmit} onClose={onClose} />
   </Modal>
 ));
