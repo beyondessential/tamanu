526c43bf410b6ce6efde54fbd0092e14779cc0ae
Tom McLean <tom.a.mclean@gmail.com>
2019-10-30 16:20:16 +1100

    Clean up get controller a bit

--- packages/lan/app/controllers/realm/get.js
+++ packages/lan/app/controllers/realm/get.js
@@ -16,58 +16,55 @@ export default function(req, res) {
   const defaultPageSize = 10;
 
   try {
-    return db.write(() => {
-      let objects = db.objects(modelName);
-      const filters = [];
+    // If id is provided, return single object
+    if (id) {
+      const object = db.objectForPrimaryKey(modelName, id);
+      if (!object) return res.status(404).end();
+      // Get first item from the list
+      const data = objectToJSON(object, OBJECTS_MAX_DEPTH);
+      return res.json(data);
+    }
 
-      // If id is provided, return single object
-      if (id) {
-        objects = objects.filtered(`_id = '${id}'`);
-        if (objects.length <= 0) return res.status(404).end();
-        // Get first item from the list
-        const object = objectToJSON(objects[0], OBJECTS_MAX_DEPTH);
-        return res.json(object);
+    // Add any additional filters from query parameters
+    const { properties: fieldSchemata } = db.schema.find(({ name }) => name === modelName);
+    const filters = Object.entries(restOfQuery).map(([field, value]) => {
+      const fieldSchema = fieldSchemata[field] || {};
+      const isString = fieldSchema === 'string' || fieldSchema.type === 'string';
+      let operator = '=';
+      let newValue = value;
+      if (/([|])/.test(value)) {
+        [operator, newValue] = value.split('|');
+      } else if (isString && fuzzy) {
+        // one day this could be proper fuzzy matching!
+        operator = 'CONTAINS[c]';
       }
-
-      // Add any additional filters from query parameters
-      const { properties: fieldSchemata } = db.schema.find(({ name }) => name === modelName);
-      Object.entries(restOfQuery).forEach(([field, value]) => {
-        const fieldSchema = fieldSchemata[field] || {};
-        const isString = fieldSchema === 'string' || fieldSchema.type === 'string';
-        let operator = '=';
-        let newValue = value;
-        if (/([|])/.test(value)) {
-          [operator, newValue] = value.split('|');
-        } else if (isString && fuzzy) {
-          // one day this could be proper fuzzy matching!
-          operator = 'CONTAINS[c]';
-        }
-        const valueString = isString ? `"${newValue}"` : newValue;
-        filters.push(`${field} ${operator} ${valueString}`);
-      });
-
-      // Filter collection on all filters
-      if (!isEmpty(filters)) objects = objects.filtered(filters.join(' AND '));
-
-      // Sort results
-      if (orderBy) objects = objects.sorted(orderBy, order === 'desc');
-
-      // Create pagination options before limiting
-      const response = { count: objects.length };
-
-      // Limit results
-      if (currentPageString) {
-        const currentPage = parseInt(currentPageString);
-        const pageSize = pageSizeString ? parseInt(pageSizeString) : defaultPageSize;
-        const start = currentPage * pageSize;
-        const end = start + pageSize;
-        objects = objects.slice(start, end);
-      }
-
-      // Convert to JSON as response
-      response.data = objects.map(object => objectToJSON(object, OBJECTS_MAX_DEPTH));
-      return res.send(response);
+      const valueString = isString ? `"${newValue}"` : newValue;
+      return `${field} ${operator} ${valueString}`;
     });
+
+    let objects = db.objects(modelName);
+
+    // Filter collection on all filters
+    if (!isEmpty(filters)) objects = objects.filtered(filters.join(' AND '));
+
+    // Sort results
+    if (orderBy) objects = objects.sorted(orderBy, order === 'desc');
+
+    // Create pagination options before limiting
+    const response = { count: objects.length };
+
+    // Limit results
+    if (currentPageString) {
+      const currentPage = parseInt(currentPageString);
+      const pageSize = pageSizeString ? parseInt(pageSizeString) : defaultPageSize;
+      const start = currentPage * pageSize;
+      const end = start + pageSize;
+      objects = objects.slice(start, end);
+    }
+
+    // Convert to JSON as response
+    response.data = objects.map(object => objectToJSON(object, OBJECTS_MAX_DEPTH));
+    return res.send(response);
   } catch (err) {
     console.error(err);
     return res.status(500).send(err.toString());
