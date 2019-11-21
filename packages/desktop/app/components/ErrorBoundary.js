2c20372e93bb4bdf714dbd7c83c7114e95b4a065
Tom McLean <tom.a.mclean@gmail.com>
2019-11-21 15:01:53 +1100

    Wrap the main view pane in an error boundary w/ more debug info

--- packages/desktop/app/components/ErrorBoundary.js
+++ packages/desktop/app/components/ErrorBoundary.js
@@ -1,29 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 
-const ErrorMessage = styled.div`
-  display: block;
-  background: red;
-  width: 100%;
-  height: 100%;
-  color: white;
-  cursor: pointer;
+import { ContentPane } from './ContentPane';
+
+const DebugInfo = styled.pre`
+  max-height: 10rem;
+  max-width: 50rem;
+  overflow: scroll;
+  padding: 1rem;
+  border: 1px solid grey;
+  background: #fff;
+  color: #000;
 `;
+
+const DumbErrorView = React.memo(({ error, state }) => {
+  const showError = React.useCallback(() => { console.log(error) });
+  const showState = React.useCallback(() => { console.log(state) });
+  
+  return (
+    <ContentPane>
+      <h2>Oops!</h2>
+      <p>The application encountered an error when trying to display this information.</p>
+      <p>The details of the error are:</p>
+      <DebugInfo onClick={showError}>{error.stack}</DebugInfo>
+      <p>The contents of the application state are:</p>
+      <DebugInfo onClick={showState}>{JSON.stringify(state, null, 2)}</DebugInfo>
+    </ContentPane>
+  );
+});
+
+const ErrorView = connect(
+  state => ({ state })
+)(DumbErrorView);
 
 export class ErrorBoundary extends React.PureComponent {
   state = { error: null };
-
-  showMessage = () => {
-    console.log(this.state.error);
-  };
 
   componentDidCatch(error) {
     this.setState({ error });
   }
 
+  componentDidUpdate(prevProps) {
+    if(prevProps.errorKey !== this.props.errorKey) {
+      this.setState({ error: null });
+    }
+  }
+
   render() {
-    if (this.state.error) {
-      return <ErrorMessage onClick={this.showMessage}>ERROR</ErrorMessage>;
+    const { ErrorComponent = ErrorView } = this.props;
+    const { error } = this.state;
+    if (error) {
+      return <ErrorComponent error={error} />;
     }
 
     return this.props.children || null;
