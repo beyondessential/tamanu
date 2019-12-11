acd5fdf1ba1b1bc2186fa91039dd3ba75a9895c6
Tom McLean <tom.a.mclean@gmail.com>
2019-12-11 14:52:53 +1100

    Update date formatting on query

--- packages/desktop/app/views/reports/VillageDiagnosesByWeekReport.js
+++ packages/desktop/app/views/reports/VillageDiagnosesByWeekReport.js
@@ -5,18 +5,31 @@ import { Table } from '../../components/Table';
 import { LoadingIndicator } from '../../components/LoadingIndicator';
 import { ReportGraph } from './ReportGraph';
 
-const Results = ({ columns, data }) => {
+import moment from 'moment';
+
+const Results = React.memo(({ data }) => {
   if(!data) return <LoadingIndicator loading />;
+
+  const { results, meta } = data;
+  const dataColumns = meta.columns.map((d, i) => ({
+    title: d,
+    key: `values[${i}]`,
+    accessor: row => row.values[i] || '',
+  }));
+
+  const columns = [ { key: 'formatted', title: meta.title }, ...dataColumns, ];
+
   return (
     <div>
-      <Table data={data} columns={columns} />
+      <Table data={results} columns={columns} />
     </div>
   );
-};
+});
 
-const Report = React.memo(({ runQuery, FilterForm  }) => {
+const Report = React.memo(({ onRunQuery, FilterForm  }) => {
   const [filters, setFilters] = useState(null);
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const onFilterSubmit = useCallback(data => setFilters(data));
 
@@ -25,8 +38,10 @@ const Report = React.memo(({ runQuery, FilterForm  }) => {
     (async () => {
       setData(null);
       if(filters) {
-        const result = await runQuery(filters);
+        setLoading(true);
+        const result = await onRunQuery(filters);
         if(canceled) return;
+        setLoading(false);
         setData(result);
       }
     })();
@@ -34,46 +49,48 @@ const Report = React.memo(({ runQuery, FilterForm  }) => {
     return () => { canceled = true };
   }, [filters]);
 
-  const title = "Diagnoses";
-  const dataColumns = (data && data.length > 0)
-    ? data[0].values.map((d, i) => ({
-      title: `${i}`,
-      key: `values[${i}]`,
-      accessor: row => row.values[i] || '',
-    }))
-    : [];
-
-  const columns = [ { key: 'formatted', title }, ...dataColumns, ];
-
   return (
     <div>
       <FilterForm onSubmit={onFilterSubmit} />
-      { filters && <Results data={data} columns={columns} /> }
+      { filters && <Results data={data} /> }
     </div>
   );
 });
 
-async function runQuery(filters) {
+async function onRunQuery(filters) {
   // TODO: api request
   await new Promise(resolve => setTimeout(resolve, 1000));
 
   const { diagnoses, startDate, endDate } = filters;
-  return diagnoses.map(({ _id }) => ({
+  const start = moment(startDate);
+  const end = moment(endDate);
+  const ranges = [ ];
+
+  let date = moment(start);
+  while(date < end) {
+    ranges.push(date);
+    date = moment(date).add(1, 'week');
+  }
+
+  const columns = ranges.map(x => x.format('DD/MM/YYYY'));
+
+  const results = diagnoses.map(({ _id }) => ({
     key: _id,
-    formatted: "K" + _id,
-    values: [
-      Math.floor(Math.random() * 10),
-      Math.floor(Math.random() * 10),
-      Math.floor(Math.random() * 10),
-      Math.floor(Math.random() * 10),
-      Math.floor(Math.random() * 10),
-    ]
+    formatted: "~~!!~~ " + _id,
+    values: columns.map(c => Math.floor(Math.random() * 10)),
   }));
+
+  const meta = {
+    title: "Diagnosis",
+    columns,
+  };
+
+  return { results, meta };
 }
 
 export const VillageDiagnosesByWeekReport = ({ icd10Suggester }) => (
   <Report
     FilterForm={props => <ReportGeneratorForm icd10Suggester={icd10Suggester} {...props} />}
-    runQuery={runQuery}
+    onRunQuery={onRunQuery}
   />
 );
