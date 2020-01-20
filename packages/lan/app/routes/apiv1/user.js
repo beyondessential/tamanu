e807146fd2f47117995e77d8aa5b168ef501af76
Tom McLean <tom.a.mclean@gmail.com>
2020-01-20 14:59:24 +1100

    Add CRUD helpers

--- packages/lan/app/routes/apiv1/user.js
+++ packages/lan/app/routes/apiv1/user.js
@@ -1,42 +1,9 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 
-import { NotFoundError } from 'Lan/app/errors';
+import { simpleGet, simplePut, simplePost } from './crudHelpers';
 
 export const user = express.Router();
 
-user.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { models, params } = req;
-    const u = await models.User.findByPk(params.id);
-    if (!u) return new NotFoundError();
-    res.send(u);
-  }),
-);
-
-user.put(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { models, params } = req;
-    const u = await models.User.findByPk(params.id);
-    if (!u) return new NotFoundError();
-    await u.update(req.body);
-    res.send(u);
-  }),
-);
-
-user.post(
-  '/$',
-  asyncHandler(async (req, res) => {
-    const { models } = req;
-    const { password, ...details } = req.body;
-    const u = await models.User.build(details);
-    if (password) {
-      await u.setPassword(password);
-    }
-    await u.save();
-
-    res.send(u);
-  }),
-);
+user.get('/:id', simpleGet('User'));
+user.put('/:id', simplePut('User'));
+user.post('/$', simplePost('User'));
--- packages/lan/app/routes/apiv1/visit.js
+++ packages/lan/app/routes/apiv1/visit.js
@@ -1,37 +1,11 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 
-import { NotFoundError } from 'Lan/app/errors';
+import { simpleGet, simplePut, simplePost, simpleGetList } from './crudHelpers';
 
 export const visit = express.Router();
 
-visit.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { models, params } = req;
-    const v = await models.Visit.findByPk(params.id);
-    if (!v) throw new NotFoundError();
-    res.send(v);
-  }),
-);
+visit.get('/:id', simpleGet('Visit'));
+visit.put('/:id', simplePut('Visit'));
+visit.post('/$', simplePost('Visit'));
 
-visit.put(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const { models, params } = req;
-    const v = await models.Visit.findByPk(params.id);
-    if (!v) throw new NotFoundError();
-    await v.update(req.body);
-    res.send(v);
-  }),
-);
-
-visit.post(
-  '/$',
-  asyncHandler(async (req, res) => {
-    const { models } = req;
-    const v = await models.Visit.create(req.body);
-
-    res.send(v);
-  }),
-);
+visit.get('/:id/vitals', simpleGetList('Vitals', 'visitId'));
