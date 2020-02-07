9d06b7f031a10cdd977e81ef728e6b11bbe7b013
Tom McLean <tom.a.mclean@gmail.com>
2020-02-07 11:59:40 +1100

    Add endpoints for managing reference data

--- packages/lan/app/routes/apiv1/crudHelpers.js
+++ packages/lan/app/routes/apiv1/crudHelpers.js
@@ -5,15 +5,20 @@ import { NotFoundError } from 'lan/app/errors';
 export const simpleGet = modelName =>
   asyncHandler(async (req, res) => {
     const { models, params } = req;
+    // check the user can read this model type before searching for it
+    // (otherwise, they can see if they get a "not permitted" or a 
+    // "not found" to snoop for objects)
+    req.checkPermission('read', modelName);
     const object = await models[modelName].findByPk(params.id);
-    req.checkPermission('read', object);
     if (!object) throw new NotFoundError();
+    req.checkPermission('read', object);
     res.send(object);
   });
 
 export const simplePut = modelName =>
   asyncHandler(async (req, res) => {
     const { models, params } = req;
+    req.checkPermission('read', modelName);
     const object = await models[modelName].findByPk(params.id);
     if (!object) throw new NotFoundError();
     req.checkPermission('write', object);
