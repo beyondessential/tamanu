8979e89fc9b318cf1e10b5ad7cd75d89dd37f324
Tom McLean <tom.a.mclean@gmail.com>
2020-03-02 16:41:29 +1100

    Add diagnoses by village report

--- packages/lan/app/reports/index.js
+++ packages/lan/app/reports/index.js
@@ -63,8 +63,26 @@ const returnVivaxReport = async (db, { startDate, endDate }) => {
   };
 };
 
-const diagnosesByVillageReport = async (db, params) => {
+const diagnosesByVillageReport = async (db, { startDate, endDate, village }) => {
+  const baseDiagnoses = db.objects('patientDiagnosis')
+    .filtered("date >= $0 AND date <= $1 AND visit.patient.village.name = $2", startDate, endDate, village)
 
+  const counts = {};
+
+  const rowData = baseDiagnoses.forEach(d => {
+    if(!counts[d.diagnosis._id]) {
+      counts[d.diagnosis._id] = {
+        count: 0,
+        diagnosis: d.diagnosis.name,
+      };
+    }
+    counts[d.diagnosis._id].count += 1;
+  });
+
+  return {
+    headers: ['diagnosis', 'count'],
+    rowData: Object.values(counts),
+  };
 };
 
 const anemiaVivaxCodiagnosesReport = async (db, { startDate, endDate }) => {
