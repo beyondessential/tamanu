ccf9a6cc4c75c57c97ceaa2c7e918e7f01ed3fdd
Tom McLean <tom.a.mclean@gmail.com>
2020-03-02 16:51:55 +1100

    Lint

--- packages/lan/app/reports/index.js
+++ packages/lan/app/reports/index.js
@@ -4,12 +4,13 @@ import moment from 'moment';
 const formatPatientInfo = p => ({
   name: [p.firstName, p.culturalName, p.lastName].filter(x => x).join(' '),
   village: p.village && p.village.name,
-  id: p.displayId
+  id: p.displayId,
 });
 
 const visitsReport = async (db, { startDate, endDate }) => {
-  const rowData = db.objects('visit')
-    .filtered("startDate >= $0 AND startDate <= $1", startDate, endDate)
+  const rowData = db
+    .objects('visit')
+    .filtered('startDate >= $0 AND startDate <= $1', startDate, endDate)
     .map(visit => ({
       ...formatPatientInfo(visit.patient[0]),
       visitType: visit.visitType,
@@ -22,12 +23,11 @@ const visitsReport = async (db, { startDate, endDate }) => {
   };
 };
 
-const numberOfVisitsReport = async (db) => {
-  const rowData = db.objects('patient')
-    .map(patient => ({
-      ...formatPatientInfo(patient),
-      visits: patient.visits.length,
-    }));
+const numberOfVisitsReport = async db => {
+  const rowData = db.objects('patient').map(patient => ({
+    ...formatPatientInfo(patient),
+    visits: patient.visits.length,
+  }));
 
   return {
     headers: ['id', 'name', 'village', 'visits'],
@@ -36,17 +36,18 @@ const numberOfVisitsReport = async (db) => {
 };
 
 const returnVivaxReport = async (db, { startDate, endDate }) => {
-  const baseDiagnoses = db.objects('patientDiagnosis')
+  const baseDiagnoses = db
+    .objects('patientDiagnosis')
     .filtered("diagnosis.name CONTAINS[c] 'vivax'")
-    .filtered("date >= $0 AND date <= $1", startDate, endDate)
-    .sorted("date");
-  
+    .filtered('date >= $0 AND date <= $1', startDate, endDate)
+    .sorted('date');
+
   const counts = {};
   const repeats = {};
   baseDiagnoses.forEach(x => {
     const patientId = x.visit[0].patient[0]._id;
     counts[patientId] = (counts[patientId] || 0) + 1;
-    if(counts[patientId] > 1) {
+    if (counts[patientId] > 1) {
       repeats[patientId] = x;
     }
   });
@@ -64,13 +65,19 @@ const returnVivaxReport = async (db, { startDate, endDate }) => {
 };
 
 const diagnosesByVillageReport = async (db, { startDate, endDate, village }) => {
-  const baseDiagnoses = db.objects('patientDiagnosis')
-    .filtered("date >= $0 AND date <= $1 AND visit.patient.village.name = $2", startDate, endDate, village)
+  const baseDiagnoses = db
+    .objects('patientDiagnosis')
+    .filtered(
+      'date >= $0 AND date <= $1 AND visit.patient.village.name = $2',
+      startDate,
+      endDate,
+      village,
+    );
 
   const counts = {};
 
-  const rowData = baseDiagnoses.forEach(d => {
-    if(!counts[d.diagnosis._id]) {
+  baseDiagnoses.forEach(d => {
+    if (!counts[d.diagnosis._id]) {
       counts[d.diagnosis._id] = {
         count: 0,
         diagnosis: d.diagnosis.name,
@@ -86,23 +93,19 @@ const diagnosesByVillageReport = async (db, { startDate, endDate, village }) => 
 };
 
 const anemiaVivaxCodiagnosesReport = async (db, { startDate, endDate }) => {
-  const baseDiagnoses = db.objects('patientDiagnosis')
-    .filtered("date >= $0 AND date <= $1", startDate, endDate);
+  const baseDiagnoses = db
+    .objects('patientDiagnosis')
+    .filtered('date >= $0 AND date <= $1', startDate, endDate);
 
-  const vivaxDiagnoses = baseDiagnoses
-    .filtered("diagnosis.name CONTAINS[c] 'vivax'");
+  const vivaxDiagnoses = baseDiagnoses.filtered("diagnosis.name CONTAINS[c] 'vivax'");
 
-  const anemiaDiagnoses = baseDiagnoses
-    .filtered("diagnosis.name CONTAINS[c] 'anemia'");
+  const anemiaDiagnoses = baseDiagnoses.filtered("diagnosis.name CONTAINS[c] 'anemia'");
 
   const vivaxPatientIds = new Set(vivaxDiagnoses.map(d => d.visit[0].patient[0]._id));
   const anemiaPatientIds = new Set(anemiaDiagnoses.map(d => d.visit[0].patient[0]._id));
   const bothPatientIds = new Set([...vivaxPatientIds].filter(x => anemiaPatientIds.has(x)));
 
-  const rowData = [
-    ...vivaxDiagnoses,
-    ...anemiaDiagnoses
-  ]
+  const rowData = [...vivaxDiagnoses, ...anemiaDiagnoses]
     .filter(d => bothPatientIds.has(d.visit[0].patient[0]._id))
     .map(d => ({
       ...formatPatientInfo(d.visit[0].patient[0]),
@@ -124,15 +127,15 @@ const reports = {
   visitsReport,
 };
 
-const tabulate = ({ headers, rowData }) => ([
+const tabulate = ({ headers, rowData }) => [
   headers,
-  ...rowData.map(row => headers.map(h => row[h]))
-]);
+  ...rowData.map(row => headers.map(h => row[h])),
+];
 
 const writeToExcel = async (path, data) => {
   const sheet = XLSX.utils.aoa_to_sheet(tabulate(data));
 
-  const book = new XLSX.utils.book_new();
+  const book = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(book, sheet, 'values');
 
   XLSX.writeFile(book, path);
@@ -140,12 +143,14 @@ const writeToExcel = async (path, data) => {
 
 export const generateReport = async (db, reportName, userParams) => {
   const report = reports[reportName];
-  if(!report) {
-    throw new Error("No such report");
+  if (!report) {
+    throw new Error('No such report');
   }
 
-  const params = { 
-    startDate: moment(userParams.endDate).subtract(1, 'month').toDate(),
+  const params = {
+    startDate: moment(userParams.endDate)
+      .subtract(1, 'month')
+      .toDate(),
     endDate: moment().toDate(),
     ...userParams,
   };
@@ -155,7 +160,7 @@ export const generateReport = async (db, reportName, userParams) => {
   const date = moment().format('YYYY-MM-DD');
   const filename = `${date}_${reportName}.xlsx`;
 
-  const workbook = await writeToExcel(filename, data);
+  await writeToExcel(filename, data);
 
   return {};
 };
