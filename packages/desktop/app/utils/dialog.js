707257eb744c0c024bb2374d16bdd8aea99a973c
Tom McLean <tom.a.mclean@gmail.com>
2020-03-12 15:30:01 +1100

    Platform flexibility for showFileDialog

--- packages/desktop/app/utils/dialog.js
+++ packages/desktop/app/utils/dialog.js
@@ -1,8 +1,21 @@
 import { remote } from 'electron';
 
 export async function showFileDialog(filters, filename) {
-  return remote.dialog.showSaveDialog({
+  const result = remote.dialog.showSaveDialog({
     filters,
     defaultPath: filename,
   });
+
+  // mac just returns a string
+  if(typeof result === "string") {
+    return result;
+  }
+
+  // windows returns an object
+  const { canceled, filePath } = result;
+  if(canceled) {
+    return '';
+  }
+
+  return filePath;
 }
