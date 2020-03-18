e97fa497c8fc49cb535ce64435c220f3e377feea
Tom McLean <tom.a.mclean@gmail.com>
2020-03-18 16:25:24 +1100

    Wrap visit model update in a transaction

--- packages/shared/models/Visit.js
+++ packages/shared/models/Visit.js
@@ -95,38 +95,40 @@ export class Visit extends Model {
   async update(data) {
     const { ReferenceData } = this.sequelize.models;
 
-    if (data.endDate && !this.endDate) {
-      await this.addSystemNote(`Discharged patient.`);
-    }
-
-    if (data.patientId && data.patientId !== this.patientId) {
-      throw new InvalidOperationError("A visit's patient cannot be changed");
-    }
-
-    if (data.visitType && data.visitType !== this.visitType) {
-      await this.addSystemNote(`Changed type from ${this.visitType} to ${data.visitType}`);
-    }
-
-    if (data.locationId && data.locationId !== this.locationId) {
-      const oldLocation = await ReferenceData.findByPk(this.locationId);
-      const newLocation = await ReferenceData.findByPk(data.locationId);
-      if (!newLocation) {
-        throw new InvalidOperationError('Invalid location specified');
+    return this.sequelize.transaction(async () => { 
+      if (data.endDate && !this.endDate) {
+        await this.addSystemNote(`Discharged patient.`);
       }
-      await this.addSystemNote(`Changed location from ${oldLocation.name} to ${newLocation.name}`);
-    }
 
-    if (data.departmentId && data.departmentId !== this.departmentId) {
-      const oldDepartment = await ReferenceData.findByPk(this.departmentId);
-      const newDepartment = await ReferenceData.findByPk(data.departmentId);
-      if (!newDepartment) {
-        throw new InvalidOperationError('Invalid department specified');
+      if (data.patientId && data.patientId !== this.patientId) {
+        throw new InvalidOperationError("A visit's patient cannot be changed");
       }
-      await this.addSystemNote(
-        `Changed department from ${oldDepartment.name} to ${newDepartment.name}`,
-      );
-    }
 
-    return super.update(data);
+      if (data.visitType && data.visitType !== this.visitType) {
+        await this.addSystemNote(`Changed type from ${this.visitType} to ${data.visitType}`);
+      }
+
+      if (data.locationId && data.locationId !== this.locationId) {
+        const oldLocation = await ReferenceData.findByPk(this.locationId);
+        const newLocation = await ReferenceData.findByPk(data.locationId);
+        if (!newLocation) {
+          throw new InvalidOperationError('Invalid location specified');
+        }
+        await this.addSystemNote(`Changed location from ${oldLocation.name} to ${newLocation.name}`);
+      }
+
+      if (data.departmentId && data.departmentId !== this.departmentId) {
+        const oldDepartment = await ReferenceData.findByPk(this.departmentId);
+        const newDepartment = await ReferenceData.findByPk(data.departmentId);
+        if (!newDepartment) {
+          throw new InvalidOperationError('Invalid department specified');
+        }
+        await this.addSystemNote(
+          `Changed department from ${oldDepartment.name} to ${newDepartment.name}`,
+        );
+      }
+
+      return super.update(data);
+    });
   }
 }
