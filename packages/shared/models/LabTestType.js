92d2cf2115d091146bc5f22dfaaec7227799d846
Tom McLean <tom.a.mclean@gmail.com>
2020-03-20 15:12:59 +1100

    Update fields to match realm schema a little better

--- packages/shared/models/LabTestType.js
+++ packages/shared/models/LabTestType.js
@@ -1,6 +1,13 @@
 import { Sequelize } from 'sequelize';
 import { Model } from './Model';
 
+const QUESTION_TYPES = {
+  NUMBER: 'number',
+  STRING: 'string',
+};
+
+const QUESTION_TYPE_VALUES = Object.values(QUESTION_TYPES);
+
 export class LabTestType extends Model {
   static init({ primaryKey, ...options }) {
     super.init(
@@ -17,11 +24,29 @@ export class LabTestType extends Model {
           allowNull: false,
           defaultValue: '',
         },
-        rangeMin: Sequelize.FLOAT,
-        rangeMax: Sequelize.FLOAT,
+        maleMin: Sequelize.FLOAT,
+        maleMax: Sequelize.FLOAT,
+        femaleMin: Sequelize.FLOAT,
+        femaleMax: Sequelize.FLOAT,
         rangeText: Sequelize.STRING,
+        questionType: Sequelize.ENUM(QUESTION_TYPE_VALUES),
+        options: {
+          type: Sequelize.TEXT,
+          allowNull: true,
+        },
       },
-      options,
+      {
+        ...options,
+        validate: {
+          mustHaveValidOptions() {
+            if(!this.options) return;
+            const parsed = JSON.parse(this.options);
+            if(!Array.isArray(parsed)) {
+              throw new InvalidOperationError("options must be a valid JSON array");
+            }
+          },
+        }
+      },
     );
   }
 
