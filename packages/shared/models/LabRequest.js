8a1127044ad97d3fb19b6f0d67cec676a6195507
Tom McLean <tom.a.mclean@gmail.com>
2020-03-27 15:00:35 +1100

    Address PR comment

--- packages/shared/models/LabRequest.js
+++ packages/shared/models/LabRequest.js
@@ -53,15 +53,14 @@ export class LabRequest extends Model {
   static create(data) {
     return this.sequelize.transaction(async () => {
       const { labTestTypeIds = [] } = data;
+      if (!labTestTypeIds.length) {
+        throw new InvalidOperationError('A request must have at least one test');
+      }
 
       const base = await super.create(data);
 
       // then create tests
       const { LabTest } = this.sequelize.models;
-
-      if (!labTestTypeIds.length) {
-        throw new InvalidOperationError('A request must have at least one test');
-      }
 
       await Promise.all(
         labTestTypeIds.map(t =>
