1eaf2f66243cefb1b1f7b9c9ae536245b41294f9
Vinicius Vieira <vinipachecov@gmail.com>
2020-04-27 13:08:04 -0300

    refactor: added aliases to getProgram factory

--- packages/mobile/App/factories/programs/getPrograms.ts
+++ packages/mobile/App/factories/programs/getPrograms.ts
@@ -1,9 +1,14 @@
-import { HttpGetProgramsList } from '../../data/usecases/get-programs-list/http-get-programs-list';
-import { GetProgramsController } from '../../presentation/controllers/programs/get-programs';
-import { ProgramAxiosRepository } from '../../infra/httpClient/axios/program-repository/program';
+import { HttpGetProgramsList } from '/data/usecases/get-programs-list/http-get-programs-list';
+import { GetProgramsController } from '/presentation/controllers/programs/get-programs';
+import { ProgramAxiosRepository } from '/infra/httpClient/axios/program-repository/program';
+import { Client } from '/infra/httpClient/http-client';
+import { AxiosAdapter } from '/infra/httpClient/axios/adapter/axios-adapter';
+import { API } from '/infra/httpClient/axios/helper/BaseAPI';
 
 export const makeGetProgramsController = (): GetProgramsController => {
-  const programAxiosRepository = new ProgramAxiosRepository();
+  const axiosDapater = new AxiosAdapter(API);
+  const httpClient = new Client(axiosDapater);
+  const programAxiosRepository = new ProgramAxiosRepository(httpClient);
   const httpGetProgramsList = new HttpGetProgramsList(programAxiosRepository);
   const getProgramController = new GetProgramsController(httpGetProgramsList);
   return getProgramController;
