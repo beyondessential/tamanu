65810f81725466cd05cf22abb06a6fb5fe113cef
Tom McLean <tom.a.mclean@gmail.com>
2020-05-01 15:55:23 +1000

    Add nullable boolean control

---
+++ packages/desktop/app/components/Field/NullableBooleanField.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { OuterLabelFieldWrapper } from './OuterLabelFieldWrapper';
+
+import FormLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+
+import MuiButton from '@material-ui/core/Button';
+import MuiButtonGroup from '@material-ui/core/ButtonGroup';
+
+const StyledButtonGroup = styled(MuiButtonGroup)`
+  margin: 0 1rem;
+`;
+
+const ControlLabel = styled(FormLabel)`
+  width: max-content;
+
+  > span {
+    font-size: 16px;
+  }
+`;
+
+const NullableBooleanControl = React.memo(({ value, onChange, disabled }) => {
+  const onClickTrue = () => { 
+    const newValue = (value === true) ? undefined : true;
+    onChange({ target: { name, value: newValue } });
+  };
+
+  const onClickFalse = () => { 
+    const newValue = (value === false) ? undefined : false;
+    onChange({ target: { name, value: newValue } });
+  };
+
+  return (
+    <StyledButtonGroup size="small" variant="contained">
+      <MuiButton disabled={disabled} onClick={onClickTrue} color={ value === true && "primary" }>Yes</MuiButton>
+      <MuiButton disabled={disabled} onClick={onClickFalse} color={ value === false && "primary" }>No</MuiButton>
+    </StyledButtonGroup>
+  );
+});
+
+export const NullableBooleanInput = React.memo(({ label, helperText, className, style, error, ...props }) => {
+  return (
+    <FormControl style={style} error={error} className={className}>
+      <ControlLabel
+        control={<NullableBooleanControl {...props} />} 
+        label={label} 
+      />
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
+  );
+});
+
+export const NullableBooleanField = React.memo(({ field, error, ...props }) => (
+  <NullableBooleanInput
+    name={field.name}
+    value={field.value || false}
+    onChange={field.onChange}
+    {...props}
+  />
+));
+
+NullableBooleanInput.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.bool,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+NullableBooleanInput.defaultProps = {
+  value: null,
+  label: '',
+  onChange: undefined,
+  name: undefined,
+};
