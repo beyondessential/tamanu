409cf02e104199a97b34a3aefeea4de327467c7a
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-05-19 21:53:19 -0300

    chore: added typeorm setup

--- packages/mobile/babel.config.js
+++ packages/mobile/babel.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
     [
       'module-resolver',
       {
         root: ['./'],
-        extensions: ['ts','.tsx', '.json'],
+        extensions: ['ts', '.tsx', '.json'],
         alias: {
           '/styled': './App/ui/styled',
           '/components': './App/ui/components',
