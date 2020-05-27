a3f8370eeecdc02b9c94e0077ac0dee86b86b975
Tom McLean <tom.a.mclean@gmail.com>
2020-05-27 10:56:31 +1000

    Remove debug logging

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,6 @@
 set -euxo pipefail
 echo "Installing now"
 
-# load nvm
-node -v
-yarn -v
-
 yarn config set workspaces-experimental true
 yarn config set workspaces-nohoist-experimental true
 yarn install --non-interactive
