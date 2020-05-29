5656c5b99c7e7c1a3996c067d44736c37c825a7a
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-05-28 21:31:41 -0300

    feat: added mockedConnection method and forcedSync strategy for development environment

--- packages/mobile/App/infra/db/sqlite/helpers/sqlite-helper.ts
+++ packages/mobile/App/infra/db/sqlite/helpers/sqlite-helper.ts
@@ -1,21 +1,46 @@
-import { Connection, createConnection } from 'typeorm/browser'
-import { Patient } from '../entities/patient'
+
+import {Connection, createConnection, getConnectionManager } from 'typeorm';
+import {Patient} from '../entities/patient';
+
+
 
 export const SqliteHelper = {
-    client: null as Connection,
+  client: null as Connection,
 
-    async connect(): Promise<void> {
-        if (this.client === null) {
-            this.client = await createConnection({
-                type: 'react-native',
-                database: 'tamanu',
-                location: 'default',
-                logging: __DEV__ ? ['error', 'query', 'schema'] : [],
-                synchronize: true,
-                entities: [
-                Patient
-      ]
-    });
-        }        
-    }
+  async forceSync(): Promise<any> {
+     if (__DEV__) {        
+      await SqliteHelper.client.dropDatabase();
+      await SqliteHelper.client.synchronize();
+     }
+  },
+
+  async connect(): Promise<void> {        
+    try {                  
+      this.client = await createConnection({
+        type: 'react-native',
+        database: 'tamanu',
+        location: 'default',
+        logging: __DEV__ ? ['error', 'query', 'schema']: [],
+        synchronize: false,
+        entities: [Patient],
+      });             
+      await this.forceSync()            
+    } catch (error) {
+        if (error.name === "AlreadyHasActiveConnectionError") {
+        const existentConn = getConnectionManager().get("default");
+        this.client = existentConn        
+     } 
+    }             
+  }
+};
+
+
+export const createFakeConnection = () => {
+    return createConnection({
+      type: 'sqlite',
+      database: '/Users/viniciuspvieira/programming/tamanuapp/App/infra/db/sqlite/helpers/test.db',
+      logging: __DEV__ ? ['error', 'query', 'schema'] : [],
+      synchronize: true,
+      entities: [Patient],
+    });   
 }
