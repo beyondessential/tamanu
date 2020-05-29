7d233c4acc6a85ec0c046ad653bf7164886ebe06
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2020-05-29 16:30:55 -0300

    refactor: changed hardcoded gender to chance generator

--- packages/mobile/App/ui/components/PatientSectionList/fixture.tsx
+++ packages/mobile/App/ui/components/PatientSectionList/fixture.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { groupEntriesByLetter } from '/helpers/list';
-import { StyledView } from '/styled/common';
-import { PatientSectionList } from './index';
-import { PatientModel } from '../../models/Patient';
-import { Chance } from 'chance';
-import { GenderOptions, BloodTypes } from '/helpers/constants';
+import {groupEntriesByLetter} from '/helpers/list';
+import {StyledView} from '/styled/common';
+import {PatientSectionList} from './index';
+import {PatientModel} from '../../models/Patient';
+import {Chance} from 'chance';
+import {BloodTypes} from '/helpers/constants';
 
 const chance = new Chance();
 
 export const genPatientSectionList = (): PatientModel[] =>
   new Array(80).fill(1).map(data => {
     const [firstName, middleName, lastName] = chance
-      .name({ middle: true })
+      .name({middle: true})
       .split(' ');
     return {
-      id: chance.guid({ version: 4 }),
+      id: chance.guid({version: 4}),
       displayId: chance.string({
         symbols: false,
         length: 6,
@@ -27,7 +27,7 @@ export const genPatientSectionList = (): PatientModel[] =>
       middleName,
       email: chance.email(),
       lastName,
-      gender: GenderOptions[0].label,
+      gender: chance.gender().toLowerCase(),
       birthDate: chance.birthday(),
       bloodType:
         BloodTypes[Math.floor(Math.random() * BloodTypes.length)].value,
