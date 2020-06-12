7101963119dd23c4db4935bc8dd8822851a571d9
Tom McLean <tom.a.mclean@gmail.com>
2020-06-12 13:57:07 +1000

    Lint

--- packages/lan/app/routes/apiv1/suggestions.js
+++ packages/lan/app/routes/apiv1/suggestions.js
@@ -13,62 +13,59 @@ const defaultMapper = ({ name, code, id }) => ({ name, code, id });
 const defaultLimit = 10;
 
 function createSuggester(endpoint, modelName, whereSql, mapper = defaultMapper) {
-  suggestions.get(`/${endpoint}/:id`, asyncHandler(async (req, res) => {
-    const { models, params } = req;
-    req.checkPermission('list', modelName);
-    const record = await models[modelName].findByPk(params.id);
-    if (!record) throw new NotFoundError();
-    req.checkPermission('read', record);
-    res.send(mapper(record));
-  }));
+  suggestions.get(
+    `/${endpoint}/:id`,
+    asyncHandler(async (req, res) => {
+      const { models, params } = req;
+      req.checkPermission('list', modelName);
+      const record = await models[modelName].findByPk(params.id);
+      if (!record) throw new NotFoundError();
+      req.checkPermission('read', record);
+      res.send(mapper(record));
+    }),
+  );
 
-  suggestions.get(`/${endpoint}`, asyncHandler(async (req, res) => {
-    req.checkPermission('list', modelName);
-    const { models, query } = req;
-    const search = (query.q || '').trim().toLowerCase();
-    if (!search) {
-      res.send([]);
-      return;
-    }
+  suggestions.get(
+    `/${endpoint}`,
+    asyncHandler(async (req, res) => {
+      req.checkPermission('list', modelName);
+      const { models, query } = req;
+      const search = (query.q || '').trim().toLowerCase();
+      if (!search) {
+        res.send([]);
+        return;
+      }
 
-    const model = models[modelName];
-    const sequelize = model.sequelize;
-    const results = await sequelize.query(
-      `
+      const model = models[modelName];
+      const sequelize = model.sequelize;
+      const results = await sequelize.query(
+        `
       SELECT * 
       FROM :tableName
       WHERE ${whereSql}
       LIMIT :limit
     `,
-      {
-        replacements: {
-          tableName: model.tableName,
-          search: `%${search}%`,
-          limit: defaultLimit,
+        {
+          replacements: {
+            tableName: model.tableName,
+            search: `%${search}%`,
+            limit: defaultLimit,
+          },
+          type: QueryTypes.SELECT,
         },
-        type: QueryTypes.SELECT,
-      },
-    );
+      );
 
-    const listing = results.map(mapper);
-    res.send(listing);
-  }));
+      const listing = results.map(mapper);
+      res.send(listing);
+    }),
+  );
 }
 
-
-REFERENCE_TYPE_VALUES.map(typeName => createSuggester(
-  typeName, 
-  'ReferenceData',
-  `name LIKE :search AND type = '${typeName}'`,
-));
-
-createSuggester(
-  'practitioner',
-  'User',
-  'display_name LIKE :search',
-  ({ id, display_name }) => ({
-    id,
-    name: display_name,
-  }),
+REFERENCE_TYPE_VALUES.map(typeName =>
+  createSuggester(typeName, 'ReferenceData', `name LIKE :search AND type = '${typeName}'`),
 );
 
+createSuggester('practitioner', 'User', 'display_name LIKE :search', ({ id, display_name }) => ({
+  id,
+  name: display_name,
+}));
