f7b8e389ce68b2b526d55201a7903ae828991a47
Tom McLean <tom.a.mclean@gmail.com>
2020-07-22 11:16:34 +1000

    Import demo programs as part of initial startup

--- packages/lan/index.js
+++ packages/lan/index.js
@@ -8,8 +8,26 @@ import { startScheduledTasks } from './app/tasks';
 import { startDataChangePublisher } from './app/DataChangePublisher';
 
 import { importDataDefinition } from './app/dataDefinitionImporter';
+import { 
+  readSurveyXSLX,
+  writeProgramToDatabase,
+  writeSurveyToDatabase,
+} from './app/surveyImporter';
 
 const port = config.port;
+
+async function importDemoPrograms(models) {
+  const path =  './data/demo_survey.xlsx';
+
+  log.info(`Importing test surveys from ${path}...`);
+  const program = await writeProgramToDatabase(models, {
+    code: 'demo',
+    name: 'Demo program',
+  });
+  const data = await readSurveyXSLX('Covid-19 risk assessment', path);
+  await writeSurveyToDatabase(models, program, data);
+  log.info('Surveys imported.');
+}
 
 async function performInitialSetup({ sequelize, models }) {
   // sync models with database
@@ -33,6 +51,8 @@ async function performInitialSetup({ sequelize, models }) {
     });
   });
   log.info(`Data import completed.`);
+
+  await importDemoPrograms(models);
 }
 
 export async function run() {
