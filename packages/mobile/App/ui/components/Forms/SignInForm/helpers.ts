173320ff59c40686c3b272f6396f470528c8479e
Tom McLean <tom.a.mclean@gmail.com>
2020-08-01 15:25:28 +1000

    Allow blank fields on sign in

--- packages/mobile/App/ui/components/Forms/SignInForm/helpers.ts
+++ packages/mobile/App/ui/components/Forms/SignInForm/helpers.ts
@@ -2,9 +2,9 @@ import * as Yup from 'yup';
 
 export const signInValidationSchema = Yup.object().shape({
   email: Yup.string()
-    .email()
-    .required(),
-  password: Yup.string().required(),
+    .email(),
+  // .required(),
+  password: Yup.string(),//.required(),
 });
 export const signIninitialValues = {
   email: '',
