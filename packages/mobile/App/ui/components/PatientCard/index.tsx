d7fb0c7c4af58ef1225c104761e9e1b22a7e17c5
Bastien Goepfert <bas.goepfert@gmail.com>
2020-08-05 17:19:42 +1000

    use age prop provided

--- packages/mobile/App/ui/components/PatientCard/index.tsx
+++ packages/mobile/App/ui/components/PatientCard/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {TouchableWithoutFeedback} from 'react-native';
-import {RowView, ColumnView, StyledView, StyledText} from '/styled/common';
-import {DateFormats} from '/helpers/constants';
-import {formatDate} from '/helpers/date';
-import {UserAvatar} from '../UserAvatar';
-import {screenPercentageToDP, Orientation} from '/helpers/screen';
+import { TouchableWithoutFeedback } from 'react-native';
+import { RowView, ColumnView, StyledView, StyledText } from '/styled/common';
+import { DateFormats } from '/helpers/constants';
+import { formatDate } from '/helpers/date';
+import { UserAvatar } from '../UserAvatar';
+import { screenPercentageToDP, Orientation } from '/helpers/screen';
 import * as styles from './styles';
-import {theme} from '/styled/theme';
-import {getGender} from '../../helpers/user';
+import { theme } from '/styled/theme';
+import { getGender } from '../../helpers/user';
 
 export interface PatientCardProps {
   lastVisit: Date;
@@ -33,8 +33,7 @@ export const PatientCard = ({
       <RowView
         justifyContent="space-between"
         height={screenPercentageToDP(5.46, Orientation.Height)}
-        width="100%"
-      >
+        width="100%">
         <UserAvatar
           size={screenPercentageToDP(4.86, Orientation.Height)}
           displayName={name}
@@ -44,21 +43,18 @@ export const PatientCard = ({
         <StyledText
           color={theme.colors.TEXT_DARK}
           fontSize={screenPercentageToDP(1.09, Orientation.Height)}
-          fontWeight={500}
-        >
+          fontWeight={500}>
           {`Last viewed \n${formatDate(lastVisit, DateFormats.short)}`}
         </StyledText>
       </RowView>
       <ColumnView
         width="100%"
-        marginTop={screenPercentageToDP(1.82, Orientation.Height)}
-      >
+        marginTop={screenPercentageToDP(1.82, Orientation.Height)}>
         <StyledView width="75%" marginBottom={10}>
           <StyledText
             fontSize={screenPercentageToDP(1.82, Orientation.Height)}
             fontWeight={500}
-            color={theme.colors.TEXT_DARK}
-          >
+            color={theme.colors.TEXT_DARK}>
             {name}
           </StyledText>
         </StyledView>
@@ -66,15 +62,13 @@ export const PatientCard = ({
           <StyledText
             fontSize={screenPercentageToDP(1.45, Orientation.Height)}
             fontWeight={500}
-            color={theme.colors.TEXT_MID}
-          >
+            color={theme.colors.TEXT_MID}>
             {`${getGender(gender)}, ${age}yrs`}
           </StyledText>
           <StyledText
             fontSize={screenPercentageToDP(1.45, Orientation.Height)}
             fontWeight={500}
-            color={theme.colors.TEXT_MID}
-          >
+            color={theme.colors.TEXT_MID}>
             {city}
           </StyledText>
         </StyledView>
