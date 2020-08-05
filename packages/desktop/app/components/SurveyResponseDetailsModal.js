6fbf20e6d59fa96d0a4eb53e9abf4d956f514d20
Tom McLean <tom.a.mclean@gmail.com>
2020-08-05 19:07:40 +1000

    Show detailed results in table

--- packages/desktop/app/components/SurveyResponseDetailsModal.js
+++ packages/desktop/app/components/SurveyResponseDetailsModal.js
@@ -1,7 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { Modal } from './Modal';
 
+import { Table } from './Table';
+import { SurveyResultBadge } from './SurveyResultBadge';
+
 import { connectApi } from '../api/connectApi';
+
+const COLUMNS = [
+  { key: 'text', title: 'Indicator', accessor: ({ indicator }) => indicator },
+  { key: 'value', title: 'Value', accessor: ({ answer, type }) => {
+    if(type === 'Result') {
+      const value = parseFloat(answer);
+      return <SurveyResultBadge result={value} />;
+    } else if(type === 'Calculated') {
+      return parseFloat(answer).toFixed(2);
+    } else {
+      return answer;
+    }
+  } },
+];
+
+function shouldShow(component) {
+  switch(component.dataElement.type) {
+    case 'Instruction':
+      return false;
+    default:
+      return true;
+  }
+}
 
 export const SurveyResponseDetailsModal = connectApi(api => ({
   fetchResponseDetails: surveyResponseId => api.get(`surveyResponse/${surveyResponseId}`),
@@ -30,9 +56,29 @@ export const SurveyResponseDetailsModal = connectApi(api => ({
     );
   }
 
+  const { components, answers } = surveyDetails;
+  const answerRows = components
+    .filter(shouldShow)
+    .map(component => {
+      const { dataElement, text, id } = component;
+      const { defaultText, type } = dataElement;
+      const indicator = text || defaultText;
+      const answerObject = answers.find(a => a.dataElementId === dataElement.id);
+      const answer = answerObject ? answerObject.body : 'N/A';
+      return { 
+        id,
+        type,
+        answer,
+        indicator
+      };
+    });
+
   return (
     <Modal title="Survey response" open={surveyResponseId} onClose={onClose}>
-      <pre>{ JSON.stringify(surveyDetails, null, 2) }</pre>
+      <Table
+        data={answerRows}
+        columns={COLUMNS}
+      />
     </Modal>
   );
 });
