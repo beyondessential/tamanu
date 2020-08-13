357c05bc3fd0a1454e24739a607267d3db5117f0
Tom McLean <tom.a.mclean@gmail.com>
2020-08-13 15:20:47 +1000

    Remove unused httpclient infra

--- packages/mobile/App/infra/httpClient/axios/errors/internatl-error.ts
+++
@@ -1,6 +0,0 @@
-export class AxiosInternalError extends Error {
-  constructor(errorMessage: string) {
-    super(errorMessage);
-    this.name = 'AxiosInternalError';
-  }
-}
--- packages/mobile/App/infra/httpClient/axios/helper/programs.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosResponse } from 'axios';
-import { API } from './BaseAPI';
-import { ProgramModel } from '/models/Program';
-
-export const programs = {
-  get: (): Promise<AxiosResponse<ProgramModel[]>> => API.get('programs'),
-};
