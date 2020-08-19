b3d4322f7781df870bb9d2efbca1d7c86ce03bf6
Bastien Goepfert <bas.goepfert@gmail.com>
2020-08-19 12:48:26 +1000

    update column titles

--- packages/desktop/app/components/ImmunisationsTable.js
+++ packages/desktop/app/components/ImmunisationsTable.js
@@ -11,12 +11,12 @@ const getBatch = ({ batch }) => (batch ? batch.name : 'Unknown');
 const getManufacturer = ({ manufacturer }) => (manufacturer ? manufacturer.name : 'Unknown');
 
 const columns = [
-  { key: 'vaccine', title: 'Referral date', accessor: getVaccineName },
-  { key: 'date', title: 'Referral date', accessor: getDate },
-  { key: 'administered', title: 'Referral date', accessor: getAdministerer },
+  { key: 'vaccine', title: 'Vaccine', accessor: getVaccineName },
+  { key: 'date', title: 'Date', accessor: getDate },
+  { key: 'administered', title: 'Given by', accessor: getAdministerer },
   { key: 'facility', title: 'Facility', accessor: getFacility },
-  { key: 'batch', title: 'Referral date', accessor: getBatch },
-  { key: 'manafacturer', title: 'Referring doctor', accessor: getManufacturer },
+  { key: 'batch', title: 'Batch', accessor: getBatch },
+  { key: 'manafacturer', title: 'Manufacturer', accessor: getManufacturer },
 ];
 
 export const ImmunisationsTable = React.memo(({ patient }) => (
