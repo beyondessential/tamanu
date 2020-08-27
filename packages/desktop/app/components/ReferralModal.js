e36e19edb61da3eb43e43821b8c874d2c88bf5f7
Bastien Goepfert <bas.goepfert@gmail.com>
2020-08-27 10:34:58 +1000

    todo

--- packages/desktop/app/components/ReferralModal.js
+++ packages/desktop/app/components/ReferralModal.js
@@ -35,6 +35,7 @@ const DumbReferralModal = React.memo(
 
 export const ReferralModal = connectApi((api, dispatch, { patientId }) => ({
   onCreateReferral: async data => {
+    // TODO: Add refferedTo field to form once referral flow is confirmed.
     await api.post(`referral`, { ...data, patientId, referredToId: data.referredById });
     dispatch(reloadPatient(patientId));
   },
