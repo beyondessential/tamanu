5bd267973969b6507bfb9fe8d9c9539ff3dfdae2
Bastien Goepfert <bas.goepfert@gmail.com>
2020-08-28 10:18:03 +1000

    remove extrenous relation

--- packages/lan/app/routes/apiv1/imaging.js
+++ packages/lan/app/routes/apiv1/imaging.js
@@ -1,18 +1,9 @@
 import express from 'express';
 
-import {
-  simpleGet,
-  simplePut,
-  simplePost,
-  simpleGetList,
-  permissionCheckingRouter,
-} from './crudHelpers';
+import { simpleGet, simplePut, simplePost } from './crudHelpers';
 
 export const imagingRequest = express.Router();
 
 imagingRequest.get('/:id', simpleGet('ImagingRequest'));
 imagingRequest.put('/:id', simplePut('ImagingRequest'));
 imagingRequest.post('/$', simplePost('ImagingRequest'));
-
-const encounterRelations = permissionCheckingRouter('read', 'ImagingRequest');
-encounterRelations.get('/:id/imagingType', simpleGetList('imagingType', 'encounterId'));
