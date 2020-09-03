e01d24ec70483f05a7f3fd1f2ceffa15fbf8898c
Bastien Goepfert <bas.goepfert@gmail.com>
2020-09-03 12:57:46 +1000

    fix up the imports after changing the folder for imms listing view

--- packages/desktop/app/views/index.js
+++ packages/desktop/app/views/index.js
@@ -1,6 +1,7 @@
 export { LoginView, ConnectedLoginView } from './LoginView';
 export { NotActiveView } from './NotActiveView';
 export { TriageListingView } from './TriageListingView';
+export { ImmunisationListingView } from './ImmunisationListingView';
 export * from './patients';
 export * from './reports';
 export * from './administration';
