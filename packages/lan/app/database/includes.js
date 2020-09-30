5dcf94f67a3a2cb67e55da969ec2a7b29a5daf57
Bastien Goepfert <bas.goepfert@gmail.com>
2020-09-30 17:37:51 +1000

    todo comment

--- packages/lan/app/database/includes.js
+++ packages/lan/app/database/includes.js
@@ -3,4 +3,5 @@
  *  e.g. simpleGetList('LabRequest', '', { include: [ENCOUNTER_PATIENT] })
  */
 
+// TODO: Only include name fields
 export const ENCOUNTER_PATIENT = { association: 'encounter', include: ['patient'] };
