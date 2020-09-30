390756e54987b871650e4da843e6fcbc76a3774c
Bastien Goepfert <bas.goepfert@gmail.com>
2020-09-30 17:37:10 +1000

    add usage comment

--- packages/lan/app/database/includes.js
+++ packages/lan/app/database/includes.js
@@ -1,3 +1,6 @@
-/** These objects are for use in sequelize include parameter queries */
+/**
+ * These objects are for use in sequelize include parameter queries
+ *  e.g. simpleGetList('LabRequest', '', { include: [ENCOUNTER_PATIENT] })
+ */
 
 export const ENCOUNTER_PATIENT = { association: 'encounter', include: ['patient'] };
