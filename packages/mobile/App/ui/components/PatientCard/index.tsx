18d404a24709b02bcdc07906fe5af6010b6d92d1
Bastien Goepfert <bas.goepfert@gmail.com>
2020-10-16 08:19:06 +1100

    delete unused files

--- packages/mobile/App/ui/components/PatientCard/index.tsx
+++ packages/mobile/App/ui/components/PatientCard/index.tsx
@@ -28,57 +28,57 @@ export const PatientCard = ({
   lastVisit,
   onPress,
 }: PatientCardProps): JSX.Element => (
-    <TouchableWithoutFeedback onPress={(): void => onPress()}>
-      <styles.StyledCardContainer>
-        <RowView
-          justifyContent="space-between"
-          height={screenPercentageToDP(5.46, Orientation.Height)}
-          width="100%"
+  <TouchableWithoutFeedback onPress={(): void => onPress()}>
+    <styles.StyledCardContainer>
+      <RowView
+        justifyContent="space-between"
+        height={screenPercentageToDP(5.46, Orientation.Height)}
+        width="100%"
+      >
+        <UserAvatar
+          size={screenPercentageToDP(4.86, Orientation.Height)}
+          displayName={name}
+          image={image}
+          gender={gender}
+        />
+        <StyledText
+          color={theme.colors.TEXT_DARK}
+          fontSize={screenPercentageToDP(1.09, Orientation.Height)}
+          fontWeight={500}
         >
-          <UserAvatar
-            size={screenPercentageToDP(4.86, Orientation.Height)}
-            displayName={name}
-            image={image}
-            gender={gender}
-          />
+          {`Last viewed \n${formatDate(lastVisit, DateFormats.short)}`}
+        </StyledText>
+      </RowView>
+      <ColumnView
+        width="100%"
+        marginTop={screenPercentageToDP(1.82, Orientation.Height)}
+      >
+        <StyledView width="75%" marginBottom={10}>
           <StyledText
-            color={theme.colors.TEXT_DARK}
-            fontSize={screenPercentageToDP(1.09, Orientation.Height)}
+            fontSize={screenPercentageToDP(1.82, Orientation.Height)}
             fontWeight={500}
+            color={theme.colors.TEXT_DARK}
           >
-            {`Last viewed \n${formatDate(lastVisit, DateFormats.short)}`}
+            {name}
           </StyledText>
-        </RowView>
-        <ColumnView
-          width="100%"
-          marginTop={screenPercentageToDP(1.82, Orientation.Height)}
-        >
-          <StyledView width="75%" marginBottom={10}>
-            <StyledText
-              fontSize={screenPercentageToDP(1.82, Orientation.Height)}
-              fontWeight={500}
-              color={theme.colors.TEXT_DARK}
-            >
-              {name}
-            </StyledText>
-          </StyledView>
-          <StyledView width="80%">
-            <StyledText
-              fontSize={screenPercentageToDP(1.45, Orientation.Height)}
-              fontWeight={500}
-              color={theme.colors.TEXT_MID}
-            >
-              {`${getGender(gender)}, ${age}yrs`}
-            </StyledText>
-            <StyledText
-              fontSize={screenPercentageToDP(1.45, Orientation.Height)}
-              fontWeight={500}
-              color={theme.colors.TEXT_MID}
-            >
-              {city}
-            </StyledText>
-          </StyledView>
-        </ColumnView>
-      </styles.StyledCardContainer>
-    </TouchableWithoutFeedback>
-  );
+        </StyledView>
+        <StyledView width="80%">
+          <StyledText
+            fontSize={screenPercentageToDP(1.45, Orientation.Height)}
+            fontWeight={500}
+            color={theme.colors.TEXT_MID}
+          >
+            {`${getGender(gender)}, ${age}yrs`}
+          </StyledText>
+          <StyledText
+            fontSize={screenPercentageToDP(1.45, Orientation.Height)}
+            fontWeight={500}
+            color={theme.colors.TEXT_MID}
+          >
+            {city}
+          </StyledText>
+        </StyledView>
+      </ColumnView>
+    </styles.StyledCardContainer>
+  </TouchableWithoutFeedback>
+);
