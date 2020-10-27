e206cbb5b1174b23d35bc2c7e0ca30d8b90a033c
Bastien Goepfert <bas.goepfert@gmail.com>
2020-10-27 15:14:32 +1100

    update type to match model nullable fields

--- packages/mobile/App/types/IMedication.ts
+++ packages/mobile/App/types/IMedication.ts
@@ -4,11 +4,11 @@ import { ID } from './ID';
 export interface IMedication {
   id: ID;
   date: Date;
-  endDate: Date;
-  prescription: string;
-  note: string;
-  indication: string;
-  route: string;
   quantity: number;
   medication: ReferenceData;
+  endDate?: Date;
+  note?: string;
+  prescription?: string;
+  indication?: string;
+  route?: string;
 }
