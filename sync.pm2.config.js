c1d5a0f9017d9e1bcc3a22c6400a4bd267bf82c0
Tom McLean <tom.a.mclean@gmail.com>
2020-11-05 16:32:53 +1100

    Add CI for sync server

---
+++ sync.pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'sync',
+    script: 'yarn run sync-start-dev',
+  }],
+  deploy: {
+    dev: {
+      user: 'ubuntu',
+      host: 'sync-dev.tamanu.io',
+      ref: 'origin/dev',
+      repo: 'git@github.com:beyondessential/tamanu',
+      path: '/home/ubuntu/tamanu',
+      'post-deploy' : 'yarn && pm2 reload sync.pm2.config.js',
+    }
+  }
+};
