8cc1e7fc005927e3885b42d5a015bffde7bff6e8
Igor Nadj <igor@beyondessential.com.au>
2020-11-05 13:19:46 +1100

    Storybook toggle persists through app reload

--- packages/mobile/Root.tsx
+++ packages/mobile/Root.tsx
@@ -1,14 +1,30 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { DevSettings } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
 import Storybook from './storybook';
 import { App } from './App';
+
+const storeValue = async (value: boolean): Promise<void> => AsyncStorage.setItem('storybookActive', value ? 'true' : 'false');
 
 export const Root = (): any => {
   const [storybookActive, setStorybookActive] = useState(false);
   const toggleStorybook = useCallback(
-    () => setStorybookActive(active => !active),
+    () => {
+      const newValue = !storybookActive;
+      storeValue(newValue);
+      setStorybookActive(newValue);
+    },
     [],
   );
+
+  useEffect(() => {
+    // read value from local storage so that storybook toggle persists through reloads
+    const setInitialStorybookActive = async (): Promise<void> => {
+      const value = await AsyncStorage.getItem('storybookActive');
+      setStorybookActive(value === 'true');
+    };
+    setInitialStorybookActive();
+  });
 
   useEffect(() => {
     if (__DEV__) {
