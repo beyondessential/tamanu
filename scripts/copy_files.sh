6815d7068c8ace6685a2dfbad597e30feb047715
Tom McLean <tom.a.mclean@gmail.com>
2020-11-09 10:19:07 +1100

    Include pm2 configs in copy_files command

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -4,7 +4,9 @@ rm -rf /tamanu/scripts
 mkdir -p /tamanu
 cp -r /.tmp/packages/ /tamanu/packages/
 cp -r /.tmp/scripts/ /tamanu/scripts/
-cp -r /.tmp/package.json /tamanu/
-cp -r /.tmp/yarn.lock /tamanu/
-cp -r /.tmp/babel.config.js /tamanu/
+cp /.tmp/package.json /tamanu/.
+cp /.tmp/yarn.lock /tamanu/.
+cp /.tmp/babel.config.js /tamanu/.
+cp /.tmp/babel.config.js /tamanu/.
+cp /.tmp/*.pm2.config.js /tamanu/.
 rm -rf /.tmp
