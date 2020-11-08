f633012ba81e48c0ebca9ce3768398674a7d20fc
Tom McLean <tom.a.mclean@gmail.com>
2020-11-09 10:27:30 +1100

    Copy more files

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -2,11 +2,13 @@
 echo "Copying files to /tamanu"
 rm -rf /tamanu/scripts
 mkdir -p /tamanu
-cp -r /.tmp/packages/ /tamanu/packages/
-cp -r /.tmp/scripts/ /tamanu/scripts/
-cp /.tmp/package.json /tamanu/.
-cp /.tmp/yarn.lock /tamanu/.
-cp /.tmp/babel.config.js /tamanu/.
-cp /.tmp/babel.config.js /tamanu/.
-cp /.tmp/*.pm2.config.js /tamanu/.
+cd /.tmp
+mv packages/ /tamanu/.
+mv scripts/ /tamanu/.
+mv .git /tamanu/.
+mv package.json /tamanu/.
+mv yarn.lock /tamanu/.
+mv babel.config.js /tamanu/.
+mv babel.config.js /tamanu/.
+mv *.pm2.config.js /tamanu/.
 rm -rf /.tmp
