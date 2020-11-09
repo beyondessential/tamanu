fdc3b9466b49d02ba5bef149706d39a05d6c184e
Tom McLean <tom.a.mclean@gmail.com>
2020-11-09 13:57:52 +1100

    Update deploy_remote script to just tell the remote server to pull & refresh

--- scripts/deploy_remote.sh
+++ scripts/deploy_remote.sh
@@ -1,44 +1,17 @@
-#!/bin/bash
-type=$1
-branch=$2
-filename=$3
-basedir=$PWD
-dir=${PWD}/tamanu-${type}-${branch}
-dir_bk=${dir}.bk
-dir_tmp=${dir}.tmp
-app_name="server-${type}-${branch}"
-pm2='/home/ubuntu/.npm-global/bin/pm2'
-export NODE_ENV='production'
-export DB_BASE_PATH="${basedir}/data/"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-nvm use 10.14.0
-node -v
+#!/usr/bin/env bash
 
-echo "Setting up - ${branch}"
-rm -rf ${dir_bk}
-rm -rf ${dir_tmp}
-mkdir -p ${dir}
-mkdir -p ${dir_tmp}
-mkdir -p ${DB_BASE_PATH}
-unzip -q ./${filename} -d ${dir_tmp}/
-echo "unzip ${filename} > ${dir_tmp}"
+set -euxo pipefail
 
-cd ${dir_tmp}
-$pm2 delete "${app_name}"
-rm -rf ${dir_tmp}/**/node_modules
-# Use production=false to ensure devDependencies are installed so we have webpack
-yarn install --cwd ${dir_tmp}/packages/server --production=false
-yarn install --cwd ${dir_tmp}/packages/shared --production=false
+BRANCH=$1
+CURRENT_BRANCH=`git branch --show-current`
 
-echo "${dir} > ${dir_bk}" \
-  && mv ${dir} ${dir_bk} || echo " " \
-  && echo "${dir_tmp} > ${dir}" \
-  && mv ${dir_tmp} ${dir} || echo " "
-rm -rf ${dir_bk}
+if [ $BRANCH == $CURRENT_BRANCH ]; then
+  cd ~/tamanu
+  git pull
+  yarn
+  pm2 reload sync.pm2.config.js
+else
+  echo "Wrong branch for this deploy - expected $CURRENT_BRANCH, got $BRANCH."
+fi
 
-cd ${dir}/packages/server/
-yarn build
-$pm2 start --name "${app_name}" ./dist/app.bundle.js
-$pm2 log --nostream --lines 20 "${app_name}"
+
