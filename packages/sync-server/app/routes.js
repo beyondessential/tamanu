069a52513ff4b002e0d9799f212c6a3da49febf6
Tom McLean <tom.a.mclean@gmail.com>
2020-11-12 10:03:07 +1100

    Update url structure to include version and separate sync endpoint

--- packages/sync-server/app/routes.js
+++ packages/sync-server/app/routes.js
@@ -1,67 +1,8 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 
-import { log } from './logging';
-
-import { InvalidParameterError } from 'shared/errors';
+import { syncRoutes } from './sync';
 
 export const routes = express.Router();
 
-routes.get('/:channel', asyncHandler(async (req, res) => {
-  const { store, query, params } = req;
-  const { channel } = params;
-  const { since, limit = "100", page = "0" } = query;
+routes.use('/sync', syncRoutes);
 
-  if(!since) {
-    throw new InvalidParameterError('Sync GET request must include a "since" parameter');
-  }
-
-  // grab the requested time before running any queries
-  const requestedAt = new Date();
-
-  const count = await store.countSince(channel, since);
-
-  const limitNum = parseInt(limit, 10) || undefined;
-  const offsetNum = limitNum ? parseInt(page, 10) * limit : undefined;
-  const records = await store.findSince(channel, since, {
-    limit: limitNum,
-    offset: offsetNum,
-  });
-  
-  log.info(`GET from ${channel} : ${count} records`);
-  res.send({
-    count,
-    requestedAt,
-    records,
-  });
-}));
-
-routes.post('/:channel', asyncHandler(async (req, res) => {
-  const { store, params, body } = req;
-  const { channel } = params;
-
-
-  if(Array.isArray(body)) {
-    const inserts = await Promise.all(
-      body.map(record => store.insert(channel, {
-        lastSynced: (new Date()).valueOf(),
-        ...record
-      }))
-    );
-    const count = inserts.filter(x => x).length;
-    log.info(`POST to ${channel} : ${count} records`);
-    res.send({
-      count,
-    });
-  } else {
-    log.info(`POST to ${channel} : 1 record`);
-    const count = await store.insert(channel, {
-      lastSynced: (new Date()).valueOf(),
-      ...body
-    });
-
-    res.send({
-      count 
-    });
-  }
-}));
---
+++ packages/sync-server/app/sync.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+
+import { log } from './logging';
+
+import { InvalidParameterError } from 'shared/errors';
+
+export const syncRoutes = express.Router();
+
+syncRoutes.get('/:channel', asyncHandler(async (req, res) => {
+  const { store, query, params } = req;
+  const { channel } = params;
+  const { since, limit = "100", page = "0" } = query;
+
+  if(!since) {
+    throw new InvalidParameterError('Sync GET request must include a "since" parameter');
+  }
+
+  // grab the requested time before running any queries
+  const requestedAt = new Date();
+
+  const count = await store.countSince(channel, since);
+
+  const limitNum = parseInt(limit, 10) || undefined;
+  const offsetNum = limitNum ? parseInt(page, 10) * limit : undefined;
+  const records = await store.findSince(channel, since, {
+    limit: limitNum,
+    offset: offsetNum,
+  });
+  
+  log.info(`GET from ${channel} : ${count} records`);
+  res.send({
+    count,
+    requestedAt,
+    records,
+  });
+}));
+
+syncRoutes.post('/:channel', asyncHandler(async (req, res) => {
+  const { store, params, body } = req;
+  const { channel } = params;
+
+
+  if(Array.isArray(body)) {
+    const inserts = await Promise.all(
+      body.map(record => store.insert(channel, {
+        lastSynced: (new Date()).valueOf(),
+        ...record
+      }))
+    );
+    const count = inserts.filter(x => x).length;
+    log.info(`POST to ${channel} : ${count} records`);
+    res.send({
+      count,
+    });
+  } else {
+    log.info(`POST to ${channel} : 1 record`);
+    const count = await store.insert(channel, {
+      lastSynced: (new Date()).valueOf(),
+      ...body
+    });
+
+    res.send({
+      count 
+    });
+  }
+}));
