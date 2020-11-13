f40275adbf2884525f6bd49c6ab7605dd48260ab
Tom McLean <tom.a.mclean@gmail.com>
2020-11-13 17:21:32 +1100

    Parse excel data into array of records

--- packages/lan/app/admin/index.js
+++ packages/lan/app/admin/index.js
@@ -1,20 +1,90 @@
 import { log } from '../logging';
 
 import config from 'config';
+import shortid from 'shortid';
+
+import { readSurveyXSLX } from '../surveyImporter';
+
+/***********
+ * Update your local.json to something like this to import data files
+ 
+{
+  "adminTasks": [
+    { "task": "importData", "file": "./data/demo_definitions.xlsx" },
+    { "task": "importData", "file": "./data/demo_survey_pen_assessment.xlsx" }
+  ]
+}
+ **********/
+
+const makeRecord = (recordType, data) => ({
+  recordType,
+  data: {
+    id: shortid(),
+    ...data,
+  }
+});
+
+function makeScreen(screen, componentData) {
+  return screen.dataElements.map(((component, i) => {
+    const {
+      visibilityCriteria,
+      ...elementData
+    } = component;
+
+    const dataElement = makeRecord('programDataElement', {
+      defaultOptions: '',
+      ...elementData,
+    });
+
+    const surveyScreenComponent = makeRecord('surveyScreenComponent', {
+      dataElementId: dataElement.data.id, 
+      componentIndex: i,
+      ...componentData,
+    });
+
+    return [dataElement, surveyScreenComponent];
+  })).flat();
+}
+
+async function importSurvey({ file }) {
+  log.info(`Importing surveys from ${file}...`);
+
+  const data = readSurveyXSLX("Test", file);
+
+  const programElement = makeRecord('program', { name: data.name });
+  const surveyElement = makeRecord('survey', { programId: programElement.data.id });
+
+  // component elements
+  const screenElements = data.screens.map((x, i) => makeScreen(x, {
+    surveyId: surveyElement.data.id,
+    screenIndex: i,
+  })).flat();
+
+  // data elements
+  
+  const records = [
+    programElement,
+    surveyElement,
+    ...screenElements,
+  ];
+
+  console.log(records);
+}
 
 async function importData({ file }) {
-  log.info(`Importing ${file}`);
+  log.info(`Importing data definitions from ${file}...`);
 
   // parse file to xlsx using the usual method
   
   // then restructure the parsed data to sync record format 
   
   // then send the records to sync server
-  // - idempotent
+  // - idempotent?
 }
 
 const tasks = {
   importData,
+  importSurvey,
 };
 
 async function runTask(definition) {
