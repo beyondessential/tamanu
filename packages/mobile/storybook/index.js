b10421bb1f2eed6fbb386b25c9ed0b49037f661b
Tom McLean <tom.a.mclean@gmail.com>
2020-11-18 20:43:22 +1100

    Don't load storybook stories

--- packages/mobile/storybook/index.js
+++ packages/mobile/storybook/index.js
@@ -1,11 +1,11 @@
 import { AppRegistry } from 'react-native';
 import { getStorybookUI, configure } from '@storybook/react-native';
-import { loadStories } from './storyLoader';
+// import { loadStories } from './storyLoader';
 
 import './rn-addons';
 
 configure(() => {
-  loadStories();
+  // loadStories();
 }, module);
 
 const StorybookUIRoot = getStorybookUI({
