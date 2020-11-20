98cc1e89a77f47f02bcaf935c8f9cd754cb2e901
Tom McLean <tom.a.mclean@gmail.com>
2020-11-20 13:45:18 +1100

    Hotfix - declare SSC in order and fully flesh out ManyToOne relationship

--- packages/mobile/App/models/Survey.ts
+++ packages/mobile/App/models/Survey.ts
@@ -8,50 +8,6 @@ import { Database } from '~/infra/db';
 import { ISurveyScreenComponent, ISurvey, IProgramDataElement } from '~/types';
 import { SurveyResponse } from './SurveyResponse';
 
-@Entity('survey_screen_component')
-export class SurveyScreenComponent extends BaseModel
-  implements ISurveyScreenComponent {
-  required: boolean;
-
-  @Column('int')
-  screenIndex: number;
-
-  @Column('int')
-  componentIndex: number;
-
-  @Column({ nullable: true })
-  text?: string;
-
-  @Column({ nullable: true })
-  visibilityCriteria?: string;
-
-  @Column({ nullable: true })
-  options?: string;
-
-  @ManyToOne(type => Survey, survey => survey.components)
-  survey: Survey;
-
-  @ManyToOne(type => ProgramDataElement)
-  dataElement: ProgramDataElement;
-
-  getOptions(): any {
-    try {
-      const optionString = (this.options || this.dataElement.defaultOptions || '');
-      if(!optionString) {
-        return [];
-      }
-      const optionArray = JSON.parse(optionString);
-      return optionArray
-        .map(x => x.trim())
-        .filter(x => x)
-        .map(x => ({ label: x, value: x }));
-    } catch(e) {
-      console.error(e);
-      return [];
-    }
-  }
-}
-
 @Entity('survey')
 export class Survey extends BaseModel implements ISurvey {
   programId: string;
@@ -67,7 +23,7 @@ export class Survey extends BaseModel implements ISurvey {
   components: any;
 
   getComponents(): Promise<BaseModel[]> {
-    const repo = SurveyScreenComponent.getRepository();
+    const repo = Database.models.SurveyScreenComponent.getRepository();
     return repo.find({
       where: { survey: { id: this.id } },
       relations: ['dataElement'],
@@ -103,5 +59,49 @@ export class ProgramDataElement extends BaseModel
 
   @Column()
   type: string;
+}
+
+@Entity('survey_screen_component')
+export class SurveyScreenComponent extends BaseModel
+  implements ISurveyScreenComponent {
+  required: boolean;
+
+  @Column('int')
+  screenIndex: number;
+
+  @Column('int')
+  componentIndex: number;
+
+  @Column({ nullable: true })
+  text?: string;
+
+  @Column({ nullable: true })
+  visibilityCriteria?: string;
+
+  @Column({ nullable: true })
+  options?: string;
+
+  @ManyToOne(type => Survey, survey => survey.components)
+  survey: Survey;
+
+  @ManyToOne(type => ProgramDataElement, element => element.components)
+  dataElement: ProgramDataElement;
+
+  getOptions(): any {
+    try {
+      const optionString = (this.options || this.dataElement.defaultOptions || '');
+      if(!optionString) {
+        return [];
+      }
+      const optionArray = JSON.parse(optionString);
+      return optionArray
+        .map(x => x.trim())
+        .filter(x => x)
+        .map(x => ({ label: x, value: x }));
+    } catch(e) {
+      console.error(e);
+      return [];
+    }
+  }
 }
 
