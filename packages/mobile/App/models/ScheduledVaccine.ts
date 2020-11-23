bc96d12963e461496abfe3066ec76b6a2e7b235f
Bastien Goepfert <bas.goepfert@gmail.com>
2020-11-24 10:39:55 +1100

    fix c dep on scheduled vaccine

--- packages/mobile/App/models/ScheduledVaccine.ts
+++ packages/mobile/App/models/ScheduledVaccine.ts
@@ -1,7 +1,8 @@
-import { Entity, Column } from 'typeorm/browser';
+import { Entity, Column, OneToMany } from 'typeorm/browser';
 import { BaseModel } from './BaseModel';
 import { IScheduledVaccine } from '~/types';
 import { ReferenceDataRelation, ReferenceData } from './ReferenceData';
+import { AdministeredVaccine } from './AdministeredVaccine';
 
 @Entity('scheduled_vaccine')
 export class ScheduledVaccine extends BaseModel implements IScheduledVaccine {
@@ -16,6 +17,12 @@ export class ScheduledVaccine extends BaseModel implements IScheduledVaccine {
 
   @Column()
   category: string;
+
+  @OneToMany(
+    type => AdministeredVaccine,
+    administeredVaccine => administeredVaccine.scheduledVaccine,
+  )
+  administeredVaccine: AdministeredVaccine;
 
   @ReferenceDataRelation()
   vaccine: ReferenceData;
