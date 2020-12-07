5bcd91a4ccf9d7f5e2e358618191fa4d8a4b32f7
Tim Kennedy <t.m.knndy@gmail.com>
2020-12-07 13:41:25 +1100

    Add mangle options as well as compressor options

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -11,6 +11,10 @@ module.exports = {
     minifierConfig: {
       keep_classnames: true,
       keep_fnames: true,
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+      },
     },
   },
 };
