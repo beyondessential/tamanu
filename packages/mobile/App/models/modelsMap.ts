7b9e41f8d9b078b722959586eaa4391003882169
Tim Kennedy <t.m.knndy@gmail.com>
2020-12-10 16:46:49 +1100

    Don't remove type info from MODELS_MAP

--- packages/mobile/App/models/modelsMap.ts
+++ packages/mobile/App/models/modelsMap.ts
@@ -4,22 +4,18 @@ import { User } from './User';
 import { Encounter } from './Encounter';
 import { Program } from './Program';
 import { ProgramDataElement } from './ProgramDataElement';
-import { Survey }from './Survey';
+import { Survey } from './Survey';
 import { SurveyScreenComponent } from './SurveyScreenComponent';
 import { SurveyResponse } from './SurveyResponse';
 import { SurveyResponseAnswer } from './SurveyResponseAnswer';
 import { Vitals } from './Vitals';
 import { Diagnosis } from './Diagnosis';
 import { ScheduledVaccine } from './ScheduledVaccine';
-import { AdministeredVaccine }  from './AdministeredVaccine';
+import { AdministeredVaccine } from './AdministeredVaccine';
 import { Referral } from './Referral';
 import { Medication } from './Medication';
 
-export interface ModelMap {
-  [key: string]: BaseModel;
-}
-
-export const MODELS_ARRAY : BaseModel[] = [
+export const MODELS_ARRAY: BaseModel[] = [
   ReferenceData,
   Patient,
   User,
@@ -38,7 +34,7 @@ export const MODELS_ARRAY : BaseModel[] = [
   Medication,
 ];
 
-export const MODELS_MAP : ModelMap = {
+export const MODELS_MAP = {
   ReferenceData,
   Patient,
   User,
