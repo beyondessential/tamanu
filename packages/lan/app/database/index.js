8dd1faadae433e0247614901d42c7f85f7cf73af
Tim Kennedy <t.m.knndy@gmail.com>
2020-12-14 15:23:24 +1100

    Extract initDatabase from lan server to shared

--- packages/lan/app/database/index.js
+++ packages/lan/app/database/index.js
@@ -1,16 +1,9 @@
 import { Sequelize } from 'sequelize';
 import config from 'config';
-import { createNamespace } from 'cls-hooked';
-
-import * as models from 'shared/models';
+import { initDatabase as sharedInitDatabase } from 'shared/services/database';
 
 import { v4 as uuid } from 'uuid';
 import { log } from '../logging';
-
-// an issue in how webpack's require handling interacts with sequelize means we need
-// to provide the module to sequelize manually
-// issue & resolution here: https://github.com/sequelize/sequelize/issues/9489#issuecomment-486047783
-import sqlite3 from 'sqlite3';
 
 // make a 'fake' uuid that looks like 'test-766-9794-4491-8612-eb19fd959bf2'
 // this way we can run tests against real data and clear out everything that was
@@ -20,51 +13,7 @@ const createTestUUID = () => `test-${uuid().slice(5)}`;
 export function initDatabase({ testMode = false }) {
   // connect to database
   const { username, password, name, verbose, sqlitePath } = config.db;
+  const uuidGenerator = testMode ? createTestUUID : Sequelize.UUIDV4;
 
-  if (sqlitePath) {
-    log.info(`Connecting to sqlite database at ${sqlitePath}...`);
-  } else {
-    log.info(`Connecting to database ${username}@${name}...`);
-  }
-
-  // this allows us to use transaction callbacks without manually managing a transaction handle
-  // https://sequelize.org/master/manual/transactions.html#automatically-pass-transactions-to-all-queries
-  const namespace = createNamespace('sequelize-transaction-namespace');
-  Sequelize.useCLS(namespace);
-
-  const logging = verbose ? s => log.debug(s) : null;
-  const options = sqlitePath 
-    ? { dialect: 'sqlite', dialectModule: sqlite3, storage: sqlitePath } 
-    : { dialect: 'postgres' };
-  const sequelize = new Sequelize(name, username, password, {
-    ...options,
-    logging,
-  });
-
-  // init all models
-  const modelClasses = Object.values(models);
-  const primaryKey = {
-    type: Sequelize.UUID,
-    defaultValue: testMode ? createTestUUID : Sequelize.UUIDV4,
-    primaryKey: true,
-  };
-  log.info(`Registering ${modelClasses.length} models...`);
-  modelClasses.map(modelClass => {
-    modelClass.init(
-      {
-        underscored: true,
-        primaryKey,
-        sequelize,
-      },
-      models,
-    );
-  });
-
-  modelClasses.map(modelClass => {
-    if (modelClass.initRelations) {
-      modelClass.initRelations(models);
-    }
-  });
-
-  return { sequelize, models };
+  return sharedInitDatabase({ username, password, name, sqlitePath, verbose, log, uuidGenerator });
 }
