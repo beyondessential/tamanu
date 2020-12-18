a39cb008893f94452632fc0c1337f7ef45c1a705
Tim Kennedy <t.m.knndy@gmail.com>
2020-12-18 18:10:38 +1100

    When running tests against postgres, create one db per worker

--- packages/sync-server/app/database/index.js
+++ packages/sync-server/app/database/index.js
@@ -1,9 +1,28 @@
 import config from 'config';
+import pg from 'pg';
 
 import { SqlWrapper } from './sqlWrapper';
 import { log } from '../logging';
 
 let existingConnection = null;
+
+// this is dangerous and should only be used in test mode
+const recreateDb = async name => {
+  const { username, password } = config.db;
+  const client = new pg.Client({
+    user: username,
+    password,
+  });
+  try {
+    await client.connect();
+    await client.query(`DROP DATABASE IF EXISTS "${name}"`);
+    await client.query(`CREATE DATABASE "${name}"`);
+  } catch (e) {
+    log.error(e.message);
+  } finally {
+    await client.end();
+  }
+};
 
 export async function initDatabase({ testMode = false }) {
   // connect to database
@@ -11,8 +30,16 @@ export async function initDatabase({ testMode = false }) {
     return existingConnection;
   }
 
+  let { name } = config.db;
+  const { sqlitePath } = config.db;
+  if (testMode && !sqlitePath && process.env.JEST_WORKER_ID) {
+    name = `${name}-${process.env.JEST_WORKER_ID}`;
+    await recreateDb(name);
+  }
+
   const store = await new SqlWrapper({
     ...config.db,
+    name,
     log,
     makeEveryModelParanoid: true,
     saltRounds: config.auth.saltRounds,
