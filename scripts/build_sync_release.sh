394d0f053bfcd5bfda8cb957326aa04905e27f3b
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-07 19:38:00 +1100

    Copy shared to the right place

--- scripts/build_sync_release.sh
+++ scripts/build_sync_release.sh
@@ -11,8 +11,11 @@ pushd "$SYNC_SERVER_ROOT"
 rm -rf "./$RELEASE_DIR"
 mkdir -p "./$RELEASE_DIR"
 cp -R ./[!"$RELEASE_DIR"]* "./$RELEASE_DIR"
-cp -R ../shared/ "./$RELEASE_DIR/node_modules/"
 
 # run yarn install now that we're not in a known workspace
 pushd "$RELEASE_DIR"
 yarn install --non-interactive --production
+popd
+
+# copy across shared
+cp -vR ../shared "./$RELEASE_DIR/node_modules/"
