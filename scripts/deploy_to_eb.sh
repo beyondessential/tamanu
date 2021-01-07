95fe1965df63ebcd14b06dd126c2d33bebe28746
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-07 15:44:15 +1100

    Set env vars in build script, for debugging

--- scripts/deploy_to_eb.sh
+++ scripts/deploy_to_eb.sh
@@ -1,2 +1,5 @@
 #!/bin/bash -euxo pipefail
+SYNC_SERVER_EB_APP=tamanu-sync-server
+SYNC_SERVER_EB_ENV=tamanu-sync-server-dev
+SYNC_SERVER_EB_S3=elasticbeanstalk-ap-southeast-2-843218180240
 codeship_aws eb_deploy . "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3" | sed "/^\s*adding:/d"
