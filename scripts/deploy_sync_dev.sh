a73036ce7404da7db7ade43c73e2bd02636e7764
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-07 17:55:42 +1100

    Hardcode sync server directory in deploy script

--- scripts/deploy_sync_dev.sh
+++ scripts/deploy_sync_dev.sh
@@ -6,4 +6,4 @@ SYNC_SERVER_EB_ENV=tamanu-sync-server-dev
 SYNC_SERVER_EB_S3=elasticbeanstalk-ap-southeast-2-843218180240
 
 # deploy dist dir, with complete node_modules
-codeship_aws eb_deploy "$SYNC_SERVER_ROOT/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3" | sed "/^\s*adding:/d"
+codeship_aws eb_deploy "./packages/sync-server/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3" | sed "/^\s*adding:/d"
