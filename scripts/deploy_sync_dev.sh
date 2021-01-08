8dbdf52c7c4e5a73158986a2e096c98ef551b3fa
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-08 11:17:55 +1100

    Load EB setup from codeship env

--- scripts/deploy_sync_dev.sh
+++ scripts/deploy_sync_dev.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -euxo pipefail
 
-SYNC_SERVER_EB_APP=tamanu-sync-server
-SYNC_SERVER_EB_ENV=tamanu-sync-server-dev
-SYNC_SERVER_EB_S3=elasticbeanstalk-ap-southeast-2-843218180240
-
 # deploy dist dir, with complete node_modules
-codeship_aws eb_deploy "./packages/sync-server/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3" | sed "/^\s*adding:/d"
+codeship_aws eb_deploy "./packages/sync-server/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3"
