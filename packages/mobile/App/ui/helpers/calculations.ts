fbfe2197af20d060081851cbfe24eefc6267611d
Tom McLean <tom.a.mclean@gmail.com>
2021-01-15 14:36:15 +1100

    Add math.js

--- packages/mobile/App/ui/helpers/calculations.ts
+++ packages/mobile/App/ui/helpers/calculations.ts
@@ -1,10 +1,12 @@
 import { ISurveyScreenComponent } from '~/types/ISurvey';
+import { create, all as allMath } from 'mathjs';
 
 export function runCalculations(components: ISurveyScreenComponent[], values: {}): {} {
+  const math = create(allMath);
   const calculatedResults = {};
   for(const c of components) {
     if(!c.calculation) continue;
-    calculatedResults[c.dataElement.code] = 1;
+    calculatedResults[c.dataElement.code] = math.evaluate(c.calculation);
   }
   return calculatedResults;
 }
