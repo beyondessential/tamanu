f8fbbcc5296719981a3becfef7136325a4357e85
tindn <hi@tindn.io>
2021-01-17 22:27:56 -0500

    fix indentation on ReportsRoutes

--- packages/desktop/app/routes/ReportsRoutes.js
+++ packages/desktop/app/routes/ReportsRoutes.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import { ReportGenerator } from '../views';
 
 export const ReportsRoutes = (({ match }) => (
-    <Switch>
-        <Route exact path={match.path} component={ReportGenerator} />
-    </Switch>
+  <Switch>
+    <Route exact path={match.path} component={ReportGenerator} />
+  </Switch>
 ));
