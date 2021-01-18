043fcbe2f78a78ee15d3300f0c1c3d0636a083be
tindn <hi@tindn.io>
2021-01-17 21:47:59 -0500

    update report router to point to new page

--- packages/desktop/app/routes/ReportsRoutes.js
+++ packages/desktop/app/routes/ReportsRoutes.js
@@ -1,3 +1,11 @@
-import { ReportScreen } from '../views';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { ReportGenerator } from '../views';
 
-export const ReportsRoutes = ReportScreen;
+export const ReportsRoutes = React.memo(({ match }) => (
+    <div>
+        <Switch>
+            <Route exact path={match.path} component={ReportGenerator} />
+        </Switch>
+    </div>
+));
