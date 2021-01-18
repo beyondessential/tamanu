01b917ab4a600879214aaa7b6a79876885208bc5
tindn <hi@tindn.io>
2021-01-17 22:27:21 -0500

    fixed indentation and hoisting

--- packages/desktop/app/views/reports/ReportGenerator.js
+++ packages/desktop/app/views/reports/ReportGenerator.js
@@ -6,69 +6,69 @@ import { FormGrid } from '../../components/FormGrid';
 import { Suggester } from '../../utils/suggester';
 
 const REPORT_TYPE_OPTIONS = [
-    { label: 'Incomplete referrals', value: 'incomplete-referrals' },
-    { label: 'Recent Diagnoses', value: 'recent-diagnoses' },
-    { label: 'Admissions Report', value: 'admissions-report' }
+  { label: 'Incomplete referrals', value: 'incomplete-referrals' },
+  { label: 'Recent Diagnoses', value: 'recent-diagnoses' },
+  { label: 'Admissions Report', value: 'admissions-report' }
 ];
 
-const DumbReportGenerator = ({ villageSuggester, practitionerSuggester }) => {
-    return (
-        <PageContainer >
-            <TopBar title="Report Generator" />
-            <PageContent>
-                <Form
-                    initialValues={{
-                        reportType: '',
-                        village: '',
-                        practitioner: ''
-                    }}
-                    onSubmit={(values) => {
-                        console.log('on submit', values);
-                    }}
-                    render={() => (
-                        <>
-                            <FormGrid columns={3}>
-                                <Field
-                                    name="reportType"
-                                    placeholder="Report type"
-                                    component={AutocompleteField}
-                                    options={REPORT_TYPE_OPTIONS}
-                                    required
-                                />
-                                <Field
-                                    name="village"
-                                    placeholder="Village"
-                                    component={AutocompleteField}
-                                    suggester={villageSuggester}
-                                />
-                                <Field
-                                    name="practitioner"
-                                    placeholder="Doctor/Nurse"
-                                    component={AutocompleteField}
-                                    suggester={practitionerSuggester}
-                                />
-                            </FormGrid>
-                            <GenerateButton variant="contained" color="primary" type="submit">
-                                Generate
-                            </GenerateButton>
-                        </>
-                    )}>
-
-                </Form>
-            </PageContent>
-        </PageContainer>
-    )
-}
-
 const PageContent = styled.div`
-    padding: 24px;
+  padding: 24px;
 `;
 
 const GenerateButton = styled(Button)`
-    margin-top: 48px;
+  margin-top: 48px;
 `
 
+const DumbReportGenerator = ({ villageSuggester, practitionerSuggester }) => {
+  return (
+    <PageContainer >
+      <TopBar title="Report Generator" />
+      <PageContent>
+        <Form
+          initialValues={{
+            reportType: '',
+            village: '',
+            practitioner: ''
+          }}
+          onSubmit={(values) => {
+            console.log('on submit', values);
+          }}
+          render={() => (
+            <>
+              <FormGrid columns={3}>
+                <Field
+                  name="reportType"
+                  placeholder="Report type"
+                  component={AutocompleteField}
+                  options={REPORT_TYPE_OPTIONS}
+                  required
+                />
+                <Field
+                  name="village"
+                  placeholder="Village"
+                  component={AutocompleteField}
+                  suggester={villageSuggester}
+                />
+                <Field
+                  name="practitioner"
+                  placeholder="Doctor/Nurse"
+                  component={AutocompleteField}
+                  suggester={practitionerSuggester}
+                />
+              </FormGrid>
+              <GenerateButton variant="contained" color="primary" type="submit">
+                Generate
+              </GenerateButton>
+            </>
+          )}>
+
+        </Form>
+      </PageContent>
+    </PageContainer>
+  )
+}
+
 export const ReportGenerator = connectApi(api => ({
-    villageSuggester: new Suggester(api, 'village'),
-    practitionerSuggester: new Suggester(api, 'practitioner'),
+  villageSuggester: new Suggester(api, 'village'),
+  practitionerSuggester: new Suggester(api, 'practitioner'),
 }))(DumbReportGenerator);
