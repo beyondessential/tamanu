c5e7eea2e481af802e919431d454770b7c5bb62d
tindn <hi@tindn.io>
2021-01-17 21:52:21 -0500

    simplify reports route component

--- packages/desktop/app/routes/ReportsRoutes.js
+++ packages/desktop/app/routes/ReportsRoutes.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ReportGenerator } from '../views';
 
-export const ReportsRoutes = React.memo(({ match }) => (
-    <div>
-        <Switch>
-            <Route exact path={match.path} component={ReportGenerator} />
-        </Switch>
-    </div>
+export const ReportsRoutes = (({ match }) => (
+    <Switch>
+        <Route exact path={match.path} component={ReportGenerator} />
+    </Switch>
 ));
