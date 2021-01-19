64d56e59a1785d45ef7319814003e077149244cc
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-19 19:21:43 +1100

    Lint auth.js

--- packages/sync-server/app/middleware/auth.js
+++ packages/sync-server/app/middleware/auth.js
@@ -16,80 +16,82 @@ const stripUser = user => {
   return userData;
 };
 
-authMiddleware.post('/login', asyncHandler(async (req, res) => {
-  const { store, body } = req;
-  const { email, password } = body;
+authMiddleware.post(
+  '/login',
+  asyncHandler(async (req, res) => {
+    const { store, body } = req;
+    const { email, password } = body;
 
-  if(!email || !password) {
-    throw new BadAuthenticationError('Missing credentials');
-  }
+    if (!email || !password) {
+      throw new BadAuthenticationError('Missing credentials');
+    }
 
-  const user = await store.findUser(email);
+    const user = await store.findUser(email);
 
-  if(!user && config.auth.reportNoUserError) {
-    // an attacker can use this to get a list of user accounts
-    // but hiding this error entirely can make debugging a hassle
-    // so we just put it behind a config flag
-    throw new BadAuthenticationError('No such user');
-  }
+    if (!user && config.auth.reportNoUserError) {
+      // an attacker can use this to get a list of user accounts
+      // but hiding this error entirely can make debugging a hassle
+      // so we just put it behind a config flag
+      throw new BadAuthenticationError('No such user');
+    }
 
-  const hashedPassword = user?.hashedPassword || '';
+    const hashedPassword = user?.hashedPassword || '';
 
-  if(!await bcrypt.compare(password, hashedPassword)) {
-    throw new BadAuthenticationError('Invalid credentials');
-  }
+    if (!(await bcrypt.compare(password, hashedPassword))) {
+      throw new BadAuthenticationError('Invalid credentials');
+    }
 
-  const token = jwt.sign({
-    userId: user.data.id,
-  }, JWT_SECRET);
+    const token = jwt.sign({ userId: user.data.id }, JWT_SECRET);
 
-  res.send({ 
-    token,
-    user: stripUser(user),
-  });
-}));
+    res.send({ token, user: stripUser(user) });
+  }),
+);
 
-authMiddleware.use(asyncHandler(async (req, res, next) => {
-  const { store, headers } = req;
-  
-  // get token
-  const { authorization } = headers;
-  if(!authorization) {
-    throw new ForbiddenError();
-  }
+authMiddleware.use(
+  asyncHandler(async (req, res, next) => {
+    const { store, headers } = req;
 
-  // verify token
-  const [bearer, token] = authorization.split(/\s/);
-  if(bearer.toLowerCase() !== 'bearer') {
-    throw new BadAuthenticationError('Only Bearer token is supported');
-  }
+    // get token
+    const { authorization } = headers;
+    if (!authorization) {
+      throw new ForbiddenError();
+    }
 
-  if(config.auth.allowFakeToken && token === 'fake-token') {
+    // verify token
+    const [bearer, token] = authorization.split(/\s/);
+    if (bearer.toLowerCase() !== 'bearer') {
+      throw new BadAuthenticationError('Only Bearer token is supported');
+    }
+
+    if (config.auth.allowFakeToken && token === 'fake-token') {
+      next();
+      return;
+    }
+
+    let contents = null;
+    try {
+      contents = jwt.verify(token, JWT_SECRET);
+    } catch (e) {
+      throw new BadAuthenticationError('Invalid token');
+    }
+
+    const { userId } = contents;
+
+    const user = await store.findUserById(userId);
+
+    if (!user) {
+      throw new BadAuthenticationError(`User specified in token (${userId}) does not exist`);
+    }
+
+    req.user = stripUser(user);
+
     next();
-    return;
-  }
+  }),
+);
 
-  let contents = null;
-  try {
-    contents = jwt.verify(token, JWT_SECRET);
-  } catch(e) {
-    throw new BadAuthenticationError('Invalid token');
-  }
-
-  const { userId } = contents;
-
-  const user = await store.findUserById(userId);
-
-  if(!user) {
-    throw new BadAuthenticationError(`User specified in token (${userId}) does not exist`);
-  }
-
-  req.user = stripUser(user);
-
-  next();
-}));
-
-authMiddleware.get('/whoami', asyncHandler((req, res) => {
-  res.send(req.user);
-}));
-
+authMiddleware.get(
+  '/whoami',
+  asyncHandler((req, res) => {
+    res.send(req.user);
+  }),
+);
