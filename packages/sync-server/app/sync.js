6242b33cf2533a3ba8055c54313f19bc61033a1f
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-19 17:51:32 +1100

    Lint express handlers

--- packages/sync-server/app/sync.js
+++ packages/sync-server/app/sync.js
@@ -6,55 +6,61 @@ import { log } from './logging';
 
 export const syncRoutes = express.Router();
 
-syncRoutes.get('/:channel', asyncHandler(async (req, res) => {
-  const { store, query, params } = req;
-  const { channel } = params;
-  const { since, limit = "100", page = "0" } = query;
+syncRoutes.get(
+  '/:channel',
+  asyncHandler(async (req, res) => {
+    const { store, query, params } = req;
+    const { channel } = params;
+    const { since, limit = '100', page = '0' } = query;
 
-  if(!since) {
-    throw new InvalidParameterError('Sync GET request must include a "since" parameter');
-  }
+    if (!since) {
+      throw new InvalidParameterError('Sync GET request must include a "since" parameter');
+    }
 
-  // grab the requested time before running any queries
-  const requestedAt = new Date();
+    // grab the requested time before running any queries
+    const requestedAt = new Date();
 
-  const count = await store.countSince(channel, since);
+    const count = await store.countSince(channel, since);
 
-  const limitNum = parseInt(limit, 10) || undefined;
-  const offsetNum = limitNum ? parseInt(page, 10) * limit : undefined;
-  const records = await store.findSince(channel, since, {
-    limit: limitNum,
-    offset: offsetNum,
-  });
+    const limitNum = parseInt(limit, 10) || undefined;
+    const offsetNum = limitNum ? parseInt(page, 10) * limit : undefined;
+    const records = await store.findSince(channel, since, {
+      limit: limitNum,
+      offset: offsetNum,
+    });
 
-  log.info(`GET from ${channel} : ${count} records`);
-  res.send({
-    count,
-    requestedAt,
-    records,
-  });
-}));
+    log.info(`GET from ${channel} : ${count} records`);
+    res.send({
+      count,
+      requestedAt,
+      records,
+    });
+  }),
+);
 
-syncRoutes.post('/:channel', asyncHandler(async (req, res) => {
-  const { store, params, body } = req;
-  const { channel } = params;
+syncRoutes.post(
+  '/:channel',
+  asyncHandler(async (req, res) => {
+    const { store, params, body } = req;
+    const { channel } = params;
 
-  const insert = record => {
-    const lastSynced = (new Date()).valueOf();
-    return store.insert(channel, { lastSynced, ...record });
-  };
+    const insert = record => {
+      const lastSynced = new Date().valueOf();
+      return store.insert(channel, { lastSynced, ...record });
+    };
 
-  if(Array.isArray(body)) {
-    const inserts = await Promise.all(body.map(insert));
-    const count = inserts.filter(x => x).length;
-    log.info(`POST to ${channel} : ${count} records`);
-    res.send({ count });
-  } else {
-    log.info(`POST to ${channel} : 1 record`);
-    const count = await insert(body);
-    res.send({ count });
-  }
-}));
+    if (Array.isArray(body)) {
+      const inserts = await Promise.all(body.map(insert));
+      const count = inserts.filter(x => x).length;
+      log.info(`POST to ${channel} : ${count} records`);
+      res.send({ count });
+    } else {
+      log.info(`POST to ${channel} : 1 record`);
+      const count = await insert(body);
+      res.send({ count });
+    }
+  }),
+);
 
 syncRoutes.delete(
   '/:channel/:recordId',
