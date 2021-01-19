4531413bfd4661a76c2ad65ed1d00f089d90ac97
Alastair-L <lockealastair@gmail.com>
2021-01-19 16:45:08 +1100

    Revert mistaken mock data

--- packages/lan/app/routes/apiv1/referral.js
+++ packages/lan/app/routes/apiv1/referral.js
@@ -1,22 +1,10 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 
 import { simpleGet, simplePut, simplePost } from './crudHelpers';
 
 export const referral = express.Router();
 
-const postReferral = asyncHandler(async (req, res) => {
-  const { models } = req;
-  req.checkPermission('create', 'Referral');
-  const mockEncounter = (await models.Encounter.findAll({ limit: 1 }))[0];
-  const referral = {
-    encounterId: mockEncounter.id, ...req.body
-  }
-  const object = await models.Referral.create(referral);
-  res.send(object);
-});
-
 referral.get('/:id', simpleGet('Referral'));
 referral.put('/:id', simplePut('Referral'));
-referral.post('/$', postReferral);
+referral.post('/$', simplePost('Referral'));
 
