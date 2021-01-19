5628c4ab80d545ed7c992dacff95299920f1bf87
Alastair-L <lockealastair@gmail.com>
2021-01-19 15:37:49 +1100

    Demonstrate successful submission

--- packages/lan/app/routes/apiv1/referral.js
+++ packages/lan/app/routes/apiv1/referral.js
@@ -1,9 +1,23 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
 import { simpleGet, simplePut, simplePost } from './crudHelpers';
 
 export const referral = express.Router();
 
+const postReferral = asyncHandler(async (req, res) => {
+  const { models } = req;
+  req.checkPermission('create', 'Referral');
+  const mockEncounter = (await models.Encounter.findAll({ limit: 1 }))[0];
+  const referral = {
+    encounterId: mockEncounter.id, ...req.body
+  }
+  console.log(referral);
+  const object = await models.Referral.create(referral);
+  res.send(object);
+});
+
 referral.get('/:id', simpleGet('Referral'));
 referral.put('/:id', simplePut('Referral'));
-referral.post('/$', simplePost('Referral'));
+referral.post('/$', postReferral);
+
