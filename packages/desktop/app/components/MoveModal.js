d28a78b948502f04140311d198766a48a82d68c6
Alastair-L <lockealastair@gmail.com>
2021-01-20 16:26:04 +1100

    Update frontend to reflect backend flow

--- packages/desktop/app/components/MoveModal.js
+++ packages/desktop/app/components/MoveModal.js
@@ -9,11 +9,23 @@ import { Suggester } from '../utils/suggester';
 import { connectApi } from '../api/connectApi';
 import { viewEncounter } from '../store/encounter';
 
-const BeginMoveForm = ({ onSubmit, onClose, encounter, locationSuggester }) => {
+export const MoveModal = connectApi((api, dispatch, { encounter }) => ({
+  locationSuggester: new Suggester(api, 'location'),
+  onSubmit: async data => {
+    await api.put(`encounter/${encounter.id}`, data);
+    dispatch(viewEncounter(encounter.id));
+  },
+}))(({ open, onClose, ...rest }) => (
+  <Modal title={"Move patient"} open={open} onClose={onClose}>
+    <MoveForm onClose={onClose} {...rest} />
+  </Modal>
+));
+
+const MoveForm = ({ onSubmit, onClose, encounter, locationSuggester }) => {
   const renderForm = React.useCallback(({ submitForm }) => (
     <FormGrid columns={1}>
       <Field
-        name="plannedLocation.id"
+        name="locationId"
         component={AutocompleteField}
         suggester={locationSuggester}
         label="New location"
@@ -27,68 +39,89 @@ const BeginMoveForm = ({ onSubmit, onClose, encounter, locationSuggester }) => {
     <Form
       onSubmit={onSubmit}
       render={renderForm}
-      initialValues={{ plannedLocation: encounter.plannedLocation }}
+      initialValues={{ locationId: encounter.location.id }}
     />
   );
 };
+// const BeginMoveForm = ({ onSubmit, onClose, encounter, locationSuggester }) => {
+//   const renderForm = React.useCallback(({ submitForm }) => (
+//     <FormGrid columns={1}>
+//       <Field
+//         name="plannedLocation.id"
+//         component={AutocompleteField}
+//         suggester={locationSuggester}
+//         label="New location"
+//         required
+//       />
+//       <ConfirmCancelRow onConfirm={submitForm} onCancel={onClose} />
+//     </FormGrid>
+//   ));
 
-const FinaliseMoveForm = ({ onSubmit, encounter, onClose }) => (
-  <FormGrid columns={1}>
-    <div>{`Are you sure you want to move ${encounter.patient[0].firstName} to ${encounter.plannedLocation.name}?`}</div>
-    <ConfirmCancelRow
-      onConfirm={() => onSubmit({ location: encounter.plannedLocation })}
-      onCancel={onClose}
-    />
-  </FormGrid>
-);
+//   return (
+//     <Form
+//       onSubmit={onSubmit}
+//       render={renderForm}
+//       initialValues={{ plannedLocation: encounter.plannedLocation }}
+//     />
+//   );
+// };
+// const FinaliseMoveForm = ({ onSubmit, encounter, onClose }) => (
+//   <FormGrid columns={1}>
+//     <div>{`Are you sure you want to move ${encounter.patient[0].firstName} to ${encounter.plannedLocation.name}?`}</div>
+//     <ConfirmCancelRow
+//       onConfirm={() => onSubmit({ location: encounter.plannedLocation })}
+//       onCancel={onClose}
+//     />
+//   </FormGrid>
+// );
 
-const CancelMoveForm = ({ onSubmit, encounter, onClose }) => (
-  <FormGrid columns={1}>
-    <div>{`Are you sure you want to cancel ${encounter.patient[0].firstName}'s scheduled move to ${encounter.plannedLocation.name}?`}</div>
-    <ConfirmCancelRow
-      onConfirm={() => onSubmit({ plannedLocation: null })}
-      confirmText="Yes, cancel"
-      cancelText="Keep it"
-      onCancel={onClose}
-    />
-  </FormGrid>
-);
+// const CancelMoveForm = ({ onSubmit, encounter, onClose }) => (
+//   <FormGrid columns={1}>
+//     <div>{`Are you sure you want to cancel ${encounter.patient[0].firstName}'s scheduled move to ${encounter.plannedLocation.name}?`}</div>
+//     <ConfirmCancelRow
+//       onConfirm={() => onSubmit({ plannedLocation: null })}
+//       confirmText="Yes, cancel"
+//       cancelText="Keep it"
+//       onCancel={onClose}
+//     />
+//   </FormGrid>
+// );
 
-const BaseMoveModal = connectApi((api, dispatch, { encounter, endpoint }) => ({
-  locationSuggester: new Suggester(api, 'location'),
-  onSubmit: async data => {
-    await api.put(`encounter/${encounter.id}/${endpoint}`, data);
-    dispatch(viewEncounter(encounter.id));
-  },
-}))(({ title, open, onClose, Component, ...rest }) => (
-  <Modal title={title} open={open} onClose={onClose}>
-    <Component onClose={onClose} {...rest} />
-  </Modal>
-));
+// const BaseMoveModal = connectApi((api, dispatch, { encounter, endpoint }) => ({
+//   locationSuggester: new Suggester(api, 'location'),
+//   onSubmit: async data => {
+//     await api.put(`encounter/${encounter.id}/${endpoint}`, data);
+//     dispatch(viewEncounter(encounter.id));
+//   },
+// }))(({ title, open, onClose, Component, ...rest }) => (
+//   <Modal title={title} open={open} onClose={onClose}>
+//     <Component onClose={onClose} {...rest} />
+//   </Modal>
+// ));
 
-export const BeginMoveModal = props => (
-  <BaseMoveModal
-    {...props}
-    Component={BeginMoveForm}
-    title="Move patient"
-    endpoint="plannedLocation"
-  />
-);
+// const BeginMoveModal = props => (
+//   <BaseMoveModal
+//     {...props}
+//     Component={BeginMoveForm}
+//     title="Move patient"
+//     endpoint="plannedLocation"
+//   />
+// );
 
-export const FinaliseMoveModal = props => (
-  <BaseMoveModal
-    {...props}
-    Component={FinaliseMoveForm}
-    title="Finalise move"
-    endpoint="location"
-  />
-);
+// const FinaliseMoveModal = props => (
+//   <BaseMoveModal
+//     {...props}
+//     Component={FinaliseMoveForm}
+//     title="Finalise move"
+//     endpoint="location"
+//   />
+// );
 
-export const CancelMoveModal = props => (
-  <BaseMoveModal
-    {...props}
-    Component={CancelMoveForm}
-    title="Cancel move"
-    endpoint="plannedLocation"
-  />
-);
+// const CancelMoveModal = props => (
+//   <BaseMoveModal
+//     {...props}
+//     Component={CancelMoveForm}
+//     title="Cancel move"
+//     endpoint="plannedLocation"
+//   />
+// );
