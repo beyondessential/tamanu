6502b65d6e4ccf26e146c5721d9b4b82e940151b
tindn <hi@tindn.io>
2021-01-21 21:50:02 -0500

    add report request tests

---
+++ packages/lan/__tests__/apiv1/ReportRequest.test.js
@@ -0,0 +1,30 @@
+import { createTestContext } from '../utilities';
+
+const { baseApp, models } = createTestContext();
+
+describe('ReportRequest', () => {
+  let app = null;
+  beforeAll(async () => {
+    app = await baseApp.asRole('practitioner');
+  });
+
+  it('should reject reading a patient with insufficient permissions', async () => {
+    const noPermsApp = await baseApp.asRole('base');
+    const result = await noPermsApp.post('/v1/reportRequest/', {});
+    expect(result).toBeForbidden();
+  });
+
+  describe('write', () => {
+    it('should create a new report request', async () => {
+      const result = await app.post('/v1/reportRequest').send({
+        reportType: 'incomplete-referrals',
+        emailList: ['example@gmail.com', 'other@gmail.com'],
+      });
+      expect(result).toHaveSucceeded();
+      expect(result.body).toHaveProperty('id');
+      expect(result.body).toHaveProperty('reportType', 'incomplete-referrals');
+      expect(result.body).toHaveProperty('recipients', 'example@gmail.com,other@gmail.com');
+      expect(result.body).toHaveProperty('requestedByUserId');
+    });
+  });
+});
