99280190697212f4c320bfc6f9b033891875bffa
Tim Kennedy <t.m.knndy@gmail.com>
2021-01-27 15:38:10 +1100

    Allow history table cells to show multiline content

--- packages/mobile/App/ui/components/HistoryTable/index.tsx
+++ packages/mobile/App/ui/components/HistoryTable/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { DataTable } from 'react-native-paper';
+import MultilineCell from './MultilineCell';
+
+// TODO: define more restrictive types for data and rows
 
 /**
  * @param data Object
  * @param rows Object where each key matches a key in data to render,
  *             and each value has a name and optional accessor function.
  */
-export const HistoryTable = ({ data, rows }: { data: object; rows: object}): JSX.Element => (
+export const HistoryTable = ({ data, rows }: { data: object; rows: object }): JSX.Element => (
   <DataTable style={{ paddingHorizontal: 10 }}>
     {Object.entries(rows).map(([key, row]): JSX.Element => {
       const cellValue = row.accessor
@@ -14,8 +17,8 @@ export const HistoryTable = ({ data, rows }: { data: object; rows: object}): JSX
         : ((data[key] === null || data[key] === undefined) || '');
       return (
         <DataTable.Row>
-          <DataTable.Cell>{row.name}</DataTable.Cell>
-          <DataTable.Cell>{cellValue}</DataTable.Cell>
+          <MultilineCell>{row.name}</MultilineCell>
+          <MultilineCell>{cellValue}</MultilineCell>
         </DataTable.Row>
       );
     })}
