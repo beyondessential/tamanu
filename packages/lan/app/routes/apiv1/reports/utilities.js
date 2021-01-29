9f80be2774f2ddb7b3ec3e995db7564ce1b5757e
Tin Nguyen <tindn@users.noreply.github.com>
2021-01-28 21:14:03 -0500

    fix test failing
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/lan/app/routes/apiv1/reports/utilities.js
+++ packages/lan/app/routes/apiv1/reports/utilities.js
@@ -1,3 +1,3 @@
 export function generateReportFromQueryData(queryData, columnTemplate) {
-  return [columnTemplate.map(c => c.title), ...queryData.map(r => columns.map(c => c.accessor(r)))];
+  return [columnTemplate.map(c => c.title), ...queryData.map(r => columnTemplate.map(c => c.accessor(r)))];
 }
