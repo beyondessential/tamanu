621546d3f1f28bb57cb5d76347ef28720b65eed8
Tim Kennedy <t.m.knndy@gmail.com>
2021-02-17 10:37:14 +1100

    Change array function to return

--- packages/mobile/App/ui/navigation/screens/home/PatientDetails/Screen.tsx
+++ packages/mobile/App/ui/navigation/screens/home/PatientDetails/Screen.tsx
@@ -51,77 +51,79 @@ export const Screen = ({
   onEditField,
   changeReminder,
   reminders,
-}: PatientDetailScreenProps): ReactElement => (
-  <FullView>
-    <StyledSafeAreaView background={theme.colors.PRIMARY_MAIN}>
-      <StyledView background={theme.colors.PRIMARY_MAIN} height={170}>
-        <RowView justifyContent="space-between">
-          <StyledTouchableOpacity padding={20} onPress={onNavigateBack}>
-            <ArrowLeftIcon
-              height={screenPercentageToDP(2.43, Orientation.Height)}
-              width={screenPercentageToDP(2.43, Orientation.Height)}
-            />
-          </StyledTouchableOpacity>
-          <StyledTouchableOpacity padding={20} onPress={onNavigateToFilters}>
-            <KebabIcon />
-          </StyledTouchableOpacity>
-        </RowView>
-        <RowView paddingLeft={screenPercentageToDP(4.86, Orientation.Width)}>
-          <UserAvatar
-            size={screenPercentageToDP(7.29, Orientation.Height)}
-            displayName={joinNames(patientData.generalInfo)}
-            {...avatarMock}
-          />
-          <StyledView alignItems="flex-start" marginLeft={10}>
-            <StyledText
-              color={theme.colors.WHITE}
-              fontSize={screenPercentageToDP(3.4, Orientation.Height)}
-              fontWeight="bold"
-            >
-              {joinNames(patientData.generalInfo)}
-            </StyledText>
-            <StyledText
-              color={theme.colors.WHITE}
-              fontSize={screenPercentageToDP(1.94, Orientation.Height)}
-            >
-              {getGender(patientData.generalInfo.sex)},{' '}
-              {getAgeFromDate(new Date(patientData.generalInfo.dateOfBirth))} years old,{' '}
-            </StyledText>
-          </StyledView>
-        </RowView>
-      </StyledView>
-      <HealthIdentificationRow patientId={patientData.id} />
-    </StyledSafeAreaView>
+}: PatientDetailScreenProps): ReactElement => {
+  return (
     <FullView>
-      <StyledScrollView
-        background={theme.colors.BACKGROUND_GREY}
-        paddingLeft={20}
-        paddingRight={20}
-        paddingTop={20}
-      >
-        <GeneralInfo
-          id={patientData.id}
-          generalInfo={patientData.generalInfo}
-        />
-        <NotificationCheckbox value={reminders} onChange={changeReminder} />
-        <FamilyInformation
-          onEdit={onEditField}
-          parentsInfo={patientData.parentsInfo}
-        />
-        <OnGoingConditions
-          onEdit={onEditField}
-          ongoingConditions={patientData.ongoingConditions}
-        />
-        <FamilyHistory
-          onEdit={onEditField}
-          familyHistory={patientData.familyHistory}
-        />
-        <AllergiesList onEdit={onEditField} allergies={patientData.allergies} />
-        <PatientIssues
-          onEdit={onEditField}
-          patientIssues={patientData.patientIssues}
-        />
-      </StyledScrollView>
+      <StyledSafeAreaView background={theme.colors.PRIMARY_MAIN}>
+        <StyledView background={theme.colors.PRIMARY_MAIN} height={170}>
+          <RowView justifyContent="space-between">
+            <StyledTouchableOpacity padding={20} onPress={onNavigateBack}>
+              <ArrowLeftIcon
+                height={screenPercentageToDP(2.43, Orientation.Height)}
+                width={screenPercentageToDP(2.43, Orientation.Height)}
+              />
+            </StyledTouchableOpacity>
+            <StyledTouchableOpacity padding={20} onPress={onNavigateToFilters}>
+              <KebabIcon />
+            </StyledTouchableOpacity>
+          </RowView>
+          <RowView paddingLeft={screenPercentageToDP(4.86, Orientation.Width)}>
+            <UserAvatar
+              size={screenPercentageToDP(7.29, Orientation.Height)}
+              displayName={joinNames(patientData.generalInfo)}
+              {...avatarMock}
+            />
+            <StyledView alignItems="flex-start" marginLeft={10}>
+              <StyledText
+                color={theme.colors.WHITE}
+                fontSize={screenPercentageToDP(3.4, Orientation.Height)}
+                fontWeight="bold"
+              >
+                {joinNames(patientData.generalInfo)}
+              </StyledText>
+              <StyledText
+                color={theme.colors.WHITE}
+                fontSize={screenPercentageToDP(1.94, Orientation.Height)}
+              >
+                {getGender(patientData.generalInfo.sex)},{' '}
+                {getAgeFromDate(new Date(patientData.generalInfo.dateOfBirth))} years old,{' '}
+              </StyledText>
+            </StyledView>
+          </RowView>
+        </StyledView>
+        <HealthIdentificationRow patientId={patientData.id} />
+      </StyledSafeAreaView>
+      <FullView>
+        <StyledScrollView
+          background={theme.colors.BACKGROUND_GREY}
+          paddingLeft={20}
+          paddingRight={20}
+          paddingTop={20}
+        >
+          <GeneralInfo
+            id={patientData.id}
+            generalInfo={patientData.generalInfo}
+          />
+          <NotificationCheckbox value={reminders} onChange={changeReminder} />
+          <FamilyInformation
+            onEdit={onEditField}
+            parentsInfo={patientData.parentsInfo}
+          />
+          <OnGoingConditions
+            onEdit={onEditField}
+            ongoingConditions={patientData.ongoingConditions}
+          />
+          <FamilyHistory
+            onEdit={onEditField}
+            familyHistory={patientData.familyHistory}
+          />
+          <AllergiesList onEdit={onEditField} allergies={patientData.allergies} />
+          <PatientIssues
+            onEdit={onEditField}
+            patientIssues={patientData.patientIssues}
+          />
+        </StyledScrollView>
+      </FullView>
     </FullView>
-  </FullView>
-);
+  );
+};
