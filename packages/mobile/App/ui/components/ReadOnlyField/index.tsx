6c4d5af1b00d19a6b092775847791e8d00d9f51d
Tom McLean <tom.a.mclean@gmail.com>
2021-02-16 20:03:02 +1100

    Round readonly numeric values to 2 decimal places

--- packages/mobile/App/ui/components/ReadOnlyField/index.tsx
+++ packages/mobile/App/ui/components/ReadOnlyField/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { StyledText } from '~/ui/styled/common';
 
-export const ReadOnlyField = ({ name, value }) => {
-  if(!value && value !== 0) {
-    return (<StyledText>N/A</StyledText>);
+export const ReadOnlyField = ({ name, value }: { name: string, value: any }) => {
+  if(typeof value === "number") {
+    return <StyledText>{value.toFixed(2)}</StyledText>;
+  }
+  
+  if(!value) {
+    return <StyledText color="#aaa">N/A</StyledText>;
   }
 
   return <StyledText>{value}</StyledText>;
