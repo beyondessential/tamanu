ad7e78d3525354c0e2faf0b8b1e95ca66b9d7250
Tim Kennedy <t.m.knndy@gmail.com>
2021-02-17 17:26:35 +1100

    Desktop: Warn on startup if env var HOST isn't set

--- packages/desktop/app/api/singletons.js
+++ packages/desktop/app/api/singletons.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { TamanuApi } from './TamanuApi';
 
+if (!process.env.HOST) {
+  console.error('Warning: environmental variable HOST must be set');
+}
 export const API = new TamanuApi(process.env.HOST);
 export const ApiContext = React.createContext(API);
