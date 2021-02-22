c30178ad2d21c534690c5633a3d0c21f89b5b678
Edwin <edmofro@gmail.com>
2021-02-22 18:25:13 +1300

    Remove prettier changes from main.dev.js

--- packages/desktop/app/main.dev.js
+++ packages/desktop/app/main.dev.js
@@ -9,10 +9,7 @@
  */
 import { autoUpdater } from 'electron-updater';
 import { app, BrowserWindow } from 'electron';
-import installExtension, {
-  REDUX_DEVTOOLS,
-  REACT_DEVELOPER_TOOLS,
-} from 'electron-devtools-installer';
+import installExtension, { REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 // production only
 import sourceMapSupport from 'source-map-support';
@@ -59,7 +56,7 @@ app.on('ready', async () => {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-      enableRemoteModule: true,
+      enableRemoteModule: true
     },
   });
 
