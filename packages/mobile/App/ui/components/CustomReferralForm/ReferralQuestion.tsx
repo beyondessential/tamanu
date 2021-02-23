b43c4283eaec93b7f04de5efd6bcffb9189874a7
Bastien Goepfert <bas.goepfert@gmail.com>
2021-02-23 17:17:36 +1100

    use question for autocomplete

--- packages/mobile/App/ui/components/CustomReferralForm/ReferralQuestion.tsx
+++ packages/mobile/App/ui/components/CustomReferralForm/ReferralQuestion.tsx
@@ -55,7 +55,7 @@ export const ReferralQuestion = ({ data, patientData, navigation }) => {
         return (
           <Field
             component={fieldInput}
-            placeholder="Search..."
+            placeholder={question}
             navigation={navigation}
             suggester={source && createSuggester(models, source, options)}
             options={!source && scsvToOptions(options)}
