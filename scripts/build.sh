b671c4e972575bf467a4030f9682fc0245e86d83
Edwin <edmofro@gmail.com>
2021-02-24 18:52:39 +1300

    Use new command rather than breaking old one

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ touch ${DESKTOP_ROOT}/.env &&
   printf '%s\n' "${desktop_config[@]}" > ${DESKTOP_ROOT}/.env &&
   echo ${DESKTOP_ROOT}/.env
 
-yarn --cwd ${DESKTOP_ROOT} run package-win
+yarn --cwd ${DESKTOP_ROOT} run package-and-publish-win
 yarn --cwd ${LAN_ROOT} run package
 
 ./scripts/pack.sh ${type}
