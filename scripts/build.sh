d8672dd9c76d0589d72cbe2557876d5a48aa6f64
Edwin <edmofro@gmail.com>
2021-02-24 18:49:10 +1300

    Revert to previous build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -euxo pipefail
 node --version
-echo "Building"
+type=$1
+echo "Building - ${type}"
 
-yarn run build-shared
+yarn run build-shared # make sure shared package exists
 
 IFS='|' read -a desktop_config <<< "${CONFIG_DESKTOP}"
 touch ${DESKTOP_ROOT}/.env &&
@@ -11,3 +12,6 @@ touch ${DESKTOP_ROOT}/.env &&
   echo ${DESKTOP_ROOT}/.env
 
 yarn --cwd ${DESKTOP_ROOT} run package-win
+yarn --cwd ${LAN_ROOT} run package
+
+./scripts/pack.sh ${type}
