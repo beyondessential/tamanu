152f9af15194d5a5f6b755394b16f09a8a8e1c82
Tim Kennedy <t.m.knndy@gmail.com>
2021-02-26 11:34:35 +1100

    Move getCodeForErrorName to shared-src

--- packages/shared-src/src/errors.js
+++ packages/shared-src/src/errors.js
@@ -11,3 +11,29 @@ export class ForbiddenError extends BaseError {}
 export class InvalidOperationError extends BaseError {}
 export class InvalidParameterError extends BaseError {}
 export class InappropriateEndpointError extends BaseError {}
+
+export function getCodeForErrorName(name) {
+  switch (name) {
+    case 'BadAuthenticationError':
+      return 401;
+    case 'ForbiddenError':
+      return 403;
+    case 'NotFoundError':
+      return 404;
+    case 'InappropriateEndpointError':
+      // method not allowed - usually for PUTting an endpoint that expects POST
+      // but it's the closest status code we have without getting in to redirects
+      return 405;
+    case 'SequelizeUniqueConstraintError':
+    case 'SequelizeValidationError':
+    case 'SequelizeForeignKeyConstraintError':
+    case 'InvalidOperationError':
+    case 'InvalidParameterError':
+      // unprocessable entity - syntax is correct but data is bad
+      return 422;
+    default:
+      // error isn't otherwise caught - this is a problem with the server
+      return 500;
+  }
+}
+
