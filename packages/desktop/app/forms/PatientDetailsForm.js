6f80adf1e3d06e860c9fb5a5a77c2c768d7cbb51
tindn <hi@tindn.io>
2021-03-03 21:14:18 -0500

    remove random data for demo purpose

--- packages/desktop/app/forms/PatientDetailsForm.js
+++ packages/desktop/app/forms/PatientDetailsForm.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Chance } from 'chance';
 
 import { FormGrid } from '../components/FormGrid';
 import { ButtonRow } from '../components/ButtonRow';
@@ -83,15 +82,6 @@ export const PatientDetailsForm = ({
   patient,
   onSubmit,
 }) => {
-  const chance = new Chance(patient.id); // seed random with user id for reproducible values
-  const dummyData = {
-    residentialAddress: `${chance.address()}, ${chance.city()}, Fiji`,
-    contactNumber: `${chance.phone({ formatted: false }).slice(0, 3)} ${chance.phone({ formatted: false }).slice(0, 4)}`,
-    socialMediaPlatform: chance.pickone(['Facebook', 'Instagram', 'LinkedIn', 'Twitter', 'Viber', 'Whatsapp']),
-    socialMediaName: `@${chance.animal().replace(/[^a-zA-Z]/g, '')}${chance.natural({ min: 0, max: 99, exclude: [69] })}`,
-    email: chance.email(),
-  };
-
   const render = React.useCallback(
     ({ submitForm }) => (
       <FormGrid>
@@ -110,5 +100,5 @@ export const PatientDetailsForm = ({
     [patientSuggester, facilitySuggester],
   );
 
-  return <Form render={render} initialValues={{ ...patient, ...dummyData }} onSubmit={onSubmit} />;
+  return <Form render={render} initialValues={patient} onSubmit={onSubmit} />;
 };
