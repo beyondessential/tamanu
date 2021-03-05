7fd5a147c1a382fd772b1d68887681c932d737c2
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-05 15:05:11 +1100

    Break SureyScreenComponent and ProgramDataElement into separate files

---
+++ packages/shared-src/src/models/ProgramDataElement.js
@@ -0,0 +1,32 @@
+import { Sequelize } from 'sequelize';
+import { PROGRAM_DATA_ELEMENT_TYPE_VALUES } from 'shared/constants';
+import { parseOrNull } from 'shared/utils/parse-or-null';
+import { Model } from './Model';
+
+export class ProgramDataElement extends Model {
+  static init({ primaryKey, ...options }) {
+    super.init(
+      {
+        id: primaryKey,
+        code: Sequelize.STRING,
+        name: Sequelize.STRING,
+        indicator: Sequelize.STRING,
+        defaultText: Sequelize.STRING,
+        defaultOptions: Sequelize.STRING,
+        type: Sequelize.ENUM(PROGRAM_DATA_ELEMENT_TYPE_VALUES),
+      },
+      {
+        ...options,
+        indexes: [{ unique: true, fields: ['code'] }],
+      },
+    );
+  }
+
+  forResponse() {
+    const { defaultOptions, ...values } = this.dataValues;
+    return {
+      ...values,
+      defaultOptions: parseOrNull(defaultOptions),
+    };
+  }
+}
