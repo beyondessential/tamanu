f4bc089fdf38f210d2b0560d9fc498229221a637
Edwin <edmofro@gmail.com>
2021-03-05 14:51:05 +1300

    Use node to replace publish path

--- scripts/build_desktop.sh
+++ scripts/build_desktop.sh
@@ -12,9 +12,21 @@ if [[ $1 == "build-only" ]]; then
     yarn --cwd ${DESKTOP_ROOT} run package-win
 else
     RELEASE_BRANCH_PREFIX="release-desktop-"
-    BUILD_FOLDER="${CI_BRANCH:${#RELEASE_BRANCH_PREFIX}}"
+    BUILD_FOLDER="desktop/${CI_BRANCH:${#RELEASE_BRANCH_PREFIX}}"
+
+    # Replace the publish path in package.json with the build folder
+    node -e "
+      let pkg=require('./packages/desktop/package.json');     // read in package.json
+      pkg.build.publish.path='${BUILD_FOLDER}';               // replace publish path
+      require('fs')                                           // write package.json back out
+        .writeFileSync(
+          'packages/desktop/package.json',
+          JSON.stringify(pkg, null, 2)
+        );
+    "
+
     echo "Publishing to ${BUILD_FOLDER}"
-    S3_BUILD_PATH="desktop-app/${BUILD_FOLDER}" yarn --cwd ${DESKTOP_ROOT} run package-and-publish-win
+    yarn --cwd ${DESKTOP_ROOT} run package-and-publish-win
 fi
 
 ./scripts/pack.sh desktop
