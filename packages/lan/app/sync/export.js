e58b80c61f0f93138ed2116aa8cf9456ad598ae5
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-07 13:34:59 +1100

    WIP: non-functional nested record export

--- packages/lan/app/sync/export.js
+++ packages/lan/app/sync/export.js
@@ -1,22 +1,39 @@
 import { Op } from 'sequelize';
 import { memoize } from 'lodash';
+import { propertyPathsToTree } from './metadata';
 
 // TODO: nested model support
-export const createExportPlan = memoize(model => ({ model }));
+export const createExportPlan = memoize(model => {
+  const relationTree = propertyPathsToTree(model.includedSyncRelations);
+  const include = includeFromTree(relationTree);
+  return { model, include, relationTree };
+});
 
-export const executeExportPlan = async ({ model }, { after, limit = 100 }) => {
-  const where = {
-    markedForPush: true,
-  };
-  if (after) {
-    where.id = { [Op.gt]: after.data.id };
-  }
+export const includeFromTree = relationTree =>
+  Object.entries(relationTree).map(([association, children]) => {
+    const include = includeFromTree(children);
+    if (include.length > 0) {
+      return { association, include };
+    }
+    return { association };
+  });
 
-  const dbRecords = await model.findAll({
-    where,
+export const executeExportPlan = async ({ model, include }, { after, limit = 100 }) => {
+  const options = {
+    where: {
+      markedForPush: true,
+    },
     limit,
     order: [['id', 'ASC']],
-  });
+  };
+  if (after) {
+    options.where.id = { [Op.gt]: after.data.id };
+  }
+  if (include) {
+    options.include = include;
+  }
+  const dbRecords = await model.findAll(options);
+  console.log(dbRecords[0]?.administeredVaccines);
 
   return dbRecords.map(record => ({ data: record.dataValues }));
 };
