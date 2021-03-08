8befbdf436930f1b686530d720b58870e8047637
Edwin <edmofro@gmail.com>
2021-03-08 14:56:02 +1300

    Attach client version header to supertest

--- packages/lan/app/middleware/versionCompatibility.js
+++ packages/lan/app/middleware/versionCompatibility.js
@@ -4,7 +4,7 @@ import { VERSION_COMPATIBILITY_ERRORS } from 'shared/constants';
 // If a new version of the desktop app is being released in conjunction with an update to the LAN
 // server, set MIN_CLIENT_VERSION to reflect that, and desktop users will be logged out until they
 // have updated.
-const MIN_CLIENT_VERSION = '1.0.0';
+export const MIN_CLIENT_VERSION = '1.0.0';
 const MAX_CLIENT_VERSION = '1.0.0';
 
 export const versionCompatibility = (req, res, next) => {
