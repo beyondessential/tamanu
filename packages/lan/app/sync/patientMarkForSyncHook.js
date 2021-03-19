8dbdad35ad2510f4cd0e07b53a6ed1b2215abe11
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-19 13:05:15 +1100

    Fix error handling in hook

--- packages/lan/app/sync/patientMarkForSyncHook.js
+++ packages/lan/app/sync/patientMarkForSyncHook.js
@@ -2,17 +2,15 @@ import { log } from '~/logging';
 
 export const addPatientMarkForSyncHook = context => {
   context.models.Patient.addHook('afterSave', 'syncWhenMarkedForSync', async patient => {
-    if (patient.changed('markedForSync') && patient.markedForSync && patient.id) {
-      log.debug(
-        `[syncWhenMarkedForSync] patient ${patient.id} was newly marked for sync, syncing immediately...`,
-      );
-      try {
+    try {
+      if (patient.changed('markedForSync') && patient.markedForSync && patient.id) {
+        log.debug(
+          `[syncWhenMarkedForSync] patient ${patient.id} was newly marked for sync, syncing immediately...`,
+        );
         await context.syncManager.runSync(patient.id);
-      } catch(e) {
-        log.warn(
-          `[syncWhenMarkedForSync] patient ${patient.id} failed to sync`,
-        );        
       }
+    } catch (e) {
+      log.warn(`[syncWhenMarkedForSync] patient ${patient.id} failed to sync: ${e.stack}`);
     }
   });
 };
