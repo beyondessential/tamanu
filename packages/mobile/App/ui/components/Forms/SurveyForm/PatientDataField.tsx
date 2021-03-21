d8419093f8af382f405dcb74a7cb345d4d91b64c
Bastien Goepfert <bas.goepfert@gmail.com>
2021-03-22 00:59:19 +1100

    update with transforms

--- packages/mobile/App/ui/components/Forms/SurveyForm/PatientDataField.tsx
+++ packages/mobile/App/ui/components/Forms/SurveyForm/PatientDataField.tsx
@@ -3,6 +3,20 @@ import React from 'react';
 import { StyledView } from '/styled/common';
 import { Field } from '../FormField';
 import { TextField } from '../../TextField/TextField';
+import { getAgeFromDate } from '~/ui/helpers/date';
+import { joinNames } from '~/ui/helpers/user';
+
+const transformPatientData = (patient, config) => {
+  const { dateOfBirth, firstName, lastName } = patient;
+  switch (config.column) {
+    case 'age':
+      return getAgeFromDate(dateOfBirth);
+    case 'fullName':
+      return joinNames({ firstName, lastName });
+    default:
+      return patient[config.column];
+  }
+}
 
 export const PatientDataField = ({ patient, name, config, defaultText }) => (
   <StyledView marginTop={10}>
@@ -10,7 +24,7 @@ export const PatientDataField = ({ patient, name, config, defaultText }) => (
       component={TextField}
       name={name}
       label={defaultText}
-      value={patient[config.column]}
+      value={transformPatientData(patient, config)}
       disabled
     />
   </StyledView>
