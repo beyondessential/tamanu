7f71748af1e0ccc7c104c4771ffbeb3c78737447
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-26 11:06:18 +1100

    Update packages/meta-server/app/servers.js
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/meta-server/app/servers.js
+++ packages/meta-server/app/servers.js
@@ -4,7 +4,7 @@ export const serversRouter = express.Router();
 
 const servers = [
   { name: 'Dev', type: 'dev', host: 'https://sync-dev.tamanu.io' },
-  { name: 'Demo', type: 'dev', host: 'https://sync-demo.tamanu.io' },
+  { name: 'Demo', type: 'live', host: 'https://sync-demo.tamanu.io' },
   { name: 'Stress testing', type: 'dev', host: 'https://sync-stress-test.tamanu.io' },
   { name: 'Fiji', type: 'live', host: 'https://fiji.tamanu.io' },
   { name: 'Tonga', type: 'live', host: 'https://tonga.tamanu.io' },
