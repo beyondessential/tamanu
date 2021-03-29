da24eece076d3627272d1be6ca932713cad3a3ae
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-29 11:09:53 +1100

    Deploy master (and test branch) to sync demo environment

--- scripts/deploy_sync_dev.sh
+++ scripts/deploy_sync_dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-codeship_aws eb_deploy "./packages/sync-server/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV" "$SYNC_SERVER_EB_S3"
+codeship_aws eb_deploy "./packages/sync-server/release" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_ENV_DEV" "$SYNC_SERVER_EB_S3_DEV"
