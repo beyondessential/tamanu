1c79fce824efb2322a1abee16035c8cbb7c67a2d
Tim Kennedy <t.m.knndy@gmail.com>
2021-03-31 16:45:51 +1100

    Strip undefined and null on server-side imports

--- packages/shared-src/__tests__/models/sync/import.test.js
+++ packages/shared-src/__tests__/models/sync/import.test.js
@@ -29,140 +29,175 @@ const toSyncRecord = record => ({
 });
 
 describe('import', () => {
-  let models;
-  let context;
-  const patientId = uuidv4();
-  beforeAll(async () => {
-    context = await initDb({ syncClientMode: true }); // TODO: test server mode too
-    models = context.models;
-    await models.Patient.create({ ...fakePatient(), id: patientId });
+  describe('in client mode', () => {
+    let models;
+    let context;
+    const patientId = uuidv4();
+    beforeAll(async () => {
+      context = await initDb({ syncClientMode: true }); // TODO: test server mode too
+      models = context.models;
+      await models.Patient.create({ ...fakePatient(), id: patientId });
+    });
+
+    const rootTestCases = [
+      ['Patient', fakePatient],
+      ['Program', fakeProgram],
+      ['ProgramDataElement', fakeProgramDataElement],
+      ['ReferenceData', fakeReferenceData],
+      ['ScheduledVaccine', () => buildScheduledVaccine(context)],
+      ['Survey', fakeSurvey],
+      ['SurveyScreenComponent', fakeSurveyScreenComponent],
+      ['User', fakeUser],
+      [
+        'Encounter',
+        async () => buildNestedEncounter(context, patientId),
+        `patient/${patientId}/encounter`,
+        {
+          include: [
+            { association: 'administeredVaccines' },
+            { association: 'diagnoses' },
+            { association: 'medications' },
+            {
+              association: 'surveyResponses',
+              include: [{ association: 'answers' }],
+            },
+            {
+              association: 'labRequests',
+              include: [{ association: 'tests' }],
+            },
+            { association: 'imagingRequests' },
+          ],
+        },
+      ],
+      [
+        'PatientAllergy',
+        () => ({ ...fake(models.PatientAllergy), patientId }),
+        `patient/${patientId}/allergy`,
+      ],
+      [
+        'PatientCarePlan',
+        () => ({ ...fake(models.PatientCarePlan), patientId }),
+        `patient/${patientId}/carePlan`,
+      ],
+      [
+        'PatientCondition',
+        () => ({ ...fake(models.PatientCondition), patientId }),
+        `patient/${patientId}/condition`,
+      ],
+      [
+        'PatientFamilyHistory',
+        () => ({ ...fake(models.PatientFamilyHistory), patientId }),
+        `patient/${patientId}/familyHistory`,
+      ],
+      [
+        'PatientIssue',
+        () => ({ ...fake(models.PatientIssue), patientId }),
+        `patient/${patientId}/issue`,
+      ],
+      [
+        'LabTestType',
+        async () => {
+          const labTestCategory = {
+            ...fake(models.ReferenceData),
+            type: REFERENCE_TYPES.LAB_TEST_TYPE,
+          };
+          await models.ReferenceData.create(labTestCategory);
+          return { ...fake(models.LabTestType), labTestCategoryId: labTestCategory.id };
+        },
+      ],
+    ];
+
+    rootTestCases.forEach(([modelName, fakeRecord, overrideChannel = null, options = {}]) => {
+      describe(modelName, () => {
+        it('creates the record', async () => {
+          // arrange
+          const model = models[modelName];
+          const record = await fakeRecord();
+          const channel = overrideChannel || (await model.getChannels())[0];
+
+          // act
+          const plan = createImportPlan(model);
+          await executeImportPlan(plan, channel, toSyncRecord(record));
+
+          // assert
+          const dbRecord = await model.findByPk(record.id, options);
+          expect(dbRecord.get({ plain: true })).toMatchObject({
+            ...record,
+            ...(model.tableAttributes.pushedAt ? { pulledAt: expect.any(Date) } : {}),
+          });
+        });
+
+        it('updates the record', async () => {
+          // arrange
+          const model = models[modelName];
+          const oldRecord = await fakeRecord();
+          const newRecord = {
+            ...(await fakeRecord()),
+            id: oldRecord.id,
+          };
+          await model.create(oldRecord);
+          const channel = overrideChannel || (await model.getChannels())[0];
+
+          // act
+          const plan = createImportPlan(model);
+          await executeImportPlan(plan, channel, toSyncRecord(newRecord));
+
+          // assert
+          const dbRecord = await model.findByPk(oldRecord.id, options);
+          expect(dbRecord.get({ plain: true })).toMatchObject({
+            ...newRecord,
+            ...(model.tableAttributes.pushedAt ? { pulledAt: expect.any(Date) } : {}),
+          });
+        });
+
+        it('deletes tombstones', async () => {
+          // arrange
+          const model = models[modelName];
+          const record = await fakeRecord();
+          await model.create(record);
+          const channel = overrideChannel || (await model.getChannels())[0];
+
+          // act
+          const plan = createImportPlan(model);
+          await executeImportPlan(plan, channel, { ...toSyncRecord(record), isDeleted: true });
+
+          // assert
+          const dbRecord = await model.findByPk(record.id, options);
+          expect(dbRecord).toEqual(null);
+        });
+      });
+    });
   });
 
-  const rootTestCases = [
-    ['Patient', fakePatient],
-    ['Program', fakeProgram],
-    ['ProgramDataElement', fakeProgramDataElement],
-    ['ReferenceData', fakeReferenceData],
-    ['ScheduledVaccine', () => buildScheduledVaccine(context)],
-    ['Survey', fakeSurvey],
-    ['SurveyScreenComponent', fakeSurveyScreenComponent],
-    ['User', fakeUser],
-    [
-      'Encounter',
-      async () => buildNestedEncounter(context, patientId),
-      `patient/${patientId}/encounter`,
-      {
-        include: [
-          { association: 'administeredVaccines' },
-          { association: 'diagnoses' },
-          { association: 'medications' },
-          {
-            association: 'surveyResponses',
-            include: [{ association: 'answers' }],
-          },
-          {
-            association: 'labRequests',
-            include: [{ association: 'tests' }],
-          },
-          { association: 'imagingRequests' },
-        ],
-      },
-    ],
-    [
-      'PatientAllergy',
-      () => ({ ...fake(models.PatientAllergy), patientId }),
-      `patient/${patientId}/allergy`,
-    ],
-    [
-      'PatientCarePlan',
-      () => ({ ...fake(models.PatientCarePlan), patientId }),
-      `patient/${patientId}/carePlan`,
-    ],
-    [
-      'PatientCondition',
-      () => ({ ...fake(models.PatientCondition), patientId }),
-      `patient/${patientId}/condition`,
-    ],
-    [
-      'PatientFamilyHistory',
-      () => ({ ...fake(models.PatientFamilyHistory), patientId }),
-      `patient/${patientId}/familyHistory`,
-    ],
-    [
-      'PatientIssue',
-      () => ({ ...fake(models.PatientIssue), patientId }),
-      `patient/${patientId}/issue`,
-    ],
-    [
-      'LabTestType',
-      async () => {
-        const labTestCategory = {
-          ...fake(models.ReferenceData),
-          type: REFERENCE_TYPES.LAB_TEST_TYPE,
-        };
-        await models.ReferenceData.create(labTestCategory);
-        return { ...fake(models.LabTestType), labTestCategoryId: labTestCategory.id };
-      },
-    ],
-  ];
+  describe('in server mode', () => {
+    let models;
+    beforeAll(async () => {
+      const context = await initDb({ syncClientMode: false });
+      models = context.models;
+    });
+    
+    it('removes null or undefined fields when importing', async () => {
+      // arrange
+      const { Patient } = models;
+      const oldPatient = await fake(Patient);
+      const newPatient = {
+        ...(await fake(Patient)),
+        firstName: null,
+        id: oldPatient.id,
+      };
+      await Patient.create(oldPatient);
+      const channel = 'patient';
 
-  rootTestCases.forEach(([modelName, fakeRecord, overrideChannel = null, options = {}]) => {
-    describe(modelName, () => {
-      it('creates the record', async () => {
-        // arrange
-        const model = models[modelName];
-        const record = await fakeRecord();
-        const channel = overrideChannel || (await model.getChannels())[0];
+      // act
+      const plan = createImportPlan(Patient);
+      await executeImportPlan(plan, channel, toSyncRecord(newPatient));
 
-        // act
-        const plan = createImportPlan(model);
-        await executeImportPlan(plan, channel, toSyncRecord(record));
-
-        // assert
-        const dbRecord = await model.findByPk(record.id, options);
-        expect(dbRecord.get({ plain: true })).toMatchObject({
-          ...record,
-          ...(model.tableAttributes.pushedAt ? { pulledAt: expect.any(Date) } : {}),
-        });
-      });
-
-      it('updates the record', async () => {
-        // arrange
-        const model = models[modelName];
-        const oldRecord = await fakeRecord();
-        const newRecord = {
-          ...(await fakeRecord()),
-          id: oldRecord.id,
-        };
-        await model.create(oldRecord);
-        const channel = overrideChannel || (await model.getChannels())[0];
-
-        // act
-        const plan = createImportPlan(model);
-        await executeImportPlan(plan, channel, toSyncRecord(newRecord));
-
-        // assert
-        const dbRecord = await model.findByPk(oldRecord.id, options);
-        expect(dbRecord.get({ plain: true })).toMatchObject({
-          ...newRecord,
-          ...(model.tableAttributes.pushedAt ? { pulledAt: expect.any(Date) } : {}),
-        });
-      });
-
-      it('deletes tombstones', async () => {
-        // arrange
-        const model = models[modelName];
-        const record = await fakeRecord();
-        await model.create(record);
-        const channel = overrideChannel || (await model.getChannels())[0];
-
-        // act
-        const plan = createImportPlan(model);
-        await executeImportPlan(plan, channel, { ...toSyncRecord(record), isDeleted: true });
-
-        // assert
-        const dbRecord = await model.findByPk(record.id, options);
-        expect(dbRecord).toEqual(null);
+      // assert
+      const dbPatient = await Patient.findByPk(oldPatient.id);
+      expect(dbPatient.get({ plain: true })).toMatchObject({
+        ...newPatient,
+        firstName: oldPatient.firstName,
+        ...(Patient.tableAttributes.pushedAt ? { pulledAt: expect.any(Date) } : {}),
       });
     });
   });
