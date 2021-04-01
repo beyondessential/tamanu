19418f7a9cf67881099d13105ab6da719d3c0263
Tin Nguyen <tindn@users.noreply.github.com>
2021-03-31 21:38:26 -0400

    Update packages/sync-server/app/tasks/ReportRequestProcessor.js
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/sync-server/app/tasks/ReportRequestProcessor.js
+++ packages/sync-server/app/tasks/ReportRequestProcessor.js
@@ -33,7 +33,7 @@ export class ReportRequestProcessor extends ScheduledTask {
         });
       }
 
-      const reportDataGenerator = getReportModule(requestObject.reportType).dataGenerator;
+      const reportDataGenerator = getReportModule(requestObject.reportType)?.dataGenerator;
 
       if (!reportDataGenerator) {
         log.error(
