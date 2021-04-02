ec0797c7c9f721752a30e28cf7a4e8f9fcaa1514
Tom McLean <tom.a.mclean@gmail.com>
2021-04-02 16:35:54 +1100

    Simplify construction of validator schemas

--- packages/lan/app/admin/importerValidators.js
+++ packages/lan/app/admin/importerValidators.js
@@ -1,37 +1,34 @@
 import * as yup from 'yup';
 
-function createValidationSchemas(context) {
-  const safeIdRegex = /^[A-Za-z0-9-]+$/;
-  const baseSchema = yup.object()
-    .shape({
-      id: yup.string().required().matches(safeIdRegex, 'id must not have spaces or punctuation other than -'),
-    });
+const safeIdRegex = /^[A-Za-z0-9-]+$/;
+const baseSchema = yup.object()
+  .shape({
+    id: yup.string().required().matches(safeIdRegex, 'id must not have spaces or punctuation other than -'),
+  });
 
-  const safeCodeRegex = /^[A-Za-z0-9-.\/]+$/;
-  const referenceDataSchema = baseSchema
-    .shape({
-      type: yup.string().required(),
-      code: yup.string().required().matches(safeCodeRegex, 'code must not have spaces or punctuation other than -./'),
-      name: yup.string().required().max(255),
-    });
+const safeCodeRegex = /^[A-Za-z0-9-.\/]+$/;
+const referenceDataSchema = baseSchema
+  .shape({
+    type: yup.string().required(),
+    code: yup.string().required().matches(safeCodeRegex, 'code must not have spaces or punctuation other than -./'),
+    name: yup.string().required().max(255),
+  });
 
-  const patientSchema = baseSchema;
+const patientSchema = baseSchema;
 
-  const userSchema = baseSchema
-    .shape({
-      email: yup.string().required(),
-      displayName: yup.string().required(),
-      password: yup.string().required(),
-    });
+const userSchema = baseSchema
+  .shape({
+    email: yup.string().required(),
+    displayName: yup.string().required(),
+    password: yup.string().required(),
+  });
 
-  return {
-    base: baseSchema,
-    referenceData: referenceDataSchema,
-    patient: patientSchema,
-    user: userSchema,
-  };
-}
-
+const validationSchemas = {
+  base: baseSchema,
+  referenceData: referenceDataSchema,
+  patient: patientSchema,
+  user: userSchema,
+};
 
 export async function validateRecordSet(records, options = {}) {
   const {
@@ -49,18 +46,16 @@ export async function validateRecordSet(records, options = {}) {
     },
     {}
   );
-  const validationContext = { recordsById };
 
-  const schemas = createValidationSchemas(validationContext);
   const validate = async (record) => {
     const { recordType, data } = record;
-    const schema = schemas[recordType] || schemas.base;
+    const schema = validationSchemas[recordType] || schemas.base;
 
     try {
       await schema.validate(data);
   
       // perform id duplicate check outside of schemas as it relies on consistent
-      // object identities, which is yup's validation does not guarantee
+      // object identities, which yup's validation does not guarantee
       const existing = recordsById[data.id];
       if(existing !== record) {
         throw new yup.ValidationError(`id ${data.id} is already being used at ${existing.sheet}:${existing.row}`);
