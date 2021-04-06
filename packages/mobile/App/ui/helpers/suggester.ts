b027bb435d9cc1c95796261b42d895613e52a9ff
Alastair-L <lockealastair@gmail.com>
2021-04-06 15:28:47 +1000

    Don't hide type errors

--- packages/mobile/App/ui/helpers/suggester.ts
+++ packages/mobile/App/ui/helpers/suggester.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, FindManyOptions, Like, ObjectLiteral } from 'typeorm/browser';
+import { FindManyOptions, Like } from 'typeorm/browser';
 import { BaseModel } from '~/models/BaseModel';
 
 export interface OptionType {
@@ -6,15 +6,11 @@ export interface OptionType {
   value: string;
 }
 
-type BaseModelType = typeof BaseModel;
-export interface BaseModelSubclass extends BaseModelType {
-  id: string,
-  name: string,
-}
+export type BaseModelSubclass = typeof BaseModel;
 
 interface SuggesterOptions<ModelType> extends FindManyOptions<ModelType> {
-  column: string,
-  where: ObjectLiteral, // Override FindManyOptions to disallow 'where' being type string
+  column: string;
+  // where: ObjectLiteral, // Suggester only takes 'where' of type object.
 }
 
 const defaultFormatter = ({ name, id }): OptionType => ({ label: name, value: id });
@@ -29,12 +25,11 @@ export class Suggester<ModelType extends BaseModelSubclass> {
   constructor(model: ModelType, options, formatter = defaultFormatter) {
     this.model = model;
     this.options = options;
+    // If you don't provide a formatter, the suggester assumes that your model has "name" and "id" fields
     this.formatter = formatter;
   }
 
   async fetch(options): Promise<ModelType[]> {
-    // Not sure why this.model.getRepository returns type:
-    // Repository<BaseModel>, not Repository<ModelType>, but it does
     const data = await this.model
       .getRepository()
       .find(options);
@@ -43,7 +38,7 @@ export class Suggester<ModelType extends BaseModelSubclass> {
   }
 
   fetchCurrentOption = async (value: string | null): Promise<OptionType> => {
-    if(!value) return undefined;
+    if (!value) return undefined;
     try {
       const data = await this.model
         .getRepository()
