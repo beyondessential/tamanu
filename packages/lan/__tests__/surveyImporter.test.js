62a5374cb65a55e2b8608a9b6cd58191b08bcffc
Tom McLean <tom.a.mclean@gmail.com>
2021-04-07 19:23:09 +1000

    Some fixes and a little validation for the survey importer

--- packages/lan/__tests__/surveyImporter.test.js
+++ packages/lan/__tests__/surveyImporter.test.js
@@ -1,20 +1,42 @@
+import util from 'util';
+import { importSurvey } from '~/admin/importProgram';
+import { preprocessRecordSet } from '~/admin/preprocessRecordSet';
+
 import { readSurveyXSLX } from 'lan/app/surveyImporter';
-import { createTestContext } from './utilities';
 
 const TEST_SURVEY_PATH = './data/test_programs.xlsx';
 
 describe('Importing surveys', () => {
 
+  let resultInfo = null;
+  let recordGroups = null;
+
+  beforeAll(async () => {
+    const rawData = await importSurvey({ 
+      file: TEST_SURVEY_PATH,
+      programName: 'Test Program',
+      programCode: 'test-program',
+      surveyName: 'Test Survey',
+      surveyCode: 'test-survey',
+    });
+
+    const { 
+      recordGroups: rg, 
+      ...rest 
+    } = await preprocessRecordSet(rawData);
+
+    resultInfo = rest;
+    recordGroups = rg;
+    
+    console.log(util.inspect(resultInfo, { depth: 10 }));
+  });
+
   it('Should import a survey', () => {
-    const surveyData = readSurveyXSLX('Test Survey', TEST_SURVEY_PATH);
-
-    expect(surveyData).toHaveProperty('name', 'Test Survey');
-
-    const { screens } = surveyData;
-    expect(screens).toHaveLength(3);
-
-    const { dataElements } = screens[1];
-    expect(dataElements).toHaveLength(10);
+    const { records } = resultInfo.stats;
+    expect(records).toHaveProperty('program', 1);
+    expect(records).toHaveProperty('survey', 1);
+    expect(records).toHaveProperty('programDataElement', 19);
+    expect(records).toHaveProperty('surveyScreenComponent', 19);
   });
 
 });
