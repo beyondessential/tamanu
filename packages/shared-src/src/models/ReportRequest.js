b96896ab49da2a40138ad5b64e5b1ea5c9079982
tindn <hi@tindn.io>
2021-04-06 21:30:26 -0400

    add markedForPush column to ReportRequest model

--- packages/shared-src/src/models/ReportRequest.js
+++ packages/shared-src/src/models/ReportRequest.js
@@ -3,7 +3,7 @@ import { REPORT_REQUEST_STATUS_VALUES, SYNC_DIRECTIONS } from 'shared/constants'
 import { Model } from './Model';
 
 export class ReportRequest extends Model {
-  static syncDirection = SYNC_DIRECTIONS.BIDIRECTIONAL;
+  static syncDirection = SYNC_DIRECTIONS.PUSH_ONLY;
 
   static init({ primaryKey, ...options }) {
     super.init(
