82f83c98cea0d9fc1fa90ae0026313bab626ac20
Edwin <edmofro@gmail.com>
2021-04-15 13:26:50 +1200

    Match sql max params as download limit

--- packages/mobile/App/services/sync/manager.ts
+++ packages/mobile/App/services/sync/manager.ts
@@ -17,7 +17,7 @@ type Timestamp = number;
 const UPLOAD_LIMIT = 100;
 const INITIAL_DOWNLOAD_LIMIT = 100;
 const MIN_DOWNLOAD_LIMIT = 1;
-const MAX_DOWNLOAD_LIMIT = 10000;
+const MAX_DOWNLOAD_LIMIT = 999; // match sql max params for optimum speed (avoid chunking id fetches)
 const OPTIMAL_DOWNLOAD_TIME_PER_PAGE = 2000; // aim for 2 seconds per page
 const MAX_LIMIT_CHANGE_PER_PAGE = 0.2; // max 20% increase from batch to batch, or it is too jumpy
 
