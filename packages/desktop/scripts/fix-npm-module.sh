ff49df162c1df328acfa24ac3a55278dbfcda939
Kurt Johnson <kurtjohnson@Kurts-MacBook-Pro.local>
2021-04-22 12:54:49 +1000

    Revert "Commit for open source repository as of Date: 20212204"
    This reverts commit 33aca1de09742f64113b04f406a91266d9738c72.

--- packages/desktop/scripts/fix-npm-module.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-file_path="./node_modules/dotenv-webpack/index.js"
-content="const { default: plugin } = require('./dist');\n\nmodule.exports = plugin;";
-if [ ! -f "${file_path}" ]; then
-  file_path = "../../../node_modules/dotenv-webpack/index.js"
-fi
-echo "${content}" > "${file_path}"
-echo "NPM module {dotenv-webpack} updated123 "
-echo "cwd ${file_path}"
