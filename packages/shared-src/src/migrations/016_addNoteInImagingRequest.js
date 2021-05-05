2313aba93f81227855ac29935bf8bc7b8d023670
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-05-05 11:30:29 +1000

    Fix migration

--- packages/shared-src/src/migrations/016_addNoteInImagingRequest.js
+++ packages/shared-src/src/migrations/016_addNoteInImagingRequest.js
@@ -2,12 +2,12 @@ const Sequelize = require('sequelize');
 
 module.exports = {
   up: async query => {
-    await query.addColumn('imaging_request', 'note', {
+    await query.addColumn('imaging_requests', 'note', {
       type: Sequelize.STRING,
       allowNull: true,
     });
   },
   down: async query => {
-    await query.removeColumn('imaging_request', 'note');
+    await query.removeColumn('imaging_requests', 'note');
   },
 };
