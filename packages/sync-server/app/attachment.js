14e34203224fb5c7a5732ec1d01c06352f5bef9d
chris-bes <chris@beyondessential.com.au>
2021-05-07 14:20:05 +1000

    141: addressed review comments

--- packages/sync-server/app/attachment.js
+++ packages/sync-server/app/attachment.js
@@ -8,7 +8,7 @@ attachmentRoutes.get(
   '/:id',
   asyncHandler(async (req, res) => {
     const { query, params } = req;
-    const { base64 = false } = query;
+    const { base64 } = query;
     const { id } = params;
     const attachment = await req.store.models.Attachment.findByPk(id);
 
@@ -16,10 +16,9 @@ attachmentRoutes.get(
       throw new ForbiddenError('You do not have permission to view this attachment.');
     }
 
-    if (base64) {
+    if (base64 === 'true') {
       res.send({ data: Buffer.from(attachment.data).toString('base64') });
     } else {
-      res.status(200);
       res.setHeader('Content-Type', attachment.type);
       res.setHeader('Content-Length', attachment.size);
       res.send(Buffer.from(attachment.data));
