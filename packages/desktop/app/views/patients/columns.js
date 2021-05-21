8ad25e11e61396548fb588225be49a01872704bb
Tim Kennedy <t.m.knndy@gmail.com>
2021-05-21 14:31:35 +1000

    Remove titles from columns.js (Table handles them)

--- packages/desktop/app/views/patients/columns.js
+++ packages/desktop/app/views/patients/columns.js
@@ -11,74 +11,63 @@ const SyncedCell = React.memo(({ value }) => value === true ? <CloudDownload /> 
 
 export const markedForSync = {
   key: 'markedForSync',
-  title: 'Sync',
   minWidth: 26,
   CellComponent: SyncedCell,
 };
 
 export const displayId = {
   key: 'displayId',
-  title: <ConfigurableText flag="fields.displayId.shortLabel"/>,
   minWidth: 80,
   accessor: row => row.displayId || `(${row.id})`,
 };
 
 export const firstName = {
   key: 'firstName',
-  title: <ConfigurableText flag="fields.firstName.shortLabel"/>,
   minWidth: 100,
 };
 
 export const lastName = {
   key: 'lastName',
-  title: <ConfigurableText flag="fields.lastName.shortLabel"/>,
   minWidth: 100,
 };
 
 export const culturalName = {
   key: 'culturalName',
-  title: <ConfigurableText flag="fields.culturalName.shortLabel"/>,
   minWidth: 100,
 };
 
 export const sex = {
   key: 'sex',
-  title: <ConfigurableText flag="fields.sex.shortLabel"/>,
   minWidth: 80,
   CellComponent: SexCell,
 };
 
 export const dateOfBirth = {
   key: 'dateOfBirth',
-  title: <ConfigurableText flag="fields.dateOfBirth.shortLabel"/>,
   minWidth: 100,
   CellComponent: DateOfBirthCell,
 };
 
 export const village = {
   key: 'villageName',
-  title: <ConfigurableText flag="fields.villageName.shortLabel"/>,
   minWidth: 100,
   accessor: row => row?.villageName || null,
 };
 
 export const location = {
   key: 'locationName',
-  title: <ConfigurableText flag="fields.locationName.shortLabel"/>,
   minWidth: 100,
   accessor: row => row.locationName,
 };
 
 export const department = {
   key: 'departmentName',
-  title: <ConfigurableText flag="fields.departmentName.shortLabel"/>,
   minWidth: 100,
   accessor: row => row.departmentName,
 };
 
 export const status = {
   key: 'encounterType',
-  title: <ConfigurableText flag="fields.encounterType.shortLabel"/>,
   minWidth: 100,
   accessor: ({ encounterType }) => {
     if (!encounterType) return '';
@@ -90,10 +79,6 @@ export const status = {
 
 export const vaccinationStatus = {
   key: 'vaccinationStatus',
-  title: <ConfigurableText flag="fields.vaccinationStatus.shortLabel"/>,
   minWidth: 100,
   accessor: row => row.vaccinationStatus || 'Unknown',
 };
-
-export const filterHiddenColumns =
-  (columns, getFlag) => columns.filter(({ key }) => !getFlag(`fields.${key}.hidden`));
