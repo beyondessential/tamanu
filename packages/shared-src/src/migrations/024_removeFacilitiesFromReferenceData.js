4efc79b76f7167f3fbc66576ccb8ea47c3de63d8
Kostas Karvounis <kos.karvounis.zoumpos@gmail.com>
2021-05-28 11:09:04 +1000

    Do not clean up data before re-linking records

--- packages/shared-src/src/migrations/024_removeFacilitiesFromReferenceData.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: async query => {
-    await query.sequelize.query("DELETE FROM reference_data WHERE type = 'facility'");
-  },
-  down: async () => {
-    // No down migration
-  },
-};
