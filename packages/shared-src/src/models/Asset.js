537d74c452733ec2d0bb76e91d58791264671591
chris-bes <chris@beyondessential.com.au>
2021-05-30 15:19:28 +1000

    609: Added http request to insert assets to sync server db

--- packages/shared-src/src/models/Asset.js
+++ packages/shared-src/src/models/Asset.js
@@ -17,8 +17,21 @@ export class Asset extends Model {
     );
   }
 
+  /**
+   * This is only used when inserting asset manually through RestClient
+   * Asset is PULL_ONLY and we don't sync asset up from devices to sync servers.
+   */
+  static sanitizeForSyncServer({ data, ...restOfValues }) {
+    //base64
+    if (typeof data === 'string') {
+      return { ...restOfValues, data: Buffer.from(data, 'base64') };
+    }
+    return { ...restOfValues, data: Buffer.from(data) };
+  }
+
   static sanitizeForSyncClient({ data, ...restOfValues }) {
-    // Need to do this to import blob data properly, otherwise blob data will be truncated
+    // Need to do this to import blob data properly when pulling,
+    // otherwise blob data will be truncated
     return { ...restOfValues, data: Buffer.from(data) };
   }
 
