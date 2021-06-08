6c6b1b92242c07ade2633f16de0e17e9dfabddac
Igor Nadj <igor@beyondessential.com.au>
2021-06-08 10:05:56 +1000

    Refactor: move resetPassword and changePassword into auth middleware

--- packages/sync-server/app/routes.js
+++ packages/sync-server/app/routes.js
@@ -1,14 +1,9 @@
 import express from 'express';
 
-import { resetPassword } from './resetPassword';
-import { changePassword } from './changePassword';
 import { syncRoutes } from './sync';
 import { attachmentRoutes } from './attachment';
 
 export const routes = express.Router();
-
-routes.use('/resetPassword', resetPassword);
-routes.use('/changePassword', changePassword);
 
 routes.use('/sync', syncRoutes);
 routes.use('/attachment', attachmentRoutes);
