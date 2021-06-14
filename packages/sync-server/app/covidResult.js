a07121c7cda1a72ba44af499f042b79b96540691
Alastair-L <lockealastair@gmail.com>
2021-06-14 16:40:15 +1000

    Fix bugs

--- packages/sync-server/app/covidResult.js
+++ packages/sync-server/app/covidResult.js
@@ -10,6 +10,25 @@ const COVID_SWAB_LAB_TEST_TYPE_ID = 'labTestType-Lymphocytes';
 
 const getPatientInitials = patient => `${patient.firstName ? patient.firstName.substring(0, 1) : ''}${patient.middleName ? patient.middleName.substring(0, 1) : ''}${patient.lastName ? patient.lastName.substring(0, 1) : ''}`;
 
+const transformLabRequest = async (models, labRequest) => {
+  const { id, createdAt, encounterId, status } = labRequest;
+  const encounter = await models.Encounter.findOne({ where: { id: encounterId } });
+  const patient = await models.Patient.findOne({ where: { id: encounter.patientId } });
+
+  const { result } = await models.LabTest.findOne({
+    where: {
+      labRequestId: id,
+      labTestTypeId: COVID_SWAB_LAB_TEST_TYPE_ID
+    }
+  });
+
+  return {
+    testDate: createdAt,
+    patientInitials: getPatientInitials(patient),
+    testResult: status === LAB_REQUEST_STATUSES.PUBLISHED ? result : 'Unpublished',
+  };
+}
+
 covidResultRoutes.get(
   '/:displayId',
   asyncHandler(async (req, res) => {
@@ -21,27 +40,10 @@ covidResultRoutes.get(
         id: displayId,
         labTestCategoryId: COVID_LAB_TEST_CATEGORY_ID,
       },
-      order: [['createdAt', 'DESC']],
+      // order: [['createdAt', 'DESC']],
     });
 
-    const labRequestsToReport = labRequests
-      .map(({ createdAt, encounterId, status }) => {
-        const encounter = await models.Encounter.findOne({ where: { id: encounterId } });
-        const patient = await models.Patient.findOne({ where: { id: encounter.patientId } });
-
-        const { result } = await models.LabTest.findOne({
-          where: {
-            labRequestId: displayId,
-            labTestTypeId: COVID_SWAB_LAB_TEST_TYPE_ID
-          }
-        });
-
-        return {
-          testDate: createdAt,
-          patientInitials: getPatientInitials(patient),
-          testResult: status === LAB_REQUEST_STATUSES.PUBLISHED ? result : 'Unpublished',
-        }
-      });
+    const labRequestsToReport = await Promise.all(labRequests.map(labRequest => transformLabRequest(models, labRequest)));
 
     res.send({
       data: labRequestsToReport,
