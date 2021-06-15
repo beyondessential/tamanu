078b7d59a1c497d9c3d5ccbd0c608498b7bb3de7
Tim Kennedy <t.m.knndy@gmail.com>
2021-06-15 12:59:37 +1000

    Move channelRouter to shared-src init and decouple from BasicHandler

--- packages/sync-server/app/database/wrapper/sqlWrapper.js
+++ packages/sync-server/app/database/wrapper/sqlWrapper.js
@@ -1,4 +1,3 @@
-import wayfarer from 'wayfarer';
 import { initDatabase } from 'shared/services/database';
 import { BasicHandler } from './handlers';
 
@@ -6,8 +5,6 @@ export class SqlWrapper {
   models = null;
 
   sequelize = null;
-
-  builtRoutes = [];
 
   constructor(dbOptions) {
     // init database
@@ -18,10 +15,10 @@ export class SqlWrapper {
   }
 
   async init() {
-    const { sequelize, models } = await this._dbPromise;
+    const { sequelize, models, channelRouter } = await this._dbPromise;
     this.sequelize = sequelize;
     this.models = models;
-    this.channelRouter = this.buildChannelRouter();
+    this.channelRouter = channelRouter;
     return this;
   }
 
@@ -29,54 +26,48 @@ export class SqlWrapper {
     await this.sequelize.close();
   }
 
-  buildChannelRouter() {
-    const channelRouter = wayfarer();
-    for (const model of Object.values(this.models)) {
-      for (const route of model.channelRoutes) {
-        this.builtRoutes.push(route);
-        const handler = new BasicHandler(model); // TODO: deprecate handlers
-        channelRouter.on(route, async (urlParams, f) => {
-          const params = { ...urlParams, route };
-          return f(handler, params, model);
-        });
-      }
-    }
-    return channelRouter;
-  }
-
   // ONLY FOR TESTS, ignores "paranoid"'s soft deletion
   async unsafeRemoveAllOfChannel(channel) {
     if (process.env.NODE_ENV !== 'test') {
       throw new Error('DO NOT use unsafeRemoveAllOfChannel outside tests!');
     }
-    return this.channelRouter(channel, handler => handler.unsafeRemoveAll());
+    return this.channelRouter(channel, model => {
+      const handler = new BasicHandler(model);
+      return handler.unsafeRemoveAll();
+    });
   }
 
   async upsert(channel, record) {
-    return this.channelRouter(channel, (handler, params) =>
-      handler.upsert(record, params, channel),
-    );
+    return this.channelRouter(channel, (model, params) => {
+      const handler = new BasicHandler(model);
+      return handler.upsert(record, params, channel);
+    });
   }
 
   // TODO: this is a hack to enable sharing import/export across sync and lan
   async withModel(channel, f) {
-    return this.channelRouter(channel, (handler, params, model) => f(model));
+    return this.channelRouter(channel, (model, params) => f(model, params));
   }
 
   async countSince(channel, since) {
-    return this.channelRouter(channel, (handler, params) =>
-      handler.countSince({ ...params, since }, channel),
-    );
+    return this.channelRouter(channel, (model, params) => {
+      const handler = new BasicHandler(model);
+      return handler.countSince({ ...params, since }, channel);
+    });
   }
 
   async findSince(channel, since, { limit, offset } = {}) {
-    return this.channelRouter(channel, (handler, params) =>
-      handler.findSince({ ...params, since, limit, offset }, channel),
-    );
+    return this.channelRouter(channel, (model, params) => {
+      const handler = new BasicHandler(model);
+      return handler.findSince({ ...params, since, limit, offset }, channel);
+    });
   }
 
   async markRecordDeleted(channel, id) {
-    return this.channelRouter(channel, handler => handler.markRecordDeleted(id));
+    return this.channelRouter(channel, model => {
+      const handler = new BasicHandler(model);
+      return handler.markRecordDeleted(id);
+    });
   }
   //------------------------------------
   // required for auth middleware
