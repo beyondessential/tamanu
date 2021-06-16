85ed556a9942249b5a657467d28a126b0c397fac
Bastien Goepfert <bas.goepfert@gmail.com>
2021-06-16 13:21:29 +1000

    add extra step to deleting vaccine

--- packages/desktop/app/components/EditAdministeredVaccineModal.js
+++ packages/desktop/app/components/EditAdministeredVaccineModal.js
@@ -1,31 +1,74 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
 
 import { Modal } from './Modal';
+import { ConfirmAdministeredVaccineDelete } from './ConfirmAdministeredVaccineDelete';
 
 import { connectApi } from '../api/connectApi';
 import { reloadPatient } from '../store/patient';
 
 import { VACCINE_STATUS } from '../constants';
 import { ContentPane } from './ContentPane';
-import { Button } from './Button';
+import { DeleteButton } from './Button';
+import { TextInput } from './Field';
+import { FormGrid } from './FormGrid';
 
-const ModalContent = React.memo(({ open, onClose, onMarkRecordedInError }) => {
+const Button = styled(DeleteButton)`
+  margin-top: 12px;
+`;
+
+const ModalContent = React.memo(({ open, onClose, onMarkRecordedInError, vaccineRecord }) => {
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const closeWithoutDeletingRecord = useCallback(() => {
+    setConfirmDelete(false);
+    onClose();
+  }, []);
+
+  if (!vaccineRecord) return null;
+
+  const {
+    status,
+    injectionSite,
+    scheduledVaccine: { label, schedule },
+    encounter: {
+      examiner: { displayName },
+    },
+  } = vaccineRecord;
+
+  if (confirmDelete) {
+    return (
+      <Modal title="Delete Vaccination Record" open={open} onClose={closeWithoutDeletingRecord}>
+        <ContentPane>
+          <ConfirmAdministeredVaccineDelete
+            onDelete={onMarkRecordedInError}
+            onClose={closeWithoutDeletingRecord}
+          />
+        </ContentPane>
+      </Modal>
+    );
+  }
+
   return (
-    <Modal title="Delete Vaccination" open={open} onClose={onClose}>
+    <Modal title="Edit Vaccination Record" open={open} onClose={closeWithoutDeletingRecord}>
       <ContentPane>
-        <h3>WARNING: This will delete this vaccination from the patients record.</h3>
-        <Button onClick={() => onMarkRecordedInError()} variant="contained" color="primary">
-          Delete vaccine record
-        </Button>
+        <FormGrid columns={2}>
+          <TextInput disabled value={`${label} (${schedule})`} label="Vaccine" />
+          <TextInput disabled value={status} label="Status" />
+          <TextInput disabled value={injectionSite} label="Injection site" />
+          <TextInput disabled value={displayName} label="Practitioner" />
+          <Button onClick={() => setConfirmDelete(true)} variant="contained" color="primary">
+            DELETE RECORD
+          </Button>
+        </FormGrid>
       </ContentPane>
     </Modal>
   );
 });
 
 export const EditAdministeredVaccineModal = connectApi(
-  (api, dispatch, { patientId, vaccineRecordId }) => ({
+  (api, dispatch, { patientId, vaccineRecord }) => ({
     onMarkRecordedInError: async () => {
-      await api.put(`patient/${patientId}/administeredVaccine/${vaccineRecordId}`, {
+      await api.put(`patient/${patientId}/administeredVaccine/${vaccineRecord.id}`, {
         status: VACCINE_STATUS.RECORDED_IN_ERROR,
       });
       dispatch(reloadPatient(patientId));
