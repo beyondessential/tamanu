87ec98e6e2770383266724f0f716e2fb3862d572
Alastair-L <lockealastair@gmail.com>
2021-06-17 13:16:22 +1000

    Add comments

--- packages/shared-src/src/migrations/028_addLabRequestDisplayIdColumn.js
+++ packages/shared-src/src/migrations/028_addLabRequestDisplayIdColumn.js
@@ -2,11 +2,22 @@ const sequelize = require('sequelize');
 
 module.exports = {
   up: async query => {
-    // await query.bulkDelete('lab_requests', {});
+    // Adding a non-nullable column will fail if there are records in the db
+
+    // Could delete all current lab requests:
+    // await query.bulkDelete('lab_requests', {}); 
     await query.addColumn('lab_requests', 'display_id', {
-      type: sequelize.STRING, // Should we enforce the format at db level?
-      allowNull: true, // (just for keeping requests in dev)
+      type: sequelize.STRING,
+      allowNull: false,
+      // Or could add a default value while adding the column, then remove it later.
+      // defaultValue: 'NO_DISPLAY_ID',
     });
+
+    // Removing the default value
+    // await query.changeColumn('lab_requests', 'display_id', {
+    //   type: sequelize.STRING,
+    //   allowNull: false,
+    // });
   },
   down: async query => {
     await query.removeColumn('lab_requests', 'display_id');
