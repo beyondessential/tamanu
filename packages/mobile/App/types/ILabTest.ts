262075a24ca0e79ae4d8caaba2d57977d200d947
Alastair-L <lockealastair@gmail.com>
2021-06-17 15:33:50 +1000

    Stop trying to be clever

--- packages/mobile/App/types/ILabTest.ts
+++ packages/mobile/App/types/ILabTest.ts
@@ -4,8 +4,13 @@ import { ILabRequest, LabRequestStatus} from './ILabRequest';
 import { ILabTestType } from './ILabTestType';
 import { IReferenceData } from './IReferenceData';
 
-export import LabTestStatus = LabRequestStatus; // TODO: fix this mess
-
+export enum LabTestStatus {
+  RECEPTION_PENDING = 'reception_pending',
+  RESULTS_PENDING = 'results_pending',
+  TO_BE_VERIFIED = 'to_be_verified',
+  VERIFIED = 'verified',
+  PUBLISHED = 'published',
+};
 export const LAB_TEST_STATUS_OPTIONS = createDropdownOptionsFromObject(LabTestStatus);
 
 export interface ILabTest {
