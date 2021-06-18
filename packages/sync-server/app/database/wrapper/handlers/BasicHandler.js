104ef6deaf7627a89b8191798d49ac9782edb501
Tim Kennedy <t.m.knndy@gmail.com>
2021-06-18 12:48:53 +1000

    Remove last syncParentIdKey mention from BasicHandler

--- packages/sync-server/app/database/wrapper/handlers/BasicHandler.js
+++ packages/sync-server/app/database/wrapper/handlers/BasicHandler.js
@@ -1,40 +1,40 @@
 import { Sequelize } from 'sequelize';
 import { syncCursorToWhereCondition, assertParentIdsMatch } from 'shared/models/sync';
 
-export function upsertQuery(values) {
-  // added for consistency with the other queries
-  // currently just passes values and an empty options object directly through to upsert
-  return [values, {}];
-}
+// added for consistency with the other queries
+// currently just passes values and an empty options object directly through to upsert
+export const upsertQuery = values => [values, {}];
 
-export function countSinceQuery({ since }) {
-  return {
-    where: syncCursorToWhereCondition(since),
-    paranoid: false,
-  };
-}
+export const countSinceQuery = ({ since }) => ({
+  where: syncCursorToWhereCondition(since),
+  paranoid: false,
+});
 
-export function findSinceQuery({ since, limit, offset }) {
-  return {
-    limit,
-    offset,
-    where: syncCursorToWhereCondition(since),
-    order: ['updated_at', 'id'],
-    paranoid: false,
-  };
-}
+export const findSinceQuery = ({ since, limit, offset }) => ({
+  limit,
+  offset,
+  where: syncCursorToWhereCondition(since),
+  order: ['updated_at', 'id'],
+  paranoid: false,
+});
 
-export function markDeletedQuery({ id }) {
-  return [
-    {
-      deletedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
-      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
-    },
-    {
-      where: { id },
-    },
-  ];
-}
+export const markDeletedQuery = ({ id }) => [
+  {
+    deletedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+  },
+  {
+    where: { id },
+  },
+];
+
+export const queryWithParentIds = (parentIds, query) => ({
+  ...query,
+  where: {
+    ...query.where,
+    ...parentIds,
+  },
+});
 
 export class BasicHandler {
   model = null;
@@ -77,19 +77,5 @@ export class BasicHandler {
     // use update instead of destroy so we can change both fields
     const [num] = await this.model.update(...markDeletedQuery({ id }));
     return num;
-  }
-
-  queryWithParentIds(parentIds, query) {
-    if (!this.model.syncParentIdKey) {
-      return query;
-    }
-
-    return {
-      ...query,
-      where: {
-        ...query.where,
-        ...parentIds,
-      },
-    };
   }
 }
