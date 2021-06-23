0263fcb5331d9ca8a1fb92d6540ef8f4928157e9
Tom McLean <tom.a.mclean@gmail.com>
2021-06-23 13:52:18 +1000

    Undo increase to min version

--- packages/sync-server/app/middleware/versionCompatibility.js
+++ packages/sync-server/app/middleware/versionCompatibility.js
@@ -6,7 +6,7 @@ import { buildVersionCompatibilityCheck } from 'shared/utils';
 // not supported.
 export const SUPPORTED_CLIENT_VERSIONS = {
   'Tamanu LAN Server': {
-    min: '1.3.0',
+    min: '1.0.0',
     max: '1.3.0', // note that higher patch versions will be allowed to connect
   },
   'Tamanu Mobile': {
