f16193e097bad82e48fa201edf944a04095fa1ea
Alastair-L <lockealastair@gmail.com>
2021-06-28 10:12:27 +1000

    Fix bugs and use display_id not id

--- packages/sync-server/app/publicRoutes/labResultWidget.js
+++ packages/sync-server/app/publicRoutes/labResultWidget.js
@@ -8,7 +8,7 @@ export const labResultWidgetRoutes = express.Router();
 
 const getPatientInitials = patient => `${patient.firstName ? patient.firstName.substring(0, 1) : ''}${patient.middleName ? patient.middleName.substring(0, 1) : ''}${patient.lastName ? patient.lastName.substring(0, 1) : ''}`;
 
-const transformLabRequest = async (models, labRequest) => {
+const transformLabRequest = async (models, labRequest, testTypeWhitelist) => {
   const { id, createdAt, encounterId, status } = labRequest;
   const encounter = await models.Encounter.findOne({ where: { id: encounterId } });
   const patient = await models.Patient.findOne({ where: { id: encounter.patientId } });
@@ -20,13 +20,13 @@ const transformLabRequest = async (models, labRequest) => {
     }
   }
 
-  const labTests = await models.LabTest.findOne({
+  const labTests = await models.LabTest.findAll({
     where: {
       labRequestId: id,
     }
   });
 
-  const returnableLabTests = labTests.filter(({ labTestTypeId }) => config.testTypeWhitelist.includes(labTestTypeId));
+  const returnableLabTests = labTests.filter(({ labTestTypeId }) => testTypeWhitelist.includes(labTestTypeId));
 
   return {
     testDate: createdAt,
@@ -44,14 +44,16 @@ labResultWidgetRoutes.get(
     const { params } = req;
     const { displayId } = params;
     const { models } = req.store;
+    const { testTypeWhitelist, categoryWhitelist } = config.localisation.labResultWidget;
     const labRequests = await models.LabRequest.findAll({
       where: {
-        id: displayId,
+        display_id: displayId,
       },
     });
-    const returnableLabRequests = labRequests.filter(({ labTestCategoryId }) => config.categoryWhitelist.includes(labTestCategoryId));
 
-    const labRequestsToReport = await Promise.all(returnableLabRequests.map(labRequest => transformLabRequest(models, labRequest)));
+    const returnableLabRequests = labRequests.filter(({ labTestCategoryId }) => categoryWhitelist.includes(labTestCategoryId));
+
+    const labRequestsToReport = await Promise.all(returnableLabRequests.map(labRequest => transformLabRequest(models, labRequest, testTypeWhitelist)));
 
     res.send({
       data: labRequestsToReport,
