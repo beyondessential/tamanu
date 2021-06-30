03c937f9ff6614634abff983445b3fe462a3ac51
Alastair-L <lockealastair@gmail.com>
2021-06-30 17:02:19 +1000

    Use localisation

--- packages/desktop/app/views/reports/VillageField.js
+++ packages/desktop/app/views/reports/VillageField.js
@@ -7,7 +7,7 @@ const DumbVillageField = ({ villageSuggester, required }) => {
   return (
     <LocalisedField
       name="village"
-      label="Village"
+      path="fields.villageId"
       component={AutocompleteField}
       suggester={villageSuggester}
       required={required}
