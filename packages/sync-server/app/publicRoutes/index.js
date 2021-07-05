f14301143d17d2e35c2a487a182b96b6fb7aebc3
Tom McLean <tom.a.mclean@gmail.com>
2021-07-05 10:29:29 +1000

    Make cors allowed origin configurable

--- packages/sync-server/app/publicRoutes/index.js
+++ packages/sync-server/app/publicRoutes/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import config from 'config';
 
 import { labResultWidgetRoutes } from './labResultWidget';
 
 export const publicRoutes = express.Router();
 
-publicRoutes.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
+const { cors } = config;
+
+if (cors.allowedOrigin) {
+  publicRoutes.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", cors.allowedOrigin);
+    next();
+  });
+}
 
 publicRoutes.use('/labResultWidget', labResultWidgetRoutes);
