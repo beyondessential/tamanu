35f9ea5321f0ad63b731accf3ed64fafc32dc00d
Tom McLean <tom.a.mclean@gmail.com>
2021-07-05 11:36:54 +1000

    Add CORS warning when allowedOrigin is not configured

--- packages/sync-server/app/publicRoutes/index.js
+++ packages/sync-server/app/publicRoutes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 
 import { labResultWidgetRoutes } from './labResultWidget';
+import { log } from 'shared/services/logging';
 
 export const publicRoutes = express.Router();
 
@@ -12,6 +13,8 @@ if (cors.allowedOrigin) {
     res.setHeader("Access-Control-Allow-Origin", cors.allowedOrigin);
     next();
   });
+} else {
+  log.warn('publicRoutes: CORS has not been set up for this server; external widgets like the COVID test results will be unavailable until cors.allowedOrigin is set to the appropriate domain');
 }
 
 publicRoutes.use('/labResultWidget', labResultWidgetRoutes);
