d3b92e2b5290112fde4c9f791f758213f5f2b262
Tom McLean <tom.a.mclean@gmail.com>
2021-07-09 13:51:24 +1000

    Cleanup script output

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-# This script outputs all version numbers in the repo 
-# so that it's easy to eyeball what needs updating.
-
-# TODO: a function to actually bump the version numbers
-
 V=$1
-TEMP_PATH=/tmp/package_json_version_bump
 
 if [ -z "$V" ]; then
-  echo "Usage: ./scripts/version.sh 1.2.3"
+  cat << EOF
+This script sets all the relevant version numbers in the repo 
+
+Usage:
+$ ./scripts/version.sh 1.2.3
+EOF
   exit 1
 fi
 
+TEMP_PATH=/tmp/package_json_version_bump
 version() {
-  echo "Bumping $1 to $V"
+  echo "  $1"
   cat $1 | jq '.version = $v' --arg v $V > $TEMP_PATH
   mv $TEMP_PATH $1
 }
 
+echo "Bumping package.jsons to $V"
 version package.json
 version packages/desktop/package.json
 version packages/desktop/app/package.json
