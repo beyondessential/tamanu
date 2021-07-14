fcedd123f33179509f2dc4b5c515da6c948e31c2
Bastien Goepfert <bas.goepfert@gmail.com>
2021-07-15 00:40:27 +1000

    add test

--- packages/lan/__tests__/apiv1/LabRequestLogs.test.js
+++ packages/lan/__tests__/apiv1/LabRequestLogs.test.js
@@ -1,8 +1,28 @@
-import { createDummyPatient } from 'shared/demoData/patients';
+import { LAB_REQUEST_STATUSES } from 'shared/constants';
+import { createDummyPatient, randomReferenceId } from 'shared/demoData/patients';
 import { createTestContext } from '../utilities';
 
-describe('LabRequestLogs', () => {
-  let patient = null;
+const randomLabTests = (models, labTestCategoryId, amount) =>
+  models.LabTestType.findAll({
+    where: {
+      labTestCategoryId,
+    },
+    limit: amount,
+  });
+
+const randomLabRequest = async (models, overrides) => {
+  const categoryId = await randomReferenceId(models, 'labTestCategory');
+  const labTestTypeIds = (await randomLabTests(models, categoryId, 2)).map(({ id }) => id);
+  return {
+    categoryId,
+    labTestTypeIds,
+    displayId: 'TESTID',
+    ...overrides,
+  };
+};
+
+describe('Lab request logs', () => {
+  let patientId = null;
   let app = null;
   let baseApp = null;
   let models = null;
@@ -11,20 +31,21 @@ describe('LabRequestLogs', () => {
     const ctx = await createTestContext();
     baseApp = ctx.baseApp;
     models = ctx.models;
+    const patient = await models.Patient.create(await createDummyPatient(models));
+    patientId = patient.id;
     app = await baseApp.asRole('practitioner');
-    patient = await models.Patient.create(await createDummyPatient(models));
   });
 
-  it('should add a lab request log', async () => {
-    const result = await app.post('/v1/immunisation').send({
-      patientId: patient.id,
-      givenById: app.user.id,
-      date: Date.now(),
-      schedule: '24 hours after birth',
-      batch: 'ifz-0101',
-      timeliness: 'On time',
+  it.only('should create a lab request log when updating a labs status', async () => {
+    const user = await baseApp.get('/v1/user/me');
+    const labRequest = await randomLabRequest(models, { patientId });
+    const response = await app.post('/v1/labRequest').send(labRequest);
+    const updatedRequest = await app.put(`labRequest/${response.body.id}`, {
+      status: LAB_REQUEST_STATUSES.PUBLISHED,
+      userId: user.id,
     });
-    expect(result).toHaveSucceeded();
-    expect(result.body.date).toBeTruthy();
+    expect(updatedRequest).toBeTruthy();
+    expect(updatedRequest.body.createdAt).toBeTruthy();
+    expect(updatedRequest.body.status).toEqual(LAB_REQUEST_STATUSES.PUBLISHED);
   });
 });
