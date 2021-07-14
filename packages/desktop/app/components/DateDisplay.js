c110942121d8247be0b505d82049c9e44aab72a4
Tri Nguyen <tri@tridnguyen.com>
2021-07-13 22:16:50 -0400

    prettier

--- packages/desktop/app/components/DateDisplay.js
+++ packages/desktop/app/components/DateDisplay.js
@@ -26,14 +26,14 @@ const StyledAbbr = styled.abbr`
   text-decoration: none;
 `;
 
-export const DateDisplay = React.memo(({ date, showDate = true, showTime = false, showDuration = false, ...props }) => (
-  <StyledAbbr {...props} title={formatLong(date)}>
-    {showDate && formatShort(date)}
-    {' '}
-    {showDuration && ` (${formatDuration(date)})`}
-    {' '}
-    {showTime && formatTime(date)}
-  </StyledAbbr>
-));
+export const DateDisplay = React.memo(
+  ({ date, showDate = true, showTime = false, showDuration = false, ...props }) => (
+    <StyledAbbr {...props} title={formatLong(date)}>
+      {showDate && formatShort(date)}
+      {showDuration && ` (${formatDuration(date)})`}
+      {showTime && formatTime(date)}
+    </StyledAbbr>
+  ),
+);
 
 DateDisplay.rawFormat = formatShort;
