0d4da4a02248c1055c66363ed0d302e977de9f17
Tom McLean <tom.a.mclean@gmail.com>
2021-07-14 11:00:33 +1000

    Rename multipart method to postWithFileUpload

--- packages/desktop/app/views/administration/ProgramsAdminView.js
+++ packages/desktop/app/views/administration/ProgramsAdminView.js
@@ -1,34 +1,17 @@
 import React, { memo, useState, useCallback } from 'react';
 import styled from 'styled-components';
 
-import { readFileSync } from 'fs';
-
 import { useApi } from '../../api';
 import { DataDocumentUploadForm } from './DataDocumentUploadForm';
-
-function readFileAsBlob(path) {
-  const fileData = readFileSync(path);
-  return new Blob([fileData]);
-}
 
 const Container = styled.div`
   padding: 32px;
 `;
 
-
 export const ProgramsAdminView = memo(() => {
   const api = useApi();
   const onSubmit = useCallback(
-    async ({ file, ...data }) => {
-      const fileData = readFileAsBlob(file);
-      // send to api
-      const response = await api.multipart('admin/importProgram', {
-        file: fileData,
-        ...data,
-      });
-
-      return response;
-    }
+    ({ file, ...data }) => api.postWithFileUpload('admin/importProgram', file, data)
   );
 
   return (
--- packages/desktop/app/views/administration/ReferenceDataAdminView.js
+++ packages/desktop/app/views/administration/ReferenceDataAdminView.js
@@ -1,16 +1,9 @@
 import React, { memo, useState, useCallback } from 'react';
 import styled from 'styled-components';
 
-import { readFileSync } from 'fs';
-
 import { useApi } from '../../api';
 import { CheckArrayInput } from '../../components/Field/CheckArrayInput';
 import { DataDocumentUploadForm } from './DataDocumentUploadForm';
-
-function readFileAsBlob(path) {
-  const fileData = readFileSync(path);
-  return new Blob([fileData]);
-}
 
 const Container = styled.div`
   padding: 32px;
@@ -21,16 +14,7 @@ import { Field } from '../../components/Field';
 export const ReferenceDataAdminView = memo(() => {
   const api = useApi();
   const onSubmit = useCallback(
-    async ({ file, ...data }) => {
-      const fileData = readFileAsBlob(file);
-      // send to api
-      const response = await api.multipart('admin/importData', {
-        file: fileData,
-        ...data,
-      });
-
-      return response;
-    }
+    ({ file, ...data }) => api.postWithFileUpload('admin/importProgram', file, data)
   );
 
   const whitelist = (
