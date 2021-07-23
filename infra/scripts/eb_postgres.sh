6342df4348b48432c992c0c5684d4af3f2ed75f6
Tim Kennedy <t.m.knndy@gmail.com>
2021-07-23 11:52:49 +1000

    Move postgres connection script to shared connect_postgres function

--- infra/scripts/eb_postgres.sh
+++ infra/scripts/eb_postgres.sh
@@ -2,28 +2,5 @@
 
 source "$(dirname "${BASH_SOURCE[0]}")/common/common.bash"
 
-PORT="${PORT:-5433}"
-
-# retrieve details from AWS
-ENDPOINT="$(aws rds describe-db-instances | jq -r '.DBInstances[] | select((.TagList[] | select(.Key == "elasticbeanstalk:environment-name")).Value == "'"$ENVIRONMENT"'") | .Endpoint.Address')"
-prlog "db endpoint: $ENDPOINT"
-NODE_CONFIG="$(eb printenv "$ENVIRONMENT" | grep NODE_CONFIG | sed -e 's/.*NODE_CONFIG = //')"
-NAME="$(echo "$NODE_CONFIG" | jq -r '.db.name')"
-prlog "name: $NAME"
-USERNAME="$(echo "$NODE_CONFIG" | jq -r '.db.username')"
-prlog "username: $USERNAME"
-PASSWORD="$(echo "$NODE_CONFIG" | jq -r '.db.password')"
-prlog "password: <found password in environment NODE_CONFIG>"
-
-# connect ssh
-eb ssh --quiet --custom "ssh -i $KEYPAIR" --command ':' "$ENVIRONMENT"
-eb ssh --quiet --custom "ssh -i $KEYPAIR -N -L $PORT:$ENDPOINT:5432" "$ENVIRONMENT" &
-prlog "waiting for eb ssh to connect"
-sleep 5 # let ssh connect
-prlog "attempting psql connection"
-
-# echo warning
-prwarn "CONNECTING TO $ENVIRONMENT"
-
-# showtime
-psql "postgresql://$USERNAME:$PASSWORD@localhost:$PORT/$NAME"
+connect_postgres
+psql "$PG_CONNECTION_URL"
