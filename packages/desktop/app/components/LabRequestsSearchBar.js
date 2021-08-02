2a95e5452e0b63b05728995f95bbea786bd669f1
Bastien Goepfert <bas.goepfert@gmail.com>
2021-08-02 14:55:58 +1000

    add suggested text filter abstraction and fix border on moved field

--- packages/desktop/app/components/LabRequestsSearchBar.js
+++ packages/desktop/app/components/LabRequestsSearchBar.js
@@ -8,8 +8,8 @@ import { LAB_REQUEST_STATUS_LABELS, LAB_REQUEST_STATUSES } from '../constants';
 const StyledSelectField = styled(SelectField)`
   .styled-select-container {
     padding: 8px 8px 2px 8px;
-    border-radius: 0 3px 3px 0;
     border: 1px solid #dedede;
+    border-right: none;
   }
 
   .styled-select__control,
