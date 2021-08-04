1adb355245834a5e4d586174c3844bfb67710558
Igor Nadj <igor@beyondessential.com.au>
2021-08-04 15:17:15 +1000

    Refactor: move json field parsing to model

--- packages/sync-server/app/tasks/ReportRequestProcessor.js
+++ packages/sync-server/app/tasks/ReportRequestProcessor.js
@@ -27,7 +27,6 @@ export class ReportRequestProcessor extends ScheduledTask {
 
     for (let i = 0; i < requests.length; i++) {
       const request = requests[i];
-      const requestObject = request.get({ plain: true });
       if (!config.mailgun.from) {
         log.error(`ReportRequestProcessorError - Email config missing`);
         request.update({
@@ -36,10 +35,10 @@ export class ReportRequestProcessor extends ScheduledTask {
         return;
       }
 
-      const reportDataGenerator = getReportModule(requestObject.reportType)?.dataGenerator;
+      const reportDataGenerator = getReportModule(request.reportType)?.dataGenerator;
       if (!reportDataGenerator) {
         log.error(
-          `ReportRequestProcessorError - Unable to find report generator for report ${requestObject.id} of type ${requestObject.reportType}`,
+          `ReportRequestProcessorError - Unable to find report generator for report ${request.id} of type ${request.reportType}`,
         );
         request.update({
           status: REPORT_REQUEST_STATUSES.ERROR,
@@ -49,8 +48,7 @@ export class ReportRequestProcessor extends ScheduledTask {
 
       let reportData = null;
       try {
-        const parameters = requestObject.parameters ? JSON.parse(requestObject.parameters) : {};
-        reportData = await reportDataGenerator(this.context.store.models, parameters);
+        reportData = await reportDataGenerator(this.context.store.models, request.getParameters());
       } catch (e) {
         log.error(`ReportRequestProcessorError - Failed to generate report, ${e.message}`);
         log.error(e.stack);
@@ -60,7 +58,7 @@ export class ReportRequestProcessor extends ScheduledTask {
       }
 
       try {
-        await this.sendReport(requestObject, reportData);
+        await this.sendReport(request, reportData);
         await request.update({
           status: REPORT_REQUEST_STATUSES.PROCESSED,
         });
@@ -75,19 +73,26 @@ export class ReportRequestProcessor extends ScheduledTask {
   }
 
   /**
-   * @param request {}
+   * @param request ReportRequest
    * @param reportData []
    * @returns {Promise<void>}
    */
   async sendReport(request, reportData) {
-    if (request.recipients.email) {
-      await this.sendReportToEmail(request, reportData, request.recipients.email);
+    const recipients = request.getRecipients();
+    if (recipients.email) {
+      await this.sendReportToEmail(request, reportData, recipients.email);
     }
-    if (request.recipients.tupaia){
+    if (recipients.tupaia) {
       await this.sendReportToTupaia(request, reportData);
     }
   }
 
+  /**
+   * @param request ReportRequest
+   * @param reportData []
+   * @param emailAddresses string[]
+   * @returns {Promise<void>}
+   */
   async sendReportToEmail(request, reportData, emailAddresses) {
     const fileName = await createFilePathForEmailAttachment(
       `${request.reportType}-report-${new Date().getTime()}.xlsx`,
@@ -115,6 +120,11 @@ export class ReportRequestProcessor extends ScheduledTask {
     }
   }
 
+  /**
+   * @param request ReportRequest
+   * @param reportData []
+   * @returns {Promise<void>}
+   */
   async sendReportToTupaia(request, reportData) {
     // TODO: implement
   }
