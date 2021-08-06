a43625f200bded25d2c8f4af0c115f516429b5e9
Tim Kennedy <t.m.knndy@gmail.com>
2021-08-06 12:16:26 +1000

    Add /servers/status and /servers/status/readable to meta-server

---
+++ packages/meta-server/app/render/cell.js
@@ -0,0 +1,10 @@
+import { sanitise } from './sanitise';
+
+export const getUrl = url => `<a href="${url}">${sanitise(url)}</a>`;
+
+export const getBool = bool =>
+  bool
+    ? '<div style="background-color: green; color: white; padding: 2px; width: 30px;">Yes</div>'
+    : '<div style="background-color: red; color: white; padding: 2px; width: 30px">No</div>';
+
+export const getMilliseconds = ms => (Number.isFinite(ms) ? `${ms}ms` : '');
