728da3c1da5161ca375157167a9208d85bdf2cde
Tom McLean <tom.a.mclean@gmail.com>
2021-08-06 11:56:17 +1000

    Add rate limiter

--- packages/sync-server/app/auth/index.js
+++ packages/sync-server/app/auth/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import config from 'config';
+import rateLimit from 'express-rate-limit';
 
 import { convertFromDbRecord } from '../convertDbRecord';
 
@@ -12,9 +13,16 @@ import { userMiddleware } from './userMiddleware';
 
 export const authModule = express.Router();
 
-authModule.use('/resetPassword', resetPassword);
-authModule.use('/changePassword', changePassword);
-authModule.post('/login', login);
+// don't allow too many calls against the account endpoints
+const rateLimitOptions = {
+  windowMs: config.auth.rateLimit.windowMinutes * 60 * 1000,
+  max: config.auth.rateLimit.tries 
+};
+const limiter = rateLimit(rateLimitOptions);
+
+authModule.use('/resetPassword', limiter, resetPassword);
+authModule.use('/changePassword', limiter, changePassword);
+authModule.post('/login', limiter, login);
 
 authModule.use(userMiddleware);
 
