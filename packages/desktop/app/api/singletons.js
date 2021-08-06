9b5e08b9af65355baefb6a7a6efd17dd7b3a5b81
Tri Nguyen <tri@tridnguyen.com>
2021-08-06 06:05:06 -0400

    revert pkg change

--- packages/desktop/app/api/singletons.js
+++ packages/desktop/app/api/singletons.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { TamanuApi } from './TamanuApi';
-import pkg from '../package.json';
+import { version } from '../package.json';
 import { Suggester } from '../utils/suggester';
 
-export const API = new TamanuApi(pkg.version);
+export const API = new TamanuApi(version);
 export const ApiContext = React.createContext(API);
 export const useApi = () => React.useContext(ApiContext);
 export const useSuggester = type => new Suggester(API, type);
