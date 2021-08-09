be4c489cce768f177ecb5495188fe548df450c45
Tom McLean <tom.a.mclean@gmail.com>
2021-08-09 18:07:35 +1000

    Only throw on already exists type errors

--- packages/shared-src/src/migrations/033_addLabRequestNoteRecordType.js
+++ packages/shared-src/src/migrations/033_addLabRequestNoteRecordType.js
@@ -1,13 +1,22 @@
+const { DatabaseError } = require('sequelize');
+
 module.exports = {
   up: async query => {
     // This migration can fail if enum_notes_record_type already includes LabRequest
     // so if it fails we can just ignore it
-    // (if it fails for a different reason, we have bigger problems)
     try {
       await query.sequelize.query("ALTER TYPE enum_notes_record_type ADD VALUE 'LabRequest'");
     } catch(e) {
+      if (e instanceof DatabaseError) {
+        if (e.message.match(`already exists`)) {
+          return;
+        }
+      }
+      // it failed for a different reason - rethrow
+      throw e;
     }
   },
   down: async query => {
+    // no down migration - it's unsafe to delete enums
   },
 };
