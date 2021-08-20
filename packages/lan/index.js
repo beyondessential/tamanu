4e5ad52669a6c8ebfe3a29c892abd2f1f0bb3f0e
Tom McLean <tom.a.mclean@gmail.com>
2021-08-20 21:30:57 +1000

    Implement subcommands for facility server

--- packages/lan/index.js
+++ packages/lan/index.js
@@ -1,5 +1,6 @@
 import config from 'config';
 import { log } from 'shared/services/logging';
+import { parseArguments } from 'shared/arguments';
 import { initDatabase } from './app/database';
 import { addPatientMarkForSyncHook, SyncManager, WebRemote } from './app/sync';
 
@@ -10,18 +11,16 @@ import { startDataChangePublisher } from './app/DataChangePublisher';
 
 import { listenForServerQueries } from './app/discovery';
 
-const port = config.port;
-
-export async function run() {
+async function serve(options) {
   const context = await initDatabase();
-
-  await context.sequelize.migrate();
 
   context.remote = new WebRemote(context);
   context.remote.connect(); // preemptively connect remote to speed up sync
   context.syncManager = new SyncManager(context);
 
   const app = createApp(context);
+
+  const port = config.port;
   const server = app.listen(port, () => {
     log.info(`Server is running on port ${port}!`);
   });
@@ -38,12 +37,33 @@ export async function run() {
   startDataChangePublisher(server, context);
 }
 
+async function migrate(options) {
+  const store = await initDatabase();
+  await store.sequelize.migrate(options);
+  process.exit(0);
+}
+
+async function run(command, options) {
+  const subcommand = {
+    serve,
+    migrate,
+  }[command];
+
+  if (!subcommand) {
+    throw new Error(`Unrecognised subcommand: ${command}`);
+  }
+
+  return subcommand(options);
+}
+
 // catch and exit if run() throws an error
 (async () => {
   try {
+    const { command, ...options } = parseArguments();
     await run();
   } catch (e) {
-    log.error('run(): fatal error:', e.stack);
+    log.error(`run(): fatal error: ${e.toString()}`);
+    log.error(e.stack);
     process.exit(1);
   }
 })();
