0dac2c055493fe7d42ea29b16c650b681953f5c5
Tim Kennedy <t.m.knndy@gmail.com>
2021-08-24 10:47:02 +1000

    954: Don't try to run migrations when sqlitePath is set

--- packages/lan/index.js
+++ packages/lan/index.js
@@ -14,8 +14,10 @@ import { listenForServerQueries } from './app/discovery';
 async function serve(options) {
   const context = await initDatabase();
 
-  if (config.db.migrateOnStartup) {
-    await context.sequelize.migrate({ migrateDirection: "up" });
+  if (config.db.sqlitePath) {
+    // do nothing
+  } else if (config.db.migrateOnStartup) {
+    await context.sequelize.migrate({ migrateDirection: 'up' });
   } else {
     await context.sequelize.assertUpToDate(options);
   }
