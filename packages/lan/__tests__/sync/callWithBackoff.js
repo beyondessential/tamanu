c157dc97c35e03b1d57618910128f086e4fc05b4
Tim Kennedy <t.m.knndy@gmail.com>
2021-08-24 16:14:10 +1000

    Add test stubs for callWithBackoff

---
+++ packages/lan/__tests__/sync/callWithBackoff.js
@@ -0,0 +1,6 @@
+describe('callWithBackoff', () => {
+  it.todo('retries attempts up to a maximum number');
+  it.todo('fails after the maximum number of retries');
+  it.todo('waits an increasing amount of time');
+  it.todo('obeys the upper bound on wait time');
+});
