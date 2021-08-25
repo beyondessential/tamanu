356996c67a5add4135b91b6a25057d498258a06d
Tim Kennedy <t.m.knndy@gmail.com>
2021-08-25 11:42:21 +1000

    Remove unused json parsing utils

--- packages/shared-src/src/utils/index.js
+++ packages/shared-src/src/utils/index.js
@@ -1,7 +1,5 @@
 export * from './buildVersionCompatibilityCheck';
 export * from './getResponseJsonSafely';
-export * from './parse-object';
 export * from './valueIndex';
 export * from './fetchWithTimeout';
 export * from './sleepAsync';
-export * from './jsonParse';
--- packages/shared-src/src/utils/jsonParse.js
+++
@@ -1,9 +0,0 @@
-import jsonPrune from 'json-prune';
-
-export const jsonParse = object => {
-  try {
-    return JSON.parse(jsonPrune(object));
-  } catch (err) {
-    throw err;
-  }
-};
