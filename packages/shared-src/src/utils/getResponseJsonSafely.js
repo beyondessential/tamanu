741aab57cfc3694369a22dcd42643eb28a8f538d
Tom McLean <tom.a.mclean@gmail.com>
2021-08-26 11:12:42 +1000

    Revert unintended inclusion

--- packages/shared-src/src/utils/getResponseJsonSafely.js
+++ packages/shared-src/src/utils/getResponseJsonSafely.js
@@ -1,13 +1,11 @@
 import { log } from 'shared/services/logging';
 
 export const getResponseJsonSafely = async response => {
-  const body = await response.text();
   try {
-    return JSON.parse(body);
+    return response.json();
   } catch (e) {
     // log json parsing errors, but still return a valid object
     log.warn(`getResponseJsonSafely: Error parsing JSON: ${e}`);
-    log.warn(body);
     return {};
   }
 };
