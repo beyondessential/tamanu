887e829019f7278f8ff62a9b22d2162564b695d1
Tim Kennedy <t.m.knndy@gmail.com>
2021-09-01 14:08:38 +1000

    Change 'host' key to 'syncHost'

--- packages/lan/app/database/integrity.js
+++ packages/lan/app/database/integrity.js
@@ -8,10 +8,10 @@ async function ensureHostMatches(context) {
   const { LocalSystemFact } = context.models;
 
   const configuredHost = config.sync.host;
-  const lastHost = await LocalSystemFact.get('host');
+  const lastHost = await LocalSystemFact.get('syncHost');
   if (lastHost && lastHost !== configuredHost) {
     throw new Error(
-      `integrity check failed: sync.host mismatch: read ${configuredHost} from config, but already connected to ${lastHost} (you may need to drop and recreate the database, change the config back, or if you're 100% sure, remove the "host" key from the "local_metadata" table)`,
+      `integrity check failed: sync.host mismatch: read ${configuredHost} from config, but already connected to ${lastHost} (you may need to drop and recreate the database, change the config back, or if you're 100% sure, remove the "syncHost" key from the "local_metadata" table)`,
     );
   }
 }
