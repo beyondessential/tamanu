7a2140982184f14b874be92e26332ccdffcf07fb
Igor Nadj <igor@beyondessential.com.au>
2021-09-14 13:55:15 +1000

    Revert "reports api (#1282)"
    This reverts commit 7b05ebc6b4c88743ae39e08a969c9212709e4bdf.

--- packages/sync-server/app/tasks/ReportRequestProcessor.js
+++ packages/sync-server/app/tasks/ReportRequestProcessor.js
@@ -4,7 +4,6 @@ import { COMMUNICATION_STATUSES, REPORT_REQUEST_STATUSES } from 'shared/constant
 import { getReportModule } from 'shared/reports';
 import { ScheduledTask } from 'shared/tasks';
 import { log } from 'shared/services/logging';
-import { createTupaiaApiClient, translateReportDataToSurveyResponses } from 'shared/utils';
 
 import { removeFile, createZippedExcelFile } from '../utils/files';
 
@@ -26,6 +25,7 @@ export class ReportRequestProcessor extends ScheduledTask {
 
     for (let i = 0; i < requests.length; i++) {
       const request = requests[i];
+      const requestObject = request.get({ plain: true });
       if (!config.mailgun.from) {
         log.error(`ReportRequestProcessorError - Email config missing`);
         request.update({
@@ -35,135 +35,59 @@ export class ReportRequestProcessor extends ScheduledTask {
       }
 
       const disabledReports = config.localisation.data.disabledReports;
-      if (disabledReports.includes(request.reportType)) {
-        log.error(`Report "${request.reportType}" is disabled`);
+      if (disabledReports.includes(requestObject.reportType)) {
+        log.error(`Report "${requestObject.reportType}" is disabled`);
         request.update({
           status: REPORT_REQUEST_STATUSES.ERROR,
         });
         return;
       }
 
-      const reportModule = getReportModule(request.reportType);
-      const reportDataGenerator = reportModule?.dataGenerator;
-      if (!reportModule || !reportDataGenerator) {
+      const reportDataGenerator = getReportModule(requestObject.reportType)?.dataGenerator;
+      if (!reportDataGenerator) {
         log.error(
-          `ReportRequestProcessorError - Unable to find report generator for report ${request.id} of type ${request.reportType}`,
+          `ReportRequestProcessorError - Unable to find report generator for report ${requestObject.id} of type ${requestObject.reportType}`,
         );
         request.update({
           status: REPORT_REQUEST_STATUSES.ERROR,
         });
         return;
       }
-
-      let reportData = null;
+      const reportName = `${requestObject.reportType}-report-${new Date().getTime()}`;
+      let zipFile;
       try {
-        if (reportModule.needsTupaiaApiClient) {
-          if (!this.tupaiaApiClient) {
-            this.tupaiaApiClient = createTupaiaApiClient();
-          }
+        const parameters = requestObject.parameters ? JSON.parse(requestObject.parameters) : {};
+        const excelData = await reportDataGenerator(this.context.store.models, parameters);
+        zipFile = await createZippedExcelFile(reportName, excelData);
+        const result = await this.context.emailService.sendEmail({
+          from: config.mailgun.from,
+          to: request.recipients,
+          subject: 'Report delivery',
+          text: `Report requested: ${requestObject.reportType}`,
+          attachment: zipFile,
+        });
+        if (result.status === COMMUNICATION_STATUSES.SENT) {
+          log.info(`ReportRequestProcessor - Sent report "${zipFile}" to "${request.recipients}"`);
+          await request.update({
+            status: REPORT_REQUEST_STATUSES.PROCESSED,
+          });
+        } else {
+          log.error(`ReportRequestProcessorError - Mailgun error: ${result.error}`);
+          await request.update({
+            status: REPORT_REQUEST_STATUSES.ERROR,
+          });
         }
-        reportData = await reportDataGenerator(
-          this.context.store.models,
-          request.getParameters(),
-          this.tupaiaApiClient,
-        );
       } catch (e) {
         log.error(`ReportRequestProcessorError - Failed to generate report, ${e.message}`);
         log.error(e.stack);
         await request.update({
           status: REPORT_REQUEST_STATUSES.ERROR,
         });
-        return;
-      }
-
-      try {
-        await this.sendReport(request, reportData);
-        await request.update({
-          status: REPORT_REQUEST_STATUSES.PROCESSED,
-        });
-      } catch (e) {
-        log.error(`ReportRequestProcessorError - Failed to send, ${e.message}`);
-        log.error(e.stack);
-        await request.update({
-          status: REPORT_REQUEST_STATUSES.ERROR,
-        });
+      } finally {
+        if (zipFile) {
+          await removeFile(zipFile);
+        }
       }
     }
-  }
-
-  /**
-   * @param request ReportRequest
-   * @param reportData []
-   * @returns {Promise<void>}
-   */
-  async sendReport(request, reportData) {
-    let sent = false;
-    const recipients = request.getRecipients();
-    if (recipients.email) {
-      await this.sendReportToEmail(request, reportData, recipients.email);
-      sent = true;
-    }
-    if (recipients.tupaia) {
-      await this.sendReportToTupaia(request, reportData);
-      sent = true;
-    }
-    if (!sent) {
-      throw new Error('No recipients');
-    }
-  }
-
-  /**
-   * @param request ReportRequest
-   * @param reportData []
-   * @param emailAddresses string[]
-   * @returns {Promise<void>}
-   */
-  async sendReportToEmail(request, reportData, emailAddresses) {
-    const reportName = `${request.reportType}-report-${new Date().getTime()}`;
-
-    let zipFile = null;
-    try {
-      zipFile = await createZippedExcelFile(reportName, reportData);
-
-      const result = await this.context.emailService.sendEmail({
-        from: config.mailgun.from,
-        to: emailAddresses.join(','),
-        subject: 'Report delivery',
-        text: `Report requested: ${request.reportType}`,
-        attachment: zipFile,
-      });
-      if (result.status === COMMUNICATION_STATUSES.SENT) {
-        log.info(
-          `ReportRequestProcessorError - Sent report ${zipFile} to ${emailAddresses.join(',')}`,
-        );
-      } else {
-        throw new Error(`Mailgun error: ${result.error}`);
-      }
-    } finally {
-      await removeFile(zipFile);
-    }
-  }
-
-  /**
-   * @param request ReportRequest
-   * @param reportData []
-   * @returns {Promise<void>}
-   */
-  async sendReportToTupaia(request, reportData) {
-    const reportConfig = config.reports?.[request.reportType];
-
-    if (!reportConfig) {
-      throw new Error('Report not configured');
-    }
-
-    const { surveyId } = reportConfig;
-
-    const translated = translateReportDataToSurveyResponses(surveyId, reportData);
-
-    if (!this.tupaiaApiClient) {
-      this.tupaiaApiClient = createTupaiaApiClient();
-    }
-
-    await this.tupaiaApiClient.meditrak.createSurveyResponses(translated);
   }
 }
