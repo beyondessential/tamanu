4aeb5366cd7ca7eb35f72f13391e2abcd034ffb2
Tri Nguyen <tri@tridnguyen.com>
2021-09-14 22:50:57 -0400

    permission check for list

--- packages/lan/app/routes/apiv1/appointments.js
+++ packages/lan/app/routes/apiv1/appointments.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 import { simplePost, simpleGetList } from './crudHelpers';
 
 export const appointments = express.Router();
 
 appointments.post('/$', simplePost('Appointment'));
 
-appointments.get('/$', simpleGetList('Appointment'));
+appointments.get(
+  '/$',
+  asyncHandler(async (req, res) => {
+    req.checkPermission('list', 'Appointment');
+    simpleGetList('Appointment')(req, res);
+  }),
+);
