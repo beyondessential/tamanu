ac8b8033230fefae6981fad03bfc19dae55192ec
Tri Nguyen <tri@tridnguyen.com>
2021-09-15 19:32:22 -0400

    Update lab request delete behavior (#1400)

--- packages/desktop/app/components/ButtonRow.js
+++ packages/desktop/app/components/ButtonRow.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Button } from './Button';
-import { Colors } from '../constants';
 
 const Row = styled.div`
   display: grid;
