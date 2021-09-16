5f163f064032b81d2ba4efa3e508a006556bde7d
Tom McLean <tom.a.mclean@gmail.com>
2021-09-16 17:47:10 +1000

    Start on switching big migration to more pure-sql

---
+++ packages/shared-src/src/migrations/045_0_populateFacilityTables.js
@@ -0,0 +1,63 @@
+
+module.exports = {
+  up: async query => {
+    // TODO notes:
+    // - are facilites.type and .division ok to leave null?
+    //    - configurable values?
+    // - problematic to carry over set updated_at and created_at?
+    //    - should set to NOW() instead?
+    // - should pulled_at really be omitted from the facility table?
+    // - select default facility better?
+    //    - these records almost certainly require a reimport but still
+
+    // populate facilities   
+    await query.sequelize.query(`
+      INSERT INTO facilities
+        (id, name, code, updated_at, created_at, type, division)
+        SELECT id, name, code, updated_at, created_at, :type, :division
+          FROM reference_data
+          WHERE reference_data.type = 'facility';
+    `, {
+      replacements: {
+        type: '',
+        division: '',
+      }
+    });
+
+    const [records] = await query.sequelize.query(`
+      SELECT id FROM facilities LIMIT 1
+    `);
+    const facilityId = records[0].id;
+
+    // populate departments   
+    await query.sequelize.query(`
+      INSERT INTO departments
+        (id, name, code, updated_at, created_at, facility_id)
+        SELECT id, name, code, updated_at, created_at, :facilityId
+          FROM reference_data
+          WHERE reference_data.type = 'department';
+    `, {
+      replacements: {
+        facilityId,
+      }
+    });
+
+    // populate locations   
+    await query.sequelize.query(`
+      INSERT INTO locations
+        (id, name, code, updated_at, created_at, facility_id)
+        SELECT id, name, code, updated_at, created_at, :facilityId
+          FROM reference_data
+          WHERE reference_data.type = 'location';
+    `, {
+      replacements: {
+        facilityId,
+      }
+    });
+  },
+  down: async query => {
+    await query.sequelize.query(`DELETE FROM locations;`);
+    await query.sequelize.query(`DELETE FROM departments;`);
+    await query.sequelize.query(`DELETE FROM facilities;`);
+  },
+};
---
+++ packages/shared-src/src/migrations/045_1_updateFacilityReferenceColumns.js
@@ -0,0 +1,41 @@
+
+async function switchConstraint(query, table, type, up) {
+  const column = `${target}_id`;
+  const constraint = `${table}_${column}_fkey`;
+
+  // remove existing constraint
+  await query.sequelize.query(`
+    ALTER TABLE ${table} 
+      DROP CONSTRAINT ${constraint};
+  `);
+
+  // add constraint to new table
+  const reference = up ? `${target}s` : `reference_data`;
+  await query.sequelize.query(`
+    ALTER TABLE ${table}
+      ADD CONSTRAINT ${constraint}
+        FOREIGN KEY 
+        REFERENCES ${reference}(id);
+  `);
+}
+
+const TARGETS = [
+  { table: 'encounters', type: 'location' },
+  { table: 'encounters', type: 'department' },
+  { table: 'procedures', type: 'location' },
+];
+
+module.exports = {
+  up: async query => {
+    for (const t of TARGETS) {
+      await switchConstraint(query, t.table, t.type, true);
+    }
+  },
+  down: async query => {
+    const targets = [...TARGETS];
+    targets.reverse();
+    for (const t of targets) {
+      await switchConstraint(query, t.table, t.type, false);
+    }
+  },
+};
