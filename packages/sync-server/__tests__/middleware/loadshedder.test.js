d69c031f2011c328c3d2d829180339cdfaddcaef
Tim Kennedy <t.m.knndy@gmail.com>
2021-09-22 19:06:22 +1000

    TAN-707: Add loadshedder middleware to sync-server (#1402)
    * Add loadshedder middleware to sync-server
    
    * Let loadshedder queues handle multiple prefixes
    
    * Make error logging more readable
    
    * Properly catch 5xx errors in WebRemote
    
    * Improve default.json loadshedder comment
    
    * Refactor requestQueue to queue
    
    * Add explanatory comment to loadshedder break
    
    * Refactor RequestQueue debug output
    
    * Fix bug where maxQueuedRequests=0 would fail all requests
    
    * Write tests around loadshedder
    
    * RequestQueue errors should extend BaseError
    
    * Simplify queue release logic.
    
    * Break queue patch-matching logic out into QueueManager class
    
    * Normalise trailing slashes on loadshedder prefixes
    
    * Write tests around QueueManager catch-all routes

---
+++ packages/sync-server/__tests__/middleware/loadshedder.test.js
@@ -0,0 +1,144 @@
+import { RequestQueueTimeoutError, RequestQueueExceededError } from 'shared/errors';
+
+import { RequestQueue, QueueManager } from 'sync-server/app/middleware/loadshedder';
+
+describe('RequestQueue', () => {
+  it('allows multiple parallel requests through under the threshold', async () => {
+    const queue = new RequestQueue({
+      maxActiveRequests: 2,
+      maxQueuedRequests: 0,
+      queueTimeout: 1000,
+    });
+    const release1 = await queue.acquire();
+    const release2 = await queue.acquire();
+
+    expect(queue.activeRequestCount).toEqual(2);
+    expect(queue.queuedRequests.length).toEqual(0);
+
+    release1();
+    const release3 = await queue.acquire();
+    expect(queue.activeRequestCount).toEqual(2);
+    expect(queue.queuedRequests.length).toEqual(0);
+
+    release2();
+    expect(queue.activeRequestCount).toEqual(1);
+    expect(queue.queuedRequests.length).toEqual(0);
+
+    release3();
+    expect(queue.activeRequestCount).toEqual(0);
+    expect(queue.queuedRequests.length).toEqual(0);
+  });
+
+  it('queues parallel requests over the threshold', async () => {
+    const queue = new RequestQueue({
+      maxActiveRequests: 2,
+      maxQueuedRequests: 2,
+      queueTimeout: 1000,
+    });
+
+    const release1 = await queue.acquire();
+    const release2 = await queue.acquire();
+    const release3Promise = queue.acquire();
+    const release4Promise = queue.acquire();
+
+    expect(queue.activeRequestCount).toEqual(2);
+    expect(queue.queuedRequests.length).toEqual(2);
+
+    release1();
+    release2();
+    const [release3, release4] = await Promise.all([release3Promise, release4Promise]);
+    expect(queue.activeRequestCount).toEqual(2);
+    expect(queue.queuedRequests.length).toEqual(0);
+
+    release3();
+    release4();
+    expect(queue.activeRequestCount).toEqual(0);
+    expect(queue.queuedRequests.length).toEqual(0);
+  });
+
+  it('rejects parallel requests once the queue is full', async () => {
+    const queue = new RequestQueue({
+      maxActiveRequests: 1,
+      maxQueuedRequests: 1,
+      queueTimeout: 1000,
+    });
+    await expect(queue.acquire()).resolves.toEqual(expect.anything());
+    expect(queue.acquire()).toEqual(expect.any(Promise));
+    await expect(queue.acquire()).rejects.toEqual(expect.any(RequestQueueExceededError));
+  });
+
+  it('rejects parallel requests that take too long', async () => {
+    const queue = new RequestQueue({
+      maxActiveRequests: 1,
+      maxQueuedRequests: 1,
+      queueTimeout: 100,
+    });
+    await expect(queue.acquire()).resolves.toEqual(expect.anything());
+    const startMs = Date.now();
+    await expect(queue.acquire()).rejects.toEqual(expect.any(RequestQueueTimeoutError));
+    const elapsedMs = Date.now() - startMs;
+    expect(elapsedMs).toBeLessThan(150);
+  });
+});
+
+describe('QueueManager', () => {
+  {
+    const manager = new QueueManager([
+      {
+        name: 'a',
+        prefixes: ['/1', '/2/'],
+      },
+      {
+        name: 'b',
+        prefixes: ['/3'],
+      },
+      {
+        name: 'c',
+        prefixes: ['/4/4', '/4/5/'],
+      },
+      {
+        name: 'd',
+        prefixes: ['/4/'],
+      },
+    ]);
+
+    it('routes requests to the first matching queue', () => {
+      expect(manager.getQueue('/4/5/foobar')).toHaveProperty('queueName', 'c');
+    });
+
+    it('returns null if no queue matches the path', () => {
+      expect(manager.getQueue('/5')).toEqual(null);
+    });
+
+    it('normalises trailing slashes on prefixes', () => {
+      expect(manager.getQueue('/1234')).toEqual(null);
+    });
+
+    it('normalises trailing slashes on paths', () => {
+      expect(manager.getQueue('/4/5/foobar/')).toHaveProperty('queueName', 'c');
+    });
+  }
+
+  it('allows a route to match everything', () => {
+    const manager = new QueueManager([
+      {
+        name: 'a',
+        prefixes: ['/1'],
+      },
+      {
+        name: 'b',
+        prefixes: ['/'],
+      },
+      {
+        name: 'c',
+        prefixes: ['/2'],
+      },
+    ]);
+    expect(manager.getQueue('/1')).toHaveProperty('queueName', 'a');
+    expect(manager.getQueue('/1/')).toHaveProperty('queueName', 'a');
+    expect(manager.getQueue('/2')).toHaveProperty('queueName', 'b');
+    expect(manager.getQueue('/')).toHaveProperty('queueName', 'b');
+    expect(manager.getQueue('/foo')).toHaveProperty('queueName', 'b');
+    expect(manager.getQueue('/foo/')).toHaveProperty('queueName', 'b');
+  });
+});
