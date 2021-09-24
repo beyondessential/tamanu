5b87166e458041a697d6c00766ae69d214fd0323
Tim Kennedy <t.m.knndy@gmail.com>
2021-09-24 11:29:45 +1000

    Fix LabTestType creation in tests

--- packages/shared-src/src/demoData/labRequests.js
+++ packages/shared-src/src/demoData/labRequests.js
@@ -3,9 +3,11 @@ import { fake } from 'shared/test-helpers/fake';
 
 export const randomLabRequest = async (models, overrides) => {
   const { LabTestType } = models;
-  const labTestTypes = Array(2).map(() => fake(LabTestType));
-  await Promise.all(labTestTypes.map(t => LabTestType.create(t)));
   const categoryId = await randomReferenceId(models, 'labTestCategory');
+  const labTestTypes = Array(2)
+    .fill()
+    .map(() => ({ ...fake(LabTestType), labTestCategoryId: categoryId }));
+  await Promise.all(labTestTypes.map(t => LabTestType.create(t)));
 
   return {
     categoryId,
