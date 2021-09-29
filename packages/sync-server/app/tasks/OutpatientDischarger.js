f47dea49d516b99f03b06746e70caa1bdacfe5de
chris-bes <chris@beyondessential.com.au>
2021-09-29 16:58:49 +1000

    tan-733: batch loading encounters in OutpatientDischarger and fixed schedule

--- packages/sync-server/app/tasks/OutpatientDischarger.js
+++ packages/sync-server/app/tasks/OutpatientDischarger.js
@@ -1,16 +1,16 @@
 import config from 'config';
 import moment from 'moment';
-
 import { Op } from 'sequelize';
 
 import { ScheduledTask } from 'shared/tasks';
 import { log } from 'shared/services/logging';
 
+const ENCOUNTERS_BATCH_SIZE = 1000;
+
 // As well as the sync import auto-discharging old encounters on the way in, we also need a daily
 // task to clean up any that synced in on the same day as they were created
 export class OutpatientDischarger extends ScheduledTask {
-
-  getName() { 
+  getName() {
     return 'OutpatientDischarger';
   }
 
@@ -23,30 +23,47 @@ export class OutpatientDischarger extends ScheduledTask {
   }
 
   async run() {
-    const oldEncounters = await this.models.Encounter.findAll({
-      where: {
-        encounterType: 'clinic',
-        endDate: null,
-        startDate: {
-          [Op.lt]: moment()
+    const where = {
+      encounterType: 'clinic',
+      endDate: null,
+      startDate: {
+        [Op.lt]: moment()
+          .startOf('day')
+          .toDate(),
+      },
+    };
+
+    const oldEncountersCount = await this.models.Encounter.count({ where });
+
+    if (oldEncountersCount === 0) {
+      log.info('No old clinic encounters. Stopping OutpatientDischarger...');
+      return;
+    }
+
+    const batchCount = Math.ceil(oldEncountersCount / ENCOUNTERS_BATCH_SIZE);
+
+    log.info(
+      `Auto-closing ${oldEncountersCount} clinic encounters in ${batchCount} batches (${ENCOUNTERS_BATCH_SIZE} records per batch)`,
+    );
+
+    for (let i = 0; i < batchCount; i++) {
+      const oldEncounters = await this.models.Encounter.findAll({
+        where,
+        limit: ENCOUNTERS_BATCH_SIZE,
+      });
+      const tasks = oldEncounters.map(async encounter => {
+        await encounter.update({
+          endDate: moment()
             .startOf('day')
             .toDate(),
-        },
-      },
-    });
-
-    if (oldEncounters.length === 0) return;
-
-    log.info(`Auto-closing ${oldEncounters.length} clinic encounters`);
-
-    const tasks = oldEncounters.map(async encounter => {
-      await encounter.update({
-        endDate: this.models.Encounter.getAutoDischargeEndDate(encounter),
-        dischargeNote: 'Automatically discharged',
+          dischargeNote: 'Automatically discharged',
+        });
+        log.info(`Auto-closed encounter with id ${encounter.id}`);
       });
-      log.info(`Auto-closed encounter with id ${encounter.id}`);
-    });
 
-    await Promise.all(tasks);
+      await Promise.all(tasks);
+    }
+
+    log.info('OutpatientDischarger finished running');
   }
 }
