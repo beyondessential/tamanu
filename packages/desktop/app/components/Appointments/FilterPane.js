bee249937c622b294f98a7d2a257c7d9cc6dad42
Tri Nguyen <tri@tridnguyen.com>
2021-10-07 01:30:49 -0400

    Filter action

--- packages/desktop/app/components/Appointments/FilterPane.js
+++ packages/desktop/app/components/Appointments/FilterPane.js
@@ -1,28 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { ButtonGroup, Typography } from '@material-ui/core';
 
-import { Button } from '../../components';
+import { Button } from '../Button';
 import { Colors } from '../../constants';
-
-export const FilterPane = () => {
-  return (
-    <>
-      <ViewCalendarBy>
-        <Typography variant="subtitle2">View calendar by:</Typography>
-        <ButtonGroup>
-          <Button color="primary" variant="contained">
-            Locations
-          </Button>
-          <Button disabled>Clinicians</Button>
-        </ButtonGroup>
-      </ViewCalendarBy>
-      <ViewBySelection>
-        <Typography variant="subtitle2">Locations</Typography>
-      </ViewBySelection>
-    </>
-  );
-};
 
 const ViewCalendarBy = styled.div`
   padding: 20px;
@@ -35,3 +16,37 @@ const ViewBySelection = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+const FilterSwitch = styled(ButtonGroup)`
+  margin-top: 0.5rem;
+`;
+
+export const FilterPane = () => {
+  const filters = ['Locations', 'Clinicians'];
+
+  const [activeFilter, setActiveFilter] = useState(0);
+
+  return (
+    <>
+      <ViewCalendarBy>
+        <Typography variant="subtitle2">View calendar by:</Typography>
+        <FilterSwitch>
+          {filters.map((filter, index) => (
+            <Button
+              color={index === activeFilter ? 'primary' : null}
+              variant={index === activeFilter ? 'contained' : null}
+              onClick={() => {
+                setActiveFilter(index);
+              }}
+            >
+              {filter}
+            </Button>
+          ))}
+        </FilterSwitch>
+      </ViewCalendarBy>
+      <ViewBySelection>
+        <Typography variant="subtitle2">{filters[activeFilter]}</Typography>
+      </ViewBySelection>
+    </>
+  );
+};
