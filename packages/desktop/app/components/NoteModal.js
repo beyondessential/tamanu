8b1d83675c01fbf090e31b7c92a73af316fe855b
Tri Nguyen <tri@tridnguyen.com>
2021-10-12 00:40:02 -0400

    remove useCallback

--- packages/desktop/app/components/NoteModal.js
+++ packages/desktop/app/components/NoteModal.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 
 import { useApi } from '../api';
 import { Suggester } from '../utils/suggester';
@@ -10,19 +10,17 @@ export const NoteModal = ({ open, onClose, onSaved, encounterId, noteId, editedO
   const api = useApi();
   const practitionerSuggester = new Suggester(api, 'practitioner');
 
-  const saveNote = useCallback(async data => {
-    if (noteId || data.id) {
-      await api.put(`note/${noteId || data.id}`, data);
-    } else {
-      await api.post(`encounter/${encounterId}/notes`, data);
-    }
-    onSaved();
-  }, []);
-
   return (
     <Modal title="Note" open={open} onClose={onClose}>
       <NoteForm
-        onSubmit={saveNote}
+        onSubmit={async data => {
+          if (noteId || data.id) {
+            await api.put(`note/${noteId || data.id}`, data);
+          } else {
+            await api.post(`encounter/${encounterId}/notes`, data);
+          }
+          onSaved();
+        }}
         onCancel={onClose}
         practitionerSuggester={practitionerSuggester}
         editedObject={editedObject}
