6d3269da8ac18b6c045f673acb5afbabb6a667a0
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2021-10-25 23:23:48 -0500

    Modify backend route to filter database query based on query params

---
+++ packages/lan/app/database/utils.js
@@ -0,0 +1,72 @@
+import { Sequelize, Op } from 'sequelize';
+
+/* 
+Maps query params to expected Sequelize query format.
+
+searchParams: Object
+options: Array<Object>
+option: {
+  key: String
+  alias: String (not required, will default to key)
+  operator: Sequelize Operator (Sequelize.Op)
+  mapFn: Function that returns an accepted sequelize 'where' filter object
+}
+*/
+export const mapQueryFilters = (params, options) => {
+
+  // Helper function used as the default mapFn
+  const defaultFilter = (fieldName, operator, value) => {
+    return {[fieldName]: {[operator]: value}};
+  };
+
+  const queryFilters = [];
+
+  // Go through each filter option
+  options.forEach(({ key, alias, operator, mapFn = defaultFilter }) => {
+
+    // Extract specified keys without undefined or null values
+    if (params[key]) {
+
+      // Map key to specified alias
+      const newKey = alias ? alias : key;
+
+      // Add new filter
+      queryFilters.push(mapFn(newKey, operator, params[key]));
+    }
+  });
+
+  if (queryFilters.length > 0)
+    return {[Op.and]: queryFilters};
+  else
+    return {};
+};
+
+/*
+  Returns a function that creates a case insensitive filter by calling the 
+  'UPPER' function from SQL in both the column and the value to match against 
+  using a custom operator. It binds a fields object that maps the model's
+  fieldnames to the actual database column names.
+*/
+export const getCaseInsensitiveFilter = fields => {
+  return (fieldName, operator, value) => {
+    const columnName = fields[fieldName];
+    const filterValue = Sequelize.where(Sequelize.fn('upper', Sequelize.col(columnName)), {
+      [operator]: value.toUpperCase()
+    });
+  
+    return {[fieldName]: filterValue};
+  };
+};
+  
+/*
+  Returns a function that creates an equal comparison filter that maps 
+  a text value to a boolean value. Useful for displaying dropdowns
+  on form fields instead of checkboxes. It binds a string expected
+  to be transformed to true, other strings will default to false.
+*/
+export const getTextToBooleanFilter = trueString => {
+  return (fieldName, operator, value) => {
+    const booleanValue = value === trueString ? true : false;
+    return {[fieldName]: {[operator]: booleanValue}};
+  };
+};
--- packages/lan/app/routes/apiv1/imaging.js
+++ packages/lan/app/routes/apiv1/imaging.js
@@ -1,13 +1,31 @@
 import express from 'express';
-import { ENCOUNTER_PATIENT } from '../../database/includes';
+import asyncHandler from 'express-async-handler';
+import { Op } from 'sequelize';
+import {
+  mapQueryFilters,
+  getCaseInsensitiveFilter,
+  getTextToBooleanFilter
+} from '../../database/utils';
 
 import {
   simpleGet,
   simplePut,
   simplePost,
-  simpleGetList,
   permissionCheckingRouter,
 } from './crudHelpers';
+
+// Object used to map field names to database column names
+const SNAKE_CASE_COLUMN_NAMES = {
+  firstName: 'first_name',
+  lastName: 'last_name',
+  displayId: 'display_id',
+  id: 'ImagingRequest.id',
+  name: 'name'
+};
+
+// Filtering functions for sequelize queries
+const caseInsensitiveFilter = getCaseInsensitiveFilter(SNAKE_CASE_COLUMN_NAMES);
+const urgencyTextToBooleanFilter = getTextToBooleanFilter('urgent');
 
 export const imagingRequest = express.Router();
 
@@ -16,8 +34,87 @@ imagingRequest.put('/:id', simplePut('ImagingRequest'));
 imagingRequest.post('/$', simplePost('ImagingRequest'));
 
 const globalImagingRequests = permissionCheckingRouter('list', 'ImagingRequest');
+
+// Route used on ImagingRequestsTable component
 globalImagingRequests.get(
   '/$',
-  simpleGetList('ImagingRequest', '', { include: [ENCOUNTER_PATIENT] }),
+  asyncHandler(async (req, res) => {
+    const { models, query } = req;
+    const { order = 'ASC', orderBy, rowsPerPage = 10, page = 0, ...filterParams } = query;
+
+    // Model filters for Sequelize 'where' clauses
+    const imagingTypeFilters = mapQueryFilters(
+      filterParams,
+      [
+        {
+          key: 'imagingType',
+          alias: 'name',
+          operator: Op.startsWith,
+          mapFn: caseInsensitiveFilter
+        }
+      ]
+    );
+    const patientFilters = mapQueryFilters(
+      filterParams,
+      [
+        {key: 'firstName', operator: Op.startsWith, mapFn: caseInsensitiveFilter},
+        {key: 'lastName', operator: Op.startsWith, mapFn: caseInsensitiveFilter},
+        {key: 'displayId', operator: Op.startsWith, mapFn: caseInsensitiveFilter}
+      ]
+    );
+    const imagingRequestFilters = mapQueryFilters(
+      filterParams,
+      [
+        {
+          key: 'requestId',
+          alias: 'id',
+          operator: Op.startsWith,
+          mapFn: caseInsensitiveFilter
+        },
+        {key: 'status', operator: Op.eq},
+        {
+          key: 'urgency',
+          alias: 'urgent',
+          operator: Op.eq,
+          mapFn: urgencyTextToBooleanFilter
+        },
+        {key: 'requestedDateFrom', alias: 'requestedDate', operator: Op.gte},
+        {key: 'requestedDateTo', alias: 'requestedDate', operator: Op.lte}
+      ]
+    );
+
+    // Associations to include on query
+    const requestedBy = {
+      association: 'requestedBy'
+    };
+    const imagingType = {
+      association: 'imagingType',
+      where: imagingTypeFilters
+    };
+    const patient = {
+      association: 'patient',
+      where: patientFilters
+    };
+    const encounter = { 
+      association: 'encounter',
+      include: [patient],
+      required: true
+    };
+
+    // Query database
+    const objects = await models['ImagingRequest'].findAll({
+      where: imagingRequestFilters,
+      order: orderBy ? [[orderBy, order.toUpperCase()]] : undefined,
+      include: [requestedBy, imagingType, encounter],
+    });
+
+    // Normalize data calling a base model method
+    const data = objects.map(x => x.forResponse());
+
+    res.send({
+      count: objects.length,
+      data: data,
+    });
+  })
 );
 imagingRequest.use(globalImagingRequests);
