afdc151cfa8b7d2f2a1d267d8ce87e1e217c7658
Tim Kennedy <t.m.knndy@gmail.com>
2021-10-29 19:36:57 +1100

    Log bound parameters in db query logging

--- packages/shared-src/src/services/database.js
+++ packages/shared-src/src/services/database.js
@@ -2,6 +2,7 @@ import { Sequelize } from 'sequelize';
 import { createNamespace } from 'cls-hooked';
 import pg from 'pg';
 import wayfarer from 'wayfarer';
+import util from 'util';
 
 // an issue in how webpack's require handling interacts with sequelize means we need
 // to provide the module to sequelize manually
@@ -75,7 +76,12 @@ async function connectToDatabase(dbOptions) {
   const namespace = createNamespace('sequelize-transaction-namespace');
   Sequelize.useCLS(namespace);
 
-  const logging = verbose ? s => log.debug(s) : null;
+  const logging = verbose
+    ? (query, obj) =>
+        log.debug(
+          `${util.inspect(query)}; -- ${util.inspect(obj.bind || [], { breakLength: Infinity })}`,
+        )
+    : null;
   const options = sqlitePath
     ? { dialect: 'sqlite', dialectModule: sqlite3, storage: sqlitePath }
     : { dialect: 'postgres' };
