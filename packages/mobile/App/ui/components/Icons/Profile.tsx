50be4eeb5bcecbeaea33740784c9677eb4325b64
Alastair-L <lockealastair@gmail.com>
2021-11-03 18:44:42 +1100

    Fix type error

--- packages/mobile/App/ui/components/Icons/Profile.tsx
+++ packages/mobile/App/ui/components/Icons/Profile.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { Image, ImageProps } from 'react-native';
 
-export const ProfileIcon = memo((props: ImageProps) => (
+export const ProfileIcon = memo((props: Partial<ImageProps>) => (
   <Image
     source={require('../../assets/newPatientIcon.png')}
     resizeMode="contain"
