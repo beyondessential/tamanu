77295d3d79de588b0159e2fa8a4d88aebd9af3eb
Tim Kennedy <t.m.knndy@gmail.com>
2021-11-03 16:27:21 +1100

    Slightly DRY up repeated config flag checks

--- packages/sync-server/app/integrations/fiji/index.js
+++ packages/sync-server/app/integrations/fiji/index.js
@@ -3,17 +3,19 @@ import config from 'config';
 
 import { vrsRoutes, publicVrsRoutes, VRSRemote } from './vrs';
 
+const isVRSEnabled = !!config.integrations.fiji.vrs.enabled;
+
 export const fijiRoutes = express.Router();
 export const publicFijiRoutes = express.Router();
 
-if (config.integrations.fiji.vrs.enabled) {
+if (isVRSEnabled) {
   fijiRoutes.use('/vrs', vrsRoutes);
   publicFijiRoutes.use('/vrs', publicVrsRoutes);
 }
 
 export const initFijiServices = ctx => {
   const services = {};
-  if (config.integrations.fiji.vrs.enabled) {
+  if (isVRSEnabled) {
     services.vrsRemote = new VRSRemote(ctx.store, config.integrations.fiji.vrs);
   }
   return services;
