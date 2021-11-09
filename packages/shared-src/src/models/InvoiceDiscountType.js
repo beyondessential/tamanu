fd40ec5d793d6935c63d81c450ae409fd0ebfbf7
chris-bes <chris@beyondessential.com.au>
2021-11-09 23:23:51 +1100

    tan-920: simplify table names

---
+++ packages/shared-src/src/models/InvoiceDiscountType.js
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize';
+import { SYNC_DIRECTIONS } from 'shared/constants';
+import { Model } from './Model';
+
+export class InvoiceDiscountType extends Model {
+  static init({ primaryKey, ...options }) {
+    super.init(
+      {
+        id: primaryKey,
+        itemId: Sequelize.STRING,
+        itemType: Sequelize.STRING,
+        name: Sequelize.TEXT,
+        discount: Sequelize.STRING,
+      },
+      {
+        ...options,
+        syncConfig: { syncDirection: SYNC_DIRECTIONS.PULL_ONLY },
+      },
+    );
+  }
+}
