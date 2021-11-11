3db8f97013ae6b307d4014e709ed540d99c9a5be
Tim Kennedy <t.m.knndy@gmail.com>
2021-11-11 15:16:59 +1100

    Log integration initialisation on startup

--- packages/sync-server/app/integrations/index.js
+++ packages/sync-server/app/integrations/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import config from 'config';
+
+import { log } from 'shared/services/logging';
+
 import * as fijiVrs from './fiji-vrs';
 
 const integrations = {
@@ -12,6 +15,7 @@ export const publicIntegrationRoutes = express.Router();
 export const initIntegrations = async ctx => {
   for (const [key, integration] of Object.entries(integrations)) {
     if (config.integrations[key].enabled) {
+      log.info(`initIntegrations: ${key}: initialising`);
       const { routes, publicRoutes, initAppContext } = integration;
       await initAppContext(ctx);
       if (routes) {
@@ -20,6 +24,8 @@ export const initIntegrations = async ctx => {
       if (publicRoutes) {
         publicIntegrationRoutes.use(`/${key}`, publicRoutes);
       }
+    } else {
+      log.info(`initIntegrations: ${key}: disabled, did not initialise`);
     }
   }
 };
