dbd0d93a42abf6a67f3fa5fd22f01a91455e04c2
Tri Nguyen <tri@tridnguyen.com>
2021-11-16 01:03:45 -0500

    Add cancel modal when canceling appointment

--- packages/desktop/app/components/Appointments/AppointmentDetail.js
+++ packages/desktop/app/components/Appointments/AppointmentDetail.js
@@ -10,7 +10,8 @@ import { DateDisplay } from '../DateDisplay';
 import { Colors, appointmentStatusOptions } from '../../constants';
 import { useApi } from '../../api';
 import { AppointmentModal } from './AppointmentModal';
-import { Button } from '..';
+import { Button, DeleteButton } from '../Button';
+import { Modal } from '../Modal';
 
 const Container = styled.div`
   display: flex;
@@ -79,43 +80,92 @@ const PatientInfo = ({ patient }) => {
         <InvertedDisplayIdLabel>{displayId}</InvertedDisplayIdLabel>
       </PatientNameRow>
       <table>
-        <tr>
-          <PatientInfoLabel>Sex</PatientInfoLabel>
-          <PatientInfoValue>{sex}</PatientInfoValue>
-        </tr>
-        <tr>
-          <PatientInfoLabel>Date of Birth</PatientInfoLabel>
-          <PatientInfoValue>
-            <DateDisplay date={dateOfBirth} />
-          </PatientInfoValue>
-        </tr>
-        {additionalData && additionalData.primaryContactNumber && (
+        <tbody>
           <tr>
-            <PatientInfoLabel>Contact Number</PatientInfoLabel>
-            <PatientInfoValue>{additionalData.primaryContactNumber}</PatientInfoValue>
+            <PatientInfoLabel>Sex</PatientInfoLabel>
+            <PatientInfoValue>{sex}</PatientInfoValue>
           </tr>
-        )}
-        {village && (
           <tr>
-            <PatientInfoLabel>Village</PatientInfoLabel>
-            <PatientInfoValue>{village.name}</PatientInfoValue>
+            <PatientInfoLabel>Date of Birth</PatientInfoLabel>
+            <PatientInfoValue>
+              <DateDisplay date={dateOfBirth} />
+            </PatientInfoValue>
           </tr>
-        )}
+          {additionalData && additionalData.primaryContactNumber && (
+            <tr>
+              <PatientInfoLabel>Contact Number</PatientInfoLabel>
+              <PatientInfoValue>{additionalData.primaryContactNumber}</PatientInfoValue>
+            </tr>
+          )}
+          {village && (
+            <tr>
+              <PatientInfoLabel>Village</PatientInfoLabel>
+              <PatientInfoValue>{village.name}</PatientInfoValue>
+            </tr>
+          )}
+        </tbody>
       </table>
     </PatientInfoContainer>
   );
 };
 
+const AppointmentTime = ({ startTime, endTime }) => {
+  return (
+    <span>
+      {format(new Date(startTime), 'ccc dd LLL')}
+      {' - '}
+      {format(new Date(startTime), 'h:mm aaa')}
+      {endTime && ` - ${format(new Date(endTime), 'h:mm aaa')}`}
+    </span>
+  );
+};
+
+const Row = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Details = styled.div`
+  margin-bottom: 1rem;
+`;
+
+const CancelAppointmentModal = ({ open, onClose, onConfirm, appointment }) => {
+  const { type, patient } = appointment;
+  return (
+    <Modal width="sm" title="Cancel Appointment" onClose={onClose} open={open}>
+      <Heading>Are you sure you want to cancel this appointment?</Heading>
+      <Details>
+        {`${type} appointment for `}
+        <PatientNameDisplay patient={patient} />
+        {' - '}
+        <AppointmentTime {...appointment} />
+      </Details>
+      <Row>
+        <DeleteButton onClick={onConfirm}>Yes, Cancel</DeleteButton>
+      </Row>
+    </Modal>
+  );
+};
+
 export const AppointmentDetail = ({ appointment, updated }) => {
   const api = useApi();
-  const { id, type, status, clinician, startTime, endTime, patient, location } = appointment;
-  const [newStatus, setNewStatus] = useState(
+  const { id, type, status, clinician, patient, location } = appointment;
+  const [statusOption, setStatusOption] = useState(
     appointmentStatusOptions.find(option => option.value === status),
   );
-  const [openModal, setOpenModal] = useState(false);
+  const [appointmentModal, setAppointmentModal] = useState(false);
+  const [cancelModal, setCancelModal] = useState(false);
+  const [cancelConfirmed, setCancelConfirmed] = useState(false);
   useEffect(() => {
-    setNewStatus(appointmentStatusOptions.find(option => option.value === status));
+    setStatusOption(appointmentStatusOptions.find(option => option.value === status));
   }, [status]);
+
+  const updateAppointmentStatus = async newValue => {
+    await api.put(`appointments/${id}`, {
+      status: newValue,
+    });
+    updated();
+  };
   return (
     <Container>
       <FirstRow>
@@ -124,27 +174,21 @@ export const AppointmentDetail = ({ appointment, updated }) => {
           {type}
           <Heading>Time</Heading>
           <div>
-            {format(new Date(startTime), 'ccc dd LLL')}
-            {' - '}
-            {format(new Date(startTime), 'h:mm aaa')}
-            {endTime && ` - ${format(new Date(endTime), 'h:mm aaa')}`}
+            <AppointmentTime {...appointment} />
           </div>
         </div>
         <Select
           placeholder="Select Status"
           options={appointmentStatusOptions}
-          value={newStatus}
+          value={statusOption}
           name="status"
           onChange={async selectedOption => {
-            setNewStatus(selectedOption);
-            await api.put(`appointments/${id}`, {
-              status: selectedOption.value,
-            });
-            // hide the tooltip if cancelling appointment
-            if (selectedOption.value === APPOINTMENT_STATUSES.CANCELLED) {
-              Tooltip.hide();
+            if (selectedOption.value === APPOINTMENT_STATUSES.CANCELLED && !cancelConfirmed) {
+              setCancelModal(true);
+              return;
             }
-            updated();
+            setStatusOption(selectedOption);
+            await updateAppointmentStatus(selectedOption.value);
           }}
         />
       </FirstRow>
@@ -161,20 +205,39 @@ export const AppointmentDetail = ({ appointment, updated }) => {
         variant="outlined"
         color="primary"
         onClick={() => {
-          setOpenModal(true);
+          setAppointmentModal(true);
         }}
       >
         Reschedule
       </Button>
       <AppointmentModal
-        open={openModal}
+        open={appointmentModal}
         onClose={() => {
-          setOpenModal(false);
+          setAppointmentModal(false);
         }}
         appointment={appointment}
         onSuccess={() => {
           Tooltip.hide();
           updated();
+        }}
+      />
+      <CancelAppointmentModal
+        appointment={appointment}
+        open={cancelModal}
+        onClose={() => {
+          setCancelModal(false);
+        }}
+        onConfirm={async () => {
+          setCancelConfirmed(true);
+          try {
+            await updateAppointmentStatus(APPOINTMENT_STATUSES.CANCELLED);
+            setCancelConfirmed(false);
+            // hide the tooltip if cancelling appointment
+            Tooltip.hide();
+          } catch (e) {
+            console.error(e);
+          }
+          setCancelModal(false);
         }}
       />
     </Container>
