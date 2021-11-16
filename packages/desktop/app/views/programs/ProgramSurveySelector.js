4593fa47e947d0883f6af5e54e65cfb8f56e4447
Tri Nguyen <tri@tridnguyen.com>
2021-11-15 23:51:27 -0500

    refactor program survey

--- packages/desktop/app/views/programs/ProgramSurveySelector.js
+++ packages/desktop/app/views/programs/ProgramSurveySelector.js
@@ -1,57 +1,62 @@
 import React, { useState, useCallback, useEffect } from 'react';
 
+import { useApi } from 'desktop/app/api';
 import { FormGrid } from 'desktop/app/components/FormGrid';
-
 import { SelectInput } from 'desktop/app/components/Field/SelectField';
+
+import { SURVEY_TYPES } from 'shared/constants';
 
 import { PatientDisplay } from './PatientDisplay';
 import { ProgramsPane, ProgramsPaneHeader, ProgramsPaneHeading } from './ProgramsPane';
 import { SurveySelector } from './SurveySelector';
 
-export const ProgramSurveySelector = React.memo(
-  ({ onSelectSurvey, programs, onFetchSurveysList }) => {
-    const [selectedProgramId, setSelectedProgramId] = useState(null);
-    const [surveyOptions, setSurveyOptions] = useState(null);
-    const [programOptions, setProgramOptions] = useState(null);
+export const ProgramSurveySelector = React.memo(({ onSelectSurvey, programs }) => {
+  const api = useApi();
+  const [selectedProgramId, setSelectedProgramId] = useState(null);
+  const [surveyOptions, setSurveyOptions] = useState(null);
+  const [programOptions, setProgramOptions] = useState(null);
 
-    useEffect(() => {
-      setProgramOptions(programs.map(x => ({ value: x.id, label: x.name })));
-    }, [programs]);
+  useEffect(() => {
+    setProgramOptions(programs.map(x => ({ value: x.id, label: x.name })));
+  }, [programs]);
 
-    const onChangeProgram = useCallback(async event => {
-      const programId = event.target.value;
-      if (programId === selectedProgramId) {
-        return;
-      }
+  const onChangeProgram = useCallback(async event => {
+    const programId = event.target.value;
+    if (programId === selectedProgramId) {
+      return;
+    }
 
-      setSelectedProgramId(programId);
+    setSelectedProgramId(programId);
 
-      const surveys = await onFetchSurveysList(programId);
-      setSurveyOptions(surveys.map(x => ({ value: x.id, label: x.name })));
-    });
-
-    return (
-      <>
-        <PatientDisplay />
-        <ProgramsPane>
-          <ProgramsPaneHeader>
-            <ProgramsPaneHeading variant="h6">Select a survey</ProgramsPaneHeading>
-          </ProgramsPaneHeader>
-          <FormGrid columns={1}>
-            <SelectInput
-              options={programOptions}
-              value={selectedProgramId}
-              onChange={onChangeProgram}
-              label="Select program"
-            />
-            <SurveySelector
-              onSelectSurvey={onSelectSurvey}
-              surveys={surveyOptions}
-              buttonText="Begin survey"
-            />
-          </FormGrid>
-        </ProgramsPane>
-      </>
+    const { data: surveys } = await api.get(`program/${programId}/surveys`);
+    setSurveyOptions(
+      surveys
+        .filter(s => s.surveyType === SURVEY_TYPES.PROGRAMS)
+        .map(x => ({ value: x.id, label: x.name })),
     );
-  },
-);
+  });
+
+  return (
+    <>
+      <PatientDisplay />
+      <ProgramsPane>
+        <ProgramsPaneHeader>
+          <ProgramsPaneHeading variant="h6">Select a survey</ProgramsPaneHeading>
+        </ProgramsPaneHeader>
+        <FormGrid columns={1}>
+          <SelectInput
+            options={programOptions}
+            value={selectedProgramId}
+            onChange={onChangeProgram}
+            label="Select program"
+          />
+          <SurveySelector
+            onSelectSurvey={onSelectSurvey}
+            surveys={surveyOptions}
+            buttonText="Begin survey"
+          />
+        </FormGrid>
+      </ProgramsPane>
+    </>
+  );
+});
