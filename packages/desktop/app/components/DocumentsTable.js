a720b2e99ef907400fe2a20578ae8c9afe3a6867
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2021-11-29 22:21:09 -0500

    Remove buggy accessors from DocumentsTable

--- packages/desktop/app/components/DocumentsTable.js
+++ packages/desktop/app/components/DocumentsTable.js
@@ -12,8 +12,8 @@ const COLUMNS = [
   { key: 'createdDate', title: 'Creation', accessor: getDate },
   { key: 'uploadedDate', title: 'Upload', accessor: getDate },
   { key: 'owner', title: 'Owner', accessor: getDisplayName },
-  { key: 'comments', title: 'Comments', accessor: '' },
-  { key: 'actions', title: 'Actions', accessor: '' },
+  { key: 'comments', title: 'Comments' },
+  { key: 'actions', title: 'Actions' },
 ];
 
 export const DocumentsTable = React.memo(({ encounterId, patientId, searchParameters }) => {
