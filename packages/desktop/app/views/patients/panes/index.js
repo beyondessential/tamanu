3609e60768be8b2b019f347efd17df4260983d74
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2021-11-29 20:05:29 -0500

    Errata: Fix file import directory because of renaming

--- packages/desktop/app/views/patients/panes/index.js
+++ packages/desktop/app/views/patients/panes/index.js
@@ -3,6 +3,6 @@ export { ConnectedPatientDetailsForm } from './DetailsPane';
 export { HistoryPane } from './HistoryPane';
 export { ImmunisationsPane } from './ImmunisationsPane';
 export { MedicationsPane } from './MedicationsPane';
-export { PatientDocumentsPane } from './DocumentsPane';
+export { PatientDocumentsPane } from './PatientDocumentsPane';
 export { ProgramsPane } from './ProgramsPane';
 export { ReferralPane } from './ReferralPane';
