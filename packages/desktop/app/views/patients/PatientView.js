778597a0634f9194490a1a93d8e059f3f231ae7b
Alastair-L <lockealastair@gmail.com>
2021-12-07 16:22:36 +1100

    Fix messy merge dev

--- packages/desktop/app/views/patients/PatientView.js
+++ packages/desktop/app/views/patients/PatientView.js
@@ -9,53 +9,6 @@ import { PatientInfoPane } from '../../components/PatientInfoPane';
 import { EncounterModal } from '../../components/EncounterModal';
 import { TriageModal } from '../../components/TriageModal';
 import { connectRoutedModal } from '../../components/Modal';
-import { PatientEncounterSummary } from './components/PatientEncounterSummary';
-import { DataFetchingProgramsTable } from '../../components/ProgramResponsesTable';
-
-import { PatientDetailsForm } from '../../forms/PatientDetailsForm';
-import { reloadPatient } from '../../store/patient';
-
-import { useEncounter } from '../../contexts/Encounter';
-
-const AppointmentPane = React.memo(({ patient, readonly }) => {
-  const [modalOpen, setModalOpen] = React.useState(false);
-
-  return (
-    <div>
-      <AppointmentModal
-        open={modalOpen}
-        patientId={patient.id}
-        onClose={() => setModalOpen(false)}
-      />
-      <AppointmentTable appointments={patient.appointments} />
-      <ContentPane>
-        <Button
-          onClick={() => setModalOpen(true)}
-          variant="contained"
-          color="primary"
-          disabled={readonly}
-        >
-          New appointment
-        </Button>
-      </ContentPane>
-    </div>
-  );
-});
-
-const ReferralPane = connect(null, dispatch => ({
-  onNavigateToReferrals: () => dispatch(push('/referrals')),
-}))(
-  React.memo(({ onNavigateToReferrals, patient }) => (
-    <div>
-      <ReferralTable patientId={patient.id} />
-      <ContentPane>
-        <Button onClick={onNavigateToReferrals} variant="contained" color="primary">
-          New referral
-        </Button>
-      </ContentPane>
-    </div>
-  )),
-);
 
 import {
   AppointmentPane,
@@ -71,67 +24,9 @@ import {
 const RoutedEncounterModal = connectRoutedModal('/patients/view', 'checkin')(EncounterModal);
 const RoutedTriageModal = connectRoutedModal('/patients/view', 'triage')(TriageModal);
 
-  state => ({
-    currentEncounter: state.patient.currentEncounter,
-    patient: state.patient,
-  }),
-  dispatch => ({
-    onOpenCheckin: () => dispatch(push('/patients/view/checkin')),
-    onOpenTriage: () => dispatch(push('/patients/view/triage')),
-  }),
-)(
-  React.memo(({ patient, currentEncounter, onOpenCheckin, onOpenTriage, disabled }) => {
-    const { encounter, loadEncounter } = useEncounter();
-    const onViewEncounter = useCallback(
-      async id => {
-        await loadEncounter(id, true);
-      },
-      [encounter],
-    );
-    return (
-      <div>
-        <PatientEncounterSummary
-          encounter={currentEncounter}
-          viewEncounter={onViewEncounter}
-          openCheckin={onOpenCheckin}
-          openTriage={onOpenTriage}
-          disabled={disabled}
-        />
-        <PatientHistory patient={patient} onItemClick={onViewEncounter} />
-      </div>
-    );
-  }),
-);
-
-const ConnectedPatientDetailsForm = connectApi((api, dispatch, { patient }) => ({
-  onSubmit: async data => {
-    await api.put(`patient/${patient.id}`, data);
-    dispatch(reloadPatient(patient.id));
-  },
-}))(
-  React.memo(props => (
-    <ContentPane>
-      <PatientDetailsForm {...props} />
-    </ContentPane>
-  )),
-);
-
-const ProgramsPane = connect(null, dispatch => ({
-  onNavigateToPrograms: () => dispatch(push('/programs')),
-}))(
-  React.memo(({ onNavigateToPrograms, patient }) => (
-    <div>
-      <DataFetchingProgramsTable patientId={patient.id} />
-      <ContentPane>
-        <Button onClick={onNavigateToPrograms} variant="contained" color="primary">
-          New survey
-        </Button>
-      </ContentPane>
-    </div>
-  )),
-);
-
 const TABS = [
+  {
+    label: 'History',
     key: 'history',
     icon: 'fa fa-calendar-day',
     render: () => <HistoryPane />,
