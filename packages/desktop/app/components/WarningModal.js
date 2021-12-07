5122ebb86ca5119329b470d3c3efb4bc9479db82
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2021-12-06 21:30:18 -0500

    Modify WarningModal display

--- packages/desktop/app/components/WarningModal.js
+++ packages/desktop/app/components/WarningModal.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { DeleteButton, Button } from './Button';
-import { ButtonRow } from './ButtonRow';
 import { Modal } from './Modal';
+
+const BackButton = styled(Button)`
+  margin-left: 8px;
+`;
 
 export const WarningModal = React.memo(({ open, title, text, onConfirm, onClose }) => (
   <Modal open={open} onClose={onClose} title={title}>
     <h3>WARNING: This action is irreversible!</h3>
     <p>{text}</p>
-    <ButtonRow>
-      <DeleteButton onClick={onConfirm}>Yes</DeleteButton>
-      <Button variant="contained" color="primary" onClick={onClose}>
-        No
-      </Button>
-    </ButtonRow>
+    <DeleteButton onClick={onConfirm} variant="contained">
+      Yes
+    </DeleteButton>
+    <BackButton onClick={onClose} variant="contained" color="primary">
+      No
+    </BackButton>
   </Modal>
 ));
