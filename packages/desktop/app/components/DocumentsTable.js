1a7543f6e2430ce94a59c061687de00c2bb2a9dc
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2021-12-08 22:17:57 -0500

    Create appropiate accessors and finish documents table UI

--- packages/desktop/app/components/DocumentsTable.js
+++ packages/desktop/app/components/DocumentsTable.js
@@ -1,19 +1,54 @@
 import React from 'react';
+import { extension } from 'mime-types';
 
 import { DataFetchingTable } from './Table';
 import { DateDisplay } from './DateDisplay';
+import { DropdownButton } from './DropdownButton';
 
-const getDate = ({ date }) => (date ? <DateDisplay date={date} /> : '');
-const getDisplayName = ({ owner }) => (owner || {}).displayName || '';
+const ActionDropdown = React.memo(({ row }) => {
+  const actions = [
+    {
+      label: 'Delete',
+      onClick: () => console.log('clicked delete'),
+    },
+    {
+      label: 'Attach to care plan',
+      onClick: () => console.log('clicked attach to care plan'),
+    },
+  ];
+
+  return (
+    <>
+      <DropdownButton color="primary" actions={actions} />
+    </>
+  );
+});
+
+const getType = ({ type }) => {
+  const fileExtension = extension(type);
+  if (typeof fileExtension === 'string') return fileExtension.toUpperCase();
+  return 'Unknown';
+};
+const getUploadedDate = ({ documentUploadedAt }) =>
+  documentUploadedAt ? <DateDisplay date={documentUploadedAt} /> : '';
+const getDisplayName = ({ documentOwner }) => documentOwner?.displayName || '';
+const getDepartmentName = ({ departmentId }) => departmentId?.name || '';
+const getActions = ({ row }) => <ActionDropdown row={row} />;
 
 const COLUMNS = [
   { key: 'name', title: 'Name' },
-  { key: 'type', title: 'Type' },
-  { key: 'createdDate', title: 'Creation', accessor: getDate },
-  { key: 'uploadedDate', title: 'Upload', accessor: getDate },
-  { key: 'owner', title: 'Owner', accessor: getDisplayName },
-  { key: 'comments', title: 'Comments' },
-  { key: 'actions', title: 'Actions' },
+  { key: 'type', title: 'Type', accessor: getType },
+  { key: 'document_uploaded_at', title: 'Upload', accessor: getUploadedDate },
+  { key: 'document_owner', title: 'Owner', accessor: getDisplayName },
+  { key: 'department_id', title: 'Department', accessor: getDepartmentName, sortable: false },
+  { key: 'note', title: 'Comments', sortable: false },
+  {
+    key: 'actions',
+    title: 'Actions',
+    accessor: getActions,
+    dontCallRowInput: true,
+    sortable: false,
+  },
 ];
 
 export const DocumentsTable = React.memo(({ encounterId, patientId, searchParameters }) => {
