0936459c6b984f99d94371def09fe482554a66f3
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-01-10 17:46:53 -0500

    Add document metadata POST endpoint logic to encounters

--- packages/lan/app/routes/apiv1/encounter.js
+++ packages/lan/app/routes/apiv1/encounter.js
@@ -2,8 +2,9 @@ import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { Op } from 'sequelize';
 import { NotFoundError } from 'shared/errors';
-import { LAB_REQUEST_STATUSES } from 'shared/constants';
+import { LAB_REQUEST_STATUSES, DOCUMENT_SIZE_LIMIT } from 'shared/constants';
 import { NOTE_RECORD_TYPES } from 'shared/models/Note';
+import { uploadAttachment } from '../../utils/uploadAttachment';
 
 import {
   simpleGet,
@@ -87,15 +88,26 @@ encounter.post(
 encounter.post(
   '/:id/documentMetadata',
   asyncHandler(async (req, res) => {
-    // TODO: After the endpoint logic on patientDocumentMetadata gets
-    // finalized, reutilize same logic and create tests for this route
     const { models, params } = req;
+    // TODO: figure out permissions with Attachment and DocumentMetadata
     req.checkPermission('write', 'DocumentMetadata');
-    const patientId = params.id;
-    const { file: fileName, ...documentMetadata } = req.body;
 
-    // Do nothing at the moment, logic will be created in another card
-    res.send({});
+    // Make sure the specified encounter exists
+    const specifiedEncounter = await models.Encounter.findByPk(params.id);
+    if (!specifiedEncounter) {
+      throw new NotFoundError();
+    }
+
+    // Create file on the sync server
+    const { attachmentId, metadata } = await uploadAttachment(req, DOCUMENT_SIZE_LIMIT);
+
+    const documentMetadataObject = await models.DocumentMetadata.create({
+      ...metadata,
+      attachmentId,
+      encounterId: params.id,
+    });
+
+    res.send(documentMetadataObject);
   }),
 );
 
