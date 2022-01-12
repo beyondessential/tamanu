82dbc71258757310acb72729bd481a02acd99d50
Tri Nguyen <tri@tridnguyen.com>
2022-01-12 16:47:27 -0500

    TAN-920: more model changes (#1686)

--- packages/shared-src/src/models/InvoiceLineType.js
+++ packages/shared-src/src/models/InvoiceLineType.js
@@ -7,8 +7,8 @@ export class InvoiceLineType extends Model {
     super.init(
       {
         id: primaryKey,
-        item_id: Sequelize.STRING,
-        item_type: Sequelize.STRING,
+        itemId: Sequelize.STRING,
+        itemType: Sequelize.STRING,
         name: Sequelize.TEXT,
         price: Sequelize.DECIMAL,
       },
@@ -17,5 +17,74 @@ export class InvoiceLineType extends Model {
         syncConfig: { syncDirection: SYNC_DIRECTIONS.PULL_ONLY },
       },
     );
+  }
+
+  static initRelations(models) {
+    this.belongsTo(models.ReferenceData, {
+      foreignKey: 'item_id',
+      as: 'procedureType',
+      constraint: false,
+    });
+
+    this.belongsTo(models.ReferenceData, {
+      foreignKey: 'item_id',
+      as: 'imagingType',
+      constraint: false,
+    });
+
+    this.belongsTo(models.LabTestType, {
+      foreignKey: 'item_id',
+      as: 'labTestType',
+      constraint: false,
+    });
+  }
+
+  static getFullLinkedItemsInclude(models) {
+    return [
+      {
+        model: models.ReferenceData,
+        on: {
+          itemId: Sequelize.where(
+            Sequelize.col('invoiceLineType->procedureType.id'),
+            '=',
+            Sequelize.col('invoiceLineType.item_id'),
+          ),
+          itemType: Sequelize.where(
+            Sequelize.col('invoiceLineType->procedureType.type'),
+            '=',
+            'procedureType',
+          ),
+        },
+        as: 'procedureType',
+      },
+      {
+        model: models.ReferenceData,
+        on: {
+          itemId: Sequelize.where(
+            Sequelize.col('invoiceLineType->imagingType.id'),
+            '=',
+            Sequelize.col('invoiceLineType.item_id'),
+          ),
+          itemType: Sequelize.where(
+            Sequelize.col('invoiceLineType->imagingType.type'),
+            '=',
+            'imagingType',
+          ),
+        },
+        as: 'imagingType',
+      },
+      {
+        model: models.LabTestType,
+        on: {
+          itemId: Sequelize.where(
+            Sequelize.col('invoiceLineType->labTestType.id'),
+            '=',
+            Sequelize.col('invoiceLineType.item_id'),
+          ),
+          itemType: Sequelize.where(Sequelize.col('invoiceLineType.item_type'), '=', 'labTestType'),
+        },
+        as: 'labTestType',
+      },
+    ];
   }
 }
