4cd34f3a21998504c2a784008380e830da0ba958
Alastair-L <lockealastair@gmail.com>
2022-01-12 16:14:15 +1100

    First attempt at rewrite

--- packages/shared-src/src/reports/fiji-statistical-report-for-phis-summary.js
+++ packages/shared-src/src/reports/fiji-statistical-report-for-phis-summary.js
@@ -1,99 +1,102 @@
 import { generateReportFromQueryData } from './utilities';
-
 const query = `
-select *, case when "Date1" is null then "Date2" else "Date1" end as date from (
-  SELECT 
-    sr.end_time::date as "Date1",
-    count(distinct(patient.id)) as patients_screened, -- testing
-    count(*) as number_of_cvd_screenings,
-    count(case when snap_counselling_table.snap_counselling = 'Yes' then 1 end) as received_snap_counselling,
-    count(case when additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as screened_itaukei,
-    count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as received_snap_counselling_itaukei,
-    count(case when additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as screened_fid,
-    count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as received_snap_counselling_fid,
-    count(case when additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as screened_others,
-    count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as received_snap_counselling_itaukei_others
-  FROM patients AS patient
-  join (
-      SELECT
-          e.patient_id, sr4.end_time::date as date_group, max(sr4.end_time) AS max_end_time , count(*) as count_for_testing 
-       FROM
-          survey_responses sr4
-      join encounters e on e.id = sr4.encounter_id 
-        GROUP by e.patient_id, sr4.end_time::date
-    ) max_time_per_group_table
-  on max_time_per_group_table.patient_id = patient.id
-  inner JOIN survey_responses AS sr 
-    ON sr.end_time = max_time_per_group_table.max_end_time
-  inner join encounters sr_encounter
-  on sr_encounter.id = sr.encounter_id and sr_encounter.patient_id = patient.id
-  left join patient_additional_data as additional_data on 
-    additional_data.id = (
-      select id from patient_additional_data 
-      where patient_id = patient.id
-      limit 1
-    )
-  left join (
-    select response_id, body as snap_counselling from survey_response_answers sra
-    where sra.data_element_id in ('pde-FijCVD038', 'pde-FijSNAP13')
-  ) snap_counselling_table on sr.id = snap_counselling_table.response_id
-  group by "Date1" order by "Date1" desc) table1
-  full outer join (
-    SELECT 
-      e.start_date::date as "Date2",
-      count(distinct(p.id)) as unique_patients_with_encounters,
+
+select 
+case when diagnosis_date is null then 
+case when sr_date is null then snap_date else sr_date end
+  else diagnosis_date end as date,
+      count(distinct(patient.id)) as unique_patients_with_encounters,
       count(*) as encounters_that_day,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date then 1 end) as diabetes_u30,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date then 1 end) as diabetes_o30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') > current_date then 1 end) as hypertension_u30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') <= current_date then 1 end) as hypertension_o30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date then 1 end) as dual_u30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date then 1 end) as dual_o30,
-      'buffer' as buffer,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_diabetes_u30,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_diabetes_o30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_hypertension_u30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_hypertension_o30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_dual_u30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_dual_o30,
-      'buffer' as buffer,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_diabetes_u30,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_diabetes_o30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_hypertension_u30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_hypertension_o30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_dual_u30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_dual_o30,
-      'buffer' as buffer,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_diabetes_u30,
-      count(case when diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_diabetes_o30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_hypertension_u30,
-      count(case when hypertension_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_hypertension_o30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_dual_u30,
-      count(case when hypertension_temp.id is not null and diabetes_temp.id is not null and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_dual_o30
-    FROM patients AS p
-    left join patient_additional_data as additional_data on 
-    additional_data.id = (
-      select id from patient_additional_data 
-      where patient_id = p.id
-      limit 1
+      count(case when diabetes_diagnosed then 1 end) as diabetes_u30,
+      count(case when snap_counselling_table.snap_counselling = 'Yes' then 1 end) as received_snap_counselling,
+      count(case when snap_counselling_table.snap_counselling = 'Yes' then 1 end) as received_snap_counselling,
+count(case when additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as screened_itaukei,
+count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as received_snap_counselling_itaukei,
+count(case when additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as screened_fid,
+count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as received_snap_counselling_fid,
+count(case when additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as screened_others,
+count(case when snap_counselling_table.snap_counselling = 'Yes' and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as received_snap_counselling_itaukei_others,
+count(*) as encounters_that_day,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date then 1 end) as diabetes_u30,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date then 1 end) as diabetes_o30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') > current_date then 1 end) as hypertension_u30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') <= current_date then 1 end) as hypertension_o30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date then 1 end) as dual_u30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date then 1 end) as dual_o30,
+'buffer' as buffer,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_diabetes_u30,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_diabetes_o30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_hypertension_u30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_hypertension_o30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_dual_u30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-ITaukei' then 1 end) as itaukei_dual_o30,
+'buffer' as buffer,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_diabetes_u30,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_diabetes_o30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_hypertension_u30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_hypertension_o30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_dual_u30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-FID' then 1 end) as fid_dual_o30,
+'buffer' as buffer,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_diabetes_u30,
+count(case when diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_diabetes_o30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_hypertension_u30,
+count(case when hypertension_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_hypertension_o30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') > current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_dual_u30,
+count(case when hypertension_diagnosed and diabetes_diagnosed and (date_of_birth + interval '30 year') <= current_date and additional_data.ethnicity_id = 'ethnicity-others' then 1 end) as others_dual_o30
+from patients AS patient
+  	join patient_additional_data as additional_data on 
+    	additional_data.id = (
+     	 select id from patient_additional_data 
+     	 where patient_id = patient.id
+     	 limit 1
     )
-    join encounters e on e.patient_id = p.id
+	left join (
+		select *, sr_encounter.patient_id as pid, sr.end_time::date as sr_date from (
+	      SELECT
+	          e.patient_id, sr4.end_time::date as date_group, max(sr4.end_time) AS max_end_time , count(*) as count_for_testing 
+	       FROM
+	          survey_responses sr4
+	      join encounters e on e.id = sr4.encounter_id 
+	        GROUP by e.patient_id, sr4.end_time::date
+	    ) max_time_per_group_table
+	  inner JOIN survey_responses AS sr 
+	    ON sr.end_time = max_time_per_group_table.max_end_time
+	  inner join encounters sr_encounter
+	  on sr_encounter.id = sr.encounter_id and sr_encounter.patient_id = max_time_per_group_table.patient_id
+  ) cvd_response_table
+  on cvd_response_table.pid = patient.id
+  full outer join (
+    select 
+    	*,
+    	diagnosis_encounter.start_date::date as diagnosis_date,
+    	diabetes_temp.id is not null as diabetes_diagnosed,
+    	hypertension_temp.id is not null as hypertension_diagnosed
+    from encounters diagnosis_encounter
     left join (
       select encounter_id, ed.id from encounter_diagnoses ed
       join reference_data rd 
       on rd."type" = 'icd10' and rd.id = ed.diagnosis_id 
       WHERE rd.code IN ('icd10-E11')
-    ) diabetes_temp on e.id = diabetes_temp.encounter_id
+    ) diabetes_temp on diagnosis_encounter.id = diabetes_temp.encounter_id
     left join (
       select encounter_id, ed.id from encounter_diagnoses ed
       join reference_data rd 
       on rd."type" = 'icd10' and rd.id = ed.diagnosis_id 
-      WHERE rd.code IN ('icd10-I10')
-    ) hypertension_temp on e.id = hypertension_temp.encounter_id
-    group by "Date2" 
-    having count(diabetes_temp.id) > 0 or count(hypertension_temp.id) > 0
-  ) table2
-  on table1."Date1" = table2."Date2";
+      WHERE rd.code like ('icd10-I10')
+    ) hypertension_temp on diagnosis_encounter.id = hypertension_temp.encounter_id
+  ) diagnosis_table
+  on diagnosis_table.patient_id = patient.id
+  full outer join (
+  	select patient_id, response_id, body as snap_counselling, snap_response.end_time::date as snap_date from survey_response_answers sra
+  	join survey_responses snap_response
+  	on snap_response.id = sra.response_id 
+  	join encounters snap_encounter
+  	on snap_encounter.id = snap_response.encounter_id 
+    where sra.data_element_id in ('pde-FijCVD038', 'pde-FijSNAP13')
+  ) snap_counselling_table
+  on snap_counselling_table.patient_id = patient.id
+  group by date;
   `;
 
 const FIELD_TO_TITLE = {
