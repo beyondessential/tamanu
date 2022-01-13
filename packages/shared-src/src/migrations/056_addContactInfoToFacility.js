4a0aabdda0d006005ac41c6713b797ab28d816fa
Tom McLean <tom.a.mclean@gmail.com>
2022-01-13 16:48:12 +1100

    Update facility model and add migration

---
+++ packages/shared-src/src/migrations/056_addContactInfoToFacility.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+module.exports = {
+  up: async query => {
+    const nullableString = {
+      type: Sequelize.STRING,
+      allowNull: true,
+    };
+    await query.addColumn('facility', 'email', nullableString);
+    await query.addColumn('facility', 'contact_number', nullableString);
+    await query.addColumn('facility', 'street_address', nullableString);
+  },
+  down: async query => {
+    await query.removeColumn('facility', 'email');
+    await query.removeColumn('facility', 'contact_number');
+    await query.removeColumn('facility', 'street_address');
+  },
+};
