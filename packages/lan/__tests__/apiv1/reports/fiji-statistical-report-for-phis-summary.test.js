2d9b08b248eeaaeda5572829aaf7d3c914b16c32
Alastair-L <lockealastair@gmail.com>
2022-01-17 15:57:28 +1100

    Test for dual diagnosis from separate encounters

--- packages/lan/__tests__/apiv1/reports/fiji-statistical-report-for-phis-summary.test.js
+++ packages/lan/__tests__/apiv1/reports/fiji-statistical-report-for-phis-summary.test.js
@@ -160,7 +160,7 @@ describe('Covid swab lab test list', () => {
      * Patient 3 - Under 30, ethnicity: ITAUKEI
      *
      * 2020-05-02: Diagnosed with hypertension
-     * 2020-05-02: Diagnosed with diabetes (separate encounter) - KNOWN ISSUE
+     * 2020-05-02: Diagnosed with diabetes (separate encounter)
      * 2020-05-02: Had a CVD screening - SNAP councilling
      *
      * 2020-05-03: Diagnosed with hypertension
@@ -263,7 +263,6 @@ describe('Covid swab lab test list', () => {
      *
      **/
 
-    /*
     // 2020-05-02: Diagnosed with hypertension
     const diagnosisEncounter3 = await models.Encounter.create(
       await createDummyEncounter(models, {
@@ -294,7 +293,6 @@ describe('Covid swab lab test list', () => {
         date: moment.utc('2020-05-02'),
       }),
     );
-    */
 
     // 2020-05-02: Had a CVD screening - yes SNAP councilling
     await createCVDFormSurveyResponse(app, expectedPatient3, moment.utc('2020-05-02'), {
@@ -395,7 +393,7 @@ describe('Covid swab lab test list', () => {
         o30_diabetes: 1,
         u30_hypertension: 0,
         o30_hypertension: 0,
-        u30_dual: 1,
+        u30_dual: 2,
         o30_dual: 0,
         itaukei_cvd_responses: 1,
         itaukei_snaps: 1,
@@ -403,7 +401,7 @@ describe('Covid swab lab test list', () => {
         itaukei_o30_diabetes: 0,
         itaukei_u30_hypertension: 0,
         itaukei_o30_hypertension: 0,
-        itaukei_u30_dual: 0,
+        itaukei_u30_dual: 1,
         itaukei_o30_dual: 0,
         fid_cvd_responses: 0,
         fid_snaps: 0,
