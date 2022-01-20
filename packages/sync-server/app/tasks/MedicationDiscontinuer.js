e2d338a88d74120f9d7ef18021a9125875e81b9c
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-01-19 23:28:04 -0500

    Create task for auto discontinuing medications on sync-server (based on OutpatientDischarger)

---
+++ packages/sync-server/app/tasks/MedicationDiscontinuer.js
@@ -0,0 +1,77 @@
+import config from 'config';
+import moment from 'moment';
+import { Op } from 'sequelize';
+
+import { ScheduledTask } from 'shared/tasks';
+import { log } from 'shared/services/logging';
+import { sleepAsync } from 'shared/utils';
+
+export class MedicationDiscontinuer extends ScheduledTask {
+  getName() {
+    return 'MedicationDiscontinuer';
+  }
+
+  constructor(context) {
+    super(config.schedules.medicationDiscontinuer.schedule, log);
+    this.models = context.store.models;
+
+    // Run once on startup (in case the server was down when it was scheduled)
+    this.run();
+  }
+
+  async run() {
+    // Get end of day
+    const endOfToday = moment()
+      .endOf('day')
+      .toDate();
+
+    // Medications that are not discontinued and have an
+    // end date less or equal than end of today
+    const where = {
+      discontinued: false,
+      endDate: {
+        [Op.lte]: endOfToday,
+      },
+    };
+
+    // Count all found medications
+    const discontinuableMedicationsCount = await this.models.EncounterMedication.count({ where });
+
+    // Early bail
+    if (discontinuableMedicationsCount === 0) {
+      log.info('No medications to auto-discontinue. Stopping MedicationDiscontinuer...');
+      return;
+    }
+
+    // Process in batches
+    const {
+      batchSize,
+      batchSleepAsyncDurationInMilliseconds,
+    } = config.schedules.outpatientDischarger;
+    const batchCount = Math.ceil(discontinuableMedicationsCount / batchSize);
+
+    log.info(
+      `Auto-discontinuing ${discontinuableMedicationsCount} encounter medications in ${batchCount} batches (${batchSize} records per batch)`,
+    );
+
+    // Discontinue medications
+    for (let i = 0; i < batchCount; i++) {
+      const discontinuableMedications = await this.models.EncounterMedication.findAll({
+        where,
+        limit: batchSize,
+      });
+
+      for (const medication of discontinuableMedications) {
+        await medication.update({
+          discontinued: true,
+          discontinuingReason: 'Finished treatment',
+        });
+        log.info(`Auto-discontinued encounter medication with id ${medication.id}`);
+      }
+
+      await sleepAsync(batchSleepAsyncDurationInMilliseconds);
+    }
+
+    log.info('MedicationDiscontinuer finished running');
+  }
+}
