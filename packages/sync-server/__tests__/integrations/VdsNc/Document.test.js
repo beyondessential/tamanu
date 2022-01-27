72d782c8b68f74f57efa7d7934596b4c5da41aa2
Félix Saparelli <felix@passcod.name>
2022-01-27 18:01:02 +1300

    Ditch vds-sdk library and its internal webcrypto

--- packages/sync-server/__tests__/integrations/VdsNc/Document.test.js
+++ packages/sync-server/__tests__/integrations/VdsNc/Document.test.js
@@ -1,5 +1,5 @@
 import { createTestContext } from 'sync-server/__tests__/utilities';
-import { newKeypairAndCsr, TestCSCA, loadCertificateIntoSigner } from 'sync-server/app/utils/vdsCrypto';
+import { newKeypairAndCsr, TestCSCA, loadCertificateIntoSigner, pem } from 'sync-server/app/utils/vdsCrypto';
 import { ICAO_DOCUMENT_TYPES } from 'shared/constants';
 import { expect } from 'chai';
 
@@ -7,7 +7,13 @@ describe('VDS-NC: Document cryptography', () => {
   let ctx;
   beforeAll(async () => {
     ctx = await createTestContext();
+  });
+  afterAll(() => ctx.close());
 
+  it('can sign documents', async () => {
+    const { VdsNcDocument, VdsNcSigner } = ctx.store.models;
+
+    // Arrange
     const testCSCA = await TestCSCA.generate();
 
     const {
@@ -22,23 +28,15 @@ describe('VDS-NC: Document cryptography', () => {
       },
     });
 
-    const { VdsNcSigner } = ctx.store.models;
     const signer = await VdsNcSigner.create({
-      publicKey,
-      privateKey,
+      publicKey: Buffer.from(publicKey),
+      privateKey: Buffer.from(privateKey),
       request,
       countryCode: 'UTO',
     });
     const signerCert = await testCSCA.signCSR(request);
-    await signer.update(await loadCertificateIntoSigner(signerCert));
-  });
-  afterAll(() => ctx.close());
-
-  it('can sign documents', async () => {
-    // Arrange
-    const { VdsNcDocument, VdsNcSigner } = ctx.store.models;
-    const signer = await VdsNcSigner.findActive();
-    if (!signer) throw new Error('no active signer');
+    const signedCert = await loadCertificateIntoSigner(signerCert);
+    await signer.update(signedCert);
 
     const document = await VdsNcDocument.create({
       type: ICAO_DOCUMENT_TYPES.PROOF_OF_TESTING.JSON,
@@ -46,6 +44,7 @@ describe('VDS-NC: Document cryptography', () => {
     });
 
     // Pre-check
+    expect(signer.isActive()).to.be.true;
     expect(document.isSigned()).to.be.false;
     const signCount = signer.signaturesIssued;
 
@@ -55,7 +54,7 @@ describe('VDS-NC: Document cryptography', () => {
     // Assert
     expect(document.isSigned()).to.be.true;
     expect(document.algorithm).to.equal('SHA-256');
-    await signer.refresh();
+    await signer.reload();
     expect(signer.signaturesIssued).to.equal(signCount + 1);
   });
 });
