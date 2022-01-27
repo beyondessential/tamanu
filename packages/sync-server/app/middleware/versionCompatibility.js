2badde4fbae6188b980dc392fe374a2d156ee23b
Alastair-L <lockealastair@gmail.com>
2022-01-27 17:33:50 +1100

    Also bump versionCompatibility

--- packages/sync-server/app/middleware/versionCompatibility.js
+++ packages/sync-server/app/middleware/versionCompatibility.js
@@ -6,11 +6,11 @@ import { buildVersionCompatibilityCheck } from 'shared/utils';
 // not supported.
 export const SUPPORTED_CLIENT_VERSIONS = {
   'Tamanu LAN Server': {
-    min: '1.12.0',
-    max: '1.12.0', // note that higher patch versions will be allowed to connect
+    min: '1.12.1',
+    max: '1.12.1', // note that higher patch versions will be allowed to connect
   },
   'Tamanu Mobile': {
-    min: '1.12.0',
+    min: '1.12.1',
     max: '1.12.99', // note that higher patch versions will be allowed to connect
   },
   'Fiji VPS': {
