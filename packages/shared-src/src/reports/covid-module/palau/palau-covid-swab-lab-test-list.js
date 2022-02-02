3bd2a8ee1d081f7ba00a8f903c0a40c3a726e58f
Alastair-L <lockealastair@gmail.com>
2022-02-02 11:37:52 +1100

    Update columns of Palau report

--- packages/shared-src/src/reports/covid-module/palau/palau-covid-swab-lab-test-list.js
+++ packages/shared-src/src/reports/covid-module/palau/palau-covid-swab-lab-test-list.js
@@ -3,43 +3,17 @@ import { baseDataGenerator } from '../covid-swab-lab-test-list';
 const SURVEY_ID = 'program-palaucovid19-palaucovidtestregistrationform';
 
 const SURVEY_QUESTION_CODES = {
-  Consent: 'pde-PalauCOVSamp2',
-  'First name of guardian': 'pde-PalauCOVSamp3',
-  'Last name of guardian': 'pde-PalauCOVSamp4',
   'Contact number': 'pde-PalauCOVSamp6',
-  Citizenship: 'pde-PalauCOVSamp7',
+  Nationality: 'pde-PalauCOVSamp7',
   Ethnicity: 'pde-PalauCOVSamp43',
-  Race: 'pde-PalauCOVSamp44',
-  'State for the last 14 days': 'pde-PalauCOVSamp8',
-  'Test conducted at BNH': 'pde-PalauCOVSamp10',
-  'If test not conducted at BNH, which facility?': 'pde-PalauCOVSamp11',
+  'Where was the test conducted': 'pde-PalauCOVSamp46',
   'Purpose of sample collection': 'pde-PalauCOVSamp12',
   'Other purpose': 'pde-PalauCOVSamp13',
-  'Printed result': 'pde-PalauCOVSamp14',
-  'Individual paid': 'pde-PalauCOVSamp15',
   'Passport number': 'pde-PalauCOVSamp16',
   'Final destination': 'pde-PalauCOVSamp17',
   'Transit country': 'pde-PalauCOVSamp18',
   Airline: 'pde-PalauCOVSamp19',
-  'Medical problems': 'pde-PalauCOVSamp21',
-  'Other medical problems': 'pde-PalauCOVSamp22',
-  Pregnant: 'pde-PalauCOVSamp23',
-  'Link to cluster/case': 'pde-PalauCOVSamp24',
-  'Name cluster/case': 'pde-PalauCOVSamp25',
-  Occupation: 'pde-PalauCOVSamp27',
-  'MHHS department': 'pde-PalauCOVSamp28',
-  'MHHS facility': 'pde-PalauCOVSamp29',
-  'Other employer': 'pde-PalauCOVSamp30',
-  School: 'pde-PalauCOVSamp31',
-  'Experiencing symptoms': 'pde-PalauCOVSamp34',
-  'Date of first symptom': 'pde-PalauCOVSamp35',
-  Symptoms: 'pde-PalauCOVSamp36',
-  'Other symptoms': 'pde-PalauCOVSamp37',
-  Vaccinated: 'pde-PalauCOVSamp38',
-  '1st dose': 'pde-PalauCOVSamp39',
-  '2nd dose': 'pde-PalauCOVSamp40',
-  'Booster dose': 'pde-PalauCOVSamp41',
-  'Date of last dose': 'pde-PalauCOVSamp42',
+  'Testing cost': 'pde-PalauCOVSamp19a',
 };
 
 const reportColumnTemplate = [
@@ -58,7 +32,8 @@ const reportColumnTemplate = [
   { title: 'Sex', accessor: data => data.sex },
   { title: 'Patient ID', accessor: data => data.patientId },
   { title: 'Hamlet', accessor: data => data.homeSubDivision },
-
+  { title: 'Nationality', accessor: data => data.Nationality }, // TODO
+  { title: 'Ethnicity', accessor: data => data.Ethnicity }, // TODO
   { title: 'Lab request ID', accessor: data => data.labRequestId },
   {
     title: 'Lab request type',
