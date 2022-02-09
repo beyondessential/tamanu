2ac2a4b9d81e1505de85320407db3eda130cbb87
Tri Nguyen <tri@tridnguyen.com>
2022-02-09 01:32:57 -0500

    Filter down survey link results to the same survey

--- packages/desktop/app/components/Field/SurveySelect.js
+++ packages/desktop/app/components/Field/SurveySelect.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import { format, compareDesc } from 'date-fns';
 import { SelectInput } from './SelectField';
 import { useApi } from '../../api';
 
-export const SurveySelect = ({ field, patient, options: _, ...props }) => {
+export const SurveySelect = ({ field, patient, options: _, config, ...props }) => {
   const api = useApi();
   const [options, setOptions] = useState([]);
+  const { source } = config;
+
   useEffect(() => {
-    api
-      .get(`/patient/${patient.id}/programResponses?page=0&rowsPerPage=10&order=desc&orderBy=date`)
-      .then(resultData => {
-        setOptions(
-          resultData.data.map(({ id, createdAt, surveyName }) => ({
+    api.get(`/patient/${patient.id}/programResponses?page=0&rowsPerPage=10`).then(resultData => {
+      setOptions(
+        resultData.data
+          .filter(({ surveyId }) => surveyId === source.toLowerCase())
+          .sort((a, b) => compareDesc(new Date(a.endTime), new Date(b.endTime)))
+          .map(({ id, endTime, surveyName }) => ({
             value: id,
-            label: `${moment(createdAt).format('DD/MM/YYYY')} ${surveyName}`,
+            label: `${format(new Date(endTime), 'dd/MM/yyyy')} ${surveyName}`,
           })),
-        );
-      });
-  }, [api, patient.id]);
+      );
+    });
+  }, [api, patient.id, source]);
   return (
     <SelectInput
       {...props}
