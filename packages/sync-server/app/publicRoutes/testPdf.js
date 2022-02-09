17fab384ee0b2b0a31d3ffd3179c17397c9d497f
Tom Caiger <caigertom@gmail.com>
2022-02-10 11:08:30 +1300

    WAITM-36: Generate Patient Certificates on Sync Server (#1711)
    * set up pdf generation
    
    * get patient
    
    * wip add react-pdf
    
    * move pdf templates to sync server
    
    * remove roboto
    
    * connect patient data
    
    * update table
    
    * remove trial code
    
    * reset dependencies
    
    * test fonts
    
    * wip covid cert
    
    * add getter to Patient model
    
    * add certificates dir
    
    * add fonts
    
    * clean up debug code
    
    * Update certificate.test.js
    
    * return file path
    
    * integrate images
    
    * test qr code
    
    * move templates to shared
    
    * move certificate template to shared
    
    * remove roboto
    
    * rename certificate to patient certificate
    
    * clean up assets
    
    * re organise dependencies
    
    * put back missing file
    
    * integrate with email code
    
    * Update patientCertificate.test.js
    
    * code review suggestions

---
+++ packages/sync-server/app/publicRoutes/testPdf.js
@@ -0,0 +1,20 @@
+// Todo: Remove before merging vds-epic
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { makePatientCertificate } from 'shared/utils';
+
+export const testPdf = express.Router();
+
+testPdf.get(
+  '/:patientId',
+  asyncHandler(async (req, res) => {
+    const { params } = req;
+    const { patientId } = params;
+    const { models } = req.store;
+
+    const patient = await models.Patient.findByPk(patientId);
+
+    await makePatientCertificate(patient, models);
+    res.send({});
+  }),
+);
