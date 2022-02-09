8f41952cc0676255840f1e1b25a395a7e173d010
Félix Saparelli <felix@beyondessential.com.au>
2022-02-09 15:11:56 +1300

    Discharge any active encounters on death

--- packages/lan/app/routes/apiv1/patient/patientDeath.js
+++ packages/lan/app/routes/apiv1/patient/patientDeath.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { InvalidOperationError, NotFoundError } from 'shared/errors';
+import { User } from 'shared/models/User';
 import * as yup from 'yup';
 
 export const patientDeath = express.Router();
@@ -11,7 +12,7 @@ patientDeath.post(
     req.checkPermission('write', 'Patient');
 
     const {
-      models: { Patient },
+      models: { Discharge, Encounter, Patient },
       params: { id: patientId },
     } = req;
 
@@ -19,12 +20,33 @@ patientDeath.post(
       date: yup.date().required(),
     });
     const body = await schema.validate(req.body);
-
+    
     const patient = await Patient.findByPk(patientId);
     if (!patient) throw new NotFoundError('Patient not found');
     if (patient.dateOfDeath) throw new InvalidOperationError('Patient is already deceased');
+    
+    const physician = await User.findByPk(body.physician.id);
+    if (!physician) throw new NotFoundError('Discharge physician not found');
+    // TODO: check role?
 
-    await patient.update({ dateOfDeath: body.date });
+    await db.transaction(async () => {
+      await patient.update({ dateOfDeath: body.date });
+      
+      const activeEncounters = await patient.getEncounters({
+        where: {
+          endDate: null,
+        },
+      });
+      for (const encounter of activeEncounters) {
+        await Discharge.create({
+          encounterId: encounter.id,
+          dischargerId: physician.id,
+        });
+        await encounter.update({
+          endDate: body.date,
+        });
+      }
+    });
 
     res.send({
       data: {},
