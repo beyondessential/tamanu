a54564796fd3d5b2f56ec2b1c828084b67355d84
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-02-09 21:47:58 -0500

    Add proper identation and a comment to helper function

--- packages/sync-server/app/utils/removeSensitiveAnswers.js
+++ packages/sync-server/app/utils/removeSensitiveAnswers.js
@@ -10,25 +10,26 @@ const filterFromSurveyResponseAnswer = (records, responseIds) => {
 };
 
 const filterFromEncounter = (records, responseIds) => {
-return records.map(record => ({
-  ...record,
-  data: {
-    ...record.data,
-    surveyResponses: record.data.surveyResponses.map(response => {
-      // Response should not contain answers
-      if (responseIds.includes(response.data.id)) {
-        // Overwrite only response.data.answers
-        return {
-          ...response,
-          data: { ...response.data, answers: [] },
-        };
-      }
+  return records.map(record => ({
+    ...record,
+    data: {
+      ...record.data,
+      // Only overwrite record.data.surveyResponses
+      surveyResponses: record.data.surveyResponses.map(response => {
+        // Response should not contain answers
+        if (responseIds.includes(response.data.id)) {
+          // Overwrite only response.data.answers
+          return {
+            ...response,
+            data: { ...response.data, answers: [] },
+          };
+        }
 
-      // Don't filter response
-      return response;
-    }),
-  },
-}));
+        // Don't filter response
+        return response;
+      }),
+    },
+  }));
 };
 
 // This will return a new array after filtering all SurveyResponseAnswers
