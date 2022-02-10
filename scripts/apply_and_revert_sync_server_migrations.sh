cb6287ca24d36ab3e9c01976a3da226174e153f1
Tim Kennedy <t.m.knndy@gmail.com>
2022-02-10 17:24:33 +1100

    In CI, reverse most migrations (except very old ones)

--- scripts/apply_and_revert_sync_server_migrations.sh
+++ scripts/apply_and_revert_sync_server_migrations.sh
@@ -3,3 +3,4 @@ set -euxo pipefail
 
 yarn workspace sync-server run build
 yarn workspace sync-server run start migrate --up
+yarn workspace sync-server run start migrate --downToLastReversibleMigration
