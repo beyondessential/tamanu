69b21ff7beb08e2246e49b781a92c8d4c75a0b1a
Tin Nguyen <tindn14@gmail.com>
2022-02-15 22:29:42 -0500

    fix LogBox error

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -1,10 +1,9 @@
 import { AppRegistry, LogBox } from 'react-native';
 import { name as appName } from './app.json';
-import { env } from './env';
 import { Root } from './Root';
 import 'react-native-gesture-handler';
 
-LogBox.ignoreWarnings([
+LogBox.ignoreLogs([
   'to contain units',
   'Setting a timer', // our usage of timers is appropriate, see #53
 ]);
