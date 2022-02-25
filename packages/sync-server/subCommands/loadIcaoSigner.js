1b4afed351b0a47700ff8e40eade60073edfede8
Félix Saparelli <felix@beyondessential.com.au>
2022-02-25 17:27:39 +1300

    WAITM-102: Add CSCA tool for Nauru (#1822)
    Includes some critical fixes and some minor changes to the VDS workflow:
    - Fix model/db/task for Signer so it works and a first signer can be configured
    - Config hint on renewal sender schedule
    
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

---
+++ packages/sync-server/subCommands/loadIcaoSigner.js
@@ -0,0 +1,34 @@
+import { promises as fs } from 'fs';
+import { log } from 'shared/services/logging';
+import { VdsNcSigner } from 'shared/models';
+import { loadCertificateIntoSigner } from '../app/integrations/VdsNc';
+import { Op } from 'sequelize';
+import { initDatabase } from '../app/database';
+import moment from 'moment';
+
+export async function loadIcaoSigner(options) {
+  await initDatabase({ testMode: false });
+  const signerFile = await fs.readFile(options.icaoSigner, 'utf8');
+  const signerData = await loadCertificateIntoSigner(signerFile);
+
+  const pending = await VdsNcSigner.findAll({
+    where: {
+      certificate: { [Op.is]: null },
+      privateKey: { [Op.not]: null },
+    },
+  });
+
+  if (pending.length === 0) {
+    throw new Error('No pending signer, did you do this already?');
+  }
+
+  if (pending.length > 1) {
+    throw new Error('More than one pending signer, you need to fix this manually');
+  }
+
+  const pendingSigner = pending[0];
+  await pendingSigner.update(signerData);
+  log.info(`Loaded ICAO Signer (${moment(signerData.notBefore).format('YYYY-MM-DD')} - ${moment(signerData.notAfter).format('YYYY-MM-DD')})`);
+
+  process.exit(0);
+}
