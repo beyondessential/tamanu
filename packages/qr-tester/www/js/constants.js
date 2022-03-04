d48784b028ff783b891cfe53e0f50fda9088d585
Félix Saparelli <felix@beyondessential.com.au>
2022-03-04 14:59:55 +1300

    Remove obsolete parts of QR checker

--- packages/qr-tester/www/js/constants.js
+++
@@ -1,6 +0,0 @@
-export const CANVAS_SIZE = {
-  WIDTH: window.innerWidth,
-  HEIGHT: window.innerHeight
-};
-
-export const WORKER_DATA_RATE = 600;
--- packages/qr-tester/www/js/worker.js
+++
@@ -1,8 +0,0 @@
-export function initWorker(cb) {
-  const worker = new Worker("/js/wasmWorker.js");
-  worker.onmessage = ({ data: { data, ms } }) => {
-    worker.terminate();
-    cb(data, ms);
-  };
-  return worker;
-}
