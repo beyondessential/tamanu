e862494172dd39111687402f1b28e5476e7280e0
tindn <hi@tindn.io>
2022-03-08 23:14:06 -0500

    TAN-1260 refactor PatientSectionList to use ScrollView instead of 3rd party large list

--- packages/mobile/App/ui/components/PatientSectionList/index.tsx
+++ packages/mobile/App/ui/components/PatientSectionList/index.tsx
@@ -1,148 +1,79 @@
-import React, { useRef, useMemo, RefObject, useCallback } from 'react';
-import { StyleSheet, TouchableWithoutFeedback } from 'react-native';
-import { LargeList } from 'react-native-largelist-v3';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { PatientTile } from '../PatientTile';
-import { StyledView, StyledText } from '/styled/common';
-import { theme } from '/styled/theme';
-import { screenPercentageToDP, Orientation } from '/helpers/screen';
+import React, { RefObject, useMemo, useRef } from 'react';
+import { ScrollView, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import { IPatient } from '~/types';
+import { PatientTile } from '../PatientTile';
 import { groupEntriesByLetter } from '/helpers/list';
-
-export type PatientSectionListItem = {
-  items: IPatient[];
-  header: string;
-};
+import { Orientation, screenPercentageToDP } from '/helpers/screen';
+import { StyledText, StyledView } from '/styled/common';
+import { theme } from '/styled/theme';
 
 interface PatientSectionListProps {
   onPressItem: (patient: IPatient) => void;
-  patients: PatientSectionListItem[];
+  patients: IPatient[];
 }
 
-const ListSeparator = (): JSX.Element => (
-  <StyledView
-    height={StyleSheet.hairlineWidth}
-    background={theme.colors.DEFAULT_OFF}
-    width="90%"
-  />
-);
-
-export const PatientSectionList = ({
-  patients,
-  onPressItem = (): null => null,
-}: PatientSectionListProps): JSX.Element => {
-  const ref: RefObject<LargeList> = useRef(null);
-
-  const groupedPatients = useMemo(
-    () => groupEntriesByLetter(patients),
-    [patients],
+const SECTION_HEADER_HEIGHT = 30;
+function SectionHeader(props: { header: string }) {
+  return (
+    <StyledView
+      height={SECTION_HEADER_HEIGHT}
+      justifyContent="center"
+      background={theme.colors.BOX_OUTLINE}
+      paddingLeft={screenPercentageToDP('4.86', Orientation.Width)}
+    >
+      <StyledText fontSize={screenPercentageToDP('1.45', Orientation.Height)}>
+        {props.header}
+      </StyledText>
+    </StyledView>
   );
+}
 
-  const scrollToSection = useCallback(
-    (header: string) => {
-      const headerIndex = groupedPatients.findIndex(
-        (entry: PatientSectionListItem) => entry.header === header,
-      );
-      if (headerIndex !== -1) {
-        if (ref.current) {
-          ref.current.scrollToIndexPath({
-            section: headerIndex,
-            row: 0,
-          });
-        }
-      }
-    },
-    [groupedPatients],
+const ITEM_HEIGHT = 85;
+function Item(props: { item: IPatient; onPressItem: (patient: IPatient) => void }) {
+  return (
+    <TouchableOpacity
+      style={{
+        height: ITEM_HEIGHT,
+        alignContent: 'center',
+        justifyContent: 'center',
+        backgroundColor: theme.colors.BACKGROUND_GREY,
+      }}
+      onPress={() => props.onPressItem(props.item)}
+    >
+      <PatientTile {...props.item} />
+      <ItemSeparator />
+    </TouchableOpacity>
   );
+}
 
-  const renderHeader = useCallback(
-    (section: number) => (
-      <StyledView
-        height={screenPercentageToDP(3, Orientation.Height)}
-        justifyContent="center"
-        background={theme.colors.BOX_OUTLINE}
-        paddingLeft={screenPercentageToDP('4.86', Orientation.Width)}
-      >
-        <StyledText fontSize={screenPercentageToDP('1.45', Orientation.Height)}>
-          {groupedPatients[section].header}
-        </StyledText>
-      </StyledView>
-    ),
-    [groupedPatients],
+function ItemSeparator() {
+  return (
+    <StyledView
+      height={StyleSheet.hairlineWidth}
+      background={theme.colors.DEFAULT_OFF}
+      width="90%"
+    />
   );
+}
 
-  const renderItem = React.useCallback(
-    ({ section, row }) => {
-      const patient = groupedPatients[section].items[row];
-      const onPress = (): void => onPressItem(patient);
-      return (
-        <TouchableOpacity onPress={onPress}>
-          <StyledView
-            height={85}
-            alignItems="center"
-            justifyContent="center"
-            background={theme.colors.BACKGROUND_GREY}
-          >
-            <PatientTile
-              {...patient}
-            />
-            <ListSeparator />
-          </StyledView>
-        </TouchableOpacity>
-      );
-    },
-    [groupedPatients],
-  );
-
-  const heightForSection = React.useCallback(
-    () => screenPercentageToDP('3', Orientation.Height),
-    [],
-  );
-  const heightForIndexPath = React.useCallback(
-    () => screenPercentageToDP('10.32', Orientation.Height),
-    [],
-  );
-
-  const scrollToLetter = useCallback(
-    header => (): void => {
-      scrollToSection(header);
-    },
-    [scrollToSection],
-  );
-
-  const renderAlphabetLetter = useCallback(
-    (section: PatientSectionListItem) => (
-      <TouchableWithoutFeedback
-        onPress={scrollToLetter(section.header)}
-        key={section.header}
-      >
-        <StyledView
-          height={screenPercentageToDP('3.03', Orientation.Height)}
-          justifyContent="center"
-          alignItems="center"
-        >
-          <StyledText
-            fontSize={screenPercentageToDP('1.33', Orientation.Height)}
-          >
-            {section.header}
-          </StyledText>
-        </StyledView>
-      </TouchableWithoutFeedback>
-    ),
-    [scrollToLetter],
-  );
+export function PatientSectionList(props: PatientSectionListProps) {
+  const scrollViewRef: RefObject<ScrollView> = useRef(null);
+  const groupedPatients = useMemo(() => groupEntriesByLetter(props.patients), [props.patients]);
 
   return (
     <StyledView flex={1} width="100%">
-      <LargeList
-        bounces
-        ref={ref}
-        data={groupedPatients}
-        heightForSection={heightForSection}
-        renderSection={renderHeader}
-        heightForIndexPath={heightForIndexPath}
-        renderIndexPath={renderItem}
-      />
+      <ScrollView ref={scrollViewRef} style={{ flex: 1 }}>
+        {groupedPatients.map(group => {
+          return (
+            <React.Fragment key={group.header}>
+              <SectionHeader header={group.header} />
+              {group.items.map(item => {
+                return <Item key={item.id} onPressItem={props.onPressItem} item={item} />;
+              })}
+            </React.Fragment>
+          );
+        })}
+      </ScrollView>
       <StyledView
         width={25}
         position="absolute"
@@ -151,8 +82,34 @@ export const PatientSectionList = ({
         top="5%"
         background={theme.colors.WHITE}
       >
-        {groupedPatients.map(renderAlphabetLetter)}
+        {groupedPatients.map((section, sectionIndex) => (
+          <TouchableWithoutFeedback
+            onPress={() => {
+              if (scrollViewRef.current) {
+                let offset = 0;
+                for (var i = 0; i < sectionIndex; i++) {
+                  offset = offset + SECTION_HEADER_HEIGHT;
+                  offset = offset + groupedPatients[i].items.length * ITEM_HEIGHT;
+                }
+                scrollViewRef.current.scrollTo({
+                  y: offset,
+                });
+              }
+            }}
+            key={section.header}
+          >
+            <StyledView
+              height={screenPercentageToDP('3.03', Orientation.Height)}
+              justifyContent="center"
+              alignItems="center"
+            >
+              <StyledText fontSize={screenPercentageToDP('1.33', Orientation.Height)}>
+                {section.header}
+              </StyledText>
+            </StyledView>
+          </TouchableWithoutFeedback>
+        ))}
       </StyledView>
     </StyledView>
   );
-};
+}
