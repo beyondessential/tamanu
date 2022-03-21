88d2c18940641964b7c2c817b813afd42a1b9203
Alastair-L <lockealastair@gmail.com>
2022-03-21 12:44:20 +1100

    Write without reduce

--- packages/shared-src/src/reports/tuvalu-vaccine-list.js
+++ packages/shared-src/src/reports/tuvalu-vaccine-list.js
@@ -37,50 +37,38 @@ const reportColumnTemplate = [
 const SURVEY_ID = 'program-tuvalucovid19-tuvalucovidconsent';
 // const SURVEY_ID = 'program-kiribaticovid19-kiribaticovidtestregistration';
 
+const getAnswersKeyedByDataElementId = (answers = []) => {
+  const keyedAnswers = {};
+
+  answers.forEach(({ dataElementId, body }) => {
+    keyedAnswers[dataElementId] = body;
+  });
+
+  return keyedAnswers;
+};
+
 const addabc = async (models, vaccineData) => {
   const allPatientIds = vaccineData.map(({ patientId }) => patientId);
-  console.log(allPatientIds);
   const where = {
     '$surveyResponse->encounter.patient_id$': allPatientIds,
     '$surveyResponse.survey_id$': SURVEY_ID,
   };
-  // console.log(where);
   const rawAnswers = await getAnswers(models, where);
-  // console.log(rawAnswers);
   const surveyComponents = await models.SurveyScreenComponent.getComponentsForSurvey(SURVEY_ID);
-  // console.log(surveyComponents);
   const transformedAnswers = await transformAnswers(models, rawAnswers, surveyComponents);
-  // console.log(transformedAnswers);
   const answersByPatient = groupBy(transformedAnswers, 'patientId');
-  console.log(answersByPatient);
 
-  const abcd = answersByPatient.map(allAnswers);
-
-  const func = ([acc, { body, dataElementId }]) => {
-    console.log(body);
-    return {
-      ...acc,
-      [dataElementId]: body,
-    };
-  };
-  console.log(func);
-  console.log(func([{}, { body: 'hi', dataElementId: 'hello' }]));
-
-  const mapper = data => ({
+  const mergeDataWithAnswers = data => ({
     ...data,
-    ...(answersByPatient[data.patientId] || [{ hi: 'asdfads' }])[0],
+    ...getAnswersKeyedByDataElementId(answersByPatient[data.patientId]),
   });
-  console.log(vaccineData);
-  console.log(mapper({}));
-  console.log(mapper({ patientId: '9d16bfc8-add4-497a-9536-a5fc717a48d6' }));
 
-  return vaccineData.map(mapper);
+  return vaccineData.map(mergeDataWithAnswers);
 };
 
 export async function dataGenerator({ models }, parameters) {
   const queryResults = await queryCovidVaccineListData(models, parameters);
   const reportData = await addabc(models, queryResults);
-  console.log(reportData);
   return generateReportFromQueryData(reportData, reportColumnTemplate);
 }
 
