caf154bbfe6b4ba92bb0b6556ab3f9f41eecf019
Tri Nguyen <tri@tridnguyen.com>
2022-03-24 18:52:40 -0400

    simplify build_shared

--- scripts/build_shared.sh
+++ scripts/build_shared.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -euxo pipefail
 
-# babel doesn't remove old files, and this can cause bugs - particularly when something is reverted!
-echo "Removing old shared package"
-rm -rf packages/shared/[!package.json][!.gitignore]*
-
-echo "Building shared package"
 yarn run build-shared
