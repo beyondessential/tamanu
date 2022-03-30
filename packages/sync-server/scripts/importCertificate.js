3656e5b3b8087517e0c987ab8f5f1e7e26372e03
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-03-31 11:55:49 +1300

    WAITM-113 First pass of import/export scripts

---
+++ packages/sync-server/scripts/importCertificate.js
@@ -0,0 +1,70 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+// TODO: Don't put passwords on the command line
+const [nodeName, scriptName, baseUrl, inputFile, userEmail, password] = process.argv;
+
+const API_VERSION = 'v1';
+let token = null;
+
+if (!baseUrl || !userEmail) {
+  throw new Error(
+    `Usage ${nodeName} ${scriptName} https://example.com/from-this-server path/to/output/file user@example.com`,
+  );
+}
+
+async function fetchFromSyncServer(endpoint, params = {}) {
+  const { headers = {}, body, method = 'GET', ...otherParams } = params;
+
+  const url = `${baseUrl}/${API_VERSION}/${endpoint}`;
+
+  const response = await fetch(url, {
+    method,
+    headers: {
+      Accept: 'application/json',
+      Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': body ? 'application/json' : undefined,
+      ...headers,
+    },
+    body: body && JSON.stringify(body),
+    timeout: 10000,
+    ...otherParams,
+  });
+  const responseBody = await response.json();
+
+  if (!response.ok) {
+    console.log('!!! ERROR !!!');
+    console.log(`Server responded with status code ${response.status} (${responseBody.error})`);
+    process.exit(1);
+  }
+
+  return responseBody;
+}
+
+// Main function
+(async () => {
+  console.log(`Logging into ${baseUrl} as ${userEmail}`);
+  const loginResponse = await fetchFromSyncServer('login', {
+    method: 'POST',
+    body: {
+      email: userEmail,
+      password,
+      // facilityId ??
+    },
+  });
+  token = loginResponse.token;
+
+  console.log(`Uploading certificate: ${inputFile}`);
+  const certificate = await fs.promises.readFile(inputFile, 'utf8');
+  const importResponse = await fetchFromSyncServer('integration/signer/importCertificate', {
+    method: 'POST',
+    body: {
+      certificate,
+    },
+  });
+
+  console.log(importResponse.message);
+
+  console.log('Success');
+  return 0;
+})();
