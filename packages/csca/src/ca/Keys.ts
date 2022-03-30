a9ffbb51de83b2e10caf8cae3b83b8143abf47ae
Félix Saparelli <felix@passcod.name>
2022-03-31 00:17:04 +1300

    Use custom aes-gcm schema for private key wrapping
    - AES-KW isn't supported on current/old OpenSSL
    - The key wrapping with "pkcs8" don't actually output pkcs DER, but raw data
    - (the format is just the output format for the key export)
    
    Hence, there's no point trying to output a PKCS-compatible key, it wasn't
    doing that in the first place. Instead, let's write the IV and the data
    straight out, and add a version/schema byte on top just to future-proof it.

--- packages/csca/src/ca/Keys.ts
+++ packages/csca/src/ca/Keys.ts
@@ -2,21 +2,51 @@ import { promises as fs } from 'fs';
 
 import crypto from '../crypto';
 
+// Layout:
+// 12-byte IV
+// rest of it is AES-GCM wrapping a JWK
+const PRIVATE_KEY_LAYOUT_SCHEMA = 1;
+
+// It may be better to use AES-KW, but that's only supported by OpenSSL 3.
+
 export async function writePrivateKey(
   path: string,
   privateKey: CryptoKey,
   encryptionKey: CryptoKey,
 ) {
-  const wrapped = await crypto.subtle.wrapKey('pkcs8', privateKey, encryptionKey, 'AES-KW');
-  await fs.writeFile(path, Buffer.from(wrapped));
+  const schema = new Uint8Array([PRIVATE_KEY_LAYOUT_SCHEMA]);
+  const iv = crypto.getRandomValues(new Uint8Array(12));
+  const wrapped = await crypto.subtle.wrapKey('jwk', privateKey, encryptionKey, {
+    name: 'AES-GCM',
+    iv,
+  });
+
+  const data = Buffer.concat([schema, iv, Buffer.from(wrapped)]);
+  await fs.writeFile(path, data);
 }
 
 export async function readPrivateKey(path: string, encryptionKey: CryptoKey): Promise<CryptoKey> {
-  const wrapped = await fs.readFile(path);
-  return await crypto.subtle.unwrapKey('pkcs8', wrapped, encryptionKey, 'AES-KW', 'AES-GCM', true, [
-    'sign',
-    'verify',
-  ]);
+  const data = await fs.readFile(path);
+  const schema = data.slice(0, 1);
+  if (schema[0] !== PRIVATE_KEY_LAYOUT_SCHEMA)
+    throw new Error(`Invalid private key file: schema ${schema[0]} not supported`);
+
+  const iv = data.slice(1, 13);
+  const wrapped = data.slice(13);
+  return await crypto.subtle.unwrapKey(
+    'jwk',
+    wrapped,
+    encryptionKey,
+    {
+      name: 'AES-GCM',
+      iv,
+    },
+    {
+      name: 'AES-GCM',
+    },
+    true,
+    ['sign', 'verify'],
+  );
 }
 
 export async function writePublicKey(path: string, publicKey: CryptoKey) {
