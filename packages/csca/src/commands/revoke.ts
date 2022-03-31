a0844a606e9a0b59e767c6b8ff2edfbfac7b1360
Félix Saparelli <felix@passcod.name>
2022-03-31 23:48:36 +1300

    No need for reason options as that's disallowed by spec

--- packages/csca/src/commands/revoke.ts
+++ packages/csca/src/commands/revoke.ts
@@ -1,33 +1,13 @@
 import { Command } from 'commander';
 import { parseDate } from 'chrono-node';
-import { enumFromStringValue, enumValues } from '../utils';
-
-export enum Reasons {
-  Unspecified = 'unspecified',
-  KeyCompromise = 'keyCompromise',
-  CaCompromise = 'CACompromise',
-  AffiliationChanged = 'affiliationChanged',
-  Superseded = 'superseded',
-  CessationOfOperation = 'cessationOfOperation',
-}
 
 function run(
   folder: string,
   certificate: string,
-  options: {
-    reason: string;
-    compromiseTime: string;
-  },
+  options: { date: string }
 ) {
-  const reason = enumFromStringValue(Reasons, options.reason);
-
-  let parsedTime: Date;
-  if (reason === Reasons.CaCompromise || reason === Reasons.KeyCompromise) {
-    parsedTime = parseDate(options.compromiseTime);
-    parsedTime.toISOString(); // throws on invalid date
-  }
-
-  //
+  const date = parseDate(options.date);
+  date.toISOString(); // throws on invalid date
 }
 
 export default new Command('revoke')
@@ -35,13 +15,10 @@ export default new Command('revoke')
   .argument('folder', 'path to CSCA folder')
   .argument('certificate', 'path to certificate file')
   .option(
-    '-R, --reason <reason>',
-    `reason for revocation (one of: ${enumValues(Reasons).join(', ')})`,
-    'unspecified',
-  )
-  .option(
-    '-T, --compromise-time <datetime>',
-    'date and time of compromise for keyCompromise and CACompromise',
+    '--date <datetime>',
+    'date and/or time of revocation',
     'now',
   )
   .action(run);
+
+// CRL reason extension is explicitly disallowed by 9303-12
