4946c47dbd95f2bd8e45d3e661588735e55d037e
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-03-31 17:03:08 -0500

    Create new mobile constants (educational attainment, title and social media)

--- packages/mobile/App/ui/helpers/constants.ts
+++ packages/mobile/App/ui/helpers/constants.ts
@@ -230,3 +230,32 @@ export const AutocompleteSourceToColumnMap = {
   Location: 'name',
   ReferenceData: 'name',
 };
+
+export const titleOptions = [
+  { value: 'Mr', label: 'Mr' },
+  { value: 'Mrs', label: 'Mrs' },
+  { value: 'Ms', label: 'Ms' },
+  { value: 'Miss', label: 'Miss' },
+  { value: 'Dr', label: 'Dr' },
+  { value: 'Sr', label: 'Sr' },
+  { value: 'Sn', label: 'Sn' },
+];
+
+export const socialMediaOptions = [
+  { value: 'Facebook', label: 'Facebook' },
+  { value: 'Instagram', label: 'Instagram' },
+  { value: 'LinkedIn', label: 'LinkedIn' },
+  { value: 'Twitter', label: 'Twitter' },
+  { value: 'Viber', label: 'Viber' },
+  { value: 'WhatsApp', label: 'WhatsApp' },
+];
+
+export const educationalAttainmentOptions = [
+  { value: 'No formal schooling', label: 'No formal schooling' },
+  { value: 'Less than primary school', label: 'Less than primary school' },
+  { value: 'Primary school completed', label: 'Primary school completed' },
+  { value: 'Sec school completed', label: 'Sec school completed' },
+  { value: 'High school completed', label: 'High school completed' },
+  { value: 'University completed', label: 'University completed' },
+  { value: 'Post grad completed', label: 'Post grad completed' },
+];
