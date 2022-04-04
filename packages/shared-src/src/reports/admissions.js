86aef9678f4c11f1ee952341afce4c1c8a770a96
Alastair-L <lockealastair@gmail.com>
2022-04-04 14:55:05 +1000

    Update report pt 1

--- packages/shared-src/src/reports/admissions.js
+++ packages/shared-src/src/reports/admissions.js
@@ -1,22 +1,23 @@
 import { Op } from 'sequelize';
-import { subDays } from 'date-fns';
-import { ENCOUNTER_TYPES } from 'shared/constants';
+import { subDays, format } from 'date-fns';
+import { ENCOUNTER_TYPES, DIAGNOSIS_CERTAINTY } from 'shared/constants';
+import { inspect } from 'util';
 import { generateReportFromQueryData } from './utilities';
 
 const reportColumnTemplate = [
   { title: 'Patient First Name', accessor: data => data.patient.firstName },
   { title: 'Patient Last Name', accessor: data => data.patient.lastName },
   { title: 'Patient ID', accessor: data => data.patient.displayId },
-  { title: 'Date of Birth', accessor: data => 'testData' },
-  { title: 'Location', accessor: data => 'testData' },
-  { title: 'Department', accessor: data => 'testData' },
-  { title: 'Primary diagnoses', accessor: data => 'testData' },
-  { title: 'Secondary diagnoses', accessor: data => 'testData' },
+  { title: 'Date of Birth', accessor: data => format(data.patient.dateOfBirth, 'dd/MM/yyyy') },
+  { title: 'Location', accessor: data => data.location.name },
+  { title: 'Department', accessor: data => data.department.name },
+  { title: 'Primary diagnoses', accessor: data => data.primaryDiagnoses },
+  { title: 'Secondary diagnoses', accessor: data => data.secondaryDiagnoses },
   { title: 'Sex', accessor: data => data.patient.sex },
-  { title: 'Village', accessor: data => data.patient.ReferenceDatum.name },
+  { title: 'Village', accessor: data => data.patient.village.name },
   { title: 'Doctor/Nurse', accessor: data => data.examiner?.displayName },
-  { title: 'Admission Date', accessor: data => data.startDate },
-  { title: 'Discharge Date', accessor: data => data.endDate },
+  { title: 'Admission Date', accessor: data => format(data.startDate, 'dd/MM/yyyy') },
+  { title: 'Discharge Date', accessor: data => format(data.endDate, 'dd/MM/yyyy') },
 ];
 
 function parametersToSqlWhere(parameters) {
@@ -40,25 +41,48 @@ function parametersToSqlWhere(parameters) {
   };
 }
 
+const stringifyDiagnoses = (diagnoses, shouldBePrimary) =>
+  diagnoses
+    .filter(({ isPrimary }) => isPrimary === shouldBePrimary)
+    .map(({ Diagnosis }) => `${Diagnosis.code} ${Diagnosis.name}`)
+    .join('; ');
+
 async function queryAdmissionsData(models, parameters) {
-  const result = await models.Encounter.findAll({
+  const results = await models.Encounter.findAll({
     include: [
       {
         model: models.Patient,
         as: 'patient',
-        include: [{ model: models.ReferenceData, as: 'village' }],
+        include: ['village'],
       },
-      { model: models.User, as: 'examiner' },
-      { model: models.Location, as: 'location' },
-      { model: models.Department, as: 'department' },
+      'examiner',
+      'location',
+      'department',
+      {
+        model: models.EncounterDiagnosis,
+        as: 'diagnoses',
+        required: false,
+        where: {
+          certainty: DIAGNOSIS_CERTAINTY.CONFIRMED,
+        },
+        include: ['Diagnosis'],
+      },
     ],
     where: parametersToSqlWhere(parameters),
   });
-  return result;
+
+  console.log(inspect(results));
+  return results.map(result => ({
+    ...result,
+    primaryDiagnoses: stringifyDiagnoses(result.diagnoses, true),
+    secondaryDiagnoses: stringifyDiagnoses(result.diagnoses, false),
+  }));
 }
 
 export async function dataGenerator({ models }, parameters) {
   const queryResults = await queryAdmissionsData(models, parameters);
+  console.log(queryResults);
+  console.log(queryResults.startDate);
   return generateReportFromQueryData(queryResults, reportColumnTemplate);
 }
 
