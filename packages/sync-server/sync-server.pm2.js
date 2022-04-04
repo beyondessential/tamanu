e0507cf9ffc15090b0f413e3db4e47277a4d90de
Tom McLean <tom.a.mclean@gmail.com>
2022-04-04 15:55:14 +1000

    Add memory variable

--- packages/sync-server/sync-server.pm2.js
+++ packages/sync-server/sync-server.pm2.js
@@ -1,8 +1,10 @@
+const memory = process.env.TAMANU_MEMORY_ALLOCATION || 8192;
+
 module.exports = {
   apps: [{
     name: "tamanu-sync-server",
     script: "./dist/app.bundle.js",
-    interpreter_args: "--max_old_space_size=8192",
+    interpreter_args: `--max_old_space_size=${memory}`,
     instances: "max",
     exec_mode: "cluster",
     env: {
