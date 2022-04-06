6eef22dee2bb74e551c6118c33bb530e6a051630
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-04-06 11:57:56 -0500

    Add testID and accessibilityLabel to EditButton

--- packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/EditButton.tsx
+++ packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/EditButton.tsx
@@ -1,11 +1,26 @@
 import React, { ReactElement } from 'react';
-import { TouchableProps } from '/interfaces/TouchableProps';
 import { StyledTouchableOpacity } from '/styled/common';
 import { screenPercentageToDP, Orientation } from '/helpers/screen';
 import { PencilIcon } from '/components/Icons';
 
-export const EditButton = ({ onPress }: TouchableProps): ReactElement => (
-  <StyledTouchableOpacity onPress={onPress}>
+interface EditButtonProps {
+  sectionTitle: string;
+  onPress: () => void;
+}
+
+const createTestID = (sectionTitle: string): string => {
+  const lowercaseSectionTitle = sectionTitle.toLowerCase();
+
+  // Adds 'edit' prefix and replaces spaces for hyphens
+  return `edit-${lowercaseSectionTitle.replace(/ /g, '-')}`;
+};
+
+export const EditButton = ({ sectionTitle, onPress }: EditButtonProps): ReactElement => (
+  <StyledTouchableOpacity
+    testID={createTestID(sectionTitle)}
+    accessibilityLabel={`Edit ${sectionTitle}`}
+    onPress={onPress}
+  >
     <PencilIcon
       height={screenPercentageToDP('2.5', Orientation.Height)}
       width={screenPercentageToDP('2.5', Orientation.Height)}
