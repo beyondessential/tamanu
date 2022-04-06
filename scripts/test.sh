4c20efaffd6b2492fa6c25663ca79a615bea306c
Félix Saparelli <felix@passcod.name>
2022-04-06 18:06:13 +1200

    Add clear delineations between tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,10 +6,12 @@ yarn run build-shared
 
 echo "Running tests"
 for workspace in shared-src lan sync-server meta-server; do
-  echo "Running tests in $workspace"
+  echo "=== Running tests in $workspace"
   yarn workspace $workspace \
     run test-coverage \
     --coverageReporters=json-summary
+  echo "==============================="
+  echo
 done
 
 echo "Aggregating coverage"
@@ -18,5 +20,5 @@ node scripts/aggregate-coverage.mjs | tee coverage.md
 if [[ ! -z "$CI_PR_NUMBER" ]]; then
   echo "Posting coverage to PR"
   export GITHUB_TOKEN=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 # temporary, for demo only!
-  node scripts/pr-comment.js coverage.md 'Coverage Report'
+  node scripts/pr-comment.mjs coverage.md 'Coverage Report'
 fi
