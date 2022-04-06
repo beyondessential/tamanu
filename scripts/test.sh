b9c5f7301593f51acc4cd0b08389b09432117cc4
Félix Saparelli <felix@passcod.name>
2022-04-06 16:04:51 +1200

    Aggregate coverage for entire CI run

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -euo pipefail
+
+echo "Building shared"
+yarn run build-shared
+
 echo "Running tests"
-yarn test-coverage
+for workspace in shared-src lan sync-server meta-server; do
+  echo "Running tests in $workspace"
+  yarn workspace $workspace \
+    run test-coverage \
+    --coverageReporters=json-summary
+done
+
+echo "Aggregating coverage"
+node scripts/aggregate-coverage.mjs | tee coverage.md
+
