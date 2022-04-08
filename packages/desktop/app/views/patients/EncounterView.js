8c2b750060e6305b1700f4bdf53bc499c026ba4d
Tom Caiger <caigertom@gmail.com>
2022-04-08 14:33:40 +1200

    Update EncounterView.js

--- packages/desktop/app/views/patients/EncounterView.js
+++ packages/desktop/app/views/patients/EncounterView.js
@@ -476,7 +476,7 @@ function getHeaderText({ encounterType }) {
 
 const GridColumnContainer = styled.div`
   // set min-width to 0 to stop the grid column getting bigger than it's parent
-  // as it defaults to auto
+  // as grid column children default to min-width: auto
   min-width: 0;
 `;
 
