3e4934726ca98ac7dcb0ab35fa0adb90c2eb6458
Félix Saparelli <felix@passcod.name>
2022-04-08 14:27:02 +1200

    Use actual github token

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,6 +16,5 @@ node scripts/aggregate-coverage.mjs | tee coverage.md
 
 if [[ "${1:-}" == "pr-coverage" ]]; then
   echo "Posting coverage to PR"
-  export GITHUB_TOKEN=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 # temporary, for demo only!
   node scripts/pr-comment.mjs coverage.md 'Coverage Report' || true
 fi
