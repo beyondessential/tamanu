b011837c6374fc46a5b9784ce6bb065325f9f5d6
Alastair-L <lockealastair@gmail.com>
2022-04-11 11:20:45 +1000

    Update tests to test historical filtering

--- packages/lan/__tests__/apiv1/reports/Admissions.test.js
+++ packages/lan/__tests__/apiv1/reports/Admissions.test.js
@@ -155,8 +155,7 @@ describe('Admissions report', () => {
         parameters: {
           fromDate: new Date(2021, 1, 1),
           location: expectedLocation.id,
-          // TODO: the report should still fetch with the below filter enabled
-          // department: expectedDepartment1.id, // Historical department filtered for
+          department: expectedDepartment1.id, // Historical department filtered for
         },
       });
       expect(result).toHaveSucceeded();
