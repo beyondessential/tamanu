4245a57149d48bda12ed3157e73481edfec9dc7a
Tri Nguyen <tri@tridnguyen.com>
2022-04-13 00:00:04 -0400

    Update document upload UI (#2081)

--- packages/desktop/app/components/DocumentsSearchBar.js
+++ packages/desktop/app/components/DocumentsSearchBar.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
 
 import { Button } from './Button';
 import { ButtonRow } from './ButtonRow';
 import { Form, Field, TextField } from './Field';
 import { FormGrid } from './FormGrid';
 
-const StyledDiv = styled.div`
-  margin: 1rem;
-  padding: 16px;
-  border: 1px solid #dedede;
+const Container = styled.div`
+  padding: 1rem 2rem 1rem 2rem;
   border-radius: 3px 3px 0 0;
   background-color: #ffffff;
 `;
 
-const Label = styled.div`
+const HeaderBar = styled.div`
   font-size: 16px;
   font-weight: 500;
-  color: ${props => props.theme.palette.primary.main};
-  margin-bottom: 5px;
+  margin-bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h3 {
+    font-size: 1rem;
+    font-weight: 500;
+    color: ${props => props.theme.palette.text.primary};
+  }
 `;
 
 const PaddedSearchIcon = styled(SearchIcon)`
@@ -32,20 +38,25 @@ const renderSearchBar = ({ submitForm, clearForm }) => (
     <Field name="documentOwner" placeholder="Owner" component={TextField} />
     <Field name="departmentName" placeholder="Department" component={TextField} />
     <ButtonRow>
+      <Button onClick={clearForm} variant="outlined" color="primary">
+        Clear search
+      </Button>
       <Button color="primary" variant="contained" onClick={submitForm} type="submit">
         <PaddedSearchIcon />
         Search
-      </Button>
-      <Button onClick={clearForm} variant="outlined">
-        Clear search
       </Button>
     </ButtonRow>
   </FormGrid>
 );
 
-export const DocumentsSearchBar = ({ setSearchParameters }) => (
-  <StyledDiv>
-    <Label>Documents search</Label>
+export const DocumentsSearchBar = ({ setSearchParameters, onAddDocument }) => (
+  <Container>
+    <HeaderBar>
+      <Typography variant="h3">Documents search</Typography>
+      <Button onClick={onAddDocument} variant="contained" color="primary">
+        Add document
+      </Button>
+    </HeaderBar>
     <Form onSubmit={values => setSearchParameters(values)} render={renderSearchBar} />
-  </StyledDiv>
+  </Container>
 );
