81169004956985c18220d7f7aff03991bbb3454c
Félix Saparelli <felix@passcod.name>
2022-04-16 16:55:00 +1200

    Don't even start postgres when it's not needed

--- scripts/copy_files.sh
+++ scripts/copy_files.sh
@@ -7,7 +7,3 @@ rm -rf /tamanu/* || true
 
 echo "Copy files to runtime dir"
 cp -r --reflink=auto /pre/* /pre/.*{rc,ignore} /tamanu/
-
-echo "Reinstate SSH Private key"
-echo -e $PRIVATE_SSH_KEY >> /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
