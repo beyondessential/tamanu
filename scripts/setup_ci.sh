ef50967c18647def0836c4dce99bde4458eb3d65
Félix Saparelli <felix@passcod.name>
2022-04-16 15:35:21 +1200

    Perform reflink copies when possible

--- scripts/setup_ci.sh
+++ scripts/setup_ci.sh
@@ -6,7 +6,7 @@ echo "Clean runtime dir"
 rm -rf /tamanu/* || true
 
 echo "Copy files to runtime dir"
-cp -r /pre/* /pre/.*{rc,ignore} /tamanu/
+cp -r --reflink=auto /pre/* /pre/.*{rc,ignore} /tamanu/
 
 echo "Reinstate SSH Private key"
 echo -e $PRIVATE_SSH_KEY >> /root/.ssh/id_rsa
