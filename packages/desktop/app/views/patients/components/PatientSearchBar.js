2c647ef31cdfcfbd086e28eec84df15021b0d7e4
Edwin <edmofro@gmail.com>
2022-05-02 15:57:52 +1200

    Improve exact search tooltip

--- packages/desktop/app/views/patients/components/PatientSearchBar.js
+++ packages/desktop/app/views/patients/components/PatientSearchBar.js
@@ -41,10 +41,10 @@ export const PatientSearchBar = ({ onSearch, fields = DEFAULT_FIELDS, ...props }
           InputProps: {
             endAdornment: (
               <InputAdornment position="end">
-                <Tooltip title="Toggle EXACT search by ID">
+                <Tooltip title="Exact term search">
                   <SpellcheckIcon
                     style={{ cursor: 'pointer' }}
-                    aria-label="Toggle exact search by ID"
+                    aria-label="Exact term search"
                     onClick={toggleSearchIdExact}
                     color={displayIdExact ? '' : 'disabled'}
                   />
