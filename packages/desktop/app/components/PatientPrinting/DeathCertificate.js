e120fff6376dae0be80acf20407af0aee7951ada
Edwin <edmofro@gmail.com>
2022-05-03 13:51:12 +1200

    Show causes on death certificate

--- packages/desktop/app/components/PatientPrinting/DeathCertificate.js
+++ packages/desktop/app/components/PatientPrinting/DeathCertificate.js
@@ -62,6 +62,17 @@ const FormLineInner = styled.div`
   }
 `;
 
+const FormFieldUnderline = styled(Text)`
+  border-bottom: 1px solid black;
+  width: 100%;
+  margin: 0 0 0 10px;
+  font-size: 14px;
+`;
+
+const UnderlinedFormHelper = styled(Text)`
+  font-weight: 500;
+`;
+
 const FormLine = ({ children, helperText }) => {
   return (
     <FormLineContainer>
@@ -74,10 +85,26 @@ const FormLine = ({ children, helperText }) => {
   );
 };
 
+const UnderlinedFormField = ({ label, children, helperText }) => {
+  return (
+    <FormLineContainer>
+      <FormLineInner>
+        <Text>{label}</Text>
+        <FormFieldUnderline>{children}</FormFieldUnderline>
+      </FormLineInner>
+      <UnderlinedFormHelper>{helperText}</UnderlinedFormHelper>
+    </FormLineContainer>
+  );
+};
+
+const getCauseName = cause => cause?.condition?.name;
+
 export const DeathCertificate = React.memo(({ patientData, certificateData }) => {
   const { firstName, lastName, dateOfBirth, sex, causes, dateOfDeath, facility } = patientData;
   const { title, subTitle, logo, watermark, printedBy, deathCertFooterImg } = certificateData;
-  const causeOfDeath = causes?.primary?.condition?.name;
+  const causeOfDeath = getCauseName(causes?.primary);
+  const antecedentCause1 = getCauseName(causes?.antecedent1);
+  const antecedentCause2 = getCauseName(causes?.antecedent2);
   const dateOfPrinting = new Date();
 
   return (
@@ -117,41 +144,42 @@ export const DeathCertificate = React.memo(({ patientData, certificateData }) =>
             </StrongText>
             <br />
             <br />
+
             <StrongText>Antecedent causes</StrongText>
             <Text>
-              Morbid conditions, if any, giving rise to the above cause, stating the underlying
-              condition last
+              Morbid conditions, giving rise to the above cause, stating the underlying condition
+              last
             </Text>
           </Box>
           <Box>
             <Box>
-              <FormLine helperText="due to (or as a consequence of)">
-                <Text>(a)</Text>
-              </FormLine>
+              <UnderlinedFormField label="(a)" helperText="due to (or as a consequence of)">
+                {causeOfDeath}
+              </UnderlinedFormField>
             </Box>
             <Box>
-              <FormLine helperText="due to (or as a consequence of)">
-                <Text>(b)</Text>
-              </FormLine>
+              <UnderlinedFormField label="(b)" helperText="due to (or as a consequence of)">
+                {antecedentCause1}
+              </UnderlinedFormField>
             </Box>
             <Box>
-              <FormLine helperText="due to (or as a consequence of)">
-                <Text>(c)</Text>
-              </FormLine>
+              <UnderlinedFormField label="(c)">{antecedentCause2}</UnderlinedFormField>
             </Box>
           </Box>
         </Grid>
         <Grid borderTop={1} px={3} pt={2} pb={3}>
           <Box maxWidth="240px">
             <StrongText>
-              II<br />
+              II
+              <br />
               Other significant conditions contributing to the death but not related to the disease
               or condition causing it.
             </StrongText>
           </Box>
           <Box>
-            <FormLine />
-            <FormLine />
+            {causes?.contributing?.map(cause => (
+              <UnderlinedFormField>{getCauseName(cause)}</UnderlinedFormField>
+            ))}
           </Box>
         </Grid>
       </Box>
