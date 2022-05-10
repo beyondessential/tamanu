a39638b3710f585ca657d6568da351a15d7908aa
Félix Saparelli <felix@passcod.name>
2022-05-10 14:49:02 +1200

    Add working days to signer validities

--- packages/csca/src/ca/profile.ts
+++ packages/csca/src/ca/profile.ts
@@ -27,15 +27,15 @@ export function signerWorkingDays(profile: Profile): number {
 /**
  * Default validity of issued signer certificates (BSC) is derived from the issuance profile.
  *
- * - VDS: 10 years (for maximum flexibility) + 2 leap days
- * - EUDCC: 1 year (recommendation from the spec)
+ * - VDS: working + 10 years (for maximum flexibility)
+ * - EUDCC: working + 1 year (recommendation from the spec)
  */
 export function signerDefaultValidityDays(profile: Profile): number {
   switch (profile) {
     case Profile.VDS:
-      return 10 * 365 + 2;
+      return signerWorkingDays(profile) + 10 * 365;
     case Profile.EUDCC:
-      return 365;
+      return signerWorkingDays(profile) + 365;
     default:
       throw new Error('Unknown profile');
   }
