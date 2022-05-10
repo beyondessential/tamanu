b7d674c852622c4df8c3ef9fffb6c37cd91a0329
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-05-09 22:42:35 -0500

    Add a bunch of utils for HL7 FHIR functionality

--- packages/sync-server/app/hl7fhir/utils.js
+++ packages/sync-server/app/hl7fhir/utils.js
@@ -1,4 +1,4 @@
-import { Op } from 'sequelize';
+import { Sequelize, Op } from 'sequelize';
 import { jsonFromBase64, jsonToBase64 } from 'shared/utils/encodings';
 
 export function hl7SortToTamanu(hl7Sort) {
@@ -57,4 +57,53 @@ export function addPaginationToWhere(where, after) {
       },
     ],
   };
+}
+
+// HL7 query parameters might have modifiers,
+// this will split them into an array.
+export function getParamAndModifier(fullParam) {
+  return fullParam.split(':', 2);
+}
+
+// Prefixes supported by Tamanu with the corresponding
+// sequelize operators.
+const prefixes = {
+  eq: Op.eq,
+  co: Op.substring,
+  sw: Op.startsWith,
+  ew: Op.endsWith,
+};
+
+// Modifiers supported by Tamanu with the corresponding
+// sequelize operator.
+const modifiers = {
+  contains: Op.substring,
+  'starts-with': Op.startsWith,
+  'ends-with': Op.endsWith,
+  exact: Op.eq,
+};
+
+export const stringTypeModifiers = ['contains', 'starts-with', 'ends-with', 'exact'];
+
+// Helper function that returns a sequelize operator or equality by default
+export function getOperator(modifier, supportedModifiers) {
+  if (modifier && supportedModifiers.includes(modifier) && modifier in modifiers) {
+    return modifiers[modifier];
+  }
+
+  return Op.eq;
+}
+
+// Helper function to deal with case insensitive searches for strings
+export function getQueryObject(columnName, value, operator, modifier) {
+  // Only exact should be case insensitive
+  if (modifier === 'exact' || typeof value !== 'string') {
+    return { [operator]: value };
+  }
+
+  // Perform case insensitive search by using SQL function UPPER
+  // and modifying the string to be uppercase.
+  return Sequelize.where(Sequelize.fn('upper', Sequelize.col(columnName)), {
+    [operator]: value.toUpperCase(),
+  });
 }
