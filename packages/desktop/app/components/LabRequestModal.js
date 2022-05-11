66b4898ff044b8262a117210cc6e515ce6cdd42d
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-05-11 14:10:57 +1200

    WAITM-164 Refactor lab request form to consolidate finalise and print logic

--- packages/desktop/app/components/LabRequestModal.js
+++ packages/desktop/app/components/LabRequestModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { customAlphabet } from 'nanoid';
 
 import { useApi } from '../api';
@@ -8,26 +8,25 @@ import { Modal } from './Modal';
 import { ConnectedLabRequestForm } from '../forms/LabRequestForm';
 import { ALPHABET_FOR_ID } from '../constants';
 
-export const LabRequestModal = ({ open, onClose, onSaved, onPrint, encounter }) => {
+export const LabRequestModal = ({ open, onClose, encounter }) => {
   const api = useApi();
   const practitionerSuggester = new Suggester(api, 'practitioner');
+  const [requestId, setRequestId] = useState();
 
   return (
     <Modal width="md" title="New lab request" open={open} onClose={onClose}>
       <ConnectedLabRequestForm
-        onSubmit={async (data, isPrinting = false) => {
+        onSubmit={async data => {
           const newRequest = await api.post(`labRequest`, {
             ...data,
             encounterId: encounter.id,
           });
-          if (isPrinting) {
-            onPrint(newRequest.id);
-          } else {
-            onSaved();
-          }
+          setRequestId(newRequest.id);
+          onClose();
         }}
         onCancel={onClose}
         encounter={encounter}
+        requestId={requestId}
         practitionerSuggester={practitionerSuggester}
         generateDisplayId={customAlphabet(ALPHABET_FOR_ID, 7)}
       />
