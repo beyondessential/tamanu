da3e963147d0b3e2d6c9cf7ce4dea72780028a05
Félix Saparelli <felix@beyondessential.com.au>
2022-05-16 22:48:31 +1200

    WAITM-220: Add reconfig command to csca tool and update issuance validity defaults (#2213)
    * Add working days to signer validities
    
    * Add leap days to periods in a consistent manner
    
    * Write config in condensed JSON
    
    * Add validation schema for config
    
    * Add new reconfig command

--- packages/csca/src/ca/Config.ts
+++ packages/csca/src/ca/Config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
 import { add } from 'date-fns';
+import * as yup from 'yup';
 
 import AuthenticatedFile from './AuthenticatedFile';
 import { Extension } from './certificateExtensions';
@@ -16,7 +17,7 @@ export default class Config extends AuthenticatedFile {
   }
 
   private async write(config: ConfigFile): Promise<void> {
-    await this.writeFile(Buffer.from(JSON.stringify(config, null, 2), 'utf-8'));
+    await this.writeFile(Buffer.from(JSON.stringify(config), 'utf-8'));
   }
 
   public async create(config: ConfigFile): Promise<void> {
@@ -47,6 +48,14 @@ export default class Config extends AuthenticatedFile {
   public async getCrlS3Bucket(): Promise<S3Bucket> {
     const config = await this.load();
     return config.crl.bucket;
+  }
+
+  public async export(): Promise<ConfigFile> {
+    return this.load();
+  }
+
+  public async validateAndImport(config: any): Promise<void> {
+    return this.write(await validate(config));
   }
 }
 
@@ -100,4 +109,136 @@ export function period(start: Date, days: number): Period {
     start: truncateToSeconds(start),
     end: truncateToSeconds(add(start, { days })),
   };
+}
+
+const SCHEMA = yup.object({
+  name: yup
+    .string()
+    .required()
+    .min(1)
+    .matches(/^[a-zA-Z0-9_\-]+$/),
+
+  country: yup
+    .object({
+      name: yup.string().min(1),
+
+      alpha2: yup
+        .string()
+        .required()
+        .length(2),
+
+      alpha3: yup
+        .string()
+        .required()
+        .length(3),
+    })
+    .required(),
+
+  subject: yup
+    .object({
+      country: yup
+        .string()
+        .required()
+        .min(1),
+
+      commonName: yup
+        .string()
+        .required()
+        .min(1),
+
+      organisation: yup.string().min(1),
+
+      organisationUnit: yup.string().min(1),
+    })
+    .required(),
+
+  crl: yup
+    .object({
+      filename: yup
+        .string()
+        .required()
+        .min(1),
+
+      distribution: yup
+        .array()
+        .of(
+          yup
+            .string()
+            .required()
+            .min(1),
+        )
+        .default([]),
+
+      bucket: yup
+        .object({
+          region: yup
+            .string()
+            .required()
+            .min(1),
+
+          name: yup
+            .string()
+            .required()
+            .min(1),
+        })
+        .required(),
+    })
+    .required(),
+
+  validityPeriod: yup
+    .object({
+      start: yup.date().required(),
+      end: yup.date().required(),
+    })
+    .required(),
+
+  workingPeriod: yup
+    .object({
+      start: yup.date().required(),
+      end: yup.date().required(),
+    })
+    .required(),
+
+  issuance: yup
+    .object({
+      extensions: yup.array().of(
+        yup
+          .object({
+            name: yup
+              .string()
+              .required()
+              .min(1),
+
+            critical: yup.boolean().default(false),
+
+            value: yup.lazy((value: any) =>
+              (typeof value === 'string' && value === 'computed'
+                ? yup.string().matches(/^computed$/)
+                : yup.array()
+              ).required(),
+            ),
+          })
+          .required(),
+      ),
+
+      validityPeriodDays: yup
+        .number()
+        .required()
+        .min(1),
+
+      workingPeriodDays: yup
+        .number()
+        .required()
+        .min(1),
+    })
+    .required(),
+});
+
+export async function validate(config: object): Promise<ConfigFile> {
+  const conf = await SCHEMA.validate(config);
+  if (conf.issuance.validityPeriodDays < conf.issuance.workingPeriodDays) {
+    throw new Error('validityPeriodDays must be greater than or equal to workingPeriodDays');
+  }
+
+  return conf as ConfigFile;
 }
