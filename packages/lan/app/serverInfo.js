c76e9b28c9cb744108527f2c8ff24edc6d175773
Tom McLean <tom.a.mclean@gmail.com>
2022-05-19 14:44:23 +1000

    Add explanatory comment

--- packages/lan/app/serverInfo.js
+++ packages/lan/app/serverInfo.js
@@ -1,5 +1,7 @@
 import { version as appVersion } from '../package.json';
 
+// Set a global serverInfo object so that it can be accessed 
+// from within the shared modules (eg in honeycomb)
 global.serverInfo = {
   version: appVersion,
   serverType: 'lan',
