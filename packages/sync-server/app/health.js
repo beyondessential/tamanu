15baf8410f4c4e880a14f1a9e2dc19abee2e2eb4
Tom McLean <tom.a.mclean@gmail.com>
2022-05-19 11:47:26 +1000

    Fix wrong import

--- packages/sync-server/app/health.js
+++ packages/sync-server/app/health.js
@@ -5,7 +5,7 @@ import config from 'config';
 import { log } from 'shared/services/logging';
 import { createMigrationInterface } from 'shared/services/migrations';
 
-import { version } from '../serverInfo';
+import { version } from './serverInfo';
 import { canUploadAttachment } from './utils/getFreeDiskSpace';
 
 export const healthRoutes = express.Router();
