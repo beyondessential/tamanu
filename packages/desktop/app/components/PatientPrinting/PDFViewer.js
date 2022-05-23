c6d89653d99356ee78404c3614e4d70fee0cab08
Felix Saparelli <felix@beyondessential.com.au>
2022-05-23 14:02:59 +1200

    Fix flash of null
    This could probably use a "loading" state instead of rendering nothing

--- packages/desktop/app/components/PatientPrinting/PDFViewer.js
+++ packages/desktop/app/components/PatientPrinting/PDFViewer.js
@@ -16,6 +16,8 @@ export const PDFViewer = ({ id, children, props }) => {
   useEffect(() => {
     updateInstance();
   }, [updateInstance, children]);
+  
+  if (!instance.url) return null;
 
   return (
     <FullIframe
