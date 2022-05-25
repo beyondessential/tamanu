787586076d16f71c84d53e302dbdb6cb3a42924f
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-05-24 20:26:42 -0500

    Create tests that cover the mSupply use case searches

--- packages/sync-server/__tests__/integrations/mSupply/Patient.test.js
+++ packages/sync-server/__tests__/integrations/mSupply/Patient.test.js
@@ -1,4 +1,5 @@
 import { format } from 'date-fns';
+import moment from 'moment';
 
 import { fake } from 'shared/test-helpers/fake';
 import { createTestContext } from 'sync-server/__tests__/utilities';
@@ -152,6 +153,247 @@ describe('mSupply integration - Patient', () => {
         .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
 
       // assert
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+  });
+
+  describe('filters search', () => {
+    beforeEach(async () => {
+      const { Patient, PatientAdditionalData } = ctx.store.models;
+      await Patient.destroy({ where: {} });
+      await PatientAdditionalData.destroy({ where: {} });
+    });
+
+    it('filters patient by displayId (identifier)', async () => {
+      const { Patient } = ctx.store.models;
+      const [patientOne] = await Promise.all([
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+      ]);
+
+      const identifier = encodeURIComponent(`${IDENTIFIER_NAMESPACE}|${patientOne.displayId}`);
+      const path = `/v1/integration/mSupply/Patient?identifier=${identifier}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(1);
+    });
+
+    it('filters patients by firstName (given)', async () => {
+      const { Patient } = ctx.store.models;
+      const firstName = 'John';
+      await Promise.all([
+        Patient.create(fake(Patient, { firstName })),
+        Patient.create(fake(Patient, { firstName })),
+        Patient.create(fake(Patient, { firstName: 'Bob' })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?given=${firstName}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by lastName (family)', async () => {
+      const { Patient } = ctx.store.models;
+      const lastName = 'Doe';
+      await Promise.all([
+        Patient.create(fake(Patient, { lastName })),
+        Patient.create(fake(Patient, { lastName })),
+        Patient.create(fake(Patient, { lastName: 'Gray' })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?family=${lastName}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by sex (gender)', async () => {
+      const { Patient } = ctx.store.models;
+      const sex = 'other';
+      await Promise.all([
+        Patient.create(fake(Patient, { sex })),
+        Patient.create(fake(Patient, { sex })),
+        Patient.create(fake(Patient, { sex: 'female' })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?gender=${sex}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by dateOfBirth (birthdate)', async () => {
+      const { Patient } = ctx.store.models;
+      const dateString = '1990-05-25';
+      const dateOfBirth = moment.utc(dateString);
+      await Promise.all([
+        Patient.create(fake(Patient, { dateOfBirth })),
+        Patient.create(fake(Patient, { dateOfBirth })),
+        Patient.create(fake(Patient, { dateOfBirth: moment.utc('1985-10-20') })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?birthdate=${dateString}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by additionalData.cityTown (address-city)', async () => {
+      const { Patient, PatientAdditionalData } = ctx.store.models;
+      const [patientOne, patientTwo, patientThree] = await Promise.all([
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+      ]);
+
+      const cityTown = 'luxembourg';
+      const [a, b, c] = await Promise.all([
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown }),
+          patientId: patientOne.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown }),
+          patientId: patientTwo.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown: 'quito' }),
+          patientId: patientThree.id,
+        }),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?address-city=${cityTown}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    test.todo('filtering by address looks up a bunch of fields');
+    test.todo('filtering by telecom looks up a bunch of fields');
+
+    // This test can be replaced after address is able to look up several fields
+    it('filtering by address only looks up additionalData.cityTown', async () => {
+      const { Patient, PatientAdditionalData } = ctx.store.models;
+      const [patientOne, patientTwo, patientThree] = await Promise.all([
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+      ]);
+
+      const cityTown = 'luxembourg';
+      await Promise.all([
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown }),
+          patientId: patientOne.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown }),
+          patientId: patientTwo.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { cityTown: 'quito' }),
+          patientId: patientThree.id,
+        }),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?address=${cityTown}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    // This test can be replaced after telecom is able to look up several fields
+    it('filtering by telecom only looks up additionalData.primaryContactNumber', async () => {
+      const { Patient, PatientAdditionalData } = ctx.store.models;
+      const [patientOne, patientTwo, patientThree] = await Promise.all([
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+        Patient.create(fake(Patient)),
+      ]);
+
+      const primaryContactNumber = '123456789';
+      await Promise.all([
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { primaryContactNumber }),
+          patientId: patientOne.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { primaryContactNumber }),
+          patientId: patientTwo.id,
+        }),
+        PatientAdditionalData.create({
+          ...fake(PatientAdditionalData, { primaryContactNumber: '987654321' }),
+          patientId: patientThree.id,
+        }),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?telecom=${primaryContactNumber}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by params with supported modifiers', async () => {
+      const { Patient } = ctx.store.models;
+      const firstName = 'Jane';
+      await Promise.all([
+        Patient.create(fake(Patient, { firstName })),
+        Patient.create(fake(Patient, { firstName })),
+        Patient.create(fake(Patient, { firstName: 'Alice' })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?given:contains=${firstName.slice(1, 3)}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
+      expect(response).toHaveSucceeded();
+      expect(response.body.total).toBe(2);
+    });
+
+    it('filters patients by combining params and modifiers (all need to match)', async () => {
+      const { Patient } = ctx.store.models;
+      const firstName = 'Jane';
+      const lastName = 'Doe';
+      const dateString = '1990-05-20';
+      const dateOfBirth = moment.utc(dateString);
+      await Promise.all([
+        Patient.create(fake(Patient, { firstName, lastName, dateOfBirth })),
+        Patient.create(fake(Patient, { firstName, lastName, dateOfBirth })),
+        Patient.create(fake(Patient, { firstName: 'Alice', lastName, dateOfBirth })),
+      ]);
+
+      const path = `/v1/integration/mSupply/Patient?given:contains=${firstName.slice(1, 3)}&family=${lastName}&birthdate=${dateString}`;
+      const response = await app
+        .get(path)
+        .set({ 'X-Tamanu-Client': 'mSupply', 'X-Version': '0.0.1' });
+
       expect(response).toHaveSucceeded();
       expect(response.body.total).toBe(2);
     });
