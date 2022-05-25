f774fa251348170dd999cfa57b1453b5af7780c1
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-05-26 09:02:44 +1200

    WAITM-166 Move api calls outside of prescription printout

--- packages/desktop/app/components/PatientPrinting/PrescriptionPrintout.js
+++ packages/desktop/app/components/PatientPrinting/PrescriptionPrintout.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import moment from 'moment';
 import styled from 'styled-components';
 import { Typography, Box } from '@material-ui/core';
 
-import { useApi } from '../../api';
 import { NotesSection, LocalisedLabel } from './SimplePrintout';
 import { PrintLetterhead } from './PrintLetterhead';
 import { CertificateWrapper } from './CertificateWrapper';
 import { DateDisplay } from '../DateDisplay';
 import { PatientBarcode } from './PatientBarcode';
 import { GridTable } from './GridTable';
+import { LoadingIndicator } from '../LoadingIndicator';
 
 const RowContainer = styled.div`
   display: flex;
@@ -25,99 +25,76 @@ const SignatureBox = styled(Box)`
   height: 60px;
 `;
 
-export const PrescriptionPrintout = React.memo(({ prescriptionData, certificateData }) => {
-  const api = useApi();
-  const [encounter, setEncounter] = useState({});
-  const [patient, setPatient] = useState({});
-  const [patientAdditionalData, setPatientAdditionalData] = useState({});
-  const [village, setVillage] = useState({});
+export const PrescriptionPrintout = React.memo(
+  ({ patientData, prescriptionData, encounterData, certificateData, isLoading }) => {
+    const {
+      firstName,
+      lastName,
+      dateOfBirth,
+      sex,
+      displayId,
+      additionalData,
+      village,
+    } = patientData;
+    const { streetVillage } = additionalData;
+    const { name: villageName } = village;
+    const { title, subTitle, logo } = certificateData;
+    const {
+      prescriber,
+      medication,
+      route,
+      prescription,
+      quantity,
+      repeats,
+      date,
+      note,
+    } = prescriptionData;
 
-  useEffect(() => {
-    (async () => {
-      if (prescriptionData.encounterId) {
-        const res = await api.get(`encounter/${prescriptionData.encounterId}`);
-        setEncounter(res);
-      }
-    })();
-  }, [api, prescriptionData.encounterId]);
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
 
-  useEffect(() => {
-    (async () => {
-      if (encounter.patientId) {
-        const res = await api.get(`patient/${encounter.patientId}`);
-        setPatient(res);
-      }
-    })();
-  }, [api, encounter.patientId]);
-
-  useEffect(() => {
-    (async () => {
-      if (encounter.patientId) {
-        const res = await api.get(`patient/${encounter.patientId}/additionalData`);
-        setPatientAdditionalData(res);
-      }
-    })();
-  }, [api, encounter.patientId]);
-
-  useEffect(() => {
-    (async () => {
-      if (patient.villageId) {
-        const res = await api.get(`referenceData/${encodeURIComponent(patient.villageId)}`);
-        setVillage(res);
-      }
-    })();
-  }, [api, patient.villageId]);
-
-  const { firstName, lastName, dateOfBirth, sex, displayId } = patient;
-  const { streetVillage } = patientAdditionalData;
-  const { name: villageName } = village;
-  const { title, subTitle, logo } = certificateData;
-  const {
-    prescriber,
-    medication,
-    route,
-    prescription,
-    quantity,
-    repeats,
-    date,
-    note,
-  } = prescriptionData;
-
-  return (
-    <CertificateWrapper>
-      <PrintLetterhead title={title} subTitle={subTitle} logoSrc={logo} pageTitle="Prescription" />
-      <RowContainer>
-        <div>
-          <LocalisedLabel name="firstName">{firstName}</LocalisedLabel>
-          <LocalisedLabel name="lastName">{lastName}</LocalisedLabel>
-          <LocalisedLabel name="dateOfBirth">
-            <DateDisplay date={dateOfBirth} showDate={false} showExplicitDate />
-          </LocalisedLabel>
-          <LocalisedLabel name="sex">{sex}</LocalisedLabel>
-          <LocalisedLabel name="streetVillage">{streetVillage}</LocalisedLabel>
-        </div>
-        <div>
-          <LocalisedLabel name="villageName">{villageName}</LocalisedLabel>
-          <LocalisedLabel name="displayId">{displayId}</LocalisedLabel>
-          <PatientBarcode patient={patient} barWidth={2} barHeight={60} margin={0} />
-        </div>
-      </RowContainer>
-      <GridTable
-        data={{
-          Date: date ? moment(date).format('DD/MM/YYYY') : null,
-          Prescriber: prescriber.displayName,
-          'Prescriber ID': prescriber.id,
-          Facility: encounter?.location?.Facility?.name,
-          Medication: medication.name,
-          Instructions: prescription,
-          Route: route,
-          Quantity: quantity,
-          Repeats: repeats,
-        }}
-      />
-      <NotesSection notes={[{ content: note }]} />
-      <Text>Signed:</Text>
-      <SignatureBox />
-    </CertificateWrapper>
-  );
-});
+    return (
+      <CertificateWrapper>
+        <PrintLetterhead
+          title={title}
+          subTitle={subTitle}
+          logoSrc={logo}
+          pageTitle="Prescription"
+        />
+        <RowContainer>
+          <div>
+            <LocalisedLabel name="firstName">{firstName}</LocalisedLabel>
+            <LocalisedLabel name="lastName">{lastName}</LocalisedLabel>
+            <LocalisedLabel name="dateOfBirth">
+              <DateDisplay date={dateOfBirth} showDate={false} showExplicitDate />
+            </LocalisedLabel>
+            <LocalisedLabel name="sex">{sex}</LocalisedLabel>
+            <LocalisedLabel name="streetVillage">{streetVillage}</LocalisedLabel>
+          </div>
+          <div>
+            <LocalisedLabel name="villageName">{villageName}</LocalisedLabel>
+            <LocalisedLabel name="displayId">{displayId}</LocalisedLabel>
+            <PatientBarcode patient={patientData} barWidth={2} barHeight={60} margin={0} />
+          </div>
+        </RowContainer>
+        <GridTable
+          data={{
+            Date: date ? moment(date).format('DD/MM/YYYY') : null,
+            Prescriber: prescriber?.displayName,
+            'Prescriber ID': prescriber?.id,
+            Facility: encounterData?.location?.Facility?.name,
+            Medication: medication.name,
+            Instructions: prescription,
+            Route: route,
+            Quantity: quantity,
+            Repeats: repeats,
+          }}
+        />
+        <NotesSection notes={[{ content: note }]} />
+        <Text>Signed:</Text>
+        <SignatureBox />
+      </CertificateWrapper>
+    );
+  },
+);
--- packages/desktop/app/forms/MedicationForm.js
+++ packages/desktop/app/forms/MedicationForm.js
@@ -19,6 +19,7 @@ import {
 import { Modal } from '../components/Modal';
 import { useCertificate } from '../utils/useCertificate';
 import { PrescriptionPrintout } from '../components/PatientPrinting/PrescriptionPrintout';
+import { useApi } from '../api';
 
 const drugRouteOptions = [
   { label: 'Dermal', value: 'dermal' },
@@ -66,11 +67,70 @@ const DiscontinuePrintButtonRow = styled.div`
 
 const PrintModal = ({ medication, open, onClose }) => {
   const certificateData = useCertificate();
+  const api = useApi();
+  const [encounter, setEncounter] = useState({});
+  const [patient, setPatient] = useState({});
+  const [additionalData, setAdditionalData] = useState({});
+  const [village, setVillage] = useState({});
+  const [encounterLoading, setEncounterLoading] = useState(false);
+  const [patientLoading, setPatientLoading] = useState(false);
+  const [additionalDataLoading, setAdditionalDataLoading] = useState(false);
+  const [villageLoading, setVillageLoading] = useState(false);
+
+  useEffect(() => {
+    setEncounterLoading(true);
+    (async () => {
+      if (medication.encounterId) {
+        const res = await api.get(`encounter/${medication.encounterId}`);
+        setEncounter(res);
+      }
+    })();
+    setEncounterLoading(false);
+  }, [api, medication.encounterId]);
+
+  useEffect(() => {
+    setPatientLoading(true);
+    (async () => {
+      if (encounter.patientId) {
+        const res = await api.get(`patient/${encounter.patientId}`);
+        setPatient(res);
+      }
+    })();
+    setPatientLoading(false);
+  }, [api, encounter.patientId]);
+
+  useEffect(() => {
+    setAdditionalDataLoading(true);
+    (async () => {
+      if (encounter.patientId) {
+        const res = await api.get(`patient/${encounter.patientId}/additionalData`);
+        setAdditionalData(res);
+      }
+    })();
+    setAdditionalDataLoading(false);
+  }, [api, encounter.patientId]);
+
+  useEffect(() => {
+    setVillageLoading(true);
+    (async () => {
+      if (patient.villageId) {
+        const res = await api.get(`referenceData/${encodeURIComponent(patient.villageId)}`);
+        setVillage(res);
+      }
+    })();
+    setVillageLoading(false);
+  }, [api, patient.villageId]);
 
   return (
     <>
       <Modal title="Prescription" open={open} onClose={onClose} width="md" printable>
-        <PrescriptionPrintout prescriptionData={medication} certificateData={certificateData} />
+        <PrescriptionPrintout
+          patientData={{ ...patient, additionalData, village }}
+          prescriptionData={medication}
+          encounterData={encounter}
+          certificateData={certificateData}
+          isLoading={encounterLoading || patientLoading || additionalDataLoading || villageLoading}
+        />
       </Modal>
     </>
   );
