9add8d06a8f1db8450dac189d59561bf5188fbf2
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-05-29 22:34:22 -0500

    Add closable functionality to additional data patient sections

--- packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/PatientSection.tsx
+++ packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/PatientSection.tsx
@@ -3,23 +3,39 @@ import { StyledView, RowView } from '/styled/common';
 import { SectionHeader } from '/components/SectionHeader';
 import { EditButton } from './EditButton';
 import { theme } from '/styled/theme';
+import { ArrowButton } from './ArrowButton';
 
 interface PatientDetailSectionProps {
   title: string;
   onEdit?: () => void;
+  isClosable?: boolean;
 }
 
 export const PatientSection = ({
   title,
   onEdit,
+  isClosable = false,
   children,
 }: PropsWithChildren<PatientDetailSectionProps>): ReactElement => {
+  // Closable sections should be closed by default
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSection = (): void => {
+    setIsOpen(prevValue => !prevValue);
+  }
+
   const OverlapeddButton = (
     <StyledView alignItems="flex-end">
       <StyledView position="absolute" paddingTop={10} paddingRight={20}>
         <EditButton sectionTitle={title} onPress={onEdit} />
       </StyledView>
     </StyledView>
+  );
+
+  const content = (
+    <>
+      {onEdit && OverlapeddButton}
+      {children}
+    </>
   );
 
   return (
@@ -31,11 +47,9 @@ export const PatientSection = ({
         padding={20}
       >
         <SectionHeader h1>{title}</SectionHeader>
+        {isClosable && <ArrowButton isOpen={isOpen} sectionTitle={title} onPress={toggleSection} />}
       </RowView>
-      <>
-        {onEdit && OverlapeddButton}
-        {children}
-      </>
+      {!isClosable || isOpen ? content : null}
     </StyledView>
   );
 };
