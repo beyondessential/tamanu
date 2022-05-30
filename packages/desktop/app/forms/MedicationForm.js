f9e288ba0658b0427516d8cc3eccb21cbed1a25c
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-05-30 15:02:57 +1200

    WAITM-166 Move PrescriptionPrintModal to separate file

--- packages/desktop/app/forms/MedicationForm.js
+++ packages/desktop/app/forms/MedicationForm.js
@@ -16,11 +16,7 @@ import {
   NumberField,
   DateField,
 } from '../components/Field';
-import { Modal } from '../components/Modal';
-import { useCertificate } from '../utils/useCertificate';
-import { LoadingIndicator } from '../components/LoadingIndicator';
-import { PrescriptionPrintout } from '../components/PatientPrinting/PrescriptionPrintout';
-import { useApi } from '../api';
+import { PrescriptionPrintModal } from '../components/PatientPrinting/PrescriptionPrintModal';
 
 const drugRouteOptions = [
   { label: 'Dermal', value: 'dermal' },
@@ -65,80 +61,6 @@ const DiscontinuePrintButtonRow = styled.div`
   grid-template-columns: 8rem auto 8rem 8rem;
   grid-column: -1 / 1;
 `;
-
-const PrintModal = ({ medication, open, onClose }) => {
-  const certificateData = useCertificate();
-  const api = useApi();
-  const [encounter, setEncounter] = useState({});
-  const [patient, setPatient] = useState({});
-  const [additionalData, setAdditionalData] = useState({});
-  const [village, setVillage] = useState({});
-  const [encounterLoading, setEncounterLoading] = useState(false);
-  const [patientLoading, setPatientLoading] = useState(false);
-  const [additionalDataLoading, setAdditionalDataLoading] = useState(false);
-  const [villageLoading, setVillageLoading] = useState(false);
-
-  useEffect(() => {
-    setEncounterLoading(true);
-    (async () => {
-      if (medication.encounterId) {
-        const res = await api.get(`encounter/${medication.encounterId}`);
-        setEncounter(res);
-      }
-    })();
-    setEncounterLoading(false);
-  }, [api, medication.encounterId]);
-
-  useEffect(() => {
-    setPatientLoading(true);
-    (async () => {
-      if (encounter.patientId) {
-        const res = await api.get(`patient/${encounter.patientId}`);
-        setPatient(res);
-      }
-    })();
-    setPatientLoading(false);
-  }, [api, encounter.patientId]);
-
-  useEffect(() => {
-    setAdditionalDataLoading(true);
-    (async () => {
-      if (encounter.patientId) {
-        const res = await api.get(`patient/${encounter.patientId}/additionalData`);
-        setAdditionalData(res);
-      }
-    })();
-    setAdditionalDataLoading(false);
-  }, [api, encounter.patientId]);
-
-  useEffect(() => {
-    setVillageLoading(true);
-    (async () => {
-      if (patient.villageId) {
-        const res = await api.get(`referenceData/${encodeURIComponent(patient.villageId)}`);
-        setVillage(res);
-      }
-    })();
-    setVillageLoading(false);
-  }, [api, patient.villageId]);
-
-  return (
-    <>
-      <Modal title="Prescription" open={open} onClose={onClose} width="md" printable>
-        {encounterLoading || patientLoading || additionalDataLoading || villageLoading ? (
-          <LoadingIndicator />
-        ) : (
-          <PrescriptionPrintout
-            patientData={{ ...patient, additionalData, village }}
-            prescriptionData={medication}
-            encounterData={encounter}
-            certificateData={certificateData}
-          />
-        )}
-      </Modal>
-    </>
-  );
-};
 
 export const MedicationForm = React.memo(
   ({
@@ -357,7 +279,7 @@ export const MedicationForm = React.memo(
             </FormGrid>
           )}
         />
-        <PrintModal
+        <PrescriptionPrintModal
           medication={submittedMedication || medication}
           open={printModalOpen}
           onClose={() => {
