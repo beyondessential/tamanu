18a14227c3dbd398551e04ed710093808083eff9
Tri Nguyen <tri@tridnguyen.com>
2022-05-31 00:53:10 -0400

    Create unique indexes for permissions

--- packages/shared-src/src/models/Permission.js
+++ packages/shared-src/src/models/Permission.js
@@ -26,6 +26,28 @@ export class Permission extends Model {
             resetPermissionCache();
           },
         },
+        indexes: [
+          {
+            name: 'permissions_role_id_noun_verb',
+            unique: true,
+            fields: ['role_id', 'noun', 'verb'],
+            where: {
+              object_id: {
+                [Sequelize.Op.eq]: null,
+              },
+            },
+          },
+          {
+            name: 'permissions_role_id_noun_verb_objectId',
+            unique: true,
+            fields: ['role_id', 'noun', 'verb', 'object_id'],
+            where: {
+              object_id: {
+                [Sequelize.Op.ne]: null,
+              },
+            },
+          },
+        ],
       },
     );
   }
