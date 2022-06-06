87412caf4d10e77be55b93c8d1d1581564c95872
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-06-06 18:57:21 -0500

    Rename parameter to match the rest of the codebase

--- packages/lan/app/utils/getFilteredListByPermission.js
+++ packages/lan/app/utils/getFilteredListByPermission.js
@@ -1,9 +1,9 @@
 /**
   ability: a CASL ability with the permissions that the user has
   modelRecords: array of sequelize model records
-  verb: string
+  action: string
 */
-export function getFilteredListByPermission(ability, modelRecords, verb) {
+export function getFilteredListByPermission(ability, modelRecords, action) {
   // For some reason the endpoint is not passing a list of records
   if (Array.isArray(modelRecords) === false) {
     throw new Error('The function filterByPermission expects modelRecords to be an array.');
@@ -11,5 +11,5 @@ export function getFilteredListByPermission(ability, modelRecords, verb) {
 
   // Only return records that the role has access to, check with the actual
   // model so the 'can' function can read the model name and match properly.
-  return modelRecords.filter(row => ability.can(verb, row));
+  return modelRecords.filter(row => ability.can(action, row));
 }
