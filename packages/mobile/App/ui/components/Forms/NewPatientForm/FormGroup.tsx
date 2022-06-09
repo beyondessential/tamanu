5cc475c48c410eed343eaae237b61472a9ec4309
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-06-09 17:38:55 -0500

    Stop using fixed height to display fields that might grow/shrink

--- packages/mobile/App/ui/components/Forms/NewPatientForm/FormGroup.tsx
+++ packages/mobile/App/ui/components/Forms/NewPatientForm/FormGroup.tsx
@@ -13,26 +13,18 @@ export const FormGroup = ({
   sectionName,
   children,
   marginTop,
-}: FormGroupProps): ReactElement => {
-  const height = useMemo(() => {
-    const count = React.Children.count(children);
-    return count > 0
-      ? screenPercentageToDP(count * 7.29, Orientation.Height)
-      : screenPercentageToDP(7.29, Orientation.Height);
-  }, [children]);
-  return (
-    <StyledView
-      marginTop={marginTop ? screenPercentageToDP(2.43, Orientation.Height) : 0}
-    >
-      <StyledView marginBottom={screenPercentageToDP(0.6, Orientation.Height)}>
-        <SectionHeader h3>{sectionName}</SectionHeader>
-      </StyledView>
-      <StyledView height={height} justifyContent="space-between">
-        {children}
-      </StyledView>
+}: FormGroupProps): ReactElement => (
+  <StyledView
+    marginTop={marginTop ? screenPercentageToDP(2.43, Orientation.Height) : 0}
+  >
+    <StyledView marginBottom={screenPercentageToDP(0.6, Orientation.Height)}>
+      <SectionHeader h3>{sectionName}</SectionHeader>
     </StyledView>
-  );
-};
+    <StyledView justifyContent="space-between">
+      {children}
+    </StyledView>
+  </StyledView>
+);
 
 FormGroup.defaultProps = {
   marginTop: null,
