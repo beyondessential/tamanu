3e397a5a67ca2a5f263256c04dabee89d7d74b7b
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-06-09 16:01:52 +1200

    Add comment explaining unusual naming

--- packages/lan/app/routes/apiv1/triage.js
+++ packages/lan/app/routes/apiv1/triage.js
@@ -34,6 +34,8 @@ triage.post(
       });
     }
 
+    // The triage form groups notes as a single string for submission
+    // so put it into a single note record
     if (notes) {
       await triageRecord.createNote({
         noteType: NOTE_TYPES.OTHER,
