9b90bcf12ad4546f0cf4fb9c716fa4259c04a628
Ethan McQuarrie <69437175+EMcQ-BES@users.noreply.github.com>
2022-06-09 14:40:05 +1200

    Minor print fixups (#2316)
    * Pull lab request facility from encounter data
    
    * Fixup issue with medication finalise not validating correctly
    
    * Add onSuccess to form, fixup medication submission process

--- packages/desktop/app/components/PatientPrinting/LabRequestPrintout.js
+++ packages/desktop/app/components/PatientPrinting/LabRequestPrintout.js
@@ -25,7 +25,7 @@ export const LabRequestPrintout = React.memo(
         tableData={{
           'Test ID': displayId,
           'Request date': requestedDate ? moment(requestedDate).format('DD/MM/YYYY') : null,
-          Facility: laboratory?.name,
+          Facility: encounterData?.location?.facility?.name,
           Department: encounterData?.department?.name,
           'Requested by': requestedBy?.displayName,
           'Sample time': sampleTime ? moment(sampleTime).format('DD/MM/YYYY hh:mm a') : null,
