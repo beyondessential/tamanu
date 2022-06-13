5dd98c8f50712d52e0abb7a1e73330a4122c25c4
Félix Saparelli <felix@passcod.name>
2022-06-14 11:04:16 +1200

    Use values from adm vaccine in data table

--- packages/shared-src/src/models/Patient.js
+++ packages/shared-src/src/models/Patient.js
@@ -105,6 +105,14 @@ export class Patient extends Model {
           as: 'scheduledVaccine',
           include: models.ScheduledVaccine.getListReferenceAssociations(),
         },
+        {
+          model: models.User,
+          as: 'giver',
+        },
+        {
+          model: models.User,
+          as: 'recorder',
+        },
       ],
     });
 
