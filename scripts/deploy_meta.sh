40e4d994208efdcab3a0fe12ff0d14a068dd5486
Tim Kennedy <t.m.knndy@gmail.com>
2022-06-20 16:56:16 +1000

    Fix deploy scripts

--- scripts/deploy_meta.sh
+++ scripts/deploy_meta.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ENV_SUFFIX="${1:?Must pass an environment suffix like dev or staging}"
-codeship_aws eb_deploy "./packages/meta-server/release-nodejs" "$META_SERVER_EB_APP" "${META_SERVER_EB_APP}-${ENV_SUFFIX}" "${META_SERVER_EB_S3}/${ENV_SUFFIX}"
+"$(dirname $0)/deploy_eb.sh" "./packages/meta-server/release-nodejs" "$META_SERVER_EB_APP" "$META_SERVER_EB_APP-$ENV_SUFFIX" "$META_SERVER_EB_S3/$ENV_SUFFIX"
--- scripts/deploy_sync.sh
+++ scripts/deploy_sync.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ENV_SUFFIX="${1:?Must pass an environment suffix like dev or staging}"
-./deploy_eb.sh "./packages/sync-server/release-nodejs" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_APP-$ENV_SUFFIX" "$SYNC_SERVER_EB_S3/$ENV_SUFFIX"
+"$(dirname $0)/deploy_eb.sh" "./packages/sync-server/release-nodejs" "$SYNC_SERVER_EB_APP" "$SYNC_SERVER_EB_APP-$ENV_SUFFIX" "$SYNC_SERVER_EB_S3/$ENV_SUFFIX"
