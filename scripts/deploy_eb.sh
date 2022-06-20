ee734d3347a8d21540dfd4d492787a35931faa49
Tim Kennedy <t.m.knndy@gmail.com>
2022-06-20 15:37:56 +1000

    Wait for up to 5min for EB envs to become healthy

---
+++ scripts/deploy_eb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euxo pipefail
+
+CODE_PATH="${1:?Must pass the path of the code to deploy}"
+EB_APP="${2:?Must pass an EB application}"
+EB_ENV="${3:?Must pass an EB environment}"
+S3_PATH="${4:?Must pass an S3 path}"
+
+codeship_aws eb_deploy "$CODE_PATH" "$EB_APP" "$EB_ENV" "$S3_PATH" ||
+    ./eb_wait_until_healthy.sh "$EB_APP" "$EB_ENV"
