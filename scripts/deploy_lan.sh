69fbcb93371ed6b57b0276e139302374ba9fc238
Tim Kennedy <t.m.knndy@gmail.com>
2022-06-20 17:41:50 +1000

    Fix lan release path

--- scripts/deploy_lan.sh
+++ scripts/deploy_lan.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 
 ENV_SUFFIX="${1:?Must pass an environment suffix like dev or staging}"
-"$(dirname $0)/deploy_eb.sh" lan "$LAN_SERVER_EB_APP" "$LAN_SERVER_EB_APP-$ENV_SUFFIX" "$LAN_SERVER_EB_S3/$ENV_SUFFIX"
+"$(dirname $0)/deploy_eb.sh" "./packages/lan/release-nodejs" "$LAN_SERVER_EB_APP" "$LAN_SERVER_EB_APP-$ENV_SUFFIX" "$LAN_SERVER_EB_S3/$ENV_SUFFIX"
