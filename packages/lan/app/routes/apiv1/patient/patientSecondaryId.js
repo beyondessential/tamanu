b2d914563d26ba77e4b32975787f264ec80a4269
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-06-30 18:02:47 -0500

    Co-authored by: Tim Kennedy. Check permissions against actual object

--- packages/lan/app/routes/apiv1/patient/patientSecondaryId.js
+++ packages/lan/app/routes/apiv1/patient/patientSecondaryId.js
@@ -8,10 +8,17 @@ patientSecondaryIdRoutes.get(
   '/:id/secondaryId',
   asyncHandler(async (req, res) => {
     const { models, params } = req;
-    req.checkPermission('read', 'Patient');
+    const patient = await models.Patient.findByPk(params.id);
+    if (!patient) throw new NotFoundError();
+    req.checkPermission('read', patient);
 
     const { rows, count } = await models.PatientSecondaryId.findAndCountAll({
       where: { patientId: params.id },
+    });
+
+    // Check read permissions on every patient secondary ID
+    rows.forEach(secondaryId => {
+      req.checkPermission('read', secondaryId);
     });
 
     res.send({
@@ -25,10 +32,11 @@ patientSecondaryIdRoutes.put(
   '/:id/secondaryId/:secondaryIdId',
   asyncHandler(async (req, res) => {
     const { models, params } = req;
-    req.checkPermission('read', 'PatientSecondaryId');
     const secondaryId = await models.PatientSecondaryId.findByPk(params.secondaryIdId);
     if (!secondaryId) throw new NotFoundError();
+    req.checkPermission('read', secondaryId);
     req.checkPermission('write', secondaryId);
+
     await secondaryId.update(req.body);
     res.send(secondaryId);
   }),
@@ -38,12 +46,10 @@ patientSecondaryIdRoutes.post(
   '/:id/secondaryId',
   asyncHandler(async (req, res) => {
     const { models, params } = req;
-    req.checkPermission('read', 'Patient');
-
     const patient = await models.Patient.findByPk(params.id);
-    if (!patient) {
-      throw new NotFoundError();
-    }
+    if (!patient) throw new NotFoundError();
+    req.checkPermission('read', patient);
+    req.checkPermission('create', 'PatientSecondaryId');
 
     const secondaryId = await models.PatientSecondaryId.create({
       value: req.body.value,
