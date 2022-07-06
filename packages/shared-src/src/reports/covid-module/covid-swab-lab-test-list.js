28591beb99ba7d4fbfd29fa3f448c654fb5a5f33
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2022-07-05 23:40:58 -0500

    TAN-1537: Modify Nauru COVID swab report (#2439)
    * Use correct key and accessor for sample collection time
    
    * Add sample collection date to the report
    
    * Apply default fromDate filter ONLY for nauru report
    
    * Add new column to test and fix expected value

--- packages/shared-src/src/reports/covid-module/covid-swab-lab-test-list.js
+++ packages/shared-src/src/reports/covid-module/covid-swab-lab-test-list.js
@@ -1,3 +1,4 @@
+import { subDays } from 'date-fns';
 import { groupBy } from 'lodash';
 import { Op } from 'sequelize';
 import moment from 'moment';
@@ -52,6 +53,15 @@ const parametersToLabTestSqlWhere = parameters => {
 };
 
 const parametersToSurveyResponseSqlWhere = (parameters, { surveyId }) => {
+  // In the meantime, only apply the default to the nauru report.
+  // Then, it will just be a matter of removing that check to include in all
+  // other covid swab reports.
+  const NAURU_SURVEY_ID = 'program-naurucovid19-naurucovidtestregistration';
+  if (surveyId === NAURU_SURVEY_ID && !parameters.fromDate) {
+    // eslint-disable-next-line no-param-reassign
+    parameters.fromDate = subDays(new Date(), 30).toISOString();
+  }
+
   const defaultWhereClause = {
     '$surveyResponse.survey_id$': surveyId,
   };
