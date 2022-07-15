9e05ae829e04cab6b60df1e35ee1d827f6917985
Dan Nash <danielnash100@gmail.com>
2022-07-15 17:07:39 +1200

    Working migration

--- packages/shared-src/src/migrations/082_renameLabTestTypeQuestionType.ts
+++ packages/shared-src/src/migrations/082_renameLabTestTypeQuestionType.ts
@@ -1,18 +1,29 @@
 import { QueryInterface, ENUM, STRING } from 'sequelize';
 
+const PREVIOUS_ENUM_VALUES = {
+  NUMBER: 'number',
+  STRING: 'string',
+};
+
+const CORRECTED_VALUES = {
+  NUMBER: 'Number',
+  FREE_TEXT: 'FreeText',
+  SELECT: 'Select',
+};
+
 export async function up(query: QueryInterface) {
   await query.changeColumn('lab_test_types', 'question_type', {
-    type: 'STRING USING CAST("question_type" as STRING)',
-    allowNull: true,
-    defaultValue: 'FreeText',
+    type: STRING,
+    allowNull: false,
   });
   await query.sequelize.query('DROP TYPE IF EXISTS "enum_lab_test_types_question_type";');
   await query.renameColumn('lab_test_types', 'question_type', 'result_type');
+  // Correct the type values
   await query.sequelize.query(
     `UPDATE lab_test_types SET result_type = CASE result_type 
-      WHEN 'number' THEN 'Number' 
-      WHEN 'string' THEN 'FreeText'
-      END; 
+     WHEN '${PREVIOUS_ENUM_VALUES.NUMBER}' THEN '${CORRECTED_VALUES.NUMBER}' 
+     WHEN '${PREVIOUS_ENUM_VALUES.STRING}' THEN '${CORRECTED_VALUES.FREE_TEXT}'
+     END
     `,
   );
 }
@@ -20,15 +31,14 @@ export async function up(query: QueryInterface) {
 export async function down(query: QueryInterface) {
   await query.sequelize.query(
     `UPDATE lab_test_types SET result_type = CASE result_type 
-      WHEN 'Number' THEN 'number' 
-      WHEN 'FreeText' THEN 'string'
-      END; 
+     WHEN '${CORRECTED_VALUES.NUMBER}' THEN '${PREVIOUS_ENUM_VALUES.NUMBER}' 
+     WHEN '${CORRECTED_VALUES.FREE_TEXT}' THEN '${PREVIOUS_ENUM_VALUES.STRING}'
+     END
     `,
   );
   await query.renameColumn('lab_test_types', 'result_type', 'question_type');
   await query.changeColumn('lab_test_types', 'question_type', {
-    type: `ENUM USING CAST("question_type" as ENUM)`,
-    values: ['number', 'string'],
-    allowNull: true
+    type: ENUM(...Object.values(PREVIOUS_ENUM_VALUES)),
+    allowNull: false,
   });
 }
