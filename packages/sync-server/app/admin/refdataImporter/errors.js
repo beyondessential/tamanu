da6877b3988bb9ee3e2cb7856bbec90ce8d4ecf1
Félix Saparelli <felix@passcod.name>
2022-07-23 21:13:08 +1200

    Improve error message

--- packages/sync-server/app/admin/refdataImporter/errors.js
+++ packages/sync-server/app/admin/refdataImporter/errors.js
@@ -1,9 +1,9 @@
 export class DataImportError extends Error {
     constructor(sheetName, rowNumber, error) {
         if (typeof error === 'string') {
-            super(`${error} on ${sheetName}:${rowNumber}`);
+            super(`${error} on ${sheetName} at row ${rowNumber + 1}`);
         } else if (error instanceof Error) {
-            super(`${error.message} on ${sheetName}:${rowNumber}`);
+            super(`${error.message} on ${sheetName} at row ${rowNumber + 1}`);
             this.previous = error;
         } else {
             throw new Error('DEV ERROR: pass either a string or Error error');
@@ -17,7 +17,7 @@ export class UpstertionError extends DataImportError {}
 export class ValidationError extends DataImportError {}
 export class WorkSheetError extends DataImportError {}
 
-class DryRun extends Error {
+export class DryRun extends Error {
   constructor() {
     super('Dry run: rollback');
   }
