efb3e422d02e0e0d875a3ee5eabbe9dd9c51bd91
Félix Saparelli <felix@passcod.name>
2022-07-25 02:02:13 +1200

    Resolve foreign keys

--- packages/sync-server/app/admin/refdataImporter/sheet.js
+++ packages/sync-server/app/admin/refdataImporter/sheet.js
@@ -1,3 +1,4 @@
+import { camelCase, lowerCase, lowerFirst, startCase, upperFirst } from 'lodash';
 import { utils } from 'xlsx';
 import { ValidationError as YupValidationError } from 'yup';
 
@@ -10,6 +11,37 @@ import {
 } from './errors';
 import * as schemas from './schemas';
 import { newStatsRow } from './stats';
+
+// lowerCamelCase are refdata types
+// UpperCamelCase are other models
+const FOREIGN_KEY_FIELDS = {
+  CertifiableVaccine: 'vaccine',
+  Department: 'Facility',
+  LabTestType: 'labTestCategory',
+  Location: 'Facility',
+  Patient: 'village',
+  Permission: 'Role',
+  ScheduledVaccine: 'vaccine',
+};
+
+function isRefData(kind) {
+  return kind[0] === kind[0].toLowerCase();
+}
+
+function findFieldName(values, fkField) {
+  const fkFieldLower = foreignKeyField.toLowerCase();
+  const fkFieldCamel = camelCase(foreignKeyField);
+  const fkFieldUcfirst = upperFirst(foreignKeyField);
+  const fkFieldSplit = lowerCase(foreignKeyField);
+  const fkFieldSplitUcwords = startCase(fkFieldSplit);
+  if (values[fkField]) return fkField;
+  if (values[fkFieldLower]) return fkFieldLower;
+  if (values[fkFieldCamel]) return fkFieldCamel;
+  if (values[fkFieldUcfirst]) return fkFieldUcfirst;
+  if (values[fkFieldSplit]) return fkFieldSplit;
+  if (values[fkFieldSplitUcwords]) return fkFieldSplitUcwords;
+  return null;
+}
 
 export async function importSheet({ errors, log, models }, { loader, sheetName, sheet }) {
   const stats = {};
@@ -36,10 +68,67 @@ export async function importSheet({ errors, log, models }, { loader, sheetName, 
     }
   }
 
+  log.debug('Building reverse lookup table');
+  const lookup = new Map();
+  for (const {
+    model,
+    values: { id, type = null, name = null },
+  } of tableRows) {
+    if (!id) continue;
+    const kind = model === 'ReferenceData' ? type : model;
+    lookup.set({ kind, id }, null);
+    if (name) lookup.set({ kind, name: name.toLowerCase() }, id);
+  }
+
   log.debug('Resolving foreign keys', { rows: tableRows.length });
   const resolvedRows = [];
   for (const { model, sheetRow, values } of tableRows) {
     try {
+      const foreignKeyField = FOREIGN_KEY_FIELDS[model];
+      if (foreignKeyField) {
+        const fkFieldName = findFieldName(values, foreignKeyField);
+        if (fkFieldName) {
+          const fkFieldValue = values[fkFieldName];
+          const fkNameLowerId = `${lowerFirst(foreignKeyField)}Id`;
+
+          const hasLocalId = lookup.has({ kind: foreignKeyField, id: fkFieldValue });
+          const idByLocalName = lookup.get({
+            kind: foreignKeyField,
+            name: fkFieldValue.toLowerCase(),
+          });
+
+          if (hasLocalId) {
+            delete values[fkFieldName];
+            values[fkNameLowerId] = fkFieldValue;
+          } else if (idByLocalName) {
+            delete values[fkFieldName];
+            values[fkNameLowerId] = idByLocalName;
+          } else {
+            const hasRemoteId =
+              (isRefData(foreignKeyField)
+                ? await models.ReferenceData.count({
+                    where: { type: foreignKeyField, id: fkFieldValue },
+                  })
+                : await models[model].count({ where: { id: fkFieldValue } })) > 0;
+
+            const idByRemoteName = (isRefData(foreignKeyField)
+              ? await models.ReferenceData.findOne({
+                  where: { type: foreignKeyField, name: fkFieldValue.toLowerCase() },
+                })
+              : await models[model].findOne({ where: { name: fkFieldValue.toLowerCase() } })
+            )?.id;
+
+            if (hasRemoteId) {
+              delete values[fkFieldName];
+              values[fkNameLowerId] = fkFieldValue;
+            } else if (idByRemoteName) {
+              delete values[fkFieldName];
+              values[fkNameLowerId] = idByRemoteName;
+            }
+          }
+        }
+      }
+
       resolvedRows.push({ model, sheetRow, values });
     } catch (err) {
       stats[model].errored += 1;
