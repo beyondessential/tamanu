89ddab6f48fd15a0c4f3f08b772f04963200f5e2
Tom Caiger <caigertom@gmail.com>
2022-07-28 15:22:08 +1200

    save lab request dates as strings

--- packages/desktop/app/components/Field/DateField.js
+++ packages/desktop/app/components/Field/DateField.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import styled from 'styled-components';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import { TextInput } from './TextField';
+import { toDateTimeString } from '../../utils/dateTime';
 
 // This component is pretty tricky! It has to keep track of two layers of state:
 //
@@ -39,6 +39,7 @@ export const DateInput = ({
   name,
   placeholder,
   max = '9999-12-31',
+  saveDateAsString = false,
   ...props
 }) => {
   const [currentText, setCurrentText] = useState(fromRFC3339(value, format));
@@ -46,15 +47,17 @@ export const DateInput = ({
   const onValueChange = useCallback(
     event => {
       const formattedValue = event.target.value;
-      const rfcValue = toRFC3339(formattedValue, format);
+      const outputValue = saveDateAsString
+        ? toDateTimeString(formattedValue)
+        : toRFC3339(formattedValue, format);
 
       setCurrentText(formattedValue);
-      if (rfcValue === 'Invalid date') {
+      if (outputValue === 'Invalid date') {
         onChange({ target: { value: '', name } });
         return;
       }
 
-      onChange({ target: { value: rfcValue, name } });
+      onChange({ target: { value: outputValue, name } });
     },
     [onChange, format, name],
   );
