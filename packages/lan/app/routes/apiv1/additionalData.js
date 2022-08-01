fe46d8a96c07fd3da3cbe57038690a6c3ea35ade
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-01 18:44:35 -0500

    Co-authored by: Tom McLean. Rewrite PAD endpoints to stop checking permission against it

--- packages/lan/app/routes/apiv1/additionalData.js
+++ packages/lan/app/routes/apiv1/additionalData.js
@@ -1,9 +1,56 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
-import { simpleGet, simplePut, simplePost } from './crudHelpers';
+import { NotFoundError } from 'shared/errors';
 
 export const additionalData = express.Router();
 
-additionalData.get('/:id', simpleGet('PatientAdditionalData'));
-additionalData.put('/:id', simplePut('PatientAdditionalData'));
-additionalData.post('/$', simplePost('PatientAdditionalData'));
+/*
+  PatientAdditionalData is just a backend distinction
+  with Patient, so the permission check should just
+  be against Patient.
+
+  pad = PatientAdditionalData
+*/
+additionalData.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const {
+      models: { PatientAdditionalData },
+      params,
+    } = req;
+    req.checkPermission('read', 'Patient');
+    const pad = await PatientAdditionalData.findByPk(params.id, {
+      include: PatientAdditionalData.getFullReferenceAssociations(),
+    });
+    if (!pad) throw new NotFoundError();
+    res.send(pad);
+  }),
+);
+
+additionalData.put(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const {
+      models: { PatientAdditionalData },
+      params,
+    } = req;
+    req.checkPermission('write', 'Patient');
+    const pad = await PatientAdditionalData.findByPk(params.id);
+    if (!pad) throw new NotFoundError();
+    await pad.update(req.body);
+    res.send(pad);
+  }),
+);
+
+additionalData.post(
+  '/$',
+  asyncHandler(async (req, res) => {
+    const {
+      models: { PatientAdditionalData },
+    } = req;
+    req.checkPermission('create', 'Patient');
+    const pad = await PatientAdditionalData.create(req.body);
+    res.send(pad);
+  }),
+);
