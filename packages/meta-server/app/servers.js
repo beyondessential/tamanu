9d2265f47800052a27421ee734dec07afae09357
Tim Kennedy <tim@beyondessential.com.au>
2022-08-01 15:31:24 +1000

    Fix UAT server entry in meta-server (#2525)

--- packages/meta-server/app/servers.js
+++ packages/meta-server/app/servers.js
@@ -45,9 +45,9 @@ const servers = [
 
   // development servers
   { name: 'Dev', type: 'dev', host: 'https://sync-dev.tamanu.io' },
-  { name: 'Fiji VPS/VRS UAT', type: 'dev', host: 'https://sync-uat-fiji-vps.tamanu.io' },
   { name: 'Staging', type: 'dev', host: 'https://sync-staging.tamanu.io' },
   { name: 'Stress testing', type: 'dev', host: 'https://sync-stress-test.tamanu.io' },
+  { name: 'UAT', type: 'dev', host: 'https://sync-uat.tamanu.io' },
 ];
 
 serversRouter.get('/', (req, res) => {
