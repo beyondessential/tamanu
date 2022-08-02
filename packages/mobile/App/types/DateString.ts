cbd4aac2199d7e41323b97e34a53f2e806e4246e
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-01 21:36:57 -0500

    Quick hack. Comment out template literal declared type until we upgrade typescript

--- packages/mobile/App/types/DateString.ts
+++ packages/mobile/App/types/DateString.ts
@@ -1,1 +1,2 @@
-export type DateString = `${number}-${number}-${number} ${number}:${number}:${number}`;
+// Commented out because we need to upgrade typescript!
+export type DateString = string; //`${number}-${number}-${number} ${number}:${number}:${number}`;
