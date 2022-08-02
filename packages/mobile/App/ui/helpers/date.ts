5fde79f47ca90fd796d03667c7c711db38342052
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-01 20:52:33 -0500

    Co-authored by: Chris Tran. Remove unused helpers. Also use proper type

--- packages/mobile/App/ui/helpers/date.ts
+++ packages/mobile/App/ui/helpers/date.ts
@@ -1,4 +1,5 @@
-import { format, parseISO, differenceInYears, isValid, formatISO9075 } from 'date-fns';
+import { format, parseISO, differenceInYears } from 'date-fns';
+import { DateString } from '~/types';
 
 export function formatDate(date: Date, dateFormat: string): string {
   return format(date, dateFormat);
@@ -9,33 +10,12 @@ export function getAgeFromDate(date: Date): number {
 }
 
 export function formatStringDate(date: string, dateFormat: string): string {
-  const dateValue: Date = parseISO(date); 
+  const dateValue: Date = parseISO(date);
   return formatDate(dateValue, dateFormat);
-}
-
-/*
-  toDateTimeString, getCurrentDateTimeString and getCurrentDateString
-  were created to handle dates as a string type. These are ported from desktop
-  see https://github.com/beyondessential/tamanu/pull/2501/files
-*/
-export function toDateTimeString(date: string):string {
-  const dateObj = new Date(date);
-  if (!isValid(dateObj)) {
-    throw new Error('Not a valid date');
-  }
-  return formatISO9075(dateObj);
-}
-
-export function getCurrentDateTimeString(): string {
-  return formatISO9075(new Date());
-}
-
-export function getCurrentDateString(): string {
-  return formatISO9075(new Date(), { representation: 'date' });
 }
 
 // It seems that some JS implementations have problems
 // parsing strings to dates.
-export function parseISO9075(date: string): Date {
+export function parseISO9075(date: DateString): Date {
   return new Date(date.replace(' ', 'T'));
 }
