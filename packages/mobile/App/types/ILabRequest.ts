82823a1aee255f69fd862e366e36265b2864961a
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-02 00:09:53 -0500

    Remove DateString as it seems TypeORM doesn't handle nice things

--- packages/mobile/App/types/ILabRequest.ts
+++ packages/mobile/App/types/ILabRequest.ts
@@ -4,7 +4,6 @@ import { IEncounter } from './IEncounter';
 import { ILabTest } from './ILabTest';
 import { IReferenceData } from './IReferenceData';
 import { IUser } from './IUser';
-import { DateString } from './DateString';
 
 export enum LabRequestStatus {
   RECEPTION_PENDING = 'reception_pending',
@@ -18,8 +17,8 @@ export const LAB_REQUEST_STATUS_OPTIONS = createDropdownOptionsFromObject(LabReq
 
 export interface ILabRequest {
   id: ID;
-  sampleTime: DateString;
-  requestedDate: DateString;
+  sampleTime: string;
+  requestedDate: string;
   urgent?: boolean;
   specimenAttached?: boolean;
   status?: LabRequestStatus;
@@ -45,8 +44,8 @@ export interface ILabRequest {
 
 export interface IDataRequiredToCreateLabRequest {
   id?: ID; // has default
-  sampleTime?: DateString; // has default
-  requestedDate?: DateString; // has default
+  sampleTime?: string; // has default
+  requestedDate?: string; // has default
   urgent?: boolean;
   specimenAttached?: boolean;
   status?: LabRequestStatus;
