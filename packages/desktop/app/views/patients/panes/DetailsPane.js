baa2234c0f950a3562fd34207fd031b49809da81
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2022-08-01 22:42:06 -0500

    TAN-1652: Remove items from patient state again (#2514)
    * Add permission check for reading patient additional data
    
    * Remove additional data from patient state
    
    * Show a forbidden error if a patient doesnt have permissions to view additional data
    
    * Create special new route to check if current encounter exists
    
    * Check if current encounter exists with new endpoint
    
    * Remove current encounter from patient state
    
    * Display death summary if patient is deceased instead of checking for an encounter
    
    * Refactor PatientDeathSummary to use react-query and render conditionally on load and error
    
    * Create helper component, ditch loading indicator for a text display and use typography to avoid text-transform
    
    * Add query endpoints as query keys
    
    * Rename query key to match retrieved data and include patient ID
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    * Stop using endpoint as query key
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    * Stop using endpoint as query key 2
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    * Stop using endpoint as query key and rename query key
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    * Co-authored by: Tom Caiger. Lint files
    
    * Co-authored by: Tom Caiger. Display error message on error instead of fixed one
    
    * Co-authored by: Tom Caiger. Stop using endpoint as query key
    
    * Only render items if there is no error to avoid cached data leak
    
    * Co-authored. Remove new endpoint and stop using helper, create new that supports everything
    
    * Update default react-query options, drop refetchOnWindowFocus
    
    * Update copy
    
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>
    
    * Actually remove checking permission to read patientAdditionalData
    
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>
    
    * Co-authored by: Tom McLean. Remove last permission check against PatientAdditionalData
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/desktop/app/views/patients/panes/DetailsPane.js
+++ packages/desktop/app/views/patients/panes/DetailsPane.js
@@ -1,20 +1,49 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { Typography } from '@material-ui/core';
 import { useApi } from '../../../api';
+import { useAuth } from '../../../contexts/Auth';
 import { ContentPane } from '../../../components';
 import { PatientDetailsForm } from '../../../forms/PatientDetailsForm';
 import { reloadPatient } from '../../../store/patient';
 
-export const PatientDetailsPane = React.memo(({ patient }) => {
+// Momentary component to just display a message, will need design and
+// refactor later.
+const ForbiddenMessage = () => (
+  <ContentPane>
+    <Typography variant="h4">Forbidden</Typography>
+    <Typography variant="body2">
+      You do not have permission to read, create or write patient data.
+    </Typography>
+  </ContentPane>
+);
+
+export const PatientDetailsPane = React.memo(({ patient, additionalData }) => {
   const api = useApi();
   const dispatch = useDispatch();
+  const { ability } = useAuth();
   const handleSubmit = async data => {
     await api.put(`patient/${patient.id}`, data);
     dispatch(reloadPatient(patient.id));
   };
+
+  // Display form if user can read, write or create patient additional data.
+  // It's assumed that if a user got this far, they can read a patient.
+  const canViewForm = ['read', 'write', 'create'].some(verb =>
+    ability.can(verb, 'Patient'),
+  );
+
+  if (canViewForm === false) {
+    return <ForbiddenMessage />;
+  }
+
   return (
     <ContentPane>
-      <PatientDetailsForm patient={patient} onSubmit={handleSubmit} />
+      <PatientDetailsForm
+        patient={patient}
+        additionalData={additionalData}
+        onSubmit={handleSubmit}
+      />
     </ContentPane>
   );
 });
