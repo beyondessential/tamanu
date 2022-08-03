7969f0af809fe68160d677f608c33c3de9a7f3c2
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-02 21:34:02 -0500

    Add endpoints to FHIR integration

--- packages/sync-server/app/integrations/fhir/routes.js
+++ packages/sync-server/app/integrations/fhir/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 
-import { patientHandler } from '../../hl7fhir';
+import { patientHandler, diagnosticReportHandler, immunizationHandler } from '../../hl7fhir';
 
 export const routes = express.Router();
 
 routes.get('/Patient', patientHandler());
+routes.get('/DiagnosticReport', diagnosticReportHandler());
+routes.get('/Immunization', immunizationHandler());
