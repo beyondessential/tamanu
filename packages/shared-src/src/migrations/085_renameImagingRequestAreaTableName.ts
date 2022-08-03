497a026aca9591560dc40c3c0f44c37f379e0130
Dan Nash <danielnash100@gmail.com>
2022-08-04 11:42:32 +1200

    await rename

--- packages/shared-src/src/migrations/085_renameImagingRequestAreaTableName.ts
+++ packages/shared-src/src/migrations/085_renameImagingRequestAreaTableName.ts
@@ -1,9 +1,9 @@
 import { QueryInterface } from 'sequelize';
 
 export async function up(query: QueryInterface) {
-  query.renameTable('imaging_request_area', 'imaging_request_areas');
+  await query.renameTable('imaging_request_area', 'imaging_request_areas');
 }
 
 export async function down(query: QueryInterface) {
-  query.renameTable('imaging_request_areas', 'imaging_request_area');
+  await query.renameTable('imaging_request_areas', 'imaging_request_area');
 }
