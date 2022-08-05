051465817b1b54dd93a071f97abeab1180ffd576
Tim Kennedy <t.m.knndy@gmail.com>
2022-08-05 17:13:46 +1000

    Add test stubs

---
+++ packages/lan/__tests__/apiv1/SurveyResponse.test.js
@@ -0,0 +1,6 @@
+describe('SurveyResponse', () => {
+  it("should look up an autocomplete component's sourcen model and extract a name");
+  it('should error if a question has no source');
+  it("should error if the answer body doesn't point to a valid source");
+  it('should hint if users might have ReferenceData pointing to a Location/Facility/Department');
+});
