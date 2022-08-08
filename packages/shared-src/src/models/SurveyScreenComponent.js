3889c3f59737526e7468f1c8de7f7c1b93a98571
Félix Saparelli <felix@passcod.name>
2022-08-08 16:14:30 +1200

    Fix bluebird api use

--- packages/shared-src/src/models/SurveyScreenComponent.js
+++ packages/shared-src/src/models/SurveyScreenComponent.js
@@ -40,15 +40,17 @@ export class SurveyScreenComponent extends Model {
     });
   }
 
-  static getComponentsForSurveys(surveyIds) {
-    return this.findAll({
+  static async getComponentsForSurveys(surveyIds) {
+    const components = await this.findAll({
       where: {
         surveyId: {
           [Op.in]: surveyIds,
         },
       },
       include: this.getListReferenceAssociations(),
-    }).map(c => c.forResponse());
+    })
+
+    return components.map(c => c.forResponse());
   }
 
   static getComponentsForSurvey(surveyId) {
