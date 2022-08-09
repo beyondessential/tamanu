0b0e62fa30a7a93317ddf01591360ae268c38dfe
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-08 20:58:13 -0500

    Fix test name to match file

--- packages/sync-server/__tests__/integrations/mSupply/DiagnosticReport.test.js
+++ packages/sync-server/__tests__/integrations/mSupply/DiagnosticReport.test.js
@@ -1,6 +1,6 @@
 import { testDiagnosticReportHandler } from '../../hl7fhir/routeHandlersTests';
 
-describe('mSupply integration - Immunization', () => {
+describe('mSupply integration - DiagnosticReport', () => {
   testDiagnosticReportHandler('mSupply', {
     'X-Tamanu-Client': 'mSupply',
     'X-Version': '0.0.1',
