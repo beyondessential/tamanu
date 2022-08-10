f22fbc513243e93de1f21ea6c5b2062cec96c1b2
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-08-11 00:18:00 +1200

    WAITM-299 Allow undefined date in getDisplayDate

--- packages/shared-src/src/utils/patientCertificates/getDisplayDate.js
+++ packages/shared-src/src/utils/patientCertificates/getDisplayDate.js
@@ -6,10 +6,11 @@ const DEFAULT_DATE_FORMAT = 'dd/MM/yyyy';
 // Display the date in a configured timezone if one is set
 export const getDisplayDate = (date, format = DEFAULT_DATE_FORMAT, getLocalisation) => {
   const timeZone = getLocalisation('timeZone');
+  const dateObject = date ? new Date(date) : new Date();
 
   if (timeZone) {
-    return formatInTimeZone(date, timeZone, format);
+    return formatInTimeZone(dateObject, timeZone, format);
   }
 
-  return formatDate(date, format);
+  return formatDate(dateObject, format);
 };
