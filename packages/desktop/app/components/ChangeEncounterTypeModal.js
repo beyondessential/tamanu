ea4083bfa7edd23bd0b11c56f3f095b20a3d66e5
Dan Nash <danielnash100@gmail.com>
2022-08-11 11:02:16 +1200

    move type into form component

--- packages/desktop/app/components/ChangeEncounterTypeModal.js
+++ packages/desktop/app/components/ChangeEncounterTypeModal.js
@@ -4,10 +4,8 @@ import { useEncounter } from '../contexts/Encounter';
 
 import { Modal } from './Modal';
 import { ChangeEncounterTypeForm } from '../forms/ChangeEncounterTypeForm';
-import { useUrlSearchParams } from '../utils/useUrlSearchParams';
 
 export const ChangeEncounterTypeModal = React.memo(({ open, encounter, onClose }) => {
-  const query = useUrlSearchParams();
   const { writeAndViewEncounter } = useEncounter();
   const { navigateToEncounter } = usePatientNavigation();
   const changeEncounterType = useCallback(
@@ -25,7 +23,6 @@ export const ChangeEncounterTypeModal = React.memo(({ open, encounter, onClose }
         onSubmit={changeEncounterType}
         onCancel={onClose}
         encounter={encounter}
-        type={query.get('type')}
       />
     </Modal>
   );
