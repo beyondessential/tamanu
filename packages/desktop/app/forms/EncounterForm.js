1d42363908b406fc430e28346f6d86b23d64dee4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-16 22:48:40 -0500

    Lint EncounterForm (indentation change)

--- packages/desktop/app/forms/EncounterForm.js
+++ packages/desktop/app/forms/EncounterForm.js
@@ -18,96 +18,98 @@ import {
 import { encounterOptions } from '../constants';
 import { useSuggester } from '../api';
 
-export const EncounterForm = React.memo(({ editedObject, onSubmit, patientBillingTypeId, encounterType }) => {
-  const locationSuggester = useSuggester('location');
-  const practitionerSuggester = useSuggester('practitioner');
-  const departmentSuggester = useSuggester('department');
+export const EncounterForm = React.memo(
+  ({ editedObject, onSubmit, patientBillingTypeId, encounterType }) => {
+    const locationSuggester = useSuggester('location');
+    const practitionerSuggester = useSuggester('practitioner');
+    const departmentSuggester = useSuggester('department');
 
-  const renderForm = ({ submitForm }) => {
-    const buttonText = editedObject ? 'Update encounter' : 'Confirm';
+    const renderForm = ({ submitForm }) => {
+      const buttonText = editedObject ? 'Update encounter' : 'Confirm';
+
+      return (
+        <FormGrid>
+          <Field
+            name="encounterType"
+            label="Encounter type"
+            disabled
+            component={SelectField}
+            options={encounterOptions}
+          />
+          <Field
+            name="startDate"
+            label="Check-in date"
+            required
+            component={DateField}
+            options={encounterOptions}
+          />
+          <Field
+            name="departmentId"
+            label="Department"
+            required
+            component={AutocompleteField}
+            suggester={departmentSuggester}
+          />
+          <Field
+            name="locationId"
+            label="Location"
+            required
+            component={AutocompleteField}
+            suggester={locationSuggester}
+          />
+          <LocalisedField
+            name="patientBillingTypeId"
+            endpoint="patientBillingType"
+            component={SuggesterSelectField}
+          />
+          <Field
+            name="examinerId"
+            label="Practitioner"
+            required
+            component={AutocompleteField}
+            suggester={practitionerSuggester}
+          />
+          <Field
+            name="reasonForEncounter"
+            label="Reason for encounter"
+            component={TextField}
+            multiline
+            rows={2}
+            style={{ gridColumn: 'span 2' }}
+          />
+          <div style={{ gridColumn: 2, textAlign: 'right' }}>
+            <Button variant="contained" onClick={submitForm} color="primary">
+              {buttonText}
+            </Button>
+          </div>
+        </FormGrid>
+      );
+    };
 
     return (
-      <FormGrid>
-        <Field
-          name="encounterType"
-          label="Encounter type"
-          disabled
-          component={SelectField}
-          options={encounterOptions}
-        />
-        <Field
-          name="startDate"
-          label="Check-in date"
-          required
-          component={DateField}
-          options={encounterOptions}
-        />
-        <Field
-          name="departmentId"
-          label="Department"
-          required
-          component={AutocompleteField}
-          suggester={departmentSuggester}
-        />
-        <Field
-          name="locationId"
-          label="Location"
-          required
-          component={AutocompleteField}
-          suggester={locationSuggester}
-        />
-        <LocalisedField
-          name="patientBillingTypeId"
-          endpoint="patientBillingType"
-          component={SuggesterSelectField}
-        />
-        <Field
-          name="examinerId"
-          label="Practitioner"
-          required
-          component={AutocompleteField}
-          suggester={practitionerSuggester}
-        />
-        <Field
-          name="reasonForEncounter"
-          label="Reason for encounter"
-          component={TextField}
-          multiline
-          rows={2}
-          style={{ gridColumn: 'span 2' }}
-        />
-        <div style={{ gridColumn: 2, textAlign: 'right' }}>
-          <Button variant="contained" onClick={submitForm} color="primary">
-            {buttonText}
-          </Button>
-        </div>
-      </FormGrid>
+      <Form
+        onSubmit={onSubmit}
+        render={renderForm}
+        initialValues={{
+          startDate: new Date(),
+          encounterType,
+          patientBillingTypeId,
+          ...editedObject,
+        }}
+        validationSchema={yup.object().shape({
+          examinerId: foreignKey('Examiner is required'),
+          locationId: foreignKey('Location is required'),
+          departmentId: foreignKey('Department is required'),
+          startDate: yup.date().required(),
+          encounterType: yup
+            .mixed()
+            .oneOf(encounterOptions.map(x => x.value))
+            .required(),
+        })}
+      />
     );
-  };
-
-  return (
-    <Form
-      onSubmit={onSubmit}
-      render={renderForm}
-      initialValues={{
-        startDate: new Date(),
-        encounterType,
-        patientBillingTypeId,
-        ...editedObject,
-      }}
-      validationSchema={yup.object().shape({
-        examinerId: foreignKey('Examiner is required'),
-        locationId: foreignKey('Location is required'),
-        departmentId: foreignKey('Department is required'),
-        startDate: yup.date().required(),
-        encounterType: yup
-          .mixed()
-          .oneOf(encounterOptions.map(x => x.value))
-          .required(),
-      })}
-    />
-  );
-});
+  },
+);
 
 EncounterForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
