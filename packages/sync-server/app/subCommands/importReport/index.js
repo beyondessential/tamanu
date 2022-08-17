237c5a6cb450ed8045e5bc50e6beeb93789230f7
Dan Nash <danielnash100@gmail.com>
2022-08-18 11:15:18 +1200

    WAITM-81: Add improvements to import report subcommand like ability to list all versions

---
+++ packages/sync-server/app/subCommands/importReport/index.js
@@ -0,0 +1,24 @@
+import { Command } from 'commander';
+import { initDatabase } from '../../database';
+import * as importUtils from './utils';
+import * as importActions from './actions';
+
+export async function importReport(options) {
+  const store = await initDatabase({ testMode: false });
+  const definition = await importUtils.findOrCreateDefinition(options.name, store);
+  const versions = await definition.getVersions();
+  if (options.list) {
+    await importActions.listVersions(definition, versions, store);
+  }
+  if (options.file) {
+    await importActions.createVersion(options.file, definition, versions, store);
+  }
+  process.exit(0);
+}
+
+export const importReportCommand = new Command('importReport')
+  .description('Imports a JSON report definition version into Tamanu')
+  .requiredOption('-n, --name <string>', 'Name of the report')
+  .option('-f, --file <path>', 'Path to report definition version data JSON')
+  .option('-l, --list', 'List all report definition versions')
+  .action(importReport);
