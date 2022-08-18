799af59381d08a5625817708bed0ce709a67e7b4
Dan Nash <danielnash100@gmail.com>
2022-08-19 10:35:50 +1200

    make consistent

--- packages/shared-src/src/services/createDateTypes.js
+++ packages/shared-src/src/services/createDateTypes.js
@@ -43,24 +43,21 @@ export function createDateType() {
     if (!(this instanceof PgTypes.DATETIMESTRING)) return new PgTypes.DATETIMESTRING();
     return DataTypes.DATETIMESTRING.apply(this, args);
   };
-
-  util.inherits(PgTypes.DATETIMESTRING, DataTypes.DATETIMESTRING);
-  // Reassign postgres-specific parser
-  PgTypes.DATETIMESTRING.parse = DataTypes.DATETIMESTRING.parse;
-
-  // These two extra steps are seemingly necessary altho not described in sequelize docs
-  PgTypes.DATETIMESTRING.types = { postgres: ['date_time_string'] };
-  DataTypes.postgres.DATETIMESTRING.key = 'date_time_string';
-
   PgTypes.DATESTRING = function dateString(...args) {
     if (!(this instanceof PgTypes.DATESTRING)) return new PgTypes.DATESTRING();
     return DataTypes.DATESTRING.apply(this, args);
   };
 
+  util.inherits(PgTypes.DATETIMESTRING, DataTypes.DATETIMESTRING);
   util.inherits(PgTypes.DATESTRING, DataTypes.DATESTRING);
+
   // Reassign postgres-specific parser
+  PgTypes.DATETIMESTRING.parse = DataTypes.DATETIMESTRING.parse;
   PgTypes.DATESTRING.parse = DataTypes.DATESTRING.parse;
+
   // These two extra steps are seemingly necessary altho not described in sequelize docs
+  PgTypes.DATETIMESTRING.types = { postgres: ['date_time_string'] };
+  DataTypes.postgres.DATETIMESTRING.key = 'date_time_string';
   PgTypes.DATESTRING.types = { postgres: ['date_string'] };
   DataTypes.postgres.DATESTRING.key = 'date_string';
 }
