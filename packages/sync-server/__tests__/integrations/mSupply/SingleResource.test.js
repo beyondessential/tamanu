40ef9c160fcc6bda828ed98e23368e75c3c9dcc1
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-08-17 20:19:33 -0500

    Errata: rename test

--- packages/sync-server/__tests__/integrations/mSupply/SingleResource.test.js
+++ packages/sync-server/__tests__/integrations/mSupply/SingleResource.test.js
@@ -1,6 +1,6 @@
 import { testSingleResourceHandler } from '../../hl7fhir/routeHandlersTests';
 
-describe('FHIR integration - Single resource', () => {
+describe('mSupply integration - Single resource', () => {
   testSingleResourceHandler('mSupply', {
     'X-Tamanu-Client': 'mSupply',
     'X-Version': '0.0.1',
