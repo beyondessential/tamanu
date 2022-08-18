dd7380db073556cec888f8e65985782686cd67cb
Dan Nash <danielnash100@gmail.com>
2022-08-19 10:06:01 +1200

    add comment

--- packages/shared-src/src/types/sequelize.d.ts
+++ packages/shared-src/src/types/sequelize.d.ts
@@ -1,5 +1,6 @@
 import sequelize from 'sequelize'
 
+// Extend Sequelize DataTypes with our own custom types.
 declare module "sequelize" {
     namespace DataTypes {
         export const DATETIMESTRING: AbstractDataType
