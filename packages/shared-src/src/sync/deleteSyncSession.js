1bf13006a58e275ace70d69083bbe8734c1d2cf3
Chris Tran <chris@beyondessential.com.au>
2022-08-26 00:25:12 +1000

    epi-137: changed delete statement

--- packages/shared-src/src/sync/deleteSyncSession.js
+++ packages/shared-src/src/sync/deleteSyncSession.js
@@ -1,4 +1,29 @@
 export const deleteSyncSession = async (store, sessionIndex) => {
-  await store.models.SessionSyncRecord.destroy({ where: { sessionIndex } });
-  await store.models.SyncSession.destroy({ where: { id: sessionIndex } });
-};
+  // Explicitly delete all the session_sync_records and sync_sessions because
+  // they are not synced, and we should free up the storage because the number of records
+  // can be very large
+  await store.sequelize.query(
+    `
+      DELETE FROM session_sync_records
+      WHERE session_index = :sessionIndex
+    `,
+    {
+      replacements: {
+        sessionIndex,
+      },
+    },
+  );
+
+  await store.sequelize.query(
+    `
+      DELETE FROM sync_sessions
+      WHERE id = :sessionIndex
+    `,
+    {
+      replacements: {
+        sessionIndex,
+      },
+    },
+  );
+}
+  
