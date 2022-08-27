d509a083865f643f033c5b35278de1b4a3026b15
Chris Tran <chris@beyondessential.com.au>
2022-08-27 21:24:59 +1000

    epi-137: fixed migration

--- packages/shared-src/src/migrations/096_migrateExistingFacilityPatientSyncData.js
+++ packages/shared-src/src/migrations/096_migrateExistingFacilityPatientSyncData.js
@@ -3,15 +3,15 @@ import Sequelize from 'sequelize';
 
 module.exports = {
   up: async query => {
-    // // thanks Félix for this uuid generator, copied from migration 81
-    // const uuidgen = `uuid_in(overlay(overlay(md5(random()::text || ':' || random()::text) placing '4' from 13) placing to_hex(floor(random()*(11-8+1) + 8)::int)::text from 17)::cstring)`;
-    // await query.sequelize.query(`
-    //   INSERT INTO patient_facilities (id, patient_id, facility_id, created_at, updated_at)
-    //   SELECT ${uuidgen}, patients.id, '${config.serverFacilityId}', now(), now()
-    //   FROM patients
-    //   WHERE patients.marked_for_sync = TRUE;
-    // `);
-    // await query.removeColumn('patients', 'marked_for_sync');
+    // thanks Félix for this uuid generator, copied from migration 81
+    const uuidgen = `uuid_in(overlay(overlay(md5(random()::text || ':' || random()::text) placing '4' from 13) placing to_hex(floor(random()*(11-8+1) + 8)::int)::text from 17)::cstring)`;
+    await query.sequelize.query(`
+      INSERT INTO patient_facilities (id, patient_id, facility_id, created_at, updated_at)
+      SELECT ${uuidgen}, patients.id, '${config.serverFacilityId}', now(), now()
+      FROM patients
+      WHERE patients.marked_for_sync = TRUE;
+    `);
+    await query.removeColumn('patients', 'marked_for_sync');
   },
   down: async query => {
     await query.addColumn('patients', 'marked_for_sync', {
