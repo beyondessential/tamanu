2707abd6568c83f786689f7ceb92b87c0ab55c9e
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2022-08-29 11:33:43 +1200

    WAITM 391: Upgrade Date Fields to Strings For Patient Death Fields (#2680)
    * WAITM-391: Alter patient death dates to date strings
    
    * remove legacy dates from model
    
    * lint
    
    * remove test deference to legacy date
    
    * revert code owner lint thing
    
    * remove const for date format due to potential confusion with date-fns format syntax

--- packages/desktop/app/forms/DeathForm.js
+++ packages/desktop/app/forms/DeathForm.js
@@ -282,6 +282,7 @@ export const DeathForm = React.memo(
             name="timeOfDeath"
             label="Date/Time"
             component={props => <DateTimeField {...props} InputProps={{}} />}
+            saveDateAsString
             required
           />
           <Field
@@ -347,6 +348,7 @@ export const DeathForm = React.memo(
             name="mannerOfDeathDate"
             label="What date did this external cause occur?"
             component={DateField}
+            saveDateAsString
             visibilityCriteria={mannerOfDeathVisibilityCriteria}
           />
           <Field
