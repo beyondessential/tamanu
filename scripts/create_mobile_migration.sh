da021eb07f6ee69e96629b242effdac6b2975f4b
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-08-29 14:56:51 +1200

    WAITM-404 Update migration script to include adding to migration list

--- scripts/create_mobile_migration.sh
+++ scripts/create_mobile_migration.sh
@@ -7,4 +7,5 @@ MIGRATION_DIR_PATH=App/migrations
 FILENAME=${MIGRATION_DIR_PATH}/${TIMESTAMP}-${MIGRATION_NAME}.ts
 
 sed "s/{MIGRATION_NAME}/${MIGRATION_NAME}/g;s/{TIMESTAMP}/${TIMESTAMP}/g" ${MIGRATION_DIR_PATH}/.migrationTemplate.ts > ${FILENAME}
+sed -i.bak "s/^$/import { ${MIGRATION_NAME}${TIMESTAMP} } from \'\.\/${TIMESTAMP}-${MIGRATION_NAME}\'\;\n/g;s/\]\;/  ${MIGRATION_NAME}${TIMESTAMP},\n\]\;/g" ${MIGRATION_DIR_PATH}/index.ts && rm ${MIGRATION_DIR_PATH}/index.ts.bak
 echo "Created new migration ${FILENAME}"
