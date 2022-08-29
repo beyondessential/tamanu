6b7e6ac08b8f60c5cc581e010fee980a7b8f2e63
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-08-29 13:49:48 +1200

    WAITM-404 Add migration for LabRequest dateTime columns

---
+++ packages/mobile/App/migrations/1661717539000-updateLabRequestDateTimeColumns.ts
@@ -0,0 +1,69 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { getTable } from './utils/queryRunner';
+const ISO9075_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+const ISO9075_FORMAT_LENGTH = ISO9075_FORMAT.length;
+
+async function createDateTimeStringUpMigration(
+  queryRunner: QueryRunner,
+  tableName: string,
+  columnName: string,
+): Promise<void> {
+  const tableObject = await getTable(queryRunner, tableName);
+
+  // 1. Create legacy columns
+  await queryRunner.addColumn(
+    tableObject,
+    new TableColumn({
+      name: `${columnName}_legacy`,
+      type: 'date',
+      isNullable: true,
+    }),
+  );
+
+  // 2. Copy data to legacy columns for backup
+  await queryRunner.query(
+    `UPDATE ${tableName}
+      SET ${columnName}_legacy = ${columnName}`,
+  );
+
+  // 3.Change column types from of original columns from date to string & convert data to string
+  // NOTE: SQLite doesn't like to update columns, drop the column and recreate it as the new type
+  await queryRunner.dropColumn(tableName, columnName);
+  await queryRunner.addColumn(
+    tableObject,
+    new TableColumn({
+      name: columnName,
+      type: 'string',
+      length: `${ISO9075_FORMAT_LENGTH}`,
+      isNullable: true,
+    }),
+  );
+  await queryRunner.query(
+    `UPDATE ${tableName}
+      SET ${columnName} = ${columnName}_legacy`,
+  );
+}
+
+async function createDateTimeStringDownMigration(
+  queryRunner: QueryRunner,
+  tableName: string,
+  columnName: string,
+): Promise<void> {
+  // 1. Drop the string column
+  await queryRunner.query(`ALTER TABLE ${tableName} DROP COLUMN ${columnName}`);
+
+  // 2. Move legacy data back to main column
+  await queryRunner.query(`ALTER TABLE ${tableName} RENAME COLUMN ${columnName}_legacy TO ${columnName}`);
+}
+
+export class updateLabRequestDateTimeColumns1661717539000 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    await createDateTimeStringUpMigration(queryRunner, 'labRequest', 'sampleTime');
+    await createDateTimeStringUpMigration(queryRunner, 'labRequest', 'requestedDate');
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    await createDateTimeStringDownMigration(queryRunner, 'labRequest', 'sampleTime');
+    await createDateTimeStringDownMigration(queryRunner, 'labRequest', 'requestedDate');
+  }
+}
--- packages/mobile/App/migrations/index.ts
+++ packages/mobile/App/migrations/index.ts
@@ -1,5 +1,7 @@
 import { databaseSetup1661160427226 } from './1661160427226-databaseSetup';
+import { updateLabRequestDateTimeColumns1661717539000 } from './1661717539000-updateLabRequestDateTimeColumns';
 
 export const migrationList = [
   databaseSetup1661160427226,
+  updateLabRequestDateTimeColumns1661717539000,
 ];
