c49563427f1f531e7bf28bd5b732cee2a3d52f69
Tim Kennedy <t.m.knndy@gmail.com>
2022-08-30 10:27:49 +1000

    Add test for ImagingRequest.results length

---
+++ packages/shared-src/__tests__/models/ImagingRequest.test.js
@@ -0,0 +1,29 @@
+import Chance from 'chance';
+import { fake } from 'shared/test-helpers';
+import { initDb } from '../initDb';
+
+const chance = new Chance();
+
+describe('ImagingRequest', () => {
+  let context;
+  beforeAll(async () => {
+    context = await initDb({ testMode: true });
+  });
+
+  it('allows more than 255 characters in the results', async () => {
+    // arrange
+    const { ImagingRequest, User } = context.models;
+    const results = chance.string({ length: 5000 });
+    const { id: requestedById } = await User.create(fake(User));
+
+    // act
+    const irPromise = ImagingRequest.create({
+      ...fake(ImagingRequest),
+      results,
+      requestedById,
+    });
+
+    // assert
+    await expect(irPromise).resolves.toMatchObject({ results });
+  });
+});
