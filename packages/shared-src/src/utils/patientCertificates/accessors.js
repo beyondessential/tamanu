1c28d9e4125f755e9be272745d24ff23a95e03a4
Tom McLean <tom.a.mclean@gmail.com>
2022-08-30 10:30:27 +1000

    Fix storybook-breaking syntax in shared

--- packages/shared-src/src/utils/patientCertificates/accessors.js
+++ packages/shared-src/src/utils/patientCertificates/accessors.js
@@ -15,14 +15,14 @@ export const getDOB = ({ dateOfBirth }, getLocalisation) =>
   dateOfBirth ? getDisplayDate(dateOfBirth, 'Do MMM YYYY', getLocalisation) : 'Unknown';
 
 export const getLaboratory = ({ laboratory }, getLocalisation) =>
-  laboratory?.name || getLocalisation('templates.covidTestCertificate.laboratoryName');
+  (laboratory || {}).name || getLocalisation('templates.covidTestCertificate.laboratoryName');
 
-export const getLabMethod = ({ labTestMethod }) => labTestMethod?.name || 'Unknown';
+export const getLabMethod = ({ labTestMethod }) => (labTestMethod || {}).name || 'Unknown';
 
 export const getRequestId = ({ displayId }) => displayId || 'Unknown';
 
-export const getPlaceOfBirth = ({ additionalData }) => additionalData?.placeOfBirth;
+export const getPlaceOfBirth = ({ additionalData }) => (additionalData || {}).placeOfBirth;
 
-export const getNationality = ({ additionalData }) => additionalData?.nationality?.name;
+export const getNationality = ({ additionalData }) => ((additionalData || {}).nationality || {}).name;
 
-export const getPassportNumber = ({ additionalData }) => additionalData?.passport;
+export const getPassportNumber = ({ additionalData }) => (additionalData || {}).passport;
