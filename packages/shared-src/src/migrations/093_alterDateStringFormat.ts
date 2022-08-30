6b8c3b43ddef8a2ccc67e73b8a1b21f6d50d2482
Daniel Nash <dannash200@gmail.com>
2022-08-30 14:14:27 +1200

    WAITM-434: correct format for date time strings

---
+++ packages/shared-src/src/migrations/093_alterDateStringFormat.ts
@@ -0,0 +1,54 @@
+import { QueryInterface } from 'sequelize';
+
+const ISO9075_DATE_TIME_FMT_PREVIOUS = 'YYYY-MM-DD HH:mm:ss';
+const ISO9075_DATE_TIME_FMT_CORRECTION = 'YYYY-MM-DD HH24:MI:SS';
+
+const tableColumns = {
+  patients: ['date_of_death'],
+  appointments: ['start_time', 'end_time'],
+  triages: ['arrival_time', 'triage_time', 'closed_time'],
+  lab_requests: ['sample_time', 'requested_date'],
+};
+
+export async function up(query: QueryInterface) {
+  const promises = [];
+  Object.entries(tableColumns).forEach(([tableName, columns]) => {
+    columns.forEach(columnName => {
+      promises.push(
+        query.sequelize.query(
+          `
+        UPDATE ${tableName}
+        SET ${columnName} = TO_CHAR(${`${columnName}_legacy`}, :dateTimeFmt);
+        `,
+          {
+            replacements: {
+              dateTimeFmt: ISO9075_DATE_TIME_FMT_CORRECTION,
+            },
+          },
+        ),
+      );
+    });
+  });
+  await Promise.all(promises);
+}
+
+export async function down(query: QueryInterface) {
+  const promises = [];
+  Object.entries(tableColumns).forEach(([tableName, columns]) => {
+    columns.forEach(columnName => {
+      promises.push(
+        query.sequelize.query(
+          `
+          UPDATE ${tableName}
+          SET ${columnName} = TO_CHAR(${`${columnName}_legacy`}, :dateTimeFmt);`,
+          {
+            replacements: {
+              dateTimeFmt: ISO9075_DATE_TIME_FMT_PREVIOUS,
+            },
+          },
+        ),
+      );
+    });
+  });
+  await Promise.all(promises);
+}
