c924df7c5426b94efac98da97f187b95d822588b
Tom Caiger <caigertom@gmail.com>
2022-09-01 16:46:01 +1200

    Update 096_reMigrateDateStringFieldsWithTimezoneOffset.ts

--- packages/shared-src/src/migrations/096_reMigrateDateStringFieldsWithTimezoneOffset.ts
+++ packages/shared-src/src/migrations/096_reMigrateDateStringFieldsWithTimezoneOffset.ts
@@ -2,20 +2,35 @@ import { QueryInterface } from 'sequelize';
 
 // This is the correct format for postgres date_time_string fields
 const ISO9075_DATE_TIME_FMT = 'YYYY-MM-DD HH24:MI:SS';
+
+// This is the format for postgres date_time_string fields
+const ISO9075_DATE_FMT = 'YYYY-MM-DD';
+
 // This is the date format that was incorrectly used for first round of date migrations
 const JAVASCRIPT_ISO9075_DATE_TIME_FMT = 'YYYY-MM-DD HH:mm:ss';
 
-// Table columns that have been migrated so far
-const tableColumns = {
+// Date time table columns that have been migrated so far
+const dateTimeTableColumns = {
   patients: ['date_of_death'],
   appointments: ['start_time', 'end_time'],
   triages: ['arrival_time', 'triage_time', 'closed_time'],
   lab_requests: ['sample_time', 'requested_date'],
 };
 
+// Date table columns that have been migrated so far
+const dateTableColumns = {
+  invoice_line_items: ['date_generated'],
+  invoices: ['date'],
+  invoice_price_change_items: ['date'],
+  patient_death_data: ['external_cause_date', 'last_surgery_date'],
+};
+
 export async function up(query: QueryInterface) {
   const promises = [];
-  Object.entries(tableColumns).forEach(([tableName, columns]) => {
+
+  // migrate date_time_string columns
+  // only include data that still matches the legacy column
+  Object.entries(dateTimeTableColumns).forEach(([tableName, columns]) => {
     columns.forEach(columnName => {
       promises.push(
         query.sequelize.query(
@@ -28,6 +43,26 @@ export async function up(query: QueryInterface) {
             replacements: {
               dateTimeFmt: ISO9075_DATE_TIME_FMT,
               oldDateTimeFmt: JAVASCRIPT_ISO9075_DATE_TIME_FMT,
+            },
+          },
+        ),
+      );
+    });
+  });
+
+  // migrate date_string columns
+  // only include data that still matches the legacy column
+  Object.entries(dateTableColumns).forEach(([tableName, columns]) => {
+    columns.forEach(columnName => {
+      promises.push(
+        query.sequelize.query(
+          `UPDATE ${tableName}
+        SET ${columnName} = to_char(${columnName}_legacy::TIMESTAMPTZ AT TIME ZONE 'UTC', :dateFmt)
+        WHERE ${columnName} = to_char(${columnName}_legacy, :dateFmt);
+        `,
+          {
+            replacements: {
+              dateFmt: ISO9075_DATE_FMT,
             },
           },
         ),
