0236178795dc6caadb67b4504ad7d56e31b12445
Tom McLean <tom.a.mclean@gmail.com>
2022-09-02 16:55:25 +1000

    Remove dependency cycle

---
+++ packages/desktop/app/views/patients/encounterTabNames.js
@@ -0,0 +1,10 @@
+
+export const ENCOUNTER_TAB_NAMES = {
+  VITALS: 'vitals',
+  NOTES: 'notes',
+  PROCEDURES: 'procedures',
+  LABS: 'labs',
+  IMAGING: 'imaging',
+  MEDICATION: 'medication',
+  PROGRAMS: 'programs',
+};
