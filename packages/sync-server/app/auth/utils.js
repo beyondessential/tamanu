0dc877cdf713a140ade9a3c33852cfcd6c5802c1
Edwin <edmofro@gmail.com>
2022-09-06 16:33:49 +1200

    Bring in email case insensitivity

--- packages/sync-server/app/auth/utils.js
+++ packages/sync-server/app/auth/utils.js
@@ -1,3 +1,4 @@
+import Sequelize from 'sequelize';
 import config from 'config';
 import jwt from 'jsonwebtoken';
 import { v4 as uuid } from 'uuid';
@@ -23,4 +24,26 @@ export const getToken = async (user, expiry) => {
 
 export const verifyToken = token => {
   return jwt.verify(token, JWT_SECRET);
+};
+
+export const findUser = async (models, email) => {
+  const user = await models.User.scope('withPassword').findOne({
+    // email addresses are case insensitive so compare them as such
+    where: Sequelize.where(
+      Sequelize.fn('lower', Sequelize.col('email')),
+      Sequelize.fn('lower', email),
+    ),
+  });
+  if (!user) {
+    return null;
+  }
+  return user.get({ plain: true });
+};
+
+export const findUserById = async (models, id) => {
+  const user = await models.User.findByPk(id);
+  if (!user) {
+    return null;
+  }
+  return user.get({ plain: true });
 };
