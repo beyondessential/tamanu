3a2db47cf3435b5273dd3339089308a39e292a95
Edwin <edmofro@gmail.com>
2022-09-07 13:38:39 +1200

    Clarify comments

--- packages/lan/app/database/initDatabase.js
+++ packages/lan/app/database/initDatabase.js
@@ -4,6 +4,7 @@ import { v4 as uuid } from 'uuid';
 import { initDatabase as sharedInitDatabase } from 'shared/services/database';
 
 // make a 'fake' uuid that looks like 'abcd7766-9794-4491-8612-eb19fd959bf2'
+// (n.b. the prefix is chosen because it's obviously fake, but valid hexadecimal)
 // this way we can run tests against real data and clear out everything that was
 // created by the tests with just "DELETE FROM table WHERE id LIKE 'abcd%'"
 const createTestUUID = () => `abcd${uuid().slice(4)}`;
