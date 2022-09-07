9c2c94812943332bdb4dee4dff1b2774c9f1f16d
Chris Tran <chris@beyondessential.com.au>
2022-09-07 16:35:59 +1000

    epi-141: added more doco

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   rules: {
-    'references-empty': [2, 'never'],
+    'references-empty': [2, 'never'], // validate referencing ticket with error level
   },
-  ignores: [message => message.toUpperCase().startsWith('NO-ISSUE')],
+  ignores: [message => message.toUpperCase().startsWith('NO-ISSUE')], // allow bypassing the validation (if wanted)
   parserPreset: {
     parserOpts: {
-      issuePrefixes: ['EPI-', 'TAN-', 'WAITM-', 'RN-', 'NOT-'],
+      issuePrefixes: ['EPI-', 'TAN-', 'WAITM-', 'RN-', 'NOT-'], // case insensitive
     },
   },
 };
