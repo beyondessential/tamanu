ba10b5d8f806fbba90ebb4f2fb91271e20c71673
Félix Saparelli <felix@passcod.name>
2022-09-13 04:15:36 +1200

    Split constants into many files

---
+++ packages/shared-src/src/constants/datetimes.js
@@ -0,0 +1,12 @@
+
+export const ISO9075_DATETIME_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+export const ISO9075_DATE_FORMAT = 'YYYY-MM-DD';
+
+export const TIME_UNIT_OPTIONS = [
+  { unit: 'minutes', minutes: 1 },
+  { unit: 'hours', minutes: 60 },
+  { unit: 'days', minutes: 60 * 24 },
+  { unit: 'weeks', minutes: 60 * 24 * 7 },
+  { unit: 'months', minutes: 60 * 24 * 30 },
+  { unit: 'years', minutes: 60 * 24 * 365 },
+];
