3aaa514d13cca8df99436e7222a977b90ac96da6
Tom Caiger <caigertom@gmail.com>
2022-09-20 10:57:58 +1200

    show errors on sync button

--- packages/desktop/app/views/NotActiveView.js
+++ packages/desktop/app/views/NotActiveView.js
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { connectApi } from '../api';
-
+import { toast } from 'react-toastify';
+import MuiButton from '@material-ui/core/Button';
+import { useApi } from '../api';
 import { TopBar, Notification } from '../components';
 
 // adding an invisible button as a hack to allow manually triggering a sync
-const InvisibleButton = styled.div`
-  height: 300px;
+const InvisibleButton = styled(MuiButton)`
+  height: 200px;
   width: 300px;
 `;
-const InvisibleSyncButton = connectApi(api => ({
-  onClick: async () => {
+
+const ErrorMessage = styled.div`
+  word-break: break-word;
+  width: 300px;
+`;
+
+const Error = ({ errorMessage }) => (
+  <div>
+    <b>Manual sync failed</b>
+    <ErrorMessage>{errorMessage}</ErrorMessage>
+  </div>
+);
+
+const InvisibleSyncButton = () => {
+  const [loading, setLoading] = useState(false);
+  const api = useApi();
+
+  const onClick = async () => {
+    setLoading(true);
+
     // eslint-disable-next-line no-console
     console.log('Triggering manual sync on LAN server');
+    toast.info('Starting manual sync...');
     try {
       await api.post(`sync/run`);
       // eslint-disable-next-line no-console
       console.log('Manual sync complete');
-    } catch (e) {
+      toast.success('Manual sync complete');
+    } catch (error) {
       // eslint-disable-next-line no-console
-      console.log('Manual sync failed');
+      console.log('Manual sync failed', error);
+      toast.error(<Error errorMessage={error.message} />);
+    } finally {
+      setLoading(false);
     }
-  },
-}))(InvisibleButton);
+  };
+  return <InvisibleButton onClick={onClick} disabled={loading} />;
+};
 
 export const NotActiveView = React.memo(() => (
   <>
