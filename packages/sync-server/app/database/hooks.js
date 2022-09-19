0a8b2a279088ed26b695fd26dc962b38bf9b5d6e
Félix Saparelli <felix@passcod.name>
2022-09-20 01:27:08 +1200

    EPI-160:III: Add materialiser task

--- packages/sync-server/app/database/hooks.js
+++ packages/sync-server/app/database/hooks.js
@@ -1,6 +1,9 @@
 import config from 'config';
 
 import { createReferralNotification } from 'shared/tasks/CreateReferralNotification';
+import { fhirQueue } from '../tasks/FhirMaterialiser';
+import { FHIR_RESOURCE_TYPES } from 'shared/constants';
+import { log } from 'shared/services/logging';
 
 export async function addHooks(store) {
   if (config.notifications) {
@@ -12,6 +15,30 @@ export async function addHooks(store) {
           createReferralNotification(referral, store.models);
         },
       );
+    }
+  }
+
+  if (config.integrations?.fhir) {
+    const singleHooks = ['afterCreate', 'afterUpdate'];
+    const bulkHooks = ['afterBulkCreate'];
+
+    for (const resource of Object.values(FHIR_RESOURCE_TYPES)) {
+      const Upstream = store.models[`Fhir${resource}`].UpstreamModel;
+      if (!Upstream) continue;
+
+      log.info(`Installing ${Upstream.name} hooks for FHIR materialisation`);
+
+      for (const hook of singleHooks) {
+        Upstream.addHook(hook, 'fhirMaterialisation', patient => {
+          fhirQueue(resource, patient.id);
+        });
+      }
+
+      for (const hook of bulkHooks) {
+        Upstream.addHook(hook, 'fhirMaterialisation', patients => {
+          patients.forEach(patient => fhirQueue(resource, patient.id));
+        });
+      }
     }
   }
 }
