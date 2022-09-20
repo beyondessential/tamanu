ebfcd0dbaefbd3eaf634f03e5a55fbfca32c4e05
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-09-20 18:08:19 -0500

    EPI-160:V: Draft Immunization setter

--- packages/shared-src/src/models/fhir/Immunization.js
+++ packages/shared-src/src/models/fhir/Immunization.js
@@ -1,7 +1,9 @@
 import { Sequelize, DataTypes } from 'sequelize';
 
+import { VACCINE_STATUS, INJECTION_SITE_OPTIONS } from 'shared/constants';
 import { FhirResource } from './Resource';
 import { dateType } from '../dateTimeTypes';
+import { FhirCodeableConcept, FhirCoding } from '../../services/fhirTypes';
 
 export class FhirImmunization extends FhirResource {
   static init(options, models) {
@@ -37,17 +39,143 @@ export class FhirImmunization extends FhirResource {
 
   // TODO: add fields and check if any other models are missing from include (hint: yes)
   async updateMaterialisation() {
-    const { ScheduledVaccine } = this.sequelize.models;
+    const { ScheduledVaccine, Encounter, Patient, ReferenceData } = this.sequelize.models;
 
-    const upstream = await this.getUpstream({
+    const administeredVaccine = await this.getUpstream({
       include: [
         {
           model: ScheduledVaccine,
           as: 'scheduledVaccine',
+          include: [
+            {
+              model: ReferenceData,
+              as: 'vaccine',
+            },
+          ],
+        },
+        {
+          model: Encounter,
+          as: 'encounter',
+          include: [
+            {
+              model: Patient,
+              as: 'patient',
+            },
+          ],
         },
       ],
     });
 
-    this.set({});
+    const { encounter, scheduledVaccine } = administeredVaccine;
+    const { patient } = encounter;
+
+    this.set({
+      status: status(administeredVaccine.status),
+      vaccineCode: vaccineCode(scheduledVaccine),
+      patient: patient.id,
+      encounter: encounter.id,
+      occurrenceDateTime: administeredVaccine.date,
+      lotNumber: administeredVaccine.batch,
+      site: site(administeredVaccine.injectionSite),
+      performer: performer(administeredVaccine.recorder),
+      protocolApplied: protocolApplied(scheduledVaccine.schedule),
+    });
   }
+}
+
+function status(administeredVaccineStatus) {
+  switch (administeredVaccineStatus) {
+    case VACCINE_STATUS.GIVEN:
+      return 'completed';
+    case VACCINE_STATUS.RECORDED_IN_ERROR:
+      return 'entered-in-error';
+    case VACCINE_STATUS.NOT_GIVEN:
+    case VACCINE_STATUS.SCHEDULED:
+    case VACCINE_STATUS.MISSED:
+    case VACCINE_STATUS.DUE:
+    case VACCINE_STATUS.UPCOMING:
+    case VACCINE_STATUS.OVERDUE:
+    case VACCINE_STATUS.UNKNOWN:
+    default:
+      return 'not-done';
+  }
+}
+
+function vaccineCode(scheduledVaccine) {
+  function vaccineIdToAIRVCode(vaccineId) {
+    switch (vaccineId) {
+      case KNOWN_VACCINE_IDS.PFIZER:
+        return KNOWN_AIRV_CODES.COMIRN;
+      case KNOWN_VACCINE_IDS.ASTRAZENECA:
+        return KNOWN_AIRV_CODES.COVAST;
+      default:
+        return null;
+    }
+  }
+
+  // AIRV: Australian Immunisation Register Vaccine
+  const AIRV_TERMINOLOGY_URL =
+    'https://www.healthterminologies.gov.au/integration/R4/fhir/ValueSet/australian-immunisation-register-vaccine-1';
+
+  // All known vaccines are reference data IDs (type 'drug')
+  const KNOWN_VACCINE_IDS = {
+    PFIZER: 'drug-COVID-19-Pfizer',
+    ASTRAZENECA: 'drug-COVAX',
+  };
+
+  // All currently supported AIRV vaccine codes
+  const KNOWN_AIRV_CODES = {
+    COMIRN: 'COMIRN',
+    COVAST: 'COVAST',
+  };
+
+  const code = vaccineIdToAIRVCode(scheduledVaccine.vaccine.id);
+
+  // TODO: We don't want to save unsupported vaccine codes, do we?
+  if (code === null) {
+    return []; // TODO: Should this be [{}] ?
+  }
+
+  return [
+    new FhirCodeableConcept({
+      coding: [
+        new FhirCoding({
+          system: AIRV_TERMINOLOGY_URL,
+          code,
+        }),
+      ],
+    }),
+  ];
+}
+
+function site(injectionSite) {
+  const HL7_INJECTION_SITE_URL = 'http://terminology.hl7.org/CodeSystem/v3-ActSite';
+
+  // Dictionary that maps Tamanu injection site to HL7 code
+  const INJECTION_SITE_TO_HL7_CODE = {
+    [INJECTION_SITE_OPTIONS.RIGHT_ARM]: 'RA',
+    [INJECTION_SITE_OPTIONS.LEFT_ARM]: 'LA',
+    [INJECTION_SITE_OPTIONS.RIGHT_THIGH]: 'RT',
+    [INJECTION_SITE_OPTIONS.LEFT_THIGH]: 'LT',
+  };
+
+  return [
+    new FhirCodeableConcept({
+      coding: [
+        new FhirCoding({
+          system: HL7_INJECTION_SITE_URL,
+          code: INJECTION_SITE_TO_HL7_CODE[injectionSite] || null,
+          display: injectionSite,
+        }),
+      ],
+    }),
+  ];
+}
+
+function performer(recorder) {
+  return null; // TODO: figure out what to save here
+}
+
+function protocolApplied(schedule) {
+  return null; // TODO: figure out what to save here
 }
