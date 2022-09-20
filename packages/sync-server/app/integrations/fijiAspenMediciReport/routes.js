fa0d9e473bb42daecb97e27abf3063bc8e4b3908
Alastair-L <lockealastair@gmail.com>
2022-09-20 15:58:30 +1000

    Medici report returns age as an int

--- packages/sync-server/app/integrations/fijiAspenMediciReport/routes.js
+++ packages/sync-server/app/integrations/fijiAspenMediciReport/routes.js
@@ -250,7 +250,7 @@ triage_info as (
   lateral (
     select
       case when t.closed_time is null
-        then (extract(EPOCH from now()) - extract(EPOCH from t.triage_time::timestamp))/60
+        then (extract(EPOCH from now()) - extract(EPOCH from t.triage_time::timestamp))/60 -- TODO: Potential Timezone bug here
         else (extract(EPOCH from t.closed_time::timestamp) - extract(EPOCH from t.triage_time::timestamp))/60
       end total_minutes
   ) total_minutes,
@@ -366,6 +366,7 @@ routes.get(
 
     const mappedData = data.map(encounter => ({
       ...encounter,
+      age: parseInt(encounter.age),
       weight: parseFloat(encounter.weight),
     }));
 
