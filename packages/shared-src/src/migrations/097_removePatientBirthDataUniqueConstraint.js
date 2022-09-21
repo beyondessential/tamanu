a405fcf53208cbcf0862a1746a3524f655af9935
Félix Saparelli <felix@beyondessential.com.au>
2022-09-21 19:05:21 +1200

    no ticket: fix dev tests (#2816)

--- packages/shared-src/src/migrations/097_removePatientBirthDataUniqueConstraint.js
+++ packages/shared-src/src/migrations/097_removePatientBirthDataUniqueConstraint.js
@@ -1,12 +1,10 @@
-import { QueryInterface } from 'sequelize';
-
-export async function up(query: QueryInterface): Promise<void> {
+export async function up(query) {
   await query.sequelize.query(`
     ALTER TABLE patient_birth_data DROP CONSTRAINT patient_birth_data_patient_id_key;
   `);
 }
 
-export async function down(query: QueryInterface): Promise<void> {
+export async function down(query) {
   await query.sequelize.query(`
     ALTER TABLE patient_birth_data ADD CONSTRAINT patient_birth_data_patient_id_key UNIQUE (patient_id);
   `);
