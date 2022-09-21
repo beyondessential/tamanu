51ada91bb3b22b1f05c43f3a9c49e2f585f5c11a
Félix Saparelli <felix@passcod.name>
2022-09-21 22:47:06 +1200

    EPI-160:III: Fix deleteAllTestIds with the power of sequelize

--- packages/lan/__tests__/setupUtilities.js
+++ packages/lan/__tests__/setupUtilities.js
@@ -1,10 +1,13 @@
+import { Op, Sequelize } from 'sequelize';
 import { FAKE_UUID_PATTERN } from 'shared/utils/generateId';
 
-export function deleteAllTestIds({ models, sequelize }) {
-  const tableNames = Object.values(models).map(m => m.tableName);
-  const deleteTasks = tableNames.map(table =>
-    sequelize.query(`DELETE FROM ${table} WHERE id::text LIKE $1`, {
-      bind: [FAKE_UUID_PATTERN],
+export function deleteAllTestIds({ models }) {
+  const deleteTasks = Object.values(models).map(Model =>
+    Model.destroy({
+      force: true,
+      where: Sequelize.where(Sequelize.cast(Sequelize.col('id'), 'text'), {
+        [Op.like]: FAKE_UUID_PATTERN,
+      }),
     }),
   );
   return Promise.all(deleteTasks);
