1b9e2d847f51066f06d013fd3e1bee3acf9508fa
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-09-21 17:09:16 -0500

    EPI-160:V: Continue to draft DiagnosticReport model

--- packages/shared-src/src/models/fhir/DiagnosticReport.js
+++ packages/shared-src/src/models/fhir/DiagnosticReport.js
@@ -1,7 +1,10 @@
+import config from 'config';
 import { Sequelize, DataTypes } from 'sequelize';
 
+import { LAB_REQUEST_STATUSES } from 'shared/constants';
 import { FhirResource } from './Resource';
 import { dateType } from '../dateTimeTypes';
+import { FhirCodeableConcept, FhirCoding, FhirIdentifier } from '../../services/fhirTypes';
 
 export class FhirDiagnosticReport extends FhirResource {
   static init(options, models) {
@@ -34,17 +37,102 @@ export class FhirDiagnosticReport extends FhirResource {
 
   // TODO: add fields and check if any other models are missing from include (hint: yes)
   async updateMaterialisation() {
-    const { LabRequest } = this.sequelize.models;
+    const { LabRequest, LabTestType, ReferenceData, Encounter, Patient } = this.sequelize.models;
 
-    const upstream = await this.getUpstream({
+    const labTest = await this.getUpstream({
       include: [
         {
           model: LabRequest,
           as: 'labRequest',
+          include: [
+            {
+              model: ReferenceData,
+              as: 'laboratory',
+            },
+            {
+              model: Encounter,
+              as: 'encounter',
+              include: [
+                {
+                  model: Patient,
+                  as: 'patient',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          model: LabTestType,
+          as: 'labTestType',
+        },
+        {
+          model: ReferenceData,
+          as: 'labTestMethod',
         },
       ],
     });
 
-    this.set({});
+    const { labTestType, labTestMethod, labRequest } = labTest;
+    const { encounter, laboratory } = labRequest;
+    const { patient, examiner } = encounter;
+
+    this.set({
+      identifier: identifiers(labRequest),
+      subject: patient.id,
+      status: status(labRequest),
+      effectiveDateTime: labRequest.sampleTime,
+      issued: labRequest.requestedDate,
+      code: code(labTestType),
+      performer: performer(laboratory, examiner),
+      result: result(labTest, labRequest),
+      extension: extension(labTestMethod),
+    });
   }
+}
+
+function identifiers(labRequest) {
+  return [
+    new FhirIdentifier({
+      use: 'official',
+      system: config.hl7.dataDictionaries.labRequestDisplayId,
+      value: labRequest.displayId,
+    }),
+  ];
+}
+
+function status(labRequest) {
+  switch (labRequest.status) {
+    case LAB_REQUEST_STATUSES.PUBLISHED:
+      return 'final';
+    case LAB_REQUEST_STATUSES.RESULTS_PENDING:
+      return 'registered';
+    default:
+      return 'unknown';
+  }
+}
+
+function code(labTestType) {
+  return [
+    new FhirCodeableConcept({
+      text: labTestType.name,
+      coding: [
+        new FhirCoding({
+          code: labTestType.code,
+          display: labTestType.name,
+        }),
+      ],
+    }),
+  ];
+}
+
+function performer(laboratory, examiner) {
+  return null; // TODO: figure out what to save here
+}
+
+function result(labTest, labRequest) {
+  return null; // TODO: figure out what to save here
+}
+
+function extension(labTestMethod) {
+  return null; // TODO: figure out what to save here
 }
