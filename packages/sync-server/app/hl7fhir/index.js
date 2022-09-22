69bc59a32d1b9b5e203b7b84a3b3c5c12e90debf
Félix Saparelli <felix@passcod.name>
2022-09-22 20:00:55 +1200

    Take control of FHIR routes

--- packages/sync-server/app/hl7fhir/index.js
+++ packages/sync-server/app/hl7fhir/index.js
@@ -1,1 +1,28 @@
-export * from './routeHandlers';
+import {
+  patientHandler,
+  diagnosticReportHandler,
+  immunizationHandler,
+  singlePatientHandler,
+  singleDiagnosticReportHandler,
+  singleImmunizationHandler,
+} from './routeHandlers';
+
+import { requireClientHeaders as requireClientHeadersMiddleware } from '../../middleware/requireClientHeaders';
+
+export function fhirRoutes({ requireClientHeaders }) {
+  const routes = express.Router();
+
+  if (requireClientHeaders) {
+    routes.use(requireClientHeadersMiddleware);
+  }
+
+  routes.get('/Patient', patientHandler());
+  routes.get('/DiagnosticReport', diagnosticReportHandler());
+  routes.get('/Immunization', immunizationHandler());
+
+  routes.get('/Patient/:id', singlePatientHandler());
+  routes.get('/DiagnosticReport/:id', singleDiagnosticReportHandler());
+  routes.get('/Immunization/:id', singleImmunizationHandler());
+
+  return routes;
+}
