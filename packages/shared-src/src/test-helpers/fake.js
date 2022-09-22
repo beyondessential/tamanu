4ea2988e7e589f68b8054b30aec8f0d63b7ab2dc
Edwin <edmofro@gmail.com>
2022-09-22 14:36:53 +1200

    Handle updatedByField

--- packages/shared-src/src/test-helpers/fake.js
+++ packages/shared-src/src/test-helpers/fake.js
@@ -270,6 +270,7 @@ const MODEL_SPECIFIC_OVERRIDES = {
       passport: chance.character() + chance.natural({ min: 10000000, max: 99999999 }).toString(),
       emergencyContactName: chance.name(),
       emergencyContactNumber: chance.phone(),
+      updatedByField: {},
     };
   },
   PatientFacility: ({ patientId = uuidv4(), facilityId = uuidv4() }) => {
