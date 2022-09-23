7dc56d1fc5bcf9beef6b9cf2c6d194cf032e317a
Félix Saparelli <felix@passcod.name>
2022-09-23 16:47:34 +1200

    commented out test

--- packages/sync-server/__tests__/integrations/fhir/Patient.test.js
+++ packages/sync-server/__tests__/integrations/fhir/Patient.test.js
@@ -3,3 +3,7 @@ import { testPatientHandler } from '../../hl7fhir/routeHandlersTests';
 describe('FHIR integration - Patient', () => {
   testPatientHandler('fhir');
 });
+
+// describe('FHIR Materialised integration - Patient', () => {
+//   testPatientHandler('fhir/mat');
+// });
