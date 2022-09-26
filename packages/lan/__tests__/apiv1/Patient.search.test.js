a91be28a7c07212ec945d6409755b674e226efb7
Daniel Nash <dannash200@gmail.com>
2022-09-27 09:23:13 +1300

    update test

--- packages/lan/__tests__/apiv1/Patient.search.test.js
+++ packages/lan/__tests__/apiv1/Patient.search.test.js
@@ -1,6 +1,8 @@
 import { createDummyPatient, createDummyEncounter } from 'shared/demoData/patients';
 import moment from 'moment';
 import Chance from 'chance';
+import { addMinutes, startOfDay, subDays, subYears } from 'date-fns';
+import { toDateString } from 'shared/utils/dateTime';
 import { createTestContext } from '../utilities';
 
 const chance = new Chance();
@@ -12,13 +14,15 @@ const withFirstName = name => ({ firstName }) => firstName === name;
 
 // function to pick a random time x years ago today
 const yearsAgo = (years, days = 0) =>
-  moment
-    .utc()
-    .startOf('day')
-    .add(chance.integer({ min: 1, max: 23.9 * 60 }), 'minutes')
-    .subtract(years, 'years')
-    .subtract(days, 'days')
-    .toISOString();
+  toDateString(
+    subDays(
+      subYears(
+        addMinutes(startOfDay(new Date()), chance.integer({ min: 1, max: 23.9 * 60 })),
+        years,
+      ),
+      days,
+    ),
+  );
 
 // add a bunch of patients at the top rather than per-search, so that the
 // tests have a healthy population of negative examples as well
