f3134dde970d36b15cd0a0e46589292c45a4e178
Félix Saparelli <felix@passcod.name>
2022-09-27 17:38:22 +1300

    Allow OperationOutcome to glom other OperationOutcomes

--- packages/sync-server/app/hl7fhir/materialised/errors.js
+++ packages/sync-server/app/hl7fhir/materialised/errors.js
@@ -4,62 +4,77 @@ import { v4 as uuidv4 } from 'uuid';
 import { FHIR_ISSUE_SEVERITY, FHIR_ISSUE_TYPE } from 'shared/constants';
 
 export class FhirError extends Error {
-  constructor(message, {
-    status = 500,
-    severity = FHIR_ISSUE_SEVERITY.ERROR,
-    code = FHIR_ISSUE_TYPE.TRANSIENT._,
-    diagnostics = null,
-  } = {}) {
+  constructor(
+    message,
+    {
+      status = 500,
+      severity = FHIR_ISSUE_SEVERITY.ERROR,
+      code = FHIR_ISSUE_TYPE.TRANSIENT._,
+      diagnostics = null,
+      expression = undefined,
+    } = {},
+  ) {
     super(message);
     this.status = status;
     this.severity = severity;
     this.code = code;
     this.diagnostics = diagnostics;
+    this.expression = expression;
   }
-  
+
   asFhir() {
     return {
       severity: this.severity,
       code: this.code,
       diagnostics: this.diagnostics ?? this.stack,
+      expression: this.expression,
       details: {
         text: this.message,
-      }
+      },
     };
   }
 }
 
 export class Invalid extends FhirError {
-  constructor(message) {
+  constructor(message, options = {}) {
     super(message, {
       status: 400,
-      code: FHIR_ISSUE_TYPE.INVALID._
+      code: FHIR_ISSUE_TYPE.INVALID._,
+      ...options,
     });
   }
 }
 
 export class Unsupported extends FhirError {
-  constructor(message) {
+  constructor(message, options = {}) {
     super(message, {
       status: 501,
       code: FHIR_ISSUE_TYPE.PROCESSING.NOT_SUPPORTED,
+      ...options,
     });
   }
 }
 
-export class OperationOutcome {
+export class OperationOutcome extends Error {
   constructor(errors) {
-    this.errors = errors.map(err => {
-      if (err instanceof FhirError) {
-        return err;
+    super('OperationOutcome: one or more errors (THIS SHOULD NEVER BE SEEN)');
+    this.errors = errors.flatMap(err => {
+      if (err instanceof OperationOutcome) {
+        return err.errors;
       }
 
-      return new FhirError(err.toString(), {
-        diagnostics: err.stack,
-      });
+      if (err instanceof FhirError) {
+        return [err];
+      }
+
+      return [
+        new FhirError(err.toString(), {
+          diagnostics: err.stack,
+        }),
+      ];
     });
   }
-  
+
   status() {
     const codes = this.errors.map(err => err.status);
     codes.sort();
