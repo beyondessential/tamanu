3381f71a7f1d66b0de3b99c4f8ad2c3340fbc47b
Félix Saparelli <felix@passcod.name>
2022-09-28 13:09:53 +1300

    Remove comment on FHIR_RESOURCE_TYPES
    instead there's a Checklist in slab

--- packages/shared-src/src/constants/fhir.js
+++ packages/shared-src/src/constants/fhir.js
@@ -1,14 +1,6 @@
 import { Op } from 'sequelize';
 
-// When adding to this:
-// - add the migration for the fhir.Name resource table
-//   - don't forget the trigger to update the version_id and add to versioning
-// - add the migration for the versioning table:
-//   - the fhir.resource_enum type
-//   - the fhir.resource_type enum
-//
-// must be in dependency order
-export const FHIR_RESOURCE_TYPES = ['Patient', 'Practitioner', 'ServiceRequest'];
+export const FHIR_RESOURCE_TYPES = ['Patient'];
 
 // All possible search parameter types
 export const FHIR_SEARCH_PARAMETERS = {
