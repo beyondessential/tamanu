266527decab5adac48200f0fdbc23a092b81fefc
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-09-27 22:30:51 -0500

    Point to the right table/column on the where clause

--- packages/shared-src/src/reports/registered-births-line-list.js
+++ packages/shared-src/src/reports/registered-births-line-list.js
@@ -86,8 +86,8 @@ where
     'c11229a7-b95c-4416-a3ad-560cd75d8f21',
     'cebdd9a4-2744-4ad2-9919-98dc0b15464c'
   )
-  and case when :from_date is not null then ir.requested_date::date >= :from_date::date else true end
-  and case when :to_date is not null then ir.requested_date::date <= :to_date::date else true end
+  and case when :from_date is not null then p.date_of_birth::date >= :from_date::date else true end
+  and case when :to_date is not null then p.date_of_birth::date <= :to_date::date else true end
   and case when :village_id is not null then p.village_id = :village_id else true end
 order by (p.date_of_birth::date + pbd.time_of_birth::timestamp::time);
 `;
