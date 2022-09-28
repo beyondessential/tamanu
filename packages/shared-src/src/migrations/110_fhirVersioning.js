6718532f25b3d51168cfac0951a363bb542a31da
Félix Saparelli <felix@passcod.name>
2022-09-28 13:09:28 +1300

    Replace versioning trigger with a single function for all tables

--- packages/shared-src/src/migrations/110_fhirVersioning.js
+++ packages/shared-src/src/migrations/110_fhirVersioning.js
@@ -1,21 +1,21 @@
 const RESOURCE_TABLES = ['patients', 'practitioners', 'service_requests'];
 
 export async function up(query) {
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION fhir.trigger_versioning()
+    RETURNS TRIGGER LANGUAGE plpgsql VOLATILE
+    AS $vers$
+      BEGIN
+        NEW.version_id := uuid_generate_v4();
+        RETURN NEW;
+      END;
+    $vers$
+  `);
+
   for (const table of RESOURCE_TABLES) {
     await query.sequelize.query(`
-      CREATE OR REPLACE FUNCTION fhir.trigger_versioning_${table}()
-      RETURNS TRIGGER LANGUAGE plpgsql VOLATILE
-      AS $vers$
-        BEGIN
-          NEW.version_id := uuid_generate_v4();
-          RETURN NEW;
-        END;
-      $vers$
-    `);
-
-    await query.sequelize.query(`
       CREATE TRIGGER versioning BEFORE UPDATE ON fhir.${table}
-      FOR EACH ROW EXECUTE FUNCTION fhir.trigger_versioning_${table}()
+      FOR EACH ROW EXECUTE FUNCTION fhir.trigger_versioning()
     `);
   }
 }
@@ -23,6 +23,7 @@ export async function up(query) {
 export async function down(query) {
   for (const table of RESOURCE_TABLES) {
     await query.sequelize.query(`DROP TRIGGER versioning ON fhir.${table}`);
-    await query.sequelize.query(`DROP FUNCTION fhir.trigger_versioning_${table}`);
   }
+
+  await query.sequelize.query(`DROP FUNCTION fhir.trigger_versioning`);
 }
