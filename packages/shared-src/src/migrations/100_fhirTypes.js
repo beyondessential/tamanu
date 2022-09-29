aaaba6f836ff11a2631111804bf66e713c412f0f
Félix Saparelli <felix@passcod.name>
2022-09-29 15:38:05 +1300

    Add identifier.type field

--- packages/shared-src/src/migrations/100_fhirTypes.js
+++ packages/shared-src/src/migrations/100_fhirTypes.js
@@ -6,8 +6,22 @@ export async function up(query) {
     "end"           date_time_string
   )`);
 
+  await query.sequelize.query(`CREATE TYPE fhir.coding AS (
+    system          text,
+    version         text,
+    code            text,
+    display         text,
+    user_selected   boolean
+  )`);
+
+  await query.sequelize.query(`CREATE TYPE fhir.codeable_concept AS (
+    coding          fhir.coding[],
+    text            text
+  )`);
+
   await query.sequelize.query(`CREATE TYPE fhir.identifier AS (
     use             text,
+    type            fhir.codeable_concept,
     system          text,
     value           text,
     period          fhir.period,
@@ -44,28 +58,15 @@ export async function up(query) {
     country         text,
     period          fhir.period
   )`);
-
-  await query.sequelize.query(`CREATE TYPE fhir.coding AS (
-    system          text,
-    version         text,
-    code            text,
-    display         text,
-    user_selected   boolean
-  )`);
-
-  await query.sequelize.query(`CREATE TYPE fhir.codeable_concept AS (
-    coding          fhir.coding[],
-    text            text
-  )`);
 }
 
 export async function down(query) {
-  await query.sequelize.query('DROP TYPE fhir.codeable_concept');
-  await query.sequelize.query('DROP TYPE fhir.coding');
   await query.sequelize.query('DROP TYPE fhir.address');
   await query.sequelize.query('DROP TYPE fhir.contact_point');
   await query.sequelize.query('DROP TYPE fhir.human_name');
   await query.sequelize.query('DROP TYPE fhir.identifier');
+  await query.sequelize.query('DROP TYPE fhir.codeable_concept');
+  await query.sequelize.query('DROP TYPE fhir.coding');
   await query.sequelize.query('DROP TYPE fhir.period');
   await query.dropSchema('fhir');
 }
