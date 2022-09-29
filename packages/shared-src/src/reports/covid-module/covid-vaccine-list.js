ebd95404a116a737217c4b3365898a65f7ecc7fb
Félix Saparelli <felix@passcod.name>
2022-09-29 17:54:02 +1300

    Merge error

--- packages/shared-src/src/reports/covid-module/covid-vaccine-list.js
+++ packages/shared-src/src/reports/covid-module/covid-vaccine-list.js
@@ -5,8 +5,6 @@ import { subDays, format } from 'date-fns';
 import { parseISO9075 } from 'shared/utils/dateTime';
 import { generateReportFromQueryData } from '../utilities';
 
-import { generateReportFromQueryData } from '../utilities';
-
 const DATE_FORMAT = 'yyyy/MM/dd';
 
 const reportColumnTemplate = [
