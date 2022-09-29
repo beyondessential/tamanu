46278f3636ae2f0739544cfa913fd83ad10102f9
Tom Caiger <caigertom@gmail.com>
2022-09-29 15:29:17 +1300

    WAITM-387: Update patient info fields (#2801)
    * Create 100_update_patient_info_date_fields.js
    
    * update models
    
    * update migration
    
    * Mobile dates migration
    
    * Update 100_update_patient_info_date_fields.js
    
    * Update PatientCarePlan.test.js
    
    * Update 1663564207000-updatePatientIssueDate.ts
    
    * refactor mobile migration
    
    * Update 1663564207000-updatePatientIssueDate.ts
    
    * Update 100_update_patient_info_date_fields.js
    
    * Update PatientCarePlan.test.js

--- packages/mobile/App/models/PatientIssue.ts
+++ packages/mobile/App/models/PatientIssue.ts
@@ -2,19 +2,23 @@ import { Entity, Column, ManyToOne, RelationId, BeforeUpdate, BeforeInsert } fro
 import { BaseModel, FindMarkedForUploadOptions } from './BaseModel';
 import { Patient } from './Patient';
 import { IPatientIssue, PatientIssueType } from '~/types';
+import { ISO9075_SQLITE_DEFAULT } from './columnDefaults';
 
 @Entity('patient_issue')
 export class PatientIssue extends BaseModel implements IPatientIssue {
   @Column({ nullable: true })
   note?: string;
 
-  @Column()
-  recordedDate: Date;
+  @Column({ nullable: false, default: ISO9075_SQLITE_DEFAULT })
+  recordedDate: string;
 
   @Column('text')
   type: PatientIssueType;
 
-  @ManyToOne(() => Patient, patient => patient.issues)
+  @ManyToOne(
+    () => Patient,
+    patient => patient.issues,
+  )
   patient: Patient;
   @RelationId(({ patient }) => patient)
   patientId: string;
@@ -36,9 +40,7 @@ export class PatientIssue extends BaseModel implements IPatientIssue {
     }
   }
 
-  static async findMarkedForUpload(
-    opts: FindMarkedForUploadOptions,
-  ): Promise<BaseModel[]> {
+  static async findMarkedForUpload(opts: FindMarkedForUploadOptions): Promise<BaseModel[]> {
     const patientId = opts.channel.match(/^patient\/(.*)\/issue$/)[1];
     if (!patientId) {
       throw new Error(`Could not extract patientId from ${opts.channel}`);
