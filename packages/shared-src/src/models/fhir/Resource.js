2f7063730f34a98b7c869172a28981f0d83e9f96
Félix Saparelli <felix@passcod.name>
2022-09-30 13:28:12 +1300

    Format lastUpdated to fhir precision

--- packages/shared-src/src/models/fhir/Resource.js
+++ packages/shared-src/src/models/fhir/Resource.js
@@ -2,8 +2,9 @@ import { snakeCase } from 'lodash';
 import { Sequelize, Utils, QueryTypes } from 'sequelize';
 import * as yup from 'yup';
 
-import { SYNC_DIRECTIONS, FHIR_SEARCH_PARAMETERS, FHIR_SEARCH_TOKEN_TYPES } from '../../constants';
+import { SYNC_DIRECTIONS, FHIR_SEARCH_PARAMETERS, FHIR_SEARCH_TOKEN_TYPES, FHIR_DATETIME_PRECISION } from '../../constants';
 import { objectAsFhir } from '../../utils/pgComposite';
+import { formatDateTime } from '../../utils/fhir';
 import { Model } from '../Model';
 
 const missingRecordsPrivateMethod = Symbol('missingRecords');
@@ -145,7 +146,7 @@ export class FhirResource extends Model {
       id: this.id,
       meta: {
         versionId: this.versionId,
-        lastUpdated: this.lastUpdated,
+        lastUpdated: formatDateTime(this.lastUpdated, FHIR_DATETIME_PRECISION.SECONDS_WITH_TIMEZONE),
       },
       ...objectAsFhir(fields),
     };
