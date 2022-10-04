2d1b937ff0211142bb49b36fa3f453b55ac93b71
Edwin <edmofro@gmail.com>
2022-10-05 03:57:45 +1300

    Start sync clock at 1

--- packages/shared-src/src/migrations/122_initialiseSyncClock.js
+++ packages/shared-src/src/migrations/122_initialiseSyncClock.js
@@ -4,7 +4,7 @@ const CURRENT_SYNC_TIME_KEY = 'currentSyncTime';
 
 export async function up(query) {
   await query.bulkInsert('local_system_facts', [
-    { id: Sequelize.fn('uuid_generate_v4'), key: CURRENT_SYNC_TIME_KEY, value: '0' },
+    { id: Sequelize.fn('uuid_generate_v4'), key: CURRENT_SYNC_TIME_KEY, value: '1' },
   ]);
 }
 
