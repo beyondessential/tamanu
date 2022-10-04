51877ce0e6409b558b7c751caa154fb0a6202225
Edwin <edmofro@gmail.com>
2022-10-04 14:07:59 +1300

    Fix migrations

--- packages/shared-src/src/migrations/102_initialiseSyncClock.js
+++ packages/shared-src/src/migrations/102_initialiseSyncClock.js
@@ -1,7 +1,11 @@
+import Sequelize from 'sequelize';
+
 const CURRENT_SYNC_TIME_KEY = 'currentSyncTime';
 
 export async function up(query) {
-  await query.bulkInsert('local_system_facts', [{ key: CURRENT_SYNC_TIME_KEY, value: '0' }]);
+  await query.bulkInsert('local_system_facts', [
+    { id: Sequelize.fn('uuid_generate_v4'), key: CURRENT_SYNC_TIME_KEY, value: '0' },
+  ]);
 }
 
 export async function down(query) {
