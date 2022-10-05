9c09622cbda92bb3a8fc2c0aca4d2a708d9d94f9
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-10-04 19:32:34 -0500

    Wrap helper functions in asyncHandler to be able to properly handle errors

--- packages/lan/app/routeHandlers/notes/notePageListHandler.js
+++ packages/lan/app/routeHandlers/notes/notePageListHandler.js
@@ -1,4 +1,5 @@
 import { Op } from 'sequelize';
+import asyncHandler from 'express-async-handler';
 import { NOTE_TYPES, NOTE_RECORD_TYPES } from 'shared/constants';
 
 // Encounter notes should check for their own permission, otherwise,
@@ -10,66 +11,68 @@ function checkListNotePermission(req, owner, noteRecordType) {
   return req.checkPermission('read', owner);
 }
 
-export const notePageListHandler = recordType => async (req, res) => {
-  const { models, params, query } = req;
-  const { order = 'ASC', orderBy } = query;
+export const notePageListHandler = recordType =>
+  asyncHandler(async (req, res) => {
+    const { models, params, query } = req;
+    const { order = 'ASC', orderBy } = query;
 
-  const recordId = params.id;
-  const owner = await models[recordType].findByPk(recordId);
+    const recordId = params.id;
+    const owner = await models[recordType].findByPk(recordId);
 
-  checkListNotePermission(req, owner, recordType);
+    checkListNotePermission(req, owner, recordType);
 
-  const rows = await models.NotePage.findAll({
-    include: [
-      {
-        model: models.NoteItem,
-        as: 'noteItems',
-        include: [
-          {
-            model: models.User,
-            as: 'author',
-          },
-          {
-            model: models.User,
-            as: 'onBehalfOf',
-          },
-        ],
-      },
-    ],
-    where: { recordType, recordId, noteType: { [Op.not]: NOTE_TYPES.SYSTEM } },
-    order: orderBy ? [[orderBy, order.toUpperCase()]] : undefined,
+    const rows = await models.NotePage.findAll({
+      include: [
+        {
+          model: models.NoteItem,
+          as: 'noteItems',
+          include: [
+            {
+              model: models.User,
+              as: 'author',
+            },
+            {
+              model: models.User,
+              as: 'onBehalfOf',
+            },
+          ],
+        },
+      ],
+      where: { recordType, recordId, noteType: { [Op.not]: NOTE_TYPES.SYSTEM } },
+      order: orderBy ? [[orderBy, order.toUpperCase()]] : undefined,
+    });
+
+    res.send({ data: rows, count: rows.length });
   });
 
-  res.send({ data: rows, count: rows.length });
-};
+export const notePagesWithSingleItemListHandler = recordType =>
+  asyncHandler(async (req, res) => {
+    const { models, params } = req;
 
-export const notePagesWithSingleItemListHandler = recordType => async (req, res) => {
-  const { models, params } = req;
+    const recordId = params.id;
+    const owner = await models[recordType].findByPk(recordId);
 
-  const recordId = params.id;
-  const owner = await models[recordType].findByPk(recordId);
+    checkListNotePermission(req, owner, recordType);
 
-  checkListNotePermission(req, owner, recordType);
-
-  const notePages = await models.NotePage.findAll({
-    include: [
-      {
-        model: models.NoteItem,
-        as: 'noteItems',
-        include: [
-          { model: models.User, as: 'author' },
-          { model: models.User, as: 'onBehalfOf' },
-        ],
+    const notePages = await models.NotePage.findAll({
+      include: [
+        {
+          model: models.NoteItem,
+          as: 'noteItems',
+          include: [
+            { model: models.User, as: 'author' },
+            { model: models.User, as: 'onBehalfOf' },
+          ],
+        },
+      ],
+      where: {
+        recordId,
+        recordType,
+        noteType: { [Op.not]: NOTE_TYPES.SYSTEM },
       },
-    ],
-    where: {
-      recordId,
-      recordType,
-      noteType: { [Op.not]: NOTE_TYPES.SYSTEM },
-    },
-    order: [['createdAt', 'ASC']],
-  });
+      order: [['createdAt', 'ASC']],
+    });
 
-  const notes = await Promise.all(notePages.map(n => n.getCombinedNoteObject(models)));
-  res.send({ data: notes, count: notes.length });
-};
+    const notes = await Promise.all(notePages.map(n => n.getCombinedNoteObject(models)));
+    res.send({ data: notes, count: notes.length });
+  });
