83e18da45240a3c41ee2539a3ed8f7b5901192e4
Ethan McQuarrie <69437175+EMcQ-BES@users.noreply.github.com>
2022-10-06 10:56:17 +1300

    Waitm 390 Lab Test date string (#2771)
    * WAITM-390 Add migration for labTest sampleTime to string type
    
    * WAITM-390 Update sampleTime type in LabTest model
    
    * WAITM-390 Update sampleTime type in interface
    
    * WAITM-390 Update lab test date to string in shared-src
    
    * WAITM-390 Fixup lab test migration
    
    * WAITM-390 Use date field correctly in swab test report
    
    * WAITM-390 Update swab list again
    
    * WAITM-390 Clean up unit tests
    
    * WAITM-390 Remove time section from lab test date string in mobile
    
    * WAITM-390 Update labTest to rename sampleTime to date
    
    * WAITM-390 Don't keep sampleTime name for legacy column
    
    * WAITM-390 Add completed date to server migration
    
    * WAITM-390 Update uses of completedDate
    
    * WAITM-390 Update usage of completedDate in covid swab report
    
    * WAITM-390 Update additional usages
    
    * WAITM-390 Fixup LabTest defaults in model
    
    * WAITM-390 Change migration to js
    
    * Bump dev version to 1.22.0
    
    * Merge pull request #2875 from beyondessential/resolve-v1-20-1-conflicts (#2876)
    
    * match version number with staging
    
    * Update 092_alterLabTestDateFields.js
    
    Co-authored-by: Tim Kennedy <t.m.knndy@gmail.com>
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

---
+++ packages/mobile/App/migrations/1662006885000-updateLabTestDate.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+import { getTable } from './utils/queryRunner';
+const ISO9075_FORMAT = 'YYYY-MM-DD';
+const ISO9075_FORMAT_LENGTH = ISO9075_FORMAT.length;
+
+export class updateLabTestDate1662006885000 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    const tableObject = await getTable(queryRunner, 'labTest');
+    // Move old data to legacy column
+    await queryRunner.query('ALTER TABLE labTest RENAME COLUMN sampleTime TO date_legacy');
+    // Add new column (with rename sampleTime -> date)
+    await queryRunner.addColumn(
+      tableObject,
+      new TableColumn({
+        name: 'date',
+        type: 'string',
+        length: `${ISO9075_FORMAT_LENGTH}`,
+        isNullable: false,
+        default: "strftime('%Y-%m-%d', CURRENT_TIMESTAMP)",
+      }),
+    );
+    // Fill data
+    queryRunner.query(
+      'UPDATE labTest SET date = date_legacy',
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    // 1. Drop the string column
+    await queryRunner.query('ALTER TABLE labTest DROP COLUMN date');
+    // 2. Move legacy data back to main column (with undo rename
+    await queryRunner.query('ALTER TABLE labTest RENAME COLUMN date_legacy TO sampleTime');
+  }
+}
