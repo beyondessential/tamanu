e5b442ac9a52e49195c9d51f2610fd668cd49c5e
Tom Caiger <caigertom@gmail.com>
2022-10-06 10:06:48 +1300

    Update 092_alterLabTestDateFields.js

--- packages/shared-src/src/migrations/092_alterLabTestDateFields.js
+++ packages/shared-src/src/migrations/092_alterLabTestDateFields.js
@@ -1,51 +1,79 @@
-import { DataTypes } from 'sequelize';
+import { QueryTypes, DataTypes } from 'sequelize';
+import config from 'config';
 
-const TABLE_NAME = 'lab_tests';
 const ISO9075_DATE_FMT = 'YYYY-MM-DD';
 const ISO9075_DATE_TIME_FMT = 'YYYY-MM-DD HH24:MI:SS';
 
+const MIGRATIONS = [
+  { TABLE: 'lab_tests', FIELD: 'date', TYPE: 'date_string' },
+  { TABLE: 'lab_tests', FIELD: 'completed_date', TYPE: 'date_time_string' },
+];
+
+const alterSchemaOnly = async (query, table, field, type) => {
+  // Change column types from of original columns from date to string
+  return query.sequelize.query(`
+      ALTER TABLE ${table}
+      ALTER COLUMN ${field} TYPE ${type});
+    `);
+};
+
+const alterSchemaAndBackUpLegacyData = async (query, table, field, type) => {
+  const COUNTRY_TIMEZONE = config?.countryTimeZone;
+
+  if (!COUNTRY_TIMEZONE) {
+    throw Error('A countryTimeZone must be configured in local.json for this migration to run.');
+  }
+
+  // Copy data to legacy columns for backup
+  await query.sequelize.query(`
+      UPDATE ${table}
+      SET
+          ${field}_legacy = ${field};
+  `);
+
+  // Change column types from of original columns from date to string & convert data to string
+  const iso9075Format = type === 'date_time_string' ? ISO9075_DATE_TIME_FMT : ISO9075_DATE_FMT;
+
+  return query.sequelize.query(`
+    ALTER TABLE ${table}
+    ALTER COLUMN ${field} TYPE ${type}
+    USING TO_CHAR(${field}::TIMESTAMPTZ AT TIME ZONE '${COUNTRY_TIMEZONE}', '${iso9075Format}');
+  `);
+};
+
 export async function up(query) {
-  // 1. Create legacy columns
-  await query.addColumn(TABLE_NAME, 'date_legacy', {
-    type: DataTypes.DATE,
-  });
-  await query.addColumn(TABLE_NAME, 'completed_date_legacy', {
-    type: DataTypes.DATE,
-  });
+  for (const migration of MIGRATIONS) {
+    // Create legacy columns
+    await query.addColumn(migration.TABLE, `${migration.FIELD}_legacy`, {
+      type: DataTypes.DATE,
+    });
 
-  // 2. Copy data to legacy columns for backup
-  await query.sequelize.query(`
-    UPDATE ${TABLE_NAME}
-    SET
-      date_legacy = date;
-  `);
-  await query.sequelize.query(`
-    UPDATE ${TABLE_NAME}
-    SET
-      completed_date_legacy = completed_date;
-  `);
+    // Check for legacy data
+    // Check if there is any legacy data in the system
+    // For example, newly deployed instances of tamanu won't have legacy data
+    const legacyDataCount = await query.sequelize.query(
+      `SELECT COUNT(*) FROM ${migration.TABLE} WHERE ${migration.FIELD} IS NOT NULL;`,
+      {
+        type: QueryTypes.SELECT,
+      },
+    );
 
-  // 3.Change column types from of original columns from date to string & convert data to string
-  await query.sequelize.query(`
-    ALTER TABLE ${TABLE_NAME}
-    ALTER COLUMN date TYPE date_string USING TO_CHAR(date, '${ISO9075_DATE_FMT}');
-  `);
-  await query.sequelize.query(`
-    ALTER TABLE ${TABLE_NAME}
-    ALTER COLUMN completed_date TYPE date_time_string USING TO_CHAR(completed_date, '${ISO9075_DATE_TIME_FMT}');
-  `);
+    // If there is no legacy column data, then we don't need to run the data migration or check
+    // for the timezone in the config
+    if (legacyDataCount === 0) {
+      await alterSchemaOnly(query, migration.TABLE, migration.FIELD, migration.TYPE);
+    } else {
+      await alterSchemaAndBackUpLegacyData(query, migration.TABLE, migration.FIELD, migration.TYPE);
+    }
+  }
 }
 
 export async function down(query) {
-  await query.sequelize.query(`
-    ALTER TABLE ${TABLE_NAME}
-    ALTER COLUMN date TYPE timestamp with time zone USING date_legacy;
-  `);
-  await query.removeColumn(TABLE_NAME, 'date_legacy');
-
-  await query.sequelize.query(`
-    ALTER TABLE ${TABLE_NAME}
-    ALTER COLUMN completed_date TYPE timestamp with time zone USING completed_date_legacy;
-  `);
-  await query.removeColumn(TABLE_NAME, 'completed_date_legacy');
+  for (const migration of MIGRATIONS) {
+    await query.sequelize.query(`
+      ALTER TABLE ${migration.TABLE}
+      ALTER COLUMN ${migration.FIELD} TYPE timestamp with time zone USING ${migration.FIELD}_legacy;
+    `);
+    await query.removeColumn(migration.TABLE, `${migration.FIELD}_legacy`);
+  }
 }
