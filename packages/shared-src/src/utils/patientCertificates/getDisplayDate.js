81e2d2b81d69c34af2ccc03f6457a01dff557e2c
Tom Caiger <caigertom@gmail.com>
2022-10-07 13:07:58 +1300

    WAITM-390: Fix Cert Printing Date (#2929)
    * Update getDisplayDate.js
    
    * Update getDisplayDate.js
    
    * Update getDisplayDate.js

--- packages/shared-src/src/utils/patientCertificates/getDisplayDate.js
+++ packages/shared-src/src/utils/patientCertificates/getDisplayDate.js
@@ -1,8 +1,19 @@
-import { format as formatDate } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+import { format as formatDate } from '../dateTime';
 
 const DEFAULT_DATE_FORMAT = 'dd/MM/yyyy';
 
-export const getDisplayDate = (date, format = DEFAULT_DATE_FORMAT) => {
-  const dateObject = date ? new Date(date) : new Date();
-  return formatDate(dateObject, format);
+export const getDisplayDate = (date, format = DEFAULT_DATE_FORMAT, getLocalisation) => {
+  // Format the date if it's passed in
+  if (date) {
+    return formatDate(date, format);
+  }
+
+  // Display the current date in a configured timezone if one is set
+  if (getLocalisation && getLocalisation('timeZone')) {
+    return formatInTimeZone(new Date(), getLocalisation('timeZone'), format);
+  }
+
+  // Finally return a current date
+  return formatDate(new Date(), format);
 };
