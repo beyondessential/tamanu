e6c8c72bb4e184bb2a72179675e6d40b5a44d3f6
Félix Saparelli <felix@passcod.name>
2022-10-11 13:30:11 +1300

    Write conversion function for identifier type update

--- packages/shared-src/src/migrations/115_fhirReferences.js
+++ packages/shared-src/src/migrations/115_fhirReferences.js
@@ -12,11 +12,135 @@ export async function up(query) {
     )
   `);
 
+  await query.sequelize.query(`
+    CREATE TYPE fhir.new_identifier AS (
+      use             text,
+      type            fhir.codeable_concept,
+      system          text,
+      value           text,
+      period          fhir.period,
+      assigner        fhir.reference
+    )
+  `);
 
-  await query.sequelize.query(`ALTER TYPE fhir.identifier ALTER ATTRIBUTE assigner SET DATA TYPE fhir.reference`);
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION fhir.migrate_identifier(
+      IN oldids fhir.identifier[],
+      OUT newids fhir.new_identifier[]
+    )
+    RETURNS fhir.new_identifier[] LANGUAGE plpgsql
+    IMMUTABLE PARALLEL SAFE RETURNS NULL ON NULL INPUT
+    AS $migrate$
+      DECLARE
+        old fhir.identifier;
+      BEGIN
+        FOREACH old IN ARRAY oldids LOOP
+          newids := newids || ARRAY[ROW(
+            old.use,
+            old.type,
+            old.system,
+            old.value,
+            old.period,
+            ROW(null, null, null, old.assigner)
+          )::fhir.new_identifier];
+        END LOOP;
+      END;
+    $migrate$
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.patients
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.new_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.practitioners
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.new_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.service_requests
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.new_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`DROP FUNCTION fhir.migrate_identifier`);
+  await query.sequelize.query(`DROP TYPE fhir.identifier`);
+  await query.sequelize.query(`ALTER TYPE fhir.new_identifier RENAME TO identifier`);
 }
 
 export async function down(query) {
-  await query.sequelize.query(`ALTER TYPE fhir.identifier ALTER ATTRIBUTE assigner SET DATA TYPE text`);
+  await query.sequelize.query(`
+    CREATE TYPE fhir.old_identifier AS (
+      use             text,
+      type            fhir.codeable_concept,
+      system          text,
+      value           text,
+      period          fhir.period,
+      assigner        text
+    )
+  `);
+
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION fhir.migrate_identifier(
+      IN newids fhir.identifier[],
+      OUT oldids fhir.old_identifier[]
+    )
+    RETURNS fhir.old_identifier[] LANGUAGE plpgsql
+    IMMUTABLE PARALLEL SAFE RETURNS NULL ON NULL INPUT
+    AS $migrate$
+      DECLARE
+        new fhir.identifier;
+      BEGIN
+        FOREACH new IN ARRAY newids LOOP
+          oldids := oldids || ARRAY[ROW(
+            new.use,
+            new.type,
+            new.system,
+            new.value,
+            new.period,
+            ((new).assigner).display
+          )::fhir.old_identifier];
+        END LOOP;
+      END;
+    $migrate$
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.patients
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.old_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.practitioners
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.old_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`
+    ALTER TABLE fhir.service_requests
+    ALTER COLUMN identifier DROP DEFAULT,
+    ALTER COLUMN identifier TYPE fhir.old_identifier[]
+    USING fhir.migrate_identifier(identifier),
+    ALTER COLUMN identifier SET DEFAULT '{}'
+  `);
+
+  await query.sequelize.query(`DROP FUNCTION fhir.migrate_identifier`);
+  await query.sequelize.query(`DROP TYPE fhir.identifier`);
+  await query.sequelize.query(`ALTER TYPE fhir.old_identifier RENAME TO identifier`);
+
   await query.sequelize.query('DROP TYPE fhir.reference');
 }
