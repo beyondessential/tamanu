a60af12bf061d5646f2c653c2b47e53adbb96988
Tom Caiger <caigertom@gmail.com>
2022-10-17 16:31:19 +1300

    Update 1662006885000-updateLabTestDate.ts

--- packages/mobile/App/migrations/1662006885000-updateLabTestDate.ts
+++ packages/mobile/App/migrations/1662006885000-updateLabTestDate.ts
@@ -7,9 +7,7 @@ const ISO9075_FORMAT_LENGTH = ISO9075_FORMAT.length;
 // When we're upgrading into a version that uses migrations, we may have run a model sync
 // Test if this is the case, and if it was, don't try to rename the column
 async function testSyncedBeforeMigration(queryRunner: QueryRunner): Promise<boolean> {
-  const legacyColumn = await queryRunner.query(
-  "PRAGMA table_info('labTest') WHERE name='date';"
-  );
+  const legacyColumn = await queryRunner.query("PRAGMA table_info('labTest') WHERE name='date';");
   return legacyColumn.length > 0;
 }
 
