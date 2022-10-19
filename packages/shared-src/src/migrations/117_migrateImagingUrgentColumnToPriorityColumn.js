189b27ddaf0390406bc8d7c9a6a75cca42f1ae48
Chris Tran <chris@beyondessential.com.au>
2022-10-19 11:22:57 +1100

    epi-143: migrate non urgent IR to have priority routine

--- packages/shared-src/src/migrations/117_migrateImagingUrgentColumnToPriorityColumn.js
+++ packages/shared-src/src/migrations/117_migrateImagingUrgentColumnToPriorityColumn.js
@@ -12,6 +12,12 @@ export async function up(query) {
     WHERE urgent IS TRUE;
   `);
 
+  await query.sequelize.query(`
+    UPDATE imaging_requests
+    SET priority = 'routine'
+    WHERE urgent IS FALSE;
+  `);
+
   await query.removeColumn('imaging_requests', 'urgent');
 }
 
@@ -26,6 +32,12 @@ export async function down(query) {
     UPDATE imaging_requests
     SET urgent = TRUE
     WHERE priority = 'urgent';
+  `);
+
+  await query.sequelize.query(`
+    UPDATE imaging_requests
+    SET urgent = FALSE
+    WHERE priority = 'routine';
   `);
 
   await query.removeColumn('imaging_requests', 'priority');
