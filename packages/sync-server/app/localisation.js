50eb590401ad8765218925bb92609d388b8106bd
Edwin <edmofro@gmail.com>
2022-10-21 10:10:08 +1300

    Shift syncAllEncountersForTheseVaccines out of localisation

--- packages/sync-server/app/localisation.js
+++ packages/sync-server/app/localisation.js
@@ -335,12 +335,6 @@ const rootLocalisationSchema = yup
       .required()
       .noUnknown(),
     printMeasures: printMeasuresSchema,
-    sync: yup
-      .object({
-        syncAllEncountersForTheseVaccines: yup.array(yup.string().required()).defined(),
-      })
-      .required()
-      .noUnknown(),
     disabledReports: yup.array(yup.string().required()).defined(),
   })
   .required()
