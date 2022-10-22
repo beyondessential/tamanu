8a33b9677f3375436f6318222840f5736e569073
Edwin <edmofro@gmail.com>
2022-10-23 00:00:38 +1300

    Wait longer for count

--- packages/lan/app/sync/CentralServerConnection.js
+++ packages/lan/app/sync/CentralServerConnection.js
@@ -215,7 +215,7 @@ export class CentralServerConnection {
     // poll the pull count endpoint until we get a valid response - it takes a while for
     // setPullFilter to finish populating the snapshot of changes
     const waitTime = 1000; // retry once per second
-    const maxAttempts = 300; // for a maximum of five minutes
+    const maxAttempts = 1200; // for a maximum of twenty minutes
     for (let attempt = 1; attempt <= maxAttempts; attempt++) {
       const count = await this.fetch(`sync/${sessionId}/pull/count`);
       if (count !== null) {
