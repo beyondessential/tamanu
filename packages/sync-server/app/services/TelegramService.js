b3852ba0bd575d7fbcc1174a202622bfe9948e09
Daniel Nash <dannash200@gmail.com>
2022-10-27 12:11:34 +1100

    hackathon: more specific messages telegram

--- packages/sync-server/app/services/TelegramService.js
+++ packages/sync-server/app/services/TelegramService.js
@@ -3,51 +3,48 @@ import TG from 'telegram-bot-api';
 import config from 'config';
 
 import { log } from 'shared/services/logging';
+import { addWeeks, format } from 'date-fns';
 import {
-  PATIENT_COMMUNICATION_CHANNELS, 
-  PATIENT_COMMUNICATION_TYPES 
+  PATIENT_COMMUNICATION_CHANNELS,
+  PATIENT_COMMUNICATION_TYPES,
 } from 'shared/constants/comms';
 import { COMMUNICATION_STATUSES } from 'shared/constants/statuses';
 
 export class TelegramService {
-
   constructor(context) {
     this.context = context;
     this.api = new TG({
       token: config.telegram.apiKey,
-    })
+    });
 
     const mp = new TG.GetUpdateMessageProvider();
     this.api.setMessageProvider(mp);
-    
+
     this.api.on('update', ({ message }) => {
       try {
         this.onMessage(message);
-      } catch(e) {
+      } catch (e) {
         log.error(e);
       }
     });
   }
 
   start() {
-    log.info("Starting telegram bot");
+    log.info('Starting telegram bot');
     this.api.start();
   }
-  
+
   stop() {
     log.info('Stopping telegram bot');
     try {
       this.api.stop();
-    } catch(e) {
-      log.error("Error stopping telegram bot", e);
+    } catch (e) {
+      log.error('Error stopping telegram bot', e);
     }
   }
 
   onMessage(message) {
-    const { 
-      text,
-      chat,
-    } = message;
+    const { text, chat } = message;
 
     // telegram interaction starts with the user's client sending us
     // a message that looks like `/start 1234-1234-1234-1234-abcde`
@@ -59,7 +56,7 @@ export class TelegramService {
       const [, patientId] = startMatch;
       this.onLinkAccount(patientId, chat.id);
     } else {
-      log.warn("Unrecognised telegram message", message);
+      log.warn('Unrecognised telegram message', message);
     }
   }
 
@@ -87,18 +84,19 @@ export class TelegramService {
     await models.PatientCommunication.create({
       channel: PATIENT_COMMUNICATION_CHANNELS.WHATSAPP, // TODO: de-enumify and use 'telegram'
       status: COMMUNICATION_STATUSES.QUEUED,
-      type: PATIENT_COMMUNICATION_TYPES.CERTIFICATE,  // TODO: de-enumify and use 'vaccineReminder'
-      content: `Here's the reminder we promised for *{patientName}*`,
+      type: PATIENT_COMMUNICATION_TYPES.CERTIFICATE, // TODO: de-enumify and use 'vaccineReminder'
+      content:
+        'Talofa Lava,\nThis is a reminder that the next {vaccineType} vaccination for {patientName} is scheduled for {appointmentDate}. If you cannot attend on this day, please contact your local health centre. Do it for Samoa!',
       patientId,
-    })
+    });
 
     const patientName = patient.getFormattedName();
 
     // Send a confirmation message to the patient
-    log.info("Sending telegram link confirmation", { patientId });
+    log.info('Sending telegram link confirmation', { patientId });
     this.api.sendMessage({
       chat_id: chatId,
-      text: `Successfully linked to Tamanu patient: *${patientName}*.`,
+      text: `Tolafa Lava,\nThank you for registering ${patientName} to receive notifications from Tumanu. We use this service to notify you and your family about all your upcoming appointments and health reminders. Do it for Samoa!`,
       parse_mode: 'Markdown',
     });
   }
@@ -116,7 +114,11 @@ export class TelegramService {
     }
 
     // TODO: Smarter text replacement system
-    const text = content.replace("{patientName}", patient.getFormattedName());
+    const text = content
+      .replace('{patientName}', patient.getFormattedName())
+      // hardcoded extra info for demo
+      .replace('{vaccineType}', 'COVID-19 Vaccine')
+      .replace('{vaccineDate}', format(addWeeks(new Date(), 2), 'PPPPpppp'));
 
     this.api.sendMessage({
       chat_id: chatId,
