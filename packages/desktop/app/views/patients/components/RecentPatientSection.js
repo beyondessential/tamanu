e45f67f58217b0ec9ceafaa0344dd1a67e320f8b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-10-27 12:21:26 +1100

    Combine both efforts to make this work

--- packages/desktop/app/views/patients/components/RecentPatientSection.js
+++ packages/desktop/app/views/patients/components/RecentPatientSection.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Typography } from '@material-ui/core';
+import { useQuery } from '@tanstack/react-query';
 import { Colors, SEX_VALUE_INDEX } from '../../../constants';
 import { DateDisplay } from '../../../components';
 import { getPatientNameAsString } from '../../../components/PatientNameDisplay';
+import { useApi } from '../../../api';
 
 const Container = styled.div`
   border-bottom: 1px solid ${Colors.outline};
@@ -39,21 +41,18 @@ const CardContainer = styled.div`
 `;
 
 export const RecentPatientSection = () => {
-  const cards = [1, 2, 3, 4, 5, 6];
+  const api = useApi();
+  const { data: queryData } = useQuery(['recentPatients'], () => api.get(`recentPatients`), {
+    initialData: {},
+  });
+  const recentPatients = queryData?.data?.slice(0, 6);
+
   return (
     <>
       <SectionLabel>Recent patients</SectionLabel>
       <CardContainer>
-        {cards.map(a => (
-          <PatientCard
-            patient={{
-              firstName: `Test-${a}`,
-              lastName: 'Patient',
-              sex: 'female',
-              dateOfBirth: '12/12/2012',
-              displayId: 'ABCD123456',
-            }}
-          />
+        {recentPatients?.map(({ createdAt, Patient }) => (
+          <PatientCard patient={Patient} key={createdAt} />
         ))}
       </CardContainer>
     </>
