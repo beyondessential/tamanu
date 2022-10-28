c86817e23cd3a18214472606bff6e7cde59e7206
Tim Kennedy <t.m.knndy@gmail.com>
2022-10-28 16:17:52 +1100

    Add TODO comment so this doesn't get merged without tests

--- packages/sync-server/app/integrations/fhir/routes.js
+++ packages/sync-server/app/integrations/fhir/routes.js
@@ -3,6 +3,7 @@ import { fhirRoutes } from '../../hl7fhir';
 import { userMiddleware } from '../../auth/userMiddleware';
 
 // handles its own authentication using a separate secret + token issuance workflow
+// TODO: write tests for this once I've verified with Emilio it's the right route
 export const publicRoutes = fhirRoutes({
   middleware: [
     userMiddleware({
