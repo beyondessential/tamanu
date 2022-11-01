9d736e59622810b75c804b7533c7ea41f4f50356
Tom McLean <tom.a.mclean@gmail.com>
2022-11-01 13:07:21 +1100

    Bump version

--- packages/sync-server/app/middleware/versionCompatibility.js
+++ packages/sync-server/app/middleware/versionCompatibility.js
@@ -8,15 +8,15 @@ import { InvalidClientHeadersError } from 'shared/errors';
 export const SUPPORTED_CLIENT_VERSIONS = {
   'Tamanu LAN Server': {
     min: '1.21.0',
-    max: '1.21.3', // note that higher patch versions will be allowed to connect
+    max: '1.21.4', // note that higher patch versions will be allowed to connect
   },
   'Tamanu Desktop': {
     min: '1.21.0',
-    max: '1.21.3', // note that higher patch versions will be allowed to connect
+    max: '1.21.4', // note that higher patch versions will be allowed to connect
   },
   'Tamanu Mobile': {
     min: '1.21.44',
-    max: '1.21.44', // note that higher patch versions will be allowed to connect
+    max: '1.21.46', // note that higher patch versions will be allowed to connect
   },
   'fiji-vps': {
     min: null,
