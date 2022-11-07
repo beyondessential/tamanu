7afc0d05a5075bd3505af86785a00247a64b04a1
Félix Saparelli <felix@passcod.name>
2022-11-08 11:29:21 +1300

    Test that tick columns exist or not as needed

--- packages/lan/jest.config.js
+++ packages/lan/jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   testRegex: '(\\.|/)(test|spec)\\.[jt]sx?$',
   globalSetup: '<rootDir>/__tests__/setup.js',
   globalTeardown: '<rootDir>/__tests__/teardown.js',
-  setupFilesAfterEnv: ['<rootDir>/__tests__/configureEnvironment.js'],
+  setupFilesAfterEnv: ['<rootDir>/__tests__/configureEnvironment.js', 'jest-expect-message'],
   collectCoverageFrom: ['app/**/*.js'],
 };
