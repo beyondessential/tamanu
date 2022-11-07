5bdbc70a359786b3cd908c7c69e1949048be6e09
Edwin <edmofro@gmail.com>
2022-11-07 14:04:39 +1300

    Add additional metadata fields

--- packages/shared-src/src/migrations/131_createFieldWiseUpdatedAtTrigger.js
+++ packages/shared-src/src/migrations/131_createFieldWiseUpdatedAtTrigger.js
@@ -1,5 +1,13 @@
 const CURRENT_SYNC_TIME_KEY = 'currentSyncTime';
 
+const METADATA_FIELDS = [
+  'created_at',
+  'updated_at',
+  'deleted_at',
+  'updated_at_sync_tick',
+  'updated_at_by_field',
+];
+
 export async function up(query) {
   await query.sequelize.query(`
     CREATE FUNCTION set_updated_at_by_field()
@@ -12,14 +20,18 @@ export async function up(query) {
         IF (OLD IS NULL) THEN
           SELECT JSON_OBJECT_AGG(new_json.key, (SELECT value FROM local_system_facts WHERE key = '${CURRENT_SYNC_TIME_KEY}'))::jsonb
           FROM jsonb_each(to_jsonb(NEW)) AS new_json
-          WHERE new_json.value <> 'null'::jsonb AND new_json.key <> 'updated_at_sync_tick' AND new_json.key <> 'updated_at_by_field'
+          WHERE new_json.value <> 'null'::jsonb AND new_json.key NOT IN (${METADATA_FIELDS.map(
+            m => `'${m}'`,
+          ).join(',')})
           INTO NEW.updated_at_by_field;
         ELSIF (OLD.updated_at_by_field IS NULL OR OLD.updated_at_by_field::text = NEW.updated_at_by_field::text) THEN
           SELECT COALESCE(OLD.updated_at_by_field::jsonb, '{}'::jsonb) || COALESCE(JSON_OBJECT_AGG(changed_columns.column_name, (SELECT value FROM local_system_facts WHERE key = '${CURRENT_SYNC_TIME_KEY}'))::jsonb, '{}'::jsonb) FROM (
             SELECT old_json.key AS column_name
             FROM jsonb_each(to_jsonb(OLD)) AS old_json
             CROSS JOIN jsonb_each(to_jsonb(NEW)) AS new_json
-            WHERE old_json.key = new_json.key AND new_json.value IS DISTINCT FROM old_json.value AND old_json.key <> 'updated_at_sync_tick' AND old_json.key <> 'updated_at_by_field'
+            WHERE old_json.key = new_json.key AND new_json.value IS DISTINCT FROM old_json.value AND old_json.key NOT IN (${METADATA_FIELDS.map(
+              m => `'${m}'`,
+            ).join(',')})
           ) as changed_columns INTO NEW.updated_at_by_field;
         END IF;
         RETURN NEW;
