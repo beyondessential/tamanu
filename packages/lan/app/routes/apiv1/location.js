f1e49bc0a786c0967c404a8a08fb8bcf77e1660e
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-11-07 14:59:29 +1300

    WAITM-537 Revert "WAITM-537 Update location api endpoints"
    This reverts commit e14d9e4c2a52e62cc23083131b370e33fd4f2f4c.

--- packages/lan/app/routes/apiv1/location.js
+++ packages/lan/app/routes/apiv1/location.js
@@ -1,21 +1,10 @@
 import config from 'config';
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import { Op } from 'sequelize';
 
 import { simpleGet, simplePut, simplePost } from './crudHelpers';
 
 export const location = express.Router();
-
-const locationLookup = async (req, whereClause = {}) => {
-  req.checkPermission('list', 'Location');
-  if (!config.serverFacilityId) {
-    return [];
-  }
-  return req.models.Location.findall({
-    where: { facilityId: config.serverFacilityId, ...whereClause },
-  });
-};
 
 location.get('/:id', simpleGet('Location'));
 location.put('/:id', simplePut('Location'));
@@ -23,34 +12,16 @@ location.post('/$', simplePost('Location'));
 location.get(
   '/$',
   asyncHandler(async (req, res) => {
-    const localisation = req.getLocalisation();
-    const locations = await locationLookup(req);
-    // parentLocationId will always be a column in the db, but we only want to expose it if the feature is enabled
-    if (!localisation?.features.locationHierarchy) {
-      res.send(locations.map(({ parentLocationId, ...rest }) => rest));
+    req.checkPermission('list', 'Location');
+    if (!config.serverFacilityId) {
+      res.send([]);
+      return;
     }
-    res.send(locations);
-  }),
-);
-
-// These routes are specific to the location heirarchy feature
-// We assume they'll only be requested from the front end if the feature is enabled
-location.get(
-  '/:id/children',
-  asyncHandler(async (req, res) => {
-    const locations = await locationLookup(req, {
-      // Return the parent location along side all its children so the parent info will be available to the frontend
-      [Op.or]: [{ id: req.params.id }, { parentLocationId: req.params.id }],
+    const locations = await req.models.Location.findAll({
+      where: {
+        facilityId: config.serverFacilityId,
+      },
     });
-    res.send(locations);
-  }),
-);
-
-location.get(
-  '/roots',
-  asyncHandler(async (req, res) => {
-    // A location without a parent is assumed to be a root
-    const locations = locationLookup(req, { parentLocationId: null });
     res.send(locations);
   }),
 );
