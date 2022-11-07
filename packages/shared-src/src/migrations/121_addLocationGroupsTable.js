e5a1fc5a3e3948fa12e995cc89c7ed269ae07b50
Ethan McQuarrie <69437175+EMcQ-BES@users.noreply.github.com>
2022-11-08 11:26:27 +1300

    WAITM-537 Location hierarchy db models (#3105)
    * WAITM-537 Migration to add parent location id to location table
    
    * WAITM-537 Add parent location id to location model
    
    * WAITM-537 Update location api endpoints
    
    * WAITM-537 Update migration to add new location groups table
    
    * WAITM-537 Update location model to refer to (currently non-exisitant)
    location groups model
    
    * WAITM-537 Revert "WAITM-537 Update location api endpoints"
    
    This reverts commit e14d9e4c2a52e62cc23083131b370e33fd4f2f4c.
    
    * WAITM-537 Add LocationGroup model
    
    * WAITM-537 Add LocationGroups to refdataImporter dependencies
    
    * WAITM-537 Add locationGroup api endpoints
    
    * WAITM-537 Add location group permissions to the roles file
    
    * WAITM-537 Migration fixup
    
    * WAITM-537 Update LocationGroup model to include facility association

---
+++ packages/shared-src/src/migrations/121_addLocationGroupsTable.js
@@ -0,0 +1,57 @@
+import Sequelize from 'sequelize';
+
+export async function up(query) {
+  await query.createTable('location_groups', {
+    id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
+    },
+    deleted_at: {
+      type: Sequelize.DATE,
+      allowNull: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    code: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    facility_id: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      references: {
+        model: 'facilities',
+        key: 'id',
+      },
+    },
+  });
+
+  await query.addColumn('locations', 'location_group_id', {
+    type: Sequelize.STRING,
+    allowNull: true,
+    default: null,
+    references: {
+      model: 'location_groups',
+      key: 'id',
+    },
+  });
+}
+
+export async function down(query) {
+  await query.removeColumn('locations', 'location_group_id');
+  await query.dropTable('location_groups');
+}
