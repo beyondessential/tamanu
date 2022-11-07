0b21c6b0fa8c2eb14ad1ff911a69124e27bd56ed
Ethan McQuarrie <ethan@beyondessential.com.au>
2022-11-07 15:59:38 +1300

    WAITM-537 Add LocationGroup model

---
+++ packages/shared-src/src/models/LocationGroup.js
@@ -0,0 +1,41 @@
+import { Sequelize } from 'sequelize';
+import { SYNC_DIRECTIONS } from 'shared/constants';
+import { InvalidOperationError } from 'shared/errors';
+import { Model } from './Model';
+
+export class LocationGroup extends Model {
+  static init({ primaryKey, ...options }) {
+    const validate = {
+      mustHaveFacility() {
+        if (!this.deletedAt && !this.facilityId) {
+          throw new InvalidOperationError('A location group must have a facility.');
+        }
+      },
+    };
+    super.init(
+      {
+        id: primaryKey,
+        code: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        ...options,
+        validate,
+        syncConfig: { syncDirection: SYNC_DIRECTIONS.PULL_ONLY },
+        indexes: [{ unique: true, fields: ['code'] }],
+      },
+    );
+  }
+
+  static initRelations(models) {
+    this.hasMany(models.Location, {
+      foreignKey: 'locationGroupId',
+    });
+  }
+}
