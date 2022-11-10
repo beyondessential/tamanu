8e6301a687f0581b2347e4ad628a1889c2c10b01
Tim Kennedy <t.m.knndy@gmail.com>
2022-11-10 11:39:17 +1100

    Add omniLab stub to integrations

---
+++ packages/sync-server/app/integrations/omniLab/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import config from 'config';
+import { userMiddleware } from '../../auth/userMiddleware';
+
+export const publicRoutes = Router();
+
+// handles its own authentication using a separate secret + token issuance workflow, see default.json
+// TODO: write tests for this
+publicRoutes.use(userMiddleware({ secret: config.integrations.omniLab.secret }));
+
+publicRoutes.get('/', (req, res) => {
+  res.status(200).send({ implemented: false });
+});
