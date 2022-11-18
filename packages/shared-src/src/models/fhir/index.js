731a995f4cf10dfdc86483d452b80278e25b9f38
Félix Saparelli <felix@passcod.name>
2022-11-18 18:50:47 +1300

    remove todo tracked in extant card

--- packages/shared-src/src/models/fhir/index.js
+++ packages/shared-src/src/models/fhir/index.js
@@ -1,7 +1,2 @@
 export * from './Patient';
 export * from './ServiceRequest';
-// TODO:
-// export * from './DiagnosticReport';
-// export * from './Immunization';
-// export * from './Observation';
-// export * from './Device';
