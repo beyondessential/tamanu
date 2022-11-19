b18dbf70a54af804bcdf6aa9823d15d01994cff1
Félix Saparelli <felix@passcod.name>
2022-11-19 13:13:38 +1300

    Test servers

---
+++ .github/scripts/install-postgres-ubuntu.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+pgversion="${1:?version must be provided}"
+
+# from https://wiki.postgresql.org/wiki/Apt
+sudo apt install -y curl ca-certificates gnupg
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt.postgresql.org.gpg >/dev/null
+echo 'deb http://apt.postgresql.org/pub/repos/apt jammy-pgdg main' | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+sudo apt update
+sudo apt install -y "postgresql-$pgversion"
