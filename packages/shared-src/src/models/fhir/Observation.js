780f025c229041ff744984d98ade16ea4aa0119d
Félix Saparelli <felix@passcod.name>
2022-11-24 16:02:40 +1300

    Add create route

--- packages/shared-src/src/models/fhir/Observation.js
+++ packages/shared-src/src/models/fhir/Observation.js
@@ -1,6 +1,9 @@
 import { DataTypes } from 'sequelize';
+import * as yup from 'yup';
+
 import { FhirResource } from './Resource';
 import { arrayOf } from './utils';
+
 import { FhirAnnotation, FhirIdentifier, FhirReference } from '../../services/fhirTypes';
 import { FHIR_INTERACTIONS } from '../../constants';
 
@@ -8,13 +11,13 @@ export class FhirObservation extends FhirResource {
   static init(options) {
     super.init(
       {
-        identifier: arrayOf(FhirIdentifier),
-        basedOn: arrayOf(FhirReference),
+        identifier: arrayOf('identifier', DataTypes.FHIR_IDENTIFIER),
+        basedOn: arrayOf('basedOn', DataTypes.FHIR_REFERENCE),
         status: {
           type: DataTypes.TEXT,
           allowNull: false,
         },
-        note: arrayOf(FhirAnnotation),
+        note: arrayOf('note', DataTypes.FHIR_ANNOTATION),
       },
       options,
     );
@@ -38,16 +41,3 @@ export class FhirObservation extends FhirResource {
     // Take a FhirResource and save it into Tamanu
   }
 }
-
-/*
-The plan:
-- in the server, a POST route (named after create FHIR op) per resource
-- the route takes a JSON body and validates it against the schema
-- also check headers and such as per spec
-- if valid, create the corresponding FhirResource from it (*in memory*)
-  - call pushUpstream on it, that returns an instance of the UpstreamModel
-  - if the resource has an UpstreamModel defined:
-    - queue it for materialisation
-    - include in the materialisation request the ID of the incoming, if present
-- return status and body as per spec
-*/
--- packages/sync-server/app/hl7fhir/materialised/create.js
+++ packages/sync-server/app/hl7fhir/materialised/create.js
@@ -2,12 +2,34 @@ import asyncHandler from 'express-async-handler';
 
 import { OperationOutcome } from 'shared/utils/fhir';
 
+import { fhirQueue } from '../../tasks/FhirMaterialiser';
+
+async function mapErr(promise, fn) {
+  try {
+    return await promise;
+  } catch (err) {
+    throw fn(err);
+  }
+}
+
 export function createHandler(FhirResource) {
   return asyncHandler(async (req, res) => {
-    res.header('Content-Type', 'application/fhir+json; fhirVersion=4.3');
-
     try {
-      // res.send(record.asFhir());
+      const validated = await mapErr(
+        FhirResource.INTAKE_SCHEMA.validate(req.body, { stripUnknown: true }),
+        err => OperationOutcome.fromYupError(err),
+      );
+
+      const resource = new FhirResource(validated);
+      const upstream = await resource.pushUpstream();
+
+      if (FhirResource.CAN_DO.has(FHIR_INTERACTIONS.INTERNAL.MATERIALISE)) {
+        fhirQueue(FhirResource.fhirName, upstream.id);
+      }
+
+      // in spec, we should Location: to the resource, but we don't have it yet
+      // TODO: generate ID here, and return it (if resource can be materialised)
+      res.status(201).send();
     } catch (err) {
       const oo = new OperationOutcome([err]);
       res.status(oo.status()).send(oo.asFhir());
