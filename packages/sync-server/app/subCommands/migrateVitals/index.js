825f0199b3c434aae170dac6bffba34dced13fbd
Daniel Nash <dannash200@gmail.com>
2022-11-25 18:11:50 +1300

    waitm-526: heavy wip

--- packages/sync-server/app/subCommands/migrateVitals/index.js
+++ packages/sync-server/app/subCommands/migrateVitals/index.js
@@ -1,11 +1,19 @@
 import { Command } from 'commander';
+import { Sequelize } from 'sequelize/types';
 import { SURVEY_TYPES } from 'shared/constants';
 import { log } from 'shared/services/logging';
 import { initDatabase } from '../../database';
 
+const COLUMNS_TO_CODE = {
+  date: 'PatientVitalsDate',
+  temperature: 'PatientVitalsTemperature',
+};
+
 export async function migrateVitals(options) {
   const store = await initDatabase({ testMode: false });
   const { models } = store;
+
+  const batchCount = 100;
 
   const vitalsSurvey = await models.Survey.findOne({
     where: {
@@ -18,11 +26,58 @@ export async function migrateVitals(options) {
     process.exit(1);
   }
 
-//   const vitals = await models.Vital.findAll();
-//   console.log(vitals);
+  log.info(`Migrating ${toProcess} vitals to survey responses`);
+
+  let toProcess = await models.Vital.count({
+    where: {
+      converted: false,
+    },
+  });
+  while (toProcess) {
+    log.debug('Starting transaction');
+    /**
+     *     
+      return models.SurveyResponse.createWithAnswers(updatedBody);
+
+       models.SurveyResponseAnswer.create({
+        dataElementId: dataElement.id,
+        body,
+        responseId: record.id,
+      });
+    });
+     */
+    await models.Vital.sequelize.transaction(
+      {
+        // strongest level to be sure to read/write good data
+        isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ,
+      },
+      async () => {
+        const vitalsChunk = await models.Vital.findAll({
+          where: {
+            converted: false,
+          },
+          limit: batchCount,
+        });
+
+        const surveyResponses = vitalsChunk.map(
+          ({ encounterId, updatedAt, createdAt, dateRecorded }) => {
+            return {
+              encounterId,
+              updatedAt,
+              createdAt,
+              // Should these be date recorded or created at
+              startTime: dateRecorded,
+              endTime: dateRecorded,
+              surveyId: vitalsSurvey.id,
+            };
+          },
+        );
+      },
+    );
+  }
 }
 
 export const migrateVitalsCommand = new Command('migrateVitals')
-  .description('Migrates vitals from legacy format to new format')
+  .description('Migrates vitals from legacy format to new format as survey responses')
 
   .action(migrateVitals);
