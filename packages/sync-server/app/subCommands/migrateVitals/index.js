9e7f139a4ae11b13ed6fa7be06ec7c389d2c7e19
Daniel Nash <dannash200@gmail.com>
2022-11-28 10:07:05 +1300

    waitm-526: heavy wip2

--- packages/sync-server/app/subCommands/migrateVitals/index.js
+++ packages/sync-server/app/subCommands/migrateVitals/index.js
@@ -4,8 +4,10 @@ import { SURVEY_TYPES } from 'shared/constants';
 import { log } from 'shared/services/logging';
 import { initDatabase } from '../../database';
 
-const COLUMNS_TO_CODE = {
-  date: 'PatientVitalsDate',
+const BATCH_COUNT = 100;
+// or should it be to data element code
+const COLUMNS_TO_DATA_ELEMENT_ID = {
+  dateRecorded: 'PatientVitalsDate',
   temperature: 'PatientVitalsTemperature',
 };
 
@@ -13,7 +15,6 @@ export async function migrateVitals(options) {
   const store = await initDatabase({ testMode: false });
   const { models } = store;
 
-  const batchCount = 100;
 
   const vitalsSurvey = await models.Survey.findOne({
     where: {
@@ -35,17 +36,7 @@ export async function migrateVitals(options) {
   });
   while (toProcess) {
     log.debug('Starting transaction');
-    /**
-     *     
-      return models.SurveyResponse.createWithAnswers(updatedBody);
 
-       models.SurveyResponseAnswer.create({
-        dataElementId: dataElement.id,
-        body,
-        responseId: record.id,
-      });
-    });
-     */
     await models.Vital.sequelize.transaction(
       {
         // strongest level to be sure to read/write good data
@@ -56,22 +47,33 @@ export async function migrateVitals(options) {
           where: {
             converted: false,
           },
-          limit: batchCount,
+          limit: BATCH_COUNT,
         });
 
-        const surveyResponses = vitalsChunk.map(
-          ({ encounterId, updatedAt, createdAt, dateRecorded }) => {
-            return {
-              encounterId,
-              updatedAt,
-              createdAt,
-              // Should these be date recorded or created at
-              startTime: dateRecorded,
-              endTime: dateRecorded,
-              surveyId: vitalsSurvey.id,
-            };
-          },
-        );
+        // Should we bulk create the responses then bulk create the answers - currently we are looping over
+        // the vitals and creating a response and answer for each one which kinda defeats the purpose of batching doesnt it.
+        // We could also maybe start these concurrently if we cant do the bulk survey response method.
+
+        for await (const vital of vitalsChunk) {
+          const { encounterId, updatedAt, createdAt, dateRecorded } = vital;
+          const { id: responseId } = await models.SurveyResponse.create({
+            encounterId,
+            updatedAt,
+            createdAt,
+            // Should these be date recorded or created at
+            startTime: dateRecorded,
+            endTime: dateRecorded,
+            surveyId: vitalsSurvey.id,
+          });
+          const answerData = Object.entries(vital)
+            .filter(([key]) => COLUMNS_TO_DATA_ELEMENT_ID[key])
+            .map(([key, value]) => ({
+              dataElementId: COLUMNS_TO_DATA_ELEMENT_ID[key],
+              responseId,
+              body: value,
+            }));
+          await models.Answer.bulkCreate(answerData);
+        }
       },
     );
   }
