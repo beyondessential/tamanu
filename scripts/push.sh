4b5b630dc427bf86d076faf123bde973cbc5578b
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2022-11-29 10:15:45 +1300

    Revert "TAN-1409: Build server zips on CI (#3232)" (#3242)
    This reverts commit 6d9487e2717b58b538aa9e3a24351b2f3c218af2.

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "Pushing to S3"
 ls ./deploy
-
-MAYBE_VERSION="$(jq '.version' ./package.json --raw-output)"
-VERSION="${MAYBE_VERSION?could not calculate version}"
-
-TRUNCATED_BRANCH=${CI_BRANCH%%'-'*}
-S3DIR=tamanu-${TRUNCATED_BRANCH}/${VERSION}
-aws s3 cp ./deploy/ s3://${S3_BUCKET}/${S3DIR} --recursive --no-progress
+aws s3 cp ./deploy/ s3://${S3_BUCKET}/ --recursive --exclude "server-*" --no-progress
