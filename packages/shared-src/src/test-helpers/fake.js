808735ccd8bd3831c9fe468b7146140666f6327d
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2022-11-27 23:55:52 -0500

    Create models for new fhir types

--- packages/shared-src/src/test-helpers/fake.js
+++ packages/shared-src/src/test-helpers/fake.js
@@ -24,6 +24,9 @@ import {
   FhirHumanName,
   FhirPatientLink,
   FhirReference,
+  FhirExtension,
+  FhirImmunizationPerformer,
+  FhirImmunizationProtocolApplied,
 } from '../services/fhirTypes';
 
 const chance = new Chance();
@@ -237,6 +240,9 @@ const FIELD_HANDLERS = {
   FHIR_HUMAN_NAME: (...args) => FhirHumanName.fake(...args),
   FHIR_PATIENT_LINK: (...args) => FhirPatientLink.fake(...args),
   FHIR_REFERENCE: (...args) => FhirReference.fake(...args),
+  FHIR_EXTENSION: (...args) => FhirExtension.fake(...args),
+  FHIR_IMMUNIZATION_PERFORMER: (...args) => FhirImmunizationPerformer.fake(...args),
+  FHIR_IMMUNIZATION_PROTOCOL_APPLIED: (...args) => FhirImmunizationProtocolApplied.fake(...args),
 };
 
 const IGNORED_FIELDS = ['createdAt', 'updatedAt', 'deletedAt', 'updatedAtSyncTick'];
