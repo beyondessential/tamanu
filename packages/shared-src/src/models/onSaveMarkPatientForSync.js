df4dc2f11d5e222b36b0033831fd18f2c7ac0c02
Edwin <edmofro@gmail.com>
2022-12-12 11:38:38 +1300

    Mark patient for sync on sub-table interaction

---
+++ packages/shared-src/src/models/onSaveMarkPatientForSync.js
@@ -0,0 +1,34 @@
+import config from 'config';
+
+const HOOK_TRIGGER = 'beforeSave';
+const HOOK_NAME = 'markPatientForSync';
+
+// any interaction with a non-syncing patient should mark it for ongoing sync
+export const onSaveMarkPatientForSync = model => {
+  const facilityId = config.serverFacilityId;
+  if (!facilityId) {
+    // no need to add the hook on the central server
+    return;
+  }
+
+  // we remove and add the hook because Sequelize doesn't have a good way
+  // to detect which hooks have already been added to a model in its
+  // public API
+  model.removeHook(HOOK_TRIGGER, HOOK_NAME);
+  model.addHook(HOOK_TRIGGER, HOOK_NAME, async ({ patientId }) => {
+    // for some models (e.g. DocumentMetadata) patient is an optional field; check it is defined
+    if (!patientId) {
+      return;
+    }
+
+    // upsert patient_facilities record to mark the patient for sync in this facility
+    await model.sequelize.query(
+      `
+      INSERT INTO patient_facilities (patient_id, facility_id)
+      VALUES (:patientId, :facilityId )
+      ON CONFLICT (patient_id, facility_id) DO NOTHING;
+    `,
+      { replacements: { patientId, facilityId } },
+    );
+  });
+};
