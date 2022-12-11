08d7491e0d85b6d79001f33f58b44f241adc11f8
Tim Kennedy <tim@beyondessential.com.au>
2022-12-12 10:49:56 +1100

    EPI-287: Show a toast on unexpected network errors (status>=500) (#3263)
    * EPI-287: Show a toast on unexpected network errors (status>=500)
    
    * Throw an error immediately if auth has expired on desktop
    
    * Lint
    
    * Allow isErrorUnknown to be passed to fetch
    
    * Don't show error toast in DataFetchingTable

--- packages/desktop/app/api/index.js
+++ packages/desktop/app/api/index.js
@@ -2,3 +2,4 @@ export * from './ApiContext';
 export * from './connectApi';
 export * from './suggesters';
 export * from './useApi';
+export { isErrorUnknownDefault, isErrorUnknownAllow404s } from './TamanuApi';
