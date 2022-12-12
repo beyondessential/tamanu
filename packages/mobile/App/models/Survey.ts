8a706041df41de6be491a6d18dde3d001d2a0cd2
Tom McLean <tom.a.mclean@gmail.com>
2022-12-12 16:04:37 +1100

    SAV-42: Fix program survey performance (#3303)
    * Don't load survey responses in the background
    
    * Perform patient filtering on backend rather than in JS memory

--- packages/mobile/App/models/Survey.ts
+++ packages/mobile/App/models/Survey.ts
@@ -45,10 +45,17 @@ export class Survey extends BaseModel implements ISurvey {
     return ability.can('submit', this);
   }
 
-  static async getResponses(surveyId: string): Promise<ISurveyResponse[]> {
+  static async getResponses(
+    surveyId: string,
+    patientId?: string
+  ): Promise<ISurveyResponse[]> {
+    const patientFilter = patientId
+      ? { encounter: { patient: { id: patientId } } }
+      : {};
     const responses = await Database.models.SurveyResponse.find({
       where: {
         survey: surveyId,
+        ...patientFilter,
       },
       relations: ['encounter', 'survey', 'encounter.patient'],
     });
