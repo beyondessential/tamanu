712946275bcf4ac4552889e1d21ff308610aae8e
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-01-04 21:42:38 -0400

    Query for existing death data and pass information to form

--- packages/desktop/app/components/DeathModal.js
+++ packages/desktop/app/components/DeathModal.js
@@ -4,7 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { reloadPatient } from '../store/patient';
 import { Modal } from './Modal';
 import { DeathForm } from '../forms/DeathForm';
-import { useApi, useSuggester } from '../api';
+import { useApi, useSuggester, isErrorUnknownAllow404s } from '../api';
 import { usePatientNavigation } from '../utils/usePatientNavigation';
 
 export const DeathModal = React.memo(({ open, onClose }) => {
@@ -15,8 +15,8 @@ export const DeathModal = React.memo(({ open, onClose }) => {
   const icd10Suggester = useSuggester('icd10');
   const practitionerSuggester = useSuggester('practitioner');
   const facilitySuggester = useSuggester('facility');
-  const { data: queryData } = useQuery(['openPatientEncounters', patient.id], () =>
-    api.get(`patient/${patient.id}/encounters?open=true`),
+  const { data: deathData } = useQuery(['patientDeathSummary', patient.id], () =>
+    api.get(`patient/${patient.id}/death`, {}, { isErrorUnknown: isErrorUnknownAllow404s }),
   );
 
   const recordPatientDeath = async data => {
@@ -34,7 +34,7 @@ export const DeathModal = React.memo(({ open, onClose }) => {
         onSubmit={recordPatientDeath}
         onCancel={onClose}
         patient={patient}
-        hasCurrentEncounter={queryData?.count > 0}
+        deathData={deathData}
         icd10Suggester={icd10Suggester}
         practitionerSuggester={practitionerSuggester}
         facilitySuggester={facilitySuggester}
