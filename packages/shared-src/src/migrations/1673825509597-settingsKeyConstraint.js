ed929757d239120ffdae54b462b25088d120fb1b
Félix Saparelli <felix@passcod.name>
2023-01-16 23:23:22 +1300

    Soft-delete bad old duplicated settings

--- packages/shared-src/src/migrations/1673825509597-settingsKeyConstraint.js
+++ packages/shared-src/src/migrations/1673825509597-settingsKeyConstraint.js
@@ -1,6 +1,22 @@
 import { Op } from 'sequelize';
 
 export async function up(query) {
+  // Delete all but the most recent setting for each (key, facility_id).
+  // This is to get rid of duplicated data from the prior settings update code.
+  await query.sequelize.query(`
+    UPDATE settings
+    SET deleted_at = current_timestamp(3)
+    WHERE id NOT IN (
+      SELECT (array_agg(id))[1]
+      FROM (
+        SELECT * FROM settings
+        WHERE deleted_at IS NULL
+        ORDER BY key ASC, facility_id ASC, updated_at DESC
+      ) t
+      GROUP BY key, facility_id
+    );
+  `);
+
   // Constraint is overly broad (includes deleted_at), but we can't add a partial unique constraint,
   // so we enforce it with the second unique index (the first being implied by the constraint).
   await query.addConstraint('settings', {
