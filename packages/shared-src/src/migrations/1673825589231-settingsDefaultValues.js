a05d56b6cd9dc5eea3fb42847a5abaf31acf80cd
Félix Saparelli <felix@passcod.name>
2023-01-16 18:53:22 +1300

    sployy

--- packages/shared-src/src/migrations/1673825589231-settingsDefaultValues.js
+++ packages/shared-src/src/migrations/1673825589231-settingsDefaultValues.js
@@ -1,21 +1,21 @@
-import { DataTypes } from 'sequelize';
+import { DataTypes, Sequelize } from 'sequelize';
 
 export async function up(query) {
   await query.changeColumn('settings', 'id', {
     type: DataTypes.UUID,
     allowNull: false,
     primaryKey: true,
-    defaultValue: query.fn('uuid_generate_v4'),
+    defaultValue: Sequelize.fn('uuid_generate_v4'),
   });
   
   await query.changeColumn('settings', 'created_at', {
     type: DataTypes.DATE,
-    defaultValue: query.fn('current_timestamp', 3),
+    defaultValue: Sequelize.fn('current_timestamp', 3),
     allowNull: false,
   });
   await query.changeColumn('settings', 'updated_at', {
     type: DataTypes.DATE,
-    defaultValue: query.fn('current_timestamp', 3),
+    defaultValue: Sequelize.fn('current_timestamp', 3),
     allowNull: false,
   });
 }
