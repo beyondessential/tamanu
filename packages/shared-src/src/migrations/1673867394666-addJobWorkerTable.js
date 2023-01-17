2f9391d08f243d35dfa5a102d9317bd0e197cf3d
Félix Saparelli <felix@passcod.name>
2023-01-17 14:39:29 +1300

    Job worker functions

--- packages/shared-src/src/migrations/1673867394666-addJobWorkerTable.js
+++ packages/shared-src/src/migrations/1673867394666-addJobWorkerTable.js
@@ -24,8 +24,46 @@ export async function up(query) {
       type: DataTypes.JSONB,
       allowNull: false,
       defaultValue: {},
-    }
+    },
   });
+
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION job_worker_register(
+      IN worker_info JSONB,
+      OUT worker_id UUID
+    )
+      RETURNS NULL ON NULL INPUT
+      LANGUAGE SQL
+      VOLATILE PARALLEL UNSAFE
+    AS $$
+      INSERT INTO job_workers (metadata) VALUES (worker_info)
+      RETURNING id
+    $$
+  `);
+
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION job_worker_heartbeat(
+      IN worker_id UUID
+    )
+      RETURNS NULL ON NULL INPUT
+      LANGUAGE SQL
+      VOLATILE PARALLEL UNSAFE
+    AS $$
+      UPDATE job_workers SET updated_at = current_timestamp(3) WHERE id = worker_id
+    $$
+  `);
+
+  await query.sequelize.query(`
+    CREATE OR REPLACE FUNCTION job_worker_deregister(
+      IN worker_id UUID
+    )
+      RETURNS NULL ON NULL INPUT
+      LANGUAGE SQL
+      VOLATILE PARALLEL UNSAFE
+    AS $$
+      DELETE FROM job_workers WHERE id = worker_id
+    $$
+  `);
 
   await query.sequelize.query(`
     CREATE OR REPLACE FUNCTION job_worker_is_alive(
@@ -45,5 +83,8 @@ export async function up(query) {
 
 export async function down(query) {
   await query.sequelize.query(`DROP FUNCTION IF EXISTS job_worker_is_alive`);
+  await query.sequelize.query(`DROP FUNCTION IF EXISTS job_worker_deregister`);
+  await query.sequelize.query(`DROP FUNCTION IF EXISTS job_worker_heartbeat`);
+  await query.sequelize.query(`DROP FUNCTION IF EXISTS job_worker_register`);
   await query.dropTable(TABLE_NAME);
 }
