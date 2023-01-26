2af43158a95d219ac3d3b10040815ad6274c9977
Daniel Nash <dannash200@gmail.com>
2023-01-26 17:17:17 +1300

    no-issue: try fix github action error for verify storybook

--- packages/desktop/.storybook/webpack.config.js
+++ packages/desktop/.storybook/webpack.config.js
@@ -5,7 +5,7 @@
  */
 
 const path = require('path');
-const webpack = require('webpack')
+const webpack = require('webpack');
 
 /**
  * The doc doesn't really mention using webpack.config.js, but .storybook/main.js instead.
@@ -21,19 +21,23 @@ module.exports = async ({ config }) => {
   /**
    * Pretty odd workaround but prevented changing more core configs
    *  @see https://github.com/vercel/next.js/issues/28774#issuecomment-1264555395 for similar issue
-  */
-
+   */
   config.plugins.push(
-    new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
-      resource.request = resource.request.replace(/^node:/, '')
-    })
-  )
+    new webpack.NormalModuleReplacementPlugin(/^node:/, resource => {
+      resource.request = resource.request.replace(/^node:/, '');
+    }),
+  );
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development'),
+    }),
+  );
   config.resolve.fallback = {
     ...config.resolve.fallback,
-      fs: false,
-      stream: false,
-      zlib: false
-  }
+    fs: false,
+    stream: false,
+    zlib: false,
+  };
   config.resolve.alias = {
     ...config.resolve.alias,
     electron: require.resolve('./__mocks__/electron.js'),
