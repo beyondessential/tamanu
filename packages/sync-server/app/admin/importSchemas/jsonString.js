d3ece3a865b2c5cdc8dd48a858afc6ac60b90a6d
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-01-31 10:58:37 +1300

    WAITM-617 PR changes

--- packages/sync-server/app/admin/importSchemas/jsonString.js
+++ packages/sync-server/app/admin/importSchemas/jsonString.js
@@ -17,25 +17,14 @@ export const jsonString = () =>
 // This means default yup errors won't give the field name by default, so we pass it in instead
 export const jsonStringShape = (name, objectShape) =>
   yup.string().test('json-shape', value => {
-    let parseValue = value;
-    // We usually accept empty strings for configs, but there might be required fields
-    // So attempt the validation with an empty object just in case
-    if (!value) parseValue = '{}';
     let parsedObject = null;
     try {
-      parsedObject = JSON.parse(parseValue);
+      parsedObject = JSON.parse(value || '{}');
       // Will throw a validation error if shape doesn't match
       return objectShape.validateSync(parsedObject, { strict: true });
     } catch (e) {
-      let errors = [];
-      // ValidationError has multiple errors within
-      if (e.errors) {
-        errors = e.errors.map(err => `${name}: ${err}`);
-      } else {
-        // We land here if JSON.parse fails
-        errors = [`${name}: ${e.message}`];
-      }
-      return new yup.ValidationError(errors);
+      const errors = e.errors || [e.message];
+      return new yup.ValidationError(errors.map(err => `${name}: ${err}`));
     }
   });
 
