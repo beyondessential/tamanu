5a7f0dd13afe0137b031b2e71272b725d994cc07
Félix Saparelli <felix@passcod.name>
2023-02-02 10:50:02 +1300

    Create "bindings" for job and job worker

---
+++ packages/shared-src/src/models/Job.js
@@ -0,0 +1,115 @@
+import Sequelize, { DataTypes, QueryTypes } from 'sequelize';
+import { Model } from './Model';
+import { SYNC_DIRECTIONS } from '../constants';
+
+export class Job extends Model {
+  static init({ primaryKey, ...options }) {
+    super.init(
+      {
+        id: {
+          ...primaryKey,
+          type: DataTypes.UUID,
+          defaultValue: Sequelize.fn('uuid_generate_v4'),
+        },
+
+        // queue
+        priority: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 1000,
+        },
+        status: {
+          type: DataTypes.TEXT,
+          defaultValue: 'Queued',
+          allowNull: false,
+        },
+        worker_id: DataTypes.UUID,
+        started_at: DataTypes.DATE,
+        completed_at: DataTypes.DATE,
+        errored_at: DataTypes.DATE,
+        error: DataTypes.TEXT,
+
+        // routing
+        topic: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        discriminant: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          defaultValue: Sequelize.fn('uuid_generate_v4'),
+          unique: true,
+        },
+
+        // data
+        payload: {
+          type: DataTypes.JSONB,
+          allowNull: false,
+          defaultValue: {},
+        },
+      },
+      {
+        ...options,
+        syncDirection: SYNC_DIRECTIONS.DO_NOT_SYNC,
+        indexes: [
+          {
+            fields: ['topic', 'status', 'priority', 'created_at'],
+          },
+        ],
+      },
+    );
+  }
+
+  static async backlog(topic, includeDropped = true) {
+    const [{ count }] = await this.sequelize.query('SELECT job_backlog($topic, $includeDropped)', {
+      type: QueryTypes.SELECT,
+      bind: { topic, includeDropped },
+    });
+    return count;
+  }
+
+  static async grab(workerId, topic) {
+    const job = await this.sequelize.query('SELECT job_grab($workerId, $topic)', {
+      type: QueryTypes.SELECT,
+      bind: { workerId, topic },
+    });
+
+    if (!job || !job[0]) {
+      return null;
+    }
+
+    const { jobId } = job[0];
+    return Job.findByPk(jobId);
+  }
+
+  static async submit(topic, payload, { priority = 1000, discriminant = null } = {}) {
+    const [{ jobId }] = await this.sequelize.query(`
+      SELECT job_backlog(
+          $topic
+        , $payload
+        , $priority
+        ${discriminant ? ', $discriminant' : ''}
+      )
+    `, {
+      type: QueryTypes.SELECT,
+      bind: { topic, payload, priority, discriminant },
+    });
+    return jobId;
+  }
+
+  async complete(workerId) {
+    await this.sequelize.query('SELECT job_complete($jobId, $workerId)', {
+      type: QueryTypes.SELECT,
+      bind: { jobId: this.id, workerId },
+    });
+  }
+
+  async error(workerId, error) {
+    await this.sequelize.query('SELECT job_error($jobId, $workerId, $error)', {
+      type: QueryTypes.SELECT,
+      bind: { jobId: this.id, workerId, error },
+    });
+    await this.reload();
+    return this;
+  }
+}
---
+++ packages/shared-src/src/models/JobWorker.js
@@ -0,0 +1,57 @@
+import Sequelize, { DataTypes, QueryTypes } from 'sequelize';
+import { Model } from './Model';
+import { Job } from './Job';
+import { SYNC_DIRECTIONS } from '../constants';
+
+export class JobWorker extends Model {
+  static init({ primaryKey, ...options }) {
+    super.init(
+      {
+        id: {
+          ...primaryKey,
+          type: DataTypes.UUID,
+          defaultValue: Sequelize.fn('uuid_generate_v4'),
+        },
+        metadata: {
+          type: DataTypes.JSONB,
+          allowNull: false,
+          defaultValue: {},
+        },
+      },
+      {
+        ...options,
+        syncDirection: SYNC_DIRECTIONS.DO_NOT_SYNC,
+      },
+    );
+  }
+
+  static async register(metadata) {
+    const [{ workerId }] = await this.sequelize.query('SELECT job_worker_register($metadata)', {
+      type: QueryTypes.SELECT,
+      bind: { metadata },
+    });
+    return workerId;
+  }
+ 
+  static async clearDead() {
+    await this.sequelize.query('SELECT job_worker_garbage_collect()');
+  }
+  
+  async heartbeat() {
+    await this.sequelize.query('SELECT job_worker_heartbeat($workerId)', {
+      type: QueryTypes.SELECT,
+      bind: { workerId: this.id },
+    });
+  }
+  
+  async deregister() {
+    await this.sequelize.query('SELECT job_worker_deregister($workerId)', {
+      type: QueryTypes.SELECT,
+      bind: { workerId: this.id },
+    });
+  }
+
+  grabJob(topic) {
+    return Job.grab(this.id, topic);
+  }
+}
