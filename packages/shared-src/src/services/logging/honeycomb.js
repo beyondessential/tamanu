97c0fdecc7c654ec85211d01b2c544cbd9ec6e7a
Félix Saparelli <felix@passcod.name>
2023-02-03 05:03:24 +1300

    Change honeycomb integration to use spans

--- packages/shared-src/src/services/logging/honeycomb.js
+++ packages/shared-src/src/services/logging/honeycomb.js
@@ -1,34 +1,58 @@
-import Transport from 'winston-transport';
-import Libhoney from 'libhoney';
+import { HoneycombSDK } from '@honeycombio/opentelemetry-node';
+import { trace } from '@opentelemetry/api';
+import { Resource } from '@opentelemetry/resources';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import config from 'config';
 import shortid from 'shortid';
 import os from 'os';
 
-const serverInfo = {
-  deployment: config?.canonicalHostName || config?.sync?.host,
-  facilityId: config?.serverFacilityId,
-  nodeEnv: process.env.NODE_ENV,
-  processId: shortid.generate(),
-  hostname: os.hostname(),
-  ...global.serverInfo,
-};
+function setupHoneycomb() {
+  const { apiKey, sampleRate = 1, enabled } = config?.honeycomb || {};
 
-const { apiKey, dataset, enabled } = config?.honeycomb || {};
-
-const honeyApi = new Libhoney({
-  writeKey: apiKey,
-  dataset,
-  disabled: !(apiKey && enabled),
-});
-
-class HoneycombTransport extends Transport {
-  log(info, callback) {
-    const event = honeyApi.newEvent();
-    event.add(info);
-    event.add(serverInfo);
-    event.send();
-    callback();
+  if (!enabled || !apiKey) {
+    return;
   }
+
+  const { serverType, version } = global.serverInfo;
+  const deploymentHost = config?.canonicalHostName || config?.sync?.host;
+  const deployment = new URL(deploymentHost).hostname.replace(/[^a-z0-9]+/gi, '-');
+  const facilityId = config?.serverFacilityId?.replace(/([^a-z0-9]+|^(ref\/)?facility[-/])/gi, '');
+
+  let serviceName = `${deployment}-${serverType}`;
+  if (facilityId) serviceName += `-${facilityId}`;
+
+  const honey = new HoneycombSDK({
+    apiKey,
+    serviceName,
+    sampleRate,
+    instrumentations: [getNodeAutoInstrumentations()],
+    resource: new Resource({
+      'net.host.name': os.hostname(),
+      'process.id': shortid.generate(),
+      'service.deployment': deployment,
+      'service.environment': process.env.NODE_ENV,
+      'service.facility': facilityId,
+      'service.type': serverType,
+      'service.version': version,
+    }),
+  });
+
+  honey.start();
+  return honey;
 }
 
-export const honeycombTransport = new HoneycombTransport();
+export const honey = setupHoneycomb();
+export const getTracer = (name = 'tamanu') => trace.getTracer(name);
+export const spanWrapFn = async (name, fn, attributes = {}, tracer = 'tamanu') =>
+  getTracer(tracer).startActiveSpan(name, async span => {
+    span.setAttribute('code.function', name);
+    span.setAttributes(attributes);
+    try {
+      return await fn(span);
+    } catch (e) {
+      span.recordException(e);
+      throw e;
+    } finally {
+      span.end();
+    }
+  });
--- packages/shared-src/src/services/logging/index.js
+++ packages/shared-src/src/services/logging/index.js
@@ -1,2 +1,3 @@
 export * from './log';
 export * from './http';
+export * from './honeycomb';
