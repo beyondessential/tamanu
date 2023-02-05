2c8c5b7e2c57d2e05327dbb8813c92f4861ed2cb
Tom Caiger <caigertom@gmail.com>
2023-02-06 10:07:42 +1300

    fix formik validation

--- packages/desktop/app/components/Field/Form.js
+++ packages/desktop/app/components/Field/Form.js
@@ -58,14 +58,20 @@ export class Form extends React.PureComponent {
     }
 
     setSubmitting(true);
+    const values = getValues();
 
     // validation phase
-    const values = getValues();
-    const formErrors = await validateForm(values);
-    if (Object.entries(formErrors).length) {
-      this.setErrors(formErrors);
-      setSubmitting(false);
-      throw new ValidationError('Form was not filled out correctly');
+    const { validateOnChange, validateOnBlur } = this.props;
+
+    // Only validate here if we are not validating on blur or change elsewhere. Formik doesn't
+    // handle double validating. @see https://github.com/jaredpalmer/formik/issues/1209
+    if (!validateOnChange && !validateOnBlur) {
+      const formErrors = await validateForm(values);
+      if (Object.entries(formErrors).length) {
+        this.setErrors(formErrors);
+        setSubmitting(false);
+        throw new ValidationError('Form was not filled out correctly');
+      }
     }
 
     // submission phase
@@ -75,6 +81,7 @@ export class Form extends React.PureComponent {
         ...rest,
         setErrors: this.setErrors,
       });
+      setSubmitting(false);
       if (onSuccess) {
         onSuccess(result);
       }
