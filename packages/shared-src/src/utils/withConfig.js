2ba756d41d3eb6a09f422fc36870a6b48b6b5764
Félix Saparelli <felix@beyondessential.com.au>
2023-02-07 09:51:25 +1300

    Remove @injectConfig decorator and decorators from babel (#3478)
    * Remove @injectConfig decorator
    
    * Remove decorators from babel config
    
    * Config is static

--- packages/shared-src/src/utils/withConfig.js
+++ packages/shared-src/src/utils/withConfig.js
@@ -9,22 +9,3 @@ export function withConfig(fn) {
   inner.overrideConfig = fn;
   return inner;
 }
-
-/** Injects a .config static property into the class, use as a decorator */
-export function injectConfig(value, { kind }) {
-  if (kind !== 'class') {
-    throw new Error('injectConfig can only be used on classes');
-  }
-
-  return class extends value {
-    static config = config;
-
-    static overrideConfig(override) {
-      this.config = override;
-    }
-
-    static restoreConfig() {
-      this.config = config;
-    }
-  };
-}
