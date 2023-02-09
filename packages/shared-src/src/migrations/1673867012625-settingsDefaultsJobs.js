2b488b21df3d97e410139686f40605c239703e8b
Félix Saparelli <felix@passcod.name>
2023-02-09 15:59:55 +1300

    Fix default settings

--- packages/shared-src/src/migrations/1673867012625-settingsDefaultsJobs.js
+++ packages/shared-src/src/migrations/1673867012625-settingsDefaultsJobs.js
@@ -1,8 +1,10 @@
 import config from 'config';
+import { QueryTypes } from 'sequelize';
 
 const DEFAULT_SETTINGS = {
-  'jobs.worker.hearbeat': '1 minute',
+  'jobs.worker.heartbeat': '1 minute',
   'jobs.worker.assumeDroppedAfter': '10 minutes',
+  'jobs.topics.default.enabled': true,
   'jobs.topics.default.schedule': '* * * * *', // once a minute
   'jobs.topics.default.maxConcurrency': 10,
 };
@@ -12,13 +14,21 @@ export async function up(query) {
   if (config?.serverFacilityId) return;
 
   for (const [key, value] of Object.entries(DEFAULT_SETTINGS)) {
-    await query.sequelize.query(`
-      INSERT INTO settings (key, value)
-      VALUES ('${key}', '${JSON.stringify(value)}')
-      ON CONFLICT (key, facility_id, deleted_at)
-        WHERE facility_id IS NULL AND deleted_at IS NULL
-          DO NOTHING
-    `);
+    const [{ count }] = await query.sequelize.query(
+      'SELECT count(*) FROM settings WHERE key = $key AND facility_id IS NULL AND deleted_at IS NULL',
+      {
+        bind: { key },
+        type: QueryTypes.SELECT,
+      },
+    );
+    if (count > 0) continue;
+
+    await query.sequelize.query('INSERT INTO settings (key, value) VALUES ($key, $value)', {
+      bind: {
+        key,
+        value: JSON.stringify(value),
+      },
+    });
   }
 }
 
