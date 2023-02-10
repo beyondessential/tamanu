e12bac7189814f38cc736aa66760fb40494a4152
georgekamar <gkamar97@gmail.com>
2023-02-10 08:32:04 +0200

    restore finalise dropdown button before discontinue

--- packages/desktop/app/forms/MedicationForm.js
+++ packages/desktop/app/forms/MedicationForm.js
@@ -4,7 +4,7 @@ import * as yup from 'yup';
 import { Box } from '@material-ui/core';
 import { getCurrentDateTimeString } from 'shared/utils/dateTime';
 import { foreignKey } from '../utils/validation';
-// import { DropdownButton } from '../components/DropdownButton';
+import { DropdownButton } from '../components/DropdownButton';
 import { PrescriptionPrintModal } from '../components/PatientPrinting/PrescriptionPrintModal';
 import {
   FormGrid,
@@ -267,33 +267,36 @@ export const MedicationForm = React.memo(
                   <Button variant="outlined" color="primary" onClick={onCancel}>
                     Cancel
                   </Button>
-                  <Button
-                    color="primary"
-                    onClick={data => {
-                      setAwaitingPrint(false);
-                      submitForm(data);
-                    }}
-                  >
-                    Finalise
-                  </Button>
-                  {/* <DropdownButton
-                    actions={[
-                      {
-                        label: 'Finalise',
-                        onClick: data => {
-                          setAwaitingPrint(false);
-                          submitForm(data);
+                  {shouldDiscontinue ? (
+                    <Button
+                      color="primary"
+                      onClick={data => {
+                        setAwaitingPrint(false);
+                        submitForm(data);
+                      }}
+                    >
+                      Finalise
+                    </Button>
+                  ) : (
+                    <DropdownButton
+                      actions={[
+                        {
+                          label: 'Finalise',
+                          onClick: data => {
+                            setAwaitingPrint(false);
+                            submitForm(data);
+                          },
                         },
-                      },
-                      {
-                        label: 'Finalise & print',
-                        onClick: data => {
-                          setAwaitingPrint(true);
-                          submitForm(data, true);
+                        {
+                          label: 'Finalise & print',
+                          onClick: data => {
+                            setAwaitingPrint(true);
+                            submitForm(data, true);
+                          },
                         },
-                      },
-                    ]}
-                  /> */}
+                      ]}
+                    />
+                  )}
                 </ButtonRow>
               )}
             </FormGrid>
