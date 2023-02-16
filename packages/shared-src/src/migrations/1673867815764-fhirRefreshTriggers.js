f771e0f15af32d9053d37bb0719b527175cf9c2e
Félix Saparelli <felix@passcod.name>
2023-02-17 10:22:14 +1300

    Pg12 can't OR REPLACE triggers

--- packages/shared-src/src/migrations/1673867815764-fhirRefreshTriggers.js
+++ packages/shared-src/src/migrations/1673867815764-fhirRefreshTriggers.js
@@ -44,7 +44,7 @@ export async function up(query) {
 
   for (const table of UPSTREAMS) {
     await query.sequelize.query(`
-      CREATE OR REPLACE TRIGGER fhir_refresh
+      CREATE TRIGGER fhir_refresh
       AFTER INSERT OR UPDATE OR DELETE ON ${table} FOR EACH ROW
       EXECUTE FUNCTION fhir_refresh_trigger()
     `);
