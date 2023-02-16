066436b7673b44739a80d47255f293cf9f9fd10d
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-02-17 10:07:34 +1300

    WAITM-629 Add migrations for lab test panel tables

---
+++ packages/shared-src/src/migrations/1676619492367-AddLabPanelsTable.js
@@ -0,0 +1,34 @@
+import { DataTypes, Sequelize } from 'sequelize';
+
+export async function up(query) {
+  await query.createTable('lab_test_panels', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('uuid_generate_v4'),
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.fn('current_timestamp', 3),
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: Sequelize.fn('current_timestamp', 3),
+      allowNull: false,
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+}
+
+export async function down(query) {
+  await query.dropTable('lab_test_panels');
+}
