4c5bba13bd80778030eb8b35ede9fd4539da7994
Tom McLean <tom.a.mclean@gmail.com>
2023-02-20 13:55:56 +1100

    Add subcommand

---
+++ packages/lan/app/subCommands/sync.js
@@ -0,0 +1,18 @@
+import { Command } from 'commander';
+
+import { initDatabase } from '../database';
+import { FacilitySyncManager, CentralServerConnection } from '../sync';
+
+async function sync({ }) {
+  const context = await initDatabase();
+
+  context.centralServer = new CentralServerConnection(context);
+  context.centralServer.connect(); // preemptively connect central server to speed up sync
+  context.syncManager = new FacilitySyncManager(context);
+
+  await context.syncManager.triggerSync('subcommand');
+}
+
+export const syncCommand = new Command('sync')
+  .description('Sync to central server')
+  .action(sync);
