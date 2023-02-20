31ac5002e076bf82ae1a8809d55d3dc310637d88
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-02-19 20:08:22 -0400

    Co-authored by: Daniel Nash. Remove styled div with null CSS, replace for plain divs

--- packages/desktop/app/components/PatientPrinting/printouts/reusable/DateFacilitySection.js
+++ packages/desktop/app/components/PatientPrinting/printouts/reusable/DateFacilitySection.js
@@ -15,29 +15,25 @@ const RowContainer = styled.div`
   justify-content: space-around;
 `;
 
-const StyledDiv = styled.div`
-  ${props => (props.$marginLeft ? `margin-left: ${props.$marginLeft}px;` : '')}
-`;
-
 export const DateFacilitySection = ({ encounter }) => {
   const { facility } = useAuth();
   return (
     <RowContainer>
-      <StyledDiv>
+      <div>
         <CertificateLabel name="Print date" size="14px">
           <DateDisplay date={getCurrentDateString()} />
         </CertificateLabel>
-      </StyledDiv>
-      <StyledDiv>
+      </div>
+      <div>
         <LocalisedLabel name="facility" size="14px">
           {facility.name}
         </LocalisedLabel>
-      </StyledDiv>
-      <StyledDiv>
+      </div>
+      <div>
         <LocalisedLabel name="locationId" size="14px">
           {getFullLocationName(encounter?.location)}
         </LocalisedLabel>
-      </StyledDiv>
+      </div>
     </RowContainer>
   );
 };
