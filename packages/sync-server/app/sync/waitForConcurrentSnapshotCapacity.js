f8a24b32c02a5f8fecd69a34f8cc205339ad5e64
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2023-02-22 15:57:43 +1300

    SAV-127: Limit the number of concurrent snapshots (#3566)
    * SAV-127: Limit the number of concurrent snapshots
    
    * Clarifications
    
    * sav-127: fixed some bugs
    
    * sav-127: added completedAt filter
    
    * SAV-128: Snapshot pull in chunks (#3568)
    
    * sav-128: snapshot pull in chunks
    
    * sav-128: addressed review suggestions
    
    * sav-128: fixed test
    
    ---------
    
    Co-authored-by: Edwin <edmofro@gmail.com>
    Co-authored-by: Chris Tran <chris@beyondessential.com.au>
    Co-authored-by: chris-bes <63621318+chris-bes@users.noreply.github.com>

---
+++ packages/sync-server/app/sync/waitForConcurrentSnapshotCapacity.js
@@ -0,0 +1,23 @@
+import { Op } from 'sequelize';
+import config from 'config';
+import { sleepAsync } from 'shared/utils/sleepAsync';
+
+const checkForConcurrentSnapshotCapacity = async models => {
+  // work out how many sessions are currently in the snapshot phase
+  const count = await models.SyncSession.count({
+    where: {
+      pullSince: { [Op.not]: null },
+      snapshotCompletedAt: { [Op.is]: null },
+      error: { [Op.is]: null },
+      completedAt: { [Op.is]: null },
+    },
+  });
+  return count < config.sync.numberConcurrentPullSnapshots;
+};
+
+export const waitForConcurrentSnapshotCapacity = async models => {
+  // wait for there to be enough capacity to start a snapshot
+  while (!(await checkForConcurrentSnapshotCapacity(models))) {
+    await sleepAsync(500); // wait for half a second
+  }
+};
