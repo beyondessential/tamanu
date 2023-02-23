f3400cb52ac353210a7c30aabfd15ac8e6d07629
Tom Caiger <caigertom@gmail.com>
2023-02-23 14:24:12 +1300

    Update LabRequestNoteForm.js

--- packages/desktop/app/forms/LabRequestNoteForm.js
+++ packages/desktop/app/forms/LabRequestNoteForm.js
@@ -1,43 +1,71 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import * as yup from 'yup';
+import NotesIcon from '@material-ui/icons/Notes';
+import { Button, Box } from '@material-ui/core';
 import { NOTE_TYPES } from 'shared/constants';
 import { useApi } from '../api';
-import { Form, Field, TextField, AddButton, TextInput } from '../components';
+import { Form, Field, TextField, DateDisplay } from '../components';
 
 const Container = styled.div`
-  grid-column: 1 / -1;
+  display: flex;
+  align-items: flex-start;
+  background: white;
+  border-radius: 3px;
+  margin-bottom: 15px;
+  padding: 12px;
 `;
 
-const FlexRow = styled.div`
-  display: flex;
+const List = styled.ul`
+  margin: 0;
+  padding: 4px 0 0 15px;
+  min-height: 25px;
+`;
+
+const ListItem = styled.li`
+  margin: 0 0 5px 0;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+`;
+
+const Caption = styled.span`
+  color: ${props => props.theme.palette.text.tertiary};
+  margin-left: 6px;
 `;
 
 const NotesInput = styled(Field)`
   flex: 1;
-  margin-right: 12px;
+
+  .MuiInputBase-input {
+    font-size: 12px;
+    line-height: 15px;
+    padding: 8px;
+  }
 `;
 
-const AddNoteButton = styled(AddButton)`
-  height: 43px;
-  align-self: flex-end;
-`;
+const TextButton = styled(Button)`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 15px;
+  text-transform: none;
+  padding-left: 8px;
+  padding-right: 8px;
+  min-width: auto;
+  background: none;
+  color: ${props =>
+    props.$underline ? props.theme.palette.primary.main : props.theme.palette.text.tertiary};
+  text-decoration: ${props => (props.$underline ? 'underline' : 'none')};
 
-const ReadOnlyNotesField = ({ notes }) => {
-  const notesText = notes.map(note => note.content).join('. ');
-  return (
-    <TextInput
-      multiline
-      value={notesText}
-      label="Notes"
-      style={{ gridColumn: '1 / -1', minHeight: '60px' }}
-      disabled
-    />
-  );
-};
+  &.MuiButton-root:hover {
+    background: none;
+    text-decoration: underline;
+    color: ${props => props.theme.palette.primary.main};
+  }
+`;
 
 export const LabRequestNoteForm = React.memo(({ labRequest, isReadOnly }) => {
   const api = useApi();
+  const [active, setActive] = useState(false);
   const [notes, setNotes] = useState([]);
 
   useEffect(() => {
@@ -54,33 +82,46 @@ export const LabRequestNoteForm = React.memo(({ labRequest, isReadOnly }) => {
       noteType: NOTE_TYPES.OTHER,
     });
     setNotes([...notes, newNote]);
+    setActive(false);
     resetForm();
   };
 
-  if (isReadOnly) {
-    return <ReadOnlyNotesField notes={notes} />;
-  }
-
   return (
     <Container>
-      <Form
-        onSubmit={saveNote}
-        render={({ submitForm }) => (
-          <FlexRow>
-            <NotesInput label="Note" name="content" component={TextField} />
-            <AddNoteButton onClick={submitForm} />
-          </FlexRow>
+      <NotesIcon color="primary" />
+      <Box flex="1" ml={1}>
+        <List>
+          {notes.map(note => (
+            <ListItem key={`${note.id}`}>
+              {note.content}
+              <Caption>
+                {note.author?.displayName} <DateDisplay date={note.date} />
+              </Caption>
+            </ListItem>
+          ))}
+        </List>
+        {!isReadOnly && (
+          <Form
+            onSubmit={saveNote}
+            render={({ submitForm, values }) => {
+              const formSubmitIsDisabled = !values.content || values.content.length < 2;
+              return active ? (
+                <Box display="flex" alignItems="center">
+                  <NotesInput label="" name="content" component={TextField} />
+                  <TextButton onClick={submitForm} disabled={formSubmitIsDisabled}>
+                    Save
+                  </TextButton>
+                  <TextButton onClick={() => setActive(false)}>Cancel</TextButton>
+                </Box>
+              ) : (
+                <TextButton $underline onClick={() => setActive(true)}>
+                  Add note
+                </TextButton>
+              );
+            }}
+          />
         )}
-        initialValues={{}}
-        validationSchema={yup.object().shape({
-          content: yup.string().required(),
-        })}
-      />
-      <ul>
-        {notes.map(note => (
-          <li key={`${note.id}`}>{note.content}</li>
-        ))}
-      </ul>
+      </Box>
     </Container>
   );
 });
