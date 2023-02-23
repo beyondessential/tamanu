ef1a729bbd02298b7ad0847a3dfbd1f3b2878042
Tom Caiger <caigertom@gmail.com>
2023-02-24 09:53:43 +1300

    remove unused decision support modal

--- packages/desktop/app/components/DiagnosisModal.js
+++ packages/desktop/app/components/DiagnosisModal.js
@@ -1,39 +1,28 @@
-import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-
+import React from 'react';
 import { useEncounter } from '../contexts/Encounter';
-import { showDecisionSupport } from '../store/specialModals';
-
 import { Modal } from './Modal';
 import { DiagnosisForm } from '../forms/DiagnosisForm';
 import { useApi } from '../api';
 
-export const DiagnosisModal = ({ diagnosis, onClose, encounterId, ...rest }) => {
+export const DiagnosisModal = React.memo(({ diagnosis, onClose, encounterId, ...props }) => {
   const api = useApi();
-  const dispatch = useDispatch();
   const { loadEncounter } = useEncounter();
-  const onSaveDiagnosis = useCallback(
-    async data => {
-      if (data.id) {
-        await api.put(`diagnosis/${data.id}`, data);
-      } else {
-        const result = await api.post(`diagnosis`, {
-          ...data,
-          encounterId,
-        });
-        if (result.previousDiagnoses?.length > 0) {
-          dispatch(showDecisionSupport('repeatDiagnosis', result));
-        }
-      }
-      await loadEncounter(encounterId);
-      onClose();
-    },
-    [api, dispatch, loadEncounter, onClose, encounterId],
-  );
+  const onSaveDiagnosis = async data => {
+    if (data.id) {
+      await api.put(`diagnosis/${data.id}`, data);
+    } else {
+      await api.post(`diagnosis`, {
+        ...data,
+        encounterId,
+      });
+    }
+    await loadEncounter(encounterId);
+    onClose();
+  };
 
   return (
     <Modal title="Diagnosis" open={!!diagnosis} onClose={onClose}>
-      <DiagnosisForm onCancel={onClose} diagnosis={diagnosis} onSave={onSaveDiagnosis} {...rest} />
+      <DiagnosisForm onCancel={onClose} diagnosis={diagnosis} onSave={onSaveDiagnosis} {...props} />
     </Modal>
   );
-};
+});
