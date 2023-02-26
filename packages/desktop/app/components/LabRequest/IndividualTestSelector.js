1472c7bcce25b1706df20621656f29b25bff0dcb
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2023-02-27 10:01:33 +1300

    Update packages/desktop/app/components/LabRequest/IndividualTestSelector.js
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

--- packages/desktop/app/components/LabRequest/IndividualTestSelector.js
+++ packages/desktop/app/components/LabRequest/IndividualTestSelector.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Box } from '@material-ui/core';
-
 import { subStrSearch } from '../../utils/subStringSearch';
 import { FormSeparatorLine } from '../FormSeparatorLine';
 import { Field, Form, SearchField } from '../Field';
