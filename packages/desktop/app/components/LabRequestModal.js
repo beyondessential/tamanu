449e114119ae2bfeb8795f108454ab0974f8771a
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2023-02-27 14:52:23 +1300

    WAITM-637: Panel and individual test selector workflow & multistep lab form (#3600)
    * waitm-638: individual lab test selecter story
    
    * waitm-638: reorder import cleanup
    
    * waitm-638: simplify search
    
    * waitm-637: renames for panel state of test selector
    
    * waitm-637: restructure story
    
    * waitm-637: conditionally display panel selector field
    
    * waitm-637: refactor to make form own component
    
    * waitm-637: add panel fetch
    
    * waitm-637: panel state
    
    * waitm-637: useQuery customize with config for panel test type query
    
    * waitm-637: behavior to requirements for panel form
    
    * waitm-637: fix prop
    
    * waitm-637: few cleanups
    
    * waitm-637: make disabled panel select more obvious
    
    * waitm-637: extend story
    
    * waitm-637: multi step form
    
    * waitm-637: lab request form multistep basics
    
    * waitm-637: make test selector use state to avoid nested formik form
    
    * waitm-637: remove mention of wizard XD
    
    * waitm-637: review change cleanups
    
    * waitm-637: lint
    
    * waitm-637: restructure story
    
    * waitm-637: restructure story rename
    
    * waitm-637: move to views
    
    * waitm-637: rename const and suggester initial options tweak
    
    * Update packages/desktop/app/views/labRequest/TestItem.js
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>
    
    * waitm-637: fix select all when selecting panel
    
    * waitm-637: style suggestion
    
    * waitm-637: rename suggestion
    
    * waitm-637: abstract into hook
    
    * waitm-637: remove duplicate
    
    * waitm-637: change return
    
    * waitm-637: multistep form refactor
    
    * waitm-637: multistep form refactor2
    
    * waitm-637: handle async populate of full lab test list better visually
    
    * waitm-637: fix improts
    
    * waitm-637: fix story for test selector
    
    * waitm-638: changes for real data integration
    
    * waitm-637: fix story for real usecase
    
    * waitm-637: fix warnings in console
    
    * waitm-637: account for removal of custom card
    
    * waitm-637: final cleanup
    
    * waitm-638: style tweak
    
    * waitm-638: lint
    
    ---------
    
    Co-authored-by: Tom Caiger <caigertom@gmail.com>

--- packages/desktop/app/components/LabRequestModal.js
+++ packages/desktop/app/components/LabRequestModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { customAlphabet } from 'nanoid';
 import { useApi, useSuggester } from '../api';
 import { Modal } from './Modal';
-import { LabRequestForm } from '../forms/LabRequestForm';
+import { LabRequestMultiStepForm } from '../forms/LabRequestForm/LabRequestMultiStepForm';
 import { ALPHABET_FOR_ID } from '../constants';
 
 export const LabRequestModal = ({ open, onClose, encounter }) => {
@@ -12,13 +12,13 @@ export const LabRequestModal = ({ open, onClose, encounter }) => {
 
   return (
     <Modal width="md" title="New lab request" open={open} onClose={onClose}>
-      <LabRequestForm
-        onSubmit={async data =>
-          api.post('labRequest', {
+      <LabRequestMultiStepForm
+        onSubmit={async data => {
+          await api.post(`labRequest`, {
             ...data,
             encounterId: encounter.id,
-          })
-        }
+          });
+        }}
         onCancel={onClose}
         encounter={encounter}
         practitionerSuggester={practitionerSuggester}
