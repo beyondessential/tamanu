e6a39e386c57c05d6ecc0431a3a35a38b4cf31d3
Alastair-L <lockealastair@gmail.com>
2023-02-27 13:23:38 +1100

    Clean up notePage model

--- packages/mobile/App/models/NotePage.ts
+++ packages/mobile/App/models/NotePage.ts
@@ -1,133 +1,76 @@
-import { Entity, Column, ManyToOne, RelationId } from 'typeorm/browser';
+import { Entity, Column } from 'typeorm/browser';
 import { OneToMany } from 'typeorm';
-import { BaseModel } from './BaseModel';
-import { INotePage } from '~/types';
-import { SYNC_DIRECTIONS } from './types';
-import { Encounter } from './Encounter';
-import { ReferenceData, ReferenceDataRelation } from './ReferenceData';
-import { LabTest } from './LabTest';
-import { User } from './User';
-import { ISO9075_SQLITE_DEFAULT } from './columnDefaults';
+
 import { DateTimeStringColumn } from './DateColumns';
+import { ISO9075_DATE_SQLITE_DEFAULT } from './columnDefaults';
+
+import { ID, INoteItem, INotePage, NoteRecordType, NoteType, DateString } from '~/types';
+import { SYNC_DIRECTIONS } from './types';
+
+import { BaseModel } from './BaseModel';
+import { NoteItem } from './NoteItem';
 
 @Entity('notePage')
 export class NotePage extends BaseModel implements INotePage {
   static syncDirection = SYNC_DIRECTIONS.PUSH_TO_CENTRAL;
 
-  // id: {
-  //   ...primaryKey,
-  //   type: DataTypes.UUID,
-  // },
-  @Column({
-    type: 'varchar',
-    nullable: false,
-  })
-  noteType?: LabRequestStatus;
+  @Column({ type: 'varchar', nullable: false })
+  noteType: NoteType;
 
-  recordType: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  date: dateTimeType('date', {
-    allowNull: false,
-    defaultValue: getCurrentDateTimeString,
-  }),
-},
+  @DateTimeStringColumn({ nullable: false, default: ISO9075_DATE_SQLITE_DEFAULT })
+  date: DateString;
 
-  @DateTimeStringColumn({ nullable: false, default: ISO9075_SQLITE_DEFAULT })
-  sampleTime: string;
-
-  @DateTimeStringColumn({ nullable: false, default: ISO9075_SQLITE_DEFAULT })
-  requestedDate: string;
-
-  @Column({ nullable: true, default: false })
-  urgent?: boolean;
-
-  @Column({ nullable: true, default: false })
-  specimenAttached?: boolean;
-
-  @Column({
-    type: 'varchar',
-    nullable: true,
-    default: LabRequestStatus.RECEPTION_PENDING,
-  })
-  status?: LabRequestStatus;
-
-  @Column({ type: 'varchar', nullable: true })
-  senaiteId?: string;
-
-  @Column({ type: 'varchar', nullable: true })
-  sampleId?: string;
+  // Can't link to record
+  // @ManyToOne(
+  //   () => Encounter,
+  //   encounter => encounter.labRequests,
+  // )
+  // encounter: Encounter;
+  // @RelationId(({ encounter }) => encounter)
+  // encounterId: string;
 
   @Column({ type: 'varchar', nullable: false })
-  displayId: string;
-
-  @Column({ type: 'varchar', nullable: true })
-  note?: string;
-
-  @ManyToOne(
-    () => Encounter,
-    encounter => encounter.labRequests,
-  )
-  encounter: Encounter;
-  @RelationId(({ encounter }) => encounter)
-  encounterId: string;
-
-  @ManyToOne(
-    () => User,
-    user => user.labRequests,
-  )
-  requestedBy: User;
-  @RelationId(({ requestedBy }) => requestedBy)
-  requestedById: string;
-
-  @ReferenceDataRelation()
-  labTestCategory: ReferenceData;
-  @RelationId(({ labTestCategory }) => labTestCategory)
-  labTestCategoryId: string;
-
-  @ReferenceDataRelation()
-  labTestPriority: ReferenceData;
-  @RelationId(({ labTestPriority }) => labTestPriority)
-  labTestPriorityId: string;
+  recordType: NoteRecordType;
+  @Column({ type: 'varchar', nullable: false })
+  recordId: ID;
 
   @OneToMany(
-    () => LabTest,
-    labTest => labTest.labRequest,
+    () => NoteItem,
+    noteItem => noteItem.notePage,
   )
-  tests: LabTest[];
+  noteItems: INoteItem[]
 
   static getTableNameForSync(): string {
-    return 'lab_requests'; // unusual camel case table here on mobile
+    return 'note_page'; // unusual camel case table here on mobile
   }
 
-  static async getForPatient(patientId: string): Promise<LabRequest[]> {
-    return this.getRepository()
-      .createQueryBuilder('labRequest')
-      .leftJoinAndSelect('labRequest.encounter', 'encounter')
-      .where('encounter.patient = :patientId', { patientId })
-      .leftJoinAndSelect('labRequest.labTestCategory', 'labTestCategory')
-      .getMany();
-  }
+  // static async getForPatient(patientId: string): Promise<LabRequest[]> {
+  //   return this.getRepository()
+  //     .createQueryBuilder('labRequest')
+  //     .leftJoinAndSelect('labRequest.encounter', 'encounter')
+  //     .where('encounter.patient = :patientId', { patientId })
+  //     .leftJoinAndSelect('labRequest.labTestCategory', 'labTestCategory')
+  //     .getMany();
+  // }
 
-  static async createWithTests(data: IDataRequiredToCreateLabRequest): Promise<BaseModel> {
-    const { labTestTypeIds = [] } = data;
-    if (!labTestTypeIds.length) {
-      throw new Error('A request must have at least one test');
-    }
+  // static async createWithTests(data: IDataRequiredToCreateLabRequest): Promise<BaseModel> {
+  //   const { labTestTypeIds = [] } = data;
+  //   if (!labTestTypeIds.length) {
+  //     throw new Error('A request must have at least one test');
+  //   }
 
-    const labRequest = await this.createAndSaveOne(data);
+  //   const labRequest = await this.createAndSaveOne(data);
 
-    // then create tests
-    await Promise.all(
-      labTestTypeIds.map(labTestTypeId =>
-        LabTest.createAndSaveOne({
-          labTestType: labTestTypeId,
-          labRequest: labRequest.id,
-        }),
-      ),
-    );
+  //   // then create tests
+  //   await Promise.all(
+  //     labTestTypeIds.map(labTestTypeId =>
+  //       LabTest.createAndSaveOne({
+  //         labTestType: labTestTypeId,
+  //         labRequest: labRequest.id,
+  //       }),
+  //     ),
+  //   );
 
-    return labRequest;
-  }
+  //   return labRequest;
+  // }
 }
