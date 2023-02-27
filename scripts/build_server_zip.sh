6752e7c9cdb420b3396010794086766c44b4a59e
Chris Tran <chris@beyondessential.com.au>
2023-02-28 09:40:22 +1100

    sav-125: attempt 9

--- scripts/build_server_zip.sh
+++ scripts/build_server_zip.sh
@@ -5,13 +5,28 @@ WORKSPACE="${1?must specify a workspace}"
 LINUX_RELEASE_FOLDER="release-nodejs"
 WINDOWS_RELEASE_FOLDER="release-windows"
 TARGET_PATH="${2-.}"
+RELEASE_NODE_JS_DIR="./packages/$WORKSPACE/$LINUX_RELEASE_FOLDER"
+FACILITY_DESKTOP_UPGRADE_DIR="$RELEASE_NODE_JS_DIR/dist/upgrade"
 
 # this script depends on the following steps having been run:
 # ./scripts/build_shared.sh
 # ./scripts/build_package_release.sh "$WORKSPACE"
 
+# If package-desktop then package the latest Tmanu desktop
+# along with the facility server
+if [[ $WORKSPACE == "lan" && $3 == "package-desktop" ]]; then
+    # if [ ! -d "$DESKTOP_RELEASE_DIR" ]; then
+    #     echo "Building desktop"
+    #     ../../scripts/build_desktop.sh build-only
+    # fi
+
+    echo "Packaging desktop with facility server"
+    mkdir -p "$DESKTOP_UPGRADE_DIR"
+    cp -r "$DESKTOP_RELEASE_DIR" "$FACILITY_DESKTOP_UPGRADE_DIR"
+fi
+
 # copy folder before modifying so we don't break the linux release
-cp -r "./packages/$WORKSPACE/$LINUX_RELEASE_FOLDER" "./packages/$WORKSPACE/$WINDOWS_RELEASE_FOLDER"
+cp -r "$RELEASE_NODE_JS_DIR" "./packages/$WORKSPACE/$WINDOWS_RELEASE_FOLDER"
 
 # get rid of node_modules from the copied linux release
 pushd "./packages/$WORKSPACE/$WINDOWS_RELEASE_FOLDER"
@@ -26,23 +41,9 @@ VERSION="${MAYBE_VERSION?could not calculate version}"
 DIR_NAME="release-v$VERSION"
 SUFFIX="$CI_BRANCH-v$VERSION-${CI_COMMIT_ID:0:10}"
 ZIP_NAME="tamanu-$WORKSPACE-$SUFFIX.zip"
-DESKTOP_UPGRADE_DIR="$DIR_NAME/dist/upgrade"
 mv "$WINDOWS_RELEASE_FOLDER" "$DIR_NAME"
 
 pwd 
-
-# If package-desktop then package the latest Tmanu desktop
-# along with the facility server
-if [[ $WORKSPACE == "lan" && $3 == "package-desktop" ]]; then
-    # if [ ! -d "$DESKTOP_RELEASE_DIR" ]; then
-    #     echo "Building desktop"
-    #     ../../scripts/build_desktop.sh build-only
-    # fi
-
-    echo "Packaging desktop with facility server"
-    mkdir -p "$DESKTOP_UPGRADE_DIR"
-    cp -r "$DESKTOP_RELEASE_DIR" "$DESKTOP_UPGRADE_DIR"
-fi
 
 zip -r "$ZIP_NAME" "$DIR_NAME"
 rm -rf "$DIR_NAME"
