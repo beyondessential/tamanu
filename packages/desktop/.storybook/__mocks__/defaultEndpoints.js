078d34f3683a24f9e8ef6d482aeb425479599ee7
Daniel Nash <dannash200@gmail.com>
2023-02-28 16:51:34 +1300

    waitm-656: add default endpoint to stories to start mocking suggestors generally

---
+++ packages/desktop/.storybook/__mocks__/defaultEndpoints.js
@@ -0,0 +1,48 @@
+export const defaultEndpoints = {
+  'suggestions/labTestLaboratory/all': () => [
+    {
+      id: '1',
+      name: 'ABC Medical Center',
+    },
+    {
+      id: '2',
+      name: 'XYZ Diagnostics',
+    },
+    {
+      id: '3',
+      name: 'City Hospital Lab',
+    },
+    {
+      id: '4',
+      name: 'Sunrise Diagnostic Center',
+    },
+  ],
+  'suggestions/labTestPriority/all': () => [
+    {
+      id: '1',
+      name: 'Chill',
+    },
+    {
+      id: '2',
+      name: 'Very Important',
+    },
+    {
+      id: '3',
+      name: 'Critical',
+    }
+  ],
+  'suggestions/labTestCategory/all': () => [
+    {
+      id: '1',
+      name: 'Blood Tests',
+    },
+    {
+      id: '2',
+      name: 'Urine Tests',
+    },
+    {
+      id: '3',
+      name: 'Radiology Tests',
+    },
+  ],
+};
--- packages/desktop/stories/searchbar.stories.js
+++ packages/desktop/stories/searchbar.stories.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { MockedApi } from './utils/mockedApi';
 import {
   AllPatientsSearchBar,
   PatientSearchBar,
@@ -13,41 +12,7 @@ import {
   CovidPatientsSearchBar,
 } from '../app/components';
 
-const mockEndpoints = {
-  'suggestions/labTestLaboratory/all': () => [
-    {
-      id: '1',
-      name: 'Laboratory 1',
-    },
-    {
-      id: '2',
-      name: 'Laboratory 2',
-    },
-  ],
-  'suggestions/labTestPriority/all': () => [
-    {
-      id: '1',
-      name: 'Chill',
-    },
-    {
-      id: '2',
-      name: 'Urgent',
-    },
-  ],
-  'suggestions/labTestCategory/all': () => [
-    {
-      id: '1',
-      name: 'Category 1',
-    },
-    {
-      id: '2',
-      name: 'Category 2',
-    },
-  ],
-};
-
 storiesOf('SearchBar', module)
-  .addDecorator(story => <MockedApi endpoints={mockEndpoints}>{story()}</MockedApi>)
   .add('AllPatientSearchBar', () => <AllPatientsSearchBar onSearch={action('search')} />)
   .add('AppointmentsSearchBar', () => <AppointmentsSearchBar onSearch={action('search')} />)
   .add('CovidPatientsSearchBar', () => <CovidPatientsSearchBar onSearch={action('search')} />)
