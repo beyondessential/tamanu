df53e91357214497192788d1dbb0e46815a5d502
Renato Koyama <renato.koyama@gmail.com>
2023-02-28 18:29:24 -0300

    TAN-2068: Change All patients search bar component to display advanced filters

--- packages/desktop/app/components/SearchBar/AllPatientsSearchBar.js
+++ packages/desktop/app/components/SearchBar/AllPatientsSearchBar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { getCurrentDateString } from 'shared/utils/dateTime';
+import Box from '@material-ui/core/Box';
 import { CustomisableSearchBar } from './CustomisableSearchBar';
 import {
   AutocompleteField,
@@ -8,32 +10,65 @@ import {
   DisplayIdField,
   DOBFields,
   SearchField,
+  SelectField,
 } from '../Field';
 import { useSuggester } from '../../api';
+import { DateField } from '../Field/DateField';
+
+import { useSexOptions } from '../../hooks';
 
 export const AllPatientsSearchBar = React.memo(({ onSearch, searchParameters }) => {
   const villageSuggester = useSuggester('village');
+  const sexOptions = useSexOptions(true);
+  const [showAdvancedFields, setShowAdvancedFields] = React.useState();
+
   return (
     <CustomisableSearchBar
-      title="Search for Patients"
       variant="small"
       renderCheckField={
-        <Field name="deceased" label="Include deceased patients" component={CheckField} />
+        showAdvancedFields && (
+          <Field name="deceased" label="Include deceased patients" component={CheckField} />
+        )
       }
+      showExpandButton
       onSearch={onSearch}
       initialValues={{ displayIdExact: true, ...searchParameters }}
+      onExpandChange={expanded => {
+        setShowAdvancedFields(expanded);
+      }}
     >
+      <DisplayIdField />
       <LocalisedField component={SearchField} name="firstName" />
       <LocalisedField component={SearchField} name="lastName" />
       <LocalisedField component={SearchField} name="culturalName" />
-      <LocalisedField
-        name="villageId"
-        component={AutocompleteField}
-        suggester={villageSuggester}
-        size="small"
+      <Field
+        name="dateOfBirthExact"
+        component={DateField}
+        saveDateAsString
+        label="DOB"
+        max={getCurrentDateString()}
       />
-      <DisplayIdField />
-      <DOBFields />
+      {showAdvancedFields && (
+        <>
+          <LocalisedField component={SearchField} name="culturalName" />
+          <Box style={{ gridColumn: 'span 2', display: 'flex', gap: 10 }}>
+            <DOBFields showExactBirth={false} />
+            <LocalisedField
+              style={{ minWidth: 100, flex: 1 }}
+              name="sex"
+              component={SelectField}
+              options={sexOptions}
+            />
+          </Box>
+          <LocalisedField
+            name="villageId"
+            component={AutocompleteField}
+            suggester={villageSuggester}
+            style={{ fontSize: '11px' }}
+            size="small"
+          />
+        </>
+      )}
     </CustomisableSearchBar>
   );
 });
