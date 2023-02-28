ac389d246f2d68fc3be7007c4bc29d273244b3ca
Chris Tran <chris@beyondessential.com.au>
2023-02-28 11:47:02 +1100

    sav-125: attempt 14

--- scripts/deploy_lan.sh
+++ scripts/deploy_lan.sh
@@ -2,6 +2,12 @@
 set -euxo pipefail
 cd "$(realpath $(dirname "$(realpath "$BASH_SOURCE")")/..)"
 
+echo "folder ./packages/lan/release-nodejs/dist"
+for eachfile in "./packages/lan/release-nodejs/dist"
+do
+   echo $eachfile
+done
+
 BUCKET_DIR="${1:?Must pass a bucket directory like dev, staging}"
 ENV_NAME="${2:?Must pass an environment like tamanu-central-server-dev}"
 scripts/deploy_eb.sh "./packages/lan/release-nodejs" "$LAN_SERVER_EB_APP" "$ENV_NAME" "$LAN_SERVER_EB_S3/$BUCKET_DIR"
