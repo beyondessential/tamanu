e18798a2280b1dfa695fdf0a7e912b54ac30b7c5
georgekamar <gkamar97@gmail.com>
2023-03-01 11:33:51 +0200

    add post new viewed patient within EncounterView as well

--- packages/desktop/app/views/patients/EncounterView.js
+++ packages/desktop/app/views/patients/EncounterView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Divider, Box } from '@material-ui/core';
@@ -10,6 +10,7 @@ import { EncounterTopBar, ContentPane } from '../../components';
 import { DiagnosisView } from '../../components/DiagnosisView';
 import { LoadingIndicator } from '../../components/LoadingIndicator';
 import { TabDisplay } from '../../components/TabDisplay';
+import { useApi } from '../../api';
 import {
   VitalsPane,
   NotesPane,
@@ -118,12 +119,17 @@ const StyledTabDisplay = styled(TabDisplay)`
 `;
 
 export const EncounterView = () => {
+  const api = useApi();
   const query = useUrlSearchParams();
   const { getLocalisation } = useLocalisation();
   const patient = useSelector(state => state.patient);
   const { encounter, isLoadingEncounter } = useEncounter();
   const [currentTab, setCurrentTab] = useState(query.get('tab') || ENCOUNTER_TAB_NAMES.VITALS);
   const disabled = encounter?.endDate || patient.death;
+
+  useEffect(() => {
+    api.post(`user/recently-viewed-patients/${patient.id}`);
+  }, [api, patient.id]);
 
   if (!encounter || isLoadingEncounter || patient.loading) return <LoadingIndicator />;
 
