b2c7644872982cf7be4ed0b0da5a66b2c3b7717d
Edwin <edmofro@gmail.com>
2023-03-06 11:30:22 +1300

    Use common consts for importable data types

--- packages/desktop/app/views/administration/PermissionsAdminView.js
+++ packages/desktop/app/views/administration/PermissionsAdminView.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import { PERMISSIONS_IMPORTABLE_DATA_TYPES } from 'shared/constants/importable';
 import { ImporterView } from './components/ImporterView';
 
 export const PermissionsAdminView = () => (
-  <ImporterView title="Permissions" endpoint="refData" dataTypes={['permissions']} />
+  <ImporterView
+    title="Permissions"
+    endpoint="refData"
+    dataTypes={PERMISSIONS_IMPORTABLE_DATA_TYPES}
+  />
 );
