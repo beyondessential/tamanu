4239947c3ab9122c0184e2d0a019e04d3049574b
Edwin <edmofro@gmail.com>
2023-03-06 11:57:28 +1300

    Disable export for Programs

--- packages/desktop/app/views/administration/components/ImportExportView.js
+++ packages/desktop/app/views/administration/components/ImportExportView.js
@@ -41,62 +41,64 @@ const TabContainer = styled.div`
   padding: 20px;
 `;
 
-export const ImportExportView = memo(({ title, endpoint, dataTypes, dataTypesSelectable }) => {
-  const [currentTab, setCurrentTab] = useState('import');
-  const [isLoading, setIsLoading] = useState(false);
+export const ImportExportView = memo(
+  ({ title, endpoint, dataTypes, dataTypesSelectable, disableExport }) => {
+    const [currentTab, setCurrentTab] = useState('import');
+    const [isLoading, setIsLoading] = useState(false);
 
-  const tabs = useMemo(
-    () => [
-      {
-        label: 'Import',
-        key: 'import',
-        icon: 'fa fa-file-import',
-        render: () => (
-          <TabContainer>
-            <ImporterView
-              endpoint={endpoint}
-              dataTypes={dataTypes}
-              dataTypesSelectable={dataTypesSelectable}
-              setIsLoading={setIsLoading}
-            />
-          </TabContainer>
-        ),
-      },
-      {
-        label: 'Export',
-        key: 'export',
-        icon: 'fa fa-file-export',
-        render: () => (
-          <TabContainer>
-            <ExporterView
-              endpoint={endpoint}
-              dataTypes={dataTypes}
-              dataTypesSelectable={dataTypesSelectable}
-              setIsLoading={setIsLoading}
-            />
-          </TabContainer>
-        ),
-      },
-    ],
-    [endpoint, dataTypes, dataTypesSelectable],
-  );
+    const tabs = useMemo(
+      () => [
+        {
+          label: 'Import',
+          key: 'import',
+          icon: 'fa fa-file-import',
+          render: () => (
+            <TabContainer>
+              <ImporterView
+                endpoint={endpoint}
+                dataTypes={dataTypes}
+                dataTypesSelectable={dataTypesSelectable}
+                setIsLoading={setIsLoading}
+              />
+            </TabContainer>
+          ),
+        },
+        !disableExport && {
+          label: 'Export',
+          key: 'export',
+          icon: 'fa fa-file-export',
+          render: () => (
+            <TabContainer>
+              <ExporterView
+                endpoint={endpoint}
+                dataTypes={dataTypes}
+                dataTypesSelectable={dataTypesSelectable}
+                setIsLoading={setIsLoading}
+              />
+            </TabContainer>
+          ),
+        },
+      ],
+      [endpoint, dataTypes, dataTypesSelectable],
+    );
 
-  return (
-    <OuterContainer>
-      {isLoading && (
-        <LoadingContainer>
-          <LoadingIndicator />
-        </LoadingContainer>
-      )}
-      <ContentContainer>
-        <Title>{title}</Title>
-        <StyledTabDisplay
-          tabs={tabs}
-          currentTab={currentTab}
-          onTabSelect={setCurrentTab}
-          scrollable={false}
-        />
-      </ContentContainer>
-    </OuterContainer>
-  );
-});
+    return (
+      <OuterContainer>
+        {isLoading && (
+          <LoadingContainer>
+            <LoadingIndicator />
+          </LoadingContainer>
+        )}
+        <ContentContainer>
+          <Title>{title}</Title>
+          <StyledTabDisplay
+            tabs={tabs}
+            currentTab={currentTab}
+            onTabSelect={setCurrentTab}
+            scrollable={false}
+          />
+        </ContentContainer>
+      </OuterContainer>
+    );
+  },
+);
