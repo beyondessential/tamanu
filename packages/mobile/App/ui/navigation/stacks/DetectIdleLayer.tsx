ab0657a1b4c2904b58f50b6bba7ac5bcd3e6612c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2023-03-07 14:39:13 +1300

    WAITM-605: Fix IdleDetectionLayer interaction with login status and add connection status updates to refresh endpoint (#3648)
    * waitm-605: fix idle signedIn state frozen as false
    
    * waitm-605: fix resetting of idle timeout
    
    * waitm-605: no need to pass loggedIn anymo
    
    * waitm-605: no need to initalize listener variables
    
    * waitm-605: rename test file
    
    * waitm-605: check emitter status change in refresh test
    
    * waitm-605: add emitter checks to refresh token flow test
    
    * waitm-605: auth service tests
    
    * waitm-605: reorder event emit
    
    * waitm-605: refactor status emitters
    
    * waitm-605: missing class property
    
    * waitm-605: weird typo glitch
    
    ---------
    
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/mobile/App/ui/navigation/stacks/DetectIdleLayer.tsx
+++ packages/mobile/App/ui/navigation/stacks/DetectIdleLayer.tsx
@@ -1,5 +1,13 @@
 import { debounce } from 'lodash';
-import React, { ReactElement, ReactNode, useCallback, useRef, useEffect, useState } from 'react';
+import React, {
+  ReactElement,
+  ReactNode,
+  useCallback,
+  useRef,
+  useEffect,
+  useState,
+  ReactChildren,
+} from 'react';
 import { Keyboard, PanResponder } from 'react-native';
 import { useSelector } from 'react-redux';
 import { authSignedInSelector } from '~/ui/helpers/selectors';
@@ -13,24 +21,18 @@ interface DetectIdleLayerProps {
 const ONE_MINUTE = 1000 * 60;
 const UI_EXPIRY_TIME = ONE_MINUTE * 30;
 
-export const DetectIdleLayer = ({ children }: DetectIdleLayerProps): ReactElement => {
+export const PanResponderView = ({ children }: DetectIdleLayerProps): ReactElement => {
   const [idle, setIdle] = useState(0);
-  const signedIn = useSelector(authSignedInSelector);
   const { signOutClient } = useAuth();
 
   const resetIdle = (): void => {
     setIdle(0);
   };
 
-  const debouncedResetIdle = useCallback(
-    debounce(resetIdle, 300),
-    [],
-  );
+  const debouncedResetIdle = useCallback(debounce(resetIdle, 300), []);
 
   const handleResetIdle = (): boolean => {
-    if (signedIn) {
-      debouncedResetIdle();
-    }
+    debouncedResetIdle();
     // Returns false to indicate that this component
     // shouldn't block native components from becoming the JS responder
     return false;
@@ -41,33 +43,26 @@ export const DetectIdleLayer = ({ children }: DetectIdleLayerProps): ReactElemen
   };
 
   useEffect(() => {
-    let hideEvent;
-    let showEvent;
-    if (signedIn) {
-      hideEvent = Keyboard.addListener('keyboardDidHide', handleResetIdle);
-      showEvent = Keyboard.addListener('keyboardDidShow', handleResetIdle);
-    }
+    const hideEvent = Keyboard.addListener('keyboardDidHide', handleResetIdle);
+    const showEvent = Keyboard.addListener('keyboardDidShow', handleResetIdle);
     return () => {
       hideEvent?.remove();
       showEvent?.remove();
     };
-  }, [signedIn]);
+  }, []);
 
   useEffect(() => {
-    let timer;
-    if (signedIn) {
-      timer = setInterval(() => {
-        const newIdle = idle + ONE_MINUTE;
-        setIdle(newIdle);
-        if (newIdle >= UI_EXPIRY_TIME) {
-          handleIdleLogout();
-        }
-      }, ONE_MINUTE);
-    }
+    const timer = setInterval(() => {
+      const newIdle = idle + ONE_MINUTE;
+      setIdle(newIdle);
+      if (newIdle >= UI_EXPIRY_TIME) {
+        handleIdleLogout();
+      }
+    }, ONE_MINUTE);
     return () => {
       clearInterval(timer);
     };
-  }, [idle, signedIn]);
+  }, [idle]);
 
   const panResponder = useRef(
     PanResponder.create({
@@ -82,4 +77,9 @@ export const DetectIdleLayer = ({ children }: DetectIdleLayerProps): ReactElemen
       {children}
     </StyledView>
   );
+};
+
+export const DetectIdleLayer = ({ children }: DetectIdleLayerProps): ReactElement | ReactNode => {
+  const signedIn = useSelector(authSignedInSelector);
+  return signedIn ? <PanResponderView>{children}</PanResponderView> : children;
 };
