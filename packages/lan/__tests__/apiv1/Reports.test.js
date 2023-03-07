c98f38e4af74fcafca7656db3b040f43070d0dcb
Félix Saparelli <felix@passcod.name>
2023-03-07 15:00:12 +1300

    Actually just rework that test

--- packages/lan/__tests__/apiv1/Reports.test.js
+++ packages/lan/__tests__/apiv1/Reports.test.js
@@ -1,8 +1,10 @@
 import { fake } from 'shared/test-helpers';
-import { spyOnModule } from 'shared/test-helpers/spyOn';
 import { createTestContext } from '../utilities';
 
-const reportsUtils = spyOnModule(jest, 'shared/reports');
+const reportsUtils = {
+  __esModule: true,
+  ...jest.requireActual('shared/reports'),
+};
 
 describe('Reports', () => {
   let baseApp = null;
@@ -81,13 +83,12 @@ describe('Reports', () => {
       expect(res.body).toEqual({ error: { message: 'Report module not found' } });
     });
     it('should fail with 400 and error message if dataGenerator encounters error', async () => {
-      jest.spyOn(reportsUtils, 'getReportModule').mockResolvedValue({
-        dataGenerator: jest.fn().mockRejectedValue(new Error('test-error-message')),
-        permission: 'Patient', // just need any valid permission
-      });
-      const res = await app.post('/v1/reports/incomplete-referrals', {});
+      const res = await app.post('/v1/reports/incomplete-referrals').send({ parameters: {
+        fromDate: '2020-01-01',
+        toDate: 'invalid-date',
+      } });
       expect(res).toHaveStatus(400);
-      expect(res.body).toEqual({ error: { message: 'test-error-message' } });
+      expect(res.body).toEqual({ error: { message: 'Not a valid date' } });
     });
   });
 });
