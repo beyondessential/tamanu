b0cf9fc1f129d4b602de92e047cd32636d3dbf07
Félix Saparelli <felix@beyondessential.com.au>
2023-03-08 13:29:26 +1300

    NOT-1032: Configure Terser to leave classnames intact (#3667)

--- common.webpack.config.mjs
+++ common.webpack.config.mjs
@@ -1,5 +1,9 @@
-import nodeExternals from 'webpack-node-externals';
 import NodemonPlugin from 'nodemon-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
+
+// we assume a Node.js environment, desktop has its own config
+
 
 export default {
   entry: ['core-js/stable', './index.js'],
@@ -18,6 +22,18 @@ export default {
           },
         },
       },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          ecma: 2020,
+          keep_classnames: true,
+          keep_fnames: true,
+        },
+      }),
     ],
   },
   output: {
