0dd735380b9028f6bc9c6e8d27e85227c8851436
Félix Saparelli <felix@passcod.name>
2023-03-10 12:13:01 +1300

    Use shared frame for {lint,test}-all

--- scripts/lint-all.mjs
+++ scripts/lint-all.mjs
@@ -1,22 +1,14 @@
 import { execFileSync } from 'child_process';
-import { readFileSync } from 'fs';
+import { doWithAllPackages } from './_do-with-all-packages.mjs';
 
-const topPkg = JSON.parse(readFileSync('./package.json'));
-
-for (const name of topPkg.workspaces.packages) {
-  let pkg;
-  try {
-    pkg = JSON.parse(readFileSync(`./${name}/package.json`));
-  } catch (err) {
-    console.log(`Skipping ${name} as we can't read its package.json...`);
-    continue;
-  }
-
+doWithAllPackages((name, pkg) => {
   if (!pkg?.scripts?.lint) {
     console.log(`Skipping ${name} as it doesn't have a lint script...`);
-    continue;
+    return;
   }
 
   console.log(`Linting ${name}...`);
-  execFileSync('yarn', ['workspace', pkg.name, 'run', 'lint', ...process.argv.slice(2)], { stdio: 'inherit' });
-}
+  execFileSync('yarn', ['workspace', pkg.name, 'run', 'lint', ...process.argv.slice(2)], {
+    stdio: 'inherit',
+  });
+});
--- scripts/test-all.mjs
+++ scripts/test-all.mjs
@@ -1,22 +1,14 @@
 import { execFileSync } from 'child_process';
-import { readFileSync } from 'fs';
+import { doWithAllPackages } from './_do-with-all-packages.mjs';
 
-const topPkg = JSON.parse(readFileSync('./package.json'));
-
-for (const name of topPkg.workspaces.packages) {
-  let pkg;
-  try {
-    pkg = JSON.parse(readFileSync(`./${name}/package.json`));
-  } catch (err) {
-    console.log(`Skipping ${name} as we can't read its package.json...`);
-    continue;
-  }
-
+doWithAllPackages((name, pkg) => {
   if (!pkg?.scripts?.test) {
     console.log(`Skipping ${name} as it doesn't have a test script...`);
-    continue;
+    return;
   }
 
   console.log(`Testing ${name}...`);
-  execFileSync('yarn', ['workspace', pkg.name, 'run', 'test', ...process.argv.slice(2)], { stdio: 'inherit' });
-}
+  execFileSync('yarn', ['workspace', pkg.name, 'run', 'test', ...process.argv.slice(2)], {
+    stdio: 'inherit',
+  });
+});
