ede02295ee2da7379c44b38fd0f7df6af022362c
Edwin <edmofro@gmail.com>
2023-03-15 07:55:42 +1300

    Add comment

--- packages/mobile/App/migrations/1678779906000-resyncSince123Deployed.ts
+++ packages/mobile/App/migrations/1678779906000-resyncSince123Deployed.ts
@@ -1,15 +1,20 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+// This is a specific migration for Nauru
+// see https://beyondessential.slack.com/archives/C022N48Q3JQ/p1678779818104859?thread_ts=1678766342.369209&cid=C022N48Q3JQ
 import { TABLE_DEFINITIONS } from './firstTimeSetup/databaseDefinition';
 
-// this is a specific migration for Nauru
-// see https://beyondessential.slack.com/archives/C022N48Q3JQ/p1678779818104859?thread_ts=1678766342.369209&cid=C022N48Q3JQ
+// Any records that haven't been touched, or that have synced from the central server into the
+// mobile app, will have a sync tick of -999. We want to mark for resync all records that have
+// a higher sync tick than that
+const UNTOUCHED_SYNC_TICK = -999;
+
 export class resyncSince123Deployed1678779906000 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
     for (const tableName of TABLE_DEFINITIONS.map(t => t.name)) {
       await queryRunner.query(`
         UPDATE ${tableName}
         SET updated_at_sync_tick = 1
-        WHERE updated_at_sync_tick > -999;
+        WHERE updated_at_sync_tick > ${UNTOUCHED_SYNC_TICK};
       `);
     }
   }
