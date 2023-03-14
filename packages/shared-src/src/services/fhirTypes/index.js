b7b5e2f28f790f1caf0d2efd88ba4ada19975711
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-03-14 19:16:24 -0400

    Move newPgDataType to different file unrelated to FHIR
    Co-authored-by: Félix Saparelli <felix@beyondessential.com.au>

--- packages/shared-src/src/services/fhirTypes/index.js
+++ packages/shared-src/src/services/fhirTypes/index.js
@@ -1,5 +1,3 @@
-import { DataTypes, Utils } from 'sequelize';
-
 export { FhirAddress } from './address';
 export { FhirAnnotation } from './annotation';
 export { FhirCoding } from './coding';
@@ -13,14 +11,3 @@ export { FhirImmunizationProtocolApplied } from './immunizationProtocolApplied';
 export { FhirPatientLink } from './patientLink';
 export { FhirPeriod } from './period';
 export { FhirReference } from './reference';
-
-/**
- * Register a new type with sequelize.
- * CANNOT register an extension of an existing sequelize type (like STRING, etc).
- * Only use with fully-new types.
- */
-export function newPgDataType(Klass) {
-  const name = Klass.key;
-  DataTypes[name] = Utils.classToInvokable(Klass);
-  DataTypes[name].types.postgres = [Klass.pgName];
-}
