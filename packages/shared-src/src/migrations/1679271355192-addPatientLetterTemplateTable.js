1c4048528ea0fa2efc6784812d34425e3a2ba282
Alastair-L <lockealastair@gmail.com>
2023-03-20 11:24:45 +1100

    1.2 Create migration

---
+++ packages/shared-src/src/migrations/1679271355192-addPatientLetterTemplateTable.js
@@ -0,0 +1,40 @@
+import Sequelize from 'sequelize';
+
+export async function up(query) {
+  await query.createTable('patient_letter_templates', {
+    id: {
+      type: Sequelize.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('uuid_generate_v4'),
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    title: {
+      type: Sequelize.STRING,
+      // allowNull: false,
+    },
+    body: {
+      type: Sequelize.STRING,
+      // allowNull: false,
+    },
+    deleted_at: {
+      type: Sequelize.DATE,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+    },
+  });
+}
+
+export async function down(query) {
+  await query.dropTable('patient_letter_templates');
+}
