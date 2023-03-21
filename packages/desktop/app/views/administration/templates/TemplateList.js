f05390f54927fa721ada3b630c781023e1d756be
Alastair-L <lockealastair@gmail.com>
2023-03-21 16:58:00 +1100

    Add structure to template

---
+++ packages/desktop/app/views/administration/templates/TemplateList.js
@@ -0,0 +1,43 @@
+import ms from 'ms';
+import React from 'react';
+
+import { TopBar, PageContainer, DataFetchingTable, DateDisplay } from '../../../components';
+
+export const TemplateList = React.memo(props => (
+  <DataFetchingTable
+    endpoint={'admin/patientLetterTemplate'}
+    columns={[
+      {
+        key: 'type',
+        title: 'Type',
+        minWidth: 100,
+        accessor: () => 'Patient Letter',
+      },
+      {
+        key: 'name',
+        title: 'Template name',
+      },
+      {
+        key: 'title',
+        title: 'Title',
+        // accessor: ({ completedAt }) => <DateDisplay date={completedAt} showTime />,
+      },
+      {
+        key: 'date_created',
+        title: 'Created on',
+        accessor: ({ completedAt }) => <DateDisplay date={completedAt} showTime />,
+      },
+      {
+        key: 'created_by',
+        title: 'Created by',
+        // accessor: ({ completedAt }) => <DateDisplay date={completedAt} showTime />,
+      },
+      {
+        key: 'body',
+        title: 'Contents',
+      },
+    ]}
+    noDataMessage="No data"
+    {...props}
+  />
+));
