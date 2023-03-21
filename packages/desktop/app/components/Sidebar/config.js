b6d31cd1d1f9104cc8940fe3b5edacd8460af8dc
Alastair-L <lockealastair@gmail.com>
2023-03-21 12:27:18 +1100

    Create Template Admin view

--- packages/desktop/app/components/Sidebar/config.js
+++ packages/desktop/app/components/Sidebar/config.js
@@ -192,6 +192,11 @@ export const SYNC_MENU_ITEMS = [
     path: '/admin/patientMerge',
   },
   {
+    key: 'templates',
+    label: 'Templates',
+    path: '/admin/templates',
+  },
+  {
     key: 'sync',
     label: 'Sync status',
     path: '/admin/sync',
