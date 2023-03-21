81d6c5e6624eeeb8d36f5b9e4278936937bacca7
Chris Tran <chris@beyondessential.com.au>
2023-03-22 10:59:10 +1100

    sav-48: set jest timeout

--- packages/sync-server/__tests__/sync/CentralSyncManager.test.js
+++ packages/sync-server/__tests__/sync/CentralSyncManager.test.js
@@ -9,6 +9,8 @@ import { createTestContext } from '../utilities';
 import { importerTransaction } from '../../app/admin/importerEndpoint';
 import { referenceDataImporter } from '../../app/admin/referenceDataImporter';
 
+jest.setTimeout(100000);
+
 const doImport = (options, models) => {
   const { file, ...opts } = options;
   return importerTransaction({
