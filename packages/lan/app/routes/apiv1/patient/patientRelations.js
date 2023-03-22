799e87d427b6c5a05c8d3a6c4d1302760afb25bc
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-03-23 11:14:11 +1300

    WAITM-583 Enable panel filter on both parts of endpoint

--- packages/lan/app/routes/apiv1/patient/patientRelations.js
+++ packages/lan/app/routes/apiv1/patient/patientRelations.js
@@ -335,16 +335,15 @@ patientRelations.get(
         AND lab_tests.status = '${status}'
         ${categoryId ? `AND category_id = '${categoryId}'` : ''}
         ${
-          // TODO: Enable this section when panels epic is merged
-          // panelId
-          //   ? `AND lab_test_type_id IN (
-          //     SELECT lab_test_type_id
-          //     FROM
-          //       lab_test_panel_lab_test_types
-          //     WHERE
-          //       lab_test_panel_id = ${panelId}
-          //   )`:
-          ''
+          panelId
+            ? `AND lab_test_type_id IN (
+               SELECT lab_test_type_id
+               FROM
+                 lab_test_panel_lab_test_types
+               WHERE
+                 lab_test_panel_id = ${panelId}
+             )`
+            : ''
         }
         ${orderBy ? `ORDER BY ${orderBy} ${sortDirection}` : ''}
       `,
