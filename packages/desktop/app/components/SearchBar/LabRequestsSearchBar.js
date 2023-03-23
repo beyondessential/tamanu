42af867e23a5b28dbd5cc27115f315536f3cc110
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-03-23 13:12:40 +1300

    Update lab request labels to be more consistent

--- packages/desktop/app/components/SearchBar/LabRequestsSearchBar.js
+++ packages/desktop/app/components/SearchBar/LabRequestsSearchBar.js
@@ -15,8 +15,8 @@ export const LabRequestsSearchBar = () => {
       <LocalisedField name="firstName" />
       <LocalisedField name="lastName" />
       <LocalisedField name="displayId" />
-      <LocalisedField name="requestId" defaultLabel="Request ID" />
-      <LocalisedField name="category" defaultLabel="Type" />
+      <LocalisedField name="requestId" defaultLabel="Test ID" />
+      <LocalisedField name="category" defaultLabel="Test category" />
       <LocalisedField
         name="status"
         defaultLabel="Status"
