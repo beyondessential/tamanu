40dd44d3f5dbfe6f63c0514f7cab3fba22af4a75
Tim Kennedy <t.m.knndy@gmail.com>
2023-03-24 16:12:05 +1100

    Fix date/facility/location section

--- packages/desktop/app/components/PatientPrinting/printouts/reusable/DateFacilitySection.js
+++ packages/desktop/app/components/PatientPrinting/printouts/reusable/DateFacilitySection.js
@@ -6,34 +6,40 @@ import { getCurrentDateString } from 'shared/utils/dateTime';
 import { getFullLocationName } from '../../../../utils/location';
 import { DateDisplay } from '../../../DateDisplay';
 
-import { LocalisedLabel } from './SimplePrintout';
-import { CertificateLabel } from './CertificateLabels';
+import { CertificateLabel, LocalisedCertificateLabel } from './CertificateLabels';
 
 const RowContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: start;
   flex-wrap: wrap;
   padding: 0 20px;
+`;
+
+const Item = styled.div`
+  margin-right: 36px;
+  p {
+    margin-bottom: 0px;
+  }
 `;
 
 export const DateFacilitySection = ({ encounter }) => {
   return (
     <RowContainer>
-      <div>
+      <Item>
         <CertificateLabel name="Print date" size="14px">
           <DateDisplay date={getCurrentDateString()} />
         </CertificateLabel>
-      </div>
-      <div>
-        <LocalisedLabel name="facility" size="14px">
+      </Item>
+      <Item>
+        <LocalisedCertificateLabel name="facility" size="14px">
           {encounter?.location?.facility?.name}
-        </LocalisedLabel>
-      </div>
-      <div>
-        <LocalisedLabel name="locationId" size="14px">
+        </LocalisedCertificateLabel>
+      </Item>
+      <Item>
+        <LocalisedCertificateLabel name="locationId" size="14px">
           {getFullLocationName(encounter?.location)}
-        </LocalisedLabel>
-      </div>
+        </LocalisedCertificateLabel>
+      </Item>
     </RowContainer>
   );
 };
