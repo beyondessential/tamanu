704625230c69ae77220995cdb5654ad841e138c7
Tom Caiger <caigertom@gmail.com>
2023-03-24 16:03:54 +1300

    refactor styles

--- packages/mobile/App/ui/components/Checkbox/CustomCheckbox.tsx
+++ packages/mobile/App/ui/components/Checkbox/CustomCheckbox.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { StyledView, StyledText, RowView } from '/styled/common';
 import { theme } from '/styled/theme';
 import { Orientation, screenPercentageToDP } from '/helpers/screen';
@@ -14,6 +14,47 @@ interface CheckboxProps extends BaseInputProps {
   value: boolean;
 }
 
+const styles = StyleSheet.create({
+  checked: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    backgroundColor: theme.colors.PRIMARY_MAIN,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  unchecked: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderColor: theme.colors.BOX_OUTLINE,
+    backgroundColor: theme.colors.WHITE,
+    borderWidth: 1,
+  },
+  icon: {
+    stroke: theme.colors.WHITE,
+    borderColor: theme.colors.PRIMARY_MAIN,
+    height: 10,
+    width: 10,
+  },
+  row: {
+    backgroundColor: theme.colors.WHITE,
+    paddingLeft: 7,
+    paddingRight: 15,
+    paddingTop: 7,
+    paddingBottom: 7,
+    alignSelf: 'flex-start',
+    borderRadius: 100,
+    alignItems: 'center',
+  },
+  text: {
+    marginLeft: 5,
+    fontSize: screenPercentageToDP('1.70', Orientation.Height),
+    fontWeight: '400',
+    color: theme.colors.TEXT_DARK,
+  },
+});
+
 export const CustomCheckbox = ({
   value,
   onChange,
@@ -21,67 +62,24 @@ export const CustomCheckbox = ({
   text,
   error,
   required,
-}: CheckboxProps): JSX.Element => {
-  const ChangeCallback = () => onChange(!value, id);
-
-  const Checked = (
-    <StyledView
-      height={20}
-      width={20}
-      borderRadius={10}
-      background={theme.colors.PRIMARY_MAIN}
-      alignItems="center"
-      justifyContent="center"
+}: CheckboxProps): JSX.Element => (
+  <StyledView>
+    <Pressable
+      onPress={() => onChange(!value, id)}
+      android_ripple={{ color: theme.colors.LIGHT_GREY, foreground: true }}
+      style={{ alignSelf: 'flex-start', borderRadius: 100, overflow: 'hidden' }}
     >
-      <CheckboxMarkIcon
-        stroke={theme.colors.WHITE}
-        background={theme.colors.PRIMARY_MAIN}
-        height={12}
-        width={12}
-      />
-    </StyledView>
-  );
-
-  const UnChecked = (
-    <StyledView
-      height={20}
-      width={20}
-      borderRadius={10}
-      borderColor={theme.colors.BOX_OUTLINE}
-      background={theme.colors.WHITE}
-      borderWidth={1}
-    />
-  );
-
-  return (
-    <StyledView>
-      <Pressable onPress={ChangeCallback}>
-        {({ pressed }) => (
-          <RowView
-            background={pressed ? theme.colors.LIGHT_GREY : theme.colors.WHITE}
-            paddingLeft={7}
-            paddingRight={15}
-            paddingTop={7}
-            paddingBottom={7}
-            alignSelf="flex-start"
-            borderRadius={100}
-            alignItems="center"
-          >
-            {value ? Checked : UnChecked}
-            {text && (
-              <StyledText
-                marginLeft={5}
-                fontSize={screenPercentageToDP('1.70', Orientation.Height)}
-                fontWeight={400}
-                color={theme.colors.TEXT_DARK}
-              >
-                {`${text}${required ? '*' : ''}`}
-              </StyledText>
-            )}
-          </RowView>
+      <RowView style={styles.row}>
+        {value ? (
+          <StyledView style={styles.checked}>
+            <CheckboxMarkIcon style={styles.icon} />
+          </StyledView>
+        ) : (
+          <StyledView style={styles.unchecked} />
         )}
-      </Pressable>
-      {error && <TextFieldErrorMessage>{error}</TextFieldErrorMessage>}
-    </StyledView>
-  );
-};
+        {text && <StyledText style={styles.text}>{`${text}${required ? '*' : ''}`}</StyledText>}
+      </RowView>
+    </Pressable>
+    {error && <TextFieldErrorMessage>{error}</TextFieldErrorMessage>}
+  </StyledView>
+);
