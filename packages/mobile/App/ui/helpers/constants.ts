746e1655bef3b314ea695baa204e45dfff23c682
Alastair <lockealastair@gmail.com>
2023-03-24 13:04:47 +1100

    TAN-1996: Update sick/injured page and add notes to mobile (#3724)
    * Rename Sick/injured tab to diagnosis/treatment
    
    * Allow submitting form with no diagnosis
    
    * Lint
    
    * Disable certainty if no diagnosis
    
    * Half create notes on mobile
    
    * Fix merge conflicts
    
    * Fix spelling of PatientAdditionalData
    
    * Clean up notePage model
    
    * Lint
    
    * Add noteItem model
    
    * Commit changes
    
    * Models progress
    
    * Sync starts!
    
    * Now stuck on onBehalfOfId somehow having a notNull constraint
    
    * No progress
    
    * Temp
    
    * Fix merge conflict
    
    * Allow submitting new notes
    
    * Notes display
    
    * Remove Test results field
    
    * Progress?
    
    * Trying mui - can't get it to work
    
    * Validate NoteItem: ensure non-empty content
    
    * Clean up non-ui stuff
    
    * Give up on material UI for now
    
    * Progress
    
    * Bring multipleselect into the codebase
    
    * Disabled finally works!
    
    * Only display clinical_mobile notes
    
    * Make multiple-select work disabled and enabled
    
    * More updates
    
    * Clean up code
    
    * Lowercase mobile readme
    
    Co-authored-by: Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
    
    * Replace !!val with Boolean(val)
    
    Co-authored-by: Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
    
    * Merge fix: label change
    
    * Fix noteTypes merge conflict
    
    * Lint fix
    
    ---------
    
    Co-authored-by: Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>

--- packages/mobile/App/ui/helpers/constants.ts
+++ packages/mobile/App/ui/helpers/constants.ts
@@ -216,6 +216,34 @@ export const VitalsDataElements = {
   dateRecorded: 'pde-PatientVitalsDate',
 };
 
+export const NOTE_RECORD_TYPES = {
+  ENCOUNTER: 'Encounter',
+  PATIENT: 'Patient',
+  TRIAGE: 'Triage',
+  PATIENT_CARE_PLAN: 'PatientCarePlan',
+  LAB_REQUEST: 'LabRequest',
+  IMAGING_REQUEST: 'ImagingRequest',
+  // IMPORTANT: if you add any more record types, you must also alter buildNoteLinkedSyncFilter
+};
+
+export const NOTE_TYPES = {
+  TREATMENT_PLAN: 'treatmentPlan',
+  MEDICAL: 'medical',
+  SURGICAL: 'surgical',
+  NURSING: 'nursing',
+  DIETARY: 'dietary',
+  PHARMACY: 'pharmacy',
+  PHYSIOTHERAPY: 'physiotherapy',
+  SOCIAL: 'social',
+  DISCHARGE: 'discharge',
+  AREA_TO_BE_IMAGED: 'areaToBeImaged',
+  RESULT_DESCRIPTION: 'resultDescription',
+  SYSTEM: 'system',
+  OTHER: 'other',
+  CLINICAL_MOBILE: 'clinicalMobile',
+  HANDOVER: 'handover',
+};
+
 export const FORM_STATUSES = {
   SUBMIT_SCREEN_ATTEMPTED: 'SUBMIT_SCREEN_ATTEMPTED',
 };
