9f8959dff8a1e5214d334a41435238727785f352
Tom Caiger <caigertom@gmail.com>
2023-03-27 13:12:51 +1300

    fix labs filter (#3814)

--- packages/desktop/app/views/LabRequestListingView.js
+++ packages/desktop/app/views/LabRequestListingView.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { TopBar, PageContainer, LabRequestsSearchBar, ContentPane } from '../components';
-import { LabRequestsTable } from '../components/LabRequestsTable';
+import { LabRequestsTable } from './LabRequestsTable';
 
 export const LabRequestListingView = React.memo(() => (
   <PageContainer>
