aa5612c4a50710f4ab387a7dd2c6a5ab5c1a39c6
Félix Saparelli <felix@beyondessential.com.au>
2023-03-30 09:24:50 +1300

    EPI-211: FHIR jobs (#3517)
    Co-authored-by: chris-bes <63621318+chris-bes@users.noreply.github.com>

--- packages/shared-src/src/models/fhir/ServiceRequest.js
+++ packages/shared-src/src/models/fhir/ServiceRequest.js
@@ -1,5 +1,5 @@
 import config from 'config';
-import { DataTypes } from 'sequelize';
+import { DataTypes, Op } from 'sequelize';
 
 import { FhirResource } from './Resource';
 
@@ -50,6 +50,18 @@ export class FhirServiceRequest extends FhirResource {
     );
 
     this.UpstreamModel = models.ImagingRequest;
+    this.upstreams = [
+      models.ImagingRequest,
+      models.ImagingRequestArea,
+      models.ImagingAreaExternalCode,
+      models.Encounter,
+      models.Facility,
+      models.Location,
+      models.LocationGroup,
+      models.Patient,
+      models.ReferenceData,
+      models.User,
+    ];
   }
 
   static CAN_DO = new Set([
@@ -186,6 +198,172 @@ export class FhirServiceRequest extends FhirResource {
       }),
       locationCode: locationCode(upstream),
     });
+  }
+
+  static async queryToFindUpstreamIdsFromTable(table, id) {
+    const {
+      ImagingRequest,
+      ImagingRequestArea,
+      ImagingAreaExternalCode,
+      Encounter,
+      Facility,
+      Location,
+      LocationGroup,
+      Patient,
+      ReferenceData,
+      User,
+    } = this.sequelize.models;
+
+    switch (table) {
+      case ImagingRequest.tableName:
+        return { where: { id } };
+      case ImagingRequestArea.tableName:
+        return {
+          include: [
+            {
+              model: ImagingRequestArea,
+              as: 'areas',
+              where: { id },
+            },
+          ],
+        };
+      case Encounter.tableName:
+        return {
+          include: [
+            {
+              model: Encounter,
+              as: 'encounter',
+              where: { id },
+            },
+          ],
+        };
+      case Facility.tableName:
+        return {
+          include: [
+            {
+              model: Encounter,
+              as: 'encounter',
+              include: [
+                {
+                  model: Location,
+                  as: 'location',
+                  include: [
+                    {
+                      model: Facility,
+                      as: 'facility',
+                      where: { id },
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        };
+      case Location.tableName:
+        return {
+          include: [
+            {
+              model: Encounter,
+              as: 'encounter',
+              include: [
+                {
+                  model: Location,
+                  as: 'location',
+                  where: { id },
+                },
+              ],
+            },
+          ],
+        };
+      case LocationGroup.tableName:
+        return {
+          include: [
+            {
+              model: Encounter,
+              as: 'encounter',
+              include: [
+                {
+                  model: LocationGroup,
+                  as: 'locationGroup',
+                  where: { id },
+                },
+              ],
+            },
+          ],
+        };
+      case Patient.tableName:
+        return {
+          include: [
+            {
+              model: Encounter,
+              as: 'encounter',
+              include: [
+                {
+                  model: Patient,
+                  as: 'patient',
+                  where: { id },
+                },
+              ],
+            },
+          ],
+        };
+      case ReferenceData.tableName:
+        return {
+          include: [
+            {
+              model: ImagingRequestArea,
+              as: 'areas',
+              include: [
+                {
+                  model: ReferenceData,
+                  as: 'area',
+                  where: { id },
+                },
+              ],
+            },
+          ],
+        };
+      case ImagingAreaExternalCode.tableName:
+        return {
+          include: [
+            {
+              model: ImagingRequestArea,
+              as: 'areas',
+              include: [
+                {
+                  model: ReferenceData,
+                  as: 'area',
+                  include: [
+                    {
+                      model: ImagingAreaExternalCode,
+                      as: 'imagingAreaExternalCode',
+                      where: { id },
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        };
+      case User.tableName:
+        return {
+          include: [
+            {
+              model: User,
+              as: 'requestedBy',
+            },
+            {
+              model: User,
+              as: 'completedBy',
+            },
+          ],
+          where: {
+            [Op.or]: [{ '$requestedBy.id$': id }, { '$completedBy.id$': id }],
+          },
+        };
+      default:
+        return null;
+    }
   }
 
   static searchParameters() {
