021385147c2fce9eaa49cebeb3a15566d54abc6a
Renato Koyama <renato.koyama@gmail.com>
2023-03-29 16:27:55 -0300

    TAN-2068 UI/UX fixes (#3841)
    * TAN-2068: fix sort icon on table
    
    * TAN-2068: fix all search columns order
    
    * TAN-2068: fix by cultural name when null
    
    * fix: DOB labels to lower case

--- packages/lan/app/routes/apiv1/patient/constants.js
+++ packages/lan/app/routes/apiv1/patient/constants.js
@@ -2,7 +2,7 @@ export const PATIENT_SORT_KEYS = {
   markedForSync: 'patient_facilities.patient_id',
   displayId: 'patients.display_id',
   lastName: 'UPPER(patients.last_name)',
-  culturalName: 'UPPER(patients.cultural_name)',
+  culturalName: `UPPER(COALESCE(patients.cultural_name, ''))`,
   firstName: 'UPPER(patients.first_name)',
   age: 'patients.date_of_birth',
   dateOfBirth: 'patients.date_of_birth',
