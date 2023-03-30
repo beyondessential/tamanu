ad3d91fe03d3bd351eaaa3ace3dfe59aa6d83a6c
Daniel Nash <dannash200@gmail.com>
2023-03-30 15:43:24 +1300

    no-issue: import select from existing

--- packages/desktop/app/views/administration/reports/import/index.js
+++ packages/desktop/app/views/administration/reports/import/index.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useQuery } from '@tanstack/react-query'
 import { useApi } from '../../../../api';
-import { FormGrid, OutlinedButton, TextInput } from '../../../../components';
+import { BodyText, FormGrid, Heading4, OutlinedButton, SelectInput, TextInput } from '../../../../components';
 import { FileChooserInput } from '../../../../components/Field/FileChooserField';
 
 const InnerContainer = styled.div`
@@ -13,14 +14,25 @@ const StyledButton = styled(OutlinedButton)`
   margin-top: 30px
 `;
 
+const FormContainer = styled(FormGrid)`
+  margin-bottom: 30px;
+`
+
 export const ReportsImportView = () => {
   const api = useApi();
   const [name, setName] = useState('');
+  const [reportId, setReportId] = useState(null);
   const [file, setFile] = useState(null);
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState(null);
 
+  const { data: reportData = [], isLoading: reportLoading, error: reportError } = useQuery(
+    ['reportList'],
+    () => api.get('admin/reports'),
+  );
+
   const handleChangeName = (event) => {
+    if (reportId) setReportId(null);
     setName(event.target.value);
   };
 
@@ -28,13 +40,20 @@ export const ReportsImportView = () => {
     setFile(event.target.value);
   };
 
+  const handleSelectReport = ({ target: {
+    value
+  } }) => {
+    setName(reportData.find(report => report.id === value).name)
+    setReportId(value)
+  };
+
+
   const handleSubmit = async (event) => {
     setSubmitting(true);
     setError(null);
     try {
-      const report = await api.post('admin/reports', {file, name})
-
-    } catch(err) {
+      const report = await api.post('admin/reports', { file, name })
+    } catch (err) {
       setError(err);
     } finally {
       setSubmitting(false);
@@ -43,10 +62,16 @@ export const ReportsImportView = () => {
 
   return (
     <InnerContainer>
-      <FormGrid columns={1}>
+
+      <FormContainer columns={1}>
         <TextInput required label="Report Name" name='name' onChange={handleChangeName} value={name} />
-        <FileChooserInput required label="Report JSON" name='file' onChange={handleChangeFile} value={file} filters={[{ name: 'JSON (.json)', extensions: ['json'] }]} />
-      </FormGrid>
+        <Heading4>or</Heading4>
+        <SelectInput label="Select Report" name='report' onChange={handleSelectReport} value={reportId} options={reportData.map(report => ({
+          label: report.name,
+          value: report.id,
+        }))} />
+      </FormContainer>
+      <FileChooserInput required label="Report JSON" name='file' onChange={handleChangeFile} value={file} filters={[{ name: 'JSON (.json)', extensions: ['json'] }]} />
       <StyledButton onClick={handleSubmit} disabled={submitting || !name || !file}>Import</StyledButton>
     </InnerContainer>
   );
