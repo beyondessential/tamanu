b481f3bc34275923d6af55aa4d3636683699a59c
Ethan McQuarrie <ethan@beyondessential.com.au>
2023-04-03 12:16:18 +1200

    NASS-727 Commenting

--- packages/desktop/app/views/patients/components/LabRequestPrintModal.js
+++ packages/desktop/app/views/patients/components/LabRequestPrintModal.js
@@ -25,7 +25,7 @@ export const LabRequestPrintModal = React.memo(({ labRequest, patient, open, onC
     },
   );
   const { data: notes, isLoading: areNotesLoading } = useQuery(
-    ['labRequestNotes', labRequest.id, open],
+    ['labRequestNotes', labRequest.id, open], // Refresh notes each time we open the modal
     async () => {
       const notesRes = await api.get(`labRequest/${encodeURIComponent(labRequest.id)}/notes`);
       return notesRes.data;
