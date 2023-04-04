600b0f53545ef4ffc8bf1c7c18823e26d460cd3d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-04-04 15:16:16 +1200

    create story for dynamic select field

---
+++ packages/desktop/stories/dynamicSelectField.stories.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { DynamicSelectField } from '../app/components';
+
+const FRUITS = [
+  { value: 'apples', label: 'Apples' },
+  { value: 'oranges', label: 'Oranges' },
+  { value: 'bananas', label: 'Bananas' },
+];
+
+const FURNITURE = [
+  { label: 'Sofa', value: 'sofa' },
+  { label: 'Armchair', value: 'armchair' },
+  { label: 'Coffee Table', value: 'coffeeTable' },
+  { label: 'End Table', value: 'endTable' },
+  { label: 'Dining Table', value: 'diningTable' },
+  { label: 'Dining Chair', value: 'diningChair' },
+  { label: 'Bed', value: 'bed' },
+  { label: 'Dresser', value: 'dresser' },
+  { label: 'Nightstand', value: 'nightstand' },
+  { label: 'Bookshelf', value: 'bookshelf' },
+];
+
+export default {
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
+  title: 'FormControls/DynamicSelectField',
+  component: DynamicSelectField,
+};
+
+const Template = args => {
+  const [value, setValue] = useState(null);
+  const handleChange = e => {
+    setValue(e.target.value);
+  };
+  return <DynamicSelectField field={{ name: 'name', onChange: handleChange, value }} {...args} />;
+};
+
+export const SevenOrLessItems = Template.bind({});
+SevenOrLessItems.args = {
+  options: FRUITS,
+};
+
+export const MoreThanSevenItems = Template.bind({});
+MoreThanSevenItems.args = {
+  options: FURNITURE,
+};
