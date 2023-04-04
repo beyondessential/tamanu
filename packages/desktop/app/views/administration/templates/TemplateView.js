56f702c2e0ab06675419359498ad85c19677b1c0
Alastair-L <lockealastair@gmail.com>
2023-04-04 13:10:46 +1000

    Improve formatting

--- packages/desktop/app/views/administration/templates/TemplateView.js
+++ packages/desktop/app/views/administration/templates/TemplateView.js
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
-import { TopBar, PageContainer, DataFetchingTable, DateDisplay } from '../../../components';
+import styled from 'styled-components';
+import { TopBar, PageContainer, ContentPane, DataFetchingTable, DateDisplay } from '../../../components';
+import { Colors } from '../../../constants';
 import { NewTemplateForm } from '../../../forms';
 import { TemplateList } from './TemplateList';
 import { useApi } from '../../../api';
+
+const ContentContainer = styled.div`
+  padding: 32px 30px;
+  border: 1px solid ${Colors.outline};
+  background: ${Colors.white};
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
 
 export const TemplateView = ({ }) => {
 
   return (
     <PageContainer>
       <TopBar title="Templates" />
-      <NewTemplateForm />
-      <TemplateList />
+      <ContentPane>
+        <ContentContainer>
+          <NewTemplateForm />
+        </ContentContainer>
+        <TemplateList />
+      </ContentPane>
       {/* {modal} */}
     </PageContainer>
   );
