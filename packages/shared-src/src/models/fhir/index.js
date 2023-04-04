ee8955e6a0ef4646033e92b0f8cb82a7159339ea
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-04-04 18:20:34 -0400

    Split resource into materialisable and other

--- packages/shared-src/src/models/fhir/index.js
+++ packages/shared-src/src/models/fhir/index.js
@@ -3,6 +3,7 @@ export * from './ImagingStudy';
 export * from './Immunization';
 export * from './Patient';
 export * from './ServiceRequest';
+export * from './ServiceRequestImaging';
 
 export * from './Job';
 export * from './JobWorker';
