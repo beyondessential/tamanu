2555466630c72ba10ed15f1e28b8926508768ba6
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-04-06 11:36:45 +1200

    make logic more readable

--- packages/desktop/app/components/Field/DynamicSelectField.js
+++ packages/desktop/app/components/Field/DynamicSelectField.js
@@ -5,42 +5,31 @@ import { AutocompleteInput } from './AutocompleteField';
 
 export const DynamicSelectField = ({ field, options, suggester, name, ...props }) => {
   const [selectOptions, setSelectOptions] = useState([]);
-  const [autocompleteOptions, setAutocompleteOptions] = useState([]);
-  const [autocompleteSuggester, setAutocompleteSuggester] = useState(null);
-
   const SELECT_OPTIONS_LIMIT = 7;
 
   useEffect(() => {
     async function setInputOptions() {
       const optionsList = suggester ? await suggester.fetchSuggestions() : options;
-      if (optionsList.length > SELECT_OPTIONS_LIMIT) {
-        if (suggester) {
-          setAutocompleteSuggester(suggester);
-        } else {
-          setAutocompleteOptions(optionsList);
-        }
-      } else {
-        setSelectOptions(optionsList);
-      }
+      setSelectOptions(optionsList);
     }
     setInputOptions();
   }, [options, suggester]);
 
-  return selectOptions.length > 0 ? (
+  return selectOptions.length > SELECT_OPTIONS_LIMIT ? (
+    <AutocompleteInput
+      name={field.name}
+      onChange={field.onChange}
+      value={field.value}
+      suggester={suggester}
+      options={selectOptions}
+      {...props}
+    />
+  ) : (
     <SelectInput
       name={field.name}
       onChange={field.onChange}
       value={field.value}
       options={selectOptions}
-      {...props}
-    />
-  ) : (
-    <AutocompleteInput
-      name={field.name}
-      onChange={field.onChange}
-      value={field.value}
-      suggester={autocompleteSuggester}
-      options={autocompleteOptions}
       {...props}
     />
   );
