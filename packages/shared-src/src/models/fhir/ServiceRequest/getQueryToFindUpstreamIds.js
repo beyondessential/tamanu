133a819bf038c551850f9891d8c35b4b691877df
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-04-11 19:09:51 -0400

    Create helper that handles querying upstream IDs

---
+++ packages/shared-src/src/models/fhir/ServiceRequest/getQueryToFindUpstreamIds.js
@@ -0,0 +1,167 @@
+import { Op } from 'sequelize';
+
+export function getQueryToFindUpstreamIds(models, table, id) {
+  const {
+    ImagingRequest,
+    ImagingRequestArea,
+    ImagingAreaExternalCode,
+    Encounter,
+    Facility,
+    Location,
+    LocationGroup,
+    Patient,
+    ReferenceData,
+    User,
+  } = models;
+
+  switch (table) {
+    case ImagingRequest.tableName:
+      return { where: { id } };
+    case ImagingRequestArea.tableName:
+      return {
+        include: [
+          {
+            model: ImagingRequestArea,
+            as: 'areas',
+            where: { id },
+          },
+        ],
+      };
+    case Encounter.tableName:
+      return {
+        include: [
+          {
+            model: Encounter,
+            as: 'encounter',
+            where: { id },
+          },
+        ],
+      };
+    case Facility.tableName:
+      return {
+        include: [
+          {
+            model: Encounter,
+            as: 'encounter',
+            include: [
+              {
+                model: Location,
+                as: 'location',
+                include: [
+                  {
+                    model: Facility,
+                    as: 'facility',
+                    where: { id },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      };
+    case Location.tableName:
+      return {
+        include: [
+          {
+            model: Encounter,
+            as: 'encounter',
+            include: [
+              {
+                model: Location,
+                as: 'location',
+                where: { id },
+              },
+            ],
+          },
+        ],
+      };
+    case LocationGroup.tableName:
+      return {
+        include: [
+          {
+            model: Encounter,
+            as: 'encounter',
+            include: [
+              {
+                model: LocationGroup,
+                as: 'locationGroup',
+                where: { id },
+              },
+            ],
+          },
+        ],
+      };
+    case Patient.tableName:
+      return {
+        include: [
+          {
+            model: Encounter,
+            as: 'encounter',
+            include: [
+              {
+                model: Patient,
+                as: 'patient',
+                where: { id },
+              },
+            ],
+          },
+        ],
+      };
+    case ReferenceData.tableName:
+      return {
+        include: [
+          {
+            model: ImagingRequestArea,
+            as: 'areas',
+            include: [
+              {
+                model: ReferenceData,
+                as: 'area',
+                where: { id },
+              },
+            ],
+          },
+        ],
+      };
+    case ImagingAreaExternalCode.tableName:
+      return {
+        include: [
+          {
+            model: ImagingRequestArea,
+            as: 'areas',
+            include: [
+              {
+                model: ReferenceData,
+                as: 'area',
+                include: [
+                  {
+                    model: ImagingAreaExternalCode,
+                    as: 'imagingAreaExternalCode',
+                    where: { id },
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      };
+    case User.tableName:
+      return {
+        include: [
+          {
+            model: User,
+            as: 'requestedBy',
+          },
+          {
+            model: User,
+            as: 'completedBy',
+          },
+        ],
+        where: {
+          [Op.or]: [{ '$requestedBy.id$': id }, { '$completedBy.id$': id }],
+        },
+      };
+    default:
+      return null;
+  }
+}
--- packages/shared-src/src/models/fhir/ServiceRequest/index.js
+++ packages/shared-src/src/models/fhir/ServiceRequest/index.js
@@ -1,4 +1,4 @@
-import { DataTypes, Op } from 'sequelize';
+import { DataTypes } from 'sequelize';
 
 import { FhirResource } from '../Resource';
 
@@ -9,6 +9,7 @@ import {
 } from '../../../constants';
 import { getQueryOptions } from './getQueryOptions';
 import { getValues } from './getValues';
+import { getQueryToFindUpstreamIds } from './getQueryToFindUpstreamIds';
 
 export class FhirServiceRequest extends FhirResource {
   static init(options, models) {
@@ -66,169 +67,7 @@ export class FhirServiceRequest extends FhirResource {
   }
 
   static async queryToFindUpstreamIdsFromTable(table, id) {
-    const {
-      ImagingRequest,
-      ImagingRequestArea,
-      ImagingAreaExternalCode,
-      Encounter,
-      Facility,
-      Location,
-      LocationGroup,
-      Patient,
-      ReferenceData,
-      User,
-    } = this.sequelize.models;
-
-    switch (table) {
-      case ImagingRequest.tableName:
-        return { where: { id } };
-      case ImagingRequestArea.tableName:
-        return {
-          include: [
-            {
-              model: ImagingRequestArea,
-              as: 'areas',
-              where: { id },
-            },
-          ],
-        };
-      case Encounter.tableName:
-        return {
-          include: [
-            {
-              model: Encounter,
-              as: 'encounter',
-              where: { id },
-            },
-          ],
-        };
-      case Facility.tableName:
-        return {
-          include: [
-            {
-              model: Encounter,
-              as: 'encounter',
-              include: [
-                {
-                  model: Location,
-                  as: 'location',
-                  include: [
-                    {
-                      model: Facility,
-                      as: 'facility',
-                      where: { id },
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        };
-      case Location.tableName:
-        return {
-          include: [
-            {
-              model: Encounter,
-              as: 'encounter',
-              include: [
-                {
-                  model: Location,
-                  as: 'location',
-                  where: { id },
-                },
-              ],
-            },
-          ],
-        };
-      case LocationGroup.tableName:
-        return {
-          include: [
-            {
-              model: Encounter,
-              as: 'encounter',
-              include: [
-                {
-                  model: LocationGroup,
-                  as: 'locationGroup',
-                  where: { id },
-                },
-              ],
-            },
-          ],
-        };
-      case Patient.tableName:
-        return {
-          include: [
-            {
-              model: Encounter,
-              as: 'encounter',
-              include: [
-                {
-                  model: Patient,
-                  as: 'patient',
-                  where: { id },
-                },
-              ],
-            },
-          ],
-        };
-      case ReferenceData.tableName:
-        return {
-          include: [
-            {
-              model: ImagingRequestArea,
-              as: 'areas',
-              include: [
-                {
-                  model: ReferenceData,
-                  as: 'area',
-                  where: { id },
-                },
-              ],
-            },
-          ],
-        };
-      case ImagingAreaExternalCode.tableName:
-        return {
-          include: [
-            {
-              model: ImagingRequestArea,
-              as: 'areas',
-              include: [
-                {
-                  model: ReferenceData,
-                  as: 'area',
-                  include: [
-                    {
-                      model: ImagingAreaExternalCode,
-                      as: 'imagingAreaExternalCode',
-                      where: { id },
-                    },
-                  ],
-                },
-              ],
-            },
-          ],
-        };
-      case User.tableName:
-        return {
-          include: [
-            {
-              model: User,
-              as: 'requestedBy',
-            },
-            {
-              model: User,
-              as: 'completedBy',
-            },
-          ],
-          where: {
-            [Op.or]: [{ '$requestedBy.id$': id }, { '$completedBy.id$': id }],
-          },
-        };
-      default:
-        return null;
-    }
+    return getQueryToFindUpstreamIds(this.sequelize.models, table, id);
   }
 
   static searchParameters() {
