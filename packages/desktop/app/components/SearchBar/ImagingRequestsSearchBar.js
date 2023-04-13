3920658cdbe5805e4610c24410f8b5509b9211a6
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-04-13 16:00:12 +1200

    add suggesters for new fields

--- packages/desktop/app/components/SearchBar/ImagingRequestsSearchBar.js
+++ packages/desktop/app/components/SearchBar/ImagingRequestsSearchBar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IMAGING_REQUEST_STATUS_OPTIONS } from '../../constants';
-import { DateField, LocalisedField, SelectField } from '../Field';
+import { DateField, LocalisedField, SelectField, AutocompleteField } from '../Field';
 import { CustomisableSearchBar } from './CustomisableSearchBar';
 import { useLocalisation } from '../../contexts/Localisation';
+import { useSuggester } from '../../api';
 
 export const ImagingRequestsSearchBar = ({
   searchParameters,
@@ -12,6 +13,10 @@ export const ImagingRequestsSearchBar = ({
   const { getLocalisation } = useLocalisation();
   const imagingTypes = getLocalisation('imagingTypes') || {};
   const imagingPriorities = getLocalisation('imagingPriorities') || [];
+  const areaSuggester = useSuggester('locationGroup') || [];
+  const departmentSuggester = useSuggester('department') || [];
+
+  const [showAdvancedFields, setShowAdvancedFields] = useState(false);
 
   const imagingTypeOptions = Object.entries(imagingTypes).map(([key, val]) => ({
     label: val.label,
@@ -20,9 +25,34 @@ export const ImagingRequestsSearchBar = ({
 
   return (
     <CustomisableSearchBar
+      showExpandButton
+      isExpanded={showAdvancedFields}
+      setIsExpanded={setShowAdvancedFields}
       title="Search imaging requests"
       onSearch={setSearchParameters}
       initialValues={{ displayIdExact: true, ...searchParameters }}
+      hiddenFields={
+        <>
+          <LocalisedField
+            name="area"
+            defaultLabel="Area"
+            component={AutocompleteField}
+            suggester={areaSuggester}
+          />
+          <LocalisedField
+            name="department"
+            defaultLabel="Department"
+            component={AutocompleteField}
+            suggester={departmentSuggester}
+          />
+          <LocalisedField
+            name="completedDate"
+            defaultLabel="Completed"
+            saveDateAsString
+            component={DateField}
+          />
+        </>
+      }
     >
       <LocalisedField name="displayId" />
       <LocalisedField name="firstName" />
