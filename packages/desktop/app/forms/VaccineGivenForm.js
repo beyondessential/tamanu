ec4e1b40d593d7c7a697f4d064d596877323e008
georgekamar <gkamar97@gmail.com>
2023-04-15 20:13:51 +0300

    Revert "SAV-188: Desktop vaccine modal updates to include 'Circumstance' field"
    This reverts commit 2732d15e6487b19fd3511432fd4f7b182c23f39f.

--- packages/desktop/app/forms/VaccineGivenForm.js
+++ packages/desktop/app/forms/VaccineGivenForm.js
@@ -116,7 +116,7 @@ export const VaccineGivenForm = ({
         />
       ) : null}
 
-      <VaccineDateField label="Date given" required={!values.givenElsewhere} />
+      <VaccineDateField label="Date given" />
       <InjectionSiteField />
 
       <StyledDivider />
