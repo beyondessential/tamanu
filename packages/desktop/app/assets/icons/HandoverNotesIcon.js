d7e8d66440b5f64052411ffd0924bb227cc0c31e
georgekamar <gkamar97@gmail.com>
2023-04-18 17:21:45 +0300

    Trivial commit

--- packages/desktop/app/assets/icons/HandoverNotesIcon.js
+++ packages/desktop/app/assets/icons/HandoverNotesIcon.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export const HandoverNotesIcon = ({ color }) => (
   <svg width="17" height="20" viewBox="0 0 17 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
