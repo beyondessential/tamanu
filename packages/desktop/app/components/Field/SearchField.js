99e04b01524bae6aa4ca47185dbf4bcd96fa5984
Renato Koyama <renato.koyama@gmail.com>
2023-04-19 18:35:21 -0300

    TAN-2165: fix Search nhn placeholder to be uppercase"

--- packages/desktop/app/components/Field/SearchField.js
+++ packages/desktop/app/components/Field/SearchField.js
@@ -21,7 +21,8 @@ const StyledTextField = styled(TextField)`
   }
 `;
 
-export const SearchField = props => {
+export const SearchField = (props = {}) => {
+  const { label, keepLetterCase = false } = props;
   return (
     <StyledTextField
       InputProps={{
@@ -31,7 +32,7 @@ export const SearchField = props => {
           </Icon>
         ),
       }}
-      placeholder={props?.label ? `Search ${props?.label.toLowerCase()}` : ''}
+      placeholder={label ? `Search ${keepLetterCase ? label : label.toLowerCase()}` : ''}
       {...props}
     />
   );
