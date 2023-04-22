c3464e386d9c3a70049efe05fa9c242038f17d37
Alastair-L <lockealastair@gmail.com>
2023-04-23 00:25:55 +1000

    Delete unneeded imports from desktop

--- packages/desktop/app/forms/NewTemplateForm.js
+++ packages/desktop/app/forms/NewTemplateForm.js
@@ -1,6 +1,5 @@
 import React, { memo } from 'react';
 import * as yup from 'yup';
-import styled from 'styled-components';
 
 import { TEMPLATE_TYPES } from 'shared/constants';
 
@@ -8,7 +7,7 @@ import { Form, Field, TextField, MultilineTextField, SelectField } from '../comp
 import { FormGrid, SmallGridSpacer } from '../components/FormGrid';
 import { TEMPLATE_TYPE_OPTIONS } from '../constants';
 
-import { ConfirmCancelRow, ConfirmClearRow, ButtonRow } from '../components/ButtonRow';
+import { ConfirmClearRow } from '../components/ButtonRow';
 
 const TallMultilineTextField = props => (
   <MultilineTextField style={{ minHeight: '156px' }} {...props} />
