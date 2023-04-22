5697e2538c1cbb62684d75f07a0d9cdd8129373f
Alastair-L <lockealastair@gmail.com>
2023-04-22 15:31:05 +1000

    Changes from working on the UI

--- packages/shared-src/src/models/PatientLetterTemplate.js
+++ packages/shared-src/src/models/PatientLetterTemplate.js
@@ -14,7 +14,7 @@ export class PatientLetterTemplate extends Model {
           allowNull: false,
         },
         // TODO: Should this be nullable?
-        date_created: dateType('date_created', {
+        dateCreated: dateType('dateCreated', {
           defaultValue: getCurrentDateString,
         }),
         title: {
@@ -40,5 +40,9 @@ export class PatientLetterTemplate extends Model {
       foreignKey: 'createdById',
       as: 'createdBy',
     });
+  }
+
+  static getListReferenceAssociations() {
+    return ['createdBy'];
   }
 }
