157fc9d2ec167a76bf8a345c30f7b8d2c8d68ed6
Tom McLean <tom.a.mclean@gmail.com>
2023-04-27 17:20:51 +1000

    Put asset names in constants

--- packages/sync-server/app/admin/asset.js
+++ packages/sync-server/app/admin/asset.js
@@ -2,30 +2,14 @@ import * as yup from 'yup';
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 
+import { ASSET_NAMES, ASSET_MIME_TYPES } from 'shared/constants/importable';
+
 export const assetRoutes = express.Router();
 
-const ASSETS = {
-  LETTERHEAD_LOGO: 'letterhead-logo',
-
-  VACCINE_CERTIFICATE_WATERMARK: 'vaccine-certificate-watermark',
-  CERTIFICATE_BOTTOM_HALF_IMG: 'certificate-bottom-half-img',
-  DEATH_CERTIFICATE_BOTTOM_HALF_IMG: 'death-certificate-bottom-half-img',
-};
-export const ASSET_NAMES = Object.values(ASSETS);
-
-const MIME_TYPES = [
-  'image/png',
-  'image/svg',
-];
-
 const assetSchema = yup.object().shape({
-  name: yup.string().oneOf(ASSET_NAMES).required(),
-  type: yup.string().oneOf(MIME_TYPES).required(),
+  name: yup.string().oneOf(Object.values(ASSET_NAMES)).required(),
+  type: yup.string().oneOf(Object.values(ASSET_MIME_TYPES)).required(),
   data: yup.string().required(),
-});
-
-assetRoutes.get('/$', (req, res) => {
-  res.send(ASSET_NAMES);
 });
 
 assetRoutes.put('/:name', asyncHandler(async (req, res) => {
