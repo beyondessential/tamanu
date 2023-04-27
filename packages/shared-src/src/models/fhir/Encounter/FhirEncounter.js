747be6b833843796b342f3915c710b3a6feeec88
Félix Saparelli <felix@bes.au>
2023-04-27 16:05:24 +1200

    EPI-446: Encounter from upstream

--- packages/shared-src/src/models/fhir/Encounter/FhirEncounter.js
+++ packages/shared-src/src/models/fhir/Encounter/FhirEncounter.js
@@ -24,7 +24,7 @@ export class FhirEncounter extends FhirResource {
     );
 
     this.UpstreamModels = [models.Encounter];
-    this.upstreams = [models.Encounter];
+    this.upstreams = [models.Encounter, models.Discharge, models.Patient, models.Location];
   }
 
   static CAN_DO = new Set([
--- packages/shared-src/src/models/fhir/Encounter/getQueryToFindUpstreamIds.js
+++ packages/shared-src/src/models/fhir/Encounter/getQueryToFindUpstreamIds.js
@@ -1,9 +1,47 @@
-export function fromEncounters(models, table, id) {
-  const { Encounter } = models;
+export function fromEncounters(models, table, id, deletedRow) {
+  const { Encounter, Discharge, Patient, Location } = models;
 
   switch (table) {
     case Encounter.tableName:
       return { where: { id } };
+
+    case Discharge.tableName:
+      if (deletedRow) {
+        return { where: { id: deletedRow.encounter_id } };
+      }
+
+      return {
+        include: [
+          {
+            model: Discharge,
+            as: 'discharge',
+            where: { id },
+          },
+        ],
+      };
+
+    case Patient.tableName:
+      return {
+        include: [
+          {
+            model: Patient,
+            as: 'patient',
+            where: { id },
+          },
+        ],
+      };
+
+    case Location.tableName:
+      return {
+        include: [
+          {
+            model: Location,
+            as: 'location',
+            where: { id },
+          },
+        ],
+      };
+
     default:
       return null;
   }
