88e447f3fadcbb0346d33787295d762e828bc941
Tim Kennedy <t.m.knndy@gmail.com>
2023-05-02 17:07:31 +1000

    EPI-451: Add notes to FHIR (RISPACs) ServiceRequest

--- packages/shared-src/src/utils/notePages.js
+++ packages/shared-src/src/utils/notePages.js
@@ -1,10 +1,20 @@
 /*
-Returns the first note (Sequelize Model) with the specified
-noteType or undefined.
+Returns the first NotePage (Sequelize model instance) with the specified noteType, or undefined.
 
 notes: Array<SequelizeModel>
 noteType: string
 */
-export const getNoteWithType = (notes, noteType) => {
-  return notes.filter(note => note.noteType === noteType)[0];
+export const getNotePageWithType = (notes, noteType) => {
+  return getNotePagesWithType(notes, noteType)[0];
+};
+
+/*
+Returns all NotePages (Sequelize model instances) with the specified noteType, which may be an empty
+array.
+
+notes: Array<SequelizeModel>
+noteType: string
+*/
+export const getNotePagesWithType = (notes, noteType) => {
+  return notes.filter(note => note.noteType === noteType);
 };
