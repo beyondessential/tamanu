2f7f2933f24044a574ac7ac664d2fd2b126910ce
Renato Koyama <renato.koyama@gmail.com>
2023-05-03 13:21:27 -0300

    [NASS-708] Create endpoint for exporting Reference data (#3889)
    * [NASS-708] Create endpoint for exporting Reference data
    
    * NASS-708: Create tests for export reference data.
    
    * NASS-708: Enable import by using "Patient Field Def Category" as the tab name
    
    * Update packages/sync-server/__tests__/exporters/referenceDataExporter.test.js
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    
    * Update packages/sync-server/__tests__/exporters/referenceDataExporter.test.js
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    
    * NASS-708: Create a test for importing from an exported file.
    
    * fix: exporter tests
    
    * fix: lint issues
    
    ---------
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>

--- packages/shared-src/src/constants/importable.js
+++ packages/shared-src/src/constants/importable.js
@@ -58,6 +58,7 @@ export const GENERAL_IMPORTABLE_DATA_TYPES = [
   'triageReason',
   'ultrasoundImagingArea',
   'user',
+  'vaccine',
   'vascularStudyImagingArea',
   'village',
   'xRayImagingArea',
@@ -106,6 +107,7 @@ export const REFERENCE_TYPES = {
   REFERRAL_SOURCE: 'referralSource',
   ARRIVAL_MODE: 'arrivalMode',
   VACCINE_NOT_GIVEN_REASON: 'vaccineNotGivenReason',
+  ADDITIONAL_INVOICE_LINE: 'additionalInvoiceLine',
   ...IMAGING_AREA_TYPES,
 };
 
