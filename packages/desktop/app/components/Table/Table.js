9a0ae09c42e43b2c4f5fc1c596f5c164d027fe5b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-05-05 15:07:33 +1200

    Always show sortable icons

--- packages/desktop/app/components/Table/Table.js
+++ packages/desktop/app/components/Table/Table.js
@@ -38,6 +38,10 @@ const CellErrorMessage = styled.div`
   cursor: pointer;
 `;
 
+const TransparentExpandMore = styled(ExpandMore)`
+  opacity: 50% !important;
+`;
+
 const CellError = React.memo(({ error }) => {
   const showMessage = useCallback(() => {
     // eslint-disable-next-line no-console
@@ -240,10 +244,10 @@ class TableComponent extends React.Component {
 
       const headerElement = sortable ? (
         <TableSortLabel
-          active={orderBy === key}
-          direction={order}
+          active
+          direction={orderBy === key ? order : 'desc'}
           onClick={() => onChangeOrderBy(key)}
-          IconComponent={ExpandMore}
+          IconComponent={orderBy === key ? ExpandMore : TransparentExpandMore}
         >
           {title || getLocalisation(`fields.${key}.shortLabel`) || key}
         </TableSortLabel>
