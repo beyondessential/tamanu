31295baa9bacc1faab86c3fb474689b8d07400ef
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-05-09 17:10:25 -0400

    Move SQL formatting
    Co-authored-by: Félix Saparelli <felix@beyondessential.com.au>

--- packages/shared-src/src/migrations/1683602526329-addMissingFhirTriggers.js
+++ packages/shared-src/src/migrations/1683602526329-addMissingFhirTriggers.js
@@ -20,8 +20,8 @@ export async function up(query) {
   for (const table of TABLES_FOR_REFRESH) {
     await query.sequelize.query(`
       CREATE TRIGGER fhir_refresh
-      AFTER INSERT OR UPDATE OR DELETE ON ${table} FOR EACH ROW
-      EXECUTE FUNCTION fhir.refresh_trigger()
+      AFTER INSERT OR UPDATE OR DELETE ON ${table}
+      FOR EACH ROW EXECUTE FUNCTION fhir.refresh_trigger()
     `);
   }
 }
