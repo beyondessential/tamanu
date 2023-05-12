5b38849b036aa4bf0db24c89ed45168b1dd9e6ff
Biao Li <lbit123@outlook.com>
2023-05-12 15:52:16 +1000

    Refactor MultipleLabRequestsPrintout

--- packages/desktop/app/components/PatientPrinting/printouts/MultipleLabRequestsPrintout.js
+++ packages/desktop/app/components/PatientPrinting/printouts/MultipleLabRequestsPrintout.js
@@ -11,56 +11,54 @@ import { NotesPagesSection } from './reusable/NotesPagesSection';
 import { Divider } from './reusable/Divider';
 import { DateFacilitySection } from './reusable/DateFacilitySection';
 
-const columns = [
-  {
-    key: 'displayId',
-    title: 'Test ID',
-    widthProportion: 2,
-  },
-  {
-    key: 'date',
-    title: 'Request date',
-    accessor: ({ requestedDate }) => <DateDisplay date={requestedDate} />,
-    widthProportion: 2,
-  },
-  {
-    key: 'requestedBy',
-    title: 'Requesting clinician',
-    accessor: ({ requestedBy }) => requestedBy?.displayName,
-    widthProportion: 2,
-  },
-  {
-    key: 'sampleTime',
-    title: 'Sample time',
-    accessor: ({ sampleTime }) => <DateDisplay date={sampleTime} showTime />,
-    style: { textAlign: 'center' },
-    widthProportion: 2,
-  },
-  {
-    key: 'priority',
-    title: 'Priority',
-    accessor: ({ priority }) => priority?.name || '',
-    widthProportion: 2,
-  },
-  {
-    key: 'category',
-    title: 'Test category',
-    accessor: ({ category }) => category?.name || '',
-    widthProportion: 2,
-  },
-  {
-    key: 'testType',
-    title: 'Test type',
-    accessor: ({ tests }) => tests?.map(test => test.labTestType?.name).join(', ') || '',
-    widthProportion: 3,
-  },
-];
-
 export const MultipleLabRequestsPrintout = React.memo(
   ({ patient, labRequests, encounter, village, additionalData, certificateData }) => {
     const { title, subTitle, logo } = certificateData;
     const idsAndNotePages = labRequests.map(lr => [lr.displayId, lr.notePages]);
-
+    const columns = [
+      {
+        key: 'displayId',
+        title: 'Test ID',
+        widthProportion: 2,
+      },
+      {
+        key: 'date',
+        title: 'Request date',
+        accessor: ({ requestedDate }) => <DateDisplay date={requestedDate} />,
+        widthProportion: 2,
+      },
+      {
+        key: 'requestedBy',
+        title: 'Requesting clinician',
+        accessor: ({ requestedBy }) => requestedBy?.displayName,
+        widthProportion: 2,
+      },
+      {
+        key: 'sampleTime',
+        title: 'Sample time',
+        accessor: ({ sampleTime }) => <DateDisplay date={sampleTime} showTime />,
+        style: { textAlign: 'center' },
+        widthProportion: 2,
+      },
+      {
+        key: 'priority',
+        title: 'Priority',
+        accessor: ({ priority }) => priority?.name || '',
+        widthProportion: 2,
+      },
+      {
+        key: 'category',
+        title: 'Test category',
+        accessor: ({ category }) => category?.name || '',
+        widthProportion: 2,
+      },
+      {
+        key: 'testType',
+        title: 'Test type',
+        accessor: ({ tests }) => tests?.map(test => test.labTestType?.name).join(', ') || '',
+        widthProportion: 3,
+      },
+    ];
     return (
       <CertificateWrapper>
         <PrintLetterhead title={title} subTitle={subTitle} logoSrc={logo} pageTitle="Lab Request" />
