d11b03fd7313aabc02dec715872f35b74224b524
chris-bes <63621318+chris-bes@users.noreply.github.com>
2023-05-15 05:43:02 +0700

    SAV-184: Handle empty date in edit vaccine form mobile (#4052)
    * sav-184: handled empty date in edit vaccine form
    
    * sav-184: hide Edit details if vaccine is GIVEN

--- packages/mobile/App/ui/components/VaccineCard/NotGivenFields.tsx
+++ packages/mobile/App/ui/components/VaccineCard/NotGivenFields.tsx
@@ -12,16 +12,20 @@ export const NotGivenFields: FC<VaccineDataProps> = ({ administeredVaccine }) =>
     height={screenPercentageToDP(20.04, Orientation.Height)}
     background={theme.colors.WHITE}
   >
-    <RowField label="Date recorded" value={formatStringDate(administeredVaccine.date, DateFormats.DDMMYY)} />
+    <RowField
+      label="Date recorded"
+      value={
+        administeredVaccine.date
+          ? formatStringDate(administeredVaccine.date, DateFormats.DDMMYY)
+          : null
+      }
+    />
     <RowField label="Schedule" value={administeredVaccine.scheduledVaccine?.schedule} />
     <RowField label="Reason" value={administeredVaccine.notGivenReason?.name} />
     <RowField label="Area" value={administeredVaccine.location?.locationGroup?.name} />
     <RowField label="Location" value={administeredVaccine.location?.name} />
     <RowField label="Department" value={administeredVaccine.department?.name} />
     <RowField label="Supervising clinician" value={administeredVaccine.givenBy} />
-    <RowField
-      label="Recorded by"
-      value={administeredVaccine.encounter.examiner.displayName}
-    />
+    <RowField label="Recorded by" value={administeredVaccine.encounter.examiner.displayName} />
   </StyledView>
 );
