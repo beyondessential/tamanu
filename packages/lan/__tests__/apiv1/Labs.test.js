3953eb0060c162b26c979500846c566a5de36e17
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-05-16 15:28:30 +1200

    SAV-168: Fix flaky lab test (#4076)
    * always give a status to lab test
    
    * Randomly select a valud lab status

--- packages/lan/__tests__/apiv1/Labs.test.js
+++ packages/lan/__tests__/apiv1/Labs.test.js
@@ -1,12 +1,20 @@
-import {
-  LAB_TEST_STATUSES,
-  LAB_REQUEST_STATUSES,
-} from 'shared/constants';
+import { LAB_TEST_STATUSES, LAB_REQUEST_STATUSES } from 'shared/constants';
 import config from 'config';
+import Chance from 'chance';
 import { createDummyPatient, createDummyEncounter, randomLabRequest } from 'shared/demoData';
 import { fake } from 'shared/test-helpers/fake';
 
 import { createTestContext } from '../utilities';
+
+const chance = new Chance();
+const VALID_LAB_REQUEST_STATUSES = [
+  LAB_REQUEST_STATUSES.RECEPTION_PENDING,
+  LAB_REQUEST_STATUSES.RESULTS_PENDING,
+  LAB_REQUEST_STATUSES.TO_BE_VERIFIED,
+  LAB_REQUEST_STATUSES.VERIFIED,
+  LAB_REQUEST_STATUSES.SAMPLE_NOT_COLLECTED,
+  LAB_REQUEST_STATUSES.PUBLISHED,
+];
 
 describe('Labs', () => {
   let patientId = null;
@@ -133,6 +141,7 @@ describe('Labs', () => {
         ...fake(models.LabRequest),
         encounterId: encounter.id,
         requestedById: app.user.id,
+        status: chance.pickone(VALID_LAB_REQUEST_STATUSES),
       });
     };
 
