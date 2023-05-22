bf60fa6449442ff94191214e32ef36f9aa07139b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-05-22 14:38:43 +1200

    Revert "stop from being able to scroll just the header"
    This reverts commit 9dfb466d8ee7bdc1e9767399d3ca203048f0548a.

--- packages/mobile/App/ui/components/VaccinesTable/index.tsx
+++ packages/mobile/App/ui/components/VaccinesTable/index.tsx
@@ -102,7 +102,7 @@ export const VaccinesTable = ({
     <ScrollView bounces={false} stickyHeaderIndices={[0]}>
       <StyledView flexDirection="row">
         <VaccinesTableTitle />
-        <ScrollView ref={scrollViewRef} horizontal scrollEnabled={false}>
+        <ScrollView ref={scrollViewRef} horizontal>
           {columns.map((column: any) => (
             <StyledView key={`${column}`}>
               {vaccineTableHeader.accessor(column, onPressItem)}
