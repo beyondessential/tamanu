4f9e2eabeec2d97619fdfa3367a0b4c609992049
Alastair-L <lockealastair@gmail.com>
2023-05-22 15:55:15 +1000

    Improve table styling

--- packages/desktop/app/views/administration/templates/TemplateList.js
+++ packages/desktop/app/views/administration/templates/TemplateList.js
@@ -13,28 +13,35 @@ export const TemplateList = React.memo(props => (
         key: 'type',
         title: 'Type',
         accessor: () => 'Patient Letter',
+        sortable: false,
       },
       {
         key: 'name',
         title: 'Template name',
+        sortable: false,
       },
       {
         key: 'title',
         title: 'Title',
+        sortable: false,
       },
       {
         key: 'dateCreated',
         title: 'Created on',
         accessor: ({ dateCreated }) => <DateDisplay date={dateCreated} />,
+        sortable: false,
       },
       {
         key: 'createdBy',
         title: 'Created by',
         accessor: getDisplayName,
+        sortable: false,
       },
       {
         key: 'body',
         title: 'Contents',
+        maxWidth: 200,
+        sortable: false,
       },
     ]}
     noDataMessage="No templates found"
