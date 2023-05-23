f456b633fef63be4445b5829f7006f1412d98fd2
Chris Tran <chris@beyondessential.com.au>
2023-05-23 16:30:55 +1000

    nass-769: filtered by `ShareLock` only

--- packages/shared-src/src/sync/getSyncTicksOfPendingEdits.js
+++ packages/shared-src/src/sync/getSyncTicksOfPendingEdits.js
@@ -1,9 +1,15 @@
 export const getSyncTicksOfPendingEdits = async (sequelize, syncTick) => {
-  // Get the keys (ie: syncTicks) of all the pending locks
+  // Get the keys (ie: syncTicks) of all the in-flight transaction locks of previously pending edits.
+  // Since advisory locks are global, and:
+  // - in-flight transaction locks are 'ShareLock'
+  // - sync snapshot locks which are `ExclusiveLock`
+  // => Only select for in-flight transaction locks by filtering for `ShareLock`
+  // to avoid clashing with the sync snapshot locks
   const [results] = await sequelize.query(
     `
       SELECT objid AS tick FROM pg_locks
       WHERE locktype = 'advisory'
+      AND mode = 'ShareLock' -- filter by
       AND objid < :syncTick;
     `,
     {
