2feded192e2f55558276b4f348145a03d9efdb44
Alastair-L <lockealastair@gmail.com>
2023-05-23 13:46:56 +1000

    Add PatientLetterModal

---
+++ packages/desktop/app/components/PatientLetterModal.js
@@ -0,0 +1,134 @@
+import React, { useState, useCallback } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Box } from '@material-ui/core';
+
+import { DOCUMENT_SOURCES } from 'shared/constants';
+import { getCurrentDateTimeString } from 'shared/utils/dateTime';
+import { useApi } from '../api';
+import { Modal, ModalLoader } from './Modal';
+import { PatientLetterForm } from '../forms/PatientLetterForm';
+// import { PatientLetterForm } from '../forms/PatientLetterForm';
+import { Colors, SEX_VALUE_INDEX } from '../constants';
+import { DateDisplay } from '.';
+
+const Card = styled(Box)`
+  background: white;
+  border-radius: 5px;
+  border: 1px solid ${Colors.outline};
+  padding: 20px 10px;
+  display: flex;
+  align-items: flex-start;
+  margin-top: 10px;
+`;
+
+const Column = styled.div`
+  flex: 1;
+  padding-left: 20px;
+
+  :first-of-type {
+    border-right: 1px solid ${Colors.outline};
+  }
+`;
+
+const CardCell = styled.div`
+  font-size: 14px;
+  line-height: 18px;
+  color: ${props => props.theme.palette.text.tertiary};
+  margin-bottom: 20px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+const CardLabel = styled.div`
+  margin-right: 5px;
+`;
+
+const CardValue = styled(CardLabel)`
+  font-weight: 500;
+  color: ${props => props.theme.palette.text.secondary};
+`;
+
+const CardItem = ({ label, value, ...props }) => (
+  <CardCell {...props}>
+    <CardLabel>{label}</CardLabel>
+    <CardValue>{value}</CardValue>
+  </CardCell>
+);
+
+const PatientDetails = ({ patient }) => (
+  <Card mb={4}>
+    <Column>
+      <CardItem label="Patient ID" value={patient?.displayId} />
+      <CardItem label="First name" value={patient?.firstName} />
+      <CardItem label="Last name" value={patient?.lastName} />
+    </Column>
+    <Column>
+      <CardItem label="DOB" value={<DateDisplay date={patient?.dateOfBirth} />} />
+      <CardItem label="Sex" value={SEX_VALUE_INDEX[patient?.sex]?.label} />
+    </Column>
+  </Card>
+);
+
+export const PatientLetterModal = React.memo(
+  ({ open, onClose, endpoint, refreshTable, patient, setSelectedDocument }) => {
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const api = useApi();
+
+    const onSubmit = useCallback(
+      async ({ submissionType, ...data }) => {
+        setIsSubmitting(true);
+
+        const document = await api.post(`${endpoint}/createPatientLetter`, {
+          patientLetterData: {
+            todo: 'TODO: will pass through in the next PR',
+          },
+          type: 'application/pdf',
+          source: DOCUMENT_SOURCES.PATIENT_LETTER,
+          name: data.title,
+          clinicianId: data.clinicianId,
+          documentCreatedAt: getCurrentDateTimeString(),
+          documentUploadedAt: getCurrentDateTimeString(),
+        });
+        refreshTable();
+        setIsSubmitting(false);
+
+        if (submissionType === 'FinaliseAndPrint') {
+          onClose();
+          setSelectedDocument(document);
+        } else {
+          onClose();
+        }
+      },
+      [onClose, api, endpoint, refreshTable, setSelectedDocument],
+    );
+
+    return (
+      <Modal width="sm" title="Patient letter" open={open} onClose={onClose}>
+        {isSubmitting ? (
+          <ModalLoader loadingText="Please wait while we create your patient letter" />
+        ) : (
+          <>
+            <PatientDetails patient={patient} />
+            <PatientLetterForm onSubmit={onSubmit} onCancel={onClose} />
+          </>
+        )}
+      </Modal>
+    );
+  },
+);
+
+PatientLetterModal.propTypes = {
+  open: PropTypes.bool,
+  onClose: PropTypes.func.isRequired,
+  endpoint: PropTypes.string.isRequired,
+  refreshTable: PropTypes.func.isRequired,
+  patient: PropTypes.object.isRequired,
+  setSelectedDocument: PropTypes.func.isRequired,
+};
+
+PatientLetterModal.defaultProps = {
+  open: false,
+};
