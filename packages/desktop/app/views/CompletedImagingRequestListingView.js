ee685e252ad96b78ea85014a18bed5cf66c8c551
Daniel Nash <dannash200@gmail.com>
2023-05-30 16:05:13 +1200

    NASS-682: Fix issue with imaging route listing view causing some unexpected component behavior

--- packages/desktop/app/views/CompletedImagingRequestListingView.js
+++ packages/desktop/app/views/CompletedImagingRequestListingView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { IMAGING_REQUEST_STATUS_TYPES } from 'shared-src/src/constants/statuses';
 import {
   TopBar,
   PageContainer,
@@ -8,13 +9,13 @@ import {
 } from '../components';
 import { ImagingRequestsTable } from '../components/ImagingRequestsTable';
 
-export const ImagingRequestListingView = () => (
+export const CompletedImagingRequestListingView = () => (
   <PageContainer>
-    <TopBar title="Imaging requests" />
+    <TopBar title="Completed imaging requests" />
     <ContentPane>
       <SearchTableTitle>Imaging request search</SearchTableTitle>
-      <ImagingRequestsSearchBar />
-      <ImagingRequestsTable />
+      <ImagingRequestsSearchBar status={IMAGING_REQUEST_STATUS_TYPES.COMPLETED} />
+      <ImagingRequestsTable status={IMAGING_REQUEST_STATUS_TYPES.COMPLETED} />
     </ContentPane>
   </PageContainer>
 );
