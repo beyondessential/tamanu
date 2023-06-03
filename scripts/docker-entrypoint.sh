8f0fe7c273df56b1352959fc770c950bfb9948f2
Félix Saparelli <felix@beyondessential.com.au>
2023-06-03 15:25:29 +1200

    TAV-540: Linux-based production container images (#3989)

---
+++ scripts/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### This expects to be run in the production docker container in /Dockerfile.
+# While not strictly required, it is prefered to run with an init process.
+
+if [[ -d /config ]]; then
+  # copy the config file(s) from the configurator into the expected place
+  cp -v /config/* ./config/
+fi
+
+if [[ "$1" == "healthcheck" ]]; then
+  # used as healthcheck command in ECS
+  set -eo pipefail
+
+  # rest of the arguments are those used to start the container
+  # that can be used to determine what kind of healthcheck to run
+  shift
+
+  case "$1" in
+    serve)
+      curl http://localhost:3000 | jq '.index == true' | grep -F true
+      ;;
+    # TODO: healthcheck for task runner (check process is running?)
+    *)
+      echo "Unknown healthcheck type: $1"
+      exit 0 # assume success
+      ;;
+  esac
+elif which "$1" >/dev/null; then
+  # if the first arg is a binary, run it
+  # that provides a convenient way to run arbitrary commands in the container
+  exec $*
+else
+  # otherwise, run the app
+  exec node dist/app.bundle.js $*
+fi
