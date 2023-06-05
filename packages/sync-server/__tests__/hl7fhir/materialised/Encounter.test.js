b035f31df4cf8c26a6b84223fb947dae42b3b432
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-05 19:37:31 -0400

    Set a deterministic encounterType to avoid intermittent errors

--- packages/sync-server/__tests__/hl7fhir/materialised/Encounter.test.js
+++ packages/sync-server/__tests__/hl7fhir/materialised/Encounter.test.js
@@ -280,7 +280,7 @@ describe(`Materialised FHIR - Encounter`, () => {
 
     describe('filters', () => {
       it('by lastUpdated=gt', async () => {
-        const [newEncounter, newMat] = await makeEncounter();
+        const [newEncounter, newMat] = await makeEncounter({ encounterType: 'emergency' });
         newMat.update({ lastUpdated: addDays(new Date(), 5) });
         const response = await app.get(
           `/v1/integration/${INTEGRATION_ROUTE}/Encounter?_lastUpdated=gt${encodeURIComponent(
