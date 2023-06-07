1324a9edc1d015188d465933bad4059ec8fa471b
Biao Li <lbit123@outlook.com>
2023-06-07 14:57:31 +1000

    Add TooltipContent

---
+++ packages/desktop/app/components/Charts/components/TooltipContent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Colors } from '../../../constants';
+import { formatShortest, formatTime } from '../../DateDisplay';
+import { CustomDot } from './CustomDot';
+
+const FlexColumn = styled.div`
+  flex-direction: column;
+  display: flex;
+`;
+
+const FlexRow = styled.div`
+  flex-direction: row;
+  display: flex;
+`;
+
+const ValueWrapper = styled(FlexRow)`
+  align-items: baseline;
+  gap: 5px;
+  font-size: 11px;
+`;
+
+const Wrapper = styled(FlexColumn)`
+  gap: 5px;
+  padding: 9px;
+  font-size: 11px;
+`;
+
+const TimeText = styled.span`
+  color: ${Colors.midText};
+`;
+
+export const TooltipContent = ({ label, value, description, dotColor }) => {
+  return (
+    <Wrapper>
+      <span>
+        {formatShortest(label)} <TimeText>{formatTime(label)}</TimeText>
+      </span>
+      <ValueWrapper>
+        <CustomDot payload={{ dotColor }} />
+        <FlexColumn>
+          <span>{value}</span>
+          <span>{description}</span>
+        </FlexColumn>
+      </ValueWrapper>
+    </Wrapper>
+  );
+};
