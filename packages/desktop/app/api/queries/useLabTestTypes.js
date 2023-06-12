bf3977852d9ec95f447f945519adb4581e28190d
Renato Koyama <renato.koyama@gmail.com>
2023-06-12 18:39:04 -0300

    chore: removing unused file

--- packages/desktop/app/api/queries/useLabTestTypes.js
+++
@@ -1,7 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { useApi } from '../useApi';
-
-export const useLabTestTypes = () => {
-  const api = useApi();
-  return useQuery(['labTestType'], () => api.get('labTestType'));
-};
