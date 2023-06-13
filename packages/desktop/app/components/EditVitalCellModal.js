703ecfd9e831a22ceb40849e15584c870ef0f726
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-13 17:17:48 -0400

    Replace custom styling in favor of FormGrid

--- packages/desktop/app/components/EditVitalCellModal.js
+++ packages/desktop/app/components/EditVitalCellModal.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import * as yup from 'yup';
-import styled from 'styled-components';
 import { Modal } from './Modal';
 import { ConfirmCancelRow } from './ButtonRow';
 import { SelectField, Form, Field } from './Field';
-
-const ModalBody = styled.div`
-  margin-top: 30px;
-
-  .MuiTypography-root {
-    top: -10px;
-    margin-bottom: 30px;
-  }
-`;
-
-const Wrapper = styled.div`
-  margin: 30px auto 50px;
-  max-width: 350px;
-`;
+import { FormGrid } from './FormGrid';
 
 export const EditVitalCellModal = ({ value, onConfirm, onClose, open }) => (
   <Modal width="sm" title={value} onClose={onClose} open={open}>
@@ -30,18 +16,16 @@ export const EditVitalCellModal = ({ value, onConfirm, onClose, open }) => (
         // value: formValue,
         submitForm,
       }) => (
-        <ModalBody>
-          <Wrapper>
-            <Field
-              required
-              component={SelectField}
-              label="Reason for cancellation"
-              name="reasonForCancellation"
-              // options={options}
-            />
-          </Wrapper>
+        <FormGrid columns={4}>
+          <Field
+            required
+            component={SelectField}
+            label="Reason for cancellation"
+            name="reasonForCancellation"
+            // options={options}
+          />
           <ConfirmCancelRow onCancel={onClose} onConfirm={submitForm} confirmText="Save" />
-        </ModalBody>
+        </FormGrid>
       )}
     />
   </Modal>
