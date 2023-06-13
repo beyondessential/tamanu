f197190537052a7a74beb5abd881b2688561fb94
Tim Kennedy <t.m.knndy@gmail.com>
2023-06-13 10:55:29 +1000

    Update copy

--- packages/desktop/app/views/reports/ReportGeneratorForm.js
+++ packages/desktop/app/views/reports/ReportGeneratorForm.js
@@ -192,7 +192,7 @@ export const ReportGeneratorForm = () => {
       onSubmit={submitRequestReport}
       validationSchema={Yup.object().shape({
         reportId: Yup.string().required(
-          "Report id is required. A report must be selected in the dropdown; just entering a report name will not work. If you can't see a specific report, your user account may need additional permissions configured by an administrator.",
+          "Report id is required. A report must be selected from the dropdown; just entering a report name will not work. If you can't see a specific report, please contact your system administrator.",
         ),
         ...parameters.reduce(
           (schema, field) => ({
