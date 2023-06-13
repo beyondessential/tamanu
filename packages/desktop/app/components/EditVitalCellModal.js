e47784927606cc09a748e7f508bba66d6dfb3512
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-13 17:30:11 -0400

    Add missing fields as a mock

--- packages/desktop/app/components/EditVitalCellModal.js
+++ packages/desktop/app/components/EditVitalCellModal.js
@@ -2,8 +2,9 @@ import React from 'react';
 import * as yup from 'yup';
 import { Modal } from './Modal';
 import { ConfirmCancelRow } from './ButtonRow';
-import { SelectField, Form, Field } from './Field';
+import { SelectField, Form, Field, TextField } from './Field';
 import { FormGrid } from './FormGrid';
+import { FormSeparatorLine } from './FormSeparatorLine';
 
 export const EditVitalCellModal = ({ value, onConfirm, onClose, open }) => (
   <Modal width="sm" title={value} onClose={onClose} open={open}>
@@ -17,12 +18,23 @@ export const EditVitalCellModal = ({ value, onConfirm, onClose, open }) => (
         submitForm,
       }) => (
         <FormGrid columns={4}>
+          <Field component={TextField} label="Main" name="value" />
           <Field
             required
             component={SelectField}
             label="Reason for cancellation"
             name="reasonForCancellation"
             // options={options}
+            style={{ gridColumn: '1 / 4' }}
+          />
+          <FormSeparatorLine />
+          <Field
+            name="history"
+            label="History"
+            component={TextField}
+            multiline
+            style={{ gridColumn: '1 / -1' }}
+            rows={6}
           />
           <ConfirmCancelRow onCancel={onClose} onConfirm={submitForm} confirmText="Save" />
         </FormGrid>
