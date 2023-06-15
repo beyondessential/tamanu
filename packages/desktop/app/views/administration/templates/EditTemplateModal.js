ebf72dc118d2b6dd7177951f532fcdee9ceec9cc
Alastair <lockealastair@gmail.com>
2023-06-15 13:40:09 +1000

    Avoid unnecessarily wrapping EditTemplateModal component
    Co-authored-by: Tom McLean <tom.a.mclean@gmail.com>

--- packages/desktop/app/views/administration/templates/EditTemplateModal.js
+++ packages/desktop/app/views/administration/templates/EditTemplateModal.js
@@ -2,15 +2,13 @@ import React from 'react';
 import { Modal } from '../../../components';
 import { EditPatientLetterTemplateForm } from './EditPatientLetterTemplateForm';
 
-export const EditTemplateModal = ({ template, onClose, open, onSubmit, onDelete }) => {
-  return (
-    <Modal title="Patient Letter" open={open} onClose={onClose}>
-      <EditPatientLetterTemplateForm
-        onSubmit={onSubmit}
-        editedObject={template}
-        onDelete={onDelete}
-        onClose={onClose}
-      />
-    </Modal>
-  );
-};
+export const EditTemplateModal = ({ template, onClose, open, onSubmit, onDelete }) => (
+  <Modal title="Patient Letter" open={open} onClose={onClose}>
+    <EditPatientLetterTemplateForm
+      onSubmit={onSubmit}
+      editedObject={template}
+      onDelete={onDelete}
+      onClose={onClose}
+    />
+  </Modal>
+);
