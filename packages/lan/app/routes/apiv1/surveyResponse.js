ba9a62253ad82594208e8a2891e4999e7a5ebcd7
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-18 17:33:44 -0400

    Update calculated fields accordingly

--- packages/lan/app/routes/apiv1/surveyResponse.js
+++ packages/lan/app/routes/apiv1/surveyResponse.js
@@ -3,6 +3,8 @@ import asyncHandler from 'express-async-handler';
 import { Op } from 'sequelize';
 import { NotFoundError } from 'shared/errors';
 import { SURVEY_TYPES, PROGRAM_DATA_ELEMENT_TYPES } from 'shared/constants';
+import { runCalculations } from 'shared/utils/calculations';
+import { getStringValue } from 'shared/utils/fields';
 
 export const surveyResponse = express.Router();
 
@@ -116,7 +118,14 @@ surveyResponse.put(
   '/vital/:id',
   asyncHandler(async (req, res) => {
     const { db, models, user, params } = req;
-    const { SurveyResponseAnswer, SurveyResponse, Survey, VitalLog, ProgramDataElement } = models;
+    const {
+      SurveyResponseAnswer,
+      SurveyResponse,
+      Survey,
+      VitalLog,
+      ProgramDataElement,
+      SurveyScreenComponent,
+    } = models;
     const { id } = params;
     req.checkPermission('write', 'Vitals');
     const answerObject = await SurveyResponseAnswer.findByPk(id, {
@@ -143,6 +152,11 @@ surveyResponse.put(
     });
     if (!answerObject) throw new NotFoundError();
 
+    const screenComponents = await SurveyScreenComponent.getComponentsForSurvey(
+      answerObject.surveyResponse.survey.id,
+    );
+    const calculatedScreenComponents = screenComponents.filter(c => c.calculation);
+
     await db.transaction(async () => {
       const { previousValue = '', newValue = '', reasonForChange } = req.body;
       await VitalLog.create({
@@ -153,6 +167,49 @@ surveyResponse.put(
         answerId: id,
       });
       await answerObject.update({ body: newValue });
+
+      // Check if any calculated question is affected by this change
+      let answers;
+      let calculatedValues;
+      for (const component of calculatedScreenComponents) {
+        if (component.calculation.includes(answerObject.ProgramDataElement.code) === false) {
+          continue;
+        }
+
+        // Grab answers only once
+        if (!calculatedValues) {
+          const response = await SurveyResponse.findByPk(answerObject.surveyResponse.id, {
+            include: [
+              {
+                required: true,
+                model: SurveyResponseAnswer,
+                as: 'answers',
+              },
+            ],
+          });
+          answers = response.answers;
+          const values = answers.map(answer => ({ [answer.dataElementId]: answer.body }));
+          calculatedValues = runCalculations(screenComponents, values);
+        }
+
+        // Modify survey response answer for calculated value and create log
+        const calculatedAnswer = answers.find(
+          answer => answer.dataElementId === component.dataElement.id,
+        );
+        if (!calculatedAnswer) continue;
+
+        const newCalculatedValue = getStringValue(calculatedValues[component.dataElement.id]) ?? '';
+        await VitalLog.create({
+          reasonForChange,
+          previousValue: calculatedAnswer.body,
+          newValue: newCalculatedValue,
+          recordedById: user.id,
+          answerId: calculatedAnswer.id,
+        });
+        await calculatedAnswer.update({ body: newCalculatedValue });
+      }
+
+      // TODO: Figure out if we should update the whole survey response for sync purposes
     });
 
     res.send(answerObject);
