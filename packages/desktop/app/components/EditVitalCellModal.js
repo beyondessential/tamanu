6d7a3ee7596bd4c9e3035d4751c289e1403f7a89
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-20 16:01:33 -0400

    Add delete entry button

--- packages/desktop/app/components/EditVitalCellModal.js
+++ packages/desktop/app/components/EditVitalCellModal.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import * as yup from 'yup';
+import styled from 'styled-components';
+import { Box, IconButton, Typography } from '@material-ui/core';
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import { Modal } from './Modal';
 import { ConfirmCancelRow } from './ButtonRow';
 import { SelectField, Form, Field, TextField } from './Field';
 import { FormGrid } from './FormGrid';
 import { FormSeparatorLine } from './FormSeparatorLine';
 import { formatShortest, formatTime } from './DateDisplay';
+
+const Text = styled(Typography)`
+  font-size: 14px;
+  line-height: 24px;
+  font-weight: 500;
+  text-decoration: underline;
+`;
+
+const DeleteEntryButton = ({ disabled, onClick }) => (
+  <Box display="flex" alignSelf="end">
+    <IconButton color="primary" edge="start" disabled={disabled} onClick={onClick} disableRipple>
+      <DeleteOutlineIcon fontSize="small" />
+      <Text>Delete entry</Text>
+    </IconButton>
+  </Box>
+);
 
 export const EditVitalCellModal = ({ cell, onConfirm, onClose }) => {
   const vitalLabel = cell?.vitalLabel;
@@ -26,6 +45,7 @@ export const EditVitalCellModal = ({ cell, onConfirm, onClose }) => {
         }) => (
           <FormGrid columns={4}>
             <Field component={TextField} label={vitalLabel} name="value" />
+            <DeleteEntryButton />
             <Field
               required
               component={SelectField}
