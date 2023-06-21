238b74bd0868e0dd9db5a08f916ecd731297e00e
Félix Saparelli <felix@bes.au>
2023-06-22 10:35:53 +1200

    TAV-679: Package MSIs for Aspen

--- scripts/build_desktop_update_config.mjs
+++ scripts/build_desktop_update_config.mjs
@@ -16,13 +16,34 @@ pkg.build.publish.path = buildFolder;
 // Determine installation method:
 // - omitted in this list: install to appdata
 // - included: install to Program Files
+//   - nsis: produce an exe, with auto-updates
+//   - msi: produce an msi, without auto-updates
 // Add new deployments here as required.
-const programFilesDeployments = ['aspen-medical-fiji', 'aspen-demo', 'tuvalu'];
-const isProgramFiles = programFilesDeployments.includes(deployment);
-pkg.build.nsis.perMachine = isProgramFiles;
+const programFilesDeployments = new Map([
+  ['aspen-medical-fiji', 'msi'],
+  ['aspen-demo', 'msi'],
+  ['tuvalu', 'nsis'],
+]);
+
+let installTarget;
+switch (programFilesDeployments.get(deployment)) {
+  case 'nsis':
+    pkg.build.nsis.perMachine = true;
+    installTarget = 'Program Files';
+    break;
+  case 'msi':
+    installTarget = 'Program Files';
+    delete pkg.build.nsis;
+    pkg.build.msi = {
+      oneClick: false,
+      perMachine: true,
+    };
+    break;
+  default:
+    pkg.build.nsis.perMachine = false;
+    installTarget = 'appdata';
+}
 
 // Write back to desktop package.json to be read by build task
 writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
-
-const installTarget = isProgramFiles ? 'Program Files' : 'appdata';
 console.log(`Publishing to ${buildFolder} (will install to ${installTarget})`);
