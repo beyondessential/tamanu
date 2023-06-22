9ffff51719a5e689ae7c541fc327fe6b160c536b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-22 18:07:49 -0400

    Accept models parameter instead of importing directly from shared

--- packages/shared/src/utils/fhir/resources.js
+++ packages/shared/src/utils/fhir/resources.js
@@ -1,11 +1,10 @@
-import * as resources from '../../models/fhir';
-
 /**
+ * @param {Model[]} models
  * @param  {...string} interactions
  * @returns {FhirResource[]}
  */
-export function resourcesThatCanDo(...interactions) {
-  return Object.values(resources).filter(Resource =>
+export function resourcesThatCanDo(models, ...interactions) {
+  return Object.values(models).filter(Resource =>
     interactions.every(interaction => Resource.CAN_DO?.has(interaction)),
   );
 }
