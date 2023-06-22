4f70e5296d6fb311081842537145f0374f8c754e
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-06-23 09:51:20 +1200

    store result search context

--- packages/desktop/app/contexts/PatientLabResults.js
+++ packages/desktop/app/contexts/PatientLabResults.js
@@ -1,13 +1,9 @@
-import React, { useContext, createContext, useState, useCallback } from 'react';
+import React, { useContext, createContext, useState } from 'react';
 
 const PatientLabResultsContext = createContext({});
 
 export const usePatientLabResultsContext = () => {
   const { searchParameters, setSearchParameters } = useContext(PatientLabResultsContext);
-
-  const setSearchParametersData = useCallback(() => {
-    setSearchParameters(searchParameters);
-  }, [searchParameters, setSearchParameters]);
 
   return { searchParameters, setSearchParameters };
 };
@@ -15,15 +11,11 @@ export const usePatientLabResultsContext = () => {
 export const PatientLabResultsProvider = ({ children }) => {
   const [searchParameters, setSearchParameters] = useState({});
 
-  const setSearchParametersData = data => {
-    setSearchParameters(data);
-  };
-
   return (
     <PatientLabResultsContext.Provider
       value={{
         searchParameters,
-        setSearchParametersData,
+        setSearchParameters,
       }}
     >
       {children}
