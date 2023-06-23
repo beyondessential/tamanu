5b106dfc8ffc0a768cf7e2be1905e2068aaddd65
Tim Kennedy <t.m.knndy@gmail.com>
2023-06-23 15:43:35 +1000

    Fix unnecessary and outdated comments

--- packages/shared/src/reports/utilities/checkReportModulePermissions.js
+++ packages/shared/src/reports/utilities/checkReportModulePermissions.js
@@ -4,14 +4,12 @@ import { canRunStaticReport } from './canRunStaticReport';
 export async function checkReportModulePermissions(req, reportModule, reportId) {
   const { ReportDefinitionVersion } = req.models;
   if (reportModule instanceof ReportDefinitionVersion) {
-    // for db-defined reports, check permission for specific report
     const definition = await reportModule.getReportDefinition();
     req.checkPermission('run', definition);
   } else {
-    // for static reports, check EITHER defined permission OR explicit run permission
     if (!canRunStaticReport(req.ability, reportId)) {
       throw new ForbiddenError('User does not have permission to run the report');
     }
-    req.flagPermissionChecked(); // flag because we're checking for either of two permissions
+    req.flagPermissionChecked();
   }
 }
