01a97c696d51e9e6b684c3e85726cab2e78b5c58
Alastair-L <lockealastair@gmail.com>
2023-06-23 14:16:02 +1200

    Rename "Reason for encounter"

--- packages/desktop/app/components/PatientHistory.js
+++ packages/desktop/app/components/PatientHistory.js
@@ -22,7 +22,12 @@ const columns = [
   { key: 'startDate', title: 'Date', accessor: getDate },
   { key: 'encounterType', title: 'Type', accessor: getType, sortable: false },
   { key: 'facilityName', title: 'Facility', accessor: getFacility },
-  { key: 'reasonForEncounter', title: 'Description', accessor: getDescription, sortable: false },
+  {
+    key: 'reasonForEncounter',
+    title: 'Reason for encounter',
+    accessor: getDescription,
+    sortable: false,
+  },
 ];
 
 const SyncWarning = styled.p`
