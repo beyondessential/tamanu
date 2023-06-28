ae2e8f9a33f9c525a426654ab373db8aa51cc23d
Biao Li <lbit123@outlook.com>
2023-06-28 15:15:10 +1000

    Refactor Dot logic

--- packages/desktop/app/components/Charts/components/CustomDot.js
+++ packages/desktop/app/components/Charts/components/CustomDot.js
@@ -4,9 +4,11 @@ import { Colors } from '../../../constants';
 export const CustomDot = props => {
   // cx, cy is the position of the current dot
   const { cx, cy, payload, size = 7, active } = props;
-  if (!cx || !cy || !payload) {
+  if (!payload) {
     return null;
   }
+  const x = cx ? cx - (size / 2 + 0.5) : 0;
+  const y = cy ? cy - (size / 2 + 0.5) : 0;
 
   const color = payload.dotColor;
 
@@ -20,17 +22,11 @@ export const CustomDot = props => {
     //    │     │   ⬤  │
     //    │     └───────┼
     //    v
-    <svg
-      x={cx - (size / 2 + 0.5)}
-      y={cy - (size / 2 + 0.5)}
-      width={size + 1}
-      height={size + 1}
-      viewBox={`0 0 ${size + 1} ${size + 1}`}
-    >
+    <svg x={x} y={y} width={size + 1} height={size + 1} viewBox={`0 0 ${size + 1} ${size + 1}`}>
       <circle
         cx={size / 2 + 0.5}
         cy={size / 2 + 0.5}
-        r={size / 2}
+        r={size / 2 - 0.5}
         fill={active ? color : Colors.white}
         stroke={color}
       />
