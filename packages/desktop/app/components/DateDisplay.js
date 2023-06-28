c451b08832435fa71a344d3a753e6dda501b9154
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-06-28 15:54:54 -0400

    Stop exporting formatShort

--- packages/desktop/app/components/DateDisplay.js
+++ packages/desktop/app/components/DateDisplay.js
@@ -28,7 +28,7 @@ const intlFormatDate = (date, formatOptions, fallback = 'Unknown') => {
 export const formatShortest = date =>
   intlFormatDate(date, { month: '2-digit', day: '2-digit', year: '2-digit' }, '--/--'); // 12/04/20
 
-export const formatShort = date =>
+const formatShort = date =>
   intlFormatDate(date, { day: '2-digit', month: '2-digit', year: 'numeric' }, '--/--/----'); // 12/04/2020
 
 export const formatTime = date =>
