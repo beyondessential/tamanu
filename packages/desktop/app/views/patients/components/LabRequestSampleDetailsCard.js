a31535aec1bb230593070a9fb63489a19868d168
Renato Koyama <renato.koyama@gmail.com>
2023-06-29 18:25:13 -0300

    [NASS-757] Fix value when field is empty and date time label

--- packages/desktop/app/views/patients/components/LabRequestSampleDetailsCard.js
+++ packages/desktop/app/views/patients/components/LabRequestSampleDetailsCard.js
@@ -5,11 +5,11 @@ import { DateDisplay } from '../../../components';
 export const LabRequestSampleDetailsCard = ({ labRequest }) => (
   <InfoCard>
     <InfoCardItem
-      label="Sample date & time"
+      label="Date & time"
       value={<DateDisplay date={labRequest.sampleTime} showTime />}
     />
-    <InfoCardItem label="Collected by" value={labRequest.collectedBy?.displayName} />
-    <InfoCardItem label="Specimen type" value={labRequest.specimenType?.name} />
-    <InfoCardItem label="Site" value={labRequest.site?.name} />
+    <InfoCardItem label="Collected by" value={labRequest.collectedBy?.displayName || '-'} />
+    <InfoCardItem label="Specimen type" value={labRequest.specimenType?.name || '-'} />
+    <InfoCardItem label="Site" value={labRequest.site?.name || '-'} />
   </InfoCard>
 );
