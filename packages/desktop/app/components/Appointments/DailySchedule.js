c5618de52c5ef319dd20114af6b8fb05c996e35c
Biao Li <lbit123@outlook.com>
2023-06-29 14:52:21 +1000

    Use 100% width if it is smaller than the content

--- packages/desktop/app/components/Appointments/DailySchedule.js
+++ packages/desktop/app/components/Appointments/DailySchedule.js
@@ -9,11 +9,16 @@ const Column = ({ header, appointments, onAppointmentUpdated }) => {
   const appointmentsByStartTime = [...appointments].sort((a, b) => a.startTime - b.startTime);
   // If header's length is larger than 14 characters, split it into two lines. Width expands if needed.
   const hasSpace = header.includes(' ');
-  const width = header.length > 14 && hasSpace ? `${(header.length * 15) / 2}px` : '100%';
+  let width = '100%';
+  let midWidth = null;
+  if (header.length > 14 && hasSpace) {
+    width = `${(header.length * 15) / 2}px`; // shrink width to make text becomes two lines
+    midWidth = '100%'; // expand width if it is smaller than the appointment content below it
+  }
 
   return (
     <>
-      <ColumnHeader className="location" $width={width}>
+      <ColumnHeader className="location" $width={width} $midWidth={midWidth}>
         {header}
       </ColumnHeader>
       <ColumnBody className="appointments">
@@ -92,7 +97,8 @@ const ColumnHeader = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
-  width: ${({ $width }) => $width}};
+  width: ${({ $width }) => $width};
+  min-width: ${({ $midWidth }) => $midWidth};
   border: 1px solid ${Colors.outline};
   border-right: none;
   font-weight: bold;
