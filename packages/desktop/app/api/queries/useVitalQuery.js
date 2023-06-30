6b8fb9c2433af4f58d7add798a0ba5ac807a9873
Biao Li <lbit123@outlook.com>
2023-06-30 15:57:00 +1000

    Remove useVitalQueries

--- packages/desktop/app/api/queries/useVitalQuery.js
+++ packages/desktop/app/api/queries/useVitalQuery.js
@@ -1,13 +1,5 @@
-import { useQueries, useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 import { useApi, isErrorUnknownAllow404s } from '../index';
-
-const queryFn = (api, encounterId, vitalDataElementId, startDate, endDate) => {
-  return api.get(
-    `encounter/${encounterId}/vitals/${vitalDataElementId}`,
-    { startDate, endDate },
-    { isErrorUnknown: isErrorUnknownAllow404s },
-  );
-};
 
 const transformVitalDataToChartData = vitalQuery => {
   const { data: vitalDataAndCount = {} } = vitalQuery;
@@ -26,7 +18,13 @@ export const useVitalQuery = (encounterId, vitalDataElementId, startDate, endDat
 
   const vitalQuery = useQuery(
     ['encounterVital', encounterId, vitalDataElementId, startDate, endDate],
-    () => queryFn(api, encounterId, vitalDataElementId, startDate, endDate),
+    () => {
+      return api.get(
+        `encounter/${encounterId}/vitals/${vitalDataElementId}`,
+        { startDate, endDate },
+        { isErrorUnknown: isErrorUnknownAllow404s },
+      );
+    },
   );
 
   const chartData = transformVitalDataToChartData(vitalQuery);
@@ -34,27 +32,5 @@ export const useVitalQuery = (encounterId, vitalDataElementId, startDate, endDat
   return {
     ...vitalQuery,
     data: chartData,
-  };
-};
-
-export const useVitalQueries = (encounterId, vitalDataElementIds, startDate, endDate) => {
-  const api = useApi();
-
-  const vitalQueries = useQueries({
-    queries: vitalDataElementIds.map(vitalDataElementId => ({
-      queryKey: ['encounterVital', encounterId, vitalDataElementId, startDate, endDate],
-      queryFn: () => queryFn(api, encounterId, vitalDataElementId, startDate, endDate),
-    })),
-  });
-
-  const isLoading = vitalQueries.some(query => query.isLoading);
-  const chartsData = vitalQueries.map(vitalQuery => {
-    const chartData = transformVitalDataToChartData(vitalQuery);
-    return chartData;
-  });
-
-  return {
-    isLoading,
-    data: chartsData,
   };
 };
