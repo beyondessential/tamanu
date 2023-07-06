b040057468cdc68b7d787ecd74f295455801d92f
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-07-06 19:47:54 -0400

    Revert "Hacky solution to include test server - needs to be reverted before merging"
    This reverts commit 8fc08829fb7c8547e1fe32bc136f42fbf9ae0d4c.

--- packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
+++ packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
@@ -22,10 +22,8 @@ const fetchServers = async (): Promise<SelectOption[]> => {
 
   const response = await fetch(`${META_SERVER}/servers`);
   const servers: Server[] = await response.json();
-  const testBranchServer = { name: 'EPI-492', host: 'https://central.epi-492-add-ability-to-configure.internal.tamanu.io' };
-  const hackySolution = [...servers, testBranchServer];
 
-  return hackySolution.map(s => ({ label: s.name, value: s.host }));
+  return servers.map(s => ({ label: s.name, value: s.host }));
 };
 
 export const ServerSelector = ({ onChange, label, value, error }): ReactElement => {
