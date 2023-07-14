dc5515d49f427bca1ac92c1d5f950e3232eb6baf
Biao Li <lbit123@outlook.com>
2023-07-14 18:03:13 +1000

    Apply getNormalRangeByAge to normalRange in visualisationConfigs

--- packages/desktop/app/api/queries/useVisualisationConfigs.js
+++ packages/desktop/app/api/queries/useVisualisationConfigs.js
@@ -1,9 +1,18 @@
+import { BLOOD_PRESSURE, LINE, bloodPressureChartKeys } from '../../components/Charts/constants';
+import { useEncounter } from '../../contexts/Encounter';
+import { getConfigObject, getNormalRangeByAge } from '../../utils';
+import { usePatientData } from './usePatientData';
 import { useVitalsSurvey } from './useVitalsSurvey';
 
 export const useVisualisationConfigs = () => {
+  const encounterQuery = useEncounter();
+  const { encounter } = encounterQuery;
+  const patientQuery = usePatientData(encounter.patientId);
   const vitalsSurveyQuery = useVitalsSurvey();
 
   const { data: surveyData, isLoading: isVitalsSurveyLoading } = vitalsSurveyQuery;
+  const { data: patient } = patientQuery;
+
   let visualisationConfigs = [];
   if (!isVitalsSurveyLoading && surveyData && patient) {
     visualisationConfigs = surveyData.components.map(({ id, dataElement, config }) => {
@@ -12,6 +21,12 @@ export const useVisualisationConfigs = () => {
 
       const visualisationConfig = getConfigObject(id, dataElement.visualisationConfig);
       const { yAxis = {} } = visualisationConfig;
+      const { normalRange } = yAxis;
+      if (normalRange && Array.isArray(normalRange)) {
+        const normalRangeByAge = getNormalRangeByAge(yAxis, patient);
+        visualisationConfig.yAxis.normalRange = normalRangeByAge;
+      }
+
       return {
         chartType: isBloodPressureChart ? BLOOD_PRESSURE : LINE,
         key: dataElement.id,
