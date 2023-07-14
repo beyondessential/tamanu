51edc79ec3886bbfd9395fe8573947d410d037d3
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2023-07-14 00:16:57 -0400

    EPI-552: Fix misc issues found in testing (#4403)
    * Fix copy in all subcommands, copying work evidence
    
    * Create a proper buildSyncFilter for VitalLog
    
    * Allow null reference to recorder - lets cover for historical data issues
    
    * Use explicit joins!
    
    Co-authored-by: Tim Kennedy <tim@beyondessential.com.au>
    
    ---------
    
    Co-authored-by: Tim Kennedy <tim@beyondessential.com.au>

--- packages/shared/src/models/VitalLog.js
+++ packages/shared/src/models/VitalLog.js
@@ -48,4 +48,42 @@ export class VitalLog extends Model {
       as: 'recordedBy',
     });
   }
+
+  static buildSyncFilter(patientIds, sessionConfig) {
+    if (patientIds.length === 0) {
+      return null;
+    }
+
+    // manually construct "joins", as survey_response and survey_response_answer
+    // joins use a non-conventional join column names
+    const joins = `
+      INNER JOIN survey_response_answers ON vital_logs.answer_id = survey_response_answers.id
+      INNER JOIN survey_responses ON survey_response_answers.response_id = survey_responses.id
+      INNER JOIN encounters ON survey_responses.encounter_id = encounters.id
+    `;
+
+    // remove answers to sensitive surveys from mobile
+    // this is copied over from SurveyResponseAnswer model, although VitalLog
+    // is currently not used in mobile, better to get this sooner than later
+    if (sessionConfig.isMobile) {
+      return `
+        ${joins}
+        INNER JOIN surveys ON survey_responses.survey_id = surveys.id
+        WHERE
+          encounters.patient_id in (:patientIds)
+        AND
+          surveys.is_sensitive = FALSE
+        AND
+          ${this.tableName}.updated_at_sync_tick > :since
+      `;
+    }
+
+    return `
+      ${joins}
+      WHERE
+        encounters.patient_id in (:patientIds)
+      AND
+        ${this.tableName}.updated_at_sync_tick > :since
+    `;
+  }
 }
