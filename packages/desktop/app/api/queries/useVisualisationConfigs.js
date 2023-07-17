f4b09a82dab8b3152221bd51539bd30c8cfdc07c
Biao Li <lbit123@outlook.com>
2023-07-17 20:05:57 +1000

    Assign normalRange to validationCriteria

--- packages/desktop/app/api/queries/useVisualisationConfigs.js
+++ packages/desktop/app/api/queries/useVisualisationConfigs.js
@@ -15,29 +15,35 @@ export const useVisualisationConfigs = () => {
 
   let visualisationConfigs = [];
   if (!isVitalsSurveyLoading && surveyData && patient) {
-    visualisationConfigs = surveyData.components.map(({ id, dataElement, config }) => {
-      const hasVitalChart = !!dataElement.visualisationConfig;
-      const isBloodPressureChart = bloodPressureChartKeys.includes(dataElement.id);
+    visualisationConfigs = surveyData.components.map(
+      ({ id, dataElement, config, validationCriteria: validationCriteriaString }) => {
+        const hasVitalChart = !!dataElement.visualisationConfig;
+        const isBloodPressureChart = bloodPressureChartKeys.includes(dataElement.id);
+        const visualisationConfig = getConfigObject(id, dataElement.visualisationConfig);
+        const validationCriteria = getConfigObject(id, validationCriteriaString);
 
-      const visualisationConfig = getConfigObject(id, dataElement.visualisationConfig);
-      const { yAxis = {} } = visualisationConfig;
-      const { normalRange } = yAxis;
-      if (normalRange && Array.isArray(normalRange)) {
-        const normalRangeByAge = getNormalRangeByAge(yAxis, patient);
-        visualisationConfig.yAxis.normalRange = normalRangeByAge;
-      }
+        // TODO: Remove this once charts can read normal range from the validationCriteria
+        if (hasVitalChart) {
+          const { normalRange } = validationCriteria;
+          visualisationConfig.yAxis.normalRange = normalRange;
+          if (normalRange && Array.isArray(normalRange)) {
+            const normalRangeByAge = getNormalRangeByAge(validationCriteria, patient);
+            visualisationConfig.yAxis.normalRange = normalRangeByAge;
+          }
+        }
 
-      return {
-        chartType: isBloodPressureChart ? BLOOD_PRESSURE : LINE,
-        key: dataElement.id,
-        name: bloodPressureChartKeys.includes(dataElement.id)
-          ? 'Blood pressure (mm Hg)'
-          : dataElement.name,
-        hasVitalChart,
-        config: getConfigObject(id, config),
-        ...visualisationConfig,
-      };
-    });
+        return {
+          chartType: isBloodPressureChart ? BLOOD_PRESSURE : LINE,
+          key: dataElement.id,
+          name: bloodPressureChartKeys.includes(dataElement.id)
+            ? 'Blood pressure (mm Hg)'
+            : dataElement.name,
+          hasVitalChart,
+          config: getConfigObject(id, config),
+          ...visualisationConfig,
+        };
+      },
+    );
   }
 
   return visualisationConfigs;
