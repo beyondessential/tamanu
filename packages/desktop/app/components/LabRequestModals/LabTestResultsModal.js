34ebeb8cea08eae545a46e60ff71abb3b9fa2dde
Daniel Nash <dannash200@gmail.com>
2023-07-18 11:03:29 +1200

    nass-788: pick the right values

--- packages/desktop/app/components/LabRequestModals/LabTestResultsModal.js
+++ packages/desktop/app/components/LabRequestModals/LabTestResultsModal.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useMemo } from 'react';
 import { Box } from '@material-ui/core';
 import styled from 'styled-components';
-import { keyBy, omit } from 'lodash';
+import { keyBy, omit, pick } from 'lodash';
 import { Skeleton } from '@material-ui/lab';
 import { Modal } from '../Modal';
 import { Heading4, SmallBodyText } from '../Typography';
@@ -29,7 +29,18 @@ const StyledTableFormFields = styled(TableFormFields)`
   }
 `;
 
-const AUTOFILL_FIELD_NAMES = ['completedDate', 'labTestMethodId'];
+const LAB_TEST_PROPERTIES = {
+  ID: 'id',
+  COMPLETED_DATE: 'completedDate',
+  LAB_TEST_METHOD_ID: 'labTestMethodId',
+  RESULT: 'result',
+  VERIFICATION: 'verification',
+};
+
+const AUTOFILL_FIELD_NAMES = [
+  LAB_TEST_PROPERTIES.COMPLETED_DATE,
+  LAB_TEST_PROPERTIES.LAB_TEST_METHOD_ID,
+];
 
 const getColumns = (count, onChangeResult) => {
   // Generate tab index for vertical tabbing through the table
@@ -38,11 +49,11 @@ const getColumns = (count, onChangeResult) => {
     {
       key: 'labTestType',
       title: 'Test type',
-      width: '100px',
+      width: '120px',
       accessor: row => row.labTestType.name,
     },
     {
-      key: 'result',
+      key: LAB_TEST_PROPERTIES.RESULT,
       title: 'Result',
       accessor: (row, i) => {
         const { resultType, options } = row.labTestType;
@@ -50,7 +61,7 @@ const getColumns = (count, onChangeResult) => {
           <LabResultAccessorField
             resultType={resultType}
             options={options}
-            name="result"
+            name={LAB_TEST_PROPERTIES.RESULT}
             onChange={e => onChangeResult(e.target.value, row.id)}
             id={row.id}
             tabIndex={tabIndex(0, i)}
@@ -65,39 +76,39 @@ const getColumns = (count, onChangeResult) => {
       accessor: row => row.labTestType.unit,
     },
     {
-      key: 'method',
+      key: LAB_TEST_PROPERTIES.LAB_TEST_METHOD_ID,
       title: 'Method',
       accessor: (row, i) => (
         <AccessorField
           id={row.id}
           endpoint="labTestMethod"
-          name="labTestMethodId"
+          name={LAB_TEST_PROPERTIES.LAB_TEST_METHOD_ID}
           component={SuggesterSelectField}
           tabIndex={tabIndex(1, i)}
         />
       ),
     },
     {
-      key: 'verification',
+      key: LAB_TEST_PROPERTIES.VERIFICATION,
       title: 'Verification',
       accessor: (row, i) => (
         <AccessorField
           id={row.id}
           component={TextField}
-          name="verification"
+          name={LAB_TEST_PROPERTIES.VERIFICATION}
           tabIndex={tabIndex(2, i)}
         />
       ),
     },
     {
-      key: 'completedDate',
+      key: LAB_TEST_PROPERTIES.COMPLETED_DATE,
       title: 'Completed',
       width: '260px',
       accessor: (row, i) => (
         <AccessorField
           id={row.id}
           component={DateTimeField}
-          name="completedDate"
+          name={LAB_TEST_PROPERTIES.COMPLETED_DATE}
           tabIndex={tabIndex(3, i)}
         />
       ),
@@ -114,8 +125,8 @@ const ResultsForm = ({ labTestResults, values, setFieldValue }) => {
    * 2. The user has not already entered a value for this field in the current row
    */
   const onChangeResult = useCallback(
-    (value, resultId) => {
-      const rowValues = values[resultId];
+    (value, labTestId) => {
+      const rowValues = values[labTestId];
       if (rowValues?.result || !value) return;
       AUTOFILL_FIELD_NAMES.forEach(name => {
         // Get unique values for this field across all rows
@@ -125,7 +136,7 @@ const ResultsForm = ({ labTestResults, values, setFieldValue }) => {
         );
         if (unique.length !== 1 || rowValues?.[name]) return;
         // Prefill the field with the unique value
-        setFieldValue(`${resultId}.${name}`, unique[0]);
+        setFieldValue(`${labTestId}.${name}`, unique[0]);
       });
     },
     [values, setFieldValue],
@@ -150,48 +161,58 @@ const ResultsForm = ({ labTestResults, values, setFieldValue }) => {
 };
 
 const ResultsFormSkeleton = () => (
-  <Box>
-    <Box display="flex" justifyContent="space-between" marginBottom="20px">
-      <div>
-        <Skeleton variant="text" width={124} style={{ fontSize: 20, marginBottom: 4 }} />
-        <Skeleton variant="text" width={270} style={{ fontSize: 12 }} />
-      </div>
-      <div>
-        <Skeleton variant="text" width={70} style={{ fontSize: 18 }} />
-        <Skeleton variant="rect" width={241} height={40} style={{ borderRadius: 4 }} />
-      </div>
+  <>
+    <Box>
+      <Box display="flex" justifyContent="space-between" marginBottom="20px">
+        <div>
+          <Skeleton variant="text" width={124} style={{ fontSize: 20, marginBottom: 4 }} />
+          <Skeleton variant="text" width={270} style={{ fontSize: 12 }} />
+        </div>
+        <div>
+          <Skeleton variant="text" width={70} style={{ fontSize: 18 }} />
+          <Skeleton variant="rect" width={241} height={40} style={{ borderRadius: 4 }} />
+        </div>
+      </Box>
+      <Skeleton variant="rect" height={257} style={{ borderRadius: 4, marginBottom: 30 }} />
     </Box>
-    <Skeleton variant="rect" height={257} style={{ borderRadius: 4, marginBottom: 30 }} />
-  </Box>
+  </>
 );
 
 export const LabTestResultsModal = ({ labRequest, onClose, open }) => {
-  const { data: labTestResults, isLoading, error, isError } = useLabTestResultsQuery(labRequest.id);
+  const {
+    data: labTestResults = { data: [], count: 0 },
+    isLoading,
+    error,
+    isError,
+  } = useLabTestResultsQuery(labRequest.id);
   const { displayId } = labRequest;
-  // TODO select only needed data
+
+  // Select editable values to prefill the form on edit
   const initialData = useMemo(
     () =>
       keyBy(
-        labTestResults?.data.map(data => omit(data, ['labTestType'])),
-        'id',
+        labTestResults?.data.map(data => pick(data, Object.values(LAB_TEST_PROPERTIES))),
+        LAB_TEST_PROPERTIES.ID,
       ),
     [labTestResults],
   );
+
   return (
     <Modal width="lg" title={`Enter results | Test ID ${displayId}`} open={open} onClose={onClose}>
-      {isLoading ? (
-        <ResultsFormSkeleton />
-      ) : (
-        labTestResults && (
-          <Form
-            initialValues={initialData}
-            render={props => (
+      <Form
+        initialValues={initialData}
+        enableReinitialize
+        render={props => (
+          <>
+            {isLoading ? (
+              <ResultsFormSkeleton />
+            ) : (
               <ResultsForm labTestResults={labTestResults} onClose={onClose} {...props} />
             )}
-          />
-        )
-      )}
-      <ModalActionRow onConfirm={onClose} onCancel={onClose} confirmDisabled={isLoading} />
+            <ModalActionRow onConfirm={onClose} onCancel={onClose} confirmDisabled={isLoading} />
+          </>
+        )}
+      />
     </Modal>
   );
 };
