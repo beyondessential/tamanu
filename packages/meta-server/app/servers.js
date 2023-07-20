d4fe12c74b0656c9c7b97842be8706e2f3620f6c
Nick Williams <62525113+nick-beyondessential@users.noreply.github.com>
2023-07-20 15:07:43 +1000

    Update servers.js fiji demo

--- packages/meta-server/app/servers.js
+++ packages/meta-server/app/servers.js
@@ -29,7 +29,7 @@ const servers = [
   { name: 'Demo', type: 'demo', host: 'https://sync-demo.tamanu.io' },
   { name: 'Demo 2', type: 'demo', host: 'https://central-demo2.internal.tamanu.io' },
   { name: 'Demo (Aspen)', type: 'demo', host: 'https://central-demo.aspen-dev.tamanu.io' },
-  { name: 'Demo (Fiji)', type: 'demo', host: 'https://sync-demo.tamanu-fiji.org' },
+  { name: 'Demo (Fiji)', type: 'demo', host: 'https://central-demo.fiji-dev.tamanu.io' },
   { name: 'Demo (Fiji Tourism)', type: 'demo', host: 'https://sync.demo-tourism.tamanu-fiji.org' },
   { name: 'Demo (Kiribati)', type: 'demo', host: 'https://central-demo.kiribati-dev.tamanu.io' },
   { name: 'Demo (Nauru)', type: 'demo', host: 'https://central-demo.nauru-dev.tamanu.io' },
