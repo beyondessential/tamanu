8d60540546a725b9b8f0cecb2a852dd3cdaad532
Biao Li <lbit123@outlook.com>
2023-07-20 11:19:58 +1000

    Make chart width to be dynamic

--- packages/desktop/app/components/Charts/LineChart.js
+++ packages/desktop/app/components/Charts/LineChart.js
@@ -9,6 +9,7 @@ import {
   Line,
   LineChart as LineChartComponent,
   Customized,
+  ResponsiveContainer,
 } from 'recharts';
 import { DISPLAY_VALUE_KEY, getMeasureData } from './helpers/getMeasureData';
 import { CustomisedXAxisTick, CustomisedYAxisTick } from './components/CustomisedTick';
@@ -29,65 +30,67 @@ export const LineChart = props => {
   const DotComponent = useInwardArrowVector ? InwardArrowVectorDot : CustomDot;
 
   return (
-    <LineChartComponent width={1856} height={height} data={measureData} margin={margin}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis
-        interval={0}
-        tick={<CustomisedXAxisTick />}
-        dataKey="timestamp"
-        tickLine={false}
-        ticks={xAxisTicks}
-        type="number"
-        scale="time"
-        domain={[xAxisTicks[0], xAxisTicks[xAxisTicks.length - 1]]}
-      />
-      <YAxis
-        width={Y_AXIS_WIDTH}
-        domain={[yAxisConfigs.graphRange.min, yAxisConfigs.graphRange.max]}
-        interval={0}
-        tick={<CustomisedYAxisTick />}
-        ticks={yAxisTicks}
-        tickLine={false}
-        allowDataOverflow
-      />
-      {yAxisConfigs.normalRange.min !== yAxisConfigs.graphRange.min && (
-        <ReferenceLine y={yAxisConfigs.normalRange.min} stroke={Colors.alert} />
-      )}
-      {yAxisConfigs.normalRange.max !== yAxisConfigs.graphRange.max && (
-        <ReferenceLine y={yAxisConfigs.normalRange.max} stroke={Colors.alert} />
-      )}
-      <ReferenceArea
-        shape={shapeProps => (
-          <ReferenceBands
-            {...shapeProps}
-            rangesToHighlight={[
-              [yAxisConfigs.normalRange.min, yAxisConfigs.graphRange.min],
-              [yAxisConfigs.normalRange.max, yAxisConfigs.graphRange.max],
-            ]}
-            yAxisConfigs={yAxisConfigs}
-          />
+    <ResponsiveContainer width="100%" height={height}>
+      <LineChartComponent data={measureData} margin={margin}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis
+          interval={0}
+          tick={<CustomisedXAxisTick />}
+          dataKey="timestamp"
+          tickLine={false}
+          ticks={xAxisTicks}
+          type="number"
+          scale="time"
+          domain={[xAxisTicks[0], xAxisTicks[xAxisTicks.length - 1]]}
+        />
+        <YAxis
+          width={Y_AXIS_WIDTH}
+          domain={[yAxisConfigs.graphRange.min, yAxisConfigs.graphRange.max]}
+          interval={0}
+          tick={<CustomisedYAxisTick />}
+          ticks={yAxisTicks}
+          tickLine={false}
+          allowDataOverflow
+        />
+        {yAxisConfigs.normalRange.min !== yAxisConfigs.graphRange.min && (
+          <ReferenceLine y={yAxisConfigs.normalRange.min} stroke={Colors.alert} />
         )}
-      />
-      <Tooltip
-        wrapperStyle={{
-          backgroundColor: Colors.white,
-          boxShadow: `0px 4px 20px rgba(0, 0, 0, 0.1)`,
-          borderRadius: '5px',
-        }}
-        content={<CustomTooltip useInwardArrowVector={useInwardArrowVector} />}
-      />
-      <Line
-        type="linear"
-        dataKey={DISPLAY_VALUE_KEY}
-        stroke={Colors.blue}
-        strokeWidth={2}
-        dot={<DotComponent tableHeight={tableHeight} />}
-        activeDot={<DotComponent active tableHeight={tableHeight} />}
-        isAnimationActive={false}
-      />
-      {(chartData.length === 0 || isLoading) && (
-        <Customized component={<NoDataStateScreen isLoading={isLoading} />} />
-      )}
-    </LineChartComponent>
+        {yAxisConfigs.normalRange.max !== yAxisConfigs.graphRange.max && (
+          <ReferenceLine y={yAxisConfigs.normalRange.max} stroke={Colors.alert} />
+        )}
+        <ReferenceArea
+          shape={shapeProps => (
+            <ReferenceBands
+              {...shapeProps}
+              rangesToHighlight={[
+                [yAxisConfigs.normalRange.min, yAxisConfigs.graphRange.min],
+                [yAxisConfigs.normalRange.max, yAxisConfigs.graphRange.max],
+              ]}
+              yAxisConfigs={yAxisConfigs}
+            />
+          )}
+        />
+        <Tooltip
+          wrapperStyle={{
+            backgroundColor: Colors.white,
+            boxShadow: `0px 4px 20px rgba(0, 0, 0, 0.1)`,
+            borderRadius: '5px',
+          }}
+          content={<CustomTooltip useInwardArrowVector={useInwardArrowVector} />}
+        />
+        <Line
+          type="linear"
+          dataKey={DISPLAY_VALUE_KEY}
+          stroke={Colors.blue}
+          strokeWidth={2}
+          dot={<DotComponent tableHeight={tableHeight} />}
+          activeDot={<DotComponent active tableHeight={tableHeight} />}
+          isAnimationActive={false}
+        />
+        {(chartData.length === 0 || isLoading) && (
+          <Customized component={<NoDataStateScreen isLoading={isLoading} />} />
+        )}
+      </LineChartComponent>
+    </ResponsiveContainer>
   );
 };
