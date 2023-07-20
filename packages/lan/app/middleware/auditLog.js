506e5a2ea52c21b260c6313e28f3da82648c3e78
Tom McLean <tom.a.mclean@gmail.com>
2023-07-20 16:30:51 +1000

    Minor fixes

--- packages/lan/app/middleware/auditLog.js
+++ packages/lan/app/middleware/auditLog.js
@@ -2,32 +2,39 @@ class AuditLogItem {
   userId = '';
   resolved = false;
 
-  data = {};
-
+  annotations = null;
   permissionChecks = [];
 
-  shouldPersist() {
-    // user only accessed publicly-accessible data, no permissions checked
-    if (this.permissionChecks.length === 0) return false;
-    return true;
+  shouldKeep() {
+    // any permissions check should be logged
+    if (this.permissionChecks.length > 0) return true;
+
+    // or if we've annotated it with anything extra
+    if (this.annotations) return true;
+
+    // nothing relevant to log, discard
+    return false;
   }
 
   resolve() {
-    if (this.shouldPersist()) return;
     if (this.resolved) {
       throw new Error("Audit log entry resolved twice somehow");
     }
     this.resolved = true;
 
+    if (!this.shouldKeep()) return;
+
+    // just log to console for now, 
     log.info('auditLogEntry', {
       userId: this.userId,
-      data: this.data,
+      data: this.annotations,
       perms: this.permissionChecks,
     });
   }
 
   annotate(data) {
-    this.data = { ...this.data, ...data };
+    // in the first annotation this.annotations will be null but that's fine
+    this.annotations = { ...this.annotations, ...data };
   }
 
   addPermissionCheck(verb, noun, objectId) {
@@ -35,11 +42,13 @@ class AuditLogItem {
   }
 }
 
-const auditMiddleware = (req, res, next) => {
+const auditMiddleware = async (req, res, next) => {
   const audit = new AuditLogItem();
   req.audit = audit;
 
   const localisation = await req.getLocalisation();
+  // only attach the resolver if audit logs are enabled
+  // (without this bit, all the audit logs will just be discarded)
   if (localisation.features.enableAuditLogs) {
     res.on('finish', (...args) => {
       audit.userId = req.user?.id;
