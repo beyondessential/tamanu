0ea1f1be7f5494e56c4eb0b456134b76664cd430
Tim Kennedy <t.m.knndy@gmail.com>
2023-07-20 11:50:02 +1000

    Make login on central asynchronous and do more concurrently

--- packages/sync-server/app/auth/login.js
+++ packages/sync-server/app/auth/login.js
@@ -16,23 +16,69 @@ import {
   isInternalClient,
 } from './utils';
 
+const getRefreshToken = async (models, { refreshSecret, userId, deviceId }) => {
+  const { RefreshToken } = models;
+  const { auth, canonicalHostName } = config;
+  const {
+    saltRounds,
+    refreshToken: { refreshIdLength, tokenDuration: refreshTokenDuration },
+  } = auth;
+
+  const refreshId = await getRandomBase64String(refreshIdLength);
+  const refreshTokenJwtId = getRandomU32();
+  const [hashedRefreshId, refreshToken] = await Promise.all([
+    bcrypt.hash(refreshId, saltRounds),
+    getToken(
+      {
+        userId,
+        refreshId,
+      },
+      refreshSecret,
+      {
+        expiresIn: refreshTokenDuration,
+        audience: JWT_TOKEN_TYPES.REFRESH,
+        issuer: canonicalHostName,
+        jwtid: `${refreshTokenJwtId}`,
+      },
+    ),
+  ]);
+
+  // Extract expiry as set by jwt.sign
+  const { exp } = jwt.decode(refreshToken);
+  await RefreshToken.upsert(
+    {
+      refreshId: hashedRefreshId,
+      expiresAt: new Date(exp * 1000),
+      userId,
+      deviceId,
+    },
+    {
+      where: {
+        userId,
+        deviceId,
+      },
+    },
+  );
+
+  return refreshToken;
+};
+
 export const login = ({ secret, refreshSecret }) =>
   asyncHandler(async (req, res) => {
     const { store, body } = req;
+    const { models } = store;
     const { email, password, facilityId, deviceId } = body;
-
-    const internalClient = isInternalClient(req.header('X-Tamanu-Client'));
 
     if (!email || !password) {
       throw new BadAuthenticationError('Missing credentials');
     }
 
+    const internalClient = isInternalClient(req.header('X-Tamanu-Client'));
     if (internalClient && !deviceId) {
       throw new BadAuthenticationError('Missing deviceId');
     }
 
     const user = await findUser(store.models, email);
-
     if (!user && config.auth.reportNoUserError) {
       // an attacker can use this to get a list of user accounts
       // but hiding this error entirely can make debugging a hassle
@@ -41,82 +87,37 @@ export const login = ({ secret, refreshSecret }) =>
     }
 
     const hashedPassword = user?.password || '';
-
     if (!(await bcrypt.compare(password, hashedPassword))) {
       throw new BadAuthenticationError('Invalid credentials');
     }
 
     const { auth, canonicalHostName } = config;
-
-    const {
-      tokenDuration,
-      saltRounds,
-      refreshToken: { refreshIdLength, tokenDuration: refreshTokenDuration },
-    } = auth;
-
+    const { tokenDuration } = auth;
     const accessTokenJwtId = getRandomU32();
-    const token = getToken(
-      {
-        userId: user.id,
-        deviceId,
-      },
-      secret,
-      {
-        expiresIn: tokenDuration,
-        audience: JWT_TOKEN_TYPES.ACCESS,
-        issuer: canonicalHostName,
-        jwtid: `${accessTokenJwtId}`,
-      },
-    );
-
-    let refreshToken;
-
-    if (internalClient) {
-      const refreshId = await getRandomBase64String(refreshIdLength);
-      const refreshTokenJwtId = getRandomU32();
-      const hashedRefreshId = await bcrypt.hash(refreshId, saltRounds);
-
-      refreshToken = getToken(
+    const [token, refreshToken, facility, localisation, permissions] = await Promise.all([
+      getToken(
         {
-          userId: user.id,
-          refreshId,
-        },
-        refreshSecret,
-        {
-          expiresIn: refreshTokenDuration,
-          audience: JWT_TOKEN_TYPES.REFRESH,
-          issuer: canonicalHostName,
-          jwtid: `${refreshTokenJwtId}`,
-        },
-      );
-
-      // Extract expiry as set by jwt.sign
-      const { exp } = jwt.decode(refreshToken);
-
-      await store.models.RefreshToken.upsert(
-        {
-          refreshId: hashedRefreshId,
-          expiresAt: new Date(exp * 1000),
           userId: user.id,
           deviceId,
         },
+        secret,
         {
-          where: {
-            userId: user.id,
-            deviceId,
-          },
+          expiresIn: tokenDuration,
+          audience: JWT_TOKEN_TYPES.ACCESS,
+          issuer: canonicalHostName,
+          jwtid: `${accessTokenJwtId}`,
         },
-      );
-    }
-
-    // Send some additional data with login to tell the user about
-    // the context they've just logged in to.
-    const [facility, localisation, permissions] = await Promise.all([
+      ),
+      internalClient
+        ? getRefreshToken(models, { refreshSecret, userId: user.id, deviceId })
+        : undefined,
       store.models.Facility.findByPk(facilityId),
       getLocalisation(),
       getPermissionsForRoles(store.models, user.role),
     ]);
 
+    // Send some additional data with login to tell the user about
+    // the context they've just logged in to.
     res.send({
       token,
       refreshToken,
