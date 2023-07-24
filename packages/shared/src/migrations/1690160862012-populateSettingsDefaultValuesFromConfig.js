c0169343dd112aa92de80b53fcbf0acdf6399e10
Daniel Nash <dannash200@gmail.com>
2023-07-24 15:50:06 +1200

    nass-737: use replacements for bulk insert

--- packages/shared/src/migrations/1690160862012-populateSettingsDefaultValuesFromConfig.js
+++ packages/shared/src/migrations/1690160862012-populateSettingsDefaultValuesFromConfig.js
@@ -1,9 +1,8 @@
-import { DataTypes } from 'sequelize';
 import { readFile } from 'fs/promises';
 import config from 'config';
 import { isObject } from 'lodash';
 
-const REMOVE_COMMENT_REGEX = /(\/\/(?!(.*)"\s*,).*\n)+/g;
+const REMOVE_COMMENTS_REGEX = /(\/\/(?!(.*)"\s*,).*\n)+/g;
 
 export async function up(query) {
   const { serverFacilityId } = config;
@@ -12,32 +11,46 @@ export async function up(query) {
       const path = `${prefix}${!isNaN(Number(key)) ? `[${key}]` : `${prefix && '.'}${key}`}`;
       return isObject(value)
         ? getDataFromEntries(Object.entries(value), path)
-        : [
-            {
-              key: path,
-              defaultValue: value,
-              facilityId: serverFacilityId,
-            },
-          ];
+        : [[path, serverFacilityId, JSON.stringify(value)]];
     });
-  const defaultsFile = await readFile('config/default.json');
-  const defaults = JSON.parse(defaultsFile.toString().replace(REMOVE_COMMENT_REGEX, '\n'));
+  const defaultsFile = await readFile('config/bigdog.json');
+  if (!defaultsFile) {
+    throw new Error('Could not find config/default.json');
+  }
+  const defaults = JSON.parse(defaultsFile.toString().replace(REMOVE_COMMENTS_REGEX, '\n'));
   const data = getDataFromEntries(Object.entries(defaults));
 
   // Upsert data defaultValues and don't override anything
-  // On conflict includes where claus to deal with partial indexes
-  await query.sequelize.query(`
-    INSERT INTO settings (key, facility_id, default_value)
-    VALUES ${data
-      .map(
-        ({ key, defaultValue, facilityId }) =>
-          `('${key}', '${facilityId}', '${JSON.stringify(defaultValue)}')`,
-      )
-      .join(', ')}
-    ON CONFLICT (key, facility_id) WHERE key IS NOT NULL AND facility_id IS NOT NULL AND deleted_at IS NULL
-    DO UPDATE SET default_value = EXCLUDED.default_value;
-  `);
+  // On conflict references one of two partial unique indexes based on server context.
+  if (serverFacilityId) {
+    await query.sequelize.query(
+      `
+        INSERT INTO settings (key, facility_id, default_value)
+        VALUES ${data.map(() => '(?)').join(', ')}
+        ON CONFLICT (key, facility_id) WHERE key IS NOT NULL AND facility_id IS NOT NULL AND deleted_at IS NULL
+        DO UPDATE SET default_value = EXCLUDED.default_value;
+      `,
+      {
+        replacements: data,
+        type: query.sequelize.QueryTypes.INSERT,
+      },
+    );
+  } else {
+    await query.sequelize.query(
+      `
+        INSERT INTO settings (key, default_value)
+        VALUES ${data.map(() => '(?)').join(', ')}
+        ON CONFLICT (key) WHERE key IS NOT NULL AND deleted_at IS NULL
+        DO UPDATE SET default_value = EXCLUDED.default_value;
+      `,
+      {
+        replacements: data,
+        type: query.sequelize.QueryTypes.INSERT,
+      },
+    );
+  }
 }
 
 export async function down(query) {
+  // No down migration
 }
