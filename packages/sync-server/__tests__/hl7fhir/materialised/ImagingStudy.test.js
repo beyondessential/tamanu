be2987ea2bc1072b48e71a70747b0c09d8e26759
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-07-26 11:55:43 -0400

    Add comment to fool GitHub into making PR viable

--- packages/sync-server/__tests__/hl7fhir/materialised/ImagingStudy.test.js
+++ packages/sync-server/__tests__/hl7fhir/materialised/ImagingStudy.test.js
@@ -284,6 +284,7 @@ describe(`Materialised FHIR - ImagingStudy`, () => {
 
         // This was failing intermittently, apparently we have to
         // seize control to let the FhirWriteLog create itself the second time
+        // Just a comment to make PR viable
         await sleepAsync(1);
 
         // assert
