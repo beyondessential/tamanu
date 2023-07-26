fd5c30c295aa0cce478ac0d1e66f22d5e52da285
Félix Saparelli <felix@bes.au>
2023-07-26 13:43:57 +1200

    Iterate packages in dependency order for build

--- scripts/_do-with-all-packages.mjs
+++ scripts/_do-with-all-packages.mjs
@@ -1,18 +1,31 @@
 import { readFileSync } from 'fs';
+import { execFileSync } from 'child_process';
 
 export function doWithAllPackages(fn) {
-  const topPkg = JSON.parse(readFileSync('./package.json'));
+  const workspaceTree = JSON.parse(
+    execFileSync('yarn', ['-s', 'workspaces', 'info'], { encoding: 'utf8' }),
+  );
+  const workspaces = new Set(Object.keys(workspaceTree));
+  const processed = new Set();
 
-  for (const name of topPkg.workspaces.packages) {
-    const pkgPath = `./${name}/package.json`;
-    let pkg;
-    try {
-      pkg = JSON.parse(readFileSync(pkgPath));
-    } catch (err) {
-      console.log(`Skipping ${name} as we can't read its package.json...`);
-      continue;
+  while (processed.size < workspaces.size) {
+    for (const workspace of workspaces) {
+      if (processed.has(workspace)) continue;
+      const { location, workspaceDependencies } = workspaceTree[workspace];
+      if (workspaceDependencies.every(dep => processed.has(dep))) {
+        processed.add(workspace);
+
+        const pkgPath = `./${location}/package.json`;
+        let pkg;
+        try {
+          pkg = JSON.parse(readFileSync(pkgPath));
+        } catch (err) {
+          console.log(`Skipping ${workspace} as we can't read its package.json...`);
+          continue;
+        }
+
+        fn(workspace, pkg, pkgPath);
+      }
     }
-
-    fn(name, pkg, pkgPath);
   }
 }
