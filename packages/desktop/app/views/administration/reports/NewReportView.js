b9b636b89b523615f61a969b655e3e66f14344ab
Edwin <edmofro@gmail.com>
2023-07-26 17:24:36 +1200

    Add SQL editor

--- packages/desktop/app/views/administration/reports/NewReportView.js
+++ packages/desktop/app/views/administration/reports/NewReportView.js
@@ -1,3 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { OutlinedButton } from '../../../components';
+import { QueryEditor } from './QueryEditor';
 
-export const NewReportView = () => <div>TODO</div>;
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  align-items: flex-start;
+`;
+
+export const NewReportView = () => {
+  const [showSqlEditor, setShowSqlEditor] = useState(false);
+  const handleUpdate = () => {};
+  const handleCloseSqlEditor = () => setShowSqlEditor(false);
+
+  return (
+    <Container>
+      <OutlinedButton onClick={() => setShowSqlEditor(true)}>Edit SQL</OutlinedButton>
+      <QueryEditor
+        title="Edit Query"
+        initialValue=""
+        onSubmit={handleUpdate}
+        open={showSqlEditor}
+        onClose={handleCloseSqlEditor}
+      />
+    </Container>
+  );
+};
