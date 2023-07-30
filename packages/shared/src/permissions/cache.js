bb31cf8a15db7c13c0ae84dd2245842c9d66a578
Daniel Nash <dannash200@gmail.com>
2023-07-31 11:12:43 +1200

    tamoc-2: permission cache ttl

--- packages/shared/src/permissions/cache.js
+++ packages/shared/src/permissions/cache.js
@@ -1,7 +1,16 @@
 import { isEmpty } from 'lodash';
+import config from 'config';
 
 class PermissionCache {
   cache = {};
+
+  constructor() {
+    if (config.permissions.cacheTTL) {
+      this.resetInterval = setInterval(() => {
+        this.reset();
+      }, config.permissions.cacheTTL);
+    }
+  }
 
   get(key) {
     return this.cache[key];
