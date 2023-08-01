29cfd6a42bc1b4c6ff2e030b10866eb1af06810b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-08-01 12:16:09 +1200

    add scope column

--- packages/shared/src/constants/settings.js
+++ packages/shared/src/constants/settings.js
@@ -2,3 +2,9 @@ export const SETTING_KEYS = {
   VACCINATION_DEFAULTS: 'vaccinations.defaults',
   VACCINATION_GIVEN_ELSEWHERE_DEFAULTS: 'vaccinations.givenElsewhere.defaults',
 };
+
+export const SETTINGS_SCOPES = {
+  CENTRAL: 'central',
+  GLOBAL: 'global',
+  FACILITY: 'facility',
+};
