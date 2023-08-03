30f16ada375f5ee43eab7b0da08434a44e75c7ea
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-08-02 21:31:09 -0400

    Add updatedAtByField to patient fake function overrides

--- packages/shared/src/test-helpers/fake.js
+++ packages/shared/src/test-helpers/fake.js
@@ -286,6 +286,7 @@ const MODEL_SPECIFIC_OVERRIDES = {
       culturalName: chance.first({ gender: nameGender }),
       dateOfDeath: null,
       email: chance.email(),
+      updatedAtByField: {},
     };
   },
   PatientAdditionalData: ({ id, patientId }) => {
