ad2df2fbca8ae5d3ede8a5df40b23116731fb4b4
Félix Saparelli <felix@bes.au>
2023-08-04 14:26:31 +1200

    wip

--- packages/simulator/src/activities/TakeVitals.ts
+++ packages/simulator/src/activities/TakeVitals.ts
@@ -4,12 +4,12 @@ export class TakeVitals extends Activity {
   #patientId?: string;
   #encounterId?: string;
 
-  async gather(args: CallArgs): Promise<void> {
+  async gather(_role: Role, args: CallArgs): Promise<void> {
     this.#patientId = args.patientId as string;
     this.#encounterId = args.encounterId as string;
   }
 
   async act(role: Role): Promise<void> {
-    const _api = await this.context.api.as(role);
+    const api = await this.context.api.as(role);
   }
 }
