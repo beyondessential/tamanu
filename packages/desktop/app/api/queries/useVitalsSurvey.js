a07eaa8da5aeef43472ef9dddaa85dd8fe7ff38e
Félix Saparelli <felix@bes.au>
2023-08-04 15:21:56 +1200

    style

--- packages/desktop/app/api/queries/useVitalsSurvey.js
+++ packages/desktop/app/api/queries/useVitalsSurvey.js
@@ -5,7 +5,7 @@ import { useApi, isErrorUnknownAllow404s } from '../index';
 export const useVitalsSurvey = () => {
   const api = useApi();
   const vitalsSurvey = useQuery(['survey', { type: 'vitals' }], () =>
-    api.get(`survey/vitals`, {}, { isErrorUnknown: isErrorUnknownAllow404s }),
+    api.get('survey/vitals', {}, { isErrorUnknown: isErrorUnknownAllow404s }),
   );
 
   return vitalsSurvey;
