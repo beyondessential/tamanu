d6618388af844680b284b2f29c9f84e82d82c69d
Félix Saparelli <felix@bes.au>
2023-08-04 17:50:29 +1200

    Count amount of requests made

--- packages/simulator/src/index.ts
+++ packages/simulator/src/index.ts
@@ -1,6 +1,13 @@
-import { FetchImplementation, setFetchImplementation } from '@tamanu/api-client';
-import { fetch } from 'undici';
-setFetchImplementation(fetch as unknown as FetchImplementation);
+import { FetchImplementation, setFetchImplementation, RequestOptions } from '@tamanu/api-client';
+import { fetch, RequestInfo } from 'undici';
+
+let requestCounter = 0;
+const countedFetch: FetchImplementation = async (url: Request | string | URL, config?: RequestOptions) => {
+  requestCounter += 1;
+  return fetch(url as unknown as RequestInfo, config);
+};
+
+setFetchImplementation(countedFetch);
 
 import { ApiFactory } from './ApiFactory.js';
 import { Game } from './board/Game.js';
@@ -32,4 +39,5 @@ class MakePatientsGame extends Game {
     makeContext({ api: new ApiFactory('http://localhost:3000', 'http://localhost:4000') }),
   );
   await game.run();
+  console.log('Made', requestCounter, 'requests');
 })().catch(console.error);
