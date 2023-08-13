1dc1ad6d4beec0c15af6e4ca7de69fc5816969c4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2023-08-13 18:37:22 -0400

    Remove unused import

--- packages/sync-server/__tests__/admin/patientMerge.test.js
+++ packages/sync-server/__tests__/admin/patientMerge.test.js
@@ -2,7 +2,6 @@ import { fake, fakeUser } from 'shared/test-helpers/fake';
 import { NOTE_TYPES } from '@tamanu/constants/notes';
 import { VISIBILITY_STATUSES } from '@tamanu/constants';
 import { InvalidParameterError } from 'shared/errors';
-import { LocalSystemFact } from 'shared/models/LocalSystemFact';
 import { PATIENT_FIELD_DEFINITION_TYPES } from '@tamanu/constants/patientFields';
 import {
   mergePatient,
