c33e28795e7d232d3e9267a2d6a2ef40a16a9de3
Renato Koyama <renato.koyama@gmail.com>
2023-08-17 20:18:53 -0300

    NASS-825: Shared suggester endpoints constant (#4593)

---
+++ packages/constants/src/suggesters.ts
@@ -0,0 +1,19 @@
+import { REFERENCE_TYPE_VALUES } from './importable';
+
+export const SUGGESTER_ENDPOINTS = [
+  ...REFERENCE_TYPE_VALUES,
+  'department',
+  'facility',
+  'facilityLocationGroup',
+  'invoiceLineTypes',
+  'labTestPanel',
+  'labTestType',
+  'location',
+  'locationGroup',
+  'patient',
+  'patientLabTestCategories',
+  'patientLabTestPanelTypes',
+  'patientLetterTemplate',
+  'practitioner',
+  'survey',
+];
