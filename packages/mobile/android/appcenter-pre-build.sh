9df196322389c879ccfc76e736d4abd36cd105d4
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-22 19:53:06 +1000

    Fixups

--- packages/mobile/android/appcenter-pre-build.sh
+++ packages/mobile/android/appcenter-pre-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 SERVER_OVERRIDE_PATH=../serverOverrides.json
+echo "SERVER_OVERRIDES=$SERVER_OVERRIDES"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
-    echo "Outputting server overrides to $(readlink -f $"SERVER_OVERRIDE_PATH")"
+    echo "Outputting server overrides to $(readlink -f "$SERVER_OVERRIDE_PATH")"
     echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
 fi
