be8b92cd971d90fc6a7828d282fa08dc6c9584ea
georgekamar <gkamar97@gmail.com>
2023-08-24 02:25:56 +0300

    EPI-605: Table Layout Fixes

--- packages/desktop/app/components/Table/Table.js
+++ packages/desktop/app/components/Table/Table.js
@@ -79,11 +79,18 @@ const StyledTableRow = styled(TableRow)`
 
   ${p => (p.$rowStyle ? p.$rowStyle : '')}
 
-  &.MuiTableRow-root {
-    display: table;
-    table-layout: fixed;
-    width: 100%;
-  }
+  ${p =>
+    p.$lazyLoading
+      ? `
+      &.MuiTableRow-root {
+        display: table;
+        table-layout: fixed;
+        width: 100%;
+      }
+    `
+      : ''}
+
+
 `;
 
 const StyledTableContainer = styled.div`
@@ -96,9 +103,14 @@ const StyledTableContainer = styled.div`
 
 const StyledTableBody = styled(TableBody)`
   &.MuiTableBody-root {
-    overflow: auto;
-    display: block;
-    ${props => (props.$lazyLoading ? `max-height: 300px;` : '')};
+    ${props =>
+      props.$lazyLoading
+        ? `
+        overflow: auto;
+        max-height: 300px;
+        display: block;
+      `
+        : ''};
   }
 `;
 
@@ -138,9 +150,14 @@ const StyledTable = styled(MaterialTable)`
 `;
 
 const StyledTableHead = styled(TableHead)`
-  display: table;
-  table-layout: fixed;
-  width: 100%;
+  ${props =>
+    props.$lazyLoading
+      ? `
+      display: table;
+      table-layout: fixed;
+      width: 100%;
+    `
+      : ''}
   background: ${props => (props.$headerColor ? props.$headerColor : Colors.background)};
   white-space: nowrap;
   .MuiTableCell-head {
@@ -169,8 +186,8 @@ const HeaderContainer = React.memo(({ children, numeric }) => (
   <StyledTableCell align={numeric ? 'right' : 'left'}>{children}</StyledTableCell>
 ));
 
-const RowContainer = React.memo(({ children, rowStyle, onClick }) => (
-  <StyledTableRow onClick={onClick} $rowStyle={rowStyle}>
+const RowContainer = React.memo(({ children, lazyLoading, rowStyle, onClick }) => (
+  <StyledTableRow onClick={onClick} $rowStyle={rowStyle} $lazyLoading={lazyLoading}>
     {children}
   </StyledTableRow>
 ));
@@ -183,7 +200,7 @@ const StatusTableCell = styled(StyledTableCell)`
 `;
 
 const Row = React.memo(
-  ({ rowIndex, columns, data, onClick, cellOnChange, rowStyle, refreshTable }) => {
+  ({ rowIndex, columns, data, onClick, cellOnChange, lazyLoading, rowStyle, refreshTable }) => {
     const cells = columns.map(
       ({ key, accessor, CellComponent, numeric, maxWidth, cellColor, dontCallRowInput }) => {
         const onChange = cellOnChange ? event => cellOnChange(event, key, rowIndex, data) : null;
@@ -215,6 +232,7 @@ const Row = React.memo(
       <RowContainer
         onClick={onClick && (() => onClick(data))}
         rowStyle={rowStyle ? rowStyle(data) : ''}
+        lazyLoading={lazyLoading}
       >
         {cells}
       </RowContainer>
@@ -368,11 +386,12 @@ class TableComponent extends React.Component {
               cellOnChange={cellOnChange}
               refreshTable={refreshTable}
               rowStyle={rowStyle}
+              lazyLoading={lazyLoading}
             />
           );
         })}
         {isLoadingMore && (
-          <StyledTableRow>
+          <StyledTableRow $lazyLoading={lazyLoading}>
             <CenteredLoadingIndicatorContainer>
               <LoadingIndicator
                 backgroundColor="transparent"
@@ -413,7 +432,7 @@ class TableComponent extends React.Component {
 
     return (
       <StyledTableFooter>
-        <StyledTableRow>
+        <StyledTableRow $lazyLoading={lazyLoading}>
           {allowExport ? (
             <TableCell colSpan={page !== null ? 2 : columns.length}>
               <DownloadDataButton exportName={exportName} columns={columns} data={data} />
@@ -446,8 +465,12 @@ class TableComponent extends React.Component {
           $backgroundColor={data.length || isLoading ? Colors.white : noDataBackgroundColor}
         >
           {!hideHeader && (
-            <StyledTableHead $headerColor={headerColor} $fixedHeader={fixedHeader}>
-              <StyledTableRow>{this.renderHeaders()}</StyledTableRow>
+            <StyledTableHead
+              $headerColor={headerColor}
+              $fixedHeader={fixedHeader}
+              $lazyLoading={lazyLoading}
+            >
+              <StyledTableRow $lazyLoading={lazyLoading}>{this.renderHeaders()}</StyledTableRow>
             </StyledTableHead>
           )}
           <StyledTableBody onScroll={this.handleScroll} $lazyLoading={lazyLoading}>
