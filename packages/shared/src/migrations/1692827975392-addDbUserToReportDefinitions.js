54ebb398097a5babb9784204f7e70bc34727c7a8
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-08-24 11:18:46 +1200

    add db user column migration

---
+++ packages/shared/src/migrations/1692827975392-addDbUserToReportDefinitions.js
@@ -0,0 +1,18 @@
+import Sequelize from 'sequelize';
+
+export async function up(query) {
+  await query.addColumn('report_definitions', 'db_user', {
+    type: Sequelize.ENUM('tamanu_dataset_reporting', 'tamanu_raw_reporting'),
+    allowNull: false,
+    defaultValue: 'tamanu_dataset_reporting'
+  });
+
+  await query.sequelize.query(`
+    UPDATE "report_definitions"
+    SET "db_user" = 'tamanu_raw_reporting'
+  `);
+}
+
+export async function down(query) {
+  await query.removeColumn('report_definitions', 'db_user');
+}
