824aef5819eebae767f683dc900da7ba1725b7e4
Renato Koyama <renato.koyama@gmail.com>
2023-08-23 19:31:50 -0300

    Fix populate settings from local config when having no data

--- packages/shared/src/migrations/1691371570736-populateSettingsFromLocalConfig.js
+++ packages/shared/src/migrations/1691371570736-populateSettingsFromLocalConfig.js
@@ -131,22 +131,28 @@ export async function up(query) {
     serverFacilityId ? SETTINGS_SCOPES.FACILITY : SETTINGS_SCOPES.CENTRAL,
   );
 
-  // Create the settings for either the facility or central scope
-  await query.sequelize.query(
-    `
-      INSERT INTO settings (key, value, facility_id, scope)
-      VALUES ${scopedSettingData.map(() => '(?)').join(', ')}
-    `,
-    {
-      replacements: scopedSettingData,
-    },
-  );
+  if(scopedSettingData.length){
+    // Create the settings for either the facility or central scope
+    await query.sequelize.query(
+      `
+        INSERT INTO settings (key, value, facility_id, scope)
+        VALUES ${scopedSettingData.map(() => '(?)').join(', ')}
+      `,
+      {
+        replacements: scopedSettingData,
+      },
+    );
+  }
 
   if (serverFacilityId) return;
 
   // Central server only
   // Create the settings for global scope
   const globalSettingData = prepareReplacementsForInsert(localConfig, null, SETTINGS_SCOPES.GLOBAL);
+  if(!globalSettingData.length){
+    return;
+  }
+  
   await query.sequelize.query(
     `
       INSERT INTO settings (key, value, facility_id, scope)
