40d445111a298952cb22b6fa95bed385ae6465b8
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-08-24 14:18:20 +1200

    use constants for db users

--- packages/shared/src/migrations/1692827975392-addDbUserToReportDefinitions.js
+++ packages/shared/src/migrations/1692827975392-addDbUserToReportDefinitions.js
@@ -1,15 +1,20 @@
 import Sequelize from 'sequelize';
 
+const DB_ROLES = {
+  RAW: 'tamanu_raw_reporting',
+  DATASET: 'tamanu_dataset_reporting',
+};
+
 export async function up(query) {
   await query.addColumn('report_definitions', 'db_user', {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'tamanu_dataset_reporting',
+    defaultValue: DB_ROLES.DATASET,
   });
 
   await query.sequelize.query(`
     UPDATE "report_definitions"
-    SET "db_user" = 'tamanu_raw_reporting'
+    SET "db_user" = '${DB_ROLES.RAW}'
   `);
 }
 
