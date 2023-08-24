bb4c1e33dad7a0a6e12ae5cf3a4f6eb5c9e1f76a
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 16:25:51 +1000

    Hexdump the server overrides file (why's it escaped?)

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -6,5 +6,5 @@ echo "cwd is $(pwd)"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
     echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
     echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
-    cat "$SERVER_OVERRIDE_PATH"
+    hexdump -C "$SERVER_OVERRIDE_PATH"
 fi
