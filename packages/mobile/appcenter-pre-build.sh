f13b025a2afc776bcde467c2449bd5d335e04b9f
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 17:46:47 +1000

    Remove echo, script sets -x and it's unnecessary

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -3,7 +3,6 @@ set -euxo pipefail
 
 SERVER_OVERRIDE_PATH=./serverOverrides.json
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
-    echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
     # sed because the server overrides are coming through quoted for some reason
     echo "$SERVER_OVERRIDES" | sed 's/\\"/"/gi' > "$SERVER_OVERRIDE_PATH"
 fi
