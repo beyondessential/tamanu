34b4ac9765509d0dfec7297acda900c979a7a4a9
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 16:34:47 +1000

    Unquote $SERVER_OVERRIDES

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -2,9 +2,8 @@
 set -euxo pipefail
 
 SERVER_OVERRIDE_PATH=./serverOverrides.json
-echo "cwd is $(pwd)"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
     echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
-    echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
-    hexdump -C "$SERVER_OVERRIDE_PATH"
+    # sed because the server overrides are coming through quotedfor some reason
+    echo "$SERVER_OVERRIDES" | sed 's/\\"/"/gi' > "$SERVER_OVERRIDE_PATH"
 fi
