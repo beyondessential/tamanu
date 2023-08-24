dc59206d19980a5f982d4d973b36ae54c149a03b
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 13:28:06 +1000

    Fixes to script

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
 SERVER_OVERRIDE_PATH=./serverOverrides.json
 echo "SERVER_OVERRIDES=$SERVER_OVERRIDES"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
-    echo "Outputting server overrides to $(readlink -f "$SERVER_OVERRIDE_PATH")"
+    echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
     echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
 fi
