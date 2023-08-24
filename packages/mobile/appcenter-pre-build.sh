61b53eb8d8d0d2495f08b50dc68dedf18f28ab7b
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 15:04:00 +1000

    Work out what cwd is

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -3,6 +3,7 @@ set -euxo pipefail
 
 SERVER_OVERRIDE_PATH=./serverOverrides.json
 echo "SERVER_OVERRIDES=$SERVER_OVERRIDES"
+echo "cwd is $(cwd)"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
     echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
     echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
