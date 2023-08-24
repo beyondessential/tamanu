a1e9dbd1cf02e0cb0d95824de82108dd9cbcb9c6
Tim Kennedy <t.m.knndy@gmail.com>
2023-08-24 16:16:47 +1000

    Output full overrides file

--- packages/mobile/appcenter-pre-build.sh
+++ packages/mobile/appcenter-pre-build.sh
@@ -2,9 +2,9 @@
 set -euxo pipefail
 
 SERVER_OVERRIDE_PATH=./serverOverrides.json
-echo "SERVER_OVERRIDES=$SERVER_OVERRIDES"
 echo "cwd is $(pwd)"
 if [[ ! -z "$SERVER_OVERRIDES" ]]; then
     echo "Outputting server overrides to $SERVER_OVERRIDE_PATH"
     echo "$SERVER_OVERRIDES" > "$SERVER_OVERRIDE_PATH"
+    cat "$SERVER_OVERRIDE_PATH"
 fi
