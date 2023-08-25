52aba65ccba2f267e5f63b5f05bc559ad8b738ff
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-08-25 13:45:40 +1200

    add custom sorting hook

--- packages/desktop/app/views/administration/reports/ReportTables.js
+++ packages/desktop/app/views/administration/reports/ReportTables.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { REPORT_STATUSES } from '@tamanu/constants';
 import { formatShort, formatTime } from '../../../components';
@@ -42,66 +42,108 @@ const ReportStatusTag = ({ status }) => {
   );
 };
 
-export const ReportTable = React.memo(({ data, selected, onRowClick, loading, error }) => (
-  <StyledTable
-    onRowClick={onRowClick}
-    rowStyle={({ id }) => ({
-      backgroundColor: selected === id ? Colors.veryLightBlue : Colors.white,
-    })}
-    columns={[
-      {
-        title: 'Name',
-        key: 'name',
-        minWidth: 400,
-      },
-      {
-        title: 'Last updated',
-        key: 'lastUpdated',
-        minWidth: 300,
-        accessor: ({ lastUpdated }) =>
-          lastUpdated ? `${formatShort(lastUpdated)} ${formatTime(lastUpdated)}` : '-',
-      },
-      {
-        title: 'Version count',
-        key: 'versionCount',
-        numeric: true,
-        minWidth: 200,
-        accessor: ({ versionCount }) => versionCount || 0,
-      },
-    ]}
-    data={data}
-    isLoading={loading}
-    errorMessage={error}
-    elevated={false}
-    allowExport={false}
-  />
-));
+const useTableSorting = () => {
+  const [orderBy, setOrderBy] = useState(null);
+  const [order, setOrder] = useState('asc');
 
-export const VersionTable = React.memo(({ data, onRowClick, loading, error }) => (
-  <StyledTable
-    allowExport={false}
-    onRowClick={onRowClick}
-    columns={[
-      {
-        title: 'Version',
-        key: 'versionNumber',
-        minWidth: 200,
-      },
-      {
-        title: 'Created time',
-        key: 'createdAt',
-        minWidth: 300,
-        accessor: ({ updatedAt }) => `${formatShort(updatedAt)} ${formatTime(updatedAt)}`,
-      },
-      {
-        title: 'Status',
-        key: 'status',
-        accessor: ({ status, active }) => <ReportStatusTag status={active ? 'active' : status} />,
-      },
-    ]}
-    data={data}
-    elevated={false}
-    isLoading={loading}
-    errorMessage={error}
-  />
-));
+  const customSort = (data = []) => {
+    const sortedData = data.sort((a, b) => {
+      if (typeof a[orderBy] === 'string') {
+        return order === 'asc'
+          ? a[orderBy].localeCompare(b[orderBy])
+          : b[orderBy].localeCompare(a[orderBy]);
+      }
+      return order === 'asc' ? a[orderBy] - b[orderBy] : b[orderBy] - a[orderBy];
+    });
+    return sortedData;
+  };
+
+  const onChangeOrderBy = sortKey => {
+    setOrderBy(sortKey);
+    const isDesc = orderBy === sortKey && order === 'desc';
+    setOrder(isDesc ? 'asc' : 'desc');
+  };
+
+  return { orderBy, order, onChangeOrderBy, customSort };
+};
+
+export const ReportTable = React.memo(({ data, selected, onRowClick, loading, error }) => {
+  const { orderBy, order, onChangeOrderBy, customSort } = useTableSorting();
+
+  return (
+    <StyledTable
+      onRowClick={onRowClick}
+      rowStyle={({ id }) => ({
+        backgroundColor: selected === id ? Colors.veryLightBlue : Colors.white,
+      })}
+      columns={[
+        {
+          title: 'Name',
+          key: 'name',
+          minWidth: 400,
+        },
+        {
+          title: 'Last updated',
+          key: 'lastUpdated',
+          minWidth: 300,
+          accessor: ({ lastUpdated }) =>
+            lastUpdated ? `${formatShort(lastUpdated)} ${formatTime(lastUpdated)}` : '-',
+        },
+        {
+          title: 'Version count',
+          key: 'versionCount',
+          numeric: true,
+          minWidth: 200,
+          accessor: ({ versionCount }) => versionCount || 0,
+        },
+      ]}
+      data={data}
+      isLoading={loading}
+      errorMessage={error}
+      elevated={false}
+      allowExport={false}
+      onChangeOrderBy={onChangeOrderBy}
+      customSort={customSort}
+      orderBy={orderBy}
+      order={order}
+      initialSort={{ orderBy: 'name', order: 'desc' }}
+    />
+  );
+});
+
+export const VersionTable = React.memo(({ data, onRowClick, loading, error }) => {
+  const { orderBy, order, onChangeOrderBy, customSort } = useTableSorting();
+
+  return (
+    <StyledTable
+      allowExport={false}
+      onRowClick={onRowClick}
+      columns={[
+        {
+          title: 'Version',
+          key: 'versionNumber',
+          minWidth: 200,
+        },
+        {
+          title: 'Created time',
+          key: 'createdAt',
+          minWidth: 300,
+          accessor: ({ updatedAt }) => `${formatShort(updatedAt)} ${formatTime(updatedAt)}`,
+        },
+        {
+          title: 'Status',
+          key: 'status',
+          accessor: ({ status, active }) => <ReportStatusTag status={active ? 'active' : status} />,
+        },
+      ]}
+      data={data}
+      elevated={false}
+      isLoading={loading}
+      errorMessage={error}
+      onChangeOrderBy={onChangeOrderBy}
+      customSort={customSort}
+      orderBy={orderBy}
+      order={order}
+    />
+  );
+});
