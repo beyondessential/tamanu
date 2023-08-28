7f965911ae649598dd0564e0a0b726e36df3a221
Biao Li <lbit123@outlook.com>
2023-08-28 15:48:56 +1000

    Resolve merge conflict

--- packages/sync-server/__tests__/subCommands/fhir.test.js
+++ packages/sync-server/__tests__/subCommands/fhir.test.js
@@ -12,13 +12,7 @@ describe('fhir sub commands', () => {
 
   beforeAll(async () => {
     ctx = await createTestContext();
-    const {
-      FhirEncounter,
-      ImagingRequest,
-      NotePage,
-      NoteItem,
-      FhirServiceRequest,
-    } = ctx.store.models;
+    const { FhirEncounter, ImagingRequest, FhirServiceRequest } = ctx.store.models;
     resources = await fakeResourcesOfFhirServiceRequest(ctx.store.models);
 
     await FhirEncounter.materialiseFromUpstream(resources.encounter.id);
@@ -34,31 +28,6 @@ describe('fhir sub commands', () => {
         imagingType: 'xRay',
       }),
     );
-
-    const [np1, np2] = await NotePage.bulkCreate([
-      fake(NotePage, {
-        date: '2022-03-05',
-        visibilityStatus: VISIBILITY_STATUSES.CURRENT,
-        noteType: NOTE_TYPES.OTHER,
-        recordType: ImagingRequest.name,
-        recordId: imagingRequest.id,
-      }),
-      fake(NotePage, {
-        date: '2022-03-06',
-        visibilityStatus: VISIBILITY_STATUSES.CURRENT,
-        noteType: NOTE_TYPES.OTHER,
-        recordType: ImagingRequest.name,
-        recordId: imagingRequest.id,
-      }),
-    ]);
-    await NoteItem.bulkCreate([
-      fake(NoteItem, { notePageId: np1.id, content: 'Suspected adenoma' }),
-      fake(NoteItem, { notePageId: np1.id, content: 'Patient may need mobility assistance' }),
-      fake(NoteItem, {
-        notePageId: np2.id,
-        content: 'Patient may have shrapnel in leg - need to confirm beforehand',
-      }),
-    ]);
 
     await imagingRequest.setAreas([resources.area1.id, resources.area2.id]);
     await FhirServiceRequest.materialiseFromUpstream(imagingRequest.id);
