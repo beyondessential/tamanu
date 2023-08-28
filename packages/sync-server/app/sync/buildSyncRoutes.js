2b3764ee36bcd5d1e4fc575d65a55abaa0ca611a
Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
2023-08-28 13:03:50 +1200

    SAV-358: Include facility id in start session logs (#4600)
    * SAV-358: Include facility id in start session logs
    
    * Restructure parameter to debugInfo object
    
    ---------
    
    Co-authored-by: Edwin <edmofro@gmail.com>

--- packages/sync-server/app/sync/buildSyncRoutes.js
+++ packages/sync-server/app/sync/buildSyncRoutes.js
@@ -13,8 +13,12 @@ export const buildSyncRoutes = ctx => {
   syncRoutes.post(
     '/',
     asyncHandler(async (req, res) => {
-      const { user, deviceId } = req;
-      const { sessionId, tick } = await syncManager.startSession(user.id, deviceId);
+      const { user, deviceId, facilityId } = req;
+      const { sessionId, tick } = await syncManager.startSession({
+        userId: user.id,
+        deviceId,
+        facilityId,
+      });
       res.json({ sessionId, tick });
     }),
   );
