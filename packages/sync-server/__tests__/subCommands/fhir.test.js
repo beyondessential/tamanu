ae0560524055b0796874f5fb7ff6b8dddf8eb928
Biao Li <lbit123@outlook.com>
2023-09-01 14:23:12 +1000

    Add a unit test to catch when multiple Upstream models get updated

--- packages/sync-server/__tests__/subCommands/fhir.test.js
+++ packages/sync-server/__tests__/subCommands/fhir.test.js
@@ -1,5 +1,10 @@
 import { fake } from 'shared/test-helpers';
-import { IMAGING_REQUEST_STATUS_TYPES } from '@tamanu/constants';
+import { randomLabRequest } from '@tamanu/shared/demoData';
+import {
+  IMAGING_REQUEST_STATUS_TYPES,
+  LAB_REQUEST_STATUSES,
+  FHIR_REQUEST_STATUS,
+} from '@tamanu/constants';
 import { createTestContext } from '../utilities';
 import { fhir } from '../../app/subCommands/fhir';
 import { ApplicationContext } from '../../app/ApplicationContext';
@@ -9,10 +14,19 @@ describe('fhir sub commands', () => {
   let ctx;
   let resources;
   let imagingRequest;
+  let labRequest;
 
   beforeAll(async () => {
     ctx = await createTestContext();
-    const { FhirEncounter, ImagingRequest, FhirServiceRequest } = ctx.store.models;
+    const {
+      FhirEncounter,
+      ImagingRequest,
+      LabRequest,
+      FhirServiceRequest,
+      ReferenceData,
+      LabTestPanel,
+      LabTestPanelRequest,
+    } = ctx.store.models;
     resources = await fakeResourcesOfFhirServiceRequest(ctx.store.models);
 
     await FhirEncounter.materialiseFromUpstream(resources.encounter.id);
@@ -28,9 +42,35 @@ describe('fhir sub commands', () => {
         imagingType: 'xRay',
       }),
     );
-
     await imagingRequest.setAreas([resources.area1.id, resources.area2.id]);
+
+    const category = await ReferenceData.create({
+      id: 'test1',
+      type: 'labTestCategory',
+      code: 'test1',
+      name: 'Test 1',
+    });
+    const labTestPanel = await LabTestPanel.create({
+      ...fake(LabTestPanel),
+      categoryId: category.id,
+    });
+    const labTestPanelRequest = await LabTestPanelRequest.create({
+      ...fake(LabTestPanelRequest),
+      labTestPanelId: labTestPanel.id,
+      encounterId: resources.encounter.id,
+    });
+    const labRequestData = await randomLabRequest(ctx.store.models, {
+      requestedById: resources.practitioner.id,
+      patientId: resources.patient.id,
+      encounterId: resources.encounter.id,
+      status: LAB_REQUEST_STATUSES.PUBLISHED,
+      labTestPanelRequestId: labTestPanelRequest.id, // make one of them part of a panel
+      requestedDate: '2022-07-27 16:30:00',
+    });
+    labRequest = await LabRequest.create(labRequestData);
+
     await FhirServiceRequest.materialiseFromUpstream(imagingRequest.id);
+    await FhirServiceRequest.materialiseFromUpstream(labRequest.id);
     await FhirServiceRequest.resolveUpstreams();
 
     jest
@@ -50,11 +90,37 @@ describe('fhir sub commands', () => {
         upstreamId: imagingRequest.id,
       },
     });
-    await fhirServiceRequest.update({ status: IMAGING_REQUEST_STATUS_TYPES.CANCELLED });
+    await imagingRequest.update({ status: IMAGING_REQUEST_STATUS_TYPES.IN_PROGRESS });
 
     await fhir({ refresh: 'ServiceRequest' });
 
     await fhirServiceRequest.reload();
-    expect(fhirServiceRequest.status).toEqual(IMAGING_REQUEST_STATUS_TYPES.COMPLETED);
+    // See mapping at packages/shared/src/models/fhir/ServiceRequest/getValues.js
+    expect(fhirServiceRequest.status).toEqual(FHIR_REQUEST_STATUS.ACTIVE);
+  });
+
+  it('should catch when multiple Upstream models get updated', async () => {
+    const { FhirServiceRequest } = ctx.store.models;
+    const fhirImagingServiceRequest = await FhirServiceRequest.findOne({
+      where: {
+        upstreamId: imagingRequest.id,
+      },
+    });
+    const fhirLabServiceRequest = await FhirServiceRequest.findOne({
+      where: {
+        upstreamId: labRequest.id,
+      },
+    });
+
+    await imagingRequest.update({ status: IMAGING_REQUEST_STATUS_TYPES.IN_PROGRESS });
+    await labRequest.update({ status: LAB_REQUEST_STATUSES.TO_BE_VERIFIED });
+
+    await fhir({ refresh: 'ServiceRequest' });
+
+    await fhirImagingServiceRequest.reload();
+    await fhirLabServiceRequest.reload();
+    // See mapping at packages/shared/src/models/fhir/ServiceRequest/getValues.js
+    expect(fhirImagingServiceRequest.status).toEqual(FHIR_REQUEST_STATUS.ACTIVE);
+    expect(fhirLabServiceRequest.status).toEqual(FHIR_REQUEST_STATUS.ACTIVE);
   });
 });
