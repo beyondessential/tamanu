36204f42edb3743948452b5d4a0ebb42f95e5a2b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-09-04 12:32:09 +1200

    add error message to duplicate report name

--- packages/sync-server/app/admin/reports/createReportDefinitionVersion.js
+++ packages/sync-server/app/admin/reports/createReportDefinitionVersion.js
@@ -2,24 +2,9 @@ import Sequelize from 'sequelize';
 import { InvalidOperationError } from '@tamanu/shared/errors';
 import { verifyQuery } from './utils';
 
-const createReportDefinition = async (store, name) => {
-  const {
-    models: { ReportDefinition },
-  } = store;
-  try {
-    return (await ReportDefinition.create({ name })).id;
-  } catch (error) {
-    if (error.parent.code === '23505') {
-      throw new Error('Report name already exists');
-    } else {
-      throw new Error(error.message);
-    }
-  }
-};
-
 export async function createReportDefinitionVersion(store, reportId, definition, userId) {
   const {
-    models: { ReportDefinitionVersion },
+    models: { ReportDefinition, ReportDefinitionVersion },
     sequelize,
   } = store;
 
@@ -35,7 +20,16 @@ export async function createReportDefinitionVersion(store, reportId, definition,
     },
     async () => {
       const { name, ...definitionVersion } = definition;
-      const reportDefinitionId = reportId || (await createReportDefinition(store, name));
+      if (!reportId) {
+        const existingDefinition = await ReportDefinition.findOne({
+          where: { name },
+          attributes: ['id'],
+        });
+        if (existingDefinition) {
+          throw new InvalidOperationError('Report name already exists');
+        }
+      }
+      const reportDefinitionId = reportId || (await ReportDefinition.create({ name })).id;
       const latestVersion = await ReportDefinitionVersion.findOne({
         where: { reportDefinitionId: reportId },
         attributes: ['versionNumber'],
