7e604e8e1053bb515195ddace116f5d76d338ad0
tareq89 <tareq.aziz@dsinnovators.com>
2023-09-06 17:28:20 +0600

    feat: ChangeStatusFormModal added

---
+++ packages/desktop/app/views/programRegistry/ChangeStatusFormModal.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import * as yup from 'yup';
+import {
+  Modal,
+  ConfirmCancelRow,
+  Form,
+  FormSeparatorLine,
+  FormGrid,
+  AutocompleteField,
+  Field,
+} from '../../components';
+import { useSuggester } from '../../api';
+import { useAuth } from '../../contexts/Auth';
+import { foreignKey } from '../../utils/validation';
+
+export const ChangeStatusFormModal = ({ onSubmit, onCancel, program, patient }) => {
+  const { currentUser } = useAuth();
+  const programRegistryStatusSuggester = useSuggester('programRegistryClinicalStatus', {
+    baseQueryParameters: { programId: program.id },
+  });
+  return (
+    <Modal title="Change Status" open>
+      <Form
+        onSubmit={data =>
+          onSubmit({
+            ...data,
+            programId: program.id,
+            clinicianId: currentUser.id,
+            patientId: patient.id,
+          })
+        }
+        render={({ submitForm }) => {
+          const handleCancel = () => onCancel && onCancel();
+          return (
+            <div>
+              <FormGrid
+                columns={1}
+                style={{
+                  gridColumn: '1 / -1',
+                  width: '70%',
+                  display: 'block',
+                  margin: 'auto',
+                  marginTop: '30px',
+                }}
+              >
+                <Field
+                  name="programRegistryClinicalStatusId"
+                  label="Status"
+                  component={AutocompleteField}
+                  suggester={programRegistryStatusSuggester}
+                />
+              </FormGrid>
+              <FormSeparatorLine style={{ marginTop: '60px', marginBottom: '30px' }} />
+              <ConfirmCancelRow onConfirm={submitForm} onCancel={handleCancel} />
+            </div>
+          );
+        }}
+        initialValues={{
+          programRegistryClinicalStatusId: program.programRegistryClinicalStatusId,
+        }}
+        validationSchema={yup.object().shape({
+          programRegistryClinicalStatusId: foreignKey('Status must be selected'),
+        })}
+      />
+    </Modal>
+  );
+};
---
+++ packages/desktop/stories/programRegistry.stories.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { ChangeStatusFormModal } from '../app/views/programRegistry/ChangeStatusFormModal';
+import { MockedApi } from './utils/mockedApi';
+
+const mockProgramRegistrytFormEndpoints = {
+  'suggestions/programRegistryClinicalStatus': () => [
+    { id: '1', name: 'current' },
+    { id: '2', name: 'historical' },
+    { id: '3', name: 'merged' },
+  ],
+};
+
+storiesOf('Program Registry', module).add('ProgramRegistry Status Cahnge', () => {
+  return (
+    <MockedApi endpoints={mockProgramRegistrytFormEndpoints}>
+      <ChangeStatusFormModal
+        onSubmit={action('submit')}
+        onCancel={action('cancel')}
+        program={{ id: '3e2r23r23r' }}
+        patient={{ id: '3e2r23r23r' }}
+      />
+    </MockedApi>
+  );
+});
