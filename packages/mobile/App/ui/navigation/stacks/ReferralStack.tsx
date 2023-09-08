d75f7b0c62ef94a56bed779e2ac4835e6a28fbaf
Biao Li <lbit123@outlook.com>
2023-09-08 14:38:20 +1000

    Apply update to ReferralStack

--- packages/mobile/App/ui/navigation/stacks/ReferralStack.tsx
+++ packages/mobile/App/ui/navigation/stacks/ReferralStack.tsx
@@ -4,22 +4,36 @@ import { ReferralTabs } from './ReferralTabs';
 import { SurveyResponseDetailsScreen } from '../screens/programs/SurveyResponseDetailsScreen';
 import { Routes } from '/helpers/routes';
 import { ErrorBoundary } from '~/ui/components/ErrorBoundary';
+import { SurveyResponseScreen } from '../screens/programs/tabs/SurveyResponseScreen';
+import { SurveyTypes } from '~/types';
+import { ReduxStoreProps } from '~/ui/interfaces/ReduxStoreProps';
+import { PatientStateProps } from '~/ui/store/ducks/patient';
+import { useSelector } from 'react-redux';
 
 const Stack = createStackNavigator();
 
-export const ReferralStack = (): ReactElement => (
-  <ErrorBoundary>
-    <Stack.Navigator headerMode="none">
-      <Stack.Screen
-        name={Routes.HomeStack.ReferralStack.Index}
-        component={ReferralTabs}
-      />
-      <Stack.Screen
-        name={
-          Routes.HomeStack.ReferralStack.ViewHistory.SurveyResponseDetailsScreen
-        }
-        component={SurveyResponseDetailsScreen}
-      />
-    </Stack.Navigator>
-  </ErrorBoundary>
-);
+export const ReferralStack = (): ReactElement => {
+  const { selectedPatient } = useSelector(
+    (state: ReduxStoreProps): PatientStateProps => state.patient,
+  );
+
+  return (
+    <ErrorBoundary>
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name={Routes.HomeStack.ReferralStack.Index} component={ReferralTabs} />
+        <Stack.Screen
+          name={Routes.HomeStack.ReferralStack.ViewHistory.SurveyResponseDetailsScreen}
+          component={SurveyResponseDetailsScreen}
+        />
+        <Stack.Screen
+          name={Routes.HomeStack.ReferralStack.ReferralList.AddReferralDetails}
+          initialParams={{
+            selectedPatient,
+            surveyType: SurveyTypes.Referral,
+          }}
+          component={SurveyResponseScreen}
+        />
+      </Stack.Navigator>
+    </ErrorBoundary>
+  );
+};
