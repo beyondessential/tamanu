ef3bc5ac72a115c8cf6ed77fda2adfd3fc0cf0db
Daniel Nash <dannash200@gmail.com>
2023-09-12 10:57:02 +1200

    nass-857: set user settings for assumed role

--- packages/shared/src/utils/assumedRoles.js
+++ packages/shared/src/utils/assumedRoles.js
@@ -4,6 +4,18 @@ export async function performAssumedRoleOperation(context, role, query, replacem
   const { sequelize } = context;
   sequelize.transaction(async () => {
     await sequelize.query(`SET ROLE ${role}`);
+    const rolconfig = await sequelize.query(
+      `SELECT unnest(rolconfig) config from pg_roles where rolname = '${role}'`,
+      {
+        type: QueryTypes.SELECT,
+      },
+    );
+    // When assuming roles in postgres, the role's configuration is not automatically applied.
+    const roleSettingsQuery = rolconfig.reduce((acc, { config }) => {
+      const [key, value] = config.split('=');
+      return `${acc} SET ${key} TO ${value};`;
+    }, '');
+    await sequelize.query(roleSettingsQuery);
     const res = await sequelize.query(query, {
       type: QueryTypes.SELECT,
       replacements,
