33594a616a984d70677db4ad3d52e6dc4aa9116d
Daniel Nash <dannash200@gmail.com>
2023-09-14 11:38:55 +1200

    nass-739: pass db config unsanitized as now contains no secrets keys or passwords

--- packages/sync-server/app/health.js
+++ packages/sync-server/app/health.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
-import config from 'config';
-import { isObject } from 'lodash';
 
 import { log } from 'shared/services/logging';
 import { createMigrationInterface } from 'shared/services/migrations';
@@ -18,26 +16,6 @@ function uptime() {
   const uptimeS = Math.floor(uptimeMs / 1000);
   const uptimeM = uptimeS / 60;
   return `${Math.floor(uptimeM)}m${(uptimeS % 60).toFixed(0)}s`;
-}
-
-// quick utility to recurse through an object
-// (to use with sanitising the config object)
-function recurse(object, cb, prefix = '') {
-  return Object.entries(object).reduce((state, [k, v]) => {
-    if (isObject(v)) {
-      return { ...state, [k]: recurse(v, cb, `${prefix}${k}.`) };
-    }
-    return { ...state, [k]: cb(`${prefix}${k}`, v) };
-  }, {});
-}
-
-function sanitise(object) {
-  const re = /secret|key|password/i;
-  return recurse(object, (k, v) => {
-    if (!v) return v;
-    if (!k.match(re)) return v;
-    return '********';
-  });
 }
 
 async function getMigrations(sequelize) {
@@ -73,6 +51,8 @@ healthRoutes.get(
       return;
     }
 
+    const config = await req.settings.get();
+
     res.send({
       version,
       uptime: uptime(),
@@ -82,8 +62,7 @@ healthRoutes.get(
         options: req.store.sequelize.options,
         ...(await getMigrations(req.store.sequelize)),
       },
-      // TODO
-      config: sanitise(config),
+      config,
     });
   }),
 );
