6f1e807c508a0c9fd355df838b7fd17e33afe3a4
Daniel Nash <dannash200@gmail.com>
2023-09-14 14:17:03 +1200

    nass-739: fix mistake

--- packages/sync-server/app/admin/importSchemas/baseSchemas.js
+++ packages/sync-server/app/admin/importSchemas/baseSchemas.js
@@ -162,12 +162,12 @@ export const baseConfigShape = yup.object().noUnknown();
 
 export const SurveyScreenComponent = Base.shape({
   visibilityCriteria: jsonString(),
-  validationCriteria: yup.string().whenSetting('validateQuestionEnabled', {
+  validationCriteria: yup.string().whenSetting('validateQuestionConfig', {
     is: true,
     then: validationString(baseValidationShape),
     otherwise: jsonString(),
   }),
-  config: yup.string().whenSetting('validateQuestionEnabled', {
+  config: yup.string().whenSetting('validateQuestionConfig', {
     is: true,
     then: configString(baseConfigShape),
     otherwise: jsonString(),
