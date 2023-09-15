ade921c54e40065279c585864772a031746584f4
Daniel Nash <dannash200@gmail.com>
2023-09-15 14:03:30 +1200

    nass-739: fix issue

--- packages/sync-server/app/admin/programImporter/programImporter.js
+++ packages/sync-server/app/admin/programImporter/programImporter.js
@@ -1,7 +1,7 @@
 import { log } from 'shared/services/logging';
 import { readFile } from 'xlsx';
 
-import { ReadSettings } from '@tamanu/settings';
+import config from 'config';
 import { importRows } from '../importRows';
 
 import { readMetadata } from './readMetadata';
@@ -24,8 +24,7 @@ export async function programImporter({ errors, models, stats, file, whitelist =
 
   const workbook = readFile(file);
 
-  const readSettings = new ReadSettings(models);
-  const canonicalHostName = await readSettings.get('canonicalHostName');
+  const { canonicalHostName } = config;
   const { programRecord, surveyMetadata } = await readMetadata(
     workbook.Sheets.Metadata,
     canonicalHostName,
@@ -62,7 +61,6 @@ export async function programImporter({ errors, models, stats, file, whitelist =
   for (const surveyInfo of surveysToImport) {
     try {
       const context = createContext(surveyInfo.name);
-      console.log({ context });
       const result = await importSurvey(context, workbook, surveyInfo);
       stats.push(result);
     } catch (e) {
