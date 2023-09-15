36abb0ec9aec1311e75209a81d0cd09f6e4e5e92
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-09-15 14:24:37 +1200

    fix horizontal scroll bug when editing

--- packages/desktop/app/views/administration/settings/JSONEditor.js
+++ packages/desktop/app/views/administration/settings/JSONEditor.js
@@ -96,7 +96,7 @@ export const JSONEditor = React.memo(
         onLoad={onLoad}
         tabSize={2}
         markers={marker ? [marker] : null}
-        wrapEnabled
+        wrapEnabled={!editMode}
       />
     );
   },
