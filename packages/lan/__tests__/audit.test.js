767064e424882c3b41b87ca889e44ade2a8de951
Renato Koyama <renato.koyama@gmail.com>
2023-09-16 11:35:28 -0300

    test: fix audit logs tests

--- packages/lan/__tests__/audit.test.js
+++ packages/lan/__tests__/audit.test.js
@@ -1,19 +1,22 @@
-import { createTestContext } from './utilities';
 import { fake } from 'shared/test-helpers';
-import { AuditLogItem } from '../app/middleware/auditLog';
 import { createDummyPatient } from 'shared/demoData/patients';
+import { SETTINGS_SCOPES } from '@tamanu/constants';
+import config from 'config';
+import { createTestContext } from './utilities';
+import { AuditLogItem } from '../app/middleware/auditLog';
 
 describe('Audit log', () => {
   let ctx = null;
   let app = null;
   let baseApp = null;
   let models = null;
-  
+  const ENABLE_AUDIT_LOG_SETTINGS_KEY = 'log.enableAuditLog';
+
   // log entries aren't persisted (yet), so use this array to track audit log entries
   // as they get resolved.
   const recentEntries = [];
   // this flushes the recent entries array as well, so that tests can use it without additional boilerplate
-  const getRecentEntries = () => { 
+  const getRecentEntries = () => {
     const c = [...recentEntries];
     recentEntries.length = 0; // erase the array
     return c;
@@ -23,10 +26,17 @@ describe('Audit log', () => {
     jest.spyOn(AuditLogItem.prototype, 'resolve').mockImplementation(function() {
       recentEntries.push(this);
     });
-    
+
     ctx = await createTestContext();
     baseApp = ctx.baseApp;
     models = ctx.models;
+    const { Setting } = models;
+    await Setting.set(
+      ENABLE_AUDIT_LOG_SETTINGS_KEY,
+      true,
+      config.serverFacilityId,
+      SETTINGS_SCOPES.FACILITY,
+    );
     app = await baseApp.asRole('practitioner');
   });
   afterAll(() => {
@@ -41,7 +51,7 @@ describe('Audit log', () => {
 
     const [log] = getRecentEntries();
     expect(log).toHaveProperty('userId');
-    expect(log.permissionChecks[0]).toMatchObject({ 
+    expect(log.permissionChecks[0]).toMatchObject({
       noun: 'PatientAllergy',
       verb: 'create',
       objectId: undefined,
@@ -54,7 +64,7 @@ describe('Audit log', () => {
 
     const [log] = getRecentEntries();
     expect(log).toHaveProperty('userId');
-    expect(log.permissionChecks[0]).toMatchObject({ 
+    expect(log.permissionChecks[0]).toMatchObject({
       noun: 'PatientAllergy',
       verb: 'create',
       objectId: undefined,
@@ -76,7 +86,7 @@ describe('Audit log', () => {
     expect(log.permissionChecks[0]).toMatchObject({
       verb: 'read',
       noun: 'Patient',
-      objectId: undefined,  // intentional; happens before looking up a particular patient
+      objectId: undefined, // intentional; happens before looking up a particular patient
     });
     expect(log.permissionChecks[1]).toMatchObject({
       verb: 'write',
@@ -86,7 +96,7 @@ describe('Audit log', () => {
   });
 
   it('should discard an audit log when appropriate', async () => {
-    // we don't care about the result of this, we just need any endpoint that doesn't 
+    // we don't care about the result of this, we just need any endpoint that doesn't
     // perform any permission checks - login is one of these
     await app.post('/v1/login').send({});
 
