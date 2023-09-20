ba5f3e978a541c70848486292cef5d9ed193a86c
Daniel Nash <dannash200@gmail.com>
2023-09-21 11:53:05 +1200

    nass-739: fix program importer tests! woo

--- packages/sync-server/app/utils/customYupMethods.js
+++ packages/sync-server/app/utils/customYupMethods.js
@@ -1,17 +1,17 @@
 import * as yup from 'yup';
 
 yup.addMethod(yup.string, 'whenSetting', function whenSetting(key, { is, then, otherwise }) {
-  return this.test('whenSetting', '', async function doTest(value) {
+  return this.test('whenSetting', 'failed When setting', async function doTest(value) {
     const { settings } = this.options.context;
     if (!settings) {
       throw new Error(
         'Settings reader must be passed to yup validation context to use custom "whenSetting" method',
       );
     }
-    const config = await settings.get(key);
-    if (typeof is === 'function' ? is(config) : is === config) {
-      return then.validate(value);
-    }
-    return otherwise.validate(value);
+    const settingValue = await settings.get(key);
+    const settingIs = typeof is === 'function' ? is(settingValue) : is === settingValue;
+    const schema = settingIs ? then : otherwise;
+    await schema.validate(value);
+    return true;
   });
 });
