7228446a5c81523d842c2a405527175333e5627e
Biao Li <lbit123@outlook.com>
2023-09-26 13:43:11 +1000

    Filter out vital program

--- packages/mobile/App/ui/navigation/screens/programs/ProgramListScreen.tsx
+++ packages/mobile/App/ui/navigation/screens/programs/ProgramListScreen.tsx
@@ -11,17 +11,32 @@ import { withPatient } from '/containers/Patient';
 import { useBackendEffect } from '~/ui/hooks';
 import { ErrorScreen } from '~/ui/components/ErrorScreen';
 import { Program } from '~/models/Program';
+import { LoadingScreen } from '~/ui/components/LoadingScreen';
 
 const Screen = (): ReactElement => {
   const navigation = useNavigation();
 
-  const [programs, error] = useBackendEffect(({ models }) =>
+  const [programs, programsError, programsIsLoading] = useBackendEffect(({ models }) =>
     models.Program.find({
       order: {
         name: 'ASC',
       },
     }),
   );
+
+  const [vitalsSurvey, vitalsError, isVitalsLoading] = useBackendEffect(({ models: m }) =>
+    m.Survey.getVitalsSurvey(),
+  );
+
+  if (programsIsLoading || isVitalsLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (programsError || vitalsError) {
+    return <ErrorScreen error={programsError || vitalsError} />;
+  }
+
+  const programsWithNoVital = programs.filter(program => program.id !== vitalsSurvey?.programId);
 
   const onNavigateToSurveyList = (program: Program): void => {
     navigation.navigate(Routes.HomeStack.ProgramStack.ProgramTabs.SurveyTabs.Index, {
@@ -44,7 +59,7 @@ const Screen = (): ReactElement => {
             paddingTop: 5,
           }}
           showsVerticalScrollIndicator={false}
-          data={programs}
+        data={programsWithNoVital}
           keyExtractor={(item): string => item.id}
           renderItem={({ item }): ReactElement => (
             <MenuOptionButton
