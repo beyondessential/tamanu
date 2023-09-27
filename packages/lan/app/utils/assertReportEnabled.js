d204dc1972ca5d85c87094f138f140d495b3a6c8
Daniel Nash <dannash200@gmail.com>
2023-09-28 09:50:56 +1300

    nass-739: fix disabled issue 2

--- packages/lan/app/utils/assertReportEnabled.js
+++ packages/lan/app/utils/assertReportEnabled.js
@@ -1,10 +1,5 @@
 import { ForbiddenError } from 'shared/errors';
 
-export const isReportEnabled = (localisation, reportId) => {
-  const disabledReports = localisation?.disabledReports || [];
-  return disabledReports.includes(reportId);
-};
-
 export const assertReportEnabled = async (settings, reportId) => {
   const disabledReports = await settings.get('disabledReports');
   if (disabledReports.includes(reportId)) {
