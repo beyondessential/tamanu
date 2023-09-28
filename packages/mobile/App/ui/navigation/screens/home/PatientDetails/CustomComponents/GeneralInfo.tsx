75b1872759ca9a7cf44c1177a64a917772295ec5
Tim Kennedy <t.m.knndy@gmail.com>
2023-09-28 13:59:17 +1000

    Fix stale data in General Information

--- packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/GeneralInfo.tsx
+++ packages/mobile/App/ui/navigation/screens/home/PatientDetails/CustomComponents/GeneralInfo.tsx
@@ -1,4 +1,6 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useCallback, useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
+import { compose } from 'redux';
 
 import { formatStringDate } from '/helpers/date';
 import { DateFormats } from '/helpers/constants';
@@ -7,15 +9,21 @@ import { PatientSection } from './PatientSection';
 import { useLocalisation } from '~/ui/contexts/LocalisationContext';
 import { getGender } from '~/ui/helpers/user';
 import { IPatient } from '~/types';
+import { withPatient } from '~/ui/containers/Patient';
+import { Patient } from '~/models/Patient';
+import { LoadingScreen } from '~/ui/components/LoadingScreen';
+import { ErrorScreen } from '~/ui/components/ErrorScreen';
 
 interface GeneralInfoProps {
   onEdit: () => void;
+  setSelectedPatient: (patient: IPatient) => void;
   patient: IPatient;
 }
 
-export const GeneralInfo = ({
+const DumbGeneralInfo = ({
   onEdit,
   patient,
+  setSelectedPatient,
 }: GeneralInfoProps): ReactElement => {
   const fields = [
     ['firstName', patient.firstName],
@@ -27,10 +35,30 @@ export const GeneralInfo = ({
     ['email', patient.email],
     ['villageId', patient.village?.name ?? ''],
   ];
+  const [error, setError] = useState<Error | undefined>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Reload patient when general info is focused
+  useFocusEffect(
+    useCallback(async () => {
+      try {
+        setIsLoading(true);
+        const reloadedPatient = await Patient.findOne(patient.id);
+        setSelectedPatient(reloadedPatient);
+      } catch (e) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }, [patient.id]),
+  );
 
   // Check if patient information should be editable
   const { getBool } = useLocalisation();
   const isEditable = getBool('features.editPatientDetailsOnMobile');
+
+  if (isLoading) return <LoadingScreen />;
+  if (error) return <ErrorScreen error={error} />;
 
   return (
     <PatientSection
@@ -41,3 +69,5 @@ export const GeneralInfo = ({
     </PatientSection>
   );
 };
+
+export const GeneralInfo = compose(withPatient)(DumbGeneralInfo);
