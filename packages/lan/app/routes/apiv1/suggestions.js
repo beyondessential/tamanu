af93e3fe96dedacce7757e2960b9b264ef809be6
Alastair-L <lockealastair@gmail.com>
2023-10-03 11:18:34 +1100

    Lint

--- packages/lan/app/routes/apiv1/suggestions.js
+++ packages/lan/app/routes/apiv1/suggestions.js
@@ -213,7 +213,7 @@ createNameSuggester('facilityLocationGroup', 'LocationGroup', (search, query) =>
 );
 
 createNameSuggester('survey', 'Survey', (search, { programId }) => ({
-    name: { [Op.iLike]: search },
+  name: { [Op.iLike]: search },
   ...(programId ? { programId } : programId),
   surveyType: {
     [Op.notIn]: [SURVEY_TYPES.OBSOLETE, SURVEY_TYPES.VITALS],
