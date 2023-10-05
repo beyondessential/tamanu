cc30f9149e4f5c90699ed5fc382da97432b4c105
Biao Li <lbit123@outlook.com>
2023-10-05 11:36:39 +1100

    Make unit test more restrict value keys should use data element ids

--- packages/desktop/__tests__/utils/survey.test.js
+++ packages/desktop/__tests__/utils/survey.test.js
@@ -3,10 +3,10 @@ import { checkVisibility } from '../../app/utils';
 describe('checkVisibility()', () => {
   const generateAllComponents = components =>
     components.map((component, index) => ({
-      id: `component-${component.code}`,
+      code: `component-${component.code}`,
       required: false,
       dataElement: {
-        id: component.code,
+        id: component.id,
         code: component.code,
         name: component.name,
         type: component.type,
@@ -24,8 +24,8 @@ describe('checkVisibility()', () => {
 
   it('should be visible without any visibility criteria', () => {
     const allComponents = generateAllComponents([
-      { code: 'TEST', type: 'Number' },
-      { code: 'TEST_2', type: 'Number' },
+      { id: 'TEST_ID', code: 'TEST_CODE', type: 'Number' },
+      { id: 'TEST_2_ID', code: 'TEST_2_CODE', type: 'Number' },
     ]);
 
     const result = checkVisibility(allComponents[0], { TEST: 1, TEST_2: 2 }, allComponents);
@@ -34,14 +34,19 @@ describe('checkVisibility()', () => {
 
   it('should not be visible if component type is result', () => {
     const allComponents = generateAllComponents([
-      { code: 'TEST_ALWAYS', type: 'Result' },
-      { code: 'REF', type: 'Binary' },
-      { code: 'TEST_CHECK', type: 'Result', visibilityCriteria: 'REF: Yes' },
+      { id: 'TEST_ALWAYS_ID', code: 'TEST_ALWAYS_CODE', type: 'Result' },
+      { id: 'REF_ID', code: 'REF_CODE', type: 'Binary' },
+      {
+        id: 'TEST_CHECK_ID',
+        code: 'TEST_CHECK_CODE',
+        type: 'Result',
+        visibilityCriteria: 'REF_CODE: Yes',
+      },
     ]);
 
     const result = checkVisibility(
       allComponents[2],
-      { TEST_CHECK: 100, TEST_ALWAYS: 0, REF: true },
+      { TEST_CHECK_ID: 100, TEST_ALWAYS_ID: 0, REF_ID: true },
       allComponents,
     );
 
@@ -50,14 +55,19 @@ describe('checkVisibility()', () => {
 
   it('should be visible if criteria is met', () => {
     const allComponents = generateAllComponents([
-      { code: 'TEST_ALWAYS', type: 'Result' },
-      { code: 'REF', type: 'Binary' },
-      { code: 'TEST_CHECK', type: 'Binary', visibilityCriteria: 'REF: Yes' },
+      { id: 'TEST_ALWAYS_ID', code: 'TEST_ALWAYS_CODE', type: 'Result' },
+      { id: 'REF_ID', code: 'REF_CODE', type: 'Binary' },
+      {
+        id: 'TEST_CHECK_ID',
+        code: 'TEST_CHECK_CODE',
+        type: 'Binary',
+        visibilityCriteria: 'REF_ID: Yes',
+      },
     ]);
 
     const result = checkVisibility(
       allComponents[2],
-      { TEST_CHECK: 100, TEST_ALWAYS: 0, REF: true },
+      { TEST_CHECK_ID: 100, TEST_ALWAYS_ID: 0, REF_ID: true },
       allComponents,
     );
 
@@ -66,14 +76,19 @@ describe('checkVisibility()', () => {
 
   it('should ignore a non-visible result field', () => {
     const allComponents = generateAllComponents([
-      { code: 'TEST_ALWAYS', type: 'Result' },
-      { code: 'REF', type: 'Binary' },
-      { code: 'TEST_CHECK', type: 'Binary', visibilityCriteria: 'REF: Yes' },
+      { id: 'TEST_ALWAYS_ID', code: 'TEST_ALWAYS_CODE', type: 'Result' },
+      { id: 'REF_ID', code: 'REF_CODE', type: 'Binary' },
+      {
+        id: 'TEST_CHECK_ID',
+        code: 'TEST_CHECK_CODE',
+        type: 'Binary',
+        visibilityCriteria: 'REF_CODE: Yes',
+      },
     ]);
 
     const result = checkVisibility(
       allComponents[2],
-      { TEST_CHECK: 0, TEST_ALWAYS: 100, REF: false },
+      { TEST_CHECK_ID: 0, TEST_ALWAYS_ID: 100, REF_ID: false },
       allComponents,
     );
 
@@ -83,31 +98,32 @@ describe('checkVisibility()', () => {
   describe('Range type criteria', () => {
     const allComponents = generateAllComponents([
       {
-        code: 'TEST_RESULT',
+        id: 'TEST_RESULT_ID',
+        code: 'TEST_RESULT_CODE',
         type: 'Binary',
         visibilityCriteria: JSON.stringify({
-          TEST_A: { type: 'range', start: 30 },
-          TEST_B: { type: 'range', end: 50 },
+          TEST_A_CODE: { type: 'range', start: 30 },
+          TEST_B_CODE: { type: 'range', end: 50 },
           _conjunction: 'and',
         }),
       },
-      { code: 'TEST_A', type: 'Binary' },
-      { code: 'TEST_B', type: 'Binary' },
+      { id: 'TEST_A_ID', code: 'TEST_A_CODE', type: 'Binary' },
+      { id: 'TEST_B_ID', code: 'TEST_B_CODE', type: 'Binary' },
     ]);
 
     const testData = [
-      { TEST_A: 30, TEST_B: 40, expected: true },
-      { TEST_A: 30, TEST_B: 50, expected: false },
-      { TEST_A: 30, TEST_B: 51, expected: false },
-      { TEST_A: 29, TEST_B: 40, expected: false },
+      { TEST_A_ID: 30, TEST_B_ID: 40, expected: true },
+      { TEST_A_ID: 30, TEST_B_ID: 50, expected: false },
+      { TEST_A_ID: 30, TEST_B_ID: 51, expected: false },
+      { TEST_A_ID: 29, TEST_B_ID: 40, expected: false },
     ];
 
     it.each(testData)(
       'should return $expected for TEST_A: $TEST_A and TEST_B: $TEST_B',
-      ({ TEST_A, TEST_B, expected }) => {
+      ({ TEST_A_ID, TEST_B_ID, expected }) => {
         const result = checkVisibility(
           allComponents[0],
-          { TEST_A, TEST_B, TEST_RESULT: 20 },
+          { TEST_A_ID, TEST_B_ID, TEST_RESULT: 20 },
           allComponents,
         );
 
