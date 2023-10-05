02fec93db8a1b1913581e4a00df9471d36d33b63
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-05 15:09:45 +1300

    reogranise component

--- packages/desktop/app/components/Translation/TranslatedText.js
+++ packages/desktop/app/components/Translation/TranslatedText.js
@@ -9,13 +9,12 @@ const DebugHighlighed = styled.span`
 
 const replaceStringVariables = (templateString, replacements) => {
   const colonReplacementRegex = /:([a-zA-Z]+)/g;
-  const stringParts = templateString.split(colonReplacementRegex)
+  const stringParts = templateString.split(colonReplacementRegex);
   const jsxElements = stringParts.map((part, index) => {
     // Even indexes are the unchanged parts of the string
     if (index % 2 === 0) {
       return part;
     }
-
     // Return the replacement if exists
     return replacements[part] || `:${part}`;
   });
@@ -26,20 +25,20 @@ const replaceStringVariables = (templateString, replacements) => {
 // eslint-disable-next-line no-unused-vars
 export const TranslatedText = ({ stringId, fallback, replacements }) => {
   const translation = null; // Placeholder for checking db for translation
-  const debugMode = JSON.parse(localStorage.getItem('debugTranslation'));
-
-  const TextWrapper = debugMode ? DebugHighlighed : React.Fragment;
 
   if (!translation) {
     // Register as untranslated in DB
-    return (
-      <TextWrapper>
-        {replacements ? replaceStringVariables(fallback, replacements) : fallback}
-      </TextWrapper>
-    );
   }
 
-  return <TextWrapper>{replaceStringVariables(translation, replacements)}</TextWrapper>;
+  const stringToReplace = translation || fallback;
+  const stringWithReplacements = replacements
+    ? replaceStringVariables(stringToReplace, replacements)
+    : stringToReplace;
+
+  const debugMode = JSON.parse(localStorage.getItem('debugTranslation'));
+  const TextWrapper = debugMode ? DebugHighlighed : React.Fragment;
+
+  return <TextWrapper>{stringWithReplacements}</TextWrapper>;
 };
 
 TranslatedText.propTypes = {
