7a85d51a4349e64a578a0f0d47b3d8d5d61b0254
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-05 13:17:05 +1300

    stop linting error

--- packages/desktop/app/components/Translation/TranslatedText.js
+++ packages/desktop/app/components/Translation/TranslatedText.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// "stringId" is used in future functionality
+// eslint-disable-next-line no-unused-vars
 export const TranslatedText = ({ stringId, fallback }) => {
   const translation = null; // Placeholder for checking db for translation
 
