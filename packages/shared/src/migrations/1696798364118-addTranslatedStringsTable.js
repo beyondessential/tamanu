c51209635c79bda40220c7376e56450a2bcf6b7a
Daniel Nash <dannash200@gmail.com>
2023-10-09 10:12:12 +1300

    nass-891: add translated strings table migration

---
+++ packages/shared/src/migrations/1696798364118-addTranslatedStringsTable.js
@@ -0,0 +1,62 @@
+import Sequelize, { DataTypes } from 'sequelize';
+
+export async function up(query) {
+  await query.createTable(
+    'translated_strings',
+    {
+      // For translated_strings, we use a composite primary key of string_id plus language,
+      // N.B. because ';' is used to join the two, we replace any actual occurrence of ';' with ':'
+      // to avoid clashes on the joined id
+      id: {
+        type: `TEXT GENERATED ALWAYS AS (REPLACE("string_id", ';', ':') || ';' || REPLACE("language", ';', ':')) STORED`,
+      },
+      string_id: {
+        type: DataTypes.TEXT,
+        required: true,
+      },
+      language: {
+        type: DataTypes.TEXT,
+        required: true,
+      },
+      text: {
+        type: DataTypes.TEXT,
+        required: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.fn('current_timestamp', 3),
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.fn('current_timestamp', 3),
+        allowNull: false,
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      uniqueKeys: {
+        string_language_unique: {
+          fields: ['string_id', 'language'],
+        },
+      },
+      indexes: [
+        {
+          name: 'string_id_index',
+          fields: ['string_id'],
+        },
+        {
+          name: 'language_index',
+          fields: ['language'],
+        },
+      ],
+    },
+  );
+}
+
+export async function down(query) {
+  await query.dropTable('translated_strings');
+}
