36f049141f36cee2e97ed69ec6734de738ed1e3d
Daniel Nash <dannash200@gmail.com>
2023-10-09 10:22:37 +1300

    nass-891: add translated strings model

---
+++ packages/shared/src/models/TranslatedString.js
@@ -0,0 +1,50 @@
+import { SYNC_DIRECTIONS } from '@tamanu/constants';
+import { DataTypes } from 'sequelize';
+import { Model } from './Model';
+
+export class TranslatedStrings extends Model {
+  static init(options) {
+    super.init(
+      {
+        id: {
+          // translated_string records use a generated primary key that enforces one per string and language,
+          type: `TEXT GENERATED ALWAYS AS (REPLACE("string_id", ';', ':') || ';' || REPLACE("language", ';', ':')) STORED`,
+          set() {
+            // any sets of the convenience generated "id" field can be ignored
+          },
+        },
+        stringId: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        language: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        text: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        ...options,
+        syncDirection: SYNC_DIRECTIONS.BIDIRECTIONAL,
+        indexes: [
+          {
+            name: 'string_language_unique',
+            fields: ['string_id', 'language'],
+            unique: true,
+          },
+          {
+            name: 'string_id_index',
+            fields: ['string_id'],
+          },
+          {
+            name: 'language_index',
+            fields: ['language'],
+          },
+        ],
+      },
+    );
+  }
+}
