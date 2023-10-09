486b81f7a915119cadb2101007e2a047eb699e52
Alastair-L <lockealastair@gmail.com>
2023-10-10 10:31:45 +1100

    Add test for endpoint

--- packages/lan/__tests__/apiv1/PatientProgramRegistration.test.js
+++ packages/lan/__tests__/apiv1/PatientProgramRegistration.test.js
@@ -149,4 +149,37 @@ describe('PatientProgramRegistration', () => {
       expect(createdRegistration.updatedAt).not.toEqual(existingRegistration.updatedAt);
     });
   });
+
+  describe('POST patient/:patientId/programRegistration/:programRegistryId/condition', () => {
+    it('creates a new condition', async () => {
+      const patient = await models.Patient.create(fake(models.Patient));
+      const program1 = await models.Program.create(fake(models.Program));
+      const programRegistry1 = await models.ProgramRegistry.create(
+        fake(models.ProgramRegistry, { programId: program1.id }),
+      );
+      const programRegistryCondition = await models.ProgramRegistryCondition.create(
+        fake(models.ProgramRegistryCondition, { programRegistryId: programRegistry1.id }),
+      );
+      const result = await app
+        .post(`/v1/patient/${patient.id}/programRegistration/${programRegistry1.id}/condition`)
+        .send({
+          programRegistryConditionId: programRegistryCondition.id,
+          date: '2023-09-02 08:00:00',
+          // clinicianId: clinician.id, // No clinician, just to switch it up
+        });
+
+      expect(result).toHaveSucceeded();
+
+      const createdCondition = await models.PatientProgramRegistrationCondition.findByPk(
+        result.body.id,
+      );
+
+      expect(createdCondition).toMatchObject({
+        programRegistryId: programRegistry1.id,
+        patientId: patient.id,
+        programRegistryConditionId: programRegistryCondition.id,
+        date: '2023-09-02 08:00:00',
+      });
+    });
+  });
 });
