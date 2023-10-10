3f7a56423756359e064075c88d14460e8bc5e25c
Chris Tran <chris@beyondessential.com.au>
2023-10-11 10:26:05 +1100

    sav-444: added server for testing

--- packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
+++ packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
@@ -17,7 +17,12 @@ const fetchServers = async (): Promise<SelectOption[]> => {
   // To use a local server, just edit this and select it.
   // The sync server config is sticky, so you can safely revert it after
   // the first sync begins and it'll stay connecting to your local server.
-  // return [{ label: 'Local', value: 'http://192.168.0.1:3000' }];
+  return [
+    {
+      label: 'Local',
+      value: 'https://central.feature-sav-444-ability-to-make-pad-fields-ma.internal.tamanu.io',
+    },
+  ];
 
   // allows overriding the central server list or meta server in builds
   const { metaServer: metaServerOverride, centralServers: centralServerOverrides } = overrides;
