e893f735b7c491945b5bfa7a208421d01e660b86
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-16 11:25:26 +1300

    add translation permission to roles

--- packages/shared/src/roles.js
+++ packages/shared/src/roles.js
@@ -225,6 +225,8 @@ export const practitioner = [
   { verb: 'list', noun: 'EncounterNote' },
   { verb: 'create', noun: 'EncounterNote' },
   { verb: 'write', noun: 'EncounterNote' },
+
+  { verb: 'list', noun: 'Translation' },
 ];
 
 // "Manage all" is a special case in CASL for the admin to grant everything
