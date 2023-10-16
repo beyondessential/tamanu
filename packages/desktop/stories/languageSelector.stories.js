8260b96525245ae0203d90579aa634e983fd0dd7
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-16 15:43:00 +1300

    setup storybook

---
+++ packages/desktop/stories/languageSelector.stories.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+import { LANGUAGE_CODES, LANGUAGE_NAMES } from '@tamanu/constants';
+import { LanguageSelector } from '../app/components/LanguageSelector';
+import { ApiContext } from '../app/api';
+
+const dummyApi = {
+  get: async () => {
+    return [
+      {
+        label: LANGUAGE_NAMES[LANGUAGE_CODES.ENGLISH],
+        value: LANGUAGE_CODES.ENGLISH,
+      },
+      {
+        label: LANGUAGE_NAMES[LANGUAGE_CODES.KHMER],
+        value: LANGUAGE_CODES.KHMER,
+      },
+    ];
+  },
+};
+
+const Container = styled.div`
+  padding: 1rem;
+  max-width: 500px;
+`;
+
+export default {
+  title: 'Translation/LanguageSelector',
+  component: LanguageSelector,
+};
+
+const Template = args => {
+  return (
+    <ApiContext.Provider value={dummyApi}>
+      <Container>
+        <LanguageSelector {...args} />
+      </Container>
+    </ApiContext.Provider>
+  );
+};
+
+export const Basic = Template.bind({});
+Basic.args = {
+  stringId: 'fruitBowl.banana',
+  fallback: 'Banana',
+};
