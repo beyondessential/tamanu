fc842092cb98dcdb35803866219eb7bc8bf7b1ab
Daniel Nash <dannash200@gmail.com>
2023-10-17 10:06:08 +1300

    nass-892: list route for translation

--- packages/sync-server/app/admin/translation/translationRoutes.js
+++ packages/sync-server/app/admin/translation/translationRoutes.js
@@ -1,3 +1,21 @@
 import express from 'express';
+import asyncHandler from 'express-async-handler';
 
 export const translationRouter = express.Router();
+
+translationRouter.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    req.checkPermission('list', 'Translation');
+    const { store } = req;
+    const {
+      models: { TranslatedString },
+    } = store;
+
+    const translatedStrings = await TranslatedString.findAll({
+      order: [['stringId', 'ASC']],
+    });
+
+    res.send(translatedStrings);
+  }),
+);
