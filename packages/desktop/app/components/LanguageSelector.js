8511ee06515963b6f9f930a9fa0fb68997f20ccd
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-17 09:49:32 +1300

    add style for language selector

--- packages/desktop/app/components/LanguageSelector.js
+++ packages/desktop/app/components/LanguageSelector.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
+import { Colors } from '../constants';
 import { useApi } from '../api';
 import { SelectInput } from './Field';
 
-export const LanguageSelector = () => {
+const LanguageSelectInput = styled(SelectInput)`
+  width: 143px;
+  .label-field {
+    font-family: Roboto;
+    font-size: 11px;
+    font-weight: 400;
+    line-height: 15px;
+    letter-spacing: 0px;
+    text-align: left;
+    color: #888888;
+  }
+  border-bottom: 0.1px solid ${Colors.primary};
+`;
+
+const customStyles = {
+  control: provided => ({
+    ...provided,
+    '&:hover': {
+      borderColor: 'transparent',
+    },
+    borderColor: 'transparent',
+    borderRadius: 0,
+    boxShadow: 'none',
+    cursor: 'pointer',
+    fontSize: '11px',
+  }),
+  indicatorSeparator: () => ({ display: 'none' }),
+  menu: provided => ({
+    ...provided,
+    marginTop: 5,
+    marginBottom: 0,
+    boxShadow: 'none',
+    borderWidth: '1px',
+    border: `1px solid ${Colors.primary}`,
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isFocused || state.isSelected ? Colors.hoverGrey : Colors.white,
+    ...(state.isDisabled ? {} : { color: Colors.darkestText }),
+    cursor: 'pointer',
+    fontSize: '11px',
+  }),
+};
+
+export const LanguageSelector = ({ selectedOption, onChange }) => {
   const api = useApi();
 
   const { data: languageOptions = [], error } = useQuery(['languageList'], () =>
@@ -11,6 +57,14 @@ export const LanguageSelector = () => {
   );
 
   return (
-    <SelectInput options={languageOptions} label="Language" isClearable={false} error={!!error} />
+    <LanguageSelectInput
+      value={selectedOption}
+      onChange={onChange}
+      options={languageOptions}
+      label="Language"
+      isClearable={false}
+      error={!!error}
+      customStyleObject={customStyles}
+    />
   );
 };
