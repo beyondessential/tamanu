5e9fa7f160cbfb9381e7dd03462598b2e2ff2ed5
Daniel Nash <dannash200@gmail.com>
2023-10-17 15:09:38 +1300

    nass-892: give short id as stringId for form if new rows created which can be turned into stringId on submittion

--- packages/desktop/app/views/administration/translation/TranslationForm.js
+++ packages/desktop/app/views/administration/translation/TranslationForm.js
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { LANGUAGE_CODES, LANGUAGE_NAMES_IN_ENGLISH } from '@tamanu/constants';
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { keyBy, mapValues, omit } from 'lodash';
 import { useApi } from '../../../api';
 import { Form, TableFormFields, TextField } from '../../../components';
 import { AccessorField } from '../../patients/components/AccessorField';
+import { Box, IconButton } from '@material-ui/core';
+import { AddCircle } from '@material-ui/icons';
+import shortid from 'shortid';
 
 const StyledTableFormFields = styled(TableFormFields)`
   thead tr th {
@@ -13,34 +16,69 @@ const StyledTableFormFields = styled(TableFormFields)`
   }
 `;
 
-const columns = [
-  {
-    key: 'stringId',
-    title: 'String ID',
-    accessor: ({ stringId }) => stringId,
-  },
-  ...Object.values(LANGUAGE_CODES).map(code => ({
-    key: code,
-    title: LANGUAGE_NAMES_IN_ENGLISH[code],
-    // If you don't pass the id like this the values will be nested on dot delimiter
-    accessor: row => <AccessorField id={`['${row.stringId}']`} name={code} component={TextField} />,
-  })),
-];
+const StyledIconButton = styled(IconButton)`
+  padding: 3px;
+  margin-left: 10px;
+  color: #2f4358;
+`;
 
 const useTranslationQuery = () => {
   const api = useApi();
   return useQuery(['translation'], () => api.get(`admin/translation`));
 };
 
-const renderForm = ({ data = [] }) => {
-  return <StyledTableFormFields columns={columns} data={data} />;
-};
-
 export const TranslationForm = () => {
-  const { data, error, isLoading } = useTranslationQuery();
+  const { data = [], error, isLoading } = useTranslationQuery();
+  const [additionalColumns, setAdditionalColumns] = useState([]);
+
   const initialValues = useMemo(
     () => mapValues(keyBy(data, 'stringId'), val => omit(val, 'stringId')),
     [data],
+  );
+
+  const columns = useMemo(
+    () => [
+      {
+        key: 'stringId',
+        title: (
+          <Box display="flex" alignItems="center">
+            String ID
+            <StyledIconButton
+              onClick={() => {
+                setAdditionalColumns([
+                  ...additionalColumns,
+                  {
+                    placeholderId: shortid(),
+                    stringId: '',
+                    ...Object.values(LANGUAGE_CODES).reduce(
+                      (acc, code) => ({ ...acc, [code]: '' }),
+                      {},
+                    ),
+                  },
+                ]);
+              }}
+            >
+              <AddCircle />
+            </StyledIconButton>
+          </Box>
+        ),
+        accessor: ({ stringId }) =>
+          stringId || <AccessorField name="stringId" component={TextField} />,
+      },
+      ...Object.values(LANGUAGE_CODES).map(code => ({
+        key: code,
+        title: LANGUAGE_NAMES_IN_ENGLISH[code],
+        // If you don't pass the id like this the values will be nested on dot delimiter
+        accessor: row => (
+          <AccessorField
+            id={`['${row.stringId || row.placeholderId}']`}
+            name={code}
+            component={TextField}
+          />
+        ),
+      })),
+    ],
+    [additionalColumns],
   );
 
   return (
@@ -50,7 +88,9 @@ export const TranslationForm = () => {
       onSubmit={() => {
         console.log('submit');
       }}
-      render={props => renderForm({ ...props, data })}
+      render={() => (
+        <StyledTableFormFields columns={columns} data={[...data, ...additionalColumns]} />
+      )}
     />
   );
 };
