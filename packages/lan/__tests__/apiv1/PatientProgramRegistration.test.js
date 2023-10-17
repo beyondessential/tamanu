d6dd59bda0a31d47ffd7e52ff1fe1c4d04f1307b
Alastair <lockealastair@gmail.com>
2023-10-17 14:23:50 +1100

    TAN-2357: List registrations for patient endpoint (#4810)
    * Marry frontend and backend
    
    * Give up on "best practice" ordering
    
    * Fix and add tests
    
    * Fix and update test
    
    * Clean up getName
    
    * Clean up storybook too
    
    * Continue cleanup

--- packages/lan/__tests__/apiv1/PatientProgramRegistration.test.js
+++ packages/lan/__tests__/apiv1/PatientProgramRegistration.test.js
@@ -27,34 +27,82 @@ describe('PatientProgramRegistration', () => {
       const clinician = await models.User.create(fake(models.User));
       const patient = await models.Patient.create(fake(models.Patient));
       const program1 = await models.Program.create(fake(models.Program));
-      const program2 = await models.Program.create(fake(models.Program));
       const programRegistry1 = await models.ProgramRegistry.create(
-        fake(models.ProgramRegistry, { programId: program1.id }),
+        fake(models.ProgramRegistry, { programId: program1.id, name: 'a' }),
       );
+      const programRegistryClinicalStatus = await models.ProgramRegistryClinicalStatus.create(
+        fake(models.ProgramRegistryClinicalStatus, {
+          programRegistryId: programRegistry1.id,
+          name: 'aa',
+        }),
+      );
+      const program2 = await models.Program.create(fake(models.Program));
       const programRegistry2 = await models.ProgramRegistry.create(
-        fake(models.ProgramRegistry, { programId: program2.id }),
+        fake(models.ProgramRegistry, { programId: program2.id, name: 'b' }),
       );
+      const program3 = await models.Program.create(fake(models.Program));
+      const programRegistry3 = await models.ProgramRegistry.create(
+        fake(models.ProgramRegistry, { programId: program3.id, name: 'a' }),
+      );
+      const program4 = await models.Program.create(fake(models.Program));
+      const programRegistry4 = await models.ProgramRegistry.create(
+        fake(models.ProgramRegistry, { programId: program4.id, name: 'a' }),
+      );
+
+      // Registration 1: Should show
       await models.PatientProgramRegistration.create(
         fake(models.PatientProgramRegistration, {
           programRegistryId: programRegistry1.id,
           clinicianId: clinician.id,
+          clinicalStatusId: programRegistryClinicalStatus.id,
+          registrationStatus: REGISTRATION_STATUSES.ACTIVE,
           patientId: patient.id,
-          date: '2023-09-02 08:00:00',
+          date: '2023-09-04 08:00:00',
+        }),
+      );
+
+      // Registry 2: Should show
+      await models.PatientProgramRegistration.create(
+        fake(models.PatientProgramRegistration, {
+          programRegistryId: programRegistry2.id,
+          registrationStatus: REGISTRATION_STATUSES.ACTIVE,
+          clinicianId: clinician.id,
+          patientId: patient.id,
+          date: '2023-09-04 08:00:00',
+        }),
+      );
+
+      // Registry 3: Should not show
+      await models.PatientProgramRegistration.create(
+        fake(models.PatientProgramRegistration, {
+          programRegistryId: programRegistry3.id,
+          registrationStatus: REGISTRATION_STATUSES.ACTIVE,
+          patientId: patient.id,
         }),
       );
       await models.PatientProgramRegistration.create(
         fake(models.PatientProgramRegistration, {
-          programRegistryId: programRegistry1.id,
-          clinicianId: clinician.id,
+          programRegistryId: programRegistry3.id,
+          registrationStatus: REGISTRATION_STATUSES.RECORDED_IN_ERROR,
+          patientId: patient.id,
+        }),
+      );
+
+      // Registry 4: Should show the most recent details
+      await models.PatientProgramRegistration.create(
+        fake(models.PatientProgramRegistration, {
+          programRegistryId: programRegistry4.id,
+          registrationStatus: REGISTRATION_STATUSES.INACTIVE,
           patientId: patient.id,
           date: '2023-09-04 08:00:00',
         }),
       );
       await models.PatientProgramRegistration.create(
         fake(models.PatientProgramRegistration, {
-          clinicianId: clinician.id,
-          programRegistryId: programRegistry2.id,
+          programRegistryId: programRegistry4.id,
+          registrationStatus: REGISTRATION_STATUSES.INACTIVE,
           patientId: patient.id,
+          date: '2023-09-04 09:00:00',
         }),
       );
 
@@ -63,15 +111,26 @@ describe('PatientProgramRegistration', () => {
       expect(result).toHaveSucceeded();
       expect(result.body.data).toMatchObject([
         {
+          registrationStatus: REGISTRATION_STATUSES.ACTIVE,
           clinicianId: clinician.id,
           date: '2023-09-04 08:00:00',
           patientId: patient.id,
           programRegistryId: programRegistry1.id,
+          programRegistry: {
+            name: 'a',
+          },
+          clinicalStatus: {
+            name: 'aa',
+          },
         },
         {
-          clinicianId: clinician.id,
-          patientId: patient.id,
+          registrationStatus: REGISTRATION_STATUSES.ACTIVE,
           programRegistryId: programRegistry2.id,
+        },
+        {
+          registrationStatus: REGISTRATION_STATUSES.INACTIVE,
+          date: '2023-09-04 09:00:00',
+          programRegistryId: programRegistry4.id,
         },
       ]);
     });
