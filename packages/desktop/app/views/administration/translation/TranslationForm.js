56bd7c935721f710f125833515c10dc322aa5ac7
Daniel Nash <dannash200@gmail.com>
2023-10-17 16:08:24 +1300

    nass-892: refactor to be cleaner

--- packages/desktop/app/views/administration/translation/TranslationForm.js
+++ packages/desktop/app/views/administration/translation/TranslationForm.js
@@ -4,7 +4,7 @@ import React, { useMemo, useState } from 'react';
 import styled from 'styled-components';
 import { keyBy, mapValues, omit } from 'lodash';
 import { Box, IconButton } from '@material-ui/core';
-import { AddCircle as AddCircleIcon, Delete as DeleteIcon } from '@material-ui/icons';
+import { Add as AddIcon, Delete as DeleteIcon } from '@material-ui/icons';
 import shortid from 'shortid';
 import { useApi } from '../../../api';
 import { Form, TableFormFields, TextField } from '../../../components';
@@ -27,71 +27,79 @@ const useTranslationQuery = () => {
   return useQuery(['translation'], () => api.get(`admin/translation`));
 };
 
+const StringIdHeadCell = ({ onClick }) => (
+  <Box display="flex" alignItems="center">
+    String ID
+    <StyledIconButton onClick={onClick}>
+      <AddIcon />
+    </StyledIconButton>
+  </Box>
+);
+
+const StringIdField = ({ placeholderId, stringId, onClick }) => {
+  if (!placeholderId) return stringId;
+  return (
+    <Box display="flex" alignItems="center">
+      <AccessorField id={placeholderId} name="stringId" component={TextField} />
+      <StyledIconButton>
+        <DeleteIcon onClick={onClick} />
+      </StyledIconButton>
+    </Box>
+  );
+};
+
+export const FormContents = ({ data, setFieldValue }) => {
+  const [additionalColumns, setAdditionalColumns] = useState([]);
+
+  const handleAddColumn = () => {
+    setAdditionalColumns([
+      ...additionalColumns,
+      {
+        placeholderId: shortid(),
+        stringId: '',
+        ...Object.values(LANGUAGE_CODES).reduce((acc, code) => ({ ...acc, [code]: '' }), {}),
+      },
+    ]);
+  };
+
+  const handleRemoveColumn = placeholderId => {
+    setAdditionalColumns(
+      additionalColumns.filter(column => column.placeholderId !== placeholderId),
+    );
+    setFieldValue(placeholderId, undefined);
+  };
+
+  const columns = [
+    {
+      key: 'stringId',
+      title: <StringIdHeadCell onClick={handleAddColumn} />,
+      accessor: ({ stringId, placeholderId }) => (
+        <StringIdField
+          placeholderId={placeholderId}
+          stringId={stringId}
+          onClick={() => handleRemoveColumn(placeholderId)}
+        />
+      ),
+    },
+    ...Object.values(LANGUAGE_CODES).map(code => ({
+      key: code,
+      title: LANGUAGE_NAMES_IN_ENGLISH[code],
+      accessor: ({ stringId, placeholderId }) => (
+        // If you don't pass the id like this the values will be nested on dot delimiter
+        <AccessorField id={`['${stringId || placeholderId}']`} name={code} component={TextField} />
+      ),
+    })),
+  ];
+
+  return <StyledTableFormFields columns={columns} data={[...data, ...additionalColumns]} />;
+};
+
 export const TranslationForm = () => {
   const { data = [], error, isLoading } = useTranslationQuery();
-  const [additionalColumns, setAdditionalColumns] = useState([]);
 
   const initialValues = useMemo(
     () => mapValues(keyBy(data, 'stringId'), val => omit(val, 'stringId')),
     [data],
-  );
-
-  const columns = useMemo(
-    () => [
-      {
-        key: 'stringId',
-        title: (
-          <Box display="flex" alignItems="center">
-            String ID
-            <StyledIconButton
-              onClick={() => {
-                setAdditionalColumns([
-                  ...additionalColumns,
-                  {
-                    placeholderId: shortid(),
-                    stringId: '',
-                    ...Object.values(LANGUAGE_CODES).reduce(
-                      (acc, code) => ({ ...acc, [code]: '' }),
-                      {},
-                    ),
-                  },
-                ]);
-              }}
-            >
-              <AddCircleIcon />
-            </StyledIconButton>
-          </Box>
-        ),
-        accessor: ({ stringId, placeholderId }) =>
-          stringId || (
-            <Box display="flex" alignItems="center">
-              <AccessorField id={placeholderId} name="stringId" component={TextField} />
-              <StyledIconButton>
-                <DeleteIcon
-                  onClick={() => {
-                    setAdditionalColumns(
-                      additionalColumns.filter(column => column.placeholderId !== placeholderId),
-                    );
-                  }}
-                />
-              </StyledIconButton>
-            </Box>
-          ),
-      },
-      ...Object.values(LANGUAGE_CODES).map(code => ({
-        key: code,
-        title: LANGUAGE_NAMES_IN_ENGLISH[code],
-        // If you don't pass the id like this the values will be nested on dot delimiter
-        accessor: row => (
-          <AccessorField
-            id={`['${row.stringId || row.placeholderId}']`}
-            name={code}
-            component={TextField}
-          />
-        ),
-      })),
-    ],
-    [additionalColumns],
   );
 
   return (
@@ -101,9 +109,7 @@ export const TranslationForm = () => {
       onSubmit={() => {
         console.log('submit');
       }}
-      render={() => (
-        <StyledTableFormFields columns={columns} data={[...data, ...additionalColumns]} />
-      )}
+      render={props => <FormContents {...props} data={data} />}
     />
   );
 };
