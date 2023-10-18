377c4ad2cd7d0b172f614ee2e9266dfcf1d481ef
Chris Tran <chris@beyondessential.com.au>
2023-10-18 11:17:32 +1100

    sav-397: fixed syntax error

--- packages/mobile/App/models/PatientFieldValue.ts
+++ packages/mobile/App/models/PatientFieldValue.ts
@@ -1,4 +1,11 @@
-import { BeforeInsert, Entity, Column, PrimaryColumn, ManyToOne, RelationId } from 'typeorm/browser';
+import {
+  BeforeInsert,
+  Entity,
+  Column,
+  PrimaryColumn,
+  ManyToOne,
+  RelationId,
+} from 'typeorm/browser';
 
 import { IPatientFieldValue } from '~/types';
 import { BaseModelWithoutId } from './BaseModelWithoutId';
@@ -45,7 +52,10 @@ export class PatientFieldValue extends BaseModelWithoutId implements IPatientFie
     return 'patient_field_values';
   }
 
-  static async getForPatientAndDefinition(patientId: string, definitionId: string): Promise<PatientFieldValue?> {
+  static async getForPatientAndDefinition(
+    patientId: string,
+    definitionId: string,
+  ): Promise<PatientFieldValue> {
     // use a query builder instead of find, as apparently there's some
     // misbehaviour around how typeorm traverses this relation
     return await PatientFieldValue.getRepository()
@@ -55,7 +65,11 @@ export class PatientFieldValue extends BaseModelWithoutId implements IPatientFie
       .getOne();
   }
 
-  static async updateOrCreateForPatientAndDefinition(patientId: string, definitionId: string, value: string): Promise<PatientFieldValue?> {
+  static async updateOrCreateForPatientAndDefinition(
+    patientId: string,
+    definitionId: string,
+    value: string,
+  ): Promise<PatientFieldValue> {
     const existing = await PatientFieldValue.getForPatientAndDefinition(patientId, definitionId);
     if (existing) {
       if (existing.value === value) return; // to avoid unnecessary updates
@@ -63,7 +77,7 @@ export class PatientFieldValue extends BaseModelWithoutId implements IPatientFie
       return existing.save();
     }
 
-    if (!value) return;  // to not create a null / empty string value entry
+    if (!value) return; // to not create a null / empty string value entry
     return PatientFieldValue.createAndSaveOne({
       patientId,
       definitionId,
