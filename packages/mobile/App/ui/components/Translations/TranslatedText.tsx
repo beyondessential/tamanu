9eb2b5de28ad01ec01c5f00c86a1241d3e331330
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-20 13:45:24 +1300

    add replacements logic

--- packages/mobile/App/ui/components/Translations/TranslatedText.tsx
+++ packages/mobile/App/ui/components/Translations/TranslatedText.tsx
@@ -3,16 +3,33 @@ import React, { ReactElement, useEffect, useState } from 'react';
 type TranslatedTextProps = {
   stringId: string;
   fallback: string;
+  replacements?: object;
 };
 
-export const TranslatedText = ({ stringId, fallback }: TranslatedTextProps): ReactElement => {
+// Duplicated from TranslatedText.js on desktop
+const replaceStringVariables = (templateString, replacements) => {
+    const jsxElements = templateString.split(/(:[a-zA-Z]+)/g).map((part, index) => {
+      // Even indexes are the unchanged parts of the string
+      if (index % 2 === 0) return part;
+      return replacements[part.slice(1)] || part;
+    });
+  
+    return jsxElements;
+  };
+
+export const TranslatedText = ({ stringId, fallback, replacements }: TranslatedTextProps): ReactElement => {
   const [displayElements, setDisplayElements] = useState(fallback);
   // Placeholder for fetching translation from context
-  const translation = null;
+  const translation = 'My name is :name';
+
+  const stringToDisplay = translation || fallback;
 
   useEffect(() => {
-    if (!translation) return;
-    setDisplayElements(translation);
+    if (!replacements) {
+        setDisplayElements(stringToDisplay);
+        return;
+    }
+    setDisplayElements(replaceStringVariables(stringToDisplay, replacements));
   }, [translation]);
 
   return <>{displayElements}</>;
