654bb0db527fefedc4404abfe8732728d191083c
Biao Li <lbit123@outlook.com>
2023-10-23 11:36:52 +1100

    Add migrations to add deletion status column

---
+++ packages/shared/src/migrations/1697778073630-addDeletionStatusToClinicalFeatures.js
@@ -0,0 +1,31 @@
+import Sequelize from 'sequelize';
+
+const tables = [
+  'encounters',
+  'document_metadata',
+  'referrals',
+  'notes',
+  'encounter_medications',
+  'invoices',
+  'vitals',
+  'procedures',
+  'lab_requests',
+  'imaging_requests',
+  'medications',
+  'survey_responses',
+];
+
+export async function up(query) {
+  for (const table of tables) {
+    await query.addColumn(table, 'deletion_status', {
+      type: Sequelize.STRING,
+      defaultValue: 'current',
+    });
+  }
+}
+
+export async function down(query) {
+  for (const table of tables) {
+    await query.removeColumn(table, 'deletion_status');
+  }
+}
