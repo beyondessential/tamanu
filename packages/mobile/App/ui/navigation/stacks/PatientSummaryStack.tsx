d2d3d9bcadb45d84daa60eb651bacaed91d0926c
Muhammed Tareq Aziz <tareq.aziz@dsinnovators.com>
2023-10-30 01:26:56 +0600

    feature: Program registry list and add button in mobile (#4849)

---
+++ packages/mobile/App/ui/navigation/stacks/PatientSummaryStack.tsx
@@ -0,0 +1,46 @@
+import React, { ReactElement, useCallback } from 'react';
+import { Routes } from '/helpers/routes';
+import { FullView } from '/styled/common';
+import { ErrorBoundary } from '/components/ErrorBoundary';
+import { StackHeader } from '~/ui/components/StackHeader';
+import { joinNames } from '~/ui/helpers/user';
+import { compose } from 'redux';
+import { withPatient } from '~/ui/containers/Patient';
+import { NavigationProp } from '@react-navigation/native';
+import { BaseAppProps } from '~/ui/interfaces/BaseAppProps';
+import { createStackNavigator } from '@react-navigation/stack';
+import { PatientProgramRegistrySummary } from '../screens/patientProgramRegistry/PatientProgramRegistrySummary';
+
+export const Stack = createStackNavigator();
+interface PatientSummaryStackProps extends BaseAppProps {
+  navigation: NavigationProp<any>;
+}
+
+const PatientSummary = ({
+  navigation,
+  selectedPatient,
+}: PatientSummaryStackProps): ReactElement => {
+  const goBack = useCallback(() => {
+    navigation.goBack();
+  }, []);
+
+  return (
+    <ErrorBoundary>
+      <FullView>
+        <StackHeader
+          title="Patient Summary"
+          subtitle={joinNames(selectedPatient)}
+          onGoBack={goBack}
+        />
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen
+            name={Routes.HomeStack.PatientSummaryStack.Index}
+            component={PatientProgramRegistrySummary}
+          />
+        </Stack.Navigator>
+      </FullView>
+    </ErrorBoundary>
+  );
+};
+
+export const PatientSummaryStack = compose(withPatient)(PatientSummary);
