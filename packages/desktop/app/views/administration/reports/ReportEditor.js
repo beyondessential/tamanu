e44ef3c2bdbb503047952e90d4380633ad57bc89
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-10-30 09:20:59 +1300

    NASS-919: Report creator permission fixes (#4844)
    * also account for null schema value on form
    
    * add comment
    
    * alter comment
    
    * filter reports on endpoint based on report flag and permission
    
    * delete old imports

--- packages/desktop/app/views/administration/reports/ReportEditor.js
+++ packages/desktop/app/views/administration/reports/ReportEditor.js
@@ -121,11 +121,14 @@ const ReportEditorForm = ({ isSubmitting, values, setValues, dirty, isEdit }) =>
   const onParamsDelete = paramId => setParams(params.filter(p => p.id !== paramId));
 
   const canWriteRawReportUser = Boolean(ability?.can('write', 'ReportDbSchema'));
-  const showDataSourceField = values.dbSchema === REPORT_DB_SCHEMAS.RAW;
 
-  const { data: schemaOptions } = useQuery(['dbSchemaOptions'], () =>
+  const { data: schemaOptions = [] } = useQuery(['dbSchemaOptions'], () =>
     api.get(`admin/reports/dbSchemaOptions`),
   );
+
+  // Show data source field if user is writing a raw report OR if reporting schema is disabled.
+  const showDataSourceField =
+    values.dbSchema === REPORT_DB_SCHEMAS.RAW || schemaOptions.length === 0;
 
   return (
     <>
