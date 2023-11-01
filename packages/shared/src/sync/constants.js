09a8be901faac25698ba07b90fb680220548897f
Biao Li <lbit123@outlook.com>
2023-11-01 14:13:24 +1100

    Exclude deletedAt from sync

--- packages/shared/src/sync/constants.js
+++ packages/shared/src/sync/constants.js
@@ -7,4 +7,9 @@ export const CURRENT_SYNC_TIME_KEY = 'currentSyncTick';
 export const LAST_SUCCESSFUL_SYNC_PULL_KEY = 'lastSuccessfulSyncPull';
 export const LAST_SUCCESSFUL_SYNC_PUSH_KEY = 'lastSuccessfulSyncPush';
 
-export const COLUMNS_EXCLUDED_FROM_SYNC = ['createdAt', 'updatedAt', 'updatedAtSyncTick'];
+export const COLUMNS_EXCLUDED_FROM_SYNC = [
+  'createdAt',
+  'updatedAt',
+  'deletedAt',
+  'updatedAtSyncTick',
+];
