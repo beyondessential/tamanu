3d6a8cf2625b44a9923794cd8a66dc32ba14e1e9
Daniel Nash <dannash200@gmail.com>
2023-11-02 14:56:28 +1300

    debug hover

--- packages/desktop/app/components/Translation/TranslatedText.js
+++ packages/desktop/app/components/Translation/TranslatedText.js
@@ -2,11 +2,77 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { ipcRenderer } from 'electron';
+import { Tooltip } from '@material-ui/core';
+import { Colors } from '../../constants';
 
-const DebugHighlighed = styled.span`
+const DebugHighlighted = styled.span`
   background-color: red;
-  color: white;
+  color: ${Colors.white};
 `;
+const StyledTooltip = styled(props => (
+  <Tooltip classes={{ popper: props.className }} {...props}>
+    {props.children}
+  </Tooltip>
+))`
+  z-index: 1500;
+  pointer-events: auto;
+
+  & .MuiTooltip-tooltip {
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    background-color: ${Colors.white};
+    color: red;
+  }
+  & .MuiTooltip-arrow {
+    color: ${Colors.white};
+  }
+`;
+
+const StyledList = styled.ul`
+  margin: 0;
+  padding-left: 20px;
+  padding-bottom: 0;
+`;
+
+const DebugTooltip = React.memo(({ children, stringId, replacements, fallback }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenTooltip = e => {
+    if (!e.shiftKey) return;
+    setOpen(true);
+  };
+  const handleCloseTooltip = () => setOpen(false);
+  return (
+    <StyledTooltip
+      onOpen={handleOpenTooltip}
+      onClose={handleCloseTooltip}
+      title={
+        <>
+          <b>string_id:</b> {stringId}
+          {replacements && (
+            <>
+              <br />
+              <b>replacements:</b>
+              <StyledList>
+                {Object.entries(replacements).map(([key, value]) => (
+                  <li key={key}>
+                    {key}: {value}
+                  </li>
+                ))}
+              </StyledList>
+              {/* <br /> */}
+              <b>template:</b> {fallback}
+            </>
+          )}
+        </>
+      }
+      open={open}
+      arrow
+      placement="top"
+    >
+      {children}
+    </StyledTooltip>
+  );
+});
 
 const safeGetIsDebugMode = () => {
   try {
@@ -32,16 +98,9 @@ const replaceStringVariables = (templateString, replacements) => {
   return jsxElements;
 };
 
-const stringIds = [];
-
 // "stringId" is used in future functionality
 // eslint-disable-next-line no-unused-vars
 export const TranslatedText = ({ stringId, fallback, replacements }) => {
-  if (!stringIds.includes(stringId)) {
-    stringIds.push(stringId);
-    if (!stringId) console.log(fallback);
-    console.log(stringIds.sort((a, b) => a.localeCompare(b)));
-  }
   const [isDebugMode, setIsDebugMode] = useState(false);
   // "setTranslation" is used in future functionality
   // eslint-disable-next-line no-unused-vars
@@ -67,9 +126,13 @@ export const TranslatedText = ({ stringId, fallback, replacements }) => {
     setDisplayElements(replaceStringVariables(translation, replacements));
   }, [translation, replacements]);
 
-  const TextWrapper = isDebugMode ? DebugHighlighed : React.Fragment;
-
-  return <TextWrapper>{displayElements}</TextWrapper>;
+  if (isDebugMode)
+    return (
+      <DebugTooltip stringId={stringId} replacements={replacements} fallback={fallback}>
+        <DebugHighlighted>{displayElements}</DebugHighlighted>
+      </DebugTooltip>
+    );
+  return displayElements;
 };
 
 TranslatedText.propTypes = {
