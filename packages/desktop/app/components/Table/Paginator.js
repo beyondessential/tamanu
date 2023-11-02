129cf516be5a4f0ee99f55c7ef9207d3dca81661
Daniel Nash <dannash200@gmail.com>
2023-11-03 12:16:51 +1300

    tweak mistakes

--- packages/desktop/app/components/Table/Paginator.js
+++ packages/desktop/app/components/Table/Paginator.js
@@ -123,7 +123,6 @@ export const Paginator = React.memo(
     return (
       <PaginatorWrapper colSpan={colSpan}>
         <FooterContent>
-          {/* TODO: replacement one */}
           <PageRecordCount>
             <TranslatedText
               stringId="general.table.pageRecordCount"
