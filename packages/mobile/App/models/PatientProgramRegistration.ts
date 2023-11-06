93bb55cf301e0830c6d1f3d2a6de0810c038754e
Alastair Locke <lockealastair@gmail.com>
2023-11-06 12:44:55 +1100

    Add PatientProgramRegistration

--- packages/mobile/App/models/PatientProgramRegistration.ts
+++ packages/mobile/App/models/PatientProgramRegistration.ts
@@ -1,15 +1,72 @@
-import { Entity, ManyToOne, RelationId, Column, OneToMany } from 'typeorm/browser';
+import { Entity, ManyToOne, RelationId, Column } from 'typeorm/browser';
 
-import { IPatientProgramRegistration } from '~/types';
+import {
+  DateTimeString,
+  ID,
+  IFacility,
+  IPatient,
+  IPatientProgramRegistration,
+  IProgramRegistry,
+  IProgramRegistryClinicalStatus,
+  IReferenceData,
+  IUser,
+} from '~/types';
 import { BaseModel } from './BaseModel';
 import { SYNC_DIRECTIONS } from './types';
-import { Encounter } from './Encounter';
-import { LabTestPanel } from './LabTestPanel';
-import { VisibilityStatus } from '~/visibilityStatuses';
-import { Program } from './Program';
+import { ProgramRegistry } from './ProgramRegistry';
+import { Patient } from './Patient';
+import { ProgramRegistryClinicalStatus } from './ProgramRegistryClinicalStatus';
+import { NullableReferenceDataRelation } from './ReferenceData';
+import { Facility } from './Facility';
+import { User } from './User';
 
 @Entity('patient_program_registration')
 export class PatientProgramRegistration extends BaseModel implements IPatientProgramRegistration {
   static syncDirection = SYNC_DIRECTIONS.BIDIRECTIONAL;
 
+  // TODO: emum
+  // @Column({ default: VisibilityStatus.Current, nullable: true })
+  // visibilityStatus: VisibilityStatus;
+  @Column({ nullable: false })
+  registrationStatus: string;
+
+  date: DateTimeString;
+
+  // Relations
+  @ManyToOne(() => ProgramRegistry)
+  programRegistry: IProgramRegistry;
+  @RelationId<PatientProgramRegistration>(({ programRegistry }) => programRegistry)
+  programRegistryId: ID;
+
+  @ManyToOne(() => Patient)
+  patient: IPatient;
+  @RelationId<PatientProgramRegistration>(({ patient }) => patient)
+  patientId: ID;
+
+  @ManyToOne(() => ProgramRegistryClinicalStatus, undefined, { nullable: true })
+  programRegistryClinicalStatus?: IProgramRegistryClinicalStatus;
+  @RelationId<PatientProgramRegistration>(
+    ({ programRegistryClinicalStatus }) => programRegistryClinicalStatus,
+  )
+  programRegistryClinicalStatusId?: ID;
+
+  @NullableReferenceDataRelation()
+  village?: IReferenceData;
+  @RelationId<PatientProgramRegistration>(({ village }) => village)
+  villageId?: ID;
+
+  @ManyToOne(() => Facility, undefined, { nullable: true })
+  facility?: IFacility;
+  @RelationId<PatientProgramRegistration>(({ facility }) => facility)
+  facilityId?: ID;
+
+  @ManyToOne(() => Facility, undefined, { nullable: true })
+  registeringFacility?: IFacility;
+  @RelationId<PatientProgramRegistration>(({ registeringFacility }) => registeringFacility)
+  registeringFacilityId?: ID;
+
+  @ManyToOne(() => User, undefined, { nullable: true })
+  clinician?: IUser;
+  @RelationId<PatientProgramRegistration>(({ clinician }) => clinician)
+  clinicianId?: ID;
 }
