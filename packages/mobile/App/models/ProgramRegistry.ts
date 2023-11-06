1495aa2fdd6921ec4574df22b47ca9a1c61c7cd0
Alastair Locke <lockealastair@gmail.com>
2023-11-06 12:17:22 +1100

    Progress

--- packages/mobile/App/models/ProgramRegistry.ts
+++ packages/mobile/App/models/ProgramRegistry.ts
@@ -1,12 +1,11 @@
 import { Entity, ManyToOne, RelationId, Column, OneToMany } from 'typeorm/browser';
 
-import { IProgramRegistry } from '~/types';
+import { IProgramRegistry, IPatientProgramRegistration } from '~/types';
 import { BaseModel } from './BaseModel';
 import { SYNC_DIRECTIONS } from './types';
-import { Encounter } from './Encounter';
-import { LabTestPanel } from './LabTestPanel';
 import { VisibilityStatus } from '~/visibilityStatuses';
 import { Program } from './Program';
+import { PatientProgramRegistration } from './PatientProgramRegistration';
 import { ProgramRegistryClinicalStatus } from './ProgramRegistryClinicalStatus';
 
 @Entity('lab_test_panel_request')
@@ -18,47 +17,62 @@ export class ProgramRegistry extends BaseModel implements IProgramRegistry {
 
   @Column({ nullable: false })
   name?: string;
-  
+
   @Column({ default: VisibilityStatus.Current, nullable: true })
   visibilityStatus: VisibilityStatus;
 
   // TODO: Make this an enum
   currentlyAtType: string;
 
-  @OneToMany(
+  @OneToMany<ProgramRegistryClinicalStatus>(
     () => ProgramRegistryClinicalStatus,
     ({ programRegistry }) => programRegistry,
   )
   clinicalStatuses: ProgramRegistryClinicalStatus[];
 
-
-  @OneToMany(
-    () => IPatientProgramRegistration,
+  @OneToMany<PatientProgramRegistration>(
+    () => PatientProgramRegistration,
     ({ programRegistry }) => programRegistry,
   )
   patientProgramRegistrations: IPatientProgramRegistration[];
-
 
   // patientProgramRegistrations: IPatientProgramRegistration[];
 
   // patientProgramRegistrationConditions: IPatientProgramRegistrationCondition[];
 
-  @ManyToOne(
-    () => Program,
-  )
+  @ManyToOne(() => Program)
   program: Program;
-  @RelationId({ program }) => program)
+  @RelationId<ProgramRegistry>(({ program }) => program)
   programId: string;
-
 }
 
-// type hi = string | ((object: unknown) => any);
-type hi2 = typeof RelationId; //(param: unknown) => string;
-const myFn2: hi2 = (x) => '';
-// type hi = (object: unknown) => any;
+// // type hi = string | ((object: unknown) => any);
+// type hi2 = typeof RelationId; //(param: unknown) => string;
+// const myFn2: hi2 = (x) => '';
+// // type hi = (object: unknown) => any;
 
-function myFn<T>(param: string | ((object: T) => any)) {
-  console.log(param);
-}
+// function myFn<T>(param: string | ((object: T) => any)) {
+//   console.log(param);
+// }
 
-myFn(({ x }) => x);
+// interface XX {
+//   x: string;
+// }
+// myFn<XX>(({ x }) => x);
+
+// function myDec<T>(param: string | ((object: T) => any)) {
+//   console.log("first(): factory evaluated", param);
+//   return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//     console.log("first(): called", param);
+//   };
+// }
+// function myDec(target: Object, propertyKey: string | symbol, parameterIndex: number) {
+//   let existingRequiredParameters: number[] = Reflect.getOwnMetadata(requiredMetadataKey, target, propertyKey) || [];
+//   existingRequiredParameters.push(parameterIndex);
+//   Reflect.defineMetadata( requiredMetadataKey, existingRequiredParameters, target, propertyKey);
+// }
+
+// class Testing {
+//   @myDec<XX>(({ x }) => x)
+//   hello: string;
+// }
