00e525055e38b8494353abd2844329986bc5e9eb
Daniel Nash <dannash200@gmail.com>
2023-11-13 10:30:12 +1300

    nass-949: simplify localised field and also start the details

--- packages/desktop/app/components/Field/LocalisedField.js
+++ packages/desktop/app/components/Field/LocalisedField.js
@@ -4,20 +4,14 @@ import * as yup from 'yup';
 import { Field } from './Field';
 import { useLocalisation } from '../../contexts/Localisation';
 
-export const LocalisedField = ({
-  name,
-  useShortLabel,
-  path = `fields.${name}`,
-  defaultLabel,
-  ...props
-}) => {
+export const LocalisedField = ({ name, path = `fields.${name}`, label, ...props }) => {
   const { getLocalisation } = useLocalisation();
   const hidden = getLocalisation(`${path}.hidden`);
   const required = getLocalisation(`${path}.required`) || false;
   if (hidden) {
     return null;
   }
-  return <Field name={name} required={required} {...props} />;
+  return <Field label={label} name={name} required={required} {...props} />;
 };
 
 export const useLocalisedSchema = () => {
