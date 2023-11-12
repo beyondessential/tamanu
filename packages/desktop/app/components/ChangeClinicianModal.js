365cb68009ec3ddbc2945a9ec03f4f280c807ef4
Daniel Nash <dannash200@gmail.com>
2023-11-13 11:59:01 +1300

    nass-949: solution for usage of useLocalisedText for practitioner

--- packages/desktop/app/components/ChangeClinicianModal.js
+++ packages/desktop/app/components/ChangeClinicianModal.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 
+import styled from 'styled-components';
 import { useSuggester } from '../api';
 import { useEncounter } from '../contexts/Encounter';
 import { usePatientNavigation } from '../utils/usePatientNavigation';
@@ -8,6 +9,11 @@ import { usePatientNavigation } from '../utils/usePatientNavigation';
 import { ChangeClinicianForm } from '../forms/ChangeClinicianForm';
 import { FormModal } from './FormModal';
 import { useLocalisedText } from './LocalisedText';
+import { TranslatedText } from './Translation/TranslatedText';
+
+const LowerCase = styled.span`
+  text-transform: lowercase;
+`;
 
 export const ChangeClinicianModal = React.memo(({ open, onClose }) => {
   const clinicianText = useLocalisedText({ path: 'fields.clinician.shortLabel' });
@@ -23,7 +29,23 @@ export const ChangeClinicianModal = React.memo(({ open, onClose }) => {
   );
 
   return (
-    <FormModal title={`Change ${clinicianText.toLowerCase()}`} open={open} onClose={onClose}>
+    <FormModal
+      title={
+        <TranslatedText
+          stringId="encounter.modal.changeClinician.title"
+          fallback="Change :clinician"
+          replacements={{
+            clinician: (
+              <LowerCase>
+                <TranslatedText stringId="general.clinician.shortLabel" fallback={clinicianText} />
+              </LowerCase>
+            ),
+          }}
+        />
+      }
+      open={open}
+      onClose={onClose}
+    >
       <ChangeClinicianForm
         clinicianSuggester={clinicianSuggester}
         onSubmit={onSubmit}
