efb29ac4db11a8a43fb2939ab34d825995236a7f
Félix Saparelli <felix@bes.au>
2023-11-15 15:46:24 +1300

    okay babel whatever you say

--- packages/desktop/babel.config.js
+++ packages/desktop/babel.config.js
@@ -7,6 +7,7 @@ module.exports = api => {
   config.plugins = plugins.concat([
     '@babel/plugin-transform-classes',
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-transform-private-methods',
   ]);
   return config;
 };
