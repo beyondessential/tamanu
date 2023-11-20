0b29ffafd204a66c0570ca4a082a5852be29b34d
Biao Li <lbit123@outlook.com>
2023-11-21 10:42:58 +1100

    Add test database to shared package

--- packages/shared/__tests__/sync/saveChangesForModel.test.js
+++ packages/shared/__tests__/sync/saveChangesForModel.test.js
@@ -1,167 +1,185 @@
 import { saveChangesForModel } from '../../src/sync';
 import * as saveChangeModules from '../../src/sync/saveChanges';
+import { closeDatabase, createTestDatabase } from './utilities';
 
-jest.mock('../../src/sync/saveChanges');
-
-// Mock dependencies like `model.findByIds`
-const findByIds = jest.fn();
-const get = jest.fn();
-const getModel = jest.fn(() => ({
-  findByIds,
-  sanitizeForCentralServer: jest.fn().mockImplementation(d => d),
-  get,
+jest.mock('../../src/sync/saveChanges', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../src/sync/saveChanges'),
 }));
-const Model = getModel();
 
-const generateExistingRecord = (id, data = {}) => ({
-  id,
-  data: { id, ...data },
-  get: () => ({ id, ...data }),
-});
-const mockExistingRecords = records => {
-  findByIds.mockImplementation(() => records);
-  get.mockImplementation(() => records);
-};
+jest.spyOn(saveChangeModules, 'saveCreates');
+jest.spyOn(saveChangeModules, 'saveUpdates');
+jest.spyOn(saveChangeModules, 'saveDeletes');
+jest.spyOn(saveChangeModules, 'saveRestores');
 
 describe('saveChangesForModel', () => {
-  afterEach(() => {
+  let models;
+
+  beforeAll(async () => {
+    const database = await createTestDatabase();
+    models = database.models;
+  });
+
+  afterEach(async () => {
+    await models.SurveyScreenComponent.destroy({ truncate: true, force: true });
     jest.clearAllMocks();
   });
 
-  afterAll(() => {
-    jest.restoreAllMocks();
+  afterAll(async () => {
+    await closeDatabase();
   });
 
   describe('saveCreates', () => {
     it('should create new records correctly', async () => {
       // setup test data
-      const existingRecords = [];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'new_record_id' };
+      const newRecord = { id: 'new_record_id', text: 'new_record_name' };
       const isDeleted = false;
       const changes = [{ data: newRecord, isDeleted }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(1);
-      expect(saveChangeModules.saveCreates).toBeCalledWith(Model, [
+      expect(saveChangeModules.saveCreates).toBeCalledWith(models.SurveyScreenComponent, [
         { ...newRecord, isDeleted }, // isDeleted flag for soft deleting record after creation
       ]);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(0);
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(0);
       expect(saveChangeModules.saveRestores).toBeCalledTimes(0);
+
+      const newRecordInDb = await models.SurveyScreenComponent.findByPk('new_record_id');
+      expect(newRecordInDb).toBeDefined();
+      expect(newRecordInDb.text).toEqual(newRecord.text);
     });
 
     it('should create new records even if they are soft undeleted', async () => {
       // setup test data
-      const existingRecords = [];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'new_record_id' }; // does not pass down deletedAt from central
+      const newRecord = { id: 'new_record_id', text: 'new_record_name' }; // does not pass down deletedAt from central
       const isDeleted = true;
       const changes = [{ data: newRecord, isDeleted }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(1);
-      expect(saveChangeModules.saveCreates).toBeCalledWith(Model, [
+      expect(saveChangeModules.saveCreates).toBeCalledWith(models.SurveyScreenComponent, [
         { ...newRecord, isDeleted }, // isDeleted flag for soft deleting record after creation
       ]);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(0);
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(0);
       expect(saveChangeModules.saveRestores).toBeCalledTimes(0);
+
+      const newRecordInDb = await models.SurveyScreenComponent.findByPk(newRecord.id, {
+        paranoid: false,
+      });
+      expect(newRecordInDb).toBeDefined();
+      expect(newRecordInDb.text).toEqual(newRecord.text);
     });
   });
 
   describe('saveUpdates', () => {
     it('should update existing records correctly', async () => {
       // setup test data
-      const existingRecords = [
-        generateExistingRecord('existing_record_id', { status: 'historical' }),
-      ];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'existing_record_id', status: 'current' };
+      const existingRecord = { id: 'existing_record_id', text: 'historical' };
+      await models.SurveyScreenComponent.create(existingRecord);
+      const newRecord = { id: existingRecord.id, text: 'current' };
       const changes = [{ data: newRecord, isDeleted: false }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(0);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(1);
       expect(saveChangeModules.saveUpdates).toBeCalledWith(
-        Model,
+        models.SurveyScreenComponent,
         [newRecord],
         expect.anything(),
         true,
       );
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(0);
       expect(saveChangeModules.saveRestores).toBeCalledTimes(0);
+      const updatedRecordInDb = await models.SurveyScreenComponent.findByPk(existingRecord.id);
+      expect(updatedRecordInDb).toBeDefined();
+      expect(updatedRecordInDb.text).toEqual(newRecord.text);
     });
 
     it('should not update soft deleted records', async () => {
       // setup test data
-      const existingRecords = [
-        generateExistingRecord('existing_record_id', {
-          deletedAt: new Date(),
-          status: 'historical',
-        }),
-      ];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'existing_record_id', status: 'current' };
+      const existingRecord = await models.SurveyScreenComponent.create({
+        id: 'existing_record_id',
+        text: 'historical',
+      });
+      await existingRecord.destroy();
+      const newRecord = { id: existingRecord.id, text: 'current' };
       const changes = [{ data: newRecord, isDeleted: true }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(0);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(0);
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(0);
       expect(saveChangeModules.saveRestores).toBeCalledTimes(0);
+      const updatedRecordInDb = await models.SurveyScreenComponent.findByPk(existingRecord.id, {
+        paranoid: false,
+      });
+      expect(updatedRecordInDb).toBeDefined();
+      expect(updatedRecordInDb.text).toEqual(existingRecord.text);
     });
   });
 
   describe('saveDeletes', () => {
-    it('should delete records correctly', async () => {
+    it('should update record, then delete record in saveDeletes()', async () => {
       // setup test data
-      const existingRecords = [generateExistingRecord('existing_record_id')];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'existing_record_id' };
+      const existingRecord = await models.SurveyScreenComponent.create({
+        id: 'existing_record_id',
+        text: 'historical',
+      });
+      const newRecord = { id: existingRecord.id, text: 'current' };
       const changes = [{ data: newRecord, isDeleted: true }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(0);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(0);
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(1);
       expect(saveChangeModules.saveDeletes).toBeCalledWith(
-        Model,
+        models.SurveyScreenComponent,
         [newRecord],
         expect.anything(),
         true,
       );
       expect(saveChangeModules.saveRestores).toBeCalledTimes(0);
+      const updatedRecordInDb = await models.SurveyScreenComponent.findByPk(existingRecord.id, {
+        paranoid: false,
+      });
+      expect(updatedRecordInDb.deletedAt).toBeDefined();
+      expect(updatedRecordInDb.text).toBe(newRecord.text);
     });
   });
 
   describe('saveRestore', () => {
-    it('should restore records correctly', async () => {
+    it('should restore records,then update record in saveRestore()', async () => {
       // setup test data
-      const existingRecords = [
-        generateExistingRecord('existing_record_id', { deletedAt: new Date() }),
-      ];
-      mockExistingRecords(existingRecords);
-      const newRecord = { id: 'existing_record_id' };
+      const existingRecord = await models.SurveyScreenComponent.create({
+        id: 'existing_record_id',
+        text: 'historical',
+      });
+      await existingRecord.destroy();
+      const newRecord = { id: existingRecord.id, text: 'current' };
       const changes = [{ data: newRecord, isDeleted: false }];
       // act
-      await saveChangesForModel(Model, changes, true);
+      await saveChangesForModel(models.SurveyScreenComponent, changes, true);
       // assertions
       expect(saveChangeModules.saveCreates).toBeCalledTimes(0);
       expect(saveChangeModules.saveUpdates).toBeCalledTimes(0);
       expect(saveChangeModules.saveDeletes).toBeCalledTimes(0);
       expect(saveChangeModules.saveRestores).toBeCalledTimes(1);
       expect(saveChangeModules.saveRestores).toBeCalledWith(
-        Model,
+        models.SurveyScreenComponent,
         [newRecord],
         expect.anything(),
         true,
       );
+      const updatedRecordInDb = await models.SurveyScreenComponent.findByPk(existingRecord.id);
+      expect(updatedRecordInDb).toBeDefined();
+      expect(updatedRecordInDb.text).toEqual(newRecord.text);
     });
   });
 });
