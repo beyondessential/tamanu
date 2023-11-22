cb2b71a832fd15a85da8bd565a92a695919657e4
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2023-11-23 09:45:22 +1300

    fix mobile tests

--- packages/mobile/App/models/TranslatedString.spec.ts
+++ packages/mobile/App/models/TranslatedString.spec.ts
@@ -2,17 +2,20 @@ import { Database } from '~/infra/db';
 
 beforeAll(async () => {
   await Database.connect();
+  const translatedStrings = [
+    { language: 'en', stringId: 'languageName', text: 'English' },
+    { language: 'en', stringId: 'login.username', text: 'Username' },
+    { language: 'en', stringId: 'login.password', text: 'Password' },
+    { language: 'km', stringId: 'languageName', text: 'Khmer' },
+    { language: 'km', stringId: 'login.username', text: 'ឈ្មោះអ្នកប្រើប្រាស់' },
+    { language: 'km', stringId: 'login.password', text: 'ពាក្យសម្ងាត់' },
+  ];
+  await Database.models.TranslatedString.insert(translatedStrings);
 });
 
 describe('TranslatedString', () => {
   describe('getLanguageOptions', () => {
     it('gets language options', async () => {
-      const translatedStrings = [
-        { language: 'en', stringId: 'languageName', text: 'English' },
-        { language: 'km', stringId: 'languageName', text: 'Khmer' },
-      ];
-      await Database.models.TranslatedString.insert(translatedStrings);
-
       const result = await Database.models.TranslatedString.getLanguageOptions();
       expect(result).toEqual([
         { value: 'en', label: 'English' },
@@ -20,19 +23,12 @@ describe('TranslatedString', () => {
       ]);
     });
   });
-});
+
   describe('getForLanguage', () => {
     it('gets translated strings for language', async () => {
-      const translatedStrings = [
-        { language: 'en', stringId: 'login.username', text: 'Username' },
-        { language: 'en', stringId: 'login.password', text: 'Password' },
-        { language: 'km', stringId: 'login.username', text: 'ឈ្មោះអ្នកប្រើប្រាស់' },
-        { language: 'km', stringId: 'login.password', text: 'ពាក្យសម្ងាត់' },
-      ];
-      await Database.models.TranslatedString.insert(translatedStrings);
-
       const result = await Database.models.TranslatedString.getForLanguage('en');
       expect(result).toEqual({
+        languageName: 'English',
         'login.username': 'Username',
         'login.password': 'Password',
       });
