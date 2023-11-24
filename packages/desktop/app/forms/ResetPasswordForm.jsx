be31c56d13668d2f11b1603dd0419e2c23be5d40
Félix Saparelli <felix@bes.au>
2023-11-24 15:58:13 +1300

    TAV-885: Remove server selection field

--- packages/desktop/app/forms/ResetPasswordForm.jsx
+++ packages/desktop/app/forms/ResetPasswordForm.jsx
@@ -1,33 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as yup from 'yup';
 import styled from 'styled-components';
-import Collapse from '@material-ui/core/Collapse';
 import { FormGrid } from '../components/FormGrid';
 import {
   Button,
   Field,
   Form,
-  MinusIconButton,
-  PlusIconButton,
   TextField,
   StyledPrimarySubmitButton,
 } from '../components';
-import { ServerDetectingField, getSavedServer } from '../components/Field/ServerDetectingField';
-
-const AdvancedRow = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  justify-content: space-between;
-  font-size: 16px;
-`;
-
-const AdvancedButtonSpan = styled.span`
-  .MuiButtonBase-root {
-    padding: 0px 0px 0px 9px;
-    font-size: 20px;
-  }
-`;
 
 const SuccessMessage = styled.p`
   margin-top: 0;
@@ -44,37 +25,12 @@ export const ResetPasswordForm = React.memo(
     onNavToChangePassword,
     onNavToLogin,
   }) => {
-    const needsServer = !getSavedServer();
-    const [isAdvancedExpanded, setAdvancedExpanded] = useState(needsServer);
-
-    const renderForm = ({ setFieldValue }) => (
+    const renderForm = () => (
       <FormGrid columns={1}>
         <h3>Reset Password</h3>
         <div>Enter your account email</div>
         <div>{errorMessage}</div>
         <Field name="email" type="email" label="Email" required component={TextField} />
-        <AdvancedRow>
-          <AdvancedButtonSpan>
-            Advanced
-            {isAdvancedExpanded ? (
-              <MinusIconButton
-                onClick={() => setAdvancedExpanded(false)}
-                styles={{ padding: '0px' }}
-              />
-            ) : (
-              <PlusIconButton onClick={() => setAdvancedExpanded(true)} />
-            )}
-          </AdvancedButtonSpan>
-        </AdvancedRow>
-        <Collapse in={isAdvancedExpanded}>
-          <Field
-            name="host"
-            label="LAN server address"
-            required
-            component={ServerDetectingField}
-            setFieldValue={setFieldValue}
-          />
-        </Collapse>
         <StyledPrimarySubmitButton text="Reset Password" />
         <Button onClick={onNavToLogin} color="default" variant="text">
           Back
