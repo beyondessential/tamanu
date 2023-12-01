99fa5f5edb45f779418475f1b8f6f199e961aaa0
Tim Kennedy <t.m.knndy@gmail.com>
2023-12-01 13:11:49 +1100

    Fix tests to better reflect the HTTP spec
    After many reverts, I've decided 403 Forbidden is the best error
    for a deactivated account, since the credentials are valid and
    the server understands them but the request itself (to log in) is
    not authorized.

--- packages/lan/__tests__/apiv1/User.test.js
+++ packages/lan/__tests__/apiv1/User.test.js
@@ -175,7 +175,7 @@ describe('User', () => {
             email: deactivatedUser.email,
             password: rawPassword,
           });
-          expect(result).toHaveRequestError();
+          expect(result).toBeForbidden();
         });
       });
     });
