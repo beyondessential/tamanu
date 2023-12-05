22cda84ddf70e216420ca3fc1cfcf69cfb9a4179
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-05 16:48:37 +1300

    removed the test results field

--- packages/mobile/App/ui/navigation/screens/historyvitals/tabs/VisitsScreen.tsx
+++ packages/mobile/App/ui/navigation/screens/historyvitals/tabs/VisitsScreen.tsx
@@ -1,12 +1,9 @@
-import React, { ReactElement, useCallback } from 'react';
+import React, { ReactElement } from 'react';
 import { compose } from 'redux';
-import { FullView, StyledSafeAreaView, StyledView } from '/styled/common';
+import { FullView, StyledSafeAreaView } from '/styled/common';
 import { PatientHistoryAccordion } from '~/ui/components/PatientHistoryAccordion';
 import { theme } from '/styled/theme';
-import { Button } from '/components/Button';
-import { FilterIcon } from '/components/Icons';
 import { NOTE_TYPES } from '~/ui/helpers/constants';
-import { screenPercentageToDP, Orientation } from '~/ui/helpers/screen';
 import { useBackendEffect } from '~/ui/hooks';
 import { LoadingScreen } from '~/ui/components/LoadingScreen';
 import { ErrorScreen } from '~/ui/components/ErrorScreen';
@@ -15,21 +12,17 @@ import { IDiagnosis } from '~/types';
 
 const DEFAULT_FIELD_VAL = 'N/A';
 
-const displayNotes = (notes): string => notes
-  .filter(note => note.noteType === NOTE_TYPES.CLINICAL_MOBILE)
-  .map(note => note.content)
-  .join('\n\n')
-  || DEFAULT_FIELD_VAL;
+const displayNotes = (notes): string =>
+  notes
+    .filter(note => note.noteType === NOTE_TYPES.CLINICAL_MOBILE)
+    .map(note => note.content)
+    .join('\n\n') || DEFAULT_FIELD_VAL;
 
 const visitsHistoryRows = {
-  labRequest: {
-    name: 'Test results',
-    accessor: (): string => DEFAULT_FIELD_VAL,
-  },
   diagnoses: {
     name: 'Diagnosis',
-    accessor: (diagnoses: IDiagnosis[]): string => diagnoses.map((d) => `${d.diagnosis?.name} (${d.certainty})`).join('\n\n')
-      || DEFAULT_FIELD_VAL,
+    accessor: (diagnoses: IDiagnosis[]): string =>
+      diagnoses.map(d => `${d.diagnosis?.name} (${d.certainty})`).join('\n\n') || DEFAULT_FIELD_VAL,
   },
   notes: {
     name: 'Clinical Note',
