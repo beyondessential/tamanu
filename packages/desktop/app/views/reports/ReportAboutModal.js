1d82707264279fb19ed019796cacc0cc8fc072a2
Michelle <100409601+mlie788@users.noreply.github.com>
2023-12-05 14:57:10 +1300

    Restyled the report notes modal and made the button row fixed to the bottom of the modal

--- packages/desktop/app/views/reports/ReportAboutModal.js
+++ packages/desktop/app/views/reports/ReportAboutModal.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Typography } from '@material-ui/core';
-import { Modal, ModalGenericButtonRow, OutlinedButton } from '../../components';
+import { BodyText, Modal, OutlinedButton } from '../../components';
+import { Colors } from '../../constants';
 
-const Spacer = styled.div`
-  padding-top: 15px;
+const TextContainer = styled.div`
+  padding: 15px;
 `;
-export const ReportAboutModal = ({
-  title,
-  open,
-  onClose,
-  content,
-  ConfirmButton = OutlinedButton,
-}) => (
-  <Modal title={title} open={open} onClose={onClose} cornerExitButton={false}>
-    {content.split('\n').map(line => (
-      <Typography>{line}</Typography>
-    ))}
-    <Spacer />
-    <ModalGenericButtonRow>
-      <ConfirmButton variant="contained" onClick={onClose}>
-        Close
-      </ConfirmButton>
-    </ModalGenericButtonRow>
+
+const StyledButtonRow = styled.div`
+  background-color: ${Colors.background};
+  border-top: 1px solid ${Colors.outline};
+  padding: 30px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const CloseButtonRow = ({ onClose }) => (
+  <StyledButtonRow>
+    <OutlinedButton variant="contained" onClick={onClose}>
+      Close
+    </OutlinedButton>
+  </StyledButtonRow>
+);
+export const ReportAboutModal = ({ title, open, onClose, content }) => (
+  <Modal
+    title={title}
+    open={open}
+    onClose={onClose}
+    cornerExitButton={false}
+    fixedBottomRow
+    bottomRowContent={<CloseButtonRow onClose={onClose} />}
+  >
+    <TextContainer>
+      {content.split('\n').map(line => (line === '' ? <br /> : <BodyText>{line}</BodyText>))}
+    </TextContainer>
   </Modal>
 );
