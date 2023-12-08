3d25c782a0921bebe709719870616cd7c1457654
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-08 16:41:22 +1300

    fixed issue :)

--- packages/desktop/app/components/DocumentPreview/PDFPreview.jsx
+++ packages/desktop/app/components/DocumentPreview/PDFPreview.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
 import * as pdfjsLib from 'pdfjs-dist';
-import pdfjsWorker from 'pdfjs-dist/build/pdf.worker?worker&url';
+import pdfjsWorker from 'pdfjs-dist/build/pdf.worker';
 
 import styled from 'styled-components';
 import { useApi } from '../../api';
@@ -33,7 +33,6 @@ export default function PDFPreview({
   scrollPage,
   setScrollPage,
 }) {
-
   const api = useApi();
   const [pages, setPages] = useState([]);
 
