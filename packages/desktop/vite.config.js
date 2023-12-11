bce01ce34f8a5866aa38f00fc5ae49f515979d00
Félix Saparelli <felix@bes.au>
2023-12-11 19:38:55 +1300

    Write app version via Vite

--- packages/desktop/vite.config.js
+++ packages/desktop/vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { readFile } from 'node:fs/promises';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,11 @@ export default defineConfig({
     loader: 'jsx',
   },
   plugins: [react()],
+  define: {
+    __VERSION__: JSON.stringify(
+      await readFile('package.json')
+        .then(JSON.parse)
+        .then(({ version }) => version),
+    ),
+  },
 })
