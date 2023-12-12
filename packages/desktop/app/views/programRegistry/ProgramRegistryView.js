1188faaf83cf847f4df7a66877c9c45688c73039
tareq89 <tareq.aziz@dsinnovators.com>
2023-12-12 19:20:54 +0600

    check if program registry is active before loading Program Registry View page

--- packages/desktop/app/views/programRegistry/ProgramRegistryView.js
+++ packages/desktop/app/views/programRegistry/ProgramRegistryView.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Redirect } from 'react-router-dom';
 import { Colors } from '../../constants';
+import { useListOfProgramRegistryQuery } from '../../api/queries/useProgramRegistryQuery';
+import { LoadingIndicator } from '../../components/LoadingIndicator';
 import { useUrlSearchParams } from '../../utils/useUrlSearchParams';
 import { ProgramRegistrySearchBar } from './ProgramRegistrySearchBar';
 import { ProgramRegistryTable } from './ProgramRegistryTable';
@@ -27,19 +30,29 @@ const Container = styled.div`
 export const ProgramRegistryView = () => {
   const searchParams = useUrlSearchParams();
   const [searchParameters, setSearchParameters] = useState({});
-  return (
-    <>
-      <ViewHeader>
-        <h1>{searchParams.get('name')}</h1>
-      </ViewHeader>
-      <Container>
-        <span>Program patient search</span>
-        <ProgramRegistrySearchBar
-          searchParameters={searchParameters}
-          setSearchParameters={setSearchParameters}
-        />
-        <ProgramRegistryTable searchParameters={searchParameters} />
-      </Container>
-    </>
-  );
+  const { data: programRegistries, isLoading, isSuccess } = useListOfProgramRegistryQuery();
+
+  if (isLoading) return <LoadingIndicator />;
+  else if (
+    isSuccess &&
+    programRegistries?.data &&
+    programRegistries.data.length > 0 &&
+    programRegistries.data.find(x => x.name === searchParams.get('name'))
+  )
+    return (
+      <>
+        <ViewHeader>
+          <h1>{searchParams.get('name')}</h1>
+        </ViewHeader>
+        <Container>
+          <span>Program patient search</span>
+          <ProgramRegistrySearchBar
+            searchParameters={searchParameters}
+            setSearchParameters={setSearchParameters}
+          />
+          <ProgramRegistryTable searchParameters={searchParameters} />
+        </Container>
+      </>
+    );
+  return <Redirect to="/patients/all" />;
 };
