f7c01e7b0eec2d139e3dd5c4ff39844282918ff2
Félix Saparelli <felix@bes.au>
2023-12-13 21:48:12 +1300

    Add react and typescript to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,154 @@
 import js from '@eslint/js';
 import markdown from 'eslint-plugin-markdown';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import typescriptParser from '@typescript-eslint/parser';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+
+const JS_EXTS = '{js,jsx,mjs,cjs,ts,tsx}';
+const TS_EXTS = '{ts,tsx}';
+const EXTS = `{${JS_EXTS},${TS_EXTS}}`;
 
 export default [
   {
-    files: ['packages/**/*.[cm]?[jt]sx?', 'scripts/**/*.[cm]?[jt]sx?'],
-    env: {
-      browser: true,
-      node: true,
-      es2022: true,
-    },
+    files: [`packages/**/*.${TS_EXTS}`, `scripts/**/*.${TS_EXTS}`],
     plugins: {
-      'react-hooks': reactHooks,
+      '@typescript-eslint': typescriptPlugin,
     },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-useless-rename': 'error',
-      'no-duplicate-imports': 'error',
-      'sort-imports': 'warn',
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    // Disable rules that are incompatible with or better handled by TypeScript
+    rules: typescriptPlugin.configs['eslint-recommended'].overrides[0].rules,
+  },
+  {
+    files: [`packages/**/*.${JS_EXTS}`, `scripts/**/*.${JS_EXTS}`, '**/*.config.js'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
   {
-    files: ['packages/*/__tests__/**/*.[cm]?[jt]sx?'],
-    env: {
-      jest: true,
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+
+      // import rules
+      'no-useless-rename': 'error',
+      'no-duplicate-imports': 'error',
+      'sort-imports': 'warn',
+
+      // static analysis rules
+      'no-new-native-nonconstructor': 'error',
+      'no-self-compare': 'error',
+      'no-template-curly-in-string': 'error',
+      'no-unmodified-loop-condition': 'error',
+      'no-unreachable-loop': 'error',
+      'no-unused-private-class-members': 'error',
+
+      // https://eslint.org/blog/2022/07/interesting-bugs-caught-by-no-constant-binary-expression/
+      'no-constant-binary-expression': 'error',
+
+      // rules we should enforce but have too many of right now
+      'no-unused-vars': 'warn',
+      'no-constructor-return': 'warn',
+      'no-promise-executor-return': 'warn',
+      'require-atomic-updates': 'warn',
+      'react-hooks/rules-of-hooks': 'warn',
+      'react/jsx-key': 'warn',
+
+      // overrides for react
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+    },
+  },
+  {
+    files: [
+      `packages/*/__{mocks,tests}__/**/*.${EXTS}`,
+      `packages/mobile/e2e/**/*.${EXTS}`,
+      `packages/shared/src/test-helpers/**/*.${EXTS}`,
+      `**/jest.*.${EXTS}`,
+      `**/*.{spec,test}.${EXTS}`,
+    ],
+    languageOptions: {
+      globals: globals.jest,
+    },
+  },
+  {
+    files: [`packages/mobile/e2e/**/*.${JS_EXTS}`],
+    languageOptions: {
+      globals: globals.jasmine,
+    },
+  },
+  {
+    // react-native globals
+    files: [`packages/mobile/**/*.${JS_EXTS}`],
+    languageOptions: {
+      globals: {
+        JSX: 'readonly',
+        __DEV__: 'readonly',
+        Element: 'readonly',
+        React: 'readonly',
+        StyleProp: 'readonly',
+        ViewStyle: 'readonly',
+        TextInputProps: 'readonly',
+        TextStyle: 'readonly',
+        FlatListProps: 'readonly',
+        AnimatedCircularProgress: 'readonly',
+      },
+    },
+  },
+  {
+    files: [`packages/desktop/**/*.${EXTS}`],
+    languageOptions: {
+      globals: {
+        '__VERSION__': 'readonly',
+      }
+    },
+  },
+  {
+    files: [`packages/desktop/**/*.${JS_EXTS}`, 'packages/qr-tester/www/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        CryptoKeyPair: 'readonly',
+      }
+    },
+  },
+  {
+    files: [
+      `packages/!({desktop,qr-tester})/**/*.${JS_EXTS}`,
+      `scripts/**/*.${JS_EXTS}`,
+      '**/*.config.js',
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        CryptoKeyPair: 'readonly',
+      }
+    },
+  },
+  {
+    files: ['packages/qr-tester/www/js/**/*.js'],
+    languageOptions: {
+      globals: {
+        X509: 'readonly',
+        KEYUTIL: 'readonly',
+      },
     },
   },
   {
@@ -45,9 +169,10 @@ export default [
       '**/*.md',
 
       // generated files
-      'packages/*/dist',
-      'packages/*/build',
-      'packages/*/{src,app}/**/*.d.ts',
+      '**/dist/**/*',
+      '**/build/**/*',
+      '**/vendor/**/*',
+      '**/*.d.ts',
 
       // local config
       '**/config/local.*',
