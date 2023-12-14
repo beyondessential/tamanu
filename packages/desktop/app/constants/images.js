312f58d2002b1596ca005cd5439cf09805abbca2
Félix Saparelli <felix@bes.au>
2023-12-14 16:33:33 +1300

    Fix merge: image exports

--- packages/desktop/app/constants/images.js
+++ packages/desktop/app/constants/images.js
@@ -4,19 +4,19 @@ import splash3 from '../assets/images/splashscreens/screen_3.jpg';
 
 export const splashImages = [splash1, splash2, splash3];
 
-export administrationIcon from '../assets/images/administration.svg';
-export labsIcon from '../assets/images/labs.svg';
-export logoutIcon from '../assets/images/logout.svg';
-export medicationIcon from '../assets/images/medication.svg';
-export patientIcon from '../assets/images/patient.svg';
-export pregnancyIcon from '../assets/images/pregnancy.svg';
-export programsIcon from '../assets/images/programs.svg';
-export radiologyIcon from '../assets/images/radiology.svg';
-export scheduleIcon from '../assets/images/schedule.svg';
-export vaccineIcon from '../assets/images/vaccine.svg';
+export { default as administrationIcon } from '../assets/images/administration.svg';
+export { default as labsIcon } from '../assets/images/labs.svg';
+export { default as logoutIcon } from '../assets/images/logout.svg';
+export { default as medicationIcon } from '../assets/images/medication.svg';
+export { default as patientIcon } from '../assets/images/patient.svg';
+export { default as pregnancyIcon } from '../assets/images/pregnancy.svg';
+export { default as programsIcon } from '../assets/images/programs.svg';
+export { default as radiologyIcon } from '../assets/images/radiology.svg';
+export { default as scheduleIcon } from '../assets/images/schedule.svg';
+export { default as vaccineIcon } from '../assets/images/vaccine.svg';
 
-export reportLine from '../assets/images/report_line.png';
-export reportBar from '../assets/images/report_bar.png';
-export reportPie from '../assets/images/report_pie.png';
-export reportTable from '../assets/images/report_table.png';
-export reportRaw from '../assets/images/report_raw.png';
+export { default as reportLine } from '../assets/images/report_line.png';
+export { default as reportBar } from '../assets/images/report_bar.png';
+export { default as reportPie } from '../assets/images/report_pie.png';
+export { default as reportTable } from '../assets/images/report_table.png';
+export { default as reportRaw } from '../assets/images/report_raw.png';
