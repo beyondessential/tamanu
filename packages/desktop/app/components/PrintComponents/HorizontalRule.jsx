ad98ce39d42046121837d529a60b6c9d0e5fc9f8
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-18 13:48:50 +1300

    unused prop

--- packages/desktop/app/components/PrintComponents/HorizontalRule.jsx
+++ packages/desktop/app/components/PrintComponents/HorizontalRule.jsx
@@ -1,7 +1,7 @@
 import { View } from '@react-pdf/renderer'
 import React from 'react'
 
-export const HorizontalRule = (width) => {
+export const HorizontalRule = () => {
   return (
     <View style={{ borderBottom: '1px solid black', marginVertical: '3px'}} />
   )
