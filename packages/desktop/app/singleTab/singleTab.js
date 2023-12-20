fc878c039532d633eb27a0918cd121afafd89ed6
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-21 11:48:52 +1300

    changing to broadcast
    needs more work

--- packages/desktop/app/singleTab/singleTab.js
+++ packages/desktop/app/singleTab/singleTab.js
@@ -1,16 +1,42 @@
-import shortid from 'shortid';
+import { useState, useEffect } from 'react';
 
-export const isPrimaryTab = () => {
-  return window.localStorage.getItem('primaryTab') === window.name;
-};
+export const useSingleTab = () => {
+  const [isPrimaryTab, setIsPrimaryTab] = useState(true);
+  const channel = new BroadcastChannel('tamanuSingleTab');
 
-export const initialiseSingleTabDetection = () => {
-  if (!window.name) window.name = shortid();
+  useEffect(() => {
+    const wasPrimaryTab = sessionStorage.getItem('wasPrimaryTab');
+    let startTime = parseInt(sessionStorage.getItem('startTime'));
+    if (!startTime) {
+      startTime = Date.now();
+      sessionStorage.setItem('startTime', startTime);
+    }
 
-  if (!window.localStorage.getItem('primaryTab')) {
-    window.localStorage.setItem('primaryTab', window.name);
-    window.onbeforeunload = () => {
-      window.localStorage.removeItem('primaryTab');
+    console.log('start time', startTime);
+
+    const handleBroadcastMessage = event => {
+      const otherTabStart = event.data;
+
+      if (wasPrimaryTab) {
+        channel.postMessage(otherTabStart - 1);
+        return;
+      }
+
+      if (otherTabStart < startTime) {
+        setIsPrimaryTab(false);
+      } else if (isPrimaryTab) {
+        channel.postMessage(startTime);
+        sessionStorage.setItem('wasPrimaryTab', 'true');
+      }
     };
-  }
+
+    channel.addEventListener('message', handleBroadcastMessage);
+    channel.postMessage(startTime);
+
+    return () => {
+      channel.removeEventListener('message', handleBroadcastMessage);
+    };
+  }, [isPrimaryTab, channel]);
+
+  return isPrimaryTab;
 };
