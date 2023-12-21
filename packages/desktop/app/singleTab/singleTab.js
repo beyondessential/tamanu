16287399cc14f8f7e57239921253da08308cacd0
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-21 13:06:20 +1300

    updated to not use dates

--- packages/desktop/app/singleTab/singleTab.js
+++ packages/desktop/app/singleTab/singleTab.js
@@ -1,42 +1,41 @@
 import { useState, useEffect } from 'react';
 
+const newTabChannel = new BroadcastChannel('tamanuNewTab');
+const primaryTabChannel = new BroadcastChannel('tamanuPrimaryTab');
+
 export const useSingleTab = () => {
   const [isPrimaryTab, setIsPrimaryTab] = useState(true);
-  const channel = new BroadcastChannel('tamanuSingleTab');
+
+  const newTabListener = () => {
+    // Tell any new tabs that a primary tab exists
+    primaryTabChannel.postMessage('primaryTab');
+  };
+
+  const primaryTabListener = () => {
+    // If a message is received on the primary tab channel, we know that this tab is not the primary tab
+    setIsPrimaryTab(false);
+    sessionStorage.removeItem('wasPrimaryTab');
+    newTabChannel.removeEventListener('message', newTabListener);
+  };
 
   useEffect(() => {
     const wasPrimaryTab = sessionStorage.getItem('wasPrimaryTab');
-    let startTime = parseInt(sessionStorage.getItem('startTime'));
-    if (!startTime) {
-      startTime = Date.now();
-      sessionStorage.setItem('startTime', startTime);
+
+    newTabChannel.addEventListener('message', newTabListener);
+
+    if (!wasPrimaryTab) {
+      // Assume that this is a primary tab until told otherwise
+      sessionStorage.setItem('wasPrimaryTab', 'true');
+      primaryTabChannel.addEventListener('message', primaryTabListener);
+      newTabChannel.postMessage('newTab');
     }
 
-    console.log('start time', startTime);
-
-    const handleBroadcastMessage = event => {
-      const otherTabStart = event.data;
-
-      if (wasPrimaryTab) {
-        channel.postMessage(otherTabStart - 1);
-        return;
-      }
-
-      if (otherTabStart < startTime) {
-        setIsPrimaryTab(false);
-      } else if (isPrimaryTab) {
-        channel.postMessage(startTime);
-        sessionStorage.setItem('wasPrimaryTab', 'true');
-      }
-    };
-
-    channel.addEventListener('message', handleBroadcastMessage);
-    channel.postMessage(startTime);
-
     return () => {
-      channel.removeEventListener('message', handleBroadcastMessage);
+      // Clean up listeners on demount
+      newTabChannel.removeEventListener('message', newTabListener);
+      primaryTabChannel.removeEventListener('message', primaryTabListener);
     };
-  }, [isPrimaryTab, channel]);
+  }, []);
 
   return isPrimaryTab;
 };
