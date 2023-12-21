3377b423ffc07abf42d7acc5e5d2626c5e930482
Félix Saparelli <felix@bes.au>
2023-12-22 12:34:49 +1300

    Update packages/shared/docker/postgres/init.sh
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

--- packages/shared/docker/postgres/init.sh
+++ packages/shared/docker/postgres/init.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 psql -v ON_ERROR_STOP=1 <<-EOF
     CREATE ROLE "tamanu-facility" WITH LOGIN ENCRYPTED PASSWORD 'tamanu-facility';
     CREATE DATABASE "tamanu-facility" WITH OWNER "tamanu-facility";
-    CREATE ROLE "tamanu-centralp" WITH LOGIN ENCRYPTED PASSWORD 'tamanu-central';
+    CREATE ROLE "tamanu-central" WITH LOGIN ENCRYPTED PASSWORD 'tamanu-central';
     CREATE DATABASE "tamanu-central" WITH OWNER "tamanu-central";
 EOF
