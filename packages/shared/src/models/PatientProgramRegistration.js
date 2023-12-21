1e681541a4b07056318c51a6c688fbad4cbc55f6
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2023-12-20 20:44:18 -0400

    TAN-2448: Program registry sync (#5090)
    * Add special buildPatientSyncFilter logic to patient program registry and conditions
    
    * Pass an extra session config
    
    * Add a default in case setting is not set
    
    * Use empty string placeholder to avoid query error
    
    * Explicitly use an empty string to fill in the IN clause
    
    * Errata, more than 0!

--- packages/shared/src/models/PatientProgramRegistration.js
+++ packages/shared/src/models/PatientProgramRegistration.js
@@ -136,10 +136,16 @@ export class PatientProgramRegistration extends Model {
     });
   }
 
-  // syncs everywhere because for the pilot program,
-  // the number of patients is guaranteed to be low.
-  // https://github.com/beyondessential/tamanu/pull/4773#discussion_r1356087015
-  static buildSyncFilter() {
-    return null;
+  static buildPatientSyncFilter(patientIds, { syncTheseProgramRegistries }) {
+    const escapedProgramRegistryIds =
+      syncTheseProgramRegistries?.length > 0
+        ? syncTheseProgramRegistries.map(id => this.sequelize.escape(id)).join(',')
+        : "''";
+
+    if (patientIds.length === 0) {
+      return `WHERE program_registry_id IN (${escapedProgramRegistryIds}) AND updated_at_sync_tick > :since`;
+    }
+
+    return `WHERE (patient_id IN (:patientIds) OR program_registry_id IN (${escapedProgramRegistryIds})) AND updated_at_sync_tick > :since`;
   }
 }
