95ef85215650d2d3b59907de9865b558f9ae3031
flynncs <108244616+flynncs@users.noreply.github.com>
2023-12-22 11:51:13 +1300

    updated method for detecting multiple tabs

--- packages/desktop/app/singleTab/singleTab.js
+++ packages/desktop/app/singleTab/singleTab.js
@@ -1,42 +1,30 @@
 import { useState, useEffect } from 'react';
 
-const newTabChannel = new BroadcastChannel('tamanuNewTab');
-const primaryTabChannel = new BroadcastChannel('tamanuPrimaryTab');
+const tabStart = Date.now();
+const channel = new BroadcastChannel('tamanuSingleTab');
 
 export const useSingleTab = () => {
   const [isPrimaryTab, setIsPrimaryTab] = useState(true);
 
-  const newTabListener = () => {
-    // Tell any new tabs that a primary tab exists
-    primaryTabChannel.postMessage('primaryTab');
-  };
-
-  const primaryTabListener = () => {
-    // If a message is received on the primary tab channel, we know that this tab is not the primary tab
-    setIsPrimaryTab(false);
-    sessionStorage.removeItem('wasPrimaryTab');
-    newTabChannel.removeEventListener('message', newTabListener);
-  };
-
   useEffect(() => {
-    const wasPrimaryTab = sessionStorage.getItem('wasPrimaryTab');
-    const isDuplicateTab = sessionStorage.getItem('currentlyOpen');
-    sessionStorage.setItem('currentlyOpen', 'true');
+    const listener = event => {
+      const otherTabStart = event.data;
 
-    newTabChannel.addEventListener('message', newTabListener);
+      if (otherTabStart < tabStart) {
+        // Another tab is older than this one
+        setIsPrimaryTab(false);
+        channel.removeEventListener('message', listener);
+      } else if (isPrimaryTab) {
+        // another tab is checking if it's the primary - current tab is primary
+        channel.postMessage(tabStart);
+      }
+    };
 
-    if (!wasPrimaryTab || isDuplicateTab) {
-      // Assume that this is a primary tab until told otherwise
-      sessionStorage.setItem('wasPrimaryTab', 'true');
-      primaryTabChannel.addEventListener('message', primaryTabListener);
-      newTabChannel.postMessage('newTab');
-    }
+    channel.addEventListener('message', listener);
+    channel.postMessage(tabStart);
 
     return () => {
-      // Clean up listeners on demount
-      newTabChannel.removeEventListener('message', newTabListener);
-      primaryTabChannel.removeEventListener('message', primaryTabListener);
-      sessionStorage.removeItem('currentlyOpen');
+      channel.removeEventListener('message', listener);
     };
   }, []);
 
