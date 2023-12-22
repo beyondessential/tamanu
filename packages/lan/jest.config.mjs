1e44294be4f8dc3355384c76460fd7081be4e7cf
Félix Saparelli <felix@bes.au>
2023-12-22 14:08:25 +1300

    Copy jest config across centra;/facility

--- packages/lan/jest.config.mjs
+++ packages/lan/jest.config.mjs
@@ -1,6 +1,7 @@
 import { config } from '../../common.jest.config.mjs';
 
 export default config(import.meta, {
+  testEnvironment: 'node',
   globalTeardown: '<rootDir>/__tests__/teardown.js',
   setupFilesAfterEnv: ['<rootDir>/__tests__/configureEnvironment.js', 'jest-expect-message'],
 });
