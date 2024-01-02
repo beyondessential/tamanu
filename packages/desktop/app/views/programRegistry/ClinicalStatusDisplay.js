f07b933c3ec004dae0d84d22c20317434cb228af
tareq89 <tareq.aziz@dsinnovators.com>
2024-01-02 16:51:51 +0600

    Discrepancies with Figma in ppr details header fixed

--- packages/desktop/app/views/programRegistry/ClinicalStatusDisplay.js
+++ packages/desktop/app/views/programRegistry/ClinicalStatusDisplay.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { STATUS_COLOR } from '@tamanu/constants';
 import { TableCellTag } from '../../components';
+import { ConditionalTooltip } from '../../components/Tooltip';
 
 export const ClinicalStatusDisplay = ({ clinicalStatus }) => {
   if (!clinicalStatus || !clinicalStatus?.color) return <></>;
   const { background, color } = STATUS_COLOR[clinicalStatus.color];
   return (
-    <TableCellTag $background={background} $color={color} $position="initial">
-      {clinicalStatus.name}
-    </TableCellTag>
+    <ConditionalTooltip visible title="Current status">
+      <TableCellTag $background={background} $color={color} $position="initial">
+        {clinicalStatus.name}
+      </TableCellTag>
+    </ConditionalTooltip>
   );
 };
