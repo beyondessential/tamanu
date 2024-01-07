06aa97076318a57b4d425850e5efcc4190e719ce
flynncs <108244616+flynncs@users.noreply.github.com>
2024-01-08 10:22:06 +1300

    render multiple data items
    function for rendering multiple data items with localisation and accessors

---
+++ packages/shared/src/utils/patientCertificates/printComponents/renderDataItems.jsx
@@ -0,0 +1,9 @@
+import { DataItem } from './DataItem';
+
+export const renderDataItems = (fields, patient, getLocalisation) => {
+  return fields.map(({ key, label: defaultLabel, accessor }) => {
+    const value = (accessor ? accessor(patient, getLocalisation) : patient[key]) || '';
+    const label = getLocalisation(`fields.${key}.shortLabel`) || defaultLabel;
+    return (<DataItem label={label} value={value} />);
+  });
+};
