c07302655a5a0adfd767fcc9d682d29eea49899b
flynncs <108244616+flynncs@users.noreply.github.com>
2024-01-09 12:14:23 +1300

    lint

--- packages/shared/src/utils/patientCertificates/printComponents/PrintableBarcode.jsx
+++ packages/shared/src/utils/patientCertificates/printComponents/PrintableBarcode.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Image } from '@react-pdf/renderer';
 import JsBarcode from 'jsbarcode';
 
-export const PrintableBarcode = ({ patient, width = 124, barWidth = 1, barHeight = 45, fontSize = 13 }) => {
+export const PrintableBarcode = ({
+  patient,
+  width = 124,
+  barWidth = 1,
+  barHeight = 45,
+  fontSize = 13,
+}) => {
   const canvas = document.createElement('canvas');
   JsBarcode(canvas, patient.displayId, { width: barWidth, height: barHeight, fontSize: fontSize });
   const barcode = canvas.toDataURL();
