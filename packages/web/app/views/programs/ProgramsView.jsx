7a887167ddcc2c144e5f0c18d51894cc620bfb9b
Tom McLean <tom.a.mclean@gmail.com>
2024-01-08 13:57:53 +1100

    Merge main into program registries epic (#5110)
    * No issue: more aggressive yarn cache in CI (#5100)
    
    * Restrict codeship to the deploy-meta branch (#5101)
    
    * EPI-699: Label updated on lab request history screen (#5034)
    
    Co-authored-by: Da-ntp <105630649+Da-ntp@users.noreply.github.com>
    
    * EPI-707: Recently view patient screen changes (#5038)
    
    Co-authored-by: Da-ntp <105630649+Da-ntp@users.noreply.github.com>
    
    * EPI-696: Procedure table initial sorting added (#4981)
    
    * EPI-696: Procedure table initial sorting added
    
    * EPI-696: Linting issue resolved
    
    ---------
    
    Co-authored-by: Da-ntp <105630649+Da-ntp@users.noreply.github.com>
    
    * TAV-917: The Big Rename (#5099)
    
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>
    
    * Bump version to 2.0.0
    
    * Epic: Tamanu Web (#5043)
    
    Co-authored-by: Daniel Nash <dannash200@gmail.com>
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    Co-authored-by: Flynn Teh <108244616+flynncs@users.noreply.github.com>
    
    * Fix frontend deploy (#5105)
    
    * Don't version missing file (#5106)
    
    * Bump version to 2.1.0
    
    * Fix some renames
    
    * Merge release/2.0 to main (#5111)
    
    * Cut-off for release/2.0
    
    * Fix web: remove mentions of process.env (#5107)
    
    * Fix: shim process for misbehaving libraries (#5108)
    
    * Allow NODE_ENV
    
    * Contain global to one util
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    
    * Fix bad import
    
    ---------
    
    Co-authored-by: Félix Saparelli <felix@bes.au>
    Co-authored-by: BhargavDY <149754603+BhargavDY@users.noreply.github.com>
    Co-authored-by: Da-ntp <105630649+Da-ntp@users.noreply.github.com>
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Daniel Nash <dannash200@gmail.com>
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    Co-authored-by: Flynn Teh <108244616+flynncs@users.noreply.github.com>

---
+++ packages/web/app/views/programs/ProgramsView.jsx
@@ -0,0 +1,163 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getCurrentDateTimeString } from '@tamanu/shared/utils/dateTime';
+import { SURVEY_TYPES } from '@tamanu/constants';
+import { reloadPatient } from '../../store/patient';
+import { getCurrentUser } from '../../store/auth';
+import { SurveyView } from './SurveyView';
+import { SurveySelector } from './SurveySelector';
+import { FormGrid } from '../../components/FormGrid';
+import { SelectInput } from '../../components/Field/SelectField';
+import { ProgramsPane, ProgramsPaneHeader, ProgramsPaneHeading } from './ProgramsPane';
+import { LoadingIndicator } from '../../components/LoadingIndicator';
+import { PatientListingView } from '..';
+import { usePatientAdditionalDataQuery } from '../../api/queries';
+import { ErrorMessage } from '../../components/ErrorMessage';
+import { getAnswersFromData } from '../../utils';
+import { usePatientNavigation } from '../../utils/usePatientNavigation';
+import { useEncounter } from '../../contexts/Encounter';
+import { PATIENT_TABS } from '../../constants/patientPaths';
+import { ENCOUNTER_TAB_NAMES } from '../../constants/encounterTabNames';
+import { useApi } from '../../api';
+
+const SurveyFlow = ({ patient, currentUser }) => {
+  const api = useApi();
+  const params = useParams();
+  const { encounter, loadEncounter } = useEncounter();
+  const { navigateToEncounter, navigateToPatient } = usePatientNavigation();
+  const [survey, setSurvey] = useState(null);
+  const [programs, setPrograms] = useState(null);
+  const [selectedProgramId, setSelectedProgramId] = useState(null);
+  const [selectedSurveyId, setSelectedSurveyId] = useState(null);
+  const [startTime, setStartTime] = useState(null);
+  const [surveys, setSurveys] = useState(null);
+
+  useEffect(() => {
+    if (params.encounterId) {
+      loadEncounter(params.encounterId);
+    }
+  }, [loadEncounter, params.encounterId]);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await api.get('program');
+      setPrograms(data);
+    })();
+  }, [api]);
+
+  const setSelectedSurvey = useCallback(
+    async id => {
+      const response = await api.get(`survey/${encodeURIComponent(id)}`);
+      setSurvey(response);
+      setStartTime(getCurrentDateTimeString());
+    },
+    [api],
+  );
+
+  const unsetSurvey = useCallback(() => {
+    setSurvey(null);
+  }, []);
+
+  const clearProgram = useCallback(() => {
+    setSelectedSurveyId(null);
+    setSurveys(null);
+  }, []);
+
+  const selectProgram = useCallback(
+    async event => {
+      const programId = event.target.value;
+      if (programId === selectedProgramId) {
+        return;
+      }
+      setSelectedProgramId(programId);
+
+      if (!programId) {
+        clearProgram();
+        return;
+      }
+
+      const { data } = await api.get(`program/${programId}/surveys`);
+      setSurveys(
+        data
+          .filter(s => s.surveyType === SURVEY_TYPES.PROGRAMS)
+          .map(x => ({ value: x.id, label: x.name })),
+      );
+    },
+    [api, selectedProgramId, clearProgram],
+  );
+
+  const submitSurveyResponse = async data => {
+    await api.post('surveyResponse', {
+      surveyId: survey.id,
+      startTime,
+      patientId: patient.id,
+      endTime: getCurrentDateTimeString(),
+      answers: getAnswersFromData(data, survey),
+    });
+    if (params?.encounterId && encounter && !encounter.endDate) {
+      navigateToEncounter(params.encounterId, { tab: ENCOUNTER_TAB_NAMES.FORMS });
+    } else {
+      navigateToPatient(patient.id, { tab: PATIENT_TABS.PROGRAMS });
+    }
+  };
+
+  const { isLoading, data: patientAdditionalData, isError, error } = usePatientAdditionalDataQuery(
+    patient.id,
+  );
+
+  if (isLoading || !programs) {
+    return <LoadingIndicator />;
+  }
+
+  if (isError) {
+    return <ErrorMessage title="Error" error={error} />;
+  }
+
+  if (!survey) {
+    return (
+      <ProgramsPane>
+        <ProgramsPaneHeader>
+          <ProgramsPaneHeading variant="h6">Select form</ProgramsPaneHeading>
+        </ProgramsPaneHeader>
+        <FormGrid columns={1}>
+          <SelectInput
+            options={programs.map(p => ({ value: p.id, label: p.name }))}
+            value={selectedProgramId}
+            onChange={selectProgram}
+            label="Select program"
+          />
+          <SurveySelector
+            onSubmit={setSelectedSurvey}
+            onChange={setSelectedSurveyId}
+            value={selectedSurveyId}
+            surveys={surveys}
+            buttonText="Begin form"
+          />
+        </FormGrid>
+      </ProgramsPane>
+    );
+  }
+
+  return (
+    <SurveyView
+      onSubmit={submitSurveyResponse}
+      survey={survey}
+      onCancel={unsetSurvey}
+      patient={patient}
+      patientAdditionalData={patientAdditionalData}
+      currentUser={currentUser}
+    />
+  );
+};
+
+export const ProgramsView = () => {
+  const dispatch = useDispatch();
+  const patient = useSelector(state => state.patient);
+  const currentUser = useSelector(getCurrentUser);
+  if (!patient.id) {
+    return <PatientListingView onViewPatient={id => dispatch(reloadPatient(id))} />;
+  }
+
+  return <SurveyFlow patient={patient} currentUser={currentUser} />;
+};
