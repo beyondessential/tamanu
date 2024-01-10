b9c42bb827444662011e4fe1e4f660fc56bdfdb4
Félix Saparelli <felix@bes.au>
2024-01-11 11:32:06 +1300

    Remove old tonga

--- packages/meta-server/app/servers.js
+++ packages/meta-server/app/servers.js
@@ -20,7 +20,6 @@ const servers = [
   { name: 'Nauru', type: 'live', host: 'https://sync.tamanu-nauru.org' },
   { name: 'Palau', type: 'live', host: 'https://sync.tamanu-palau.org' },
   { name: 'Samoa', type: 'live', host: 'https://tamanu-sync.health.gov.ws' },
-  { name: 'Tonga', type: 'live', host: 'https://central-tonga.tamanu.io' },
   { name: 'Tonga VMCP', type: 'live', host: 'https://central.tamanu-tonga.org' },
   { name: 'Tuvalu', type: 'live', host: 'https://sync.tamanu-tuvalu.org' },
 
