5ff163a876679a7ff9c4f52f424e2319772681bf
flynncs <108244616+flynncs@users.noreply.github.com>
2024-01-16 16:18:49 +1300

    medications

--- packages/shared/src/utils/patientCertificates/DischargeSummaryPrintout.jsx
+++ packages/shared/src/utils/patientCertificates/DischargeSummaryPrintout.jsx
@@ -5,6 +5,12 @@ import { LetterheadSection } from './LetterheadSection';
 import { PatientDetailsWithAddress } from './printComponents/PatientDetailsWithAddress';
 import { DIAGNOSIS_CERTAINTIES_TO_HIDE } from '@tamanu/constants';
 
+const generalStyles = StyleSheet.create({
+  tableContainer: {
+    marginVertical: 5,
+  },
+});
+
 const infoBoxStyles = StyleSheet.create({
   infoCol: {
     border: '1 solid black',
@@ -18,15 +24,35 @@ const infoBoxStyles = StyleSheet.create({
   },
   labelText: {
     fontFamily: 'Helvetica-Bold',
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: 500,
   },
   infoText: {
     fontFamily: 'Helvetica',
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: 400,
   },
 });
+
+const medicationsTableStyles = StyleSheet.create({
+  flexCol: {
+    flex: 1,
+  },
+  innerLabelCol: {
+    border: '1 solid black',
+    width: 71,
+    padding: 10,
+  },
+});
+
+const notesSectionStyles = StyleSheet.create({
+  notesBox: {
+    border: '1 solid black',
+    height: 76,
+  },
+});
+
+const TableContainer = props => <View style={generalStyles.tableContainer} {...props} />;
 
 const extractDiagnosesInfo = ({ diagnoses, getLocalisation }) => {
   const displayIcd10Codes = getLocalisation('features.displayIcd10CodesInDischargeSummary');
@@ -35,6 +61,15 @@ const extractDiagnosesInfo = ({ diagnoses, getLocalisation }) => {
     return diagnoses.map(item => item?.diagnosis?.name);
   } else {
     return diagnoses.map(item => `${item?.diagnosis?.name} (${item?.diagnosis?.code})`);
+  }
+};
+
+const extractProceduresInfo = ({ procedures, getLocalisation }) => {
+  const displayProcedureCodes = getLocalisation('features.displayProcedureCodesInDischargeSummary');
+  if (!displayProcedureCodes) {
+    return procedures.map(item => item?.procedureType?.name);
+  } else {
+    return procedures.map(item => `${item?.diagnosis?.name} (${item?.procedureType?.code})`);
   }
 };
 
@@ -59,6 +94,53 @@ const DiagnosesTable = ({ title, diagnoses, getLocalisation }) => (
   <InfoBox label={title} info={extractDiagnosesInfo({ diagnoses, getLocalisation })} />
 );
 
+const ProceduresTable = ({ procedures, getLocalisation }) => (
+  <InfoBox label="Procedures" info={extractProceduresInfo({ procedures, getLocalisation })} />
+);
+
+const NotesSection = () => <View style={notesSectionStyles.notesBox} />;
+
+const MedicationsTable = ({ medications }) => {
+  const currentMedications = medications.filter(m => !m.discontinued);
+  const discontinuedMedications = medications.filter(m => m.discontinued);
+
+  console.log('current', currentMedications);
+
+  return (
+    <Row>
+      <View style={infoBoxStyles.labelCol}>
+        <Text style={infoBoxStyles.labelText}>Medications</Text>
+      </View>
+      <View style={medicationsTableStyles.flexCol}>
+        <Row>
+          <View style={medicationsTableStyles.innerLabelCol}>
+            <Text style={infoBoxStyles.infoText}>Current</Text>
+          </View>
+          <View style={infoBoxStyles.infoCol}>
+            {currentMedications.map((item, index) => (
+              <Text style={infoBoxStyles.infoText} key={index}>
+                {item?.medication?.name}
+              </Text>
+            ))}
+          </View>
+        </Row>
+        <Row>
+          <View style={medicationsTableStyles.innerLabelCol}>
+            <Text style={infoBoxStyles.infoText}>Discontinued</Text>
+          </View>
+          <View style={infoBoxStyles.infoCol}>
+            {discontinuedMedications.map((item, index) => (
+              <Text style={infoBoxStyles.infoText} key={index}>
+                {item?.medication?.name}
+              </Text>
+            ))}
+          </View>
+        </Row>
+      </View>
+    </Row>
+  );
+};
+
 export const DischargeSummaryPrintout = ({
   patientData,
   encounter,
@@ -79,12 +161,9 @@ export const DischargeSummaryPrintout = ({
   const visibleDiagnoses = diagnoses.filter(
     ({ certainty }) => !DIAGNOSIS_CERTAINTIES_TO_HIDE.includes(certainty),
   );
-  console.log('encounter', encounter);
-  console.log('diagnoses', diagnoses);
+  console.log('medications', medications);
   const primaryDiagnoses = visibleDiagnoses.filter(d => d.isPrimary);
-  console.log('primary', primaryDiagnoses);
   const secondaryDiagnoses = visibleDiagnoses.filter(d => !d.isPrimary);
-  console.log('secondary', secondaryDiagnoses);
 
   return (
     <Document>
@@ -93,8 +172,27 @@ export const DischargeSummaryPrintout = ({
           <LetterheadSection getLocalisation={getLocalisation} />
         </CertificateHeader>
         <PatientDetailsWithAddress patient={patientData} getLocalisation={getLocalisation} />
-        <DiagnosesTable title="Primary diagnoses" diagnoses={primaryDiagnoses} getLocalisation={getLocalisation} />
-        <DiagnosesTable title="Secondary diagnoses" diagnoses={secondaryDiagnoses} getLocalisation={getLocalisation} />
+        <TableContainer>
+          <DiagnosesTable
+            title="Primary diagnoses"
+            diagnoses={primaryDiagnoses}
+            getLocalisation={getLocalisation}
+          />
+        </TableContainer>
+        <TableContainer>
+          <DiagnosesTable
+            title="Secondary diagnoses"
+            diagnoses={secondaryDiagnoses}
+            getLocalisation={getLocalisation}
+          />
+        </TableContainer>
+        <TableContainer>
+          <ProceduresTable procedures={procedures} getLocalisation={getLocalisation} />
+        </TableContainer>
+        <TableContainer>
+          <MedicationsTable medications={medications} />
+        </TableContainer>
+        <NotesSection />
       </Page>
     </Document>
   );
