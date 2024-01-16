d54a491320cc9043da54d942a5debc6ad6a27fdb
Daniel Nash <dannash200@gmail.com>
2024-01-16 16:09:29 +1300

    nass-1054: remove unintented vite config update

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -50,7 +50,7 @@ export default defineConfig({
     host: 'localhost',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:4000',
         // you can also specify other servers to use as backend, e.g.
         // target: 'https://central.main.internal.tamanu.io',
         // target: 'https://facility-1.main.internal.tamanu.io',
