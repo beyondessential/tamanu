a3089fe2a04105c214742e2bcb4dc85b6a140bd1
Michelle <100409601+mlie788@users.noreply.github.com>
2024-01-22 12:19:01 +1300

    removed console logs

--- packages/shared/src/utils/patientCertificates/printComponents/renderDataItems.jsx
+++ packages/shared/src/utils/patientCertificates/printComponents/renderDataItems.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { DataItem } from './DataItem';
 
 export const renderDataItems = (fields, patient, village, getLocalisation) => {
-  console.log('village', village);
   return fields.map(({ key, label: defaultLabel, accessor }) => {
     const label = getLocalisation?.(`fields.${key}.shortLabel`) || defaultLabel;
     if (key === 'villageName') {
