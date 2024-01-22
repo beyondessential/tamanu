5d3da0537fa4a30c166b55c475cf001a42bbf2b9
Michelle <100409601+mlie788@users.noreply.github.com>
2024-01-22 14:45:29 +1300

    Removed header and footer, also allow for letterhead to be configured from facility

--- packages/shared/src/utils/patientCertificates/EncounterRecordPrintout.jsx
+++ packages/shared/src/utils/patientCertificates/EncounterRecordPrintout.jsx
@@ -370,20 +370,12 @@ export const EncounterRecordPrintout = ({
     <Document>
       <Page size="A4" style={{ paddingHorizontal: 50, paddingVertical: 30 }}>
         {watermark && <Watermark src={watermark} />}
-        <FixedHeader>
-          <View
-            fixed
-            render={({ pageNumber }) =>
-              pageNumber > 1 && <EncounterRecordHeader patient={patientData} />
-            }
-          />
-        </FixedHeader>
-        <View fixed render={({ pageNumber }) => pageNumber > 1 && <PageBreakPadding />} />
         <CertificateHeader>
           <LetterheadSection
             getLocalisation={getLocalisation}
             logoSrc={logo}
             certificateTitle="Patient Encounter Record"
+            letterheadConfig={certificateData}
           />
         </CertificateHeader>
         <SectionSpacing />
@@ -425,10 +417,6 @@ export const EncounterRecordPrintout = ({
           <TableSection title="Medications" data={medications} columns={COLUMNS.medications} />
         )}
         {notes.length > 0 && <NotesSection notes={notes} />}
-        <PageBreakPadding />
-        <FixedFooter>
-          <Footer style={{ width: '100%', left: 0, right: 0 }} />
-        </FixedFooter>
       </Page>
     </Document>
   );
