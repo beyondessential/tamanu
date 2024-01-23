ee2a880020db20871a20098cf17a951df421b482
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-01-23 14:01:34 +1300

    no need for language refresh in prod

--- packages/desktop/app/contexts/Translation.js
+++ packages/desktop/app/contexts/Translation.js
@@ -7,9 +7,11 @@ const TranslationContext = React.createContext();
 
 export const useTranslation = () => useContext(TranslationContext);
 
+const isDev = process.env.NODE_ENV === 'development';
+
 export const TranslationProvider = ({ children }) => {
   const api = useApi();
-  const [translations, setTranslations] = useState({});
+  const [translations, setTranslations] = useState(null);
 
   const fetchTranslations = useCallback(
     async language => {
@@ -21,22 +23,24 @@ export const TranslationProvider = ({ children }) => {
   );
 
   const getTranslation = (stringId, fallback) => {
+    if (!translations) return fallback;
     if (translations[stringId]) return translations[stringId];
     // This section here is a dev tool to help populate the db with the translation ids we have defined
     // in components. It will only populate the db with English strings, so that we can then translate them.
     const storedLanguage = localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE);
-    if (process.env.NODE_ENV === 'development' && storedLanguage === 'en') {
+    if (isDev && storedLanguage === 'en') {
       api.post('translation', { stringId, fallback, text: fallback });
     }
     return fallback;
   };
 
-  // This likely shouldnt be needed in prod? This is to help with development with lots of autorefeshing
-  // which interferres with the workflow of login fetching language and storing to context
+  // Dev helper to stop data loss in context from hotreloads when developing
   useEffect(() => {
-    const storedLanguage = localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE);
-    if (isEmpty(translations) && storedLanguage) {
-      fetchTranslations(storedLanguage);
+    if (isDev) {
+      const storedLanguage = localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE);
+      if (isEmpty(translations) && storedLanguage) {
+        fetchTranslations(storedLanguage);
+      }
     }
   }, [translations, fetchTranslations]);
 
