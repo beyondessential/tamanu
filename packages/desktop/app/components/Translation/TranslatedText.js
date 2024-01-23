a98c0fa4a85a3b2f1f615db6385ec1908f60c2aa
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-01-23 13:55:11 +1300

    remove electron logic from debug mode

--- packages/desktop/app/components/Translation/TranslatedText.js
+++ packages/desktop/app/components/Translation/TranslatedText.js
@@ -1,23 +1,17 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
-import { ipcRenderer } from 'electron';
 import { useTranslation } from '../../contexts/Translation';
 import { DebugTooltip } from './DebugTooltip';
 
-const DEBUG_TRANSLATION_STORAGE_KEY = 'debugTranslation';
+const DEBUG_TRANSLATION = 'DEBUG_TRANSLATION';
 
 const safeGetIsDebugMode = () => {
   try {
-    return JSON.parse(localStorage.getItem(DEBUG_TRANSLATION_STORAGE_KEY));
+    return JSON.parse(localStorage.getItem(DEBUG_TRANSLATION));
   } catch (e) {
     return false;
   }
 };
-
-ipcRenderer.on('toggleTranslationDebug', () => {
-  localStorage.setItem(DEBUG_TRANSLATION_STORAGE_KEY, !safeGetIsDebugMode());
-  window.dispatchEvent(new Event(DEBUG_TRANSLATION_STORAGE_KEY));
-});
 
 const replaceStringVariables = (templateString, replacements) => {
   const jsxElements = templateString.split(/(:[a-zA-Z]+)/g).map((part, index) => {
@@ -32,22 +26,15 @@ const replaceStringVariables = (templateString, replacements) => {
 
 export const TranslatedText = ({ stringId, fallback, replacements }) => {
   const { getTranslation } = useTranslation();
-  const [isDebugMode, setIsDebugMode] = useState(false);
 
   const translation = getTranslation(stringId, fallback?.split('\\n').join('\n'));
-
-  useEffect(() => {
-    const getDebugMode = async () => setIsDebugMode(safeGetIsDebugMode());
-    getDebugMode();
-    window.addEventListener(DEBUG_TRANSLATION_STORAGE_KEY, getDebugMode);
-    return () => window.removeEventListener(DEBUG_TRANSLATION_STORAGE_KEY, getDebugMode);
-  }, []);
 
   const displayElements = useMemo(() => {
     if (!replacements) return translation;
     return replaceStringVariables(translation, replacements);
   }, [translation, replacements]);
 
+  const isDebugMode = safeGetIsDebugMode();
   if (isDebugMode)
     return (
       <DebugTooltip stringId={stringId} replacements={replacements} fallback={fallback}>
