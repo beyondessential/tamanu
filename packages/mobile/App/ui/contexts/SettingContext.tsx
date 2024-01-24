faffbd52fdd14a010eaffc3462a386756aaff871
Daniel Nash <dannash200@gmail.com>
2024-01-25 11:09:54 +1300

    nass-1081: mobile setting context

--- packages/mobile/App/ui/contexts/SettingContext.tsx
+++ packages/mobile/App/ui/contexts/SettingContext.tsx
@@ -1,9 +1,36 @@
-import { createContext } from "react"
+import { PropsWithChildren, createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { useBackend } from '../hooks/index';
+import { SettingService } from '~/services/settings';
 
 interface SettingContextData {
-  getSetting: (path: string) => any;
-  getString: (path: string, defaultValue?: string) => string;
-  getBool: (path: string, defaultValue?: boolean) => boolean;
+  getSetting: <T>(path: string, defaultValue: T) => T;
 }
 
-const SettingContext = createContext<
+const SettingContext = createContext<SettingContextData>({} as SettingContextData);
+
+const makeHelpers = (settings: SettingService): SettingContextData => ({
+  getSetting: (path, defaultValue) => settings.getSetting(path) || defaultValue,
+});
+
+export const SettingProvider = ({ children }: PropsWithChildren<object>) => {
+  const backend = useBackend();
+
+  const defaultHelpers = useMemo(() => makeHelpers(backend.settings), [backend, backend.settings]);
+  const [helpers, setHelpers] = useState(defaultHelpers);
+
+  useEffect(() => {
+    const onChanged = (): void => {
+      // updates the helper functions whenever a setting changes,
+      // in order to make components update with the new value
+      setHelpers(makeHelpers(backend.settings));
+    };
+    backend.settings.emitter.on('settingChanged', onChanged);
+    return () => {
+      backend.settings.emitter.off('settingChanged', onChanged);
+    };
+  }, [backend, backend.settings]);
+
+  return <SettingContext.Provider value={helpers}>{children}</SettingContext.Provider>;
+};
+
+export const useSetting = (): SettingContextData => useContext(SettingContext);
