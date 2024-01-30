df951f017ce7998f901a8503598526fcda6a9d85
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-01-30 16:09:25 +1300

    remvoe useLocalisation imports

--- packages/web/app/components/PatientPrinting/modals/PatientStickerLabelPage.jsx
+++ packages/web/app/components/PatientPrinting/modals/PatientStickerLabelPage.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { useLocalisation } from '../../../contexts/Localisation';
+import { useSettings } from '../../../contexts/Settings';
 import { PDFViewer, printPDF } from '../PDFViewer';
 import { IDLabelPrintout } from '@tamanu/shared/utils/patientCertificates';
 import { Modal } from '../../Modal';
 
 export const PatientStickerLabelPage = React.memo(({ patient }) => {
   const [open, setOpen] = useState(true);
-  const { getLocalisation } = useLocalisation();
-  const measures = getLocalisation('printMeasures.stickerLabelPage');
+  const { getSetting } = useSettings();
+  const measures = getSetting('printMeasures.stickerLabelPage');
   return (
     <Modal
       open={open}
