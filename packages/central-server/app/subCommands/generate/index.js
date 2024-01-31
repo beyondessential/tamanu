665dc4f3162535238e77bbac809072c42e48838f
rail-rain <takayuki@bes.au>
2024-02-01 11:53:22 +1300

    Remove leftover junk

--- packages/central-server/app/subCommands/generate/index.js
+++ packages/central-server/app/subCommands/generate/index.js
@@ -1,7 +1,5 @@
 import { Command } from 'commander';
 import { fijiCommand } from './fiji';
-import { fakeCommand } from './fake';
 
 export const generateCommand = new Command('generate').description('Generate fake data');
 generateCommand.addCommand(fijiCommand);
-generateCommand.addCommand(fakeCommand);
