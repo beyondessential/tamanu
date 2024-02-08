ac3a32833f9cc58532cfb9e5ed5e43e5196f4ead
Renato Koyama <renato.koyama@gmail.com>
2024-02-08 18:12:38 -0300

    fix: set fallback as required

--- packages/web/app/components/Translation/TranslatedReferenceData.jsx
+++ packages/web/app/components/Translation/TranslatedReferenceData.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TranslatedText } from './TranslatedText';
 
 export const TranslatedReferenceData = ({ category, value, fallback }) => {
   return (
     <TranslatedText
       stringId={`referenceData.${category}.${value}`}
-      fallback={`${fallback || value}`}
+      fallback={`${fallback }`}
     />
   );
 };
 
-TranslatedText.propTypes = {
+TranslatedReferenceData.propTypes = {
   category: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
-  fallback: PropTypes.string,
+  fallback: PropTypes.string.isRequired,
 };
