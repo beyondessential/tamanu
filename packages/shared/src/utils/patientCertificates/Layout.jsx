5012efd3788c60b0fb67b93716dff018a8e7facf
Daniel Nash <dannash200@gmail.com>
2024-02-08 16:46:02 +1300

    fix: ensure watermark does not exceed view width and height

--- packages/shared/src/utils/patientCertificates/Layout.jsx
+++ packages/shared/src/utils/patientCertificates/Layout.jsx
@@ -51,6 +51,8 @@ export const styles = StyleSheet.create({
     bottom: 0,
     minWidth: '100%',
     minHeight: '100%',
+    maxHeight: '100vw',
+    maxWidth: '100vw',
     backgroundColor: '#aaaaaa',
     opacity: 0.05,
     flexDirection: 'row',
