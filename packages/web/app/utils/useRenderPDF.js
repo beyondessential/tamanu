267bf763f8730160d735455fa351a7c70dc74b82
Daniel Nash <dannash200@gmail.com>
2024-02-09 08:41:48 +1300

    replace react-use useAsync with useQuery

--- packages/web/app/utils/useRenderPDF.js
+++ packages/web/app/utils/useRenderPDF.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useAsync } from 'react-use';
+import { useQuery } from '@tanstack/react-query';
 import { proxy, wrap } from 'comlink';
 import Worker from '../workers/pdf.worker?worker';
 
@@ -7,10 +7,14 @@ export const pdfWorker = wrap(new Worker());
 pdfWorker.onProgress(proxy(info => console.log(info)));
 
 export const useRenderPDF = props => {
-  const { value: url, loading, error } = useAsync(async () => {
-    return pdfWorker.renderPDFInWorker(props);
-  }, []);
+  const { data: url, isLoading, error } = useQuery(
+    ['renderPDF', props.id],
+    () => pdfWorker.renderPDFInWorker(props),
+    {
+      enabled: !!props.id,
+    },
+  );
 
   useEffect(() => (url ? () => URL.revokeObjectURL(url) : undefined), [url]);
-  return { url, loading, error };
+  return { url, isLoading, error };
 };
