2ae3e2b13a0e1c7117bf23eb7b235c6595d5ea79
Michelle <100409601+mlie788@users.noreply.github.com>
2024-02-12 08:58:51 +1300

    Created alternative translated enum component

--- packages/web/app/components/Translation/TranslatedEnum.jsx
+++ packages/web/app/components/Translation/TranslatedEnum.jsx
@@ -2,6 +2,7 @@ import { useTranslation } from '../../contexts/Translation.jsx';
 import { DebugTooltip } from './DebugTooltip.jsx';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TranslatedText } from './TranslatedText.jsx';
 
 // Set DEBUG_TRANSLATION to true in localstorage if you want to highlight all translated strings in red
 const DEBUG_TRANSLATION_KEY = 'DEBUG_TRANSLATION';
@@ -29,6 +30,13 @@ export const TranslatedEnum = ({ prefix, value, enumValues }) => {
       </DebugTooltip>
     );
   return translation;
+};
+
+export const TranslatedEnumV2 = ({ prefix, value, enumValues }) => {
+  const stringId = `${prefix}.${value}`;
+  const fallback = enumValues[value];
+
+  return <TranslatedText stringId={stringId} fallback={fallback} />;
 };
 
 TranslatedEnum.propTypes = {
