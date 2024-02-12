7f43d3a7bfb972115397a51fd90744d97a4a5daa
rail-rain <takayuki@bes.au>
2024-02-12 21:49:58 +1300

    Use deeper fetch over gh api

--- .github/scripts/find-dumped.py
+++ .github/scripts/find-dumped.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python3
 
-import argparse
-from gql import gql, Client
-from gql.transport.httpx import HTTPXTransport
+import subprocess
 import boto3
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--github-token", required=True)
-parser.add_argument("--branch", required=True)
-args = parser.parse_args()
 
 aws_client = boto3.client("s3")
 dumped_iter = iter(
@@ -17,62 +10,27 @@ dumped_iter = iter(
     )
 )
 
-transport = HTTPXTransport(
-    url="https://api.github.com/graphql",
-    headers={"authorization": f"Bearer {args.github_token}"},
-)
-gql_client = Client(transport=transport, fetch_schema_from_transport=True)
-query_string = """
-query($endCursor: String) {
-  repository(owner: "beyondessential", name: "tamanu") {
-    ref(qualifiedName: "{{branch}}") {
-      target {
-        ...on Commit {
-          history(after: $endCursor) {
-            nodes { oid }
-              pageInfo {
-                  hasNextPage
-                  endCursor
-              }
-          }
-        }
-      }
-    }
-  }
-}
-""".replace(
-    "{{branch}}", args.branch
-)
-gh_query = gql(query_string)
+commits_bytes = subprocess.run(
+    ["/usr/bin/git", "log", "--pretty=format:%H"], check=True, capture_output=True
+).stdout.splitlines()
+commits = map(lambda c: str(c, encoding="ascii"), commits_bytes)
 
 dumped = next(dumped_iter)
-commits = gql_client.execute(gh_query)["repository"]["ref"]["target"]["history"]
-
-commits["nodes"] = commits["nodes"][1:] # Skip the latest commit as we want an old db dump.
-commits_iter = iter(commits["nodes"])
+commits_iter = iter(commits)
 while True:
     try:
         # Return if this commit hash is in the dumped list.
-        commit = next(commits_iter)["oid"]
+        commit = next(commits_iter)
         hashes = map(lambda d: d["Prefix"][:-1], dumped["CommonPrefixes"])
         if any([hash == commit for hash in hashes]):
             print(commit)
             exit(0)
     except StopIteration:
-        # Fetch more commits if any.
-        if commits["pageInfo"]["hasNextPage"]:
-            new_commits = gql_client.execute(
-                gh_query, {"endCursor": commits["pageInfo"]["endCursor"]}
-            )["repository"]["ref"]["target"]["history"]
-            commits["nodes"].extend(new_commits["nodes"])
-            commits = {"nodes": commits["nodes"], "pageInfo": new_commits["pageInfo"]}
-            commits_iter = iter(new_commits["nodes"])
-            continue
-        # With all commits fetched, retry with the next page of the dumped list until the end.
+        # Retry with the next page of the dumped list until the end.
         # Fetching the commits first ensures we check the latest commit from the branch.
-        commits_iter = iter(commits["nodes"])
+        commits_iter = iter(commits)
         try:
             dumped = next(dumped_iter)
         except StopIteration:
-            print(f"No commit found from {args.branch} in the bucket")
+            print(f"No commit found in the bucket")
             exit(1)
