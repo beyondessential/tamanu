436b35508ab1eb48a1a1571282fe71c29c6398bc
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-02-20 11:13:44 +1300

    fix(mobile): Missing responsive field font sizes (#5348)
    * Update AutocompleteModalField.tsx
    
    * add in missing fonts
    
    * remaining font points
    
    * refractor
    
    * fix(mobile): Missing responsive mobile fonts
    
    * rename fontsize variables

--- packages/mobile/App/ui/components/Dropdown/index.tsx
+++ packages/mobile/App/ui/components/Dropdown/index.tsx
@@ -103,11 +103,12 @@ export const Dropdown = React.memo(
       [selectedItems],
     );
     const filterable = options.length >= MIN_COUNT_FILTERABLE_BY_DEFAULT;
+    const fontSize = screenPercentageToDP(2.1, Orientation.Height);
     return (
       <StyledView width="100%" marginBottom={screenPercentageToDP(2.24, Orientation.Height)}>
         {!!label && (
           <StyledText
-            fontSize={14}
+            fontSize={fontSize}
             fontWeight={600}
             marginBottom={2}
             color={labelColor || theme.colors.TEXT_SUPER_DARK}
@@ -134,7 +135,7 @@ export const Dropdown = React.memo(
           selectedItemTextColor={theme.colors.PRIMARY_MAIN}
           selectedItemIconColor={theme.colors.PRIMARY_MAIN}
           itemTextColor={theme.colors.TEXT_SUPER_DARK}
-          itemFontSize={14}
+          itemFontSize={fontSize}
           searchInputStyle={{ color: theme.colors.PRIMARY_MAIN }}
           submitButtonColor={theme.colors.SAFE}
           submitButtonText="Confirm selection"
@@ -169,6 +170,7 @@ export const Dropdown = React.memo(
           textInputProps={filterable ? {} : { editable: false, autoFocus: false }}
           searchIcon={filterable ? undefined : null}
           disabled={disabled}
+          fontSize={fontSize}
           {...getStyleProps(error, disabled)}
         />
         {error && <TextFieldErrorMessage>{error}</TextFieldErrorMessage>}
