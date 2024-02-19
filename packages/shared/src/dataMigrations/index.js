1d54f5e99f00cb113d01b4bbf1a295623292e7de
rail-rain <takayuki@bes.au>
2024-02-20 11:17:39 +1300

    Add a function to disable a sync trigger

--- packages/shared/src/dataMigrations/index.js
+++ packages/shared/src/dataMigrations/index.js
@@ -1,2 +1,14 @@
 export { DataMigration } from './DataMigration';
 export { CursorDataMigration } from './CursorDataMigration';
+
+/*
+ * No query done in `callback` trigger the sync tick update.
+ * It's recommended to wrap this in a transaction.
+ * Otherwise, the trigger kept disabled when the callback throws an error.
+ */
+export const disableSyncTrigger = async (sequelize, callback) => {
+    const { LocalSystemFact } = sequelize.models;
+    await LocalSystemFact.set('syncTrigger', 'disabled');
+    await callback();
+    await LocalSystemFact.set('syncTrigger', 'enabled');
+};
