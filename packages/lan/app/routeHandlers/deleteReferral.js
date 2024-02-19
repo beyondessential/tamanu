3c5b94775161bfe4221f448b17663b318675b506
Chris Tran <chris@beyondessential.com.au>
2024-02-20 10:17:49 +1100

    epi-569: fixed review

--- packages/lan/app/routeHandlers/deleteReferral.js
+++ packages/lan/app/routeHandlers/deleteReferral.js
@@ -8,9 +8,10 @@ export const deleteReferral = asyncHandler(async (req, res) => {
   const model = models.Referral;
   const object = await model.findByPk(params.referralId);
 
-  if (object) {
-    await object.destroy();
+  if (!object) {
+    throw new Error(`Cannot find referral with id ${params.referralId}`)
   }
 
+  await object.destroy();
   res.send({ message: 'Referral deleted successfully' });
 });
