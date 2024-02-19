936b46b1967bb8e9c8595ee7014e38e3be7abf49
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-02-19 16:34:24 +1300

    Update AutocompleteModalField.tsx

--- packages/mobile/App/ui/components/AutocompleteModal/AutocompleteModalField.tsx
+++ packages/mobile/App/ui/components/AutocompleteModal/AutocompleteModalField.tsx
@@ -41,10 +41,11 @@ export const AutocompleteModalField = ({
     setLabel(selectedItem.label);
   };
 
-  const openModal = (): void => navigation.navigate(modalRoute, {
-    callback: onPress,
-    suggester,
-  });
+  const openModal = (): void =>
+    navigation.navigate(modalRoute, {
+      callback: onPress,
+      suggester,
+    });
 
   useEffect(() => {
     if (!suggester) return;
@@ -62,7 +63,7 @@ export const AutocompleteModalField = ({
     <StyledView marginBottom={screenPercentageToDP('2.24', Orientation.Height)} width="100%">
       {!!fieldLabel && (
         <StyledText
-          fontSize={14}
+          fontSize={screenPercentageToDP(2.1, Orientation.Height)}
           fontWeight={600}
           marginBottom={2}
           color={theme.colors.TEXT_SUPER_DARK}
@@ -83,7 +84,7 @@ export const AutocompleteModalField = ({
         borderColor={error ? theme.colors.ERROR : '#EBEBEB'}
         borderWidth={1}
         fontWeight={400}
-        fontSize={15}
+        fontSize={screenPercentageToDP(2.1, Orientation.Height)}
         padding={10}
         onPress={openModal}
         disabled={disabled}
