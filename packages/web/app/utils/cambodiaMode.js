55533ee2f96e5fb5025b5cc311476fabe3e52534
Tom Caiger <caigertom@gmail.com>
2024-02-22 09:52:35 +1300

    Update cambodiaMode.js

--- packages/web/app/utils/cambodiaMode.js
+++ packages/web/app/utils/cambodiaMode.js
@@ -16,6 +16,9 @@ export const checkIsURLCambodia = (currentURL = window.location.hostname) => {
   return regex.test(currentURL);
 };
 
+// We would ideally like to use .env variables to switch between brands, but this is not possible at the moment with
+// our build setup. There is a ticket to update the build system (NOT-298) which will allow us to use .env variables
+// Note that normal config or settings is not suitable since the brand config also needs to be accessed from .html files
 const getBrandConfig = () => (checkIsURLCambodia() ? CAMBODIA_CONFIG : TAMANU_CONFIG);
 
 export const getBrandName = () => getBrandConfig().name;
