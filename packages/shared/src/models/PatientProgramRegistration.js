86b6e8a9fe5e1303dafc2c38204f10cacfe45690
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2024-02-21 23:52:43 -0400

    fix(programRegistries): TAN-2482: Add missing foreign keys and constraints (#5370)
    * Derive clinician ID from submitting user ID
    
    * Ensure we will always keep a clinician ID
    
    * Make color nullable
    
    * Make clinician ID non nullable
    
    * Add missing foreign keys in server and change clinician ID to be required in patient program registrations
    
    * Errata: wrong fk name
    
    * Include required clinician ID
    
    * Add required clinician ID
    
    * Change truncate order to accommodate for dependencies
    
    * Inject missing user ID

--- packages/shared/src/models/PatientProgramRegistration.js
+++ packages/shared/src/models/PatientProgramRegistration.js
@@ -63,7 +63,7 @@ export class PatientProgramRegistration extends Model {
     });
 
     this.belongsTo(models.User, {
-      foreignKey: 'clinicianId',
+      foreignKey: { name: 'clinicianId', allowNull: false },
       as: 'clinician',
     });
 
