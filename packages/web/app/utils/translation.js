e287921b81d04591817cf76c42303fa0387c9a84
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-02-29 08:48:02 +1300

    lint

--- packages/web/app/utils/translation.js
+++ packages/web/app/utils/translation.js
@@ -1,4 +1,5 @@
 import { LOCAL_STORAGE_KEYS } from '../constants';
 import { ENGLISH_LANGUAGE_CODE } from '@tamanu/constants';
 
-export const getCurrentLanguageCode = () => localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE) || ENGLISH_LANGUAGE_CODE;
+export const getCurrentLanguageCode = () =>
+  localStorage.getItem(LOCAL_STORAGE_KEYS.LANGUAGE) || ENGLISH_LANGUAGE_CODE;
