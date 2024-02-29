5ce5b10e6f4cee386b102eec866bb018cf2b7772
tareq89 <tareq.aziz@dsinnovators.com>
2024-02-29 21:23:26 +0600

    Update 'ID Forms' modal to new UI

--- packages/web/app/components/PatientPrinting/modals/PrintPatientDetailsModal.jsx
+++ packages/web/app/components/PatientPrinting/modals/PrintPatientDetailsModal.jsx
@@ -15,32 +15,51 @@ import { CovidClearanceCertificateModal } from './CovidClearanceCertificateModal
 import { BirthNotificationCertificateModal } from './BirthNotificationCertificateModal';
 import { TranslatedText } from '../../Translation/TranslatedText';
 import { IPSQRCodeModal } from './IPSQRCodeModal';
+import { IdCardIcon } from '../icons/IdCardIcon';
+import { MultilabelIdIcon } from '../icons/MultilabelIdIcon';
+import { TestCertificateCovid19Icon } from '../icons/TestCertificateCovid19Icon';
+import { ClearenceCertificateCovid19Icon } from '../icons/ClearenceCertificateCovid19Icon';
+import { BirthNotificationIcon } from '../icons/BirthNotificationIcon';
+import { InternationPatientSummaryIcon } from '../icons/InternationPatientSummaryIcon';
 
 const PRINT_OPTIONS = {
   barcode: {
-    label: <TranslatedText stringId="patientDetails.print.action.idLabels" fallback="ID Labels" />,
+    label: (
+      <TranslatedText
+        stringId="patientDetails.print.action.idLabels"
+        fallback="Multiple ID labels"
+      />
+    ),
+    caption: 'A4 sheet of multipele patient identification labels',
+    icon: MultilabelIdIcon,
     component: PatientStickerLabelPage,
   },
   idcard: {
     label: <TranslatedText stringId="patientDetails.print.action.idCard" fallback="ID Card" />,
+    caption: 'Patient identification card',
+    icon: IdCardIcon,
     component: PatientIDCardPage,
   },
   covidTestCert: {
     label: (
       <TranslatedText
         stringId="patientDetails.print.action.covid19TestCertificate"
-        fallback="COVID-19 test certificate"
+        fallback="Test certificate - COVID-19"
       />
     ),
+    caption: 'Patient COVID-19 test certificate',
+    icon: TestCertificateCovid19Icon,
     component: CovidTestCertificateModal,
   },
   covidClearanceCert: {
     label: (
       <TranslatedText
         stringId="patientDetails.print.action.covid19ClearanceCertificate"
-        fallback="COVID-19 clearance certificate"
+        fallback="Clearance certificate - COVID-19"
       />
     ),
+    caption: 'Patient COVID-19 clearance certificate',
+    icon: ClearenceCertificateCovid19Icon,
     component: CovidClearanceCertificateModal,
     condition: getLocalisation => getLocalisation('features.enableCovidClearanceCertificate'),
   },
@@ -51,10 +70,14 @@ const PRINT_OPTIONS = {
         fallback="Birth notification"
       />
     ),
+    caption: 'Patient birth notification document',
+    icon: BirthNotificationIcon,
     component: BirthNotificationCertificateModal,
   },
   ipsQrCode: {
     label: 'International Patient Summary',
+    caption: 'Email International Patient Summary QR Code',
+    icon: InternationPatientSummaryIcon,
     component: IPSQRCodeModal,
     condition: (_, ability) => ability?.can('create', 'IPSRequest'),
   },
@@ -64,44 +87,160 @@ const PrintOptionList = ({ className, setCurrentlyPrinting }) => {
   const { getLocalisation } = useLocalisation();
   const { ability } = useAuth();
 
+  const isVisible = condition => !condition || condition(getLocalisation, ability);
+
   return (
     <div className={className}>
-      {Object.entries(PRINT_OPTIONS)
-        .filter(([, { condition }]) => !condition || condition(getLocalisation, ability))
-        .map(([type, { label, icon }]) => (
+      <Header>Identification</Header>
+      <StyledPrintOptionsRow>
+        {isVisible(PRINT_OPTIONS.idcard.condition) && (
           <PrintOption
-            key={type}
-            label={label}
-            onPress={() => setCurrentlyPrinting(type)}
-            icon={icon}
+            label={PRINT_OPTIONS.idcard.label}
+            caption={PRINT_OPTIONS.idcard.caption}
+            onPress={() => setCurrentlyPrinting('idcard')}
+            icon={PRINT_OPTIONS.idcard.icon}
           />
-        ))}
+        )}
+        {isVisible(PRINT_OPTIONS.barcode.condition) && (
+          <PrintOption
+            label={PRINT_OPTIONS.barcode.label}
+            caption={PRINT_OPTIONS.barcode.caption}
+            onPress={() => setCurrentlyPrinting('barcode')}
+            icon={PRINT_OPTIONS.barcode.icon}
+          />
+        )}
+      </StyledPrintOptionsRow>
+      <div></div>
+      <Header>Certificates</Header>
+      <StyledPrintOptionsRow>
+        {isVisible(PRINT_OPTIONS.birthNotification.condition) && (
+          <PrintOption
+            label={PRINT_OPTIONS.birthNotification.label}
+            caption={PRINT_OPTIONS.birthNotification.caption}
+            onPress={() => setCurrentlyPrinting('birthNotification')}
+            icon={PRINT_OPTIONS.birthNotification.icon}
+          />
+        )}
+        {isVisible(PRINT_OPTIONS.covidTestCert.condition) && (
+          <PrintOption
+            label={PRINT_OPTIONS.covidTestCert.label}
+            caption={PRINT_OPTIONS.covidTestCert.caption}
+            onPress={() => setCurrentlyPrinting('covidTestCert')}
+            icon={PRINT_OPTIONS.covidTestCert.icon}
+          />
+        )}
+      </StyledPrintOptionsRow>
+      <StyledPrintOptionsRow>
+        {isVisible(PRINT_OPTIONS.covidClearanceCert.condition) && (
+          <PrintOption
+            label={PRINT_OPTIONS.covidClearanceCert.label}
+            caption={PRINT_OPTIONS.covidClearanceCert.caption}
+            onPress={() => setCurrentlyPrinting('covidClearanceCert')}
+            icon={PRINT_OPTIONS.covidClearanceCert.icon}
+          />
+        )}
+        {isVisible(PRINT_OPTIONS.ipsQrCode.condition) && (
+          <PrintOption
+            label={PRINT_OPTIONS.ipsQrCode.label}
+            caption={PRINT_OPTIONS.ipsQrCode.caption}
+            onPress={() => setCurrentlyPrinting('ipsQrCode')}
+            icon={PRINT_OPTIONS.ipsQrCode.icon}
+          />
+        )}
+      </StyledPrintOptionsRow>
     </div>
   );
 };
-const StyledPrintOptionList = styled(PrintOptionList)`
+const StyledPrintOptionContainer = styled(PrintOptionList)`
   padding: 20px 50px;
   display: flex;
   flex-direction: column;
 `;
 
+const Header = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 21px;
+  letter-spacing: 0px;
+  text-align: left;
+  color: ${Colors.darkestText};
+`;
+
+const StyledPrintOptionsRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  button:first-child {
+    margin-right: 30px;
+  }
+`;
+
 const PrintOptionButton = styled(Button)`
   background: ${Colors.white};
-  border: 2px solid ${Colors.primary};
+  border: 1px solid ${Colors.outline};
   border-radius: 5px;
   color: ${Colors.primary};
 
   justify-content: center;
   text-align: -webkit-center;
 
-  height: 63px;
-  width: 100%;
+  height: 100px;
+  width: 435px;
   margin: 14px 0px;
+  .MuiButton-label {
+    .Container {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: flex-start;
+      width: 435px;
+      .Icon {
+        width: 20%;
+        height: 43px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+      }
+      .Title {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        align-self: baseline;
+        margin-top: 5px;
+        width: 80%;
+        .Heading {
+          font-size: 16px;
+          font-weight: 500;
+          line-height: 21px;
+          letter-spacing: 0px;
+          text-align: left;
+          color: ${Colors.darkestText};
+        }
+        .SubHeading {
+          font-size: 14px;
+          font-weight: 400;
+          line-height: 18px;
+          letter-spacing: 0px;
+          text-align: left;
+          color: ${Colors.midText};
+        }
+      }
+    }
+  }
 `;
 
-const PrintOption = ({ label, onPress }) => (
+const PrintOption = ({ label, caption, icon: Icon, onPress }) => (
   <PrintOptionButton color="default" onClick={onPress}>
-    {label}
+    <div className="Container">
+      <div className="Icon">
+        <Icon />
+      </div>
+      <div className="Title">
+        <div className="Heading">{label}</div>
+        <div className="SubHeading">{caption}</div>
+      </div>
+    </div>
   </PrintOptionButton>
 );
 
@@ -154,8 +293,14 @@ export const PrintPatientDetailsModal = ({ patient }) => {
     if (!printType) {
       // no selection yet -- show selection modal
       return (
-        <Modal title="Select item to print" open={isModalOpen} onClose={closeModal}>
-          <StyledPrintOptionList setCurrentlyPrinting={setCurrentlyPrinting} />
+        <Modal
+          title="Select item"
+          open={isModalOpen}
+          onClose={closeModal}
+          fullWidth={false}
+          width={false}
+        >
+          <StyledPrintOptionContainer setCurrentlyPrinting={setCurrentlyPrinting} />
         </Modal>
       );
     }
@@ -195,10 +340,7 @@ export const PrintPatientDetailsModal = ({ patient }) => {
   return (
     <>
       <Button size="small" onClick={openModal}>
-        <TranslatedText
-          stringId="patient.detailsSidebar.action.printIdForms"
-          fallback="ID forms"
-        />
+        <TranslatedText stringId="patient.detailsSidebar.action.printIdForms" fallback="ID forms" />
       </Button>
       {mainComponent}
     </>
