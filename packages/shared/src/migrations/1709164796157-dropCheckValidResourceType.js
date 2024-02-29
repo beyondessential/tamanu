28449f951152d2404d1f987ab3cfaffc2aa593d0
Chris Tran <chris@beyondessential.com.au>
2024-02-29 11:27:22 +1100

    sav-541: more doco

--- packages/shared/src/migrations/1709164796157-dropCheckValidResourceType.js
+++ packages/shared/src/migrations/1709164796157-dropCheckValidResourceType.js
@@ -1,3 +1,6 @@
+// In a hot-fix for some deployments, we turned off materialisation of some FHIR resources through a pre-insert
+// trigger on `fhir.jobs`. In this release, which resources get materialised is configurable, so we no longer need
+// the db hack to do that
 export async function up(query) {
   await query.sequelize.query(`
     DROP TRIGGER IF EXISTS check_valid_resource_type
