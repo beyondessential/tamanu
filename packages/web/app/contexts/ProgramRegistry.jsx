e5b7e70655280fd08c37842f4f14fafe217fe92b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-03-03 22:41:03 -0400

    Add new setter to find registries by program ID

--- packages/web/app/contexts/ProgramRegistry.jsx
+++ packages/web/app/contexts/ProgramRegistry.jsx
@@ -1,20 +1,33 @@
 import React, { createContext, useContext, useState } from 'react';
+import { useListOfProgramRegistryQuery } from '../api/queries/useProgramRegistryQuery';
 
 export const ProgramRegistryContext = createContext({
   programRegistryId: null,
   setProgramRegistryId: () => {},
+  setProgramRegistryIdByProgramId: () => {},
 });
 
 export const useProgramRegistryContext = () => useContext(ProgramRegistryContext);
 
 export const ProgramRegistryProvider = ({ children }) => {
   const [programRegistryId, setProgramRegistryId] = useState(null);
+  const { data: programRegistries, refetch } = useListOfProgramRegistryQuery();
+  const setProgramRegistryIdByProgramId = async programId => {
+    await refetch();
+    const programRegistry = programRegistries.data.find(pr => pr.programId === programId);
+    if (programRegistry) {
+      setProgramRegistryId(programRegistry.id);
+      return;
+    }
+    setProgramRegistryId(null);
+  };
 
   return (
     <ProgramRegistryContext.Provider
       value={{
         programRegistryId,
         setProgramRegistryId,
+        setProgramRegistryIdByProgramId,
       }}
     >
       {children}
