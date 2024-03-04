421d8ba6c0067842ff584796f51e4e4bf033dcfc
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-03-03 20:59:47 -0400

    Include setter for context hook

--- packages/web/app/contexts/ProgramRegistry.js
+++ packages/web/app/contexts/ProgramRegistry.js
@@ -1,9 +1,23 @@
-import { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 
 export const ProgramRegistryContext = createContext({
   programRegistryId: null,
+  setProgramRegistryId: () => {},
 });
 
 export const useProgramRegistry = () => useContext(ProgramRegistryContext);
 
-export const ProgramRegistryProvider = ProgramRegistryContext.Provider;
+export const ProgramRegistryProvider = ({ children }) => {
+  const [programRegistryId, setProgramRegistryId] = useState(null);
+
+  return (
+    <ProgramRegistryContext.Provider
+      value={{
+        programRegistryId,
+        setProgramRegistryId,
+      }}
+    >
+      {children}
+    </ProgramRegistryContext.Provider>
+  );
+};
