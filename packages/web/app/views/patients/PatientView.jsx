8542820501a7260f1c25d55862f017b025b4f07b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-07 11:30:54 +1300

    fix(web): NASS-1189: Pad data query invalidation bug (#5461)

--- packages/web/app/views/patients/PatientView.jsx
+++ packages/web/app/views/patients/PatientView.jsx
@@ -26,6 +26,7 @@ import { useUrlSearchParams } from '../../utils/useUrlSearchParams';
 import { PatientSearchParametersProvider } from '../../contexts/PatientViewSearchParameters';
 import { invalidatePatientDataQueries } from '../../utils';
 import { usePatientNavigation } from '../../utils/usePatientNavigation';
+import { useSyncState } from '../../contexts/SyncState';
 
 const StyledDisplayTabs = styled(TabDisplay)`
   overflow: initial;
@@ -119,6 +120,8 @@ export const PatientView = () => {
   const [currentTab, setCurrentTab] = useState(queryTab || PATIENT_TABS.HISTORY);
   const disabled = !!patient.death;
   const api = useApi();
+  const syncState = useSyncState();
+  const isSyncing = syncState.isPatientSyncing(patient.id);
   const { data: additionalData, isLoading: isLoadingAdditionalData } = useQuery(
     ['additionalData', patient.id],
     () => api.get(`patient/${patient.id}/additionalData`),
@@ -144,14 +147,14 @@ export const PatientView = () => {
   }, [api, patient.id]);
 
   useEffect(() => {
-    if (!patient.syncing) {
+    if (!isSyncing) {
       // invalidate the cache of patient data queries to reload the patient data
       invalidatePatientDataQueries(queryClient, patient.id);
     }
 
     // invalidate queries only when syncing is done (changed from true to false)
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [patient.syncing]);
+  }, [isSyncing]);
 
   const visibleTabs = usePatientTabs();
 
