d1facab3c31d18935b055707b10e8609ef4c61e3
Kurt <kujo4pm@gmail.com>
2024-03-06 11:05:41 +0100

    Removing legacy code from VPS integration

--- packages/shared/src/models/fhir/DiagnosticReport/FhirDiagnosticReport.js
+++ packages/shared/src/models/fhir/DiagnosticReport/FhirDiagnosticReport.js
@@ -8,16 +8,11 @@ import {
 } from '@tamanu/constants';
 import { FhirCodeableConcept, FhirReference } from '../../../services/fhirTypes';
 import { FhirResource } from '../Resource';
-import { fromLabTests } from './getQueryToFindUpstreamIds';
-import { getQueryOptions } from './getQueryOptions';
-import { getValues } from './getValues';
 
 export class FhirDiagnosticReport extends FhirResource {
   static init(options, models) {
     super.init(
       {
-        extension: DataTypes.JSONB, // This field is part of DomainResource
-        identifier: DataTypes.JSONB,
         basedOn: DataTypes.JSONB,
         status: {
           type: Sequelize.TEXT,
@@ -27,20 +22,6 @@ export class FhirDiagnosticReport extends FhirResource {
           type: DataTypes.JSONB,
           allowNull: false,
         },
-        subject: {
-          type: DataTypes.JSONB,
-          allowNull: true,
-        },
-        effectiveDateTime: {
-          type: DataTypes.TEXT,
-          allowNull: true,
-        },
-        issued: {
-          type: DataTypes.TEXT,
-          allowNull: true,
-        },
-        performer: DataTypes.JSONB,
-        result: DataTypes.JSONB,
       },
       options,
     );
@@ -50,19 +31,10 @@ export class FhirDiagnosticReport extends FhirResource {
       models.LabTest,
       models.LabRequest,
       models.LabTestType,
-      models.ReferenceData,
-      models.Encounter,
-      models.Patient,
-      models.User,
     ];
   }
 
-  static CAN_DO = new Set([
-    FHIR_INTERACTIONS.INSTANCE.READ,
-    FHIR_INTERACTIONS.TYPE.SEARCH,
-    FHIR_INTERACTIONS.INTERNAL.MATERIALISE,
-    FHIR_INTERACTIONS.TYPE.CREATE,
-  ]);
+  static CAN_DO = new Set([FHIR_INTERACTIONS.TYPE.CREATE]);
 
   static get INTAKE_SCHEMA() {
     return yup.object({
@@ -78,6 +50,7 @@ export class FhirDiagnosticReport extends FhirResource {
   // This is beginning very modestly - can extend to handle full 
   // results soon.
   async pushUpstream() {
+    console.log('buildin12g');
     const { FhirServiceRequest, LabRequest } = this.sequelize.models;
 
     const { type, reference } = this.basedOn;
@@ -90,10 +63,10 @@ export class FhirDiagnosticReport extends FhirResource {
     }
     const serviceRequestFhirId = ref[1];
 
-    const serviceRequest = await FhirServiceRequest.findByPk(serviceRequestFhirId);
+    const serviceRequest = await FhirServiceRequest.findOne({ where: { id: serviceRequestFhirId } });
 
     if (!serviceRequest) {
-      throw new Invalid(`ServiceRequest ${failedId} does not exist in Tamanu`, {
+      throw new Invalid(`ServiceRequest ${serviceRequestFhirId} does not exist in Tamanu`, {
         code: FHIR_ISSUE_TYPE.INVALID.VALUE,
       });
     }
@@ -135,21 +108,6 @@ export class FhirDiagnosticReport extends FhirResource {
       default:
         return null;
     }
-  }
-
-  async updateMaterialisation() {
-    const upstream = await this.getUpstream(getQueryOptions(this.sequelize.models));
-    const values = await getValues(upstream, this.sequelize.models);
-    this.set(values);
-  }
-
-  static async queryToFindUpstreamIdsFromTable(upstreamTable, table, id) {
-    const { LabTest } = this.sequelize.models;
-
-    if (upstreamTable === LabTest.tableName) {
-      return fromLabTests(this.sequelize.models, table, id);
-    }
-    return null;
   }
 
 }
