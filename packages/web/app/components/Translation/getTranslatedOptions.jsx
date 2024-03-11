d15439b0f9daadfb60c057781049f91e18e59f7c
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-12 12:43:57 +1300

    allow for undefined options

--- packages/web/app/components/Translation/getTranslatedOptions.jsx
+++ packages/web/app/components/Translation/getTranslatedOptions.jsx
@@ -2,17 +2,12 @@ import { TranslatedText } from './TranslatedText';
 import { camelCase } from 'lodash';
 import React from 'react';
 
-export const getTranslatedOptions = (options, prefix) =>
-  options.map(option => ({
+export const getTranslatedOptions = (options, prefix) => {
+  if (!options) return [];
+  return options.map(option => ({
     value: option.value,
     label: (
-      <TranslatedText
-        stringId={
-          option.label.type === TranslatedText
-            ? option.label.props.stringId
-            : `${prefix}.${camelCase(option.label)}`
-        }
-        fallback={option.label.type === TranslatedText ? option.label.props.fallback : option.label}
-      />
+      <TranslatedText stringId={`${prefix}.${camelCase(option.label)}`} fallback={option.label} />
     ),
   }));
+};
