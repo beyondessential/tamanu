d7b6a6384a25af7f9ba78480f426e0074123e294
Tom Caiger <caigertom@gmail.com>
2024-03-11 17:29:52 +1300

    Update jest.config.mjs

--- packages/central-server/jest.config.mjs
+++ packages/central-server/jest.config.mjs
@@ -2,7 +2,7 @@ import { config } from '../../common.jest.config.mjs';
 
 export default config(import.meta, {
   testEnvironment: 'jest-environment-node',
-  testPathIgnorePatterns: ['<rootDir>/app/', '<rootDir>/__disttests__/', '/node_modules/'],
+  testPathIgnorePatterns: ['<rootDir>/app/', '<rootDir>/__tests__/', '/node_modules/'],
   globalTeardown: '<rootDir>/__disttests__/teardown.js',
   setupFiles: ['<rootDir>/__disttests__/setup.js'],
   setupFilesAfterEnv: ['<rootDir>/__disttests__/configureEnvironment.js', 'jest-expect-message'],
