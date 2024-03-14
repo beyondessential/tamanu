0b2fb871c341d51376c26cd5ffb9ab0f201852ac
Daniel Nash <dannash200@gmail.com>
2024-03-14 13:03:17 +1300

    feat/nass-1111: add catchmentId migration

---
+++ packages/shared/src/migrations/1710374155691-addCatchmentIdToFacility.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+// remove the above line
+
+import { DataTypes } from 'sequelize';
+
+export async function up(query) {
+  await query.addColumn('facilities', 'catchment_id', {
+    type: DataTypes.TEXT,
+    references: {
+      model: 'reference_data',
+      key: 'id',
+    },
+    allowNull: true,
+  });
+  await query.addIndex('facilities', ['catchment_id']);
+}
+
+export async function down(query) {
+  await query.removeColumn('facilities', 'catchment_id');
+}
