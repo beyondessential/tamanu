0449e6615b490ac6532ad66f401717a31b24b74e
Nam Hoang <nam@bes.au>
2024-03-14 13:25:35 +0700

    misc(vaccine): combine migrations

--- packages/shared/src/migrations/1710144510920-insertRoutineVaccineThresholdSetting.js
+++ packages/shared/src/migrations/1710144510920-insertRoutineVaccineThresholdSetting.js
@@ -1,43 +1,54 @@
 /** @typedef {import('sequelize').QueryInterface} QueryInterface */
+import config from 'config';
+import { Op } from 'sequelize';
+
+const DEFAULT_SETTINGS = {
+  'routineVaccine.thresholds': JSON.stringify([
+    {
+      threshold: 28,
+      status: 'SCHEDULED',
+    },
+    {
+      threshold: 7,
+      status: 'UPCOMING',
+    },
+    {
+      threshold: -7,
+      status: 'DUE',
+    },
+    {
+      threshold: -55,
+      status: 'OVERDUE',
+    },
+    {
+      threshold: '-Infinity',
+      status: 'MISSED',
+    },
+  ]),
+  'outineVaccine.ageLimit': 15,
+};
 
 /**
  * @param {QueryInterface} query
  */
 export async function up(query) {
-  await query.insert(null, 'settings', {
-    key: 'routineVaccine.thresholds',
-    value: JSON.stringify([
-      {
-        threshold: 28,
-        status: 'SCHEDULED',
-      },
-      {
-        threshold: 7,
-        status: 'UPCOMING',
-      },
-      {
-        threshold: -7,
-        status: 'DUE',
-      },
-      {
-        threshold: -55,
-        status: 'OVERDUE',
-      },
-      {
-        threshold: '-Infinity',
-        status: 'MISSED',
-      },
-    ]),
-  });
+  if (config?.serverFacilityId) return;
+  await query.bulkInsert(
+    'settings',
+    Object.entries(DEFAULT_SETTINGS).map(([key, value]) => ({
+      key,
+      value,
+    })),
+  );
 }
 
 /**
  * @param {QueryInterface} query
  */
 export async function down(query) {
-  await query.delete(null, 'settings', {
-    comparator: {
-      key: 'routineVaccine.thresholds',
+  await query.bulkDelete('settings', {
+    key: {
+      [Op.in]: Object.entries(DEFAULT_SETTINGS).map(([key]) => key),
     },
   });
 }
