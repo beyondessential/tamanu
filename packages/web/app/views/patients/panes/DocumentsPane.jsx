189e9e8386d544ce7fed909d3050d68857e8b41a
Huy-Tran-Huu-Le <huy@bes.au>
2024-03-14 13:56:16 +0700

    feat: NASS-1175: continue using TranslatedText for toast

--- packages/web/app/views/patients/panes/DocumentsPane.jsx
+++ packages/web/app/views/patients/panes/DocumentsPane.jsx
@@ -3,7 +3,6 @@ import { extension } from 'mime-types';
 
 import { useApi } from '../../../api';
 import { notify, notifyError, notifySuccess } from '../../../utils';
-import { useTranslation } from '../../../contexts/Translation';
 import { DocumentPreviewModal } from '../../../components/DocumentPreview';
 import { DocumentsTable } from '../../../components/DocumentsTable';
 import { DocumentModal } from '../../../components/DocumentModal';
@@ -30,7 +29,6 @@ const base64ToUint8Array = base64 => {
 export const DocumentsPane = React.memo(({ encounter, patient }) => {
   const api = useApi();
   const [dataUrl, setDataUrl] = useState('');
-  const { getTranslation } = useTranslation();
   const [modalStatus, setModalStatus] = useState(MODAL_STATES.CLOSED);
   const [searchParameters, setSearchParameters] = useState({});
   const [selectedDocument, setSelectedDocument] = useState(null);
@@ -68,7 +66,13 @@ export const DocumentsPane = React.memo(({ encounter, patient }) => {
     async document => {
       try {
         // Give feedback to user that download is starting
-        notify(getTranslation('document.notification.downloadStart', 'Your download has started, please wait.'), { type: 'info' });
+        notify(
+          <TranslatedText
+            stringId='document.notification.downloadStart'
+            fallback='Your download has started, please wait.'
+          />, 
+          { type: 'info' }
+        );
 
         // Download attachment (*currently the API only supports base64 responses)
         const { data } = await api.get(`attachment/${document.attachmentId}`, {
@@ -83,7 +87,12 @@ export const DocumentsPane = React.memo(({ encounter, patient }) => {
           extensions: [fileExtension],
         });
 
-        notifySuccess(getTranslation('document.notification.downloadSuccess', 'Successfully downloaded file'));
+        notifySuccess(
+          <TranslatedText
+            stringId='document.notification.downloadSuccess'
+            fallback='Successfully downloaded file'
+          />
+        );
       } catch (error) {
         notifyError(error.message);
       }
