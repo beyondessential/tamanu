475980e102be60d75c7da0fe44174e1047090b1a
Daniel Nash <dannash200@gmail.com>
2024-03-14 14:02:00 +1300

    feat/nass-1111: story working

--- packages/web/app/components/Field/LinkedField.jsx
+++ packages/web/app/components/Field/LinkedField.jsx
@@ -1,5 +1,5 @@
 import { Field, useField } from 'formik';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../../api/useApi';
 
@@ -13,7 +13,15 @@ const useLinkedFieldQuery = (endpoint, name, value) => {
 
 export const LinkedField = ({ linkedFieldName, endpoint, ...props }) => {
   const [field] = useField(props.name);
+  const linkedFieldRes = useField(linkedFieldName);
   const { data, isLoading } = useLinkedFieldQuery(endpoint, props.name, field.value);
+
+  useEffect(() => {
+    if (data && !isLoading) {
+      console.log('Setting linked field', linkedFieldName, data);
+      linkedFieldRes[2].setValue(data.id);
+    }
+  }, [data, isLoading, linkedFieldName, linkedFieldRes]);
 
   return <Field {...props} />;
 };
