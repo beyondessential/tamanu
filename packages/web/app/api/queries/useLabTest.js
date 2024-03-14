7aaeb7bc8050ad22e6d9bb86e5830dac28bc2d5a
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-15 11:12:03 +1300

    Update useLabTest.js

--- packages/web/app/api/queries/useLabTest.js
+++ packages/web/app/api/queries/useLabTest.js
@@ -3,8 +3,9 @@ import { useApi } from '../useApi';
 
 export const useLabTest = labTestId => {
   const api = useApi();
-
-  return useQuery(['labTest', labTestId], () =>
-    api.get(`labTest/${encodeURIComponent(labTestId)}`),
+  return useQuery(
+    ['labTest', labTestId],
+    () => api.get(`labTest/${encodeURIComponent(labTestId)}`),
+    { enabled: !!labTestId },
   );
 };
