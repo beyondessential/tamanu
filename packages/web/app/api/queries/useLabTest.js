4786ad63999103adcbb4ff20965bc12cee70ea35
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-15 14:41:33 +1300

    fix(web): useLabTest throwing error when undefined (#5513)
    * Update useLabTest.js
    
    * [generate-fake-db] commit

--- packages/web/app/api/queries/useLabTest.js
+++ packages/web/app/api/queries/useLabTest.js
@@ -3,8 +3,10 @@ import { useApi } from '../useApi';
 
 export const useLabTest = labTestId => {
   const api = useApi();
-
-  return useQuery(['labTest', labTestId], () =>
-    api.get(`labTest/${encodeURIComponent(labTestId)}`),
+  
+  return useQuery(
+    ['labTest', labTestId],
+    () => api.get(`labTest/${encodeURIComponent(labTestId)}`),
+    { enabled: !!labTestId },
   );
 };
