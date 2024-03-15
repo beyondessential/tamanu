1cbbc8385cba4631960d9caa32e8e69eccd661fb
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-15 14:44:23 +1300

    fix(web): allow generic translations in select fields (#5508)
    * use proper tranlslatable select options format
    
    * only translate supplied strings
    
    * refractor

--- packages/web/app/components/Translation/getTranslatedOptions.jsx
+++ packages/web/app/components/Translation/getTranslatedOptions.jsx
@@ -4,10 +4,13 @@ import React from 'react';
 
 export const getTranslatedOptions = (options, prefix) => {
   if (!options) return [];
-  return options.map(option => ({
-    value: option.value,
-    label: (
-      <TranslatedText stringId={`${prefix}.${camelCase(option.label)}`} fallback={option.label} />
-    ),
-  }));
+  return options.map(option => {
+    const { label, value } = option;
+    return typeof label === 'string'
+      ? {
+          value,
+          label: <TranslatedText stringId={`${prefix}.${camelCase(label)}`} fallback={label} />,
+        }
+      : option;
+  });
 };
