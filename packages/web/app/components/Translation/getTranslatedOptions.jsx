bdd99b7516f4ab4f67d013f5d6dc1b3dfb159908
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-03-18 11:57:40 +1300

    get disabled option logic working with select

--- packages/web/app/components/Translation/getTranslatedOptions.jsx
+++ packages/web/app/components/Translation/getTranslatedOptions.jsx
@@ -5,11 +5,12 @@ import React from 'react';
 export const getTranslatedOptions = (options, prefix) => {
   if (!options) return [];
   return options.map(option => {
-    const { label, value } = option;
+    const { label, value, isDisabled } = option;
     return typeof label === 'string'
       ? {
           value,
           label: <TranslatedText stringId={`${prefix}.${camelCase(label)}`} fallback={label} />,
+          isDisabled,
         }
       : option;
   });
