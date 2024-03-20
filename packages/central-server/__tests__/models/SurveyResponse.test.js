99e9cec658056d5e7b63f457db7e76c2b333dc0f
Tom Caiger <caigertom@gmail.com>
2024-03-21 12:25:36 +1300

    Update SurveyResponse.test.js

--- packages/central-server/__tests__/models/SurveyResponse.test.js
+++ packages/central-server/__tests__/models/SurveyResponse.test.js
@@ -1,6 +1,12 @@
 import { fake } from '@tamanu/shared/test-helpers/fake';
+import { PATIENT_FIELD_DEFINITION_TYPES } from '@tamanu/constants/patientFields';
 import { findOneOrCreate } from '@tamanu/shared/test-helpers/factory';
-import { PROGRAM_DATA_ELEMENT_TYPES, REGISTRATION_STATUSES, SURVEY_TYPES, VISIBILITY_STATUSES } from '@tamanu/constants';
+import {
+  PROGRAM_DATA_ELEMENT_TYPES,
+  REGISTRATION_STATUSES,
+  SURVEY_TYPES,
+  VISIBILITY_STATUSES,
+} from '@tamanu/constants';
 import { createTestContext } from '../utilities';
 
 async function createDummySurvey(models) {
@@ -27,6 +33,130 @@ async function createDummyDataElement(models, survey, { config, ...dataElementOv
 
   return { dataElement };
 }
+
+describe('SurveyResponse.createWithAnswers Patient Fields', () => {
+  let ctx, models, patientId, encounterId, surveyId, dataElementId, patientFieldDefinitionId;
+
+  beforeAll(async () => {
+    ctx = await createTestContext();
+    models = ctx.store.models;
+    const patient = await models.Patient.create(fake(models.Patient));
+    const encounter = await findOneOrCreate(models, models.Encounter, {
+      patientId: patient.id,
+    });
+
+    const { PatientFieldDefinitionCategory, PatientFieldDefinition } = models;
+    const category = await PatientFieldDefinitionCategory.create(
+      fake(PatientFieldDefinitionCategory),
+    );
+    const definition = await PatientFieldDefinition.create({
+      id: 'customFatherNameField',
+      categoryId: category.id,
+      name: 'father',
+      fieldType: PATIENT_FIELD_DEFINITION_TYPES.STRING,
+    });
+    const survey = await createDummySurvey(models);
+    const { dataElement } = await createDummyDataElement(models, survey, {
+      type: PROGRAM_DATA_ELEMENT_TYPES.PATIENT_DATA,
+      config: {
+        column: definition.id,
+        writeToPatient: {
+          fieldName: definition.id,
+          isCustomPatientField: true,
+        },
+      },
+    });
+
+    patientId = patient.id;
+    encounterId = encounter.id;
+    surveyId = survey.id;
+    dataElementId = dataElement.id;
+    patientFieldDefinitionId = definition.id;
+  });
+
+  afterEach(async () => {
+    await models.SurveyResponse.truncate();
+    await models.SurveyResponseAnswer.truncate();
+    await models.ProgramRegistry.truncate();
+    await models.ProgramRegistryClinicalStatus.truncate();
+    await models.PatientProgramRegistration.truncate();
+  });
+
+  afterAll(async () => {
+    await ctx.close();
+  });
+
+  it('writes to patient custom fields from actions', async () => {
+    const { SurveyResponse, SurveyResponseAnswer, PatientFieldValue } = models;
+
+    await SurveyResponse.sequelize.transaction(() =>
+      models.SurveyResponse.createWithAnswers({
+        patientId,
+        encounterId,
+        surveyId: surveyId,
+        answers: {
+          [dataElementId]: 'alastair',
+        },
+      }),
+    );
+
+    expect(await SurveyResponse.findOne()).toMatchObject({
+      surveyId: surveyId,
+      encounterId,
+      result: 0,
+      resultText: '',
+    });
+    expect(await SurveyResponseAnswer.findOne()).toMatchObject({
+      dataElementId: dataElementId,
+      body: 'alastair',
+    });
+
+    expect(
+      await PatientFieldValue.findOne({
+        where: {
+          definitionId: patientFieldDefinitionId,
+          patientId,
+        },
+        order: [['updatedAt', 'DESC']],
+      }),
+    ).toMatchObject({
+      value: 'alastair',
+    });
+  });
+
+  it('edits patient custom fields from actions', async () => {
+    const { SurveyResponse, PatientFieldValue } = models;
+
+    await PatientFieldValue.create({
+      patientId: patientId,
+      definitionId: patientFieldDefinitionId,
+      value: 'john',
+    });
+
+    await SurveyResponse.sequelize.transaction(() =>
+      models.SurveyResponse.createWithAnswers({
+        patientId,
+        encounterId,
+        surveyId: surveyId,
+        answers: {
+          [dataElementId]: 'James Smith',
+        },
+      }),
+    );
+
+    expect(
+      await PatientFieldValue.findOne({
+        where: {
+          definitionId: patientFieldDefinitionId,
+          patientId,
+        },
+        order: [['updatedAt', 'DESC']],
+      }),
+    ).toMatchObject({
+      value: 'James Smith',
+    });
+  });
+});
 
 describe('SurveyResponse.createWithAnswers', () => {
   let ctx;
@@ -147,12 +277,12 @@ describe('SurveyResponse.createWithAnswers', () => {
       fake(models.ProgramRegistry, {
         visibilityStatus: VISIBILITY_STATUSES.CURRENT,
         programId: survey.programId,
-      })
+      }),
     );
     const clinicalStatus = await models.ProgramRegistryClinicalStatus.create(
       fake(models.ProgramRegistryClinicalStatus, {
         programRegistryId: registry.id,
-      })
+      }),
     );
     const { dataElement } = await createDummyDataElement(models, survey, {
       type: PROGRAM_DATA_ELEMENT_TYPES.PATIENT_DATA,
