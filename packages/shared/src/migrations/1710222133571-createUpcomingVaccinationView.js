5a72df95053a549b24d3ce45afd7ebb27f12fab7
Nam Hoang <nam@bes.au>
2024-03-26 09:08:14 +0700

    perf(vaccine): improve performance on the vaccine status query

--- packages/shared/src/migrations/1710222133571-createUpcomingVaccinationView.js
+++ packages/shared/src/migrations/1710222133571-createUpcomingVaccinationView.js
@@ -6,8 +6,7 @@
 export async function up(query) {
   await query.sequelize.query(`
 CREATE OR REPLACE VIEW upcoming_vaccinations
-AS WITH
-vaccine_thresholds AS (
+AS with vaccine_thresholds AS (
 	SELECT
 		(jsonb_array_elements(s.value) ->> 'threshold'::text)::double precision AS threshold,
 		jsonb_array_elements(s.value) ->> 'status'::text AS status
@@ -22,109 +21,59 @@ vaccine_agelimit as (
 	where s.deleted_at is null
 	and s.key = 'routineVaccine.ageLimit'
 ),
-patient_filtered as (
-	select
-		 p.id patient_id,
-		 p.date_of_birth::date
-	from patients p
-	where p.deleted_at is null
-	and p.visibility_status = 'current'
-	and p.date_of_birth::date > (select "date" from vaccine_agelimit)
+filtered_patients as (
+	select p.id patient_id, p.date_of_birth::date 
+	from patients p where p.deleted_at is null and p.visibility_status = 'current' and p.date_of_birth::date > (select "date" from vaccine_agelimit)
 ),
-scheduled_vaccine_filtered as (
-	select
-		sv.id scheduled_vaccine_id,
-		sv.vaccine_id,
-		sv.index dose_order,
-		sv.weeks_from_birth_due,
-		sv.weeks_from_last_vaccination_due
-	from scheduled_vaccines sv
-	where sv.deleted_at is null
-	and sv.visibility_status = 'current'
-	and sv.category = 'Routine'
-	and (sv.weeks_from_birth_due is not null or sv.weeks_from_last_vaccination_due is not null)
+filtered_scheduled_vaccines as (
+	select sv.id scheduled_vaccine_id, sv.vaccine_id, sv.index, sv.weeks_from_birth_due, sv.weeks_from_last_vaccination_due from scheduled_vaccines sv 
+	where sv.deleted_at is null and sv.visibility_status = 'current' and sv.category = 'Routine'
 ),
-patient_scheduled_vaccines as (
-	select
-		pf.patient_id,
-		svf.scheduled_vaccine_id,
-		svf.vaccine_id,
-		svf.dose_order,
-		case
-			when svf.weeks_from_birth_due is null then null
-			else pf.date_of_birth + svf.weeks_from_birth_due * 7
-		end fixed_due,
-		case
-			when svf.weeks_from_birth_due is not null then null
-			else svf.weeks_from_last_vaccination_due
-		end weeks_from_last_vaccination_due
-	from patient_filtered pf
-	cross join scheduled_vaccine_filtered svf
+filtered_administered_vaccines as (
+	select e.patient_id, av.scheduled_vaccine_id, av."date"::date administered_date from administered_vaccines av 
+	join encounters e on e.id = av.encounter_id
+	WHERE av.deleted_at is null and av.status = 'GIVEN'
+	and e.deleted_at is null and e.encounter_type = 'vaccination'
 ),
-patient_administered_vaccines AS (
-    SELECT
-    	e.patient_id,
-		av.scheduled_vaccine_id,
-		av."date"::date AS administered_date
-	FROM administered_vaccines av
-	JOIN encounters e
-		ON e.deleted_at IS NULL
-		AND e.id = av.encounter_id
-		and e.encounter_type = 'vaccination'
-	WHERE av.deleted_at IS NULL
-	AND av.status = 'GIVEN'
+patient_vaccine_fixed_schedule as (
+select 
+	fp.patient_id, 
+	fsv.scheduled_vaccine_id,
+	fsv.vaccine_id, 
+	fp.date_of_birth + fsv.weeks_from_birth_due * 7 due_date
+from filtered_patients fp
+cross join filtered_scheduled_vaccines fsv
+left join filtered_administered_vaccines fav on fav.patient_id = fp.patient_id and fav.scheduled_vaccine_id = fsv.scheduled_vaccine_id
+where fav.scheduled_vaccine_id is null
+and fsv.weeks_from_birth_due is not null and fsv.weeks_from_last_vaccination_due is null
 ),
-patient_last_administered_vaccines as (
-	select pav.patient_id, svf.vaccine_id, max(pav.administered_date) last_administered_date
-	from patient_administered_vaccines pav
-	join scheduled_vaccine_filtered svf on svf.scheduled_vaccine_id = pav.scheduled_vaccine_id
-	group by pav.patient_id, svf.vaccine_id
-)
-,
-patient_next_vaccine_details AS (
-    SELECT
-    	psv.patient_id,
-		psv.scheduled_vaccine_id,
-		psv.vaccine_id,
-		psv.dose_order,
-		coalesce (
-			psv.fixed_due,
-			case
-				when psv.weeks_from_last_vaccination_due is null or plav.last_administered_date is null then null
-				else psv.weeks_from_last_vaccination_due * 7 + plav.last_administered_date
-				end
-		) due_date,
-		rank(*) OVER (PARTITION BY psv.patient_id, psv.vaccine_id, pav.scheduled_vaccine_id ORDER BY psv.dose_order) = 1 AS is_next
-	FROM patient_scheduled_vaccines psv
-	LEFT JOIN patient_administered_vaccines pav
-        	ON pav.patient_id = psv.patient_id
-    		AND pav.scheduled_vaccine_id = psv.scheduled_vaccine_id
-	left join patient_last_administered_vaccines plav
-		on plav.patient_id = psv.patient_id
-		and plav.vaccine_id = psv.vaccine_id
-	where pav.scheduled_vaccine_id is null
-)
-,
-patient_next_vaccines AS (
-    SELECT
-    	pnvd.patient_id,
-    	pnvd.scheduled_vaccine_id,
-    	pnvd.vaccine_id,
-    	pnvd.dose_order,
-    	pnvd.due_date,
-    	pnvd.due_date - CURRENT_DATE days_till_due
-      FROM patient_next_vaccine_details pnvd
-     where pnvd.due_date is not null
-     and pnvd.is_next = true
+patient_vaccine_dynamic_schedule as (
+select 
+	distinct on (fsv2.vaccine_id)
+	fp.patient_id,
+	fsv2.scheduled_vaccine_id,
+	fsv2.vaccine_id,
+	fsv2.weeks_from_last_vaccination_due * 7 + fav.administered_date
+from filtered_administered_vaccines fav 
+join filtered_patients fp on fp.patient_id = fav.patient_id
+join filtered_scheduled_vaccines fsv on fsv.scheduled_vaccine_id  = fav.scheduled_vaccine_id 
+join filtered_scheduled_vaccines fsv2 on fsv.vaccine_id = fsv2.vaccine_id and fsv2.weeks_from_birth_due is null and fsv2.weeks_from_last_vaccination_due is not null and fsv2.index > fsv.index
+order by fsv2.vaccine_id, fsv2.index
+),
+patient_vaccine_schedule as (
+	select * from patient_vaccine_fixed_schedule
+	union all
+	select * from patient_vaccine_dynamic_schedule
 )
 SELECT
 	*,
+	CURRENT_DATE - pvs.due_date days_till_due,
 	(SELECT vst.status
 	FROM vaccine_thresholds vst
-	WHERE pnx.days_till_due > vst.threshold
+	WHERE CURRENT_DATE - pvs.due_date > vst.threshold
 	ORDER BY vst.threshold DESC
 	LIMIT 1) AS status
-FROM patient_next_vaccines pnx
+FROM patient_vaccine_schedule pvs;
   `);
 }
 
