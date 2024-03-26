d4a31b7578156ca906a702b88e49c93a62f30cdf
Tom Caiger <caigertom@gmail.com>
2024-03-26 14:56:05 +1300

    Update useTranslations.js

--- packages/web/app/api/queries/useTranslations.js
+++ packages/web/app/api/queries/useTranslations.js
@@ -5,13 +5,10 @@ export const useTranslations = (language = 'en') => {
   const api = useApi();
   return useQuery(
     ['translations', language],
-    () => {
-      return api.get(`/public/translation/${language}`);
-    },
+    () => api.get(`public/translation/${language}`, null, { showUnknownErrorToast: false }),
     {
       staleTime: 1000 * 60 * 60, // 1 hour
       cacheTime: 1000 * 60 * 60, // 1 hour
-      refetchOnWindowFocus: false,
     },
   );
 };
