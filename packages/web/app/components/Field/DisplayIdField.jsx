2305ee3276034e1177afccac47e56edd2d3ffedd
Tom Caiger <caigertom@gmail.com>
2024-03-28 10:27:26 +1300

    Update DisplayIdField.jsx

--- packages/web/app/components/Field/DisplayIdField.jsx
+++ packages/web/app/components/Field/DisplayIdField.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 import { useFormikContext } from 'formik';
-
 import { useLocalisation } from '../../contexts/Localisation';
 import { TextField } from './TextField';
 import { LocalisedField } from './LocalisedField';
-import { TranslatedText } from '../Translation/TranslatedText';
+import { useTranslation } from '../../contexts/Translation';
 
-export const DisplayIdField = ({ required }) => {
+const useDisplayIdValidation = label => {
   const { initialValues } = useFormikContext();
   const { getLocalisation } = useLocalisation();
   const pattern = getLocalisation('fields.displayId.pattern') || null;
   const regex = pattern ? new RegExp(pattern) : null;
+  return value =>
+    value !== initialValues[name] && regex && !regex.test(value) ? `Invalid ${label}` : undefined;
+};
 
-  const validateFn = value => {
-    let errorMessage;
-    if (value !== initialValues[name] && regex && !regex.test(value)) {
-      // #TODO translation
-      errorMessage = `Invalid National Health Number`;
-    }
-    return errorMessage;
-  };
+export const DisplayIdField = ({ required }) => {
+  const { getTranslation } = useTranslation();
+  const label = getTranslation('general.localisedField.displayId.label', 'National Health Number');
+  const validateFn = useDisplayIdValidation(label);
 
   return (
     <LocalisedField
       name="displayId"
-      label={
-        <TranslatedText
-          stringId="general.localisedField.displayId.label"
-          fallback="National Health Number"
-        />
-      }
+      label={label}
       component={TextField}
       required={required}
       validate={validateFn}
