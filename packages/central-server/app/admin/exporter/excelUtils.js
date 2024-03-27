ff91fa23f790857d3edd2e8d5e06defafe2c0fed
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-03-27 12:38:53 -0500

    The file extension should remain lowercase

--- packages/central-server/app/admin/exporter/excelUtils.js
+++ packages/central-server/app/admin/exporter/excelUtils.js
@@ -6,7 +6,7 @@ export function writeExcelFile(sheets, fileName) {
     const worksheet = XLSX.utils.aoa_to_sheet(sheet.data);
     XLSX.utils.book_append_sheet(workbook, worksheet, sheet.name);
   });
-  const excelFileName = fileName || `./export-${Date.now()}.XLSX`;
+  const excelFileName = fileName || `./export-${Date.now()}.xlsx`;
   XLSX.writeFile(workbook, excelFileName);
   return excelFileName;
 }
