6f1651541e67492a98cda223b43952f9628bfc70
Daniel Nash <dannash200@gmail.com>
2024-04-02 13:12:46 +1300

    nass-1177: fix

--- packages/web/app/utils/errorMessages.js
+++ packages/web/app/utils/errorMessages.js
@@ -3,7 +3,7 @@ import { capitaliseFirstLetter } from './capitalise';
 import { startCase } from 'lodash';
 
 const REQUIRED_MESSAGE_STRING_ID = 'validation.required';
-const LOCALISATION_TEMPLATE_STRING = ':localisedField=';
+const LOCALISATION_TEMPLATE_STRING = '@localisedField=';
 
 export const localisedErrorLabel = path => `:${LOCALISATION_TEMPLATE_STRING}${path}`;
 
