bf45320f6303c75d7b3344ca4c4bace0fcc841e9
Daniel Nash <dannash200@gmail.com>
2024-04-05 13:42:05 +1300

    nass-1111: rough out mobile

--- packages/constants/src/patientFields.ts
+++ packages/constants/src/patientFields.ts
@@ -4,7 +4,6 @@ export const PATIENT_FIELD_DEFINITION_TYPES = {
   STRING: 'string',
   NUMBER: 'number',
   SELECT: 'select',
-  AUTO_COMPLETE: 'autoComplete',
 };
 
 export const PATIENT_FIELD_DEFINITION_TYPE_VALUES = Object.values(PATIENT_FIELD_DEFINITION_TYPES);
