438edcbb80a2fc7844029c11ea245a4bd6307f4b
Tom Caiger <caigertom@gmail.com>
2024-04-08 12:47:11 +1200

    show base level field as fallback

--- packages/web/app/components/Field/HierarchyFields.jsx
+++ packages/web/app/components/Field/HierarchyFields.jsx
@@ -6,16 +6,15 @@ import { get } from 'lodash';
 
 export const HierarchyFields = ({ fields, baseLevel, relationType }) => {
   const { values } = useFormikContext();
-  const { data } = useHierarchyTypesQuery({ baseLevel, relationType });
-
-  const hierarchyToShow = data?.filter(type => fields.find(f => f.referenceType === type));
-
-  if (!hierarchyToShow?.length) return null;
+  const { data = [] } = useHierarchyTypesQuery({ baseLevel, relationType });
+  const configuredFields = data.filter(type => fields.find(f => f.referenceType === type));
+  const hierarchyToShow = configuredFields.length > 0 ? configuredFields : [baseLevel];
 
   return (
     <>
       {hierarchyToShow.map((type, index) => {
         const fieldData = fields.find(f => f.referenceType === type);
+        console.log('fieldData', fieldData);
         const parentFieldData = fields.find(f => f.referenceType === hierarchyToShow[index - 1]);
         const parentId = get(values, parentFieldData?.name);
 
