850da8e5f2ff05c0eb570fc61a34ca30a92cdb2b
khoi.nguyen <khoi@bes.au>
2024-04-09 17:32:25 +0700

    EPI-674: update reminder contact getList api

--- packages/facility-server/app/routes/apiv1/patient/patientContact.js
+++ packages/facility-server/app/routes/apiv1/patient/patientContact.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { simpleGetList } from '@tamanu/shared/utils/crudHelpers';
+import asyncHandler from 'express-async-handler';
 
 export const patientContact = express.Router();
 
-patientContact.get('/:id/reminderContacts', simpleGetList('PatientContact', 'patientId'));
+patientContact.get(
+  '/:id/reminderContacts',
+  asyncHandler(async (req, res) => {
+    req.checkPermission('read', 'Patient');
+    return simpleGetList('PatientContact', 'patientId')(req, res);
+  }),
+);
