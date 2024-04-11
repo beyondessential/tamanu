c81310f00c0d619222905d350c4371c7d4dee706
Nam Hoang <nam@bes.au>
2024-04-11 02:06:35 +0000

    feat(reminder-message): endpoint for getting setting

---
+++ packages/facility-server/app/routes/apiv1/setting.js
@@ -0,0 +1,17 @@
+import config from 'config';
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+
+export const settingRoutes = express.Router();
+
+settingRoutes.get(
+  '/setting/:key',
+  asyncHandler(async (req, res) => {
+    // TODO: not sure what permission to check here
+    //req.checkPermission();
+    req.flagPermissionChecked();
+
+    const setting = await req.models.Setting.get(req.params.key, null);
+    res.send(setting);
+  }),
+);
