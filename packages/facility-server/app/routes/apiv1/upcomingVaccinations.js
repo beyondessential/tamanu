c23c2320df0e50fbc7c5a2aa963a9a8dffbbcd9e
Tom Caiger <caigertom@gmail.com>
2024-04-11 15:34:37 +1200

    Update upcomingVaccinations.js

--- packages/facility-server/app/routes/apiv1/upcomingVaccinations.js
+++ packages/facility-server/app/routes/apiv1/upcomingVaccinations.js
@@ -1,13 +1,71 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
 import { QueryTypes } from 'sequelize';
+import { makeFilter } from '../../utils/query';
+import { mapQueryFilters } from '../../database/utils';
 
 export const upcomingVaccinations = express.Router();
+
+const createUpcomingVaccinationFilters = filterParams => {
+  const filters = [
+    makeFilter(
+      filterParams.displayId,
+      `(UPPER(p.display_id) LIKE UPPER(:displayId))`,
+      ({ displayId }) => ({ displayId: `%${displayId}%` }),
+    ),
+    makeFilter(
+      filterParams.firstName,
+      `UPPER(p.first_name) LIKE UPPER(:firstName)`,
+      ({ firstName }) => ({ firstName: `%${firstName}%` }),
+    ),
+    makeFilter(
+      filterParams.lastName,
+      `UPPER(p.last_name) LIKE UPPER(:lastName)`,
+      ({ lastName }) => ({ lastName: `%${lastName}%` }),
+    ),
+    makeFilter(filterParams.sex, `p.sex = :sex`),
+    makeFilter(filterParams.villageId, `p.village_id = :villageId`),
+  ];
+  // facilityId
+
+  return filters.filter(f => f);
+};
 
 upcomingVaccinations.get(
   '/$',
   asyncHandler(async (req, res) => {
     req.checkPermission('read', 'PatientVaccine');
+
+    const sortKeys = {
+      vaccine: 'label',
+      dueDate: 'due_date',
+      date: 'due_date',
+    };
+
+    const {
+      orderBy = 'dueDate',
+      order = 'ASC',
+      rowsPerPage = 10,
+      page = 0,
+      ...filterParams
+    } = req.query;
+    let sortKey = sortKeys[orderBy];
+    const sortDirection = order.toLowerCase() === 'asc' ? 'ASC' : 'DESC';
+
+    const filters = createUpcomingVaccinationFilters(filterParams);
+    const filterClauses = filters.map(f => f.sql).join(' AND ');
+    const whereClauses = `uv.status <> 'MISSED' ${filterClauses ? `AND ${filterClauses}` : ''}`;
+
+    const filterReplacements = filters
+      .filter(f => f.transform)
+      .reduce(
+        (current, { transform }) => ({
+          ...current,
+          ...transform(current),
+        }),
+        filterParams,
+      );
+    console.log('whereClauses', whereClauses);
 
     const results = await req.db.query(
       `
@@ -35,10 +93,15 @@ upcomingVaccinations.get(
       JOIN scheduled_vaccines sv ON sv.id = uv.scheduled_vaccine_id
       JOIN patients p ON p.id = uv.patient_id
       JOIN reference_data village ON village.id = p.village_id
-      WHERE uv.status <> 'MISSED'
+      WHERE ${whereClauses}
       AND row_number = 1
       ORDER BY uv.due_date, sv.label;`,
       {
+        replacements: {
+          limit: rowsPerPage,
+          offset: page * rowsPerPage,
+          ...filterReplacements,
+        },
         type: QueryTypes.SELECT,
       },
     );
