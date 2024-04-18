429e8f9bd426ce77f93bfdf7d465168ea01820cd
Chris Tran <chris@beyondessential.com.au>
2024-04-18 15:02:21 +1000

    sav-304: added more comment

--- packages/central-server/app/admin/index.js
+++ packages/central-server/app/admin/index.js
@@ -64,11 +64,14 @@ adminRoutes.get(
     const { includedDataTypes = [] } = query;
 
     for (const dataType of includedDataTypes) {
+      // When it is ReferenceData, check if user has permission to list ReferenceData
       if (REFERENCE_TYPE_VALUES.includes(dataType)) {
         req.checkPermission('list', 'ReferenceData');
         continue;
       }
   
+      // Otherwise, if it is other types (eg: patient, lab_test_types,... ones that have their own models)
+      // check the permission against the models
       const nonReferenceDataModelName = upperFirst(dataType);
       req.checkPermission('list', nonReferenceDataModelName);
     }
