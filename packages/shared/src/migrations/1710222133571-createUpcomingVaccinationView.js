a5c2992e131dfd0a235b6bafd16ac2c23c4727d4
Tom Caiger <caigertom@gmail.com>
2024-04-18 14:31:31 +1200

    Update 1710222133571-createUpcomingVaccinationView.js

--- packages/shared/src/migrations/1710222133571-createUpcomingVaccinationView.js
+++ packages/shared/src/migrations/1710222133571-createUpcomingVaccinationView.js
@@ -4,7 +4,7 @@
  * @param {QueryInterface} query
  */
 export async function up(query) {
-	await query.sequelize.query(`
+  await query.sequelize.query(`
   CREATE OR REPLACE VIEW upcoming_vaccinations
   AS with vaccine_thresholds AS (
 	  SELECT
@@ -22,26 +22,26 @@ export async function up(query) {
 	  and s.key = 'vaccine.ageLimit'
   ),
   filtered_patients as (
-	  select p.id patient_id, p.date_of_birth::date 
+	  select p.id patient_id, p.date_of_birth::date
 	  from patients p where p.deleted_at is null and p.visibility_status = 'current' and p.date_of_birth::date > (select "date" from vaccine_agelimit)
   ),
   filtered_scheduled_vaccines as (
-	  select sv.id scheduled_vaccine_id, sv.category vaccine_category, sv.vaccine_id, sv.index, sv.weeks_from_birth_due, sv.weeks_from_last_vaccination_due from scheduled_vaccines sv 
+	  select sv.id scheduled_vaccine_id, sv.category vaccine_category, sv.vaccine_id, sv.index, sv.weeks_from_birth_due, sv.weeks_from_last_vaccination_due from scheduled_vaccines sv
 	  where sv.deleted_at is null and sv.visibility_status = 'current'
   ),
   filtered_administered_vaccines as (
-	  select e.patient_id, av.scheduled_vaccine_id, av."date"::date administered_date from administered_vaccines av 
+	  select e.patient_id, av.scheduled_vaccine_id, av."date"::date administered_date from administered_vaccines av
 	  join encounters e on e.id = av.encounter_id
 	  WHERE av.deleted_at is null and av.status = 'GIVEN'
 	  and e.deleted_at is null and e.encounter_type = 'vaccination'
   ),
   patient_vaccine_fixed_schedule as (
-  select 
+  select
 	  --distinct on (fp.patient_id, fsv.vaccine_category, fsv.vaccine_id)
-	  fp.patient_id, 
+	  fp.patient_id,
 	  fsv.scheduled_vaccine_id,
 	  fsv.vaccine_category,
-	  fsv.vaccine_id, 
+	  fsv.vaccine_id,
 	  fp.date_of_birth + fsv.weeks_from_birth_due * 7 due_date
   from filtered_patients fp
   cross join filtered_scheduled_vaccines fsv
@@ -52,16 +52,16 @@ export async function up(query) {
   --order by fp.patient_id,fsv.vaccine_id, fsv.index
   ),
   patient_vaccine_dynamic_schedule as (
-  select 
+  select
 	  distinct on (fp.patient_id, fsv2.vaccine_category, fsv2.vaccine_id)
 	  fp.patient_id,
 	  fsv2.scheduled_vaccine_id,
 	  fsv2.vaccine_category,
 	  fsv2.vaccine_id,
 	  fsv2.weeks_from_last_vaccination_due * 7 + fav.administered_date due_date
-  from filtered_administered_vaccines fav 
+  from filtered_administered_vaccines fav
   join filtered_patients fp on fp.patient_id = fav.patient_id
-  join filtered_scheduled_vaccines fsv on fsv.scheduled_vaccine_id  = fav.scheduled_vaccine_id 
+  join filtered_scheduled_vaccines fsv on fsv.scheduled_vaccine_id  = fav.scheduled_vaccine_id
   join filtered_scheduled_vaccines fsv2 on fsv.vaccine_id = fsv2.vaccine_id and fsv.vaccine_category = fsv2.vaccine_category and fsv2.weeks_from_birth_due is null and fsv2.weeks_from_last_vaccination_due is not null and fsv2.index > fsv.index
   order by fp.patient_id, fsv2.vaccine_category, fsv2.vaccine_id, fsv2.index
   ),
@@ -80,12 +80,11 @@ export async function up(query) {
 	  LIMIT 1) AS status
   FROM patient_vaccine_schedule pvs;
 	`);
-  }
-  
-  /**
-   * @param {QueryInterface} query
-   */
-  export async function down(query) {
-	await query.sequelize.query(`DROP VIEW upcoming_vaccinations;`);
-  }
-  
+}
+
+/**
+ * @param {QueryInterface} query
+ */
+export async function down(query) {
+  await query.sequelize.query(`DROP VIEW upcoming_vaccinations;`);
+}
