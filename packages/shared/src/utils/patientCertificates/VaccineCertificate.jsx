bdb4c89f8ac9566095982598fc9f9e2cd72dac99
khoi.nguyen <khoi@bes.au>
2024-04-20 18:08:52 +0700

    NASS-1143: fixing merge main

--- packages/shared/src/utils/patientCertificates/VaccineCertificate.jsx
+++ packages/shared/src/utils/patientCertificates/VaccineCertificate.jsx
@@ -49,7 +49,7 @@ const columns = getTranslation => [
 
   {
     key: 'countryName',
-    title: getTranslation('pdf.table.column.country', 'Country'),
+    title: getTranslation('pdf.table.column.country', 'Facility/Country'),
     accessor: record => {
       const facility = record.givenElsewhere ? record.givenBy : record.location?.facility?.name;
       return facility || '';
