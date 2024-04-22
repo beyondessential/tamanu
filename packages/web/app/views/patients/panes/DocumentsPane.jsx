c044b9d70cecb990f1d242afa2608e693b11a01c
Kurt <kujo4pm@gmail.com>
2024-04-22 17:14:52 +1000

    removing duplicate onDownload

--- packages/web/app/views/patients/panes/DocumentsPane.jsx
+++ packages/web/app/views/patients/panes/DocumentsPane.jsx
@@ -53,32 +53,6 @@ export const DocumentsPane = React.memo(({ encounter, patient }) => {
     };
   }, [dataUrl]);
 
-  const onDownload = useCallback(
-    async document => {
-      try {
-        // Give feedback to user that download is starting
-        notify('Your download has started, please wait.', { type: 'info' });
-
-        // Download attachment (*currently the API only supports base64 responses)
-        const { data } = await api.get(`attachment/${document.attachmentId}`, {
-          base64: true,
-        });
-
-        await saveFile({
-          defaultFileName: document.name,
-          data: base64ToUint8Array(data),
-          extension: extension(document.type),
-          mimetype: document.type,
-        });
-
-        notifySuccess('Successfully downloaded file');
-      } catch (error) {
-        notifyError(error.message);
-      }
-    },
-    [api],
-  );
-
   const onPrintPDF = useCallback(
     async attachmentId => {
       try {
