f581695dece4f33ed6d3f595b61f420aa51ac0d6
Nam Hoang <nam@bes.au>
2024-04-22 03:52:03 +0000

    refactor(vaccination-reminder): rename sql function paramter

--- packages/shared/src/migrations/1712995147342-createStringTranslateFunction.js
+++ packages/shared/src/migrations/1712995147342-createStringTranslateFunction.js
@@ -9,7 +9,7 @@ export async function up(query) {
       i_language text,
       i_string_id text,
       i_fallback_string text,
-      json_data json
+      i_replacements json
   )
   RETURNS text AS $$
   declare
@@ -18,17 +18,17 @@ export async function up(query) {
       key_value text;
   begin
     select text into translated_string from translated_strings where language = i_language and string_id = i_string_id;
-    
-    if translated_string is null or translated_string = '' 
+
+    if translated_string is null or translated_string = ''
       then translated_string = i_fallback_string;
     end if;
-    
+
       -- Loop through each key-value pair in the JSON object
-      FOR key_name, key_value IN SELECT * FROM json_each_text(json_data) LOOP
+      FOR key_name, key_value IN SELECT * FROM json_each_text(i_replacements) LOOP
           -- Replace placeholders in the input string
           translated_string := REPLACE(translated_string, ':' || key_name, key_value);
       END LOOP;
-  
+
       RETURN translated_string;
   END;
   $$ LANGUAGE plpgsql;`);
