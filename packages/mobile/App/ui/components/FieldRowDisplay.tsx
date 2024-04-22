243f0ada5ae7a7f65bdb2fff702fe8a221284a33
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-04-22 13:17:34 +1200

    use proper type

--- packages/mobile/App/ui/components/FieldRowDisplay.tsx
+++ packages/mobile/App/ui/components/FieldRowDisplay.tsx
@@ -5,10 +5,11 @@ import { isTablet } from 'react-native-device-info';
 import { useLocalisation } from '../contexts/LocalisationContext';
 import { RowView, StyledView } from '../styled/common';
 import { InformationBox } from '../navigation/screens/home/PatientDetails/CustomComponents';
+import { IPatientFieldDefinition } from '~/types';
 
 interface FieldRowDisplayProps {
   fields: string[][];
-  customFields?: ReactElement;
+  customFields?: IPatientFieldDefinition[];
 }
 
 export const FieldRowDisplay = ({ fields, customFields }: FieldRowDisplayProps): ReactElement => {
@@ -16,7 +17,6 @@ export const FieldRowDisplay = ({ fields, customFields }: FieldRowDisplayProps):
   const localisedFields = getLocalisation('fields');
   const fieldsPerRow = isTablet() ? 2 : 1;
   const rows = chunk(fields, fieldsPerRow);
-
   const customFieldsById = keyBy(customFields, 'id')
 
   return (
@@ -25,7 +25,7 @@ export const FieldRowDisplay = ({ fields, customFields }: FieldRowDisplayProps):
         <RowView key={row.map(([name]) => name).join(',')} marginTop={20}>
           {row.map(([name, info]) => {
             let title = name;
-            
+
             const isLocalised = Object.keys(localisedFields).includes(name);
             if (isLocalised) {
               if (getBool(`fields.${name}.hidden`)) return null;
