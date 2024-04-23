ded908aed00f58e0e366e28de21d4552b440bd00
khoi.nguyen <khoi@bes.au>
2024-04-23 13:44:38 +0700

    Refactor useSocket hook to optimize socket connection

--- packages/mobile/App/ui/hooks/useSocket.ts
+++ packages/mobile/App/ui/hooks/useSocket.ts
@@ -1,25 +1,55 @@
-import io, { ManagerOptions, Socket, SocketOptions } from 'socket.io-client';
+import io, { Socket } from 'socket.io-client';
 import { useEffect, useState } from 'react';
 import { readConfig } from '~/services/config';
 
-interface Props extends Partial<ManagerOptions & SocketOptions> {
+interface Props {
   uri?: string;
 }
 
+const cachedWebSocketInstances: Record<string, { instance: Socket; count: number }> = {};
+
 export const useSocket = (props: Props = {}) => {
-  const { uri, ...others } = props;
+  const { uri } = props;
   const [socket, setSocket] = useState<Socket | null>(null);
+  const [connectionUrl, setConnectionUrl] = useState('');
 
   useEffect(() => {
+    setupConnectionUrl();
+  }, []);
+
+  useEffect(() => {
+    if (!connectionUrl) return;
     initSocket();
     return () => {
+      if (cachedWebSocketInstances[connectionUrl]?.count > 1) {
+        cachedWebSocketInstances[connectionUrl].count -= 1;
+        return;
+      }
+
+      delete cachedWebSocketInstances[connectionUrl];
       socket?.disconnect();
     };
-  }, [uri]);
+  }, [connectionUrl]);
+
+  const setupConnectionUrl = async () => {
+    const syncServerLocation = await readConfig('syncServerLocation');
+    const connectionUrl = uri || syncServerLocation;
+    setConnectionUrl(connectionUrl);
+  };
 
   const initSocket = async () => {
-    const syncServerLocation = await readConfig('syncServerLocation');
-    const newSocket = io(uri || syncServerLocation, others)
+    const cached = cachedWebSocketInstances[connectionUrl];
+    if (cached) {
+      cachedWebSocketInstances[connectionUrl].count += 1;
+      cached.instance;
+      setSocket(cached.instance);
+    }
+
+    const newSocket = io(connectionUrl);
+    cachedWebSocketInstances[connectionUrl] = {
+      instance: newSocket,
+      count: 1,
+    };
     setSocket(newSocket);
   };
 
