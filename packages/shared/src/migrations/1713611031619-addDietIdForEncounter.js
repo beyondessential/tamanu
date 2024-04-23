9e47f560a3d60db64f1e9a3c10672658c7775aa2
Huy-Tran-Huu-Le <160376020+Huy-Tran-Huu-Le@users.noreply.github.com>
2024-04-23 11:44:27 +0700

    Update packages/shared/src/migrations/1713611031619-addDietIdForEncounter.js
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

--- packages/shared/src/migrations/1713611031619-addDietIdForEncounter.js
+++ packages/shared/src/migrations/1713611031619-addDietIdForEncounter.js
@@ -2,22 +2,15 @@ import { DataTypes } from 'sequelize';
 
 export async function up(query) {
   await query.addColumn('encounters', 'diet_id', {
-    type: DataTypes.STRING,
+    type: DataTypes.TEXT,
     allowNull: true,
-  });
-
-  await query.addConstraint('encounters', {
-    name: 'encounters_diet_id_fkey',
-    fields: ['diet_id'],
-    type: 'foreign key',
     references: {
-      table: 'reference_data',
+      model: 'reference_data',
       field: 'id',
     },
   });
 }
 
 export async function down(query) {
-  await query.removeConstraint('encounters', 'encounters_diet_id_fkey');
   await query.removeColumn('encounters', 'diet_id');
 }
