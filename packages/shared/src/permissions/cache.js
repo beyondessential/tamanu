6497ba881d26857e5a7907994ebc2c5328a42015
Daniel Nash <dannash200@gmail.com>
2024-04-26 09:57:35 +1200

    fix: tamoc-179: basic ttl for permission cache

--- packages/shared/src/permissions/cache.js
+++ packages/shared/src/permissions/cache.js
@@ -1,22 +1,40 @@
 import { isEmpty } from 'lodash';
+import config from 'config';
+
+const ttl = config.permissionCache.ttl;
 
 class PermissionCache {
   cache = {};
 
+  expires = null;
+
   get(key) {
+    this.invalidateIfExpired();
     return this.cache[key];
   }
 
   set(key, value) {
     this.cache[key] = value;
+    if (this.expires) return;
+    this.expires = Date.now() + ttl;
+  }
+
+  delete(key) {
+    delete this.cache[key];
   }
 
   reset() {
     this.cache = {};
+    this.expires = null;
   }
 
   isEmpty() {
     return isEmpty(this.cache);
+  }
+
+  invalidateIfExpired() {
+    if (this.expires > Date.now()) return;
+    this.reset();
   }
 }
 
