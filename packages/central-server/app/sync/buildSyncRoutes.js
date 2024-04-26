6b65ade8c76c3b913ebb7039787b052571f317b7
Félix Saparelli <felix@bes.au>
2024-04-26 15:49:14 +1200

    fix import

--- packages/central-server/app/sync/buildSyncRoutes.js
+++ packages/central-server/app/sync/buildSyncRoutes.js
@@ -3,7 +3,7 @@ import asyncHandler from 'express-async-handler';
 
 import { Op } from 'sequelize';
 import { log } from '@tamanu/shared/services/logging';
-import { ForbiddenError } from '@tamanu/shared/src/errors';
+import { ForbiddenError } from '@tamanu/shared/errors';
 import { completeSyncSession } from '@tamanu/shared/sync/completeSyncSession';
 
 import { CentralSyncManager } from './CentralSyncManager';
