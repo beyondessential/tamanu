e228a887372c41a62ccaea7c88cc2ca70e8bb46e
Huy Tran <huy@bes.au>
2024-04-29 16:54:38 +0700

    feat(programRegistry): EPI-844: change related condition search field type to autocomplete

--- packages/web/app/views/programRegistry/ProgramRegistrySearchBar.jsx
+++ packages/web/app/views/programRegistry/ProgramRegistrySearchBar.jsx
@@ -14,7 +14,6 @@ import {
   BaseSelectField,
 } from '../../components';
 import { useProgramRegistryQuery } from '../../api/queries/useProgramRegistryQuery';
-import { useProgramRegistryConditions } from '../../api/queries/useProgramRegistryConditions';
 import { useSexOptions } from '../../hooks';
 import { TranslatedText } from '../../components/Translation/TranslatedText';
 
@@ -40,9 +39,9 @@ export const ProgramRegistrySearchBar = ({ searchParameters, setSearchParameters
     baseQueryParameters: { programRegistryId: params.programRegistryId },
   });
 
-  const { data: programRegistryConditions } = useProgramRegistryConditions(
-    params.programRegistryId,
-  );
+  const programRegistryConditionSuggester = useSuggester('programRegistryCondition', {
+    baseQueryParameters: { programRegistryId: params.programRegistryId },
+  });
 
   return (
     <CustomisableSearchBar
@@ -141,9 +140,8 @@ export const ProgramRegistrySearchBar = ({ searchParameters, setSearchParameters
       <Field
         label="Related condition"
         name="programRegistryCondition"
-        component={BaseSelectField}
-        options={programRegistryConditions?.data.map(x => ({ label: x.name, value: x.id }))}
-        size="small"
+        component={AutocompleteField}
+        suggester={programRegistryConditionSuggester}
       />
       <Field
         label="Status"
