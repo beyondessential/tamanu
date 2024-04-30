e162455876d5599c0e15768f4c03643fcb52c952
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-04-30 16:50:21 +1200

    Update VaccineForm.jsx

--- packages/web/app/forms/VaccineForm.jsx
+++ packages/web/app/forms/VaccineForm.jsx
@@ -22,6 +22,7 @@ import { useLocalisation } from '../contexts/Localisation';
 import { useSettings } from '../contexts/Settings';
 import { usePatientData } from '../api/queries/usePatientData';
 import { isBefore, parse } from 'date-fns';
+import { TranslatedReferenceData } from '../components/Translation/TranslatedReferenceData';
 
 const validateGivenElsewhereRequiredField = (status, givenElsewhere) =>
   (status === VACCINE_RECORDING_TYPES.GIVEN && !givenElsewhere) ||
@@ -123,7 +124,14 @@ export const VaccineForm = ({
         const availableScheduledVaccines = await getScheduledVaccines({ category });
         setVaccineOptions(
           availableScheduledVaccines.map(vaccine => ({
-            label: vaccine.label,
+            id: vaccine.id,
+            label: (
+              <TranslatedReferenceData
+                category="scheduledVaccine"
+                value={vaccine.id}
+                fallback={vaccine.label}
+              />
+            ),
             value: vaccine.label,
             schedules: vaccine.schedules,
           })),
