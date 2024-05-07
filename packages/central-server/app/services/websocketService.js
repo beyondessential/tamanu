42cbf77ba47e6c85a5d93585ecd2c27f58052bae
khoi.nguyen <khoi@bes.au>
2024-05-08 01:00:01 +0700

    EPI-853: add support for multi ws server

--- packages/central-server/app/services/websocketService.js
+++ packages/central-server/app/services/websocketService.js
@@ -1,6 +1,5 @@
 import { Server } from 'socket.io';
 import { createAdapter } from '@socket.io/postgres-adapter';
-import { Emitter } from '@socket.io/postgres-emitter';
 
 /**
  *
@@ -13,16 +12,15 @@ export const defineWebsocketService = injector => {
   });
   const getServer = () => server;
 
-  server.adapter(createAdapter(injector.sequelize.connectionManager.getConnection()));
+  server.adapter(createAdapter(injector.sequelize.connectionManager.pool));
 
-  const emitter = new Emitter(injector.sequelize.connectionManager.getConnection());
   /**
    *
    * @param {string} eventName
    * @param  {...unknown} args
    * @returns
    */
-  const emit = (eventName, ...args) => emitter.emit(eventName, ...args);
+  const emit = (eventName, ...args) => server.emit(eventName, ...args);
 
   const registerEvent = (eventName, handler) => {
     server.on('connection', socket => {
