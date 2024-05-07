512f4ff96ec18c42076392a39a11316373bec605
Huy Tran <huy@bes.au>
2024-05-07 16:07:58 +0700

    feat(invoicing): add two fields for insurer in patient details

--- packages/web/app/forms/PatientDetailsForm/layouts/generic/patientFields/GenericIdentifiationFields.jsx
+++ packages/web/app/forms/PatientDetailsForm/layouts/generic/patientFields/GenericIdentifiationFields.jsx
@@ -3,15 +3,38 @@ import React from 'react';
 import { PATIENT_REGISTRY_TYPES } from '@tamanu/constants';
 
 import { useLocalisation } from '../../../../../contexts/Localisation';
-import { DisplayIdField, TextField } from '../../../../../components';
+import { AutocompleteField, DisplayIdField, TextField } from '../../../../../components';
 import { ConfiguredMandatoryPatientFields } from '../../../ConfiguredMandatoryPatientFields';
 import { TranslatedText } from '../../../../../components/Translation/TranslatedText';
+import { useSuggester } from '../../../../../api';
 
 export const GenericIdentificationFields = ({ isEdit, patientRegistryType, filterByMandatory }) => {
   const { getLocalisation } = useLocalisation();
+  const insurerSuggester = useSuggester('insurer');
   const canEditDisplayId = isEdit && getLocalisation('features.editPatientDisplayId');
 
   const IDENTIFICATION_FIELDS = {
+    insurerId: {
+      component: AutocompleteField,
+      suggester: insurerSuggester,
+      label: (
+        <TranslatedText
+          stringId="general.localisedField.insurer.label"
+          fallback="Insurer"
+        />
+      ),
+      condition: () => !!isEdit,
+    },
+    insurerPolicyNumber: {
+      component: TextField,
+      label: (
+        <TranslatedText
+          stringId="general.localisedField.insurerPolicyNumber.label"
+          fallback="Insurance policy number"
+        />
+      ),
+      condition: () => !!isEdit,
+    },
     displayId: {
       component: DisplayIdField,
       condition: () => !!canEditDisplayId,
