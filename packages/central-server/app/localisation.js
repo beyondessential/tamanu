b16729f71e541e0c82fad0acb8b329961582ad2a
chris-bes <63621318+chris-bes@users.noreply.github.com>
2024-05-08 17:03:37 +1000

    fix(web): SAV-670: Filter discharge dispositions (HOTFIX 2.6) (#5813)
    * feat(web): SAV-670: Filter discharge dispositions (HOTFIX 2.3) (#5804)
    
    * sav-670: filter discharge dispositions
    
    * sav-670: fixed typo
    
    * fix(web): SAV-673: Update prefix for discharge disposition codes (#5809)
    
    sav-673: updated prefix

--- packages/central-server/app/localisation.js
+++ packages/central-server/app/localisation.js
@@ -574,6 +574,7 @@ const rootLocalisationSchema = yup
     features: yup
       .object({
         enableVaccineConsent: yup.boolean().required(),
+        filterDischargeDispositions: yup.boolean().required(),
         editPatientDetailsOnMobile: yup.boolean().required(),
         quickPatientGenerator: yup.boolean().required(),
         enableInvoicing: yup.boolean().required(),
