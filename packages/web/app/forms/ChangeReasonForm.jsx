88d638f25c89dae220f3a00926e49e780f29a323
Huy Tran <huy@bes.au>
2024-05-15 11:33:57 +0700

    tweak: add characters limit for reason for encounter

--- packages/web/app/forms/ChangeReasonForm.jsx
+++ packages/web/app/forms/ChangeReasonForm.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as yup from 'yup';
 import { getCurrentDateTimeString } from '@tamanu/shared/utils/dateTime';
 import { FORM_TYPES } from '../constants';
 import { Field, Form, TextField } from '../components/Field';
 import { FormGrid } from '../components/FormGrid';
 import { TranslatedText } from '../components/Translation/TranslatedText';
 import { ModalActionRow } from '../components';
+
+const REASON_FOR_ENCOUNTER_MAX_CHARACTERS = 256;
 
 export const ChangeReasonForm = ({ onCancel, onSubmit, reasonForEncounter }) => {
   const renderForm = ({ submitForm }) => (
@@ -29,6 +32,16 @@ export const ChangeReasonForm = ({ onCancel, onSubmit, reasonForEncounter }) => 
       formType={FORM_TYPES.EDIT_FORM}
       render={renderForm}
       onSubmit={onSubmit}
+      validationSchema={yup.object().shape({
+        reasonForEncounter: yup.string().max(
+          REASON_FOR_ENCOUNTER_MAX_CHARACTERS,
+          <TranslatedText
+            stringId="general.localisedField.reasonForEncounter.validation.maxCharacters"
+            fallback="Reason for encounter must not exceed :maxChars characters"
+            replacements={{ maxChars: REASON_FOR_ENCOUNTER_MAX_CHARACTERS }}
+          />
+        )
+      })}
     />
   );
 };
