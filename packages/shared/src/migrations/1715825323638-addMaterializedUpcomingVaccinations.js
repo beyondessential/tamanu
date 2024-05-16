fe88b5bdc133b6e6f072a741fe2e07738bd7176d
Daniel Nash <dannash200@gmail.com>
2024-05-16 14:12:42 +1200

    nass-1260: migrations for materialized_upcoming_vaccinations

---
+++ packages/shared/src/migrations/1715825323638-addMaterializedUpcomingVaccinations.js
@@ -0,0 +1,11 @@
+export async function up(query) {
+  await query.sequelize.query(`
+  CREATE MATERIALIZED VIEW materialized_upcoming_vaccinations AS
+  SELECT *
+  FROM upcoming_vaccinations
+  `);
+}
+
+export async function down(query) {
+  await query.sequelize.query('DROP MATERIALIZED VIEW materialized_upcoming_vaccinations;');
+}
