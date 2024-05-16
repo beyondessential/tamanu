d078d101bcdee1e0e3411cf214583ecf775717cc
Daniel Nash <dannash200@gmail.com>
2024-05-16 15:17:47 +1200

    nass-1260: much smarter

--- packages/shared/src/migrations/1715825323638-addMaterializedUpcomingVaccinations.js
+++ packages/shared/src/migrations/1715825323638-addMaterializedUpcomingVaccinations.js
@@ -1,15 +1,15 @@
 export async function up(query) {
   await query.sequelize.query(`
-  CREATE MATERIALIZED VIEW materialized_upcoming_vaccinations AS
-  SELECT *,
-  patient_id || scheduled_vaccine_id as composite_id
-  FROM upcoming_vaccinations
+    CREATE MATERIALIZED VIEW materialized_upcoming_vaccinations AS
+    SELECT *
+    FROM upcoming_vaccinations
   `);
-  // Materialized view requires a unique index to refresh concurrently
-  await query.sequelize.query(
-    `CREATE UNIQUE INDEX materialized_upcoming_vaccinations_id_idx
-    ON materialized_upcoming_vaccinations(composite_id);`,
-  );
+  // There is a requirement of a unique index on the materialized view
+  // in order to refresh concurrently
+  await query.sequelize.query(`
+    CREATE UNIQUE INDEX materialized_upcoming_vaccinations_unique_index
+    ON materialized_upcoming_vaccinations (patient_id, scheduled_vaccine_id);
+  `);
 }
 
 export async function down(query) {
