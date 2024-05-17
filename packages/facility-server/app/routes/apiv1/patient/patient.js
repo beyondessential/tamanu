2e760b4d2cf33790a8aa9d9730889acb33b629cd
Edwin Monk-Fromont <edwin@beyondessential.com.au>
2024-05-17 17:19:31 +1200

    Add manual side effect handling where needed

--- packages/facility-server/app/routes/apiv1/patient/patient.js
+++ packages/facility-server/app/routes/apiv1/patient/patient.js
@@ -114,7 +114,7 @@ patientRoute.post(
   asyncHandler(async (req, res) => {
     const {
       db,
-      models: { Patient, PatientAdditionalData, PatientBirthData },
+      models: { Patient, PatientAdditionalData, PatientBirthData, PatientFacility },
       facilityId,
     } = req;
     req.checkPermission('create', 'Patient');
@@ -141,6 +141,12 @@ patientRoute.post(
           patientId: createdPatient.id,
         });
       }
+
+      // mark for sync in this facility
+      await PatientFacility.create({
+        where: { facilityId, patientId: createdPatient.id },
+      });
+
       return createdPatient;
     });
 
