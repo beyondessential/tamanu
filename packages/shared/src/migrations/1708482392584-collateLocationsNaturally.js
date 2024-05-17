07ea1954b9b3e5ddce06e898a3c86c80d5b3336b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-05-17 13:54:50 +1200

    try quoted default

--- packages/shared/src/migrations/1708482392584-collateLocationsNaturally.js
+++ packages/shared/src/migrations/1708482392584-collateLocationsNaturally.js
@@ -10,13 +10,21 @@ export async function up(query) {
   `);
 
   // Apply natural sorting order to locations and areas
-  await query.sequelize.query("ALTER TABLE locations ALTER COLUMN name TYPE character varying(255) COLLATE en_numeric");
-  await query.sequelize.query("ALTER TABLE location_groups ALTER COLUMN name TYPE character varying(255) COLLATE en_numeric");
+  await query.sequelize.query(
+    'ALTER TABLE locations ALTER COLUMN name TYPE character varying(255) COLLATE en_numeric',
+  );
+  await query.sequelize.query(
+    'ALTER TABLE location_groups ALTER COLUMN name TYPE character varying(255) COLLATE en_numeric',
+  );
 }
 
 export async function down(query) {
-  await query.sequelize.query("ALTER TABLE locations ALTER COLUMN name TYPE character varying(255) COLLATE default");
-  await query.sequelize.query("ALTER TABLE location_groups ALTER COLUMN name TYPE character varying(255) COLLATE default");
+  await query.sequelize.query(
+    'ALTER TABLE locations ALTER COLUMN name TYPE character varying(255) COLLATE "default"',
+  );
+  await query.sequelize.query(
+    'ALTER TABLE location_groups ALTER COLUMN name TYPE character varying(255) COLLATE "default"',
+  );
 
   await query.sequelize.query('DROP COLLATION en_numeric;');
 }
