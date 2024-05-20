6d093025916d35b4f845de05c86ea5ed893facf2
Daniel Nash <dannash200@gmail.com>
2024-05-20 17:05:04 +1200

    the solution

--- packages/facility-server/app/tasks/RefreshUpcomingVaccinations.js
+++ packages/facility-server/app/tasks/RefreshUpcomingVaccinations.js
@@ -22,6 +22,9 @@ export class RefreshUpcomingVaccinations extends ScheduledTask {
     await this.sequelize.query(
       'REFRESH MATERIALIZED VIEW CONCURRENTLY materialized_upcoming_vaccinations',
     );
+    // TODO: perform notify on materialized view refresh
+    // TODO-future: change to trigger notify
+    // this.sequelize.query(`NOTIFY ${UPCOMING_VACCINATIONS_REFRESHED_CHANNEL}`);
     this.websocketService.emit(WS_EVENTS.UPCOMING_VACCINATIONS_REFRESHED);
     await this.models.LocalSystemFact.set(
       UPCOMING_VACCINATIONS_REFRESHED_AT_KEY,
--- packages/facility-server/app/utils/TranslatedCronParser.js
+++ packages/facility-server/app/utils/TranslatedCronParser.js
@@ -1,10 +1,11 @@
-import { en as BaseLocale } from 'cronstrue/locales/en';
+import { en as EnglishLocale } from 'cronstrue/locales/en';
 
 /**
  * Connect translated strings to cron parser for custom translations
  * This only supports every n seconds, minutes, hours right now but see the base locale for more
+ * If we expect the parsing of other cron expressions, we need to add more methods here
  */
-export class TranslatedCronParser extends BaseLocale {
+export class TranslatedCronParser extends EnglishLocale {
   constructor(translationFunc) {
     super();
     this.tt = translationFunc;
