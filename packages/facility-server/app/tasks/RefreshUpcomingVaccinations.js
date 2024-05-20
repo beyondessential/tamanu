6a3c43dbfe2eb30b4585049a3c9a4601245df380
Daniel Nash <dannash200@gmail.com>
2024-05-20 12:22:26 +1200

    nass-1260: remove runImmediately

--- packages/facility-server/app/tasks/RefreshUpcomingVaccinations.js
+++ packages/facility-server/app/tasks/RefreshUpcomingVaccinations.js
@@ -18,7 +18,6 @@ export class RefreshUpcomingVaccinations extends ScheduledTask {
     super(schedule, log, jitterTime);
     this.sequelize = context.sequelize;
     this.models = context.models;
-    this.runImmediately();
   }
 
   async run() {
