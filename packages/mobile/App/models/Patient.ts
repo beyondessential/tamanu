d924062261b1cc70fc5f7d5815097b3b49bbdd0c
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-05-20 19:57:14 -0400

    Explicitly set table name

--- packages/mobile/App/models/Patient.ts
+++ packages/mobile/App/models/Patient.ts
@@ -234,7 +234,7 @@ export class Patient extends BaseModel implements IPatient {
       INNER JOIN encounter
         ON encounter.id = response.encounterId
       WHERE encounter.patientId = $1
-        AND body IS NOT NULL
+        AND answer.body IS NOT NULL
         AND answer.deleted_at IS NULL
       ORDER BY answer.createdAt desc LIMIT $2
     `, [patientId, 500]);
