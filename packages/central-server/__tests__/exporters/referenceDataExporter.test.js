e0e44a3b76ae2234c3a1eb4925d32a3b55c9d760
khoi.nguyen <khoi@bes.au>
2024-05-23 07:30:55 +0700

    Update import path for excelUtils

--- packages/central-server/__tests__/exporters/referenceDataExporter.test.js
+++ packages/central-server/__tests__/exporters/referenceDataExporter.test.js
@@ -22,8 +22,8 @@ import { REFERENCE_TYPES } from '@tamanu/constants';
 import { writeExcelFile } from '../../dist/utils/excelUtils';
 import { makeRoleWithPermissions } from '../permissions';
 
-jest.mock('../../dist/admin/exporter/excelUtils', () => {
-  const originalModule = jest.requireActual('../../dist/admin/exporter/excelUtils');
+jest.mock('../../dist/utils/excelUtils', () => {
+  const originalModule = jest.requireActual('../../dist/utils/excelUtils');
 
   return {
     __esModule: true,
