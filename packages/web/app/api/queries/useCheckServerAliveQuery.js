6b94bc6d07b817b97473995a33bcfdd5cb0680cb
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-05-23 16:15:54 -0400

    Remove noise

--- packages/web/app/api/queries/useCheckServerAliveQuery.js
+++ packages/web/app/api/queries/useCheckServerAliveQuery.js
@@ -1,16 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../useApi';
 
-let debugN = Math.floor(Math.random() * 2);
 export const useCheckServerAliveQuery = () => {
   const api = useApi();
 
-  return useQuery(['serverAlive'], () => {
-    console.log('BBBBBBBBBB', debugN);
-    if (debugN % 2 !== 0) {
-      debugN += 1;
-      return false;
-    }
-    return api.checkServerAlive();
-  }, { cacheTime: 0 });
+  return useQuery(['serverAlive'], () => api.checkServerAlive(), { cacheTime: 0 });
 };
