4c5b99fff893fb6212ea5d0e6998adc4e83347c6
Daniel Nash <dannash200@gmail.com>
2024-05-24 12:12:35 +1200

    nass-1260: use outdating query in the refresh stats one

--- packages/web/app/api/queries/useMaterializedViewRefreshStatsQuery.js
+++ packages/web/app/api/queries/useMaterializedViewRefreshStatsQuery.js
@@ -1,11 +1,10 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useTranslation } from '../../contexts/Translation';
-import { useSocket } from '../../utils/useSocket';
 import { useApi } from '../useApi';
 import { formatDistanceToNow, parseISO } from 'date-fns';
 
 import { WS_EVENT_NAMESPACES } from '@tamanu/constants';
+import { useOutdatingQuery } from './useOutdatingQuery';
 
 /**
  * Gets the latest refresh stats (last refreshed time and parsed cron schedule)
@@ -16,24 +15,25 @@ import { WS_EVENT_NAMESPACES } from '@tamanu/constants';
  */
 export const useMaterializedViewRefreshStatsQuery = viewName => {
   const api = useApi();
-  const { socket } = useSocket();
   const { storedLanguage } = useTranslation();
-  const queryClient = useQueryClient();
   const [lastUpdated, setLastUpdated] = useState();
   const [refreshTrigger, setRefreshTrigger] = useState(0);
 
-  const queryResult = useQuery(['materialisedViewRefreshStats', viewName], () =>
-    api.get(`materializedView/refreshStats/${viewName}`, { language: storedLanguage }),
+  const queryResult = useOutdatingQuery(
+    ['materialisedViewRefreshStats', viewName],
+    `${WS_EVENT_NAMESPACES.DATA_UPDATED}:${viewName}`,
+    () => api.get(`materializedView/refreshStats/${viewName}`, { language: storedLanguage }),
+    {
+      onOutdated: () => {
+        setLastUpdated(null);
+        setRefreshTrigger(count => count + 1);
+      },
+    },
   );
+
   const { data: refreshStats } = queryResult;
 
   const dateAsDistanceToNow = date => formatDistanceToNow(parseISO(date), { addSuffix: 'ago' });
-
-  const handleFreshData = useCallback(() => {
-    setLastUpdated(null);
-    setRefreshTrigger(count => count + 1);
-    queryClient.invalidateQueries(['materialisedViewRefreshStats', viewName]);
-  }, [queryClient, viewName]);
 
   const handleRefreshLastUpdated = useCallback(() => {
     const { lastRefreshed } = refreshStats;
@@ -46,17 +46,6 @@ export const useMaterializedViewRefreshStatsQuery = viewName => {
     const interval = setInterval(handleRefreshLastUpdated, 1000 * 60);
     return () => clearInterval(interval);
   }, [refreshStats, handleRefreshLastUpdated]);
-
-  // Listen for refresh event from scheduled task via websocket
-  useEffect(() => {
-    const handleDataUpdatedEvent = () => {
-      handleFreshData();
-    };
-    if (!socket) return;
-    const eventKey = `${WS_EVENT_NAMESPACES.DATA_UPDATED}:${viewName}`;
-    socket.on(eventKey, handleDataUpdatedEvent);
-    return () => socket.off(eventKey, handleDataUpdatedEvent);
-  }, [socket, handleFreshData, viewName]);
 
   return {
     ...queryResult,
