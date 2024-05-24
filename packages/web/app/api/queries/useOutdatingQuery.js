eba6bdce1cfe9541597e08d9b958cedda7fdc3b2
Daniel Nash <dannash200@gmail.com>
2024-05-24 12:08:05 +1200

    nass-1260: have a go at making useOutdatingQuery

---
+++ packages/web/app/api/queries/useOutdatingQuery.js
@@ -0,0 +1,27 @@
+import { useEffect } from 'react';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useSocket } from '../../utils/useSocket';
+
+export const useOutdatingQuery = (
+  queryKey,
+  eventName,
+  queryFunc,
+  { onOutdated, ...queryConfig },
+) => {
+  const socket = useSocket();
+  const queryClient = useQueryClient();
+
+  // Listen for outdated event via websocket
+  useEffect(() => {
+    const handleDataUpdatedEvent = msg => {
+      queryClient.invalidateQueries(queryKey);
+      if (!onOutdated) return;
+      onOutdated(msg);
+    };
+    if (!socket) return;
+    socket.on(eventName, handleDataUpdatedEvent);
+    return () => socket.off(eventName, handleDataUpdatedEvent);
+  }, [socket, onOutdated, eventName, queryClient, queryKey]);
+
+  return useQuery(queryKey, queryFunc, queryConfig);
+};
