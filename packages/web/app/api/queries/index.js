cbc9a611adec3e0d665c8a98145489fe88df2b61
Daniel Nash <dannash200@gmail.com>
2024-05-24 13:37:09 +1200

    nass-1260: export from index useOutdatingQuery

--- packages/web/app/api/queries/index.js
+++ packages/web/app/api/queries/index.js
@@ -12,3 +12,4 @@ export * from './useTranslations';
 export * from './useTranslationLanguages';
 export * from './useTelegramBotInfoQuery';
 export * from './useMaterializedViewRefreshStatsQuery';
+export * from './useOutdatingQuery';
