c3b3f9233b660cafa67061478a45296135974d71
khoi.nguyen <khoi@bes.au>
2024-05-26 12:56:51 +0700

    update migration

--- packages/shared/src/migrations/1715069649383-addAddDiscountColumnsToInvoice.js
+++ packages/shared/src/migrations/1715069649383-addAddDiscountColumnsToInvoice.js
@@ -11,6 +11,18 @@ export async function up(query) {
   await query.addColumn('invoices', 'discount_markup_reason', {
     type: DataTypes.STRING,
   });
+
+  const invoicePercentageChanges = await query.sequelize.query(
+    'SELECT invoice_id, SUM(percentage_change) AS total_percentage_change FROM invoice_price_change_items WHERE deleted_at IS NULL AND status = :status GROUP BY invoice_id',
+    { type: query.sequelize.QueryTypes.SELECT, replacements: { status: 'active' } },
+  );
+
+  for (const { invoice_id, total_percentage_change } of invoicePercentageChanges) {
+    await query.sequelize.query(
+      `UPDATE invoices SET discount_markup_percentage = :total_percentage_change WHERE id = :invoice_id;`,
+      { replacements: { total_percentage_change, invoice_id } },
+    );
+  }
 }
 
 /**
