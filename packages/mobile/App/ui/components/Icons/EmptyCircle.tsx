525d36177f051a1b63f365acf7b735bef661c6ae
Daniel Nash <dannash200@gmail.com>
2024-05-28 16:58:33 +1200

    nass-1155: rollback colors for now

--- packages/mobile/App/ui/components/Icons/EmptyCircle.tsx
+++ packages/mobile/App/ui/components/Icons/EmptyCircle.tsx
@@ -5,10 +5,11 @@ import { Orientation, screenPercentageToDP } from '../../helpers/screen';
 
 const size = screenPercentageToDP(3.03, Orientation.Height);
 
-interface EmptyCircleIconProps {
-  fill: string;
-}
-
-export const EmptyCircleIcon = memo(({ fill = theme.colors.WHITE }: EmptyCircleIconProps) => (
-  <StyledView height={size} width={size} borderRadius={50} background={fill} />
+export const EmptyCircleIcon = memo(() => (
+  <StyledView
+    height={size}
+    width={size}
+    borderRadius={50}
+    background={theme.colors.WHITE}
+  />
 ));
