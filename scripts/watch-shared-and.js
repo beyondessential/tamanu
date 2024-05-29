7f3406f3abf771d5d247b4be3361ca1ca1145853
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-05-28 21:02:21 -0400

    Use singular as these should run per workspace, not all every time

--- scripts/watch-shared-and.js
+++ scripts/watch-shared-and.js
@@ -6,8 +6,8 @@ concurrently(
   [
     // running build-shared in raw:true mode will cause its output to interfere with
     // the way the second process handles stdin (eg it will break jest-watch hotkeys)
-    { command: 'npm run build-watch --workspaces @tamanu/shared', raw: false },
-    { command: `sleep 2 && npm run ${command} --workspaces ${workspace}`, raw: true },
+    { command: 'npm run build-watch --workspace @tamanu/shared', raw: false },
+    { command: `sleep 2 && npm run ${command} --workspace ${workspace}`, raw: true },
   ], 
   {
     defaultInputTarget: 1,
