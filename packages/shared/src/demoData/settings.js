3a4995761c54ffa4272c7f1c45d5d9a629593444
Edwin Monk-Fromont <edwin@beyondessential.com.au>
2024-05-31 16:13:42 +1200

    Test

--- packages/shared/src/demoData/settings.js
+++ packages/shared/src/demoData/settings.js
@@ -16,9 +16,6 @@ const seedForScope = async (models, settings, serverFacilityIds, scopeOverride) 
     const combined = defaultsDeep(existing, settings);
     return Setting.set('', combined, scope, facilityId);
   };
-  if (serverFacilityIds) {
-    return Promise.all(serverFacilityIds.map(combineSettings));
-  }
   return combineSettings();
 };
 
