85a5d705f3a856c0a6187551ae0b759ec759d54e
Kurt <kujo4pm@gmail.com>
2024-06-03 15:30:23 +1000

    Creating test that validate results

--- packages/central-server/__tests__/hl7fhir/materialised/Observation.test.js
+++ packages/central-server/__tests__/hl7fhir/materialised/Observation.test.js
@@ -94,7 +94,8 @@ describe('Parse Observation Results', () => {
     });
 
     it('Receive Observation for test in existing Service Request', async () => {
-      const { FhirServiceRequest } = ctx.store.models;
+      // arrange
+      const { FhirServiceRequest, LabTest } = ctx.store.models;
       const { labRequest, panelTestTypes } = await fakeResourcesOfFhirServiceRequestWithLabRequest(
         ctx.store.models,
         resources,
@@ -108,166 +109,70 @@ describe('Parse Observation Results', () => {
 
       // We can't use external codes that don't exist unfortuantely
       const filteredTests = panelTestTypes.filter(x => x.externalCode !== null);
-      const randomTestInPanel = 
-      filteredTests[chance.integer({
-        min: 0,
-        max: filteredTests.length - 1,
-      })];
-      console.log({ filteredTests: filteredTests.map(x => x.externalCode ) });
+      const randomTestInPanel =
+        filteredTests[chance.integer({
+          min: 0,
+          max: filteredTests.length - 1,
+        })];
       const mat = await FhirServiceRequest.materialiseFromUpstream(labRequest.id);
       const serviceRequestId = mat.id;
       await FhirServiceRequest.resolveUpstreams();
 
       const body = postBody(serviceRequestId, randomTestInPanel.externalCode);
+
+      // act
       const response = await app.post(endpoint).send(body);
-      console.log({ response });
-      await labRequest.reload();
+
+      // assert
+      const labTest = await LabTest.findOne({
+        where: {
+          labRequestId: labRequest.id,
+          labTestTypeId: randomTestInPanel.id,
+        }
+      })
+      expect(labTest.result).toBe('30');
       expect(response).toHaveSucceeded();
     });
 
     test.todo('Receive Observation for test in Service Request using internal coding')
     test.todo('Receive Observation for new test in Service Request')
+    test.todo('Receive Observation for string valued result')
+    test.todo('Receive Observation for boolean valued result')
+    test.todo('Receive Observation for number valued result')
 
-    // describe('errors', () => {
-    //   it('error if attach a diagnosticReport to an ImagingRequest', async () => {
-    //     const { FhirServiceRequest } = ctx.store.models;
-    //     const imagingRequest = await fakeResourcesOfFhirServiceRequestWithImagingRequest(
-    //       ctx.store.models,
-    //       resources,
-    //     );
-    //     const mat = await FhirServiceRequest.materialiseFromUpstream(imagingRequest.id);
-    //     const serviceRequestId = mat.id;
-    //     await FhirServiceRequest.resolveUpstreams();
+    describe('errors', () => {
+      test.todo('Receive Observation for with multiple results of different types')
+      test.todo('Receive Observation for with no results')
+      //   it('error if attach a diagnosticReport to an ImagingRequest', async () => {
+      //     const { FhirServiceRequest } = ctx.store.models;
+      //     const imagingRequest = await fakeResourcesOfFhirServiceRequestWithImagingRequest(
+      //       ctx.store.models,
+      //       resources,
+      //     );
+      //     const mat = await FhirServiceRequest.materialiseFromUpstream(imagingRequest.id);
+      //     const serviceRequestId = mat.id;
+      //     await FhirServiceRequest.resolveUpstreams();
 
-    //     const body = postBody(serviceRequestId);
-    //     const response = await app.post(endpoint).send(body);
-    //     expect(response.body).toMatchObject({
-    //       resourceType: 'OperationOutcome',
-    //       id: expect.any(String),
-    //       issue: [
-    //         {
-    //           severity: 'error',
-    //           code: 'invalid',
-    //           diagnostics: expect.any(String),
-    //           details: {
-    //             text: `No LabRequest with id: '${imagingRequest.id}', might be ImagingRequest id`,
-    //           },
-    //         },
-    //       ],
-    //     });
-    //     expect(response.status).toBe(400);
-    //   });
+      //     const body = postBody(serviceRequestId);
+      //     const response = await app.post(endpoint).send(body);
+      //     expect(response.body).toMatchObject({
+      //       resourceType: 'OperationOutcome',
+      //       id: expect.any(String),
+      //       issue: [
+      //         {
+      //           severity: 'error',
+      //           code: 'invalid',
+      //           diagnostics: expect.any(String),
+      //           details: {
+      //             text: `No LabRequest with id: '${imagingRequest.id}', might be ImagingRequest id`,
+      //           },
+      //         },
+      //       ],
+      //     });
+      //     expect(response.status).toBe(400);
+      //   });
 
-    //   it('returns invalid if the resourceType does not match', async () => {
-    //     const { FhirServiceRequest } = ctx.store.models;
-    //     const { labRequest } = await fakeResourcesOfFhirServiceRequestWithLabRequest(
-    //       ctx.store.models,
-    //       resources,
-    //       {
-    //         isWithPanels: true,
-    //       },
-    //       {
-    //         status: LAB_REQUEST_STATUSES.TO_BE_VERIFIED
-    //       }
-    //     );
-    //     const mat = await FhirServiceRequest.materialiseFromUpstream(labRequest.id);
-    //     const serviceRequestId = mat.id;
-    //     await FhirServiceRequest.resolveUpstreams();
-
-    //     const body = postBody(serviceRequestId);
-    //     body.resourceType = 'Patient';
-    //     const response = await app.post(endpoint).send(body);
-    //     expect(response.body).toMatchObject({
-    //       resourceType: 'OperationOutcome',
-    //       id: expect.any(String),
-    //       issue: [
-    //         {
-    //           severity: 'error',
-    //           code: 'invalid',
-    //           diagnostics: expect.any(String),
-    //           details: {
-    //             text: "must be 'Observation'",
-    //           },
-    //         },
-    //       ],
-    //     });
-    //     expect(response.status).toBe(400);
-    //   });
-
-
-    //   it('reports invalid if using invalid statuses', async () => {
-    //     const { FhirServiceRequest } = ctx.store.models;
-    //     const { labRequest } = await fakeResourcesOfFhirServiceRequestWithLabRequest(
-    //       ctx.store.models,
-    //       resources,
-    //       {
-    //         isWithPanels: true,
-    //       },
-    //       {
-    //         status: LAB_REQUEST_STATUSES.TO_BE_VERIFIED
-    //       }
-    //     );
-    //     const mat = await FhirServiceRequest.materialiseFromUpstream(labRequest.id);
-    //     const serviceRequestId = mat.id;
-    //     await FhirServiceRequest.resolveUpstreams();
-    //     const body = postBody(serviceRequestId);
-
-
-    //     body.status = 'unstatus';
-    //     const response = await app.post(endpoint).send(body);
-
-    //     expect(response.body).toMatchObject({
-    //       resourceType: 'OperationOutcome',
-    //       id: expect.any(String),
-    //       issue: [
-    //         {
-    //           severity: 'error',
-    //           code: 'invalid',
-    //           diagnostics: expect.any(String),
-    //           details: {
-    //             text: "'unstatus' is an invalid ServiceRequest status",
-    //           },
-    //         },
-    //       ],
-    //     });
-    //     expect(response.status).toBe(400);
-    //   });
-
-    //   it('returns invalid structure if the service request id is missing', async () => {
-    //     const { FhirServiceRequest } = ctx.store.models;
-    //     const { labRequest } = await fakeResourcesOfFhirServiceRequestWithLabRequest(
-    //       ctx.store.models,
-    //       resources,
-    //       {
-    //         isWithPanels: true,
-    //       },
-    //       {
-    //         status: LAB_REQUEST_STATUSES.TO_BE_VERIFIED
-    //       }
-    //     );
-    //     const mat = await FhirServiceRequest.materialiseFromUpstream(labRequest.id);
-    //     const serviceRequestId = mat.id;
-    //     await FhirServiceRequest.resolveUpstreams();
-    //     const body = postBody(serviceRequestId);
-    //     delete body.basedOn;
-    //     const response = await app.post(endpoint).send(body);
-    //     expect(response.body).toMatchObject({
-    //       resourceType: 'OperationOutcome',
-    //       id: expect.any(String),
-    //       issue: [
-    //         {
-    //           severity: 'error',
-    //           code: 'invalid',
-    //           diagnostics: expect.any(String),
-    //           details: {
-    //             text: 'basedOn is a required field',
-    //           },
-    //         },
-    //       ],
-    //     });
-    //     expect(response.status).toBe(400);
-    //   });
-    // });
+    });
   });
 
 });
