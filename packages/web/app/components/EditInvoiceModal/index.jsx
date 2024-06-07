239a67458afc18dc7022fb8d2e99a4492ac3a908
Huy-Tran-Huu-Le <160376020+Huy-Tran-Huu-Le@users.noreply.github.com>
2024-06-07 08:35:51 +0700

    tweaks: EPI-825: Prevent empty first line when adding potential items (#5952)
    * prevent empty first line when Add potential item
    
    * handle add row using early return
    
    * add comment

--- packages/web/app/components/EditInvoiceModal/index.jsx
+++ packages/web/app/components/EditInvoiceModal/index.jsx
@@ -154,31 +154,43 @@ export const EditInvoiceModal = ({ open, onClose, invoiceId, displayId, encounte
 
 
   const handleAddRow = (rowData) => {
-    const newRowList = [...rowList];
-    if (Array.isArray(rowData) && rowData.length) {
-      rowData.forEach(newItem => {
-        const idExists = newRowList.some(item => item && item.id === newItem.id);
-        if (!idExists) {
-          newRowList.push({
-            id: newItem?.id,
-            details: newItem?.name,
-            date: newItem?.date,
-            orderedBy: newItem?.orderedBy,
-            price: newItem?.price,
-            invoiceLineTypeId: newItem?.invoiceLineTypeId,
-            orderedById: newItem?.orderedById,
-            code: newItem?.code,
-            toBeUpdated: true,
-            toBeCreated: !!newItem.toBeCreated
-          });
-        }
-      });
-    } else {
-      newRowList.push(defaultRow);
+    if (!Array.isArray(rowData) || !rowData.length) {
+      setRowList([...rowList, defaultRow]);
+      return;
     }
+  
+    let newRowList = [...rowList];
 
+    //when there is only 1 item and it is empty, delete it before adding potential items array
+    if (
+      rowList.length === 1 &&
+      !rowList[0].invoiceLineTypeId &&
+      !rowList[0].orderedById &&
+      !rowList[0].dateGenerated
+    ) {
+      newRowList = [];
+    }
+  
+    rowData.forEach(newItem => {
+      const idExists = newRowList.some(item => item && item.id === newItem.id);
+      if (!idExists) {
+        newRowList.push({
+          id: newItem?.id,
+          details: newItem?.name,
+          date: newItem?.date,
+          orderedBy: newItem?.orderedBy,
+          price: newItem?.price,
+          invoiceLineTypeId: newItem?.invoiceLineTypeId,
+          orderedById: newItem?.orderedById,
+          code: newItem?.code,
+          toBeUpdated: true,
+          toBeCreated: !!newItem.toBeCreated
+        });
+      }
+    });
+  
     setRowList(newRowList);
-  };
+  };  
 
   const COLUMNS = [
     {
