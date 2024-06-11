00e51c96ab157178264e366f10560544709546ab
Daniel Nash <dannash200@gmail.com>
2024-06-11 14:44:39 +1200

    nass-1184: sign in form

--- packages/mobile/App/ui/components/Forms/SignInForm.tsx
+++ packages/mobile/App/ui/components/Forms/SignInForm.tsx
@@ -19,14 +19,13 @@ import { TextField } from '../TextField/TextField';
 import { SubmitButton } from './SubmitButton';
 import { ServerSelector } from '../ServerSelectorField/ServerSelector';
 import { TranslatedText } from '../Translations/TranslatedText';
+import { useTranslation } from '~/ui/contexts/TranslationContext';
 
 interface SignInFormModelValues {
   email: string;
   password: string;
   server: string;
 }
-
-const REQUIRED_VALIDATION_MESSAGE = '*Required';
 
 const ServerInfo = __DEV__
   ? ({ host }): ReactElement => {
@@ -48,6 +47,7 @@ export const SignInForm: FunctionComponent<any> = ({ onError, onSuccess }) => {
   const [existingHost, setExistingHost] = useState('');
   const passwordRef = useRef(null);
   const { signIn } = useAuth();
+  const { getTranslation } = useTranslation();
 
   const handleSignIn = useCallback(
     async (values: SignInFormModelValues) => {
@@ -86,10 +86,12 @@ export const SignInForm: FunctionComponent<any> = ({ onError, onSuccess }) => {
       validateOnBlur={false}
       validationSchema={Yup.object().shape({
         email: Yup.string()
-          .email('Must be a valid email address')
-          .required(REQUIRED_VALIDATION_MESSAGE),
-        password: Yup.string().required(REQUIRED_VALIDATION_MESSAGE),
-        server: existingHost ? Yup.string() : Yup.string().required(REQUIRED_VALIDATION_MESSAGE),
+          .email(getTranslation('validation.rule.validEmail', 'Must be a valid email address'))
+          .required(getTranslation('validation.required.inline', '*Required')),
+        password: Yup.string().required(getTranslation('validation.required.inline', '*Required')),
+        server: existingHost
+          ? Yup.string()
+          : Yup.string().required(getTranslation('validation.required.inline', '*Required')),
       })}
       onSubmit={handleSignIn}
     >
