a46b6a0c2e0e14fde7765dfc34604c1e80312123
Daniel Nash <dannash200@gmail.com>
2024-06-13 10:06:31 +1200

    nass-1246: begin abstracting out

---
+++ packages/web/app/forms/PatientDetailsForm/useComputedInitialValues.jsx
@@ -0,0 +1,39 @@
+import { PATIENT_DETAIL_LAYOUTS } from '@tamanu/constants';
+import { useLocalisation } from '../../contexts/Localisation';
+import { SECONDARY_LOCATION_HIERARCHY_FIELDS } from './layouts/cambodia/patientFields/CambodiaLocationFields';
+import { useHierarchyAncestorsQuery } from '../../api/queries/useHierarchyAncestorsQuery';
+import { useFilterPatientFields } from './useFilterPatientFields';
+
+export const useSecondaryLocationFields = ({ layout, additionalData }) => {
+  const { data: ancestors, isLoading } = useHierarchyAncestorsQuery(
+    additionalData.secondaryVillageId,
+    {
+      enabled: layout === PATIENT_DETAIL_LAYOUTS.CAMBODIA,
+    },
+  );
+  const { fieldsToShow } = useFilterPatientFields({
+    fields: SECONDARY_LOCATION_HIERARCHY_FIELDS,
+    filterByMandatory: true,
+  });
+
+  const initialValues = Object.fromEntries(
+    fieldsToShow.map(({ name, referenceType }) => [name, ancestors[referenceType]]),
+  );
+  return { initialValues, isLoading };
+};
+
+export const useComputedInitialValues = ({ additionalData }) => {
+  const { getLocalisation } = useLocalisation();
+  const layout = getLocalisation('layouts.patientDetails') || PATIENT_DETAIL_LAYOUTS.GENERIC;
+  const {
+    initialValues: secondaryLocationInitialValues,
+    isLoading: isLoadingSecondaryLocationInitialValues,
+  } = useSecondaryLocationFields({ layout, additionalData });
+
+  return {
+    initialValues: {
+      ...secondaryLocationInitialValues,
+    },
+    isLoading: isLoadingSecondaryLocationInitialValues,
+  };
+};
