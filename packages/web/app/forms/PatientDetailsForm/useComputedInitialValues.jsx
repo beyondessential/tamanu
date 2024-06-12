0dc82279777706644a581b47452cfeaa49616151
Daniel Nash <dannash200@gmail.com>
2024-06-13 11:44:51 +1200

    oopsy

--- packages/web/app/forms/PatientDetailsForm/useComputedInitialValues.jsx
+++ packages/web/app/forms/PatientDetailsForm/useComputedInitialValues.jsx
@@ -7,7 +7,7 @@ import { useFilterPatientFields } from './useFilterPatientFields';
 // Some values for patient fields are not persisted to database and rather are derived from other fields.
 // This hook computes the initial values for these fields.
 // For example, in Cambodia layout, the secondary patient location hierarchy is not stored in the patient record beyond the secondary village ID.
-// Attempting to prepare the values from within the form state is error-prone as it interacts poorly with form logic.
+// Attempting to prepare the values from within the form state is error-prone as interacts poorly with form logic.
 export const useComputedInitialValues = ({ additionalData }) => {
   const { getLocalisation } = useLocalisation();
   const layout = getLocalisation('layouts.patientDetails') || PATIENT_DETAIL_LAYOUTS.GENERIC;
@@ -22,10 +22,10 @@ export const useComputedInitialValues = ({ additionalData }) => {
     fields: SECONDARY_LOCATION_HIERARCHY_FIELDS,
   });
 
-  const initialValues =
-    fieldsToShow?.length &&
-    Object.fromEntries(
-      fieldsToShow.map(({ name, referenceType }) => [name, ancestors[referenceType]]),
-    );
-  return { data: initialValues, isLoading };
+  if (isLoading) return { isLoading: true };
+
+  const initialValues = Object.fromEntries(
+    fieldsToShow.map(({ name, referenceType }) => [name, ancestors[referenceType]]),
+  );
+  return { data: initialValues };
 };
