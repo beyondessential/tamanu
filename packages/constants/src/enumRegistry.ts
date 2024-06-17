9ea5aec843c011833ff6e2e68059369a3542d1c6
Daniel Nash <dannash200@gmail.com>
2024-06-17 13:42:32 +1200

    nass-1194: fix import of births

--- packages/constants/src/enumRegistry.ts
+++ packages/constants/src/enumRegistry.ts
@@ -3,7 +3,7 @@ import { INVOICE_LINE_TYPE_LABELS, INVOICE_PRICE_CHANGE_TYPE_LABELS } from './in
 import { NOTE_TYPE_LABELS } from './notes';
 import { REFERRAL_STATUS_LABELS, APPOINTMENT_TYPES, APPOINTMENT_STATUSES } from './statuses';
 import { VACCINE_STATUS_LABELS, INJECTION_SITE_LABELS, VACCINE_CATEGORIES } from './vaccines';
-import { BIRTH_TYPE_LABELS } from 'births';
+import { BIRTH_TYPE_LABELS } from './births';
 
 // This is a set of all the enums that are registered to be translatable.
 // This allows us to keep track of changes to existing enums or the additional
