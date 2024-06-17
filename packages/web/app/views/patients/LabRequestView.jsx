2fc92b068d67c1e8bf8af6be8a1b4f29cb3041db
Kurt <kujo4pm@gmail.com>
2024-06-17 14:46:42 +1000

    Removed test code

--- packages/web/app/views/patients/LabRequestView.jsx
+++ packages/web/app/views/patients/LabRequestView.jsx
@@ -286,7 +286,7 @@ export const LabRequestView = () => {
             </Button>
           )}
           {canEnterResults && (
-            <Button onClick={() => handleChangeModalId(MODAL_IDS.ENTER_RESULTS)} margin="2000px">
+            <Button onClick={() => handleChangeModalId(MODAL_IDS.ENTER_RESULTS)}>
               <TranslatedText stringId="lab.action.enterResults" fallback="Enter results" />
             </Button>
           )}
