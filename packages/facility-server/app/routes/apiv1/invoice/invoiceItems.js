d69fea261abe6306c7f04e5ea6ffeda67201adfa
Nam Hoang <nam@bes.au>
2024-06-18 04:05:01 +0000

    fix: update potential invoice items endpoint

--- packages/facility-server/app/routes/apiv1/invoice/invoiceItems.js
+++ packages/facility-server/app/routes/apiv1/invoice/invoiceItems.js
@@ -1,66 +1,16 @@
 import { permissionCheckingRouter, simpleGetList } from '@tamanu/shared/utils/crudHelpers';
 import asyncHandler from 'express-async-handler';
 import { getPotentialInvoiceItems } from './getPotentialInvoiceItems';
-import { renameObjectKeys } from '@tamanu/shared/utils';
-import { transform, set } from 'lodash';
 
 export const invoiceItemsRoute = permissionCheckingRouter('read', 'Invoice');
 
 invoiceItemsRoute.get('/:id/items', simpleGetList('InvoiceItem', 'invoiceId'));
 
 invoiceItemsRoute.get(
-  '/:invoiceId/potentialInvoiceItems',
+  '/:id/potentialInvoiceItems',
   asyncHandler(async (req, res) => {
-    const { models, params, db, getLocalisation, query } = req;
-    const { invoiceId } = params;
-    const order = query.order?.toLowerCase();
-    const orderBy = query.orderBy;
-
-    const invoice = await models.Invoice.findByPk(invoiceId);
-    const { encounterId } = invoice;
-    const localisation = await getLocalisation();
-    const { imagingTypes } = localisation;
-
-    const potentialInvoiceLineItems = await getPotentialInvoiceItems(
-      db,
-      models,
-      encounterId,
-      imagingTypes,
-    );
-    const data = potentialInvoiceLineItems.map(x => renameObjectKeys(x.forResponse()));
-
-    const transformedData = data.map(it =>
-      transform(
-        it,
-        (result, value, key) => {
-          set(result, key, value);
-        },
-        {},
-      ),
-    );
-
-    if (order && orderBy) {
-      transformedData.sort((a, b) => {
-        const valA = a[orderBy];
-        const valB = b[orderBy];
-        if (orderBy === 'code' || orderBy === 'type') {
-          if (order === 'asc') {
-            return valA?.localeCompare(valB);
-          } else {
-            return valB?.localeCompare(valA);
-          }
-        } else if (orderBy === 'price') {
-          return order === 'asc' ? valA - valB : valB - valA;
-        } else if (orderBy === 'orderDate') {
-          return order === 'asc' ? Date.parse(valA) - Date.parse(valB) : Date.parse(valB) - Date.parse(valA);
-        }
-        return 0;
-      });
-    }
-
-    res.send({
-      count: transformedData.length,
-      data: transformedData,
-    });
+    req.flagPermissionChecked();
+    const data = await getPotentialInvoiceItems(req.db, req.models, req.params.id);
+    res.json(data);
   }),
 );
