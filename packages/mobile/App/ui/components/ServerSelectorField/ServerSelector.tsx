9c492f0df3b04b09f812374a7df1c7a1ed70382d
Chris Tran <chris@beyondessential.com.au>
2024-06-20 23:57:16 +1000

    sav-646: removed constants

--- packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
+++ packages/mobile/App/ui/components/ServerSelectorField/ServerSelector.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement, useEffect, useState } from 'react';
 import { useNetInfo } from '@react-native-community/netinfo';
 import { keyBy, mapValues } from 'lodash';
 
-import { GENERAL_IMPORTABLE_DATA_TYPES } from '@tamanu/constants';
+// import { GENERAL_IMPORTABLE_DATA_TYPES } from '@tamanu/constants';
 
 import { Dropdown, SelectOption } from '../Dropdown';
 import { StyledText, StyledView } from '../../styled/common';
@@ -51,7 +51,7 @@ export const ServerSelector = ({ onChange, label, value, error }): ReactElement 
     setHost,
   } = useTranslation();
 
-  console.log('GENERAL_IMPORTABLE_DATA_TYPES', GENERAL_IMPORTABLE_DATA_TYPES);
+  // console.log('GENERAL_IMPORTABLE_DATA_TYPES', GENERAL_IMPORTABLE_DATA_TYPES);
   const updateHost = value => {
     onChange(value);
     setHost(value);
