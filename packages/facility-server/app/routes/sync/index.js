d3fc86d70f5e02605233277a3583caf33269d965
Chris Tran <chris@beyondessential.com.au>
2024-06-21 09:00:34 +1000

    sav-650: tweak syncRoute

--- packages/facility-server/app/routes/sync/index.js
+++ packages/facility-server/app/routes/sync/index.js
@@ -1,10 +1,1 @@
-import express from 'express';
-
-import { sync } from '../sync/sync';
-
-export const syncRoutes = express.Router();
-
-// sync endpoints
-syncRoutes.use('/sync', sync);
-
-export default syncRoutes;
+export { sync } from './sync';
