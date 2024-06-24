3c84895b8dae1893609e27b365db4fb2c32b2a9f
Kurt <kujo4pm@gmail.com>
2024-06-24 13:19:12 +1000

    Remove dx

--- packages/constants/src/importable.ts
+++ packages/constants/src/importable.ts
@@ -19,7 +19,6 @@ export const GENERAL_IMPORTABLE_DATA_TYPES = [
   'dischargeDisposition',
   'division',
   'drug',
-  'dxImagingArea',
   'ecgImagingArea',
   'echocardiogramImagingArea',
   'endoscopyImagingArea',
