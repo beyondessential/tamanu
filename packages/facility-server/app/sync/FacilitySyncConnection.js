af7be17dd315bdfd186e70e3d4d39fa7c49f3e75
chris-bes <63621318+chris-bes@users.noreply.github.com>
2024-06-27 09:38:03 +1000

    feat(sync): SAV-650: Queue sync requests (#5920)
    * sav-650: queue sync requests
    
    * sav-650: abstract logic of starting servers
    
    * sav-650: renamed
    
    * sav-650: added more doco
    
    * sav-650: added missing file
    
    * sav-650: added layer to connect to local sync server
    
    * sav-650: more refactor
    
    * sav-650: reverted
    
    * sav-650: removed comments
    
    * sav-650: fixed failing tests
    
    * sav-650: removed unused
    
    * sav-650: more clean up
    
    * sav-650: fixed sync data
    
    * sav-650: fixed tests
    
    * sav-650: addressed reviews
    
    * sav-650: tweak syncRoute

---
+++ packages/facility-server/app/sync/FacilitySyncConnection.js
@@ -0,0 +1,56 @@
+import config from 'config';
+
+import { RemoteCallFailedError } from '@tamanu/shared/errors';
+import { getResponseJsonSafely } from '@tamanu/shared/utils';
+import { log } from '@tamanu/shared/services/logging';
+
+/**
+ * The sync triggering api is non-authed, and generally protected by making it
+ * only accessible on localhost via the reverse proxy. This is ok because it doesn't
+ * do anything sensitive or dangerous, but please keep it that way - only add new routes
+ * or functionality with healthy caution, or after implementing auth
+ */
+export class FacilitySyncConnection {
+  constructor() {
+    this.host = `${config.sync.syncApiConnection.host.trim().replace(/\/*$/, '')}:${
+      config.sync.syncApiConnection.port
+    }`;
+  }
+
+  async fetch(endpoint, params = {}) {
+    const { body, method = 'GET' } = params;
+
+    const url = `${this.host}/sync/${endpoint}`;
+    log.debug(`[FacilitySyncConnection] ${method} ${url}`);
+
+    const response = await fetch(url, {
+      method,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': body ? 'application/json' : undefined,
+      },
+      body: body && JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      const responseBody = await getResponseJsonSafely(response);
+      const { error } = responseBody;
+
+      const errorMessage = error ? error.message : 'no error message given';
+      const err = new RemoteCallFailedError(
+        `Faciliy Sync API responded with status code ${response.status} (${errorMessage})`,
+      );
+      throw err;
+    }
+
+    return response.json();
+  }
+
+  async runSync(syncData) {
+    return this.fetch('run', { method: 'POST', body: { syncData } });
+  }
+
+  async getSyncStatus() {
+    return this.fetch('status');
+  }
+}
