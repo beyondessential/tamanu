341733db1ce483f7fbbda07f3ca3536043a1ddfc
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-06-27 21:00:27 -0400

    Refactor into functions

--- packages/central-server/app/admin/programImporter/programDataElementValidation.js
+++ packages/central-server/app/admin/programImporter/programDataElementValidation.js
@@ -8,6 +8,7 @@ import {
 } from '@tamanu/constants';
 import { validateVisualisationConfig } from './visualisationConfigValidation';
 
+// Checks complex chart core question config individually
 function validateComplexChartCoreQuestion(programDataElementRecord, surveyScreenComponentRecord) {
   const { type } = programDataElementRecord.values;
   const { visibilityStatus } = surveyScreenComponentRecord.values;
@@ -26,6 +27,31 @@ function validateComplexChartCoreQuestion(programDataElementRecord, surveyScreen
   }
 }
 
+// Checks that complex chart core questions: are exactly 4 and ordered specifically
+function validateComplexChartCore(programDataElementRecords, sheetName, stats, errors) {
+  const hasFourQuestions = programDataElementRecords.length === COMPLEX_CORE_DATA_ELEMENT_TYPES.length;
+  const hasCorrectOrder = programDataElementRecords.every(
+    (element, index) => element?.values?.type === COMPLEX_CORE_DATA_ELEMENT_TYPES[index],
+  );
+
+  if (!hasFourQuestions || !hasCorrectOrder) {
+    const error = new Error('Invalid complex chart core questions');
+    updateStat(stats, statkey('ProgramDataElement', sheetName), 'errored', 1);
+    errors.push(error);
+  }
+}
+
+function validateChartingFirstQuestion(programDataElementRecords, sheetName, stats, errors) {
+  const { code, type } = programDataElementRecords[0].values;
+    if (type !== PROGRAM_DATA_ELEMENT_TYPES.DATE_TIME) {
+      const error = new Error(
+        `sheetName: ${sheetName}, code: '${code}', First question should be DateTime type`,
+      );
+      updateStat(stats, statkey('ProgramDataElement', sheetName), 'errored', 1);
+      errors.push(error);
+    }
+}
+
 export function validateProgramDataElementRecords(
   records,
   { context, sheetName, stats: previousStats = {}, surveyType },
@@ -36,26 +62,11 @@ export function validateProgramDataElementRecords(
   const programDataElementRecords = records.filter(({ model }) => model === 'ProgramDataElement');
 
   if ([SURVEY_TYPES.SIMPLE_CHART, SURVEY_TYPES.COMPLEX_CHART].includes(surveyType)) {
-    const { code, type } = programDataElementRecords[0].values;
-    if (type !== PROGRAM_DATA_ELEMENT_TYPES.DATE_TIME) {
-      const error = new Error(
-        `sheetName: ${sheetName}, code: '${code}', First question should be DateTime type`,
-      );
-      updateStat(stats, statkey('ProgramDataElement', sheetName), 'errored', 1);
-      errors.push(error);
-    }
+    validateChartingFirstQuestion(programDataElementRecords, sheetName, stats, errors);
   }
 
   if (surveyType === SURVEY_TYPES.COMPLEX_CHART_CORE) {
-    const hasFourQuestions = programDataElementRecords.length === COMPLEX_CORE_DATA_ELEMENT_TYPES.length;
-    const hasCorrectOrder = programDataElementRecords.every(
-      (element, index) => element?.values?.type === COMPLEX_CORE_DATA_ELEMENT_TYPES[index],
-    );
-    if (!hasFourQuestions || !hasCorrectOrder) {
-      const error = new Error('Invalid complex chart core questions');
-      updateStat(stats, statkey('ProgramDataElement', sheetName), 'errored', 1);
-      errors.push(error);
-    }
+    validateComplexChartCore(programDataElementRecords, sheetName, stats, errors);
   }
 
   for (const programDataElementRecord of programDataElementRecords) {
