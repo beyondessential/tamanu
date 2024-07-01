e66878f35698a362d29b109a73cdfc1fd5038a94
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2024-07-02 10:04:48 +1200

    fix(translations): NASS-1127: Clean up translated ref data selects with superfluous prefix prop (#6025)
    * nass-1194: remove prefix from imaging areas select
    
    * nass-1194: vaccine field quite hectic
    
    * nass-1127: remove prefix
    
    * nass-1127: remove prefix and update registry form
    
    * nass-1127: does it work lol
    
    * nass-1127: last fix
    
    * nass-1127: better

--- packages/web/app/views/programRegistry/ActivatePatientProgramRegistry.jsx
+++ packages/web/app/views/programRegistry/ActivatePatientProgramRegistry.jsx
@@ -28,6 +28,7 @@ import {
 import { useTranslation } from '../../contexts/Translation';
 import { FORM_TYPES } from '../../constants';
 import { TranslatedText } from '../../components/Translation/TranslatedText';
+import { TranslatedReferenceData } from '../../components';
 
 export const ActivatePatientProgramRegistry = ({ onClose, patientProgramRegistration, open }) => {
   const api = useApi();
@@ -180,9 +181,17 @@ export const ActivatePatientProgramRegistry = ({ onClose, patientProgramRegistra
                     }
                     placeholder={getTranslation('general.placeholder.select', 'Select')}
                     component={MultiselectField}
-                    options={conditions}
+                    options={conditions?.map(condition => ({
+                      label: (
+                        <TranslatedReferenceData
+                          fallback={condition.name}
+                          value={condition.id}
+                          category="condition"
+                        />
+                      ),
+                      value: condition.id,
+                    }))}
                     disabled={!conditions || conditions.length === 0}
-                    prefix="programRegistry.property.relatedCondition"
                   />
                 </FormGrid>
               </FormGrid>
