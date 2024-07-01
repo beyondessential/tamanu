3614b054cd454d037b2c6d9da29fd52ec6f0e2d0
Daniel Nash <dannash200@gmail.com>
2024-07-02 11:35:22 +1200

    fix: wrap provider

--- packages/mobile/App/ui/navigation/stacks/VaccineTableTabs.tsx
+++ packages/mobile/App/ui/navigation/stacks/VaccineTableTabs.tsx
@@ -4,6 +4,7 @@ import { Routes } from '/helpers/routes';
 import { VaccineHistoryTab } from '../screens/vaccine/tableTabs';
 import { createTopTabNavigator } from '/components/TopTabNavigator';
 import { TranslatedText } from '~/ui/components/Translations/TranslatedText';
+import { VaccineFormRefreshProvider } from '~/ui/components/Forms/VaccineForms/VaccineRefreshContext';
 
 const Tabs = createTopTabNavigator();
 
@@ -17,39 +18,44 @@ export const VaccineTableTabs = (): ReactElement => {
   }, []);
 
   return (
-    <Tabs.Navigator
-      tabBarOptions={{
-        labelStyle: { textTransform: 'none' },
-      }}
-      swipeEnabled={false}
-    >
-      <Tabs.Screen
-        options={{
-          title: () => (
-            <TranslatedText stringId="vaccine.form.category.option.routine" fallback="Routine" />
-          ),
+    <VaccineFormRefreshProvider>
+      <Tabs.Navigator
+        tabBarOptions={{
+          labelStyle: { textTransform: 'none' },
         }}
-        name={Routes.HomeStack.VaccineStack.VaccineTabs.Routine}
-        component={VaccineHistoryTab}
-      />
-      <Tabs.Screen
-        options={{
-          title: () => (
-            <TranslatedText stringId="vaccine.form.category.option.catchUp" fallback="Catchup" />
-          ),
-        }}
-        name={Routes.HomeStack.VaccineStack.VaccineTabs.Catchup}
-        component={VaccineHistoryTab}
-      />
-      <Tabs.Screen
-        options={{
-          title: () => (
-            <TranslatedText stringId="vaccine.form.category.option.campaign" fallback="Campaign" />
-          ),
-        }}
-        name={Routes.HomeStack.VaccineStack.VaccineTabs.Campaign}
-        component={VaccineHistoryTab}
-      />
-    </Tabs.Navigator>
+        swipeEnabled={false}
+      >
+        <Tabs.Screen
+          options={{
+            title: () => (
+              <TranslatedText stringId="vaccine.form.category.option.routine" fallback="Routine" />
+            ),
+          }}
+          name={Routes.HomeStack.VaccineStack.VaccineTabs.Routine}
+          component={VaccineHistoryTab}
+        />
+        <Tabs.Screen
+          options={{
+            title: () => (
+              <TranslatedText stringId="vaccine.form.category.option.catchUp" fallback="Catchup" />
+            ),
+          }}
+          name={Routes.HomeStack.VaccineStack.VaccineTabs.Catchup}
+          component={VaccineHistoryTab}
+        />
+        <Tabs.Screen
+          options={{
+            title: () => (
+              <TranslatedText
+                stringId="vaccine.form.category.option.campaign"
+                fallback="Campaign"
+              />
+            ),
+          }}
+          name={Routes.HomeStack.VaccineStack.VaccineTabs.Campaign}
+          component={VaccineHistoryTab}
+        />
+      </Tabs.Navigator>
+    </VaccineFormRefreshProvider>
   );
 };
