9f6662a37d889f9ab4accf714b76ab6bf904d40a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-07-09 16:29:15 +1200

    header comment for `isTranslatedText`

--- packages/web/app/components/Translation/utils.js
+++ packages/web/app/components/Translation/utils.js
@@ -2,6 +2,11 @@ import { isValidElement } from 'react';
 import { LocationCell } from '../LocationCell.jsx';
 import { TranslatedEnum, TranslatedReferenceData, TranslatedSex, TranslatedText } from '.';
 
+/**
+ * Given a valid React element, returns true if and only if that element is a wrapper around
+ * {@link TranslatedText}. In other words, it is an element whose root might be a `<TranslatedText>`
+ * element.
+ */
 export const isTranslatedText = element => {
   if (!isValidElement(element)) return false;
   return [
