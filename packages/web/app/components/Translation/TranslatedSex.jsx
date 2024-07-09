f8ceed52f728b246db159e1b80506fc088c99da9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-07-09 15:36:27 +1200

    rename `SexDisplay` → `TranslatedSex`

--- packages/web/app/components/Translation/TranslatedSex.jsx
+++ packages/web/app/components/Translation/TranslatedSex.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { SEX_VALUE_INDEX } from '@tamanu/constants';
 import { TranslatedText } from './TranslatedText';
 
-export const SexDisplay = ({ sex }) => {
+export const TranslatedSex = ({ sex }) => {
   switch (sex) {
     case SEX_VALUE_INDEX.male.value:
       return <TranslatedText stringId="patient.property.sex.male" fallback="Male" />;
--- packages/web/app/components/Translation/index.js
+++ packages/web/app/components/Translation/index.js
@@ -1,3 +1,4 @@
 export { TranslatedEnum } from './TranslatedEnum';
 export { TranslatedReferenceData, getReferenceDataStringId } from './TranslatedReferenceData';
+export { TranslatedSex } from './TranslatedSex';
 export { TranslatedText } from './TranslatedText';
