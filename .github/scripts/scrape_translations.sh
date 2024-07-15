898fda16a09850cfccbc77d65668ad1cdcd772a7
Daniel Nash <dannash200@gmail.com>
2024-07-15 12:01:34 +1200

    nass-1218: move to sh

---
+++ .github/scripts/scrape_translations.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get all translated string data from TranslatedText, getTranslatedString and enums.
+teoutput=$(node scripts/print-enum-translations.mjs)
+ttoutput=$(rg -PINU --multiline-dotall 'stringId="([^"]*)"\s*?fallback="([^"]*)' -or '"$1","$2"' \
+    | rg --multiline-dotall --passthru -U '\n\s*\b' -r '')
+gtoutput=$(rg -PINU --multiline-dotall "getTranslation\(\s*?[\"'](.*?)[\"'],.*?[\"'](.*?)[\"'].*?\)" -or '"$1","$2"')
+
+# Combine and sort
+data=$(printf "%s\n%s\n%s" "$ttoutput" "$gtoutput" "$teoutput" | sort -u)
+
+# Append header and write to csv file
+printf "stringId,fallback\n%s" "$data"
