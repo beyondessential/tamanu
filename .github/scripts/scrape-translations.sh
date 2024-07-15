3cc2e1d120627f994282f2261ae1d291c0604ef7
Daniel Nash <dannash200@gmail.com>
2024-07-16 11:39:01 +1200

    use tsx and remove build

--- .github/scripts/scrape-translations.sh
+++ .github/scripts/scrape-translations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Get all translated string data from TranslatedText, getTranslatedString and enums.
-teoutput=$(node scripts/print-enum-translations.mjs)
+teoutput=$(npx tsx packages/constants/scripts/printTranslatedEnums.ts)
 ttoutput=$(rg -PINU --multiline-dotall 'stringId="([^"]*)"\s*?fallback="([^"]*)' -or '"$1","$2"' \
     | rg --multiline-dotall --passthru -U '\n\s*\b' -r '')
 gtoutput=$(rg -PINU --multiline-dotall "getTranslation\(\s*?[\"'](.*?)[\"'],.*?[\"'](.*?)[\"'].*?\)" -or '"$1","$2"')
