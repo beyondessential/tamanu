f5e402a67d9c545edd3687bad2c344113d062c67
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-07-17 10:07:43 +1200

    remove refreshTranslations

--- packages/mobile/App/ui/components/Forms/SignInForm.tsx
+++ packages/mobile/App/ui/components/Forms/SignInForm.tsx
@@ -47,7 +47,7 @@ export const SignInForm: FunctionComponent<any> = ({ onError, onSuccess }) => {
   const [existingHost, setExistingHost] = useState('');
   const passwordRef = useRef(null);
   const { signIn } = useAuth();
-  const { getTranslation, refreshTranslations } = useTranslation();
+  const { getTranslation } = useTranslation();
 
   const handleSignIn = useCallback(
     async (values: SignInFormModelValues) => {
@@ -58,7 +58,6 @@ export const SignInForm: FunctionComponent<any> = ({ onError, onSuccess }) => {
           return;
         }
         await signIn(values);
-        refreshTranslations();
 
         onSuccess();
       } catch (error) {
