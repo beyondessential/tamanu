986ec11da034b8122c445a423ad2702bd8614733
Flynn <flynn.teh@gmail.com>
2024-07-17 12:29:02 +1200

    Revert "initial logic"
    This reverts commit abf48e12d33a640697b72457b2077565701f275a.

--- packages/mobile/App/ui/components/AutocompleteModal/AutocompleteModalField.tsx
+++ packages/mobile/App/ui/components/AutocompleteModal/AutocompleteModalField.tsx
@@ -11,7 +11,6 @@ import { RequiredIndicator } from '../RequiredIndicator';
 import { TranslatedTextElement, TranslatedText } from '../Translations/TranslatedText';
 import { SearchIcon } from '../Icons';
 import { ReadOnlyField } from '../ReadOnlyField/index';
-import { getReferenceDataStringId } from '~/ui/helpers/translation';
 
 interface AutocompleteModalFieldProps {
   value?: string;
@@ -42,8 +41,6 @@ export const AutocompleteModalField = ({
 }: AutocompleteModalFieldProps): ReactElement => {
   const navigation = useNavigation();
   const [label, setLabel] = useState(null);
-  const refDataType = suggester.options?.where?.type;
-
   const onPress = (selectedItem): void => {
     onChange(selectedItem.value);
     setLabel(selectedItem.label);
@@ -60,12 +57,7 @@ export const AutocompleteModalField = ({
     (async (): Promise<void> => {
       const data = await suggester.fetchCurrentOption(value);
       if (data) {
-        setLabel(
-          <TranslatedText
-            stringId={getReferenceDataStringId(refDataType, data.value)}
-            fallback={data.label}
-          />,
-        );
+        setLabel(data.label);
       } else {
         setLabel(null);
       }
--- packages/mobile/App/ui/helpers/translation.ts
+++
@@ -1,5 +0,0 @@
-import { ReferenceDataTranslationPrefix } from './constants';
-
-export const getReferenceDataStringId = (refDataType: string, value: string): string => {
-  return `${ReferenceDataTranslationPrefix}.${refDataType}.${value}`;
-};
