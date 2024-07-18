3298bc9f523a5b3d3ff1b0c272ef4af89d2c2961
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-07-18 19:12:36 -0400

    Abstract into function and also get rid of let variables

--- packages/web/app/api/queries/useVitals.js
+++ packages/web/app/api/queries/useVitals.js
@@ -15,6 +15,59 @@ function getSortedLogsByDate(logs) {
   return logs.slice().sort((a, b) => b.date.localeCompare(a.date));
 }
 
+function getDatesAndRecords(data, surveyData) {
+  if (data.length > 0 === false || Boolean(surveyData) === false) {
+    return { recordedDates: [], vitalsRecords: [] };
+  }
+
+  const recordedDates = Object.keys(
+    data.find(vital => vital.dataElementId === VITALS_DATA_ELEMENT_IDS.dateRecorded)
+      .records,
+  );
+
+  const elementIdToAnswer = data.reduce(
+    (dict, a) => ({ ...dict, [a.dataElementId]: a }),
+    {},
+  );
+
+  const vitalsRecords = surveyData.components
+    .filter(component => component.dataElementId !== VITALS_DATA_ELEMENT_IDS.dateRecorded)
+    // Show current components or ones that have historical data in them
+    .filter(
+      component =>
+        component.visibilityStatus === VISIBILITY_STATUSES.CURRENT ||
+        hasHistoricalData(elementIdToAnswer[component.dataElementId]),
+    )
+    .map(component => {
+      const { id, config, validationCriteria, dataElement } = component;
+      const { records = {} } = elementIdToAnswer[dataElement.id] || {};
+      const configs = {
+        validationCriteria: getConfigObject(id, validationCriteria),
+        config: getConfigObject(id, config),
+      };
+      return recordedDates.reduce(
+        (state, date) => ({
+          ...state,
+          [date]: {
+            component,
+            recordedDate: date,
+            answerId: records[date]?.id,
+            value: records[date]?.body,
+            historyLogs: getSortedLogsByDate(records[date]?.logs),
+            ...configs,
+          },
+        }),
+        {
+          dataElementId: dataElement.id,
+          value: dataElement.name,
+          ...configs,
+        },
+      );
+    });
+
+  return { recordedDates, vitalsRecords };
+}
+
 export const useVitals = encounterId => {
   const api = useApi();
   const vitalsQuery = useQuery(['encounterVitals', encounterId], () =>
@@ -28,57 +81,10 @@ export const useVitals = encounterId => {
   const surveyQuery = useVitalsSurveyQuery();
   const error = vitalsQuery.error || surveyQuery.error;
 
-  let vitalsRecords = [];
-  let recordedDates = [];
   const vitalsData = vitalsQuery?.data?.data || [];
   const surveyData = surveyQuery?.data;
 
-  if (vitalsData.length > 0 && surveyData) {
-    recordedDates = Object.keys(
-      vitalsData.find(vital => vital.dataElementId === VITALS_DATA_ELEMENT_IDS.dateRecorded)
-        .records,
-    );
-
-    const elementIdToAnswer = vitalsData.reduce(
-      (dict, a) => ({ ...dict, [a.dataElementId]: a }),
-      {},
-    );
-
-    vitalsRecords = surveyData.components
-      .filter(component => component.dataElementId !== VITALS_DATA_ELEMENT_IDS.dateRecorded)
-      // Show current components or ones that have historical data in them
-      .filter(
-        component =>
-          component.visibilityStatus === VISIBILITY_STATUSES.CURRENT ||
-          hasHistoricalData(elementIdToAnswer[component.dataElementId]),
-      )
-      .map(component => {
-        const { id, config, validationCriteria, dataElement } = component;
-        const { records = {} } = elementIdToAnswer[dataElement.id] || {};
-        const configs = {
-          validationCriteria: getConfigObject(id, validationCriteria),
-          config: getConfigObject(id, config),
-        };
-        return recordedDates.reduce(
-          (state, date) => ({
-            ...state,
-            [date]: {
-              component,
-              recordedDate: date,
-              answerId: records[date]?.id,
-              value: records[date]?.body,
-              historyLogs: getSortedLogsByDate(records[date]?.logs),
-              ...configs,
-            },
-          }),
-          {
-            dataElementId: dataElement.id,
-            value: dataElement.name,
-            ...configs,
-          },
-        );
-      });
-  }
+  const { recordedDates, vitalsRecords } = getDatesAndRecords(vitalsData, surveyData);
 
   return {
     ...vitalsQuery,
