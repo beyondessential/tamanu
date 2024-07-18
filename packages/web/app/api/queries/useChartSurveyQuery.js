8bfa207766b99f97f89e1f20cf2d88455dada5d3
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-07-18 19:33:47 -0400

    Create query hook to retrieve chart survey info

---
+++ packages/web/app/api/queries/useChartSurveyQuery.js
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { isErrorUnknownAllow404s, useApi } from '../index';
+
+export const useChartSurveyQuery = surveyId => {
+  const api = useApi();
+  const chartSurvey = useQuery(['chartSurvey'], () =>
+    api.get(`survey/${surveyId}`, {}, { isErrorUnknown: isErrorUnknownAllow404s }),
+  );
+
+  return chartSurvey;
+};
