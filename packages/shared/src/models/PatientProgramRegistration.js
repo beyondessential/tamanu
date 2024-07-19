2a49c99ff7a88854bde797187a0fcaf9f7a52e31
Chris Tran <chris@beyondessential.com.au>
2024-07-19 14:08:07 +1000

    sav-744: cleaned up

--- packages/shared/src/models/PatientProgramRegistration.js
+++ packages/shared/src/models/PatientProgramRegistration.js
@@ -135,7 +135,11 @@ export class PatientProgramRegistration extends Model {
     });
   }
 
-  static buildSyncFilter() {
-    return null; // syncs everywhere
+  static buildPatientSyncFilter(patientCount, markedForSyncPatientsTable) {
+    if (patientCount === 0) {
+      return null;
+    }
+
+    return `WHERE (patient_id IN (SELECT patient_id FROM ${markedForSyncPatientsTable}) AND updated_at_sync_tick > :since`;
   }
 }
