a95953330c804014c6603163213de3f9276cc32b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-07-18 21:24:33 -0400

    Add export to index

--- packages/web/app/api/queries/index.js
+++ packages/web/app/api/queries/index.js
@@ -12,3 +12,4 @@ export * from './useTranslations';
 export * from './useTranslationLanguages';
 export * from './useTelegramBotInfoQuery';
 export * from './useChartSurveys';
+export * from './useChartQuery';
