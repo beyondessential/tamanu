0ff5cd01c0e21881545b6dadee88a0e322aa0441
Daniel Nash <dannash200@gmail.com>
2024-07-19 13:46:55 +1200

    remove unused script

--- scripts/print-enum-translations.mjs
+++
@@ -1,4 +0,0 @@
-import { enumTranslations } from '@tamanu/constants/enumRegistry';
-
-// TODO: use tsnode so don't have to build before running and dont need extra script
-console.log(enumTranslations.reduce((acc, [key, value]) => `"${key}","${value}"\n${acc}`, ''));
