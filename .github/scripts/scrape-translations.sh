29ec960093567edaf8e19aa970a85b009828f906
Daniel Nash <dannash200@gmail.com>
2024-07-22 09:36:09 +1200

    another change

--- .github/scripts/scrape-translations.sh
+++ .github/scripts/scrape-translations.sh
@@ -7,9 +7,9 @@ gtregex="getTranslation\(\s*?[\"'](.*?)[\"'],.*?[\"'](.*?)[\"'].*?\)"
 
 # Get all translated string data from TranslatedText, getTranslatedString and enums.
 teoutput=$(npx tsx packages/constants/scripts/printTranslatedEnums.ts)
-ttoutput=$(rg -PINU --multiline-dotall $ttregex -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" ./packages \
+ttoutput=$(rg -PINU --multiline-dotall "$ttregex" -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" ./packages \
     | rg --multiline-dotall --passthru -U '\n\s*\b' -r '' )
-gtoutput=$(rg -PINU --multiline-dotall $gtregex -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" ./packages)
+gtoutput=$(rg -PINU --multiline-dotall "$gtregex" -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" ./packages)
 
 # Combine and sort
 data=$(printf "%s\n%s\n%s" "$ttoutput" "$gtoutput" "$teoutput" | sort -u)
