7579da73601529d2836c7d8f23368cad6a7af689
Huy-Tran-Huu-Le <huy@bes.au>
2024-07-22 17:34:12 +0700

    add clinicianId field to database schema on mobile

---
+++ packages/mobile/App/migrations/1721636585000-addClinicianToEncounterDiagnosis.ts
@@ -0,0 +1,36 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from 'typeorm';
+import { getTable } from './utils/queryRunner';
+
+const TABLE_NAME = 'diagnosis';
+const COLUMN_NAME = 'clinicianId';
+
+export class addClinicianToEncounterDiagnosis1721636585000 implements MigrationInterface {
+  async up(queryRunner: QueryRunner): Promise<void> {
+    const tableObject = await getTable(queryRunner, TABLE_NAME);
+    await queryRunner.addColumn(
+      tableObject,
+      new TableColumn({
+        name: COLUMN_NAME,
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+    await queryRunner.createForeignKey(
+      tableObject,
+      new TableForeignKey({
+        columnNames: [COLUMN_NAME],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'user',
+      }),
+    );
+  }
+
+  async down(queryRunner: QueryRunner): Promise<void> {
+    const tableObject = await getTable(queryRunner, TABLE_NAME);
+    const foreignKey = tableObject.foreignKeys.find(
+      fk => fk.columnNames.indexOf(COLUMN_NAME) !== 0,
+    );
+    await queryRunner.dropForeignKey(tableObject, foreignKey);
+    await queryRunner.dropColumn(tableObject, COLUMN_NAME);
+  }
+}
