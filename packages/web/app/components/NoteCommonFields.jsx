24208090494b7ae5c68ff3a955a509480144d00e
Daniel Nash <dannash200@gmail.com>
2024-07-22 14:22:10 +1200

    whats the problem :( octal wat?

--- packages/web/app/components/NoteCommonFields.jsx
+++ packages/web/app/components/NoteCommonFields.jsx
@@ -163,13 +163,13 @@ export const NoteTypeField = ({ required, noteTypeCountByType, onChange }) => (
     enumValues={NOTE_TYPE_LABELS}
     transformOptions={types =>
       types
-        .filter(x => !x.hideFromDropdown)
-        .map(x => ({
-          ...x,
+        .filter(option => !option.hideFromDropdown)
+        .map(option => ({
+          ...option,
           isDisabled:
             noteTypeCountByType &&
-            x.value === NOTE_TYPES.TREATMENT_PLAN &&
-            !!noteTypeCountByType[x.value],
+            option.value === NOTE_TYPES.TREATMENT_PLAN &&
+            !!noteTypeCountByType[option.value],
         }))
     }
     formatOptionLabel={option => renderOptionLabel(option, noteTypeCountByType)}
