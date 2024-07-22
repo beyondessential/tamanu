fb80d7f77908334a7741692b438d50147de60647
Chris Tran <chris@beyondessential.com.au>
2024-07-22 14:44:41 +1000

    sav-744: fixed tests

--- packages/shared/src/models/PatientProgramRegistration.js
+++ packages/shared/src/models/PatientProgramRegistration.js
@@ -140,6 +140,6 @@ export class PatientProgramRegistration extends Model {
       return null;
     }
 
-    return `WHERE (patient_id IN (SELECT patient_id FROM ${markedForSyncPatientsTable}) AND updated_at_sync_tick > :since`;
+    return `WHERE patient_id IN (SELECT patient_id FROM ${markedForSyncPatientsTable}) AND updated_at_sync_tick > :since`;
   }
 }
