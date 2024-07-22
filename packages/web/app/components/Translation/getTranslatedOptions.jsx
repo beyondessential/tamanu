0a3f0558d323cd5f90f87059c0ed10c25551eee7
Daniel Nash <dannash200@gmail.com>
2024-07-22 13:52:28 +1200

    nass-1194: fix importz?

--- packages/web/app/components/Translation/getTranslatedOptions.jsx
+++ packages/web/app/components/Translation/getTranslatedOptions.jsx
@@ -1,7 +1,7 @@
 import { TranslatedText } from './TranslatedText';
 import { camelCase } from 'lodash';
 import React from 'react';
-import { isRegisteredEnum } from '@tamanu/shared';
+import { isRegisteredEnum } from '@tamanu/shared/utilsenumRegistry';
 import { IS_DEVELOPMENT } from '../../utils/env';
 
 export const enforceRegisteredEnum = (prefix, options) => {
