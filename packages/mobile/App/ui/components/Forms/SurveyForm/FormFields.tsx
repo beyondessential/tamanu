03fff6560628404c1882bfb82a678917f5d75186
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-07-29 08:43:02 +1200

    Revert "remove redundant explicit typing"
    This reverts commit 6ccdb7ebd5a52caab183837061393fcb1454c96b.

--- packages/mobile/App/ui/components/Forms/SurveyForm/FormFields.tsx
+++ packages/mobile/App/ui/components/Forms/SurveyForm/FormFields.tsx
@@ -143,7 +143,7 @@ export const FormFields = ({
     />
   );
 
-  const submitScreen = async (handleSubmit: () => Promise<void>) => {
+  const submitScreen = async (handleSubmit: () => Promise<void>): Promise<void> => {
     // Validate form on screen before moving to the next one
     const formErrors = await validateForm();
 
@@ -166,13 +166,13 @@ export const FormFields = ({
     }
   };
 
-  const onNavigateNext = async () => {
+  const onNavigateNext = async (): Promise<void> => {
     await submitScreen(async () => {
       setCurrentScreenIndex(Math.min(currentScreenIndex + 1, maxIndex));
     });
   };
 
-  const onSubmit = async () => {
+  const onSubmit = async (): Promise<void> => {
     await submitScreen(async () => {
       await submitForm();
       resetForm();
