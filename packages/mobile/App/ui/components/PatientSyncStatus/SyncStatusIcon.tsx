be087e85d2d0abff1461619350eb907284050def
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-07-28 18:38:41 -0400

    Remove text below sync button

--- packages/mobile/App/ui/components/PatientSyncStatus/SyncStatusIcon.tsx
+++ packages/mobile/App/ui/components/PatientSyncStatus/SyncStatusIcon.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import { Orientation, screenPercentageToDP } from '~/ui/helpers/screen';
-import { ColumnView, StyledText } from '~/ui/styled/common';
 import { NotMarkedForSyncIcon, MarkedForSyncIcon } from '~/ui/components/Icons';
-import { TranslatedText } from '~/ui/components/Translations/TranslatedText';
+import { StyledView } from '~/ui/styled/common';
 
 export const SyncStatusIcon = ({ isMarkedForSync }) => {
   const CurrentStatusIcon = isMarkedForSync ? MarkedForSyncIcon : NotMarkedForSyncIcon;
   return (
-    <ColumnView alignItems="center">
+    <StyledView
+    >
       <CurrentStatusIcon
         size={screenPercentageToDP(4.86, Orientation.Height)}
         fill="white"
       />
-      <StyledText
-        fontSize={10}
-        color="white"
-        marginTop={screenPercentageToDP(-0.6, Orientation.Height)}
-      >
-        <TranslatedText stringId="general.action.syncData" fallback="Sync Data" />
-      </StyledText>
-    </ColumnView>
+    </StyledView>
   );
 }
