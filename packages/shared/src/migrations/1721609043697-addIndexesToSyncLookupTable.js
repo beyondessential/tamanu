d400bd68facac5d53d2e3eeb79c282c14d315305
Chris Tran <chris@beyondessential.com.au>
2024-07-30 08:56:06 +1000

    sav-744: tweaked index

--- packages/shared/src/migrations/1721609043697-addIndexesToSyncLookupTable.js
+++ packages/shared/src/migrations/1721609043697-addIndexesToSyncLookupTable.js
@@ -1,28 +1,9 @@
 export async function up(query) {
   await query.addIndex('sync_lookup', {
-    fields: ['updated_at_sync_tick'],
-  });
-  await query.addIndex('sync_lookup', {
-    fields: ['record_id'],
-  });
-  await query.addIndex('sync_lookup', {
-    fields: ['patient_id'],
-  });
-  await query.addIndex('sync_lookup', {
-    fields: ['facility_id'],
-  });
-  await query.addIndex('sync_lookup', {
-    fields: ['encounter_id'],
-  });
-  await query.addIndex('sync_lookup', {
-    fields: ['is_lab_request'],
+    fields: ['updated_at_sync_tick', 'record_id', 'patient_id', 'facility_id'],
   });
 }
 
 export async function down(query) {
-  await query.removeIndex('sync_lookup', 'updated_at_sync_tick');
-  await query.removeIndex('sync_lookup', 'record_id');
-  await query.removeIndex('sync_lookup', 'patient_id');
-  await query.removeIndex('sync_lookup', 'facility_id');
-  await query.removeIndex('sync_lookup', 'is_lab_request');
+  await query.removeIndex('sync_lookup', 'sync_lookup_updated_at_sync_tick_record_id_patient_id_facility_id');
 }
