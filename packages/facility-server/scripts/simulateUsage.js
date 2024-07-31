4a36639e2bbd0efb4eb50d92a13241cc833b4a33
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-07-31 13:45:35 -0400

    Add mindful comment

--- packages/facility-server/scripts/simulateUsage.js
+++ packages/facility-server/scripts/simulateUsage.js
@@ -163,6 +163,8 @@ async function simulateUsage(models, sequelize, hours = 1) {
   console.log(JSON.stringify(actionsTaken));
 }
 
+// IMPORTANT NOTE: YOU SHOULD NEVER RUN THIS IN A PRODUCTION ENVIRONMENT
+// AS IT WILL INTRODUCE MOCK RECORDS INTO THE DATABASE.
 // Usage (note that if you -await- the function you WILL NOT be able to cancel it until it's done!):
 // const { simulateUsage } = require('@tamanu/facility-server/scripts/simulateUsage.js');
 // // This will run for one hour (well, a bit longer but for simplicity sake, around that)
