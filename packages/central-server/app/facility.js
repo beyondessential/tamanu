e5ced569f611c39e05f92eb2b7f084ed1b3a20cf
Daniel Nash <dannash200@gmail.com>
2024-08-06 09:53:44 +1200

    nass-1304: fix facility tests

--- packages/central-server/app/facility.js
+++ packages/central-server/app/facility.js
@@ -7,8 +7,11 @@ export const facilityRoutes = express.Router();
 facilityRoutes.get(
   '/$',
   asyncHandler(async (req, res) => {
-    const allowed = await req.user.allowedFacilityIds();
-    const facilities = await req.store.models.Facility.findAll({
+    const { store, user } = req;
+    const { User, Facility } = store.models;
+    const userEntity = await User.findByPk(user.id);
+    const allowed = await userEntity.allowedFacilityIds();
+    const facilities = await Facility.findAll({
       where: allowed === CAN_ACCESS_ALL_FACILITIES ? {} : { id: allowed },
     });
 
