0b82331c7728e73847c11a1f8824b917990f7ff5
Daniel Nash <dannash200@gmail.com>
2024-08-08 10:42:24 +1200

    nass-1304: make sure that legacy facility id is not null

--- packages/shared/src/migrations/1722471657455-updateColumnFacilityIdsInSyncQueuedDevices.js
+++ packages/shared/src/migrations/1722471657455-updateColumnFacilityIdsInSyncQueuedDevices.js
@@ -1,4 +1,8 @@
 export async function up(query) {
+  await query.sequelize.query(`
+    ALTER TABLE sync_queued_devices
+    ALTER COLUMN facility_id DROP NOT NULL;
+  `);
   await query.renameColumn('sync_queued_devices', 'facility_id', 'facility_id_legacy');
   await query.sequelize.query(`
     ALTER TABLE sync_queued_devices
@@ -13,5 +17,9 @@ export async function up(query) {
 
 export async function down(query) {
   await query.renameColumn('sync_queued_devices', 'facility_id_legacy', 'facility_id');
+  await query.sequelize.query(`
+    ALTER TABLE sync_queued_devices
+    ALTER COLUMN facility_id SET NOT NULL;
+  `);
   await query.removeColumn('sync_queued_devices', 'facility_ids');
 }
