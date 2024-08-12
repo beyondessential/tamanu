e71847b5beb4395ea73c43f3780a63ec653ec01d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-13 10:07:42 +1200

    Update auth.js

--- packages/facility-server/app/middleware/auth.js
+++ packages/facility-server/app/middleware/auth.js
@@ -191,11 +191,8 @@ export async function setFacilityHandler(req, res, next) {
     // Run after auth middleware, requires valid token but no other permission
     req.flagPermissionChecked();
 
-    const restrictUsersToFacilities = req.settings[facilityId]?.get(
-      'auth.restrictUsersToFacilities',
-    );
     const hasAccess = await user.canAccessFacility(facilityId);
-    if (restrictUsersToFacilities && !hasAccess) {
+    if (!hasAccess) {
       throw new BadAuthenticationError('User does not have access to this facility');
     }
     const token = await buildToken(user, facilityId);
