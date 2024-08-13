37e137f8f76e1dd8c9dbfb4ec40c0ff9510e4ac3
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2024-08-13 01:22:06 -0400

    feat(imagingRequest): SAV-796: Make "areas to be imaged" field mandatory (hotfix 2.9.4) (#6262)
    * Make multitext required
    
    * Make free text required
    
    * Add validation to the schema
    
    * Reuse the same translation
    
    * SAV-796: Added comment
    
    * Blank commit to trigger CI
    
    ---------
    
    Co-authored-by: Rohan Port <rohan@bes.au>

--- packages/web/app/forms/ImagingRequestForm.jsx
+++ packages/web/app/forms/ImagingRequestForm.jsx
@@ -107,6 +107,7 @@ export const ImagingRequestForm = React.memo(
     const examinerLabel = examiner.displayName;
     const encounterLabel = getEncounterLabel(encounter);
     const { getAreasForImagingType } = useImagingRequestAreas();
+    const requiredValidationMessage = getTranslation('validation.required.inline', '*Required');
     return (
       <Form
         onSubmit={onSubmit}
@@ -117,11 +118,29 @@ export const ImagingRequestForm = React.memo(
         }}
         formType={editedObject ? FORM_TYPES.EDIT_FORM : FORM_TYPES.CREATE_FORM}
         validationSchema={yup.object().shape({
-          requestedById: foreignKey(getTranslation('validation.required.inline', '*Required')),
-          requestedDate: yup
-            .date()
-            .required(getTranslation('validation.required.inline', '*Required')),
-          imagingType: foreignKey(getTranslation('validation.required.inline', '*Required')),
+          requestedById: foreignKey(),
+          requestedDate: yup.date().required(requiredValidationMessage),
+          imagingType: foreignKey(requiredValidationMessage),
+          areas: yup.string().when('imagingType', {
+            is: imagingType => {
+              const imagingAreas = getAreasForImagingType(imagingType);
+              return imagingAreas.length > 0;
+            },
+            then: yup
+              .string()
+              .min(3, requiredValidationMessage) // Empty input is '[]', so validating that it's got at least one value in the array
+              .required(requiredValidationMessage),
+          }),
+          areaNote: yup.string().when('imagingType', {
+            is: imagingType => {
+              const imagingAreas = getAreasForImagingType(imagingType);
+              return imagingAreas.length === 0;
+            },
+            then: yup
+              .string()
+              .trim()
+              .required(requiredValidationMessage),
+          }),
         })}
         showInlineErrorsOnly
         render={({ submitForm, values }) => {
@@ -231,6 +250,7 @@ export const ImagingRequestForm = React.memo(
                   }
                   component={MultiselectField}
                   prefix="imaging.property.area"
+                  required
                 />
               ) : (
                 <Field
@@ -245,6 +265,7 @@ export const ImagingRequestForm = React.memo(
                   multiline
                   style={{ gridColumn: '1 / -1' }}
                   minRows={3}
+                  required
                 />
               )}
               <Field
