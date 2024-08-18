c3e0898262c9fefd81ec8970a6921a012971c96c
Daniel Nash <dannash200@gmail.com>
2024-08-19 11:41:10 +1200

    nass-1329: add triage category to settings defaults

--- packages/settings/src/defaults/global.ts
+++ packages/settings/src/defaults/global.ts
@@ -88,6 +88,47 @@ export const globalSettings = {
       ],
     },
   },
+  triageCategories: {
+    name: 'Triage categories',
+    description: '_',
+    schema: yup
+      .array()
+      .of(
+        yup.object({
+          level: yup.number().required(),
+          label: yup.string().required(),
+          color: yup.string().required(),
+        }),
+      )
+      .required(),
+    default: [
+      {
+        level: 1,
+        label: 'Emergency',
+        color: '#F76853',
+      },
+      {
+        level: 2,
+        label: 'Very Urgent',
+        color: '#F17F16',
+      },
+      {
+        level: 3,
+        label: 'Urgent',
+        color: '#FFCC24',
+      },
+      {
+        level: 4,
+        label: 'Non-urgent',
+        color: '#47CA80',
+      },
+      {
+        level: 5,
+        label: 'Deceased',
+        color: '#67A6E3',
+      },
+    ],
+  },
 };
 
 // export const globalDefaults = extractDefaults(globalSettings);
