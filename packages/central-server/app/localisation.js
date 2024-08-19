d7c19243b70abef86ce3a50d0edc27a63d3481f8
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-19 14:59:14 +1200

    mandatoryVitalEditReason

--- packages/central-server/app/localisation.js
+++ packages/central-server/app/localisation.js
@@ -594,7 +594,6 @@ const rootLocalisationSchema = yup
           enabled: yup.boolean().required(),
           interval: yup.number().required(),
         }),
-        mandatoryVitalEditReason: yup.boolean().required(),
         enableVitalEdit: yup.boolean().required(),
       })
       .required()
