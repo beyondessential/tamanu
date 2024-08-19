9c72849fb625bd535d0146cf77ebae5577720f2e
Daniel Nash <dannash200@gmail.com>
2024-08-19 12:08:47 +1200

    nass-1329: fix

--- packages/settings/src/schema/utils.ts
+++ packages/settings/src/schema/utils.ts
@@ -2,9 +2,9 @@ import { mapValues, isObject } from 'lodash';
 import * as yup from 'yup';
 
 export const extractDefaults = settings => {
-  return mapValues(settings, (value: { schema: yup.SchemaOf<any>; default: any }) => {
+  return mapValues(settings, (value: { schema: yup.SchemaOf<any>; defaultValue: any }) => {
     if (isObject(value) && value.schema) {
-      return value.default;
+      return value.defaultValue;
     } else if (isObject(value)) {
       return extractDefaults(value);
     }
