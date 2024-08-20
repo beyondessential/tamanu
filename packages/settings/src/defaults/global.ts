e6aff13cfde4b0c3a05e11921d6fb5d894fbaa1f
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-20 15:59:48 +1200

    Update global.ts

--- packages/settings/src/defaults/global.ts
+++ packages/settings/src/defaults/global.ts
@@ -5,117 +5,140 @@ import { extractDefaults } from './utils';
 export const globalSettings = {
   features: {
     mandateSpecimenType: {
+      name: 'Mandate specimen type',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     enableVaccineConsent: {
+      name: 'Enable vaccine consent',
       description: 'Display a required vaccine consent box on the vaccine given form',
       schema: yup.boolean().required(),
       default: true,
     },
     filterDischargeDispositions: {
+      name: 'Filter discharge dispositions',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     editPatientDetailsOnMobile: {
+      name: 'Edit patient details on mobile',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     quickPatientGenerator: {
+      name: 'Quick patient generator',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     enableInvoicing: {
+      name: 'Enable invoicing',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     registerNewPatient: {
+      name: 'Register new patient',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     hideOtherSex: {
+      name: 'Hide other sex',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     enablePatientDeaths: {
+      name: 'Enable patient deaths',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     enableNoteBackdating: {
+      name: 'Enable note backdating',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     enableCovidClearanceCertificate: {
+      name: 'Enable covid clearance certificate',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     editPatientDisplayId: {
+      name: 'Edit patient displayId',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     enablePatientInsurer: {
+      name: 'Enable patient insurer',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     patientPlannedMove: {
+      name: 'Patient planned move',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     onlyAllowLabPanels: {
+      name: 'Only allow lab panels',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     displayProcedureCodesInDischargeSummary: {
+      name: 'Display procedure codes in discarge summary',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     displayIcd10CodesInDischargeSummary: {
+      name: 'Display icd10 codes in discharge summary',
       description: '_',
       schema: yup.boolean().required(),
       default: true,
     },
     mandatoryVitalEditReason: {
+      name: 'Mandatory vital edit reason',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     enableVitalEdit: {
+      name: 'Enable vital edit',
       description: '_',
       schema: yup.boolean().required(),
       default: false,
     },
     idleTimeout: {
       enabled: {
+        name: 'Enabled',
         description: '_',
         schema: yup.boolean().required(),
         default: true,
       },
       timeoutDuration: {
+        name: 'Timeout duration',
         description: '_',
         schema: yup.number().required(),
         default: 600, // In Seconds
       },
       warningPromptDuration: {
+        name: 'Warning prompt duration',
         description: '_',
         schema: yup.number().required(),
         default: 30, // In Seconds
       },
       refreshInterval: {
+        name: 'Refresh interval',
         description: '_',
         schema: yup.number().required(),
         default: 150, // In Seconds
@@ -123,11 +146,13 @@ export const globalSettings = {
     },
     tableAutoRefresh: {
       enabled: {
+        name: 'Enabled',
         description: '_',
         schema: yup.boolean().required(),
         default: true,
       },
       interval: {
+        name: 'Interval',
         description: '_',
         schema: yup.number().required(),
         default: 300, // In Seconds
