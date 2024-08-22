0daaa444f1d99b88606468e6feafec6a3716d029
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-22 14:36:39 +1200

    Update useLayoutComponents.jsx

--- packages/web/app/forms/PatientDetailsForm/useLayoutComponents.jsx
+++ packages/web/app/forms/PatientDetailsForm/useLayoutComponents.jsx
@@ -1,22 +1,13 @@
-import { PATIENT_DETAIL_LAYOUTS } from '@tamanu/constants';
-
 import {
   GenericPatientFieldLayout,
   GenericPrimaryDetailsLayout,
   GenericSecondaryDetailsLayout,
 } from './layouts/generic/GenericLayout';
 
-// This file is leftover from a hardcoded patient details layout implementation.
-
-const LAYOUT_COMPONENTS = {
-  [PATIENT_DETAIL_LAYOUTS.GENERIC]: {
+export const useLayoutComponents = () => {
+  return {
     PrimaryDetails: GenericPrimaryDetailsLayout,
     SecondaryDetails: GenericSecondaryDetailsLayout,
     PatientFields: GenericPatientFieldLayout,
-  },
-};
-
-export const useLayoutComponents = () => {
-  const layout = PATIENT_DETAIL_LAYOUTS.GENERIC;
-  return LAYOUT_COMPONENTS[layout] ?? LAYOUT_COMPONENTS[PATIENT_DETAIL_LAYOUTS.GENERIC];
+  }
 };
