4015437d15ae76bee751d87641c8c19eb8fec040
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2024-08-22 14:04:10 +1200

    Update packages/web/app/views/patients/panes/EncounterInfoPane.jsx
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>

--- packages/web/app/views/patients/panes/EncounterInfoPane.jsx
+++ packages/web/app/views/patients/panes/EncounterInfoPane.jsx
@@ -169,7 +169,7 @@ export const EncounterInfoPane = React.memo(({ encounter, getSetting, patientBil
             {encounter.endDate && (
               <>
                 <CardLabel>
-                  {' - '}
+                  {' – '} // en dash to indicate range
                   <TranslatedText
                     stringId="encounter.summary.dischargeDate.label"
                     fallback="Discharge date"
