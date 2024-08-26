671ce32e3bb83b96cc7d9a942bed764a00de4cf5
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-27 11:06:12 +1200

    Update layouts.ts

--- packages/settings/src/schema/global-settings-properties/layouts.ts
+++ packages/settings/src/schema/global-settings-properties/layouts.ts
@@ -2,21 +2,15 @@ import * as yup from 'yup';
 
 export const unhideableLayoutModuleProperties = {
   sortPriority: {
-    description: 'Modules are ordered based on this value from lowest to highest',
+    description: 'Customise the layout of modules',
     schema: yup.number(),
     defaultValue: -100,
-  },
-//   TODO: possibly just remove this
-  hidden: {
-    description: 'Hide this module from the ui',
-    schema: yup.boolean().oneOf([false], 'unhideable tabs must not be hidden'),
-    defaultValue: false,
   },
 };
 
 export const layoutModuleProperties = {
   sortPriority: {
-    description: 'Modules are ordered based on this value from lowest to highest',
+    description: 'Customise the layout of modules',
     schema: yup.number(),
     defaultValue: 0,
   },
