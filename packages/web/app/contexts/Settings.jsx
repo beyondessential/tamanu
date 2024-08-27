27172549058576db6af71e2a22c0c2efccbb98c4
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-08-27 12:01:33 +1200

    error if `useSettings` called outside provider

--- packages/web/app/contexts/Settings.jsx
+++ packages/web/app/contexts/Settings.jsx
@@ -1,15 +1,19 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { get } from 'lodash';
 
-const SettingsContext = React.createContext({
-  getSetting: () => {},
-});
+const SettingsContext = React.createContext(null);
 
-export const useSettings = () => useContext(SettingsContext);
+export const useSettings = () => {
+  const context = useContext(SettingsContext);
+  if (!context) {
+    throw new Error('useSettings has been called outside a SettingsProvider.');
+  }
+  return context;
+};
 
 export const SettingsProvider = ({ children }) => {
-  const [settings, setSettings] = useState({});
+  const [settings, setSettings] = useState(null);
   const reduxSettings = useSelector(state => state.auth.settings);
 
   useEffect(() => {
