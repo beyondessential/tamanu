0340f2d5262104c9b346f46366af1d68c99a76cf
Félix Saparelli <felix@bes.au>
2024-08-28 16:48:16 +1200

    feat(cli): add --topics to restrict fhir worker to certain topics

--- packages/central-server/app/subCommands/startFhirWorker.js
+++ packages/central-server/app/subCommands/startFhirWorker.js
@@ -6,13 +6,20 @@ import { ApplicationContext } from '../ApplicationContext';
 import { startFhirWorkerTasks } from '../tasks';
 import pkg from '../../package.json';
 
-export const startFhirWorker = async ({ skipMigrationCheck }) => {
+export const startFhirWorker = async ({ skipMigrationCheck, topics }) => {
   log.info(`Starting Central FHIR worker version ${pkg.version}`);
 
   const context = await new ApplicationContext().init();
   await context.store.sequelize.assertUpToDate({ skipMigrationCheck });
 
-  const worker = await startFhirWorkerTasks(context);
+  if (!topics || topics === 'all') {
+    topics = null;
+  } else {
+    topics = topics.split(/,+\s*/).filter(Boolean);
+    log.info(`FHIR worker restricted to topics: ${topics.join(', ')}`);
+  }
+
+  const worker = await startFhirWorkerTasks({ store: context.store, topics });
 
   for (const sig of ['SIGINT', 'SIGTERM']) {
     process.once(sig, async () => {
@@ -28,4 +35,5 @@ export const startFhirWorker = async ({ skipMigrationCheck }) => {
 export const startFhirWorkerCommand = new Command('startFhirWorker')
   .description('Start the Tamanu Central FHIR worker')
   .option('--skipMigrationCheck', 'skip the migration check on startup')
+  .option('--topics <topics>', 'comma-separated topics to work on, defaults to all')
   .action(startFhirWorker);
