cfa81e8c24c5a14b0ac7f30a47a9e8589b021648
namhoangbes <160203225+namhoangbes@users.noreply.github.com>
2024-08-30 10:22:06 +0700

    feat(tasking): EPI-937: Convert mark task not completed to bulk action (#6371)
    * feat(tasking): convert mark task not completed to bulk action
    
    * misc(taskiing): update comments

--- packages/facility-server/app/routes/apiv1/task/tasks.js
+++ packages/facility-server/app/routes/apiv1/task/tasks.js
@@ -51,35 +51,53 @@ taskRoutes.post(
 
 /**
  * Mark task as not completed
- * Only tasks in TODO status can be marked as not completed
+ * Only tasks in TODO & COMPLETED status can be marked as not completed
  */
 const taskNonCompletionInputSchema = z.object({
+  taskIds: z
+    .string()
+    .uuid()
+    .array()
+    .length(1),
   notCompletedBy: z.string().uuid(),
   notCompletedTime: z.string().datetime(),
   notCompletedReasonId: z.string().optional(),
 });
 taskRoutes.put(
-  '/:taskId/notCompleted',
+  '/notCompleted',
   asyncHandler(async (req, res) => {
     req.checkPermission('write', 'Task');
+    const { taskIds, ...notCompletedInfo } = await taskNonCompletionInputSchema.parseAsync(
+      req.body,
+    );
+
+    //validate not completed reason
+    if (notCompletedInfo.notCompletedReasonId) {
+      const notCompleteReason = await req.models.ReferenceData.findByPk(
+        notCompletedInfo.notCompletedReasonId,
+        {
+          where: { type: REFERENCE_TYPES.TASK_NOT_COMPLETED_REASON },
+        },
+      );
+      if (!notCompleteReason) throw new NotFoundError('Not completed reason not found');
+    }
+
     //validate task
-    const taskId = req.params.taskId;
-    const task = await req.models.Task.findByPk(taskId, {
+    const tasks = await req.models.Task.findAll({
+      where: { id: { [Op.in]: taskIds } },
       attributes: ['id', 'status'],
     });
-    if (!task) throw new NotFoundError('Task not found');
-    if (task.status !== TASK_STATUSES.TODO) throw new ForbiddenError('Task is not in TODO status');
+    if (!tasks?.length) throw new NotFoundError('No tasks not found');
 
-    //validate input
-    const input = await taskNonCompletionInputSchema.parseAsync(req.body);
-    const notCompleteReason = await req.models.ReferenceData.findByPk(input.notCompletedReasonId, {
-      where: { type: REFERENCE_TYPES.TASK_NOT_COMPLETED_REASON },
-    });
-    if (!notCompleteReason) throw new NotFoundError('Not completed reason not found');
+    const allowedStatuses = [TASK_STATUSES.TODO, TASK_STATUSES.COMPLETED];
+    if (!tasks.every(task => allowedStatuses.includes(task.status))) {
+      throw new ForbiddenError('Task is not in TODO or COMPLETED status');
+    }
 
+    //update tasks
     await req.models.Task.update(
-      { ...input, status: TASK_STATUSES.NON_COMPLETED },
-      { where: { id: taskId } },
+      { ...notCompletedInfo, status: TASK_STATUSES.NON_COMPLETED },
+      { where: { id: { [Op.in]: taskIds } } },
     );
     res.json();
   }),
