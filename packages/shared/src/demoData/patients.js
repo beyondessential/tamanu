e8d06b7e94aed682e4947a9084c2edd56d43b00c
Daniel Nash <dannash200@gmail.com>
2024-08-30 14:29:35 +1200

    nass-1322: change

--- packages/shared/src/demoData/patients.js
+++ packages/shared/src/demoData/patients.js
@@ -147,7 +147,7 @@ export async function createDummyEncounterDiagnosis(models, overrides = {}) {
   const date = toDateTimeString(new Date(new Date().getTime() - duration));
   return {
     date,
-    certainty: chance.bool() ? 'suspected' : 'confirmed',
+    certainty: chance.pickone(['suspected', 'confirmed']),
     isPrimary: chance.bool(),
     diagnosisId: await randomReferenceId(models, 'diagnosis'),
     ...overrides,
