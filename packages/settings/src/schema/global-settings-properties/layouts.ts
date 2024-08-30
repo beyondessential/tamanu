e97eabc58ae1d64b9755761c590d5b9d462ee018
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-08-30 16:11:13 +1200

    Update layouts.ts

--- packages/settings/src/schema/global-settings-properties/layouts.ts
+++ packages/settings/src/schema/global-settings-properties/layouts.ts
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 export const unhideableLayoutModuleProperties = {
   sortPriority: {
     description: 'Customise the layout of modules',
-    schema: yup.number(),
+    type: yup.number(),
     defaultValue: -100,
   },
 };
@@ -11,12 +11,12 @@ export const unhideableLayoutModuleProperties = {
 export const layoutModuleProperties = {
   sortPriority: {
     description: 'Customise the layout of modules',
-    schema: yup.number(),
+    type: yup.number(),
     defaultValue: 0,
   },
   hidden: {
     description: 'Hide this module from the ui',
-    schema: yup.boolean(),
+    type: yup.boolean(),
     defaultValue: false,
   },
 };
