a8529ecbd153e75bf50377d7ed0d72bcf033dbb1
namhoangbes <160203225+namhoangbes@users.noreply.github.com>
2024-08-30 10:21:12 +0700

    feat(tasking): EPI-936: Convert mark task completed to bulk action (#6372)
    feat(tasking): convert mark task completion to bulk action

--- packages/facility-server/app/routes/apiv1/task/tasks.js
+++ packages/facility-server/app/routes/apiv1/task/tasks.js
@@ -3,35 +3,47 @@ import asyncHandler from 'express-async-handler';
 import { ForbiddenError, NotFoundError } from '@tamanu/shared/errors';
 import { REFERENCE_TYPES, TASK_STATUSES } from '@tamanu/constants';
 import { z } from 'zod';
+import { Op } from 'sequelize';
 
 const taskRoutes = express.Router();
 export { taskRoutes as tasks };
 
 /**
  * Mark task as completed
- * Only tasks in TODO status can be marked as completed
+ * Only tasks in TODO & NON_COMPLETED status can be marked as completed
  */
 const taskCompletionInputSchema = z.object({
+  taskIds: z
+    .string()
+    .uuid()
+    .array()
+    .length(1),
   completedBy: z.string().uuid(),
   completedTime: z.string().datetime(),
   completedNote: z.string().optional(),
 });
-taskRoutes.put(
-  '/:taskId/completed',
+taskRoutes.post(
+  '/completed',
   asyncHandler(async (req, res) => {
     req.checkPermission('write', 'Task');
-    const taskId = req.params.taskId;
-    const task = await req.models.Task.findByPk(taskId, {
+    const { taskIds, ...completedInfo } = await taskCompletionInputSchema.parseAsync(req.body);
+
+    //validate task
+    const tasks = await req.models.Task.findAll({
+      where: { id: { [Op.in]: taskIds } },
       attributes: ['id', 'status'],
     });
-    if (!task) throw new NotFoundError('Task not found');
-    if (task.status !== TASK_STATUSES.TODO) throw new ForbiddenError('Task is not in TODO status');
+    if (!tasks?.length) throw new NotFoundError('No tasks not found');
 
-    const input = await taskCompletionInputSchema.parseAsync(req.body);
+    const allowedStatuses = [TASK_STATUSES.TODO, TASK_STATUSES.NON_COMPLETED];
+    if (!tasks.every(task => allowedStatuses.includes(task.status))) {
+      throw new ForbiddenError('Tasks are not in TODO or NON_COMPLETED status');
+    }
 
+    //update task
     await req.models.Task.update(
-      { ...input, status: TASK_STATUSES.COMPLETED },
-      { where: { id: taskId } },
+      { ...completedInfo, status: TASK_STATUSES.COMPLETED },
+      { where: { id: { [Op.in]: taskIds } } },
     );
     res.json();
   }),
