0527b67f37a51ee65baecf3d1519ce020992997a
Félix Saparelli <felix@bes.au>
2024-08-30 13:13:20 +1200

    tweak(container): remove OTEL variables read from previously-removed /meta path

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,14 +3,6 @@
 ### This expects to be run in the production docker container in /Dockerfile.
 # While not strictly required, it is prefered to run with an init process.
 
-if [[ -d /meta ]]; then
-  # expose the build metadata as environment variables
-  # these are then pulled by Tamanu into the OpenTelemetry context
-  for f in /meta/*; do
-    export "OTEL_CONTEXT_$(basename $f)"="$(cat $f)"
-  done
-fi
-
 if [[ "$1" == "healthcheck" ]]; then
   # used as healthcheck command in ECS
   set -eo pipefail
