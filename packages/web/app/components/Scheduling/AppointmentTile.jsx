42a0a5808052ed3572e1463ad8dc8ea03ad3d920
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-09-05 17:22:35 +1200

    WIP: appointment tile

---
+++ packages/web/app/components/Scheduling/AppointmentTile.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { APPOINTMENT_STATUSES } from '@tamanu/constants';
+
+import { Colors } from '../../constants';
+import { APPOINTMENT_STATUS_COLORS } from './statusColors';
+
+const Wrapper = styled.div`
+  ${props =>
+    css`
+      background-color: ${props.$hexColor}1a; // 10% opacity
+      border-color: ${props.$hexColor};
+      border-width: ${props.$selected ? '1px' : '0'};
+
+      &:hover {
+        background-color: ${props.$hexColor}33; // 20% opacity
+      }
+    `}
+
+  border-radius: 0.3125rem;
+  border-style: solid;
+  color: ${Colors.darkestText};
+  cursor: pointer;
+  display: grid;
+  gap: 0.3125rem;
+  grid-template-columns: 1fr 0.625rem;
+  padding-block: 0.5rem;
+  padding-inline: 0.3125rem;
+  text-decoration-thickness: from-font;
+  transition: background-color 150ms ease;
+  touch-action: manipulation;
+`;
+
+const Label = styled.span`
+  padding-inline-start: 0.3125rem;
+
+  ${props =>
+    props.$strikethrough &&
+    css`
+      text-decoration-line: line-through;
+    `}
+`;
+
+export const AppointmentTile = ({
+  appointment: { appointmentStatus, startTime, patient },
+  selected = false,
+}) => (
+  <Wrapper
+    $hexColor={APPOINTMENT_STATUS_COLORS[appointmentStatus] ?? Colors.blue}
+    $selected={selected}
+  >
+    <Label $strikethrough={appointmentStatus === APPOINTMENT_STATUSES.NO_SHOW}>
+      <time dateTime={startTime.toISOString()}>{startTime.toLocaleTimeString()}</time>
+      {patient}
+    </Label>
+  </Wrapper>
+);
---
+++ packages/web/stories/scheduling/appointmentTile.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { AppointmentTile } from '../../app/components/Scheduling/AppointmentTile';
+
+export default {
+  title: 'Scheduling/Appointment Tile',
+  component: AppointmentTile,
+};
+
+export const Default = () => <AppointmentTile />;
