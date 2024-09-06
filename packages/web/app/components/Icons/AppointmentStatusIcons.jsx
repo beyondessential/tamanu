72f52772df521da759d760ff311c1906e74f6218
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-09-06 14:48:51 +1200

    alphasort

--- packages/web/app/components/Icons/AppointmentStatusIcons.jsx
+++ packages/web/app/components/Icons/AppointmentStatusIcons.jsx
@@ -13,8 +13,8 @@ const Svg = styled.svg.attrs({
   viewBox: '0 0 10 10',
   xmlns: 'http://www.w3.org/2000/svg',
 })`
-  fill: transparent;
   align-self: center;
+  fill: transparent;
 `;
 
 export const CheckIconFilled = ({ htmlColor, ...props }) => (
