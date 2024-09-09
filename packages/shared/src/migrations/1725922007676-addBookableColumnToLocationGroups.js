24b2046a45a9d6da0485f712e9f4790eeab04efd
Flynn <flynn.teh@gmail.com>
2024-09-10 11:15:11 +1200

    added column

---
+++ packages/shared/src/migrations/1725922007676-addBookableColumnToLocationGroups.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+// remove the above line
+
+import { DataTypes } from 'sequelize';
+
+export async function up(query) {
+  await query.addColumn('location_groups', 'is_bookable', {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  });
+}
+
+export async function down(query) {
+  await query.removeColumn('location_groups', 'is_bookable');
+}
