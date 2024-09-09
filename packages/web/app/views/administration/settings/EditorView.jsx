3202d3a1213c9dce7ba8217ec256fb93cba18f71
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-09-09 14:52:39 +1200

    reorganise

--- packages/web/app/views/administration/settings/EditorView.jsx
+++ packages/web/app/views/administration/settings/EditorView.jsx
@@ -1,26 +1,13 @@
 import React, { memo, useEffect, useMemo, useState } from 'react';
-import { capitalize, omitBy, pickBy, startCase, set, get, cloneDeep, has } from 'lodash';
+import { capitalize, omitBy, pickBy, startCase, set, get, cloneDeep } from 'lodash';
 import styled from 'styled-components';
 
 import { getScopedSchema, isSetting } from '@tamanu/settings';
 
-import {
-  Heading4,
-  SelectInput,
-  TranslatedText,
-  Button,
-  OutlinedButton,
-  BodyText,
-} from '../../../components';
-import { ScopeSelectorFields } from './ScopeSelectorFields';
+import { SelectInput, TranslatedText, Button, OutlinedButton } from '../../../components';
 import { Colors } from '../../../constants';
-import { ThemedTooltip } from '../../../components/Tooltip';
 import { Box, Divider } from '@material-ui/core';
-import { SettingInput } from './SettingInput';
-import { ConfirmModal } from '../../../components/ConfirmModal';
-
-const INDENT_WIDTH_PX = 20;
-const LONG_TEXT_KEYS = ['body'];
+import { Category } from './components/Category';
 
 const SettingsWrapper = styled.div`
   background-color: ${Colors.white};
@@ -28,19 +15,8 @@ const SettingsWrapper = styled.div`
   margin-top: 20px;
 `;
 
-const StyledTopBar = styled.div`
-  display: flex;
-`;
-
 const StyledSelectInput = styled(SelectInput)`
   width: 300px;
-`;
-
-const SettingLine = styled(BodyText)`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 10px;
-  width: 650px;
 `;
 
 const CategoryOptions = styled(Box)`
@@ -57,110 +33,19 @@ const CategoriesWrapper = styled.div`
   padding: 20px;
 `;
 
-const CategoryWrapper = styled.div`
-  // margin-left: ${({ $nestLevel }) => $nestLevel * INDENT_WIDTH_PX}px;
-  :not(:first-child) {
-    padding-top: 20px;
-    border-top: 1px solid ${Colors.outline};
-  }
-`;
-
-const getName = (name, path) => name || capitalize(startCase(path.split('.').pop()));
-
-const CategoryTitle = ({ name, path, description }) => {
-  const categoryTitle = getName(name, path);
-  if (!categoryTitle) return null;
-  return (
-    <ThemedTooltip placement="top" arrow title={description}>
-      <Heading4 width="fit-content" mt={0} mb={2}>
-        {categoryTitle}
-      </Heading4>
-    </ThemedTooltip>
-  );
-};
-
-const SettingName = ({ name, path, description }) => {
-  const nameText = (
-    <BodyText ml={1} mr="auto" width="fit-content">
-      {getName(name, path)}
-    </BodyText>
-  );
-
-  return description ? (
-    <ThemedTooltip arrow placement="top" title={description}>
-      {nameText}
-    </ThemedTooltip>
-  ) : (
-    nameText
-  );
-};
-
-const sortProperties = ([a0, a1], [b0, b1]) => {
-  const aName = a1.name || a0;
-  const bName = b1.name || b0;
-  const isTopLevelA = isSetting(a1);
-  const isTopLevelB = isSetting(b1);
-  // Sort top level settings first
-  if (isTopLevelA && !isTopLevelB) return -1;
-  if (!isTopLevelA && isTopLevelB) return 1;
-  // Alphabetical sort
-  return aName.localeCompare(bName);
-};
-
-export const Category = ({ schema, path = '', getSettingValue, handleChangeSetting }) => {
-  if (!schema) return null;
-  const Wrapper = path ? CategoryWrapper : Box;
-  const nestLevel = path.split('.').length;
-  const sortedProperties = Object.entries(schema.properties).sort(sortProperties);
-  return (
-    <Wrapper $nestLevel={nestLevel}>
-      <CategoryTitle name={schema.name} path={path} description={schema.description} />
-      <>
-        {sortedProperties.map(([key, schema]) => {
-          const newPath = path ? `${path}.${key}` : key;
-          const { name, description, type, defaultValue, unit } = schema;
-          return type ? (
-            <SettingLine key={newPath}>
-              <SettingName path={newPath} name={name} description={description} />
-              <SettingInput
-                // TODO: better solution for this override object
-                type={LONG_TEXT_KEYS.includes(key) ? 'longText' : type.type}
-                value={getSettingValue(newPath)}
-                defaultValue={defaultValue}
-                path={newPath}
-                handleChangeSetting={handleChangeSetting}
-                unit={unit}
-                // TODO: disabled logic
-              />
-            </SettingLine>
-          ) : (
-            <Category
-              key={newPath}
-              path={newPath}
-              schema={schema}
-              getSettingValue={getSettingValue}
-              handleChangeSetting={handleChangeSetting}
-            />
-          );
-        })}
-      </>
-    </Wrapper>
-  );
-};
-
-const parseJsonStrings = obj => {
+const recursiveJsonParse = obj => {
   if (typeof obj === 'string') {
     try {
       const parsed = JSON.parse(obj);
-      return parseJsonStrings(parsed);
+      return recursiveJsonParse(parsed);
     } catch (e) {
       return obj; // Return the string if it is not a valid JSON
     }
   } else if (Array.isArray(obj)) {
-    return obj.map(parseJsonStrings);
+    return obj.map(recursiveJsonParse);
   } else if (typeof obj === 'object' && obj !== null) {
     return Object.keys(obj).reduce((acc, key) => {
-      acc[key] = parseJsonStrings(obj[key]);
+      acc[key] = recursiveJsonParse(obj[key]);
       return acc;
     }, {});
   } else {
@@ -190,25 +75,6 @@ const prepareSchema = scope => {
   return schema;
 };
 
-/* TODO: translations */
-export const WarningModal = ({ open, setWarningModalOpen, resolveFn }) => (
-  <ConfirmModal
-    title="Unsaved changes"
-    subText="You have unsaved changes. Are you sure you would like to discard those changes?"
-    open={open}
-    onConfirm={() => {
-      setWarningModalOpen(false);
-      resolveFn(true);
-    }}
-    confirmButtonText="Discard changes"
-    onCancel={() => {
-      setWarningModalOpen(false);
-      resolveFn(false);
-    }}
-    cancelButtonText="Go back"
-  />
-);
-
 export const EditorView = memo(
   ({
     values,
@@ -229,6 +95,7 @@ export const EditorView = memo(
       scopedSchema,
     ]);
 
+    // Clear category when scope changes
     useEffect(() => setCategory(null), [scope]);
 
     const handleChangeCategory = async e => {
@@ -241,21 +108,24 @@ export const EditorView = memo(
       setCategory(newCategory);
     };
 
-    // Setting state management
     const handleChangeSetting = (path, value) => {
       const settingObject = cloneDeep(values.settings || settingsSnapshot);
       const updatedSettings = set(settingObject, `${category}.${path}`, value);
       setValues({ ...values, settings: updatedSettings });
     };
 
+    // Get initial value from snapshot, otherwise grab from current formik state once it exists
     const getSettingValue = path => get(values.settings || settingsSnapshot, `${category}.${path}`);
 
     const saveSettings = async event => {
-      const parsedObject = parseJsonStrings(values.settings);
-      setValues({ ...values, settings: parsedObject });
-      await submitForm(event);
-      setCategory(null); // Prevents flashing on form reset
-      await resetForm();
+      // Need to parse json string objects stored in keys
+      setValues({ ...values, settings: recursiveJsonParse(values.settings) });
+      const success = await submitForm(event);
+      console.log('success:', success)
+      if (success) {
+        setCategory(null)
+        await resetForm();
+      }
     };
 
     return (
@@ -263,7 +133,6 @@ export const EditorView = memo(
         <SettingsWrapper>
           <CategoryOptions p={2}>
             <StyledSelectInput
-              required
               label={<TranslatedText stringId="admin.settings.category" fallback="Category" />}
               value={category}
               onChange={handleChangeCategory}
