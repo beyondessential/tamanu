7c8cbad430bef674013edfd616c83e6ec655bc02
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-09-10 14:51:40 +1200

    use `date-fns`

--- packages/settings/src/schema/definitions/bookingSlots.ts
+++ packages/settings/src/schema/definitions/bookingSlots.ts
@@ -1,14 +1,11 @@
+import { isMatch } from 'date-fns';
 import * as yup from 'yup';
 
 export const datelessTimeStringSchema = yup.string().test({
   name: 'datelessTimeString',
   message: 'Start and end times for ‘bookingSlots’ must be a valid 24-hour time (e.g. 17:30)',
   test: str => {
-    // Don’t fail validation if falling back to default value
-    if (str === undefined) return true;
-
-    // Verify (with an arbitrary date which we don’t care about) that time can be parsed
-    const datetime = new Date(`2000-01-01T${str.trim()}`);
-    return !Number.isNaN(datetime.valueOf());
+    if (str === undefined) return true; // Don’t fail validation if falling back to default value
+    return isMatch(str, 'HH:mm');
   },
 });
