fc0c0588490fe5c192b740bf3308530a3c099cb9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-09-13 10:35:48 +1200

    again

--- packages/web/stories/scheduling/appointmentStatusChip.stories.jsx
+++ packages/web/stories/scheduling/appointmentStatusChip.stories.jsx
@@ -2,35 +2,47 @@ import React from 'react';
 
 import { APPOINTMENT_STATUSES } from '@tamanu/constants';
 
-import { AppointmentStatusToken } from '../../app/components/Scheduling/AppointmentStatusToken';
+import { AppointmentStatusChip } from '../../app/components/Scheduling/AppointmentStatusChip';
 
 export default {
   title: 'Scheduling/Appointment Status Token',
-  component: AppointmentStatusToken,
+  component: AppointmentStatusChip,
+  argTypes: {
+    appointmentStatus: {
+      control: 'select',
+      options: Object.values(APPOINTMENT_STATUSES),
+    },
+    deselected: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
 };
 
 export const Confirmed = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} />
 );
 
 export const Arrived = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.ARRIVED} />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.ARRIVED} />
 );
 
 export const Assessed = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.ASSESSED} />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.ASSESSED} />
 );
 
-export const Seen = () => <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.SEEN} />;
+export const Seen = () => <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.SEEN} />;
 
 export const NoShow = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.NO_SHOW} />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.NO_SHOW} />
 );
 
 export const Deselected = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} deselected />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} deselected />
 );
 
 export const Disabled = () => (
-  <AppointmentStatusToken appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} disabled />
+  <AppointmentStatusChip appointmentStatus={APPOINTMENT_STATUSES.CONFIRMED} disabled />
 );
