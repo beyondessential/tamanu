3a2271c3df497e4efa407ca1f8f740515e3f8939
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-09-13 16:45:19 +1200

    more robust dated header styling

--- packages/web/app/views/scheduling/locationBookings/DayHeaderCell.jsx
+++ packages/web/app/views/scheduling/locationBookings/DayHeaderCell.jsx
@@ -1,19 +1,31 @@
+import { formatISO } from 'date-fns';
 import React from 'react';
 import styled from 'styled-components';
 
-import { formatWeekdayShort } from '../../../components';
+import { Colors } from '../../../constants';
+import { formatShort, formatWeekdayShort } from '../../../components';
 import { CalendarColumnHeader } from './TableComponents';
 
 const StyledHeader = styled(CalendarColumnHeader)`
+  color: ${Colors.darkestText};
+  font-size: 1.15rem;
+  line-height: 1.3;
+`;
+
+const Weekday = styled.div`
+  color: ${Colors.midText};
   font-variant-caps: all-small-caps;
+  font-weight: 500;
   letter-spacing: 0.1em;
-  font-weight: 450;
 `;
 
 export const DayHeaderCell = ({ date }) => {
   return (
     <StyledHeader>
-      <time dateTime={date.toISOString()}>{formatWeekdayShort(date)}</time>
+      <time dateTime={formatISO(date, { representation: 'date' })}>
+        <Weekday>{formatWeekdayShort(date)}</Weekday>
+        {formatShort(date)}
+      </time>
     </StyledHeader>
   );
 };
