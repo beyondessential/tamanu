b0b3e4b31aa44bf3fb02a56294347b0dfa5515b4
rail-rain <takayuki@bes.au>
2024-09-16 17:34:48 +1200

    Load schemas from the database

--- packages/scripts/src/dbt-generate-source.mjs
+++ packages/scripts/src/dbt-generate-source.mjs
@@ -4,6 +4,17 @@ import pg from 'pg';
 import { program } from 'commander';
 import YAML from 'yaml';
 
+async function getSchemas(client) {
+  return (
+    await client.query(
+      `SELECT schema_name
+      FROM information_schema.schemata
+      WHERE schema_name not similar to 'pg%|information_schema|sync_snapshots|reporting'
+      ORDER BY schema_name`,
+    )
+  ).rows.map(table => table.schema_name);
+}
+
 async function getTablesInSchema(client, schemaName) {
   return (
     await client.query(
@@ -31,7 +42,7 @@ async function generateSource({ host, port, name: database, username, password }
   const client = new pg.Client({ host, port, user: username, database, password });
   await client.connect();
 
-  const tasks = ['public', 'fhir', 'logs'].map(async schemaName => {
+  const tasks = (await getSchemas(client)).map(async schemaName => {
     await fs.mkdir(`database/model/${schemaName}`);
     const tasks = (await getTablesInSchema(client, schemaName)).map(async table => {
       const sources = {
