c7a415deb76757dec8808970c79901fb32fe0945
Félix Saparelli <felix@bes.au>
2024-09-17 11:51:26 +1200

    fix(pm2): reduce api scaling to 1 if there's too little memory

--- packages/central-server/pm2.config.cjs
+++ packages/central-server/pm2.config.cjs
@@ -4,7 +4,9 @@ const totalMemoryMB = Math.round(os.totalmem() / (1024**2));
 const memory = process.env.TAMANU_MEMORY_ALLOCATION || (totalMemoryMB * 0.6).toFixed(0);
 
 const availableThreads = os.availableParallelism();
-const defaultApiScale = Math.max(2, Math.floor(availableThreads / 2));
+const minimumApiScale = totalMemoryMB > 3000 ? 2 : 1;
+const maximumApiScale = 4; // more requires custom caddy config
+const defaultApiScale = Math.min(maximumApiScale, Math.max(minimumApiScale, Math.floor(availableThreads / 2)));
 
 const cwd = '.'; // IMPORTANT: Leave this as-is, for production build
 
