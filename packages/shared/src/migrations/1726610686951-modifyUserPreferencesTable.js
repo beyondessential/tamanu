b9a7e21169586943337ee7fff5db1d980cbc8e40
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-09-17 18:15:43 -0400

    Merge migrations into one file and bump the timestamp

--- packages/shared/src/migrations/1726610686951-modifyUserPreferencesTable.js
+++ packages/shared/src/migrations/1726610686951-modifyUserPreferencesTable.js
@@ -1,6 +1,16 @@
 import { DataTypes } from 'sequelize';
 
 export async function up(query) {
+  await query.addColumn('user_preferences', 'preference_key', {
+    type: DataTypes.STRING,
+    allowNull: false,
+  });
+
+  await query.addColumn('user_preferences', 'preference_value', {
+    type: DataTypes.JSONB,
+    allowNull: false,
+  });
+
   await query.sequelize.query(`
     UPDATE user_preferences
     SET preference_value = (to_json(selected_graphed_vitals_on_filter::text)::jsonb)
@@ -24,4 +34,7 @@ export async function down(query) {
     SET selected_graphed_vitals_on_filter = preference_value
     WHERE key = 'selectedGraphedVitalsOnFilter'
   `);
+
+  await query.dropColumn('user_preferences', 'preference_key');
+  await query.dropColumn('user_preferences', 'preference_value');
 }
