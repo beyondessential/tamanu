42d1efe50f22338f4cf81a78ef9f521e9e5c7f44
Flynn <flynn.teh@gmail.com>
2024-09-17 13:43:31 +1200

    proper styling

--- packages/web/app/components/Field/CalendarField.jsx
+++ packages/web/app/components/Field/CalendarField.jsx
@@ -1,57 +1,74 @@
 import React, { useMemo, useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers';
-import { Popper, styled } from '@mui/material';
 import { Colors } from '../../constants';
-import { TextInput } from './TextField';
 import { StyledExpandLess, StyledExpandMore } from './FieldCommonComponents';
+import { add, endOfYear, startOfYear } from 'date-fns';
+import { TextInput } from './TextField';
 
 const getMaxDate = () => {
-  const currentYear = new Date().getFullYear();
-  const maxYear = currentYear + 8;
-  return new Date(maxYear, 11, 31);
+  return endOfYear(add(new Date(), { years: 8 }));
 };
 
 const getMinDate = () => {
-  const currentYear = new Date().getFullYear();
-  const minYear = currentYear - 3;
-  return new Date(minYear, 0, 1);
+  return startOfYear(add(new Date(), { years: -3 }));
 };
 
-const StyledDatePopper = styled(Popper)`
-  & .MuiPaper-root {
-    border: 1px solid ${Colors.outline};
-    box-shadow: none;
-  }
-  & .MuiDateCalendar-root {
-    height: auto;
-    height: 172px;
-  }
-  & .MuiMonthCalendar-root {
-    max-height: 104px;
-    overflow-y: auto;
-    margin-bottom: 12px;
-  }
-  & .MuiYearCalendar-root {
-    max-height: 96px;
-    overflow-y: auto;
-    margin-bottom: 12px;
-  }
-  & .MuiPickersYear-yearButton.Mui-selected,
-  .MuiPickersMonth-monthButton.Mui-selected {
-    background-color: ${Colors.primary};
-    color: white;
-    &:hover,
-    &:focus {
-      background-color: ${Colors.primary};
-    }
-  }
-`;
+const popperStyles = {
+  '& .MuiPaper-root': {
+    border: `1px solid ${Colors.outline}`,
+    boxShadow: 'none',
+  },
+  '& .MuiDateCalendar-root': {
+    height: 'auto',
+    maxHeight: '93px', // Prevent calendar from flickering when switching between month and year views
+    maxWidth: '210px',
+    marginBottom: '12px',
+  },
+  '& .MuiMonthCalendar-root, .MuiYearCalendar-root': {
+    width: 'auto',
+    maxHeight: '62px',
+    overflowY: 'auto',
+    paddingX: '10px',
+  },
+  '& .MuiPickersYear-yearButton, .MuiPickersMonth-monthButton': {
+    color: `${Colors.darkestText}`,
+    fontWeight: 500,
+    fontSize: '11px',
+    width: '46px',
+    height: '23px',
+    marginY: '4px',
+  },
+  '& .MuiPickersYear-yearButton.Mui-selected, .MuiPickersMonth-monthButton.Mui-selected': {
+    backgroundColor: `${Colors.primary}`,
+    color: 'white',
+    '&:hover, &:focus': {
+      backgroundColor: `${Colors.primary}`,
+    },
+  },
+  '& .MuiPickersArrowSwitcher-root': {
+    width: '0px',
+    height: '0px',
+  },
+};
 
-export const MonthYearInput = ({ minDate: propMinDate, maxDate: propMaxDate, ...props }) => {
+const calendarHeaderStyles = {
+  minHeight: '15px', // Overrides default
+  '& .MuiPickersCalendarHeader-labelContainer': {
+    fontSize: '11px',
+    lineHeight: '15px',
+  },
+};
+
+export const MonthYearInput = ({
+  minDate: propMinDate,
+  maxDate: propMaxDate,
+  onChange = () => {},
+  ...props
+}) => {
   const [open, setOpen] = useState(false);
   const minDate = useMemo(() => propMinDate || getMinDate(), [propMinDate]);
   const maxDate = useMemo(() => propMaxDate || getMaxDate(), [propMaxDate]);
-  2;
+  console.log(onChange);
   return (
     <DatePicker
       onOpen={() => setOpen(true)}
@@ -62,11 +79,21 @@ export const MonthYearInput = ({ minDate: propMinDate, maxDate: propMaxDate, ...
       defaultValue={new Date()}
       slots={{
         openPickerIcon: open ? StyledExpandLess : StyledExpandMore,
-        popper: StyledDatePopper,
+        switchViewButton: StyledExpandLess,
         textField: TextInput,
+      }}
+      slotProps={{
+        popper: { sx: popperStyles },
+        calendarHeader: {
+          sx: calendarHeaderStyles,
+        },
+        textField: {
+          size: 'small', // Manually set size to small for appropriate text size
+        },
       }}
       minDate={minDate}
       maxDate={maxDate}
+      onAccept={date => onChange(date)}
       {...props}
     />
   );
