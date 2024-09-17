8cb318fb2485601915631b9bdea355bceb7f717a
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2024-09-17 17:39:29 +1200

    fix(omniserver): Ensure no hard failure if only serverFacilityId (#6450)
    * ensure no hardfail if serverFacilityId
    
    * no hard failure
    
    * no hard failure
    
    * does this work
    
    * ohk cant use shared in settings right now
    
    * ohk cant use shared in settings right now
    
    * better?
    
    * better?
    
    * better?

--- packages/facility-server/app/utils/configUtils.js
+++
@@ -1,9 +0,0 @@
-export const selectFacilityIds = config => {
-  const { serverFacilityId, serverFacilityIds } = config;
-  if (serverFacilityId && serverFacilityIds) {
-    throw new Error(
-      'Both serverFacilityId and serverFacilityIds are set in config, a facility server should either have a single facility or multiple facilities, not both.',
-    );
-  }
-  return serverFacilityId ? [serverFacilityId] : serverFacilityIds;
-};
