beyondessential/tamanu

commit 2b5029c3ab96acb964efca64cf966af0db7d0e91

fix(web): NASS-1271: fix exports on slow networks globally (#6267)
* pass fetching fn instead of data to `saveFile`

* begin refactoring usages

* same fix in `ReportGeneratorForm`

* patch type in `ExportReportView`

* wrap comment

* WIP: fix test

* actually fix test

* randomise `exportName`

* fix no-data report generation

* notify when download is done

* ditto when exporting programs/forms

* `prepareExcelFile` compatibility patch

this step can soon be removed entirely for one-click downloads

* amend string ID

Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

* amend another string ID

Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

* JSDoc-ify comment

* fix(web): NASS-1271: fix report export timeout error (#6297)

* export `sanitizeFileName` from `utils`

* fix timeout in `ExportReportView`

* provide key when mapping

---------

Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>
Co-authored-by: Klaus-S <69126149+Klaus-S@users.noreply.github.com>

packages/web/app/views/administration/reports/ReportsSelectFields.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../../../api';
 import { BaseSelectField } from '../../../components';
 
-export const ReportSelectField = ({ includeNameChangeEvent, ...props }) => {
+export const ReportSelectField = ({ includeNameChangeEvent, setSelectedReportName, ...props }) => {
   delete props.error;
   delete props.helperText;
 
@@ -20,11 +20,14 @@ export const ReportSelectField = ({ includeNameChangeEvent, ...props }) => {
     <BaseSelectField
       {...props}
       onChange={event => {
+        const { value } = event.target;
+        const name = reportData.find(report => report.id === value)?.name;
+        setSelectedReportName(name);
+
         if (includeNameChangeEvent) {
-          const { value } = event.target;
-          const name = reportData.find(report => report.id === value)?.name;
           props.field.onChange({ target: { name: 'name', value: name } });
         }
+
         props.field.onChange(event);
       }}
       options={options}
@@ -40,6 +43,7 @@ export const VersionSelectField = props => {
     form: {
       values: { reportId },
     },
+    setSelectedVersionNumber,
   } = props;
 
   const query = useQuery(
@@ -63,6 +67,13 @@ export const VersionSelectField = props => {
   return (
     <BaseSelectField
       {...props}
+      onChange={event => {
+        const { value } = event.target;
+        const { versionNumber } = versionData.find(version => version.id === value);
+        setSelectedVersionNumber(versionNumber);
+
+        props.field.onChange(event);
+      }}
       options={options}
       error={!!fetchError || props.error}
       helperText={fetchError?.message || props.helperText}

packages/web/__tests__/components/Table/DownloadDataButton.test.jsx
@@ -9,10 +9,12 @@
 
 import { screen } from '@testing-library/react';
 import { userEvent } from '@testing-library/user-event';
+import Chance from 'chance';
 import * as React from 'react';
 import { assert, describe, it, vi } from 'vitest';
-import * as XLSX from 'xlsx';
+import { getCurrentDateString } from '@tamanu/shared/utils/dateTime';
 import { DownloadDataButton } from '../../../app/components/Table/DownloadDataButton';
+import * as fileSystemAccess from '../../../app/utils/fileSystemAccess';
 import {
   culturalName,
   dateOfBirth,
@@ -47,7 +49,7 @@ const mockTranslationContext = {
 };
 
 const getTranslationSpy = vi.spyOn(mockTranslationContext, 'getTranslation');
-const sheetToJsonSpy = vi.spyOn(XLSX.utils, 'json_to_sheet');
+const saveFileSpy = vi.spyOn(fileSystemAccess, 'saveFile');
 
 /** {@link DownloadDataButton} must be rendered within a translation context */
 const render = element => renderElementWithTranslatedText(element, null, mockTranslationContext);
@@ -87,15 +89,20 @@ describe('DownloadDataButton', () => {
     expect(button.textContent).toBe('🌐 Export 🌐');
   });
 
-  it('does attempt to generate a spreadsheet', async () => {
+  it('does attempt to save a spreadsheet', async () => {
     const user = userEvent.setup();
-    render(<DownloadDataButton exportName="Export" columns={columns} data={data} />);
+    const exportName = new Chance().string();
+    render(<DownloadDataButton exportName={exportName} columns={columns} data={data} />);
 
     const button = screen.getByTestId('download-data-button');
     await user.click(button);
 
-    // Ideally this would check the arguments with which the spy is called, but those objects depend
-    // on `renderToText` working in this testing environment
-    expect(sheetToJsonSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenCalledTimes(1);
+    expect(saveFileSpy).toHaveBeenLastCalledWith(
+      expect.objectContaining({
+        defaultFileName: `${exportName}-${getCurrentDateString()}`,
+        extension: 'xlsx',
+      }),
+    );
   });
 });

packages/web/app/components/Field/RadioField.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Radio from '@material-ui/core/Radio';
@@ -138,8 +138,8 @@ export const RadioInput = ({
           {...props}
         >
           {options.map(option => (
-            <>
-              {option.leftOptionalElement ? option.leftOptionalElement : null}
+            <Fragment key={option.value}>
+              {option.leftOptionalElement ?? null}
               <ControlLabel
                 key={option.value}
                 labelPlacement={option.description ? 'start' : 'end'}
@@ -178,7 +178,7 @@ export const RadioInput = ({
                     : DEFAULT_LABEL_THEME
                 }
               />
-            </>
+            </Fragment>
           ))}
         </StyledRadioGroup>
         {helperText && <FormHelperText>{helperText}</FormHelperText>}

packages/web/app/components/Table/DownloadDataButton.jsx
@@ -5,7 +5,7 @@ import * as XLSX from 'xlsx';
 import { QueryClientProvider, useQueryClient } from '@tanstack/react-query';
 
 import { ApiContext, useApi } from '../../api';
-import { renderToText } from '../../utils';
+import { notifySuccess, renderToText } from '../../utils';
 import { saveFile } from '../../utils/fileSystemAccess';
 import { TranslationContext, useTranslation } from '../../contexts/Translation';
 import { GreyOutlinedButton } from '../Button';
@@ -36,6 +36,7 @@ export function DownloadDataButton({ exportName, columns, data }) {
   const queryClient = useQueryClient();
   const api = useApi();
   const translationContext = useTranslation();
+  const { getTranslation } = translationContext;
 
   const safelyRenderToText = element => {
     /**
@@ -80,7 +81,7 @@ export function DownloadDataButton({ exportName, columns, data }) {
       return { ...rest, ...exportOverrides };
     });
 
-  const onDownloadData = async () => {
+  const prepareData = async () => {
     const header = exportableColumnsWithOverrides.map(getHeaderValue);
     const rows = await Promise.all(
       data.map(async d => {
@@ -128,12 +129,18 @@ export function DownloadDataButton({ exportName, columns, data }) {
     const wb = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(wb, ws, exportName);
 
-    const xlsxDataArray = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+    return XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+  };
+
+  const onDownloadData = async () => {
     await saveFile({
       defaultFileName: `${exportName}-${getCurrentDateString()}`,
-      data: xlsxDataArray,
+      getData: prepareData,
       extension: 'xlsx',
     });
+    notifySuccess(
+      getTranslation('document.notification.downloadSuccess', 'Successfully downloaded file'),
+    );
   };
 
   return (

packages/web/app/hooks/useDocumentActions.js
@@ -4,7 +4,7 @@ import { extension } from 'mime-types';
 import { useApi } from '../api';
 import { notify, notifyError, notifySuccess } from '../utils';
 import { saveFile } from '../utils/fileSystemAccess';
-import { useTranslation } from '../contexts/Translation'; 
+import { useTranslation } from '../contexts/Translation';
 
 const base64ToUint8Array = base64 => {
   const binString = atob(base64);
@@ -45,28 +45,24 @@ export const useDocumentActions = () => {
         notify(
           getTranslation(
             'document.notification.downloadStart',
-            'Your download has started, please wait.'
-          ), 
-          { type: 'info' }
+            'Your download has started, please wait.',
+          ),
+          { type: 'info' },
         );
-
-        // Download attachment (*currently the API only supports base64 responses)
-        const { data } = await api.get(`attachment/${document.attachmentId}`, {
-          base64: true,
-        });
 
         await saveFile({
           defaultFileName: document.name,
-          data: base64ToUint8Array(data),
+          getData: async () => {
+            // Download attachment (*currently the API only supports base64 responses)
+            const { data } = await api.get(`attachment/${document.attachmentId}`, { base64: true });
+            return base64ToUint8Array(data);
+          },
           extension: extension(document.type),
           mimetype: document.type,
         });
 
         notifySuccess(
-          getTranslation(
-            'document.notification.downloadSuccess',
-            'Successfully downloaded file'
-          )
+          getTranslation('document.notification.downloadSuccess', 'Successfully downloaded file'),
         );
       } catch (error) {
         notifyError(error.message);

packages/web/app/utils/fileSystemAccess.js
@@ -36,9 +36,13 @@ const createFileSystemHandle = ({ defaultFileName, filetype }) =>
     types: [filetype],
   });
 
+/**
+ * @param getData {Function} - Async function which returns the file data to write (ArrayBuffer,
+ * TypedArray, DataView, Blob, or string)
+ */
 export const saveFile = async ({
   defaultFileName,
-  data, // The file data to write, in the form of an ArrayBuffer, TypedArray, DataView, Blob, or string.
+  getData,
   extension = null, // The file extension.
   mimetype = null,
 }) => {
@@ -54,12 +58,19 @@ export const saveFile = async ({
   }
 
   if ('showSaveFilePicker' in window) {
+    // To avoid a SecurityError, get the file handle first...
     const fileHandle = await createFileSystemHandle({ defaultFileName, filetype });
     const writable = await fileHandle.createWritable();
+
+    // ...and THEN prepare the file for download.
+    // See https://developer.chrome.com/docs/capabilities/web-apis/file-system-access#write-file
+    const data = await getData();
+
     await writable.write(data);
     await writable.close();
   } else {
     // fallback to non-file-picker download if it's not available
+    const data = await getData();
     const blob = new Blob([data], {
       type: Object.keys(filetype.accept)?.[0] ?? 'application/binary',
     });

packages/web/app/utils/index.js
@@ -5,4 +5,5 @@ export { flattenObject } from './flattenObject';
 export * from './getVisibleQuestions';
 export * from './invalidatePatientDataQueries';
 export { getBrandName, getBrandId } from './brandMode.js';
+export { sanitizeFileName } from './sanitizeFileName';
 export { yupAttemptTransformToNumber } from './validation.js';

packages/web/app/utils/saveExcelFile.js
@@ -1,4 +1,5 @@
 import * as XLSX from 'xlsx';
+
 const stringifyIfNonDateObject = val =>
   typeof val === 'object' && !(val instanceof Date) && val !== null ? JSON.stringify(val) : val;
 
@@ -18,7 +19,7 @@ export function prepareExcelFile({ data, metadata, defaultFileName = '', bookTyp
   const xlsxDataArray = XLSX.write(book, { bookType, type: 'buffer' });
   return {
     defaultFileName,
-    data: xlsxDataArray,
+    getData: () => xlsxDataArray,
     extension: bookType,
   };
 }

commit d754ac9bb6615dc6ecdea787a2450a8e50c00761

fix(admin): export report was broken by #6267

packages/web/app/views/administration/reports/ReportsSelectFields.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../../../api';
 import { BaseSelectField } from '../../../components';
 
-export const ReportSelectField = ({ includeNameChangeEvent, setSelectedReportName, ...props }) => {
+export const ReportSelectField = ({ includeNameChangeEvent, setSelectedReportName = null, ...props }) => {
   delete props.error;
   delete props.helperText;
 
@@ -22,7 +22,7 @@ export const ReportSelectField = ({ includeNameChangeEvent, setSelectedReportNam
       onChange={event => {
         const { value } = event.target;
         const name = reportData.find(report => report.id === value)?.name;
-        setSelectedReportName(name);
+        if (setSelectedReportName) setSelectedReportName(name);
 
         if (includeNameChangeEvent) {
           props.field.onChange({ target: { name: 'name', value: name } });