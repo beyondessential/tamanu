37b8c07cf564d5c5055e80db351a909f723fbe53
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-09-20 10:42:00 +1200

    progress

---
+++ packages/web/stories/appointments/bookingField.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { BookingTimeField } from '../../app/components/Appointments/BookingTimeField';
+
+
+export default {
+  title: 'Appointments/Booking time field',
+  component: BookingTimeField,
+};
+
+export const Confirmed = () => <BookingTimeField />;
