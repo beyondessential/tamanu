5c49cc13a344b1593b51baeb9beada52518b4da2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-10-01 19:13:17 +1300

    feat(appointments): NASS-1390: new setting for configuring booking slots (#6528)
    * recreate #6416, ungoosed 🚫🪿
    
    * more reusable
    
    * allow only minutes and hours

--- packages/settings/src/schema/facility.ts
+++ packages/settings/src/schema/facility.ts
@@ -1,10 +1,40 @@
 import * as yup from 'yup';
+
+import { datelessTimeStringSchema, durationStringSchema } from './definitions';
 import { extractDefaults } from './utils';
 
 export const facilitySettings = {
   name: 'Facility server settings',
   description: 'Settings that apply only to a facility server',
   properties: {
+    appointments: {
+      description: 'Settings related to scheduling patient appointments and location bookings',
+      properties: {
+        bookingSlots: {
+          description: 'Configure the available booking slots for appointments',
+          properties: {
+            startTime: {
+              description:
+                'The time when bookings open for the day. (The earliest start time for a booking.) Uses 24-hour time, e.g. 13:30.',
+              type: datelessTimeStringSchema,
+              defaultValue: '09:00',
+            },
+            endTime: {
+              description:
+                'The time when bookings close for the day. (The latest booking must end by this time.) Uses 24-hour time, e.g. 13:30.',
+              type: datelessTimeStringSchema,
+              defaultValue: '17:00',
+            },
+            slotDuration: {
+              description:
+                'The length of each time slot. A single booking may span multiple consecutive slots. Supported units: ‘min’, ‘h’',
+              type: durationStringSchema('slotDuration'),
+              defaultValue: '30min',
+            },
+          },
+        },
+      },
+    },
     templates: {
       name: 'Templates',
       description: 'Settings related to templates',
