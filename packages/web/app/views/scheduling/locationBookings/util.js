abf06a549a78c3285cc34ed3d8285400e69e41c3
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-10-08 19:56:49 +1300

    more performance reducer

--- packages/web/app/views/scheduling/locationBookings/util.js
+++ packages/web/app/views/scheduling/locationBookings/util.js
@@ -2,15 +2,13 @@
 export const partitionAppointmentsByLocation = appointments =>
   appointments.reduce((acc, appt) => {
     const locationId = appt.locationId;
-    if (!acc[locationId]) acc[locationId] = [];
-    acc[locationId].push(appt);
+    (acc[locationId] || (acc[locationId] = [])).push(appt);
     return acc;
   }, {});
 
 export const partitionAppointmentsByDate = appointments =>
   appointments.reduce((acc, appt) => {
     const date = appt.startTime.slice(0, 10); // Slice out ISO date, no time
-    if (!acc[date]) acc[date] = [];
-    acc[date].push(appt);
+    (acc[date] || (acc[date] = [])).push(appt);
     return acc;
   }, {});
