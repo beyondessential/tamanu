0c1d74ad40f23190708bee73bafcc7fc511c21f2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-10-08 14:10:25 +1300

    remove dead util

--- packages/shared/src/utils/dateTime.js
+++ packages/shared/src/utils/dateTime.js
@@ -252,12 +252,6 @@ export const formatLong = date =>
     'Date information not available',
   ); // "Thursday, 14 July 2022, 03:44 pm"
 
-/** @returns {boolean} `true` if two dates lie on the same day in local time, else `false`. */
-export const areSameDay = (date1, date2) =>
-  date1.getDate() === date2.getDate() &&
-  date1.getMonth() === date2.getMonth() &&
-  date1.getFullYear() === date2.getFullYear();
-
 /**
  * Returns the number if distinct ISO weeks spanned by the month of the given date. This can only be
  * 4, 5 or 6.
