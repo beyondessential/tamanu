8dbfae843981874d3d1cc537b4911ff58ce8ca64
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-10-08 17:56:29 +1300

    simplify `MonthYearInput `

--- packages/web/app/components/Field/CalendarField.jsx
+++ packages/web/app/components/Field/CalendarField.jsx
@@ -71,16 +71,14 @@ const StyledPopper = styled(Popper)`
 `;
 
 export const MonthYearInput = ({
-  minDate: propMinDate,
-  maxDate: propMaxDate,
-  name,
+  defaultValue = startOfToday(),
+  minDate = getMinDate(),
+  maxDate = getMaxDate(),
   value,
-  onChange = () => {},
+  onChange,
   ...props
 }) => {
   const [open, setOpen] = useState(false);
-  const minDate = propMinDate || getMinDate();
-  const maxDate = useMemo(() => propMaxDate || getMaxDate(), [propMaxDate]);
   return (
     <DatePicker
       onOpen={() => setOpen(true)}
@@ -88,7 +86,7 @@ export const MonthYearInput = ({
       views={['month', 'year']}
       yearsPerRow={4}
       monthsPerRow={4}
-      defaultValue={new Date()}
+      defaultValue={defaultValue}
       slots={{
         openPickerIcon: open ? StyledExpandLess : StyledExpandMore,
         switchViewButton: StyledExpandLess,
@@ -101,9 +99,7 @@ export const MonthYearInput = ({
           ...props,
         },
       }}
-      onAccept={date => {
-        onChange({ target: { value: date, name } });
-      }}
+      onAccept={date => onChange?.(date)}
       minDate={minDate}
       maxDate={maxDate}
       value={value}
