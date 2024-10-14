2844fb897264338ad73b3c0aa179508980653ebe
Daniel Nash <dannash200@gmail.com>
2024-10-15 08:07:25 +1300

    nass-1378: calender looking good

--- packages/web/app/views/scheduling/DateSelector.jsx
+++ packages/web/app/views/scheduling/DateSelector.jsx
@@ -6,14 +6,16 @@ import {
   eachDayOfInterval,
   endOfMonth,
   isToday,
+  isWeekend,
   startOfMonth,
   subMonths,
 } from 'date-fns';
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { BodyText } from '../../components';
 import { Colors } from '../../constants';
 
 const DayWrapper = styled(Box)`
+  cursor: pointer;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -29,14 +31,19 @@ const DayWrapper = styled(Box)`
   }
 `;
 
-const DayButton = ({ day, selected, isToday }) => {
+const WeekdayText = styled(BodyText)`
+  color: ${({ $selected, $isWeekend }) =>
+    $selected ? Colors.white : $isWeekend ? Colors.softText : 'inherit'};
+`;
+
+const DayButton = ({ day, selected, onClick }) => {
   return (
-    <DayWrapper $selected={selected} $isToday={isToday}>
-      <BodyText color={selected ? Colors.white : 'textTertiary'}>
+    <DayWrapper onClick={onClick} $selected={selected} $isToday={isToday(day)}>
+      <WeekdayText $selected={selected} $isWeekend={isWeekend(day)}>
         {day.toLocaleDateString('default', {
           weekday: 'narrow',
         })}
-      </BodyText>
+      </WeekdayText>
       <BodyText>{day.getDate()}</BodyText>
     </DayWrapper>
   );
@@ -50,42 +57,30 @@ const getMonthInterval = date =>
 
 export const DateSelector = ({ date = new Date() }) => {
   const [days, setDays] = useState(getMonthInterval(date));
-  const [selectedIndex, setSelectedIndex] = useState(4);
-
-  const selectedDate = useMemo(() => days[selectedIndex], [days, selectedIndex]);
+  const [selectedDate, setSelectedDate] = useState(days.find(isToday));
 
   const handleDecrement = async () => {
-    if (selectedIndex > 0) {
-      setSelectedIndex(selectedIndex - 1);
-      return;
-    }
-    const newDays = getMonthInterval(subMonths(days[0], 1));
-    setDays(newDays);
-    setSelectedIndex(newDays.length - 1);
+    setDays(getMonthInterval(subMonths(days[0], 1)));
   };
 
   const handleIncrement = () => {
-    if (selectedIndex < days.length - 1) {
-      setSelectedIndex(selectedIndex + 1);
-      return;
-    }
     setDays(getMonthInterval(addMonths(days[0], 1)));
-    setSelectedIndex(0);
   };
 
   return (
     <div>
       <h1>DateSelector</h1>
+      <h2>Month {days[0].toLocaleString('default', { month: 'long', year: 'numeric' })}</h2>
       <Box display="flex" alignItems="center" gap="5px">
         <IconButton onClick={handleDecrement}>
           <ArrowBackIos />
         </IconButton>
-        {days.map((day, index) => (
+        {days.map(day => (
           <DayButton
             key={`day-${day.getTime()}`}
-            isToday={isToday(day)}
             day={day}
-            selected={index === selectedIndex}
+            selected={selectedDate.getTime() === day.getTime()}
+            onClick={() => setSelectedDate(day)}
           />
         ))}
         <IconButton onClick={handleIncrement}>
