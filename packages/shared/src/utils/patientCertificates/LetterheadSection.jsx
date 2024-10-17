c60ed05b377563dc44acdad0815733603fc2b2bd
huytranbes <160376020+huytranbes@users.noreply.github.com>
2024-10-17 14:45:47 +1100

    feat: EPI-990: Add ability to print program survey response (#6473)
    * feat: add ability to print program survey response
    
    * add subtitle to survey responses pdf
    
    * handle Photo type survey questions and optimize code
    
    * lint issue
    
    * optimize column height
    
    * add subname to multipage header
    
    * address feedback
    
    * lint issue
    
    * add Print button to the Form response modal
    
    * fix: last response item not showing up
    
    * address testing feedback
    
    * add Close button and ensure scrolling only for modal content
    
    * fix the answer is truncated when it is too long
    
    * change pdf design
    
    ---------
    
    Co-authored-by: Da-ntp <105630649+Da-ntp@users.noreply.github.com>

--- packages/shared/src/utils/patientCertificates/LetterheadSection.jsx
+++ packages/shared/src/utils/patientCertificates/LetterheadSection.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { CertificateLogo } from './Layout';
-import { CertificateAddress, CertificateTitle } from './Typography';
+import { CertificateAddress, CertificateTitle, CertificateSubtitle } from './Typography';
 
-export const LetterheadSection = ({ logoSrc, certificateTitle, letterheadConfig }) => {
+export const LetterheadSection = ({ logoSrc, certificateTitle, certificateSubtitle, letterheadConfig }) => {
   const { title, subTitle } = letterheadConfig;
   return (
     <>
       {logoSrc && <CertificateLogo logoSrc={logoSrc} />}
       <CertificateAddress>{`${title}\n${subTitle}`}</CertificateAddress>
       <CertificateTitle>{certificateTitle}</CertificateTitle>
+      {certificateSubtitle && <CertificateSubtitle>{certificateSubtitle}</CertificateSubtitle>}
     </>
   );
 };
