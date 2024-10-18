77327a24f84f60e760791672a15d508d78c3105f
Takayuki <12975677+rail-rain@users.noreply.github.com>
2024-10-19 09:33:12 +1300

    ci(dbmodel): KAM-282: add check for any remaining todos in source models (#6574)

---
+++ packages/scripts/src/check-todo-dbt-source.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+// A script to check any todo items in the database source model
+
+const fs = require('node:fs/promises');
+const path = require('node:path');
+const { exit } = require('node:process');
+const { readTablesFromDbt, readTableDoc } = require('./dbt-generate-source.js');
+
+async function getSchemaPaths(packageName) {
+  const packagePath = path.join('database/model', packageName);
+  return (await fs.readdir(packagePath)).map(schemaName => path.join(packagePath, schemaName));
+}
+
+async function detectTodoItemsInTable(schemaPath, dbtSrc) {
+  const table = dbtSrc.sources[0].tables[0];
+  const isDescriptionTodo = dbtSrc.sources[0].description === 'TODO';
+  if (isDescriptionTodo) {
+    console.log(`TODO: table description for ${table.name}, in ${schemaPath}/${table.name}.yml'`);
+  }
+
+  const doc = await readTableDoc(schemaPath, table.name);
+  if (doc === null) return;
+
+  const isDocTodo = doc.description === 'TODO';
+  if (isDocTodo) {
+    console.log(`TODO: table documentation for ${table.name}, in ${schemaPath}/${table.name}.md`);
+  }
+
+  const todoColumnDocs = doc.columns.filter(c => c.description === 'TODO');
+  for (const column of todoColumnDocs) {
+    console.log(
+      `TODO: column documentation for ${table.name}:${column.name}, in ${schemaPath}/${table.name}.md`,
+    );
+  }
+
+  return isDescriptionTodo || isDocTodo || todoColumnDocs.length !== 0;
+}
+
+async function run(packageName) {
+  const promises = (await getSchemaPaths(packageName)).map(async schemaPath => {
+    const detectPromises = (await readTablesFromDbt(schemaPath)).map(s =>
+      detectTodoItemsInTable(schemaPath, s),
+    );
+    return (await Promise.all(detectPromises)).some(a => a);
+  });
+  return (await Promise.all(promises)).some(a => a);
+}
+
+(async function() {
+  const promises = [run('central-server'), run('facility-server')];
+  const detected = (await Promise.all(promises)).some(a => a);
+  if (detected) {
+    exit(1);
+  }
+})();
