36547653ebf91c08635289ac5d9dd519144524a3
Daniel Nash <dannash200@gmail.com>
2024-10-22 12:08:11 +1300

    yes

--- packages/web/app/components/Appointments/AppointmentForm.jsx
+++ packages/web/app/components/Appointments/AppointmentForm.jsx
@@ -22,7 +22,7 @@ export const AppointmentForm = props => {
   if (isUpdating) {
     initialValues = {
       patientId: appointment.patientId,
-      type: appointment.typeId,
+      typeId: appointment.typeId,
       startTime: appointment.startTime,
       endTime: appointment.endTime,
       clinicianId: appointment.clinicianId,
