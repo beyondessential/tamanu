fc2d7a7b604dce99aabdc675cb3da539dcf783d6
Chris Tran <chris@beyondessential.com.au>
2024-10-23 06:57:11 +1100

    sav-646: test change

--- packages/constants/src/auth.ts
+++ packages/constants/src/auth.ts
@@ -7,4 +7,4 @@ export const SYSTEM_USER_UUID = '00000000-0000-0000-0000-000000000000';
 
 export const CAN_ACCESS_ALL_FACILITIES = 'ALL';
 
-export const LOGIN_HEADER = 'Enter your details below to log in';
+export const LOGIN_HEADER = 'Enter your detailssss below to log in';
