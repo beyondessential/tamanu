b25b9675c9d79e748a9526917770c4ed98069c8c
Chris Tran <chris@beyondessential.com.au>
2024-10-24 12:34:06 +1100

    sav-646: reverted order

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -47,8 +47,8 @@ export default async ({ mode }) => {
           NODE_ENV: process.env.NODE_ENV,
           REVISION: revision,
         },
-        platform: 'web',
         arch: 'wasm',
+        platform: 'web',
       }),
     },
     worker: {
