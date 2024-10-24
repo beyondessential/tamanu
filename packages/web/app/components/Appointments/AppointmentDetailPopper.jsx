2d50dc0de1cf4831f6542ba2952c3e7da36e23ea
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-10-25 09:48:59 +1300

    `$deselected` → `$selected`

--- packages/web/app/components/Appointments/AppointmentDetailPopper.jsx
+++ packages/web/app/components/Appointments/AppointmentDetailPopper.jsx
@@ -225,20 +225,24 @@ const AppointDetailsDisplay = ({ appointment, isOvernight }) => {
   );
 };
 
-const AppointmentStatusDisplay = ({ selectedStatus, updateAppointmentStatus }) => {
+export const AppointmentStatusSelector = ({ selectedStatus, updateAppointmentStatus }) => {
   return (
     <AppointmentStatusContainer role="radiogroup">
       {APPOINTMENT_STATUS_VALUES.filter(status => status != APPOINTMENT_STATUSES.CANCELLED).map(
-        status => (
-          <AppointmentStatusChip
-            appointmentStatus={status}
-            aria-checked={status === selectedStatus}
-            deselected={status !== selectedStatus}
-            key={status}
-            onClick={() => updateAppointmentStatus(status)}
-            role="radio"
-          />
-        ),
+        status => {
+          const isSelected = status === selectedStatus;
+          return (
+            <AppointmentStatusChip
+              appointmentStatus={status}
+              aria-checked={isSelected}
+              disabled={isSelected}
+              key={status}
+              onClick={() => updateAppointmentStatus(status)}
+              role="radio"
+              selected={isSelected}
+            />
+          );
+        },
       )}
     </AppointmentStatusContainer>
   );
@@ -262,7 +266,7 @@ export const AppointmentDetailPopper = ({
     dispatch(push(`/patients/all/${patientId}`));
   }, [dispatch, patientId]);
 
-  const debouncedUpdateAppointmentSatus = useMemo(
+  const debouncedUpdateAppointmentStatus = useMemo(
     () =>
       debounce(async newValue => {
         try {
@@ -287,9 +291,9 @@ export const AppointmentDetailPopper = ({
   const updateAppointmentStatus = useCallback(
     newValue => {
       setLocalStatus(newValue);
-      debouncedUpdateAppointmentSatus(newValue);
+      debouncedUpdateAppointmentStatus(newValue);
     },
-    [debouncedUpdateAppointmentSatus],
+    [debouncedUpdateAppointmentStatus],
   );
 
   return (
@@ -316,7 +320,7 @@ export const AppointmentDetailPopper = ({
               onClick={handlePatientDetailsClick}
             />
             <AppointDetailsDisplay appointment={appointment} isOvernight={isOvernight} />
-            <AppointmentStatusDisplay
+            <AppointmentStatusSelector
               selectedStatus={localStatus}
               updateAppointmentStatus={updateAppointmentStatus}
             />
