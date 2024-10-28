0d39fedb974e3cc4d8e1c3522efc7497841a2ca2
Daniel Nash <dannash200@gmail.com>
2024-10-29 11:48:08 +1300

    uhuh

--- packages/web/app/views/scheduling/DateSelector.jsx
+++ packages/web/app/views/scheduling/DateSelector.jsx
@@ -22,7 +22,6 @@ import { Colors } from '../../constants';
 const Wrapper = styled(Box)`
   display: flex;
   align-items: center;
-  // justify-content: space-between;
   padding: 0.3rem 0.48rem;
   gap: 0.5rem;
 `;
