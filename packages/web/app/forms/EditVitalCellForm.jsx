4a8c6a6f904ac0ce5754a8549b6d177a7c8bd72d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-10-29 14:38:10 +1300

    fix(vitals): Release fix v2.18: Missed facilityIds for vitals endpoints (#6636)
    * send facility id with endpoint requests
    
    * Update packages/web/app/forms/EditVitalCellForm.jsx
    
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jasper Lai <33956381+jaskfla@users.noreply.github.com>

--- packages/web/app/forms/EditVitalCellForm.jsx
+++ packages/web/app/forms/EditVitalCellForm.jsx
@@ -5,18 +5,8 @@ import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
 import { useQueryClient } from '@tanstack/react-query';
 import { PROGRAM_DATA_ELEMENT_TYPES } from '@tamanu/constants';
 import { getCurrentDateTimeString } from '@tamanu/shared/utils/dateTime';
-import {
-	DateDisplay,
-	FormSeparatorLine,
-	FormSubmitCancelRow,
-	TranslatedText,
-} from '../components';
-import {
-	BaseSelectField,
-	Field,
-	Form,
-	OuterLabelFieldWrapper,
-} from '../components/Field';
+import { DateDisplay, FormSeparatorLine, FormSubmitCancelRow, TranslatedText } from '../components';
+import { BaseSelectField, Field, Form, OuterLabelFieldWrapper } from '../components/Field';
 import { FormGrid } from '../components/FormGrid';
 import { SurveyQuestion } from '../components/Surveys';
 import { getValidationSchema } from '../utils';
@@ -25,6 +15,7 @@ import { useApi } from '../api';
 import { useEncounter } from '../contexts/Encounter';
 import { useSettings } from '../contexts/Settings';
 import { useTranslation } from '../contexts/Translation';
+import { useAuth } from '../contexts/Auth';
 
 const Text = styled(Typography)`
   font-size: 14px;
@@ -34,34 +25,25 @@ const Text = styled(Typography)`
 `;
 
 const DeleteEntryButton = ({ disabled, onClick }) => (
-	<Box display="flex" alignSelf="start" marginTop="18px">
-		<IconButton
-			color="primary"
-			edge="start"
-			disabled={disabled}
-			onClick={onClick}
-			disableRipple
-		>
-			<DeleteOutlineIcon fontSize="small" />
-			<Text>
-				<TranslatedText
-					stringId="encounter.vitals.action.deleteEntry"
-					fallback="Delete entry"
-				/>
-			</Text>
-		</IconButton>
-	</Box>
+  <Box display="flex" alignSelf="start" marginTop="18px">
+    <IconButton color="primary" edge="start" disabled={disabled} onClick={onClick} disableRipple>
+      <DeleteOutlineIcon fontSize="small" />
+      <Text>
+        <TranslatedText stringId="encounter.vitals.action.deleteEntry" fallback="Delete entry" />
+      </Text>
+    </IconButton>
+  </Box>
 );
 
 const getEditVitalData = (vitalComponent, mandatoryVitalEditReason) => {
-	const reasonForChangeMockComponent = {
-		dataElement: { type: PROGRAM_DATA_ELEMENT_TYPES.SELECT },
-		validationCriteria: JSON.stringify({ mandatory: mandatoryVitalEditReason }),
-		dataElementId: 'reasonForChange',
-	};
-	const editVitalData = [reasonForChangeMockComponent];
-	if (vitalComponent) editVitalData.push(vitalComponent);
-	return { components: editVitalData };
+  const reasonForChangeMockComponent = {
+    dataElement: { type: PROGRAM_DATA_ELEMENT_TYPES.SELECT },
+    validationCriteria: JSON.stringify({ mandatory: mandatoryVitalEditReason }),
+    dataElementId: 'reasonForChange',
+  };
+  const editVitalData = [reasonForChangeMockComponent];
+  if (vitalComponent) editVitalData.push(vitalComponent);
+  return { components: editVitalData };
 };
 
 const LogContainer = styled(Box)`
@@ -83,167 +65,150 @@ const LogTextSmall = styled(Typography)`
 `;
 
 const HistoryLog = ({ logData, vitalLabel, vitalEditReasons }) => {
-	const { date, newValue, reasonForChange, userDisplayName } = logData;
-	const reasonForChangeOption = vitalEditReasons.find(
-		(option) => option.value === reasonForChange,
-	);
-	const reasonForChangeLabel = reasonForChangeOption?.label ?? 'Unknown';
-	return (
-		<LogContainer>
-			<LogText>
-				{vitalLabel}: {newValue === '' ? 'Entry deleted' : newValue}
-			</LogText>
-			{reasonForChange && (
-				<LogText>
-					<TranslatedText
-						stringId="encounter.vitals.editReason.label"
-						fallback="Reason for change to record"
-					/>
-					: {reasonForChangeLabel}
-				</LogText>
-			)}
-			<LogTextSmall>
-				{userDisplayName} <DateDisplay date={date} showTime shortYear />
-			</LogTextSmall>
-		</LogContainer>
-	);
+  const { date, newValue, reasonForChange, userDisplayName } = logData;
+  const reasonForChangeOption = vitalEditReasons.find(option => option.value === reasonForChange);
+  const reasonForChangeLabel = reasonForChangeOption?.label ?? 'Unknown';
+  return (
+    <LogContainer>
+      <LogText>
+        {vitalLabel}: {newValue === '' ? 'Entry deleted' : newValue}
+      </LogText>
+      {reasonForChange && (
+        <LogText>
+          <TranslatedText
+            stringId="encounter.vitals.editReason.label"
+            fallback="Reason for change to record"
+          />
+          : {reasonForChangeLabel}
+        </LogText>
+      )}
+      <LogTextSmall>
+        {userDisplayName} <DateDisplay date={date} showTime shortYear />
+      </LogTextSmall>
+    </LogContainer>
+  );
 };
 
 export const EditVitalCellForm = ({ vitalLabel, dataPoint, handleClose }) => {
-	const { getTranslation } = useTranslation();
-	const [isDeleted, setIsDeleted] = useState(false);
-	const api = useApi();
-	const queryClient = useQueryClient();
-	const { encounter } = useEncounter();
+  const { getTranslation } = useTranslation();
+  const [isDeleted, setIsDeleted] = useState(false);
+  const api = useApi();
+  const queryClient = useQueryClient();
+  const { encounter } = useEncounter();
+  const { facilityId } = useAuth();
 
-	const { getSetting } = useSettings();
-	const mandatoryVitalEditReason = getSetting(
-		'features.mandatoryVitalEditReason',
-	);
-	const vitalEditReasons = getSetting('vitalEditReasons');
+  const { getSetting } = useSettings();
+  const mandatoryVitalEditReason = getSetting('features.mandatoryVitalEditReason');
+  const vitalEditReasons = getSetting('vitalEditReasons');
 
-	const initialValue = dataPoint.value;
-	const showDeleteEntryButton =
-		['', undefined].includes(initialValue) === false;
-	const valueName = dataPoint.component.dataElement.id;
-	const editVitalData = getEditVitalData(
-		dataPoint.component,
-		mandatoryVitalEditReason,
-	);
-	const validationSchema = getValidationSchema(editVitalData, getTranslation, {
-		encounterType: encounter.encounterType,
-	});
-	const handleDeleteEntry = useCallback(
-		(setFieldValue) => {
-			setFieldValue(valueName, '');
-			setIsDeleted(true);
-		},
-		[valueName],
-	);
-	const handleSubmit = async (data) => {
-		const newShapeData = {
-			date: getCurrentDateTimeString(),
-		};
-		Object.entries(data).forEach(([key, value]) => {
-			if (key === valueName) newShapeData.newValue = value;
-			else newShapeData[key] = value;
-		});
+  const initialValue = dataPoint.value;
+  const showDeleteEntryButton = !['', undefined].includes(initialValue);
+  const valueName = dataPoint.component.dataElement.id;
+  const editVitalData = getEditVitalData(dataPoint.component, mandatoryVitalEditReason);
+  const validationSchema = getValidationSchema(editVitalData, getTranslation, {
+    encounterType: encounter.encounterType,
+  });
+  const handleDeleteEntry = useCallback(
+    setFieldValue => {
+      setFieldValue(valueName, '');
+      setIsDeleted(true);
+    },
+    [valueName],
+  );
+  const handleSubmit = async data => {
+    const newShapeData = {
+      date: getCurrentDateTimeString(),
+    };
+    Object.entries(data).forEach(([key, value]) => {
+      if (key === valueName) newShapeData.newValue = value;
+      else newShapeData[key] = value;
+    });
 
-		// The survey response answer might not exist
-		if (dataPoint.answerId) {
-			await api.put(
-				`surveyResponseAnswer/vital/${dataPoint.answerId}`,
-				newShapeData,
-			);
-		} else {
-			const newVitalData = {
-				...newShapeData,
-				dataElementId: valueName,
-				encounterId: encounter.id,
-				recordedDate: dataPoint.recordedDate,
-			};
-			await api.post('surveyResponseAnswer/vital', newVitalData);
-		}
-		queryClient.invalidateQueries(['encounterVitals', encounter.id]);
-		handleClose();
-	};
-	const validateFn = (values) => {
-		const errors = {};
-		if (values[valueName] === initialValue) {
-			errors[valueName] = 'New value cannot be the same as previous value.';
-		}
-		return errors;
-	};
+    // The survey response answer might not exist
+    if (dataPoint.answerId) {
+      await api.put(`surveyResponseAnswer/vital/${dataPoint.answerId}`, {
+        facilityId,
+        ...newShapeData,
+      });
+    } else {
+      const newVitalData = {
+        ...newShapeData,
+        dataElementId: valueName,
+        encounterId: encounter.id,
+        recordedDate: dataPoint.recordedDate,
+      };
+      await api.post('surveyResponseAnswer/vital', { facilityId, ...newVitalData });
+    }
+    queryClient.invalidateQueries(['encounterVitals', encounter.id]);
+    handleClose();
+  };
+  const validateFn = values => {
+    const errors = {};
+    if (values[valueName] === initialValue) {
+      errors[valueName] = 'New value cannot be the same as previous value.';
+    }
+    return errors;
+  };
 
-	return (
-		<Form
-			onSubmit={handleSubmit}
-			validationSchema={validationSchema}
-			initialValues={{ [valueName]: initialValue }}
-			formType={FORM_TYPES.EDIT_FORM}
-			validate={validateFn}
-			render={({ setFieldValue, submitForm }) => (
-				<FormGrid columns={4}>
-					<SurveyQuestion
-						component={dataPoint.component}
-						disabled={isDeleted}
-					/>
-					{showDeleteEntryButton && (
-						<DeleteEntryButton
-							disabled={isDeleted}
-							onClick={() => handleDeleteEntry(setFieldValue)}
-						/>
-					)}
-					<Field
-						required={mandatoryVitalEditReason}
-						component={BaseSelectField}
-						label={
-							<TranslatedText
-								stringId="encounter.vitals.editReason.label"
-								fallback="Reason for change to record"
-							/>
-						}
-						name="reasonForChange"
-						options={vitalEditReasons}
-						style={{ gridColumn: '1 / 4' }}
-					/>
-					<FormSeparatorLine />
-					<OuterLabelFieldWrapper
-						label={
-							<TranslatedText
-								stringId="encounter.vitals.history.label"
-								fallback="History"
-							/>
-						}
-						style={{ gridColumn: '1 / -1' }}
-					>
-						<Box
-							height="162px"
-							overflow="auto"
-							padding="13px 12px 13px 15px"
-							bgcolor="white"
-							border="1px solid #dedede"
-							borderRadius="3px"
-						>
-							{dataPoint.historyLogs.map((log) => (
-								<HistoryLog
-									key={log.date}
-									vitalLabel={vitalLabel}
-									vitalEditReasons={vitalEditReasons}
-									logData={log}
-								/>
-							))}
-						</Box>
-					</OuterLabelFieldWrapper>
-					<FormSubmitCancelRow
-						onCancel={handleClose}
-						onConfirm={submitForm}
-						confirmText={
-							<TranslatedText stringId="general.action.save" fallback="Save" />
-						}
-					/>
-				</FormGrid>
-			)}
-		/>
-	);
+  return (
+    <Form
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+      initialValues={{ [valueName]: initialValue }}
+      formType={FORM_TYPES.EDIT_FORM}
+      validate={validateFn}
+      render={({ setFieldValue, submitForm }) => (
+        <FormGrid columns={4}>
+          <SurveyQuestion component={dataPoint.component} disabled={isDeleted} />
+          {showDeleteEntryButton && (
+            <DeleteEntryButton
+              disabled={isDeleted}
+              onClick={() => handleDeleteEntry(setFieldValue)}
+            />
+          )}
+          <Field
+            required={mandatoryVitalEditReason}
+            component={BaseSelectField}
+            label={
+              <TranslatedText
+                stringId="encounter.vitals.editReason.label"
+                fallback="Reason for change to record"
+              />
+            }
+            name="reasonForChange"
+            options={vitalEditReasons}
+            style={{ gridColumn: '1 / 4' }}
+          />
+          <FormSeparatorLine />
+          <OuterLabelFieldWrapper
+            label={<TranslatedText stringId="encounter.vitals.history.label" fallback="History" />}
+            style={{ gridColumn: '1 / -1' }}
+          >
+            <Box
+              height="162px"
+              overflow="auto"
+              padding="13px 12px 13px 15px"
+              bgcolor="white"
+              border="1px solid #dedede"
+              borderRadius="3px"
+            >
+              {dataPoint.historyLogs.map(log => (
+                <HistoryLog
+                  key={log.date}
+                  vitalLabel={vitalLabel}
+                  vitalEditReasons={vitalEditReasons}
+                  logData={log}
+                />
+              ))}
+            </Box>
+          </OuterLabelFieldWrapper>
+          <FormSubmitCancelRow
+            onCancel={handleClose}
+            onConfirm={submitForm}
+            confirmText={<TranslatedText stringId="general.action.save" fallback="Save" />}
+          />
+        </FormGrid>
+      )}
+    />
+  );
 };
