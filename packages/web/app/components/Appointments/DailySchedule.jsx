ce2d5dd208b8f89e3ecde2a2e3ab59698c67504c
Daniel Nash <dannash200@gmail.com>
2024-10-31 16:31:05 +1300

    yep

--- packages/web/app/components/Appointments/DailySchedule.jsx
+++ packages/web/app/components/Appointments/DailySchedule.jsx
@@ -67,7 +67,7 @@ export const DailySchedule = ({
         if (!appointmentType.length) {
           return true;
         }
-        return appointmentType.includes(appointment.type?.id);
+        return appointmentType.includes(appointment.appointmentType?.id);
       });
       return {
         header,
