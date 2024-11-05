c44397ff71a73a2730c70b1d9ba93efbb47eb5e0
Flynn <flynn.teh@gmail.com>
2024-11-05 14:12:42 +1300

    feat: clear button

--- packages/web/app/views/scheduling/locationBookings/CalendarSearchBar.jsx
+++ packages/web/app/views/scheduling/locationBookings/CalendarSearchBar.jsx
@@ -1,15 +1,14 @@
 import React, { useEffect } from 'react';
 
 import { styled } from '@mui/material';
-import { Field, Form, SearchField } from '../../../components';
+import { Field, Form, SearchField, TextButton, TranslatedText } from '../../../components';
 import { useTranslation } from '../../../contexts/Translation';
-import { SuggesterSearchMultiSelectField } from '../../../components/Field/SearchMultiSelectField';
 import { Formik, useFormikContext } from 'formik';
 import { FilterField } from '../../../components/Field/FilterField';
 
 const SearchBar = styled('search')`
   display: flex;
-  gap: 1rem;
+  gap: 0.625rem;
 `;
 
 const FormListener = ({ onFilterChange }) => {
@@ -24,44 +23,51 @@ export const CalendarSearchBar = ({ onFilterChange }) => {
   const { getTranslation } = useTranslation();
 
   return (
-    <Formik enableReinitialize>
-      <Form
-        onSubmit={async () => {}}
-        render={() => (
-          <>
-            <FormListener onFilterChange={onFilterChange} />
-            <SearchBar>
-              <Field
-                name="patientNameOrId"
-                component={SearchField}
-                placeholder={getTranslation(
-                  'scheduling.filter.placeholder.patientNameOrId',
-                  'Search patient name or ID',
-                )} // Avoids [object Object] in the placeholder
-                style={{ width: '18.75rem' }}
-              />
-              <Field
-                name="locationGroupIds"
-                label={getTranslation('general.area.label', 'Area')}
-                component={FilterField}
-                endpoint="bookableLocationGroup"
-              />
-              <Field
-                name="clinicianId"
-                label={getTranslation('general.localisedField.clinician.label.short', 'Clinician')}
-                component={FilterField}
-                endpoint="practitioner"
-              />
-              <Field
-                name="bookingTypeId"
-                label={getTranslation('general.type.label', 'Type')}
-                component={FilterField}
-                endpoint="bookingType"
-              />
-            </SearchBar>
-          </>
-        )}
-      />
-    </Formik>
+    <Form
+      onSubmit={async () => {}}
+      render={({ clearForm }) => (
+        <>
+          <FormListener onFilterChange={onFilterChange} />
+          <SearchBar>
+            <Field
+              name="patientNameOrId"
+              component={SearchField}
+              placeholder={getTranslation(
+                'scheduling.filter.placeholder.patientNameOrId',
+                'Search patient name or ID',
+              )} // Avoids [object Object] in the placeholder
+              style={{ width: '18.75rem' }}
+            />
+            <Field
+              name="locationGroupIds"
+              label={getTranslation('general.area.label', 'Area')}
+              component={FilterField}
+              endpoint="bookableLocationGroup"
+            />
+            <Field
+              name="clinicianId"
+              label={getTranslation('general.localisedField.clinician.label.short', 'Clinician')}
+              component={FilterField}
+              endpoint="practitioner"
+            />
+            <Field
+              name="bookingTypeId"
+              label={getTranslation('general.type.label', 'Type')}
+              component={FilterField}
+              endpoint="bookingType"
+            />
+            <TextButton
+              onClick={() => {
+                console.log(clearForm);
+                clearForm();
+              }}
+              style={{ textDecoration: 'underline', fontSize: '0.6875rem' }}
+            >
+              <TranslatedText stringId="general.action.clear" fallback="Clear" />
+            </TextButton>
+          </SearchBar>
+        </>
+      )}
+    />
   );
 };
