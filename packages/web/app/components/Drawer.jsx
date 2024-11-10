ee059529f091b25708bf3dd10810781f8f58d66d
Daniel Nash <dannash200@gmail.com>
2024-11-11 12:01:58 +1300

    rem

--- packages/web/app/components/Drawer.jsx
+++ packages/web/app/components/Drawer.jsx
@@ -7,20 +7,20 @@ import { Colors } from '../constants';
 import { TOP_BAR_HEIGHT } from './TopBar';
 
 const Container = styled.div`
-  width: 330px;
-  padding: 16px;
+  width: 20.625rem;
+  padding: 1rem;
   background-color: ${Colors.background};
   overflow-y: auto;
   position: relative;
 `;
 
 const Title = styled(Heading4)`
-  font-size: 16px;
-  margin-bottom: 9px;
+  font-size: 1rem;
+  margin-block-end: 0.563rem;
 `;
 
 const Description = styled(BodyText)`
-  font-size: 11px;
+  font-size: 0.688rem;
   color: ${Colors.midText};
 `;
 
