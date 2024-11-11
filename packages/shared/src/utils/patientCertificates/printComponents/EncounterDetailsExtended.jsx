0c8c03912f41d7695bdef26de3552d65dedd6085
Daniel Nash <dannash200@gmail.com>
2024-11-11 16:43:08 +1300

    consistently

--- packages/shared/src/utils/patientCertificates/printComponents/EncounterDetailsExtended.jsx
+++ packages/shared/src/utils/patientCertificates/printComponents/EncounterDetailsExtended.jsx
@@ -5,26 +5,45 @@ import React from 'react';
 import { formatShort } from '../../dateTime';
 import { useLanguageContext } from '../../pdf/languageContext';
 
-export const EncounterDetailsExtended = ({ encounter, discharge, clinicianText }) => {
+export const EncounterDetailsExtended = ({ encounter, discharge }) => {
   const { location, examiner, department, startDate, endDate, reasonForEncounter } = encounter;
   const { getTranslation } = useLanguageContext();
+
+  const clinicianText = getTranslation(
+    'general.localisedField.clinician.label.short',
+    'Clinician',
+    null,
+    false,
+    true,
+  );
   return (
     <DataSection title="Encounter details" hideBottomRule={true}>
       <Col>
-        <DataItem label="Facility" value={location.facility.name} key="facility" />
         <DataItem
-          label={`Supervising ${clinicianText.toLowerCase()}`}
+          label={getTranslation('general.localisedField.facility.label', 'Facility')}
+          value={location.facility.name}
+          key="facility"
+        />
+        <DataItem
+          label={getTranslation('general.supervisingClinician.label', 'Supervising :clinician', {
+            clinician: clinicianText,
+          })}
           value={examiner.displayName}
           key="supervisingClinician"
         />
         <DataItem
-          label={`Discharging ${clinicianText.toLowerCase()}`}
+          label={getTranslation('general.dischargingClinician.label', 'Discharging :clinician', {
+            clinician: clinicianText,
+          })}
           value={discharge?.discharger?.displayName}
           key="dischargingClinician"
         />
         {discharge?.disposition?.name && (
           <DataItem
-            label="Discharge disposition"
+            label={getTranslation(
+              'general.localisedField.dischargeDisposition.label',
+              'Discharge disposition',
+            )}
             value={discharge?.disposition?.name}
             key="dischargeDisposition"
           />
@@ -36,8 +55,16 @@ export const EncounterDetailsExtended = ({ encounter, discharge, clinicianText }
           value={department.name}
           key="department"
         />
-        <DataItem label="Date of admission" value={formatShort(startDate)} key="dateOfAdmission" />
-        <DataItem label="Date of discharge" value={formatShort(endDate)} key="dateOfDischarge" />
+        <DataItem
+          label={getTranslation('encounter.dateOfAdmission.label', 'Date of admission')}
+          value={formatShort(startDate)}
+          key="dateOfAdmission"
+        />
+        <DataItem
+          label={getTranslation('encounter.dateOfDischarge.label', 'Date of discharge')}
+          value={formatShort(endDate)}
+          key="dateOfDischarge"
+        />
       </Col>
       <DataItem
         label={getTranslation('encounter.reasonForEncounter.label', 'Reason for encounter')}
