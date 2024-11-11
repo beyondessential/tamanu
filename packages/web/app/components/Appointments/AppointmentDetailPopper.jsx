2ccaa40dc5f67e9613952d41ad6cea72d3ad3eef
Daniel Nash <dannash200@gmail.com>
2024-11-12 10:57:33 +1300

    fix regression

--- packages/web/app/components/Appointments/AppointmentDetailPopper.jsx
+++ packages/web/app/components/Appointments/AppointmentDetailPopper.jsx
@@ -162,7 +162,7 @@ const BookingTypeDisplay = ({ bookingType, isOvernight }) => (
         <TranslatedReferenceData
           value={bookingType.id}
           fallback={bookingType.name}
-          category="appointmentType"
+          category="bookingType"
         />
         {isOvernight && (
           <FlexRow sx={{ gap: '2px' }}>
@@ -269,7 +269,7 @@ const AppointmentDetailsDisplay = ({ appointment, isOvernight }) => {
           }
         />
       )}
-      {bookingType && <BookingTypeDisplay type={bookingType} isOvernight={isOvernight} />}
+      {bookingType && <BookingTypeDisplay bookingType={bookingType} isOvernight={isOvernight} />}
       {appointmentType && (
         <DetailsDisplay
           label={
