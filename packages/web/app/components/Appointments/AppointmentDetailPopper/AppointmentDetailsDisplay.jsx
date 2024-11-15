7715391880c59e48b549af1031449ec819dfa08b
Flynn <flynn.teh@gmail.com>
2024-11-15 16:02:56 +1300

    merge

--- packages/web/app/components/Appointments/AppointmentDetailPopper/AppointmentDetailsDisplay.jsx
+++ packages/web/app/components/Appointments/AppointmentDetailPopper/AppointmentDetailsDisplay.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
 import Overnight from '@mui/icons-material/Brightness2';
+import { PriorityHigh as HighPriorityIcon } from '@material-ui/icons';
 
 import { Colors } from '../../../constants';
 import { TranslatedReferenceData, TranslatedText } from '../../Translation';
@@ -10,30 +11,92 @@ import { DetailsDisplay, FlexCol, FlexRow } from './SharedComponents';
 const AppointmentDetailsContainer = styled(FlexCol)`
   padding: 0.75rem;
   gap: 0.5rem;
-  border-top: max(0.0625rem, 1px) solid ${Colors.outline};
-  border-bottom: max(0.0625rem, 1px) solid ${Colors.outline};
+  border-block: max(0.0625rem, 1px) solid ${Colors.outline};
+  position: relative;
 `;
 
-const BookingTypeDisplay = ({ bookingType, isOvernight }) => (
-  <DetailsDisplay
-    label={<TranslatedText stringId="scheduling.bookingType.label" fallback="Booking type" />}
-    value={
-      <FlexRow sx={{ alignItems: 'center', justifyContent: 'space-between' }}>
-        <TranslatedReferenceData
-          value={bookingType.id}
-          fallback={bookingType.name}
-          category="bookingType"
+const Tag = styled(FlexRow)`
+  gap: 0.125rem;
+  position: absolute;
+  inset-inline-end: 0.75rem;
+  inset-block-end: 0.75rem;
+`;
+
+const LocationBookingDetails = ({ location, bookingType, isOvernight }) => {
+  return (
+    <>
+      {location && (
+        <DetailsDisplay
+          label={
+            <TranslatedText
+              stringId="general.localisedField.locationId.label"
+              fallback="Location"
+            />
+          }
+          value={
+            <TranslatedReferenceData
+              fallback={location?.name}
+              value={location?.id}
+              category="location"
+            />
+          }
         />
-        {isOvernight && (
-          <FlexRow sx={{ gap: '2px' }}>
-            <Overnight htmlColor={Colors.primary} sx={{ fontSize: 15 }} />
-            <TranslatedText stringId="scheduling.bookingType.overnight" fallback="Overnight" />
-          </FlexRow>
-        )}
-      </FlexRow>
-    }
-  />
-);
+      )}
+      {bookingType && (
+        <DetailsDisplay
+          label={<TranslatedText stringId="scheduling.bookingType.label" fallback="Booking type" />}
+          value={
+            <TranslatedReferenceData
+              value={bookingType.id}
+              fallback={bookingType.name}
+              category="bookingType"
+            />
+          }
+        />
+      )}
+      {isOvernight && (
+        <Tag>
+          <Overnight htmlColor={Colors.primary} sx={{ fontSize: 15 }} />
+          <TranslatedText stringId="scheduling.bookingType.overnight" fallback="Overnight" />
+        </Tag>
+      )}
+    </>
+  );
+};
+
+const AppointmentTypeDetails = ({ appointmentType, isHighPriority }) => {
+  return (
+    <>
+      {appointmentType && (
+        <DetailsDisplay
+          label={
+            <TranslatedText
+              stringId="appointment.appointmentType.label.short"
+              fallback="Appt type"
+            />
+          }
+          value={
+            <TranslatedReferenceData
+              value={appointmentType.id}
+              fallback={appointmentType.name}
+              category="appointmentType"
+            />
+          }
+        />
+      )}
+      {isHighPriority && (
+        <Tag>
+          <HighPriorityIcon
+            aria-label="High priority"
+            htmlColor={Colors.alert}
+            style={{ fontSize: 15 }}
+          />
+          <TranslatedText stringId="general.highPriority.label" fallback="High priority" />
+        </Tag>
+      )}
+    </>
+  );
+};
 
 export const AppointmentDetailsDisplay = ({ appointment, isOvernight }) => {
   const {
@@ -44,6 +107,7 @@ export const AppointmentDetailsDisplay = ({ appointment, isOvernight }) => {
     location,
     bookingType,
     appointmentType,
+    isHighPriority,
   } = appointment;
   return (
     <AppointmentDetailsContainer>
@@ -72,40 +136,19 @@ export const AppointmentDetailsDisplay = ({ appointment, isOvernight }) => {
           />
         }
       />
-      {location && (
-        <DetailsDisplay
-          label={
-            <TranslatedText
-              stringId="general.localisedField.locationId.label"
-              fallback="Location"
-            />
-          }
-          value={
-            <TranslatedReferenceData
-              fallback={location?.name}
-              value={location?.id}
-              category="location"
-            />
-          }
+
+      {/* Location booking specific data */}
+      {location && bookingType && (
+        <LocationBookingDetails
+          location={location}
+          bookingType={bookingType}
+          isOvernight={isOvernight}
         />
       )}
-      {bookingType && <BookingTypeDisplay bookingType={bookingType} isOvernight={isOvernight} />}
+
+      {/* Outpatient appointment specific data */}
       {appointmentType && (
-        <DetailsDisplay
-          label={
-            <TranslatedText
-              stringId="scheduling.appointmentType.label"
-              fallback="Appointment type"
-            />
-          }
-          value={
-            <TranslatedReferenceData
-              value={appointmentType.id}
-              fallback={appointmentType.name}
-              category="appointmentType"
-            />
-          }
-        />
+        <AppointmentTypeDetails appointmentType={appointmentType} isHighPriority={isHighPriority} />
       )}
     </AppointmentDetailsContainer>
   );
