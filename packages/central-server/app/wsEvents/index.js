727eff593ff6376679a4eed5e65277e732f546ec
Chris Tran <chris@beyondessential.com.au>
2024-11-19 10:41:42 +1100

    sav-845: added line

--- packages/central-server/app/wsEvents/index.js
+++ packages/central-server/app/wsEvents/index.js
@@ -1,5 +1,6 @@
 import { registerPatientContactInsert } from './patientContactInsert';
 import { registerTelegramGetBotInfoEvent } from './telegramGetBotInfo';
+
 /**
  *@typedef {import('@tamanu/shared/models')} Models
  * @param {{ websocketService: ReturnType<import('../services/websocketService').defineWebsocketService>, telegramBotService: ReturnType<import('../services/TelegramBotService').defineTelegramBotService>, models: Models}} injector
