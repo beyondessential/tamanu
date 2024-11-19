c32c021ac82c547c3d99f3262add7d004642bb39
Rohan Port <rohan@bes.au>
2024-11-19 16:59:27 +1100

    fix(fhir): SAV-847: Throw an error when fetching a single resource if not resolved within a timeout

--- packages/central-server/app/hl7fhir/materialised/read.js
+++ packages/central-server/app/hl7fhir/materialised/read.js
@@ -2,6 +2,11 @@ import { formatRFC7231 } from 'date-fns';
 import asyncHandler from 'express-async-handler';
 
 import { NotFound } from '@tamanu/shared/utils/fhir';
+import { sleepAsync } from '@tamanu/shared';
+import { ValidationError } from '@tamanu/shared/errors';
+
+const UNRESOLVED_RESOURCE_AWAIT_TIMEOUT = 20 * 1000; // 20 seconds
+const UNRESOLVED_RESOURCE_AWAIT_BACKOFF = 0.5 * 1000; // 0.5 seconds
 
 export function readHandler(FhirResource) {
   return asyncHandler(async (req, res) => {
@@ -17,6 +22,19 @@ export function readHandler(FhirResource) {
     // const query = await parseRequest(req, parameters);
     const record = await FhirResource.findByPk(id);
     if (!record) throw new NotFound(`no ${FhirResource.fhirName} with id ${id}`);
+
+    let totalAwaitedTime = 0;
+    while (!record.resolved && totalAwaitedTime < UNRESOLVED_RESOURCE_AWAIT_TIMEOUT) {
+      await sleepAsync(totalAwaitedTime + UNRESOLVED_RESOURCE_AWAIT_BACKOFF);
+      totalAwaitedTime += totalAwaitedTime + UNRESOLVED_RESOURCE_AWAIT_BACKOFF;
+      await record.reload();
+    }
+
+    if (!record.resolved) {
+      throw new ValidationError(
+        `Awaiting resource ${FhirResource.fhirName}/${id} resolution timed out after ${totalAwaitedTime}ms`,
+      );
+    }
 
     res.header('Last-Modified', formatRFC7231(record.lastUpdated));
     // TODO: support ETag when we have full versioning support
