77fc92cddbef0691fb721dfa482c2ea27eb7800d
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-11-20 21:54:27 -0400

    Remove debug log

--- packages/shared/src/migrations/1726610686951-modifyUserPreferencesTable.js
+++ packages/shared/src/migrations/1726610686951-modifyUserPreferencesTable.js
@@ -31,11 +31,6 @@ export async function up(query) {
     allowNull: false,
   });
 
-  const test = await query.sequelize.query(`
-    \\d+ user_preferences;
-  `);
-  console.log(test);
-
   await query.sequelize.query(`
     ALTER TABLE user_preferences DROP CONSTRAINT user_preferences_pkey;
     ALTER TABLE user_preferences DROP CONSTRAINT user_preferences_user_id_uk;
