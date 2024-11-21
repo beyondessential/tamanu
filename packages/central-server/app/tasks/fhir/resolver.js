ae6be8374f355cb7a18f26582df90e543b6a39eb
Rohan Port <rohan@bes.au>
2024-11-21 17:02:19 +1100

    fix(fhir): SAV-847: Added delay in resolver job and wrapped in transaction

--- packages/central-server/app/tasks/fhir/resolver.js
+++ packages/central-server/app/tasks/fhir/resolver.js
@@ -1,15 +1,21 @@
 import { FHIR_INTERACTIONS } from '@tamanu/constants';
 import { resourcesThatCanDo } from '@tamanu/shared/utils/fhir/resources';
+import { sleepAsync } from '@tamanu/shared/utils/sleepAsync';
 
-export async function resolver(_, { log, models }) {
+export async function resolver(_, { log, sequelize, models }) {
   const materialisableResources = resourcesThatCanDo(
     models,
     FHIR_INTERACTIONS.INTERNAL.MATERIALISE,
   );
 
   log.debug('Starting resolve');
-  for (const Resource of materialisableResources) {
-    await Resource.resolveUpstreams();
-  }
+  await sequelize.transaction(async () => {
+    await sleepAsync(3000); // sleep for 3 seconds to allow materialisation jobs to complete
+
+    for (const Resource of materialisableResources) {
+      await Resource.resolveUpstreams();
+    }
+  });
+
   log.debug('Done resolving');
 }
