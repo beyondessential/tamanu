3d6d666bae03b63986ef5fb11319b6411211a263
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-11-21 17:38:23 +1300

    fix(imagingRequests): release 2.20 flash of error when finalising & printing imaging request (#6761)
    * fix flash of error
    
    * remove debug log
    
    * organise imports
    
    * restore memoisation

---
+++ packages/web/app/api/mutations/useImagingRequestMutation.js
@@ -0,0 +1,12 @@
+import { useMutation } from '@tanstack/react-query';
+
+import { useApi } from '../useApi';
+
+export const useImagingRequestMutation = (encounterId, useMutationOptions) => {
+  const api = useApi();
+
+  return useMutation(
+    async data => api.post('imagingRequest', { ...data, encounterId }),
+    useMutationOptions,
+  );
+};
