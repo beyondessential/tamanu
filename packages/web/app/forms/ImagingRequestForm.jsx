e11282291e107c7483f10091d031d6beb7dd1d04
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-11-21 17:25:21 +1300

    restore memoisation

--- packages/web/app/forms/ImagingRequestForm.jsx
+++ packages/web/app/forms/ImagingRequestForm.jsx
@@ -47,7 +47,7 @@ function getEncounterLabel(encounter) {
   return `${encounterDate} (${encounterTypeLabel})`;
 }
 
-const FormSubmitActionDropdown = ({ encounter, setOnSuccess, submitForm }) => {
+const FormSubmitActionDropdown = React.memo(({ encounter, setOnSuccess, submitForm }) => {
   const { loadEncounter } = useEncounter();
   const dispatch = useDispatch();
   const { navigateToImagingRequest } = usePatientNavigation();
@@ -79,222 +79,224 @@ const FormSubmitActionDropdown = ({ encounter, setOnSuccess, submitForm }) => {
   ];
 
   return <FormSubmitDropdownButton actions={actions} />;
-};
+});
 
-export const ImagingRequestForm = ({
-  practitionerSuggester,
-  onCancel,
-  encounter = {},
-  onSubmit,
-  editedObject,
-  generateId = shortid.generate,
-  setOnSuccess,
-}) => {
-  const { getTranslation } = useTranslation();
-  const { getLocalisation } = useLocalisation();
-  const imagingTypes = getLocalisation('imagingTypes') || {};
+export const ImagingRequestForm = React.memo(
+  ({
+    practitionerSuggester,
+    onCancel,
+    encounter = {},
+    onSubmit,
+    editedObject,
+    generateId = shortid.generate,
+    setOnSuccess,
+  }) => {
+    const { getTranslation } = useTranslation();
+    const { getLocalisation } = useLocalisation();
+    const imagingTypes = getLocalisation('imagingTypes') || {};
 
-  const { examiner = {} } = encounter;
-  const examinerLabel = examiner.displayName;
-  const encounterLabel = getEncounterLabel(encounter);
-  const { getAreasForImagingType } = useImagingRequestAreas();
-  const requiredValidationMessage = getTranslation('validation.required.inline', '*Required');
-  return (
-    <Form
-      onSubmit={onSubmit}
-      initialValues={{
-        displayId: generateId(),
-        requestedDate: getCurrentDateTimeString(),
-        ...editedObject,
-      }}
-      formType={editedObject ? FORM_TYPES.EDIT_FORM : FORM_TYPES.CREATE_FORM}
-      validationSchema={yup.object().shape({
-        requestedById: foreignKey(requiredValidationMessage),
-        requestedDate: yup.date().required(requiredValidationMessage),
-        imagingType: foreignKey(requiredValidationMessage),
-        areas: yup.string().when('imagingType', {
-          is: imagingType => {
-            const imagingAreas = getAreasForImagingType(imagingType);
-            return imagingAreas.length > 0;
-          },
-          then: yup
-            .string()
-            .min(3, requiredValidationMessage) // Empty input is '[]', so validating that it's got at least one value in the array
-            .required(requiredValidationMessage),
-        }),
-        areaNote: yup.string().when('imagingType', {
-          is: imagingType => {
-            const imagingAreas = getAreasForImagingType(imagingType);
-            return imagingAreas.length === 0;
-          },
-          then: yup
-            .string()
-            .trim()
-            .required(requiredValidationMessage),
-        }),
-      })}
-      showInlineErrorsOnly
-      render={({ submitForm, values }) => {
-        const imagingAreas = getAreasForImagingType(values.imagingType);
-        return (
-          <FormGrid>
-            <Field
-              name="displayId"
-              label={
-                <TranslatedText
-                  stringId="imaging.requestCode.label"
-                  fallback="Imaging request code"
-                />
-              }
-              disabled
-              component={TextField}
-            />
-            <Field
-              name="requestedDate"
-              label={
-                <TranslatedText
-                  stringId="imaging.requestedDate.label"
-                  fallback="Order date and time"
-                />
-              }
-              required
-              component={DateTimeField}
-              saveDateAsString
-            />
-            <TextInput
-              label={
-                <TranslatedText
-                  stringId="general.supervisingClinician.label"
-                  fallback="Supervising :clinician"
-                  replacements={{
-                    clinician: (
-                      <TranslatedText
-                        stringId="general.localisedField.clinician.label.short"
-                        fallback="Clinician"
-                        lowercase
-                      />
-                    ),
-                  }}
-                />
-              }
-              disabled
-              value={examinerLabel}
-            />
-            <Field
-              name="requestedById"
-              label={
-                <TranslatedText
-                  stringId="general.requestingClinician.label"
-                  fallback="Requesting :clinician"
-                  replacements={{
-                    clinician: (
-                      <TranslatedText
-                        stringId="general.localisedField.clinician.label.short"
-                        fallback="Clinician"
-                        lowercase
-                      />
-                    ),
-                  }}
-                />
-              }
-              required
-              component={AutocompleteField}
-              suggester={practitionerSuggester}
-            />
-            <div>
-              <ImagingPriorityField name="priority" />
-            </div>
-            <FormSeparatorLine />
-            <TextInput
-              label={<TranslatedText stringId="imaging.encounter.label" fallback="Encounter" />}
-              disabled
-              value={encounterLabel}
-            />
-            <Field
-              name="imagingType"
-              label={
-                <TranslatedText
-                  stringId="imaging.imagingType.label"
-                  fallback="Imaging request type"
-                />
-              }
-              required
-              enumValues={IMAGING_TYPES}
-              component={TranslatedSelectField}
-              transformOptions={options => {
-                const availableTypes = Object.keys(imagingTypes);
-                return options
-                  .filter(option => availableTypes.includes(camelCase(option.value)))
-                  .map(option => {
-                    const imagingTypeKey = camelCase(option.value);
-                    const { label } = imagingTypes[imagingTypeKey];
-                    return {
-                      ...option,
-                      value: imagingTypeKey,
-                      label: getTranslation(option.label.stringId, label),
-                    };
-                  });
-              }}
-            />
-            {imagingAreas.length > 0 ? (
+    const { examiner = {} } = encounter;
+    const examinerLabel = examiner.displayName;
+    const encounterLabel = getEncounterLabel(encounter);
+    const { getAreasForImagingType } = useImagingRequestAreas();
+    const requiredValidationMessage = getTranslation('validation.required.inline', '*Required');
+    return (
+      <Form
+        onSubmit={onSubmit}
+        initialValues={{
+          displayId: generateId(),
+          requestedDate: getCurrentDateTimeString(),
+          ...editedObject,
+        }}
+        formType={editedObject ? FORM_TYPES.EDIT_FORM : FORM_TYPES.CREATE_FORM}
+        validationSchema={yup.object().shape({
+          requestedById: foreignKey(requiredValidationMessage),
+          requestedDate: yup.date().required(requiredValidationMessage),
+          imagingType: foreignKey(requiredValidationMessage),
+          areas: yup.string().when('imagingType', {
+            is: imagingType => {
+              const imagingAreas = getAreasForImagingType(imagingType);
+              return imagingAreas.length > 0;
+            },
+            then: yup
+              .string()
+              .min(3, requiredValidationMessage) // Empty input is '[]', so validating that it's got at least one value in the array
+              .required(requiredValidationMessage),
+          }),
+          areaNote: yup.string().when('imagingType', {
+            is: imagingType => {
+              const imagingAreas = getAreasForImagingType(imagingType);
+              return imagingAreas.length === 0;
+            },
+            then: yup
+              .string()
+              .trim()
+              .required(requiredValidationMessage),
+          }),
+        })}
+        showInlineErrorsOnly
+        render={({ submitForm, values }) => {
+          const imagingAreas = getAreasForImagingType(values.imagingType);
+          return (
+            <FormGrid>
               <Field
-                options={imagingAreas
-                  .map(({ id, name, type }) => ({
-                    label: <TranslatedReferenceData fallback={name} value={id} category={type} />,
-                    value: id,
-                    searchString: getTranslation(getReferenceDataStringId(id, type), name),
-                  }))
-                  .sort((area1, area2) => {
-                    return area1.searchString.localeCompare(area2.searchString);
-                  })}
-                name="areas"
-                label={
-                  <TranslatedText stringId="imaging.areas.label" fallback="Areas to be imaged" />
-                }
-                component={MultiselectField}
-                prefix="imaging.property.area"
-                required
-              />
-            ) : (
-              <Field
-                name="areaNote"
+                name="displayId"
                 label={
                   <TranslatedText
-                    stringId="imaging.imagingNote.label"
-                    fallback="Areas to be imaged"
+                    stringId="imaging.requestCode.label"
+                    fallback="Imaging request code"
                   />
                 }
+                disabled
+                component={TextField}
+              />
+              <Field
+                name="requestedDate"
+                label={
+                  <TranslatedText
+                    stringId="imaging.requestedDate.label"
+                    fallback="Order date and time"
+                  />
+                }
+                required
+                component={DateTimeField}
+                saveDateAsString
+              />
+              <TextInput
+                label={
+                  <TranslatedText
+                    stringId="general.supervisingClinician.label"
+                    fallback="Supervising :clinician"
+                    replacements={{
+                      clinician: (
+                        <TranslatedText
+                          stringId="general.localisedField.clinician.label.short"
+                          fallback="Clinician"
+                          lowercase
+                        />
+                      ),
+                    }}
+                  />
+                }
+                disabled
+                value={examinerLabel}
+              />
+              <Field
+                name="requestedById"
+                label={
+                  <TranslatedText
+                    stringId="general.requestingClinician.label"
+                    fallback="Requesting :clinician"
+                    replacements={{
+                      clinician: (
+                        <TranslatedText
+                          stringId="general.localisedField.clinician.label.short"
+                          fallback="Clinician"
+                          lowercase
+                        />
+                      ),
+                    }}
+                  />
+                }
+                required
+                component={AutocompleteField}
+                suggester={practitionerSuggester}
+              />
+              <div>
+                <ImagingPriorityField name="priority" />
+              </div>
+              <FormSeparatorLine />
+              <TextInput
+                label={<TranslatedText stringId="imaging.encounter.label" fallback="Encounter" />}
+                disabled
+                value={encounterLabel}
+              />
+              <Field
+                name="imagingType"
+                label={
+                  <TranslatedText
+                    stringId="imaging.imagingType.label"
+                    fallback="Imaging request type"
+                  />
+                }
+                required
+                enumValues={IMAGING_TYPES}
+                component={TranslatedSelectField}
+                transformOptions={options => {
+                  const availableTypes = Object.keys(imagingTypes);
+                  return options
+                    .filter(option => availableTypes.includes(camelCase(option.value)))
+                    .map(option => {
+                      const imagingTypeKey = camelCase(option.value);
+                      const { label } = imagingTypes[imagingTypeKey];
+                      return {
+                        ...option,
+                        value: imagingTypeKey,
+                        label: getTranslation(option.label.stringId, label),
+                      };
+                    });
+                }}
+              />
+              {imagingAreas.length > 0 ? (
+                <Field
+                  options={imagingAreas
+                    .map(({ id, name, type }) => ({
+                      label: <TranslatedReferenceData fallback={name} value={id} category={type} />,
+                      value: id,
+                      searchString: getTranslation(getReferenceDataStringId(id, type), name),
+                    }))
+                    .sort((area1, area2) => {
+                      return area1.searchString.localeCompare(area2.searchString);
+                    })}
+                  name="areas"
+                  label={
+                    <TranslatedText stringId="imaging.areas.label" fallback="Areas to be imaged" />
+                  }
+                  component={MultiselectField}
+                  prefix="imaging.property.area"
+                  required
+                />
+              ) : (
+                <Field
+                  name="areaNote"
+                  label={
+                    <TranslatedText
+                      stringId="imaging.imagingNote.label"
+                      fallback="Areas to be imaged"
+                    />
+                  }
+                  component={TextField}
+                  multiline
+                  style={{ gridColumn: '1 / -1' }}
+                  minRows={3}
+                  required
+                />
+              )}
+              <Field
+                name="note"
+                label={<TranslatedText stringId="general.notes.label" fallback="Notes" />}
                 component={TextField}
                 multiline
                 style={{ gridColumn: '1 / -1' }}
                 minRows={3}
-                required
               />
-            )}
-            <Field
-              name="note"
-              label={<TranslatedText stringId="general.notes.label" fallback="Notes" />}
-              component={TextField}
-              multiline
-              style={{ gridColumn: '1 / -1' }}
-              minRows={3}
-            />
-            <ButtonRow>
-              <FormCancelButton onClick={onCancel}>
-                <TranslatedText stringId="general.action.cancel" fallback="Cancel" />
-              </FormCancelButton>
-              <FormSubmitActionDropdown
-                encounter={encounter}
-                setOnSuccess={setOnSuccess}
-                submitForm={submitForm}
-              />
-            </ButtonRow>
-          </FormGrid>
-        );
-      }}
-    />
-  );
-};
+              <ButtonRow>
+                <FormCancelButton onClick={onCancel}>
+                  <TranslatedText stringId="general.action.cancel" fallback="Cancel" />
+                </FormCancelButton>
+                <FormSubmitActionDropdown
+                  encounter={encounter}
+                  setOnSuccess={setOnSuccess}
+                  submitForm={submitForm}
+                />
+              </ButtonRow>
+            </FormGrid>
+          );
+        }}
+      />
+    );
+  },
+);
 
 ImagingRequestForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
