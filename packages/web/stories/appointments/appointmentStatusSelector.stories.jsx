a21bd2b00fedf2e98d117e2234da6b6a0c40f6d9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-11-22 15:31:17 +1300

    un-break storybook

--- packages/web/stories/appointments/appointmentStatusSelector.stories.jsx
+++ packages/web/stories/appointments/appointmentStatusSelector.stories.jsx
@@ -1,32 +1,40 @@
+import Chance from 'chance';
 import React from 'react';
 
-import { APPOINTMENT_STATUSES } from '@tamanu/constants';
+import { createDummyPatient } from '@tamanu/shared/demoData';
 
-import { AppointmentStatusSelector } from '../../app/components';
+import { fakePractitioner } from '../../.storybook/__mocks__/defaultEndpoints';
+import { AppointmentStatusSelector } from '../../app/components/Appointments/AppointmentDetailPopper/AppointmentStatusSelector';
 
 export default {
   title: 'Appointments/Appointment Status Selector',
   component: AppointmentStatusSelector,
 };
 
+const chance = new Chance();
+const partialAppointment = {
+  id: chance.guid(),
+  startTime: '2024-09-05 13:57:00',
+  endTime: '2024-09-05 14:57:00',
+  patient: createDummyPatient(null, { id: chance.guid() }),
+  clinician: fakePractitioner(),
+  location: { name: 'Bed 1', id: 'bed1' },
+  locationGroup: { name: 'Ward 1', id: 'ward1' },
+  appointmentType: { name: 'Standard', id: 'standard' },
+};
+
 export const StatusSelector = () => {
-  const [status, setStatus] = React.useState(APPOINTMENT_STATUSES.CONFIRMED);
   return (
     <div style={{ width: '16rem' }}>
-      <AppointmentStatusSelector selectedStatus={status} updateAppointmentStatus={setStatus} />
+      <AppointmentStatusSelector appointment={partialAppointment} />
     </div>
   );
 };
 
 export const Disabled = () => {
-  const [status, setStatus] = React.useState(APPOINTMENT_STATUSES.CONFIRMED);
   return (
     <div style={{ width: '16rem' }}>
-      <AppointmentStatusSelector
-        disabled
-        selectedStatus={status}
-        updateAppointmentStatus={setStatus}
-      />
+      <AppointmentStatusSelector appointment={partialAppointment} disabled />
     </div>
   );
 };
