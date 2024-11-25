2b188899ec82c4c47c49c9bc38b0d01adc79bae5
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-11-26 10:42:35 +1300

    fix rerenders

--- packages/web/app/views/scheduling/outpatientBookings/useOutpatientAppointmentsCalendarData.js
+++ packages/web/app/views/scheduling/outpatientBookings/useOutpatientAppointmentsCalendarData.js
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
 import { endOfDay } from 'date-fns';
 import { groupBy as lodashGroupBy } from 'lodash';
+import { useMemo } from 'react';
 
 import { useAppointmentsQuery } from '../../../api/queries';
 import { useLocationGroupsQuery } from '../../../api/queries/useLocationGroupsQuery';
@@ -8,48 +8,38 @@ import { useUsersQuery } from '../../../api/queries/useUsersQuery';
 import { APPOINTMENT_GROUP_BY } from './OutpatientAppointmentsView';
 
 export const useOutpatientAppointmentsCalendarData = ({ groupBy, selectedDate }) => {
-  const {
-    data: locationGroupData,
-    error: locationGroupsError,
-    isLoading: isLocationGroupsLoading,
-  } = useLocationGroupsQuery();
+  const locationGroupsQuery = useLocationGroupsQuery();
+  const { data: locationGroupData, error: locationGroupsError } = locationGroupsQuery;
 
-  const { data: userData, error: usersError, isLoading: isUsersLoading } = useUsersQuery(
-    {
-      orderBy: 'displayName',
-    },
+  const usersQuery = useUsersQuery(
+    { orderBy: 'displayName' },
     {
       // Add an 'unknown' user to the list for appointments that don't have a clinician
       select: ({ data }) => [...data, { id: 'unknown', displayName: 'Unknown' }],
     },
   );
+  const { data: usersData, error: usersError } = usersQuery;
 
-  const {
-    data: appointmentData,
-    error: appointmentError,
-    isFetching: isFetchingAppointmentData,
-  } = useAppointmentsQuery({
+  const appointmentsQuery = useAppointmentsQuery({
     after: selectedDate,
     before: endOfDay(selectedDate),
     locationGroupId: '',
     all: true,
   });
-
-  const isLoading = isLocationGroupsLoading || isUsersLoading || isFetchingAppointmentData;
-  const error = locationGroupsError || usersError || appointmentError;
+  const { data: appointmentsData, error: appointmentsError } = appointmentsQuery;
 
   const data = useMemo(() => {
-    if (!appointmentData?.data || appointmentData.data.length === 0) return {};
+    if (!appointmentsData?.data || appointmentsData.data.length === 0) return {};
 
     const cellData = lodashGroupBy(
-      appointmentData?.data,
+      appointmentsData?.data,
       appointment => appointment[groupBy] || 'unknown',
     );
 
     if (groupBy === APPOINTMENT_GROUP_BY.CLINICIAN) {
       return {
         cellData,
-        headData: userData.filter(user => !!cellData[user.id]),
+        headData: usersData.filter(user => !!cellData[user.id]),
         titleKey: 'displayName',
       };
     }
@@ -60,7 +50,40 @@ export const useOutpatientAppointmentsCalendarData = ({ groupBy, selectedDate })
         titleKey: 'name',
       };
     }
-  }, [appointmentData?.data, groupBy, userData, locationGroupData]);
+  }, [appointmentsData?.data, groupBy, usersData, locationGroupData]);
 
-  return { data, isLoading, error };
+  const error = locationGroupsError || usersError || appointmentsError;
+
+  const isError = locationGroupsQuery.isError || usersQuery.isError || appointmentsQuery.isError;
+  const isFetched =
+    locationGroupsQuery.isFetched || usersQuery.isFetched || appointmentsQuery.isFetched;
+  const isFetching =
+    locationGroupsQuery.isFetching || usersQuery.isFetching || appointmentsQuery.isFetching;
+  const isInitialLoading =
+    locationGroupsQuery.isInitialLoading ||
+    usersQuery.isInitialLoading ||
+    appointmentsQuery.isInitialLoading;
+  const isLoading =
+    locationGroupsQuery.isLoading || usersQuery.isLoading || appointmentsQuery.isLoading;
+  const isLoadingError =
+    locationGroupsQuery.isLoadingError ||
+    usersQuery.isLoadingError ||
+    appointmentsQuery.isLoadingError;
+  const isRefetching =
+    locationGroupsQuery.isRefetching || usersQuery.isRefetching || appointmentsQuery.isRefetching;
+  const isSuccess =
+    locationGroupsQuery.isSuccess || usersQuery.isSuccess || appointmentsQuery.isSuccess;
+
+  return {
+    data,
+    error,
+    isError,
+    isFetched,
+    isFetching,
+    isInitialLoading,
+    isLoading,
+    isLoadingError,
+    isRefetching,
+    isSuccess,
+  };
 };
