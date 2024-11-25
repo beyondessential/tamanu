c8d72005957048aeb60d38655d1266977ef08da6
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-11-25 16:23:04 +1300

    clearer string IDs
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

--- packages/web/app/components/Appointments/AppointmentDetailPopper/CheckInButton.jsx
+++ packages/web/app/components/Appointments/AppointmentDetailPopper/CheckInButton.jsx
@@ -49,7 +49,7 @@ export const CheckInButton = ({ appointment }) => {
         closeModal();
         toast.success(
           <TranslatedText
-            stringId="scheduling.success.encounterCreated"
+            stringId="scheduling.action.createEncounter.success"
             fallback="Encounter successfully started"
           />,
         );
@@ -57,7 +57,7 @@ export const CheckInButton = ({ appointment }) => {
       onError: () =>
         toast.error(
           <TranslatedText
-            stringId="schedule.error.updateStatus"
+            stringId="scheduling.action.createEncounter.error"
             fallback="Couldn’t start encounter"
           />,
         ),
