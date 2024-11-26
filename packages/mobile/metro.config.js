8db86274a452950e2dd28bcf09a5fe37390e08c5
Chris Tran <chris@beyondessential.com.au>
2024-11-26 15:38:48 +1100

    fixed metro.config.js

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -9,8 +9,8 @@ const path = require('path');
 
 const { FileStore } = require('metro-cache');
 
-// still works with npm
 const getWorkspaces = require('get-yarn-workspaces');
+const blacklist = require('metro-config/src/defaults/blacklist');
 
 const workspaces = getWorkspaces(__dirname);
 
@@ -20,6 +20,14 @@ module.exports = {
   watchFolders: [path.resolve(__dirname, '../../node_modules'), ...workspaces],
 
   resolver: {
+    blacklistRE: blacklist(
+      workspaces
+        .filter(workspacePath => workspacePath.indexOf('packages/app') === -1)
+        .map(
+          workspacePath =>
+            new RegExp(`^${escape(path.resolve(__dirname, workspacePath, 'node_modules'))}\\/.*$`),
+        ),
+    ),
     // https://github.com/facebook/metro/issues/1#issuecomment-453450709
     extraNodeModules: new Proxy(
       {},
@@ -27,7 +35,6 @@ module.exports = {
         get: (target, name) => path.join(process.cwd(), `node_modules/${name}`),
       },
     ),
-    sourceExts: ['jsx', 'js', 'ts', 'tsx', 'cjs', 'mjs', 'json'],
   },
 
   // http://facebook.github.io/react-native/blog/2019/03/12/releasing-react-native-059#faster-app-launches-with-inline-requires
