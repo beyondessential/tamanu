51bd3299c32fd0bafdfe2d2ec8787ae4be166ff5
Daniel Nash <dannash200@gmail.com>
2024-11-29 10:13:05 +1300

    pass fac id

--- packages/web/app/api/mutations/useAppointmentMutation.js
+++ packages/web/app/api/mutations/useAppointmentMutation.js
@@ -1,14 +1,16 @@
 import { useMutation } from '@tanstack/react-query';
 import { useApi } from '../useApi';
+import { useAuth } from '../../contexts/Auth';
 
 export const useAppointmentMutation = ({ isEdit = false }, useMutationOptions) => {
+  const { facilityId } = useAuth();
   const api = useApi();
 
   return useMutation(
     payload =>
       isEdit
-        ? api.put(`appointments/${payload.id}`, payload, { throwResponse: true })
-        : api.post('appointments', payload, { throwResponse: true }),
+        ? api.put(`appointments/${payload.id}`, { ...payload, facilityId }, { throwResponse: true })
+        : api.post('appointments', { ...payload, facilityId }, { throwResponse: true }),
     useMutationOptions,
   );
 };
