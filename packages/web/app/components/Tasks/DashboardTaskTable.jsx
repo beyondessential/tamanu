b8d73430cb795958774a1c229cf95b75835b19af
Huy-Tran-Huu-Le <huy@bes.au>
2024-11-28 23:48:05 +0700

    fix lint issue

--- packages/web/app/components/Tasks/DashboardTaskTable.jsx
+++ packages/web/app/components/Tasks/DashboardTaskTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Box, Divider } from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
