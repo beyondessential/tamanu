f3764a35eb401ea59cd6f5949be30998446a0640
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-12-09 11:00:32 +1300

    feat(outpatientAppointments): NASS-1527: filters (#6839)
    * `CalendarSearchBar` → `LocationBookingsFilter`
    
    * filter front-end
    
    * shallower nesting
    
    * logical properties
    
    * hide decorative element
    
    * filters! …but shows location bookings 🤔
    
    * add missing context file
    
    * organise imports
    
    * organise imports
    
    * alphasort
    
    * use correct suggesters
    
    * query only when filters retreived from context
    
    * dedupe and clarify appointments endpoint
    
    * make `appointmentTypeId` searchable
    
    * update lockfile
    
    * use correct suggester
    
    * remove redundant spread
    
    * add reset button
    
    * fix clear button hover state
    
    * fix filter
    
    * `facilityIdQuery` when `locationGropuId` is array
    
    * fix(api): fix duplicate key in `GET /appointment` WHERE clause (#6904)
    
    * destructure only necessary model
    
    * fix overwritten clause
    
    * fix reading from undefined
    
    and extract `buildPatientNameOrIdQuery`
    
    * fix boolean logic
    
    * reset filters on clear
    
    * fix flipped predicate
    
    * use `NOT NULL` over `ILIKE '%%'`
    
    * fix missing query param

--- packages/web/app/components/Field/FilterField.jsx
+++ packages/web/app/components/Field/FilterField.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { SuggesterSearchMultiSelectField } from './SearchMultiSelectField';
 import { FilterIcon } from '../Icons/FilterIcon';
+import { SuggesterSearchMultiSelectField } from './SearchMultiSelectField';
 
-export const FilterField = ({ ...props }) => {
+export const FilterField = props => {
   return <SuggesterSearchMultiSelectField startIcon={<FilterIcon />} {...props} />;
 };
---
+++ packages/web/app/views/scheduling/outpatientBookings/OutpatientAppointmentsFilter.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Field, Form, SearchField, TextButton, TranslatedText } from '../../../components';
+import { FilterField } from '../../../components/Field/FilterField';
+import {
+  OUTPATIENT_APPOINTMENTS_EMPTY_FILTER_STATE,
+  useOutpatientAppointmentsContext,
+} from '../../../contexts/OutpatientAppointments';
+import { useTranslation } from '../../../contexts/Translation';
+
+const Fieldset = styled.fieldset`
+  // Reset
+  border: none;
+  margin: 0;
+  padding: 0;
+
+  display: grid;
+  gap: 0.625rem;
+  grid-template-columns: minmax(auto, 18rem) repeat(2, minmax(5.75rem, max-content)) auto;
+`;
+
+const ResetButton = styled(TextButton).attrs({
+  type: 'reset',
+})`
+  font-size: 0.6875rem;
+
+  &,
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+export const OutpatientAppointmentsFilter = props => {
+  const { setFilters } = useOutpatientAppointmentsContext();
+  const { getTranslation } = useTranslation();
+
+  const renderForm = ({ resetForm }) => (
+    <Fieldset>
+      <Field
+        component={SearchField}
+        name="patientNameOrId"
+        onChange={e => setFilters(prev => ({ ...prev, patientNameOrId: e.target.value }))}
+        placeholder={getTranslation(
+          'scheduling.filter.placeholder.patientNameOrId',
+          'Search patient name or ID',
+        )}
+      />
+      <Field
+        component={FilterField}
+        endpoint="facilityLocationGroup"
+        label={getTranslation('general.area.label', 'Area')}
+        name="locationGroupId"
+        onChange={e => setFilters(prev => ({ ...prev, locationGroupId: e.target.value }))}
+      />
+      <Field
+        component={FilterField}
+        endpoint="appointmentType"
+        label={getTranslation('general.type.label', 'Type')}
+        name="appointmentTypeId"
+        onChange={e => setFilters(prev => ({ ...prev, appointmentTypeId: e.target.value }))}
+      />
+      <ResetButton
+        onClick={() => {
+          resetForm();
+          setFilters(OUTPATIENT_APPOINTMENTS_EMPTY_FILTER_STATE);
+        }}
+        type="reset"
+      >
+        <TranslatedText stringId="general.action.clear" fallback="Clear" />
+      </ResetButton>
+    </Fieldset>
+  );
+
+  return <Form onSubmit={async () => {}} render={renderForm} {...props} />;
+};
