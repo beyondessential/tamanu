a069f37340a4dc208fab662fc226879d9255aea6
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-12-08 20:01:17 -0400

    Use graph data provider factory for vitals

--- packages/web/app/contexts/VitalChartData.jsx
+++ packages/web/app/contexts/VitalChartData.jsx
@@ -1,8 +1,6 @@
-import React, { useContext, useState } from 'react';
-import { addDays, format } from 'date-fns';
-
-import { DATE_TIME_FORMAT } from '../components/Charts/components/DateTimeSelector';
+import React, { useContext } from 'react';
 import { useVitalsVisualisationConfigsQuery } from '../api/queries/useVitalsVisualisationConfigsQuery';
+import { GraphDataProviderFactory } from './GraphData';
 
 export const VitalChartDataContext = React.createContext({
   visualisationConfigs: [],
@@ -21,34 +19,12 @@ export const VitalChartDataContext = React.createContext({
 export const useVitalChartData = () => useContext(VitalChartDataContext);
 
 export const VitalChartDataProvider = ({ children }) => {
-  const [chartKeys, setChartKeys] = useState([]);
-  const [isInMultiChartsView, setIsInMultiChartsView] = useState(false);
-  const [modalTitle, setModalTitle] = useState(null);
-  const [dateRange, setDateRange] = useState([
-    format(addDays(new Date(), -1), DATE_TIME_FORMAT),
-    format(new Date(), DATE_TIME_FORMAT),
-  ]);
-  const [vitalChartModalOpen, setVitalChartModalOpen] = useState(false);
-  const { data } = useVitalsVisualisationConfigsQuery();
-  const { visualisationConfigs } = data;
-
   return (
-    <VitalChartDataContext.Provider
-      value={{
-        visualisationConfigs,
-        vitalChartModalOpen,
-        setVitalChartModalOpen,
-        chartKeys,
-        setChartKeys,
-        modalTitle,
-        setModalTitle,
-        dateRange,
-        setDateRange,
-        isInMultiChartsView,
-        setIsInMultiChartsView,
-      }}
+    <GraphDataProviderFactory
+      visualisationConfigQueryFn={useVitalsVisualisationConfigsQuery}
+      Context={VitalChartDataContext}
     >
       {children}
-    </VitalChartDataContext.Provider>
+    </GraphDataProviderFactory>
   );
 };
