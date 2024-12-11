78b01092aa285e04004098ca9b4af91423f256bd
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2024-12-10 22:44:20 -0400

    Create user preferences constant

---
+++ packages/constants/src/userPreferences.ts
@@ -0,0 +1,6 @@
+export const USER_PREFERENCES_KEYS = {
+  ENCOUNTER_TAB_ORDERS: 'encounterTabOrders',
+  SELECTED_GRAPHED_VITALS_ON_FILTER: 'selectedGraphedVitalsOnFilter',
+  SELECTED_GRAPHED_CHARTS_ON_FILTER: 'selectedGraphedChartsOnFilter',
+  SELECTED_CHART_TYPE_ID: 'selectedChartTypeId',
+};
