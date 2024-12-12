f46e39b5adcd4ae36ddbe6e5040c59321395b468
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-12 13:42:48 +1300

    missed fullstop

--- packages/web/app/components/Appointments/LocationBookingForm/LocationBookingDrawer.jsx
+++ packages/web/app/components/Appointments/LocationBookingForm/LocationBookingDrawer.jsx
@@ -256,7 +256,7 @@ export const LocationBookingDrawer = ({ open, onClose, initialValues }) => {
           ) : (
             <TranslatedText
               stringId="locationBooking.form.new.description"
-              fallback="Create a new booking by completing the below details and selecting ‘Confirm’"
+              fallback="Create a new booking by completing the below details and selecting ‘Confirm’."
             />
           )
         }
