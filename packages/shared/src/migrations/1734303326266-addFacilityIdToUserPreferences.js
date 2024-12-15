d1947e202ceaa297683d7793b0a08063dd946966
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-16 11:57:09 +1300

    add migration

---
+++ packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-vars */
+// remove the above line
+
+import { DataTypes } from 'sequelize';
+
+export async function up(query) {
+  await query.addColumn('user_preferences', 'facility_id', {
+    type: DataTypes.STRING,
+    allowNull: true,
+    references: {
+      model: 'facilities',
+      key: 'id',
+    },
+  });
+}
+
+export async function down(query) {
+  await query.removeColumn('user_preferences', 'facility_id');
+}
