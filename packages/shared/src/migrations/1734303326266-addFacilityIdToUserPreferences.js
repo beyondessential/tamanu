5cd2e835f11f38e2bf2bbd3ac101de19b7a9a3f1
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-16 12:43:05 +1300

    migration progress

--- packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
+++ packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
@@ -9,8 +9,37 @@ export async function up(query) {
       key: 'id',
     },
   });
+
+  await query.removeConstraint('user_preferences', 'user_preferences_user_id_uk');
+  await query.removeConstraint('user_preferences', 'user_preferences_pkey');
+
+  await query.addConstraint('user_preferences', {
+    fields: ['id'],
+    type: 'primary_key',
+    name: 'user_preferences_pkey',
+  });
+
+  await query.sequelize.query(`
+    ALTER TABLE "user_preferences"
+    ALTER COLUMN "id" SET GENERATED ALWAYS AS (CONCAT("user_id", '-', "facility_id")) STORED;
+  `);
 }
 
 export async function down(query) {
   await query.removeColumn('user_preferences', 'facility_id');
+  await query.addConstraint('user_preferences', {
+    fields: ['user_id'],
+    type: 'unique',
+    name: 'user_preferences_user_id_uk',
+  });
+  await query.removeConstraint('user_preferences', 'user_preferences_pkey');
+  await query.addConstraint('user_preferences', {
+    fields: ['user_id'],
+    type: 'primary_key',
+    name: 'user_preferences_pkey',
+  });
+  await query.sequelize.query(`
+    ALTER TABLE "user_preferences"
+    ALTER COLUMN "id" SET TEXT GENERATED ALWAYS AS ("user_id") STORED;
+  `);
 }
