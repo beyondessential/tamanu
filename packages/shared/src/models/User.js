7227f785860542ce68d91f9990a6a1e978fc26b7
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-17 14:21:22 +1300

    Update User.js

--- packages/shared/src/models/User.js
+++ packages/shared/src/models/User.js
@@ -83,10 +83,16 @@ export class User extends Model {
     return user;
   }
 
-  static init({ primaryKey, ...options }) {
+  static init(options) {
     super.init(
       {
-        id: primaryKey,
+        id: {
+          // translated_string records use a generated primary key that enforces one per string and language,
+          type: `TEXT GENERATED ALWAYS AS (COALESCE("user_id", '') || ';' || COALESCE("facility_id", '')) STORED`,
+          set() {
+            // any sets of the convenience generated "id" field can be ignored
+          },
+        },
         displayId: Sequelize.STRING,
         email: {
           type: Sequelize.STRING,
