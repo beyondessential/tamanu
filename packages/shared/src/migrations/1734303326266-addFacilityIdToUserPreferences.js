bf6b69575af5b1f69091ef049ebc74e5a082c581
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-17 16:29:37 +1300

    Update 1734303326266-addFacilityIdToUserPreferences.js

--- packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
+++ packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
@@ -4,6 +4,10 @@ export async function up(query) {
   await query.addColumn('user_preferences', 'facility_id', {
     type: DataTypes.STRING,
     allowNull: true,
+    references: {
+      model: 'facilities',
+      key: 'id',
+    },
   });
 
   await query.removeConstraint('user_preferences', 'user_preferences_user_id_uk');
