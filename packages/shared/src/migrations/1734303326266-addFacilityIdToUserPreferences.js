c9085092ca18e25531becae59c15f3bed9defb79
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2024-12-17 13:54:37 +1300

    try something

--- packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
+++ packages/shared/src/migrations/1734303326266-addFacilityIdToUserPreferences.js
@@ -13,30 +13,27 @@ export async function up(query) {
   await query.removeConstraint('user_preferences', 'user_preferences_user_id_uk');
   await query.removeConstraint('user_preferences', 'user_preferences_pkey');
 
-  await query.removeColumn('user_preferences', 'id');
-  await query.addColumn('user_preferences', 'id', {
-    type: DataTypes.STRING,
-    allowNull: true,
-    defaultValue: DataTypes.UUIDV4,
-  });
-
-  await query.sequelize.query(`
-    UPDATE "user_preferences"
-    SET "id" = uuid_generate_v5(
-        '2f206659-ee5a-4de4-a3cf-91054d192ec9', 
-        CONCAT(
-          COALESCE("user_id", ''),
-          COALESCE("facility_id", ''),
-       ) 
-    )
-    WHERE "id" IS NULL;
-  `);
+  // await query.removeColumn('user_preferences', 'id');
   await query.changeColumn('user_preferences', 'id', {
-    type: DataTypes.STRING,
-    allowNull: false,
-    defaultValue: DataTypes.UUIDV4,
+    type: `TEXT GENERATED ALWAYS AS (COALESCE("user_id", '') || ';' || COALESCE("facility_id", '')) STORED`,
     unique: true,
   });
+
+  // // await query.sequelize.query(`
+  // //   UPDATE "user_preferences"
+  // //   SET "id" = CONCAT(
+  // //         COALESCE("user_id", ''),
+
+  // //         COALESCE("facility_id", '')
+  // //       )
+  // //   WHERE "id" IS NULL;
+  // // `);
+  // await query.changeColumn('user_preferences', 'id', {
+  //   type: DataTypes.STRING,
+  //   allowNull: false,
+  //   // defaultValue: DataTypes.UUIDV4,
+  //   unique: true,
+  // });
 
   await query.addIndex('user_preferences', {
     name: 'user_facility_unique_index',
