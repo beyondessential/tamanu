e19b0813be15bffc362ded41391a31b6364f98f7
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2024-12-19 14:53:01 +1300

    more robust hook

--- packages/web/app/hooks/useBookingTimeSlots.js
+++ packages/web/app/hooks/useBookingTimeSlots.js
@@ -6,45 +6,57 @@ import { isWithinIntervalExcludingEnd } from '@tamanu/shared/utils/dateTime';
 
 import { useBookingSlotSettings } from './useBookingSlotSettings';
 
+const calculateTimeSlots = (bookingSlotSettings, date) => {
+  if (!bookingSlotSettings) return null;
+
+  const { startTime, endTime, slotDuration } = bookingSlotSettings;
+  const startOfDay = parse(startTime, 'HH:mm', date);
+  const endOfDay = parse(endTime, 'HH:mm', date);
+  const durationMs = ms(slotDuration);
+
+  const slotCount = differenceInMilliseconds(endOfDay, startOfDay) / durationMs;
+  const _slots = [];
+  for (let i = 0; i < slotCount; i++) {
+    const start = addMilliseconds(startOfDay, i * durationMs);
+    const end = addMilliseconds(start, durationMs);
+    _slots.push({ start, end });
+  }
+
+  return _slots;
+};
+
 /**
  * Returns the bookable time slots for the provided date, or `null` if the date is invalid. If the
- * booking slot settings are still pending, returns undefined.
+ * booking slot settings are still pending, returns `undefined`.
  */
 export const useBookingTimeSlots = date => {
   const bookingSlotSettings = useBookingSlotSettings();
+
+  if (date && !isValid(date)) {
+    throw new Error('useBookingTimeSlots has been called with an invalid date');
+  }
+
   const isPending = bookingSlotSettings === undefined;
 
+  // “Pointless” destructure so we can use primitives as `useMemo` dependencies
   const { startTime, endTime, slotDuration } = bookingSlotSettings;
   const slots = useMemo(
     () => {
-      if (!isValid(date)) return [];
-
-      const startOfDay = parse(startTime, 'HH:mm', date);
-      const endOfDay = parse(endTime, 'HH:mm', date);
-      const durationMs = ms(slotDuration);
-
-      const slotCount = differenceInMilliseconds(endOfDay, startOfDay) / durationMs;
-      const _slots = [];
-      for (let i = 0; i < slotCount; i++) {
-        const start = addMilliseconds(startOfDay, i * durationMs);
-        const end = addMilliseconds(start, durationMs);
-        _slots.push({ start, end });
-      }
-
-      return _slots;
+      if (!date) return null;
+      if (isPending) return undefined;
+      return calculateTimeSlots({ startTime, endTime, slotDuration }, date);
     },
     // Relying on `valueOf()` is valid here
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [startTime, endTime, slotDuration, date?.valueOf()],
+    [date?.valueOf(), endTime, isPending, slotDuration, startTime],
   );
 
-
-  const slotContaining = time => slots.find(slot => isWithinIntervalExcludingEnd(time, slot));
+  const slotContaining = time => slots?.find(slot => isWithinIntervalExcludingEnd(time, slot));
   const endOfSlotContaining = time => slotContaining(time)?.end ?? null;
 
   return {
-    slots: isPending ? undefined : slots,
     isPending,
+    slots,
     slotContaining,
     endOfSlotContaining,
   };
