cc7ea4aa2e93eeb124b1b0d48f55944d83688633
Daniel Nash <dannash200@gmail.com>
2025-01-10 10:14:47 +1300

    tweaking

--- packages/utils/src/dateTime.ts
+++ packages/utils/src/dateTime.ts
@@ -297,7 +297,7 @@ export const isStartOfThisWeek = (date: Date | number) => {
 
 // Custom validator for "YYYY-MM-DD HH:MM:SS" format
 export const datetimeCustomValidation = z.string().refine(
-  (val) => {
+  (val: string) => {
     const regex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
     if (!regex.test(val)) return false;
 
