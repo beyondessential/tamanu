d8c4cbca970ff3322926450644a2ba0850bce433
Daniel Nash <dannash200@gmail.com>
2025-01-13 10:09:03 +1300

    closer

--- packages/web/app/components/Field/SwitchField.jsx
+++ packages/web/app/components/Field/SwitchField.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import { styled } from '@mui/material/styles';
+import { Colors } from '../../constants';
 
 const StyledSwitch = styled(Switch)`
-    .Mui-checked {
-     & + .MuiSwitch-track {
-        background-color:#C2D2E1;
-        }
+  height: 45px;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .MuiSwitch-thumb {
+    position: absolute;
+    top: 15px;
+    left: 3px;
+    width: 14px;
+    height: 14px;
+  }
+  .MuiSwitch-track {
+    height: 20px;
+    width: 40px;
+    background-color: ${Colors.softText};
+    opacity: 1;
+  }
 `;
 
 export const SwitchInput = ({ label, disabled, value, ...props }) => {
