279dd38e0639d881380b7e68e8e81bcb470045bd
Daniel Nash <dannash200@gmail.com>
2025-01-13 15:56:58 +1300

    is it mabye more performant i dunno

--- packages/central-server/app/tasks/GenerateRepeatingAppointments.js
+++ packages/central-server/app/tasks/GenerateRepeatingAppointments.js
@@ -21,33 +21,34 @@ export class GenerateRepeatingAppointments extends ScheduledTask {
     return 'GenerateRepeatingAppointments';
   }
 
-  async countQueue() {
-    await this.sequelize.query(`
-      WITH latest_appointments AS (
-          SELECT schedule_id, MAX(start_time) AS latest_start_time
-          FROM appointments
-          GROUP BY schedule_id
+  async run() {
+    await this.sequelize.query(
+      `
+      WITH schedules_with_latest_appointment_in_past AS (
+          SELECT * FROM appointment_schedules
+          JOIN LATERAL (
+            SELECT start_time FROM appointments
+            WHERE appointments.schedule_id = appointment_schedules.id
+            ORDER BY appointments.start_time DESC
+            LIMIT 1
+          ) AS latest_appointment ON true
+          WHERE latest_appointment.start_time::date < NOW()
         ),
-      past_appointment_schedules AS (
-        SELECT *
-        FROM appointment_schedules
-        LEFT JOIN latest_appointments ON appointment_schedules.id = latest_appointments.schedule_id
-        WHERE latest_appointments.latest_start_time::date < NOW()
-      ),
-      schedules_not_completed AS (
+      possible_incomplete_schedules AS (
         SELECT *
         FROM past_appointment_schedules
-        WHERE (occurrence_count IS NULL AND until_date > latest_start_time) OR (occurrence_count > (
+        WHERE (occurrence_count IS NULL AND until_date > start_time) OR (occurrence_count > (
             SELECT COUNT(*)
             FROM appointments
             WHERE schedule_id = past_appointment_schedules.id
           )
         )
       )
-      select * from schedules_not_completed;
-    `);
-    // We gotta problem where by we can't easily check next tues or wateva for until_date variation so maybe we just do in js with date-fns
+      select * from possible_incomplete_schedules;
+    `,
+      { type: this.sequelize.QueryTypes.SELECT },
+    );
+    // We gotta problem where by we can't easily check next tues or
+    // 3rd tues in month for until_date variation so maybe we just do in js with date-fns
   }
-
-  async run() {}
 }
