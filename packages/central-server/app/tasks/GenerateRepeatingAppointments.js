3315c5b2e78e909db2d9b39217db70142e041a60
Daniel Nash <dannash200@gmail.com>
2025-01-13 14:54:32 +1300

    basic bootstrap task

---
+++ packages/central-server/app/tasks/GenerateRepeatingAppointments.js
@@ -0,0 +1,27 @@
+import config from 'config';
+
+import { ScheduledTask } from '@tamanu/shared/tasks';
+import { log } from '@tamanu/shared/services/logging';
+
+export class GenerateRepeatingAppointments extends ScheduledTask {
+  /**
+   *
+   * @param {import('../ApplicationContext').ApplicationContext} context
+   */
+  constructor(context) {
+    const conf = config.schedules.generateRepeatingTasks;
+    const { schedule, jitterTime, enabled } = conf;
+    super(schedule, log, jitterTime, enabled);
+    this.models = context.store.models;
+    this.config = conf;
+    this.sequelize = context.store.sequelize;
+  }
+
+  getName() {
+    return 'GenerateRepeatingAppointments';
+  }
+
+  async countQueue() {}
+
+  async run() {}
+}
