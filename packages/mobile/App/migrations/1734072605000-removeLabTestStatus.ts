40929b1ed4097ac3e410f1052973e62e475020f7
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-01-13 15:12:39 +1300

    Update 1734072605000-removeLabTestStatus.ts

--- packages/mobile/App/migrations/1734072605000-removeLabTestStatus.ts
+++ packages/mobile/App/migrations/1734072605000-removeLabTestStatus.ts
@@ -2,7 +2,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class removeLabTestStatus1734072605000 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE labTest DROP COLUMN status;`);
+    await queryRunner.dropColumn('labTest', 'status');
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
