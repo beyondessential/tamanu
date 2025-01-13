b1bbd4c35a94476f188177f3484b3404e6e13aa6
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-01-13 18:54:33 -0400

    Only run migration in central server and bump the sync tick

--- packages/shared/src/migrations/1736807653324-markMergedPatientsAsDeleted.js
+++ packages/shared/src/migrations/1736807653324-markMergedPatientsAsDeleted.js
@@ -1,10 +1,24 @@
+import config from 'config';
+import { selectFacilityIds } from '../utils/configSelectors';
+
 // We had a "rename" patient merge strategy, which didn't soft delete merged patients
 // instead, it just renamed them to "Delete Patient". This migration ensures that all
 // patients that were merged with said strategy are properly deleted.
+// Also, this one will only run on central, to be able to propagate these changes to clients.
+// Note this is safe to do only because merging patients can only ever happen on central server.
+
 export async function up(query) {
+  const isFacility = Boolean(selectFacilityIds(config));
+
+  if (isFacility) {
+    return;
+  }
+
   await query.sequelize.query(`
     UPDATE patients
-    SET deleted_at = updated_at
+    SET
+      deleted_at = updated_at,
+      updated_at_sync_tick = (SELECT CAST(value AS bigint) FROM local_system_facts WHERE key = 'currentSyncTick')
     WHERE merged_into_id IS NOT NULL AND deleted_at IS NULL
   `);
 }
