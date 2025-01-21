83f81314bf31a396d7b7bd39f18a81192175ab79
Daniel Nash <dannash200@gmail.com>
2025-01-21 19:29:45 +1300

    a speedy index

--- packages/database/src/migrations/1737439073187-addGenerateAppointmentTaskIndexes.js
+++ packages/database/src/migrations/1737439073187-addGenerateAppointmentTaskIndexes.js
@@ -1,9 +1,9 @@
 export async function up(query) {
   await query.sequelize.query(
-    'CREATE INDEX IF NOT EXISTS idx_appointments_schedule_id_start_time_desc ON appointments (schedule_id, start_time desc);',
+    'CREATE INDEX IF NOT EXISTS idx_appointments_schedule_id_start_time_desc ON appointments (schedule_id, start_time DESC);',
   );
 }
 
 export async function down(query) {
-  await query.sequelize.query('DROP INDEX IF EXISTS idx_appointments_schedule_id_start_time;');
+  await query.sequelize.query('DROP INDEX IF EXISTS idx_appointments_schedule_id_start_time_desc;');
 }
