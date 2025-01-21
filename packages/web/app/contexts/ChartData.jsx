204819086e9ca2bae0ceee8d6aef8968615d1b28
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-01-20 20:39:09 -0400

    Refactor initial chart type selection logic

--- packages/web/app/contexts/ChartData.jsx
+++ packages/web/app/contexts/ChartData.jsx
@@ -1,5 +1,8 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { useUserPreferencesQuery } from '../api/queries/useUserPreferencesQuery';
+import { useEncounter } from './Encounter';
+import { useEncounterChartWithResponseQuery } from '../api/queries/useEncounterChartWithResponseQuery';
+import { combineQueries } from '../api';
 
 const ChartDataContext = createContext({
   selectedChartTypeId: null,
@@ -9,21 +12,33 @@ const ChartDataContext = createContext({
 export const useChartData = () => useContext(ChartDataContext);
 
 export const ChartDataProvider = ({ children }) => {
-  const { data: userPreferences, isLoading } = useUserPreferencesQuery();
-  const [selectedChartTypeId, setSelectedChartTypeId] = useState(
-    userPreferences?.selectedChartTypeId,
-  );
+  const { encounter } = useEncounter();
+  const [isInitiated, setIsInitiated] = useState(false);
+  const [selectedChartTypeId, setSelectedChartTypeId] = useState('');
+  const userPreferencesQuery = useUserPreferencesQuery();
+  const chartWithResponseQuery = useEncounterChartWithResponseQuery(encounter?.id);
+  const {
+    data: [userPreferences, chartWithResponse],
+    isLoading,
+  } = combineQueries([userPreferencesQuery, chartWithResponseQuery]);
 
   useEffect(() => {
-    if (userPreferences) {
-      setSelectedChartTypeId(userPreferences?.selectedChartTypeId);
+    if (!isLoading && !isInitiated) {
+      const chartResponseId = chartWithResponse?.[0]?.id;
+      // Only set initial type if encounter has chart responses
+      if (chartResponseId) {
+        // Prioritize user preference, chart with response is only a fallback
+        const initialChart = userPreferences?.selectedChartTypeId ?? chartResponseId;
+        setSelectedChartTypeId(initialChart);
+      }
+      setIsInitiated(true);
     }
-  }, [userPreferences]);
+  }, [userPreferences, chartWithResponse, isInitiated, isLoading]);
 
   return (
     <ChartDataContext.Provider
       value={{
-        isLoading,
+        isLoading: !isInitiated,
         selectedChartTypeId,
         setSelectedChartTypeId,
       }}
