8115ec44bf4d07e576571c094b51e0c92fc35b75
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-01-30 09:09:16 +1300

    Update OutpatientBookingCalendar.jsx

--- packages/web/app/views/scheduling/outpatientBookings/OutpatientBookingCalendar.jsx
+++ packages/web/app/views/scheduling/outpatientBookings/OutpatientBookingCalendar.jsx
@@ -1,5 +1,4 @@
 import Box from '@mui/material/Box';
-import Skeleton from '@mui/material/Skeleton';
 import { omit } from 'lodash';
 import React, { useState } from 'react';
 import styled from 'styled-components';
