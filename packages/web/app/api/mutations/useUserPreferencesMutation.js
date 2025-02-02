65193b3c67b66a54e9fee356d2e2f063fc511677
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-02-03 10:08:20 +1300

    Update useUserPreferencesMutation.js

--- packages/web/app/api/mutations/useUserPreferencesMutation.js
+++ packages/web/app/api/mutations/useUserPreferencesMutation.js
@@ -12,8 +12,6 @@ export const useUserPreferencesMutation = facilityId => {
   return useMutation({
     mutationKey: ['userPreferences'],
     mutationFn: ({ key, value }) => api.post('user/userPreferences', { facilityId, key, value }),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries(['userPreferences', currentUser.id]);
-    },
+    onSuccess: () => queryClient.invalidateQueries(['userPreferences', currentUser.id]),
   });
 };
