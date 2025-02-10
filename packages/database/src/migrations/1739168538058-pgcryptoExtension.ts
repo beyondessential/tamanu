917e8b57d0cded34549687cc400d02220dc98215
Félix Saparelli <felix@bes.au>
2025-02-10 22:23:53 +1300

    only add pgcrypto to <13

--- packages/database/src/migrations/1739168538058-pgcryptoExtension.ts
+++ packages/database/src/migrations/1739168538058-pgcryptoExtension.ts
@@ -1,9 +1,14 @@
-import { QueryInterface } from 'sequelize';
+import { QueryInterface, QueryTypes } from 'sequelize';
+
 
 export async function up(query: QueryInterface): Promise<void> {
-  await query.sequelize.query('CREATE EXTENSION pgcrypto');
+  // we only need pgcrypto for postgres <13
+  const rows = await query.sequelize.query('show server_version_num', { type: QueryTypes.SELECT });
+  if (((rows?.[0] as { [key: string]: any })?.server_version_num ?? 0) < 13000) {
+    await query.sequelize.query('CREATE EXTENSION IF NOT EXISTS pgcrypto');
+  }
 }
 
 export async function down(query: QueryInterface): Promise<void> {
-  await query.sequelize.query('DROP EXTENSION pgcrypto');
+  await query.sequelize.query('DROP EXTENSION IF EXISTS pgcrypto');
 }
