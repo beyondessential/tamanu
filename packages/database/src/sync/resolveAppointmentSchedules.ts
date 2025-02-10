4051c76fcff386a59f8d35363ab3c5bfcd4f1645
Daniel Nash <dannash200@gmail.com>
2025-02-10 16:24:09 +1300

    wip"

--- packages/database/src/sync/resolveAppointmentSchedules.ts
+++ packages/database/src/sync/resolveAppointmentSchedules.ts
@@ -13,82 +13,25 @@ import { sanitizeRecord } from './sanitizeRecord';
  * @param AppointmentModel
  * @param changes
  */
-export const resolveAppointmentSchedules2 = async (
-  AppointmentModel: typeof Appointment,
-  changes: SyncSnapshotAttributes[],
-): Promise<SyncHookSnapshotChanges | undefined> => {
-  const relevantChanges = changes.filter(
-    (c) => !c.isDeleted && c.data.scheduleId && c.data.status === APPOINTMENT_STATUSES.CANCELLED,
-  );
-  const appointmentScheduleIds = [...new Set(relevantChanges.map((c) => c.data.scheduleId))];
-
-  if (appointmentScheduleIds.length === 0) {
-    return;
-  }
-
-  const outOfBoundAppointments =
-    (await AppointmentModel.sequelize.models.AppointmentSchedule.sequelize.query(
-      `
-    SELECT
-      *
-    FROM
-      appointments
-    JOIN
-      appointment_schedules ON appointments.schedule_id = appointment_schedules.id
-    WHERE
-      id NOT IN (:appointmentIds) AND schedule_id IN (:scheduleIds) AND status <> :canceledStatus
-    AND
-      start_time > appointment_schedules.until_date
-    `,
-      {
-        type: QueryTypes.SELECT,
-        replacements: {
-          canceledStatus: APPOINTMENT_STATUSES.CANCELLED,
-          scheduleIds: appointmentScheduleIds,
-          appointmentIds: relevantChanges.map((c) => c.data.id),
-        },
-      },
-    )) as Appointment[];
-
-  if (outOfBoundAppointments.length === 0) {
-    return;
-  }
-
-  return {
-    inserts: outOfBoundAppointments.map((a) => ({
-      direction: SYNC_SESSION_DIRECTION.INCOMING,
-      recordType: 'appointments',
-      recordId: a.id,
-      isDeleted: true,
-      data: sanitizeRecord(a),
-    })),
-    updates: [],
-  };
-};
-
-/**
-//  * The new changes will be persisted in the sync_snapshot table
-//  * @param AppointmentModel
-//  * @param changes
-//  */
 export const resolveAppointmentSchedules = async (
   AppointmentScheduleModel: typeof AppointmentSchedule,
   changes: SyncSnapshotAttributes[],
 ): Promise<SyncHookSnapshotChanges | undefined> => {
-  const relevantChanges = changes.filter(
-    (c) => !c.isDeleted && c.data.untilDate && c.data.isFullyGenerated,
-  );
+  const relevantChanges = changes.filter((c) => !c.isDeleted && c.data.cancelledAtDate);
 
   if (relevantChanges.length === 0) {
     return;
   }
 
-  const scheduleUntilDates = mapValues(keyBy(relevantChanges, 'data.id'), 'data.untilDate');
+  const generatedUntilDates = mapValues(
+    keyBy(relevantChanges, 'data.id'),
+    'data.generatedUntilDate',
+  );
 
   const outOfBoundAppointments = (await AppointmentScheduleModel.sequelize.query(
     `
-    WITH schedule_end_dates AS (
-     select value as date, key as id from json_each(:scheduleUntilDates)
+    WITH schedule_generated_until_dates AS (
+     select value as date, key as id from json_each(:generatedUntilDates)
     )
     SELECT
       *
@@ -98,14 +41,14 @@ export const resolveAppointmentSchedules = async (
       schedule_id IN (:scheduleIds)
       AND status <> :canceledStatus
     AND
-      start_time > (SELECT date::date_string FROM schedule_end_dates WHERE id::uuid = schedule_id)
+      start_time > (SELECT date::date_string FROM schedule_generated_until_dates WHERE id::uuid = schedule_id)
     `,
     {
       type: QueryTypes.SELECT,
       replacements: {
         canceledStatus: APPOINTMENT_STATUSES.CANCELLED,
-        scheduleIds: Object.keys(scheduleUntilDates),
-        scheduleUntilDates: JSON.stringify(scheduleUntilDates),
+        scheduleIds: Object.keys(generatedUntilDates),
+        generatedUntilDates: JSON.stringify(generatedUntilDates),
       },
     },
   )) as Appointment[];
