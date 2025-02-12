5e3671eeb13bf659a813ad0d98dde9ae05dfbab4
Daniel Nash <dannash200@gmail.com>
2025-02-13 10:07:18 +1300

    fix lint

--- packages/web/app/views/scheduling/outpatientBookings/OutpatientBookingCalendar.jsx
+++ packages/web/app/views/scheduling/outpatientBookings/OutpatientBookingCalendar.jsx
@@ -1,6 +1,5 @@
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
-import { omit } from 'lodash';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
