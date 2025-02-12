a1126b73eb82a74eeb63ce14d3d053afa1e0ce6d
Félix Saparelli <felix@bes.au>
2025-02-13 03:44:47 +1300

    fix migration checking for v12

--- packages/database/src/migrations/1739168538058-pgcryptoExtension.ts
+++ packages/database/src/migrations/1739168538058-pgcryptoExtension.ts
@@ -1,10 +1,13 @@
 import { QueryInterface, QueryTypes } from 'sequelize';
 
-
 export async function up(query: QueryInterface): Promise<void> {
   // we only need pgcrypto for postgres <13
-  const rows = await query.sequelize.query('show server_version_num', { type: QueryTypes.SELECT });
-  if (((rows?.[0] as { [key: string]: any })?.server_version_num ?? 0) < 13000) {
+  const rows = await query.sequelize.query(
+    'SELECT setting FROM pg_settings WHERE name = $name LIMIT 1',
+    { type: QueryTypes.SELECT, bind: { name: 'server_version_num' } },
+  );
+
+  if (((rows?.[0] as { [key: string]: any })?.setting ?? 0) < 130000) {
     await query.sequelize.query('CREATE EXTENSION IF NOT EXISTS pgcrypto');
   }
 }
