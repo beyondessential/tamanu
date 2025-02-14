fe4c8b11ef1efe78e6695908c66291e431fcf96b
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2025-02-14 11:02:15 +1100

    fix(fhir): SAV-847: Filter out unresolved resources from FHIR search API (hotfix 2.24.1) (#7210)
    ---------
    
    Co-authored-by: Rohan Port <rohan@bes.au>

--- packages/central-server/app/tasks/fhir/resolver.js
+++ packages/central-server/app/tasks/fhir/resolver.js
@@ -1,4 +1,21 @@
-export function resolver(_, { log, sequelize }) {
-  log.debug('Running FHIR upstream resolver');
-  return sequelize.query('CALL fhir.resolve_upstreams()');
+import { FHIR_INTERACTIONS } from '@tamanu/constants';
+import { resourcesThatCanDo } from '@tamanu/shared/utils/fhir/resources';
+import { sleepAsync } from '@tamanu/utils/sleepAsync';
+
+export async function resolver(_, { log, sequelize, models }) {
+  await sleepAsync(3000); // sleep for 3 seconds to allow materialisation jobs to complete
+
+  const materialisableResources = resourcesThatCanDo(
+    models,
+    FHIR_INTERACTIONS.INTERNAL.MATERIALISE,
+  );
+
+  log.debug('Starting resolve');
+  await sequelize.transaction(async () => {
+    for (const Resource of materialisableResources) {
+      await Resource.resolveUpstreams();
+    }
+  });
+
+  log.debug('Done resolving');
 }
