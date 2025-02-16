ea8ab95cfe9027fd50e0ea9eb750fbe23d03de47
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-02-16 18:39:20 -0400

    Add missing dependency

--- packages/mobile/App/ui/components/Forms/SurveyForm/FormFields.tsx
+++ packages/mobile/App/ui/components/Forms/SurveyForm/FormFields.tsx
@@ -101,7 +101,7 @@ export const FormFields = ({
 
   const shouldShow = useCallback(
     (component: ISurveyScreenComponent) => checkVisibilityCriteria(component, components, values),
-    [values],
+    [components, values],
   );
 
   // Handle back button press or swipe right gesture
