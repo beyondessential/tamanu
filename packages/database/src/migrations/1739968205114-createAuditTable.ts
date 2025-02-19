9a25fed0965a3f19f13c6db9a7da67d9ade03e71
Félix Saparelli <felix@bes.au>
2025-02-20 01:59:01 +1300

    add changelogs table

---
+++ packages/database/src/migrations/1739968205114-createAuditTable.ts
@@ -0,0 +1,64 @@
+import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  await query.createTable('changelogs', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('gen_random_uuid'),
+    },
+    table_oid: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    table_schema: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    table_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    logged_at: {
+      type: DataTypes.TIMESTAMP,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.TIMESTAMP,
+      allowNull: false,
+    },
+    updated_at: {
+      type: DataTypes.TIMESTAMP,
+      allowNull: false,
+    },
+    deleted_at: {
+      type: DataTypes.TIMESTAMP,
+      allowNull: true,
+    },
+    updated_at_sync_tick: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+    },
+    updated_by_user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+    record_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    record_data: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+  });
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  await query.dropTable('changelogs');
+}
