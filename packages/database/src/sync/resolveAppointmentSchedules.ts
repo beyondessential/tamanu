8805798d509fc49e468d630d7ca0459a39490e0b
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-02-25 06:53:16 +1300

    feat(repeatingAppointments): NASS-1572: Clean up appointments using sync hook (#7187)
    * some tweaks to styles to make compatable across usages
    
    * add modal
    
    * it got kinda confusing refering to it as selected appointment when it was behaving more like intialForm values
    
    * roll that bak lol:
    
    * yes yes getting close
    
    * make it so it closes modify modal 2 but what to do about mode itself where to store
    
    * ohk yeah we on now imho
    
    * whats this
    
    * this works but is ugly
    
    * getting closer
    
    * smashing yeah
    
    * i think this cray
    
    * make endMode state gotemmm
    
    * yes
    
    * slightly better rules
    
    * nice it works and way better read
    
    * shoudl be simple like this
    
    * we need this kinda annoying ?? null but ill replace with Appointment.build
    
    * and replaced
    
    * ohk cool actully cant use build
    
    * remove edge case handling due to thoams change thaank u
    
    * wip: add deletes to incomingSyncHook
    
    * make test for sync hook
    
    * first go at resolve appt sched
    
    * make test for sync hook
    
    * working
    
    * rethink
    
    * working
    
    * slightly stuck
    
    * think we good
    
    * wip
    
    * yes
    
    * wip"
    
    * all works
    
    * all works
    
    * all works
    
    * add migration thing
    
    * ulrealted
    
    * ulrealted
    
    * ulrealted
    
    * ulrealted
    
    * fix tests
    
    * yes
    
    * fix casting
    
    * fix last sync test
    
    * dbt
    
    * dbt
    
    * dbt
    
    * add catch for cancelled_at_date
    
    * silly mistake
    
    * rebase tweaks
    
    * white space

---
+++ packages/database/src/sync/resolveAppointmentSchedules.ts
@@ -0,0 +1,73 @@
+import { QueryTypes } from 'sequelize';
+import { keyBy, mapValues } from 'lodash';
+
+import { APPOINTMENT_STATUSES } from '@tamanu/constants';
+
+import type { Appointment, AppointmentSchedule } from 'models';
+import type { SyncHookSnapshotChanges, SyncSnapshotAttributes } from 'types/sync';
+import { SYNC_SESSION_DIRECTION } from './constants';
+import { sanitizeRecord } from './sanitizeRecord';
+
+/**
+ * The new changes will be persisted in the sync_snapshot table
+ * @param AppointmentModel
+ * @param changes
+ */
+export const resolveAppointmentSchedules = async (
+  AppointmentScheduleModel: typeof AppointmentSchedule,
+  changes: SyncSnapshotAttributes[],
+): Promise<SyncHookSnapshotChanges | undefined> => {
+  const relevantChanges = changes.filter((c) => !c.isDeleted && c.data.cancelledAtDate);
+
+  if (relevantChanges.length === 0) {
+    return;
+  }
+
+  // Map of incoming generatedUntilDates for each schedule
+  const generatedUntilDates = mapValues(
+    keyBy(relevantChanges, 'data.id'),
+    'data.generatedUntilDate',
+  );
+
+  const outOfBoundAppointments = (await AppointmentScheduleModel.sequelize.query(
+    `
+    WITH schedule_generated_until_dates AS (
+     SELECT value::date_string AS date, key::uuid AS id from json_each_text(:generatedUntilDates)
+    )
+    SELECT
+      *
+    FROM
+      appointments
+    WHERE
+      schedule_id IN (:scheduleIds)
+      AND status <> :canceledStatus
+    AND
+      start_time::date_string > (SELECT date FROM schedule_generated_until_dates WHERE id = schedule_id)
+    `,
+    {
+      type: QueryTypes.SELECT,
+      replacements: {
+        canceledStatus: APPOINTMENT_STATUSES.CANCELLED,
+        scheduleIds: Object.keys(generatedUntilDates),
+        generatedUntilDates: JSON.stringify(generatedUntilDates),
+      },
+    },
+  )) as Appointment[];
+
+  if (outOfBoundAppointments.length === 0) {
+    return;
+  }
+
+  const inserts = outOfBoundAppointments.map((a) => ({
+    direction: SYNC_SESSION_DIRECTION.INCOMING,
+    recordType: 'appointments',
+    recordId: a.id,
+    isDeleted: true,
+    data: sanitizeRecord(a),
+  }));
+
+  return {
+    inserts,
+    updates: [],
+  };
+};
