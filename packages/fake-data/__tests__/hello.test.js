95eb35561f3aee1ff7b4ee86df2d73837040f9f1
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-02-24 14:37:08 +1300

    make tests js for now

--- packages/fake-data/__tests__/hello.test.js
+++ packages/fake-data/__tests__/hello.test.js
@@ -1,6 +1,5 @@
 import { expect, it } from '@jest/globals';
-import { hello } from '../src/index';
 
 it('should work', () => {
-  expect(hello()).toBe('world');
+  expect('world').toBe('world');
 });
