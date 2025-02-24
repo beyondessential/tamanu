33601a7be2e5b65b908222f40c0acb6d85b07c44
Daniel Nash <dannash200@gmail.com>
2025-02-25 10:06:09 +1300

    basic audit middleare to attach user to session

---
+++ packages/facility-server/app/middleware/audit.js
@@ -0,0 +1,10 @@
+const attachAuditUserToDbSession = async (req, res, next) => {
+  const { db, user } = req;
+  const { id: userId } = user;
+
+  await db.sequelize.query(`set_config('audit.user_id', :userId, true);`, {
+    replacements: { userId },
+  });
+
+  next();
+};
