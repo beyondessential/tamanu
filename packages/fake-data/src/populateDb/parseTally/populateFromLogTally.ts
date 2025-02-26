188a39b99c8be04ecd66a7e49d4dc838c48bfc5f
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-02-27 11:30:19 +1300

    Update populateFromLogTally.ts

--- packages/fake-data/src/populateDb/parseTally/populateFromLogTally.ts
+++ packages/fake-data/src/populateDb/parseTally/populateFromLogTally.ts
@@ -1,25 +1,23 @@
 import { times } from 'lodash';
-import { createEncounter, createPatient, createRepeatingAppointment } from './populateDb';
+import { createEncounter, createPatient, createRepeatingAppointment } from '../helpers';
 import { Models } from '@tamanu/database';
 
+// TODO: this needs way more data groups
 const MODEL_TO_FUNCTION = {
   Appointment: { POST: createRepeatingAppointment, PUT: () => null },
   Patient: { POST: createPatient, PUT: () => null },
   Encounter: { POST: createEncounter, PUT: () => null },
+  ImagingRequest: { POST: () => null, PUT: () => null },
+  Invoice: { POST: () => null, PUT: () => null },
+  LabRequest: { POST: () => null, PUT: () => null },
+  ProgramRegistry: { POST: () => null, PUT: () => null },
+  Survey: { POST: () => null, PUT: () => null },
+  Tasking: { POST: () => null, PUT: () => null },
+  Vaccine: { POST: () => null, PUT: () => null },
+  ReportDefinition: { POST: () => null, PUT: () => null },
 };
 
 // Read and parse the file
-
-const exampleFile = {
-  Patient: {
-    POST: 10,
-    PUT: 10,
-  },
-  Encounter: {
-    POST: 10,
-    PUT: 10,
-  },
-};
 
 // Create/get db instance
 
@@ -29,7 +27,7 @@ const exampleFile = {
 
 export const populateDbFromTallyFile = (tallyFile: object, models: Models) => {
   Object.entries(tallyFile).forEach(([model, tally]) => {
-    times(tally.POST, () => MODEL_TO_FUNCTION[model].POST({ models }));
-    times(tally.PUT, () => MODEL_TO_FUNCTION[model].PUT({ models }));
+    times(tally.POST, async () => MODEL_TO_FUNCTION[model].POST({ models }));
+    times(tally.PUT, async () => MODEL_TO_FUNCTION[model].PUT({ models }));
   });
 };
