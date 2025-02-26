230940c6441992f694ee6461e6b40ab2d36038b2
Daniel Nash <dannash200@gmail.com>
2025-02-27 12:46:08 +1300

    changes

--- packages/facility-server/app/middleware/audit.js
+++ packages/facility-server/app/middleware/audit.js
@@ -1,9 +1,9 @@
 import { namespace } from '@tamanu/database/services/database';
+import { AUDIT_USERID_KEY } from '@tamanu/constants/auth';
 
 export const attachAuditUserToDbSession = async (req, _res, next) => {
   namespace.run(() => {
-    namespace.set('audit.userid', req.user?.id);
+    namespace.set(AUDIT_USERID_KEY, req.user?.id);
     next();
   });
 };
-
