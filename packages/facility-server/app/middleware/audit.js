a252fe3dcfe9fd96a9d458bf74f9b469d9866f36
Daniel Nash <dannash200@gmail.com>
2025-02-27 08:01:10 +1300

    use bind

--- packages/facility-server/app/middleware/audit.js
+++ packages/facility-server/app/middleware/audit.js
@@ -2,7 +2,8 @@ import { namespace } from '@tamanu/database/services/database';
 
 export const attachAuditUserToDbSession = async (req, res, next) => {
   namespace.run(() => {
-    namespace.set("audit.userid", req.user?.id);
+    namespace.set('userid', req.user?.id);
     next();
   });
 };
+
