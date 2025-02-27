743024f997a27a36e078c756cd4a6d95eac79ed3
Daniel Nash <dannash200@gmail.com>
2025-02-28 10:00:49 +1300

    remove duplicate middleware

--- packages/facility-server/app/middleware/audit.js
+++
@@ -1,9 +0,0 @@
-import { namespace } from '@tamanu/database/services/database';
-import { AUDIT_USERID_KEY } from '@tamanu/constants/auth';
-
-export const attachAuditUserToDbSession = async (req, _res, next) => {
-  namespace.run(() => {
-    namespace.set(AUDIT_USERID_KEY, req.user?.id);
-    next();
-  });
-};
