9470c95a0fb6250cab34009a7f753c54123d2dbe
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-04 13:36:45 +1300

    create migration + model

---
+++ packages/database/src/migrations/1741047127055-createUserIntentionalPatientViewLog.ts
@@ -0,0 +1,50 @@
+import { DataTypes, Sequelize, QueryInterface } from 'sequelize';
+
+const TABLE = { tableName: 'user_patient_intentional_views', schema: 'logs' };
+
+export async function up(query: QueryInterface) {
+  await query.createTable(TABLE, {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('uuid_generate_v4'),
+    },
+    viewed_by_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    patient_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'patient',
+        key: 'id',
+      },
+    },
+    facility_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+    logged_at: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    endpoint: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+}
+
+export async function down(query: QueryInterface) {
+  await query.dropTable(TABLE);
+}
---
+++ packages/database/src/models/UserPatientIntentionalView.ts
@@ -0,0 +1,53 @@
+import { DataTypes } from 'sequelize';
+
+import { SYNC_DIRECTIONS } from '@tamanu/constants';
+
+import { Model } from './Model';
+import type { InitOptions, Models } from '../types/model';
+
+export class UserPatientIntentionalView extends Model {
+  declare id: string;
+  declare viewedById: string;
+  declare patientId: string;
+  declare facilityId: string;
+  declare loggedAt: string;
+  declare endpoint: string;
+
+  static initModel({ primaryKey, ...options }: InitOptions) {
+    super.init(
+      {
+        id: primaryKey,
+        loggedAt: {
+          type: DataTypes.STRING,
+        },
+        endpoint: {
+          type: DataTypes.STRING,
+        },
+      },
+
+      { ...options, syncDirection: SYNC_DIRECTIONS.DO_NOT_SYNC },
+    );
+  }
+
+  static initRelations(models: Models) {
+    this.belongsTo(models.Patient, {
+      foreignKey: 'patientId',
+      as: 'patient',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'viewedById',
+      as: 'viewedBy',
+    });
+
+    this.belongsTo(models.Facility, {
+      foreignKey: 'facilityId',
+      as: 'facility',
+    });
+
+    this.belongsTo(models.User, {
+      foreignKey: 'viewedById',
+      as: 'viewedBy',
+    });
+  }
+}
