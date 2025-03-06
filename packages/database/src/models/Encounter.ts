26d81552ac5dd6340e7c2ccda5304650ad769fb3
Rohan Port <rohan@bes.au>
2025-03-06 16:36:46 +1100

    Removed department field

--- packages/database/src/models/Encounter.ts
+++ packages/database/src/models/Encounter.ts
@@ -21,7 +21,6 @@ import type { Discharge } from './Discharge';
 import { onCreateEncounterMarkPatientForSync } from '../utils/onCreateEncounterMarkPatientForSync';
 import type { SessionConfig } from '../types/sync';
 import type { User } from './User';
-import type { Department } from './Department';
 
 export class Encounter extends Model {
   declare id: string;
@@ -39,7 +38,6 @@ export class Encounter extends Model {
   declare patientBillingTypeId?: string;
   declare referralSourceId?: string;
 
-  declare department?: Department;
   declare location?: Location;
   declare patient?: Patient;
   declare discharge?: Discharge;
