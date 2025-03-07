079cab4c06991cd7561320a5e44c40c5bd361426
khoi.nguyen <khoi@bes.au>
2025-03-07 11:59:10 +0700

    fix: EPI-1085: Apply frequecies enabled setting to front-end (#7247)
    feat: Apply frequecies enabled setting to front-end

--- packages/web/app/utils/medications.jsx
+++ packages/web/app/utils/medications.jsx
@@ -1,9 +1,10 @@
 import { ADMINISTRATION_FREQUENCY_SYNONYMS } from '@tamanu/constants';
 import { camelCase } from 'lodash';
 
-export const getTranslatedFrequencySynonyms = getTranslation => {
+export const getTranslatedFrequencySynonyms = (frequenciesEnabled, getTranslation) => {
   const result = {};
   Object.entries(ADMINISTRATION_FREQUENCY_SYNONYMS).forEach(([frequency, synonyms]) => {
+    if (!frequenciesEnabled[frequency]) return;
     const labelKey = getTranslation(
       `medication.frequency.${camelCase(frequency)}.label`,
       frequency,
