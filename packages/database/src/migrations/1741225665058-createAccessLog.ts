858992589022cf478add4ac8a345aca5ed237766
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-07 15:20:25 +1300

    Update 1741225665058-createAccessLog.ts
    feat(audit): NASS-1601: Patient view log model + migration (#7309)
    
    * new pr
    
    * Update constants.js
    
    * doc tweaks
    
    * merge handling
    
    * try explicitly declare schema
    
    * Revert "try explicitly declare schema"
    
    This reverts commit 35e58f5e8c4a1ec2e61cff9d20b296bd0b1fe4ea.
    
    * correct schema
    
    * Update mergePatient.js
    
    * fix test
    
    * daniel changes
    
    * use string dates
    
    * Update user_patient_views.yml
    
    * Update user_patient_views.yml
    
    * Update database/model/central-server/logs/user_patient_views.md
    
    ---------
    
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>
    
    fix(web): update browser support copy for MS Edge (#7297)
    
    fix(web): update default browser support copy for MS Edge
    feat(web): make splash page messages available for translation
    
    Merge pull request #7308 from beyondessential/merge-2-26-into-main
    
    merge: 2.26 into main
    
    ci: fix toolbox image (#7311)
    
    ci: fix docker image syntax (#7312)
    
    serves me right
    
    release: Bump version to 2.28.0
    
    fix(translations): NASS-1470: Re-imported reference data overwrites non edited translations (#7248)
    
    * data migration for en null language defaults
    
    * import
    
    * import reference data translation as null
    
    * comment
    
    * ref data for migration
    
    * faster
    
    * not offer null lang in select
    
    * pass default
    
    * cool solution imho using placeholder
    
    * think this kinda clever tbh
    
    * this is actually better tbh
    
    * crazy fo dis one
    
    * me so sily
    
    * nice
    
    * We need to use an explicit default language code for id generation
    
    * yes
    
    * this is bad performance wise but just meanwhile
    
    * this works but how can i do in bulk
    
    * better
    
    * todo lets do with raw query and conflicts
    
    * much be something like this
    
    * yessir
    
    * bulk upsert
    
    * comment
    
    * change that back
    
    * chiiiiil
    
    * remove bad thign from migration
    
    * yes
    
    * yes
    
    * yes
    
    * slight cleanup and shud do it
    
    * yes yes yessir
    
    fix if translation text is a number
    
    order program surveys endpoint results by name
    
    tests
    
    sorting
    
    suggested changes
    
    Update 1741225665058-createPatientAccessLog.ts
    
    even more changes
    
    rename migration
    
    fix fake
    
    update fake
    
    update docs
    
    Update constants.js
    
    Update accesses.yml
    
    delete symlinks
    
    new symlinks
    
    Update accesses.yml
    
    Co-Authored-By: Daniel Nash <38335330+dannash100@users.noreply.github.com>

---
+++ packages/database/src/migrations/1741225665058-createAccessLog.ts
@@ -0,0 +1,66 @@
+import { DataTypes, Sequelize, QueryInterface } from 'sequelize';
+
+const TABLE = { tableName: 'accesses', schema: 'logs' };
+
+export async function up(query: QueryInterface) {
+  await query.createTable(TABLE, {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('uuid_generate_v4'),
+    },
+    user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: { tableName: 'users', schema: 'public' },
+        key: 'id',
+      },
+    },
+    record_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    record_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    facility_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      references: {
+        model: { tableName: 'facilities', schema: 'public' },
+        key: 'id',
+      },
+    },
+    session_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    device_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    logged_at: {
+      type: DataTypes.DATETIMESTRING,
+      allowNull: false,
+    },
+    front_end_context: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+    back_end_context: {
+      type: DataTypes.JSONB,
+      allowNull: false,
+    },
+    is_mobile: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  });
+}
+
+export async function down(query: QueryInterface) {
+  await query.dropTable(TABLE);
+}
