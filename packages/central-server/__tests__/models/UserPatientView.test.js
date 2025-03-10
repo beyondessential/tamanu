eaa5f9e5d0a11aba630a1e662d9aee4ec2ee1a2c
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-10 15:11:15 +1300

    central server test

---
+++ packages/central-server/__tests__/models/UserPatientView.test.js
@@ -0,0 +1,33 @@
+import { createDummyPatient } from '@tamanu/database/demoData/patients';
+import { createTestContext } from '../utilities';
+
+describe('UserPatientView', () => {
+  let patient = null;
+  let app = null;
+  let baseApp = null;
+  let models = null;
+  let ctx;
+
+  beforeAll(async () => {
+    ctx = await createTestContext();
+    baseApp = ctx.baseApp;
+    models = ctx.models;
+    app = await baseApp.asRole('practitioner');
+    patient = await models.Patient.create(createDummyPatient(models));
+  });
+  afterAll(() => ctx.close());
+
+  it('should record a patient view log with appropriate details', async () => {
+    await app.get(`/api/patient/${patient.id}`);
+    const userPatientViewLogs = await models.UserPatientView.findAll({ raw: true });
+
+    expect(userPatientViewLogs).toHaveLength(1);
+    const testLog = userPatientViewLogs[0];
+
+    expect(testLog).toMatchObject({
+      patientId: patient.id,
+      viewedById: app.user.id,
+      facilityId: null,
+    });
+  });
+});
