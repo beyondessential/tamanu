ac43ca711428bdb724d3ca3263f35caf5413a9d1
khoi.nguyen <khoi@bes.au>
2025-03-11 12:10:46 +0700

    test(sync): Remove console logs from Sync Lookup data tests

--- packages/central-server/__tests__/sync/CentralSyncManager.syncLookup.test.js
+++ packages/central-server/__tests__/sync/CentralSyncManager.syncLookup.test.js
@@ -684,8 +684,6 @@ describe('Sync Lookup data', () => {
 
     const patientLinkedModels = await getPatientLinkedModels(outgoingModels);
     const syncLookupData = await models.SyncLookup.findAll({});
-    console.log({length: syncLookupData.length, });
-    console.log(syncLookupData)
 
     for (const model of Object.values(patientLinkedModels)) {
       const syncLookupRecord = syncLookupData.find(
