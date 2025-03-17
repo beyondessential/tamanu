8b9e7f73b7afe912726587b421c16aa0bfbc0bb1
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-17 13:56:32 +1300

    Update usePatientCurrentEncounterQuery.js

--- packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
+++ packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
@@ -2,8 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../useApi';
 
 export const usePatientCurrentEncounterQuery = patientId => {
+  const { facilityId } = useAuth();
+  const facilityFetchOptions = { facilityId, ...fetchOptions };
+
   const api = useApi();
   return useQuery(['patientCurrentEncounter', patientId], () =>
-    api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`),
+    api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`, facilityFetchOptions),
   );
 };
