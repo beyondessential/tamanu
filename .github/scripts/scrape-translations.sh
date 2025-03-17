914f242a3acb50a3a610d0518c628478f98424d2
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-03-16 20:49:14 -0400

    Only include enums if we are after the master speadsheet, otherwise focus on the specified directory only

--- .github/scripts/scrape-translations.sh
+++ .github/scripts/scrape-translations.sh
@@ -10,8 +10,12 @@ gtregex="getTranslation\(\s*?[\"'](.*?)[\"'],.*?[\"'](.*?)[\"'].*?\)"
 # running git diff main..<branch> --unified=0 | grep '^+ ' > branch-diffs.txt
 directory=${1-./packages}
 
-# Get all translated string data from registered enums, TranslatedText and getTranslatedString.
-teoutput=$(npx tsx packages/constants/scripts/printTranslatedEnums.ts)
+# Get all registered enums if no directory is specified
+if [[ "$directory" == "./packages" ]]; then
+  teoutput=$(npx tsx packages/constants/scripts/printTranslatedEnums.ts)
+fi
+
+# Get all translated string data from TranslatedText and getTranslatedString.
 ttoutput=$(rg -PINU --multiline-dotall "$ttregex" -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" "$directory" \
     | rg --multiline-dotall --passthru -U '\n\s*\b' -r '' )
 gtoutput=$(rg -PINU --multiline-dotall "$gtregex" -or '"$1","$2"' -g "*.{ts,tsx,js,jsx}" "$directory")
