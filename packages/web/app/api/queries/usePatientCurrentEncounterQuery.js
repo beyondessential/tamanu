5f8e12ddaa0a36dc0c5f757df9be9d39625be16c
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-17 13:57:20 +1300

    Update usePatientCurrentEncounterQuery.js

--- packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
+++ packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
@@ -4,10 +4,8 @@ import { useAuth } from '../../contexts/Auth';
 
 export const usePatientCurrentEncounterQuery = patientId => {
   const { facilityId } = useAuth();
-  const facilityFetchOptions = { facilityId, ...fetchOptions };
-
   const api = useApi();
   return useQuery(['patientCurrentEncounter', patientId], () =>
-    api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`, facilityFetchOptions),
+    api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`, { facilityId }),
   );
 };
