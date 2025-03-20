ae617003caf590f3ac20e9b78635134bcb415af7
Flynn <flynn.teh@gmail.com>
2025-03-20 15:00:53 +1300

    fix: remove failing test

--- packages/e2e-tests/tests/page.spec.ts
+++ packages/e2e-tests/tests/page.spec.ts
@@ -4,8 +4,3 @@ test('homepage has expected title', async ({ page }) => {
   await page.goto('/');
   await expect(page).toHaveTitle(/Tamanu/);
 });
-
-test('failing test', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Wrong title/);
-});
