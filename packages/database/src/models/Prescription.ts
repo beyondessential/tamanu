b410d50987a4c93ace448460659b0ee7ee8d3ca4
khoi.nguyen <khoi@bes.au>
2025-03-24 22:14:14 +0700

    refactor: update prescription models and remove unnecessary associations

--- packages/database/src/models/Prescription.ts
+++ packages/database/src/models/Prescription.ts
@@ -105,11 +105,6 @@ export class Prescription extends Model {
       as: 'patients',
     });
 
-    this.belongsToMany(models.EncounterPausePrescription, {
-      through: models.EncounterPrescription,
-      foreignKey: 'prescriptionId',
-      as: 'pausePrescriptions',
-    })
 
     this.belongsTo(models.ReferenceData, {
       foreignKey: 'medicationId',
