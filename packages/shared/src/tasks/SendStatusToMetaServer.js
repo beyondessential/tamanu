bb31ee7e942acf7b927b0f913d3b0edbf8ca9cf6
Daniel Nash <dannash200@gmail.com>
2025-03-25 17:07:05 +1300

    yes

--- packages/shared/src/tasks/SendStatusToMetaServer.js
+++ packages/shared/src/tasks/SendStatusToMetaServer.js
@@ -20,15 +20,14 @@ export class SendStatusToMetaServer extends ScheduledTask {
 
   async run() {
     const mockServerId = '00000000-0000-0000-0000-000000000000';
-    const serverInfo = serviceContext();
+    const { 'service.type': serverType, 'service.version': version } = serviceContext();
     const currentSyncTick = await this.models.LocalSystemFact.get(FACT_CURRENT_SYNC_TICK);
-
     const response = await fetchWithTimeout(`${this.metaserverHost}/status/${mockServerId}`, {
       method: 'POST',
       headers: {
         Accept: 'application/json',
-        'X-Tamanu-Client': SERVER_TYPES[serverInfo['server.type'].toUpperCase()],
-        'X-Version': serverInfo['service.version'],
+        'X-Tamanu-Client': SERVER_TYPES[serverType.toUpperCase()],
+        'X-Version': version,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
