00320706528e59d8ab73b653887a54cd676b7941
Daniel Nash <dannash200@gmail.com>
2025-03-25 15:20:54 +1300

    think this is it

--- packages/shared/src/tasks/SendStatusToMetaServer.js
+++ packages/shared/src/tasks/SendStatusToMetaServer.js
@@ -2,33 +2,40 @@ import config from 'config';
 import { log } from '@tamanu/shared/services/logging';
 import { ScheduledTask } from './ScheduledTask';
 import { serviceContext } from '../services/logging/context';
+import { fetchWithTimeout } from '../utils/fetchWithTimeout';
+import { FACT_CURRENT_SYNC_TICK } from '@tamanu/constants';
 
 export class SendStatusToMetaServer extends ScheduledTask {
-  constructor(context, serverType, overrideConfig = null) {
+  constructor(context, overrideConfig = null) {
     const { schedule, jitterTime, enabled } =
       overrideConfig || config.schedules.sendStatusToMetaServer;
     super(schedule, log, jitterTime, enabled);
     this.context = context;
-    this.serverType = serverType;
+    this.models = context.store.models;
     this.metaserverHost = config.metaServer.host
   }
 
   async run() {
-    const serverId = '00000000-0000-0000-0000-000000000000';
+    const mockServerId = '00000000-0000-0000-0000-000000000000';
     const { version, serverType } = serviceContext()
+    const currentSyncTick = await this.models.LocalSystemFacts.get(FACT_CURRENT_SYNC_TICK)
+    const timezone = config.countryTimezone
     const response = await fetchWithTimeout(
-      `${this.metaserverHost}/statuses/${serverId}/${version}`,
+      `${this.metaserverHost}/statuses/${mockServerId}`,
       {
-        method,
+        method: 'POST',
         headers: {
           Accept: 'application/json',
           'X-Tamanu-Client': serverType,
           'X-Version': version,
-          'Content-Type': body ? 'application/json' : undefined,
-          ...headers,
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(body),
-        timeout:
+        body: JSON.stringify({
+          currentSyncTick,
+          timezone
+        }),
+        timeout: 20000
       }
+    );
   }
 }
