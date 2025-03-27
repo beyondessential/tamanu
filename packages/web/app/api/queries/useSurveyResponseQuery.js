93b3236346cdd6c7903b54abab4a125c5077f9ee
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-28 12:32:27 +1300

    pass facility id

--- packages/web/app/api/queries/useSurveyResponseQuery.js
+++ packages/web/app/api/queries/useSurveyResponseQuery.js
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../useApi';
+import { useAuth } from '../../contexts/Auth';
 
 export const useSurveyResponseQuery = surveyResponseId => {
   const api = useApi();
+  const { facilityId } = useAuth();
+
   return useQuery(
     ['surveyResponse', surveyResponseId],
-    () => api.get(`surveyResponse/${surveyResponseId}`),
+    () => api.get(`surveyResponse/${surveyResponseId}`, { facilityId }),
     { enabled: !!surveyResponseId },
   );
 };
