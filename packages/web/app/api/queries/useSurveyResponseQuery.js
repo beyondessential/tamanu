d57742c3a749a6fe85a0a22a00a9c64fbe59cd5b
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-03-28 15:24:34 +1300

    Revert "pass facility id"
    This reverts commit 93b3236346cdd6c7903b54abab4a125c5077f9ee.

--- packages/web/app/api/queries/useSurveyResponseQuery.js
+++ packages/web/app/api/queries/useSurveyResponseQuery.js
@@ -1,14 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../useApi';
-import { useAuth } from '../../contexts/Auth';
 
 export const useSurveyResponseQuery = surveyResponseId => {
   const api = useApi();
-  const { facilityId } = useAuth();
-
   return useQuery(
     ['surveyResponse', surveyResponseId],
-    () => api.get(`surveyResponse/${surveyResponseId}`, { facilityId }),
+    () => api.get(`surveyResponse/${surveyResponseId}`),
     { enabled: !!surveyResponseId },
   );
 };
