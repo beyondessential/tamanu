42b8c5f7675fc4c53fb17ede934903ff0ec472af
Daniel Nash <dannash200@gmail.com>
2025-03-28 15:52:51 +1300

    lint here lint there

--- packages/facility-server/app/tasks/index.js
+++ packages/facility-server/app/tasks/index.js
@@ -6,7 +6,11 @@ import { SendStatusToMetaServer } from '@tamanu/shared/tasks/SendStatusToMetaSer
 import { MedicationDiscontinuer } from './MedicationDiscontinuer';
 import { RefreshUpcomingVaccinations } from './RefreshMaterializedView';
 
-const DEFAULT_TASK_CLASSES = [MedicationDiscontinuer, RefreshUpcomingVaccinations, SendStatusToMetaServer];
+const DEFAULT_TASK_CLASSES = [
+  MedicationDiscontinuer,
+  RefreshUpcomingVaccinations,
+  SendStatusToMetaServer,
+];
 
 export function startScheduledTasks(context, taskClasses) {
   if (config.senaite.enabled) {
@@ -15,8 +19,8 @@ export function startScheduledTasks(context, taskClasses) {
     // senaite.beginPolling();
   }
 
-  const tasks = (taskClasses || DEFAULT_TASK_CLASSES).map(Task => new Task(context));
+  const tasks = (taskClasses || DEFAULT_TASK_CLASSES).map((Task) => new Task(context));
 
-  tasks.forEach(t => t.beginPolling());
-  return () => tasks.forEach(t => t.cancelPolling());
+  tasks.forEach((t) => t.beginPolling());
+  return () => tasks.forEach((t) => t.cancelPolling());
 }
