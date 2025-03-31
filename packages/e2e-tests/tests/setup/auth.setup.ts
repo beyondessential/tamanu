6eeabff8b4d5905fb01f657e78a26302b0d65e86
Flynn <flynn.teh@gmail.com>
2025-03-31 15:19:41 +1300

    feat: auth flow

---
+++ packages/e2e-tests/tests/setup/auth.setup.ts
@@ -0,0 +1,11 @@
+import { test as setup } from '@playwright/test';
+
+setup('authenticate', async ({ page }) => {
+  await page.goto('/');
+  await page.getByPlaceholder('Enter your email address').fill(process.env.TEST_EMAIL!);
+  await page.getByPlaceholder('Enter your password').fill(process.env.TEST_PASSWORD!);
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL('**/#/dashboard');
+
+  await page.context().storageState({ path: '.auth/user.json' });
+});
