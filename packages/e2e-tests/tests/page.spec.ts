66d8a7a59d50c399eb4eaff754ce3483ce945a7d
Andrew <vanbeekandrew@gmail.com>
2025-04-03 11:55:18 +1300

    rename urls.ts to navigation.ts

--- packages/e2e-tests/tests/page.spec.ts
+++ packages/e2e-tests/tests/page.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { goToFacilityFrontend } from '../utils/urls';
+import { goToFacilityFrontend } from '../utils/navigation';
 
 test('homepage has expected title', async ({ page }) => {
   await goToFacilityFrontend(page);
