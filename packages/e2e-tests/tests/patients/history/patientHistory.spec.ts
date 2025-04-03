56a519ca9853a825f4f7d68a0280b93ff7dc7539
Flynn <flynn.teh@gmail.com>
2025-04-03 17:07:06 +1300

    chore: setup some test files

---
+++ packages/e2e-tests/tests/patients/history/patientHistory.spec.ts
@@ -0,0 +1,5 @@
+import { test } from '../../../fixtures/baseFixture';
+
+test.describe('Patient history', () => {
+  test('Admit or check-in', async ({ page }) => {});
+});
---
+++ packages/e2e-tests/tests/patients/patientSideBar.spec.ts
@@ -0,0 +1,9 @@
+import { test } from '../../fixtures/baseFixture';
+
+test.describe('Patient Side Bar', () => {
+  test('Add ongoing condition', async ({ page }) => {});
+
+  test.describe('Death workflow', () => {
+    test('Record death - Male, 3 months', async ({ page }) => {});
+  });
+});
