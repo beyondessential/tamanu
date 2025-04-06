9e4a3230cbb6a534269dd8374ab088fe6affb16c
Flynn <flynn.teh@gmail.com>
2025-04-07 10:00:59 +1200

    feat: auth using page object model

--- packages/e2e-tests/pages/BasePage.ts
+++ packages/e2e-tests/pages/BasePage.ts
@@ -2,9 +2,15 @@ import { Page } from '@playwright/test';
 
 export class BasePage {
   readonly page: Page;
+  readonly url: string;
 
-  constructor(page) {
+  constructor(page: Page, url: string) {
     this.page = page;
+    this.url = url;
+  }
+
+  async goto() {
+    await this.page.goto(this.url);
   }
 
   async navigateTo(url) {
