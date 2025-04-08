991dac993c96cbd354025febc552a0ce09079558
Huy-Tran-Huu-Le <huy@bes.au>
2025-04-08 11:54:35 +0700

    refactor: Remove unused MedicationAdministrationRecord import from medication helper

--- packages/fake-data/src/populateDb/helpers/medication.ts
+++ packages/fake-data/src/populateDb/helpers/medication.ts
@@ -14,7 +14,6 @@ export const createMedication = async ({
     PatientOngoingPrescription,
     EncounterPausePrescription,
     EncounterPausePrescriptionHistory,
-    MedicationAdministrationRecord,
   },
   encounterId,
   patientId,
