3428c625e7e3ae67516414fce8c95ae72e69edfb
Daniel Nash <dannash200@gmail.com>
2025-04-10 17:03:12 +1200

    roll that back

--- packages/database/src/utils/audit/attachAuditUserToDbSession.ts
+++ packages/database/src/utils/audit/attachAuditUserToDbSession.ts
@@ -6,14 +6,9 @@ import { setSessionConfigInNamespace } from '../../services/database';
 import { AUDIT_USERID_KEY } from '@tamanu/constants/database';
 
 export const attachAuditUserToDbSession = async (
-  req: ExpressRequest & { facilityId: string; settings: any },
+  req: ExpressRequest,
   _res: Response,
   next: NextFunction,
 ) => {
-  const { facilityId, settings } = req;
-  const auditSettings = await (settings?.[facilityId] || settings)?.get('audit');
-  if (!auditSettings?.accesses.enabled) {
-    return next();
-  }
   setSessionConfigInNamespace(AUDIT_USERID_KEY, req.user?.id, next);
 };
