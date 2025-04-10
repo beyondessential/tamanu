ddaf8a407d2a1ded2156213591330af547a05c7d
Félix Saparelli <felix@bes.au>
2025-04-10 13:53:05 +1200

    feat(websocket): add config item to disable socket.io server (#7235)

--- packages/central-server/app/createApp.js
+++ packages/central-server/app/createApp.js
@@ -1,12 +1,16 @@
 import { createApi } from './createApi';
 import { createWebsocket } from './createWebsocket';
+import config from 'config';
 
 /**
  * @param {import('./ApplicationContext').ApplicationContext} ctx
  */
 export async function createApp(ctx) {
   const api = await createApi(ctx);
-  const websocket = await createWebsocket(api.httpServer, ctx);
 
-  return { express: api.express, server: api.httpServer, websocket };
+  if (config['socket.io'].enabled) {
+    await createWebsocket(api.httpServer, ctx);
+  }
+
+  return { express: api.express, server: api.httpServer };
 }
