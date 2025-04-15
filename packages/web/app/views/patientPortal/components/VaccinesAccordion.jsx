3dd75c2895b6841038aa9cb49904793796648707
Flynn <flynn.teh@gmail.com>
2025-04-15 15:10:59 +1200

    random statuses

--- packages/web/app/views/patientPortal/components/VaccinesAccordion.jsx
+++ packages/web/app/views/patientPortal/components/VaccinesAccordion.jsx
@@ -9,16 +9,19 @@ const fakeVaccines = [
     name: 'COVID-19 Vaccine',
     dose: 'Dose 1 of 2',
     dueDate: '2023-01-15',
+    status: 'scheduled',
   },
   {
     name: 'Influenza Vaccine',
     dose: 'Annual',
     dueDate: '2023-03-20',
+    status: 'upcoming',
   },
   {
     name: 'Tetanus Booster',
     dose: 'Booster',
     dueDate: '2023-06-10',
+    status: 'overdue',
   },
 ];
 
