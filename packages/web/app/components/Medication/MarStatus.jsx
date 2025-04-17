348df55be900fa514af140480699094c2ddc5d8d
Huy-Tran-Huu-Le <huy@bes.au>
2025-04-17 20:02:57 +0700

    refactor(MarStatus): Remove unused import for cleaner code
    - Eliminated the unused `useEffect` import from the MarStatus component to streamline the code and improve maintainability.

--- packages/web/app/components/Medication/MarStatus.jsx
+++ packages/web/app/components/Medication/MarStatus.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Box } from '@material-ui/core';
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
