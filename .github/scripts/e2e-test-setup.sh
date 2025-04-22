d9501fd2f0cfd98ffb9dcda19b7a40aff8a67d23
Daniel Nash <dannash200@gmail.com>
2025-04-22 15:10:42 +1200

    revert

--- .github/scripts/e2e-test-setup.sh
+++ .github/scripts/e2e-test-setup.sh
@@ -82,10 +82,9 @@ e2e_test_setup_start_servers() {
     nohup npm run --workspace @tamanu/central-server start > central-server.out &
     nohup npm run --workspace @tamanu/facility-server start > facility-server.out &
     # Give servers time to start before syncing
-    sleep 60
+    sleep 5
     # Sync the servers
     npm run --workspace @tamanu/facility-server start sync > facility-server-sync.out &
-    sleep 60
 }
 
 e2e_test_setup_print_users() {
