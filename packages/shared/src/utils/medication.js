a848c570d34966602f3b7fc477156e67fe4b1f31
Huy-Tran-Huu-Le <huy@bes.au>
2025-04-27 18:48:06 +0700

    feat: EPI-1146: Update discharge summary printout

--- packages/shared/src/utils/medication.js
+++ packages/shared/src/utils/medication.js
@@ -1,5 +1,6 @@
-import { MEDICATION_ADMINISTRATION_TIME_SLOTS } from '@tamanu/constants';
 import { addDays, format, set } from 'date-fns';
+import { DRUG_UNIT_SHORT_LABELS, MEDICATION_ADMINISTRATION_TIME_SLOTS } from '@tamanu/constants';
+import { camelCase } from 'lodash';
 
 export const findAdministrationTimeSlotFromIdealTime = (idealTime) => {
   const index = MEDICATION_ADMINISTRATION_TIME_SLOTS.findIndex((slot) => {
@@ -33,13 +34,20 @@ export const getDateFromTimeString = (time, initialDate = new Date()) => {
 export const getFirstAdministrationDate = (startDate, idealTimes) => {
   const firstStartTime = idealTimes
     .map((idealTime) => getDateFromTimeString(idealTime, startDate))
-    .concat(
-      idealTimes.map((idealTime) =>
-        getDateFromTimeString(idealTime, addDays(startDate, 1)),
-      ),
-    )
+    .concat(idealTimes.map((idealTime) => getDateFromTimeString(idealTime, addDays(startDate, 1))))
     .filter((idealTime) => idealTime.getTime() > startDate.getTime())
     .sort((a, b) => a - b)[0];
 
   return firstStartTime;
+};
+
+export const getDose = (medication, getTranslation, getEnumTranslation) => {
+  let { doseAmount, units, isVariableDose } = medication;
+  if (!units) return '';
+  if (isVariableDose) doseAmount = getTranslation('medication.table.variable', 'Variable');
+  return `${doseAmount} ${getEnumTranslation(DRUG_UNIT_SHORT_LABELS, units)}`;
+};
+
+export const getTranslatedFrequency = (frequency, getTranslation) => {
+  return getTranslation(`medication.frequency.${camelCase(frequency)}.label`, frequency);
 };
