6e8610ec6ed4ab55eee69da7ff69122ce5f95d88
Daniel Nash <dannash200@gmail.com>
2025-04-28 14:23:02 +1200

    yes

--- packages/database/src/utils/audit/extractChangelogFromSnapshotRecords.ts
+++ packages/database/src/utils/audit/extractChangelogFromSnapshotRecords.ts
@@ -1,4 +1,7 @@
-import type { ChangelogRecord, SyncSnapshotAttributesWithChangelog } from './attachChangelogRecordsToSnapshot';
+import type {
+  ChangelogRecord,
+  SyncSnapshotAttributesWithChangelog,
+} from './attachChangelogRecordsToSnapshot';
 
 export const extractChangelogFromSnapshotRecords = (
   snapshotRecordsWithChangelog: SyncSnapshotAttributesWithChangelog[],
@@ -10,16 +13,15 @@ export const extractChangelogFromSnapshotRecords = (
   const processedRecords = snapshotRecordsWithChangelog.reduce((acc, row) => {
     if (row.changelogRecords) {
       changelogRecords.push(...row.changelogRecords);
-      const { changelogRecords: _, ...rest } = row;
-      acc.push(rest);
-    } else {
-      acc.push(row);
+      delete row.changelogRecords;
     }
+    acc.push(row);
+
     return acc;
   }, [] as SyncSnapshotAttributesWithChangelog[]);
 
   return {
     snapshotRecords: processedRecords,
-    changelogRecords
+    changelogRecords,
   };
 };
