f9785dfbe6b76f57236382391d7cd7047ed8d2a8
Daniel Nash <dannash200@gmail.com>
2025-04-28 12:41:15 +1200

    shared try this

--- packages/database/src/utils/audit/extractChangelogFromSnapshotRecords.ts
+++ packages/database/src/utils/audit/extractChangelogFromSnapshotRecords.ts
@@ -1,17 +1,25 @@
 import type { ChangelogRecord, SyncSnapshotAttributesWithChangelog } from './attachChangelogRecordsToSnapshot';
 
 export const extractChangelogFromSnapshotRecords = (
-  snapshotRecords: SyncSnapshotAttributesWithChangelog[],
+  snapshotRecordsWithChangelog: SyncSnapshotAttributesWithChangelog[],
 ): {
   snapshotRecords: SyncSnapshotAttributesWithChangelog[];
   changelogRecords: ChangelogRecord[];
 } => {
-  const changelogRecords = [];
-  for (const row of snapshotRecords) {
+  const changelogRecords: ChangelogRecord[] = [];
+  const processedRecords = snapshotRecordsWithChangelog.reduce((acc, row) => {
     if (row.changelogRecords) {
       changelogRecords.push(...row.changelogRecords);
+      const { changelogRecords: _, ...rest } = row;
+      acc.push(rest);
+    } else {
+      acc.push(row);
     }
-    delete row.changelogRecords;
-  }
-  return { snapshotRecords, changelogRecords };
+    return acc;
+  }, [] as SyncSnapshotAttributesWithChangelog[]);
+
+  return {
+    snapshotRecords: processedRecords,
+    changelogRecords
+  };
 };
