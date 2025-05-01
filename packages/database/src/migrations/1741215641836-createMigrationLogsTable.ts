6e3968587864e20df82eb776bbf4755e95a2389c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-05-02 10:15:20 +1200

    tweak: audit column harmonize (#7604)

--- packages/database/src/migrations/1741215641836-createMigrationLogsTable.ts
+++ packages/database/src/migrations/1741215641836-createMigrationLogsTable.ts
@@ -1,3 +1,4 @@
+import { FACT_CURRENT_SYNC_TICK, FACT_CURRENT_VERSION, FACT_DEVICE_ID } from '@tamanu/constants';
 import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
 
 const TABLE = { schema: 'logs', tableName: 'migrations' };
@@ -15,19 +16,36 @@ export async function up(query: QueryInterface): Promise<void> {
       allowNull: false,
       defaultValue: Sequelize.fn('adjusted_timestamp'),
     },
-    current_sync_tick: {
+    record_sync_tick: {
       type: DataTypes.BIGINT,
       allowNull: false,
+      defaultValue: Sequelize.cast( Sequelize.fn('local_system_fact', FACT_CURRENT_SYNC_TICK, '0'), 'bigint'),
+    },
+    device_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: Sequelize.fn('local_system_fact', FACT_DEVICE_ID, 'unknown'),
+    },
+    version: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: Sequelize.fn('local_system_fact', FACT_CURRENT_VERSION, 'unknown'),
     },
     direction: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
     migrations: {
-      type: DataTypes.TEXT,
+      type: DataTypes.JSONB,
       allowNull: false,
     },
   });
+
+  await query.addIndex(TABLE, ['logged_at'], { using: 'btree' });
+  await query.addIndex(TABLE, ['record_sync_tick'], { using: 'btree' });
+  await query.addIndex(TABLE, ['version'], { using: 'btree' });
+
+  await query.addIndex(TABLE, ['device_id'], { using: 'hash' });
 }
 
 export async function down(query: QueryInterface): Promise<void> {
