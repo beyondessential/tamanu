52a3f5103f2a67bd2ab69f6eadf8bd696743e6cd
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-05-13 11:53:30 +1200

    Update packages/central-server/app/sync/CentralSyncManager.js
    Co-authored-by: Daniel Nash <38335330+dannash100@users.noreply.github.com>

--- packages/central-server/app/sync/CentralSyncManager.js
+++ packages/central-server/app/sync/CentralSyncManager.js
@@ -581,9 +581,9 @@ export class CentralSyncManager {
     try {
       // commit the changes to the db
       const persistedAtSyncTick = await sequelize.transaction(async () => {
-        // Don't produce change logs for changes made through sync
-        // Auditable change logs are generated on facility servers and will sync in to central
-        // Mobile change logs are currently not implemented
+        // Don't produce audit logs for changes made through sync
+        // Audit changelogs are generated on facility servers and will sync in to central
+        // Mobile changelogs are currently not implemented
         await sequelize.setTransactionVar(AUDIT_PAUSE_KEY, true);
 
         // we tick-tock the global clock to make sure there is a unique tick for these changes
