aec3cb41ad3111b17e8612f89cd36250803fd10d
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-05-13 16:14:21 +1200

    Update suggester.ts

--- packages/mobile/App/ui/helpers/suggester.ts
+++ packages/mobile/App/ui/helpers/suggester.ts
@@ -1,7 +1,5 @@
-import { keyBy } from 'lodash';
 import { Brackets, FindManyOptions, ObjectLiteral } from 'typeorm';
 import { BaseModel } from '~/models/BaseModel';
-import { TranslatedString } from '~/models/TranslatedString';
 
 export interface OptionType {
   label: string;
