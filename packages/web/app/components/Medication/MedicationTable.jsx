0e46c54a55be450f0234102183757e9add64a113
khoi.nguyen <khoi@bes.au>
2025-05-18 07:02:26 +0700

    fix(medication): adjust medication details duration unit displaying

--- packages/web/app/components/Medication/MedicationTable.jsx
+++ packages/web/app/components/Medication/MedicationTable.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { format } from 'date-fns';
 import { Box } from '@material-ui/core';
-import { DRUG_ROUTE_LABELS, MEDICATION_DURATION_UNITS_LABELS } from '@tamanu/constants';
+import { DRUG_ROUTE_LABELS, MEDICATION_DURATION_DISPLAY_UNITS_LABELS } from '@tamanu/constants';
 import { useLocation } from 'react-router-dom';
 import { getDose, getTranslatedFrequency } from '@tamanu/shared/utils/medication';
 
@@ -100,7 +100,7 @@ const getMedicationName = (
           (<TranslatedText stringId="medication.table.pausing" fallback="Paused" />,{' '}
           {pauseData.pauseDuration}{' '}
           {singularize(
-            getEnumTranslation(MEDICATION_DURATION_UNITS_LABELS, pauseData.pauseTimeUnit),
+            getEnumTranslation(MEDICATION_DURATION_DISPLAY_UNITS_LABELS, pauseData.pauseTimeUnit),
             pauseData.pauseDuration,
           ).toLowerCase()}{' '}
           - <TranslatedText stringId="medication.table.until" fallback="until" />{' '}
@@ -131,7 +131,7 @@ const MEDICATION_COLUMNS = (getTranslation, getEnumTranslation, disableTooltip) 
     title: <TranslatedText stringId="medication.table.column.medication" fallback="Medication" />,
     accessor: data => getMedicationName(data, getEnumTranslation),
     CellComponent: props => (
-      <LimitedLinesCell 
+      <LimitedLinesCell
         {...props}
         disableTooltip={disableTooltip}
       />
