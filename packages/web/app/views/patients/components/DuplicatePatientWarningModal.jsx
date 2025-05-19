15710f80c844870768661d3a107d6a317f90f6e2
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-05-19 17:01:37 +1200

    Update DuplicatePatientWarningModal.jsx

--- packages/web/app/views/patients/components/DuplicatePatientWarningModal.jsx
+++ packages/web/app/views/patients/components/DuplicatePatientWarningModal.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { ConfirmModal } from '../../../components/ConfirmModal';
-import { Heading3, Heading4, LargeBodyText, Table } from '../../../components';
+import {
+  ButtonRow,
+  Heading3,
+  Heading4,
+  LargeBodyText,
+  Modal,
+  OutlinedButton,
+  Table,
+} from '../../../components';
 import { usePatientNavigation } from '../../../utils/usePatientNavigation';
 import { culturalName, dateOfBirth, firstName, lastName, sex, village } from '../columns';
+import { ConfirmRowDivider } from '../../../components/ConfirmRowDivider';
 
 const COLUMNS = [firstName, lastName, culturalName, dateOfBirth, sex, village];
 
@@ -22,41 +31,46 @@ export const DuplicatePatientWarningModal = ({
 
   const hasMultipleDuplicates = potentialDuplicates.length > 1;
 
-  // TODO: use other modal probably. need 3 buttons back, cancel and add new patient
   return (
-    <ConfirmModal
+    <Modal
       width="md"
       title={'Add new patient'}
       open={open}
-      onConfirm={() => {
-        handleClose(true);
-      }}
-      cancelButtonText={'Cancel'}
-      confirmButtonText={'Add new patient'}
-      onCancel={() => {
+      onClose={() => {
         handleClose(false);
       }}
-      customContent={
-        <>
-          <Heading3>Possible duplicate patient record{hasMultipleDuplicates ? 's' : ''}</Heading3>
-          <LargeBodyText color="textTertiary">
-            The below patient record{hasMultipleDuplicates ? 's' : ''} already exist
-            {hasMultipleDuplicates ? '' : 's'}. Please review the patient details and ensure you are
-            not adding a duplicate record. If the patient you are creating is listed below, please
-            select the required record to continue. Otherwise, click &apos;Add new patient&apos; to
-            continue adding a new patient record.
-          </LargeBodyText>
-          <Heading4>Existing patient record{hasMultipleDuplicates ? 's' : ''} in Tamanu</Heading4>
-          <Table
-            columns={COLUMNS}
-            data={potentialDuplicates}
-            onRowClick={row => navigateToPatient(row.id)}
-          />
-          <Heading4>Proposed new patient record</Heading4>
-          <Table columns={COLUMNS} data={[proposedPatient]} />
-        </>
-      }
-      data-testid="confirmmodal-x4fg"
-    />
+      data-testid="modal-dgog"
+    >
+      <Heading3>Possible duplicate patient record{hasMultipleDuplicates ? 's' : ''}</Heading3>
+      <LargeBodyText color="textTertiary">
+        The below patient record{hasMultipleDuplicates ? 's' : ''} already exist
+        {hasMultipleDuplicates ? '' : 's'}. Please review the patient details and ensure you are not
+        adding a duplicate record. If the patient you are creating is listed below, please select
+        the required record to continue. Otherwise, click &apos;Add new patient&apos; to continue
+        adding a new patient record.
+      </LargeBodyText>
+      <Heading4>Existing patient record{hasMultipleDuplicates ? 's' : ''} in Tamanu</Heading4>
+      <Table
+        columns={COLUMNS}
+        data={potentialDuplicates}
+        onRowClick={row => navigateToPatient(row.id)}
+      />
+      <Heading4>Proposed new patient record</Heading4>
+      <Table columns={COLUMNS} data={[proposedPatient]} />
+      <ConfirmRowDivider data-testid="confirmrowdivider-f8hm" />
+      <ButtonRow data-testid="buttonrow-5x0v">
+        <OutlinedButton data-testid="outlinedbutton-p957">Back</OutlinedButton>
+        <OutlinedButton data-testid="outlinedbutton-p957" onClick={() => handleClose(false)}>
+          Cancel
+        </OutlinedButton>
+        <OutlinedButton
+          variant="contained"
+          data-testid="confirmbutton-y3tb"
+          onClick={() => handleClose(true)}
+        >
+          Confirm
+        </OutlinedButton>
+      </ButtonRow>
+    </Modal>
   );
 };
