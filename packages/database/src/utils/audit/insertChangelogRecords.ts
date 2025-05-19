c4de59d65d8e44a733934c1d04849cb1ea6db4c3
Daniel Nash <dannash200@gmail.com>
2025-05-20 11:44:12 +1200

    remove irrelevant tests

--- packages/database/src/utils/audit/insertChangelogRecords.ts
+++ packages/database/src/utils/audit/insertChangelogRecords.ts
@@ -1,14 +1,7 @@
-import config from 'config';
-import { selectFacilityIds } from '@tamanu/utils/selectFacilityIds';
 import type { ChangeLog } from 'models/ChangeLog';
 import type { Models } from 'types/model';
-import { SYNC_TICK_FLAGS } from '../../sync/constants';
 
-export const insertChangelogRecords = async (
-  models: Models,
-  changelogRecords: ChangeLog[],
-  isFacility = !!selectFacilityIds(config),
-) => {
+export const insertChangelogRecords = async (models: Models, changelogRecords: ChangeLog[]) => {
   const { ChangeLog } = models;
 
   if (!changelogRecords.length) {
@@ -24,12 +17,7 @@ export const insertChangelogRecords = async (
   const existingIds = existingRecords.map(({ id }) => id);
   const recordsToInsert = changelogRecords
     .filter(({ id }) => !existingIds.includes(id))
-    .map(({ recordSyncTick, ...changelogRecord }) => {
-      return {
-        ...changelogRecord,
-        recordSyncTick: isFacility ? SYNC_TICK_FLAGS.LAST_UPDATED_ELSEWHERE : Number(recordSyncTick),
-      };
-    });
+    .map((changelogRecord) => changelogRecord.get({ plain: true }));
 
   await ChangeLog.bulkCreate(recordsToInsert);
 };
