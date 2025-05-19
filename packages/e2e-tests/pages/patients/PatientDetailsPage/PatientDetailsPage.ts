42b13ccc86ec6a893f575511e92473c72c11b588
Andrew <vanbeekandrew@gmail.com>
2025-05-20 10:11:59 +1200

    fix date to use correct timezone

--- packages/e2e-tests/pages/patients/PatientDetailsPage/PatientDetailsPage.ts
+++ packages/e2e-tests/pages/patients/PatientDetailsPage/PatientDetailsPage.ts
@@ -323,7 +323,11 @@ export class PatientDetailsPage extends BasePatientPage {
 
   async getCurrentBrowserDateISOFormat() {
     const browserDate = await this.page.evaluate(() => {
-      return new Date().toISOString().split('T')[0];
+      const today = new Date();
+      const yyyy = today.getFullYear();
+      const mm = String(today.getMonth() + 1).padStart(2, '0');
+      const dd = String(today.getDate()).padStart(2, '0');
+      return `${yyyy}-${mm}-${dd}`;
     });
     return browserDate;
   }
