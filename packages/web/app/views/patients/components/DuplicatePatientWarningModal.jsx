756a8136d2e7fb34eb9db36b03b52945daf3f2e5
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-05-20 16:42:38 +1200

    translate

--- packages/web/app/views/patients/components/DuplicatePatientWarningModal.jsx
+++ packages/web/app/views/patients/components/DuplicatePatientWarningModal.jsx
@@ -7,6 +7,7 @@ import {
   Modal,
   OutlinedButton,
   Table,
+  TranslatedText,
 } from '../../../components';
 import { usePatientNavigation } from '../../../utils/usePatientNavigation';
 import { culturalName, dateOfBirth, firstName, lastName, sex, village } from '../columns';
@@ -37,34 +38,63 @@ export const DuplicatePatientWarningModal = ({
   return (
     <Modal
       width="md"
-      title={'Add new patient'}
+      title={
+        <TranslatedText
+          stringId="patient.modal.duplicateWarning.title"
+          fallback="Add new patient"
+          data-testid="translatedtext-title"
+        />
+      }
       open={open}
       onClose={() => handleClose(false)}
       data-testid="modal-dgog"
     >
-      <Heading3>Possible duplicate patient record{plural}</Heading3>
+      <Heading3>
+        <TranslatedText
+          stringId="patient.modal.duplicateWarning.heading"
+          fallback={`Possible duplicate patient record${plural}`}
+          data-testid="translatedtext-heading"
+        />
+      </Heading3>
       <LargeBodyText color="textTertiary">
-        The below patient record{plural} already exists. Please review the patient details and
-        ensure you are not adding a duplicate record. If the patient you are creating is listed
-        below, please select the required record to continue. Otherwise, click &apos;Add new
-        patient&apos; to continue adding a new patient record.
+        <TranslatedText
+          stringId="patient.modal.duplicateWarning.description"
+          fallback={`The below patient record${plural} already exist. Please review the patient details and ensure you are not adding a duplicate record. If the patient you are creating is listed below, please select the required record to continue. Otherwise, click 'Add new patient' to continue adding a new patient record.`}
+          data-testid="translatedtext-description"
+        />
       </LargeBodyText>
-      <Heading4>Existing patient record{plural} in Tamanu</Heading4>
+      <Heading4>
+        <TranslatedText
+          stringId="patient.modal.duplicateWarning.existingRecordsHeading"
+          fallback={`Existing patient record${plural} in Tamanu`}
+          replacements={{ plural }}
+          data-testid="translatedtext-existing-heading"
+        />
+      </Heading4>
       <Table
         columns={COLUMNS}
         data={potentialDuplicates}
         onRowClick={row => {
-          // TODO: why do i have to do this? take another look
           dispatch(reloadPatient(row.id));
           navigateToPatient(row.id);
         }}
       />
-      <Heading4>Proposed new patient record</Heading4>
+      <Heading4>
+        <TranslatedText
+          stringId="patient.modal.duplicateWarning.proposedRecordHeading"
+          fallback="Proposed new patient record"
+          data-testid="translatedtext-proposed-heading"
+        />
+      </Heading4>
       <Table columns={COLUMNS} data={[proposedPatient]} />
       <ConfirmRowDivider data-testid="confirmrowdivider-f8hm" />
       <ButtonRow data-testid="buttonrow-5x0v">
         <OutlinedButton data-testid="outlinedbutton-p957" onClick={() => handleClose(false)}>
-          Back
+          <TranslatedText
+            stringId="general.action.back"
+            fallback="Back"
+            data-testid="translatedtext-back"
+          />
         </OutlinedButton>
         <OutlinedButton
           data-testid="outlinedbutton-p957"
@@ -73,14 +103,22 @@ export const DuplicatePatientWarningModal = ({
             onCancelNewPatient();
           }}
         >
-          Cancel
+          <TranslatedText
+            stringId="general.action.cancel"
+            fallback="Cancel"
+            data-testid="translatedtext-cancel"
+          />
         </OutlinedButton>
         <OutlinedButton
           variant="contained"
           data-testid="confirmbutton-y3tb"
           onClick={() => handleClose(true)}
         >
-          Confirm
+          <TranslatedText
+            stringId="general.action.confirm"
+            fallback="Confirm"
+            data-testid="translatedtext-confirm"
+          />
         </OutlinedButton>
       </ButtonRow>
     </Modal>
