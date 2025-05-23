1692c0a8539590d5ffd14245a0636cba2058dc57
Félix Saparelli <felix@passcod.name>
2025-05-23 12:16:12 +1200

    merge: hotfixes to 2.16 (#7702)

--- packages/central-server/app/sync/CentralSyncManager.js
+++ packages/central-server/app/sync/CentralSyncManager.js
@@ -348,9 +348,10 @@ export class CentralSyncManager {
         { where: { id: sessionId } },
       );
 
-      await models.SyncSession.addDebugInfo(sessionId, {
+      await models.SyncSession.setParameters(sessionId, {
         isMobile,
         tablesForFullResync,
+        useSyncLookup: this.constructor.config.sync.lookupTable.enabled,
       });
 
       const modelsToInclude = tablesToInclude
