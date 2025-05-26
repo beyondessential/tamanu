2867d05749fa9e102c909f6001a2f6a57b24e82f
Daniel Nash <dannash200@gmail.com>
2025-05-26 13:47:59 +1200

    basic version b like

--- packages/database/src/migrations/1748216223615-separateSyncSessionParametersFromDebugInfo.ts
+++ packages/database/src/migrations/1748216223615-separateSyncSessionParametersFromDebugInfo.ts
@@ -5,33 +5,8 @@ export async function up(query: QueryInterface): Promise<void> {
     type: DataTypes.JSONB,
     allowNull: true,
   });
-
-  // Move parameters from debugInfo to new parameters column
-  await query.sequelize.query(`
-    UPDATE sync_sessions
-    SET parameters = jsonb_build_object(
-          'minSourceTick', debug_info->'minSourceTick',
-          'maxSourceTick', debug_info->'maxSourceTick',
-          'isMobile', COALESCE(debug_info->'isMobile', false)
-        )
-    WHERE debug_info IS NOT NULL
-    AND (debug_info::jsonb ? 'minSourceTick' OR debug_info::jsonb ? 'maxSourceTick' OR debug_info::jsonb ? 'isMobile');
-  `);
-
-  // Remove migrated fields from debugInfo
-  await query.sequelize.query(`
-    UPDATE sync_sessions
-    SET debug_info = debug_info::jsonb - 'minSourceTick' - 'maxSourceTick' - 'isMobile'
-    WHERE parameters IS NOT NULL;
-  `);
 }
 
 export async function down(query: QueryInterface): Promise<void> {
-  await query.sequelize.query(`
-    UPDATE sync_sessions
-    SET debug_info = COALESCE(debug_info::jsonb, '{}'::jsonb) || parameters
-    WHERE parameters IS NOT NULL;
-  `);
-
   await query.removeColumn('sync_sessions', 'parameters');
 }
