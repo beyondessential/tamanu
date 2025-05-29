f26b1ebaf695bd4e6bf27e4d8e87f02353d946a4
sepideh_dd <sepideh@bes.au>
2025-05-30 09:44:05 +1000

    some of the functions moved to patientTable POM in main so adjusting the test file based on that

--- packages/e2e-tests/tests/patients/recentlyViewedPatients.spec.ts
+++ packages/e2e-tests/tests/patients/recentlyViewedPatients.spec.ts
@@ -14,8 +14,8 @@ test.describe('Recently viewed patients top bar', () => {
   });
 
   test('Most recently viewed patient appears at the top of the list and confirms the correct patient data in the list', async ({ allPatientsPage, page }) => {
-    await allPatientsPage.searchTable({ NHN: patientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: patientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     await allPatientsPage.goto();
     const recentlyViewed = new RecentlyViewedPatientPage(page);
     await expect(recentlyViewed.firstRecentlyViewedName).toHaveText(patientData.firstName + ' ' + patientData.lastName);
@@ -29,15 +29,15 @@ test.describe('Recently viewed patients top bar', () => {
 
   test('Most recently viewed patient appears at the top of the list', async ({ allPatientsPage, page }) => {
     // Create and view first patient
-    await allPatientsPage.searchTable({ NHN: patientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: patientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     await allPatientsPage.goto();
     
     // Create and view second patient
     await createPatientViaApi(allPatientsPage);
     const secondPatientData = await allPatientsPage.getPatientData();
-    await allPatientsPage.searchTable({ NHN: secondPatientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: secondPatientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     
     // Verify second patient is at the top
     await allPatientsPage.goto();
@@ -47,8 +47,8 @@ test.describe('Recently viewed patients top bar', () => {
   });
 
   test('Clicking on a recently viewed patient navigates to their details', async ({ allPatientsPage, page }) => {
-    await allPatientsPage.searchTable({ NHN: patientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: patientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     await allPatientsPage.goto();
     
     const recentlyViewed = new RecentlyViewedPatientPage(page);
@@ -63,13 +63,13 @@ test.describe('Recently viewed patients top bar', () => {
 
   test('Recently viewed list updates when viewing same patient multiple times', async ({ allPatientsPage, page }) => {
     // View patient first time
-    await allPatientsPage.searchTable({ NHN: patientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: patientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     await allPatientsPage.goto();
     
     // View patient second time
-    await allPatientsPage.searchTable({ NHN: patientData.nhn, advancedSearch: false });
-    await allPatientsPage.clickOnFirstRow();
+    await allPatientsPage.patientTable.searchTable({ NHN: patientData.nhn, advancedSearch: false });
+    await allPatientsPage.patientTable.clickOnFirstRow();
     await allPatientsPage.goto();
     
     // Verify patient is still at the top
