4b4b102fb9d2163753941cdbd3242bda6ca8a3e2
dannash100 <dannash200@gmail.com>
2025-06-03 12:03:36 +1200

    put back

--- packages/web/app/components/Tooltip.jsx
+++ packages/web/app/components/Tooltip.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Tooltip } from '@material-ui/core';
 import { Colors } from '../constants';
 
-export const ThemedTooltip = styled((props) => (
+export const ThemedTooltip = styled(props => (
   <Tooltip
     classes={{ popper: props.className }}
     placement="top"
@@ -18,18 +18,22 @@ export const ThemedTooltip = styled((props) => (
     font-size: 0.6875rem;
     padding: 0.375rem 0.5rem;
     text-align: center;
-    max-inline-size: 8rem;
+    ${({ $maxWidth }) =>
+      $maxWidth &&
+      css`
+        max-inline-size: ${$maxWidth};
+      `}
   }
   .MuiTooltip-arrow {
     color: ${Colors.primaryDark};
   }
 `;
 
-export const ConditionalTooltip = ({ visible, children, ...restProps }) => {
+export const ConditionalTooltip = ({ visible, children, maxWidth, ...restProps }) => {
   if (!visible) return children;
   return (
     //  Below div is needed to make ThemedTooltip work
-    <ThemedTooltip {...restProps} data-testid="themedtooltip-50xj">
+    <ThemedTooltip $maxWidth={maxWidth} {...restProps} data-testid="themedtooltip-50xj">
       <div>{children}</div>
     </ThemedTooltip>
   );
