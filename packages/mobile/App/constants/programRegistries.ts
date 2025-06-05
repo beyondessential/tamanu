7b01edf75ca83dec509fa004084d92a7894a0bc4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-06-05 18:05:38 -0400

    Chore, move comment to file top as it applies to all constants

--- packages/mobile/App/constants/programRegistries.ts
+++ packages/mobile/App/constants/programRegistries.ts
@@ -1,3 +1,9 @@
+/**
+ * ########################################################################################################
+ *       All constants in this folder must be kept in sync with the ones in @tamanu/constants/programRegistry
+ * ########################################################################################################
+ */
+
 export enum CurrentlyAtType {
   Village = 'village',
   Facility = 'facility',
@@ -9,18 +15,12 @@ export enum RegistrationStatus {
   RecordedInError = 'recordedInError',
 }
 
-// Please keep in sync with packages/web/app/constants/index.js
 export const PROGRAM_REGISTRATION_STATUS_LABELS = {
   [RegistrationStatus.Active]: 'Active',
   [RegistrationStatus.Inactive]: 'Removed',
   [RegistrationStatus.RecordedInError]: 'Delete',
 };
 
-/**
- * ########################################################################################################
- *       These categories must be kept in sync with the ones in @tamanu/constants/programRegistry
- * ########################################################################################################
- */
 export const PROGRAM_REGISTRY_CONDITION_CATEGORIES = {
   UNKNOWN: 'unknown',
   RECORDED_IN_ERROR: 'recordedInError',
