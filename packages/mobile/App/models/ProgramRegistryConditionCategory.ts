fdba687b2e73799e99261fccd0103dd4f129fc60
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-06-09 15:21:50 -0400

    Use proper interface name

--- packages/mobile/App/models/ProgramRegistryConditionCategory.ts
+++ packages/mobile/App/models/ProgramRegistryConditionCategory.ts
@@ -1,6 +1,6 @@
 import { Entity, ManyToOne, RelationId, Column } from 'typeorm';
 
-import { ID, IProgramRegistry, IProgramRegistryCategory } from '~/types';
+import { ID, IProgramRegistry, IProgramRegistryConditionCategory } from '~/types';
 import { BaseModel } from './BaseModel';
 import { SYNC_DIRECTIONS } from './types';
 import { VisibilityStatus } from '~/visibilityStatuses';
@@ -9,7 +9,7 @@ import { ProgramRegistry } from './ProgramRegistry';
 @Entity('program_registry_condition_categories')
 export class ProgramRegistryConditionCategory
   extends BaseModel
-  implements IProgramRegistryCategory
+  implements IProgramRegistryConditionCategory
 {
   static syncDirection = SYNC_DIRECTIONS.PULL_FROM_CENTRAL;
 
