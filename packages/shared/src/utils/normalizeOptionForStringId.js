663df443258c6f442aafc3f99876a0bc096b7c3a
dannash100 <dannash200@gmail.com>
2025-06-09 15:11:06 +1200

    probably lead to less slipups

--- packages/shared/src/utils/normalizeOptionForStringId.js
+++ packages/shared/src/utils/normalizeOptionForStringId.js
@@ -1,5 +1,5 @@
 import { camelCase } from 'lodash';
 
 export const normalizeOptionForStringId = option => {
-  return camelCase(option.replace(/[^a-zA-Z0-9 ]/g, ''));
+  return camelCase(option.replace(/\s+/g, ''));
 };
