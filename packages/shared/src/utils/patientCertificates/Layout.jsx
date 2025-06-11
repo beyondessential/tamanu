cc748d9e1b8c05620f5faee68c11e6f4d83cbb40
dannash100 <dannash200@gmail.com>
2025-06-12 11:04:48 +1200

    Hel

--- packages/shared/src/utils/patientCertificates/Layout.jsx
+++ packages/shared/src/utils/patientCertificates/Layout.jsx
@@ -32,7 +32,7 @@ export const styles = StyleSheet.create({
     marginBottom: 20,
   },
   signatureText: {
-    fontFamily: 'Helvetica-Bold',
+    fontFamily: 'Rubik-Bold',
     fontWeight: 400,
     width: 100,
   },
