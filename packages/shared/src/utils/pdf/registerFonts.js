d0667f4072d58b8d17ec50404b35093600b1466e
dannash100 <dannash200@gmail.com>
2025-06-12 13:23:46 +1200

    we good

--- packages/shared/src/utils/pdf/registerFonts.js
+++ packages/shared/src/utils/pdf/registerFonts.js
@@ -5,27 +5,28 @@ const baseDir =
   typeof __dirname !== 'undefined' ? path.join(__dirname, '../../assets/fonts') : '/fonts';
 
 export const registerFonts = () => {
-Font.register({
-  family: 'Rubik',
-  fonts: [
-    {
-      src: path.join(baseDir, 'Rubik-Regular.ttf'),
-      fontWeight: 400,
-    },
-    {
-      src: path.join(baseDir, 'Rubik-LightItalic.ttf'),
-      fontStyle: 'italic',
-      fontWeight: 400,
-    },
-    {
-      src: path.join(baseDir, 'Rubik-Bold.ttf'),
-      fontWeight: 700,
-    },
-    {
-      src: path.join(baseDir, 'Rubik-BoldItalic.ttf'),
-      fontStyle: 'italic',
-      fontWeight: 700,
-    },
-  ],
+  if (Font.getRegisteredFontFamilies().includes('Rubik')) return;
+  Font.register({
+    family: 'Rubik',
+    fonts: [
+      {
+        src: path.join(baseDir, 'Rubik-Regular.ttf'),
+        fontWeight: 400,
+      },
+      {
+        src: path.join(baseDir, 'Rubik-Italic.ttf'),
+        fontStyle: 'italic',
+        fontWeight: 400,
+      },
+      {
+        src: path.join(baseDir, 'Rubik-Bold.ttf'),
+        fontWeight: 700,
+      },
+      {
+        src: path.join(baseDir, 'Rubik-BoldItalic.ttf'),
+        fontStyle: 'italic',
+        fontWeight: 700,
+      },
+    ],
   });
 };
