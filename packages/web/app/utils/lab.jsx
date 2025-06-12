97f687ba2feff297545785201d1e9632c1bdb0f3
dannash100 <dannash200@gmail.com>
2025-06-12 17:14:36 +1200

    another 1

--- packages/web/app/utils/lab.jsx
+++ packages/web/app/utils/lab.jsx
@@ -4,6 +4,7 @@ import { DateDisplay } from '../components';
 import { PatientNameDisplay } from '../components/PatientNameDisplay';
 import { TableCellTag } from '../components/Tag';
 import { TranslatedReferenceData, TranslatedText } from '../components/Translation';
+import { rest } from 'lodash';
 
 export const StatusDisplay = React.memo(({ status }) => {
   const { background, color, label } = LAB_REQUEST_STATUS_CONFIG[status];
@@ -43,7 +44,7 @@ export const getMethod = ({ labTestMethod }) =>
 
 export const getRequestedBy = ({ requestedBy }) =>
   (requestedBy || {})?.displayName || requestedBy || 'Unknown';
-export const getPatientName = (row) => <PatientNameDisplay patient={row} />;
+export const getPatientName = row => <PatientNameDisplay patient={row} />;
 export const getPatientDisplayId = ({ patientDisplayId }) => patientDisplayId || 'Unknown';
 export const getStatus = ({ status }) => <StatusDisplay status={status} />;
 export const getPanelType = ({ labTestPanelId, labTestPanelName }) => (
@@ -74,16 +75,17 @@ export const getRequestType = ({ categoryName, categoryId, category }) => {
   }
   return <TranslatedText stringId="general.fallback.unknown" fallback="Unknown" />;
 };
-export const getPriority = ({ priorityName, priority }) =>
-  priorityName ||
-  (priority && (
+export const getPriority = ({ priorityName, priorityId, priority }) =>
+  priorityName || priority ? (
     <TranslatedReferenceData
-      fallback={priority.name}
-      value={priority.id}
-      category={priority.type}
+      fallback={priorityName || priority.name}
+      value={priorityId || priority.id}
+      category="labTestPriority"
     />
-  )) ||
-  'Unknown';
+  ) : (
+    <TranslatedText stringId="general.fallback.unknown" fallback="Unknown" />
+  );
+
 export const getDateWithTimeTooltip = ({ requestedDate }) => (
   <DateDisplay date={requestedDate} timeOnlyTooltip />
 );
