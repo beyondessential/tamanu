4505a997f815b1ced9105cd33b520796368ddac5
dannash100 <dannash200@gmail.com>
2025-06-18 13:34:23 +1200

    wip

--- packages/shared/src/utils/pdf/registerFonts.js
+++ packages/shared/src/utils/pdf/registerFonts.js
@@ -7,21 +7,21 @@ const baseDir =
 export const registerFonts = () => {
   Font.register({
     family: 'Rubik',
-    src: path.join(baseDir, 'Tajawal-Regular.ttf'),
+    src: path.join(baseDir, 'NotoKufiArabic-Regular.ttf'),
   });
 
   Font.register({
     family: 'Rubik-Italic',
-    src: path.join(baseDir, 'Tajawal-Regular.ttf'),
+    src: path.join(baseDir, 'NotoKufiArabic-Regular.ttf'),
   });
 
   Font.register({
     family: 'Rubik-Bold',
-    src: path.join(baseDir, 'Tajawal-Bold.ttf'),
+    src: path.join(baseDir, 'NotoKufiArabic-Bold.ttf'),
   });
 
   Font.register({
     family: 'Rubik-BoldItalic',
-    src: path.join(baseDir, 'Tajawal-Bold.ttf'),
+    src: path.join(baseDir, 'NotoKufiArabic-Bold.ttf'),
   });
 };
