abbbcdbc629f17c46ae3f3727d7cff18e5ea0840
dannash100 <dannash200@gmail.com>
2025-06-27 05:44:05 +1200

    try up params

--- packages/mobile/App/infra/db/helpers.ts
+++ packages/mobile/App/infra/db/helpers.ts
@@ -4,7 +4,7 @@ export const formatDateForQuery = (date: Date): number => date.valueOf() / 1000;
 
 // SQLite < v3.32 has a hard limit of 999 bound parameters per query
 // see https://www.sqlite.org/limits.html for more
-export const SQLITE_MAX_PARAMETERS = 999;
+export const SQLITE_MAX_PARAMETERS = 32766;
 type RowLike = {
   [key: string]: any;
 };
