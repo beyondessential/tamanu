4b2da6b2b242b4f0376623264049e86e88290782
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-06-27 10:17:41 +1200

    Update TamanuApi.jsx

--- packages/web/app/api/TamanuApi.jsx
+++ packages/web/app/api/TamanuApi.jsx
@@ -205,7 +205,7 @@ export class TamanuApi extends ApiClient {
       if (err instanceof AuthExpiredError) {
         clearLocalStorage();
       } else if (showUnknownErrorToast && isErrorUnknown(err)) {
-        const language = config.headers['language'];
+        const language = config?.headers['language'];
         notifyError([
           <b key="general.api.notification.requestFailed">
             <TranslatedText
