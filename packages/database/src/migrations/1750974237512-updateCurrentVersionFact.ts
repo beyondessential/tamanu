c60ac4f5d18b601e4628b79d9881e0f3907e68db
dannash100 <dannash200@gmail.com>
2025-06-27 09:58:28 +1200

    do update

--- packages/database/src/migrations/1750974237512-updateCurrentVersionFact.ts
+++ packages/database/src/migrations/1750974237512-updateCurrentVersionFact.ts
@@ -3,8 +3,8 @@ import { QueryInterface } from 'sequelize';
 export async function up(query: QueryInterface): Promise<void> {
   await query.sequelize.query(`
     INSERT INTO local_system_facts("created_at", "updated_at", "deleted_at", "key", "value")
-    VALUES (now(), now(), NULL, 'nextVersion', '2.34')
-    ON CONFLICT (key) SET value = '2.34'
+    VALUES (now(), now(), NULL, 'currentVersion', '2.34')
+    ON CONFLICT (key) DO UPDATE SET value = '2.34'
   `);
 }
 
