ed76d924d450857aa672cb3c75ea59d455acb1f6
dannash100 <dannash200@gmail.com>
2025-07-01 09:14:14 +1200

    500 is best after benchmarking

--- packages/mobile/App/infra/db/helpers.ts
+++ packages/mobile/App/infra/db/helpers.ts
@@ -2,11 +2,11 @@ import { chunk } from 'lodash';
 
 export const formatDateForQuery = (date: Date): number => date.valueOf() / 1000;
 
-export const SQLITE_MAX_BULK_INSERT_ROWS = 997;
+export const SQLITE_INSERT_CHUNK_SIZE = 500;
 
 type RowLike = {
   [key: string]: any;
 };
 export function chunkRows<T extends RowLike>(rows: T[]): T[][] {
-  return chunk(rows, SQLITE_MAX_BULK_INSERT_ROWS);
+  return chunk(rows, SQLITE_INSERT_CHUNK_SIZE);
 }
