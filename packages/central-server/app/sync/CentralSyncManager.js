d0e95f6f9faa3ef7a8f1adf7e74568836e2124c1
Félix Saparelli <felix@bes.au>
2025-06-30 17:07:01 +1200

    feat(sync): KOR-3: introduce streaming where we have polling
    This commit includes the central code and the facility implementation.

--- packages/central-server/app/sync/CentralSyncManager.js
+++ packages/central-server/app/sync/CentralSyncManager.js
@@ -37,7 +37,7 @@ import { startSnapshotWhenCapacityAvailable } from './startSnapshotWhenCapacityA
 import { createMarkedForSyncPatientsTable } from './createMarkedForSyncPatientsTable';
 import { updateLookupTable, updateSyncLookupPendingRecords } from './updateLookupTable';
 
-const errorMessageFromSession = (session) =>
+const errorMessageFromSession = session =>
   `Sync session '${session.id}' encountered an error: ${session.errors[session.errors.length - 1]}`;
 
 // about variables lapsedSessionSeconds and lapsedSessionCheckFrequencySeconds:
@@ -150,6 +150,14 @@ export class CentralSyncManager {
       log.error('CentralSyncManager.prepareSession encountered an error', error);
       await this.store.models.SyncSession.markSessionErrored(syncSession.id, error.message);
     }
+  }
+
+  async sessionExists(sessionId) {
+    const session = await this.store.sequelize.models.SyncSession.findOne({
+      where: { id: sessionId },
+    });
+
+    return Boolean(session);
   }
 
   async connectToSession(sessionId) {
@@ -272,7 +280,7 @@ export class CentralSyncManager {
 
       const isInitialBuildOfLookupTable = parseInt(previouslyUpToTick, 10) === -1;
 
-      await repeatableReadTransaction(store.sequelize, async (transaction) => {
+      await repeatableReadTransaction(store.sequelize, async transaction => {
         // do not need to update pending records when it is initial build
         // because it uses ticks from the actual tables for updated_at_sync_tick
         if (isInitialBuildOfLookupTable) {
@@ -340,13 +348,13 @@ export class CentralSyncManager {
   async waitForPendingEdits(tick) {
     // get all the ticks (ie: keys of in-flight transaction advisory locks) of previously pending edits
     const pendingSyncTicks = (await getSyncTicksOfPendingEdits(this.store.sequelize)).filter(
-      (t) => t < tick,
+      t => t < tick,
     );
 
     // wait for any in-flight transactions of pending edits
     // that we don't miss any changes that are in progress
     await Promise.all(
-      pendingSyncTicks.map((t) => waitForPendingEditsUsingSyncTick(this.store.sequelize, t)),
+      pendingSyncTicks.map(t => waitForPendingEditsUsingSyncTick(this.store.sequelize, t)),
     );
   }
 
@@ -683,7 +691,7 @@ export class CentralSyncManager {
   async addIncomingChanges(sessionId, changes) {
     const { sequelize } = this.store;
     await this.connectToSession(sessionId);
-    const incomingSnapshotRecords = changes.map((c) => ({
+    const incomingSnapshotRecords = changes.map(c => ({
       ...c,
       direction: SYNC_SESSION_DIRECTION.INCOMING,
       updatedAtByFieldSum: c.data.updatedAtByField
--- packages/facility-server/app/sync/pullIncomingChanges.js
+++ packages/facility-server/app/sync/pullIncomingChanges.js
@@ -1,7 +1,11 @@
 import config from 'config';
 import { chunk } from 'lodash';
 import { log } from '@tamanu/shared/services/logging';
-import { insertSnapshotRecords, SYNC_SESSION_DIRECTION, SYNC_TICK_FLAGS } from '@tamanu/database/sync';
+import {
+  insertSnapshotRecords,
+  SYNC_SESSION_DIRECTION,
+  SYNC_TICK_FLAGS,
+} from '@tamanu/database/sync';
 import { sleepAsync } from '@tamanu/utils/sleepAsync';
 
 import { calculatePageLimit } from './calculatePageLimit';
@@ -41,7 +45,7 @@ export const pullIncomingChanges = async (centralServer, sequelize, sessionId, s
 
     log.info('FacilitySyncManager.savingChangesToSnapshot', { count: records.length });
 
-    const recordsToSave = records.map((r) => ({
+    const recordsToSave = records.map(r => ({
       ...r,
       data: { ...r.data, updatedAtSyncTick: SYNC_TICK_FLAGS.INCOMING_FROM_CENTRAL_SERVER }, // mark as never updated, so we don't push it back to the central server until the next local update
       direction: SYNC_SESSION_DIRECTION.INCOMING,
@@ -62,4 +66,62 @@ export const pullIncomingChanges = async (centralServer, sequelize, sessionId, s
   }
 
   return { totalPulled: totalToPull, pullUntil };
+};
+
+export const streamIncomingChanges = async (centralServer, sequelize, sessionId, since) => {
+  // initiating pull also returns the sync tick (or point on the sync timeline) that the
+  // central server considers this session will be up to after pulling all changes
+  log.info('FacilitySyncManager.pull.waitingForCentral');
+  const { totalToPull, pullUntil } = await centralServer.initiatePull(sessionId, since);
+  const WRITE_BATCH_SIZE = Math.min(1000, totalToPull);
+
+  const writeBatch = async records => {
+    const recordsToSave = records.map(r => ({
+      ...r,
+      data: { ...r.data, updatedAtSyncTick: SYNC_TICK_FLAGS.INCOMING_FROM_CENTRAL_SERVER }, // mark as never updated, so we don't push it back to the central server until the next local update
+      direction: SYNC_SESSION_DIRECTION.INCOMING,
+    }));
+
+    // This is an attempt to avoid storing all the pulled data
+    // in the memory because we might run into memory issue when:
+    // 1. During the first sync when there is a lot of data to load
+    // 2. When a huge number of data is imported to sync and the facility syncs it down
+    // So store the data in a sync snapshot table instead and will persist it to the actual tables later
+    for (const batchOfRows of chunk(recordsToSave, persistedCacheBatchSize)) {
+      await insertSnapshotRecords(sequelize, sessionId, batchOfRows);
+
+      await sleepAsync(pauseBetweenCacheBatchInMilliseconds);
+    }
+  };
+
+  log.info('FacilitySyncManager.pulling', { since, totalToPull });
+  let fromId;
+  let totalPulled = 0;
+  let records = [];
+  let writes = [];
+
+  const endpoint = () => `sync/${sessionId}/pull${fromId ? `?fromId=${fromId}` : ''}`;
+  stream: for await (const { kind, data } of centralServer.stream(endpoint)) {
+    if (records.length >= WRITE_BATCH_SIZE) {
+      writes.push(writeBatch(records));
+      records = [];
+    }
+
+    handler: switch (kind) {
+      case 'PULL_CHANGE':
+        records.push(data);
+        totalPulled += data.length;
+        fromId = data.id;
+        break handler;
+      case 'END':
+        log.debug(`FacilitySyncManager.pull.noMoreChanges`);
+        break stream;
+      default:
+        log.warn('FacilitySyncManager.pull.unknownMessageKind', { kind });
+    }
+  }
+
+  await Promise.all(writes);
+
+  return { totalPulled, totalToPull, pullUntil };
 };
