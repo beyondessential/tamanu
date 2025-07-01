5665c41c2e793cd4600088ab2b6b539869b9fec3
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-07-01 12:13:44 +1200

    translate vitals edit view

--- packages/web/app/components/EditVitalCellModal.jsx
+++ packages/web/app/components/EditVitalCellModal.jsx
@@ -2,12 +2,19 @@ import React, { useCallback } from 'react';
 import { FormModal } from './FormModal';
 import { formatShortest, formatTime } from '@tamanu/utils/dateTime';
 import { EditVitalCellForm } from '../forms/EditVitalCellForm';
+import { TranslatedReferenceData, TranslatedText } from './Translation';
 
 export const EditVitalCellModal = ({ open, dataPoint, onClose }) => {
-  const vitalLabel = dataPoint?.component.dataElement.name;
+  const vitalLabel = <TranslatedReferenceData 
+    category="programDataElement" 
+    value={dataPoint?.component.dataElement.id} 
+    fallback={dataPoint?.component.dataElement.name} 
+  />
   const date = formatShortest(dataPoint?.recordedDate);
   const time = formatTime(dataPoint?.recordedDate);
-  const title = `${vitalLabel} | ${date} | ${time}`;
+  const title = <TranslatedText 
+    stringId="vitals.editCell.title" 
+    fallback=":vitalLabel | :date | :time" replacements={{ vitalLabel, date, time }} />
   const handleClose = useCallback(() => {
     onClose();
   }, [onClose]);
