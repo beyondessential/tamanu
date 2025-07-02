292b933fd6df88093451a7ff09a1d60b6b806bda
dannash100 <dannash200@gmail.com>
2025-07-03 10:31:46 +1200

    verbose

--- packages/mobile/App/infra/db/helpers.ts
+++ packages/mobile/App/infra/db/helpers.ts
@@ -1,6 +1,12 @@
 import { chunk } from 'lodash';
 
-export const SQLITE_INSERT_CHUNK_SIZE = 500;
+/** @see https://www.sqlite.org/limits.html#max_variable_number */
+export const SQLITE_MAX_PARAMETERS = 32766;
+/** @see https://www.sqlite.org/limits.html#max_expr_depth */
+export const SQLITE_MAX_DEPTH_OF_EXPRESSION_TREE = 1000;
+
+export const MAX_BATCH_SIZE_FOR_BULK_INSERT = SQLITE_MAX_DEPTH_OF_EXPRESSION_TREE - 3;
+export const INSERT_CHUNK_SIZE = 500; // Should be config and just limit at 997
 
 type RowLike = {
   [key: string]: any;
@@ -9,4 +15,4 @@ type RowLike = {
 export const formatDateForQuery = (date: Date): number => date.valueOf() / 1000;
 
 export const chunkRowsForInsert = <T extends RowLike>(rows: T[]): T[][] =>
-  chunk(rows, SQLITE_INSERT_CHUNK_SIZE);
+  chunk(rows, INSERT_CHUNK_SIZE);
