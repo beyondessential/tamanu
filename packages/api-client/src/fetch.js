4d1d93a23696c1f8d023656de00f24259fe0f825
Félix Saparelli <felix@bes.au>
2025-07-03 14:46:21 +1200

    api: add timeout support

--- packages/api-client/src/fetch.js
+++ packages/api-client/src/fetch.js
@@ -1,9 +1,16 @@
 import { ServerUnavailableError } from './errors';
 
-export async function fetchOrThrowIfUnavailable(url, config = {}) {
+export async function fetchOrThrowIfUnavailable(url, { timeout = false, ...config } = {}) {
+  const abort = new AbortController();
+  let timer;
+  if (timeout && Number.isFinite(timeout) && !config.signal) {
+    timer = setTimeout(() => abort.abort(), timeout);
+  }
+
   try {
-    const response = await fetch(url, config);
-    return response;
+    return await fetch(url, { signal: abort.signal, ...config }).finally(() => {
+      clearTimeout(timer);
+    });
   } catch (e) {
     if (e instanceof Error && e.message === 'Failed to fetch') {
       // apply more helpful message if the server is not available
