cb1548745526b6957b8884b07cdea2fa6236981b
dannash100 <dannash200@gmail.com>
2025-07-07 10:28:27 +1200

    duplicate now

--- packages/database/src/demoData/createMockReportingSchemaAndRoles.js
+++ packages/database/src/demoData/createMockReportingSchemaAndRoles.js
@@ -11,18 +11,12 @@ export async function createMockReportingSchemaAndRoles({ sequelize }) {
   }
   await sequelize.query(`
     CREATE SCHEMA IF NOT EXISTS reporting;
-    -- create roles if they don't exist this can happen on local dev when running tests
-    DO $$
-    BEGIN
     CREATE ROLE ${reporting.username} WITH
       LOGIN 
       PASSWORD '${reporting.password}';
     CREATE ROLE ${raw.username} WITH
       LOGIN 
       PASSWORD '${raw.password}';
-    EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '%, skipping', SQLERRM USING ERRCODE = SQLSTATE;
-    END
-    $$;
     ALTER ROLE ${reporting.username} SET search_path TO reporting;
     GRANT USAGE ON SCHEMA reporting TO ${reporting.username};
     GRANT USAGE ON SCHEMA public TO ${raw.username};
