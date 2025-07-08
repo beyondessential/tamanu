83c3e3d58bd4994d348989cf81082f5645c14b34
Félix Saparelli <felix@bes.au>
2025-07-08 16:12:58 +1200

    reorg this stuff for clarity

--- packages/facility-server/app/database/integrity.js
+++ packages/facility-server/app/database/integrity.js
@@ -46,41 +46,33 @@ async function ensureFacilityMatches(context) {
   const configuredFacilities = selectFacilityIds(config);
   const lastFacilities = await LocalSystemFact.get(FACT_FACILITY_IDS);
 
-  if (!lastFacilities) {
-    await performInitialIntegritySetup(context);
-    return;
-  }
-
-  // ensure both arrays contain the same set of facility ids
-  const match = JSON.parse(lastFacilities).every(facilityId =>
-    configuredFacilities.includes(facilityId),
-  );
-  if (!match) {
-    // if the facility doesn't match, error
-    throw new Error(
-      `integrity check failed: serverFacilityId mismatch: read ${configuredFacilities} from config, but already registered as ${lastFacilities} (you may need to drop and recreate the database, change the config back, or if you're 100% sure, remove the "facilityIds" key from the "local_system_fact" table)`,
+  if (lastFacilities) {
+    // ensure both arrays contain the same set of facility ids
+    const match = JSON.parse(lastFacilities).every(facilityId =>
+      configuredFacilities.includes(facilityId),
     );
+    if (!match) {
+      // if the facility doesn't match, error
+      throw new Error(
+        `integrity check failed: serverFacilityId mismatch: read ${configuredFacilities} from config, but already registered as ${lastFacilities} (you may need to drop and recreate the database, change the config back, or if you're 100% sure, remove the "facilityIds" key from the "local_system_fact" table)`,
+      );
+    }
+  } else {
+    await performInitialIntegritySetup(context);
+    const facilityIdsString = JSON.stringify(configuredFacilities);
+    await LocalSystemFact.set(FACT_FACILITY_IDS, facilityIdsString);
+    log.info('Recorded facility IDs to database');
   }
 }
 
 async function performInitialIntegritySetup(context) {
   const centralServer = new CentralServerConnection(context);
-  log.info(`Verifying sync connection to ${centralServer.host}...`);
+  log.info(`Verifying central server connection to ${centralServer.host}...`);
   await centralServer.connect();
 
   if (!centralServer.hasToken()) {
     throw new Error('Could not obtain valid token from central server.');
   }
 
-  const ids = (await centralServer.loginData())?.serverFacilityIds;
-  if (!ids || ids.length === 0) {
-    throw new Error('No facility IDs found in central server login data.');
-  }
-
-  // We've ensured that our immutable config stuff is valid -- save it!
-  const { LocalSystemFact } = context.models;
-  const facilityIdsString = JSON.stringify(ids);
-  await LocalSystemFact.set(FACT_FACILITY_IDS, facilityIdsString);
-
-  log.info(`Verified with central server as facilities ${facilityIdsString}`);
+  log.info('Verified central server connection');
 }
