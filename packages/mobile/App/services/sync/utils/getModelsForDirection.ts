7756643ff914bf121b4d2be9af4a6035c9439fae
dannash100 <dannash200@gmail.com>
2025-07-08 16:02:13 +1200

    but rough but this is required by typeorm, ill rework into smoother flow in next pr

--- packages/mobile/App/services/sync/utils/getModelsForDirection.ts
+++ packages/mobile/App/services/sync/utils/getModelsForDirection.ts
@@ -5,17 +5,20 @@ import { Connection } from 'typeorm';
 export const getModelsForDirection = (
   models: typeof MODELS_MAP,
   direction: SYNC_DIRECTIONS,
-): Partial<typeof MODELS_MAP> =>  Object.fromEntries(
-    Object.entries(models).filter(
-      ([, model]) => [direction, SYNC_DIRECTIONS.BIDIRECTIONAL].includes(model.syncDirection),
+): Partial<typeof MODELS_MAP> =>
+  Object.fromEntries(
+    Object.entries(models).filter(([, model]) =>
+      [direction, SYNC_DIRECTIONS.BIDIRECTIONAL].includes(model.syncDirection),
     ),
   );
 
-
-  export const getModelsForDirectionInTransaction = (transactionEntityManager: Connection.EntityManager, direction: SYNC_DIRECTIONS) => {
-    const modelMap = {};
-    for (const [modelClass] of transactionEntityManager.connection.entityMetadatasMap) {
-      modelMap[modelClass.name] = modelClass;
-    }
-    return getModelsForDirection(modelMap, direction);
-  };
+export const getModelsForDirectionInTransaction = (
+  transactionEntityManager: Connection.EntityManager,
+  direction: SYNC_DIRECTIONS,
+) => {
+  const modelMap = {};
+  for (const [modelClass] of transactionEntityManager.connection.entityMetadatasMap) {
+    modelMap[modelClass.name] = modelClass;
+  }
+  return getModelsForDirection(modelMap, direction);
+};
