39f92c7f45752e110f45dc647d975856fc4d1094
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-07-11 14:02:26 +1200

    proper test spot

--- packages/database/__tests__/sync/buildEncounterLinkedLookupFilter.test.ts
+++
@@ -1,6 +0,0 @@
-import { describe } from 'vitest';
-
-describe('buildEncounterLinkedLookupFilter', () => {
-  test.todo('should include facility filter if provided');
-  test.todo('should not include facility filter if not provided');
-});
