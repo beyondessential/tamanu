c3ef27e6c6f1cb51d296c52434fca12ddc25d50e
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-07-11 13:38:51 +1200

    Update buildEncounterLinkedLookupFilter.test.ts

--- packages/database/__tests__/sync/buildEncounterLinkedLookupFilter.test.ts
+++ packages/database/__tests__/sync/buildEncounterLinkedLookupFilter.test.ts
@@ -1,3 +1,6 @@
 import { describe } from 'vitest';
 
-describe('buildEncounterLinkedLookupFilter', () => {});
+describe('buildEncounterLinkedLookupFilter', () => {
+  test.todo('should include facility filter if provided');
+  test.todo('should not include facility filter if not provided');
+});
