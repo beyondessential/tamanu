a05a68e69772a4dcfe7de0c41167a82515883641
dannash100 <dannash200@gmail.com>
2025-07-15 08:17:25 +1200

    weird merge thing

--- packages/facility-server/__tests__/sync/CentralServerConnection.test.js
+++ packages/facility-server/__tests__/sync/CentralServerConnection.test.js
@@ -45,11 +45,6 @@ describe('CentralServerConnection', () => {
     displayName: 'Not Real',
     email: 'notreal@example.com',
   });
-  const meSuccess = fakeSuccess({
-    id: 'not-real',
-    displayName: 'Not Real',
-    email: 'notreal@example.com',
-  });
   const authInvalid = fakeFailure(401);
   const authFailure = fakeFailure(503);
   const clientVersionLow = fakeFailure(
