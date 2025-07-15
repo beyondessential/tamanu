f63b590ac4c88b570d0fe225e58dc4f31a4898a7
dannash100 <dannash200@gmail.com>
2025-07-15 15:17:02 +1200

    yeah mabye

--- packages/shared/src/utils/translation/getReferenceDataStringId.js
+++ packages/shared/src/utils/translation/getReferenceDataStringId.js
@@ -1,10 +1,21 @@
 import { REFERENCE_DATA_TRANSLATION_PREFIX } from "@tamanu/constants";
-import { camelCase } from "lodash";
 
 export const getReferenceDataStringId = (value, category) => {
   return `${REFERENCE_DATA_TRANSLATION_PREFIX}.${category}.${value}`;
 };
 
+function toCamelCaseWithSpecialCharacters(str) {
+  return str
+    .split(/([^a-zA-Z0-9]+)/)
+    .map((word, index) => {
+      if (index === 0 || /[^a-zA-Z0-9]/.test(word)) {
+        return word;
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join('');
+}
+
 export const getReferenceDataOptionStringId = (value, category, option) => {
-  return `${getReferenceDataStringId(value, category)}.option.${camelCase(option)}`;
+  return `${getReferenceDataStringId(value, category)}.option.${toCamelCaseWithSpecialCharacters(option)}`;
 };
