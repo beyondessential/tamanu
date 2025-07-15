e426e1ac71822a2444fbc289ca4671a574fdefda
dannash100 <dannash200@gmail.com>
2025-07-15 16:24:14 +1200

    gotem

--- packages/shared/src/utils/translation/getReferenceDataStringId.js
+++ packages/shared/src/utils/translation/getReferenceDataStringId.js
@@ -4,10 +4,12 @@ import { REFERENCE_DATA_TRANSLATION_PREFIX } from '@tamanu/constants';
 
 /**
  * Returns the stringId for a reference data option.
-* @example getReferenceDataOptionStringId('question1', 'surveyScreenComponent', 'undecided') -> "refData.surveyScreenComponent.detail.question1.option.undecided"
+ * Uses slugify to clean up the option strings special characters
+ * @example getReferenceDataOptionStringId('question1', 'surveyScreenComponent', 'undecided') -> "refData.surveyScreenComponent.detail.question1.option.undecided"
+ * @example getReferenceDataOptionStringId('question1', 'surveyScreenComponent', 'A++') -> "refData.surveyScreenComponent.detail.question1.option.aPlusPlus"
  */
 export const getReferenceDataOptionStringId = (value, category, option) => {
-  return `${getReferenceDataStringId(value, category)}.option.${slugify(option)}`;
+  return `${getReferenceDataStringId(value, category)}.option.${slugify(option, { lower: true })}`;
 };
 
 /**
