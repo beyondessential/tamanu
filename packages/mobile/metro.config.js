33c54cafb44cb2c2aa821a8cfb8eba2ff72f4056
dannash100 <dannash200@gmail.com>
2025-07-17 08:25:27 +1200

    0.75.5 react native with nitro sqlite

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -3,12 +3,14 @@ const { resolve } = require('path');
 const getWorkspaces = require('get-yarn-workspaces');
 
 const config = {
-  
   watchFolders: [
-    resolve(__dirname, './node_modules'),
+    resolve(__dirname, 'node_modules'),
     resolve(__dirname, '../../node_modules'),
     ...getWorkspaces(),
   ],
+  resolver: {
+    unstable_enableSymlinks: true // Turn on symlink support
+  }
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
