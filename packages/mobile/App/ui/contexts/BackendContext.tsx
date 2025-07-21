2259ef199525702f9adb043816ba612d96cf7506
dannash100 <dannash200@gmail.com>
2025-07-21 17:14:12 +1200

    does this work or do i gotta keep it global scope

--- packages/mobile/App/ui/contexts/BackendContext.tsx
+++ packages/mobile/App/ui/contexts/BackendContext.tsx
@@ -1,21 +1,30 @@
 import React, { ReactElement, useEffect, useState } from 'react';
 import RnBgTask from 'react-native-bg-thread';
-import { BackendManager } from '../../services/BackendManager';
+import { v4 as uuidv4 } from 'uuid';
 
+import { BackendManager } from '../../services/BackendManager';8
 import { LoadingScreen } from '../components/LoadingScreen';
 import { ErrorScreen } from '../components/ErrorScreen';
+import { readConfig, writeConfig } from '~/services/config';
 
 export const BackendContext = React.createContext<BackendManager>(undefined);
 
-const backendManager = new BackendManager();
-
 export const BackendProvider = ({ Component }): ReactElement => {
   const [initialised, setInitialised] = useState(false);
+  const [backendManager, setBackendManager] = useState<BackendManager>(null);
 
   useEffect(() => {
     (async (): Promise<void> => {
       backendManager.stopSyncService();
       setInitialised(false);
+
+      const deviceId = await readConfig('deviceId');
+      if (!deviceId) {
+        const newDeviceId = `mobile-${uuidv4()}`;
+        await writeConfig('deviceId', newDeviceId);
+      }
+
+      setBackendManager(new BackendManager(deviceId));
 
       RnBgTask.runInBackground(async () => {
         await backendManager.initialise();
