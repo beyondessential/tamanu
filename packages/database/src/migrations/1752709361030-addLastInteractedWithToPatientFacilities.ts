964f919d36950c791371226f804cfbc64b6c6bbf
dannash100 <dannash200@gmail.com>
2025-07-22 11:03:03 +1200

    this should work

--- packages/database/src/migrations/1752709361030-addLastInteractedWithToPatientFacilities.ts
+++ packages/database/src/migrations/1752709361030-addLastInteractedWithToPatientFacilities.ts
@@ -10,24 +10,22 @@ export async function up(query: QueryInterface): Promise<void> {
 
   const isFacility = config.serverFacilityId || config.serverFacilityIds;
   if (isFacility) return;
-  
+
   await query.addIndex('patient_facilities', ['facility_id', 'last_interacted_time']);
   await query.sequelize.query(`
     UPDATE patient_facilities pf
     SET last_interacted_time = (
-      SELECT COALESCE(
-          GREATEST(
-              MAX(e.created_at),
-              MAX(pr.created_at)
-          ),
-          pf.created_at
+      SELECT GREATEST(
+        MAX(e.created_at),
+        MAX(pr.created_at), 
+        pf.created_at
       )
       FROM patients p
       LEFT JOIN patient_program_registrations pr ON p.id = pr.patient_id 
-          AND pr.registering_facility_id = pf.facility_id
+        AND pr.registering_facility_id = pf.facility_id
       LEFT JOIN encounters e ON p.id = e.patient_id
       LEFT JOIN locations l ON e.location_id = l.id 
-          AND l.facility_id = pf.facility_id
+        AND l.facility_id = pf.facility_id
       WHERE p.id = pf.patient_id
     );
   `);
