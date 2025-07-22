6740be506a6816fc8506071530915c3dc9c32f17
khoi.nguyen <khoi@bes.au>
2025-07-22 11:33:45 +0700

    fix(medication): EPI-1127: Mobile updates to support Medications enhancements

--- packages/mobile/App/ui/components/FrequencySearchField/FrequencySearchField.tsx
+++ packages/mobile/App/ui/components/FrequencySearchField/FrequencySearchField.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StyledText, StyledView } from '/styled/common';
+import { StyledText, StyledView } from '~/ui/styled/common';
 import { theme } from '../../styled/theme';
 import { FrequencySuggester, FrequencySuggestion } from '../../helpers/frequencySuggester';
 import {
