16fc58ac19a5812b7a98839fbfa99ceb9c43b368
dannash100 <dannash200@gmail.com>
2025-07-24 14:07:32 +1200

    merge issues

--- packages/web/app/components/PatientPrinting/modals/EncounterRecordModal.jsx
+++ packages/web/app/components/PatientPrinting/modals/EncounterRecordModal.jsx
@@ -113,8 +113,6 @@ export const EncounterRecordModal = ({ encounter, open, onClose }) => {
   const { getSetting } = useSettings();
   const { data: vitalsData, recordedDates } = useVitalsQuery(encounter.id);
 
-  const { getSetting } = useSettings();
-
   const { getLocalisation } = useLocalisation();
   const certificateQuery = useCertificate();
   const { data: certificateData } = certificateQuery;
