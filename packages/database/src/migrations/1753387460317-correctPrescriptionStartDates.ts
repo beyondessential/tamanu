eb2344d8287074873082f96a17aedd0280754f7f
dannash100 <dannash200@gmail.com>
2025-07-25 14:25:18 +1200

    better comment'

--- packages/database/src/migrations/1753387460317-correctPrescriptionStartDates.ts
+++ packages/database/src/migrations/1753387460317-correctPrescriptionStartDates.ts
@@ -1,13 +1,13 @@
 import { DataTypes, QueryInterface } from 'sequelize';
 
 export async function up(query: QueryInterface): Promise<void> {
-  // Set all defaulted empty string start_dates to NULL
+  // Set any incorrectly defaulted empty string start dates to the prescriptions "date" column
   await query.sequelize.query(`
     UPDATE prescriptions
     SET start_date = date
     WHERE start_date = ''
   `);
-  // This change also removes the default value of ''
+  // Remove the empty string default value
   await query.changeColumn('prescriptions', 'start_date', {
     type: DataTypes.DATETIMESTRING,
     allowNull: false,
--- packages/mobile/App/migrations/1753393877000-correctPrescriptionStartDates.ts
+++ packages/mobile/App/migrations/1753393877000-correctPrescriptionStartDates.ts
@@ -3,12 +3,14 @@ import { getTable } from './utils/queryRunner';
 
 export class correctPrescriptionStartDates1753393877000 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
+    // Set any incorrectly defaulted empty string start dates to the prescriptions "date" column
     const prescriptionTable = await getTable(queryRunner, 'prescriptions');
     await queryRunner.query(`
       UPDATE prescriptions
       SET startDate = date
       WHERE startDate = ''
     `);
+    // Remove the empty string default value
     await queryRunner.changeColumn(prescriptionTable, 'startDate', new TableColumn({
       name: 'startDate',
       type: 'string',
