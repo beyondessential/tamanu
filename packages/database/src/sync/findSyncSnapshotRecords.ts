f46beafc7211211f3d6eec36f75d168fdc0fbedb
dannash100 <dannash200@gmail.com>
2025-07-28 08:20:46 +1200

    here i think

--- packages/database/src/sync/findSyncSnapshotRecords.ts
+++ packages/database/src/sync/findSyncSnapshotRecords.ts
@@ -1,6 +1,8 @@
 import { camel } from 'case';
 import { QueryTypes, Sequelize } from 'sequelize';
 import { getSnapshotTableName } from './manageSnapshotTable';
+import { sortInDependencyOrder } from '../utils/sortInDependencyOrder';
+
 import type { RecordType, SyncSessionDirectionValues, SyncSnapshotAttributes } from '../types/sync';
 
 export const findSyncSnapshotRecords = async (
@@ -14,14 +16,24 @@ export const findSyncSnapshotRecords = async (
 ) => {
   const tableName = getSnapshotTableName(sessionId);
 
+  const models = sequelize.models as any; // TODO what to do here
+  const sortedModels = await sortInDependencyOrder(models);
+  const valuesSQL = sortedModels
+    .map(({ tableName }, index) => `('${tableName}', ${index + 1})`)
+    .join(',\n');
+
   const records = await sequelize.query(
     `
+      WITH priority(record_type, sort_order) AS (
+        VALUES
+          ${valuesSQL}
+      ),
       SELECT * FROM ${tableName}
       WHERE id > :fromId
       AND direction = :direction
       ${recordType ? 'AND record_type = :recordType' : ''}
       ${additionalWhere ? `AND ${additionalWhere}` : ''}
-      ORDER BY id ASC
+      ORDER BY priority.sort_order NULLS LAST
       LIMIT :limit;
     `,
     {
@@ -36,7 +48,7 @@ export const findSyncSnapshotRecords = async (
     },
   );
 
-  return records.map((r) =>
+  return records.map(r =>
     Object.fromEntries(Object.entries(r).map(([key, value]) => [camel(key), value])),
   ) as SyncSnapshotAttributes[];
 };
