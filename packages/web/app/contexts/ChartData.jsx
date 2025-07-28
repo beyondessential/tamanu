c78b9c58abef5541b23d82151a09f2eae15d0cb8
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-07-28 11:48:39 -0400

    Only use user preference if chart has current visibility status

--- packages/web/app/contexts/ChartData.jsx
+++ packages/web/app/contexts/ChartData.jsx
@@ -3,6 +3,7 @@ import { useUserPreferencesQuery } from '../api/queries/useUserPreferencesQuery'
 import { useEncounter } from './Encounter';
 import { useEncounterChartWithResponseQuery } from '../api/queries/useEncounterChartWithResponseQuery';
 import { combineQueries } from '../api';
+import { useChartSurveysQuery } from '../api/queries';
 
 const ChartDataContext = createContext({
   selectedChartTypeId: null,
@@ -17,11 +18,12 @@ export const ChartDataProvider = ({ children }) => {
   const [selectedChartTypeId, setSelectedChartTypeId] = useState('');
   const userPreferencesQuery = useUserPreferencesQuery();
   const chartWithResponseQuery = useEncounterChartWithResponseQuery(encounter?.id);
+  const chartSurveysQuery = useChartSurveysQuery();
   const {
-    data: [userPreferences, chartWithResponse],
+    data: [userPreferences, chartWithResponse, chartSurveys],
     isLoading,
     isFetching,
-  } = combineQueries([userPreferencesQuery, chartWithResponseQuery]);
+  } = combineQueries([userPreferencesQuery, chartWithResponseQuery, chartSurveysQuery]);
   const shouldInit = !isLoading && !isInitiated && !isFetching;
 
   useEffect(() => {
@@ -29,12 +31,18 @@ export const ChartDataProvider = ({ children }) => {
     // Only set initial type if encounter has chart responses
     if (chartWithResponse?.data) {
       // Prioritize user preference, chart with response is only a fallback
-      const initialChart =
-        userPreferences?.selectedChartTypeId ?? chartWithResponse?.data?.survey.id;
-      setSelectedChartTypeId(initialChart);
+      const hasUserPreference = !!userPreferences?.selectedChartTypeId;
+      const userPreferenceIsCurrent = chartSurveys?.chartSurveys?.some(
+        (survey) => survey.id === userPreferences?.selectedChartTypeId,
+      );
+      if (hasUserPreference && userPreferenceIsCurrent) {
+        setSelectedChartTypeId(userPreferences?.selectedChartTypeId);
+      } else {
+        setSelectedChartTypeId(chartWithResponse?.data?.survey.id);
+      }
     }
     setIsInitiated(true);
-  }, [userPreferences, chartWithResponse, shouldInit]);
+  }, [userPreferences, chartWithResponse, chartSurveys,shouldInit]);
 
   const contextValue = useMemo(() => ({
     isLoading: !isInitiated,
