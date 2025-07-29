a4577d74e911e3ca7538fd785ae67391a6681960
dannash100 <dannash200@gmail.com>
2025-07-29 12:10:15 +1200

    add streaming pull records func to mobile

--- packages/mobile/App/services/sync/utils/pullRecordsInBatches.ts
+++ packages/mobile/App/services/sync/utils/pullRecordsInBatches.ts
@@ -1,6 +1,7 @@
 import { calculatePageLimit } from './calculatePageLimit';
 import { SYNC_SESSION_DIRECTION } from '../constants';
 import { CentralServerConnection } from '../CentralServerConnection';
+import { SYNC_STREAM_MESSAGE_KIND } from '@tamanu/constants';
 
 type PullRecordsInBatchesParams = {
   centralServer: CentralServerConnection;
@@ -38,4 +39,58 @@ export const pullRecordsInBatches = async (
 
     progressCallback(recordsToSave.length);
   }
+};
+
+export const streamRecordsInBatches = async (
+  params: Omit<PullRecordsInBatchesParams, 'recordTotal'>,
+  processRecords: (records: any) => Promise<void>,
+) => {
+  const { centralServer, sessionId, progressCallback = () => {} } = params;
+  let records = [];
+  const writes = [];
+  let fromId;
+
+  const writeBatch = async (recordsToWrite: any[]) => {
+    if (recordsToWrite.length === 0) return;
+    await processRecords(recordsToWrite);
+  };
+
+  // keep track of the ID we're on so we can resume the stream
+  // on disconnect from where we left off rather than the start
+  const endpointFn = () => ({
+    endpoint: `sync/${sessionId}/pull/stream`,
+    query: { fromId },
+  });
+
+  stream: for await (const { kind, message } of centralServer.stream(endpointFn)) {
+    if (records.length >= 100) {
+      // do writes in the background while we're continuing to stream data
+      writes.push(writeBatch(records));
+      records = [];
+    }
+
+    handler: switch (kind) {
+      case SYNC_STREAM_MESSAGE_KIND.PULL_CHANGE:
+        records.push({
+          ...message,
+          // mark as never updated, so we don't push it back to the central server until the next update
+          data: { ...message.data, updated_at_sync_tick: -1 },
+          direction: SYNC_SESSION_DIRECTION.INCOMING,
+        });
+        fromId = btoa(JSON.stringify({ sortOrder: message.sortOrder, id: message.id }));
+        progressCallback(1);
+        break handler;
+      case SYNC_STREAM_MESSAGE_KIND.END:
+        console.debug('MobileSyncManager.pull.noMoreChanges');
+        break stream;
+      default:
+        console.warn('MobileSyncManager.pull.unknownMessageKind', { kind });
+    }
+  }
+
+  if (records.length > 0) {
+    writes.push(writeBatch(records));
+  }
+
+  await Promise.all(writes);
 };
