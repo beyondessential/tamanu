8ecabfcf2a1a3cbcbcc3f0df9dde7b5a2186cb34
Flynn <flynn.teh@gmail.com>
2025-07-29 17:18:24 +1200

    new send to patient portal button

--- packages/web/app/views/programs/SurveySelector.jsx
+++ packages/web/app/views/programs/SurveySelector.jsx
@@ -1,17 +1,23 @@
 import React, { useCallback } from 'react';
 import styled from 'styled-components';
 
-import { Button } from '../../components/Button';
+import { Button, TextButton } from '../../components/Button';
 import { ButtonRow } from '../../components/ButtonRow';
 import { SelectInput } from '../../components/Field/SelectField';
+import { SendIcon } from '../../components/Icons/SendIcon';
+import { TranslatedText } from '../../components/Translation/TranslatedText';
 
 const StyledButtonRow = styled(ButtonRow)`
   margin-top: 24px;
 `;
 
+const StyledTextButton = styled(TextButton)`
+  text-transform: none;
+`;
+
 export const SurveySelector = React.memo(({ value, onChange, onSubmit, surveys, buttonText }) => {
   const handleChange = useCallback(
-    (event) => {
+    event => {
       const surveyId = event.target.value;
       onChange(surveyId);
     },
@@ -32,12 +38,20 @@ export const SurveySelector = React.memo(({ value, onChange, onSubmit, surveys, 
         data-testid="selectinput-4g3c"
       />
       <StyledButtonRow data-testid="styledbuttonrow-nem0">
+        <StyledTextButton>
+          <SendIcon width={12} height={12} style={{ marginRight: '0.25rem' }} />
+          <TranslatedText
+            stringId="program.modal.selectSurvey.action.sendToPatientPortal"
+            fallback="Send to patient portal"
+          />
+        </StyledTextButton>
         <Button
           onClick={handleSubmit}
           disabled={!value}
           variant="contained"
           color="primary"
           data-testid="button-qsbg"
+          style={{ marginLeft: 'auto' }}
         >
           {buttonText}
         </Button>
