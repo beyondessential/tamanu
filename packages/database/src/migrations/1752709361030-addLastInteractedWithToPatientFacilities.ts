0dcda7cbd8fec3e7ed6a31ecb9f6056f55564f35
dannash100 <dannash200@gmail.com>
2025-07-29 15:11:15 +1200

    use cte

--- packages/database/src/migrations/1752709361030-addLastInteractedWithToPatientFacilities.ts
+++ packages/database/src/migrations/1752709361030-addLastInteractedWithToPatientFacilities.ts
@@ -24,22 +24,29 @@ export async function up(query: QueryInterface): Promise<void> {
   if (isFacility) return;
 
   await query.addIndex('patient_facilities', ['facility_id', 'last_interacted_time']);
+  
   await query.sequelize.query(`
-    UPDATE patient_facilities pf
-    SET last_interacted_time = (
-      SELECT GREATEST(
-        MAX(e.created_at),
-        MAX(pr.created_at), 
-        pf.created_at
-      )
-      FROM patients p
+    WITH calculated_interaction_times AS (
+      SELECT 
+        pf.id,
+        GREATEST(
+          MAX(e.created_at),
+          MAX(pr.created_at),
+          pf.created_at
+        ) as calculated_last_interacted_time
+      FROM patient_facilities pf
+      LEFT JOIN patients p ON p.id = pf.patient_id
       LEFT JOIN patient_program_registrations pr ON p.id = pr.patient_id 
         AND pr.registering_facility_id = pf.facility_id
       LEFT JOIN encounters e ON p.id = e.patient_id
       LEFT JOIN locations l ON e.location_id = l.id 
         AND l.facility_id = pf.facility_id
-      WHERE p.id = pf.patient_id
-    );
+      GROUP BY pf.id, pf.created_at
+    )
+    UPDATE patient_facilities pf
+    SET last_interacted_time = cit.calculated_last_interacted_time
+    FROM calculated_interaction_times cit
+    WHERE pf.id = cit.id;
   `);
 }
 
