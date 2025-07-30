fb834db3390a39d3c62084714a660df64f4485d1
dannash100 <dannash200@gmail.com>
2025-07-31 09:14:20 +1200

    fix casing on mobile migration

--- packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
+++ packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
@@ -1,24 +1,22 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
-import { getTable } from './utils/queryRunner';
 
 export class addLastInteractedTimeToPatientFacilities1752709361030 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
-    const table = await getTable('patient_facilities');
-    await queryRunner.addColumn(table, new TableColumn({
-      name: 'last_interacted_time',
+    await queryRunner.addColumn('patient_facilities', new TableColumn({
+      name: 'lastInteractedTime',
       type: 'datetime',
       default: "datetime('now')",
     }));
 
-    await queryRunner.addColumn(table, new TableColumn({
-      name: 'created_at_sync_tick',
+    await queryRunner.addColumn('patient_facilities', new TableColumn({
+      name: 'createdAtSyncTick',
       type: 'bigint',
       default: -999,
     }));
 
     await queryRunner.query(`
       UPDATE patient_facilities
-      SET created_at_sync_tick = updated_at_sync_tick;
+      SET createdAtSyncTick = updatedAtSyncTick;
     `);
 
     await queryRunner.query(`
@@ -26,18 +24,18 @@ export class addLastInteractedTimeToPatientFacilities1752709361030 implements Mi
         SELECT 
           pf.id,
           GREATEST(
-            MAX(e.created_at),
-            MAX(pr.created_at),
-            pf.created_at
+            MAX(e.createdAt),
+            MAX(pr.createdAt),
+            pf.createdAt
           ) as calculated_last_interacted_time
         FROM patient_facilities pf
-        LEFT JOIN patients p ON p.id = pf.patient_id
-        LEFT JOIN patient_program_registrations pr ON p.id = pr.patient_id 
-          AND pr.registering_facility_id = pf.facility_id
-        LEFT JOIN encounters e ON p.id = e.patient_id
-        LEFT JOIN locations l ON e.location_id = l.id 
-          AND l.facility_id = pf.facility_id
-        GROUP BY pf.id, pf.created_at
+        LEFT JOIN patients p ON p.id = pf.patientId
+        LEFT JOIN patient_program_registrations pr ON p.id = pr.patientId 
+          AND pr.registering_facility_id = pf.facilityId
+        LEFT JOIN encounters e ON p.id = e.patientId
+        LEFT JOIN locations l ON e.locationId = l.id 
+          AND l.facilityId = pf.facilityId
+        GROUP BY pf.id, pf.createdAt
       )
       UPDATE patient_facilities pf
       SET last_interacted_time = cit.calculated_last_interacted_time
@@ -47,8 +45,7 @@ export class addLastInteractedTimeToPatientFacilities1752709361030 implements Mi
   }
 
   async down(queryRunner: QueryRunner): Promise<void> {
-    const table = await getTable('patient_facilities');
-    await queryRunner.dropColumn(table, 'last_interacted_time');
-    await queryRunner.dropColumn(table, 'created_at_sync_tick');
+    await queryRunner.dropColumn('patient_facilities', 'lastInteractedTime');
+    await queryRunner.dropColumn('patient_facilities', 'createdAtSyncTick');
   }
 }
