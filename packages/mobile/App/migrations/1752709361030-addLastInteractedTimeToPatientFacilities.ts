39a5a6f0a5fecb0fd1a566de5598ac5b3e7b207a
dannash100 <dannash200@gmail.com>
2025-07-31 15:35:12 +1200

    woohoo

--- packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
+++ packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
@@ -27,40 +27,27 @@ export class addLastInteractedTimeToPatientFacilities1752709361030 implements Mi
 
     // Query differs from postgres version because of sqlite limitations
     await queryRunner.query(`
-    WITH max_dates AS (
+      WITH max_dates AS (
         SELECT 
           pf.id as patient_facility_id,
           MAX(e.createdAt) as max_encounter_date,
-          MAX(pr.createdAt) as max_registration_date,
           pf.createdAt as facility_created_at
         FROM patient_facilities pf
-        JOIN patients p ON p.id = pf.patientId
-        LEFT JOIN patient_program_registrations pr
-          ON p.id = pr.patientId 
-          AND pr.registeringFacilityId = pf.facilityId
         LEFT JOIN encounters e
-          ON p.id = e.patientId
+          ON pf.id = e.patientId
         LEFT JOIN locations l
           ON e.locationId = l.id
           AND l.facilityId = pf.facilityId
+        WHERE (e.id IS NULL OR l.id IS NOT NULL)
         GROUP BY pf.id, pf.createdAt
       )
       UPDATE patient_facilities
       SET lastInteractedTime = (
         SELECT 
           CASE
-            WHEN md.max_encounter_date IS NOT NULL 
-              AND md.max_registration_date IS NOT NULL
-              AND md.max_encounter_date > md.max_registration_date
+            WHEN md.max_encounter_date IS NOT NULL
               AND md.max_encounter_date > md.facility_created_at
             THEN md.max_encounter_date
-            WHEN md.max_encounter_date IS NOT NULL 
-              AND md.max_registration_date IS NULL
-              AND md.max_encounter_date > md.facility_created_at
-            THEN md.max_encounter_date
-            WHEN md.max_registration_date IS NOT NULL
-              AND md.max_registration_date > md.facility_created_at
-            THEN md.max_registration_date
             ELSE md.facility_created_at
           END
         FROM max_dates md
