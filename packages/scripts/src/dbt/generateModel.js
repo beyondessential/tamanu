0755c17edcab1cc5ce679b1028e0c16542b3c295
Félix Saparelli <felix@bes.au>
2025-07-31 14:19:51 +1200

    undo fucken editor messing up formatting

--- packages/scripts/src/dbt/generateModel.js
+++ packages/scripts/src/dbt/generateModel.js
@@ -13,11 +13,11 @@ const { dbConfig } = require('./dbConfig.js');
  */
 async function readTablesFromDbt(schemaPath, noSymlinks = false) {
   const tablePromises = (await fs.readdir(schemaPath, { withFileTypes: true }))
-    .filter(entry => (noSymlinks ? !entry.isSymbolicLink() : true))
-    .map(entry => entry.name)
-    .filter(tablePath => tablePath.endsWith('.yml'))
+    .filter((entry) => (noSymlinks ? !entry.isSymbolicLink() : true))
+    .map((entry) => entry.name)
+    .filter((tablePath) => tablePath.endsWith('.yml'))
     .sort()
-    .map(async tablePath => {
+    .map(async (tablePath) => {
       const table = await fs.readFile(path.join(schemaPath, tablePath), { encoding: 'utf-8' });
       return YAML.parse(table);
     });
@@ -36,7 +36,7 @@ async function getSchemas(client) {
       WHERE schema_name not similar to 'pg%|information_schema|sync_snapshots|reporting'
       ORDER BY schema_name`,
     )
-  ).rows.map(table => table.schema_name);
+  ).rows.map((table) => table.schema_name);
 }
 
 /**
@@ -53,7 +53,7 @@ async function getTablesInSchema(client, schemaName) {
       ORDER BY table_name`,
       [schemaName],
     )
-  ).rows.map(table => table.table_name);
+  ).rows.map((table) => table.table_name);
 }
 
 /**
@@ -70,7 +70,7 @@ async function getColumnsInRelation(client, schemaName, tableName) {
       WHERE table_schema = $1 and table_name = $2`,
       [schemaName, tableName],
     )
-  ).rows.map(row => ({
+  ).rows.map((row) => ({
     name: row.column_name,
     is_nullable: row.is_nullable === 'YES' ? true : false,
     data_type: row.character_maximum_length
@@ -97,18 +97,14 @@ async function getConstraintsForColumn(client, schemaName, tableName, columnName
     )
   ).rows;
 
-  return (
-    constraints
-      .filter(cons => cons.column_names.includes(columnName))
-      // ignore multi-column constraints, e.g. composite UNIQUE
-      .filter(cons => cons.column_names.length === 1)
-      .flatMap(cons =>
-        cons.column_names.map(col => ({
-          column_name: col,
-          constraint_type: cons.constraint_type,
-        })),
-      )
-  );
+  return constraints
+    .filter(cons => cons.column_names.includes(columnName))
+    // ignore multi-column constraints, e.g. composite UNIQUE
+    .filter(cons => cons.column_names.length === 1)
+    .flatMap(cons => cons.column_names.map(col => ({
+      column_name: col,
+      constraint_type: cons.constraint_type,
+    })));
 }
 
 /**
@@ -117,10 +113,10 @@ async function getConstraintsForColumn(client, schemaName, tableName, columnName
  * @returns A list of tables read from the DB as JS objects
  */
 async function readTablesFromDB(client, schemaName) {
-  const tablePromises = (await getTablesInSchema(client, schemaName)).map(async table => {
+  const tablePromises = (await getTablesInSchema(client, schemaName)).map(async (table) => {
     return {
       name: table,
-      columns: (await getColumnsInRelation(client, schemaName, table)).map(column => {
+      columns: (await getColumnsInRelation(client, schemaName, table)).map((column) => {
         // Lazily evaluate constraints as it's only occasionally needed.
         column.getConstraints = () =>
           getConstraintsForColumn(client, schemaName, table, column.name);
@@ -223,7 +219,7 @@ async function readTableDoc(schema, tableName) {
 async function generateDataTests(column) {
   const dataTests = [];
   const isUnique = (await column.getConstraints()).some(
-    c => c.constraint_type === 'UNIQUE' || c.constraint_type === 'PRIMARY KEY',
+    (c) => c.constraint_type === 'UNIQUE' || c.constraint_type === 'PRIMARY KEY',
   );
   if (isUnique) dataTests.push('unique');
   if (!column.is_nullable) dataTests.push('not_null');
@@ -303,7 +299,7 @@ async function generateTableModel(schema, table, genericColNames) {
               tags: [],
             },
             columns: await Promise.all(
-              table.columns.map(c =>
+              table.columns.map((c) =>
                 generateColumnModel(schema, table.name, c, genericColNames.includes(c.name)),
               ),
             ),
@@ -324,7 +320,7 @@ function generateTableDoc(table, genericColNames) {
   return {
     name: table.name,
     description: 'TODO',
-    columns: table.columns.filter(c => !genericColNames.includes(c.name)).map(generateColumnDoc),
+    columns: table.columns.filter((c) => !genericColNames.includes(c.name)).map(generateColumnDoc),
   };
 }
 
@@ -334,7 +330,7 @@ function generateTableDoc(table, genericColNames) {
  * @returns A string form of the given table doc
  */
 function stringifyTableDoc(schema, doc) {
-  const stringifyColumn = column => {
+  const stringifyColumn = (column) => {
     return `
 {% docs ${docPrefix(schema, doc.name)}__${column.name} %}
 ${column.description}
@@ -357,7 +353,7 @@ ${doc.columns.map(stringifyColumn).join('')}`;
  * @returns
  */
 function fillMissingDocColumn(index, column, hasGenericDoc, doc) {
-  if (doc.columns.some(c => c.name === column.name) || hasGenericDoc) return;
+  if (doc.columns.some((c) => c.name === column.name) || hasGenericDoc) return;
 
   doc.columns.splice(index, 0, generateColumnDoc(column));
 }
@@ -375,7 +371,7 @@ async function fillMissingDoc(schema, table, genericColNames = []) {
   // delete empty files
   if (
     (await fs.stat(docPath).then(
-      stat => stat.size,
+      (stat) => stat.size,
       () => 1,
     )) === 0
   ) {
@@ -400,8 +396,8 @@ async function fillMissingDoc(schema, table, genericColNames = []) {
  */
 async function handleRemovedColumn(tableName, column, out) {
   console.warn(` | removing ${column.name} in ${tableName}`);
-  remove(out.dbtColumns, c => c.name === column.name);
-  remove(out.doc.columns, c => c.name === column.name);
+  remove(out.dbtColumns, (c) => c.name === column.name);
+  remove(out.doc.columns, (c) => c.name === column.name);
 }
 
 /**
@@ -461,7 +457,7 @@ async function handleColumn(schema, tableName, dbtColumn, sqlColumn, hasGenericD
   if (!Object.hasOwn(dbtColumn, 'data_tests')) dbtColumn.data_tests = [];
 
   // Instead of computing differences while ignoring non-trivial data tests, simply replace them by the generated one.
-  remove(dbtColumn.data_tests, t => t === 'unique' || t === 'not_null');
+  remove(dbtColumn.data_tests, (t) => t === 'unique' || t === 'not_null');
   dbtColumn.data_tests.splice(0, 0, ...sqlDataTests);
 
   if (dbtColumn.data_tests.length === 0) delete dbtColumn.data_tests;
@@ -476,10 +472,10 @@ async function handleColumns(schema, tableName, dbtSrc, sqlColumns, genericColNa
   // This is expensive yet the most straightforward implementation to detect changes.
   // Algorithms that rely on sorted lists are out because we want preserve the original order of columns.
   // May be able to use Map, but it doesn't have convenient set operations.
-  differenceBy(out.dbtColumns, sqlColumns, 'name').forEach(column =>
+  differenceBy(out.dbtColumns, sqlColumns, 'name').forEach((column) =>
     handleRemovedColumn(tableName, column, out),
   );
-  differenceBy(sqlColumns, out.dbtColumns, 'name').forEach(column =>
+  differenceBy(sqlColumns, out.dbtColumns, 'name').forEach((column) =>
     handleMissingColumn(
       schema,
       tableName,
@@ -491,8 +487,8 @@ async function handleColumns(schema, tableName, dbtSrc, sqlColumns, genericColNa
   );
 
   const intersectionPromises = intersectionBy(out.dbtColumns, sqlColumns, 'name').map(
-    async dbtColumn => {
-      const sqlColumnIndex = sqlColumns.findIndex(c => c.name === dbtColumn.name);
+    async (dbtColumn) => {
+      const sqlColumnIndex = sqlColumns.findIndex((c) => c.name === dbtColumn.name);
       const sqlColumn = sqlColumns[sqlColumnIndex];
 
       const hasGenericDoc = genericColNames.includes(dbtColumn.name);
@@ -506,7 +502,7 @@ async function handleColumns(schema, tableName, dbtSrc, sqlColumns, genericColNa
   const tablePath = path.join(schema.path, tableName);
   const modelPromise = fs.writeFile(
     tablePath + '.yml',
-    YAML.stringify(dbtSrc, { lineWidth: -1, noRefs: true }),
+    YAML.stringify(dbtSrc, { lineWidth: -1, noRefs: true })
   );
   const docPromise = fs.writeFile(tablePath + '.md', stringifyTableDoc(schema, out.doc));
   await Promise.all([modelPromise, docPromise]);
@@ -525,19 +521,19 @@ async function handleTable(schema, dbtSrc, sqlTable, genericColNames) {
 }
 
 async function handleTables(schema, dbtSrcs, sqlTables) {
-  const genericColNames = (await readTableDoc(schema, 'generic'))?.columns.map(c => c.name) ?? [];
+  const genericColNames = (await readTableDoc(schema, 'generic'))?.columns.map((c) => c.name) ?? [];
 
-  const getName = srcOrTable =>
+  const getName = (srcOrTable) =>
     srcOrTable.sources ? srcOrTable.sources[0].tables[0].name : srcOrTable.name;
-  const removedPromises = differenceBy(dbtSrcs, sqlTables, getName).map(src =>
+  const removedPromises = differenceBy(dbtSrcs, sqlTables, getName).map((src) =>
     handleRemovedTable(schema, src.sources[0].tables[0]),
   );
-  const missingPromises = differenceBy(sqlTables, dbtSrcs, getName).map(table =>
+  const missingPromises = differenceBy(sqlTables, dbtSrcs, getName).map((table) =>
     handleMissingTable(schema, table, genericColNames),
   );
 
-  const intersectionPromises = intersectionBy(dbtSrcs, sqlTables, getName).map(async dbtSrc => {
-    const sqlTable = sqlTables.find(t => t.name === dbtSrc.sources[0].tables[0].name);
+  const intersectionPromises = intersectionBy(dbtSrcs, sqlTables, getName).map(async (dbtSrc) => {
+    const sqlTable = sqlTables.find((t) => t.name === dbtSrc.sources[0].tables[0].name);
     await handleTable(schema, dbtSrc, sqlTable, genericColNames);
   });
   await Promise.all([...removedPromises, ...missingPromises, ...intersectionPromises]);
