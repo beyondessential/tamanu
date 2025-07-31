1a478f4aa7bdceb4d59532b5aa225b4ebd091630
dannash100 <dannash200@gmail.com>
2025-07-31 13:38:04 +1200

    yessir

--- packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
+++ packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
@@ -20,6 +20,11 @@ export class addLastInteractedTimeToPatientFacilities1752709361030 implements Mi
       }),
     );
 
+    await queryRunner.query(`
+      UPDATE patient_facilities
+      SET createdAtSyncTick = updatedAtSyncTick;
+    `);
+
     // Query differs from postgres version because of sqlite limitations
     // Calculate last interaction time based on:
     // - Encounters in that facility for that patient
