1ae3cab83209fd64dcec9a5fbccfc1392eb2541d
dannash100 <dannash200@gmail.com>
2025-07-31 16:50:30 +1200

    oopsie

--- packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
+++ packages/mobile/App/migrations/1752709361030-addLastInteractedTimeToPatientFacilities.ts
@@ -27,31 +27,31 @@ export class addLastInteractedTimeToPatientFacilities1752709361030 implements Mi
 
     // Query differs from postgres version because of sqlite limitations
     await queryRunner.query(`
-   WITH calculated_interaction_times AS (
-      SELECT 
-        pf.id,
-        MAX(
-          CASE 
-            WHEN e.createdAt IS NULL THEN pf.createdAt
-            WHEN pf.createdAt > e.createdAt THEN pf.createdAt
-            ELSE e.createdAt
-          END
-        ) as calculated_last_interacted_time
-      FROM patient_facilities pf
-      LEFT JOIN encounters e 
-        ON pf.patientId = e.patientId
-      LEFT JOIN locations l 
-        ON e.locationId = l.id 
-        AND l.facilityId = pf.facilityId
-      WHERE (e.id IS NULL OR l.id IS NOT NULL)
-      GROUP BY pf.id, pf.createdAt
-    )
-    UPDATE patient_facilities 
-    SET lastInteractedTime = (
-      SELECT calculated_last_interacted_time 
-      FROM calculated_interaction_times 
-      WHERE calculated_interaction_times.id = patient_facilities.id;
-    );
+      WITH calculated_interaction_times AS (
+        SELECT 
+          pf.id,
+          MAX(
+            CASE 
+              WHEN e.createdAt IS NULL THEN pf.createdAt
+              WHEN pf.createdAt > e.createdAt THEN pf.createdAt
+              ELSE e.createdAt
+            END
+          ) as calculated_last_interacted_time
+        FROM patient_facilities pf
+        LEFT JOIN encounters e 
+          ON pf.patientId = e.patientId
+        LEFT JOIN locations l 
+          ON e.locationId = l.id 
+          AND l.facilityId = pf.facilityId
+        WHERE (e.id IS NULL OR l.id IS NOT NULL)
+        GROUP BY pf.id, pf.createdAt
+      )
+      UPDATE patient_facilities 
+      SET lastInteractedTime = (
+        SELECT calculated_last_interacted_time 
+        FROM calculated_interaction_times 
+        WHERE calculated_interaction_times.id = patient_facilities.id
+      );
     `);
   }
 
