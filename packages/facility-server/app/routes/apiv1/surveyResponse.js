768dea2c665cac912934a674fd5c96f6ef1020fd
Luis Emilio Navarro <60558370+NavarroEmilioLuis@users.noreply.github.com>
2025-07-31 20:07:01 -0400

    feat(desktopCharting): SAV-695: Add ability to edit complex charts core data (#8077)
    * Include edit button inside section
    
    * Have component handle its own logic
    
    * Include draft chart modal to handle edits
    
    * Simplify field visibility check and show historical values if they exist
    
    * Create new endpoint to handle edit
    
    * Complete edit modal
    
    * Ensure components are actually part of the survey
    
    * Import from the right place
    
    * Create unit tests for the new endpoint
    
    * Remove commented out line
    
    * Add granular permission check
    
    * Check permission before leaking info
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    
    * Error if components are missing
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    
    * Also create new answer in the full edit test
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>
    
    * Revert "Check permission before leaking info"
    
    This reverts commit ec171c066745e6664f073a9e392eebcc4f1b471e.
    
    ---------
    
    Co-authored-by: Edwin Monk-Fromont <edwin.monkfromont@gmail.com>

--- packages/facility-server/app/routes/apiv1/surveyResponse.js
+++ packages/facility-server/app/routes/apiv1/surveyResponse.js
@@ -3,6 +3,8 @@ import asyncHandler from 'express-async-handler';
 import { subject } from '@casl/ability';
 
 import { transformAnswers } from '@tamanu/shared/reports/utilities/transformAnswers';
+import { SURVEY_TYPES } from '@tamanu/constants';
+import { InvalidOperationError, NotFoundError } from '@tamanu/shared/errors';
 
 export const surveyResponse = express.Router();
 
@@ -83,6 +85,61 @@ surveyResponse.post(
     const responseRecord = await db.transaction(async () => {
       return models.SurveyResponse.createWithAnswers(updatedBody);
     });
+    res.send(responseRecord);
+  }),
+);
+
+surveyResponse.put(
+  '/complexChartInstance/:id',
+  asyncHandler(async (req, res) => {
+    const {
+      models,
+      body,
+      params,
+      db,
+    } = req;
+
+    const responseRecord = await models.SurveyResponse.findByPk(params.id);
+    if (!responseRecord) {
+      throw new NotFoundError('Response record not found');
+    }
+
+    req.checkPermission('write', subject('Charting', { id: responseRecord.surveyId }));
+
+    const survey = await responseRecord.getSurvey();
+    if (survey.surveyType !== SURVEY_TYPES.COMPLEX_CHART_CORE) {
+      throw new InvalidOperationError('Cannot edit survey responses');
+    }
+
+    const components = await models.SurveyScreenComponent.getComponentsForSurvey(survey.id);
+    const responseAnswers = await models.SurveyResponseAnswer.findAll({
+      where: { responseId: params.id },
+    });
+
+    await db.transaction(async () => {
+      for (const [dataElementId, value] of Object.entries(body.answers)) {
+        if (!components.some((c) => c.dataElementId === dataElementId)) {
+          throw new InvalidOperationError('Some components are missing from the survey');
+        }
+
+        // Ignore null values
+        if (value === null) {
+          continue;
+        }
+
+        const existingAnswer = responseAnswers.find((a) => a.dataElementId === dataElementId);
+        if (existingAnswer) {
+          await existingAnswer.update({ body: value });
+        } else {
+          await models.SurveyResponseAnswer.create({
+            dataElementId,
+            body: value,
+            responseId: params.id,
+          });
+        }
+      }
+    });
+
     res.send(responseRecord);
   }),
 );
