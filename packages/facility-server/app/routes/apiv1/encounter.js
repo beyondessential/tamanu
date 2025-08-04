bc3a1e1ddfbcaed12de2dcac692f533f6468b1ab
Rohan Port <rohan@bes.au>
2025-08-04 14:25:21 +1000

    feat(pharmacyOrders): TAN-2526: Fixed up linting error

--- packages/facility-server/app/routes/apiv1/encounter.js
+++ packages/facility-server/app/routes/apiv1/encounter.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import { Op, QueryTypes, literal } from 'sequelize';
 import { subject } from '@casl/ability';
 import { NotFoundError, InvalidParameterError, InvalidOperationError } from '@tamanu/shared/errors';
-import { getCurrentDateTimeString, toDateTimeString } from '@tamanu/utils/dateTime';
+import { getCurrentDateTimeString } from '@tamanu/utils/dateTime';
 import config from 'config';
 import { toCountryDateTimeString } from '@tamanu/shared/utils/countryDateTime';
 import {
