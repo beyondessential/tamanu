13b228f6a9e5ed8f418de41447750e7ba17eeb81
Flynn <flynn.teh@gmail.com>
2025-08-05 10:50:32 +1200

    layouts

---
+++ packages/patient-portal/src/layouts/PageHeader.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import tamanuLogoBlue from '../../assets/images/tamanu_logo_blue.svg';
+
+export const PageHeader = () => {
+  return (
+    <Box
+      sx={theme => ({
+        backgroundColor: 'background.paper',
+        p: 1.5,
+        borderBottom: `1px solid ${theme.palette.divider}`,
+      })}
+    >
+      <img src={tamanuLogoBlue} alt="Tamanu Logo" />
+    </Box>
+  );
+};
