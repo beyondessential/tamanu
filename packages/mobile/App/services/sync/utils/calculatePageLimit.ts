67a1796cbadd80375a3c7c0c21c51946950d32b7
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-08-07 11:57:10 +1200

    ya

--- packages/mobile/App/services/sync/utils/calculatePageLimit.ts
+++ packages/mobile/App/services/sync/utils/calculatePageLimit.ts
@@ -9,7 +9,7 @@ export type DynamicLimiterSettings = {
 export const DYNAMIC_LIMITER_DEFAULTS = {
   initialLimit: 10000,
   minLimit: 1000,
-  maxLimit: 200000, // over this and we hit heap limit
+  maxLimit: 20000, // over this and we hit heap limit
   maxLimitChangePerPage: 0.3, // max 30% increase from batch to batch, or it is too jumpy
   optimalTimePerPage: 5000, // aim for 5 seconds per page
 };
