4145c705554a80e4caef7f1aa069a877ada90518
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-08-11 11:57:30 +1200

    finish basic splitup of tests

---
+++ packages/central-server/__tests__/sync/CentralSyncManager.endSession.test.js
@@ -0,0 +1,90 @@
+import { FACT_CURRENT_SYNC_TICK, FACT_LOOKUP_UP_TO_TICK } from '@tamanu/constants/facts';
+import { sleepAsync } from '@tamanu/utils/sleepAsync';
+import { SYSTEM_USER_UUID } from '@tamanu/constants';
+
+import { createTestContext } from '../utilities';
+
+describe('CentralSyncManager', () => {
+  let ctx;
+  let models;
+
+  const DEFAULT_CURRENT_SYNC_TIME_VALUE = 2;
+  const DEFAULT_CONFIG = {
+    sync: {
+      lookupTable: {
+        enabled: false,
+      },
+      maxRecordsPerSnapshotChunk: 1000000000,
+    },
+  };
+
+  const initializeCentralSyncManager = config => {
+    // Have to load test function within test scope so that we can mock dependencies per test case
+    const {
+      CentralSyncManager: TestCentralSyncManager,
+    } = require('../../dist/sync/CentralSyncManager');
+
+    TestCentralSyncManager.overrideConfig(config || DEFAULT_CONFIG);
+
+    return new TestCentralSyncManager(ctx);
+  };
+
+  const waitForSession = async (centralSyncManager, sessionId) => {
+    let ready = false;
+    while (!ready) {
+      ready = await centralSyncManager.checkSessionReady(sessionId);
+      await sleepAsync(100);
+    }
+  };
+
+  beforeAll(async () => {
+    ctx = await createTestContext();
+    ({ models } = ctx.store);
+  });
+
+  beforeEach(async () => {
+    await models.LocalSystemFact.set(FACT_CURRENT_SYNC_TICK, DEFAULT_CURRENT_SYNC_TIME_VALUE);
+    await models.SyncLookupTick.truncate({ force: true });
+    await models.SyncDeviceTick.truncate({ force: true });
+    await models.Facility.truncate({ cascade: true, force: true });
+    await models.Program.truncate({ cascade: true, force: true });
+    await models.Survey.truncate({ cascade: true, force: true });
+    await models.ProgramDataElement.truncate({ cascade: true, force: true });
+    await models.SurveyScreenComponent.truncate({ cascade: true, force: true });
+    await models.ReferenceData.truncate({ cascade: true, force: true });
+    await models.User.truncate({ cascade: true, force: true });
+    await models.User.create({
+      id: SYSTEM_USER_UUID,
+      email: 'system',
+      displayName: 'System',
+      role: 'system',
+    });
+    await models.Setting.set('audit.changes.enabled', false);
+    await models.LocalSystemFact.set(FACT_LOOKUP_UP_TO_TICK, null);
+    await models.SyncLookup.truncate({ force: true });
+    await models.DebugLog.truncate({ force: true });
+  });
+
+  afterAll(() => ctx.close());
+
+  describe('endSession', () => {
+    it('set completedAt when ending an existing session', async () => {
+      const centralSyncManager = initializeCentralSyncManager();
+      const { sessionId } = await centralSyncManager.startSession();
+      await waitForSession(centralSyncManager, sessionId);
+
+      await centralSyncManager.endSession(sessionId);
+      const syncSession2 = await models.SyncSession.findOne({ where: { id: sessionId } });
+      expect(syncSession2.completedAt).not.toBeUndefined();
+    });
+
+    it('throws an error when connecting to a session that already ended', async () => {
+      const centralSyncManager = initializeCentralSyncManager();
+      const { sessionId } = await centralSyncManager.startSession();
+      await waitForSession(centralSyncManager, sessionId);
+
+      await centralSyncManager.endSession(sessionId);
+      await expect(centralSyncManager.connectToSession(sessionId)).rejects.toThrow();
+    });
+  });
+});
--- packages/central-server/__tests__/sync/CentralSyncManager.syncDeviceTick.test.js
+++ packages/central-server/__tests__/sync/CentralSyncManager.syncDeviceTick.test.js
@@ -1,6 +1,6 @@
 import { FACT_CURRENT_SYNC_TICK, FACT_LOOKUP_UP_TO_TICK } from '@tamanu/constants/facts';
 import { SYNC_SESSION_DIRECTION } from '@tamanu/database/sync';
-import { fake, fakeUser } from '@tamanu/fake-data/fake';
+import { fake } from '@tamanu/fake-data/fake';
 
 import { sleepAsync } from '@tamanu/utils/sleepAsync';
 import { SYSTEM_USER_UUID } from '@tamanu/constants';
@@ -13,7 +13,6 @@ describe('CentralSyncManager', () => {
   let sequelize;
 
   const DEFAULT_CURRENT_SYNC_TIME_VALUE = 2;
-  const DEFAULT_MAX_RECORDS_PER_SNAPSHOT_CHUNKS = 100000000;
   const DEFAULT_CONFIG = {
     sync: {
       lookupTable: {
@@ -222,190 +221,6 @@ describe('CentralSyncManager', () => {
       expect(patient.updatedAtSyncTick).not.toBe(concurrentEdit.updatedAtSyncTick);
       const syncDeviceTick = await models.SyncDeviceTick.findByPk(patient.updatedAtSyncTick);
       expect(syncDeviceTick.deviceId).toBe(facility.id);
-    });
-  });
-
-  describe('addIncomingChanges', () => {
-    beforeEach(async () => {
-      jest.resetModules();
-    });
-
-    it('does not record audit changelogs during incoming sync from facility server', async () => {
-      await models.LocalSystemFact.set(FACT_CURRENT_SYNC_TICK, '16');
-      await models.Setting.set('audit.changes.enabled', true);
-      const facility = await models.Facility.create(fake(models.Facility));
-      const patient = await models.Patient.create(fake(models.Patient));
-      const program = await models.Program.create(fake(models.Program));
-      const clinician = await models.User.create(fakeUser());
-
-      const programRegistry = await models.ProgramRegistry.create({
-        ...fake(models.ProgramRegistry),
-        programId: program.id,
-      });
-
-      const patientProgramRegistrationData = fake(models.PatientProgramRegistration, {
-        programRegistryId: programRegistry.id,
-        clinicianId: clinician.id,
-        patientId: patient.id,
-        facilityId: facility.id,
-      });
-      const changes = [
-        {
-          direction: SYNC_SESSION_DIRECTION.OUTGOING,
-          isDeleted: false,
-          recordType: 'patient_program_registrations',
-          recordId: patientProgramRegistrationData.id,
-          data: patientProgramRegistrationData,
-        },
-      ];
-
-      const centralSyncManager = initializeCentralSyncManager({
-        sync: {
-          lookupTable: {
-            enabled: true,
-          },
-          maxRecordsPerSnapshotChunk: DEFAULT_MAX_RECORDS_PER_SNAPSHOT_CHUNKS,
-        },
-      });
-      await centralSyncManager.updateLookupTable();
-      const { sessionId } = await centralSyncManager.startSession();
-      await waitForSession(centralSyncManager, sessionId);
-
-      await centralSyncManager.setupSnapshotForPull(
-        sessionId,
-        {
-          since: 1,
-          facilityIds: [facility.id],
-        },
-        () => true,
-      );
-
-      await centralSyncManager.addIncomingChanges(sessionId, changes);
-      await centralSyncManager.completePush(sessionId, facility.id, [
-        'patient_program_registrations',
-      ]);
-      await waitForPushCompleted(centralSyncManager, sessionId);
-
-      const changelogRecords = await sequelize.query(
-        `SELECT * FROM logs.changes WHERE record_id = :recordId;`,
-        {
-          type: sequelize.QueryTypes.SELECT,
-          replacements: {
-            recordId: patientProgramRegistrationData.id,
-          },
-        },
-      );
-
-      expect(changelogRecords).toHaveLength(0);
-    });
-
-    it('records audit changelogs during incoming sync from mobile', async () => {
-      await models.LocalSystemFact.set(FACT_CURRENT_SYNC_TICK, '17');
-      await models.Setting.set('audit.changes.enabled', true);
-      const facility = await models.Facility.create(fake(models.Facility));
-      const patient = await models.Patient.create(fake(models.Patient));
-      const program = await models.Program.create(fake(models.Program));
-      const clinician = await models.User.create(fakeUser());
-
-      const programRegistry = await models.ProgramRegistry.create({
-        ...fake(models.ProgramRegistry),
-        programId: program.id,
-      });
-
-      const patientProgramRegistrationData = fake(models.PatientProgramRegistration, {
-        programRegistryId: programRegistry.id,
-        clinicianId: clinician.id,
-        patientId: patient.id,
-        facilityId: facility.id,
-      });
-      const changes = [
-        {
-          direction: SYNC_SESSION_DIRECTION.OUTGOING,
-          isDeleted: false,
-          recordType: 'patient_program_registrations',
-          recordId: patientProgramRegistrationData.id,
-          data: patientProgramRegistrationData,
-        },
-      ];
-
-      const centralSyncManager = initializeCentralSyncManager({
-        sync: {
-          lookupTable: {
-            enabled: true,
-          },
-          maxRecordsPerSnapshotChunk: DEFAULT_MAX_RECORDS_PER_SNAPSHOT_CHUNKS,
-        },
-      });
-      await centralSyncManager.updateLookupTable();
-      const { sessionId } = await centralSyncManager.startSession({ isMobile: true });
-      await waitForSession(centralSyncManager, sessionId);
-
-      await centralSyncManager.addIncomingChanges(sessionId, changes);
-      await centralSyncManager.completePush(sessionId, facility.id, [
-        'patient_program_registrations',
-      ]);
-      await waitForPushCompleted(centralSyncManager, sessionId);
-
-      const changelogRecords = await sequelize.query(
-        `SELECT * FROM logs.changes WHERE record_id = :recordId;`,
-        {
-          type: sequelize.QueryTypes.SELECT,
-          replacements: {
-            recordId: patientProgramRegistrationData.id,
-          },
-        },
-      );
-
-      expect(changelogRecords).toHaveLength(1);
-      expect(changelogRecords[0]).toMatchObject(
-        expect.objectContaining({
-          table_name: 'patient_program_registrations',
-          record_id: patientProgramRegistrationData.id,
-          record_data: expect.objectContaining({
-            program_registry_id: programRegistry.id,
-            clinician_id: clinician.id,
-            patient_id: patient.id,
-            facility_id: facility.id,
-          }),
-        }),
-      );
-    });
-
-    it('inserts incoming changes into snapshots', async () => {
-      const patient1 = await models.Patient.create(fake(models.Patient));
-      const patient2 = await models.Patient.create(fake(models.Patient));
-      const changes = [patient1, patient2].map(r => ({
-        direction: SYNC_SESSION_DIRECTION.OUTGOING,
-        isDeleted: !!r.deletedAt,
-        recordType: 'patients',
-        recordId: r.id,
-        data: r.dataValues,
-      }));
-
-      jest.doMock('@tamanu/database/sync', () => ({
-        ...jest.requireActual('@tamanu/database/sync'),
-        insertSnapshotRecords: jest.fn(),
-      }));
-
-      const centralSyncManager = initializeCentralSyncManager();
-
-      const { insertSnapshotRecords } = require('@tamanu/database/sync');
-      const { sessionId } = await centralSyncManager.startSession();
-      await waitForSession(centralSyncManager, sessionId);
-
-      await centralSyncManager.addIncomingChanges(sessionId, changes);
-      const incomingChanges = changes.map(c => ({
-        ...c,
-        direction: SYNC_SESSION_DIRECTION.INCOMING,
-        updatedAtByFieldSum: null,
-      }));
-
-      expect(insertSnapshotRecords).toBeCalledTimes(1);
-      expect(insertSnapshotRecords).toBeCalledWith(
-        ctx.store.sequelize,
-        sessionId,
-        expect.arrayContaining(incomingChanges),
-      );
     });
   });
 });
