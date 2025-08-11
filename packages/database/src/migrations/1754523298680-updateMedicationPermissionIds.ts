fe9baafe7fb469f8ea93d10d6ccbde30af11a14a
Chris Tran <chris@beyondessential.com.au>
2025-08-11 12:37:34 +1000

    leave down migration empty

--- packages/database/src/migrations/1754523298680-updateMedicationPermissionIds.ts
+++ packages/database/src/migrations/1754523298680-updateMedicationPermissionIds.ts
@@ -12,13 +12,19 @@ export async function up(query: QueryInterface): Promise<void> {
 }
 
 export async function down(query: QueryInterface): Promise<void> {
-  await query.sequelize.query(
-    `
-      UPDATE permissions
-      SET id = REPLACE(id, 'medication', 'encountermedication')
-      WHERE id LIKE '%medication%' 
-        AND id NOT LIKE '%encountermedication%'
-        AND noun = 'Medication';
-    `,
-  );
+  // It's not possible to reverse the migration to its original state,
+  //
+  // An edge case:
+  // practitioner.medication.read.any (noun = 'Medication')
+  // admin.encountermedication.write.any (noun = 'Medication')
+  //
+  // After up:
+  // practitioner.medication.read.any (unchanged - doesn't contain 'encountermedication')
+  // admin.medication.write.any (changed from 'encountermedication' -> 'medication')
+  //
+  // After down:
+  // practitioner.encountermedication.read.any (WRONG! This was never 'encountermedication')
+  // admin.encountermedication.write.any (correct reversal)
+  //
+  // This is an edge case that we can't reverse. So leaving down migration empty.
 }
