e4a8286b296653ef203ba67d311899c697cb3b00
namhoangbes <nam@bes.au>
2025-08-11 09:26:00 +0700

    feat: add API to find locations by facility

--- packages/central-server/app/admin/adminRoutes.js
+++ packages/central-server/app/admin/adminRoutes.js
@@ -18,6 +18,7 @@ import { syncLastCompleted } from './sync';
 import { translationRouter } from './translation';
 import { usersRouter } from './users';
 import { locationAssignmentsRouter } from './locationAssignments';
+import { locationRouter } from './location';
 
 export const adminRoutes = express.Router();
 adminRoutes.use(ensurePermissionCheck);
@@ -70,6 +71,7 @@ adminRoutes.use('/template', templateRoutes);
 adminRoutes.use('/asset', assetRoutes);
 adminRoutes.use('/users', usersRouter);
 adminRoutes.use('/location-assignments', locationAssignmentsRouter);
+adminRoutes.use('/location', locationRouter);
 
 // These settings endpoints are setup for viewing and saving the settings in the JSON editor in the admin panel
 adminRoutes.get(
---
+++ packages/central-server/app/admin/location.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+import { VISIBILITY_STATUSES } from '@tamanu/constants';
+import { Op } from 'sequelize';
+
+export const locationRouter = express.Router();
+
+locationRouter.get(
+  '/$',
+  asyncHandler(async (req, res) => {
+    req.checkPermission('list', 'Location');
+    const {
+      models: { LocationGroup },
+      query: { bookableOnly = false, locationGroupIds },
+    } = req;
+    const { facilityId } = req.query;
+    const locations = await req.models.Location.findAll({
+      where: {
+        facilityId,
+        visibilityStatus: VISIBILITY_STATUSES.CURRENT,
+      },
+      include: [
+        {
+          required: true,
+          model: LocationGroup,
+          as: 'locationGroup',
+          where: {
+            visibilityStatus: VISIBILITY_STATUSES.CURRENT,
+            ...(bookableOnly ? { isBookable: true } : null),
+            ...(locationGroupIds ? { id: { [Op.in]: locationGroupIds } } : null),
+          },
+        },
+      ],
+      order: [
+        ['locationGroup', 'name', 'ASC'],
+        ['name', 'ASC'],
+      ],
+    });
+    res.send(locations);
+  }),
+);
