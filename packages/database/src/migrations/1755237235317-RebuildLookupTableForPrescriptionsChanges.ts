908b60ec4b3b44b8dc44f094656f3be752c9a88b
Rohan Port <rohan@bes.au>
2025-08-15 17:15:29 +1000

    fix(sync): TAMOC-331: Removed update to encounter_prescription updated_at_sync_ticks

--- packages/database/src/migrations/1755237235317-RebuildLookupTableForPrescriptionsChanges.ts
+++ packages/database/src/migrations/1755237235317-RebuildLookupTableForPrescriptionsChanges.ts
@@ -6,17 +6,6 @@ export async function up(query: QueryInterface): Promise<void> {
     WHERE record_type = 'encounter_medications';
   `);
 
-  // Set the updated_at_sync_tick to the greatest of the encounter, encounter_prescription, and prescription updated_at_sync_tick
-  // They had been initially set to 0, so just ensuring that they don't get missed
-  await query.sequelize.query(`
-    UPDATE encounter_prescriptions
-    SET updated_at_sync_tick = GREATEST(ep.updated_at_sync_tick, e.updated_at_sync_tick, p.updated_at_sync_tick)
-    FROM encounter_prescriptions ep 
-    JOIN prescriptions p ON p.id = ep.prescription_id
-    JOIN encounters e ON e.id = ep.encounter_id
-    WHERE encounter_prescriptions.id = ep.id
-  `);
-
   await query.sequelize.query(`
     SELECT flag_lookup_model_to_rebuild('prescriptions');
     SELECT flag_lookup_model_to_rebuild('encounter_prescriptions');
