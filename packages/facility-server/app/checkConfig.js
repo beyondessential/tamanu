43f5f87983bc17f9f2b1dd90bbd39739d48296ba
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-08-18 12:07:37 +1200

    Revert "Update checkConfig.js"
    This reverts commit 3741402521596fffbc5b67095732805e09cd0ba8.

--- packages/facility-server/app/checkConfig.js
+++ packages/facility-server/app/checkConfig.js
@@ -3,26 +3,16 @@ import { log } from '@tamanu/shared/services/logging';
 import { selectFacilityIds } from '@tamanu/utils/selectFacilityIds';
 
 export async function checkConfig({ settings, models }) {
-  const ensureSurveyDefaultExists = async (modelName, code) => {
+  const ensureExists = async (modelName, code) => {
     const found = await models[modelName].findOne({ where: { code } });
     if (!found) {
       log.error(`Default survey ${modelName} with code ${code} could not be found`);
     }
   };
 
-  const { enabled, reportIds } = await settings.central.get('integrations.dhis2');
-  const found = await models.Report.findAll({ where: { id: reportIds } });
-  if (enabled && found.length !== reportIds.length) {
-    const missing = reportIds.filter(id => !found.some(r => r.id === id));
-    log.error(`Reports ${missing} could not be found`);
-  }
-
   const facilityIds = selectFacilityIds(config);
   for (const facilityId of facilityIds) {
     const { department, location } = await settings[facilityId].get('survey.defaultCodes');
-    await Promise.all([
-      ensureSurveyDefaultExists('Department', department),
-      ensureSurveyDefaultExists('Location', location),
-    ]);
+    await Promise.all([ensureExists('Department', department), ensureExists('Location', location)]);
   }
 }
