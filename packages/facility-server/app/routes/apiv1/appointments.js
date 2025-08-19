ae1d1e0e36cf444334747914438ba2249d152935
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-08-19 14:52:03 +1200

    Update appointments.js

--- packages/facility-server/app/routes/apiv1/appointments.js
+++ packages/facility-server/app/routes/apiv1/appointments.js
@@ -372,8 +372,6 @@ appointments.post(
 
         if (conflictCount > 0) throw new ResourceConflictError();
 
-        // Create PatientFacility record to mark patient for sync on this facility
-        // Location booking always has a locationId
         const location = await Location.findByPk(locationId, { transaction });
         if (!location) throw new NotFoundError('Location not found');
 
