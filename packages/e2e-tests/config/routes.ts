c4760a5c8e575ed5ee94fb34684aea82b3f73232
Flynn <108244616+flynncs@users.noreply.github.com>
2025-08-19 15:24:43 +1200

    feat(synthetic): Synthetic test environment (#7851)

--- packages/e2e-tests/config/routes.ts
+++ packages/e2e-tests/config/routes.ts
@@ -1,9 +1,9 @@
 export const routes = {
   login: '/',
-  dashboard: '#/dashboard',
+  dashboard: '/#/dashboard',
   patients: {
-    all: '#/patients/all',
-    patientDetails: '#/patients/all/*',
+    all: '/#/patients/all',
+    patientDetails: '/#/patients/all/*',
   },
 };
 
