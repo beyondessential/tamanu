6b4e01ff0235f3e4ccf6c3c4465d8d2d914156f4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-08-18 20:26:30 -0400

    Create sync devices table migration

---
+++ packages/database/src/migrations/1755562640175-createSyncDevicesTable.ts
@@ -0,0 +1,46 @@
+import { DataTypes, QueryInterface, Sequelize } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  await query.createTable('sync_devices', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: Sequelize.fn('gen_random_uuid'),
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.fn('now'),
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.fn('now'),
+    },
+    deleted_at: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    device_id: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    authorised_by_user_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+    },
+  });
+
+  await query.addIndex('sync_devices', {
+    fields: ['device_id'],
+  });
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  await query.dropTable('sync_devices');
+}
