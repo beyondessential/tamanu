b2cc4fc2aa07d0c50d280c34e942ba350e74e76b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-08-18 20:34:19 -0400

    Create sync device model file

---
+++ packages/database/src/models/SyncDevice.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import { SYNC_DIRECTIONS } from '@tamanu/constants';
+import { Model } from './Model';
+import type { InitOptions, Models } from '../types/model';
+
+export class SyncDevice extends Model {
+  declare id: string;
+  declare deviceId: string;
+  declare authorisedById: string;
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  static initModel({ primaryKey, ...options }: InitOptions) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.fn('gen_random_uuid'),
+        },
+        deviceId: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        ...options,
+        syncDirection: SYNC_DIRECTIONS.DO_NOT_SYNC,
+      },
+    );
+  }
+
+  static initRelations(models: Models) {
+    this.belongsTo(models.User, {
+      foreignKey: 'authorisedById',
+      as: 'authorisedBy',
+    });
+  }
+}
