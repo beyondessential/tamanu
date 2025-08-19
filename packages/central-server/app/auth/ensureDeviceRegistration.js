d80a14b9df4c58209eb08238065b5236475d54b5
Félix Saparelli <felix@bes.au>
2025-08-19 17:11:22 +1200

    prevent another race condition

--- packages/central-server/app/auth/ensureDeviceRegistration.js
+++ packages/central-server/app/auth/ensureDeviceRegistration.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DEVICE_REGISTRATION_QUOTA_EXCEEDED_ERROR, SETTING_KEYS } from '@tamanu/constants';
 import { BadAuthenticationError } from '@tamanu/shared/errors';
 
-export async function ensureDeviceRegistration(models, settings, user, deviceId) {
+export async function ensureDeviceRegistration({ SyncDevice }, settings, user, deviceId) {
   const deviceRegistrationQuotaEnabled = await settings.get(
     SETTING_KEYS.FEATURES_DEVICE_REGISTRATION_QUOTA_ENABLED,
   );
@@ -11,34 +11,36 @@ export async function ensureDeviceRegistration(models, settings, user, deviceId)
     return;
   }
 
-  const syncDevice = await models.SyncDevice.findOne({
-    where: {
-      deviceId,
-    },
-  });
-
-  if (syncDevice) {
-    await syncDevice.markSeen();
-    return;
-  }
-
-  // Avoid race conditions by using a transaction
-  await models.SyncDevice.sequelize.transaction(
+  // There's two race conditions we seek to avoid with this transaction:
+  //
+  // 1. If the device logs in to the server twice simultaneously, we want
+  //    to avoid two SyncDevice records being created. This is also mitigated
+  //    by the UNIQUE constraint on the table.
+  //
+  // 2. If two unknown devices try to register at the same time, and the
+  //    quota is reached on the first one, the second should not be able to.
+  await SyncDevice.sequelize.transaction(
     {
       isolationLevel: Sequelize.Transaction.ISOLATION_LEVELS.REPEATABLE_READ,
     },
     async () => {
-      // If 2 unknown devices try to register at the same time,
-      // and the quota is reached on the first one,
-      // the second one should not be able to register.
+      const syncDevice = await SyncDevice.findOne({
+        where: {
+          deviceId,
+        },
+      });
 
-      // Number of devices already registered by this user
-      const currentCount = await models.SyncDevice.getCountByUserId(user.id);
+      if (syncDevice) {
+        await syncDevice.markSeen();
+        return;
+      }
+
+      const currentCount = await SyncDevice.getCountByUserId(user.id);
       if (currentCount + 1 > user.deviceRegistrationQuota) {
         throw new BadAuthenticationError(DEVICE_REGISTRATION_QUOTA_EXCEEDED_ERROR);
       }
 
-      await models.SyncDevice.create({
+      await SyncDevice.create({
         deviceId,
         registeredById: user.id,
       });
