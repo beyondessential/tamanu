12921339963c27561d45ff265f39a6dc32efc268
Tom Caiger <caigertom@gmail.com>
2025-08-25 11:11:49 +1200

    login backend

---
+++ packages/database/src/models/PortalOneTimeToken.ts
@@ -0,0 +1,41 @@
+import { DataTypes } from 'sequelize';
+import { isBefore, parseISO } from 'date-fns';
+import { SYNC_DIRECTIONS } from '@tamanu/constants';
+import { Model } from './Model';
+import { dateTimeType, type InitOptions, type Models } from '../types/model';
+
+export class PortalOneTimeToken extends Model {
+  declare id: string;
+  declare type: string;
+  declare token: string;
+  declare expiresAt: string;
+  declare portalUserId: string;
+
+  static initModel({ primaryKey, ...options }: InitOptions) {
+    super.init(
+      {
+        id: primaryKey,
+        type: { type: DataTypes.ENUM('login', 'register'), allowNull: false },
+        token: { type: DataTypes.STRING, allowNull: false, defaultValue: 'login' },
+        expiresAt: dateTimeType('expiresAt', {
+          allowNull: false,
+        }),
+      },
+      { ...options, syncDirection: SYNC_DIRECTIONS.DO_NOT_SYNC },
+    );
+  }
+
+  isExpired() {
+    return isBefore(parseISO(this.expiresAt), new Date());
+  }
+
+  static initRelations(models: Models) {
+    this.belongsTo(models.PortalUser, {
+      foreignKey: {
+        name: 'portalUserId',
+        allowNull: false,
+      },
+      as: 'portalUser',
+    });
+  }
+}
