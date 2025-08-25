8fb7b31b7168ee953b18946743a99383be8ae4fd
Tom Caiger <caigertom@gmail.com>
2025-08-25 16:27:08 +1200

    fix merge

--- packages/web/app/forms/ChartForm.jsx
+++ packages/web/app/forms/ChartForm.jsx
@@ -11,47 +11,47 @@ import { Modal } from '../components/Modal';
 import { ErrorMessage } from '../components/ErrorMessage';
 import { useAuth } from '../contexts/Auth';
 import { TranslatedText } from '../components/Translation/TranslatedText';
-import { useChartSurveyQuery } from '../api/queries/useChartSurveyQuery';
+import { useSurveyQuery } from '../api/queries/useSurveyQuery';
 import { getFormInitialValues, getValidationSchema } from '../utils';
 import { usePatientAdditionalDataQuery } from '../api/queries';
 import { combineQueries } from '../api';
 import { useTranslation } from '../contexts/Translation';
 
-export const ChartForm = React.memo(({
-  patient,
-  onSubmit,
-  onClose,
-  chartSurveyId,
-  editedObject = {},
-  confirmText = (
-    <TranslatedText
-      stringId="general.action.record"
-      fallback="Record"
-      data-testid="translatedtext-6dm3"
-    />
-  ),
-}) => {
-  const { currentUser } = useAuth();
-  const { getTranslation } = useTranslation();
-  const chartSurveyQuery = useChartSurveyQuery(chartSurveyId);
-  const patientAdditionalDataQuery = usePatientAdditionalDataQuery(patient?.id);
-  const {
-    data: [chartSurveyData, patientAdditionalData],
-    isLoading,
-    isError,
-    error,
-  } = combineQueries([chartSurveyQuery, patientAdditionalDataQuery]);
+export const ChartForm = React.memo(
+  ({
+    patient,
+    onSubmit,
+    onClose,
+    chartSurveyId,
+    editedObject = {},
+    confirmText = (
+      <TranslatedText
+        stringId="general.action.record"
+        fallback="Record"
+        data-testid="translatedtext-6dm3"
+      />
+    ),
+  }) => {
+    const { currentUser } = useAuth();
+    const { getTranslation } = useTranslation();
+    const chartSurveyQuery = useSurveyQuery(chartSurveyId);
+    const patientAdditionalDataQuery = usePatientAdditionalDataQuery(patient?.id);
+    const {
+      data: [chartSurveyData, patientAdditionalData],
+      isLoading,
+      isError,
+      error,
+    } = combineQueries([chartSurveyQuery, patientAdditionalDataQuery]);
 
-  const { components = [] } = chartSurveyData || {};
-  const visibleComponents = components.filter(
-    c => c.visibilityStatus === VISIBILITY_STATUSES.CURRENT,
-  );
+    const { components = [] } = chartSurveyData || {};
+    const visibleComponents = components.filter(
+      c => c.visibilityStatus === VISIBILITY_STATUSES.CURRENT,
+    );
 
-  const { ability } = useAuth();
-  const canCreateChart = ability.can('create', subject('Charting', { id: chartSurveyId }));
+    const { ability } = useAuth();
+    const canCreateChart = ability.can('create', subject('Charting', { id: chartSurveyId }));
 
-  const initialValues = useMemo(
-    () => {
+    const initialValues = useMemo(() => {
       const formInitialValues = getFormInitialValues(
         visibleComponents,
         patient,
@@ -59,75 +59,74 @@ export const ChartForm = React.memo(({
         currentUser,
       );
       return { ...formInitialValues, ...editedObject };
-    },
-    [visibleComponents, patient, patientAdditionalData, currentUser, editedObject],
-  );
-  const validationSchema = useMemo(
-    () => getValidationSchema(chartSurveyData, getTranslation),
-    [chartSurveyData, getTranslation],
-  );
+    }, [visibleComponents, patient, patientAdditionalData, currentUser, editedObject]);
+    const validationSchema = useMemo(() => getValidationSchema(chartSurveyData, getTranslation), [
+      chartSurveyData,
+      getTranslation,
+    ]);
 
-  if (isLoading) {
-    return <ModalLoader data-testid="modalloader-wncd" />;
-  }
+    if (isLoading) {
+      return <ModalLoader data-testid="modalloader-wncd" />;
+    }
 
-  if (!canCreateChart) {
-    return (
-      <Modal title="Permission required" open onClose={onClose} data-testid="modal-inaz">
-        <ForbiddenErrorModalContents
-          onConfirm={onClose}
-          confirmText="Close"
-          data-testid="forbiddenerrormodalcontents-nafx"
+    if (!canCreateChart) {
+      return (
+        <Modal title="Permission required" open onClose={onClose} data-testid="modal-inaz">
+          <ForbiddenErrorModalContents
+            onConfirm={onClose}
+            confirmText="Close"
+            data-testid="forbiddenerrormodalcontents-nafx"
+          />
+        </Modal>
+      );
+    }
+
+    if (isError) {
+      return (
+        <ErrorMessage
+          title="Error: Cannot load chart form"
+          errorMessage="Please contact an administrator to ensure the Chart form is configured correctly."
+          error={error}
+          data-testid="errormessage-k6hd"
         />
-      </Modal>
-    );
-  }
+      );
+    }
 
-  if (isError) {
+    const handleSubmit = async data => onSubmit({ survey: chartSurveyData, ...data });
+
     return (
-      <ErrorMessage
-        title="Error: Cannot load chart form"
-        errorMessage="Please contact an administrator to ensure the Chart form is configured correctly."
-        error={error}
-        data-testid="errormessage-k6hd"
+      <Form
+        onSubmit={handleSubmit}
+        showInlineErrorsOnly
+        validateOnChange
+        validateOnBlur
+        validationSchema={validationSchema}
+        initialValues={initialValues}
+        render={({ submitForm, values, setFieldValue }) => (
+          <>
+            <SurveyScreen
+              allComponents={visibleComponents}
+              patient={patient}
+              cols={2}
+              values={values}
+              setFieldValue={setFieldValue}
+              submitButton={
+                <FormSubmitCancelRow
+                  confirmText={confirmText}
+                  onConfirm={submitForm}
+                  onCancel={onClose}
+                  data-testid="formsubmitcancelrow-1ah9"
+                />
+              }
+              data-testid="surveyscreen-ek46"
+            />
+          </>
+        )}
+        data-testid="form-v82r"
       />
     );
-  }
-
-  const handleSubmit = async data => onSubmit({ survey: chartSurveyData, ...data });
-
-  return (
-    <Form
-      onSubmit={handleSubmit}
-      showInlineErrorsOnly
-      validateOnChange
-      validateOnBlur
-      validationSchema={validationSchema}
-      initialValues={initialValues}
-      render={({ submitForm, values, setFieldValue }) => (
-        <>
-          <SurveyScreen
-            allComponents={visibleComponents}
-            patient={patient}
-            cols={2}
-            values={values}
-            setFieldValue={setFieldValue}
-            submitButton={
-              <FormSubmitCancelRow
-                confirmText={confirmText}
-                onConfirm={submitForm}
-                onCancel={onClose}
-                data-testid="formsubmitcancelrow-1ah9"
-              />
-            }
-            data-testid="surveyscreen-ek46"
-          />
-        </>
-      )}
-      data-testid="form-v82r"
-    />
-  );
-});
+  },
+);
 
 ChartForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
