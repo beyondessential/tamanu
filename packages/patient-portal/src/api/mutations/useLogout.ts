556ffe06dacdf224f9b48817fc699dbb10a7ecac
Tom Caiger <caigertom@gmail.com>
2025-08-25 15:20:00 +1200

    Update useLogout.ts

--- packages/patient-portal/src/api/mutations/useLogout.ts
+++ packages/patient-portal/src/api/mutations/useLogout.ts
@@ -1,9 +1,15 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useApi } from '../useApi';
 
 export const useLogout = () => {
   const api = useApi();
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: () => api.logout(),
+    onSuccess: () => {
+      // completely clear the react-query cache
+      queryClient.clear();
+    },
   });
 };
