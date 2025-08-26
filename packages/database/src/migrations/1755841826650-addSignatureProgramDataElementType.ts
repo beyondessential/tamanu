2c20f595778ff8779330ec9a178483c759fa6309
Edwin Monk-Fromont <edwin@beyondessential.com.au>
2025-08-26 17:09:03 +1200

    Fixes

--- packages/database/src/migrations/1755841826650-addSignatureProgramDataElementType.ts
+++ packages/database/src/migrations/1755841826650-addSignatureProgramDataElementType.ts
@@ -4,9 +4,7 @@ export async function up(query: QueryInterface): Promise<void> {
   // This migration can fail if enum_program_data_elements_type already includes Signature
   // so if it fails we can just ignore it
   try {
-    await query.sequelize.query(
-      "ALTER TYPE enum_program_data_elements_type ADD VALUE 'Signature'",
-    );
+    await query.sequelize.query("ALTER TYPE enum_program_data_elements_type ADD VALUE 'Signature'");
   } catch (e) {
     if (e instanceof DatabaseError) {
       if (e.message.match(`already exists`)) {
@@ -22,5 +20,4 @@ export async function down(): Promise<void> {
   // Note: PostgreSQL doesn't support removing enum values directly
   // This would require recreating the enum type which is complex and risky
   // For now, we'll leave the value in the enum
-  throw new Error('Cannot remove enum values in PostgreSQL without recreating the type');
 }
