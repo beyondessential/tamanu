b6a84a3ca112a3075538653b53e8e6117d9de482
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-08-29 10:58:05 +1200

    Update convertToDHIS2Format.js

--- packages/central-server/app/utils/convertToDHIS2Format.js
+++ packages/central-server/app/utils/convertToDHIS2Format.js
@@ -1,3 +1,34 @@
 export const convertToDHIS2Format = data => {
+  // Dates must be in YYYY-MM-DD format
+
+  // Example valid json for endpoint
+  // {
+  //   "dataSet": "dataSetID",
+  //   "completeDate": "date",
+  //   "period": "period",
+  //   "orgUnit": "orgUnitID",
+  //   "attributeOptionCombo": "aocID",
+  //   "dataValues": [
+  //     {
+  //       "dataElement": "dataElementID",
+  //       "categoryOptionCombo": "cocID",
+  //       "value": "1",
+  //       "comment": "comment1"
+  //     },
+  //     {
+  //       "dataElement": "dataElementID",
+  //       "categoryOptionCombo": "cocID",
+  //       "value": "2",
+  //       "comment": "comment2"
+  //     },
+  //     {
+  //       "dataElement": "dataElementID",
+  //       "categoryOptionCombo": "cocID",
+  //       "value": "3",
+  //       "comment": "comment3"
+  //     }
+  //   ]
+  // }
+
   return data;
 };
