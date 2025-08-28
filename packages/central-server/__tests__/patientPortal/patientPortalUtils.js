1633166ddc6057013e4301bc167fb6a212ab7276
Tom Caiger <caigertom@gmail.com>
2025-08-28 15:09:26 +1200

    Update patientPortalUtils.js

--- packages/central-server/__tests__/patientPortal/patientPortalUtils.js
+++ packages/central-server/__tests__/patientPortal/patientPortalUtils.js
@@ -6,6 +6,7 @@ export const getPatientAuthToken = async (app, models, email) => {
   const { token } = await oneTimeTokenService.createLoginToken(portalUser.id);
   const loginResponse = await app.post('/api/portal/login').send({
     loginToken: token,
+    email,
   });
   expect(loginResponse).toHaveSucceeded();
   return loginResponse.body.token;
