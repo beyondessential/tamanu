193d02528f9188329b5a87c0f171c695f168c1f4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-08-31 14:28:25 -0400

    Use bind in query
    Co-authored-by: Félix Saparelli <felix@beyondessential.com.au>

--- packages/database/src/migrations/1756167650873-populateDevicesTable.ts
+++ packages/database/src/migrations/1756167650873-populateDevicesTable.ts
@@ -1,11 +1,18 @@
 import { QueryInterface } from 'sequelize';
 import { SYSTEM_USER_UUID, DEVICE_SCOPES } from '@tamanu/constants';
 
+const INITIAL_DEVICE_SCOPES = [DEVICE_SCOPES.SYNC_CLIENT];
+
 export async function up(query: QueryInterface): Promise<void> {
   await query.sequelize.query(`
     INSERT INTO devices (id, last_seen_at, scopes, registered_by_id)
-    SELECT device_id, now(), '["${DEVICE_SCOPES.SYNC_CLIENT}"]'::jsonb, '${SYSTEM_USER_UUID}' FROM sync_device_ticks GROUP BY device_id;
-  `);
+    SELECT device_id, now(), $deviceScopes, $systemUserUuid FROM sync_device_ticks GROUP BY device_id;
+  `, {
+    bind: {
+      deviceScopes: JSON.stringify(INITIAL_DEVICE_SCOPES),
+      systemUserUuid: SYSTEM_USER_UUID,
+    },
+  });
 }
 
 export async function down(query: QueryInterface): Promise<void> {
