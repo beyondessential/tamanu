3cb9172740e6fcc54ae5119e9292795bee418ad8
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-02 12:06:15 +1200

    so pretty

--- packages/patient-portal/src/views/LoginSuccessView.tsx
+++ packages/patient-portal/src/views/LoginSuccessView.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CheckCircleIcon from '@mui/icons-material/CheckCircleOutlined';
 import styled from 'styled-components';
 
 const IconDisplay = styled('div')(({ theme }) => ({
-    padding: theme.spacing(2),
+    padding: theme.spacing(1),
     background: '#EDFAF3',
     borderRadius: '50%',
     width: 'fit-content',
     display: 'flex',
+    margin: '0 auto',
 }));
 
 export const LoginSuccessView = () => {
