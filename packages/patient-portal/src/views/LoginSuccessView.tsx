efbb0864beca5b2f6fb087d19467f41d006673fd
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-02 12:10:27 +1200

    anothewr one

--- packages/patient-portal/src/views/LoginSuccessView.tsx
+++ packages/patient-portal/src/views/LoginSuccessView.tsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import CheckCircleIcon from '@mui/icons-material/CheckCircleOutlined';
 import styled from 'styled-components';
+import { Typography } from '@mui/material';
 
 const IconDisplay = styled('div')(({ theme }) => ({
-    padding: theme.spacing(1),
-    background: '#EDFAF3',
-    borderRadius: '50%',
-    width: 'fit-content',
-    display: 'flex',
-    margin: '0 auto',
+  padding: theme.spacing(1),
+  background: '#EDFAF3',
+  borderRadius: '50%',
+  width: 'fit-content',
+  display: 'flex',
+  margin: '0 auto',
+  marginBottom: theme.spacing(2),
 }));
 
 export const LoginSuccessView = () => {
-  return <>
-    <IconDisplay>
-        <CheckCircleIcon color='success'/>
-    </IconDisplay>
-  </>;
+  return (
+    <>
+      <IconDisplay>
+        <CheckCircleIcon color="success" />
+      </IconDisplay>
+        <Typography variant="h2">Account successfully created</Typography>
+    </>
+  );
 };
