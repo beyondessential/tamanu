3384374c079d1e1f65944c17deea194df5d0ca62
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-02 12:00:05 +1200

    muffin man returns

--- packages/patient-portal/src/views/LoginSuccessView.tsx
+++ packages/patient-portal/src/views/LoginSuccessView.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import styled from 'styled-components';
+
+const CheckIcon = styled(CheckCircleIcon)(({ theme }) => ({
+    padding: theme.spacing(2),
+    background: '#EDFAF3',
+    borderRadius: '50%',
+    color: theme.colors.green
+}));
 
 export const LoginSuccessView = () => {
-  return <div>LoginSuccessView</div>;
+  return <>
+    <CheckIcon />
+  </>;
 };
