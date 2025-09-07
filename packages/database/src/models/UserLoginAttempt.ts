194ff77f31c5cdde856c58eb2d8cdcfc52fc9479
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-09-07 14:21:17 -0400

    Create user login attempt model

---
+++ packages/database/src/models/UserLoginAttempt.ts
@@ -0,0 +1,43 @@
+import { SYNC_DIRECTIONS } from '@tamanu/constants';
+import { Model } from './Model';
+import type { InitOptions, Models } from '../types/model';
+import { DataTypes, Sequelize } from 'sequelize';
+
+export class UserLoginAttempt extends Model {
+  declare id: string;
+  declare deviceId: string;
+  declare outcome: string;
+  declare userId: string;
+
+  static initModel(options: InitOptions) {
+    super.init(
+      {
+        id: {
+          type: DataTypes.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.fn('gen_random_uuid'),
+        },
+        deviceId: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        outcome: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        ...options,
+        syncDirection: SYNC_DIRECTIONS.PUSH_TO_CENTRAL,
+      } as any,
+    );
+  }
+
+  static initRelations(models: Models) {
+    this.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  }
+}
