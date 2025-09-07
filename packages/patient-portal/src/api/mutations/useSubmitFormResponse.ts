812d449fea944109b22971acf25488af42113c28
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-08 11:44:59 +1200

    just make it void for create

--- packages/patient-portal/src/api/mutations/useSubmitFormResponse.ts
+++ packages/patient-portal/src/api/mutations/useSubmitFormResponse.ts
@@ -1,18 +1,17 @@
 import { useMutation, UseMutationOptions, useQueryClient } from '@tanstack/react-query';
 import { useApi } from '../useApi';
 import { type PortalCreateSurveyResponseRequest } from '@tamanu/shared/schemas/patientPortal/requests/createSurveyResponse.schema';
-  
 
 export const useSubmitFormResponse = (
   designationId: string,
-  options?: UseMutationOptions<any, Error, PortalCreateSurveyResponseRequest>,
+  options?: UseMutationOptions<void, Error, PortalCreateSurveyResponseRequest>,
 ) => {
   const queryClient = useQueryClient();
   const api = useApi();
-  return useMutation<any, Error, PortalCreateSurveyResponseRequest>({
+  return useMutation<void, Error, PortalCreateSurveyResponseRequest>({
     mutationKey: ['submitFormResponse', designationId],
-    mutationFn: async (payload) => {
-      return api.post(`/me/forms/${designationId}`, payload as any);
+    mutationFn: async payload => {
+      await api.post(`/me/forms/${designationId}`, payload as any);
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['outstandingForms'] });
