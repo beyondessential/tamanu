1aaee697396f76c749ecbd6c0ca0a35cb87fff46
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-09-07 16:27:29 -0400

    Create some constants for login attempts

--- packages/constants/src/auth.ts
+++ packages/constants/src/auth.ts
@@ -6,3 +6,11 @@ export const JWT_TOKEN_TYPES = {
 export const SYSTEM_USER_UUID = '00000000-0000-0000-0000-000000000000';
 
 export const CAN_ACCESS_ALL_FACILITIES = 'ALL';
+
+export const LOGIN_ATTEMPT_OUTCOMES = {
+  SUCCEEDED: 'succeeded',
+  FAILED: 'failed',
+  LOCKED: 'locked',
+};
+
+export const LOCKED_OUT_ERROR_MESSAGE = 'User is locked out';
