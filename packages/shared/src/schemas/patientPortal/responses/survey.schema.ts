db51150fc1c6d37be0034ad2358bfa973c1a9618
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-08 13:14:55 +1200

    doing a rename refactor

--- packages/shared/src/schemas/patientPortal/responses/survey.schema.ts
+++ packages/shared/src/schemas/patientPortal/responses/survey.schema.ts
@@ -9,4 +9,48 @@ export const SurveySchema = z.object({
   surveyType: z.enum(SURVEY_TYPES),
 });
 
+export const ProgramDataElementSchema = z.object({
+  id: z.string(),
+  code: z.string().optional().nullable(),
+  name: z.string().optional().nullable(),
+  indicator: z.string().optional().nullable(),
+  defaultText: z.string().optional().nullable(),
+  defaultOptions: z.unknown().optional().nullable(),
+  visualisationConfig: z.unknown().optional().nullable(),
+  type: z.string().optional().nullable(),
+});
+
+export const SurveyScreenComponentSchema = z.object({
+  id: z.string(),
+  screenIndex: z.number().optional().nullable(),
+  componentIndex: z.number().optional().nullable(),
+  text: z.string().optional().nullable(),
+  visibilityCriteria: z.string().optional().nullable(),
+  validationCriteria: z.string().optional().nullable(),
+  detail: z.string().optional().nullable(),
+  config: z.string().optional().nullable(),
+  options: z
+    .array(
+      z.object({
+        label: z.string(),
+        value: z.any(),
+      }),
+    )
+    .optional()
+    .nullable(),
+  calculation: z.string().optional().nullable(),
+  visibilityStatus: z.string().optional().nullable(),
+  surveyId: z.string().optional().nullable(),
+  dataElementId: z.string().optional().nullable(),
+  dataElement: ProgramDataElementSchema.optional().nullable(),
+});
+
+export const FullSurveySchema = SurveySchema.extend({
+  components: z.array(SurveyScreenComponentSchema),
+});
+
 export type Survey = z.infer<typeof SurveySchema>;
+export type FullSurvey = z.infer<typeof FullSurveySchema>;
+export type ProgramDataElement = z.infer<typeof ProgramDataElementSchema>;
+export type SurveyScreenComponent = z.infer<typeof SurveyScreenComponentSchema>;
+
