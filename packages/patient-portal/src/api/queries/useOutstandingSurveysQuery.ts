d9e6042c2b66f35a315e58c8d861a749ee4c5eef
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-08 14:41:26 +1200

    remove all duplication on those queries

--- packages/patient-portal/src/api/queries/useOutstandingSurveysQuery.ts
+++ packages/patient-portal/src/api/queries/useOutstandingSurveysQuery.ts
@@ -1,16 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import {
-  PortalSurveyAssignmentSchema,
   type PortalSurveyAssignment,
 } from '@tamanu/shared/schemas/patientPortal/responses/portalSurveyAssignment.schema';
 import { useApi } from '../useApi';
-import { transformArray } from '@utils/transformData';
 
 export const useOutstandingSurveysQuery = () => {
   const api = useApi();
   return useQuery<unknown, Error, PortalSurveyAssignment[]>({
     queryKey: ['outstandingSurveys'],
     queryFn: () => api.get('/me/surveys'),
-    select: transformArray<PortalSurveyAssignment>(PortalSurveyAssignmentSchema),
   });
 };
