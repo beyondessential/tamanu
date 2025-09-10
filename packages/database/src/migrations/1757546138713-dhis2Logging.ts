4986209a9cf37af4d6302451e72cfdb129909f80
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-11 11:15:53 +1200

    Create 1757546138713-dhis2Logging.ts

---
+++ packages/database/src/migrations/1757546138713-dhis2Logging.ts
@@ -0,0 +1,9 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  // write your up migration here
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  // write your down migration here
+}
