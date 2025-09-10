e2c4cc0966ad44ae4b677fb619afe1c2c1f869f5
Félix Saparelli <felix@bes.au>
2025-09-10 14:00:36 +1200

    quieter

--- .github/scripts/scrape-translations.sh
+++ .github/scripts/scrape-translations.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 ttregex='stringId="([^"]*)"\s*?fallback="([^"]*)'
 gtregex="getTranslation\(\s*?[\"'](.*?)[\"'],.*?[\"'](.*?)[\"'].*?\)"
