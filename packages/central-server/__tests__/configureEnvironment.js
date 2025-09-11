e6a65788465f781ecc8f5c35d1db1d52aa763c1a
Félix Saparelli <felix@bes.au>
2025-09-11 16:23:21 +1200

    messagin

--- packages/central-server/__tests__/configureEnvironment.js
+++ packages/central-server/__tests__/configureEnvironment.js
@@ -50,7 +50,7 @@ expect.extend({
     const { statusCode } = response;
     const match = expected === statusCode;
     const pass = statusCode >= 400 && (statusCode < 500 || match);
-    let expectedText = 'Expected error status code';
+    let expectedText = 'Expected 4xx error status code';
     if (expected) {
       expectedText += ` ${expected}`;
     }
