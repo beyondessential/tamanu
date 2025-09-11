920243f79f01b5c0b7766f9eec2f64cf3baed994
Félix Saparelli <felix@bes.au>
2025-09-12 09:21:13 +1200

    fix map spread

--- packages/errors/src/Problem.ts
+++ packages/errors/src/Problem.ts
@@ -77,7 +77,7 @@ export class Problem extends Error {
 
   toJSON(): Record<string, any> {
     return {
-      ...this.extra.entries(),
+      ...Object.fromEntries(this.extra.entries()),
       type: isKnownErrorType(this.type)
         ? IANA_TYPES.includes(this.type)
           ? `${IANA}${this.type}`
