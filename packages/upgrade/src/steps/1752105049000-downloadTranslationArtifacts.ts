4f2639a9772e0a2a5b8da5fbd1e865f85fbe5f56
Félix Saparelli <felix@bes.au>
2025-09-12 11:22:11 +1200

    feat(db): import report-translations on upgrade

--- packages/upgrade/src/steps/1752105049000-downloadTranslationArtifacts.ts
+++ packages/upgrade/src/steps/1752105049000-downloadTranslationArtifacts.ts
@@ -17,6 +17,68 @@ interface Translation {
   fallback?: string; // legacy
 }
 
+async function apply(artifactType: string, { toVersion, models, log }: StepArgs) {
+  const url = `${(config as any).metaServer!.host!}/versions/${toVersion}/artifacts`;
+  log.info('Downloading translation artifacts', { version: toVersion, url });
+
+  const artifactsResponse = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  if (!artifactsResponse.ok) {
+    throw new Error(
+      `Failed to fetch artifacts list: ${artifactsResponse.status} ${artifactsResponse.statusText}`,
+    );
+  }
+
+  const artifacts = (await artifactsResponse.json()) as unknown as Artifact[];
+  const translationsArtifact = artifacts.find(
+    (artifact: any) => artifact.artifact_type === artifactType,
+  );
+  if (!translationsArtifact) {
+    throw new Error(`No ${artifactType} artifact found for version ${toVersion}`);
+  }
+
+  log.info(`Downloading ${artifactType} artifact`, {
+    url: translationsArtifact.download_url,
+  });
+  const translationsResponse = await fetch(translationsArtifact.download_url);
+  if (!translationsResponse.ok) {
+    throw new Error(
+      `Failed to download ${artifactType} artifact: ${translationsResponse.status} ${translationsResponse.statusText}`,
+    );
+  }
+
+  const translationRows = parse(await translationsResponse.text(), {
+    columns: true,
+    skip_empty_lines: true,
+  }) as unknown as Translation[];
+
+  if (translationRows.length === 0) {
+    throw new Error('No valid translation rows found in CSV');
+  }
+
+  log.info(`Importing ${artifactType}`, { count: translationRows.length });
+
+  if (translationRows.length > 0) {
+    await models.TranslatedString.sequelize.query(
+      `
+          INSERT INTO translated_strings (string_id, text, language)
+          VALUES ${translationRows.map(() => `(?, ?, '${DEFAULT_LANGUAGE_CODE}')`).join(', ')}
+          ON CONFLICT (string_id, language) DO UPDATE SET text = EXCLUDED.text
+        `,
+      {
+        replacements: translationRows.flatMap(row => {
+          const text = row[DEFAULT_LANGUAGE_CODE] ?? row[ENGLISH_LANGUAGE_CODE] ?? row.fallback;
+          return text ? [row.stringId, text] : [];
+        }),
+        type: QueryTypes.INSERT,
+      },
+    );
+  }
+}
+
 export const STEPS: Steps = [
   {
     at: END,
@@ -24,76 +86,43 @@ export const STEPS: Steps = [
       // Only run on central server
       return serverType === 'central';
     },
-    async run({ toVersion, models, log }: StepArgs) {
+    async run(args: StepArgs) {
+      const zeroPatch = args.toVersion.replace(/\.(\d+)$/, '.0');
+
       try {
-        const url = `${(config as any).metaServer!.host!}/versions/${toVersion}/artifacts`;
-        log.info('Downloading translation artifacts', { version: toVersion, url });
-
-        const artifactsResponse = await fetch(url, {
-          headers: {
-            Accept: 'application/json',
-          },
-        });
-        if (!artifactsResponse.ok) {
-          throw new Error(
-            `Failed to fetch artifacts list: ${artifactsResponse.status} ${artifactsResponse.statusText}`,
-          );
-        }
-
-        const artifacts = (await artifactsResponse.json()) as unknown as Artifact[];
-        const translationsArtifact = artifacts.find(
-          (artifact: any) => artifact.artifact_type === 'translations',
-        );
-        if (!translationsArtifact) {
-          throw new Error(`No translations artifact found for version ${toVersion}`);
-        }
-
-        log.info('Downloading translations artifact', {
-          url: translationsArtifact.download_url,
-        });
-        const translationsResponse = await fetch(translationsArtifact.download_url);
-        if (!translationsResponse.ok) {
-          throw new Error(
-            `Failed to download translations artifact: ${translationsResponse.status} ${translationsResponse.statusText}`,
-          );
-        }
-
-        const translationRows = parse(await translationsResponse.text(), {
-          columns: true,
-          skip_empty_lines: true,
-        }) as unknown as Translation[];
-
-        if (translationRows.length === 0) {
-          throw new Error('No valid translation rows found in CSV');
-        }
-
-        log.info('Importing new default translations', { count: translationRows.length });
-
-        if (translationRows.length > 0) {
-          await models.TranslatedString.sequelize.query(
-            `
-                INSERT INTO translated_strings (string_id, text, language)
-                VALUES ${translationRows.map(() => `(?, ?, '${DEFAULT_LANGUAGE_CODE}')`).join(', ')}
-                ON CONFLICT (string_id, language) DO UPDATE SET text = EXCLUDED.text
-              `,
+        await apply('translations', args);
+        args.log.info('Successfully imported default translations');
+      } catch (error) {
+        try {
+          args.log.info('Trying to import default translations from release head instead', {
+            version: zeroPatch,
+          });
+          await apply('translations', { ...args, toVersion: zeroPatch });
+          args.log.info('Successfully imported default translations', {
+            version: zeroPatch,
+          });
+        } catch (error) {
+          // Failing to import translations is not world-ending... for now
+          // We may want to make this more strict in the future
+          args.log.error(
+            'Failed to import default translations, you will need to manually import them',
             {
-              replacements: translationRows.flatMap(row => {
-                const text =
-                  row[DEFAULT_LANGUAGE_CODE] ?? row[ENGLISH_LANGUAGE_CODE] ?? row.fallback;
-                return text ? [row.stringId, text] : [];
-              }),
-              type: QueryTypes.INSERT,
+              error,
             },
           );
-
-          log.info('Successfully imported default translations');
         }
+      }
+
+      try {
+        await apply('report-translations', { ...args, toVersion: zeroPatch });
+        args.log.info('Successfully imported default report translations');
       } catch (error) {
-        // Failing to import translations is not world-ending... for now
-        // We may want to make this more strict in the future
-        log.error('Failed to import translations, you will need to manually import them', {
-          error,
-        });
+        args.log.error(
+          'Failed to import default report translations, you will need to manually import them',
+          {
+            error,
+          },
+        );
       }
     },
   },
