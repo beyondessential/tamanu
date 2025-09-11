88eda012430dc82823b484a24d2a3bb909ea0c3d
Tom Caiger <caigertom@gmail.com>
2025-09-11 19:48:28 +1200

    fetch survey

--- packages/central-server/app/patientPortal/handlers/survey.js
+++ packages/central-server/app/patientPortal/handlers/survey.js
@@ -1,55 +1,32 @@
-
 import asyncHandler from 'express-async-handler';
-import { log } from '@tamanu/shared/services/logging';
 import {
   SurveySchema,
   SurveyWithComponentsSchema,
 } from '@tamanu/shared/schemas/patientPortal/responses/survey.schema';
-import { PORTAL_SURVEY_ASSIGNMENTS_STATUSES } from '@tamanu/constants';
 import { getAttributesFromSchema } from '../../utils/schemaUtils';
 import { NotFoundError } from '@tamanu/shared/errors';
 
 export const getSurvey = asyncHandler(async (req, res) => {
-    const { patient, params } = req;
-    const { models } = req.store;
-    const { assignmentId } = params;
-  
-    const assignedSurvey = await models.PortalSurveyAssignment.findOne({
-      where: {
-        id: assignmentId,
-        patientId: patient.id,
-        status: PORTAL_SURVEY_ASSIGNMENTS_STATUSES.OUTSTANDING,
-      },
-    });
-  
-    if (!assignedSurvey) {
-      log.error('Patient attempted to fetch survey for invalid assigned survey', {
-        assignmentId,
-        patientId: patient.id,
-      });
-      throw new NotFoundError('Survey was not assigned to the patient');
-    }
-  
-    const surveyRecord = await models.Survey.findByPk(assignedSurvey.surveyId, {
-      attributes: getAttributesFromSchema(SurveySchema),
-    });
-  
-    if (!surveyRecord) {
-      log.error('Unexpected survey not found, assignment has invalid surveyId', {
-        assignmentId,
-        surveyId: assignedSurvey.surveyId,
-      });
-      throw new NotFoundError('Survey was not found');
-    }
-  
-    const components = await models.SurveyScreenComponent.getComponentsForSurvey(surveyRecord.id, {
-      includeAllVitals: true,
-    });
-  
-    const payload = {
-      ...surveyRecord.forResponse(),
-      components,
-    };
-  
-    return res.send(SurveyWithComponentsSchema.parse(payload));
+  const { params } = req;
+  const { models } = req.store;
+  const { surveyId } = params;
+
+  const surveyRecord = await models.Survey.findByPk(surveyId, {
+    attributes: getAttributesFromSchema(SurveySchema),
   });
+
+  if (!surveyRecord) {
+    throw new NotFoundError('Survey was not found');
+  }
+
+  const components = await models.SurveyScreenComponent.getComponentsForSurvey(surveyRecord.id, {
+    includeAllVitals: true,
+  });
+
+  const payload = {
+    ...surveyRecord.forResponse(),
+    components,
+  };
+
+  return res.send(SurveyWithComponentsSchema.parse(payload));
+});
--- packages/patient-portal/src/views/SurveyView.tsx
+++ packages/patient-portal/src/views/SurveyView.tsx
@@ -1,22 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 //@ts-nocheck
 import React from 'react';
-import { Card } from '@components/Card';
+import { styled } from '@mui/material/styles';
 import { useCurrentUser } from '@routes/PrivateRoute';
 import { SurveyForm } from '@features/survey/SurveyForm';
 import { ENCOUNTER_TYPES } from '@tamanu/constants';
+import { useSurveyQuery } from '@api/queries/useSurveyQuery';
+
+const Container = styled('div')(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  padding: 20,
+  borderRadius: 3,
+  width: 720,
+  maxWidth: '100%',
+  margin: '30px auto',
+}));
+
+const surveyId = 'program-demendoscopyscreen-demendoref';
 
 export const SurveyView = () => {
+  const { isPending, data: survey } = useSurveyQuery(surveyId);
   const patient = useCurrentUser();
   const currentUser = {};
-  const survey = {};
   const patientAdditionalData = {};
   const encounterType = ENCOUNTER_TYPES.CLINIC;
   console.log('patientAdditionalData?', patient);
 
+  const onSubmit = async () => {};
+
+  if (isPending) {
+    return 'loading...';
+  }
+
   return (
-    <Card sx={{ width: '425px' }}>
-      <SurveyForm patient={patient} currentUser={currentUser} survey={survey} />
-    </Card>
+    <Container>
+      <SurveyForm patient={patient} currentUser={currentUser} survey={survey} onSubmit={onSubmit} />
+    </Container>
   );
 };
