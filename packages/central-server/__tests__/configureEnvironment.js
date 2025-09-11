8df8f6df4e314a8e0401ce7b0bbf3c9752d3a039
Félix Saparelli <felix@bes.au>
2025-09-11 15:58:35 +1200

    why

--- packages/central-server/__tests__/configureEnvironment.js
+++ packages/central-server/__tests__/configureEnvironment.js
@@ -49,7 +49,7 @@ expect.extend({
   toHaveRequestError(response, expected) {
     const { statusCode } = response;
     const match = expected === statusCode;
-    const pass = statusCode >= 400 && statusCode !== 403 && (statusCode < 500 || match);
+    const pass = statusCode >= 400 && (statusCode < 500 || match);
     let expectedText = 'Expected error status code';
     if (expected) {
       expectedText += ` ${expected}`;
