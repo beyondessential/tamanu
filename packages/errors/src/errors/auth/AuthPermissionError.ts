5bb8c431402a56f61a1a4a0126c838e2b2d0777e
Félix Saparelli <felix@bes.au>
2025-09-12 00:56:07 +1200

    i guess we expect a 401 there
    let's keep the bugs in

--- packages/errors/src/errors/auth/AuthPermissionError.ts
+++ packages/errors/src/errors/auth/AuthPermissionError.ts
@@ -4,6 +4,6 @@ import { ERROR_TYPE } from '../../constants';
 export class AuthPermissionError extends BaseAuthenticationError {
   constructor(detail?: string) {
     super(ERROR_TYPE.AUTH_PERMISSION_REQUIRED, 'Lacks permission to authenticate', detail);
-    this.status = 403;
+    this.status = 401;
   }
 }
