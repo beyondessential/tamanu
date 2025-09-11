3402ab489a09b8f40cc110bfb53bafb2e7a6fc3c
Tom Caiger <caigertom@gmail.com>
2025-09-11 19:26:20 +1200

    contexts

---
+++ packages/patient-portal/src/contexts/TranslationContext.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+//@ts-nocheck
+// Todo: Setup translations
+import React from 'react';
+import { TranslationContext } from '@tamanu/ui-components';
+
+export const TranslationProvider = ({ children }) => {
+  return (
+    <TranslationContext.Provider
+      value={{
+        getTranslation: () => null,
+        getEnumTranslation: () => null,
+        updateStoredLanguage: () => null,
+        storedLanguage: '',
+        translations: {},
+      }}
+    >
+      {children}
+    </TranslationContext.Provider>
+  );
+};
--- packages/patient-portal/src/views/SurveyView.tsx
+++ packages/patient-portal/src/views/SurveyView.tsx
@@ -5,9 +5,6 @@ import { Card } from '@components/Card';
 import { useCurrentUser } from '@routes/PrivateRoute';
 import { SurveyForm } from '@features/survey/SurveyForm';
 import { ENCOUNTER_TYPES } from '@tamanu/constants';
-import { TranslationContext, SettingsContext } from '@tamanu/ui-components';
-
-const getTranslation = value => value;
 
 export const SurveyView = () => {
   const patient = useCurrentUser();
@@ -19,11 +16,7 @@ export const SurveyView = () => {
 
   return (
     <Card sx={{ width: '425px' }}>
-      <TranslationContext.Provider value={{ getTranslation }}>
-        <SettingsContext.Provider value={{}}>
-          <SurveyForm patient={patient} currentUser={currentUser} survey={survey} />
-        </SettingsContext.Provider>
-      </TranslationContext.Provider>
+      <SurveyForm patient={patient} currentUser={currentUser} survey={survey} />
     </Card>
   );
 };
