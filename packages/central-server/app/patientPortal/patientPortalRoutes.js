22b55465cd3f8f8ec8250a817f61ef278b05e3e6
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-15 11:03:52 +1200

    filter suggesters

--- packages/central-server/app/patientPortal/patientPortalRoutes.js
+++ packages/central-server/app/patientPortal/patientPortalRoutes.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import config from 'config';
+import { suggestions } from '@tamanu/shared/services/suggestions';
 import { authModule } from './auth';
 import { patientDataRoutes } from './patientDataRoutes';
 
@@ -12,3 +14,17 @@ patientPortalModule.use('/me', patientDataRoutes);
 
 patientPortalModule.use('/survey/:surveyId', getSurvey);
 patientPortalModule.use('/surveyResponse/:assignmentId', createSurveyResponse);
+
+const allowedSuggesters = config?.patientPortal?.suggesters?.allowList || [];
+if (Array.isArray(allowedSuggesters) && allowedSuggesters.length > 0) {
+  const allowListMiddleware = names => (req, res, next) => {
+    const firstSegment = (req.path || '').replace(/^\//, '').split('/')[0];
+    if (!names.includes(firstSegment)) {
+      res.status(404).end();
+      return;
+    }
+    next();
+  };
+
+  patientPortalModule.use('/suggestions', allowListMiddleware(allowedSuggesters), suggestions);
+}
