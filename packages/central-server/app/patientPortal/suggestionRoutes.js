4eec15db8f49348f9ba4e71c20851429f8cf6959
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-09-15 14:42:31 +1200

    hardcode

--- packages/central-server/app/patientPortal/suggestionRoutes.js
+++ packages/central-server/app/patientPortal/suggestionRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import config from 'config';
 import { suggestions } from '@tamanu/shared/services/suggestions';
 import { allowListMiddleware } from './allowListMiddleware';
+import { REFERENCE_TYPES } from '@tamanu/constants';
 
 export const suggestionRoutes = express.Router();
 
-const allowedSuggesters = config.patientPortal.allowedSuggesters;
+const allowedSuggesters = [REFERENCE_TYPES.DRUG]
 
 if (Array.isArray(allowedSuggesters) && allowedSuggesters.length > 0) {
   suggestionRoutes.use('/', allowListMiddleware(allowedSuggesters), suggestions);
