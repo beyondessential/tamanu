01e4831c109ed0f9f3842e45f58846c0cb69c060
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-15 16:29:56 +1200

    Update central.ts

--- packages/settings/src/schema/central.ts
+++ packages/settings/src/schema/central.ts
@@ -105,7 +105,7 @@ export const centralSettings = {
           properties: {
             host: {
               description: 'The host of the DHIS2 instance',
-              type: yup.string(),
+              type: yup.string().matches(/^.*[^/]$/, 'Host URL must not end with a forward slash'),
               defaultValue: '',
             },
             reportIds: {
