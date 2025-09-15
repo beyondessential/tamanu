39e32ff5ac2014e72dc7c6206d62a622102bdc04
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-15 15:14:50 +1200

    Revert "Update ScopeSelectorFields.jsx"
    This reverts commit fdebb292255c0c81f47a03a69b05381259d03ea3.

--- packages/web/app/views/administration/settings/components/ScopeSelectorFields.jsx
+++ packages/web/app/views/administration/settings/components/ScopeSelectorFields.jsx
@@ -11,7 +11,7 @@ const ScopeSelectInput = styled(SelectInput)`
   width: 300px;
 `;
 
-const ScopeField = styled(Field)`
+const ScopeDynamicSelectInput = styled(Field)`
   width: 300px;
   margin-top: 0.5rem;
 `;
@@ -52,7 +52,7 @@ export const ScopeSelectorFields = React.memo(({ scope, onScopeChange, onFacilit
         data-testid="scopeselectinput-zxel"
       />
       {scope === SETTINGS_SCOPES.FACILITY && (
-        <ScopeField
+        <ScopeDynamicSelectInput
           name="facilityId"
           component={AutocompleteField}
           suggester={facilitySuggester}
