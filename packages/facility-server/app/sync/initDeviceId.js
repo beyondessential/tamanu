f538ecc124eb33994d89ec5957a6812209870679
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-18 09:59:14 +1200

    Update initDeviceId.js

--- packages/facility-server/app/sync/initDeviceId.js
+++ packages/facility-server/app/sync/initDeviceId.js
@@ -2,7 +2,7 @@ import config from 'config';
 import shortid from 'shortid';
 import { FACT_DEVICE_ID } from '@tamanu/constants/facts';
 
-export async function initDeviceId(context) {
+export async function initDeviceId(context) { 
   const { LocalSystemFact } = context.models;
   let deviceId = await LocalSystemFact.get(FACT_DEVICE_ID);
   if (!deviceId) {
