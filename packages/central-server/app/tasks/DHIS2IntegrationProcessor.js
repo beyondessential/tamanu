34c7f2f1b01653f697f90642390f3adcdd540201
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-18 14:53:12 +1200

    remove audit logic somehow merged

--- packages/central-server/app/tasks/DHIS2IntegrationProcessor.js
+++ packages/central-server/app/tasks/DHIS2IntegrationProcessor.js
@@ -40,22 +40,6 @@ export class DHIS2IntegrationProcessor extends ScheduledTask {
     this.context = context;
   }
 
-  async logDHIS2Push(reportId, status, importCount, conflicts) {
-    const {
-      store: { models },
-    } = this.context;
-    const { imported, updated, ignored, deleted } = importCount;
-    await models.DHIS2PushLog.create({
-      reportId,
-      status,
-      imported,
-      updated,
-      ignored,
-      deleted,
-      conflicts,
-    });
-  }
-
   async postToDHIS2(reportCSV) {
     const { idSchemes, host } = await this.context.settings.get('integrations.dhis2');
     const { username, password } = config.integrations.dhis2;
@@ -113,19 +97,12 @@ export class DHIS2IntegrationProcessor extends ScheduledTask {
     const reportData = await latestVersion.dataGenerator({ ...this.context, sequelize }, {}); // We don't support parameters in this task
     const reportCSV = arrayOfArraysToCSV(reportData);
 
-    const {
-      status,
-      message,
-      httpStatusCode,
-      response: { importCount, conflicts = [] },
-    } = await this.postToDHIS2(reportCSV);
-
-    await this.logDHIS2Push(reportId, status, importCount, conflicts);
+    const { status, message, httpStatusCode, response } = await this.postToDHIS2(reportCSV);
 
     if (httpStatusCode === 200) {
       log.info(INFO_LOGS.SUCCESSFULLY_SENT_REPORT, {
         report: reportString,
-        ...importCount,
+        ...response.importCount,
       });
     } else {
       log.warn(WARNING_LOGS.FAILED_TO_SEND_REPORT, {
@@ -136,6 +113,7 @@ export class DHIS2IntegrationProcessor extends ScheduledTask {
       });
 
       // Value is the error message returned from DHIS2 api for each errored row
+      const { conflicts = [] } = response;
       conflicts.forEach(conflict => log.warn(conflict.value));
     }
   }
