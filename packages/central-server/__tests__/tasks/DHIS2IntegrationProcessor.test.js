91135a9ee9be70006e12d82a46f381f5592a718f
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-22 07:53:06 +1200

    test updates

--- packages/central-server/__tests__/tasks/DHIS2IntegrationProcessor.test.js
+++ packages/central-server/__tests__/tasks/DHIS2IntegrationProcessor.test.js
@@ -68,108 +68,114 @@ describe('DHIS2 integration processor', () => {
     ctx.close();
   });
 
-  it('should skip if missing host in settings', async () => {
-    await setHost('');
-    await dhis2IntegrationProcessor.run();
+  describe('configuration', () => {
+    it('should skip if missing host in settings', async () => {
+      await setHost('');
+      await dhis2IntegrationProcessor.run();
 
-    expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.INTEGRATION_NOT_CONFIGURED, {
-      enabled: true,
-      host: false,
-      username: true,
-      password: true,
-      reportIds: 1,
+      expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.INTEGRATION_NOT_CONFIGURED, {
+        enabled: true,
+        host: false,
+        username: true,
+        password: true,
+        reportIds: 1,
+      });
+    });
+
+    it('should skip if no reportIds in settings', async () => {
+      await setReportIds([]);
+      await dhis2IntegrationProcessor.run();
+      expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.INTEGRATION_NOT_CONFIGURED, {
+        enabled: true,
+        host: true,
+        username: true,
+        password: true,
+        reportIds: 0,
+      });
+    });
+
+    it('should check that all reports to be processed exist', async () => {
+      await setReportIds(['non-existent-report-id']);
+      await dhis2IntegrationProcessor.run();
+
+      expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.REPORT_DOES_NOT_EXIST, {
+        reportId: 'non-existent-report-id',
+      });
+
+      await setReportIds([report.id]);
+      await dhis2IntegrationProcessor.run();
+
+      expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.REPORT_HAS_NO_PUBLISHED_VERSION, {
+        report: `Test Report (${report.id})`,
+      });
+
+      await reportVersion.update({ status: REPORT_STATUSES.PUBLISHED });
+      await dhis2IntegrationProcessor.run();
+
+      expect(logSpy.info).toHaveBeenLastCalledWith(INFO_LOGS.PROCESSING_REPORT, {
+        report: `Test Report (${report.id})`,
+      });
     });
   });
 
-  it('should skip if no reportIds in settings', async () => {
-    await setReportIds([]);
-    await dhis2IntegrationProcessor.run();
-    expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.INTEGRATION_NOT_CONFIGURED, {
-      enabled: true,
-      host: true,
-      username: true,
-      password: true,
-      reportIds: 0,
+  describe('connection', () => {
+    it('should log.error if we cant establish a connection to DHIS2', async () => {
+      await setHost('https://invalid-host.com');
+      await dhis2IntegrationProcessor.run();
+
+      expect(logSpy.error).toHaveBeenLastCalledWith(ERROR_LOGS.ERROR_PROCESSING_REPORT, {
+        reportId: report.id,
+        error: expect.any(Error),
+      });
     });
   });
 
-  it('should check that all reports to be processed exist', async () => {
-    await setReportIds(['non-existent-report-id']);
-    await dhis2IntegrationProcessor.run();
+  describe('DHIS2 response', () => {
+    it('should log.warn individual conflicts when DHIS2 returns conflicts', async () => {
+      dhis2IntegrationProcessor.postToDHIS2 = jest.fn().mockResolvedValue({
+        httpStatusCode: 409,
+        status: 'warning',
+        message: 'Report sent to DHIS2 failed',
+        response: {
+          importCount: { imported: 0, updated: 0, deleted: 0, ignored: 2 },
+          conflicts: [
+            { value: 'Data element not found: DE123' },
+            { value: 'Organisation unit not found: OU456' },
+          ],
+        },
+      });
+      await dhis2IntegrationProcessor.run();
 
-    expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.REPORT_DOES_NOT_EXIST, {
-      reportId: 'non-existent-report-id',
+      expect(logSpy.warn).toHaveBeenCalledWith(WARNING_LOGS.FAILED_TO_SEND_REPORT, {
+        report: `Test Report (${report.id})`,
+        message: 'Report sent to DHIS2 failed',
+        status: 'warning',
+        httpStatusCode: 409,
+      });
+      expect(logSpy.warn).toHaveBeenCalledWith('Data element not found: DE123');
+      expect(logSpy.warn).toHaveBeenCalledWith('Organisation unit not found: OU456');
     });
 
-    await setReportIds([report.id]);
-    await dhis2IntegrationProcessor.run();
+    it('should log.info with the importCount if we get a 200 response from DHIS2', async () => {
+      dhis2IntegrationProcessor.postToDHIS2 = jest.fn().mockResolvedValue({
+        httpStatusCode: 200,
+        status: 'success',
+        message: 'Report sent to DHIS2 successfully',
+        response: {
+          importCount: { imported: 2, updated: 0, deleted: 0, ignored: 0 },
+          conflicts: [],
+        },
+      });
 
-    expect(logSpy.warn).toHaveBeenLastCalledWith(WARNING_LOGS.REPORT_HAS_NO_PUBLISHED_VERSION, {
-      report: `Test Report (${report.id})`,
-    });
+      await dhis2IntegrationProcessor.run();
 
-    await reportVersion.update({ status: REPORT_STATUSES.PUBLISHED });
-    await dhis2IntegrationProcessor.run();
-
-    expect(logSpy.info).toHaveBeenLastCalledWith(INFO_LOGS.PROCESSING_REPORT, {
-      report: `Test Report (${report.id})`,
-    });
-  });
-
-  it('should log.error if we cant establish a connection to DHIS2', async () => {
-    await setHost('https://invalid-host.com');
-    await dhis2IntegrationProcessor.run();
-
-    expect(logSpy.error).toHaveBeenLastCalledWith(ERROR_LOGS.ERROR_PROCESSING_REPORT, {
-      reportId: report.id,
-      error: expect.any(Error),
-    });
-  });
-
-  it('should log.warn individual conflicts when DHIS2 returns conflicts', async () => {
-    dhis2IntegrationProcessor.postToDHIS2 = jest.fn().mockResolvedValue({
-      httpStatusCode: 409,
-      status: 'warning',
-      message: 'Report sent to DHIS2 failed',
-      response: {
-        importCount: { imported: 0, updated: 0, deleted: 0, ignored: 2 },
-        conflicts: [
-          { value: 'Data element not found: DE123' },
-          { value: 'Organisation unit not found: OU456' },
-        ],
-      },
-    });
-    await dhis2IntegrationProcessor.run();
-
-    expect(logSpy.warn).toHaveBeenCalledWith(WARNING_LOGS.FAILED_TO_SEND_REPORT, {
-      report: `Test Report (${report.id})`,
-      message: 'Report sent to DHIS2 failed',
-      status: 'warning',
-      httpStatusCode: 409,
-    });
-    expect(logSpy.warn).toHaveBeenCalledWith('Data element not found: DE123');
-    expect(logSpy.warn).toHaveBeenCalledWith('Organisation unit not found: OU456');
-  });
-
-  it('should log.info with the importCount if we get a 200 response from DHIS2', async () => {
-    dhis2IntegrationProcessor.postToDHIS2 = jest.fn().mockResolvedValue({
-      httpStatusCode: 200,
-      status: 'success',
-      message: 'Report sent to DHIS2 successfully',
-      response: {
-        importCount: { imported: 2, updated: 0, deleted: 0, ignored: 0 },
-        conflicts: [],
-      },
-    });
-
-    await dhis2IntegrationProcessor.run();
-
-    expect(logSpy.info).toHaveBeenLastCalledWith(INFO_LOGS.SUCCESSFULLY_SENT_REPORT, {
-      report: `Test Report (${report.id})`,
-      imported: 2,
-      updated: 0,
-      deleted: 0,
-      ignored: 0,
+      expect(logSpy.info).toHaveBeenLastCalledWith(INFO_LOGS.SUCCESSFULLY_SENT_REPORT, {
+        report: `Test Report (${report.id})`,
+        imported: 2,
+        updated: 0,
+        deleted: 0,
+        ignored: 0,
+      });
     });
   });
 });
