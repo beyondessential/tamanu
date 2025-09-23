9d36010a6ced9bd4a0e2fb17c53a6c3ab5263f60
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-09-23 17:08:39 -0400

    Make error message creation on login reusable

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -2,5 +2,6 @@ export { BaseError } from './BaseError';
 export { Problem } from './Problem';
 export { ERROR_TYPE, type ErrorType } from './constants';
 export { isRecoverable, extractErrorFromFetchResponse } from './extractErrorFromFetchResponse';
+export { getLoginErrorMessage } from './getLoginErrorMessage';
 
 export * from './errors';
