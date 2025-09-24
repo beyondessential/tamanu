882e1da3466433b8f377584b843ea0d428b5dacb
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-09-24 19:42:49 -0400

    Expect request error instead of forbidden one

--- packages/facility-server/__tests__/apiv1/User.test.js
+++ packages/facility-server/__tests__/apiv1/User.test.js
@@ -257,7 +257,7 @@ describe('User', () => {
 
       it('should fail to get the user with a null token', async () => {
         const result = await baseApp.get('/api/user/me');
-        expect(result).toBeForbidden();
+        expect(result).toHaveRequestError();
       });
 
       it('should fail to get the user with an expired token', async () => {
