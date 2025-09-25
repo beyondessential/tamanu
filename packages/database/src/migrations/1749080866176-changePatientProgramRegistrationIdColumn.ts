eb5f12bd9f09395f642fc0aaa74f59756633c446
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-09-26 10:24:11 +1200

    Update 1749080866176-changePatientProgramRegistrationIdColumn.ts

--- packages/database/src/migrations/1749080866176-changePatientProgramRegistrationIdColumn.ts
+++ packages/database/src/migrations/1749080866176-changePatientProgramRegistrationIdColumn.ts
@@ -13,7 +13,7 @@ export async function up(query: QueryInterface): Promise<void> {
       );
   `);
 
-  // First, check for duplicates
+  // First, check we have no remaining duplicates
   const duplicates = await query.sequelize.query(
     `
     SELECT program_registry_id, patient_id, COUNT(*) as count
