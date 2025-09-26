0852f3f1753a91667dd316d2fc2cfea2a3e9b88d
Félix Saparelli <felix@passcod.name>
2025-09-26 15:46:38 +1200

    handle non-problem errors

--- packages/facility-server/app/middleware/auth.js
+++ packages/facility-server/app/middleware/auth.js
@@ -161,7 +161,7 @@ async function centralServerLoginWithLocalFallback({
   } catch (e) {
     // if we get an authentication or forbidden error when login to central server,
     // throw the error instead of proceeding to local login
-    if (e.type.startsWith(ERROR_TYPE.AUTH) || e.type === ERROR_TYPE.FORBIDDEN) {
+    if (e.type && (e.type.startsWith(ERROR_TYPE.AUTH) || e.type === ERROR_TYPE.FORBIDDEN)) {
       throw e;
     }
 
