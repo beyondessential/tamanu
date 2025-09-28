d405a0191601e3ccccf291ee198bd7ca35f7d56f
Tom Caiger <caigertom@gmail.com>
2025-09-29 12:49:38 +1300

    feat(patientPortal): NASS-1804: Patient Data question (#8500)
    * permission checks
    
    * change read to list
    
    * Update patientPortal.js
    
    * Update PrintPatientDetailsModal.jsx
    
    * Update SurveySelector.jsx
    
    * naming
    
    * submission settings
    
    * portal settings
    
    * add test
    
    * error handle
    
    * Update settings.test.js
    
    * Update central.ts
    
    * Move photo field to patient portal
    
    * patient data field
    
    * patient data
    
    * fixes
    
    * Update routes.js
    
    * loose ends
    
    * Update ReadSettings.ts
    
    * refactor util
    
    * Update getComponentForQuestionType.tsx

--- packages/patient-portal/src/features/survey/getComponentForQuestionType.tsx
+++ packages/patient-portal/src/features/survey/getComponentForQuestionType.tsx
@@ -13,19 +13,11 @@ import {
   DateTimeField,
   NullableBooleanField,
   PhotoField,
+  PatientDataDisplayField,
 } from '@tamanu/ui-components';
 import { SurveyQuestionAutocompleteField } from './SurveyQuestionAutocompleteField';
 
-const PlaceholderField = ({ label, type }: { label: string; type: string }) => {
-  return (
-    <Box>
-      {label}
-      <Box sx={{ p: 2, border: '1px dashed grey' }}>{type} field</Box>
-    </Box>
-  );
-};
-
-const unsupportedField = ({ label, type }: { label: string; type?: string }) => {
+const UnsupportedField = ({ label, type }: { label: string; type?: string }) => {
   return (
     <Box>
       {label}
@@ -34,7 +26,7 @@ const unsupportedField = ({ label, type }: { label: string; type?: string }) => 
   );
 };
 
-const withSaveDateAsString = (Component: React.ComponentType<any>) => (props: any) => (
+const DateFieldAsString = (Component: React.ComponentType<any>) => (props: any) => (
   <Component {...props} saveDateAsString />
 );
 
@@ -45,32 +37,56 @@ const QUESTION_COMPONENTS = {
   [PROGRAM_DATA_ELEMENT_TYPES.SELECT]: BaseSelectField,
   [PROGRAM_DATA_ELEMENT_TYPES.MULTI_SELECT]: BaseMultiselectField,
   [PROGRAM_DATA_ELEMENT_TYPES.AUTOCOMPLETE]: SurveyQuestionAutocompleteField,
-  [PROGRAM_DATA_ELEMENT_TYPES.DATE]: withSaveDateAsString(DateField),
-  [PROGRAM_DATA_ELEMENT_TYPES.DATE_TIME]: withSaveDateAsString(DateTimeField),
-  [PROGRAM_DATA_ELEMENT_TYPES.SUBMISSION_DATE]: withSaveDateAsString(DateField),
+  [PROGRAM_DATA_ELEMENT_TYPES.DATE]: DateFieldAsString(DateField),
+  [PROGRAM_DATA_ELEMENT_TYPES.DATE_TIME]: DateFieldAsString(DateTimeField),
+  [PROGRAM_DATA_ELEMENT_TYPES.SUBMISSION_DATE]: DateFieldAsString(DateField),
   [PROGRAM_DATA_ELEMENT_TYPES.NUMBER]: NumberField,
   [PROGRAM_DATA_ELEMENT_TYPES.BINARY]: NullableBooleanField,
   [PROGRAM_DATA_ELEMENT_TYPES.CHECKBOX]: NullableBooleanField,
   [PROGRAM_DATA_ELEMENT_TYPES.CALCULATED]: ReadOnlyTextField,
-  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_LINK]: PlaceholderField,
-  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_RESULT]: null, // intentionally null
-  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_ANSWER]: PlaceholderField,
-  [PROGRAM_DATA_ELEMENT_TYPES.PATIENT_DATA]: PlaceholderField,
-  [PROGRAM_DATA_ELEMENT_TYPES.USER_DATA]: unsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_LINK]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_RESULT]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.SURVEY_ANSWER]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.PATIENT_DATA]: ReadOnlyTextField,
+  [PROGRAM_DATA_ELEMENT_TYPES.USER_DATA]: UnsupportedField,
   [PROGRAM_DATA_ELEMENT_TYPES.INSTRUCTION]: InstructionField,
   [PROGRAM_DATA_ELEMENT_TYPES.PHOTO]: PhotoField,
-  [PROGRAM_DATA_ELEMENT_TYPES.RESULT]: null, // intentionally null
-  [PROGRAM_DATA_ELEMENT_TYPES.PATIENT_ISSUE]: InstructionField,
-  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_INSTANCE_NAME]: unsupportedField,
-  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_DATE]: unsupportedField,
-  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_TYPE]: unsupportedField,
-  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_SUBTYPE]: unsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.RESULT]: LimitedTextField,
+  [PROGRAM_DATA_ELEMENT_TYPES.PATIENT_ISSUE]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_INSTANCE_NAME]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_DATE]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_TYPE]: UnsupportedField,
+  [PROGRAM_DATA_ELEMENT_TYPES.COMPLEX_CHART_SUBTYPE]: UnsupportedField,
 };
 
-export function getComponentForQuestionType(type: keyof typeof PROGRAM_DATA_ELEMENT_TYPES) {
-  const Component = QUESTION_COMPONENTS[type];
-  if (Component === PlaceholderField || Component === unsupportedField) {
-    return (props: any) => <Component {...props} type={type} />;
+interface GetComponentForQuestionTypeOptions {
+  source?: string;
+  writeToPatient?: {
+    fieldType?: keyof typeof PROGRAM_DATA_ELEMENT_TYPES;
+  };
+}
+
+export function getComponentForQuestionType(
+  type: keyof typeof PROGRAM_DATA_ELEMENT_TYPES,
+  { source, writeToPatient: { fieldType } = {} }: GetComponentForQuestionTypeOptions = {},
+) {
+  let Component = QUESTION_COMPONENTS[type];
+
+  if (Component === UnsupportedField) {
+    const TypedComponent = Component as React.ComponentType<any>;
+    return (props: any) => <TypedComponent {...props} type={type} />;
   }
-  return Component;
+
+  if (type === PROGRAM_DATA_ELEMENT_TYPES.PATIENT_DATA) {
+    if (fieldType && fieldType in QUESTION_COMPONENTS) {
+      // PatientData specifically can overwrite field type if we are writing back to patient record
+      Component = QUESTION_COMPONENTS[fieldType];
+    } else if (source) {
+      // we're displaying a relation, so use PatientDataDisplayField
+      // (using a LimitedTextField will just display the bare id)
+      Component = PatientDataDisplayField as any;
+    }
+  }
+
+  return Component as React.ComponentType<any>;
 }
--- packages/web/app/components/Field/index.js
+++ packages/web/app/components/Field/index.js
@@ -28,7 +28,6 @@ export * from './DisplayIdField';
 export * from './DOBFields';
 export * from './LocationField';
 export * from './DynamicSelectField';
-export * from './PatientDataDisplayField';
 export * from './SurveyQuestionAutocompleteField';
 
 // form helpers
