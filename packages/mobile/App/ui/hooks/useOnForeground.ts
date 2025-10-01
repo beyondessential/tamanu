ef01224fa3fd4b351d7d19ecfcb3fe6789727a43
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-09-30 22:08:49 -0400

    Simplify foreground check by only checking nextAppState

--- packages/mobile/App/ui/hooks/useOnForeground.ts
+++ packages/mobile/App/ui/hooks/useOnForeground.ts
@@ -1,27 +1,20 @@
-import { useRef, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AppState, AppStateStatus } from 'react-native';
 
 export const useOnForeground = () => {
   const [isForeground, setIsForeground] = useState(true);
-  const appState = useRef(AppState.currentState);
   console.log('isForeground', isForeground);
 
   useEffect(() => {
     const subscription = AppState.addEventListener('change', (nextAppState: AppStateStatus) => {
-      console.log('appState.current', appState.current);
       console.log('nextAppState', nextAppState);
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === 'active'
-      ) {
+      if (nextAppState === 'active') {
         console.log('changing to isForeground true');
         setIsForeground(true);
       } else {
         console.log('changing to isForeground false');
         setIsForeground(false);
       }
-
-      appState.current = nextAppState;
     });
 
     return () => {
