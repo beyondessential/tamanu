f9cb5250b48b1c920f53e89d7f9fe4bfb4367e48
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-10-10 15:56:57 +1300

    add icon files

---
+++ packages/web/app/assets/icons/CrossIcon.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const CrossIcon = color => {
+  return (
+    <svg width="24" height="24" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path fillRule="evenodd" clipRule="evenodd" fill={color} />
+    </svg>
+  );
+};
