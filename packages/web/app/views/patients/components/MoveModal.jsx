7c368e7a7571be643bd00298ece650ce94724cc8
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-10-14 16:01:29 +1300

    feat(encounters): NASS-1814: Planned patient move (#8588)
    * Update EncounterActions.jsx
    
    * baic move
    
    * Update MoveModal.jsx
    
    * Update EncounterActions.jsx
    
    * add cibstabt

--- packages/web/app/views/patients/components/MoveModal.jsx
+++ packages/web/app/views/patients/components/MoveModal.jsx
@@ -9,8 +9,12 @@ import {
   Form,
   FormGrid,
   FormModal,
+  FormSeparatorLine,
   FormSubmitCancelRow,
   Heading3,
+  LocalisedLocationField,
+  LocationAvailabilityWarningMessage,
+  RadioField,
 } from '../../../components';
 import { usePatientMove } from '../../../api/mutations';
 import { FORM_TYPES } from '../../../constants';
@@ -18,11 +22,12 @@ import { TranslatedText } from '../../../components/Translation/TranslatedText';
 import { useSuggester } from '../../../api';
 import { useEncounter } from '../../../contexts/Encounter';
 import { TAMANU_COLORS } from '@tamanu/ui-components';
+import { useSettings } from '../../../contexts/Settings';
+import { PATIENT_MOVE_ACTIONS } from '@tamanu/constants';
 
 const SectionHeading = styled(Heading3)`
   color: ${TAMANU_COLORS.darkestText};
-  margin: 0;
-  margin-bottom: 10px;
+  margin: 10px 0;
   padding: 0;
 `;
 
@@ -33,13 +38,116 @@ const SectionDescription = styled(BodyText)`
   padding: 0;
 `;
 
+const Section = styled(FormGrid)`
+  margin-bottom: 30px;
+`;
+
 const SubmitRow = styled(FormSubmitCancelRow)`
   margin-top: 20px;
 `;
 
+const BasicMoveFields = () => {
+  return (
+    <>
+      <SectionDescription>DESCRIPTION TO BE CONFIRMED</SectionDescription>
+      <Section columns={2} data-testid="formgrid-wyqp">
+        <Field
+          name="locationId"
+          component={LocalisedLocationField}
+          label={
+            <TranslatedText
+              stringId="patient.encounter.movePatient.location.label"
+              fallback="New location"
+              data-testid="translatedtext-35a6"
+            />
+          }
+          required
+          data-testid="field-tykg"
+        />
+      </Section>
+    </>
+  );
+};
+
+const PATIENT_MOVE_ACTION_OPTIONS = [
+  {
+    label: (
+      <TranslatedText
+        stringId="encounter.modal.patientMove.action.plan"
+        fallback="Plan"
+        data-testid="translatedtext-patient-move-action-plan"
+      />
+    ),
+    value: PATIENT_MOVE_ACTIONS.PLAN,
+  },
+  {
+    label: (
+      <TranslatedText
+        stringId="encounter.modal.patientMove.action.finalise"
+        fallback="Finalise"
+        data-testid="translatedtext-patient-move-action-finalise"
+      />
+    ),
+    value: PATIENT_MOVE_ACTIONS.FINALISE,
+  },
+];
+
+const AdvancedMoveFields = ({ plannedLocationId }) => {
+  const { getSetting } = useSettings();
+  const plannedMoveTimeoutHours = getSetting('templates.plannedMoveTimeoutHours');
+
+  return (
+    <>
+      <SectionDescription>
+        Select a location to plan the patient location move and reserve a bed. The new location will
+        not be reflected in the patient encounter until you finalise the move. If the change is not
+        finalised within 24 hours, the planned location move will be cancelled. Alternatively you
+        can finalise the patient move now using the option below.
+      </SectionDescription>
+      <Section columns={2} data-testid="formgrid-wyqp">
+        <Field
+          name="plannedLocationId"
+          component={LocalisedLocationField}
+          required
+          data-testid="field-n625"
+        />
+        <LocationAvailabilityWarningMessage
+          locationId={plannedLocationId}
+          style={{ gridColumn: '2', marginTop: '-35px', fontSize: '12px' }}
+          data-testid="locationavailabilitywarningmessage-6ivs"
+        />
+        <Field
+          name="action"
+          label={
+            <TranslatedText
+              stringId="encounter.modal.patientMove.action.label"
+              fallback="Would you like to plan or finalise the patient move?"
+              data-testid="translatedtext-l7v1"
+            />
+          }
+          component={RadioField}
+          options={PATIENT_MOVE_ACTION_OPTIONS}
+          style={{ gridColumn: '1/-1' }}
+          data-testid="field-ryle"
+        />
+        <TranslatedText
+          stringId="encounter.modal.patientMove.planningNote"
+          fallback="By selecting 'Plan' the new location will not be reflected in the patient encounter until you finalise the move. If the move is not finalised within :hours hours, the location will be deemed 'Available' again."
+          replacements={{ hours: plannedMoveTimeoutHours }}
+          data-testid="translatedtext-encounter-modal-patient-move-planning-note"
+        />
+      </Section>
+    </>
+  );
+};
+
 export const MoveModal = React.memo(({ open, onClose, encounter }) => {
+  const { getSetting } = useSettings();
   const { writeAndViewEncounter } = useEncounter();
   const { mutateAsync: submitPatientMove } = usePatientMove(encounter.id, onClose);
+
+  const enablePatientMoveActions = getSetting('features.patientPlannedMove');
+
   const departmentSuggester = useSuggester('department', {
     baseQueryParameters: { filterByFacility: true },
   });
@@ -69,15 +177,23 @@ export const MoveModal = React.memo(({ open, onClose, encounter }) => {
           submittedTime: getCurrentDateTimeString(),
           examinerId: encounter.examinerId,
           departmentId: encounter.departmentId,
+          ...(enablePatientMoveActions
+            ? {
+                plannedLocationId: encounter.plannedLocationId,
+                action: PATIENT_MOVE_ACTIONS.PLAN,
+              }
+            : {
+                locationId: encounter.locationId,
+              }),
         }}
         formType={FORM_TYPES.EDIT_FORM}
         onSubmit={onSubmit}
-        render={({ submitForm }) => (
+        render={({ submitForm, values }) => (
           <>
             <SectionHeading>
               <TranslatedText
                 stringId="patient.encounter.modal.movePatient.section.move.heading"
-                fallback="Move patient"
+                fallback="Patient care"
               />
             </SectionHeading>
             <SectionDescription>
@@ -86,7 +202,7 @@ export const MoveModal = React.memo(({ open, onClose, encounter }) => {
                 fallback="Please select the clinician and department for the patient."
               />
             </SectionDescription>
-            <FormGrid columns={2} data-testid="formgrid-wyqp">
+            <Section columns={2} data-testid="formgrid-wyqp">
               <Field
                 name="examinerId"
                 component={DynamicSelectField}
@@ -113,7 +229,19 @@ export const MoveModal = React.memo(({ open, onClose, encounter }) => {
                 required
                 data-testid="field-tykg"
               />
-            </FormGrid>
+            </Section>
+            <FormSeparatorLine />
+            <SectionHeading>
+              <TranslatedText
+                stringId="patient.encounter.modal.movePatient.section.basic.heading"
+                fallback="Move location"
+              />
+            </SectionHeading>
+            {enablePatientMoveActions ? (
+              <AdvancedMoveFields plannedLocationId={values?.plannedLocationId} />
+            ) : (
+              <BasicMoveFields />
+            )}
             <SubmitRow
               onConfirm={submitForm}
               onCancel={onClose}
