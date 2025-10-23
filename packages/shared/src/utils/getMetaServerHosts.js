3f73105d69d7de7cd2425d5cdd31e7aa0c04cc5b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-10-23 18:38:57 -0400

    Include a fallback with the old config shape for a smoother transition
    Co-authored-by: Félix Saparelli <felix@beyondessential.com.au>

--- packages/shared/src/utils/getMetaServerHosts.js
+++ packages/shared/src/utils/getMetaServerHosts.js
@@ -1,7 +1,7 @@
 import config from 'config';
 
 export const getMetaServerHosts = () => {
-  const metaServerHosts = config?.metaServer?.hosts;
+  const metaServerHosts = config?.metaServer?.hosts ?? [config?.metaServer?.host];
   if (!Array.isArray(metaServerHosts)) {
     throw new Error('metaServer.hosts is not an array');
   }
