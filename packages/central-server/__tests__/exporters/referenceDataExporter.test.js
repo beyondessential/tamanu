1b8f17ad48f857b44fed949a93a5835c257dfeb1
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-10-28 11:23:26 +1300

    Update referenceDataExporter.test.js

--- packages/central-server/__tests__/exporters/referenceDataExporter.test.js
+++ packages/central-server/__tests__/exporters/referenceDataExporter.test.js
@@ -53,6 +53,7 @@ describe('Reference data exporter', () => {
 
     const modelsToDestroy = [
       'AdministeredVaccine',
+      'EncounterHistory',
       'Encounter',
       'ScheduledVaccine',
       'LabTestPanelLabTestTypes',
