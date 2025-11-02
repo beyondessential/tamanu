e108033db5a8319af1b326e9c93d238f1eb9c46e
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-11-03 08:41:44 +1300

    Update EncounterHistory.ts

--- packages/mobile/App/models/EncounterHistory.ts
+++ packages/mobile/App/models/EncounterHistory.ts
@@ -57,8 +57,8 @@ export class EncounterHistory extends BaseModel {
   @Column({ type: 'varchar', nullable: false })
   encounterType: EncounterType;
 
-  @Column({ type: 'text', array: true, nullable: true })
-  changeType?: EncounterChangeType[];
+  @Column({ nullable: true })
+  changeType?: string;
 
   static async createSnapshot(encounter, { date }) {
     return EncounterHistory.createAndSaveOne({
