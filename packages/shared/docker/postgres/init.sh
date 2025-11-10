e3534ccf547331d3b2acb69b0dc074d8f7d97469
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-11-09 20:26:38 -0400

    Add doble quotes properly

--- packages/shared/docker/postgres/init.sh
+++ packages/shared/docker/postgres/init.sh
@@ -6,8 +6,8 @@ psql -v ON_ERROR_STOP=1 <<-EOF
     CREATE DATABASE "tamanu-facility" WITH OWNER "tamanu-facility";
     CREATE ROLE "tamanu-central" WITH LOGIN ENCRYPTED PASSWORD 'tamanu-central';
     CREATE DATABASE "tamanu-central" WITH OWNER "tamanu-central";
-    CREATE ROLE tamanu_reporting_user WITH LOGIN PASSWORD 'test';
-    CREATE ROLE tamanu_raw_user WITH LOGIN PASSWORD 'test';
+    CREATE ROLE "tamanu_reporting_user" WITH LOGIN PASSWORD 'test';
+    CREATE ROLE "tamanu_raw_user" WITH LOGIN PASSWORD 'test';
     \c app
     CREATE SCHEMA IF NOT EXISTS reporting;
     ALTER ROLE "tamanu_reporting_user" SET search_path TO reporting;
