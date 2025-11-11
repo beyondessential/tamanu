66b5792391c3213f31512724c19209629698d392
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-11-12 07:36:25 +1300

    copy rohan code

--- packages/web/app/components/Field/LocationField.jsx
+++ packages/web/app/components/Field/LocationField.jsx
@@ -30,7 +30,9 @@ export const LocationInput = React.memo(
     required,
     className,
     value,
+    groupValue,
     onChange,
+    onGroupChange = () => {},
     size = 'medium',
     form = {},
     enableLocationStatus = true,
@@ -45,7 +47,7 @@ export const LocationInput = React.memo(
     const { facilityId: currentFacilityId } = useAuth();
     const facilityId = (facilityIdOverride ?? currentFacilityId) || '';
 
-    const [groupId, setGroupId] = useState('');
+    const [groupId, setGroupId] = useState(groupValue);
     const [locationId, setLocationId] = useState(value);
     const suggester = useSuggester('location', {
       formatter: ({ name, id, locationGroup, availability }) => {
@@ -76,9 +78,16 @@ export const LocationInput = React.memo(
     }, [initialValues, name]);
 
     useEffect(() => {
-      setLocationId(value ?? '');
-      if (!value) setGroupId('');
+      if (value) {
+        setLocationId(value);
+      }
     }, [value]);
+
+    useEffect(() => {
+      if (groupValue) {
+        setGroupId(groupValue);
+      }
+    }, [groupValue]);
 
     // when the location is selected, set the group value automatically if it's not set yet
     useEffect(() => {
@@ -87,25 +96,29 @@ export const LocationInput = React.memo(
       if (isNotSameGroup) {
         // clear the location if the location group is changed
         setLocationId('');
-        onChange({ target: { value: '', name } });
+        onChange({ target: { value: '', name, groupValue: location.locationGroup.id } });
       }
 
       // Initialise the location group state
       // if the form is being opened in edit mode (i.e. there are existing values)
       if (value && !groupId && location?.locationGroup?.id) {
+        onGroupChange(location.locationGroup.id);
         setGroupId(location.locationGroup.id);
       }
-    }, [onChange, value, name, groupId, location?.id, location?.locationGroup]);
+    }, [onChange, value, name, groupId, location?.id, location?.locationGroup, onGroupChange]);
 
     const handleChangeCategory = event => {
       setGroupId(event.target.value);
-      setLocationId('');
-      onChange({ target: { value: '', name } });
+      onGroupChange(event.target.value);
+      if (locationId) {
+        setLocationId('');
+        onChange({ target: { value: '', name, groupValue: event.target.value } });
+      }
     };
 
     const handleChange = async event => {
       setLocationId(event.target.value);
-      onChange({ target: { value: event.target.value, name } });
+      onChange({ target: { value: event.target.value, name, groupValue: groupId } });
     };
 
     // Disable the location and location group fields if:
