23f34f74aedb2057eb4495bc911d5587e273f576
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-17 12:29:03 +1300

    gottem agian

--- packages/web/app/components/PatientHistorySearch.jsx
+++ packages/web/app/components/PatientHistorySearch.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import { Field, Form, TranslatedSelectField } from '@tamanu/ui-components';
 import { ENCOUNTER_TYPE_LABELS } from '@tamanu/constants';
+import { styled } from '@mui/material';
+
+const StyledField = styled(Field)`
+  width: 150px;
+`;
 
 export const PatientHistorySearch = () => {
   return (
     <Form
-      onSubmit={() => {}}
+      onSubmit={async () => {}}
       render={() => (
-        <Box display="flex" gap={2}>
-          <Field
+        <Box paddingY={1} display="flex" gap={2}>
+            <StyledField
             component={TranslatedSelectField}
             name="encounterType"
             label="Encounter Type"
