7f3739ff54ea43095269813af9f67e52dc0c04c4
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-17 12:23:27 +1300

    simply beatuiful

--- packages/web/app/components/PatientHistorySearch.jsx
+++ packages/web/app/components/PatientHistorySearch.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Box } from '@material-ui/core';
-import { MultiselectField } from './Field/MultiselectField';
-import { Field, useSuggester } from '@tamanu/ui-components';
+import { Field, Form, TranslatedSelectField } from '@tamanu/ui-components';
+import { ENCOUNTER_TYPE_LABELS } from '@tamanu/constants';
 
 export const PatientHistorySearch = () => {
-  const encounterTypeSuggester = useSuggester('encounterType');
   return (
-    <Box display="flex" gap={2}>
-      <Field
-        component={MultiselectField}
-        name="encounterType"
-        label="Encounter Type"
-        suggester={encounterTypeSuggester}
-      />
-    </Box>
+    <Form
+      onSubmit={() => {}}
+      render={() => (
+        <Box display="flex" gap={2}>
+          <Field
+            component={TranslatedSelectField}
+            name="encounterType"
+            label="Encounter Type"
+            enumValues={ENCOUNTER_TYPE_LABELS}
+          />
+        </Box>
+      )}
+    />
   );
 };
