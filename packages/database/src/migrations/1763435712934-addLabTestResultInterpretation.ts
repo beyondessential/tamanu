93b077ed7af62d403bfda547686a146691c2de9f
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-18 16:27:25 +1300

    add migration

---
+++ packages/database/src/migrations/1763435712934-addLabTestResultInterpretation.ts
@@ -0,0 +1,12 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  await query.addColumn('lab_tests', 'resultInterpretation', {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  });
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  await query.removeColumn('lab_tests', 'resultInterpretation');
+}
