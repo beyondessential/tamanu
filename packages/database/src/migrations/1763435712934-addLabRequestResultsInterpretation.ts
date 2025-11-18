0015ee8b39b825feca6c13511f573cd382496bef
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-18 17:18:51 +1300

    rethink

--- packages/database/src/migrations/1763435712934-addLabRequestResultsInterpretation.ts
+++ packages/database/src/migrations/1763435712934-addLabRequestResultsInterpretation.ts
@@ -1,12 +1,12 @@
 import { DataTypes, QueryInterface } from 'sequelize';
 
 export async function up(query: QueryInterface): Promise<void> {
-  await query.addColumn('lab_tests', 'result_interpretation', {
+  await query.addColumn('lab_requests', 'results_interpretation', {
     type: DataTypes.TEXT,
     allowNull: true,
   });
 }
 
 export async function down(query: QueryInterface): Promise<void> {
-  await query.removeColumn('lab_tests', 'result_interpretation');
+  await query.removeColumn('lab_requests', 'results_interpretation');
 }
--- packages/database/src/models/LabTest.ts
+++ packages/database/src/models/LabTest.ts
@@ -15,7 +15,6 @@ export class LabTest extends Model {
   declare id: string;
   declare date: string;
   declare result: string;
-  declare resultInterpretation?: string;
   declare laboratoryOfficer?: string;
   declare verification?: string;
   declare completedDate?: string;
@@ -34,10 +33,6 @@ export class LabTest extends Model {
           type: DataTypes.STRING,
           allowNull: false,
           defaultValue: '',
-        },
-        resultInterpretation: {
-          type: DataTypes.TEXT,
-          allowNull: true,
         },
         laboratoryOfficer: {
           type: DataTypes.STRING,
