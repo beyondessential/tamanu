9a0e10b3930e0840ea4a9fcde705ea05e3aa874b
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-21 10:35:59 +1300

    whitespace remove from templates

--- packages/settings/src/schema/global.ts
+++ packages/settings/src/schema/global.ts
@@ -1362,7 +1362,7 @@ export const globalSettings = {
                 body: {
                   type: yup.string().trim().min(1),
                   defaultValue:
-                    'Hi $firstName$ $lastName$,\n\n This is a confirmation that your booking has been scheduled at $facilityName$.\nDate: $startDate$\nTime: $startTime$\nLocation: $locationName$, $facilityName$$clinicianName$\n\nDo not respond to this email.',
+                    'Hi $firstName$ $lastName$,\n\nThis is a confirmation that your booking has been scheduled at $facilityName$.\nDate: $startDate$\nTime: $startTime$\nLocation: $locationName$, $facilityName$$clinicianName$\n\nDo not respond to this email.',
                 },
               },
             },
@@ -1376,7 +1376,7 @@ export const globalSettings = {
                 body: {
                   type: yup.string().trim().min(1),
                   defaultValue:
-                    'Hi $firstName$ $lastName$,\n\n This is a confirmation that your appointment has been scheduled at $facilityName$.\nDate: $startDate$\nTime: $startTime$\nLocation: $locationName$, $facilityName$$clinicianName$\n\nDo not respond to this email.',
+                    'Hi $firstName$ $lastName$,\n\nThis is a confirmation that your appointment has been scheduled at $facilityName$.\nDate: $startDate$\nTime: $startTime$\nLocation: $locationName$, $facilityName$$clinicianName$\n\nDo not respond to this email.',
                 },
               },
             },
