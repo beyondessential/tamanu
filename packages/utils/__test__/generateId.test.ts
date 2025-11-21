0b369ba3e634a6b52302f0dbd5bc30b856b020ad
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-21 15:43:12 +1300

    ya

--- packages/utils/__test__/generateId.test.ts
+++ packages/utils/__test__/generateId.test.ts
@@ -1,4 +1,4 @@
-import { generateId, isGeneratedDisplayId, fakeUUID, FAKE_UUID_PATTERN } from '../src/generateId';
+import { generateId, isGeneratedDisplayId, fakeUUID, FAKE_UUID_PATTERN, generateIdFromPattern } from '../src/generateId';
 import { describe, expect, it } from 'vitest';
 
 describe('generateId', () => {
@@ -29,5 +29,32 @@ describe('fakeUUID', () => {
     const uuid = fakeUUID();
     const regex = new RegExp(FAKE_UUID_PATTERN.replace(/_/g, '[0-9a-f]'));
     expect(uuid).toMatch(regex);
+  });
+});
+
+describe('generateIdFromPattern', () => {
+  it('should generate an ID matching A and 0 placeholders', () => {
+    const id = generateIdFromPattern('AA000');
+    expect(id).toMatch(/^[A-Z]{2}\d{3}$/);
+  });
+
+  it('should use static characters wrapped in brackets', () => {
+    const id = generateIdFromPattern('[B]000000');
+    expect(id).toMatch(/^B\d{6}$/);
+  });
+
+  it('should support multi-character static segments in brackets', () => {
+    const id = generateIdFromPattern('[ABC]00');
+    expect(id).toMatch(/^ABC\d{2}$/);
+  });
+
+  it('should preserve non-placeholder characters outside brackets', () => {
+    const id = generateIdFromPattern('XXA0YY');
+    expect(id).toMatch(/^XX[A-Z]\dYY$/);
+  });
+
+  it('should combine static, letter, and digit segments', () => {
+    const id = generateIdFromPattern('[B]AA[A]000');
+    expect(id).toMatch(/^B[A-Z]{2}A\d{3}$/);
   });
 });
