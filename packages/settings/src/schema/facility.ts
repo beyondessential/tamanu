1f107fe12fe7749a9913aba29b9ac1aa81346aee
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-11-24 09:53:51 +1300

    ya

--- packages/settings/src/schema/facility.ts
+++ packages/settings/src/schema/facility.ts
@@ -84,7 +84,9 @@ export const facilitySettings = {
       highRisk: true,
       description:
         'The pattern to use for generating patient display IDs. A will be replaced with a random letter and 0 will be replaced with a random number. Wrapping characters in [] will allow static characters to be used.',
-      type: yup.string(),
+      type: yup
+        .string()
+        .matches(/^(\[[^\]]*]|[A0])+$/, 'Invalid pattern'),
       defaultValue: 'AAAA000000',
     },
     questionCodeIds: {
