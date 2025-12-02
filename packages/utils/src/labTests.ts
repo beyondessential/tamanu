e882c6cf17c571d345a96bfd615d80ccf1b5805c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-02 14:02:12 +1300

    oops

--- packages/utils/src/labTests.ts
+++ packages/utils/src/labTests.ts
@@ -1,7 +1,6 @@
 import { SEX_VALUES } from '@tamanu/constants';
 import type { LabTestType } from '@tamanu/database/models/LabTestType';
 
-
 type Sex = 'male' | 'female' | null | undefined;
 
 const hasValue = (value?: number | null) => value || value === 0;
@@ -9,7 +8,7 @@ const hasValue = (value?: number | null) => value || value === 0;
 interface GetReferenceRangeProps {
   labTestType?: LabTestType;
   sex?: Sex;
-  getTranslation: (stringId: string, fallback: string, translationOptions?: any)=> string;
+  getTranslation: (stringId: string, fallback: string, translationOptions?: any) => string;
 }
 export const getReferenceRange = ({ labTestType, sex, getTranslation }: GetReferenceRangeProps) => {
   if (!labTestType) return '';
@@ -20,21 +19,33 @@ export const getReferenceRange = ({ labTestType, sex, getTranslation }: GetRefer
   const hasMin = hasValue(min);
 
   let baseRange: string;
-  if (hasMin && hasMax) baseRange = getTranslation('general.fallback.range', ':min - :max', { replacements: { min, max } });
-  else if (hasMin) baseRange = getTranslation('general.fallback.greaterThan', '>:min', { replacements: { min } });
-  else if (hasMax) baseRange = getTranslation('general.fallback.lessThan', '<:max', { replacements: { max } });
+  if (hasMin && hasMax)
+    baseRange = getTranslation('general.fallback.range', ':min - :max', {
+      replacements: { min, max },
+    });
+  else if (hasMin)
+    baseRange = getTranslation('general.fallback.greaterThan', '>:min', { replacements: { min } });
+  else if (hasMax)
+    baseRange = getTranslation('general.fallback.lessThan', '<:max', { replacements: { max } });
   else if (labTestType.rangeText) baseRange = labTestType.rangeText;
-  else baseRange = getTranslation('general.fallback.notApplicable', 'N/A', {casing: 'lower'});
+  else baseRange = getTranslation('general.fallback.notApplicable', 'N/A', { casing: 'lower' });
 
   return baseRange;
 };
 
-export const getReferenceRangeWithUnit = ({ labTestType, sex, getTranslation }: GetReferenceRangeProps) => {
+export const getReferenceRangeWithUnit = ({
+  labTestType,
+  sex,
+  getTranslation,
+}: GetReferenceRangeProps) => {
   if (!labTestType) return '';
 
   const referenceRange = getReferenceRange({ labTestType, sex, getTranslation });
   const { unit } = labTestType;
   if (!unit) return referenceRange;
-  if (referenceRange === getTranslation('general.fallback.notApplicable', 'N/A', {casing: 'lower'})) return referenceRange;
+  if (
+    referenceRange === getTranslation('general.fallback.notApplicable', 'N/A', { casing: 'lower' })
+  )
+    return referenceRange;
   return `${referenceRange} ${unit}`;
 };
