4133774453f99216613adf0d15cda21ad7648365
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-02 14:30:27 +1300

    yaya

--- packages/utils/src/labTests.ts
+++ packages/utils/src/labTests.ts
@@ -1,14 +1,19 @@
 import { SEX_VALUES } from '@tamanu/constants';
 import type { LabTestType } from '@tamanu/database/models/LabTestType';
 
-type Sex = 'male' | 'female' | null | undefined;
-
-const hasValue = (value?: number | null) => value || value === 0;
+const hasValue = (value?: number | string) => value || value === 0;
 
 interface GetReferenceRangeProps {
   labTestType?: LabTestType;
-  sex?: Sex;
-  getTranslation: (stringId: string, fallback: string, translationOptions?: any) => string;
+  sex?: keyof typeof SEX_VALUES | null,
+  getTranslation: (
+    stringId: string,
+    fallback: string,
+    translationOptions?: {
+      casing?: 'lower' | 'upper' | 'sentence';
+      replacements?: Record<string, string | number | undefined>;
+    },
+  ) => string;
 }
 export const getReferenceRange = ({ labTestType, sex, getTranslation }: GetReferenceRangeProps) => {
   if (!labTestType) return '';
