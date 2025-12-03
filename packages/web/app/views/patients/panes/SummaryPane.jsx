385da2a68912597fb2ce9373c1cf1e14a307de31
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-12-04 09:58:21 +1300

    Update SummaryPane.jsx

--- packages/web/app/views/patients/panes/SummaryPane.jsx
+++ packages/web/app/views/patients/panes/SummaryPane.jsx
@@ -1,9 +1,16 @@
 import React, { useCallback, useState } from 'react';
+import styled from 'styled-components';
 import { usePatientNavigation } from '../../../utils/usePatientNavigation';
 import { useEncounter } from '../../../contexts/Encounter';
 import { Box } from '@material-ui/core';
 
-import { CompactContentPane as ContentPane } from '../../../components';
+import {
+  CompactContentPane as ContentPane,
+  LargeBodyText,
+  Modal,
+  ModalActionRow,
+  TranslatedText,
+} from '../../../components';
 import { PatientEncounterSummary } from '../components/PatientEncounterSummary';
 import { PatientHistory } from '../../../components/PatientHistory';
 import { EncounterModal } from '../../../components/EncounterModal';
@@ -11,13 +18,52 @@ import { LocationBookingsTable } from '../../../components/Appointments/Location
 import { useAuth } from '../../../contexts/Auth';
 import { useSettings } from '../../../contexts/Settings';
 import { OutpatientAppointmentsTable } from '../../../components/Appointments/OutpatientAppointmentsTable';
+import { usePatientCurrentEncounterQuery } from '../../../api/queries/usePatientCurrentEncounterQuery';
+import { useQueryClient } from '@tanstack/react-query';
+
+const StyledLargeBodyText = styled(LargeBodyText)`
+  margin: 20px;
+`;
+
+export const ExistingEncounterWarningModal = React.memo(({ open, onClose }) => {
+  return (
+    <Modal
+      title={
+        <TranslatedText
+          stringId="patient.modal.activeEncounterWarning.title"
+          fallback="Cannot create new encounter"
+        />
+      }
+      width="md"
+      open={open}
+      onClose={onClose}
+      data-testid="formmodal-4oua"
+    >
+      <StyledLargeBodyText>
+        <TranslatedText
+          stringId="patient.encounterSummary.activeEncounterWarning"
+          fallback="This patient has an active encounter. The active encounter must be discharged before a new encounter can be created."
+        />
+      </StyledLargeBodyText>
+      <ModalActionRow onConfirm={onClose} confirmText="Close" />
+    </Modal>
+  );
+});
 
 export const SummaryPane = React.memo(({ patient, additionalData, disabled }) => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isCheckInModalOpen, setIsCheckInModalOpen] = useState(false);
+  const [isWarningModalOpen, setIsWarningModalOpen] = useState(false);
   const { navigateToEncounter } = usePatientNavigation();
   const { loadEncounter } = useEncounter();
   const { ability } = useAuth();
   const { getSetting } = useSettings();
+  const queryClient = useQueryClient();
+  const { refetch: refetchCurrentEncounter } = usePatientCurrentEncounterQuery(patient.id);
+
+  const checkForExistingEncounter = useCallback(async () => {
+    const { data: encounter } = await refetchCurrentEncounter();
+    return !!encounter;
+  }, [refetchCurrentEncounter]);
 
   const showLocationBookingsSetting = getSetting('layouts.patientView.showLocationBookings');
   const showOutpatientAppointmentsSetting = getSetting(
@@ -38,7 +84,11 @@ export const SummaryPane = React.memo(({ patient, additionalData, disabled }) =>
     [loadEncounter, navigateToEncounter],
   );
 
-  const onCloseModal = useCallback(() => setModalOpen(false), []);
+  const onCloseCheckInModal = useCallback(() => setIsCheckInModalOpen(false), []);
+  const onCloseWarningModal = useCallback(async () => {
+    setIsWarningModalOpen(false);
+    await queryClient.invalidateQueries(['patientCurrentEncounter', patient.id]); // Refresh the current encounter data on close
+  }, [queryClient, patient.id]);
 
   return (
     <>
@@ -46,7 +96,14 @@ export const SummaryPane = React.memo(({ patient, additionalData, disabled }) =>
       <ContentPane data-testid="contentpane-3jxx">
         <PatientEncounterSummary
           viewEncounter={onViewEncounter}
-          openCheckIn={() => setModalOpen(true)}
+          openCheckIn={async () => {
+            const hasExistingEncounter = await checkForExistingEncounter();
+            if (hasExistingEncounter) {
+              setIsWarningModalOpen(true);
+            } else {
+              setIsCheckInModalOpen(true);
+            }
+          }}
           patient={patient}
           disabled={disabled}
           data-testid="patientencountersummary-z703"
@@ -73,12 +130,13 @@ export const SummaryPane = React.memo(({ patient, additionalData, disabled }) =>
         />
       </ContentPane>
       <EncounterModal
-        open={isModalOpen}
-        onClose={onCloseModal}
+        open={isCheckInModalOpen}
+        onClose={onCloseCheckInModal}
         patient={patient}
         patientBillingTypeId={additionalData?.patientBillingTypeId}
         data-testid="encountermodal-pnpe"
       />
+      <ExistingEncounterWarningModal open={isWarningModalOpen} onClose={onCloseWarningModal} />
     </>
   );
 });
