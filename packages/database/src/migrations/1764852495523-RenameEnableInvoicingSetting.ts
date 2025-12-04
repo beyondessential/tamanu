c30590539d439dedc6b485af92f20f48e9caf2e9
Rohan Port <rohan@bes.au>
2025-12-05 00:28:51 +1100

    feat(invoicing): SAV-1080: Rename setting features.enableInvoicing -> features.invoicing.enabled

---
+++ packages/database/src/migrations/1764852495523-RenameEnableInvoicingSetting.ts
@@ -0,0 +1,21 @@
+import { QueryInterface } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  await query.sequelize.query(
+    `UPDATE settings SET key = 'invoicing.enabled' WHERE key = 'features.enableInvoicing';`,
+  );
+
+  /**
+   * Have you handled the state of the sync_lookup table after running this migration?
+   * You can add the following query to rebuild the lookup table for the tables you have modified:
+   */
+  await query.sequelize.query(`SELECT flag_lookup_model_to_rebuild('settings');`);
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  await query.sequelize.query(
+    `UPDATE settings SET key = 'features.enableInvoicing' WHERE key = 'invoicing.enabled';`,
+  );
+
+  await query.sequelize.query(`SELECT flag_lookup_model_to_rebuild('settings');`);
+}
