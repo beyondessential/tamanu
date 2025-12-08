d0c164f239a33bfa43b71629d414c5d9da722a22
huytranbes <huy@bes.au>
2025-12-09 00:39:13 +0700

    feat: Enhance chart data queries and visualisation config handling

--- packages/web/app/api/queries/useChartsVisualisationConfigsQuery.js
+++ packages/web/app/api/queries/useChartsVisualisationConfigsQuery.js
@@ -1,5 +1,6 @@
+import { useQuery } from '@tanstack/react-query';
 import { useEncounter } from '../../contexts/Encounter';
-import { combineQueries } from '../combineQueries';
+import { combineQueries, isErrorUnknownAllow404s, useApi } from '../index';
 import { usePatientDataQuery } from './usePatientDataQuery';
 import { useSurveyQuery } from './useSurveyQuery';
 import { useChartData } from '../../contexts/ChartData';
@@ -8,15 +9,28 @@ import { getVisualisationConfig } from '../../utils/getVisualisationConfig';
 export const useChartsVisualisationConfigsQuery = () => {
   const encounterQuery = useEncounter();
   const { encounter } = encounterQuery;
+  const api = useApi();
 
   const patientQuery = usePatientDataQuery(encounter?.patientId);
   const { selectedChartTypeId } = useChartData();
   const chartSurveyQuery = useSurveyQuery(selectedChartTypeId);
+  
+  // Fetch chart data to determine which historical questions have data
+  const chartDataQuery = useQuery(
+    ['encounterCharts', encounter?.id, selectedChartTypeId],
+    () =>
+      api.get(
+        `encounter/${encounter?.id}/charts/${selectedChartTypeId}`,
+        { rowsPerPage: 1 },
+        { isErrorUnknown: isErrorUnknownAllow404s },
+      ),
+    { enabled: Boolean(encounter?.id) && Boolean(selectedChartTypeId) },
+  );
 
   const {
-    data: [patientData, surveyData],
+    data: [patientData, surveyData, chartData],
     ...restOfQuery
-  } = combineQueries([patientQuery, chartSurveyQuery]);
+  } = combineQueries([patientQuery, chartSurveyQuery, chartDataQuery]);
 
-  return getVisualisationConfig(patientData, surveyData, restOfQuery);
+  return getVisualisationConfig(patientData, surveyData, restOfQuery, chartData?.data || []);
 };
--- packages/web/app/api/queries/useProgramRegistryChartsVisualisationConfigsQuery.js
+++ packages/web/app/api/queries/useProgramRegistryChartsVisualisationConfigsQuery.js
@@ -1,16 +1,30 @@
-import { combineQueries } from '../combineQueries';
+import { useQuery } from '@tanstack/react-query';
+import { combineQueries, isErrorUnknownAllow404s, useApi } from '../index';
 import { usePatientDataQuery } from './usePatientDataQuery';
 import { useSurveyQuery } from './useSurveyQuery';
 import { getVisualisationConfig } from '../../utils/getVisualisationConfig';
 
 export const useProgramRegistryChartsVisualisationConfigsQuery = (patientId, chartSurveyId) => {
+  const api = useApi();
   const patientQuery = usePatientDataQuery(patientId);
   const chartSurveyQuery = useSurveyQuery(chartSurveyId);
+  
+  // Fetch chart data to determine which historical questions have data
+  const chartDataQuery = useQuery(
+    ['programRegistryPatientCharts', patientId, chartSurveyId],
+    () =>
+      api.get(
+        `programRegistry/patient/${patientId}/charts/${chartSurveyId}`,
+        { rowsPerPage: 1 },
+        { isErrorUnknown: isErrorUnknownAllow404s },
+      ),
+    { enabled: Boolean(patientId) && Boolean(chartSurveyId) },
+  );
 
   const {
-    data: [patientData, surveyData],
+    data: [patientData, surveyData, chartData],
     ...restOfQuery
-  } = combineQueries([patientQuery, chartSurveyQuery]);
+  } = combineQueries([patientQuery, chartSurveyQuery, chartDataQuery]);
 
-  return getVisualisationConfig(patientData, surveyData, restOfQuery);
+  return getVisualisationConfig(patientData, surveyData, restOfQuery, chartData?.data || []);
 };
