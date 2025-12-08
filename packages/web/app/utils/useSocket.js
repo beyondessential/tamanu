af903e5ba799ea0aa01153e4f755758ffe5961a6
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2025-12-09 12:12:41 +1300

    Update useSocket.js

--- packages/web/app/utils/useSocket.js
+++ packages/web/app/utils/useSocket.js
@@ -1,12 +1,12 @@
 import io from 'socket.io-client';
 import { useEffect, useState } from 'react';
-import config from 'config';
+// import config from 'config';
 
 let cachedSocket;
 
 export const useSocket = () => {
   const [socket] = useState(() => {
-    if (!config['socket.io'].enabled) return null;
+    // if (!config['socket.io'].enabled) return null;
     return (cachedSocket = io('', {
       path: '/api/socket.io/',
       transports: ['websocket'],
