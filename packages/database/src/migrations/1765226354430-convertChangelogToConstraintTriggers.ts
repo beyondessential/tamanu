e53840f2303f731e8135daa8548213f59bae74f0
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-09 15:53:13 +1300

    gottem

--- packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
+++ packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
@@ -1,6 +1,6 @@
 import { QueryInterface, QueryTypes } from 'sequelize';
 
-const getChangelogTriggerTables = (query: QueryInterface) => {
+const getStandardChangelogTriggers = (query: QueryInterface) => {
   return query.sequelize.query<{ schema: string; table: string }>(
     `
     SELECT 
@@ -11,6 +11,7 @@ const getChangelogTriggerTables = (query: QueryInterface) => {
     JOIN pg_namespace n ON c.relnamespace = n.oid
     WHERE t.tgname LIKE 'record_%_changelog'
     AND NOT t.tgisinternal
+    AND NOT t.tgisconstraint
     ORDER BY n.nspname, c.relname
     `,
     { type: QueryTypes.SELECT },
@@ -18,10 +19,11 @@ const getChangelogTriggerTables = (query: QueryInterface) => {
 };
 
 export async function up(query: QueryInterface): Promise<void> {
-  const tables = await getChangelogTriggerTables(query);
+  const tables = await getStandardChangelogTriggers(query);
+
+  if (tables.length === 0) return;
 
   for (const { schema, table } of tables) {
-    console.log(`Converting trigger record_${table}_changelog to constraint trigger`);
 
     await query.sequelize.query(
       `DROP TRIGGER IF EXISTS record_${table}_changelog ON "${schema}"."${table}"`,
@@ -37,21 +39,6 @@ export async function up(query: QueryInterface): Promise<void> {
   }
 }
 
-export async function down(query: QueryInterface): Promise<void> {
-  const tables = await getChangelogTriggerTables(query);
-
-  for (const { schema, table } of tables) {
-    console.log(`Reverting constraint trigger record_${table}_changelog to standard trigger`);
-
-    await query.sequelize.query(
-      `DROP TRIGGER IF EXISTS record_${table}_changelog ON "${schema}"."${table}"`,
-    );
-
-    await query.sequelize.query(`
-      CREATE TRIGGER record_${table}_changelog
-      AFTER INSERT OR UPDATE ON "${schema}"."${table}"
-      FOR EACH ROW
-      EXECUTE FUNCTION logs.record_change();
-    `);
-  }
+export async function down(): Promise<void> {
+  // no down migration
 }
