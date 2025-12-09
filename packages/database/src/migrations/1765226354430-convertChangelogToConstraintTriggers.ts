b12bf74e84d0558d6ed3aee9259afc6ee9532e78
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-09 16:05:42 +1300

    gottem

--- packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
+++ packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
@@ -11,7 +11,7 @@ const getStandardChangelogTriggers = (query: QueryInterface) => {
     JOIN pg_namespace n ON c.relnamespace = n.oid
     WHERE t.tgname LIKE 'record_%_changelog'
     AND NOT t.tgisinternal
-    AND NOT t.tgisconstraint
+    AND t.tgconstraint = 0
     ORDER BY n.nspname, c.relname
     `,
     { type: QueryTypes.SELECT },
@@ -20,15 +20,11 @@ const getStandardChangelogTriggers = (query: QueryInterface) => {
 
 export async function up(query: QueryInterface): Promise<void> {
   const tables = await getStandardChangelogTriggers(query);
-
   if (tables.length === 0) return;
-
   for (const { schema, table } of tables) {
-
     await query.sequelize.query(
       `DROP TRIGGER IF EXISTS record_${table}_changelog ON "${schema}"."${table}"`,
     );
-
     await query.sequelize.query(`
       CREATE CONSTRAINT TRIGGER record_${table}_changelog
       AFTER INSERT OR UPDATE ON "${schema}"."${table}"
