b51e3c7b6df4c4a46fd5849bf5fc57bd6fa17861
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-12 11:14:25 +1300

    ya

--- packages/mobile/jest.setup.ts
+++ packages/mobile/jest.setup.ts
@@ -3,15 +3,12 @@ import './__mocks__/react-native-gesture-handlerMock';
 import './__mocks__/TouchableOpacity.tsx';
 import './__mocks__/react-native__Libraries__Utilities__DevSettings';
 import './__mocks__/react-native-device-infoMock';
-import './__mocks__/react-native-quick-sqlite';
+import './__mocks__/react-native-nitro-sqlite';
 
 import { LogBox } from 'react-native';
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.useFakeTimers();
 
 LogBox.ignoreLogs([
-  'Expected style', // Expected style "fontSize: 26" to contain units
+  'Expected style',
 ]);
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
