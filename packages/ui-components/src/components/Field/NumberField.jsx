1cdc20c2fd6046482959978e9d301c416a8a5fa2
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2025-12-11 20:32:15 -0400

    Prevent changing values when scrolling on focused number inputs

--- packages/ui-components/src/components/Field/NumberField.jsx
+++ packages/ui-components/src/components/Field/NumberField.jsx
@@ -11,6 +11,12 @@ export const NumberInput = ({ min, max, step, inputProps = {}, ...props }) => (
       ...inputProps,
     }}
     type="number"
+    onWheel={(event) => {
+      // Prevents increasing/decreasing the value. It needs to be blurred because
+      // it's not possible to prevent the event from bubbling up to the parent element.
+      // This makes the element no longer focused and so the value is not changed.
+      event.target.blur();
+    }}
   />
 );
 
