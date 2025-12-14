acecb10133d92a76fa369e4e0e44fd59c8899704
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-15 09:09:10 +1300

    add down migration

--- packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
+++ packages/database/src/migrations/1765226354430-convertChangelogToConstraintTriggers.ts
@@ -23,6 +23,23 @@ const getNonConstraintChangelogTriggers = (query: QueryInterface) =>
     { type: QueryTypes.SELECT },
   );
 
+const getConstraintChangelogTriggers = (query: QueryInterface) =>
+  query.sequelize.query<{ schema: string; table: string }>(
+    `
+      SELECT 
+        n.nspname AS schema,
+        c.relname AS table
+      FROM pg_trigger t
+      JOIN pg_class c ON t.tgrelid = c.oid
+      JOIN pg_namespace n ON c.relnamespace = n.oid
+      WHERE t.tgname LIKE 'record_%_changelog'
+        AND NOT t.tgisinternal
+        AND t.tgconstraint <> 0
+      ORDER BY n.nspname, c.relname
+    `,
+    { type: QueryTypes.SELECT },
+  );
+
 export async function up(query: QueryInterface): Promise<void> {
   const tables = await getNonConstraintChangelogTriggers(query);
   if (!tables.length) return;
@@ -40,5 +57,16 @@ export async function up(query: QueryInterface): Promise<void> {
 }
 
 export async function down(): Promise<void> {
-  // No down migration
+  const tables = await getConstraintChangelogTriggers(query);
+  if (!tables.length) return;
+
+  for (const { schema, table } of tables) {
+    await query.sequelize.query(`DROP TRIGGER record_${table}_changelog ON "${schema}"."${table}"`);
+    await query.sequelize.query(`
+      CREATE TRIGGER record_${table}_changelog
+      AFTER INSERT OR UPDATE ON "${schema}"."${table}"
+      FOR EACH ROW
+      EXECUTE FUNCTION logs.record_change();
+    `);
+  }
 }
