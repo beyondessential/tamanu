4f5797c63eaf6c41c12487421beb3d9a292620ec
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-19 09:42:32 +1300

    cleanup

--- packages/mobile/App/ui/navigation/stacks/VaccineTableTabs.tsx
+++ packages/mobile/App/ui/navigation/stacks/VaccineTableTabs.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement, useEffect } from 'react';
 import Orientation from 'react-native-orientation-locker';
-import { Routes } from '/helpers/routes'; 
+import { Routes } from '/helpers/routes';
 import { VaccineHistoryTab } from '../screens/vaccine/tableTabs';
 import { createTopTabNavigator } from '/components/TopTabNavigator';
 import { TranslatedText } from '~/ui/components/Translations/TranslatedText';
