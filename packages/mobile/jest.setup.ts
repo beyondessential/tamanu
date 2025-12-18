2598856b7245f62e4babeddbb2bc9bf07dd752c2
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2025-12-18 13:14:19 +1300

    less dif plz

--- packages/mobile/jest.setup.ts
+++ packages/mobile/jest.setup.ts
@@ -5,13 +5,13 @@ import './__mocks__/react-native__Libraries__Utilities__DevSettings';
 import './__mocks__/react-native-device-infoMock';
 import './__mocks__/react-native-nitro-sqlite';
 
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import { LogBox } from 'react-native';
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
 jest.useFakeTimers();
 
 LogBox.ignoreLogs([
-  'Expected style',
+  'Expected style',  // Expected style "fontSize: 26" to contain units
 ]);
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
