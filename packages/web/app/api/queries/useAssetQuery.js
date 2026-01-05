1ae0f8c0a42cefc5914eb38dc84a38aee6208a05
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-01-05 18:58:08 -0400

    Include facilityId when querying for assets

--- packages/web/app/api/queries/useAssetQuery.js
+++ packages/web/app/api/queries/useAssetQuery.js
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { ASSET_FALLBACK_NAMES } from '@tamanu/constants';
 import { useApi } from '../useApi';
+import { useAuth } from '../../contexts/Auth';
 
 const queryResponseToDataURL = (response) => {
   const assetData = Buffer.from(response.data).toString('base64');
@@ -10,6 +11,7 @@ const queryResponseToDataURL = (response) => {
 
 export const useAssetQuery = (assetName) => {
   const api = useApi();
+  const { facilityId } = useAuth();
   const fallbackAssetName = ASSET_FALLBACK_NAMES[assetName];
 
   let dataURL = null;
@@ -20,13 +22,13 @@ export const useAssetQuery = (assetName) => {
     isFetched: assetFetched,
   } = useQuery({
     queryKey: ['asset', assetName],
-    queryFn: () => api.get(`asset/${assetName}`),
+    queryFn: () => api.get(`asset/${assetName}`, { facilityId }),
     enabled: Boolean(assetName),
   });
 
   const { data: fallbackQueryData, isFetching: isFallbackFetching } = useQuery({
     queryKey: ['asset', fallbackAssetName],
-    queryFn: () => api.get(`asset/${fallbackAssetName}`),
+    queryFn: () => api.get(`asset/${fallbackAssetName}`, { facilityId }),
     enabled: Boolean(fallbackAssetName) && assetFetched && !queryData?.data,
   });
 
