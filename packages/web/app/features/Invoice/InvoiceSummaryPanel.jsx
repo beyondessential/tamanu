f3486d2e90b63baa4c49c366159a60cb45cc06d3
Tom Caiger <caigertom@gmail.com>
2026-01-12 09:30:49 +1300

    feat(invoicing): SAV-1054: Palau sliding scale discounts (#8956)
    * submit discount
    
    * apply discount
    
    * Update InvoiceSummaryPanel.jsx
    
    * fix setting
    
    * Update InvoiceSummaryPanel.jsx

--- packages/web/app/features/Invoice/InvoiceSummaryPanel.jsx
+++ packages/web/app/features/Invoice/InvoiceSummaryPanel.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Divider } from '@material-ui/core';
+import { Divider, Button } from '@material-ui/core';
 import { getInvoiceSummary } from '@tamanu/shared/utils/invoice';
+import { useSettings } from '@tamanu/ui-components';
 import { Colors } from '../../constants';
 import { TranslatedText } from '../../components/Translation';
 import { Price } from './Price';
@@ -18,38 +19,134 @@ const Container = styled.div`
   margin-left: auto;
 `;
 
-const CardItem = styled.div`
+const Row = styled.div`
   display: flex;
   font-size: 14px;
   justify-content: space-between;
   align-items: flex-start;
+  margin-left: ${props => (props.$indent ? '16px' : 0)};
 `;
 
-const TotalCardItem = styled(CardItem)`
-  font-size: 16px;
+const TotalRow = styled(Row)`
+  font-size: ${({ $fontSize = '16px' }) => $fontSize};
   font-weight: 500;
 `;
 
-export const InvoiceSummaryPanel = ({ invoiceItems }) => {
-  const { invoiceItemsTotal, insuranceCoverageTotal, patientTotal } = getInvoiceSummary({
+const AddDiscountButton = styled(Button)`
+  padding-top: 0;
+  padding-bottom: 0;
+  margin-left: -8px;
+  text-transform: none;
+  font-weight: 500;
+  font-size: 14px;
+  color: ${props => props.theme.palette.primary.main};
+`;
+
+const RemoveDiscountButton = styled(Button)`
+  padding-top: 0;
+  padding-bottom: 0;
+  margin-left: -8px;
+  text-transform: none;
+  font-weight: 400;
+  font-size: 14px;
+  text-decoration: underline;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const SlidingFeeScaleSection = ({
+  openDiscountModal,
+  handleRemoveDiscount,
+  discountTotal,
+  patientSubtotal,
+  discountPercentage,
+}) => {
+  const discountDisplay = discountTotal > 0 ? discountTotal * -1 : 0;
+  return (
+    <>
+      <TotalRow $fontSize="14px">
+        <TranslatedText stringId="invoice.summary.patientSubtotal" fallback="Patient subtotal" />
+        <Price price={patientSubtotal} data-testid="patient-subtotal" />
+      </TotalRow>
+      <Divider />
+      <Row $indent>
+        <span>
+          <TranslatedText
+            stringId="invoice.summary.feeScaleAdjustment"
+            fallback="Fee scale adjustment"
+          />
+          {discountPercentage && ` - ${discountPercentage * 100}%`}
+        </span>
+        <Price price={discountDisplay} data-testid="fee-scale-adjustment" />
+      </Row>
+      <Row $indent>
+        {discountTotal ? (
+          <RemoveDiscountButton onClick={handleRemoveDiscount}>
+            <TranslatedText
+              stringId="invoice.summary.removeSlidingFeeScale"
+              fallback="Remove sliding fee scale"
+            />
+          </RemoveDiscountButton>
+        ) : (
+          <AddDiscountButton onClick={openDiscountModal}>
+            <TranslatedText
+              stringId="invoice.summary.applySlidingFeeScale"
+              fallback="Apply sliding fee scale"
+            />
+          </AddDiscountButton>
+        )}
+      </Row>
+      <Divider />
+    </>
+  );
+};
+
+export const InvoiceSummaryPanel = ({
+  invoiceItems,
+  openDiscountModal,
+  invoiceDiscount,
+  handleRemoveDiscount,
+}) => {
+  const { getSetting } = useSettings();
+  const slidingFeeScaleEnabled = getSetting('features.invoicing.slidingFeeScale');
+
+  const {
+    invoiceItemsTotal,
+    insuranceCoverageTotal,
+    patientTotal,
+    patientSubtotal,
+    discountTotal,
+  } = getInvoiceSummary({
     items: invoiceItems,
+    discount: slidingFeeScaleEnabled ? invoiceDiscount : null,
   });
-  const coverageDisplay = insuranceCoverageTotal * -1;
+  const coverageDisplay = insuranceCoverageTotal > 0 ? insuranceCoverageTotal * -1 : 0;
+
   return (
     <Container>
-      <CardItem>
+      <Row>
         <TranslatedText stringId="invoice.summary.invoiceTotal" fallback="Invoice total" />
         <Price price={invoiceItemsTotal} data-testid="translatedtext-828s" />
-      </CardItem>
-      <CardItem>
+      </Row>
+      <Row>
         <TranslatedText stringId="invoice.summary.insuranceTotal" fallback="Insurance coverage" />
         <Price price={coverageDisplay} data-testid="translatedtext-qedx" />
-      </CardItem>
+      </Row>
       <Divider />
-      <TotalCardItem>
+      {slidingFeeScaleEnabled && (
+        <SlidingFeeScaleSection
+          openDiscountModal={openDiscountModal}
+          patientSubtotal={patientSubtotal}
+          discountTotal={discountTotal}
+          handleRemoveDiscount={handleRemoveDiscount}
+          discountPercentage={invoiceDiscount?.percentage}
+        />
+      )}
+      <TotalRow>
         <TranslatedText stringId="invoice.summary.patientTotal" fallback="Patient total due" />
         <Price price={patientTotal} data-testid="translatedtext-nst0" />
-      </TotalCardItem>
+      </TotalRow>
     </Container>
   );
 };
