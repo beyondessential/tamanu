bd0e4bac72b8f31d3b7332100d4d0761f5dfe6c9
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-01-13 15:27:14 +1300

    ya prolly gud call

--- packages/ui-components/src/components/Suggester/useSuggester.js
+++ packages/ui-components/src/components/Suggester/useSuggester.js
@@ -21,10 +21,14 @@ export const useSuggester = (type, options) => {
 export const usePatientSuggester = () => {
   const api = useApi();
   const { formatShort } = useDateTimeFormat();
-  return new Suggester(api, 'patient', {
-    formatter: ({ id, ...patient }) => ({
-      label: `${getPatientNameAsString(patient)}(${patient.displayId}) - ${patient.sex} - ${formatShort(patient.dateOfBirth)}`,
-      value: id,
-    }),
-  });
+  return useMemo(
+    () =>
+      new Suggester(api, 'patient', {
+        formatter: ({ id, ...patient }) => ({
+          label: `${getPatientNameAsString(patient)}(${patient.displayId}) - ${patient.sex} - ${formatShort(patient.dateOfBirth)}`,
+          value: id,
+        }),
+      }),
+    [api, formatShort],
+  );
 };
