9ea7182f9213d4104b6e2232341f786128bed743
Rohan Port <rohan@bes.au>
2026-01-15 11:07:39 +1100

    feat(fhir): NASS-1872: Fixed error handling in transactionBundle

--- packages/central-server/app/hl7fhir/materialised/bundle/transactionBundle.js
+++ packages/central-server/app/hl7fhir/materialised/bundle/transactionBundle.js
@@ -4,6 +4,7 @@ import { FhirTransactionBundle } from '@tamanu/shared/services/fhirTypes';
 import { OperationOutcome } from '@tamanu/shared/utils/fhir';
 import { resourcesThatCanDo } from '@tamanu/shared/utils/fhir/resources';
 import { createResource } from '../create';
+import { InvalidOperationError } from '@tamanu/errors';
 
 export const transactionBundleHandler = () => {
   return asyncHandler(async (req, res) => {
@@ -18,20 +19,22 @@ export const transactionBundleHandler = () => {
     const createableResources = resourcesThatCanDo(store.models, FHIR_INTERACTIONS.TYPE.CREATE);
 
     // Run all operations in a database transaction to ensure atomicity
-    await store.sequelize.transaction(async () => {
-      for (const { resource: rawResource } of validatedBundle.entry) {
-        const FhirResource = createableResources.find(r => r.fhirName === rawResource.resourceType);
-        if (!FhirResource) {
-          return res
-            .status(400)
-            .send(
-              OperationOutcome.fromMessage(`Resource type ${rawResource.resourceType} not found`),
-            );
-        }
+    try {
+      await store.sequelize.transaction(async () => {
+        for (const { resource: rawResource } of validatedBundle.entry) {
+          const FhirResource = createableResources.find(
+            r => r.fhirName === rawResource.resourceType,
+          );
+          if (!FhirResource) {
+            throw new InvalidOperationError(`Resource type ${rawResource.resourceType} not found`);
+          }
 
-        await createResource(store, FhirResource, rawResource, req.user?.id, settings);
-      }
-    });
+          await createResource(store, FhirResource, rawResource, req.user?.id, settings);
+        }
+      });
+    } catch (err) {
+      return res.status(400).send(new OperationOutcome([err]));
+    }
 
     // TODO: generate ID here, and return it (if resource can be materialised)
     res.status(201).send();
