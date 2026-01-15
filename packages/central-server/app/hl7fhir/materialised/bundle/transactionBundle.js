b98213edf877d9548c193df5921e1543203a4e1e
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2026-01-16 10:49:51 +1100

    feat(fhir): NASS-1885: Added FHIR /Bundle endpoint (#8975)

---
+++ packages/central-server/app/hl7fhir/materialised/bundle/transactionBundle.js
@@ -0,0 +1,51 @@
+import asyncHandler from 'express-async-handler';
+import { FHIR_BUNDLE_TYPES, FHIR_INTERACTIONS } from '@tamanu/constants';
+import {
+  FhirTransactionBundle,
+  FhirTransactionResponseBundle,
+} from '@tamanu/shared/services/fhirTypes';
+import { OperationOutcome } from '@tamanu/shared/utils/fhir';
+import { resourcesThatCanDo } from '@tamanu/shared/utils/fhir/resources';
+import { createResource } from '../create';
+import { InvalidOperationError } from '@tamanu/errors';
+
+export const transactionBundleHandler = () => {
+  return asyncHandler(async (req, res) => {
+    const { settings, store } = req;
+    let validatedBundle;
+    try {
+      validatedBundle = await FhirTransactionBundle.SCHEMA().validate(req.body);
+    } catch (err) {
+      return res.status(400).send(OperationOutcome.fromYupError(err));
+    }
+
+    const creatableResources = resourcesThatCanDo(store.models, FHIR_INTERACTIONS.TYPE.CREATE);
+
+    // Run all operations in a database transaction to ensure atomicity
+    try {
+      await store.sequelize.transaction(async () => {
+        for (const { resource: rawResource } of validatedBundle.entry) {
+          const FhirResource = creatableResources.find(
+            r => r.fhirName === rawResource.resourceType,
+          );
+          if (!FhirResource) {
+            throw new InvalidOperationError(`Resource type ${rawResource.resourceType} not found`);
+          }
+
+          await createResource(store, FhirResource, rawResource, req.user?.id, settings);
+        }
+      });
+    } catch (err) {
+      return res.status(400).send(new OperationOutcome([err]).asFhir());
+    }
+
+    const responseBundle = new FhirTransactionResponseBundle({
+      resourceType: 'Bundle',
+      type: FHIR_BUNDLE_TYPES.TRANSACTION_RESPONSE,
+      response: {
+        status: '201',
+      },
+    });
+    res.status(200).send(responseBundle);
+  });
+};
--- packages/central-server/app/hl7fhir/materialised/create.js
+++ packages/central-server/app/hl7fhir/materialised/create.js
@@ -12,35 +12,38 @@ async function mapErr(promise, fn) {
   }
 }
 
+export const createResource = async (store, FhirResource, data, requesterId, settings) => {
+  const { FhirJob } = store.models;
+  const validated = await mapErr(
+    FhirResource.INTAKE_SCHEMA.shape({
+      resourceType: yup
+        .string()
+        .test(
+          'is-same-as-route',
+          `must be '${FhirResource.fhirName}'`,
+          t => t === FhirResource.fhirName,
+        )
+        .required(),
+    }).validate(data, { stripUnknown: true }),
+    err => OperationOutcome.fromYupError(err),
+  );
+
+  const resource = new FhirResource(validated);
+  const upstream = await resource.pushUpstream({
+    settings,
+    requesterId,
+  });
+  if (FhirResource.CAN_DO.has(FHIR_INTERACTIONS.INTERNAL.MATERIALISE)) {
+    FhirJob.submit(JOB_TOPICS.FHIR.REFRESH.FROM_UPSTREAM, {
+      resource: FhirResource.fhirName,
+      upstreamId: upstream.id,
+    });
+  }
+};
+
 export function createHandler(FhirResource) {
   return asyncHandler(async (req, res) => {
-    const { settings, store } = req;
-    const { FhirJob } = store.models;
-    const validated = await mapErr(
-      FhirResource.INTAKE_SCHEMA.shape({
-        resourceType: yup
-          .string()
-          .test(
-            'is-same-as-route',
-            `must be '${FhirResource.fhirName}'`,
-            t => t === FhirResource.fhirName,
-          )
-          .required(),
-      }).validate(req.body, { stripUnknown: true }),
-      err => OperationOutcome.fromYupError(err),
-    );
-
-    const resource = new FhirResource(validated);
-    const upstream = await resource.pushUpstream({
-      settings,
-      requesterId: req.user?.id,
-    });
-    if (FhirResource.CAN_DO.has(FHIR_INTERACTIONS.INTERNAL.MATERIALISE)) {
-      FhirJob.submit(JOB_TOPICS.FHIR.REFRESH.FROM_UPSTREAM, {
-        resource: FhirResource.fhirName,
-        upstreamId: upstream.id,
-      });
-    }
+    await createResource(req.store, FhirResource, req.body, req.user?.id, req.settings);
 
     // in spec, we should Location: to the resource, but we don't have it yet
     // TODO: generate ID here, and return it (if resource can be materialised)
