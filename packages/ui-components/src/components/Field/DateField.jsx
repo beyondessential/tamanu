04c1651f9de1de39bebe2e39f0d0b35a7a47371e
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-01-16 14:20:28 +1300

    this gets own pr

--- packages/ui-components/src/components/Field/DateField.jsx
+++ packages/ui-components/src/components/Field/DateField.jsx
@@ -8,6 +8,7 @@ import { format as formatDate, toDateString, toDateTimeString } from '@tamanu/ut
 import { TextInput } from './TextField';
 import { TAMANU_COLORS } from '../../constants';
 import { DefaultIconButton } from '../Button';
+import { useDateTimeFormat } from '../../contexts';
 
 // This component is pretty tricky! It has to keep track of two layers of state:
 //
@@ -40,11 +41,6 @@ const CustomIconTextInput = styled(TextInput)`
   }
 `;
 
-function fromRFC3339(rfc3339Date, format) {
-  if (!rfc3339Date) return '';
-  return formatDate(rfc3339Date, format);
-}
-
 export const DateInput = ({
   type = 'date',
   value,
@@ -62,7 +58,24 @@ export const DateInput = ({
 }) => {
   delete props.placeholder;
 
-  const [currentText, setCurrentText] = useState(fromRFC3339(value, format));
+  const { formatDateTimeLocal } = useDateTimeFormat();
+
+  const formatInitialValue = useCallback(
+    val => {
+      if (!val) return '';
+      switch (type) {
+        case 'datetime-local':
+          return formatDateTimeLocal(val) || '';
+        case 'date':
+          return formatDate(val, format);
+        default:
+          return formatDate(val, format);
+      }
+    },
+    [type, format, formatDateTimeLocal],
+  );
+
+  const [currentText, setCurrentText] = useState(() => formatInitialValue(value));
   const [isPlaceholder, setIsPlaceholder] = useState(!value);
 
   // Weird thing alert:
@@ -165,7 +178,7 @@ export const DateInput = ({
   };
 
   useEffect(() => {
-    const formattedValue = fromRFC3339(value, format);
+    const formattedValue = formatInitialValue(value);
     if (value && formattedValue) {
       setCurrentText(formattedValue);
       setIsPlaceholder(false);
@@ -174,7 +187,7 @@ export const DateInput = ({
       setCurrentText('');
       setIsPlaceholder(true);
     };
-  }, [value, format]);
+  }, [value, formatInitialValue]);
 
   // We create two copies of the DateField component, so that we can have a temporary one visible
   // during remount (for more on that, see the remounting description at the top of this component)
