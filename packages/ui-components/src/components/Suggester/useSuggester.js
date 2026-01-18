6993ae17ca0b914146f2fef28d7054f45f62fd9c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-01-19 12:53:42 +1300

    keep this for web pr too

--- packages/ui-components/src/components/Suggester/useSuggester.js
+++ packages/ui-components/src/components/Suggester/useSuggester.js
@@ -2,7 +2,7 @@ import { useMemo } from 'react';
 import { useAuth, useApi } from '../../contexts';
 import { Suggester } from './Suggester';
 import { getPatientNameAsString } from '../PatientNameDisplay';
-import { useDateTimeFormat } from '../../contexts/DateTimeContext';
+import { DateDisplay } from '../DateDisplay';
 
 export const useSuggester = (type, options) => {
   const api = useApi();
@@ -20,15 +20,12 @@ export const useSuggester = (type, options) => {
 
 export const usePatientSuggester = () => {
   const api = useApi();
-  const { formatShort } = useDateTimeFormat();
-  return useMemo(
-    () =>
-      new Suggester(api, 'patient', {
-        formatter: ({ id, ...patient }) => ({
-          label: `${getPatientNameAsString(patient)}(${patient.displayId}) - ${patient.sex} - ${formatShort(patient.dateOfBirth)}`,
-          value: id,
-        }),
-      }),
-    [api, formatShort],
-  );
+  return new Suggester(api, 'patient', {
+    formatter: ({ id, ...patient }) => ({
+      label: `${getPatientNameAsString(patient)} (${patient.displayId}) - ${
+        patient.sex
+      } - ${DateDisplay.stringFormat(patient.dateOfBirth)}`,
+      value: id,
+    }),
+  });
 };
