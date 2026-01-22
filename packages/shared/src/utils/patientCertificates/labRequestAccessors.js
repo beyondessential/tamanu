6a6fe2f7492d12261671eb65da8540fe644ad67c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-01-23 10:05:50 +1300

    done?

--- packages/shared/src/utils/patientCertificates/labRequestAccessors.js
+++ packages/shared/src/utils/patientCertificates/labRequestAccessors.js
@@ -1,13 +1,13 @@
-export const getCompletedDate = ({ completedDate }, _, { formatShortExplicit }) =>
+export const getCompletedDate = ({ completedDate }, _getLocalisation, _getSetting, { formatShortExplicit }) =>
   completedDate ? formatShortExplicit(completedDate) : 'Unknown';
 
-export const getDateOfSwab = ({ sampleTime }, _, { formatShortExplicit }) =>
+export const getDateOfSwab = ({ sampleTime }, _getLocalisation, _getSetting, { formatShortExplicit }) =>
   sampleTime ? formatShortExplicit(sampleTime) : 'Unknown';
 
-export const getTimeOfSwab = ({ sampleTime }, _, { formatTime }) =>
+export const getTimeOfSwab = ({ sampleTime }, _getLocalisation, _getSetting, { formatTime }) =>
   sampleTime ? formatTime(sampleTime) : 'Unknown';
 
-export const getLaboratory = ({ laboratory }, _, getSetting) =>
+export const getLaboratory = ({ laboratory }, _getLocalisation, getSetting) =>
   laboratory?.name || getSetting('templates.covidTestCertificate.laboratoryName');
 
 export const getLabMethod = ({ labTestMethod }) => (labTestMethod || {}).name || 'Unknown';
