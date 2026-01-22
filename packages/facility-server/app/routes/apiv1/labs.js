c7b5527c557173d47283a171a510c39109dedc17
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2026-01-23 11:50:12 +1300

    Update labs.js

--- packages/facility-server/app/routes/apiv1/labs.js
+++ packages/facility-server/app/routes/apiv1/labs.js
@@ -626,8 +626,9 @@ labTest.get(
       }
     }
 
-    // Remove the last entry, which is the initial empty result
-    distinctChanges.pop();
+    // Remove the last entry only if it's an empty result (initial creation without a result)
+    const lastEntry = distinctChanges.at(-1);
+    if (!lastEntry.result) distinctChanges.pop();
     res.send(distinctChanges);
   }),
 );
