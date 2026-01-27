ff1b791048fc18e7874992c45c2b4103af0f732b
Tom Caiger <caigertom@gmail.com>
2026-01-27 14:57:10 +1300

    Update InvoiceSummaryPanel.jsx

--- packages/web/app/features/Invoice/InvoiceSummaryPanel.jsx
+++ packages/web/app/features/Invoice/InvoiceSummaryPanel.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { Divider, Button } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
 import { getInvoiceSummary } from '@tamanu/shared/utils/invoice';
-import { useSettings } from '@tamanu/ui-components';
-import { INVOICE_STATUSES } from '@tamanu/constants';
 import { Colors } from '../../constants';
 import { TranslatedText } from '../../components/Translation';
 import { Price } from './Price';
-import { InvoiceDiscountModal } from './InvoiceDiscountModal/InvoiceDiscountModal';
-import { useUpdateInvoice } from '../../api/mutations/useInvoiceMutation';
 
 const Container = styled.div`
   align-self: start;
@@ -34,121 +30,14 @@ const TotalRow = styled(Row)`
   font-weight: 500;
 `;
 
-const AddDiscountButton = styled(Button)`
-  padding-top: 0;
-  padding-bottom: 0;
-  margin-left: -8px;
-  text-transform: none;
-  font-weight: 500;
-  font-size: 14px;
-  color: ${props => props.theme.palette.primary.main};
-`;
-
-const RemoveDiscountButton = styled(Button)`
-  padding-top: 0;
-  padding-bottom: 0;
-  margin-left: -8px;
-  text-transform: none;
-  font-weight: 400;
-  font-size: 14px;
-  text-decoration: underline;
-  &:hover {
-    text-decoration: underline;
-  }
-`;
-
-const SlidingFeeScaleSection = ({
-  invoice,
-  discountTotal,
-  patientSubtotal,
-  discountPercentage,
-}) => {
-  const [discountModalOpen, setDiscountModalOpen] = useState(false);
-  const { mutate: updateInvoice } = useUpdateInvoice(invoice);
-
-  const onClose = () => {
-    setDiscountModalOpen(false);
-  };
-
-  const onOpen = () => {
-    setDiscountModalOpen(true);
-  };
-
-  const handleUpdateDiscount = discountData => {
-    updateInvoice({ ...invoice, discount: discountData });
-    onClose();
-  };
-
-  const handleRemoveDiscount = () => {
-    updateInvoice({ ...invoice, discount: null });
-    onClose();
-  };
-
-  const inProgress = invoice.status === INVOICE_STATUSES.IN_PROGRESS;
-  const discountDisplay = discountTotal > 0 ? discountTotal * -1 : 0;
-
-  return (
-    <>
-      <TotalRow $fontSize="14px">
-        <TranslatedText stringId="invoice.summary.patientSubtotal" fallback="Patient subtotal" />
-        <Price price={patientSubtotal} data-testid="patient-subtotal" />
-      </TotalRow>
-      <Divider />
-      <Row $indent>
-        <span>
-          <TranslatedText
-            stringId="invoice.summary.feeScaleAdjustment"
-            fallback="Fee scale adjustment"
-          />
-          {discountPercentage && ` - ${discountPercentage * 100}%`}
-        </span>
-        <Price price={discountDisplay} data-testid="fee-scale-adjustment" />
-      </Row>
-      {inProgress && (
-        <Row $indent>
-          {discountTotal ? (
-            <RemoveDiscountButton onClick={handleRemoveDiscount}>
-              <TranslatedText
-                stringId="invoice.summary.removeSlidingFeeScale"
-                fallback="Remove sliding fee scale"
-              />
-            </RemoveDiscountButton>
-          ) : (
-            <AddDiscountButton onClick={onOpen}>
-              <TranslatedText
-                stringId="invoice.summary.applySlidingFeeScale"
-                fallback="Apply sliding fee scale"
-              />
-            </AddDiscountButton>
-          )}
-        </Row>
-      )}
-      <Divider />
-      <InvoiceDiscountModal
-        open={discountModalOpen}
-        onClose={onClose}
-        handleUpdateDiscount={handleUpdateDiscount}
-      />
-    </>
-  );
-};
-
 export const InvoiceSummaryPanel = ({ invoice }) => {
-  const { getSetting } = useSettings();
-  const slidingFeeScaleEnabled = getSetting('features.invoicing.slidingFeeScale');
-  const invoiceDiscount = invoice.discount;
-
   const {
     invoiceItemsTotal,
     insuranceCoverageTotal,
-    patientSubtotal,
-    discountTotal,
     patientPaymentsTotal,
     patientPaymentRemainingBalance,
-  } = getInvoiceSummary({
-    ...invoice,
-    discount: slidingFeeScaleEnabled ? invoiceDiscount : null,
-  });
+  } = getInvoiceSummary(invoice);
+
   const coverageDisplay = insuranceCoverageTotal > 0 ? insuranceCoverageTotal * -1 : 0;
   const patientPaymentsTotalDisplay = patientPaymentsTotal > 0 ? patientPaymentsTotal * -1 : 0;
 
@@ -167,15 +56,6 @@ export const InvoiceSummaryPanel = ({ invoice }) => {
         <Price price={patientPaymentsTotalDisplay} data-testid="invoice-summary-patientPayments" />
       </Row>
       <Divider />
-      {/* Todo: Include payments total in sliding fee scale section*/}
-      {slidingFeeScaleEnabled && (
-        <SlidingFeeScaleSection
-          patientSubtotal={patientSubtotal}
-          discountTotal={discountTotal}
-          discountPercentage={invoiceDiscount?.percentage}
-          invoice={invoice}
-        />
-      )}
       <TotalRow>
         <TranslatedText stringId="invoice.summary.patientTotal" fallback="Patient total due" />
         <Price price={patientPaymentRemainingBalance} data-testid="translatedtext-nst0" />
