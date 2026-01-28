56171506f35f64fac3cbecc175c4e1586922ffea
Rohan Port <rohan@bes.au>
2026-01-28 17:24:47 +1100

    fix(fhir): TAMOC-382: Updated migration to be pg 12 compliant

--- packages/database/src/migrations/1769479133874-AddJobRecordAsPayloadToFhirJobNotification.ts
+++ packages/database/src/migrations/1769479133874-AddJobRecordAsPayloadToFhirJobNotification.ts
@@ -2,7 +2,8 @@ import { QueryInterface } from 'sequelize';
 
 export async function up(query: QueryInterface): Promise<void> {
   await query.sequelize.query(`
-    CREATE OR REPLACE TRIGGER fhir_jobs_insert_trigger after
+    DROP TRIGGER IF EXISTS fhir_jobs_insert_trigger ON fhir.jobs;
+    CREATE TRIGGER fhir_jobs_insert_trigger after
     INSERT
         ON
         fhir.jobs FOR EACH ROW EXECUTE FUNCTION fhir.jobs_notify();
@@ -41,7 +42,8 @@ export async function down(query: QueryInterface): Promise<void> {
   `);
 
   await query.sequelize.query(`
-    CREATE OR REPLACE TRIGGER fhir_jobs_insert_trigger
+    DROP TRIGGER IF EXISTS fhir_jobs_insert_trigger ON fhir.jobs;
+    CREATE TRIGGER fhir_jobs_insert_trigger
     AFTER INSERT ON fhir.jobs FOR EACH STATEMENT
     EXECUTE FUNCTION fhir.jobs_notify()
   `);
