19c3107cb9c61de457dc2476fb1f58cc6c6519d5
Thomas Codling <58054247+tcodling@users.noreply.github.com>
2026-01-29 08:58:26 +1300

    typo

--- packages/web/app/components/PatientPrinting/printouts/EncounterRecord.jsx
+++ packages/web/app/components/PatientPrinting/printouts/EncounterRecord.jsx
@@ -297,7 +297,7 @@ const COLUMNS = {
       style: { width: '20%' },
     },
     {
-      key: 'insructions',
+      key: 'instructions',
       title: 'Instructions',
       accessor: ({ prescription }) => prescription || '',
       style: { width: '30%' },
