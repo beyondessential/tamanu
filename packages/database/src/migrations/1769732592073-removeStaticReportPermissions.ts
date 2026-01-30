b869a91c5b670f60733e9d368e6a66fc09ef02f8
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-01-30 13:27:36 +1300

    just testing

---
+++ packages/database/src/migrations/1769732592073-removeStaticReportPermissions.ts
@@ -0,0 +1,15 @@
+import { QueryInterface } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+    await query.sequelize.query(
+      `
+        DELETE FROM permissions
+        WHERE noun = 'StaticReport'
+          AND (object_id IS NULL OR object_id != 'generic-survey-export-line-list');
+      `,
+    );
+}
+
+export async function down(_query: QueryInterface): Promise<void> {
+  // Cannot restore deleted permissions as we don't have a record of what they were
+}
