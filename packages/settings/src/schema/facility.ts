15f1d33d7457261cc968baea98c7df3e367bd75b
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-02-04 16:29:22 -0400

    Include blank default schema for settings

--- packages/settings/src/schema/facility.ts
+++ packages/settings/src/schema/facility.ts
@@ -83,6 +83,59 @@ export const facilitySettings = {
         },
       },
     },
+    integrations: {
+      description: 'Integrations with external services',
+      properties: {
+        mSupplyMed: {
+          description: 'mSupplyMed settings',
+          properties: {
+            host: {
+              description: 'The host of the open mSupply instance',
+              type: yup
+                .string()
+                .matches(/^(?!.*\/$).*$/, 'Host URL must not end with a forward slash'),
+              defaultValue: '',
+            },
+            storeId: {
+              description: 'The ID of the store in the open mSupply instance',
+              type: yup.string(),
+              defaultValue: '',
+            },
+            customerId: {
+              description: 'The ID of the Tamanu customer in the open mSupply instance',
+              type: yup.string(),
+              defaultValue: '',
+            },
+            backoff: {
+              name: 'Backoff',
+              description: 'Backoff settings',
+              properties: {
+                maxAttempts: {
+                  name: 'Max attempts',
+                  description: 'The maximum number of connection attempts',
+                  type: yup.number().integer().positive(),
+                  defaultValue: 15,
+                },
+                multiplierMs: {
+                  name: 'Multiplier',
+                  description: 'The multiplier for the delay between retries',
+                  type: yup.number().integer().positive(),
+                  defaultValue: 300,
+                  unit: 'ms',
+                },
+                maxWaitMs: {
+                  name: 'Max wait',
+                  description: 'The delay between retries',
+                  type: yup.number().integer().positive(),
+                  defaultValue: 10000,
+                  unit: 'ms',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
     questionCodeIds: {
       deprecated: true,
       description: questionCodeIdsDescription,
