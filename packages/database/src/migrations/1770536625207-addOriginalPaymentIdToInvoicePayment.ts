1b7b554b180261e58ee4de82fd2f9c488b5cf80a
Chris Tran <chris@beyondessential.com.au>
2026-02-09 10:27:38 +1100

    add original_payment_id

---
+++ packages/database/src/migrations/1770536625207-addOriginalPaymentIdToInvoicePayment.ts
@@ -0,0 +1,17 @@
+import { DataTypes, QueryInterface } from 'sequelize';
+
+export async function up(query: QueryInterface): Promise<void> {
+  await query.addColumn('invoice_payments', 'original_payment_id', {
+    type: DataTypes.TEXT,
+    allowNull: true,
+    unique: true,
+    references: {
+      model: 'invoice_payments',
+      key: 'id',
+    },
+  });
+}
+
+export async function down(query: QueryInterface): Promise<void> {
+  await query.removeColumn('invoice_payments', 'original_payment_id');
+}
