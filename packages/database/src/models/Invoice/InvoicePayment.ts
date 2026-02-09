f2cfc048de8c813de585b0f4d4aae7fc66bfccdc
Chris Tran <chris@beyondessential.com.au>
2026-02-09 11:03:17 +1100

    switched to uuid

--- packages/database/src/models/Invoice/InvoicePayment.ts
+++ packages/database/src/models/Invoice/InvoicePayment.ts
@@ -33,7 +33,7 @@ export class InvoicePayment extends Model {
           allowNull: false,
         },
         originalPaymentId: {
-          type: DataTypes.TEXT,
+          type: DataTypes.UUID,
           allowNull: true,
           unique: true,
         },
