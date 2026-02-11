15fc8b606bc64f6519c277c6fc0513af1f905af9
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-02-12 09:58:45 +1300

    ya

--- packages/web/app/api/queries/useEncounterMedicationQuery.js
+++ packages/web/app/api/queries/useEncounterMedicationQuery.js
@@ -1,12 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../index';
+import { useAuth } from '../../contexts/Auth';
 
 export const useEncounterMedicationQuery = (encounterId, fetchOptions) => {
+  const { facilityId } = useAuth();
   const api = useApi();
 
   return useQuery(
     ['encounterMedication', encounterId, fetchOptions],
-    () => api.get(`encounter/${encodeURIComponent(encounterId)}/medications`, fetchOptions),
+    () =>
+      api.get(`encounter/${encodeURIComponent(encounterId)}/medications`, {
+        facilityId,
+        ...fetchOptions,
+      }),
     { enabled: !!encounterId },
   );
 };
