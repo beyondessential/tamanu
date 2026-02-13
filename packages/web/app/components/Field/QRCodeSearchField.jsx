49909a085225e7d4ec715dbde3e574a861eaa5d3
Tom Caiger <caigertom@gmail.com>
2026-02-13 16:32:22 +1300

    Update QRCodeSearchField.jsx

--- packages/web/app/components/Field/QRCodeSearchField.jsx
+++ packages/web/app/components/Field/QRCodeSearchField.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { IconButton } from '@material-ui/core';
 import styled from 'styled-components';
 import { QRCodeIcon } from '../Icons';
@@ -26,72 +26,21 @@ const StyledIconButton = styled(IconButton)`
 `;
 
 export const QRCodeSearchField = props => {
-  const {
-    field: { value = '', name, onChange: fieldOnChange } = {},
-    form: { setFieldValue } = {},
-  } = props;
-
-  const [localValue, setLocalValue] = useState(value);
-
-  useEffect(() => {
-    setLocalValue(value);
-  }, [value]);
-
-  const commitValue = nextValue => {
-    setLocalValue(nextValue);
-    setFieldValue?.(name, nextValue);
-  };
-
-  const handleChange = e => {
-    const nextValue = e?.target?.value ?? '';
-    setLocalValue(nextValue);
-
-    // Prefer setFieldValue when present; otherwise fall back to field's onChange
-    if (setFieldValue) {
-      setFieldValue(name, nextValue);
-    } else {
-      fieldOnChange?.(e);
-    }
-  };
-
-  const handleClear = () => {
-    commitValue('');
-  };
-
-  const handleScan = qrValue => {
-    commitValue(qrValue);
-  };
-
-  return (
-    <QRCodeSearchInput
-      {...props}
-      name={name}
-      value={localValue}
-      onChange={handleChange}
-      onClear={handleClear}
-      onQRScan={handleScan}
-    />
-  );
-};
-
-export const QRCodeSearchInput = ({ value, onChange, name, onQRScan, ...props }) => {
   const { getTranslation } = useTranslation();
   const [qrModalOpen, setQrModalOpen] = useState(false);
+  const { field: { onChange, name, value } = {}, form: { setFieldValue } = {} } = props;
 
-  const applyScannedValue = qrValue => {
-    if (onQRScan) {
-      onQRScan(qrValue);
-      return;
-    }
+  const onClear = () => {
+    setFieldValue(name, '');
+  };
 
-    if (onChange) {
-      onChange({ target: { name, value: qrValue } });
-    }
+  const applyScannedValue = qrCodeValue => {
+    setFieldValue(name, qrCodeValue);
   };
 
   return (
     <Container>
-      <SearchInput {...props} name={name} value={value} onChange={onChange} />
+      <SearchInput {...props} onChange={onChange} name={name} value={value} onClear={onClear} />
       <ThemedTooltip title={getTranslation('general.action.scanQRCode', 'Scan QR code')}>
         <StyledIconButton onClick={() => setQrModalOpen(true)} data-testid="qr-scanner-button">
           <QRCodeIcon />
