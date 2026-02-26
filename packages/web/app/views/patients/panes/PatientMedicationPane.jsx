c8af0a38e7053dbef91f49562d60eb3aab94abd1
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-02-26 15:47:28 +1300

    ok

--- packages/web/app/views/patients/panes/PatientMedicationPane.jsx
+++ packages/web/app/views/patients/panes/PatientMedicationPane.jsx
@@ -254,7 +254,7 @@ const ONGOING_MEDICATION_COLUMNS = (getTranslation, getEnumTranslation) => [
     key: 'date',
     title: <TranslatedText stringId="patient.medication.table.column.date" fallback="Date" />,
     accessor: data => (
-      <CellText discontinued={data?.discontinued}><DateDisplay date={trimToDate(data.date)} format="shortest" /></CellText>
+      <CellText discontinued={data?.discontinued}><DateDisplay date={data.date} format="shortest" /></CellText>
     ),
     sortable: true,
   },
