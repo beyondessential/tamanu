bb2057fad51501e3da78130c15c2ce83f6664429
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-02-26 15:49:03 +1300

    thats correcT

--- packages/web/app/views/patients/panes/PatientMedicationPane.jsx
+++ packages/web/app/views/patients/panes/PatientMedicationPane.jsx
@@ -35,6 +35,7 @@ import { getMedicationLabelData, getTranslatedMedicationName } from '../../../ut
 import { useApi } from '../../../api';
 import { SendToPharmacyIcon } from '../../../assets/icons/SendToPharmacyIcon';
 import { useSettings } from '../../../contexts/Settings';
+import { trimToDate } from '@tamanu/utils/dateTime';
 
 const NotifyBanner = styled(Box)`
   padding: 13px 22px;
@@ -253,7 +254,7 @@ const ONGOING_MEDICATION_COLUMNS = (getTranslation, getEnumTranslation) => [
     key: 'date',
     title: <TranslatedText stringId="patient.medication.table.column.date" fallback="Date" />,
     accessor: data => (
-      <CellText discontinued={data?.discontinued}><DateDisplay date={data.date} format="shortest" /></CellText>
+      <CellText discontinued={data?.discontinued}><DateDisplay date={trimToDate(data.date)} format="shortest" /></CellText>
     ),
     sortable: true,
   },
