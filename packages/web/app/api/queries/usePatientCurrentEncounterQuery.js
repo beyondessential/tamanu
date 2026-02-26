e974c1f3fd06298e0e91c196a8fe6bdc56cb6247
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2026-02-26 11:36:01 +1100

    fix(appointments): TAMOC-396: Fix error when attempting to admit a patient via an outpatient appointment (hotfix 2.49) (#9233)

--- packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
+++ packages/web/app/api/queries/usePatientCurrentEncounterQuery.js
@@ -2,10 +2,12 @@ import { useQuery } from '@tanstack/react-query';
 import { useApi } from '../useApi';
 import { useAuth } from '../../contexts/Auth';
 
-export const usePatientCurrentEncounterQuery = patientId => {
+export const usePatientCurrentEncounterQuery = (patientId, options = {}) => {
   const { facilityId } = useAuth();
   const api = useApi();
-  return useQuery(['patientCurrentEncounter', patientId], () =>
-    api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`, { facilityId }),
+  return useQuery(
+    ['patientCurrentEncounter', patientId],
+    () => api.get(`patient/${encodeURIComponent(patientId)}/currentEncounter`, { facilityId }),
+    options,
   );
 };
