159247c8080bab56b5122db1476556bc2089b97a
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-03-02 11:36:38 +1300

    nice

--- packages/central-server/app/admin/userValidation.js
+++ packages/central-server/app/admin/userValidation.js
@@ -1,9 +1,9 @@
 import { Op } from 'sequelize';
 
-export async function checkUserUniqueness(User, { email, displayName, excludeId }) {
+export async function checkUserUniqueness(User, { email, displayName, excludeId, caseInsensitiveEmail = false }) {
   const duplicates = {};
   if (email) {
-    const where = { email: { [Op.iLike]: email } };
+    const where = { email: caseInsensitiveEmail ? { [Op.iLike]: email } : email };
     if (excludeId) where.id = { [Op.ne]: excludeId };
     duplicates.email = Boolean(await User.findOne({ where }));
   }
