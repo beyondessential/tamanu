0e4e62a077e98a3b3ca0627b1060357ac1bdf2b3
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-03-05 19:40:47 -0400

    Update suggestions query hook to pass facility ID

--- packages/web/app/api/queries/useSuggestionsQuery.js
+++ packages/web/app/api/queries/useSuggestionsQuery.js
@@ -1,13 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import { useAuth } from '../../contexts/Auth';
 import { useApi } from "../useApi";
 
 
 export const useSuggestionsQuery = (endpoint, options = {}) => {
   const api = useApi();
+  const { facilityId } = useAuth();
 
   return useQuery(
-    ['suggestions', endpoint],
-    () => api.get(`suggestions/${endpoint}`, { noLimit: 'true' }),
+    ['suggestions', endpoint, facilityId],
+    () => api.get(`suggestions/${endpoint}`, { noLimit: 'true', facilityId }),
     {
       enabled: !!endpoint,
       ...options,
