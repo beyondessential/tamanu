fedd1ed1b90897c9e695cb0e78d97771b2543fa3
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-03-13 10:02:36 +1300

    dont know how that was in there

--- packages/shared/src/utils/fhir/resources.js
+++ packages/shared/src/utils/fhir/resources.js
@@ -8,15 +8,15 @@ import { FHIR_INTERACTIONS } from '@tamanu/constants';
  * @returns {FhirResource[]}
  */
 export function resourcesThatCanDo(models, ...interactions) {
-  const workerConfig = config.integrations?.fhir?.worker;
-
   return Object.values(models).filter((Resource) =>
     interactions.every((interaction) => {
-      // Materialisation requires the worker to be running and the resource to be enabled
-      if (interaction === FHIR_INTERACTIONS.INTERNAL.MATERIALISE) {
-        if (!workerConfig?.enabled || !workerConfig.resourceMaterialisationEnabled?.[Resource.fhirName]) {
-          return false;
-        }
+      // Check if materialisation of resource is enabled
+      if (
+        !config.integrations.fhir.worker.enabled ||
+        (interaction === FHIR_INTERACTIONS.INTERNAL.MATERIALISE &&
+          !config.integrations.fhir.worker.resourceMaterialisationEnabled[Resource.fhirName])
+      ) {
+        return false;
       }
 
       return Resource.CAN_DO?.has(interaction);
