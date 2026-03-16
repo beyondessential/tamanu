4eede99ce3e66e6e2b785ec3f595268461d91dab
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-03-17 11:02:27 +1300

    does this break everthing?

--- packages/shared/src/utils/fhir/fhirSettings.js
+++ packages/shared/src/utils/fhir/fhirSettings.js
@@ -1,61 +1,17 @@
 import config from 'config';
+import { globalDefaults } from '@tamanu/settings';
 import { log } from '../../services/logging';
 
+const fhirDefaults = globalDefaults.fhir;
 const DEFAULTS = {
-  resourceMaterialisationEnabled: {
-    Patient: true,
-    Encounter: false,
-    Immunization: false,
-    MediciReport: false,
-    Organization: false,
-    Practitioner: false,
-    ServiceRequest: false,
-    Specimen: false,
-    MedicationRequest: false,
-    DiagnosticReport: false,
-  },
-  countDefault: 100,
-  countMax: 1000,
-  concurrency: 10,
-  extensions: { Patient: { newZealandEthnicity: false } },
-  nullLastNameValue: 'NoLastName',
-  assigners: {
-    patientDisplayId: 'Tamanu',
-    patientDrivingLicense: 'RTA',
-    patientPassport: 'Fiji Passport Office',
-  },
-  dataDictionaries: {
-    testMethod: 'http://tamanu.io/data-dictionary/covid-test-methods',
-    patientDisplayId: 'http://tamanu.io/data-dictionary/application-reference-number.html',
-    labRequestDisplayId: 'http://tamanu.io/data-dictionary/labrequest-reference-number.html',
-    areaExternalCode: 'http://data-dictionary.tamanu-fiji.org/rispacs-billing-code.html',
-    encounterClass: 'http://terminology.hl7.org/CodeSystem/v3-ActCode',
-    pharmacyOrderPrescriptionId: 'http://data-dictionary.tamanu.org/tamanu-mrid-pharmacyorderprescription.html',
-    pharmacyOrderId: 'http://data-dictionary.tamanu.org/tamanu-mrid-pharmacyorder.html',
-    medicationCodeSystem: 'http://data-dictionary.tamanu.org/tamanu-msupplyuniveralcodes.html',
-    medicationRouteCodeSystem: 'http://data-dictionary.tamanu.org/tamanu-medicationroutecodes.html',
-    serviceRequestImagingDisplayId: 'http://data-dictionary.tamanu-fiji.org/tamanu-mrid-imagingrequest.html',
-    serviceRequestImagingId: 'http://data-dictionary.tamanu-fiji.org/tamanu-id-imagingrequest.html',
-    serviceRequestImagingTypeCodeSystem: 'http://tamanu.io/data-dictionary/imaging-type-code.html',
-    serviceRequestLabDisplayId: 'http://data-dictionary.tamanu-fiji.org/tamanu-mrid-labrequest.html',
-    serviceRequestLabId: 'http://data-dictionary.tamanu-fiji.org/tamanu-id-labrequest.html',
-    serviceRequestLabPanelCodeSystem: 'https://www.senaite.com/profileCodes.html',
-    serviceRequestLabPanelExternalCodeSystem: 'http://loinc.org',
-    serviceRequestLabTestCodeSystem: 'https://www.senaite.com/testCodes.html',
-    serviceRequestLabTestExternalCodeSystem: 'http://loinc.org',
-    imagingStudyAccessionId: 'http://data-dictionary.tamanu-fiji.org/ris-accession-number.html',
-    ethnicityId: 'http://data-dictionary.tamanu-fiji.org/extensions/ethnic-group-code.html',
-    locationPhysicalType: 'http://terminology.hl7.org/CodeSystem/location-physical-type',
-    specimenType: 'http://www.senaite.com/data/sample_types',
-    sampleBodySite: 'http://bodySITE.NEW',
-    ips: {
-      medicationEncoding: 'http://nzmt.org.nz',
-      allergyIntoleranceEncoding: 'http://snomed.info/sct',
-      conditionEncoding: 'http://snomed.info/sct',
-      immunizationEncoding: 'http://nzmt.org.nz',
-      absentUnknown: 'http://hl7.org/fhir/uv/ips/CodeSystem/absent-unknown-uv-ips',
-    },
-  },
+  resourceMaterialisationEnabled: fhirDefaults.worker.resourceMaterialisationEnabled,
+  countDefault: fhirDefaults.parameters._count.default,
+  countMax: fhirDefaults.parameters._count.max,
+  concurrency: fhirDefaults.worker.concurrency,
+  extensions: fhirDefaults.extensions,
+  nullLastNameValue: fhirDefaults.nullLastNameValue,
+  assigners: fhirDefaults.assigners,
+  dataDictionaries: fhirDefaults.dataDictionaries,
 };
 
 let settings = structuredClone(DEFAULTS);
