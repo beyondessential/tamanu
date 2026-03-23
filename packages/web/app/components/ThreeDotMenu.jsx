20f0bb0408bcc85d72edfba06779bdca3c33da47
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-03-23 13:04:48 +1300

    a11y tweaks

--- packages/web/app/components/ThreeDotMenu.jsx
+++ packages/web/app/components/ThreeDotMenu.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
 import { IconButton, Menu, MenuItem } from '@material-ui/core';
 import MoreVert from '@mui/icons-material/MoreVert';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+import { useTranslation } from '@tamanu/ui-components';
 import { Colors } from '../constants';
 
 const ThreeDotMenuItem = styled(MenuItem)`
   font-size: 11px;
-  line-height: 15px;
+  line-height: 1.35;
   border-radius: 4px;
   padding: 4px;
-  margin-left: 4px;
-  margin-right: 4px;
+  margin-inline: 4px;
   white-space: normal;
-  ${props => (props.$color ? `color: ${props.$color};` : '')} :hover {
+
+  :hover {
     background: ${Colors.veryLightBlue};
   }
 `;
 
 const StyledMenu = styled(Menu)`
   & .MuiList-padding {
-    padding-top: 4px;
-    padding-bottom: 4px;
+    padding-block: 4px;
   }
 `;
 
 const StyledIconButton = styled(IconButton)`
-  margin-left: auto;
+  margin-inline-start: auto;
   padding: 7px;
 `;
 
 export const ThreeDotMenu = ({ items, disabled, className }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  const { getTranslation } = useTranslation();
 
   const onOpenKebabMenu = event => {
     setAnchorEl(event.currentTarget);
@@ -49,12 +52,13 @@ export const ThreeDotMenu = ({ items, disabled, className }) => {
   return (
     <>
       <StyledIconButton
+        aria-label={getTranslation('general.more', 'More')}
         onClick={onOpenKebabMenu}
         disabled={disabled}
         className={className}
         data-testid="stylediconbutton-szh8"
       >
-        <MoreVert data-testid="morevert-kusc" />
+        <MoreVert aria-hidden data-testid="morevert-kusc" />
       </StyledIconButton>
       <StyledMenu
         anchorEl={anchorEl}
@@ -71,19 +75,18 @@ export const ThreeDotMenu = ({ items, disabled, className }) => {
         }}
         data-testid="styledmenu-7k45"
       >
-        {items.map(
-          (item, index) =>
-            !item.hidden && (
-              <ThreeDotMenuItem
-                key={index}
-                onClick={() => handleAction(item)}
-                disabled={item.disabled}
-                data-testid={`menuitem-${index}`}
-              >
-                {item.label}
-              </ThreeDotMenuItem>
-            ),
-        )}
+        {items
+          .filter(item => !item.hidden)
+          .map((item, index) => (
+            <ThreeDotMenuItem
+              key={item.key ?? index}
+              onClick={() => handleAction(item)}
+              disabled={item.disabled}
+              data-testid={`menuitem-${index}`}
+            >
+              {item.label}
+            </ThreeDotMenuItem>
+          ))}
       </StyledMenu>
     </>
   );
