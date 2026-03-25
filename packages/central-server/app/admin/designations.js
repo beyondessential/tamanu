daa36fd4e80d23a6280ceb9c3998a97ee393f7fa
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-03-26 11:35:37 +1300

    clearer comment

--- packages/central-server/app/admin/designations.js
+++ packages/central-server/app/admin/designations.js
@@ -1,6 +1,11 @@
 import express from 'express';
+
 import { deleteDesignationById } from './designations.delete';
-import { getDesignationById, getDesignationDeletabilityById, getDesignations } from './designations.get';
+import {
+  getDesignationById,
+  getDesignationDeletabilityById,
+  getDesignations,
+} from './designations.get';
 import { createDesignation } from './designations.post';
 
 /** `/admin/designation` endpoint for a single designation (ReferenceData) */
@@ -11,7 +16,7 @@ designationRouter.get('/:id/isDeletable', getDesignationDeletabilityById);
 designationRouter.get('/:id', getDesignationById);
 designationRouter.delete('/:id', deleteDesignationById);
 
-/** `/admin/designations` endpoint for listing and creating multiple designations (ReferenceData) */
+/** `/admin/designations` endpoint for CRUD-ing multiple designations (ReferenceData) at once */
 export const designationsRouter = express.Router();
 
 designationsRouter.get('/', getDesignations);
