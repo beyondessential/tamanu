7e0223ed3c9adcdb886225e4636fc4db36c86cc4
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-03-26 18:35:54 -0400

    Properly export JSONB columns

--- packages/central-server/app/utils/excelUtils.js
+++ packages/central-server/app/utils/excelUtils.js
@@ -1,9 +1,13 @@
 import * as XLSX from 'xlsx';
 
+const stringifyIfNonDateObject = val =>
+  typeof val === 'object' && !(val instanceof Date) && val !== null ? JSON.stringify(val) : val;
+
 export function writeExcelFile(sheets, fileName) {
   const workbook = XLSX.utils.book_new();
   sheets.forEach(sheet => {
-    const worksheet = XLSX.utils.aoa_to_sheet(sheet.data);
+    const stringifiedData = sheet.data.map(row => row.map(stringifyIfNonDateObject));
+    const worksheet = XLSX.utils.aoa_to_sheet(stringifiedData);
     XLSX.utils.book_append_sheet(workbook, worksheet, sheet.name);
   });
   const excelFileName = fileName || `./export-${Date.now()}.xlsx`;
