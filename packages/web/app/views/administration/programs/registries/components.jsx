ab8f81a2bd4efe955c71c77c67fed7f1561f8817
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-07 19:54:36 +1200

    colour

--- packages/web/app/views/administration/programs/registries/components.jsx
+++ packages/web/app/views/administration/programs/registries/components.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import { Chip } from '@mui/material';
+import { VISIBILITY_STATUSES } from '@tamanu/constants';
 import styled from 'styled-components';
-import { DataFetchingTable } from '../../../../components';
+import { DataFetchingTable, TranslatedText } from '../../../../components';
 
 export const StyledDataFetchingTable = styled(DataFetchingTable)`
   border-start-end-radius: 0;
@@ -19,4 +21,47 @@ export function ColourCell({ color }) {
 
 export function VisibilityStatusCell({ visibilityStatus }) {
   return visibilityStatus || <>&mdash;</>;
+}
+
+const visibilityStatusText = /** @type {const} */ {
+  [VISIBILITY_STATUSES.CURRENT]: (
+    <TranslatedText
+      stringId="admin.programRegistries.visibilityStatus.current"
+      fallback="Current"
+    />
+  ),
+  [VISIBILITY_STATUSES.HISTORICAL]: (
+    <TranslatedText
+      stringId="admin.programRegistries.visibilityStatus.historical"
+      fallback="Historical"
+    />
+  ),
+  [VISIBILITY_STATUSES.MERGED]: (
+    <TranslatedText stringId="admin.programRegistries.visibilityStatus.merged" fallback="Merged" />
+  ),
+};
+
+const StyledChip = styled(Chip)`
+  &.MuiChip-root {
+    background-color: oklch(from currentColor l c h / 10%);
+  }
+`;
+
+export function VisibilityStatusChip({ visibilityStatus, ...props }) {
+  if (!visibilityStatus) return null;
+  return (
+    <StyledChip
+      label={
+        visibilityStatusText[visibilityStatus] ?? (
+          <TranslatedText
+            stringId="admin.programRegistries.visibilityStatus.unknown"
+            fallback="Unknown"
+          />
+        )
+      }
+      style={{ color: visibilityStatus === VISIBILITY_STATUSES.CURRENT ? '#19934e' : '#444' }}
+      size="small"
+      {...props}
+    />
+  );
 }
