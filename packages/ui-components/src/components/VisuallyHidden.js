f80550ca98e0a5b8a96bcd31251fc703cab74d8b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-09 11:46:31 +1200

    meatball menu

---
+++ packages/ui-components/src/components/VisuallyHidden.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+/**
+ * @see https://www.a11yproject.com/posts/how-to-hide-content
+ */
+export const VisuallyHidden = styled.span`
+  &:not(:active, :focus-visible, :focus-within) {
+    border: 0;
+    clip: rect(0 0 0 0);
+    height: auto;
+    margin: 0;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+  }
+`;
