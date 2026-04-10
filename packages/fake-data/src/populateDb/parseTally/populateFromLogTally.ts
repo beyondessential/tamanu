c5e1a3cd22cf8a10df14bd15fab168bfed48628c
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-04-10 12:30:42 +1200

    ya

--- packages/fake-data/src/populateDb/parseTally/populateFromLogTally.ts
+++ packages/fake-data/src/populateDb/parseTally/populateFromLogTally.ts
@@ -58,6 +58,9 @@ export const readJSON = async (path: string): Promise<object> => {
 export const populateDbFromTallyFile = async (models: Models, tallyFilePath: string) => {
   await generateImportData(models);
 
+  const { default: pLimit } = await import('p-limit');
+  const limit = pLimit(10);
+
   const tallyJson = await readJSON(tallyFilePath);
   const tallies = Object.entries(tallyJson);
   const BATCH_SIZE = 50;
@@ -67,7 +70,7 @@ export const populateDbFromTallyFile = async (models: Models, tallyFilePath: str
       const batchCount = Math.min(BATCH_SIZE, count - i);
       const results = await Promise.allSettled(
         times(batchCount, () =>
-          fn({ models, limit: (f: any) => f() }).then(print('.'), print('!', true)),
+          fn({ models, limit }).then(print('.'), print('!', true)),
         ),
       );
       const failures = results.filter((r) => r.status === 'rejected');
