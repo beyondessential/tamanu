1c233b0c3771ae09c0e0082bd4d00b6195aaf31b
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-04-13 16:38:11 +1200

    retire tabs

--- packages/web/app/views/administration/programs/registries/ManageProgramRegistriesRoutes.jsx
+++ packages/web/app/views/administration/programs/registries/ManageProgramRegistriesRoutes.jsx
@@ -6,16 +6,13 @@ import {
   ConditionsTable,
   RelatedConditionCategoriesTable,
 } from './ManageProgramRegistriesTable';
-import { ProgramRegistriesAdminView } from './ProgramRegistriesAdminView';
 
 export const ManageProgramRegistriesRoutes = React.memo(() => (
   <Routes>
-    <Route element={<ProgramRegistriesAdminView />}>
-      <Route index element={<Navigate to="statuses" replace />} />
-      <Route path="statuses" element={<ClinicalStatusesTable />} />
-      <Route path="conditions" element={<ConditionsTable />} />
-      <Route path="conditionCategories" element={<RelatedConditionCategoriesTable />} />
-      <Route path="*" element={<Navigate to="statuses" replace />} />
-    </Route>
+    <Route index element={<Navigate to="statuses" replace />} />
+    <Route path="statuses" element={<ClinicalStatusesTable />} />
+    <Route path="conditions" element={<ConditionsTable />} />
+    <Route path="conditionCategories" element={<RelatedConditionCategoriesTable />} />
+    <Route path="*" element={<Navigate to="statuses" replace />} />
   </Routes>
 ));
