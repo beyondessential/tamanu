80bf8bb30d06973519f00f7fbc8fd54cbae4a780
Rohan Port <59544282+rohan-bes@users.noreply.github.com>
2026-05-07 17:18:24 +1000

    feat(pharmacyOrders): TAM-6739: Add setting to control the default prescription type when sending an order to the pharmacy (#9726)

--- packages/web/app/views/administration/settings/components/Category.jsx
+++ packages/web/app/views/administration/settings/components/Category.jsx
@@ -122,6 +122,7 @@ export const Category = ({ schema, path = '', getSettingValue, handleChangeSetti
           unit,
           highRisk,
           suggesterEndpoint,
+          options,
         } = propertySchema;
 
         const isHighRisk = schema.highRisk || highRisk;
@@ -144,6 +145,7 @@ export const Category = ({ schema, path = '', getSettingValue, handleChangeSetti
               path={newPath}
               handleChangeSetting={handleChangeSetting}
               unit={unit}
+              options={options}
               disabled={disabled}
               facilityId={facilityId}
               data-testid={`settinginput-2wuw-${testIdSuffix}`}
