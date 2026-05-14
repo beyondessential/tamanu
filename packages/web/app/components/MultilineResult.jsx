ee2a61a324d885347bbd31955f82c971b809e392
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-05-15 09:14:06 +1200

    cherry pick

--- packages/web/app/components/MultilineResult.jsx
+++ packages/web/app/components/MultilineResult.jsx
@@ -7,7 +7,7 @@ const Pre = styled.pre`
   white-space-collapse: preserve;
 `;
 
-/** @param {React.ComponentPropsWithRef<typeof Pre> & { answer: string }} props */
+/** @param {Omit<React.ComponentPropsWithRef<typeof Pre>, 'children'> & { answer: string }} props */
 export default function MultilineResult({ answer, ...props }) {
   return <Pre {...props}>{answer}</Pre>;
 }
