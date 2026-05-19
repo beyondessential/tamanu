b1b3122a9422bf0c731e182a400dd43c0d95ddc9
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-05-19 17:08:01 +1200

    remove dead code

--- packages/mobile/App/ui/components/Forms/SurveyForm/helpers.ts
+++ packages/mobile/App/ui/components/Forms/SurveyForm/helpers.ts
@@ -110,8 +110,6 @@ export function getFormInitialValues(
   return initialValues;
 }
 
-const PLAIN_TIME_PATTERN = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
-
 function getFieldValidator(
   dataElement,
   validationCriteria: SurveyScreenValidationCriteria,
