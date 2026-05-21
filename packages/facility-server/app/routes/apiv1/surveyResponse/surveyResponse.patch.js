82e185f8268ed04912821c9abbd077395198848a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-05-22 09:58:22 +1200

    remove dead export

--- packages/facility-server/app/routes/apiv1/surveyResponse/surveyResponse.patch.js
+++ packages/facility-server/app/routes/apiv1/surveyResponse/surveyResponse.patch.js
@@ -201,34 +201,6 @@ async function handleSurveyResponseActions(
   );
 }
 
-export async function createSurveyResponse(req) {
-  const {
-    models,
-    body: { facilityId, ...body },
-    settings,
-  } = req;
-
-  // Responses for the vitals survey will check against 'Vitals' create permissions
-  // All others will check against 'SurveyResponse' create permissions
-  const noun = await models.Survey.getResponsePermissionCheck(body.surveyId);
-  if (noun === 'Charting') {
-    req.checkPermission('create', subject('Charting', { id: body.surveyId }));
-  } else {
-    req.checkPermission('create', noun);
-  }
-
-  const getDefaultId = async type =>
-    models.SurveyResponseAnswer.getDefaultId(type, settings[facilityId]);
-  const updatedBody = {
-    locationId: body.locationId || (await getDefaultId('location')),
-    departmentId: body.departmentId || (await getDefaultId('department')),
-    userId: req.user.id,
-    facilityId,
-    ...body,
-  };
-  return await models.SurveyResponse.createWithAnswers(updatedBody);
-}
-
 export const surveyResponsePatchHandler = asyncHandler(async (req, res) => {
   const { models, params, body } = req;
   req.checkPermission('read', 'SurveyResponse');
