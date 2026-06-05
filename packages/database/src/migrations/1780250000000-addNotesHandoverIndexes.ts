fe84c3e36ebb5b1dfdf8d958e1690639e86f8d57
Chris Tran <chris@beyondessential.com.au>
2026-06-05 10:31:44 +1000

    fix

--- packages/database/src/migrations/1780250000000-addNotesHandoverIndexes.ts
+++ packages/database/src/migrations/1780250000000-addNotesHandoverIndexes.ts
@@ -6,7 +6,7 @@ const REVISED_BY_INDEX_NAME = 'idx_notes_revised_by';
 export async function up(query: QueryInterface): Promise<void> {
   await query.sequelize.query(`
     CREATE INDEX IF NOT EXISTS ${HANDOVER_INDEX_NAME}
-    ON notes (record_type, note_type, revised_by_id);
+    ON notes (record_type, note_type_id, revised_by_id);
   `);
   await query.sequelize.query(`
     CREATE INDEX IF NOT EXISTS ${REVISED_BY_INDEX_NAME}
