2e4645444b48b0c7740f559e90a5d66a6c0600f2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-15 11:58:24 +1200

    perf(build): TAM-6848: build CommonJS & ES modules in parallel (#10021)
    * perf(build): build CommonJS & ES modules in parallel
    
    fix(build): declare dependencies on @tamanu/build-tooling
    
    * use `util.parseArgs`
    
    * trigger redeploy

--- packages/build-tooling/move-dts.mjs
+++ packages/build-tooling/move-dts.mjs
@@ -3,9 +3,9 @@
 import { glob } from 'glob';
 import _ from 'lodash';
 import { promises as fs } from 'node:fs';
-import { styleText } from 'node:util';
-import { ensureDirectoryExists } from './dirs.mjs';
 import path from 'node:path';
+import { parseArgs, styleText } from 'node:util';
+import { ensureDirectoryExists } from './dirs.mjs';
 
 /** @param {string} pathLike */
 function stylePath(pathLike) {
@@ -22,15 +22,32 @@ function escapeReplacement(s) {
   return s.replaceAll('$', '$$');
 }
 
-const src = process.argv[2];
-const dst = process.argv.slice(3).map((d, i) => [
+const { values, positionals } = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    copy: {
+      type: 'boolean',
+      default: false,
+    },
+  },
+  allowPositionals: true,
+});
+
+const copyOnly = values.copy;
+const src = positionals[0];
+const dst = positionals.slice(1).map((d, i) => [
   // enumerate backwards so that 1 is always the last dst,
   // so we can switch from copy to rename below
-  process.argv.slice(3).length - i,
+  positionals.slice(1).length - i,
 
   // normalise dst paths to always end with a /
   d.replace(/\/?$/, '/'),
 ]);
+
+if (!src || dst.length === 0) {
+  console.error('Usage: move-dts [--copy] <srcDir> <dstDir> [...dstDir]');
+  process.exit(1);
+}
 
 const files = await glob(`${src}/**/*.d.ts{,.map}`, { ignore: 'node_modules/**' });
 if (files.length === 0) {
@@ -42,10 +59,10 @@ for (const file of files) {
   for (const [i, d] of dst) {
     const dest = file.replace(srcPattern, escapeReplacement(d));
     ensureDirectoryExists(dest);
-    if (i === 1) {
-      await fs.rename(file, dest);
-    } else {
+    if (copyOnly || i !== 1) {
       await fs.copyFile(file, dest);
+    } else {
+      await fs.rename(file, dest);
     }
     console.log(`Wrote ${stylePath(dest)}`);
   }
