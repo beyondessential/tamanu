f5ced62e6e07476107043eed17f1283dff4aaa8e
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-06-18 12:04:10 +1200

    tweak incomplete log

--- packages/database/src/utils/databaseVersionCompatibility.ts
+++ packages/database/src/utils/databaseVersionCompatibility.ts
@@ -68,8 +68,8 @@ export class DatabaseIncompatibleError extends Error {
     super(
       `Database version compatibility check failed. Database has been used with v${storedVersion}, but this server is v${serverVersion}.` +
         '\n\nThis could mean there’s been a partial rollback after an aborted upgrade. Possible recovery steps:' +
-        '\n\n- down-migrate the database;' +
-        `\n- if you’re confident the database is compatible, manually update the "currentVersion" row in the "local_system_facts" table to ${serverVersion};` +
+        `\n\n- down-migrate the database, then manually update the currentVersion local_system_fact to ${serverVersion};` +
+        `\n- if you’re confident the database is compatible, then manually update the currentVersion local_system_fact to ${serverVersion};` +
         '\n- restart with --skipVersionCompatibilityCheck (not recommended).\n',
     );
     this.name = 'DatabaseIncompatibleError';
