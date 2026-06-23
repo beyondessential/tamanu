3f91e8ab680c08f20297fa641febedf0f6e48d53
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-06-23 19:09:46 -0400

    Properly escape

--- packages/facility-server/app/utils/triageFilters.js
+++ packages/facility-server/app/utils/triageFilters.js
@@ -1,21 +1,21 @@
-import { makeFilter } from './query';
+import { escapePatternWildcard, makeFilter } from './query';
 
 export const createTriageFilters = filterParams => {
   return [
     makeFilter(
       filterParams.displayId,
       `UPPER(patients.display_id) LIKE UPPER(:displayId)`,
-      ({ displayId }) => ({ displayId: `%${displayId}%` }),
+      ({ displayId }) => ({ displayId: `%${escapePatternWildcard(displayId)}%` }),
     ),
     makeFilter(
       filterParams.firstName,
       `UPPER(patients.first_name) LIKE UPPER(:firstName)`,
-      ({ firstName }) => ({ firstName: `%${firstName}%` }),
+      ({ firstName }) => ({ firstName: `%${escapePatternWildcard(firstName)}%` }),
     ),
     makeFilter(
       filterParams.lastName,
       `UPPER(patients.last_name) LIKE UPPER(:lastName)`,
-      ({ lastName }) => ({ lastName: `%${lastName}%` }),
+      ({ lastName }) => ({ lastName: `%${escapePatternWildcard(lastName)}%` }),
     ),
     makeFilter(filterParams.clinicianId, `encounters.examiner_id = :clinicianId`),
     makeFilter(filterParams.score, `triages.score = :score`),
