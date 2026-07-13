b611f855ea12c98be555feba8cd7ca4c8fca89ae
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-13 12:38:50 +1200

    refactor: extract `<AlertOrnament>`

--- packages/web/app/components/Medication/Mar/MarStatusIcon.jsx
+++ packages/web/app/components/Medication/Mar/MarStatusIcon.jsx
@@ -6,6 +6,8 @@ import React from 'react';
 import { ADMINISTRATION_STATUS } from '@tamanu/constants';
 import { TAMANU_COLORS } from '@tamanu/ui-components';
 
+const fontSize = 24;
+
 /**
  * @param {import('@mui/material/SvgIcon').SvgIconProps & {
  *   variant: typeof ADMINISTRATION_STATUS.GIVEN | typeof ADMINISTRATION_STATUS.NOT_GIVEN | 'missed'
@@ -14,11 +16,23 @@ import { TAMANU_COLORS } from '@tamanu/ui-components';
 export default function MarStatusIcon({ style, variant, ...props }) {
   switch (variant) {
     case ADMINISTRATION_STATUS.GIVEN:
-      return <CheckCircleRoundedIcon style={{ ...style, color: TAMANU_COLORS.green }} {...props} />;
+      return (
+        <CheckCircleRoundedIcon
+          style={{ color: TAMANU_COLORS.green, fontSize, ...style }}
+          {...props}
+        />
+      );
     case ADMINISTRATION_STATUS.NOT_GIVEN:
-      return <CancelRoundedIcon style={{ ...style, color: TAMANU_COLORS.alert }} {...props} />;
+      return (
+        <CancelRoundedIcon style={{ color: TAMANU_COLORS.alert, fontSize, ...style }} {...props} />
+      );
     case 'missed':
-      return <HelpOutlineIcon style={{ ...style, color: TAMANU_COLORS.darkOrange }} {...props} />;
+      return (
+        <HelpOutlineIcon
+          style={{ color: TAMANU_COLORS.darkOrange, fontSize, ...style }}
+          {...props}
+        />
+      );
     default:
       return null;
   }
