4e890a270a9c9ee417a325c9d89113313ce302ec
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-13 12:18:04 +1200

    refactor: extract `<MarDoseInfo>`

---
+++ packages/web/app/components/Medication/Mar/MarDoseInfo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { DRUG_UNIT_SHORT_LABELS } from '@tamanu/constants';
+import { TranslatedEnum, TranslatedText } from '@tamanu/ui-components';
+
+const Root = styled.div`
+  text-align: center;
+  text-wrap: balance;
+`;
+
+/**
+ * @param {React.ComponentPropsWithRef<typeof Root> &
+ *   Pick<
+ *     import('@tamanu/database').Prescription,
+ *     'doseAmount' | 'dosingUnit' | 'isVariableDose'
+ *   >
+ * } props
+ */
+export default function MarDoseInfo({ doseAmount, dosingUnit, isVariableDose, ...props }) {
+  const body = isVariableDose ? (
+    <TranslatedText stringId="medication.mar.status.doseDue" fallback="Dose due" />
+  ) : dosingUnit ? null : (
+    <>
+      <div>{doseAmount}</div>
+      <div>
+        <TranslatedEnum enumValues={DRUG_UNIT_SHORT_LABELS} value={dosingUnit} />
+      </div>
+    </>
+  );
+
+  return <Root {...props}>{body}</Root>;
+}
