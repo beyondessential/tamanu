ab9e592b89c9bed6b1007098044c10c63b4cf0ad
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-16 13:32:21 +1200

    break down gargantuan hook

--- packages/web/app/components/Medication/Mar/MarDoseButton.jsx
+++ packages/web/app/components/Medication/Mar/MarDoseButton.jsx
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 import { ADMINISTRATION_STATUS } from '@tamanu/constants';
 import { EditedOrnament } from '@tamanu/ui-components';
+import { useMarDoses } from '../../../api/queries/useMarDoses';
 import { MAR_WARNING_MODAL } from '../../../constants/medication';
+import { useEncounter } from '../../../contexts/Encounter';
 import { WarningModal } from '../WarningModal';
 import AlertOrnament from './AlertOrnament';
 import { MarDetails } from './MarDetails';
@@ -11,8 +13,10 @@ import MarStatusIcon from './MarStatusIcon';
 import { MarStatusTooltip } from './MarStatusTooltip';
 import { StatusPopper } from './StatusPopper';
 import TableCellButton from './TableCellButton';
-import { useMarDoseState } from './useMarDoseState';
-import { useEncounter } from '../../../contexts/Encounter';
+import useMarDoseAlerts from './useMarDoseAlerts';
+import { useMarDoseTiming } from './useMarDoseTiming';
+import useMarPermissions from './useMarPermissions';
+import { useMarDoseScheduleStatus } from './useMarStatusFlags';
 
 const IconWrapper = styled.div`
   display: grid;
@@ -49,41 +53,39 @@ export function MarDoseButton({
   anchorEl,
   onAnchorElChange,
 }) {
-  const {
-    canCreateMar,
-    canView,
-    canViewMar,
-    dosingUnit,
-    dueAt,
-    endDate,
-    isAlert,
-    isCurrent,
-    isDiscontinued,
-    isDoseAmountNotMatch,
-    isEdited,
-    isEnd,
-    isError,
-    isFuture,
-    isNotDue,
-    isPaused,
-    isPausedThenDiscontinued,
-    isPast,
-    isPrn,
-    isRecordedDuringPaused,
-    isRecordedOutsideAdministrationSchedule,
-    marDoses,
-    reasonNotGiven,
-    status,
-  } = useMarDoseState({
-    selectedDate,
+  const { canCreateMar, canView, canViewMar } = useMarPermissions(medication);
+  const { isPast, isCurrent, isFuture, isNotDue } = useMarDoseTiming({
     timeSlot,
-    marInfo,
-    previousMarInfo,
-    nextMarInfo,
-    previousSubSlot,
+    selectedDate,
+    hasRecord: Boolean(marInfo),
+  });
+  const { isDiscontinued, isEnd, isPaused, isPausedThenDiscontinued } = useMarDoseScheduleStatus({
     medication,
+    marInfo,
+    nextMarInfo,
+    previousMarInfo,
+    previousSubSlot,
+    timeSlot,
+    selectedDate,
     pauseRecords,
   });
+  const { data: { data: marDoses = [] } = {} } = useMarDoses(marInfo?.id);
+  const {
+    isAlert,
+    isDoseAmountNotMatch,
+    isError,
+    isRecordedDuringPaused,
+    isRecordedOutsideAdministrationSchedule,
+  } = useMarDoseAlerts({
+    marInfo,
+    medication,
+    marDoses,
+    isPaused,
+    isPast,
+  });
+
+  const { dueAt, status, reasonNotGiven, isEdited } = marInfo || {};
+  const { dosingUnit, endDate, isPrn } = medication || {};
 
   const { encounter } = useEncounter();
   const isEncounterDischarged = !!encounter?.endDate;
