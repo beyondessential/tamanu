877d712e37067743ea3d160345fac0fa0690e197
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-20 15:01:05 +1200

    fix(translations): allow `TranslationOptions` with `getEnumTranslation` (#10444)
    * fix(translations): allows `TranslationOptions` with `getEnumTranslation`
    
    * organise imports

--- packages/web/app/contexts/Translation.jsx
+++ packages/web/app/contexts/Translation.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useCallback, useMemo } from 'react';
-import { TranslationContext, useTranslation } from '@tamanu/ui-components';
-import { LOCAL_STORAGE_KEYS } from '../constants';
-import { useTranslationsQuery } from '../api/queries/useTranslationsQuery';
-import { translationFactory } from '@tamanu/shared/utils/translation/translationFactory';
-import { getCurrentLanguageCode } from '../utils/translation';
+import React, { useCallback, useMemo, useState } from 'react';
+
 import { getReferenceDataStringId } from '@tamanu/shared/utils/translation';
-import { getEnumStringId } from '../components/Translation/TranslatedEnum';
+import { translationFactory } from '@tamanu/shared/utils/translation/translationFactory';
+import { TranslationContext, useTranslation, getEnumStringId } from '@tamanu/ui-components';
+import { useTranslationsQuery } from '../api/queries/useTranslationsQuery';
+import { LOCAL_STORAGE_KEYS } from '../constants';
+import { getCurrentLanguageCode } from '../utils/translation';
 
 export { useTranslation };
 
@@ -21,23 +21,32 @@ export const TranslationProvider = ({ children, value }) => {
    * @param {TranslationOptions} translationOptions
    * @returns {string}
    */
-  const getTranslation = useCallback((stringId, fallback, translationOptions = {}) => {
-    const { value } = translationFunc(stringId, fallback, translationOptions);
-    return value;
-  }, [translationFunc]);
+  const getTranslation = useCallback(
+    (stringId, fallback, translationOptions = {}) => {
+      const { value } = translationFunc(stringId, fallback, translationOptions);
+      return value;
+    },
+    [translationFunc],
+  );
 
-  const getEnumTranslation = useCallback((enumValues, currentValue) => {
-    const fallback = enumValues[currentValue];
-    const stringId = getEnumStringId(currentValue ?? '', enumValues);
-    const { value } = translationFunc(stringId, fallback);
-    return value;
-  }, [translationFunc]);
+  const getEnumTranslation = useCallback(
+    (enumValues, currentValue, translationOptions) => {
+      const fallback = enumValues[currentValue];
+      const stringId = getEnumStringId(currentValue ?? '', enumValues);
+      const { value } = translationFunc(stringId, fallback, translationOptions);
+      return value;
+    },
+    [translationFunc],
+  );
 
-  const getReferenceDataTranslation = useCallback(({ value, category, fallback, placeholder }) => {
-    return value
-      ? getTranslation(getReferenceDataStringId(value, category), fallback)
-      : placeholder;
-  }, [getTranslation]);
+  const getReferenceDataTranslation = useCallback(
+    ({ value, category, fallback, placeholder }) => {
+      return value
+        ? getTranslation(getReferenceDataStringId(value, category), fallback)
+        : placeholder;
+    },
+    [getTranslation],
+  );
 
   const updateStoredLanguage = newLanguage => {
     // Save the language in local state so that it updates the react component tree on change
