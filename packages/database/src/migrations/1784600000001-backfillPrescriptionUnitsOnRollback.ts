6e815ea93d6b5313d2b29facc587d99a31660af4
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-21 12:42:37 +1200

    clearer comments

--- packages/database/src/migrations/1784600000001-backfillPrescriptionUnitsOnRollback.ts
+++ packages/database/src/migrations/1784600000001-backfillPrescriptionUnitsOnRollback.ts
@@ -1,13 +1,14 @@
+/* Companion to `1784600000000-makePrescriptionUnitColumnsNullable` */
+
 import { QueryInterface } from 'sequelize';
 
 /**
- * This migration exists to prepare data before the preceding DDL migration is rolled back. It would
- * actually be harmless to convert empty strings to NULL; I’ve just opted to make the up-migration
- * more conservative and have the application layer treat empty string and NULL both as “unitless”.
+ * Would be harmless to convert empty strings to NULL; I’ve just opted to make the up-migration more
+ * conservative and have the application layer treat empty string and NULL both as ‘unitless’.
  */
 export async function up(): Promise<void> {}
 
-/* Companion to 1784600000000-makePrescriptionUnitColumnsNullable */
+/** This migration exists to prepare data before the companion DDL migration is rolled back. */
 export async function down(queryInterface: QueryInterface): Promise<void> {
   await queryInterface.sequelize.query(
     "UPDATE prescriptions SET dosing_unit = '' WHERE dosing_unit IS NULL;",
