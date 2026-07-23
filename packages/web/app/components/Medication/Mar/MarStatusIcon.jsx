93842511a1974a98cd399d0401eddeee25f4aad0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-23 16:25:49 +1200

    shrink icons for smaller cells

--- packages/web/app/components/Medication/Mar/MarStatusIcon.jsx
+++ packages/web/app/components/Medication/Mar/MarStatusIcon.jsx
@@ -2,38 +2,55 @@ import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import React from 'react';
+import styled, { css } from 'styled-components';
 
 import { ADMINISTRATION_STATUS } from '@tamanu/constants';
 import { TAMANU_COLORS } from '@tamanu/ui-components';
+import DashedCircleOutlineIcon from './DashedCircleOutline';
+import { MarDataCell } from './MarStatus';
+import TableCellButton from './TableCellButton';
 
-const fontSize = 24;
+const styles = css`
+  font-size: 24px;
+  ${MarDataCell}:has(${TableCellButton}:nth-of-type(2)) & {
+    font-size: 16px;
+  }
+`;
+
+const GivenIcon = styled(CheckCircleRoundedIcon)`
+  ${styles}
+  color: ${TAMANU_COLORS.green};
+`;
+
+const NotGivenIcon = styled(CancelRoundedIcon)`
+  ${styles}
+  color: ${p => p.theme.palette.error.main};
+`;
+
+const MissedIcon = styled(HelpOutlineIcon)`
+  ${styles}
+  color: ${TAMANU_COLORS.darkOrange};
+`;
+
+const PendingIcon = styled(DashedCircleOutlineIcon)`
+  ${styles}
+  color: #b8b8b8;
+`;
+
+const iconMapping = /** @type {const} */ ({
+  [ADMINISTRATION_STATUS.GIVEN]: GivenIcon,
+  [ADMINISTRATION_STATUS.NOT_GIVEN]: NotGivenIcon,
+  missed: MissedIcon,
+  pending: PendingIcon,
+});
 
 /**
  * @param {import('@mui/material/SvgIcon').SvgIconProps & {
  *   variant: typeof ADMINISTRATION_STATUS.GIVEN | typeof ADMINISTRATION_STATUS.NOT_GIVEN | 'missed'
  * }} props
  */
-export default function MarStatusIcon({ style, variant, ...props }) {
-  switch (variant) {
-    case ADMINISTRATION_STATUS.GIVEN:
-      return (
-        <CheckCircleRoundedIcon
-          style={{ color: TAMANU_COLORS.green, fontSize, ...style }}
-          {...props}
-        />
-      );
-    case ADMINISTRATION_STATUS.NOT_GIVEN:
-      return (
-        <CancelRoundedIcon style={{ color: TAMANU_COLORS.alert, fontSize, ...style }} {...props} />
-      );
-    case 'missed':
-      return (
-        <HelpOutlineIcon
-          style={{ color: TAMANU_COLORS.darkOrange, fontSize, ...style }}
-          {...props}
-        />
-      );
-    default:
-      return null;
-  }
+export default function MarStatusIcon({ variant, ...props }) {
+  const Component = iconMapping[variant];
+  if (Component === undefined) return null;
+  return <Component {...props} />;
 }
