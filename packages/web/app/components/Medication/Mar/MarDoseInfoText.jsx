184231a23b9df2398d0da489f78f4822887efa67
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-24 09:41:36 +1200

    restructure

--- packages/web/app/components/Medication/Mar/MarDoseInfoText.jsx
+++ packages/web/app/components/Medication/Mar/MarDoseInfoText.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import { DRUG_UNIT_SHORT_LABELS } from '@tamanu/constants';
 import { TranslatedEnum, TranslatedText } from '@tamanu/ui-components';
 
-const Root = styled.div`
-  display: grid;
-  place-items: center;
-  text-align: center;
-  text-wrap: balance;
-`;
-
 /**
- * @param {React.ComponentPropsWithRef<typeof Root> &
- *   Pick<
- *     import('@tamanu/database').Prescription,
- *     'doseAmount' | 'dosingUnit' | 'isVariableDose'
- *   >
- * } props
+ * @param {Pick<
+ *   import('@tamanu/database').Prescription,
+ *   'doseAmount' | 'dosingUnit' | 'isVariableDose'
+ * >} props
  */
-export default function MarDoseInfoText({ doseAmount, dosingUnit, isVariableDose, ...props }) {
-  const body = isVariableDose ? (
-    <TranslatedText stringId="medication.mar.status.doseDue" fallback="Dose due" />
-  ) : dosingUnit ? (
-    <>
-      {doseAmount}
-      <br />
-      <TranslatedEnum enumValues={DRUG_UNIT_SHORT_LABELS} value={dosingUnit} />
-    </>
-  ) : null;
-
-  return <Root {...props}>{body}</Root>;
+export default function MarDoseInfoText({ doseAmount, dosingUnit, isVariableDose }) {
+  if (isVariableDose) {
+    return <TranslatedText stringId="medication.mar.status.doseDue" fallback="Dose due" />;
+  }
+  if (dosingUnit) {
+    return (
+      <>
+        {doseAmount}
+        <br />
+        <TranslatedEnum enumValues={DRUG_UNIT_SHORT_LABELS} value={dosingUnit} />
+      </>
+    );
+  }
+  return null;
 }
