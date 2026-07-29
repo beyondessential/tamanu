1cf48ff6ce208942f4665d39172557683bff9df2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-07-29 15:13:18 +1200

    un-over-engineer, just use `if not exists`

--- packages/database/src/migrations/1771472857000-addIndexToFhirJobsResourceColumn.ts
+++ packages/database/src/migrations/1771472857000-addIndexToFhirJobsResourceColumn.ts
@@ -10,35 +10,12 @@ import { QueryInterface } from 'sequelize';
  * - Jobs are created and queried based on the resource type (see FhirMissingResources.js)
  * - Adding an index on payload->>'resource' will improve query performance when
  *   filtering or grouping jobs by resource type
- *
- * Idempotent: some environments already have this index (e.g. created manually). Create only if
- * missing; if present with a non-btree access method, replace it with btree (ALTER INDEX cannot
- * change access method).
  */
 
 export async function up(query: QueryInterface): Promise<void> {
-  await query.sequelize.query(`
-    DO $$
-    DECLARE
-      idx_access_method name;
-    BEGIN
-      SELECT a.amname INTO idx_access_method
-      FROM pg_class i
-      JOIN pg_namespace n ON n.oid = i.relnamespace
-      JOIN pg_am a ON a.oid = i.relam
-      WHERE i.relname = 'job_payload_resource_idx'
-        AND n.nspname = 'fhir';
-
-      IF idx_access_method IS NULL THEN
-        CREATE INDEX job_payload_resource_idx ON fhir.jobs
-          USING btree ((payload->>'resource'));
-      ELSIF idx_access_method <> 'btree' THEN
-        DROP INDEX fhir.job_payload_resource_idx;
-        CREATE INDEX job_payload_resource_idx ON fhir.jobs
-          USING btree ((payload->>'resource'));
-      END IF;
-    END $$;
-  `);
+  await query.sequelize.query(
+    "CREATE INDEX IF NOT EXISTS job_payload_resource_idx ON fhir.jobs USING btree ((payload->>'resource'));",
+  );
 }
 
 export async function down(query: QueryInterface): Promise<void> {
