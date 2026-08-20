d730605617ed73d649c4a1be4afcd53506890704
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-08-20 17:14:59 +1200

    fix dose step buttons

--- packages/web/app/components/Medication/Mar/DoseCell/StatusPopper.jsx
+++ packages/web/app/components/Medication/Mar/DoseCell/StatusPopper.jsx
@@ -325,14 +325,24 @@ export const GivenScreen = ({
           />
           <DoseContainer>
             <DoseButton
-              onClick={() => doseInputRef.current?.stepDown()}
+              onClick={() => {
+                const input = doseInputRef.current;
+                input.stepDown();
+                setFieldValue('doseAmount', input.valueAsNumber);
+              }}
               disabled={values.doseAmount <= 0.25}
               style={isVariableDose ? { visibility: 'hidden' } : undefined}
             >
               <RemoveCircleOutlineIcon />
             </DoseButton>
             <StyledNumberFieldWrapper $units={dosingUnit}>
-              <Field name="doseAmount" component={NumberField} min={0.25} step={0.25} />
+              <Field
+                name="doseAmount"
+                component={NumberField}
+                min={0.25}
+                step={0.25}
+                inputRef={doseInputRef}
+              />
               <InputSuffix>
                 {dosingUnit && (
                   <TranslatedEnum enumValues={DRUG_UNIT_SHORT_LABELS} value={dosingUnit} />
@@ -341,7 +351,11 @@ export const GivenScreen = ({
               </InputSuffix>
             </StyledNumberFieldWrapper>
             <DoseButton
-              onClick={() => doseInputRef.current?.stepUp()}
+              onClick={() => {
+                const input = doseInputRef.current;
+                input.stepUp();
+                setFieldValue('doseAmount', input.valueAsNumber);
+              }}
               style={isVariableDose ? { visibility: 'hidden' } : undefined}
             >
               <AddCircleOutlineIcon />
