c261a2d101e21b372b658700f80b358645810b3e
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-08-26 13:33:18 +1200

    just to be safe

--- packages/mobile/App/migrations/1787706000000-dedupeLocalSystemFacts.ts
+++ packages/mobile/App/migrations/1787706000000-dedupeLocalSystemFacts.ts
@@ -3,6 +3,11 @@ import type { MigrationInterface, QueryRunner } from 'typeorm';
 /** @see `1787706001000-enforceUniqueLocalSystemFactKeys` companion schema migration*/
 export class dedupeLocalSystemFacts1787706000000 implements MigrationInterface {
   async up(queryRunner: QueryRunner): Promise<void> {
+    // No code path should ever have soft-deleted a local_system_facts record, so this is basically
+    // certain to be a no-op 100% of the time. Belting-and-bracing because goodness knows we don’t
+    // want the dedupe query below to accidentally keep a soft-deleted record.
+    await queryRunner.query('DELETE FROM local_system_facts WHERE deletedAt IS NOT NULL;');
+
     await queryRunner.query(`
       DELETE FROM local_system_facts
       WHERE EXISTS (
