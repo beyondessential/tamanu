42c0fd4bf1cd70e92dc11890d3859c3236cb729d
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-08 11:19:38 +1200

    typo

--- packages/web/app/components/ConfirmRowDivider.jsx
+++ packages/web/app/components/ConfirmRowDivider.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 /**
- * More elegant solution wouuld be to use subgrid, or make the `<ConfirmRow>` span the full width
- * and give _it_ 32px padding. Preserving this for legacy components.
+ * More elegant solution would be to use subgrid, or make the `<ConfirmRow>` span the full width and
+ * give _it_ 32px padding. Preserving this for legacy components.
  */
 export const ConfirmRowDivider = styled.hr`
   inline-size: calc(100% + 64px);
