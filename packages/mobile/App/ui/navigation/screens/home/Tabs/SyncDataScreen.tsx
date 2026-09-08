a9d6dc1d99551cf9f6f4159227177c9d27e07530
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-09 10:36:09 +1200

    fix(mobile): TAM-7090: Rules of Hooks violations (#11017)
    fix(mobile): TAM-7090: don’t use function component as plain render function
    fix(mobile): TAM-7090: rename `setStatusBar` hook to `useStatusBarStyle`
    refactor(mobile): drop redundant `useCallback`
    
    (cherry picked from commit d4a41dd4efdc0e5db5c67fecfcb87be3d2789d20)

--- packages/mobile/App/ui/navigation/screens/home/Tabs/SyncDataScreen.tsx
+++ packages/mobile/App/ui/navigation/screens/home/Tabs/SyncDataScreen.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator } from 'react-native';
 import { activateKeepAwake, deactivateKeepAwake } from '@sayem314/react-native-keep-awake';
 import { CenterView, StyledText, StyledView } from '../../../../styled/common';
 import { theme } from '../../../../styled/theme';
-import { Orientation, screenPercentageToDP, setStatusBar } from '../../../../helpers/screen';
+import { Orientation, screenPercentageToDP, useStatusBarStyle } from '../../../../helpers/screen';
 import { BackendContext } from '../../../../contexts/BackendContext';
 import { SYNC_EVENT_ACTIONS } from '../../../../../services/sync';
 import { Button } from '../../../../components/Button';
@@ -31,7 +31,7 @@ export const SyncDataScreen = ({ navigation }): ReactElement => {
   const [lastSyncPushedRecordsCount, setLastSyncPushedRecordsCount] = useState(null);
   const [lastSyncPulledRecordsCount, setLastSyncPulledRecordsCount] = useState(null);
 
-  setStatusBar('light-content', theme.colors.MAIN_SUPER_DARK);
+  useStatusBarStyle('light-content', theme.colors.MAIN_SUPER_DARK);
 
   const manualSync = useCallback(() => {
     syncManager.triggerUrgentSync();
