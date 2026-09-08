f416494b551d405be63338ad3305de268a953f65
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-09-08 13:37:37 +1200

    cover result without clinician

--- packages/facility-server/__tests__/apiv1/ImagingRequests.test.js
+++ packages/facility-server/__tests__/apiv1/ImagingRequests.test.js
@@ -350,6 +350,33 @@ describe('Imaging requests', () => {
     expect(results[0].description).toBe('new result description');
   });
 
+  it('should create a result with only a completed time', async () => {
+    // arrange
+    const ir = await models.ImagingRequest.create({
+      encounterId: encounter.id,
+      imagingType: IMAGING_TYPES.CT_SCAN,
+      requestedById: app.user.id,
+    });
+    const completedAt = getCurrentDateTimeString();
+
+    // act
+    const result = await app.put(`/api/imagingRequest/${ir.id}`).send({
+      status: 'completed',
+      newResult: { completedAt },
+      facilityId,
+    });
+
+    // assert
+    expect(result).toHaveSucceeded();
+
+    const results = await models.ImagingResult.findAll({
+      where: { imagingRequestId: ir.id },
+    });
+    expect(results.length).toBe(1);
+    expect(results[0].completedAt).toBe(completedAt);
+    expect(results[0].completedById).toBe(null);
+  });
+
   it('should create multiple results for an imaging request', async () => {
     // arrange
     const ir = await models.ImagingRequest.create({
