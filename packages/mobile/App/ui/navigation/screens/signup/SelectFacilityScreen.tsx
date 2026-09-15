d89559539e332e2b8e704553dd0d64d4e8eccd33
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-15 15:58:28 +1200

    ditto facility selection

--- packages/mobile/App/ui/navigation/screens/signup/SelectFacilityScreen.tsx
+++ packages/mobile/App/ui/navigation/screens/signup/SelectFacilityScreen.tsx
@@ -1,6 +1,24 @@
-import React, { type FunctionComponent, type ReactElement, useCallback, useEffect, useState } from 'react';
-import * as Yup from 'yup';
+import React, {
+  type FunctionComponent,
+  type ReactElement,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
+import * as Yup from 'yup';
+import { Form } from '~/ui/components/Forms/Form';
+import { Field } from '~/ui/components/Forms/FormField';
+import { TranslatedText } from '~/ui/components/Translations/TranslatedText';
+import { useAuth } from '~/ui/contexts/AuthContext';
+import { useFacility } from '~/ui/contexts/FacilityContext';
+import { useBackend } from '~/ui/hooks';
+import { FacilitySelectField } from './FacilitySelectField';
+import { Button } from '/components/Button';
+import { HomeBottomLogoIcon } from '/components/Icons';
+import { Routes } from '/helpers/routes';
+import { Orientation, screenPercentageToDP } from '/helpers/screen';
+import type { SignInProps } from '/interfaces/Screens/SignUp/SignInProps';
 import {
   FullView,
   StyledSafeAreaView,
@@ -8,27 +26,11 @@ import {
   StyledTouchableOpacity,
   StyledView,
 } from '/styled/common';
-import { HomeBottomLogoIcon } from '/components/Icons';
-import { Orientation, screenPercentageToDP } from '/helpers/screen';
 import { theme } from '/styled/theme';
-
-import { Routes } from '/helpers/routes';
-import { Button } from '/components/Button';
-import type { SignInProps } from '/interfaces/Screens/SignUp/SignInProps';
-import { useAuth } from '~/ui/contexts/AuthContext';
-
-import { Form } from '~/ui/components/Forms/Form';
-import { Field } from '~/ui/components/Forms/FormField';
-import { useFacility } from '~/ui/contexts/FacilityContext';
-import { useBackend } from '~/ui/hooks';
-import { FacilitySelectField } from './FacilitySelectField';
-import { TranslatedText } from '~/ui/components/Translations/TranslatedText';
-
-const horizontalInset = screenPercentageToDP(2.43, Orientation.Width);
 
 const styles = StyleSheet.create({
   content: {
-    paddingHorizontal: horizontalInset,
+    paddingHorizontal: theme.spacing.space150,
     marginTop: screenPercentageToDP(7.29, Orientation.Height),
     marginBottom: screenPercentageToDP(14.7, Orientation.Height),
   },
@@ -109,7 +111,6 @@ export const SelectFacilityForm = ({ onSubmitForm }) => {
             onPress={handleSubmit}
             loadingAction={isSubmitting}
             textColor={theme.colors.TEXT_SUPER_DARK}
-            fontSize={screenPercentageToDP('1.94', Orientation.Height)}
             fontWeight={500}
             buttonText={<TranslatedText stringId="general.action.save" fallback="Save" />}
           />
@@ -160,15 +161,15 @@ export const SelectFacilityScreen: FunctionComponent<any> = ({ navigation }: Sig
               fill={theme.colors.SECONDARY_MAIN}
             />
             <StyledText
-              marginTop={screenPercentageToDP('2.43', Orientation.Height)}
-              fontSize={screenPercentageToDP('2.55', Orientation.Height)}
+              marginTop={theme.spacing.space200}
+              fontSize={16}
               color={theme.colors.WHITE}
               fontWeight="bold"
               textAlign="center"
             >
               <TranslatedText
                 stringId="login.facility.heading"
-                fallback="Please link this device to a facility."
+                fallback="Please link this device to a facility"
               />
             </StyledText>
           </View>
@@ -179,7 +180,7 @@ export const SelectFacilityScreen: FunctionComponent<any> = ({ navigation }: Sig
               textAlign="center"
               marginTop={screenPercentageToDP('2.43', Orientation.Height)}
               marginBottom={screenPercentageToDP('4.86', Orientation.Height)}
-              fontSize={screenPercentageToDP('1.57', Orientation.Height)}
+              fontSize={12}
               color={theme.colors.SECONDARY_MAIN}
             >
               <TranslatedText
