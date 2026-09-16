4896515e61379e37455bf7ceb80aa5caf17868bd
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-09-17 09:43:24 +1200

    fix(fakeData): no-issue: give seeded form responses answers (HOTFIX 2.64) (#11126)
    * type seeded questions
    
    * seed form answers

--- packages/fake-data/src/populateDb/helpers/survey.ts
+++ packages/fake-data/src/populateDb/helpers/survey.ts
@@ -1,6 +1,6 @@
 import { randomRecordId } from '../randomRecord.js';
 
-import { fake } from '../../fake/index.js';
+import { fake, fakeSurveyAnswerBody } from '../../fake/index.js';
 import type { CommonParams } from './common.js';
 
 interface CreateSurveyResponseParams extends CommonParams {
@@ -12,11 +12,27 @@ export const createSurveyResponse = async ({
   encounterId,
   surveyId,
 }: CreateSurveyResponseParams): Promise<void> => {
-  const { SurveyResponse } = models;
-  await SurveyResponse.create(
+  const { SurveyResponse, SurveyResponseAnswer, SurveyScreenComponent } = models;
+  const responseSurveyId = surveyId ?? (await randomRecordId(models, 'Survey'));
+  const surveyResponse = await SurveyResponse.create(
     fake(SurveyResponse, {
-      surveyId: surveyId || (await randomRecordId(models, 'Survey')),
-      encounterId: encounterId || (await randomRecordId(models, 'Encounter')),
+      surveyId: responseSurveyId,
+      encounterId: encounterId ?? (await randomRecordId(models, 'Encounter')),
     }),
   );
+
+  const components = await SurveyScreenComponent.findAll({
+    where: { surveyId: responseSurveyId },
+    include: 'dataElement',
+  });
+
+  for (const { dataElement } of components) {
+    await SurveyResponseAnswer.create(
+      fake(SurveyResponseAnswer, {
+        responseId: surveyResponse.id,
+        dataElementId: dataElement.id,
+        body: fakeSurveyAnswerBody(dataElement),
+      }),
+    );
+  }
 };
