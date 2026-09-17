11d99d22abd408b73b19df55e800967a8bd08e55
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-17 15:00:53 +1200

    db(mobile): TAM-7130: add more high-value database indexes

--- packages/mobile/App/migrations/index.ts
+++ packages/mobile/App/migrations/index.ts
@@ -98,6 +98,7 @@ import { dropLegacyNoteTables1787220453000 } from './1787220453000-dropLegacyNot
 import { dedupeLocalSystemFacts1787706000000 } from './1787706000000-dedupeLocalSystemFacts';
 import { enforceUniqueLocalSystemFactKeys1787706001000 } from './1787706001000-enforceUniqueLocalSystemFactKeys';
 import { resetQueryPlannerStats1789608077000 } from './1789608077000-resetQueryPlannerStats';
+import { addPerPatientLookupIndexes1789611060000 } from './1789611060000-addPerPatientLookupIndexes';
 
 export const migrationList = [
   databaseSetup1661160427226,
@@ -199,4 +200,5 @@ export const migrationList = [
   dedupeLocalSystemFacts1787706000000,
   enforceUniqueLocalSystemFactKeys1787706001000,
   resetQueryPlannerStats1789608077000,
+  addPerPatientLookupIndexes1789611060000,
 ];
