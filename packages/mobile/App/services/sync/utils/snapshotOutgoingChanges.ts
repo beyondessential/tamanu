3b16423ee3ed2d621f9b33f5686661fbb2536225
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-18 15:45:03 +1200

    perf(mobile): avoid redundant shallow clones while snapshotting

--- packages/mobile/App/services/sync/utils/snapshotOutgoingChanges.ts
+++ packages/mobile/App/services/sync/utils/snapshotOutgoingChanges.ts
@@ -30,14 +30,13 @@ export const snapshotOutgoingChanges = async (
   outgoingModels: Partial<typeof MODELS_MAP>,
   since: number,
 ): Promise<SyncRecord[]> => {
-  let outgoingChanges = [];
-
   // snapshot inside a transaction (Serializable is the default isolation level),
   // so that other changes made while this snapshot
   // is underway aren't included (as this could lead to a pair of foreign records with the child in
   // the snapshot and its parent missing)
   // as the snapshot only contains read queries, there will be no concurrent update issues :)
   return Database.client.transaction(async () => {
+    const outgoingChanges = [];
     for (const model of Object.values(outgoingModels)) {
       const changesForModel = await model.find({
         where: { updatedAtSyncTick: MoreThan(since) },
@@ -49,9 +48,8 @@ export const snapshotOutgoingChanges = async (
         ? model.sanitizeRecordDataForPush(syncRecordsForModel)
         : syncRecordsForModel;
 
-      outgoingChanges = outgoingChanges.concat(sanitizedSyncRecords);
+      for (const record of sanitizedSyncRecords) outgoingChanges.push(record);
     }
-
     return outgoingChanges;
   });
 };
