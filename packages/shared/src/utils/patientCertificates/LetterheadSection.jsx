35cd5fda87de1c248caf1084b97a3fe502d6af93
Daniel Nash <38335330+dannash100@users.noreply.github.com>
2026-09-21 10:43:50 +1200

    indent letterhead title past logo

--- packages/shared/src/utils/patientCertificates/LetterheadSection.jsx
+++ packages/shared/src/utils/patientCertificates/LetterheadSection.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { CertificateLogo } from './Layout';
+import { StyleSheet, View } from '@react-pdf/renderer';
+import { CertificateLogo, styles as layoutStyles } from './Layout';
 import { CertificateAddress, CertificateTitle, CertificateSubtitle } from './Typography';
+
+const LOGO_GUTTER = 10;
+
+const styles = StyleSheet.create({
+  clearOfLogo: {
+    paddingLeft: layoutStyles.certificateLogo.width + LOGO_GUTTER,
+  },
+});
 
 export const LetterheadSection = ({ logoSrc, certificateTitle, certificateSubtitle, letterheadConfig }) => {
   const { title, subTitle } = letterheadConfig;
   return (
     <>
       {logoSrc && <CertificateLogo logoSrc={logoSrc} />}
-      <CertificateAddress>{`${title}\n${subTitle}`}</CertificateAddress>
+      <View style={logoSrc ? styles.clearOfLogo : undefined}>
+        <CertificateAddress>{`${title}\n${subTitle}`}</CertificateAddress>
+      </View>
       <CertificateTitle>{certificateTitle}</CertificateTitle>
       {certificateSubtitle && <CertificateSubtitle>{certificateSubtitle}</CertificateSubtitle>}
     </>
