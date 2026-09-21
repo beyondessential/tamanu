3c261f022d1c6e4c87dfdd14dcb381988cb10ff2
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-21 16:58:34 +1200

    silliness

--- packages/mobile/App/ui/navigation/screens/PatientSearch/PatientSearchTabs/PatientFilterScreen/Container.tsx
+++ packages/mobile/App/ui/navigation/screens/PatientSearch/PatientSearchTabs/PatientFilterScreen/Container.tsx
@@ -1,35 +1,33 @@
-import React, { type ReactElement, useCallback } from 'react';
+import React from 'react';
+import { useFilterFields } from './hooks';
 import { Screen } from './Screen';
 import { Routes } from '/helpers/routes';
 import type { BaseAppProps } from '/interfaces/BaseAppProps';
-import { useFilterFields } from './hooks';
 
-const Container = ({ navigation, route }: BaseAppProps): ReactElement => {
+export const PatientFilterScreen = ({ navigation, route }: BaseAppProps) => {
   const { onChangeFilters } = route.params;
 
   const [sex, dateOfBirth, firstName, lastName, villageId, programRegistryId] = useFilterFields();
 
-  const onNavigateBack = useCallback(() => {
+  const onNavigateBack = () => {
     navigation.navigate(Routes.HomeStack.SearchPatientStack.SearchPatientTabs.Index);
-  }, [navigation]);
+  };
 
-  const onSubmit = useCallback(() => {
+  const onSubmit = () => {
     onChangeFilters();
     navigation.navigate(Routes.HomeStack.SearchPatientStack.SearchPatientTabs.Index, {
       screen: Routes.HomeStack.SearchPatientStack.SearchPatientTabs.ViewAll,
     });
-  }, [navigation, onChangeFilters]);
+  };
 
-  const onClearFilters = useCallback(() => {
+  const onClearFilters = () => {
     sex[2].setValue('');
     dateOfBirth[2].setValue(null);
     firstName[2].setValue('');
     lastName[2].setValue('');
     villageId[2].setValue(null);
     programRegistryId[2].setValue(null);
-  }, [sex, dateOfBirth, firstName, lastName, villageId, programRegistryId]);
+  };
 
   return <Screen onCancel={onNavigateBack} onClear={onClearFilters} onSubmit={onSubmit} />;
 };
-
-export const PatientFilterScreen = Container;
