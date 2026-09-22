396a7c1e2557b7399b030b69491a4e36b875257a
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-22 15:56:59 +1200

    initialise options once

--- packages/mobile/App/ui/hooks/localConfig.ts
+++ packages/mobile/App/ui/hooks/localConfig.ts
@@ -2,9 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 import { Patient } from '~/models/Patient';
 import { patientListKeys } from './queries/queryKeys';
 
+const options = {
+  queryKey: patientListKeys.recentlyViewed(),
+  queryFn: () => Patient.findRecentlyViewed(),
+} as const;
+
 export default function useRecentlyViewedPatients() {
-  return useQuery<Patient[]>({
-    queryKey: patientListKeys.recentlyViewed(),
-    queryFn: () => Patient.findRecentlyViewed(),
-  });
+  return useQuery<Patient[]>(options);
 }
