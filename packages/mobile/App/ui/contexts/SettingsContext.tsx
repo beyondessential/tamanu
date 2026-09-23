17e913dbe3fb7546d33bd5a8d99cc136979c1a74
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-23 12:45:13 +1200

    fix(mobile): actually subscribe `SettingsContext` to settings emitter

--- packages/mobile/App/ui/contexts/SettingsContext.tsx
+++ packages/mobile/App/ui/contexts/SettingsContext.tsx
@@ -17,21 +17,20 @@ export const useSettings = () => useContext(SettingsContext);
 export const SettingsProvider = ({ children }) => {
   const backend = useContext(BackendContext);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const defaultHelpers = useMemo(() => makeHelpers(backend.settings), [backend, backend.settings]);
+  const defaultHelpers = useMemo(() => makeHelpers(backend.settings), [backend]);
   const [helpers, setHelpers] = useState(defaultHelpers);
 
   useEffect(() => {
     const onChanged = (): void => {
-      // updates the helper functions whenever the localisation changes,
+      // updates the helper functions whenever the settings change,
       // in order to make components update with the new value
       setHelpers(makeHelpers(backend.settings));
     };
-    backend.localisation.emitter.on('settingsChanged', onChanged);
+    backend.settings.emitter.on('settingsChanged', onChanged);
     return () => {
-      backend.localisation.emitter.off('settingsChanged', onChanged);
+      backend.settings.emitter.off('settingsChanged', onChanged);
     };
-  }, [backend, backend.localisation]);
+  }, [backend, backend.settings]);
 
   return <SettingsContext.Provider value={helpers}>{children}</SettingsContext.Provider>;
 };
