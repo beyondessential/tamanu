b11cf105df796abde933d423e21e7a314075fc3a
Emilio Navarro <DevNavarroEmilioLuis@gmail.com>
2026-09-24 18:56:03 -0400

    Change default cursor

--- packages/web/app/components/TabDisplayDraggable.jsx
+++ packages/web/app/components/TabDisplayDraggable.jsx
@@ -30,10 +30,7 @@ const StyledTab = styled.div.attrs({ role: 'tab' })`
   padding-inline: 12px;
   position: relative;
 
-  cursor: grab;
-  &:active {
-    cursor: grabbing;
-  }
+  cursor: pointer !important;
 
   &:hover {
     background-color: ${p => p.theme.palette.action.hover};
