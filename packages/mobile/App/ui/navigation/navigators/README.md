718aad8d3883523f197c722d73d48a615df98dc7
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2026-09-24 16:49:25 +1200

    refactor(mobile): simplify patient tabs

--- packages/mobile/App/ui/navigation/navigators/README.md
+++
@@ -1,8 +0,0 @@
-# Navigators
-
-The idea to use custom navigators usually happens when we have:
-
-- Common state shared through all screens.
-- Avoid repeating code through screens just to check an edge case
-
-Cases like status bar control does not make sense since it can be controlled via StatusBar API.
--- packages/mobile/App/ui/navigation/stacks/SearchPatientTabs.tsx
+++ packages/mobile/App/ui/navigation/stacks/SearchPatientTabs.tsx
@@ -1,57 +1,95 @@
+import type { NavigationProp } from '@react-navigation/native';
 import React, { type ReactElement } from 'react';
-// Components
-import { RecentViewedScreen, ViewAllScreen } from '../screens/PatientSearch/PatientSearchTabs';
-// Helpers
-import { theme } from '/styled/theme';
-import { Routes } from '/helpers/routes';
-// Navigator
-import { createSearchPatientNavigator } from '../navigators/SearchPatientTabs';
-import { PatientFromRoute } from '~/ui/helpers/constants';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { compose } from 'redux';
 import { useTranslation } from '~/ui/contexts/TranslationContext';
+import { PatientFromRoute } from '~/ui/helpers/constants';
+import { RecentViewedScreen, ViewAllScreen } from '../screens/PatientSearch/PatientSearchTabs';
+import { Field } from '/components/Forms/FormField';
+import { ArrowLeftIcon } from '/components/Icons';
+import { SearchInput } from '/components/SearchInput';
+import { TopTabNavigator, TopTabScreen } from '/components/TopTabNavigator';
+import { withPatient } from '/containers/Patient';
+import { Routes } from '/helpers/routes';
+import { Orientation, screenPercentageToDP } from '/helpers/screen';
+import type { WithPatientStoreProps } from '/store/ducks/patient';
+import { FullView, RowView, StyledSafeAreaView, StyledView } from '/styled/common';
+import { theme } from '/styled/theme';
 
-const Tabs = createSearchPatientNavigator();
+interface SearchPatientHeaderProps {
+  onGoBack: () => void;
+}
 
-export const SearchPatientTabs = ({ routingFrom }): ReactElement => {
+const SearchPatientHeader = ({ onGoBack }: SearchPatientHeaderProps): ReactElement => {
   const { getTranslation } = useTranslation();
 
   return (
-    <Tabs.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: theme.colors.PRIMARY_MAIN,
-        tabBarInactiveTintColor: theme.colors.TEXT_DARK,
-        tabBarLabelStyle: {
-          fontSize: 12,
-          textTransform: 'none',
-        },
-        tabBarIndicatorStyle: {
-          backgroundColor: theme.colors.PRIMARY_MAIN,
-        },
-        tabBarStyle: {
-          height: 50,
-          backgroundColor: theme.colors.WHITE,
-        },
-      }}
-      initialRouteName={
-        routingFrom === PatientFromRoute.ALL_PATIENT
-          ? Routes.HomeStack.SearchPatientStack.SearchPatientTabs.ViewAll
-          : Routes.HomeStack.SearchPatientStack.SearchPatientTabs.RecentViewed
-      }
-    >
-      <Tabs.Screen
-        options={{
-          tabBarLabel: getTranslation('patient.recentlyViewedTab.title', 'Recently viewed'),
-        }}
-        name={Routes.HomeStack.SearchPatientStack.SearchPatientTabs.RecentViewed}
-        component={RecentViewedScreen}
-      />
-      <Tabs.Screen
-        options={{
-          tabBarLabel: getTranslation('patient.allPatient.title', 'All patients'),
-        }}
-        name={Routes.HomeStack.SearchPatientStack.SearchPatientTabs.ViewAll}
-        component={ViewAllScreen}
-      />
-    </Tabs.Navigator>
+    <StyledSafeAreaView background={theme.colors.PRIMARY_MAIN}>
+      <RowView height={90} paddingTop={20} alignItems="center" paddingBottom={20} paddingRight={20}>
+        <TouchableOpacity onPress={onGoBack}>
+          <StyledView paddingLeft={20} paddingTop={20} paddingBottom={20} paddingRight={20}>
+            <ArrowLeftIcon
+              height={screenPercentageToDP(2.43, Orientation.Height)}
+              width={screenPercentageToDP(2.43, Orientation.Height)}
+            />
+          </StyledView>
+        </TouchableOpacity>
+        <StyledView flex={1}>
+          <Field
+            component={SearchInput}
+            name="search"
+            placeholder={getTranslation('patient.search.placeholder', 'Search for patients')}
+          />
+        </StyledView>
+      </RowView>
+    </StyledSafeAreaView>
   );
 };
+
+interface SearchPatientTabsProps extends WithPatientStoreProps {
+  navigation: NavigationProp<any>;
+  routingFrom: (typeof PatientFromRoute)[keyof typeof PatientFromRoute];
+}
+
+const DumbSearchPatientTabs = ({
+  navigation,
+  routingFrom,
+  setSelectedPatient,
+}: SearchPatientTabsProps): ReactElement => {
+  const { getTranslation } = useTranslation();
+
+  const onNavigateToHome = (): void => {
+    setSelectedPatient(null);
+    navigation.navigate(Routes.HomeStack.HomeTabs.Home);
+  };
+
+  return (
+    <FullView>
+      <SearchPatientHeader onGoBack={onNavigateToHome} />
+      <TopTabNavigator
+        initialRouteName={
+          routingFrom === PatientFromRoute.ALL_PATIENT
+            ? Routes.HomeStack.SearchPatientStack.SearchPatientTabs.ViewAll
+            : Routes.HomeStack.SearchPatientStack.SearchPatientTabs.RecentViewed
+        }
+      >
+        <TopTabScreen
+          options={{
+            tabBarLabel: getTranslation('patient.recentlyViewedTab.title', 'Recently viewed'),
+          }}
+          name={Routes.HomeStack.SearchPatientStack.SearchPatientTabs.RecentViewed}
+          component={RecentViewedScreen}
+        />
+        <TopTabScreen
+          options={{
+            tabBarLabel: getTranslation('patient.allPatient.title', 'All patients'),
+          }}
+          name={Routes.HomeStack.SearchPatientStack.SearchPatientTabs.ViewAll}
+          component={ViewAllScreen}
+        />
+      </TopTabNavigator>
+    </FullView>
+  );
+};
+
+export const SearchPatientTabs = compose(withPatient)(DumbSearchPatientTabs);
